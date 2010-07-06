3a5f0d824ee0f7cc980a9a874b67a6d2b01d0c23
ajaxian79 <blaketullysmith@gmail.com>
2010-07-06 13:06:18 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -410,3 +410,4 @@ char* count_and_say(int n) {
 // note 361: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 362: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 363: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 364: Vectorizes cleanly under -O2.
