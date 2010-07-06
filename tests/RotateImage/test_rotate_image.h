52323f604ecd7b569e3e5e82ab42fffd501ac325
ajaxian79 <blaketullysmith@gmail.com>
2010-07-06 08:21:18 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -360,3 +360,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 327: Time complexity: O(n).
 // edge note 328: Sub-linear in the average case thanks to early exit.
 // edge note 329: Runs in a single pass over the input.
+// edge note 330: Thread-safe so long as the input is not mutated concurrently.
