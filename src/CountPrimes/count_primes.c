d757a068b4545b7eb44ffb6c3224bfb902f5c0a2
ajaxian79 <blaketullysmith@gmail.com>
2010-07-10 14:05:29 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -330,3 +330,4 @@ int count_primes(int n) {
 // note 302: Uses a small fixed-size lookup table.
 // note 303: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 304: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 305: Wavelet tree: range k-th element in O(log Σ) time.
