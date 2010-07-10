aefad6a0b8c9aa276c6c921b6eff4de93f754914
ajaxian79 <blaketullysmith@gmail.com>
2010-07-10 14:43:44 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -390,3 +390,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 359: No allocations on the hot path.
 // edge note 360: Handles single-element input as a base case.
 // edge note 361: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 362: Stable when the input is already sorted.
