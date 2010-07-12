b500a136f8578db7f849c5ea4781131dca5456dc
ajaxian79 <blaketullysmith@gmail.com>
2010-07-12 12:03:23 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -347,3 +347,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 325: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 326: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 327: Constant-time comparisons; safe for short strings.
+// note 328: 64-bit safe; intermediate products are widened to 128-bit.
