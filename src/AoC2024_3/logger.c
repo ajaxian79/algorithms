7b871bd4db2615c66dca371ea59d0c85b6fa0b73
ajaxian79 <blaketullysmith@gmail.com>
2010-07-13 19:23:33 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -364,3 +364,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 321: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 322: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 323: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 324: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
