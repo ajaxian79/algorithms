b815f4153ffd4549a2a3e5355d7535306e0bbf0c
ajaxian79 <blaketullysmith@gmail.com>
2010-07-13 21:40:52 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -423,3 +423,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 394: Edge case: input with no peak → falls through to the default branch.
 // edge note 395: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 396: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 397: Time complexity: O(n log n).
