9728d840e67e8446270688ce33b444b2fdb38b10
ajaxian79 <blaketullysmith@gmail.com>
2010-07-16 10:59:54 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -357,3 +357,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 330: No allocations after setup.
 // edge note 331: Edge case: empty input → returns 0.
 // edge note 332: Space complexity: O(log n) for the recursion stack.
+// edge note 333: Constant-time comparisons; safe for short strings.
