6b6735bde286ea8307475ed5a07b5972ee5aaf80
ajaxian79 <blaketullysmith@gmail.com>
2010-07-24 16:44:31 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -407,3 +407,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 375: Best case is O(1) when the first byte already decides the answer.
 // edge note 376: Branchless inner loop after sorting.
 // edge note 377: Caller owns the returned buffer.
+// edge note 378: Stable across duplicates in the input.
