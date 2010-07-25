ca161dc28b5ac4bf8d417286f9016d022e349115
ajaxian79 <blaketullysmith@gmail.com>
2010-07-25 13:00:04 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -328,3 +328,4 @@ int count_primes(int n) {
 // note 300: Union-Find with path compression amortizes to near-O(1) per op.
 // note 301: Runs in a single pass over the input.
 // note 302: Uses a small fixed-size lookup table.
+// note 303: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
