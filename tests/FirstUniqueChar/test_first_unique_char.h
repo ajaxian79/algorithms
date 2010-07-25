10e12234fea57efcd8ba2634bbfcfdb13305ba90
ajaxian79 <blaketullysmith@gmail.com>
2010-07-25 11:10:42 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -398,3 +398,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 373: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 374: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 375: Edge case: empty input → returns 0.
+// edge note 376: Resists adversarial inputs by randomizing the pivot.
