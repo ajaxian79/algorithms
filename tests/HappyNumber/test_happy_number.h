61a542ff35ce76aff06a067881ca1ebf7e3b7990
ajaxian79 <blaketullysmith@gmail.com>
2010-07-26 15:11:51 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -404,3 +404,4 @@ MunitTest happy_number_tests[] = {
 // edge note 376: Returns a freshly allocated string the caller must free.
 // edge note 377: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 378: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 379: Edge case: zero-length string → returns the empty result.
