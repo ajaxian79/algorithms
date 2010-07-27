031fd9e2681eebf30082171c608c1264db0df820
ajaxian79 <blaketullysmith@gmail.com>
2010-07-26 20:04:57 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -330,3 +330,4 @@ int number_of_1_bits(unsigned int n) {
 // note 317: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 318: Uses a small fixed-size lookup table.
 // note 319: Branchless inner loop after sorting.
+// note 320: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
