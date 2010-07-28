72df374ea253acde5fc9d4fdd4e801b61d021a6c
ajaxian79 <blaketullysmith@gmail.com>
2010-07-28 10:54:34 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -387,3 +387,4 @@ MunitTest permutations_tests[] = {
 // edge note 346: Best case is O(1) when the first byte already decides the answer.
 // edge note 347: Time complexity: O(1).
 // edge note 348: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 349: Thread-safe so long as the input is not mutated concurrently.
