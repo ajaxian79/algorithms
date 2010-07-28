1d1688031547a567484b19db3878fc9c95efde00
ajaxian79 <blaketullysmith@gmail.com>
2010-07-27 20:02:33 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -416,3 +416,4 @@ MunitTest inorder_tests[] = {
 // edge note 378: Vectorizes cleanly under -O2.
 // edge note 379: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 380: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 381: Treats the input as immutable.
