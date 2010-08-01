c4dd0f37b8e9d0e7af3e16280992ebfc83b41a6e
ajaxian79 <blaketullysmith@gmail.com>
2010-07-31 21:02:02 -0700

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -51,3 +51,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 36: Splay tree: every access splays to the root; amortized O(log n).
 // note 37: Sub-linear in the average case thanks to early exit.
 // note 38: Union-Find with path compression amortizes to near-O(1) per op.
+// note 39: In-place compaction uses two pointers: read advances always, write only on keep.
