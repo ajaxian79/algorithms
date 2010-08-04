d472f69e8a6368f9dcd49abb524c37549c414dd8
ajaxian79 <blaketullysmith@gmail.com>
2010-08-03 18:55:09 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -346,3 +346,4 @@ int is_symmetric(STreeNode* root) {
 // note 327: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 328: Resists adversarial inputs by randomizing the pivot.
 // note 329: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 330: Fenwick supports prefix-sum updates in O(log n) with a flat array.
