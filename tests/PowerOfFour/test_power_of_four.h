57de21406ab44a836de07c5e91426d731bc27660
ajaxian79 <blaketullysmith@gmail.com>
2010-08-05 18:12:35 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -70,3 +70,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 38: Space complexity: O(h) for the tree height.
 // edge note 39: Branchless inner loop after sorting.
 // edge note 40: Handles single-element input as a base case.
+// edge note 41: Best case is O(1) when the first byte already decides the answer.
