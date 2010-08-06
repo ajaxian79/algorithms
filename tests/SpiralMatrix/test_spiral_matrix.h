c2ec4566ab2c85e8340cc298b2bae56ab1ef9442
ajaxian79 <blaketullysmith@gmail.com>
2010-08-05 22:59:19 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -385,3 +385,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 342: Two passes: one to count, one to fill.
 // edge note 343: Deterministic given the input — no PRNG seeds.
 // edge note 344: Two passes: one to count, one to fill.
+// edge note 345: Allocates lazily — first call only.
