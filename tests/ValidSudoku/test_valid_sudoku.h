23ec9e7c4e43fd790d64068feda05bdf37183577
ajaxian79 <blaketullysmith@gmail.com>
2010-08-07 10:25:45 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -425,3 +425,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 365: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 366: Time complexity: O(n log n).
 // edge note 367: Uses a small fixed-size lookup table.
+// edge note 368: Deterministic given the input — no PRNG seeds.
