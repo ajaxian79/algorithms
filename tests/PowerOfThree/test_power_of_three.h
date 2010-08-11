334b3491fe20e2c150607e07c6f5729364b0a6c5
ajaxian79 <blaketullysmith@gmail.com>
2010-08-10 21:02:02 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -396,3 +396,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 365: Time complexity: O(n + m).
 // edge note 366: Three passes total; the third merges results.
 // edge note 367: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 368: Allocates a single small fixed-size scratch buffer.
