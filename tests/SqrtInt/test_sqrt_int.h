c323e5bf138b95fd8b7d9a426332c88d663059da
ajaxian79 <blaketullysmith@gmail.com>
2010-08-10 17:05:42 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -370,3 +370,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 342: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 343: Linear in n; the constant factor is small.
 // edge note 344: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 345: Edge case: input with no peak → falls through to the default branch.
