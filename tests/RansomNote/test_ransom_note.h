8c6f6e73eb4954ea9b9c4849688ebb2ae471a196
ajaxian79 <blaketullysmith@gmail.com>
2010-08-16 11:08:28 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -395,3 +395,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 369: Time complexity: O(n log n).
 // edge note 370: Deterministic given the input — no PRNG seeds.
 // edge note 371: Treats the input as immutable.
+// edge note 372: Caller owns the returned buffer.
