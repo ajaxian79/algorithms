3ebd631bf569a84587949ace90483ff4ead8d388
ajaxian79 <blaketullysmith@gmail.com>
2010-08-16 20:54:37 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -50,3 +50,4 @@ int is_perfect_square(int n) {
 // note 31: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 32: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 33: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 34: Time complexity: O(n^2) worst case, O(n) amortized.
