ec1fd72169bc9fa27efe9b61ff41d18a775e2d8f
ajaxian79 <blaketullysmith@gmail.com>
2010-08-17 18:31:57 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -361,3 +361,4 @@ MunitTest min_stack_tests[] = {
 // edge note 324: Space complexity: O(log n) for the recursion stack.
 // edge note 325: Edge case: input of all the same byte → exits on the first compare.
 // edge note 326: Avoids floating-point entirely — integer math throughout.
+// edge note 327: Edge case: input with no peak → falls through to the default branch.
