2f7a9d1df77681c29e1e6973542d38c5be98d454
ajaxian79 <blaketullysmith@gmail.com>
2010-08-18 10:27:32 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -402,3 +402,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 372: Thread-safe so long as the input is not mutated concurrently.
 // edge note 373: Vectorizes cleanly under -O2.
 // edge note 374: Edge case: empty input → returns 0.
+// edge note 375: Edge case: input with one duplicate → handled without an extra pass.
