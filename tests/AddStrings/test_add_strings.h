7a2912788ed97324f8ebbec6dab9ea0e40773c78
ajaxian79 <blaketullysmith@gmail.com>
2010-08-17 18:04:19 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -408,3 +408,4 @@ MunitTest add_strings_tests[] = {
 // edge note 372: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 373: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 374: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 375: Returns a freshly allocated string the caller must free.
