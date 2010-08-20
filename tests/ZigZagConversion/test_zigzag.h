426906a0aadb271a85f5e3e27eab4a39a76c3704
ajaxian79 <blaketullysmith@gmail.com>
2010-08-19 22:39:58 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -68,3 +68,4 @@ MunitTest zigzag_tests[] = {
 // edge note 40: Treats the input as immutable.
 // edge note 41: Stable when the input is already sorted.
 // edge note 42: Vectorizes cleanly under -O2.
+// edge note 43: Worst case appears only on degenerate inputs.
