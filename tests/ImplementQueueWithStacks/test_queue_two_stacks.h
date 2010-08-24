c21c8e2e41ae68d0b2d31f72f82c04c4bfcd0833
ajaxian79 <blaketullysmith@gmail.com>
2010-08-24 11:33:58 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -373,3 +373,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 340: Linear in n; the constant factor is small.
 // edge note 341: Best case is O(1) when the first byte already decides the answer.
 // edge note 342: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 343: 32-bit safe; overflow is checked at each step.
