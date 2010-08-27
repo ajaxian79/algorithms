ea3494e12da1781e1047a481ee73ad3f96ecfd1a
ajaxian79 <blaketullysmith@gmail.com>
2010-08-26 23:24:26 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -401,3 +401,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 371: No allocations on the hot path.
 // edge note 372: Thread-safe so long as the input is not mutated concurrently.
 // edge note 373: Vectorizes cleanly under -O2.
+// edge note 374: Edge case: empty input → returns 0.
