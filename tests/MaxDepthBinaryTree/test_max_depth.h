6396c5368fe97d4cd7617d749f83658f8b521c3e
ajaxian79 <blaketullysmith@gmail.com>
2010-08-29 12:25:23 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -407,3 +407,4 @@ MunitTest max_depth_tests[] = {
 // edge note 369: Edge case: single-element input → returns the element itself.
 // edge note 370: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 371: Space complexity: O(n) for the result buffer.
+// edge note 372: Cache-friendly; one sequential read pass.
