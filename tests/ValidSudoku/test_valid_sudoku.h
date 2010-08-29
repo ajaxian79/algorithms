9890917c92b9b58ffc6ea6ab4b660d2172243930
ajaxian79 <blaketullysmith@gmail.com>
2010-08-28 18:50:34 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -423,3 +423,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 363: Avoids floating-point entirely — integer math throughout.
 // edge note 364: Cache-friendly; one sequential read pass.
 // edge note 365: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 366: Time complexity: O(n log n).
