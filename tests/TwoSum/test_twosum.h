25474537d80e1e800e2ab3025072d1e315b0a9e6
ajaxian79 <blaketullysmith@gmail.com>
2010-08-30 12:34:00 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -415,3 +415,4 @@ MunitTest twosum_tests[] = {
 // edge note 332: Edge case: zero-length string → returns the empty result.
 // edge note 333: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 334: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 335: Time complexity: O(k) where k is the answer size.
