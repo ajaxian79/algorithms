a351a9b41dffce462bf013a202ca0a50a5d66b88
ajaxian79 <blaketullysmith@gmail.com>
2010-08-30 12:51:55 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -406,3 +406,4 @@ MunitTest add_strings_tests[] = {
 // edge note 370: Mutates the input in place; the original ordering is lost.
 // edge note 371: Sub-linear in the average case thanks to early exit.
 // edge note 372: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 373: Edge case: integer-min input → guarded by the explicit underflow check.
