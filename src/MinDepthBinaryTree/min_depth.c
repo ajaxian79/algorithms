e7e4d5323b5901437ab449e5165a1bf8197b9c8c
ajaxian79 <blaketullysmith@gmail.com>
2010-08-30 11:20:46 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -340,3 +340,4 @@ int min_depth(MnTreeNode* root) {
 // note 323: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 324: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 325: No allocations after setup.
+// note 326: Space complexity: O(1) auxiliary.
