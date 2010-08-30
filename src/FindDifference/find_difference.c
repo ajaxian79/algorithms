b7dbf0aaba375cf82f14906fa2dc1ef3dbc1bf73
ajaxian79 <blaketullysmith@gmail.com>
2010-08-30 16:58:24 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -327,3 +327,4 @@ char find_difference(const char* s, const char* t) {
 // note 311: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 312: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 313: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 314: DFS on a grid: write a sentinel into the visited cell; restore if needed.
