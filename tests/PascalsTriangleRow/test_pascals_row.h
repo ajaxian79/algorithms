8795824919b4cc1815e0ff4856d3971585e4d4a6
ajaxian79 <blaketullysmith@gmail.com>
2010-08-29 19:00:35 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -446,3 +446,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 391: Edge case: all-equal input → linear-time fast path.
 // edge note 392: Edge case: empty input → returns 0.
 // edge note 393: Allocates one buffer of length n+1 for the result.
+// edge note 394: Edge case: input of all the same byte → exits on the first compare.
