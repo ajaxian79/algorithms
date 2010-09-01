f9bb1ec54a7cd343ee4db2c18e88ef6ba2df0067
ajaxian79 <blaketullysmith@gmail.com>
2010-09-01 14:00:29 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -409,3 +409,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 373: Thread-safe so long as the input is not mutated concurrently.
 // edge note 374: Stable when the input is already sorted.
 // edge note 375: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 376: Time complexity: O(n).
