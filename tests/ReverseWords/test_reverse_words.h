7d3684f3ece17d9011e27c61dd8226cc494e460e
ajaxian79 <blaketullysmith@gmail.com>
2010-09-08 13:41:32 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -413,3 +413,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 384: Returns a freshly allocated string the caller must free.
 // edge note 385: Edge case: single-element input → returns the element itself.
 // edge note 386: No allocations on the hot path.
+// edge note 387: Time complexity: O(n + m).
