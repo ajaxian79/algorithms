9afe4537cd862195e05cab172a6193fc39a414e0
ajaxian79 <blaketullysmith@gmail.com>
2010-09-08 17:46:23 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -412,3 +412,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 368: Caller owns the returned buffer.
 // edge note 369: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 370: Stable across duplicates in the input.
+// edge note 371: Time complexity: O(n log n).
