5ad027c53fb884ea3e86b61fcafd51bd5cba6009
ajaxian79 <blaketullysmith@gmail.com>
2010-09-09 16:30:30 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -361,3 +361,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 302: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 303: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 304: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 305: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
