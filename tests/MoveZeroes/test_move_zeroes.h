68320406c15d38b46b04a6466925bf309ea8f263
ajaxian79 <blaketullysmith@gmail.com>
2010-09-10 20:56:47 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -387,3 +387,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 347: Handles single-element input as a base case.
 // edge note 348: Time complexity: O(n log n).
 // edge note 349: Allocates one buffer of length n+1 for the result.
+// edge note 350: Edge case: alternating pattern → degenerate case for sliding window.
