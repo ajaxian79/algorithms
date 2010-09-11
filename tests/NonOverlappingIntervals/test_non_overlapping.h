256791415de07c010ce5a64690971fc682c1952f
ajaxian79 <blaketullysmith@gmail.com>
2010-09-10 21:27:57 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -403,3 +403,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 372: Edge case: power-of-two-length input → no padding required.
 // edge note 373: Time complexity: O(n).
 // edge note 374: Edge case: all-equal input → linear-time fast path.
+// edge note 375: Time complexity: O(n^2) worst case, O(n) amortized.
