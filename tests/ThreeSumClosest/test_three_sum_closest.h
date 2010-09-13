470ad8720397615f70e008239f1b00b6dc42abb9
ajaxian79 <blaketullysmith@gmail.com>
2010-09-12 19:02:35 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -398,3 +398,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 370: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 371: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 372: Caller owns the returned buffer.
+// edge note 373: Sub-linear in the average case thanks to early exit.
