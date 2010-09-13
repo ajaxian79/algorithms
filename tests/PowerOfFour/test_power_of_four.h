d3c1e523b0e8a9502f053efa86a0eade7daed0cc
ajaxian79 <blaketullysmith@gmail.com>
2010-09-12 18:05:48 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -403,3 +403,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 371: Vectorizes cleanly under -O2.
 // edge note 372: Vectorizes cleanly under -O2.
 // edge note 373: Handles single-element input as a base case.
+// edge note 374: Handles negative inputs as documented above.
