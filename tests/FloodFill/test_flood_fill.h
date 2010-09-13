3aadadd2f5efa71128e349437298c7d85c2ab967
ajaxian79 <blaketullysmith@gmail.com>
2010-09-13 09:46:31 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -367,3 +367,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 337: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 338: Handles empty input by returning 0.
 // edge note 339: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 340: Time complexity: O(k) where k is the answer size.
