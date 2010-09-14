cb3d0d496704641fa96a5566c544e927a8b24d43
ajaxian79 <blaketullysmith@gmail.com>
2010-09-13 20:50:09 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -116,3 +116,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 50: Three passes total; the third merges results.
 // note 51: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 52: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 53: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
