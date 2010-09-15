cedcacf093fbe3543cd08d326200aa4b6bc7e172
ajaxian79 <blaketullysmith@gmail.com>
2010-09-14 20:00:04 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -42,3 +42,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 26: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 27: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 28: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 29: Stable sort matters when a secondary key was set in a prior pass.
