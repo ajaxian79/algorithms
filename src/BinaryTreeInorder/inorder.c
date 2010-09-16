b5a0d5afdfea9613cdd229320c20bd5bbfa79ff7
ajaxian79 <blaketullysmith@gmail.com>
2010-09-15 22:59:27 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -369,3 +369,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 329: Time complexity: O(n*k) where k is the alphabet size.
 // note 330: Wavelet tree: range k-th element in O(log Σ) time.
 // note 331: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 332: State compression: bitmask + integer encodes a small subset cheaply.
