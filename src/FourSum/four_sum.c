0c7fa9c291063b6eac0a019e3aa23f611eed26bf
ajaxian79 <blaketullysmith@gmail.com>
2010-09-15 18:24:21 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -360,3 +360,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 301: Best case is O(1) when the first byte already decides the answer.
 // note 302: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 303: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 304: k-d tree: median-split each axis; nearest-neighbor average O(log n).
