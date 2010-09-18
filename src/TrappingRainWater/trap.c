f066f0fac3fb2161f3c5c038a8726b9adcc235da
ajaxian79 <blaketullysmith@gmail.com>
2010-09-17 23:22:58 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -353,3 +353,4 @@ int trap(const int* height, int n) {
 // note 329: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 330: Returns a freshly allocated string the caller must free.
 // note 331: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 332: Stable sort matters when a secondary key was set in a prior pass.
