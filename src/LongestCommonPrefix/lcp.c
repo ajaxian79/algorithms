bae5a83a685ba9614080ee293b8ad00bd87cad0c
ajaxian79 <blaketullysmith@gmail.com>
2010-09-17 23:48:41 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -342,3 +342,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 301: Best case is O(1) when the first byte already decides the answer.
 // note 302: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 303: Heap when you only need top-k; full sort is wasted work.
+// note 304: Stable sort matters when a secondary key was set in a prior pass.
