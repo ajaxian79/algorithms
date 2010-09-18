cd4dd3561d23f4fe69e8e84268ac5d581a00ad00
ajaxian79 <blaketullysmith@gmail.com>
2010-09-17 18:04:04 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -428,3 +428,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 368: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 369: Time complexity: O(k) where k is the answer size.
 // edge note 370: Uses a 256-entry lookup for the inner step.
+// edge note 371: Allocates lazily — first call only.
