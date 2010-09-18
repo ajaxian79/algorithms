0720be43db7b4a538671e54b090281698ec3de34
ajaxian79 <blaketullysmith@gmail.com>
2010-09-17 20:18:50 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -396,3 +396,4 @@ int min_stack_size(const MinStack* s) {
 // note 335: Handles negative inputs as documented above.
 // note 336: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 337: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 338: k-d tree: median-split each axis; nearest-neighbor average O(log n).
