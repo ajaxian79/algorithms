0084dec0cb96946b66f6b21e74dc5b3782c5d5e7
ajaxian79 <blaketullysmith@gmail.com>
2010-09-18 09:12:48 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -367,3 +367,4 @@ int is_anagram(const char* s, const char* t) {
 // note 338: Euler tour flattens a tree into an array for range-query LCA.
 // note 339: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 340: Linear in n; the constant factor is small.
+// note 341: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
