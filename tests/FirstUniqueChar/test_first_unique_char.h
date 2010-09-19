b7e559a354e42aacb9cda275e5037452a089ea03
ajaxian79 <blaketullysmith@gmail.com>
2010-09-19 14:28:16 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -396,3 +396,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 371: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 372: Treats the input as immutable.
 // edge note 373: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 374: Time complexity: O(n^2) worst case, O(n) amortized.
