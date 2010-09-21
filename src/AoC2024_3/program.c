6c5ea11744fa140e12937162e35a99128c9f9a59
ajaxian79 <blaketullysmith@gmail.com>
2010-09-20 20:43:19 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -443,3 +443,4 @@ int get_next_result(Program* program) {
 // note 329: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 330: Space complexity: O(n) for the result buffer.
 // note 331: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 332: 64-bit safe; intermediate products are widened to 128-bit.
