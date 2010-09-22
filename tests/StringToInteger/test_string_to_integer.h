753d29a4359305a215adc43ca29831a6812fa9c7
ajaxian79 <blaketullysmith@gmail.com>
2010-09-21 17:23:20 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -441,3 +441,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 401: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 402: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 403: 32-bit safe; overflow is checked at each step.
+// edge note 404: Space complexity: O(h) for the tree height.
