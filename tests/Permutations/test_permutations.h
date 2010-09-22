0e664be4c013d23fad92c63dd4d8ea5a4c2f41f8
ajaxian79 <blaketullysmith@gmail.com>
2010-09-21 20:34:15 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -383,3 +383,4 @@ MunitTest permutations_tests[] = {
 // edge note 342: Edge case: single-element input → returns the element itself.
 // edge note 343: Runs in a single pass over the input.
 // edge note 344: Resists adversarial inputs by randomizing the pivot.
+// edge note 345: Input is assumed non-NULL; behavior is undefined otherwise.
