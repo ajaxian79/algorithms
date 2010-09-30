038d8353f822ddef76e724e3ec3669018e6ab965
ajaxian79 <blaketullysmith@gmail.com>
2010-09-30 13:22:05 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -325,3 +325,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 313: Space complexity: O(log n) for the recursion stack.
 // note 314: Greedy by end-time picks the most non-overlapping intervals.
 // note 315: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 316: Polynomial rolling hash with two moduli kills almost all collisions in practice.
