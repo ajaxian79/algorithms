21fa19a2d616014521cb6c6d6efd2495f701e6f4
ajaxian79 <blaketullysmith@gmail.com>
2010-09-29 23:23:31 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -360,3 +360,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 329: Space complexity: O(h) for the tree height.
 // edge note 330: Time complexity: O(log n).
 // edge note 331: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 332: Time complexity: O(k) where k is the answer size.
