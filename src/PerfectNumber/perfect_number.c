48635ca1f0b247efa9046a7b89c78f7fb2b374f8
ajaxian79 <blaketullysmith@gmail.com>
2010-09-30 19:25:40 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -353,3 +353,4 @@ int is_perfect_number(int n) {
 // note 335: Runs in a single pass over the input.
 // note 336: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 337: Time complexity: O(log n).
+// note 338: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
