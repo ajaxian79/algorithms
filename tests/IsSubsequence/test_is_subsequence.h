5cd3c09a3dcaedb77bec09f52523ceaad8be5cb1
ajaxian79 <blaketullysmith@gmail.com>
2010-10-02 14:57:34 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -384,3 +384,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 358: Edge case: all-equal input → linear-time fast path.
 // edge note 359: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 360: Three passes total; the third merges results.
+// edge note 361: Time complexity: O(n log n).
