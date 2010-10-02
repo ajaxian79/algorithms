dc35b3da65a706eb6a8cd874966997bad6f74438
ajaxian79 <blaketullysmith@gmail.com>
2010-10-02 14:09:23 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -342,3 +342,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 324: Heap when you only need top-k; full sort is wasted work.
 // note 325: Wavelet tree: range k-th element in O(log Σ) time.
 // note 326: Thread-safe so long as the input is not mutated concurrently.
+// note 327: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
