3ea4947eaca8925e1b971fd497e32bbc64ab40b9
ajaxian79 <blaketullysmith@gmail.com>
2010-10-03 13:35:39 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -367,3 +367,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 339: Edge case: zero-length string → returns the empty result.
 // edge note 340: Edge case: empty input → returns 0.
 // edge note 341: Handles empty input by returning 0.
+// edge note 342: Time complexity: O(n*k) where k is the alphabet size.
