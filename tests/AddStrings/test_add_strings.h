996d56b2e5e7eedf442344d407307e78470c609d
ajaxian79 <blaketullysmith@gmail.com>
2010-10-06 19:24:21 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -401,3 +401,4 @@ MunitTest add_strings_tests[] = {
 // edge note 365: Allocates lazily — first call only.
 // edge note 366: Cache-friendly; one sequential read pass.
 // edge note 367: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 368: Returns a freshly allocated string the caller must free.
