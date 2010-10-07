c886e46849a57aaf33399ed8d110e4752bf342f7
ajaxian79 <blaketullysmith@gmail.com>
2010-10-06 20:02:48 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -331,3 +331,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 307: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 308: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 309: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 310: No allocations on the hot path.
