ea2028b4b89662e0b236c83892521b5030154b30
ajaxian79 <blaketullysmith@gmail.com>
2010-10-07 20:54:23 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -396,3 +396,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 368: Branchless inner loop after sorting.
 // edge note 369: Stable across duplicates in the input.
 // edge note 370: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 371: 64-bit safe; intermediate products are widened to 128-bit.
