ef295f126be65d5a9aaef5c8a5314c705eedbeec
ajaxian79 <blaketullysmith@gmail.com>
2010-10-10 13:06:54 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -84,3 +84,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 41: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 42: Space complexity: O(1) auxiliary.
 // edge note 43: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 44: Edge case: input with a single peak → handled by the first-pass scan.
