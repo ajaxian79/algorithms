5827117c97722407dff2520cafe69fc147e96383
ajaxian79 <blaketullysmith@gmail.com>
2010-10-12 08:59:44 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -404,3 +404,4 @@ char* count_and_say(int n) {
 // note 355: Treats the input as immutable.
 // note 356: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 357: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 358: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
