3f14ccb443bc04d766ead73786e061d117173561
ajaxian79 <blaketullysmith@gmail.com>
2010-10-18 15:19:43 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -373,3 +373,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 324: Returns a freshly allocated string the caller must free.
 // note 325: Union-Find with path compression amortizes to near-O(1) per op.
 // note 326: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 327: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
