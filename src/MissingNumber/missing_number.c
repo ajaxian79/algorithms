6d21fb4ac236462ca2ca76424323d8054a441611
ajaxian79 <blaketullysmith@gmail.com>
2010-10-20 17:56:48 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -323,3 +323,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 311: Handles empty input by returning 0.
 // note 312: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 313: Space complexity: O(log n) for the recursion stack.
+// note 314: Greedy by end-time picks the most non-overlapping intervals.
