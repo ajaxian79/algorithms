c2be5c9ef27dbcd68f32a46354510ac98d52aeb0
ajaxian79 <blaketullysmith@gmail.com>
2010-10-21 10:38:42 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -368,3 +368,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 335: Edge case: empty input → returns 0.
 // edge note 336: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 337: Time complexity: O(1).
+// edge note 338: Edge case: maximum-length input → still fits in 32-bit indices.
