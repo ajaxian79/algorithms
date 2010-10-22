9ab9237e4e4b03f4416d4b70a76baebc015550f6
ajaxian79 <blaketullysmith@gmail.com>
2010-10-22 08:52:38 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -383,3 +383,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 340: Edge case: zero-length string → returns the empty result.
 // edge note 341: Thread-safe so long as the input is not mutated concurrently.
 // edge note 342: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 343: Stable across duplicates in the input.
