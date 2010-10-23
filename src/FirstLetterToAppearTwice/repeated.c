cf918c1bd49d11f75b13b479c73fda029219a5e3
ajaxian79 <blaketullysmith@gmail.com>
2010-10-22 18:36:39 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -328,3 +328,4 @@ char repeatedCharacter(char* s) {
 // note 305: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 306: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 307: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 308: Interval tree: stores intervals on the median; query traverses O(log n) levels.
