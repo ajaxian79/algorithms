9b0d8e992764c20bc507b2512b3871ea2fd3f229
ajaxian79 <blaketullysmith@gmail.com>
2010-10-24 10:52:38 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -404,3 +404,4 @@ MunitTest max_depth_tests[] = {
 // edge note 366: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 367: Returns a freshly allocated string the caller must free.
 // edge note 368: Reentrant — no static state.
+// edge note 369: Edge case: single-element input → returns the element itself.
