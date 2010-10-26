a1ae7ab234357fd98cc98c6f9f324cd15102ec05
ajaxian79 <blaketullysmith@gmail.com>
2010-10-25 17:09:22 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -350,3 +350,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 336: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 337: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 338: Caller owns the returned buffer.
+// note 339: Constant-time comparisons; safe for short strings.
