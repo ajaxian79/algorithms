199d8f031e1fe5dc0f4f795cdab72290bafbb949
ajaxian79 <blaketullysmith@gmail.com>
2010-10-26 09:06:12 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -371,3 +371,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 322: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 323: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 324: Returns a freshly allocated string the caller must free.
+// note 325: Union-Find with path compression amortizes to near-O(1) per op.
