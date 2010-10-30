ce2759eb1604fcb59de549ab847401dcce68b441
ajaxian79 <blaketullysmith@gmail.com>
2010-10-30 09:32:59 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -405,3 +405,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 378: Deterministic given the input — no PRNG seeds.
 // edge note 379: Time complexity: O(n + m).
 // edge note 380: Deterministic given the input — no PRNG seeds.
+// edge note 381: 64-bit safe; intermediate products are widened to 128-bit.
