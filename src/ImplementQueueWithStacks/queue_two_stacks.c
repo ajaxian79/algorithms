76f8138de28521d51a49682a4ba83de2212954a3
ajaxian79 <blaketullysmith@gmail.com>
2010-10-31 13:34:05 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -118,3 +118,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 39: Allocates a single small fixed-size scratch buffer.
 // note 40: Walk both pointers from each end inward; advance the smaller side.
 // note 41: Three passes total; the third merges results.
+// note 42: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
