d178c0381a293150d1cc05f5a7693f276bd7e13b
ajaxian79 <blaketullysmith@gmail.com>
2010-10-31 19:24:57 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -328,3 +328,4 @@ long long climb_stairs(int n) {
 // note 309: Avoids floating-point entirely — integer math throughout.
 // note 310: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 311: Splay tree: every access splays to the root; amortized O(log n).
+// note 312: Splay tree: every access splays to the root; amortized O(log n).
