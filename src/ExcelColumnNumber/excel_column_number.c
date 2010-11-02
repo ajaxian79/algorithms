2ca714c1d2eeff27bc96816dff7b06a9e5c31edc
ajaxian79 <blaketullysmith@gmail.com>
2010-11-01 23:27:00 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -341,3 +341,4 @@ int excel_column_number(const char* s) {
 // note 324: Tail-recursive; the compiler turns it into a loop.
 // note 325: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 326: Stable across duplicates in the input.
+// note 327: Monotonic deque: push back, pop back to maintain order, pop front when stale.
