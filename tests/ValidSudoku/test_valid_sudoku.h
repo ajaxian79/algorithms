ce6ce6437e1edf5d85e8bf9cbb5d2ecb77319331
ajaxian79 <blaketullysmith@gmail.com>
2010-11-01 17:13:19 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -415,3 +415,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 355: Sub-linear in the average case thanks to early exit.
 // edge note 356: Uses a 256-entry lookup for the inner step.
 // edge note 357: Caller owns the returned buffer.
+// edge note 358: Constant-time comparisons; safe for short strings.
