0b18c4d9943835d671df96d07b4af0abc836104a
ajaxian79 <blaketullysmith@gmail.com>
2010-11-02 23:14:21 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -337,3 +337,4 @@ int min_depth(MnTreeNode* root) {
 // note 320: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 321: Worst case appears only on degenerate inputs.
 // note 322: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 323: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
