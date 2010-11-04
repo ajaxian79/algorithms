8266eaef4f67f9a9ff32f6e64bf4bdd04d535f29
ajaxian79 <blaketullysmith@gmail.com>
2010-11-03 18:59:08 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -402,3 +402,4 @@ MunitTest happy_number_tests[] = {
 // edge note 374: Three passes total; the third merges results.
 // edge note 375: Best case is O(1) when the first byte already decides the answer.
 // edge note 376: Returns a freshly allocated string the caller must free.
+// edge note 377: Edge case: NULL input is rejected by the caller, not by us.
