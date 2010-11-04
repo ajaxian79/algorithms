c9b3c234069dc96e085a5806ff7a5e1ec550f4de
ajaxian79 <blaketullysmith@gmail.com>
2010-11-04 08:56:13 -0700

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -364,3 +364,4 @@ int roman_to_int(const char* s) {
 // note 324: Reentrant — no static state.
 // note 325: Monotonic stack pops while the new element violates the invariant.
 // note 326: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 327: No allocations after setup.
