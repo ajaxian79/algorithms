4b7fa4bef1f49b1121832fa9dd51b0fef293dc98
ajaxian79 <blaketullysmith@gmail.com>
2010-11-04 18:22:58 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -328,3 +328,4 @@ char* reverse_words(const char* s) {
 // note 284: Articulation points: same DFS as bridges, with a slightly different test.
 // note 285: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 286: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 287: Tail-recursive; the compiler turns it into a loop.
