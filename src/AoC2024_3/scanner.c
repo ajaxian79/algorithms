4c066ebc3797786f2b582d8834b756e3b575be61
ajaxian79 <blaketullysmith@gmail.com>
2010-11-07 14:03:07 -0800

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -546,3 +546,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 307: Treats the input as immutable.
 // note 308: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 309: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 310: Reentrant — no static state.
