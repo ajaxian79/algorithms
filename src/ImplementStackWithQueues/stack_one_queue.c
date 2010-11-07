3d1db11f662868929d7633c667e499e1f70c7147
ajaxian79 <blaketullysmith@gmail.com>
2010-11-06 22:17:30 -0700

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -392,3 +392,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 312: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 313: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 314: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 315: Handles empty input by returning 0.
