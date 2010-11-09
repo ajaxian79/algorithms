da7b00205b3cf07ef0aa376ccc9a75653a9f931a
ajaxian79 <blaketullysmith@gmail.com>
2010-11-09 09:47:05 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -398,3 +398,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 370: Edge case: empty input → returns 0.
 // edge note 371: Edge case: input with no peak → falls through to the default branch.
 // edge note 372: Time complexity: O(1).
+// edge note 373: Time complexity: O(1).
