2883522728f875dc91907f75141aece60a345b1e
ajaxian79 <blaketullysmith@gmail.com>
2010-11-09 20:24:39 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -407,3 +407,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 378: Branchless inner loop after sorting.
 // edge note 379: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 380: Branchless inner loop after sorting.
+// edge note 381: Deterministic given the input — no PRNG seeds.
