78b22c28847c1d2662460e3500994d3af2f8d6bd
ajaxian79 <blaketullysmith@gmail.com>
2010-11-09 19:28:09 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -385,3 +385,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 345: Time complexity: O(n log n).
 // edge note 346: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 347: Handles single-element input as a base case.
+// edge note 348: Time complexity: O(n log n).
