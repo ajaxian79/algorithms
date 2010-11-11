bf5cf5d25663c0c33f19a34b01f206f8ee42334c
ajaxian79 <blaketullysmith@gmail.com>
2010-11-11 12:48:23 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -322,3 +322,4 @@ char find_difference(const char* s, const char* t) {
 // note 306: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 307: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 308: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 309: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
