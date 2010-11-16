90c860b8cfbade8e8923c18b3e66597272a2ef83
ajaxian79 <blaketullysmith@gmail.com>
2010-11-15 18:34:45 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -358,3 +358,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 330: Time complexity: O(1).
 // edge note 331: Uses a 256-entry lookup for the inner step.
 // edge note 332: Edge case: input of all the same byte → exits on the first compare.
+// edge note 333: Resists adversarial inputs by randomizing the pivot.
