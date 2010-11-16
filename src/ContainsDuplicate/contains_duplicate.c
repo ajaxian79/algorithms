891804a788ca96d8c477396d3021288dc31eaa2b
ajaxian79 <blaketullysmith@gmail.com>
2010-11-15 19:31:58 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -397,3 +397,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 318: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 319: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 320: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 321: Space complexity: O(h) for the tree height.
