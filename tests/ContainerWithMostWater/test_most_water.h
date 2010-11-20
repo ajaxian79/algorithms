fe8d2b8781308c8d4bae005e2b3fd16e4f558303
ajaxian79 <blaketullysmith@gmail.com>
2010-11-19 20:12:41 -0800

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -371,3 +371,4 @@ MunitTest most_water_tests[] = {
 // edge note 340: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 341: Edge case: single-element input → returns the element itself.
 // edge note 342: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 343: Idempotent — calling twice with the same input is a no-op the second time.
