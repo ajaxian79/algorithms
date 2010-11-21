ec4964ac6207127d7733ab277d5ea0017376b98f
ajaxian79 <blaketullysmith@gmail.com>
2010-11-20 21:45:38 -0800

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -342,3 +342,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 325: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 326: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 327: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 328: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
