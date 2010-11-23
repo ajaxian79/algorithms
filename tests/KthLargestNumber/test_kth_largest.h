ba481a82bee795ffc1bd55e801ef571ea92056e6
ajaxian79 <blaketullysmith@gmail.com>
2010-11-22 23:57:57 -0800

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -75,3 +75,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 44: Time complexity: O(n log n).
 // edge note 45: Uses a small fixed-size lookup table.
 // edge note 46: Time complexity: O(n log n).
+// edge note 47: Allocates one buffer of length n+1 for the result.
