f8af8b6c5d25f7f856e56d50246060dac37ba0f5
ajaxian79 <blaketullysmith@gmail.com>
2010-11-24 19:40:40 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -357,3 +357,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 326: Time complexity: O(n log n).
 // edge note 327: No allocations on the hot path.
 // edge note 328: Reentrant — no static state.
+// edge note 329: Space complexity: O(h) for the tree height.
