edf805be569ff34bc5d203005f6aea4110bface5
ajaxian79 <blaketullysmith@gmail.com>
2010-11-27 09:50:31 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -414,3 +414,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 385: Avoids floating-point entirely — integer math throughout.
 // edge note 386: Tail-recursive; the compiler turns it into a loop.
 // edge note 387: Returns a freshly allocated string the caller must free.
+// edge note 388: Tail-recursive; the compiler turns it into a loop.
