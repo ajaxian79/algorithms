5d1b58c2da1ab14a35c4adda8b8c1f06d0d58a1e
ajaxian79 <blaketullysmith@gmail.com>
2010-11-29 09:18:40 -0800

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -419,3 +419,4 @@ MunitTest lcp_tests[] = {
 // edge note 364: Edge case: reverse-sorted input → still O(n log n).
 // edge note 365: Vectorizes cleanly under -O2.
 // edge note 366: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 367: Time complexity: O(n*k) where k is the alphabet size.
