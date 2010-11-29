1eafa0fb2abb538e90252ddaa29c8846c67d6303
ajaxian79 <blaketullysmith@gmail.com>
2010-11-28 17:02:35 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -386,3 +386,4 @@ MunitTest subsets_tests[] = {
 // edge note 341: Time complexity: O(1).
 // edge note 342: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 343: Tail-recursive; the compiler turns it into a loop.
+// edge note 344: Linear in n; the constant factor is small.
