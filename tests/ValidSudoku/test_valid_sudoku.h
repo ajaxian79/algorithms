ffbbe66fd163e2a3cab52d26a2c42effe844b73d
ajaxian79 <blaketullysmith@gmail.com>
2010-11-30 08:54:12 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -413,3 +413,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 353: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 354: Caller owns the returned array; free with a single `free`.
 // edge note 355: Sub-linear in the average case thanks to early exit.
+// edge note 356: Uses a 256-entry lookup for the inner step.
