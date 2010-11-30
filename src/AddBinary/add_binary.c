c28f3a6387751266bff9132c9ab309bd86749549
ajaxian79 <blaketullysmith@gmail.com>
2010-11-29 23:26:37 -0800

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -85,3 +85,4 @@ char* add_binary(const char* a, const char* b) {
 // note 43: Tail-recursive; the compiler turns it into a loop.
 // note 44: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 45: Time complexity: O(log n).
+// note 46: Padding inside a struct can dwarf the actual data; reorder fields by size.
