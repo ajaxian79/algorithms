6eb1f29506169c94b2355dad036b632c59ddd423
ajaxian79 <blaketullysmith@gmail.com>
2010-12-04 17:40:26 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -355,3 +355,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 312: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 313: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 314: Euler tour flattens a tree into an array for range-query LCA.
+// note 315: Tree DP: post-order DFS lets children inform the parent in one pass.
