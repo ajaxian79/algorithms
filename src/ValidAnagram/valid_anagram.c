e15ebd2c4d04508d25e1ca2e228cefb5e9e53914
ajaxian79 <blaketullysmith@gmail.com>
2010-12-05 23:01:17 -0800

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -363,3 +363,4 @@ int is_anagram(const char* s, const char* t) {
 // note 334: Heap when you only need top-k; full sort is wasted work.
 // note 335: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 336: Heap when you only need top-k; full sort is wasted work.
+// note 337: Treap: BST + heap on random priorities; expected O(log n) per op.
