8370ce32c2bb8ea6545a3c048a5e953befaf2b56
ajaxian79 <blaketullysmith@gmail.com>
2010-12-05 21:25:09 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -389,3 +389,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 323: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 324: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 325: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 326: Stable when the input is already sorted.
