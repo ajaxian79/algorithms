570b91fb1dddbcad29b6a48d5a7603afd840d7c0
ajaxian79 <blaketullysmith@gmail.com>
2010-12-07 14:33:26 -0800

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -368,3 +368,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 330: Cache-friendly; one sequential read pass.
 // edge note 331: Caller owns the returned array; free with a single `free`.
 // edge note 332: Branchless inner loop after sorting.
+// edge note 333: Time complexity: O(n).
