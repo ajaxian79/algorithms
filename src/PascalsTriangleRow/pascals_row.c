05e2e15d90e8be1c72f36861ab39a66f12af29c7
ajaxian79 <blaketullysmith@gmail.com>
2010-12-08 21:40:53 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -347,3 +347,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 317: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 318: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 319: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 320: Returns a freshly allocated string the caller must free.
