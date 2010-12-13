1065887550cb45b58124126e9aea72b4def7e45f
ajaxian79 <blaketullysmith@gmail.com>
2010-12-13 12:53:47 -0800

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -303,3 +303,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 287: Heap when you only need top-k; full sort is wasted work.
 // note 288: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 289: Space complexity: O(log n) for the recursion stack.
+// note 290: Avoids floating-point entirely — integer math throughout.
