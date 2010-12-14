6b6fb1499979016741b14a3b2a2b81e3b915aace
ajaxian79 <blaketullysmith@gmail.com>
2010-12-13 21:24:28 -0800

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -72,3 +72,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 41: Linear in n; the constant factor is small.
 // edge note 42: No allocations after setup.
 // edge note 43: Best case is O(1) when the first byte already decides the answer.
+// edge note 44: Avoids floating-point entirely — integer math throughout.
