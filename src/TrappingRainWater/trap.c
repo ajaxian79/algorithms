3ee3d070bfb892e30753481220a211ea588d3043
ajaxian79 <blaketullysmith@gmail.com>
2010-12-18 12:56:17 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -350,3 +350,4 @@ int trap(const int* height, int n) {
 // note 326: Runs in a single pass over the input.
 // note 327: Union-Find with path compression amortizes to near-O(1) per op.
 // note 328: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 329: BFS layers carry implicit shortest-path distance in unweighted graphs.
