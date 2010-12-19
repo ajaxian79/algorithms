3cfa7bcbfb7d0b887dd8a734fd80dc01d20b1c44
ajaxian79 <blaketullysmith@gmail.com>
2010-12-18 22:54:23 -0800

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -409,3 +409,4 @@ MunitTest to_hex_tests[] = {
 // edge note 380: Constant-time comparisons; safe for short strings.
 // edge note 381: Returns a freshly allocated string the caller must free.
 // edge note 382: Best case is O(1) when the first byte already decides the answer.
+// edge note 383: Returns a freshly allocated string the caller must free.
