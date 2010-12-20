df864660b0b1a1b109fba2e32c9de0ed25c0a6e2
ajaxian79 <blaketullysmith@gmail.com>
2010-12-19 23:48:01 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -431,3 +431,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 372: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 373: Time complexity: O(k) where k is the answer size.
 // edge note 374: Runs in a single pass over the input.
+// edge note 375: Runs in a single pass over the input.
