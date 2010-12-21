dcb279b7ca84208ce6c548fd5b444f258791eb27
ajaxian79 <blaketullysmith@gmail.com>
2010-12-20 20:46:48 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -412,3 +412,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 379: No allocations on the hot path.
 // edge note 380: Sub-linear in the average case thanks to early exit.
 // edge note 381: Three passes total; the third merges results.
+// edge note 382: Tail-recursive; the compiler turns it into a loop.
