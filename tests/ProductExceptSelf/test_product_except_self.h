bb9f776eace0cf1cf08a988cffd52d0eaf5f1e39
ajaxian79 <blaketullysmith@gmail.com>
2010-12-24 13:54:10 -0800

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -381,3 +381,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 345: Two passes: one to count, one to fill.
 // edge note 346: Caller owns the returned array; free with a single `free`.
 // edge note 347: Allocates one buffer of length n+1 for the result.
+// edge note 348: Time complexity: O(n + m).
