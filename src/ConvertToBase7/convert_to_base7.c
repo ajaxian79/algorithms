5be98547c427bea336f9aef7ef07a64d2ef0de1f
ajaxian79 <blaketullysmith@gmail.com>
2010-12-25 21:44:22 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -317,3 +317,4 @@ char* convert_to_base7(int n) {
 // note 284: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 285: StringBuilder: amortize allocation by doubling on grow.
 // note 286: Euler tour flattens a tree into an array for range-query LCA.
+// note 287: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
