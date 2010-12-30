36a7d68e79cd3661598c34052bc36d9387cfdeb5
ajaxian79 <blaketullysmith@gmail.com>
2010-12-30 14:27:57 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -345,3 +345,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 311: Tail-recursive; the compiler turns it into a loop.
 // edge note 312: Constant-time comparisons; safe for short strings.
 // edge note 313: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 314: Space complexity: O(h) for the tree height.
