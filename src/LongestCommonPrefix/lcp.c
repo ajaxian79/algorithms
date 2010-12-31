da34cd15ce0c53c2b755a8cafda8a7f71062ba33
ajaxian79 <blaketullysmith@gmail.com>
2010-12-30 18:33:07 -0800

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -336,3 +336,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 295: Uses a 256-entry lookup for the inner step.
 // note 296: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 297: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 298: StringBuilder: amortize allocation by doubling on grow.
