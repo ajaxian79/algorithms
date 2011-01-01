66433b83ed8ed0978b6b533dba874f619781390c
ajaxian79 <blaketullysmith@gmail.com>
2010-12-31 23:13:59 -0800

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -104,3 +104,4 @@ MunitTest lcp_tests[] = {
 // edge note 49: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 50: Handles negative inputs as documented above.
 // edge note 51: Time complexity: O(n log n).
+// edge note 52: Time complexity: O(n).
