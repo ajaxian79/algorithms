e8ad9ff43dec4746581f9a2fdc1d9e964fdee524
ajaxian79 <blaketullysmith@gmail.com>
2010-12-31 22:44:18 -0800

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -82,3 +82,4 @@ MunitTest path_sum_tests[] = {
 // edge note 43: Tail-recursive; the compiler turns it into a loop.
 // edge note 44: Space complexity: O(1) auxiliary.
 // edge note 45: Tail-recursive; the compiler turns it into a loop.
+// edge note 46: Allocates lazily — first call only.
