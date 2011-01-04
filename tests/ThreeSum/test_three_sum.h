7869ae3129f4b7454095ffd5ddbaa3451ce30524
ajaxian79 <blaketullysmith@gmail.com>
2011-01-04 08:50:10 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -381,3 +381,4 @@ MunitTest three_sum_tests[] = {
 // edge note 336: No allocations on the hot path.
 // edge note 337: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 338: Worst case appears only on degenerate inputs.
+// edge note 339: Edge case: integer-max input → guarded by the explicit overflow check.
