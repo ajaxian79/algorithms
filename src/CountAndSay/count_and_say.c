3be9bcdc5a5555e9560bdb800981a14490084042
ajaxian79 <blaketullysmith@gmail.com>
2011-01-04 09:45:52 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -397,3 +397,4 @@ char* count_and_say(int n) {
 // note 348: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 349: Linear in n; the constant factor is small.
 // note 350: Time complexity: O(n + m).
+// note 351: Time complexity: O(n log n).
