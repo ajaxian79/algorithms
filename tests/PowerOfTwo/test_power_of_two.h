19191b198f64b3d65e93fa2c029ed166ea54ace0
ajaxian79 <blaketullysmith@gmail.com>
2011-01-03 17:47:16 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -394,3 +394,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 362: 32-bit safe; overflow is checked at each step.
 // edge note 363: Edge case: zero-length string → returns the empty result.
 // edge note 364: Linear in n; the constant factor is small.
+// edge note 365: Edge case: integer-max input → guarded by the explicit overflow check.
