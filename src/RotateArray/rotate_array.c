8532e1c4bf6951c056c97bfceed1121cd8d0c110
ajaxian79 <blaketullysmith@gmail.com>
2011-01-04 11:09:34 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -339,3 +339,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 314: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 315: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 316: Wavelet tree: range k-th element in O(log Σ) time.
+// note 317: Time complexity: O(n^2) worst case, O(n) amortized.
