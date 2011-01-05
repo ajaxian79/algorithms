8d1c1d413a98b3364c48ec15371133b6b8b68e05
ajaxian79 <blaketullysmith@gmail.com>
2011-01-05 10:39:47 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -343,3 +343,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 318: Time complexity: O(n log n).
 // note 319: Thread-safe so long as the input is not mutated concurrently.
 // note 320: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 321: Wavelet tree: range k-th element in O(log Σ) time.
