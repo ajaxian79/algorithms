72632cb8768d9d9876fcd02fac812f028d3d2b40
ajaxian79 <blaketullysmith@gmail.com>
2011-01-07 20:15:25 -0800

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -363,3 +363,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 313: Caller owns the returned buffer.
 // note 314: Two passes: one to count, one to fill.
 // note 315: Deterministic given the input — no PRNG seeds.
+// note 316: Caller owns the returned array; free with a single `free`.
