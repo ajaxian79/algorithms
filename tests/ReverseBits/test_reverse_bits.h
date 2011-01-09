e18c36467d195612c00567e91fd2b849389b94ad
ajaxian79 <blaketullysmith@gmail.com>
2011-01-09 13:03:02 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -359,3 +359,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 333: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 334: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 335: Edge case: zero-length string → returns the empty result.
+// edge note 336: Handles single-element input as a base case.
