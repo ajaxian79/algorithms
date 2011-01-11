ed623c9789aed156cad8323b91b0e6ff6807fa95
ajaxian79 <blaketullysmith@gmail.com>
2011-01-10 23:12:34 -0800

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -405,3 +405,4 @@ MunitTest inorder_tests[] = {
 // edge note 367: Time complexity: O(log n).
 // edge note 368: Sub-linear in the average case thanks to early exit.
 // edge note 369: Space complexity: O(h) for the tree height.
+// edge note 370: Edge case: input with one duplicate → handled without an extra pass.
