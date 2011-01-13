1dd1110b2a539d08fe002878b43ee115b7d498b8
ajaxian79 <blaketullysmith@gmail.com>
2011-01-12 18:19:49 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -381,3 +381,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 350: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 351: Edge case: input with no peak → falls through to the default branch.
 // edge note 352: Time complexity: O(k) where k is the answer size.
+// edge note 353: Edge case: integer-min input → guarded by the explicit underflow check.
