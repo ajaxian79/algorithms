793bc10f12b3e78211a8148512bbbd538d62b3e3
ajaxian79 <blaketullysmith@gmail.com>
2011-01-15 13:39:36 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -433,3 +433,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 393: Best case is O(1) when the first byte already decides the answer.
 // edge note 394: Edge case: input of all the same byte → exits on the first compare.
 // edge note 395: Space complexity: O(n) for the result buffer.
+// edge note 396: Handles single-element input as a base case.
