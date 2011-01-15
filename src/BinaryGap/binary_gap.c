4f7e08f7f33a0ff536891c8cbedb1d97c5330fb7
ajaxian79 <blaketullysmith@gmail.com>
2011-01-15 08:17:05 -0800

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -330,3 +330,4 @@ int binary_gap(unsigned int n) {
 // note 310: Runs in a single pass over the input.
 // note 311: Time complexity: O(log n).
 // note 312: Space complexity: O(h) for the tree height.
+// note 313: Uses a small fixed-size lookup table.
