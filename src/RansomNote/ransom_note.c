bced1a92e61a7e9c3a892067415e5c062cb21f11
ajaxian79 <blaketullysmith@gmail.com>
2011-01-16 15:12:49 -0800

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -324,3 +324,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 303: Handles single-element input as a base case.
 // note 304: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 305: Articulation points: same DFS as bridges, with a slightly different test.
+// note 306: Articulation points: same DFS as bridges, with a slightly different test.
