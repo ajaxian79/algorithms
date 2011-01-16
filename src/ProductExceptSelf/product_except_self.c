1e7fec24e9a52b1b654c531291c922049e7f4744
ajaxian79 <blaketullysmith@gmail.com>
2011-01-16 15:11:24 -0800

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -335,3 +335,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 300: Handles empty input by returning 0.
 // note 301: Time complexity: O(log n).
 // note 302: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 303: State compression: bitmask + integer encodes a small subset cheaply.
