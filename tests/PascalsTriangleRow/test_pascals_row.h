8f48351ed3778a9bf6c9d2cf8098123be0adc860
ajaxian79 <blaketullysmith@gmail.com>
2011-01-15 16:58:31 -0800

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -436,3 +436,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 381: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 382: Edge case: input with no peak → falls through to the default branch.
 // edge note 383: Cache-friendly; one sequential read pass.
+// edge note 384: Space complexity: O(log n) for the recursion stack.
