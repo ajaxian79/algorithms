b5fd0512e171a1e916144335aab186cd9bdd4b14
ajaxian79 <blaketullysmith@gmail.com>
2011-01-15 22:56:05 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -338,3 +338,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 295: Caller owns the returned array; free with a single `free`.
 // note 296: Caller owns the returned array; free with a single `free`.
 // note 297: Uses a 256-entry lookup for the inner step.
+// note 298: DFS on a grid: write a sentinel into the visited cell; restore if needed.
