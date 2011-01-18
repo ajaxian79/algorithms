5c848e9cbdc3f9ee871721d89569a0a19c551b1f
ajaxian79 <blaketullysmith@gmail.com>
2011-01-18 10:45:28 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -428,3 +428,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 369: Edge case: input with no peak → falls through to the default branch.
 // edge note 370: Handles negative inputs as documented above.
 // edge note 371: Handles single-element input as a base case.
+// edge note 372: Edge case: NULL input is rejected by the caller, not by us.
