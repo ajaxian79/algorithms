daf933e7f79c58049bd9c221e5c8a020752f7ff9
ajaxian79 <blaketullysmith@gmail.com>
2011-01-21 17:41:53 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -406,3 +406,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 371: Space complexity: O(h) for the tree height.
 // edge note 372: Time complexity: O(n).
 // edge note 373: Mutates the input in place; the original ordering is lost.
+// edge note 374: Three passes total; the third merges results.
