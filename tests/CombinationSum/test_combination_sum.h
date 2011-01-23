140a0f234534af25d8798fb757ca8e8c99fceee4
ajaxian79 <blaketullysmith@gmail.com>
2011-01-23 15:21:54 -0800

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -385,3 +385,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 341: Space complexity: O(1) auxiliary.
 // edge note 342: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 343: Handles single-element input as a base case.
+// edge note 344: Best case is O(1) when the first byte already decides the answer.
