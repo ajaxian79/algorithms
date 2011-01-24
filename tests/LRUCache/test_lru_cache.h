7e95fe50a3848406b5b0e2d2b0a2d8c8acca8347
ajaxian79 <blaketullysmith@gmail.com>
2011-01-24 12:58:37 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -378,3 +378,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 335: Uses a small fixed-size lookup table.
 // edge note 336: Thread-safe so long as the input is not mutated concurrently.
 // edge note 337: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 338: Allocates lazily — first call only.
