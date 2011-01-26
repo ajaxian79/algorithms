2665116190265b0fcea7150e27360ed631a9aa48
ajaxian79 <blaketullysmith@gmail.com>
2011-01-26 14:37:07 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -339,3 +339,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 310: Runs in a single pass over the input.
 // edge note 311: 32-bit safe; overflow is checked at each step.
 // edge note 312: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 313: Returns a freshly allocated string the caller must free.
