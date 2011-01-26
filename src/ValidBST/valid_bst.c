9264697b36344abb5f4c6a35a0a6ff895be2e599
ajaxian79 <blaketullysmith@gmail.com>
2011-01-26 14:16:34 -0800

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -325,3 +325,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 307: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 308: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 309: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 310: Time complexity: O(n + m).
