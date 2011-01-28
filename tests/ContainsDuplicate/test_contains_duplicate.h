830deb2d34027e9ca14b5a27d9182732eee7fdb4
ajaxian79 <blaketullysmith@gmail.com>
2011-01-27 21:32:50 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -376,3 +376,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 333: Handles single-element input as a base case.
 // edge note 334: Worst case appears only on degenerate inputs.
 // edge note 335: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 336: Time complexity: O(n log n).
