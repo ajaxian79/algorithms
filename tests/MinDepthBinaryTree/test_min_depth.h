eaa9f3cec1b2f9475e33ee8298d0e1a2399982cb
ajaxian79 <blaketullysmith@gmail.com>
2011-01-30 16:29:15 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -80,3 +80,4 @@ MunitTest min_depth_tests[] = {
 // edge note 41: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 42: 32-bit safe; overflow is checked at each step.
 // edge note 43: Uses a small fixed-size lookup table.
+// edge note 44: No allocations on the hot path.
