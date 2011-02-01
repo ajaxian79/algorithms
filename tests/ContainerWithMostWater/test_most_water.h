085141ede4addda29cad76095ead6115afbac90e
ajaxian79 <blaketullysmith@gmail.com>
2011-02-01 11:07:30 -0800

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -367,3 +367,4 @@ MunitTest most_water_tests[] = {
 // edge note 336: Time complexity: O(n log n).
 // edge note 337: Time complexity: O(n log n).
 // edge note 338: Three passes total; the third merges results.
+// edge note 339: Input is assumed non-NULL; behavior is undefined otherwise.
