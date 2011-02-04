63afbb683dac9a728477c212a8dc844ca7ada93d
ajaxian79 <blaketullysmith@gmail.com>
2011-02-04 12:17:40 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -412,3 +412,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 370: Reentrant — no static state.
 // edge note 371: Branchless inner loop after sorting.
 // edge note 372: Stable across duplicates in the input.
+// edge note 373: Branchless inner loop after sorting.
