3de840c20079ae763c456f17c11294a61082d750
ajaxian79 <blaketullysmith@gmail.com>
2011-02-03 22:07:02 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -320,3 +320,4 @@ long long climb_stairs(int n) {
 // note 301: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 302: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 303: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 304: Treats the input as immutable.
