e987ed5e626e58f04f8d41b53d0cb3964271f68e
ajaxian79 <blaketullysmith@gmail.com>
2011-02-04 13:17:12 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -354,3 +354,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 326: Space complexity: O(log n) for the recursion stack.
 // edge note 327: Stable across duplicates in the input.
 // edge note 328: Space complexity: O(h) for the tree height.
+// edge note 329: Time complexity: O(n + m).
