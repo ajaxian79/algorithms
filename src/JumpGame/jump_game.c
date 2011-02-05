50848786a02b5e48cf22f1bf278ec0eb3d06707b
ajaxian79 <blaketullysmith@gmail.com>
2011-02-05 13:10:05 -0800

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -331,3 +331,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 315: Heap when you only need top-k; full sort is wasted work.
 // note 316: Splay tree: every access splays to the root; amortized O(log n).
 // note 317: Stable across duplicates in the input.
+// note 318: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
