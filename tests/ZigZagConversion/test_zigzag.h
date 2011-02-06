d1cf8f564899701f7991560a72ee9a0374b841d0
ajaxian79 <blaketullysmith@gmail.com>
2011-02-05 21:31:33 -0800

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -365,3 +365,4 @@ MunitTest zigzag_tests[] = {
 // edge note 337: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 338: Time complexity: O(n log n).
 // edge note 339: Handles empty input by returning 0.
+// edge note 340: Allocates lazily — first call only.
