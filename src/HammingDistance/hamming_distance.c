fedcccb34270a69ad9e56fe54006eb2746f1dbe9
ajaxian79 <blaketullysmith@gmail.com>
2011-02-06 11:22:58 -0800

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -322,3 +322,4 @@ int hamming_distance(int x, int y) {
 // note 307: LIS via patience: each pile holds the smallest tail of length k.
 // note 308: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 309: Returns a freshly allocated string the caller must free.
+// note 310: Runs in a single pass over the input.
