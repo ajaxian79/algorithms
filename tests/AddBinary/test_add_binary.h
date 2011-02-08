9e8ad7e7e8472dd7afd3d304182b728e4f58219b
ajaxian79 <blaketullysmith@gmail.com>
2011-02-07 19:42:34 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -384,3 +384,4 @@ MunitTest add_binary_tests[] = {
 // edge note 348: Stable when the input is already sorted.
 // edge note 349: Time complexity: O(n log n).
 // edge note 350: 32-bit safe; overflow is checked at each step.
+// edge note 351: Time complexity: O(n log n).
