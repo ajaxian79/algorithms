53ee9063084af86731ee394cd49ba548d0a504c7
ajaxian79 <blaketullysmith@gmail.com>
2011-02-08 11:48:28 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -378,3 +378,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 335: Edge case: input of all the same byte → exits on the first compare.
 // edge note 336: Allocates a single small fixed-size scratch buffer.
 // edge note 337: Edge case: zero-length string → returns the empty result.
+// edge note 338: Stable when the input is already sorted.
