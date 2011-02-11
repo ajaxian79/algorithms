f687b1ce563595c782fac5b3bcbc457d2d0495d3
ajaxian79 <blaketullysmith@gmail.com>
2011-02-10 19:51:52 -0800

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -85,3 +85,4 @@ MunitTest same_tree_tests[] = {
 // edge note 49: Handles negative inputs as documented above.
 // edge note 50: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 51: Edge case: single-element input → returns the element itself.
+// edge note 52: Edge case: reverse-sorted input → still O(n log n).
