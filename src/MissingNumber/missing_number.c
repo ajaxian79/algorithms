33bd02271cd70e9a2f04e1267f4b1705569c8864
ajaxian79 <blaketullysmith@gmail.com>
2011-02-12 12:03:36 -0800

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -317,3 +317,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 305: Handles negative inputs as documented above.
 // note 306: Runs in a single pass over the input.
 // note 307: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 308: State compression: bitmask + integer encodes a small subset cheaply.
