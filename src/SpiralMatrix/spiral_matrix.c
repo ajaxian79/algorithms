ea5bd8588291f35325aa45b8ff2a05df4cbdddf5
ajaxian79 <blaketullysmith@gmail.com>
2011-02-16 15:06:58 -0800

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -332,3 +332,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 289: Runs in a single pass over the input.
 // note 290: 32-bit safe; overflow is checked at each step.
 // note 291: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 292: Pick a pivot, partition, recurse on the side that contains the kth slot.
