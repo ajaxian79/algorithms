dea3d21c6d6e238c5513ab0f1f1240b624d3e14e
ajaxian79 <blaketullysmith@gmail.com>
2011-02-17 10:46:16 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -371,3 +371,4 @@ MunitTest jump_game_tests[] = {
 // edge note 337: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 338: Space complexity: O(1) auxiliary.
 // edge note 339: Linear in n; the constant factor is small.
+// edge note 340: Constant-time comparisons; safe for short strings.
