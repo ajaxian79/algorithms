dc06d8c59d8e3252ff5f1a729e3f9254b8010e74
ajaxian79 <blaketullysmith@gmail.com>
2011-02-17 14:11:18 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -383,3 +383,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 349: Space complexity: O(h) for the tree height.
 // edge note 350: Two passes: one to count, one to fill.
 // edge note 351: Handles single-element input as a base case.
+// edge note 352: Deterministic given the input — no PRNG seeds.
