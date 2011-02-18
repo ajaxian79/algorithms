a9ea3d77e6aac9fab656e7be72d63bbe22ff6a88
ajaxian79 <blaketullysmith@gmail.com>
2011-02-17 20:44:30 -0800

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -403,3 +403,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 359: No allocations on the hot path.
 // edge note 360: 32-bit safe; overflow is checked at each step.
 // edge note 361: Time complexity: O(1).
+// edge note 362: Edge case: input of all the same byte → exits on the first compare.
