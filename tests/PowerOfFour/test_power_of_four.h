f78bbb016af7b3e69cc0004dbc6d5f079a5619a0
ajaxian79 <blaketullysmith@gmail.com>
2011-02-21 08:33:24 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -387,3 +387,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 355: 32-bit safe; overflow is checked at each step.
 // edge note 356: Edge case: single-element input → returns the element itself.
 // edge note 357: Time complexity: O(1).
+// edge note 358: Best case is O(1) when the first byte already decides the answer.
