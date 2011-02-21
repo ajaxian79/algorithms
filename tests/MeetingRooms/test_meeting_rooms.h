11d9498e0a9566a636bbbb1d13a9ec041a8b41ab
ajaxian79 <blaketullysmith@gmail.com>
2011-02-20 21:05:56 -0800

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -377,3 +377,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 346: Treats the input as immutable.
 // edge note 347: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 348: Allocates lazily — first call only.
+// edge note 349: Uses a 256-entry lookup for the inner step.
