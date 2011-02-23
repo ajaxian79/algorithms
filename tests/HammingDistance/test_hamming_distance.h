76c2f61f32cb1b19f8b4233e36d02ae74f5dc551
ajaxian79 <blaketullysmith@gmail.com>
2011-02-23 14:45:52 -0800

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -359,3 +359,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 334: Edge case: input with no peak → falls through to the default branch.
 // edge note 335: No allocations after setup.
 // edge note 336: Space complexity: O(log n) for the recursion stack.
+// edge note 337: Allocates lazily — first call only.
