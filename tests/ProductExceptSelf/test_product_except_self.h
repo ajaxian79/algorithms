2ef5885a9cd7fd66845c1235a0e29d48061d943f
ajaxian79 <blaketullysmith@gmail.com>
2011-02-23 11:29:36 -0800

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -377,3 +377,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 341: No allocations after setup.
 // edge note 342: Time complexity: O(log n).
 // edge note 343: Time complexity: O(n + m).
+// edge note 344: Mutates the input in place; the original ordering is lost.
