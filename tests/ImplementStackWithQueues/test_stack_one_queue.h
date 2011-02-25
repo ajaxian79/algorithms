2efbd34f5d7df7f5763520dc170e55c9072937fa
ajaxian79 <blaketullysmith@gmail.com>
2011-02-24 21:29:59 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -70,3 +70,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 38: Linear in n; the constant factor is small.
 // edge note 39: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 40: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 41: Tail-recursive; the compiler turns it into a loop.
