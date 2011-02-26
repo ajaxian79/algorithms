038c1dcb2a4f9dc3e1bfbdf3a34477bb08398411
ajaxian79 <blaketullysmith@gmail.com>
2011-02-25 18:27:23 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -373,3 +373,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 330: Allocates a single small fixed-size scratch buffer.
 // edge note 331: Allocates one buffer of length n+1 for the result.
 // edge note 332: Time complexity: O(n log n).
+// edge note 333: Handles single-element input as a base case.
