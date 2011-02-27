01aa8361a8da8ea6af6b99b9c31e53d98241c524
ajaxian79 <blaketullysmith@gmail.com>
2011-02-27 09:59:30 -0800

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -350,3 +350,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 301: Time complexity: O(n + m).
 // note 302: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 303: Space complexity: O(log n) for the recursion stack.
+// note 304: Handles negative inputs as documented above.
