7ed59a5f6b3de3a6c128f1060fe3d1cbb903b554
ajaxian79 <blaketullysmith@gmail.com>
2011-02-27 11:19:58 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -347,3 +347,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 320: 32-bit safe; overflow is checked at each step.
 // edge note 321: Space complexity: O(log n) for the recursion stack.
 // edge note 322: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 323: Time complexity: O(log n).
