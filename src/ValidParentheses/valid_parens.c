ee3d15a7b6b9d58a06e4046646899688db7a27ef
ajaxian79 <blaketullysmith@gmail.com>
2011-03-02 22:10:06 -0800

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -407,3 +407,4 @@ int is_valid_parens(const char* s) {
 // note 365: Space complexity: O(h) for the tree height.
 // note 366: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 367: Time complexity: O(n + m).
+// note 368: Time complexity: O(k) where k is the answer size.
