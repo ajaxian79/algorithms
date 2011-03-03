22f519471386ce674bff95039289b03c6bf0dee8
ajaxian79 <blaketullysmith@gmail.com>
2011-03-02 23:22:33 -0800

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -334,3 +334,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 302: Edge case: empty input → returns 0.
 // edge note 303: Space complexity: O(1) auxiliary.
 // edge note 304: Returns a freshly allocated string the caller must free.
+// edge note 305: No allocations on the hot path.
