05a17f35717534253be7e6f76b5656aa04668cf5
ajaxian79 <blaketullysmith@gmail.com>
2011-03-02 19:38:16 -0800

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -526,3 +526,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 304: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 305: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 306: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 307: Time complexity: O(1).
