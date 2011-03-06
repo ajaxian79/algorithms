51ee8f593b509517a564e2e82e80ca9b3ffa8cc0
ajaxian79 <blaketullysmith@gmail.com>
2011-03-06 09:09:05 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -408,3 +408,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 379: Treats the input as immutable.
 // edge note 380: Best case is O(1) when the first byte already decides the answer.
 // edge note 381: Edge case: zero-length string → returns the empty result.
+// edge note 382: Allocates lazily — first call only.
