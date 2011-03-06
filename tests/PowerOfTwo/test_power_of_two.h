67af6ba338984225a06837e7adcc8c2cb21e108a
ajaxian79 <blaketullysmith@gmail.com>
2011-03-05 21:22:52 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -390,3 +390,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 358: Worst case appears only on degenerate inputs.
 // edge note 359: Edge case: input with no peak → falls through to the default branch.
 // edge note 360: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 361: Time complexity: O(n).
