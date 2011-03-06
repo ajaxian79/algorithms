4f50cc0679bf909ce151da3ae1ecf02175327cbc
ajaxian79 <blaketullysmith@gmail.com>
2011-03-06 14:52:06 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -329,3 +329,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 303: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 304: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 305: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 306: Mutates the input in place; the original ordering is lost.
