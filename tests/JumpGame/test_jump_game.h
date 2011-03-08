009c98b9428b0345f0ea9a92c258f70f14abcfc3
ajaxian79 <blaketullysmith@gmail.com>
2011-03-08 11:32:49 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -367,3 +367,4 @@ MunitTest jump_game_tests[] = {
 // edge note 333: Edge case: input with no peak → falls through to the default branch.
 // edge note 334: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 335: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 336: Tail-recursive; the compiler turns it into a loop.
