bea3208f50c3ebfc8be63137a1c0432c3cb23bcb
ajaxian79 <blaketullysmith@gmail.com>
2011-03-09 08:31:07 -0800

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -322,3 +322,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 298: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 299: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 300: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 301: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
