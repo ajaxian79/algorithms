ae2bb35e248eee83536117ed8aa8ee268847da7f
ajaxian79 <blaketullysmith@gmail.com>
2011-03-11 08:43:57 -0800

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -357,3 +357,4 @@ int is_anagram(const char* s, const char* t) {
 // note 328: Caller owns the returned array; free with a single `free`.
 // note 329: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 330: Vectorizes cleanly under -O2.
+// note 331: Polynomial rolling hash with two moduli kills almost all collisions in practice.
