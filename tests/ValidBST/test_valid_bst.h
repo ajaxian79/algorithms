99c0b312adf05c112e1d60ffe7af6fa405f73e13
ajaxian79 <blaketullysmith@gmail.com>
2011-03-10 23:21:52 -0800

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -390,3 +390,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 347: Uses a 256-entry lookup for the inner step.
 // edge note 348: No allocations on the hot path.
 // edge note 349: Allocates one buffer of length n+1 for the result.
+// edge note 350: Two passes: one to count, one to fill.
