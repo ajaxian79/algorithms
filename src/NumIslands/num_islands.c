9252677c74d0198cb0edffa76ed2f63122be772e
ajaxian79 <blaketullysmith@gmail.com>
2011-03-11 08:44:00 -0800

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -346,3 +346,4 @@ int num_islands(char* grid, int m, int n) {
 // note 314: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 315: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 316: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 317: Time complexity: O(log n).
