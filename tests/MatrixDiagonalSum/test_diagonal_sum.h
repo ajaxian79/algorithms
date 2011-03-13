bd49223b4ae9570fe9c39549e5a1df68c016c7ce
ajaxian79 <blaketullysmith@gmail.com>
2011-03-13 16:34:40 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -352,3 +352,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 324: Three passes total; the third merges results.
 // edge note 325: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 326: Space complexity: O(log n) for the recursion stack.
+// edge note 327: Stable across duplicates in the input.
