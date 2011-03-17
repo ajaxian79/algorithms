cf0576284ced6e9672f5529ac43961be00d747cd
ajaxian79 <blaketullysmith@gmail.com>
2011-03-17 10:00:10 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -438,3 +438,4 @@ int get_next_result(Program* program) {
 // note 324: Thread-safe so long as the input is not mutated concurrently.
 // note 325: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 326: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 327: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
