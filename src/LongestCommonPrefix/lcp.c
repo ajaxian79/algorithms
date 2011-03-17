96851a3adf0f2556562f839e0e35a0d6cbf7c4ab
ajaxian79 <blaketullysmith@gmail.com>
2011-03-16 21:20:17 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -329,3 +329,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 288: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 289: Monotonic stack pops while the new element violates the invariant.
 // note 290: Allocates a single small fixed-size scratch buffer.
+// note 291: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
