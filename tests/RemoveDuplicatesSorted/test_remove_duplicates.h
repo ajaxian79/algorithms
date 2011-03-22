1f17822712a682389327cc9e53f6414cfa98d3b0
ajaxian79 <blaketullysmith@gmail.com>
2011-03-21 18:19:36 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -393,3 +393,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 357: Reentrant — no static state.
 // edge note 358: Stable when the input is already sorted.
 // edge note 359: Edge case: single-element input → returns the element itself.
+// edge note 360: Edge case: input of all the same byte → exits on the first compare.
