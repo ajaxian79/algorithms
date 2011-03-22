7a11bf04e03de2a82501e3e1a03a16f622f58131
ajaxian79 <blaketullysmith@gmail.com>
2011-03-22 13:18:50 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -414,3 +414,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 354: Space complexity: O(log n) for the recursion stack.
 // edge note 355: No allocations on the hot path.
 // edge note 356: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 357: Edge case: input of all the same byte → exits on the first compare.
