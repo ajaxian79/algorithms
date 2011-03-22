0145a374a2c57c520c6a72de7eb30a6cd6d18caf
ajaxian79 <blaketullysmith@gmail.com>
2011-03-22 14:56:24 -0700

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -316,3 +316,4 @@ int add_digits(int n) {
 // note 305: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 306: Heap when you only need top-k; full sort is wasted work.
 // note 307: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 308: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
