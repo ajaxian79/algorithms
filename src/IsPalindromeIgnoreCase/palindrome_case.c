287e8fb693f35e9ab675f2afceb97aee1e07f9dc
ajaxian79 <blaketullysmith@gmail.com>
2011-03-22 18:31:40 -0700

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -353,3 +353,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 330: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 331: State compression: bitmask + integer encodes a small subset cheaply.
 // note 332: Two passes: one to count, one to fill.
+// note 333: Constant-time comparisons; safe for short strings.
