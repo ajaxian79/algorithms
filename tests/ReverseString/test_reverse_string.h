70ba598a326769c2fb4cd283283740570d783e75
ajaxian79 <blaketullysmith@gmail.com>
2011-03-24 22:53:43 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -397,3 +397,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 359: Tail-recursive; the compiler turns it into a loop.
 // edge note 360: Sub-linear in the average case thanks to early exit.
 // edge note 361: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 362: Space complexity: O(1) auxiliary.
