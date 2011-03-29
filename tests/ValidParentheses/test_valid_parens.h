11c18022ef5dd6d9784bd2e3b9aa2547f43ae5d9
ajaxian79 <blaketullysmith@gmail.com>
2011-03-29 16:37:55 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -401,3 +401,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 366: Tail-recursive; the compiler turns it into a loop.
 // edge note 367: Allocates one buffer of length n+1 for the result.
 // edge note 368: Time complexity: O(log n).
+// edge note 369: Edge case: single-element input → returns the element itself.
