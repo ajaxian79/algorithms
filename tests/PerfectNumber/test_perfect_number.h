b8a7e998b29b0305b5ad4383b568c2004a31b7ef
ajaxian79 <blaketullysmith@gmail.com>
2011-03-29 14:48:52 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -392,3 +392,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 362: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 363: Allocates one buffer of length n+1 for the result.
 // edge note 364: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 365: Two passes: one to count, one to fill.
