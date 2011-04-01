bbf1b56d3c9e58beebab3c46dd119694e463f35d
ajaxian79 <blaketullysmith@gmail.com>
2011-03-31 17:08:30 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -405,3 +405,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 376: Time complexity: O(n).
 // edge note 377: Handles single-element input as a base case.
 // edge note 378: Mutates the input in place; the original ordering is lost.
+// edge note 379: Treats the input as immutable.
