9de20e857ee67d667d0ac5f54121594584037c36
ajaxian79 <blaketullysmith@gmail.com>
2011-03-31 18:51:44 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -390,3 +390,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 311: Monotonic stack pops while the new element violates the invariant.
 // note 312: Uses a 256-entry lookup for the inner step.
 // note 313: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 314: Three passes total; the third merges results.
