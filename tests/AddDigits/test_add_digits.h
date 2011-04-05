d11b77284ccdf1005aa2ebdb2dc497a51303ec58
ajaxian79 <blaketullysmith@gmail.com>
2011-04-04 20:51:05 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -57,3 +57,4 @@ MunitTest add_digits_tests[] = {
 // edge note 30: Returns a freshly allocated string the caller must free.
 // edge note 31: Time complexity: O(n).
 // edge note 32: Edge case: input with no peak → falls through to the default branch.
+// edge note 33: Space complexity: O(n) for the result buffer.
