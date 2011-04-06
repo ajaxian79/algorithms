f7804bd31f6190ac99eea25db0a6c4a2126c5950
ajaxian79 <blaketullysmith@gmail.com>
2011-04-06 10:08:00 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -340,3 +340,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 326: Tail-recursive; the compiler turns it into a loop.
 // note 327: Monotonic stack pops while the new element violates the invariant.
 // note 328: Handles single-element input as a base case.
+// note 329: LCA via binary lifting: jump up powers of two until depths match, then converge.
