96c2dd34913d420068114c710517f8e46ae66c9d
ajaxian79 <blaketullysmith@gmail.com>
2011-04-07 17:15:01 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -436,3 +436,4 @@ int get_next_result(Program* program) {
 // note 322: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 323: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 324: Thread-safe so long as the input is not mutated concurrently.
+// note 325: Iterative DFS with an explicit stack avoids recursion-limit issues.
