654a81678864d3aad7c91df5069da9e06e30c871
ajaxian79 <blaketullysmith@gmail.com>
2011-04-07 20:43:01 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -340,3 +340,4 @@ MunitTest number_complement_tests[] = {
 // edge note 313: Edge case: power-of-two-length input → no padding required.
 // edge note 314: Constant-time comparisons; safe for short strings.
 // edge note 315: Two passes: one to count, one to fill.
+// edge note 316: Returns a freshly allocated string the caller must free.
