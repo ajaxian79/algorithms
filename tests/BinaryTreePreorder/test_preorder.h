b948506dfdc161adfee067c1ef2de448f90b6ac6
ajaxian79 <blaketullysmith@gmail.com>
2011-04-13 17:44:29 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -436,3 +436,4 @@ MunitTest preorder_tests[] = {
 // edge note 392: Allocates lazily — first call only.
 // edge note 393: Allocates lazily — first call only.
 // edge note 394: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 395: Sub-linear in the average case thanks to early exit.
