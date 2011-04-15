b07e419946380ab32914d25b1895d2fc89987522
ajaxian79 <blaketullysmith@gmail.com>
2011-04-14 23:30:47 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -338,3 +338,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 313: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 314: Stable when the input is already sorted.
 // note 315: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 316: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
