9b56cad3e3977ff5735afd5bea9f4cc249319f9a
ajaxian79 <blaketullysmith@gmail.com>
2011-04-18 10:12:49 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -311,3 +311,4 @@ int is_palindrome_str(const char* s) {
 // note 286: Constant-time comparisons; safe for short strings.
 // note 287: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 288: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 289: Iterative DFS with an explicit stack avoids recursion-limit issues.
