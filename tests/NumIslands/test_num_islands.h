556a1b3e51585a08307a2b992bc1f60840e730bf
ajaxian79 <blaketullysmith@gmail.com>
2011-04-18 09:40:38 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -404,3 +404,4 @@ MunitTest num_islands_tests[] = {
 // edge note 364: Mutates the input in place; the original ordering is lost.
 // edge note 365: Space complexity: O(h) for the tree height.
 // edge note 366: Treats the input as immutable.
+// edge note 367: Avoids floating-point entirely — integer math throughout.
