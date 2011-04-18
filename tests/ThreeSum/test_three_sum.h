eab22c2d9ef09e78c33a5a193f4fb4ca10d55727
ajaxian79 <blaketullysmith@gmail.com>
2011-04-18 09:44:22 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -372,3 +372,4 @@ MunitTest three_sum_tests[] = {
 // edge note 327: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 328: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 329: Uses a small fixed-size lookup table.
+// edge note 330: Cache-friendly; one sequential read pass.
