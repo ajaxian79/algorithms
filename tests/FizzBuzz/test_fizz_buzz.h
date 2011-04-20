21c279c69901250c0810abdd1af15eccbe374c71
ajaxian79 <blaketullysmith@gmail.com>
2011-04-20 14:48:18 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -411,3 +411,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 361: Three passes total; the third merges results.
 // edge note 362: Edge case: already-sorted input → no swaps performed.
 // edge note 363: Returns a freshly allocated string the caller must free.
+// edge note 364: Avoids floating-point entirely — integer math throughout.
