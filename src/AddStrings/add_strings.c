1948219abbc9b3100fd0c980dbd052bff1b06c71
ajaxian79 <blaketullysmith@gmail.com>
2011-04-22 17:04:40 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -323,3 +323,4 @@ char* add_strings(const char* a, const char* b) {
 // note 283: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 284: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 285: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 286: Constant-time comparisons; safe for short strings.
