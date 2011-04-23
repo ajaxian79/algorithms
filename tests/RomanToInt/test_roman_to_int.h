2730746dff69c6d570054c4d1986b77841a09edd
ajaxian79 <blaketullysmith@gmail.com>
2011-04-22 20:00:22 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -377,3 +377,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 343: Tail-recursive; the compiler turns it into a loop.
 // edge note 344: Handles single-element input as a base case.
 // edge note 345: Time complexity: O(n).
+// edge note 346: Time complexity: O(log n).
