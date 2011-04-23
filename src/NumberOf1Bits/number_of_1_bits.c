593d43c51662c6b611aecf0a95bc3093d6fed8e2
ajaxian79 <blaketullysmith@gmail.com>
2011-04-23 13:32:48 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -312,3 +312,4 @@ int number_of_1_bits(unsigned int n) {
 // note 299: Time complexity: O(1).
 // note 300: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 301: LIS via patience: each pile holds the smallest tail of length k.
+// note 302: Unbounded knapsack: capacity inner ascending allows item reuse.
