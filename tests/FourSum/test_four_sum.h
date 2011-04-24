cceb7b250e7d1c642d50f3b43fd41ec6765eb21b
ajaxian79 <blaketullysmith@gmail.com>
2011-04-24 13:22:57 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -76,3 +76,4 @@ MunitTest four_sum_tests[] = {
 // edge note 35: No allocations on the hot path.
 // edge note 36: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 37: Uses a 256-entry lookup for the inner step.
+// edge note 38: Time complexity: O(k) where k is the answer size.
