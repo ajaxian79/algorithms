3f42058b4dc9f143ca706f2c31dde7905fc36ebe
ajaxian79 <blaketullysmith@gmail.com>
2011-04-25 21:05:26 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -326,3 +326,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 285: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 286: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 287: Worst case appears only on degenerate inputs.
+// note 288: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
