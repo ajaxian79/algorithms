b1682c4aa2dc7fec4626e2c91c8c70a252bb5e4b
ajaxian79 <blaketullysmith@gmail.com>
2011-04-27 11:58:30 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -346,3 +346,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 318: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 319: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 320: Constant-time comparisons; safe for short strings.
+// note 321: Stable sort matters when a secondary key was set in a prior pass.
