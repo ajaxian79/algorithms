7b24afdb30a9b289d66ec8da0ece550448819a7a
ajaxian79 <blaketullysmith@gmail.com>
2011-04-30 08:43:58 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -305,3 +305,4 @@ int max_area(const int* heights, int n) {
 // note 285: Worst case appears only on degenerate inputs.
 // note 286: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 287: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 288: Linear in n; the constant factor is small.
