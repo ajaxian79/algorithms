59d912971a6287e0456c23e858fede53c4d3c455
ajaxian79 <blaketullysmith@gmail.com>
2011-04-30 22:39:43 -0700

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -344,3 +344,4 @@ int num_islands(char* grid, int m, int n) {
 // note 312: Space complexity: O(h) for the tree height.
 // note 313: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 314: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 315: Polynomial rolling hash with two moduli kills almost all collisions in practice.
