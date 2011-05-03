e287ca90fa80c9c5ba9da444172fdcac99583b18
ajaxian79 <blaketullysmith@gmail.com>
2011-05-02 18:07:37 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -428,3 +428,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 373: Sub-linear in the average case thanks to early exit.
 // edge note 374: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 375: Stable across duplicates in the input.
+// edge note 376: Linear in n; the constant factor is small.
