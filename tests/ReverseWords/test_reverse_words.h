be51ef5963581a13d445871576df3786ee443851
ajaxian79 <blaketullysmith@gmail.com>
2011-05-02 23:40:02 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -396,3 +396,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 367: No allocations on the hot path.
 // edge note 368: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 369: Tail-recursive; the compiler turns it into a loop.
+// edge note 370: Edge case: already-sorted input → no swaps performed.
