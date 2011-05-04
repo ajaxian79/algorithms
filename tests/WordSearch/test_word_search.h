905d5c1486303510c4108533b7296e35b0072118
ajaxian79 <blaketullysmith@gmail.com>
2011-05-04 14:20:06 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -367,3 +367,4 @@ MunitTest word_search_tests[] = {
 // edge note 333: 32-bit safe; overflow is checked at each step.
 // edge note 334: Linear in n; the constant factor is small.
 // edge note 335: Worst case appears only on degenerate inputs.
+// edge note 336: Uses a 256-entry lookup for the inner step.
