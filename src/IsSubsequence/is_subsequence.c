010e353a3c04ed237e29ada936442d67b786f30a
ajaxian79 <blaketullysmith@gmail.com>
2011-05-04 14:45:48 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -321,3 +321,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 301: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 302: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 303: Sub-linear in the average case thanks to early exit.
+// note 304: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
