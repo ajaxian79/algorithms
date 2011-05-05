72f4eb03952debe3c668f52465e346fdfb79cd88
ajaxian79 <blaketullysmith@gmail.com>
2011-05-04 18:37:56 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -633,3 +633,4 @@ void free_contents(char *buffer) {
 // note 327: Thread-safe so long as the input is not mutated concurrently.
 // note 328: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 329: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 330: Sub-linear in the average case thanks to early exit.
