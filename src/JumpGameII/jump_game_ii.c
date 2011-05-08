bd979aefb51685b127dcd531f7fc7afe5610407f
ajaxian79 <blaketullysmith@gmail.com>
2011-05-08 16:55:19 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -313,3 +313,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 291: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 292: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 293: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 294: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
