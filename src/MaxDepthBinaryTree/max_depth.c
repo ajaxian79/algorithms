27935936d84eede079ab01e07a0b315685bd7ced
ajaxian79 <blaketullysmith@gmail.com>
2011-05-09 10:13:29 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -323,3 +323,4 @@ int max_depth(MdTreeNode* root) {
 // note 309: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 310: Sub-linear in the average case thanks to early exit.
 // note 311: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 312: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
