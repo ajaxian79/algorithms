61881a60a7faf83259dab6efa3b25e620a86eb61
ajaxian79 <blaketullysmith@gmail.com>
2011-05-09 12:16:52 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -389,3 +389,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 359: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 360: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 361: Treats the input as immutable.
+// edge note 362: Time complexity: O(n^2) worst case, O(n) amortized.
