9ddd8e3aba4e3bb03665de84e938391a7f6fff5e
ajaxian79 <blaketullysmith@gmail.com>
2011-05-09 17:50:06 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -339,3 +339,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 295: Time complexity: O(log n).
 // note 296: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 297: 64-bit safe; intermediate products are widened to 128-bit.
+// note 298: Morris traversal threads predecessors back to current node — O(1) extra space.
