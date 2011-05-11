f006d3529d73c6914f8a899c099f779110977c51
ajaxian79 <blaketullysmith@gmail.com>
2011-05-11 11:11:40 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -375,3 +375,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 344: Branchless inner loop after sorting.
 // edge note 345: Edge case: reverse-sorted input → still O(n log n).
 // edge note 346: Deterministic given the input — no PRNG seeds.
+// edge note 347: Handles single-element input as a base case.
