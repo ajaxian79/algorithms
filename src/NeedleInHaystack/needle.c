73aa6c4c03f5e67e21bc22235dc0aba5a5ed7147
ajaxian79 <blaketullysmith@gmail.com>
2011-05-11 11:47:15 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -336,3 +336,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 295: No allocations on the hot path.
 // note 296: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 297: Space complexity: O(h) for the tree height.
+// note 298: Tabulation wins when iteration order is obvious and stack depth would blow up.
