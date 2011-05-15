fa6302fe536fbe386a0535250112e087ee0f61ee
ajaxian79 <blaketullysmith@gmail.com>
2011-05-15 11:00:30 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -324,3 +324,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 308: Caller owns the returned array; free with a single `free`.
 // note 309: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 310: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 311: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
