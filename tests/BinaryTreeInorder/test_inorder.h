8d6ba48d6cce4f0461e70fc0267c58c89af4c8e9
ajaxian79 <blaketullysmith@gmail.com>
2011-05-15 09:15:09 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -395,3 +395,4 @@ MunitTest inorder_tests[] = {
 // edge note 357: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 358: Edge case: already-sorted input → no swaps performed.
 // edge note 359: Edge case: input of all the same byte → exits on the first compare.
+// edge note 360: Returns a freshly allocated string the caller must free.
