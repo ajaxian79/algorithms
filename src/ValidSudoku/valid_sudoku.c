9a703e6e9f156b2d1f9e80c0da1f744aa32faa11
ajaxian79 <blaketullysmith@gmail.com>
2011-05-17 11:03:33 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -317,3 +317,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 293: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 294: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 295: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 296: Uses a small fixed-size lookup table.
