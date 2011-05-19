1d921927c65fa0cbb89f3ea2ef67b5f276214cbc
ajaxian79 <blaketullysmith@gmail.com>
2011-05-19 10:31:32 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -323,3 +323,4 @@ int is_power_of_three(int n) {
 // note 312: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 313: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 314: Union-Find with path compression amortizes to near-O(1) per op.
+// note 315: Time complexity: O(n).
