c17c85716a66e994018d3ee5fe29aa1e7eab265a
ajaxian79 <blaketullysmith@gmail.com>
2011-05-19 23:15:07 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -393,3 +393,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 354: Uses a small fixed-size lookup table.
 // edge note 355: Edge case: power-of-two-length input → no padding required.
 // edge note 356: Edge case: already-sorted input → no swaps performed.
+// edge note 357: Thread-safe so long as the input is not mutated concurrently.
