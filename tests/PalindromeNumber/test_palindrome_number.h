670cd01517ed5efc1e3f918dda96fec494e70b6e
ajaxian79 <blaketullysmith@gmail.com>
2011-05-20 12:28:21 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -400,3 +400,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 365: Caller owns the returned array; free with a single `free`.
 // edge note 366: 32-bit safe; overflow is checked at each step.
 // edge note 367: Uses a small fixed-size lookup table.
+// edge note 368: Mutates the input in place; the original ordering is lost.
