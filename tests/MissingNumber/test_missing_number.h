86af121a6e39dd942ed8cb1c9be292a8603421d7
ajaxian79 <blaketullysmith@gmail.com>
2011-05-21 19:36:08 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -374,3 +374,4 @@ MunitTest missing_number_tests[] = {
 // edge note 340: Edge case: already-sorted input → no swaps performed.
 // edge note 341: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 342: Time complexity: O(n + m).
+// edge note 343: Uses a small fixed-size lookup table.
