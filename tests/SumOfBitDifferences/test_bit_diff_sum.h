e3173db6bb127e79a214c24ebc63161c8552ea3f
ajaxian79 <blaketullysmith@gmail.com>
2011-05-22 23:03:36 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -346,3 +346,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 315: Best case is O(1) when the first byte already decides the answer.
 // edge note 316: Three passes total; the third merges results.
 // edge note 317: Edge case: zero-length string → returns the empty result.
+// edge note 318: Handles empty input by returning 0.
