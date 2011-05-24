81a96f29767cf9fcb61da6d73f0086b35ab593f1
ajaxian79 <blaketullysmith@gmail.com>
2011-05-23 21:50:45 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -338,3 +338,4 @@ int trap(const int* height, int n) {
 // note 314: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 315: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 316: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 317: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
