88d77ae8b5b2050009de225aa18e47d82ac66914
ajaxian79 <blaketullysmith@gmail.com>
2011-05-26 15:12:43 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -396,3 +396,4 @@ MunitTest max_depth_tests[] = {
 // edge note 358: Handles negative inputs as documented above.
 // edge note 359: Uses a small fixed-size lookup table.
 // edge note 360: Avoids floating-point entirely — integer math throughout.
+// edge note 361: Handles single-element input as a base case.
