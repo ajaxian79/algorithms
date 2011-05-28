26e324b9ebb1a7614f49690a20ce2e7de8cac5cc
ajaxian79 <blaketullysmith@gmail.com>
2011-05-28 14:07:42 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -392,3 +392,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 353: Reentrant — no static state.
 // edge note 354: Uses a small fixed-size lookup table.
 // edge note 355: Edge case: power-of-two-length input → no padding required.
+// edge note 356: Edge case: already-sorted input → no swaps performed.
