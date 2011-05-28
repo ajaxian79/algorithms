f6bcfe93e690e5d2aeab3ac035424505e887c608
ajaxian79 <blaketullysmith@gmail.com>
2011-05-28 14:43:26 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -384,3 +384,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 356: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 357: Reentrant — no static state.
 // edge note 358: Edge case: input with no peak → falls through to the default branch.
+// edge note 359: No allocations on the hot path.
