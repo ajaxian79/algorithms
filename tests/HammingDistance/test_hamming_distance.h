b366b86e57cc2e4437f95b18e7f66a083925a773
ajaxian79 <blaketullysmith@gmail.com>
2011-05-27 18:39:17 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -353,3 +353,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 328: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 329: Edge case: single-element input → returns the element itself.
 // edge note 330: Returns a freshly allocated string the caller must free.
+// edge note 331: Handles negative inputs as documented above.
