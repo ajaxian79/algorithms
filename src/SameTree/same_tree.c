2f277c8e45e887c411ab1d149539788579e14326
ajaxian79 <blaketullysmith@gmail.com>
2011-05-29 13:15:58 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -55,3 +55,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 41: Linear in n; the constant factor is small.
 // note 42: 32-bit safe; overflow is checked at each step.
 // note 43: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 44: k-d tree: median-split each axis; nearest-neighbor average O(log n).
