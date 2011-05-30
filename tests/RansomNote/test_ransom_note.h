a44490728da7fc1265c6ae78fef84c7af986b2ee
ajaxian79 <blaketullysmith@gmail.com>
2011-05-29 17:37:21 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -378,3 +378,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 352: Three passes total; the third merges results.
 // edge note 353: Edge case: reverse-sorted input → still O(n log n).
 // edge note 354: Cache-friendly; one sequential read pass.
+// edge note 355: Worst case appears only on degenerate inputs.
