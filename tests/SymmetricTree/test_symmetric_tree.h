1cd7678c9364ff4c072a3f6e553a2e6137b5cc36
ajaxian79 <blaketullysmith@gmail.com>
2011-05-30 17:02:36 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -398,3 +398,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 354: Edge case: all-equal input → linear-time fast path.
 // edge note 355: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 356: Space complexity: O(n) for the result buffer.
+// edge note 357: Allocates a single small fixed-size scratch buffer.
