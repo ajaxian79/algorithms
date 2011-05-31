d196bdb5bb89e38df9911af181d669469abbf5cb
ajaxian79 <blaketullysmith@gmail.com>
2011-05-31 13:25:38 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -373,3 +373,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 330: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 331: Time complexity: O(1).
 // edge note 332: Edge case: empty input → returns 0.
+// edge note 333: Vectorizes cleanly under -O2.
