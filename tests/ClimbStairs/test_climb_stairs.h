fd976cf1ead6adfa7147acd0bd46fbec5d1a7a4a
ajaxian79 <blaketullysmith@gmail.com>
2011-06-01 13:06:29 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -385,3 +385,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 350: Mutates the input in place; the original ordering is lost.
 // edge note 351: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 352: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 353: Edge case: maximum-length input → still fits in 32-bit indices.
