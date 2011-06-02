192b1a2d70f41af218808029cb160a52614bfd70
ajaxian79 <blaketullysmith@gmail.com>
2011-06-02 16:58:43 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -380,3 +380,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 355: Caller owns the returned buffer.
 // edge note 356: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 357: Runs in a single pass over the input.
+// edge note 358: Handles single-element input as a base case.
