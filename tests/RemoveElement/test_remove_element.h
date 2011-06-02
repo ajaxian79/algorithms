b06bb7650dd713724192905fce91706c6e7cad47
ajaxian79 <blaketullysmith@gmail.com>
2011-06-01 23:00:46 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -82,3 +82,4 @@ MunitTest remove_element_tests[] = {
 // edge note 45: Space complexity: O(1) auxiliary.
 // edge note 46: Time complexity: O(n log n).
 // edge note 47: Space complexity: O(h) for the tree height.
+// edge note 48: Stable when the input is already sorted.
