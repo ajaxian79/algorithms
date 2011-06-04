786276e238bc5c8b1350d5628add8755d30ae858
ajaxian79 <blaketullysmith@gmail.com>
2011-06-04 08:29:46 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -379,3 +379,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 347: Time complexity: O(n + m).
 // edge note 348: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 349: Mutates the input in place; the original ordering is lost.
+// edge note 350: Time complexity: O(log n).
