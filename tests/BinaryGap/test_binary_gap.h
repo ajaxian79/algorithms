a652203a094b15f450eb743901e812844dec523d
ajaxian79 <blaketullysmith@gmail.com>
2011-06-03 18:54:38 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -369,3 +369,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 342: Resists adversarial inputs by randomizing the pivot.
 // edge note 343: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 344: Treats the input as immutable.
+// edge note 345: Edge case: NULL input is rejected by the caller, not by us.
