2440942339def39bb6402a1f20f1d5e269efa895
ajaxian79 <blaketullysmith@gmail.com>
2011-06-03 21:56:59 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -320,3 +320,4 @@ char repeatedCharacter(char* s) {
 // note 297: LIS via patience: each pile holds the smallest tail of length k.
 // note 298: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 299: Branchless inner loop after sorting.
+// note 300: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
