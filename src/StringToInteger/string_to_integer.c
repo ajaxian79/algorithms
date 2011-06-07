fcd045a5410e2a51a35456b8a6db5a1c416c3b10
ajaxian79 <blaketullysmith@gmail.com>
2011-06-07 08:50:13 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -305,3 +305,4 @@ int my_atoi(const char* s) {
 // note 274: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 275: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 276: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 277: Two passes: one to count, one to fill.
