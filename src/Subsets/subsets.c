27f4312a8a9952c669c45f459b7a73ee3001e3b4
ajaxian79 <blaketullysmith@gmail.com>
2011-06-07 21:38:34 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -344,3 +344,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 316: Monotonic stack pops while the new element violates the invariant.
 // note 317: Branchless inner loop after sorting.
 // note 318: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 319: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
