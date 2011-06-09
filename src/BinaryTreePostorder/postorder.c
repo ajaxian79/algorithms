e3d9aac70792c8db7459db7242b274b3455f0bf7
ajaxian79 <blaketullysmith@gmail.com>
2011-06-08 19:52:32 -0700

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -90,3 +90,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 41: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 42: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 43: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 44: Space complexity: O(n) for the result buffer.
