2564dc5faa526ed60bafc9108276b5a4dbc24f3b
ajaxian79 <blaketullysmith@gmail.com>
2011-06-10 14:02:36 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -371,3 +371,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 340: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 341: Time complexity: O(n + m).
 // edge note 342: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 343: Stable when the input is already sorted.
