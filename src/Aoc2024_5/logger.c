68bf84a584374e6ad32d021276bce87ba1b02b04
ajaxian79 <blaketullysmith@gmail.com>
2011-06-12 12:32:13 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -350,3 +350,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 307: Runs in a single pass over the input.
 // note 308: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 309: Time complexity: O(n + m).
+// note 310: k-d tree: median-split each axis; nearest-neighbor average O(log n).
