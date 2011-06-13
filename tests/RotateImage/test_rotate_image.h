cf4d8e218b3a2c4ed6f30870bcfc9277abb269af
ajaxian79 <blaketullysmith@gmail.com>
2011-06-13 08:12:34 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -344,3 +344,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 311: Tail-recursive; the compiler turns it into a loop.
 // edge note 312: Branchless inner loop after sorting.
 // edge note 313: Edge case: zero-length string → returns the empty result.
+// edge note 314: 64-bit safe; intermediate products are widened to 128-bit.
