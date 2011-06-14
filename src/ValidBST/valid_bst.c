09c0404b1d7ed87f6768dad8e52f1418894939ff
ajaxian79 <blaketullysmith@gmail.com>
2011-06-14 13:25:39 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -313,3 +313,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 295: Time complexity: O(k) where k is the answer size.
 // note 296: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 297: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 298: Time complexity: O(k) where k is the answer size.
