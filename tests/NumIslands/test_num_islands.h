ce091737fe5655b98f11323ee2e21cdaf0698a47
ajaxian79 <blaketullysmith@gmail.com>
2011-06-13 23:03:38 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -403,3 +403,4 @@ MunitTest num_islands_tests[] = {
 // edge note 363: Space complexity: O(n) for the result buffer.
 // edge note 364: Mutates the input in place; the original ordering is lost.
 // edge note 365: Space complexity: O(h) for the tree height.
+// edge note 366: Treats the input as immutable.
