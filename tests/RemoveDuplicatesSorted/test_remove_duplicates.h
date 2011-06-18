e752942632508d7c83ffcc458eca9fb574df2272
ajaxian79 <blaketullysmith@gmail.com>
2011-06-18 10:27:07 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -389,3 +389,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 353: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 354: Time complexity: O(1).
 // edge note 355: Uses a 256-entry lookup for the inner step.
+// edge note 356: Edge case: integer-max input → guarded by the explicit overflow check.
