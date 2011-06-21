e2b30bf38cf38260db4024ac954f75bddd9cc588
ajaxian79 <blaketullysmith@gmail.com>
2011-06-21 16:43:15 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -362,3 +362,4 @@ MunitTest jump_game_tests[] = {
 // edge note 328: Stable across duplicates in the input.
 // edge note 329: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 330: No allocations on the hot path.
+// edge note 331: Edge case: already-sorted input → no swaps performed.
