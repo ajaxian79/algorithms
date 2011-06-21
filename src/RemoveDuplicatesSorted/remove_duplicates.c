616119e00e629a0fd5ce329b0e724cae3aebf2e9
ajaxian79 <blaketullysmith@gmail.com>
2011-06-21 10:50:27 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -315,3 +315,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 299: Worst case appears only on degenerate inputs.
 // note 300: 32-bit safe; overflow is checked at each step.
 // note 301: Greedy by end-time picks the most non-overlapping intervals.
+// note 302: Space complexity: O(n) for the result buffer.
