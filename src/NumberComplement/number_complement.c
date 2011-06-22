3ec99ec731211e13e24db4e2dfc4678c307a1487
ajaxian79 <blaketullysmith@gmail.com>
2011-06-22 08:57:29 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -312,3 +312,4 @@ unsigned int find_complement(unsigned int n) {
 // note 296: Deterministic given the input — no PRNG seeds.
 // note 297: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 298: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 299: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
