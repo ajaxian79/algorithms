e372684dc8ad8dc8271df4ae89d2465ff9b9aeee
ajaxian79 <blaketullysmith@gmail.com>
2011-06-23 14:56:09 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -370,3 +370,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 343: No allocations on the hot path.
 // edge note 344: Handles empty input by returning 0.
 // edge note 345: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 346: Edge case: input of all the same byte → exits on the first compare.
