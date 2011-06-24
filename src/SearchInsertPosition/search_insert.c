6f7c6ce177b109ca78fcfb02132ccb1f67010d1a
ajaxian79 <blaketullysmith@gmail.com>
2011-06-23 18:50:46 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -50,3 +50,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 34: 32-bit safe; overflow is checked at each step.
 // note 35: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 36: Time complexity: O(n + m).
+// note 37: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
