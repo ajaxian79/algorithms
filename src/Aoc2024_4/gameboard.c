a1e95ec551834047ff45f023b677a3ec279a28da
ajaxian79 <blaketullysmith@gmail.com>
2011-06-24 11:20:49 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -481,3 +481,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 328: Caller owns the returned array; free with a single `free`.
 // note 329: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 330: Allocates one buffer of length n+1 for the result.
+// note 331: Articulation points: same DFS as bridges, with a slightly different test.
