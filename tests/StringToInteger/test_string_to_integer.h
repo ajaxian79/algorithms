5df9ada9c7547ad3271f83610f0113bef5756d6b
ajaxian79 <blaketullysmith@gmail.com>
2011-06-24 17:36:22 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -419,3 +419,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 379: No allocations on the hot path.
 // edge note 380: Deterministic given the input — no PRNG seeds.
 // edge note 381: Time complexity: O(n log n).
+// edge note 382: Caller owns the returned buffer.
