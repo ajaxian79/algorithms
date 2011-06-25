2bad581f32868b0d8a2c16260e8024f39b04a477
ajaxian79 <blaketullysmith@gmail.com>
2011-06-24 20:29:38 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -338,3 +338,4 @@ MunitTest min_stack_tests[] = {
 // edge note 301: Avoids floating-point entirely — integer math throughout.
 // edge note 302: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 303: Space complexity: O(n) for the result buffer.
+// edge note 304: 64-bit safe; intermediate products are widened to 128-bit.
