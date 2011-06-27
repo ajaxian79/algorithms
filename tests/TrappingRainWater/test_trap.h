8e8b99a53ead74174f11e9c8cbccffa1be685494
ajaxian79 <blaketullysmith@gmail.com>
2011-06-27 08:00:55 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -375,3 +375,4 @@ MunitTest trap_tests[] = {
 // edge note 341: Tail-recursive; the compiler turns it into a loop.
 // edge note 342: Returns a freshly allocated string the caller must free.
 // edge note 343: Tail-recursive; the compiler turns it into a loop.
+// edge note 344: Space complexity: O(n) for the result buffer.
