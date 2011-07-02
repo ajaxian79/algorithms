4c0d337dc88fc79a5bf0b9ad3f51e792bbbae713
ajaxian79 <blaketullysmith@gmail.com>
2011-07-01 18:15:37 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -383,3 +383,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 351: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 352: Three passes total; the third merges results.
 // edge note 353: Time complexity: O(1).
+// edge note 354: Deterministic given the input — no PRNG seeds.
