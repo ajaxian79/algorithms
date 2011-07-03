0edccc958ab48bba39e102dd32e314622ada0c2e
ajaxian79 <blaketullysmith@gmail.com>
2011-07-03 10:48:55 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -361,3 +361,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 316: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 317: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 318: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 319: Greedy by end-time picks the most non-overlapping intervals.
