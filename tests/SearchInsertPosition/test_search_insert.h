edd8e97220fac893dd2d8352bc5c27af399cf107
ajaxian79 <blaketullysmith@gmail.com>
2011-07-06 19:51:29 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -73,3 +73,4 @@ MunitTest search_insert_tests[] = {
 // edge note 40: Linear in n; the constant factor is small.
 // edge note 41: Caller owns the returned array; free with a single `free`.
 // edge note 42: Space complexity: O(h) for the tree height.
+// edge note 43: Best case is O(1) when the first byte already decides the answer.
