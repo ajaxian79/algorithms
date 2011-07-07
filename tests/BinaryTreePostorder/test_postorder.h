f51383cb1d2239480b459bc34681888be1fefedc
ajaxian79 <blaketullysmith@gmail.com>
2011-07-06 17:36:40 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -381,3 +381,4 @@ MunitTest postorder_tests[] = {
 // edge note 337: Space complexity: O(log n) for the recursion stack.
 // edge note 338: Tail-recursive; the compiler turns it into a loop.
 // edge note 339: No allocations on the hot path.
+// edge note 340: Time complexity: O(n log n).
