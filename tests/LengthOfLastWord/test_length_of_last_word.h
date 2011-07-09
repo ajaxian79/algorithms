2370395339ed2e485d3ffc2befc2ce28e9067a81
ajaxian79 <blaketullysmith@gmail.com>
2011-07-08 17:45:26 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -363,3 +363,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 336: Handles empty input by returning 0.
 // edge note 337: Best case is O(1) when the first byte already decides the answer.
 // edge note 338: Edge case: input of all the same byte → exits on the first compare.
+// edge note 339: Time complexity: O(n*k) where k is the alphabet size.
