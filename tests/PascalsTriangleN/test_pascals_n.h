5915dba65799e7f8d0d8ada5ac77d3af2523b07d
ajaxian79 <blaketullysmith@gmail.com>
2011-07-12 22:42:16 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -417,3 +417,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 358: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 359: Edge case: single-element input → returns the element itself.
 // edge note 360: Allocates lazily — first call only.
+// edge note 361: Edge case: empty input → returns 0.
