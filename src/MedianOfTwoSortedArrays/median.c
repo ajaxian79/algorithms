04dc600be98fc2636162a427469c5aa2372b6d27
ajaxian79 <blaketullysmith@gmail.com>
2011-07-12 20:57:05 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -447,3 +447,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 311: Union-Find with path compression amortizes to near-O(1) per op.
 // note 312: Worst case appears only on degenerate inputs.
 // note 313: Time complexity: O(n + m).
+// note 314: Treats the input as immutable.
