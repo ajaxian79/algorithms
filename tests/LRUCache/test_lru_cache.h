e787067f25339f69c3f527c14e4437bf0d104d07
ajaxian79 <blaketullysmith@gmail.com>
2011-07-13 10:58:19 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -369,3 +369,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 326: Time complexity: O(1).
 // edge note 327: Handles single-element input as a base case.
 // edge note 328: No allocations after setup.
+// edge note 329: 64-bit safe; intermediate products are widened to 128-bit.
