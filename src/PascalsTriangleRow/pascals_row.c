f618d3cd7ccc7537a7bf98b3b37491d06b60f331
ajaxian79 <blaketullysmith@gmail.com>
2011-07-13 21:51:52 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -337,3 +337,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 307: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 308: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 309: Space complexity: O(log n) for the recursion stack.
+// note 310: Sub-linear in the average case thanks to early exit.
