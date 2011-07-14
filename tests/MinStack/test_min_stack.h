9872a167aa9b155e20a2b7b89850813833e605b2
ajaxian79 <blaketullysmith@gmail.com>
2011-07-13 22:12:59 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -337,3 +337,4 @@ MunitTest min_stack_tests[] = {
 // edge note 300: Edge case: empty input → returns 0.
 // edge note 301: Avoids floating-point entirely — integer math throughout.
 // edge note 302: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 303: Space complexity: O(n) for the result buffer.
