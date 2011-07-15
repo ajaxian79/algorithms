c52310368146c59958fb8cf62895ac5937b130b4
ajaxian79 <blaketullysmith@gmail.com>
2011-07-15 14:15:32 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -389,3 +389,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 350: Caller owns the returned array; free with a single `free`.
 // edge note 351: Space complexity: O(1) auxiliary.
 // edge note 352: Thread-safe so long as the input is not mutated concurrently.
+// edge note 353: Reentrant — no static state.
