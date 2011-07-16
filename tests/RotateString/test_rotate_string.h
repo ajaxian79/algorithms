0ca434d4e362ac8c65681e91020d93f5f00b292d
ajaxian79 <blaketullysmith@gmail.com>
2011-07-16 16:53:14 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -341,3 +341,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 314: Edge case: empty input → returns 0.
 // edge note 315: Handles single-element input as a base case.
 // edge note 316: Time complexity: O(k) where k is the answer size.
+// edge note 317: Sub-linear in the average case thanks to early exit.
