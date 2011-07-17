6ba12737ad53491d9bf1217876d8613b5eb71f20
ajaxian79 <blaketullysmith@gmail.com>
2011-07-16 21:25:58 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -314,3 +314,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 290: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 291: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 292: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 293: Morris traversal threads predecessors back to current node — O(1) extra space.
