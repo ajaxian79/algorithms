2a3a0117fc605fb86fda9437b74104aa380aeea0
ajaxian79 <blaketullysmith@gmail.com>
2011-07-16 21:16:36 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -369,3 +369,4 @@ MunitTest three_sum_tests[] = {
 // edge note 324: No allocations on the hot path.
 // edge note 325: Edge case: single-element input → returns the element itself.
 // edge note 326: Edge case: single-element input → returns the element itself.
+// edge note 327: Edge case: maximum-length input → still fits in 32-bit indices.
