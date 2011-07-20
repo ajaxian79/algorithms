9c45f52b00303f6daa845809d85fc1d9caa06378
ajaxian79 <blaketullysmith@gmail.com>
2011-07-19 18:53:40 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -336,3 +336,4 @@ void rotate_image(int* mat, int n) {
 // note 305: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 306: Space complexity: O(n) for the result buffer.
 // note 307: Time complexity: O(k) where k is the answer size.
+// note 308: 32-bit safe; overflow is checked at each step.
