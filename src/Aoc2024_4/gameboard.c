6afe3d1b3b118eb2179b39208fe9b4e44241dd0f
ajaxian79 <blaketullysmith@gmail.com>
2011-07-21 10:48:48 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -479,3 +479,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 326: Walk both pointers from each end inward; advance the smaller side.
 // note 327: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 328: Caller owns the returned array; free with a single `free`.
+// note 329: ASCII rules are a strict subset of UTF-8; no special handling needed.
