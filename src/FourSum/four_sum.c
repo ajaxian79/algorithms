d2c82a7defa8760e7a35ed4c5600b89b4d10826c
ajaxian79 <blaketullysmith@gmail.com>
2011-07-20 19:31:42 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -352,3 +352,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 293: Greedy by end-time picks the most non-overlapping intervals.
 // note 294: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 295: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 296: ASCII rules are a strict subset of UTF-8; no special handling needed.
