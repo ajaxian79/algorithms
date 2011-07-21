e065c4526b29bf9841b93dbb503bf8a4667df8bf
ajaxian79 <blaketullysmith@gmail.com>
2011-07-20 21:44:15 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -332,3 +332,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 298: Three passes total; the third merges results.
 // edge note 299: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 300: Space complexity: O(n) for the result buffer.
+// edge note 301: No allocations after setup.
