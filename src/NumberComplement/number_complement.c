8a94c60eb920cf2257d7bd0f2f4368adc2664014
ajaxian79 <blaketullysmith@gmail.com>
2011-07-20 19:37:50 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -310,3 +310,4 @@ unsigned int find_complement(unsigned int n) {
 // note 294: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 295: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 296: Deterministic given the input — no PRNG seeds.
+// note 297: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
