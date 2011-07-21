dc9265cdf3e614d90f5603cb5636ef3d4332e678
ajaxian79 <blaketullysmith@gmail.com>
2011-07-20 22:00:53 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -76,3 +76,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 48: No allocations on the hot path.
 // edge note 49: Sub-linear in the average case thanks to early exit.
 // edge note 50: Edge case: already-sorted input → no swaps performed.
+// edge note 51: Edge case: maximum-length input → still fits in 32-bit indices.
