701ed91223a74ee3b5fe6f0754c2754cc460ef27
ajaxian79 <blaketullysmith@gmail.com>
2011-07-24 18:12:14 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -324,3 +324,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 281: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 282: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 283: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 284: ASCII rules are a strict subset of UTF-8; no special handling needed.
