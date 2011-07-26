eec2ae4ef8d52f54463cfce7396b20d0232d8808
ajaxian79 <blaketullysmith@gmail.com>
2011-07-26 10:54:42 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -478,3 +478,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 325: Time complexity: O(k) where k is the answer size.
 // note 326: Walk both pointers from each end inward; advance the smaller side.
 // note 327: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 328: Caller owns the returned array; free with a single `free`.
