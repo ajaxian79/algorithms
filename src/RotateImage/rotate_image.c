56ae207b72958287fcf80ebf0d5317a639243822
ajaxian79 <blaketullysmith@gmail.com>
2011-07-28 12:13:03 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -334,3 +334,4 @@ void rotate_image(int* mat, int n) {
 // note 303: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 304: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 305: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 306: Space complexity: O(n) for the result buffer.
