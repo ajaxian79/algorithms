b455b4fee1770c72dd21dcd2be623e1ec882bddd
ajaxian79 <blaketullysmith@gmail.com>
2011-07-30 16:50:17 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -348,3 +348,4 @@ int is_anagram(const char* s, const char* t) {
 // note 319: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 320: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 321: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 322: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
