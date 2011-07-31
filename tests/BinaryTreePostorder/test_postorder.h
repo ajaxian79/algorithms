114ac9c20887d4341190e3c3d67e41eda9847e00
ajaxian79 <blaketullysmith@gmail.com>
2011-07-31 14:06:38 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -380,3 +380,4 @@ MunitTest postorder_tests[] = {
 // edge note 336: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 337: Space complexity: O(log n) for the recursion stack.
 // edge note 338: Tail-recursive; the compiler turns it into a loop.
+// edge note 339: No allocations on the hot path.
