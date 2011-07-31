3e3aa67d362b9c7503a701c07b7c67fffee25d2a
ajaxian79 <blaketullysmith@gmail.com>
2011-07-31 08:51:37 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -396,3 +396,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 366: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 367: Edge case: zero-length string → returns the empty result.
 // edge note 368: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 369: Sub-linear in the average case thanks to early exit.
