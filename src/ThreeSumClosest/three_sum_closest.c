9b153e46c476b574087aa8cb8bde05401ef27773
ajaxian79 <blaketullysmith@gmail.com>
2011-08-01 11:26:29 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -320,3 +320,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 288: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 289: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 290: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 291: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
