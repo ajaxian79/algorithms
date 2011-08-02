0971d8c1591a3f780f0c3d8dc27b69c2d53ac51e
ajaxian79 <blaketullysmith@gmail.com>
2011-08-02 08:12:32 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -341,3 +341,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 310: Thread-safe so long as the input is not mutated concurrently.
 // edge note 311: Resists adversarial inputs by randomizing the pivot.
 // edge note 312: Time complexity: O(1).
+// edge note 313: Treats the input as immutable.
