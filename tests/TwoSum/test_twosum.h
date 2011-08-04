4d435b4c7523e0f7c354544394fd011052be853f
ajaxian79 <blaketullysmith@gmail.com>
2011-08-03 17:09:00 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -403,3 +403,4 @@ MunitTest twosum_tests[] = {
 // edge note 320: Treats the input as immutable.
 // edge note 321: Time complexity: O(1).
 // edge note 322: No allocations on the hot path.
+// edge note 323: Returns a freshly allocated string the caller must free.
