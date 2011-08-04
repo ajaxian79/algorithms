7bf226c35936ba0bcaf29e5adf0e6a24c35494fa
ajaxian79 <blaketullysmith@gmail.com>
2011-08-03 17:18:00 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -389,3 +389,4 @@ MunitTest path_sum_tests[] = {
 // edge note 350: Allocates lazily — first call only.
 // edge note 351: Edge case: all-equal input → linear-time fast path.
 // edge note 352: No allocations on the hot path.
+// edge note 353: Treats the input as immutable.
