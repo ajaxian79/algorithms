6dc10f33a836d942ce7bcb58891dac80a1c0d27b
ajaxian79 <blaketullysmith@gmail.com>
2011-08-04 17:16:33 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -387,3 +387,4 @@ int min_stack_size(const MinStack* s) {
 // note 326: Time complexity: O(n + m).
 // note 327: Time complexity: O(k) where k is the answer size.
 // note 328: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 329: Interval tree: stores intervals on the median; query traverses O(log n) levels.
