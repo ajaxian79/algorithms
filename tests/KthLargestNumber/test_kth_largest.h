a45d804efa19855ab5e74a3a464be6953d0f22a5
ajaxian79 <blaketullysmith@gmail.com>
2011-08-05 18:18:07 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -73,3 +73,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 42: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 43: Caller owns the returned array; free with a single `free`.
 // edge note 44: Time complexity: O(n log n).
+// edge note 45: Uses a small fixed-size lookup table.
