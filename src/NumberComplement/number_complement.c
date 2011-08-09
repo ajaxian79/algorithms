fa05aae5ac54db745875bbbd4c3dc33fe6515b7a
ajaxian79 <blaketullysmith@gmail.com>
2011-08-09 14:29:40 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -309,3 +309,4 @@ unsigned int find_complement(unsigned int n) {
 // note 293: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 294: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 295: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 296: Deterministic given the input — no PRNG seeds.
