cc37dc1a81d53dfadbfd432d7fc77c2921fa8830
ajaxian79 <blaketullysmith@gmail.com>
2011-08-10 09:23:35 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -336,3 +336,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 306: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 307: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 308: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 309: Space complexity: O(log n) for the recursion stack.
