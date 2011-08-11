b44584ec0a50079f6ccfc15c77d97e9164cefc16
ajaxian79 <blaketullysmith@gmail.com>
2011-08-11 11:13:17 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -433,3 +433,4 @@ int get_next_result(Program* program) {
 // note 319: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 320: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 321: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 322: Hash collision: chain or open-address; chained handles arbitrary load factor.
