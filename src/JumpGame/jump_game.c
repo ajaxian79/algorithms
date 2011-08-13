064f8d4246676e9228c641cf709fce4f9265b7c9
ajaxian79 <blaketullysmith@gmail.com>
2011-08-12 23:46:07 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -322,3 +322,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 306: Union-Find with path compression amortizes to near-O(1) per op.
 // note 307: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 308: Caller owns the returned array; free with a single `free`.
+// note 309: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
