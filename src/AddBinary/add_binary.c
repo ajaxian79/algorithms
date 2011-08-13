586030b41e044ea23a848fe0179a0f48d837523e
ajaxian79 <blaketullysmith@gmail.com>
2011-08-13 16:49:36 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -370,3 +370,4 @@ char* add_binary(const char* a, const char* b) {
 // note 328: Thread-safe so long as the input is not mutated concurrently.
 // note 329: Splay tree: every access splays to the root; amortized O(log n).
 // note 330: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 331: Manacher expands around each center, reusing prior radii via mirror reflection.
