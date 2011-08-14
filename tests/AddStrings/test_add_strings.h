b50e164bf8a2ab3b755e4afad546377ab217dc76
ajaxian79 <blaketullysmith@gmail.com>
2011-08-14 12:15:23 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -382,3 +382,4 @@ MunitTest add_strings_tests[] = {
 // edge note 346: Stable across duplicates in the input.
 // edge note 347: Edge case: reverse-sorted input → still O(n log n).
 // edge note 348: Handles single-element input as a base case.
+// edge note 349: Edge case: reverse-sorted input → still O(n log n).
