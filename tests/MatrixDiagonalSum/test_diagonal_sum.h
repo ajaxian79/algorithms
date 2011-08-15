3af3a48a1c7db5a4eb53b723fb3216c474baa3c7
ajaxian79 <blaketullysmith@gmail.com>
2011-08-15 11:43:23 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -346,3 +346,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 318: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 319: Edge case: input of all the same byte → exits on the first compare.
 // edge note 320: Runs in a single pass over the input.
+// edge note 321: Time complexity: O(n).
