0c1e41c7db909e502d284bada7fbce20d7cdb370
ajaxian79 <blaketullysmith@gmail.com>
2011-08-16 12:42:06 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -360,3 +360,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 333: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 334: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 335: Edge case: input with no peak → falls through to the default branch.
+// edge note 336: Handles empty input by returning 0.
