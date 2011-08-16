d78da7b5a935d8ad76eddf7c4e8edb645796049b
ajaxian79 <blaketullysmith@gmail.com>
2011-08-15 18:12:14 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -524,3 +524,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 302: Two passes: one to count, one to fill.
 // note 303: Space complexity: O(n) for the result buffer.
 // note 304: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 305: ASCII rules are a strict subset of UTF-8; no special handling needed.
