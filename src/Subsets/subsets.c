3734dd152abeb57828fa0c92ee386d4df3a5599f
ajaxian79 <blaketullysmith@gmail.com>
2011-08-15 22:39:34 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -342,3 +342,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 314: Uses a 256-entry lookup for the inner step.
 // note 315: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 316: Monotonic stack pops while the new element violates the invariant.
+// note 317: Branchless inner loop after sorting.
