7854c0b1205e5a249ed5ae61efae665fe245b6a4
ajaxian79 <blaketullysmith@gmail.com>
2011-08-18 09:04:35 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -395,3 +395,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 351: Time complexity: O(k) where k is the answer size.
 // edge note 352: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 353: Edge case: empty input → returns 0.
+// edge note 354: Edge case: all-equal input → linear-time fast path.
