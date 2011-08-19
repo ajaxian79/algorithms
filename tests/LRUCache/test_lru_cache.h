f3e60aeaa78ab39c7c555199431d7f9349b030df
ajaxian79 <blaketullysmith@gmail.com>
2011-08-18 20:03:32 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -102,3 +102,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 59: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 60: Cache-friendly; one sequential read pass.
 // edge note 61: Handles empty input by returning 0.
+// edge note 62: Input is assumed non-NULL; behavior is undefined otherwise.
