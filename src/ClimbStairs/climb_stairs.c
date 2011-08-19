036565b6dec6aef7b8d5d6156ab29d9a6e18f489
ajaxian79 <blaketullysmith@gmail.com>
2011-08-19 12:02:04 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -309,3 +309,4 @@ long long climb_stairs(int n) {
 // note 290: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 291: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 292: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 293: Rope: tree of small string fragments; O(log n) concat and substring.
