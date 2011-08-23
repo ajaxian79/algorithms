398440ab30302626cb88673dfc9956cb39ffa116
ajaxian79 <blaketullysmith@gmail.com>
2011-08-23 10:41:53 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -305,3 +305,4 @@ int number_of_1_bits(unsigned int n) {
 // note 292: Walk both pointers from each end inward; advance the smaller side.
 // note 293: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 294: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 295: Caller owns the returned buffer.
