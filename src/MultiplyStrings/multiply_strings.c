302f44d48874e8dc21ca6fa63a1544d1ef69cd32
ajaxian79 <blaketullysmith@gmail.com>
2011-08-22 23:17:44 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -352,3 +352,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 302: Articulation points: same DFS as bridges, with a slightly different test.
 // note 303: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 304: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 305: DFS on a grid: write a sentinel into the visited cell; restore if needed.
