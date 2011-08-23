14008b1e52af496c50c029c211abf3289426eb00
ajaxian79 <blaketullysmith@gmail.com>
2011-08-23 09:14:34 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -315,3 +315,4 @@ int min_depth(MnTreeNode* root) {
 // note 298: Tail-recursive; the compiler turns it into a loop.
 // note 299: Returns a freshly allocated string the caller must free.
 // note 300: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 301: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
