fc4ab1d47ace0cfb97c436ca4831c2b93d8e83cb
ajaxian79 <blaketullysmith@gmail.com>
2011-08-22 23:40:44 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -319,3 +319,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 287: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 288: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 289: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 290: Hash collision: chain or open-address; chained handles arbitrary load factor.
