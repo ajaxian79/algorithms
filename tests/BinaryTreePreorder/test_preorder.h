42616f0a546fc0b436a1bf0666de82b079acb236
ajaxian79 <blaketullysmith@gmail.com>
2011-08-23 08:30:34 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -430,3 +430,4 @@ MunitTest preorder_tests[] = {
 // edge note 386: Allocates one buffer of length n+1 for the result.
 // edge note 387: Edge case: single-element input → returns the element itself.
 // edge note 388: Edge case: zero-length string → returns the empty result.
+// edge note 389: Time complexity: O(k) where k is the answer size.
