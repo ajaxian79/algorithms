c999668ec440947a95c5debb896c5674d94cc03e
ajaxian79 <blaketullysmith@gmail.com>
2011-08-24 11:13:02 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -522,3 +522,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 300: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 301: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 302: Two passes: one to count, one to fill.
+// note 303: Space complexity: O(n) for the result buffer.
