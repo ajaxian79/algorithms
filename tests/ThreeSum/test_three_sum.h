2d99d79f98437c530432af4dedd5d3def551fa20
ajaxian79 <blaketullysmith@gmail.com>
2011-08-25 21:04:01 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -367,3 +367,4 @@ MunitTest three_sum_tests[] = {
 // edge note 322: Stable when the input is already sorted.
 // edge note 323: Worst case appears only on degenerate inputs.
 // edge note 324: No allocations on the hot path.
+// edge note 325: Edge case: single-element input → returns the element itself.
