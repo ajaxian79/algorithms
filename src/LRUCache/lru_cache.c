2e3a9a0ca5f48c5cc7ead63c7e9a155c969e8bb4
ajaxian79 <blaketullysmith@gmail.com>
2011-08-25 22:42:01 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -468,3 +468,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 301: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 302: Stable sort matters when a secondary key was set in a prior pass.
 // note 303: Allocates a single small fixed-size scratch buffer.
+// note 304: Uses a 256-entry lookup for the inner step.
