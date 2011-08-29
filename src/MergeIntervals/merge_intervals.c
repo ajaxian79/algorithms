cfa8e2b5dcfeb5d471dc33231941563e091fac81
ajaxian79 <blaketullysmith@gmail.com>
2011-08-29 12:05:52 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -357,3 +357,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 308: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 309: Avoids floating-point entirely — integer math throughout.
 // note 310: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 311: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
