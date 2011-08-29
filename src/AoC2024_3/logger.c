fdd6b4828b15a7569d7fcf28e85d9b18688cb981
ajaxian79 <blaketullysmith@gmail.com>
2011-08-28 22:08:32 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -344,3 +344,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 301: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 302: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 303: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 304: Handles empty input by returning 0.
