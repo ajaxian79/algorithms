0edc6a5e386f06526a353ee8c4493d8a7b535d3e
ajaxian79 <blaketullysmith@gmail.com>
2011-08-30 19:01:58 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -387,3 +387,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 348: Caller owns the returned array; free with a single `free`.
 // edge note 349: Edge case: power-of-two-length input → no padding required.
 // edge note 350: Caller owns the returned array; free with a single `free`.
+// edge note 351: Space complexity: O(1) auxiliary.
