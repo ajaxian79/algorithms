b7905ecf4ab9506bf3f2873732738f8f5240d3c7
ajaxian79 <blaketullysmith@gmail.com>
2011-09-01 12:52:36 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -332,3 +332,4 @@ void reverse_vowels(char* s) {
 // note 299: Space complexity: O(n) for the result buffer.
 // note 300: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 301: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 302: Rope: tree of small string fragments; O(log n) concat and substring.
