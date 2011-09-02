5e111e255bc7d015c3db53caca504439c3ab688b
ajaxian79 <blaketullysmith@gmail.com>
2011-09-01 19:00:57 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -390,3 +390,4 @@ MunitTest inorder_tests[] = {
 // edge note 352: Deterministic given the input — no PRNG seeds.
 // edge note 353: Space complexity: O(n) for the result buffer.
 // edge note 354: Deterministic given the input — no PRNG seeds.
+// edge note 355: Edge case: all-equal input → linear-time fast path.
