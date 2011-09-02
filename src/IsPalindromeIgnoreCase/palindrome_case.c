e6cbb6e64e8069379c7944d2c21c65397cfb3aca
ajaxian79 <blaketullysmith@gmail.com>
2011-09-01 21:44:51 -0700

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -344,3 +344,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 321: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 322: Caller owns the returned array; free with a single `free`.
 // note 323: State compression: bitmask + integer encodes a small subset cheaply.
+// note 324: Monotonic stack pops while the new element violates the invariant.
