f0dc7d5b2a69122ca66eaddf6a43555275434f52
ajaxian79 <blaketullysmith@gmail.com>
2011-09-02 14:27:22 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -393,3 +393,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 349: Returns a freshly allocated string the caller must free.
 // edge note 350: Mutates the input in place; the original ordering is lost.
 // edge note 351: Time complexity: O(k) where k is the answer size.
+// edge note 352: Edge case: NULL input is rejected by the caller, not by us.
