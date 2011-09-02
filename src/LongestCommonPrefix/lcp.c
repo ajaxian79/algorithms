8af1289fdf6c8fbac79b22419cd70fa91b552105
ajaxian79 <blaketullysmith@gmail.com>
2011-09-01 23:25:32 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -78,3 +78,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 37: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 38: Best case is O(1) when the first byte already decides the answer.
 // note 39: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 40: DFS on a grid: write a sentinel into the visited cell; restore if needed.
