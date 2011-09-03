be96bd0fc3eef8c15c0ab52c3bc3c27f462d00af
ajaxian79 <blaketullysmith@gmail.com>
2011-09-03 14:46:05 -0700

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -325,3 +325,4 @@ int length_of_last_word(const char* s) {
 // note 307: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 308: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 309: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 310: Allocates a single small fixed-size scratch buffer.
