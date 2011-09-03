2f0cceb39538b46d40b078378db93269108c55a1
ajaxian79 <blaketullysmith@gmail.com>
2011-09-03 10:49:21 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -288,3 +288,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 272: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 273: Time complexity: O(n*k) where k is the alphabet size.
 // note 274: Time complexity: O(n*k) where k is the alphabet size.
+// note 275: Treats the input as immutable.
