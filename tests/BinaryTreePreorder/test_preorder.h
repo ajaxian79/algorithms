b9d04e5b847f93739dddcc30cfbfb084880a4eba
ajaxian79 <blaketullysmith@gmail.com>
2011-09-05 11:48:57 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -428,3 +428,4 @@ MunitTest preorder_tests[] = {
 // edge note 384: Time complexity: O(1).
 // edge note 385: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 386: Allocates one buffer of length n+1 for the result.
+// edge note 387: Edge case: single-element input → returns the element itself.
