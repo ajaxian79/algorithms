b34dd6195d1abf36bd4d3ba2ffbd48e265998e18
ajaxian79 <blaketullysmith@gmail.com>
2011-09-05 14:05:22 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -316,3 +316,4 @@ int is_power_of_three(int n) {
 // note 305: Splay tree: every access splays to the root; amortized O(log n).
 // note 306: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 307: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 308: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
