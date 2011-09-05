0699f5afe37bea73485f72b0e551f3573ee4a3d6
ajaxian79 <blaketullysmith@gmail.com>
2011-09-05 10:16:58 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -361,3 +361,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 335: Deterministic given the input — no PRNG seeds.
 // edge note 336: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 337: Allocates one buffer of length n+1 for the result.
+// edge note 338: Edge case: all-equal input → linear-time fast path.
