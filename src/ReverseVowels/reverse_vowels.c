6d29b87e0499bdfeede183193c341867fa5329c7
ajaxian79 <blaketullysmith@gmail.com>
2011-09-09 11:30:58 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -331,3 +331,4 @@ void reverse_vowels(char* s) {
 // note 298: Caller owns the returned array; free with a single `free`.
 // note 299: Space complexity: O(n) for the result buffer.
 // note 300: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 301: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
