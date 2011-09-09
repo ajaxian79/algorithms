8ba1f12a5980b386280c43c56c0103b62fcd5c16
ajaxian79 <blaketullysmith@gmail.com>
2011-09-09 15:06:44 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -320,3 +320,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 304: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 305: Deterministic given the input — no PRNG seeds.
 // note 306: Union-Find with path compression amortizes to near-O(1) per op.
+// note 307: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
