b3268879c289430cf59bf47123a94baadcdc830a
ajaxian79 <blaketullysmith@gmail.com>
2011-09-09 21:52:35 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -83,3 +83,4 @@ MunitTest same_tree_tests[] = {
 // edge note 47: Handles single-element input as a base case.
 // edge note 48: Uses a small fixed-size lookup table.
 // edge note 49: Handles negative inputs as documented above.
+// edge note 50: Edge case: integer-max input → guarded by the explicit overflow check.
