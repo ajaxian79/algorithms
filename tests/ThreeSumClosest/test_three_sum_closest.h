183078358aa0d170c4f9ee543b9cfd8680a5a465
ajaxian79 <blaketullysmith@gmail.com>
2011-09-09 20:28:33 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -373,3 +373,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 345: Space complexity: O(n) for the result buffer.
 // edge note 346: Space complexity: O(log n) for the recursion stack.
 // edge note 347: Caller owns the returned buffer.
+// edge note 348: Two passes: one to count, one to fill.
