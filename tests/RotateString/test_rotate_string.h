7f5f373d443e3cd9f93ca49395640fc4a0536b69
ajaxian79 <blaketullysmith@gmail.com>
2011-09-10 14:08:44 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -339,3 +339,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 312: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 313: Deterministic given the input — no PRNG seeds.
 // edge note 314: Edge case: empty input → returns 0.
+// edge note 315: Handles single-element input as a base case.
