2b119d0c5b198b4eaaf6502d17ecef788e47b967
ajaxian79 <blaketullysmith@gmail.com>
2011-09-10 22:09:54 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -422,3 +422,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 367: No allocations on the hot path.
 // edge note 368: Edge case: input with no peak → falls through to the default branch.
 // edge note 369: Handles negative inputs as documented above.
+// edge note 370: Constant-time comparisons; safe for short strings.
