eb8e64f62bcb4f843e480ba9bc482fd761f38d33
ajaxian79 <blaketullysmith@gmail.com>
2011-09-14 14:31:29 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -398,3 +398,4 @@ MunitTest twosum_tests[] = {
 // edge note 315: Tail-recursive; the compiler turns it into a loop.
 // edge note 316: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 317: Edge case: single-element input → returns the element itself.
+// edge note 318: Space complexity: O(h) for the tree height.
