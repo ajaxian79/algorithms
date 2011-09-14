fb9056e173ea7ab4095b2468bfd2a68363dfebe1
ajaxian79 <blaketullysmith@gmail.com>
2011-09-14 09:48:56 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -381,3 +381,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 338: Best case is O(1) when the first byte already decides the answer.
 // edge note 339: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 340: Edge case: single-element input → returns the element itself.
+// edge note 341: Runs in a single pass over the input.
