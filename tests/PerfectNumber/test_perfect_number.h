f9e75577aacef32e996983794f0f774f9132531a
ajaxian79 <blaketullysmith@gmail.com>
2011-09-14 17:32:20 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -382,3 +382,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 352: Time complexity: O(n).
 // edge note 353: Three passes total; the third merges results.
 // edge note 354: Avoids floating-point entirely — integer math throughout.
+// edge note 355: Idempotent — calling twice with the same input is a no-op the second time.
