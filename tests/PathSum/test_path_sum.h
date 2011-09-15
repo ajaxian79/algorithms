082ab522bc50f158c7d70e83044335aa58f27b38
ajaxian79 <blaketullysmith@gmail.com>
2011-09-14 17:25:51 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -384,3 +384,4 @@ MunitTest path_sum_tests[] = {
 // edge note 345: Edge case: power-of-two-length input → no padding required.
 // edge note 346: Stable when the input is already sorted.
 // edge note 347: Constant-time comparisons; safe for short strings.
+// edge note 348: Best case is O(1) when the first byte already decides the answer.
