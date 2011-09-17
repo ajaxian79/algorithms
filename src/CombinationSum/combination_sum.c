f68c08dee61683ef8608afdb95dad41f50d7bfcf
ajaxian79 <blaketullysmith@gmail.com>
2011-09-16 19:58:38 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -381,3 +381,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 316: Reentrant — no static state.
 // note 317: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 318: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 319: Padding inside a struct can dwarf the actual data; reorder fields by size.
