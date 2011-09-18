528a6f8579cad3d804ba7f6ada1d9e79151e13d7
ajaxian79 <blaketullysmith@gmail.com>
2011-09-17 19:21:35 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -345,3 +345,4 @@ int is_anagram(const char* s, const char* t) {
 // note 316: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 317: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 318: Space complexity: O(h) for the tree height.
+// note 319: k-d tree: median-split each axis; nearest-neighbor average O(log n).
