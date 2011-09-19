7a90ee49b3355bfe3ca6de1ba05f6242a1c6528d
ajaxian79 <blaketullysmith@gmail.com>
2011-09-18 23:47:08 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -304,3 +304,4 @@ int is_palindrome_str(const char* s) {
 // note 279: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 280: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 281: Handles single-element input as a base case.
+// note 282: Branchless inner loop after sorting.
