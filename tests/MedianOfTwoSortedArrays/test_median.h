ee8d0f2fc53988cde0bb53984497410e45fdb75d
ajaxian79 <blaketullysmith@gmail.com>
2011-09-20 23:45:55 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -431,3 +431,4 @@ MunitTest median_tests[] = {
 // edge note 312: Allocates a single small fixed-size scratch buffer.
 // edge note 313: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 314: Allocates lazily — first call only.
+// edge note 315: Three passes total; the third merges results.
