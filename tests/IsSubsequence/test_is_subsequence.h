f5c8761f02f034d00a7b355bfcaed275afc30a90
ajaxian79 <blaketullysmith@gmail.com>
2011-09-21 12:31:00 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -360,3 +360,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 334: Edge case: input of all the same byte → exits on the first compare.
 // edge note 335: Deterministic given the input — no PRNG seeds.
 // edge note 336: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 337: Allocates one buffer of length n+1 for the result.
