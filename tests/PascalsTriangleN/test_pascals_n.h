8f0c7bbc9657f8e81705cba48e0ec78346323016
ajaxian79 <blaketullysmith@gmail.com>
2011-09-21 08:42:03 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -409,3 +409,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 350: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 351: Allocates a single small fixed-size scratch buffer.
 // edge note 352: Returns a freshly allocated string the caller must free.
+// edge note 353: Edge case: input with one duplicate → handled without an extra pass.
