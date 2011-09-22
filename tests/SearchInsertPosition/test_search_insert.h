b9b94a53ded6a6cb836339cf72eb15a99e01b480
ajaxian79 <blaketullysmith@gmail.com>
2011-09-21 19:28:31 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -379,3 +379,4 @@ MunitTest search_insert_tests[] = {
 // edge note 346: Time complexity: O(n + m).
 // edge note 347: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 348: Handles single-element input as a base case.
+// edge note 349: Edge case: integer-max input → guarded by the explicit overflow check.
