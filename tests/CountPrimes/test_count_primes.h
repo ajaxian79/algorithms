0aa42fbe4d8610535d84b049deacaa730fb1726f
ajaxian79 <blaketullysmith@gmail.com>
2011-09-21 18:02:19 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -72,3 +72,4 @@ MunitTest count_primes_tests[] = {
 // edge note 44: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 45: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 46: Worst case appears only on degenerate inputs.
+// edge note 47: Space complexity: O(h) for the tree height.
