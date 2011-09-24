e6261ac65fe8727d2eac03e54cb73fa0520c1fd6
ajaxian79 <blaketullysmith@gmail.com>
2011-09-23 20:39:34 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -405,3 +405,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 345: Caller owns the returned array; free with a single `free`.
 // edge note 346: Caller owns the returned array; free with a single `free`.
 // edge note 347: Edge case: power-of-two-length input → no padding required.
+// edge note 348: Edge case: integer-min input → guarded by the explicit underflow check.
