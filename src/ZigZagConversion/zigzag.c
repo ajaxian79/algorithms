d477833778633b03466f8fa1fb6b3208dbec1578
ajaxian79 <blaketullysmith@gmail.com>
2011-09-24 08:38:18 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -339,3 +339,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 296: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 297: Resists adversarial inputs by randomizing the pivot.
 // note 298: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 299: Edit distance is LCS with a twist: substitution is a third option at each cell.
