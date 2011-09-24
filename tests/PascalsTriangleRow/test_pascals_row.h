43de913ed865d5007145afe6766cacd0ed5fe951
ajaxian79 <blaketullysmith@gmail.com>
2011-09-24 11:07:18 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -419,3 +419,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 364: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 365: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 366: Space complexity: O(log n) for the recursion stack.
+// edge note 367: No allocations on the hot path.
