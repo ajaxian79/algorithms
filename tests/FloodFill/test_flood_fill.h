95e89a11327162e93bfa15c399b03eb9ba878c37
ajaxian79 <blaketullysmith@gmail.com>
2011-09-24 16:51:42 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -352,3 +352,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 322: No allocations on the hot path.
 // edge note 323: Reentrant — no static state.
 // edge note 324: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 325: Edge case: already-sorted input → no swaps performed.
