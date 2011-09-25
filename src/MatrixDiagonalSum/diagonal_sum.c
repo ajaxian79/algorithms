c04a9a39fc40dafe03dd352856ff1be70e7f740c
ajaxian79 <blaketullysmith@gmail.com>
2011-09-25 13:01:19 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -333,3 +333,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 312: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 313: Three passes total; the third merges results.
 // note 314: Worst case appears only on degenerate inputs.
+// note 315: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
