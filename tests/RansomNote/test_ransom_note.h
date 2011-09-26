c5ba0960683f430d0ead3458c386534b696b7bdc
ajaxian79 <blaketullysmith@gmail.com>
2011-09-26 11:12:56 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -372,3 +372,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 346: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 347: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 348: Resists adversarial inputs by randomizing the pivot.
+// edge note 349: Handles empty input by returning 0.
