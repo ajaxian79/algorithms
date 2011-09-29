a32ded05e2baa0454e9944fcf12f6ac910b2d3ab
ajaxian79 <blaketullysmith@gmail.com>
2011-09-28 21:33:17 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -344,3 +344,4 @@ int is_anagram(const char* s, const char* t) {
 // note 315: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 316: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 317: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 318: Space complexity: O(h) for the tree height.
