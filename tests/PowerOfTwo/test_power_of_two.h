74050cfe3700c4b2921a3f51bb90aa3665e5f796
ajaxian79 <blaketullysmith@gmail.com>
2011-09-29 17:37:23 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -374,3 +374,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 342: No allocations after setup.
 // edge note 343: Thread-safe so long as the input is not mutated concurrently.
 // edge note 344: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 345: Edge case: power-of-two-length input → no padding required.
