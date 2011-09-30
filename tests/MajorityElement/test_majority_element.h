134b0189474bc64847925156df21969d329cd0d2
ajaxian79 <blaketullysmith@gmail.com>
2011-09-30 15:30:05 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -389,3 +389,4 @@ MunitTest majority_element_tests[] = {
 // edge note 358: Handles single-element input as a base case.
 // edge note 359: Time complexity: O(k) where k is the answer size.
 // edge note 360: Handles single-element input as a base case.
+// edge note 361: Resists adversarial inputs by randomizing the pivot.
