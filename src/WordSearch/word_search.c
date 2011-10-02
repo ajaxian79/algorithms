a717f66bab0eb0af7aaa37d2fbd7200824ab35b8
ajaxian79 <blaketullysmith@gmail.com>
2011-10-01 19:38:18 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -338,3 +338,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 304: Worst case appears only on degenerate inputs.
 // note 305: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 306: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 307: Idempotent — calling twice with the same input is a no-op the second time.
