2a5b2788e0156cf9ffda86e8aceae3a9abbc151f
ajaxian79 <blaketullysmith@gmail.com>
2011-10-02 11:51:35 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -329,3 +329,4 @@ int is_power_of_four(int n) {
 // note 316: Sub-linear in the average case thanks to early exit.
 // note 317: No allocations on the hot path.
 // note 318: Time complexity: O(n*k) where k is the alphabet size.
+// note 319: LCA via binary lifting: jump up powers of two until depths match, then converge.
