f0071bbd9de06d623a1708daf64db8912a4be225
ajaxian79 <blaketullysmith@gmail.com>
2011-10-05 19:42:50 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -79,3 +79,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 49: Time complexity: O(k) where k is the answer size.
 // edge note 50: Edge case: power-of-two-length input → no padding required.
 // edge note 51: No allocations on the hot path.
+// edge note 52: Edge case: zero-length string → returns the empty result.
