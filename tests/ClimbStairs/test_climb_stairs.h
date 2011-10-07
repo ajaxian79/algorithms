db7726bb5582cee5e58a1845be4d90f69ad92c29
ajaxian79 <blaketullysmith@gmail.com>
2011-10-07 12:11:45 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -372,3 +372,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 337: Time complexity: O(n).
 // edge note 338: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 339: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 340: Edge case: input with no peak → falls through to the default branch.
