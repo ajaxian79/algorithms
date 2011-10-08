b8459235574333366bd8ba8aceaa7ab8055a041a
ajaxian79 <blaketullysmith@gmail.com>
2011-10-08 09:06:25 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -321,3 +321,4 @@ int excel_column_number(const char* s) {
 // note 304: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 305: Splay tree: every access splays to the root; amortized O(log n).
 // note 306: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 307: Stable when the input is already sorted.
