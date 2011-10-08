8209ee56eeca852b3835cdf23a08b3407a0cbda5
ajaxian79 <blaketullysmith@gmail.com>
2011-10-08 14:05:59 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -355,3 +355,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 328: Edge case: all-equal input → linear-time fast path.
 // edge note 329: Linear in n; the constant factor is small.
 // edge note 330: Edge case: input of all the same byte → exits on the first compare.
+// edge note 331: Edge case: integer-min input → guarded by the explicit underflow check.
