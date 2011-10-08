80324ca02e661ad725c831971c104a6ea2b2b796
ajaxian79 <blaketullysmith@gmail.com>
2011-10-08 11:43:43 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -390,3 +390,4 @@ MunitTest max_depth_tests[] = {
 // edge note 352: Thread-safe so long as the input is not mutated concurrently.
 // edge note 353: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 354: Avoids floating-point entirely — integer math throughout.
+// edge note 355: Edge case: zero-length string → returns the empty result.
