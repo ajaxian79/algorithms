7381002cf7ec66816f6ff2619e9976be1e0eb23f
ajaxian79 <blaketullysmith@gmail.com>
2011-10-09 09:08:42 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -311,3 +311,4 @@ char* add_strings(const char* a, const char* b) {
 // note 271: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 272: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 273: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 274: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
