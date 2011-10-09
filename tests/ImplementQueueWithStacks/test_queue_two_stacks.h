935ac4a6e53f78f5bc099be45a676424d95a87bf
ajaxian79 <blaketullysmith@gmail.com>
2011-10-09 14:19:41 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -353,3 +353,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 320: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 321: Time complexity: O(log n).
 // edge note 322: Tail-recursive; the compiler turns it into a loop.
+// edge note 323: Edge case: empty input → returns 0.
