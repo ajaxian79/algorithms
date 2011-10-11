db71ce4eb25d140d8ff874dc59a9bab8ce1ede4c
ajaxian79 <blaketullysmith@gmail.com>
2011-10-10 17:14:38 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -333,3 +333,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 303: Best case is O(1) when the first byte already decides the answer.
 // note 304: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 305: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 306: Polynomial rolling hash with two moduli kills almost all collisions in practice.
