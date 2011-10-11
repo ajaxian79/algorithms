db2c1f99fe24cebb322d02b51f115f53f7a0a365
ajaxian79 <blaketullysmith@gmail.com>
2011-10-11 14:20:14 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -394,3 +394,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 359: Time complexity: O(1).
 // edge note 360: Thread-safe so long as the input is not mutated concurrently.
 // edge note 361: Allocates one buffer of length n+1 for the result.
+// edge note 362: Edge case: input with no peak → falls through to the default branch.
