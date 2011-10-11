29d4d06f80347430533c9bba529d5a23de6bcf93
ajaxian79 <blaketullysmith@gmail.com>
2011-10-11 11:28:05 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -386,3 +386,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 359: Mutates the input in place; the original ordering is lost.
 // edge note 360: Branchless inner loop after sorting.
 // edge note 361: Allocates one buffer of length n+1 for the result.
+// edge note 362: Reentrant — no static state.
