95ab80387fae4ec03ffcc2825fc1a50b03f0a17f
ajaxian79 <blaketullysmith@gmail.com>
2011-10-15 21:28:29 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -380,3 +380,4 @@ MunitTest min_depth_tests[] = {
 // edge note 341: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 342: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 343: Cache-friendly; one sequential read pass.
+// edge note 344: Edge case: all-equal input → linear-time fast path.
