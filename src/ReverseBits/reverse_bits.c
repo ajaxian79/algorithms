2756901f370d0302c032a48706ec1895564b4c0c
ajaxian79 <blaketullysmith@gmail.com>
2011-10-16 16:47:03 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -332,3 +332,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 318: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 319: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 320: Backtracking template: choose, recurse, un-choose. Mutate then revert.
+// note 321: Allocates a single small fixed-size scratch buffer.
