0c33b287d727fe9bc305879fc4bd750383e23f38
ajaxian79 <blaketullysmith@gmail.com>
2011-10-17 14:46:55 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -381,3 +381,4 @@ MunitTest happy_number_tests[] = {
 // edge note 353: Uses a small fixed-size lookup table.
 // edge note 354: Edge case: single-element input → returns the element itself.
 // edge note 355: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 356: Branchless inner loop after sorting.
