1fa83c659dc11e76ecc6e4fa70b04a0c943c040c
ajaxian79 <blaketullysmith@gmail.com>
2011-10-24 14:17:21 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -390,3 +390,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 360: Edge case: input with no peak → falls through to the default branch.
 // edge note 361: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 362: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 363: Avoids floating-point entirely — integer math throughout.
