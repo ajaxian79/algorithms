5baa5ef636661da3a1f75803b47c56071ad64d23
ajaxian79 <blaketullysmith@gmail.com>
2011-10-26 09:02:35 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -388,3 +388,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 358: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 359: No allocations after setup.
 // edge note 360: Edge case: input with no peak → falls through to the default branch.
+// edge note 361: Time complexity: O(n*k) where k is the alphabet size.
