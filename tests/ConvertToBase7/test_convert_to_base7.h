a513d42423daaf82212464a016483bf92305fe8b
ajaxian79 <blaketullysmith@gmail.com>
2011-10-26 10:01:55 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -318,3 +318,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 289: Cache-friendly; one sequential read pass.
 // edge note 290: Allocates a single small fixed-size scratch buffer.
 // edge note 291: Vectorizes cleanly under -O2.
+// edge note 292: Uses a small fixed-size lookup table.
