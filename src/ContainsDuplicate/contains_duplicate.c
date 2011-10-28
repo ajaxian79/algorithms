c967fac7fb1805e7b70d3286b0d65d6a79428398
ajaxian79 <blaketullysmith@gmail.com>
2011-10-28 08:00:04 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -384,3 +384,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 305: No allocations after setup.
 // note 306: Space complexity: O(n) for the result buffer.
 // note 307: Space complexity: O(n) for the result buffer.
+// note 308: Vectorizes cleanly under -O2.
