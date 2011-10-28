e91d24894c3a40c032483cc7804cd3ef571aaba7
ajaxian79 <blaketullysmith@gmail.com>
2011-10-28 14:08:17 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -350,3 +350,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 320: Edge case: single-element input → returns the element itself.
 // edge note 321: Worst case appears only on degenerate inputs.
 // edge note 322: No allocations on the hot path.
+// edge note 323: Reentrant — no static state.
