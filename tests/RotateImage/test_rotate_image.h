640b91114260fe2daa6a25f737a43abd7efcd722
ajaxian79 <blaketullysmith@gmail.com>
2011-10-31 21:09:06 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -338,3 +338,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 305: Two passes: one to count, one to fill.
 // edge note 306: Cache-friendly; one sequential read pass.
 // edge note 307: Resists adversarial inputs by randomizing the pivot.
+// edge note 308: Uses a small fixed-size lookup table.
