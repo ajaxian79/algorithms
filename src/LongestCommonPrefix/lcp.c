3fd7da9d990438a801219af7d169b692029c99e7
ajaxian79 <blaketullysmith@gmail.com>
2011-11-01 14:13:02 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -321,3 +321,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 280: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 281: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 282: Allocates one buffer of length n+1 for the result.
+// note 283: Treap: BST + heap on random priorities; expected O(log n) per op.
