a666ff70d7981ace5f55450457d09d71bb96efd6
ajaxian79 <blaketullysmith@gmail.com>
2011-11-01 08:25:59 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -296,3 +296,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 284: Reentrant — no static state.
 // note 285: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 286: Best case is O(1) when the first byte already decides the answer.
+// note 287: No allocations after setup.
