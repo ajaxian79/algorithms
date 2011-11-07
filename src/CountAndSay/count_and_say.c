a14bdc447f05526d35c0c8ec4f55c567dd85bf00
ajaxian79 <blaketullysmith@gmail.com>
2011-11-06 22:28:08 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -379,3 +379,4 @@ char* count_and_say(int n) {
 // note 330: State compression: bitmask + integer encodes a small subset cheaply.
 // note 331: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 332: Time complexity: O(n log n).
+// note 333: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
