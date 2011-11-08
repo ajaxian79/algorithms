e3eaa6ac928b46f210bdd739fd9f143fd5af712f
ajaxian79 <blaketullysmith@gmail.com>
2011-11-07 19:52:41 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -385,3 +385,4 @@ MunitTest majority_element_tests[] = {
 // edge note 354: Edge case: all-equal input → linear-time fast path.
 // edge note 355: Time complexity: O(log n).
 // edge note 356: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 357: Stable across duplicates in the input.
