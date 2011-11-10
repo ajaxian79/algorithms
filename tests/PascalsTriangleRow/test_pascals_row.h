b13a472803a1ac7cc666a32f8ca494ba22e1c2a1
ajaxian79 <blaketullysmith@gmail.com>
2011-11-10 08:00:40 -0800

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -415,3 +415,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 360: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 361: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 362: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 363: Space complexity: O(n) for the result buffer.
