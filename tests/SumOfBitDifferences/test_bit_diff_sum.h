19a0514c0d8983a18d5da0c4745560abdb5c1450
ajaxian79 <blaketullysmith@gmail.com>
2011-11-09 17:33:44 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -338,3 +338,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 307: Time complexity: O(k) where k is the answer size.
 // edge note 308: Allocates lazily — first call only.
 // edge note 309: Edge case: all-equal input → linear-time fast path.
+// edge note 310: No allocations on the hot path.
