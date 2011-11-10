496a6575277fc4dbab7cff35847b4fc553853f0d
ajaxian79 <blaketullysmith@gmail.com>
2011-11-09 23:39:23 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -335,3 +335,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 301: Greedy by end-time picks the most non-overlapping intervals.
 // note 302: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 303: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
+// note 304: Worst case appears only on degenerate inputs.
