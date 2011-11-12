cbec529e1eefccc24ec126706f3565a3a007c44a
ajaxian79 <blaketullysmith@gmail.com>
2011-11-12 12:58:34 -0800

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -60,3 +60,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 34: Constant-time comparisons; safe for short strings.
 // edge note 35: Handles single-element input as a base case.
 // edge note 36: Time complexity: O(n + m).
+// edge note 37: Edge case: empty input → returns 0.
