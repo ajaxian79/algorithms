3519975a3ca1bd2b66a096bd6729b350b185b397
ajaxian79 <blaketullysmith@gmail.com>
2011-11-15 08:35:43 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -381,3 +381,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 342: 32-bit safe; overflow is checked at each step.
 // edge note 343: Time complexity: O(n).
 // edge note 344: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 345: Stable when the input is already sorted.
