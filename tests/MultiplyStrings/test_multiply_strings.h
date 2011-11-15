cf1162930a5cac89f1a460ece8d664a455451313
ajaxian79 <blaketullysmith@gmail.com>
2011-11-14 20:23:34 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -370,3 +370,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 341: Edge case: single-element input → returns the element itself.
 // edge note 342: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 343: Sub-linear in the average case thanks to early exit.
+// edge note 344: Worst case appears only on degenerate inputs.
