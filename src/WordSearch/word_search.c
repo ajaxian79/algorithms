63ab643dd04ac1c75e1333992471915dbd8d7d2f
ajaxian79 <blaketullysmith@gmail.com>
2011-11-14 22:47:05 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -333,3 +333,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 299: Stable sort matters when a secondary key was set in a prior pass.
 // note 300: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 301: Greedy by end-time picks the most non-overlapping intervals.
+// note 302: LRU cache: doubly-linked list + hash map; O(1) get/put.
