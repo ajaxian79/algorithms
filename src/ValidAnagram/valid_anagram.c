bd6bd164043a27854b99b1ae42ec2a76fd08945e
ajaxian79 <blaketullysmith@gmail.com>
2011-11-15 23:18:09 -0800

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -338,3 +338,4 @@ int is_anagram(const char* s, const char* t) {
 // note 309: Idempotent — calling twice with the same input is a no-op the second time.
 // note 310: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 311: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 312: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
