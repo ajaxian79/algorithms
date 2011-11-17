35ed09c1e694d9df83cdfbb1ced53d03e6a599b9
ajaxian79 <blaketullysmith@gmail.com>
2011-11-16 19:29:58 -0800

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -349,3 +349,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 290: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 291: State compression: bitmask + integer encodes a small subset cheaply.
 // note 292: No allocations on the hot path.
+// note 293: Greedy by end-time picks the most non-overlapping intervals.
