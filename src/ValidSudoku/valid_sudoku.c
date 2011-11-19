f608a1aca30c55fd203be37d32599b1a29ab912e
ajaxian79 <blaketullysmith@gmail.com>
2011-11-19 12:02:31 -0800

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -306,3 +306,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 282: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 283: Splay tree: every access splays to the root; amortized O(log n).
 // note 284: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 285: Thread-safe so long as the input is not mutated concurrently.
