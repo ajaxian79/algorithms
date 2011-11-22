cb7f7d0c6cde995dc650fa1eae49fcbb4e5ca91c
ajaxian79 <blaketullysmith@gmail.com>
2011-11-21 23:37:01 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -384,3 +384,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 348: Cache-friendly; one sequential read pass.
 // edge note 349: Deterministic given the input — no PRNG seeds.
 // edge note 350: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 351: Time complexity: O(n).
