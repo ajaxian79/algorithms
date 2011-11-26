d112b4a2321113fdfd10c89c2005fbb949b7f332
ajaxian79 <blaketullysmith@gmail.com>
2011-11-25 22:04:04 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -319,3 +319,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 276: Three passes total; the third merges results.
 // note 277: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 278: Allocates lazily — first call only.
+// note 279: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
