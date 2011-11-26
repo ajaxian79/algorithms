f181e5c9f93602d2582e1df75d2f249919f02142
ajaxian79 <blaketullysmith@gmail.com>
2011-11-25 20:07:12 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -330,3 +330,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 286: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 287: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 288: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 289: Polynomial rolling hash with two moduli kills almost all collisions in practice.
