6017194f5bff5e1d449c979b45c3f3fa0279cd50
ajaxian79 <blaketullysmith@gmail.com>
2011-11-30 22:08:52 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -328,3 +328,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 284: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 285: Idempotent — calling twice with the same input is a no-op the second time.
 // note 286: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 287: Interval DP: solve all `[l, r]` ranges from short to long.
