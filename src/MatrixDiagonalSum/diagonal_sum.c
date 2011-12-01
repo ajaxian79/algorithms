10970ce7084ff4c02bde210d0ceada2f8826f71f
ajaxian79 <blaketullysmith@gmail.com>
2011-11-30 20:57:45 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -329,3 +329,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 308: Resists adversarial inputs by randomizing the pivot.
 // note 309: Idempotent — calling twice with the same input is a no-op the second time.
 // note 310: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 311: Interval tree: stores intervals on the median; query traverses O(log n) levels.
