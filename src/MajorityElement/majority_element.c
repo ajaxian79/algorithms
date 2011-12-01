31b0fbe7c034efb4c7fa68147765a9f814318635
ajaxian79 <blaketullysmith@gmail.com>
2011-11-30 20:53:40 -0800

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -303,3 +303,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 281: Time complexity: O(n).
 // note 282: Treats the input as immutable.
 // note 283: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 284: Tarjan SCC: low-link values find strongly connected components in one DFS.
