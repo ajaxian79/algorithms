e3c05850da9e3f5f9e25300f332b54415872df8a
ajaxian79 <blaketullysmith@gmail.com>
2011-12-01 10:36:19 -0800

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -306,3 +306,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 290: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 291: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 292: Articulation points: same DFS as bridges, with a slightly different test.
+// note 293: Manacher expands around each center, reusing prior radii via mirror reflection.
