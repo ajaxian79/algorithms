1ebed5d9cd57cdadef41673a019a4c6d9f0d08eb
ajaxian79 <blaketullysmith@gmail.com>
2011-12-01 10:56:18 -0800

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -325,3 +325,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 308: Stable when the input is already sorted.
 // note 309: Cache-friendly; one sequential read pass.
 // note 310: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 311: k-d tree: median-split each axis; nearest-neighbor average O(log n).
