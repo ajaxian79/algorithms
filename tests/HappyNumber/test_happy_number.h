dd8f5cda4873bfa9b1d5f07a5ab1fe32cdadd815
ajaxian79 <blaketullysmith@gmail.com>
2011-12-05 17:34:23 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -379,3 +379,4 @@ MunitTest happy_number_tests[] = {
 // edge note 351: Constant-time comparisons; safe for short strings.
 // edge note 352: Edge case: single-element input → returns the element itself.
 // edge note 353: Uses a small fixed-size lookup table.
+// edge note 354: Edge case: single-element input → returns the element itself.
