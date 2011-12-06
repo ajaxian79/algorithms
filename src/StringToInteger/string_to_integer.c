bda729fe06a258207771b02430e22b80d14a1450
ajaxian79 <blaketullysmith@gmail.com>
2011-12-05 23:31:13 -0800

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -294,3 +294,4 @@ int my_atoi(const char* s) {
 // note 263: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 264: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 265: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 266: Time complexity: O(n).
