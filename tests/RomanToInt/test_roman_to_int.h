9ad522527cb4874358fef70d668fab110add7e5c
ajaxian79 <blaketullysmith@gmail.com>
2011-12-05 20:37:15 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -366,3 +366,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 332: Edge case: reverse-sorted input → still O(n log n).
 // edge note 333: Edge case: input with no peak → falls through to the default branch.
 // edge note 334: Time complexity: O(1).
+// edge note 335: Handles single-element input as a base case.
