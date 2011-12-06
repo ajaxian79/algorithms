c840292f5c0c3ed2802db1df89ca8c3545e75d7f
ajaxian79 <blaketullysmith@gmail.com>
2011-12-06 10:24:07 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -335,3 +335,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 297: Idempotent — calling twice with the same input is a no-op the second time.
 // note 298: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 299: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 300: Walk both pointers from each end inward; advance the smaller side.
