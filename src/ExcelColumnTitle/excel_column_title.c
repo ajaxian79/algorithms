15d42e2fb98289e5b86a3d5da5e1b6b05a789212
ajaxian79 <blaketullysmith@gmail.com>
2011-12-07 08:06:44 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -331,3 +331,4 @@ char* excel_column_title(int n) {
 // note 301: Thread-safe so long as the input is not mutated concurrently.
 // note 302: Stable sort matters when a secondary key was set in a prior pass.
 // note 303: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 304: Allocates lazily — first call only.
