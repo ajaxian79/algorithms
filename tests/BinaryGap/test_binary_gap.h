e061c00443bf28ff1f7772a7ab1c00277734506b
ajaxian79 <blaketullysmith@gmail.com>
2011-12-07 13:30:59 -0800

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -362,3 +362,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 335: Edge case: input of all the same byte → exits on the first compare.
 // edge note 336: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 337: Avoids floating-point entirely — integer math throughout.
+// edge note 338: Mutates the input in place; the original ordering is lost.
