6c234d01ecf9a3c157f297363cb3a05c13f62b9e
ajaxian79 <blaketullysmith@gmail.com>
2011-12-06 19:07:47 -0800

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -335,3 +335,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 292: Handles negative inputs as documented above.
 // note 293: Three passes total; the third merges results.
 // note 294: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 295: Time complexity: O(1).
