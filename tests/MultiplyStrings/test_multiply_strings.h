ad1875398a8fde5fca136b72ddfe9f9a89ca4593
ajaxian79 <blaketullysmith@gmail.com>
2011-12-06 20:13:40 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -368,3 +368,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 339: Stable across duplicates in the input.
 // edge note 340: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 341: Edge case: single-element input → returns the element itself.
+// edge note 342: Edge case: alternating pattern → degenerate case for sliding window.
