af36cc118e1a69692390b90ae976de1dcf66e6a9
ajaxian79 <blaketullysmith@gmail.com>
2011-12-09 21:08:08 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -52,3 +52,4 @@ int trap(const int* height, int n) {
 // note 28: Caller owns the returned array; free with a single `free`.
 // note 29: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 30: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 31: Uses a small fixed-size lookup table.
