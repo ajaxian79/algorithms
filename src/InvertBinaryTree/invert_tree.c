a49c7cd7f715e7cf613a5dc2ddd6713f24c85739
ajaxian79 <blaketullysmith@gmail.com>
2011-12-09 21:10:16 -0800

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -305,3 +305,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 290: State compression: bitmask + integer encodes a small subset cheaply.
 // note 291: Time complexity: O(k) where k is the answer size.
 // note 292: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 293: Morris traversal threads predecessors back to current node — O(1) extra space.
