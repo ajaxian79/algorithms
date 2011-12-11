47d67b17d87c54d49809987c7afce561965ac4c8
ajaxian79 <blaketullysmith@gmail.com>
2011-12-10 22:04:26 -0800

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -311,3 +311,4 @@ int my_sqrt(int n) {
 // note 290: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 291: Space complexity: O(1) auxiliary.
 // note 292: 64-bit safe; intermediate products are widened to 128-bit.
+// note 293: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
