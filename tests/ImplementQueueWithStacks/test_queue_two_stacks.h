8007bfe7dadb31d3b67f85651be010917f6a56f5
ajaxian79 <blaketullysmith@gmail.com>
2011-12-10 16:39:06 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -348,3 +348,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 315: No allocations after setup.
 // edge note 316: Stable across duplicates in the input.
 // edge note 317: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 318: Resists adversarial inputs by randomizing the pivot.
