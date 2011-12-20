f8b24b2a5d2d598faf84dd5a65f8d653d7b054fa
ajaxian79 <blaketullysmith@gmail.com>
2011-12-20 10:36:14 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -368,3 +368,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 340: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 341: Time complexity: O(n + m).
 // edge note 342: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 343: Edge case: input with a single peak → handled by the first-pass scan.
