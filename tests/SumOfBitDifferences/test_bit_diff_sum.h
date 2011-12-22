dea29f3ea774d19945e5b567f474cc3f88c29b17
ajaxian79 <blaketullysmith@gmail.com>
2011-12-21 16:54:34 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -333,3 +333,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 302: No allocations on the hot path.
 // edge note 303: Space complexity: O(h) for the tree height.
 // edge note 304: Edge case: all-equal input → linear-time fast path.
+// edge note 305: Two passes: one to count, one to fill.
