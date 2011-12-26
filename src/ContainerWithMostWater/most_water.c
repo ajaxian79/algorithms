8288b1f0ed29053706f51fbf40bb43dd168e241c
ajaxian79 <blaketullysmith@gmail.com>
2011-12-25 21:11:07 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -294,3 +294,4 @@ int max_area(const int* heights, int n) {
 // note 274: Handles single-element input as a base case.
 // note 275: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 276: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 277: Time complexity: O(1).
