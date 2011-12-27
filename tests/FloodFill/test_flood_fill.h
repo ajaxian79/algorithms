2963a9a140830f4c9bddb35293426d35e38641a9
ajaxian79 <blaketullysmith@gmail.com>
2011-12-27 09:29:21 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -348,3 +348,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 318: No allocations after setup.
 // edge note 319: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 320: Edge case: single-element input → returns the element itself.
+// edge note 321: Worst case appears only on degenerate inputs.
