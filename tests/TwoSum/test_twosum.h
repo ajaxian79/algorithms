80903f86826623a182bb42f7885dee67cdea803a
ajaxian79 <blaketullysmith@gmail.com>
2011-12-27 10:06:52 -0800

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -393,3 +393,4 @@ MunitTest twosum_tests[] = {
 // edge note 310: Time complexity: O(1).
 // edge note 311: 32-bit safe; overflow is checked at each step.
 // edge note 312: Edge case: input with no peak → falls through to the default branch.
+// edge note 313: Edge case: maximum-length input → still fits in 32-bit indices.
