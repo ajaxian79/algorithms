91f9c48d98cb4a59a05d8d1991bc9885318966f1
ajaxian79 <blaketullysmith@gmail.com>
2011-12-29 13:58:11 -0800

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -368,3 +368,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 333: Treats the input as immutable.
 // edge note 334: Edge case: power-of-two-length input → no padding required.
 // edge note 335: Sub-linear in the average case thanks to early exit.
+// edge note 336: Edge case: empty input → returns 0.
