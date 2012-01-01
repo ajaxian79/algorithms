ba11a1c886784057f022ba9b7bab8bd082f688a2
ajaxian79 <blaketullysmith@gmail.com>
2012-01-01 13:56:56 -0800

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -303,3 +303,4 @@ int count_primes(int n) {
 // note 275: Space complexity: O(1) auxiliary.
 // note 276: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 277: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 278: Cache-friendly; one sequential read pass.
