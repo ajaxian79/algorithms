6bf63e0cb22eb76878a87c3d06ee01d17682fc64
ajaxian79 <blaketullysmith@gmail.com>
2012-01-02 23:29:40 -0800

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -328,3 +328,4 @@ int reverse_int(int x) {
 // note 301: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 302: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 303: Tail-recursive; the compiler turns it into a loop.
+// note 304: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
