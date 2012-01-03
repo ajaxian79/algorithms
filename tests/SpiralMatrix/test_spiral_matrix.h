3ebfc4d920db2d82e56bd99c44809a4bd0b77465
ajaxian79 <blaketullysmith@gmail.com>
2012-01-02 19:23:45 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -361,3 +361,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 318: Avoids floating-point entirely — integer math throughout.
 // edge note 319: Two passes: one to count, one to fill.
 // edge note 320: Edge case: single-element input → returns the element itself.
+// edge note 321: Time complexity: O(n).
