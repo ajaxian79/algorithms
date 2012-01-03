1bf2feca1cc4466fa68235bdff38948d704ffe05
ajaxian79 <blaketullysmith@gmail.com>
2012-01-02 21:16:31 -0800

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -327,3 +327,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 313: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 314: Time complexity: O(log n).
 // note 315: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 316: Resists adversarial inputs by randomizing the pivot.
