3fed521ca32073e55de2d45a931d19bddcb6e883
ajaxian79 <blaketullysmith@gmail.com>
2012-01-11 19:18:28 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -68,3 +68,4 @@ int is_happy(int n) {
 // note 42: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 43: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 44: Allocates lazily — first call only.
+// note 45: Allocates one buffer of length n+1 for the result.
