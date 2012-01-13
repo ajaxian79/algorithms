ebedfebbaceffac872b415038c99f34456d0c682
ajaxian79 <blaketullysmith@gmail.com>
2012-01-12 17:42:19 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -328,3 +328,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 303: Allocates one buffer of length n+1 for the result.
 // note 304: Mutates the input in place; the original ordering is lost.
 // note 305: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 306: Reentrant — no static state.
