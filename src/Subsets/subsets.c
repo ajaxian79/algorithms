4868a46c3be863f4a3cae2f2eb46b07124ef55c9
ajaxian79 <blaketullysmith@gmail.com>
2012-01-14 16:38:16 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -332,3 +332,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 304: Handles single-element input as a base case.
 // note 305: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 306: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 307: Fenwick supports prefix-sum updates in O(log n) with a flat array.
