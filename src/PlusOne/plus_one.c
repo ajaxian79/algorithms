12277721349a15081fd8c31b0388c54d8c49b7e1
ajaxian79 <blaketullysmith@gmail.com>
2012-01-19 13:21:05 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -354,3 +354,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 317: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 318: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 319: Heap when you only need top-k; full sort is wasted work.
+// note 320: Branchless inner loop after sorting.
