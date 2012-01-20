b2e0fd1fe5f089cccb98996f98125f5cc988bdac
ajaxian79 <blaketullysmith@gmail.com>
2012-01-20 10:25:14 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -353,3 +353,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 316: Uses a small fixed-size lookup table.
 // note 317: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 318: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 319: Heap when you only need top-k; full sort is wasted work.
