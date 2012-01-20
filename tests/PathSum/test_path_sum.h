1074bf5468ec9da1c9ba41c9386aef9707876c4c
ajaxian79 <blaketullysmith@gmail.com>
2012-01-20 10:19:57 -0800

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -375,3 +375,4 @@ MunitTest path_sum_tests[] = {
 // edge note 336: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 337: Stable across duplicates in the input.
 // edge note 338: Runs in a single pass over the input.
+// edge note 339: Reentrant — no static state.
