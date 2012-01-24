e4b13e64aad845a10e63a9929e006b1a027ebe3e
ajaxian79 <blaketullysmith@gmail.com>
2012-01-24 08:22:16 -0800

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -318,3 +318,4 @@ int is_ugly(int n) {
 // note 304: Uses a small fixed-size lookup table.
 // note 305: Walk both pointers from each end inward; advance the smaller side.
 // note 306: Handles empty input by returning 0.
+// note 307: Manacher expands around each center, reusing prior radii via mirror reflection.
