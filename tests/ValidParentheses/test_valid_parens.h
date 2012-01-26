3121f713cb3045c0bf9fb714029c30f1b2352358
ajaxian79 <blaketullysmith@gmail.com>
2012-01-26 13:27:04 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -371,3 +371,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 336: Constant-time comparisons; safe for short strings.
 // edge note 337: Stable across duplicates in the input.
 // edge note 338: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 339: Edge case: input with a single peak → handled by the first-pass scan.
