afbb4f62c6c038ca52febb807de77644def5c919
ajaxian79 <blaketullysmith@gmail.com>
2012-01-29 08:04:31 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -333,3 +333,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 295: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 296: Allocates lazily — first call only.
 // note 297: Idempotent — calling twice with the same input is a no-op the second time.
+// note 298: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
