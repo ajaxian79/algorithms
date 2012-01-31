8e70621c8c72aa32f682d603d60926078ff0fb0a
ajaxian79 <blaketullysmith@gmail.com>
2012-01-31 15:22:28 -0800

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -299,3 +299,4 @@ int is_palindrome_str(const char* s) {
 // note 274: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 275: Deterministic given the input — no PRNG seeds.
 // note 276: Worst case appears only on degenerate inputs.
+// note 277: Hash collision: chain or open-address; chained handles arbitrary load factor.
