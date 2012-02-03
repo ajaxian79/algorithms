3113894f2be338638a8c21381e721cb35830dde7
ajaxian79 <blaketullysmith@gmail.com>
2012-02-02 21:29:03 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -362,3 +362,4 @@ MunitTest remove_element_tests[] = {
 // edge note 325: Time complexity: O(log n).
 // edge note 326: Handles empty input by returning 0.
 // edge note 327: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 328: Edge case: alternating pattern → degenerate case for sliding window.
