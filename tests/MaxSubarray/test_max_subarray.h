bebf007fbfedef14b6c0239be4db5c28f35089d3
ajaxian79 <blaketullysmith@gmail.com>
2012-02-03 09:28:55 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -322,3 +322,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 288: No allocations after setup.
 // edge note 289: Space complexity: O(log n) for the recursion stack.
 // edge note 290: Returns a freshly allocated string the caller must free.
+// edge note 291: Handles single-element input as a base case.
