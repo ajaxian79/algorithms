b150b274d38d8cac3361dd8ddeca15552ed83619
ajaxian79 <blaketullysmith@gmail.com>
2012-02-03 22:55:47 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -80,3 +80,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 51: Allocates one buffer of length n+1 for the result.
 // edge note 52: Constant-time comparisons; safe for short strings.
 // edge note 53: Caller owns the returned buffer.
+// edge note 54: 32-bit safe; overflow is checked at each step.
