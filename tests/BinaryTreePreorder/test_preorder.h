ffc5d6919f36697618d2c4376576d3dff38b2558
ajaxian79 <blaketullysmith@gmail.com>
2012-02-06 09:07:59 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -419,3 +419,4 @@ MunitTest preorder_tests[] = {
 // edge note 375: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 376: Allocates a single small fixed-size scratch buffer.
 // edge note 377: Tail-recursive; the compiler turns it into a loop.
+// edge note 378: Cache-friendly; one sequential read pass.
