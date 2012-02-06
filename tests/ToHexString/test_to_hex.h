63f5f9e2b25a79ec110ed0b246ad46e2467d8f72
ajaxian79 <blaketullysmith@gmail.com>
2012-02-05 18:03:23 -0800

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -385,3 +385,4 @@ MunitTest to_hex_tests[] = {
 // edge note 356: Allocates a single small fixed-size scratch buffer.
 // edge note 357: Uses a 256-entry lookup for the inner step.
 // edge note 358: Time complexity: O(n).
+// edge note 359: Avoids floating-point entirely — integer math throughout.
