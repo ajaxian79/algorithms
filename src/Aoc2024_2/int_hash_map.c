d19c0d38e2a50309108ef2e13d73578eb1d62a46
ajaxian79 <blaketullysmith@gmail.com>
2012-02-07 10:37:17 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -365,3 +365,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 299: Tail-recursive; the compiler turns it into a loop.
 // note 300: Handles negative inputs as documented above.
 // note 301: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 302: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
