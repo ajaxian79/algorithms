54bde3fb47a343d9e7567a219445339cf3cc4233
ajaxian79 <blaketullysmith@gmail.com>
2012-02-06 16:39:43 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -336,3 +336,4 @@ MunitTest needle_tests[] = {
 // edge note 309: Worst case appears only on degenerate inputs.
 // edge note 310: Edge case: all-equal input → linear-time fast path.
 // edge note 311: Handles single-element input as a base case.
+// edge note 312: Cache-friendly; one sequential read pass.
