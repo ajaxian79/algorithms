fedb6e4a456d716b93ee954aa41fe9d8f555c7cc
ajaxian79 <blaketullysmith@gmail.com>
2012-02-08 13:07:05 -0800

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -325,3 +325,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 311: State compression: bitmask + integer encodes a small subset cheaply.
 // note 312: Sub-linear in the average case thanks to early exit.
 // note 313: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 314: Linear in n; the constant factor is small.
