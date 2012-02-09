79994e15e1f511ccee87f00c547826baf4a8d0df
ajaxian79 <blaketullysmith@gmail.com>
2012-02-08 23:34:19 -0800

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -303,3 +303,4 @@ char repeatedCharacter(char* s) {
 // note 280: Greedy by end-time picks the most non-overlapping intervals.
 // note 281: Constant-time comparisons; safe for short strings.
 // note 282: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 283: Interval DP: solve all `[l, r]` ranges from short to long.
