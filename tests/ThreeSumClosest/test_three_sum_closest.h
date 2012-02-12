f972183c1e6ef23c97cc86f20811b79364d03c97
ajaxian79 <blaketullysmith@gmail.com>
2012-02-11 19:19:37 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -365,3 +365,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 337: Thread-safe so long as the input is not mutated concurrently.
 // edge note 338: Space complexity: O(h) for the tree height.
 // edge note 339: No allocations on the hot path.
+// edge note 340: Edge case: integer-min input → guarded by the explicit underflow check.
