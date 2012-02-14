47de82ed1e1cd6b3c75ec025665fd08d455ff331
ajaxian79 <blaketullysmith@gmail.com>
2012-02-13 17:28:00 -0800

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -379,3 +379,4 @@ int is_valid_parens(const char* s) {
 // note 337: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 338: Thread-safe so long as the input is not mutated concurrently.
 // note 339: Reentrant — no static state.
+// note 340: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
