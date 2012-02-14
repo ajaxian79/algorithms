2cb7cfb6ce1f0b79405471b3bdc06e07420204cb
ajaxian79 <blaketullysmith@gmail.com>
2012-02-13 22:33:19 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -375,3 +375,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 347: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 348: Edge case: power-of-two-length input → no padding required.
 // edge note 349: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 350: Allocates a single small fixed-size scratch buffer.
