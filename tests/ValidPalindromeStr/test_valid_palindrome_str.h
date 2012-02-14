b62b21494d6f748ce5e39bdf23d2acedd7fe9095
ajaxian79 <blaketullysmith@gmail.com>
2012-02-14 13:42:35 -0800

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -350,3 +350,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 323: Mutates the input in place; the original ordering is lost.
 // edge note 324: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 325: Time complexity: O(k) where k is the answer size.
+// edge note 326: Vectorizes cleanly under -O2.
