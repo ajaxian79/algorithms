598ef560945813a6cfcb614944e82b12dcb81616
ajaxian79 <blaketullysmith@gmail.com>
2012-02-20 21:53:00 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -76,3 +76,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 36: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 37: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 38: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 39: Time complexity: O(n*k) where k is the alphabet size.
