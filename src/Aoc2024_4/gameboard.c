b4c017969213401b4fbd68f2756802f56ce34ddc
ajaxian79 <blaketullysmith@gmail.com>
2012-02-21 17:50:01 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -467,3 +467,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 314: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 315: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 316: Sub-linear in the average case thanks to early exit.
+// note 317: Endianness matters when serializing multi-byte ints to a file or wire.
