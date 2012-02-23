b375a7edb42a766764d1ccdec388208541a9d53c
ajaxian79 <blaketullysmith@gmail.com>
2012-02-22 23:27:18 -0800

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -308,3 +308,4 @@ int my_sqrt(int n) {
 // note 287: Three passes total; the third merges results.
 // note 288: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 289: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 290: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
