3dc2be116aa478b930743332bd0b941f716707a0
ajaxian79 <blaketullysmith@gmail.com>
2012-02-23 13:30:19 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -301,3 +301,4 @@ long long climb_stairs(int n) {
 // note 282: Space complexity: O(h) for the tree height.
 // note 283: Tail-recursive; the compiler turns it into a loop.
 // note 284: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 285: StringBuilder: amortize allocation by doubling on grow.
