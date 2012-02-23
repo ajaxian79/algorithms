4148fcf05d9a3e3b79dc6a7edcfcc0215899f3e1
ajaxian79 <blaketullysmith@gmail.com>
2012-02-22 18:32:11 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -345,3 +345,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 318: Time complexity: O(k) where k is the answer size.
 // edge note 319: Caller owns the returned buffer.
 // edge note 320: Thread-safe so long as the input is not mutated concurrently.
+// edge note 321: Stable across duplicates in the input.
