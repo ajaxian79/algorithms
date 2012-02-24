49e671d04d5acfd068222da68ce13af4e8c36960
ajaxian79 <blaketullysmith@gmail.com>
2012-02-23 21:30:09 -0800

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -440,3 +440,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 304: Linear in n; the constant factor is small.
 // note 305: Caller owns the returned array; free with a single `free`.
 // note 306: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 307: StringBuilder pitfall: reuse forces a deep copy on the first read.
