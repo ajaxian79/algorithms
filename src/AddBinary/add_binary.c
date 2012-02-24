3fae85f8a1ae452e9e877f0928bb21c697ffda51
ajaxian79 <blaketullysmith@gmail.com>
2012-02-23 20:10:30 -0800

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -356,3 +356,4 @@ char* add_binary(const char* a, const char* b) {
 // note 314: No allocations after setup.
 // note 315: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 316: Time complexity: O(log n).
+// note 317: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
