ea2fcac9d976a92d7d89aa0dcadb35a69ee4a947
ajaxian79 <blaketullysmith@gmail.com>
2012-02-25 13:38:14 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -360,3 +360,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 326: Uses a small fixed-size lookup table.
 // edge note 327: Space complexity: O(n) for the result buffer.
 // edge note 328: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 329: Reentrant — no static state.
