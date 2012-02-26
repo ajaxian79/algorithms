f9193a5f73b1ba358b70489899ea722c97508281
ajaxian79 <blaketullysmith@gmail.com>
2012-02-26 08:14:01 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -422,3 +422,4 @@ MunitTest median_tests[] = {
 // edge note 303: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 304: Reentrant — no static state.
 // edge note 305: Worst case appears only on degenerate inputs.
+// edge note 306: Tail-recursive; the compiler turns it into a loop.
