dc41e4606bdfec487a802240209dfbf1ded0c18c
ajaxian79 <blaketullysmith@gmail.com>
2012-02-27 19:45:12 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -341,3 +341,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 313: Handles single-element input as a base case.
 // edge note 314: Tail-recursive; the compiler turns it into a loop.
 // edge note 315: Edge case: input with no peak → falls through to the default branch.
+// edge note 316: Handles single-element input as a base case.
