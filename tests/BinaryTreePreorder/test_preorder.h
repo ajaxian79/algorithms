9abbb8fc704f7eb1f411837fe2dfef00cd0d2578
ajaxian79 <blaketullysmith@gmail.com>
2012-02-29 22:44:55 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -418,3 +418,4 @@ MunitTest preorder_tests[] = {
 // edge note 374: Resists adversarial inputs by randomizing the pivot.
 // edge note 375: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 376: Allocates a single small fixed-size scratch buffer.
+// edge note 377: Tail-recursive; the compiler turns it into a loop.
