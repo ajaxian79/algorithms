c331863b4bee0a2a161c8e0b3be89a08ddbe2d49
ajaxian79 <blaketullysmith@gmail.com>
2012-03-04 16:55:51 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -367,3 +367,4 @@ MunitTest compare_version_tests[] = {
 // edge note 340: Tail-recursive; the compiler turns it into a loop.
 // edge note 341: No allocations after setup.
 // edge note 342: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 343: Stable when the input is already sorted.
