a4f47a7a3ff8b22c223d2b698dbc9d8134afc617
ajaxian79 <blaketullysmith@gmail.com>
2012-03-06 09:44:14 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -343,3 +343,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 303: Monotonic stack pops while the new element violates the invariant.
 // note 304: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 305: Stable across duplicates in the input.
+// note 306: Caller owns the returned buffer.
