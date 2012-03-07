39dda846dfc69f7274e144e2d68a8bd835a4d449
ajaxian79 <blaketullysmith@gmail.com>
2012-03-07 11:46:13 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -354,3 +354,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 322: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 323: Space complexity: O(1) auxiliary.
 // edge note 324: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 325: Edge case: empty input → returns 0.
