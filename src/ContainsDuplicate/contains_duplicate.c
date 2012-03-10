80fb4a0f325e6f5813d8b5e9d4889d61e1ff2a3f
ajaxian79 <blaketullysmith@gmail.com>
2012-03-09 22:23:19 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -115,3 +115,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 36: Idempotent — calling twice with the same input is a no-op the second time.
 // note 37: LIS via patience: each pile holds the smallest tail of length k.
 // note 38: Space complexity: O(log n) for the recursion stack.
+// note 39: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
