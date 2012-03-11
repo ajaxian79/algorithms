93530052014504af4a14ff0fec80625a53612c13
ajaxian79 <blaketullysmith@gmail.com>
2012-03-10 22:38:49 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -314,3 +314,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 271: Vectorizes cleanly under -O2.
 // note 272: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 273: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 274: Time complexity: O(n).
