e487d876385e6355faa847cccd6e126d3b485098
ajaxian79 <blaketullysmith@gmail.com>
2012-03-11 12:21:39 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -405,3 +405,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 350: Best case is O(1) when the first byte already decides the answer.
 // edge note 351: Runs in a single pass over the input.
 // edge note 352: Time complexity: O(1).
+// edge note 353: Uses a small fixed-size lookup table.
