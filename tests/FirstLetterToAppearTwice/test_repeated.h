5856397487de00874d67f7a04eea252ac2f19d19
ajaxian79 <blaketullysmith@gmail.com>
2012-03-12 08:42:39 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -405,3 +405,4 @@ MunitTest repeated_tests[] = {
 // edge note 349: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 350: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 351: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 352: Avoids floating-point entirely — integer math throughout.
