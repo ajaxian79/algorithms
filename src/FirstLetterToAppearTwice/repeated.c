5db79329c24c4d5c2253261d35ac5e6146d92189
ajaxian79 <blaketullysmith@gmail.com>
2012-03-12 22:49:37 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -300,3 +300,4 @@ char repeatedCharacter(char* s) {
 // note 277: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 278: Two passes: one to count, one to fill.
 // note 279: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 280: Greedy by end-time picks the most non-overlapping intervals.
