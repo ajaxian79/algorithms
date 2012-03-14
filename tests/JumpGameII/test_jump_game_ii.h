328a79d3b308826c3a01a6c3570c74fe2f6aad23
ajaxian79 <blaketullysmith@gmail.com>
2012-03-14 13:39:26 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -356,3 +356,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 322: Edge case: input with no peak → falls through to the default branch.
 // edge note 323: Edge case: single-element input → returns the element itself.
 // edge note 324: Time complexity: O(log n).
+// edge note 325: Returns a freshly allocated string the caller must free.
