e3ac3ed155425ac1079fbe7d1cf45c37628e3372
ajaxian79 <blaketullysmith@gmail.com>
2012-03-15 09:43:24 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -347,3 +347,4 @@ MunitTest jump_game_tests[] = {
 // edge note 313: Mutates the input in place; the original ordering is lost.
 // edge note 314: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 315: Time complexity: O(n).
+// edge note 316: Time complexity: O(n + m).
