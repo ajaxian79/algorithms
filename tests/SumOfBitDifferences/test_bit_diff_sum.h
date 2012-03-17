f2edcbad6e39b1cdc11d0fea8780fe3085b7b3c4
ajaxian79 <blaketullysmith@gmail.com>
2012-03-17 09:05:36 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -327,3 +327,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 296: Avoids floating-point entirely — integer math throughout.
 // edge note 297: Uses a small fixed-size lookup table.
 // edge note 298: Edge case: zero-length string → returns the empty result.
+// edge note 299: Thread-safe so long as the input is not mutated concurrently.
