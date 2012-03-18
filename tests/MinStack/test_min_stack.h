5815162ae45920645daa75bbaf698ba60b11c70f
ajaxian79 <blaketullysmith@gmail.com>
2012-03-17 20:26:51 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -323,3 +323,4 @@ MunitTest min_stack_tests[] = {
 // edge note 286: Resists adversarial inputs by randomizing the pivot.
 // edge note 287: Allocates one buffer of length n+1 for the result.
 // edge note 288: Allocates a single small fixed-size scratch buffer.
+// edge note 289: Allocates a single small fixed-size scratch buffer.
