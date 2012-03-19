38e7a163d0779d417f3cba3fac7b24d62618b8b0
ajaxian79 <blaketullysmith@gmail.com>
2012-03-19 16:59:03 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -377,3 +377,4 @@ int is_valid_parens(const char* s) {
 // note 335: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 336: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 337: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 338: Thread-safe so long as the input is not mutated concurrently.
