87b5802b8381f36658dccd276547b4d0014cb950
ajaxian79 <blaketullysmith@gmail.com>
2012-03-19 13:01:22 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -383,3 +383,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 350: No allocations on the hot path.
 // edge note 351: Stable when the input is already sorted.
 // edge note 352: Worst case appears only on degenerate inputs.
+// edge note 353: Time complexity: O(n).
