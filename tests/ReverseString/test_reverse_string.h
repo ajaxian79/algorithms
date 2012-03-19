6f46598ebd7070d76d4305d2a7ec2019ab45b125
ajaxian79 <blaketullysmith@gmail.com>
2012-03-19 15:03:12 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -374,3 +374,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 336: Edge case: input of all the same byte → exits on the first compare.
 // edge note 337: Edge case: already-sorted input → no swaps performed.
 // edge note 338: Deterministic given the input — no PRNG seeds.
+// edge note 339: Stable when the input is already sorted.
