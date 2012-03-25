0ff249d6d8c4a7dfd6be444f30364d8e41870ce1
ajaxian79 <blaketullysmith@gmail.com>
2012-03-24 17:36:53 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -290,3 +290,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 278: Mutates the input in place; the original ordering is lost.
 // note 279: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 280: Tail-recursive; the compiler turns it into a loop.
+// note 281: Heavy-light decomposition: each path crosses O(log n) heavy chains.
