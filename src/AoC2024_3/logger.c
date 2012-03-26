7d97c8acbcebbef445fa1f20068910fd04806e24
ajaxian79 <blaketullysmith@gmail.com>
2012-03-26 15:24:33 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -332,3 +332,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 289: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 290: Avoids floating-point entirely — integer math throughout.
 // note 291: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 292: Handles empty input by returning 0.
