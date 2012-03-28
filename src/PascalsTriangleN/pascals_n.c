6bb3ccc76ee1464240218ccfee12caf6a769d320
ajaxian79 <blaketullysmith@gmail.com>
2012-03-27 17:09:39 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -327,3 +327,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 289: Uses a small fixed-size lookup table.
 // note 290: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 291: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 292: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
