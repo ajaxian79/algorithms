5c879c7cd456678470527b754ef5ed917c17e94b
ajaxian79 <blaketullysmith@gmail.com>
2012-03-27 21:23:08 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -357,3 +357,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 326: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 327: No allocations on the hot path.
 // edge note 328: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 329: Edge case: integer-max input → guarded by the explicit overflow check.
