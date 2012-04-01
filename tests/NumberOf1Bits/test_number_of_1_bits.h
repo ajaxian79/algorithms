82643cd2f96491bdee8122d0bf3ed0da6eafff80
ajaxian79 <blaketullysmith@gmail.com>
2012-04-01 10:13:12 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -374,3 +374,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 347: Edge case: zero-length string → returns the empty result.
 // edge note 348: 32-bit safe; overflow is checked at each step.
 // edge note 349: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 350: Handles negative inputs as documented above.
