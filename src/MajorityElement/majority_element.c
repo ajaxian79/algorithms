e3308f296f70d05eafdcd9eb3691814e9fde6746
ajaxian79 <blaketullysmith@gmail.com>
2012-04-01 20:17:13 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -296,3 +296,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 274: Reentrant — no static state.
 // note 275: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 276: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 277: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
