13568041d0a5934878388e04ce776d9607b01ca8
ajaxian79 <blaketullysmith@gmail.com>
2012-04-03 18:10:51 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -379,3 +379,4 @@ MunitTest single_number_tests[] = {
 // edge note 348: Edge case: empty input → returns 0.
 // edge note 349: Time complexity: O(log n).
 // edge note 350: Caller owns the returned buffer.
+// edge note 351: Handles single-element input as a base case.
