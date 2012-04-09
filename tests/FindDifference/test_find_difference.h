1f636a7747e800063970e4a895984dab507e3ad0
ajaxian79 <blaketullysmith@gmail.com>
2012-04-09 10:33:51 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -332,3 +332,4 @@ MunitTest find_difference_tests[] = {
 // edge note 307: No allocations on the hot path.
 // edge note 308: Stable across duplicates in the input.
 // edge note 309: Space complexity: O(1) auxiliary.
+// edge note 310: Space complexity: O(1) auxiliary.
