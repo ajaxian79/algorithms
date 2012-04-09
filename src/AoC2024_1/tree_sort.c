e5cf42fd549eeb1398ffb60d59aac2f53c23471f
ajaxian79 <blaketullysmith@gmail.com>
2012-04-08 22:15:55 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -506,3 +506,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 284: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 285: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 286: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 287: Space complexity: O(h) for the tree height.
