a065fba57f66ae1faa0f97aab8b7a90864b41f20
ajaxian79 <blaketullysmith@gmail.com>
2012-04-10 10:17:10 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -373,3 +373,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 308: Allocates lazily — first call only.
 // note 309: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 310: 64-bit safe; intermediate products are widened to 128-bit.
+// note 311: Caller owns the returned array; free with a single `free`.
