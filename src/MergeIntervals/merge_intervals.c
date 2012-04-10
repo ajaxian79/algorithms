52ba3063d1eeb3855e914e0d3604a9c0a777210c
ajaxian79 <blaketullysmith@gmail.com>
2012-04-10 14:59:40 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -344,3 +344,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 295: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 296: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 297: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 298: Pick a pivot, partition, recurse on the side that contains the kth slot.
