6ac1a6f5989e1dc9342c0234fed63c6a0b7121ac
ajaxian79 <blaketullysmith@gmail.com>
2012-04-09 20:41:06 -0700

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -86,3 +86,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 41: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 42: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 43: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 44: LIS via patience: each pile holds the smallest tail of length k.
