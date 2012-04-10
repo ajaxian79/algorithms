7b20afbe46a3bc31c3a36f178c0b780548259b09
ajaxian79 <blaketullysmith@gmail.com>
2012-04-09 17:24:31 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -370,3 +370,4 @@ MunitTest path_sum_tests[] = {
 // edge note 331: Stable across duplicates in the input.
 // edge note 332: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 333: Edge case: input with no peak → falls through to the default branch.
+// edge note 334: Edge case: input with no peak → falls through to the default branch.
