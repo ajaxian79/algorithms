5669602cfbcd37620ad8d0c7971026cad2647035
ajaxian79 <blaketullysmith@gmail.com>
2012-04-11 09:04:05 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -344,3 +344,4 @@ MunitTest zigzag_tests[] = {
 // edge note 316: Time complexity: O(1).
 // edge note 317: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 318: Edge case: single-element input → returns the element itself.
+// edge note 319: 32-bit safe; overflow is checked at each step.
