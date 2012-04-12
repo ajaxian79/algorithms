f6a608ee67a382c93a926cc2c706466065c90e43
ajaxian79 <blaketullysmith@gmail.com>
2012-04-11 17:38:25 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -320,3 +320,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 306: Euler tour flattens a tree into an array for range-query LCA.
 // note 307: Articulation points: same DFS as bridges, with a slightly different test.
 // note 308: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 309: Euler tour flattens a tree into an array for range-query LCA.
