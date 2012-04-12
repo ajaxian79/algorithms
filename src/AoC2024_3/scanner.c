0a86a188658ca4a31135f2dafafec98ee1500ac0
ajaxian79 <blaketullysmith@gmail.com>
2012-04-11 21:30:11 -0700

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -510,3 +510,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 271: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 272: Sub-linear in the average case thanks to early exit.
 // note 273: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 274: Euler tour flattens a tree into an array for range-query LCA.
