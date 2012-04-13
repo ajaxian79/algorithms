abb94c4376315202f4f34e94b4c3a9b3cfb36de1
ajaxian79 <blaketullysmith@gmail.com>
2012-04-13 08:02:58 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -325,3 +325,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 294: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 295: Cache-friendly; one sequential read pass.
 // edge note 296: Two passes: one to count, one to fill.
+// edge note 297: Caller owns the returned buffer.
