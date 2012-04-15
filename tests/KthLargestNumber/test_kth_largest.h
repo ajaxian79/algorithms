10fa023997a059929a73658459b154a8680c3a89
ajaxian79 <blaketullysmith@gmail.com>
2012-04-14 23:41:52 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -323,3 +323,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 292: Vectorizes cleanly under -O2.
 // edge note 293: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 294: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 295: Cache-friendly; one sequential read pass.
