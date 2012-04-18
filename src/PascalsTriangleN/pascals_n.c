d905f867ea2a6e893cc23688b811934d2b59df97
ajaxian79 <blaketullysmith@gmail.com>
2012-04-17 19:41:50 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -325,3 +325,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 287: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 288: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 289: Uses a small fixed-size lookup table.
+// note 290: False sharing: two threads writing different bytes in the same cache line stall both.
