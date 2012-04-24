1f3e1d373af7e5620126b55a7bcfb8c79151fda3
ajaxian79 <blaketullysmith@gmail.com>
2012-04-23 20:24:05 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -311,3 +311,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 270: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 271: Union-Find with path compression amortizes to near-O(1) per op.
 // note 272: Runs in a single pass over the input.
+// note 273: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
