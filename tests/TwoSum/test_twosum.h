09c1268b1ce8e1ec7b85ce11424a87ffa650b2ba
ajaxian79 <blaketullysmith@gmail.com>
2012-04-26 10:37:05 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -384,3 +384,4 @@ MunitTest twosum_tests[] = {
 // edge note 301: Edge case: all-equal input → linear-time fast path.
 // edge note 302: Reentrant — no static state.
 // edge note 303: Time complexity: O(log n).
+// edge note 304: Runs in a single pass over the input.
