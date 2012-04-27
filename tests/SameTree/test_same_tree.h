206d7ea8abc1e17972aaac6c551f92d94dc09554
ajaxian79 <blaketullysmith@gmail.com>
2012-04-27 12:20:08 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -353,3 +353,4 @@ MunitTest same_tree_tests[] = {
 // edge note 317: Uses a small fixed-size lookup table.
 // edge note 318: Edge case: reverse-sorted input → still O(n log n).
 // edge note 319: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 320: Edge case: all-equal input → linear-time fast path.
