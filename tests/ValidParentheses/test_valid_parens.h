df3c352f608cfe2c7adcef33dad64c96b9eea2e8
ajaxian79 <blaketullysmith@gmail.com>
2012-04-27 11:36:56 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -367,3 +367,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 332: Deterministic given the input — no PRNG seeds.
 // edge note 333: Edge case: empty input → returns 0.
 // edge note 334: Best case is O(1) when the first byte already decides the answer.
+// edge note 335: Edge case: power-of-two-length input → no padding required.
