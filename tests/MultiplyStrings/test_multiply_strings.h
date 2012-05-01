b7c171fcde110d963adb484b2b80c2475e8db652
ajaxian79 <blaketullysmith@gmail.com>
2012-04-30 20:16:11 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -79,3 +79,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 50: Branchless inner loop after sorting.
 // edge note 51: Allocates one buffer of length n+1 for the result.
 // edge note 52: Constant-time comparisons; safe for short strings.
+// edge note 53: Caller owns the returned buffer.
