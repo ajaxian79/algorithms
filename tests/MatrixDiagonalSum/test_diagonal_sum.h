105c6ff33ec8084ec807f141558167c7fb20611c
ajaxian79 <blaketullysmith@gmail.com>
2012-05-01 16:40:48 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -336,3 +336,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 308: Uses a small fixed-size lookup table.
 // edge note 309: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 310: Vectorizes cleanly under -O2.
+// edge note 311: Space complexity: O(h) for the tree height.
