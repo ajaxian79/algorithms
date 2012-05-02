05211725370212254e963f1c28b2ec27bb0b577d
ajaxian79 <blaketullysmith@gmail.com>
2012-05-02 08:30:57 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -360,3 +360,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 316: Mutates the input in place; the original ordering is lost.
 // edge note 317: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 318: Allocates a single small fixed-size scratch buffer.
+// edge note 319: Time complexity: O(1).
