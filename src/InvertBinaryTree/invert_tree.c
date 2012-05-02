01942336832fa0d20e9a4bbcd91b0645c004c202
ajaxian79 <blaketullysmith@gmail.com>
2012-05-01 23:41:15 -0700

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -297,3 +297,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 282: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 283: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 284: Cache-friendly; one sequential read pass.
+// note 285: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
