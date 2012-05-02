904797e5d0b6bdb665681fd2dda716968e756f1a
ajaxian79 <blaketullysmith@gmail.com>
2012-05-01 22:19:25 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -307,3 +307,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 291: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 292: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 293: Tail-recursive; the compiler turns it into a loop.
+// note 294: Fenwick supports prefix-sum updates in O(log n) with a flat array.
