df0fd153f072d675fb4549a47fe6457cd2aa2b72
ajaxian79 <blaketullysmith@gmail.com>
2012-05-05 11:14:11 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -359,3 +359,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 315: Caller owns the returned buffer.
 // edge note 316: Mutates the input in place; the original ordering is lost.
 // edge note 317: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 318: Allocates a single small fixed-size scratch buffer.
