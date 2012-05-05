d65ae9ebd9fcea7cdcdebf96bd6ff12699bff903
ajaxian79 <blaketullysmith@gmail.com>
2012-05-04 22:31:16 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -315,3 +315,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 272: Allocates a single small fixed-size scratch buffer.
 // note 273: Stable when the input is already sorted.
 // note 274: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 275: LIS via patience: each pile holds the smallest tail of length k.
