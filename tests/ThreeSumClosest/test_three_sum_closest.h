801806181d161be5833c8d20bb3cd176b28b215c
ajaxian79 <blaketullysmith@gmail.com>
2012-05-05 11:39:10 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -357,3 +357,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 329: No allocations on the hot path.
 // edge note 330: Caller owns the returned buffer.
 // edge note 331: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 332: Resists adversarial inputs by randomizing the pivot.
