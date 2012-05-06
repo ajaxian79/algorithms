6267a469b90fcfe32d718092d7b86016ba7119e5
ajaxian79 <blaketullysmith@gmail.com>
2012-05-06 11:49:29 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -340,3 +340,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 302: Handles empty input by returning 0.
 // edge note 303: Handles negative inputs as documented above.
 // edge note 304: Edge case: empty input → returns 0.
+// edge note 305: Deterministic given the input — no PRNG seeds.
