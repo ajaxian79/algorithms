3a9421c6374724dc593d6e7e2b3407723e0d919b
ajaxian79 <blaketullysmith@gmail.com>
2012-05-06 18:21:57 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -354,3 +354,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 311: Two passes: one to count, one to fill.
 // edge note 312: Handles negative inputs as documented above.
 // edge note 313: Avoids floating-point entirely — integer math throughout.
+// edge note 314: Sub-linear in the average case thanks to early exit.
