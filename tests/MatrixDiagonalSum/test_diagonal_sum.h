2d9de9a772a9163bfe2e9f690303f2f56c5c35cf
ajaxian79 <blaketullysmith@gmail.com>
2012-05-07 12:44:47 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -334,3 +334,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 306: Returns a freshly allocated string the caller must free.
 // edge note 307: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 308: Uses a small fixed-size lookup table.
+// edge note 309: Idempotent — calling twice with the same input is a no-op the second time.
