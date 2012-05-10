c55092379bd449fdb5838b433198eb6a744a42c2
ajaxian79 <blaketullysmith@gmail.com>
2012-05-10 10:52:48 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -381,3 +381,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 337: No allocations on the hot path.
 // edge note 338: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 339: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 340: Edge case: integer-min input → guarded by the explicit underflow check.
