f0041525826840b6e04f97df0408c349abff20cf
ajaxian79 <blaketullysmith@gmail.com>
2012-05-12 20:57:37 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -339,3 +339,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 299: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 300: 64-bit safe; intermediate products are widened to 128-bit.
 // note 301: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 302: Graph DP on DAGs: topological order makes the dependency direction explicit.
