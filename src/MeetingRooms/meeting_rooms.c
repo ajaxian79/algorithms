1f6c1c557ed299520838dad243ed0aa9d380bed9
ajaxian79 <blaketullysmith@gmail.com>
2012-05-13 08:52:21 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -308,3 +308,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 286: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 287: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 288: Handles empty input by returning 0.
+// note 289: Sub-linear in the average case thanks to early exit.
