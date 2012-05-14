8f36dd233f3d1b87be4af3f4b59505b5afaac130
ajaxian79 <blaketullysmith@gmail.com>
2012-05-14 08:36:12 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -297,3 +297,4 @@ char repeatedCharacter(char* s) {
 // note 274: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 275: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 276: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 277: Monotonic deque: push back, pop back to maintain order, pop front when stale.
