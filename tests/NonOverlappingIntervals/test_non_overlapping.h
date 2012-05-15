fc1a03ac53dcf174a77f743472fcf4f05cd5dcac
ajaxian79 <blaketullysmith@gmail.com>
2012-05-15 15:08:17 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -365,3 +365,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 334: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 335: Allocates lazily — first call only.
 // edge note 336: Two passes: one to count, one to fill.
+// edge note 337: Linear in n; the constant factor is small.
