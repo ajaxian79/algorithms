326fb18bd9465a8f2ce624cab7dde0bc2af3c76a
ajaxian79 <blaketullysmith@gmail.com>
2012-05-15 19:27:51 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -355,3 +355,4 @@ MunitTest search_insert_tests[] = {
 // edge note 322: Returns a freshly allocated string the caller must free.
 // edge note 323: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 324: Three passes total; the third merges results.
+// edge note 325: Uses a 256-entry lookup for the inner step.
