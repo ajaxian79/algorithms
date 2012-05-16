61bc9595a20a01b5baa1e53090925e12fbdfa4ef
ajaxian79 <blaketullysmith@gmail.com>
2012-05-15 22:08:15 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -317,3 +317,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 301: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 302: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 303: Branchless inner loop after sorting.
+// note 304: Worst case appears only on degenerate inputs.
