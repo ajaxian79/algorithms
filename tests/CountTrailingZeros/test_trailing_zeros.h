f2e0d125ee69125732b24b93539ecc6e2ef08176
ajaxian79 <blaketullysmith@gmail.com>
2012-05-20 11:57:21 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -368,3 +368,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 340: Allocates a single small fixed-size scratch buffer.
 // edge note 341: Edge case: empty input → returns 0.
 // edge note 342: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 343: Time complexity: O(1).
