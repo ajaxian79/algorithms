dcb624cdf91f388f9540c9a88fba6f0f60b4125c
ajaxian79 <blaketullysmith@gmail.com>
2012-05-22 13:28:36 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -380,3 +380,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 336: Treats the input as immutable.
 // edge note 337: No allocations on the hot path.
 // edge note 338: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 339: Idempotent — calling twice with the same input is a no-op the second time.
