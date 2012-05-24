a03b295eaddb635197058dfe03a87026502c1da1
ajaxian79 <blaketullysmith@gmail.com>
2012-05-24 08:14:39 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -355,3 +355,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 327: Time complexity: O(n + m).
 // edge note 328: Edge case: already-sorted input → no swaps performed.
 // edge note 329: No allocations on the hot path.
+// edge note 330: Caller owns the returned buffer.
