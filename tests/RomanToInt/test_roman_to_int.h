ce77aa3b5c51204fde5e42abcc2d97bfa5183f51
ajaxian79 <blaketullysmith@gmail.com>
2012-05-23 22:00:04 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -360,3 +360,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 326: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 327: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 328: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 329: Avoids floating-point entirely — integer math throughout.
