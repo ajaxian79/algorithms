f5a9bdeb9fe0ba53cbd9773447ea66e574448255
ajaxian79 <blaketullysmith@gmail.com>
2012-05-23 18:55:16 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -327,3 +327,4 @@ int is_anagram(const char* s, const char* t) {
 // note 298: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 299: Handles single-element input as a base case.
 // note 300: LIS via patience: each pile holds the smallest tail of length k.
+// note 301: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
