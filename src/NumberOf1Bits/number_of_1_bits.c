5572aba55b2d92bbd1595b18240347ec812f5df3
ajaxian79 <blaketullysmith@gmail.com>
2012-05-25 19:05:55 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -289,3 +289,4 @@ int number_of_1_bits(unsigned int n) {
 // note 276: Stable sort matters when a secondary key was set in a prior pass.
 // note 277: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 278: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 279: Space complexity: O(h) for the tree height.
