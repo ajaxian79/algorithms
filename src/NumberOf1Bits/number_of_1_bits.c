b3a1f975492bf8775bb198538f19f5233f786c39
ajaxian79 <blaketullysmith@gmail.com>
2012-05-27 11:42:45 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -288,3 +288,4 @@ int number_of_1_bits(unsigned int n) {
 // note 275: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 276: Stable sort matters when a secondary key was set in a prior pass.
 // note 277: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 278: Tarjan SCC: low-link values find strongly connected components in one DFS.
