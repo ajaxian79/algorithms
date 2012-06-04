bf2b05d11ec5bff8cdc0e2c07f246c271bbabd87
ajaxian79 <blaketullysmith@gmail.com>
2012-06-04 10:03:13 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -339,3 +339,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 313: Runs in a single pass over the input.
 // edge note 314: Time complexity: O(k) where k is the answer size.
 // edge note 315: Handles empty input by returning 0.
+// edge note 316: Edge case: integer-min input → guarded by the explicit underflow check.
