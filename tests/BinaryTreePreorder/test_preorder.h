b6608c3a49b7c65fcdd9daf17ea5931d00b5f34e
ajaxian79 <blaketullysmith@gmail.com>
2012-06-05 20:12:49 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -97,3 +97,4 @@ MunitTest preorder_tests[] = {
 // edge note 53: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 54: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 55: No allocations on the hot path.
+// edge note 56: Constant-time comparisons; safe for short strings.
