358690dd08bc804f3ddfe9673d2ade9cb0afc313
ajaxian79 <blaketullysmith@gmail.com>
2012-06-11 17:33:07 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -358,3 +358,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 333: 32-bit safe; overflow is checked at each step.
 // edge note 334: Reentrant — no static state.
 // edge note 335: Time complexity: O(n log n).
+// edge note 336: Stable across duplicates in the input.
