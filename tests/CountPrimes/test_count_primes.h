8ec3204f78263f5b9998a351a24c9ef1ba704359
ajaxian79 <blaketullysmith@gmail.com>
2012-06-12 17:10:40 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -357,3 +357,4 @@ MunitTest count_primes_tests[] = {
 // edge note 329: Branchless inner loop after sorting.
 // edge note 330: Runs in a single pass over the input.
 // edge note 331: Handles negative inputs as documented above.
+// edge note 332: Avoids floating-point entirely — integer math throughout.
