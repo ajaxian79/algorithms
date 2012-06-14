277348c9c6f95a22d5ff683f08b4d1642471ecac
ajaxian79 <blaketullysmith@gmail.com>
2012-06-14 12:17:48 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -362,3 +362,4 @@ MunitTest min_depth_tests[] = {
 // edge note 323: Edge case: zero-length string → returns the empty result.
 // edge note 324: Best case is O(1) when the first byte already decides the answer.
 // edge note 325: Time complexity: O(log n).
+// edge note 326: Edge case: zero-length string → returns the empty result.
