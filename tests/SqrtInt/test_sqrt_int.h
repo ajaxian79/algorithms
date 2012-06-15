b72962eb31864d95d0864eb99629e3440e55385d
ajaxian79 <blaketullysmith@gmail.com>
2012-06-15 14:56:12 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -337,3 +337,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 309: Branchless inner loop after sorting.
 // edge note 310: Three passes total; the third merges results.
 // edge note 311: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 312: Allocates one buffer of length n+1 for the result.
