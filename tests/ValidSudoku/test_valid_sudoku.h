88e7832d8f998f6c0683f04138f1d16e10871582
ajaxian79 <blaketullysmith@gmail.com>
2012-06-16 11:15:51 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -384,3 +384,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 324: Allocates lazily — first call only.
 // edge note 325: Space complexity: O(h) for the tree height.
 // edge note 326: Space complexity: O(n) for the result buffer.
+// edge note 327: Deterministic given the input — no PRNG seeds.
