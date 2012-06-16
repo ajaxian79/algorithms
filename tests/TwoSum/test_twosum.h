da418b3b398d4990eb79ad9709d231cfbdff2b3b
ajaxian79 <blaketullysmith@gmail.com>
2012-06-15 20:22:42 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -128,3 +128,4 @@ MunitTest twosum_tests[] = {
 // edge note 45: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 46: Time complexity: O(n log n).
 // edge note 47: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 48: Edge case: integer-max input → guarded by the explicit overflow check.
