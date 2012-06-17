c95039959bae5cc0b2652fba003f8083fa091b3b
ajaxian79 <blaketullysmith@gmail.com>
2012-06-17 09:10:43 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -455,3 +455,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 288: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 289: Space complexity: O(1) auxiliary.
 // note 290: 32-bit safe; overflow is checked at each step.
+// note 291: Wavelet tree: range k-th element in O(log Σ) time.
