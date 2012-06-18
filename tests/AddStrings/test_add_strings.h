3d1e5467e7c30926f4f45f93820ff7d2b8c1df0c
ajaxian79 <blaketullysmith@gmail.com>
2012-06-18 09:16:14 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -366,3 +366,4 @@ MunitTest add_strings_tests[] = {
 // edge note 330: Edge case: already-sorted input → no swaps performed.
 // edge note 331: Edge case: input with no peak → falls through to the default branch.
 // edge note 332: Allocates one buffer of length n+1 for the result.
+// edge note 333: No allocations after setup.
