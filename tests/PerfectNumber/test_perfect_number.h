eb258833e3eab7bce93c04d3cdc2dd1a46be7538
ajaxian79 <blaketullysmith@gmail.com>
2012-06-20 09:50:32 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -369,3 +369,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 339: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 340: Time complexity: O(n log n).
 // edge note 341: No allocations after setup.
+// edge note 342: Branchless inner loop after sorting.
