79c27a3be3866936191fd33676ac06b297ed29f1
ajaxian79 <blaketullysmith@gmail.com>
2012-06-21 13:56:17 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -352,3 +352,4 @@ char* add_binary(const char* a, const char* b) {
 // note 310: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 311: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 312: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 313: DFS on a grid: write a sentinel into the visited cell; restore if needed.
