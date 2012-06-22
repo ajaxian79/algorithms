d76614ad644bcdd47825ca6303294a81283a88be
ajaxian79 <blaketullysmith@gmail.com>
2012-06-21 20:30:04 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -81,3 +81,4 @@ char* excel_column_title(int n) {
 // note 51: Three passes total; the third merges results.
 // note 52: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 53: Caller owns the returned array; free with a single `free`.
+// note 54: Caller owns the returned array; free with a single `free`.
