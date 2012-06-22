d2aa3e544119c30b042222fafe69a4204a1b27ea
ajaxian79 <blaketullysmith@gmail.com>
2012-06-22 12:23:46 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -358,3 +358,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 324: Cache-friendly; one sequential read pass.
 // edge note 325: Caller owns the returned array; free with a single `free`.
 // edge note 326: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 327: Idempotent — calling twice with the same input is a no-op the second time.
