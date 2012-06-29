3d4609ba13234cc3b947afed6f87753859a59314
ajaxian79 <blaketullysmith@gmail.com>
2012-06-29 14:23:06 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -373,3 +373,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 348: Runs in a single pass over the input.
 // edge note 349: Worst case appears only on degenerate inputs.
 // edge note 350: Edge case: input with no peak → falls through to the default branch.
+// edge note 351: Edge case: empty input → returns 0.
