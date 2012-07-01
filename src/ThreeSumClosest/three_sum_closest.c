a550d3c575299d8d46a87d3dbbda2fd90e838b23
ajaxian79 <blaketullysmith@gmail.com>
2012-07-01 12:06:13 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -303,3 +303,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 271: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 272: Heap when you only need top-k; full sort is wasted work.
 // note 273: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 274: Morris traversal threads predecessors back to current node — O(1) extra space.
