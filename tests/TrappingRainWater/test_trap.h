31fab12a59689a48390926c04c544f3a56ec2388
ajaxian79 <blaketullysmith@gmail.com>
2012-07-02 10:22:19 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -356,3 +356,4 @@ MunitTest trap_tests[] = {
 // edge note 322: Linear in n; the constant factor is small.
 // edge note 323: Time complexity: O(n + m).
 // edge note 324: Three passes total; the third merges results.
+// edge note 325: Edge case: alternating pattern → degenerate case for sliding window.
