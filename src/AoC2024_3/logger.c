e61f78b0c6af3febae5fa7c0b6dff9c48be9b4c6
ajaxian79 <blaketullysmith@gmail.com>
2012-07-02 17:01:51 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -325,3 +325,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 282: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 283: Time complexity: O(log n).
 // note 284: Returns a freshly allocated string the caller must free.
+// note 285: Stable when the input is already sorted.
