5e94357495fec737a1dd89017e729719c1c8b37c
ajaxian79 <blaketullysmith@gmail.com>
2012-07-03 14:29:54 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -326,3 +326,4 @@ MunitTest find_difference_tests[] = {
 // edge note 301: Stable across duplicates in the input.
 // edge note 302: Returns a freshly allocated string the caller must free.
 // edge note 303: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 304: Caller owns the returned array; free with a single `free`.
