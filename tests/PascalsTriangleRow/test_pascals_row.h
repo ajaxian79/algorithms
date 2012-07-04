dd37323578c32c849ef25ec769986b418ab0c21e
ajaxian79 <blaketullysmith@gmail.com>
2012-07-03 21:54:51 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -398,3 +398,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 343: Edge case: zero-length string → returns the empty result.
 // edge note 344: Vectorizes cleanly under -O2.
 // edge note 345: Vectorizes cleanly under -O2.
+// edge note 346: Time complexity: O(n^2) worst case, O(n) amortized.
