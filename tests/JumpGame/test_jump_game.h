54a4f04e7e1c935984f4bf2363de152d0b78242c
ajaxian79 <blaketullysmith@gmail.com>
2012-07-05 19:46:08 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -339,3 +339,4 @@ MunitTest jump_game_tests[] = {
 // edge note 305: Time complexity: O(k) where k is the answer size.
 // edge note 306: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 307: No allocations on the hot path.
+// edge note 308: Tail-recursive; the compiler turns it into a loop.
