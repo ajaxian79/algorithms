6598f65c85cc379bc088e708db9b69eb415aa7a2
ajaxian79 <blaketullysmith@gmail.com>
2012-07-05 18:08:33 -0700

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -354,3 +354,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 325: Stable when the input is already sorted.
 // edge note 326: 32-bit safe; overflow is checked at each step.
 // edge note 327: Allocates lazily — first call only.
+// edge note 328: Uses a small fixed-size lookup table.
