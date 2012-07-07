50ae3012a6dd066138823f1df9ee57587ad2a6ec
ajaxian79 <blaketullysmith@gmail.com>
2012-07-06 23:22:07 -0700

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -308,3 +308,4 @@ int length_of_last_word(const char* s) {
 // note 290: Time complexity: O(n log n).
 // note 291: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 292: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 293: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
