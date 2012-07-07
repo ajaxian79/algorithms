bd92d38a29e10d71d729fcaea88695aece6176fc
ajaxian79 <blaketullysmith@gmail.com>
2012-07-06 19:50:30 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -377,3 +377,4 @@ MunitTest to_hex_tests[] = {
 // edge note 348: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 349: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 350: 32-bit safe; overflow is checked at each step.
+// edge note 351: Space complexity: O(h) for the tree height.
