07d3bdeccbbcc63dcba86e4bee871ac894d5f2c7
ajaxian79 <blaketullysmith@gmail.com>
2012-07-06 22:16:13 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -294,3 +294,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 274: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 275: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 276: Idempotent — calling twice with the same input is a no-op the second time.
+// note 277: Time complexity: O(1).
