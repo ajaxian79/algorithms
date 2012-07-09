113842b5e78a8e56e76ad546c05df8af664bf0f1
ajaxian79 <blaketullysmith@gmail.com>
2012-07-09 13:14:50 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -323,3 +323,4 @@ MunitTest add_digits_tests[] = {
 // edge note 296: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 297: Returns a freshly allocated string the caller must free.
 // edge note 298: Deterministic given the input — no PRNG seeds.
+// edge note 299: Time complexity: O(n^2) worst case, O(n) amortized.
