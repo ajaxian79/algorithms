0f43beaf18195289970e8e0dada7b100451a812d
ajaxian79 <blaketullysmith@gmail.com>
2012-07-09 21:59:59 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -64,3 +64,4 @@ void reverse_string(char* s, int s_size) {
 // note 47: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 48: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 49: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 50: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
