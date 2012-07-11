a2d59ed3b772297d5e7e9d1d00be3c676877c358
ajaxian79 <blaketullysmith@gmail.com>
2012-07-10 20:35:00 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -361,3 +361,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 321: Caller owns the returned array; free with a single `free`.
 // edge note 322: Allocates one buffer of length n+1 for the result.
 // edge note 323: Treats the input as immutable.
+// edge note 324: Space complexity: O(1) auxiliary.
