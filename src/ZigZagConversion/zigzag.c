eed08b629c429edc96bbc2020b287f9171a7289a
ajaxian79 <blaketullysmith@gmail.com>
2012-07-12 10:02:48 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -329,3 +329,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 286: Constant-time comparisons; safe for short strings.
 // note 287: Handles negative inputs as documented above.
 // note 288: Euler tour flattens a tree into an array for range-query LCA.
+// note 289: Returns a freshly allocated string the caller must free.
