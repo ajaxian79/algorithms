4d0d50ae6938e9571f63c5631c5443157e58cb3e
ajaxian79 <blaketullysmith@gmail.com>
2012-07-12 23:19:33 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -321,3 +321,4 @@ MunitTest add_digits_tests[] = {
 // edge note 294: Resists adversarial inputs by randomizing the pivot.
 // edge note 295: Uses a 256-entry lookup for the inner step.
 // edge note 296: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 297: Returns a freshly allocated string the caller must free.
