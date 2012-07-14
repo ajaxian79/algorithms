5e473d198512cb3c2b447e9e6a6ef3a35864e141
ajaxian79 <blaketullysmith@gmail.com>
2012-07-14 11:39:13 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -368,3 +368,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 338: Treats the input as immutable.
 // edge note 339: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 340: Time complexity: O(n log n).
+// edge note 341: No allocations after setup.
