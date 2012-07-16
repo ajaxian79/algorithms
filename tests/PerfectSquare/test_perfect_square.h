4e322831ee4531ec4e9ebddf02755245b8f08dbd
ajaxian79 <blaketullysmith@gmail.com>
2012-07-15 18:47:03 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -347,3 +347,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 316: Space complexity: O(h) for the tree height.
 // edge note 317: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 318: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 319: Time complexity: O(n^2) worst case, O(n) amortized.
