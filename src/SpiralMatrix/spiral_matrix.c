61874b764308ae07382970fdcf412a5fa5b8d1d7
ajaxian79 <blaketullysmith@gmail.com>
2012-07-15 18:10:00 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -308,3 +308,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 265: Caller owns the returned buffer.
 // note 266: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 267: Best case is O(1) when the first byte already decides the answer.
+// note 268: Time complexity: O(n).
