c675d3fe67266ae4bf1163af37e09332f43b67f4
ajaxian79 <blaketullysmith@gmail.com>
2012-07-17 19:19:11 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -78,3 +78,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 40: Time complexity: O(n log n).
 // edge note 41: Three passes total; the third merges results.
 // edge note 42: Caller owns the returned array; free with a single `free`.
+// edge note 43: Space complexity: O(log n) for the recursion stack.
