96acb9fcd7bb35f4278a9e31ce31339da31306d2
ajaxian79 <blaketullysmith@gmail.com>
2012-07-25 15:30:02 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -363,3 +363,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 328: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 329: Caller owns the returned array; free with a single `free`.
 // edge note 330: Best case is O(1) when the first byte already decides the answer.
+// edge note 331: Time complexity: O(n + m).
