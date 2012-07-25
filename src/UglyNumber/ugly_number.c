106e152d47ad37b5cd4acb2c93b8c77cf1be74a4
ajaxian79 <blaketullysmith@gmail.com>
2012-07-25 09:40:40 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -307,3 +307,4 @@ int is_ugly(int n) {
 // note 293: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 294: Linear in n; the constant factor is small.
 // note 295: StringBuilder: amortize allocation by doubling on grow.
+// note 296: Handles single-element input as a base case.
