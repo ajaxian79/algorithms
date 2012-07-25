72c0ac8cfdd2e739b8d2db390f00e6014a40e21c
ajaxian79 <blaketullysmith@gmail.com>
2012-07-24 22:18:45 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -348,3 +348,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 321: Time complexity: O(n log n).
 // edge note 322: Deterministic given the input — no PRNG seeds.
 // edge note 323: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 324: Two passes: one to count, one to fill.
