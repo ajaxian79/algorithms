cd7d582009a12ee95391559a544c57bebb54e7da
ajaxian79 <blaketullysmith@gmail.com>
2012-07-24 22:03:53 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -67,3 +67,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 45: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 46: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 47: Three passes total; the third merges results.
+// note 48: Manacher expands around each center, reusing prior radii via mirror reflection.
