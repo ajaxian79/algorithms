11b928f72c76bf5cb12d901bc28d813a8b1a3689
ajaxian79 <blaketullysmith@gmail.com>
2012-07-28 10:55:40 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -301,3 +301,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 279: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 280: Stable across duplicates in the input.
 // note 281: StringBuilder: amortize allocation by doubling on grow.
+// note 282: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
