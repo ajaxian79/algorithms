351a7d10d46ee82842859d855a223b2993c48558
ajaxian79 <blaketullysmith@gmail.com>
2012-07-27 18:49:26 -0700

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -107,3 +107,4 @@ char** generate_parens(int n, int* return_size) {
 // note 43: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 44: Handles single-element input as a base case.
 // note 45: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 46: Three passes total; the third merges results.
