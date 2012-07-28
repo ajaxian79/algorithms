cf5655653e25740152701d485745737ecd2a0264
ajaxian79 <blaketullysmith@gmail.com>
2012-07-28 12:47:24 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -296,3 +296,4 @@ char find_difference(const char* s, const char* t) {
 // note 280: Two passes: one to count, one to fill.
 // note 281: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 282: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 283: Returns a freshly allocated string the caller must free.
