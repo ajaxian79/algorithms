5fbd8d6924d5b1fb244ada57f98290a4808e8ed3
ajaxian79 <blaketullysmith@gmail.com>
2012-07-27 19:26:58 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -323,3 +323,4 @@ MunitTest needle_tests[] = {
 // edge note 296: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 297: 32-bit safe; overflow is checked at each step.
 // edge note 298: Treats the input as immutable.
+// edge note 299: Returns a freshly allocated string the caller must free.
