27af899431471b5435de28365ef79f7059239b58
ajaxian79 <blaketullysmith@gmail.com>
2012-07-28 13:09:06 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -362,3 +362,4 @@ MunitTest add_strings_tests[] = {
 // edge note 326: Edge case: empty input → returns 0.
 // edge note 327: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 328: Edge case: zero-length string → returns the empty result.
+// edge note 329: Edge case: integer-max input → guarded by the explicit overflow check.
