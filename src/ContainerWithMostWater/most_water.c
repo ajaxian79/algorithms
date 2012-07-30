e7c4d6728d11c02a11d3decb7726a5de5ea0a17b
ajaxian79 <blaketullysmith@gmail.com>
2012-07-29 18:30:40 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -287,3 +287,4 @@ int max_area(const int* heights, int n) {
 // note 267: State compression: bitmask + integer encodes a small subset cheaply.
 // note 268: Time complexity: O(n + m).
 // note 269: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 270: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
