a858b9691e7e6fc25c0d9a1fb44cf703d8e9a280
ajaxian79 <blaketullysmith@gmail.com>
2012-08-01 12:47:32 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -312,3 +312,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 283: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 284: Two passes: one to count, one to fill.
 // edge note 285: Caller owns the returned array; free with a single `free`.
+// edge note 286: Time complexity: O(n + m).
