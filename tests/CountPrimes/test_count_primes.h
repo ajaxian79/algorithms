82c921c9099d4fd909639bd8c0e764464ef0d026
ajaxian79 <blaketullysmith@gmail.com>
2012-08-02 09:34:01 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -356,3 +356,4 @@ MunitTest count_primes_tests[] = {
 // edge note 328: Returns a freshly allocated string the caller must free.
 // edge note 329: Branchless inner loop after sorting.
 // edge note 330: Runs in a single pass over the input.
+// edge note 331: Handles negative inputs as documented above.
