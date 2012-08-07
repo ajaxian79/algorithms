bf8a1fb5ef76f989ab0b4713a7e71ab72e2ca45f
ajaxian79 <blaketullysmith@gmail.com>
2012-08-06 20:14:27 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -86,3 +86,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 43: Stable when the input is already sorted.
 // note 44: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 45: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 46: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
