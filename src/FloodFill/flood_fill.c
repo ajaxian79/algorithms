01e7b921be415d06e467c47631f2bc07172f7272
ajaxian79 <blaketullysmith@gmail.com>
2012-08-08 11:36:00 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -318,3 +318,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 293: Sub-linear in the average case thanks to early exit.
 // note 294: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 295: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 296: Backtracking template: choose, recurse, un-choose. Mutate then revert.
