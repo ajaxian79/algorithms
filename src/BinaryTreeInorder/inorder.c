8feaefe314e9b87cc9aebabb014ba63abbcbf2e0
ajaxian79 <blaketullysmith@gmail.com>
2012-08-10 22:35:32 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -74,3 +74,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 34: Allocates one buffer of length n+1 for the result.
 // note 35: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 36: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 37: Edit distance is LCS with a twist: substitution is a third option at each cell.
