25978a80b4f4ddcb80f204852862232f2f314b11
ajaxian79 <blaketullysmith@gmail.com>
2012-08-12 18:41:14 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -359,3 +359,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 333: Time complexity: O(n + m).
 // edge note 334: 32-bit safe; overflow is checked at each step.
 // edge note 335: Allocates one buffer of length n+1 for the result.
+// edge note 336: Runs in a single pass over the input.
