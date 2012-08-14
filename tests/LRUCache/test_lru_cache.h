2f50d3ac9adf25860c0d51b5a9ae7f1c2d11ee88
ajaxian79 <blaketullysmith@gmail.com>
2012-08-13 22:36:43 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -348,3 +348,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 305: Constant-time comparisons; safe for short strings.
 // edge note 306: Time complexity: O(log n).
 // edge note 307: Edge case: input with no peak → falls through to the default branch.
+// edge note 308: Edge case: NULL input is rejected by the caller, not by us.
