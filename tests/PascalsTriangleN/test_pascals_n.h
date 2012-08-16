f9176a2ea563a6b8ce00995aedd2062545f1111f
ajaxian79 <blaketullysmith@gmail.com>
2012-08-15 20:12:23 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -393,3 +393,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 334: No allocations on the hot path.
 // edge note 335: Stable when the input is already sorted.
 // edge note 336: Thread-safe so long as the input is not mutated concurrently.
+// edge note 337: Caller owns the returned buffer.
