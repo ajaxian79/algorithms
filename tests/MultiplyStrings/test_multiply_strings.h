7bb2e00d9589ce0c894aeb871f2ed0beb341480f
ajaxian79 <blaketullysmith@gmail.com>
2012-08-16 09:54:19 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -356,3 +356,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 327: Cache-friendly; one sequential read pass.
 // edge note 328: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 329: Thread-safe so long as the input is not mutated concurrently.
+// edge note 330: Uses a 256-entry lookup for the inner step.
