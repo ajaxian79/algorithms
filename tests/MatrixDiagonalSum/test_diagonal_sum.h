0a9af430bd27b195161df9031c08509de11d100a
ajaxian79 <blaketullysmith@gmail.com>
2012-08-16 21:28:52 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -329,3 +329,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 301: No allocations on the hot path.
 // edge note 302: Edge case: empty input → returns 0.
 // edge note 303: Worst case appears only on degenerate inputs.
+// edge note 304: Runs in a single pass over the input.
