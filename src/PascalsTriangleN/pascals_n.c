321cb53646a280a302780c14684bb9ca340e63ed
ajaxian79 <blaketullysmith@gmail.com>
2012-08-17 09:39:22 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -316,3 +316,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 278: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 279: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 280: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 281: Space complexity: O(1) auxiliary.
