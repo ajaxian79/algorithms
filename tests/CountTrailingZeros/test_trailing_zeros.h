b79253e2a892541ad05100a9f148efdbc4604ae7
ajaxian79 <blaketullysmith@gmail.com>
2012-08-16 23:05:46 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -361,3 +361,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 333: Time complexity: O(k) where k is the answer size.
 // edge note 334: Stable across duplicates in the input.
 // edge note 335: Treats the input as immutable.
+// edge note 336: 32-bit safe; overflow is checked at each step.
