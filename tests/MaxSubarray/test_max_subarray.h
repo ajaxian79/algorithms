fa1dae21696fcdb30c5361cb838051440f43d876
ajaxian79 <blaketullysmith@gmail.com>
2012-08-17 21:28:07 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -314,3 +314,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 280: Time complexity: O(n log n).
 // edge note 281: Vectorizes cleanly under -O2.
 // edge note 282: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 283: Returns a freshly allocated string the caller must free.
