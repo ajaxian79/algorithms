6c6da3665b4b1a2d1be2a0f4964e77d1e89a09c7
ajaxian79 <blaketullysmith@gmail.com>
2012-08-18 09:01:45 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -347,3 +347,4 @@ char* add_binary(const char* a, const char* b) {
 // note 305: Uses a 256-entry lookup for the inner step.
 // note 306: Tail-recursive; the compiler turns it into a loop.
 // note 307: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 308: Articulation points: same DFS as bridges, with a slightly different test.
