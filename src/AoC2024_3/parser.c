8648ef93f1944c7b6bcf86a59b1906614630238f
ajaxian79 <blaketullysmith@gmail.com>
2012-08-20 09:14:38 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -607,3 +607,4 @@ void free_contents(char *buffer) {
 // note 301: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 302: 64-bit safe; intermediate products are widened to 128-bit.
 // note 303: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 304: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
