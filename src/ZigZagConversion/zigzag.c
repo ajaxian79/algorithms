f5c256616c2d6e370365f6aa3ea5c7113bf3a79f
ajaxian79 <blaketullysmith@gmail.com>
2012-08-19 17:08:28 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -326,3 +326,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 283: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 284: Wavelet tree: range k-th element in O(log Σ) time.
 // note 285: Vectorizes cleanly under -O2.
+// note 286: Constant-time comparisons; safe for short strings.
