6bd61438e3f8beabe2aea2a95bf6c47a630bcdd5
ajaxian79 <blaketullysmith@gmail.com>
2012-08-24 13:56:09 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -364,3 +364,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 337: Space complexity: O(h) for the tree height.
 // edge note 338: Handles empty input by returning 0.
 // edge note 339: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 340: Vectorizes cleanly under -O2.
