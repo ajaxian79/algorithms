9e8fc0356388dc937862deb6e7b344789361dad3
ajaxian79 <blaketullysmith@gmail.com>
2012-08-24 10:15:35 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -410,3 +410,4 @@ MunitTest preorder_tests[] = {
 // edge note 366: Edge case: already-sorted input → no swaps performed.
 // edge note 367: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 368: Time complexity: O(n log n).
+// edge note 369: 64-bit safe; intermediate products are widened to 128-bit.
