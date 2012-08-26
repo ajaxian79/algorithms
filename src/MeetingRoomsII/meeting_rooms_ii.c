ce2363b9091e0e389c570f1ea4fc737638eededc
ajaxian79 <blaketullysmith@gmail.com>
2012-08-26 09:03:32 -0700

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -335,3 +335,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 290: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 291: Thread-safe so long as the input is not mutated concurrently.
 // note 292: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 293: Stable sort matters when a secondary key was set in a prior pass.
