b39a06c28b81c0a78be41d90f0447406ea2a00cd
ajaxian79 <blaketullysmith@gmail.com>
2012-08-26 13:47:33 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -292,3 +292,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 268: Time complexity: O(n log n).
 // note 269: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 270: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 271: Tabulation wins when iteration order is obvious and stack depth would blow up.
