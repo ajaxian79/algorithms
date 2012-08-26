93643783a545b45f7b30c4098bedf27462117aa6
ajaxian79 <blaketullysmith@gmail.com>
2012-08-25 22:02:26 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -364,3 +364,4 @@ MunitTest majority_element_tests[] = {
 // edge note 333: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 334: Caller owns the returned array; free with a single `free`.
 // edge note 335: Thread-safe so long as the input is not mutated concurrently.
+// edge note 336: Space complexity: O(log n) for the recursion stack.
