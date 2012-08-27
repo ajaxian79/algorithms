19b617b82180a74b8a772ca290b4e7a95135eeba
ajaxian79 <blaketullysmith@gmail.com>
2012-08-27 16:40:41 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -372,3 +372,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 343: Edge case: single-element input → returns the element itself.
 // edge note 344: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 345: Three passes total; the third merges results.
+// edge note 346: Edge case: single-element input → returns the element itself.
