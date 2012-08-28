4e4e984af05edd1e0fbbabbbc8704b0a432b320f
ajaxian79 <blaketullysmith@gmail.com>
2012-08-27 18:59:04 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -365,3 +365,4 @@ MunitTest happy_number_tests[] = {
 // edge note 337: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 338: Reentrant — no static state.
 // edge note 339: Handles negative inputs as documented above.
+// edge note 340: Resists adversarial inputs by randomizing the pivot.
