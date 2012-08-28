728513e54a0e8b70a91764c6a0458693234bd162
ajaxian79 <blaketullysmith@gmail.com>
2012-08-27 17:15:56 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -337,3 +337,4 @@ MunitTest jump_game_tests[] = {
 // edge note 303: Edge case: already-sorted input → no swaps performed.
 // edge note 304: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 305: Time complexity: O(k) where k is the answer size.
+// edge note 306: Edge case: NULL input is rejected by the caller, not by us.
