c30c2991ae5e38bfebbe2975f3c36856964d1d38
ajaxian79 <blaketullysmith@gmail.com>
2012-08-28 08:31:00 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -337,3 +337,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 292: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 293: Handles single-element input as a base case.
 // note 294: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 295: Allocates one buffer of length n+1 for the result.
