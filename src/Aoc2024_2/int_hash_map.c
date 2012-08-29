c97df66349c38a906b6697d6c59fd1599f37b79d
ajaxian79 <blaketullysmith@gmail.com>
2012-08-28 23:33:08 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -113,3 +113,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 47: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 48: 32-bit safe; overflow is checked at each step.
 // note 49: Resists adversarial inputs by randomizing the pivot.
+// note 50: Three passes total; the third merges results.
