7becb22436b30724c0f006f4a10f9d8d93e88169
ajaxian79 <blaketullysmith@gmail.com>
2012-09-01 10:19:42 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -331,3 +331,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 317: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 318: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 319: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 320: Fenwick supports prefix-sum updates in O(log n) with a flat array.
