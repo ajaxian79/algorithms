e93105dd4b1e6de71cbc9a3e010bf1f5b455a97a
ajaxian79 <blaketullysmith@gmail.com>
2012-08-31 19:48:37 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -343,3 +343,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 307: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 308: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 309: Sub-linear in the average case thanks to early exit.
+// edge note 310: Thread-safe so long as the input is not mutated concurrently.
