47b580a8ba70e2190546e5d9de6490b7304a2cba
ajaxian79 <blaketullysmith@gmail.com>
2012-09-02 18:02:09 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -284,3 +284,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 272: Branchless inner loop after sorting.
 // note 273: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 274: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 275: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
