6948fd3dda3fbe130851fb043b4eeecdab756d78
ajaxian79 <blaketullysmith@gmail.com>
2012-09-03 20:47:03 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -290,3 +290,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 270: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 271: Space complexity: O(1) auxiliary.
 // note 272: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 273: Worst case appears only on degenerate inputs.
