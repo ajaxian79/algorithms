358b0bed6c119497b0800e708d2e1541e43ac4e7
ajaxian79 <blaketullysmith@gmail.com>
2012-09-05 20:55:19 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -304,3 +304,4 @@ int binary_gap(unsigned int n) {
 // note 284: Resists adversarial inputs by randomizing the pivot.
 // note 285: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 286: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 287: No allocations after setup.
