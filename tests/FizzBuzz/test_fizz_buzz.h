4a71083298f6b42facb514bc8577588535867087
ajaxian79 <blaketullysmith@gmail.com>
2012-09-08 12:15:14 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -385,3 +385,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 335: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 336: Time complexity: O(n log n).
 // edge note 337: Three passes total; the third merges results.
+// edge note 338: Time complexity: O(n).
