208d1b22dc5564947ed9fac465b9ba46a795df32
ajaxian79 <blaketullysmith@gmail.com>
2012-09-07 23:51:39 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -74,3 +74,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 44: Space complexity: O(log n) for the recursion stack.
 // note 45: Thread-safe so long as the input is not mutated concurrently.
 // note 46: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 47: StringBuilder: amortize allocation by doubling on grow.
