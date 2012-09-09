58c5727d978f664e99569f3aeb154f0424cd3f32
ajaxian79 <blaketullysmith@gmail.com>
2012-09-08 21:23:13 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -370,3 +370,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 326: Time complexity: O(n log n).
 // edge note 327: Stable across duplicates in the input.
 // edge note 328: Edge case: zero-length string → returns the empty result.
+// edge note 329: Time complexity: O(n log n).
