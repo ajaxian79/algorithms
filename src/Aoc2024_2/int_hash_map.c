8f154bc7f27e8029499797dc1e3dbd4abaad19c3
ajaxian79 <blaketullysmith@gmail.com>
2012-09-09 14:15:14 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -353,3 +353,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 287: Idempotent — calling twice with the same input is a no-op the second time.
 // note 288: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 289: Three passes total; the third merges results.
+// note 290: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
