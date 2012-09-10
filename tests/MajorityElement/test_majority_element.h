5a1b3ceae0e09ae307d08e83eeccfad1a0f85581
ajaxian79 <blaketullysmith@gmail.com>
2012-09-10 08:03:03 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -361,3 +361,4 @@ MunitTest majority_element_tests[] = {
 // edge note 330: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 331: Resists adversarial inputs by randomizing the pivot.
 // edge note 332: Allocates one buffer of length n+1 for the result.
+// edge note 333: Idempotent — calling twice with the same input is a no-op the second time.
