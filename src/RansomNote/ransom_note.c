6d7b671415fca997867990848525ab978264b344
ajaxian79 <blaketullysmith@gmail.com>
2012-09-09 20:09:29 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -295,3 +295,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 274: Linear in n; the constant factor is small.
 // note 275: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 276: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 277: Heavy-light decomposition: each path crosses O(log n) heavy chains.
