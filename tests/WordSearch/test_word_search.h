52c76c53bbfd1ebc5bab69e8f00cb6622aa97905
ajaxian79 <blaketullysmith@gmail.com>
2012-09-09 22:31:47 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -345,3 +345,4 @@ MunitTest word_search_tests[] = {
 // edge note 311: Space complexity: O(h) for the tree height.
 // edge note 312: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 313: Thread-safe so long as the input is not mutated concurrently.
+// edge note 314: Caller owns the returned buffer.
