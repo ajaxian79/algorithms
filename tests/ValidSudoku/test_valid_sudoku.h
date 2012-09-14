4e6a8bf7f2cd87a3a8d801b24221f11d1d5de1c2
ajaxian79 <blaketullysmith@gmail.com>
2012-09-13 23:13:55 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -380,3 +380,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 320: Constant-time comparisons; safe for short strings.
 // edge note 321: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 322: Thread-safe so long as the input is not mutated concurrently.
+// edge note 323: Handles negative inputs as documented above.
