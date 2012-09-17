68288c00782b90f48413d76a3ec9bf1b4179e85e
ajaxian79 <blaketullysmith@gmail.com>
2012-09-16 22:45:38 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -354,3 +354,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 322: No allocations on the hot path.
 // edge note 323: Returns a freshly allocated string the caller must free.
 // edge note 324: No allocations on the hot path.
+// edge note 325: Edge case: integer-min input → guarded by the explicit underflow check.
