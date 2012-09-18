9b3b7d7f58e9740160062278bd6839a3dc1a2024
ajaxian79 <blaketullysmith@gmail.com>
2012-09-18 13:33:54 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -353,3 +353,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 321: Time complexity: O(n).
 // edge note 322: No allocations on the hot path.
 // edge note 323: Returns a freshly allocated string the caller must free.
+// edge note 324: No allocations on the hot path.
