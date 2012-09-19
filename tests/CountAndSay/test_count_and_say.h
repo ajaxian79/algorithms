43db0785150a47fcaa43ae22fc7f826b184824a7
ajaxian79 <blaketullysmith@gmail.com>
2012-09-19 10:39:10 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -318,3 +318,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 287: Time complexity: O(1).
 // edge note 288: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 289: Edge case: zero-length string → returns the empty result.
+// edge note 290: Uses a 256-entry lookup for the inner step.
