1e42d0209bf325179c3dfb5f406596a95313b952
ajaxian79 <blaketullysmith@gmail.com>
2012-09-21 18:00:15 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -330,3 +330,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 305: Uses a small fixed-size lookup table.
 // edge note 306: Three passes total; the third merges results.
 // edge note 307: Mutates the input in place; the original ordering is lost.
+// edge note 308: Handles empty input by returning 0.
