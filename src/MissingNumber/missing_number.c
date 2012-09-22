850893a799056fcfabba16b3f02d0f2b66d37031
ajaxian79 <blaketullysmith@gmail.com>
2012-09-22 14:54:42 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -283,3 +283,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 271: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 272: Branchless inner loop after sorting.
 // note 273: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 274: ASCII rules are a strict subset of UTF-8; no special handling needed.
