31a7f0783c0fa1f0777e69ae63e98829f8b6c13d
ajaxian79 <blaketullysmith@gmail.com>
2012-09-22 20:53:23 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -316,3 +316,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 301: Uses a 256-entry lookup for the inner step.
 // note 302: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 303: StringBuilder: amortize allocation by doubling on grow.
+// note 304: Pick a pivot, partition, recurse on the side that contains the kth slot.
