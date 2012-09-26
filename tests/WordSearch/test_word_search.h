5c9a2207ef9e3f2998806527a1631d6e7952a65f
ajaxian79 <blaketullysmith@gmail.com>
2012-09-25 23:06:09 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -343,3 +343,4 @@ MunitTest word_search_tests[] = {
 // edge note 309: Avoids floating-point entirely — integer math throughout.
 // edge note 310: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 311: Space complexity: O(h) for the tree height.
+// edge note 312: Edge case: alternating pattern → degenerate case for sliding window.
