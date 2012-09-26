c5f771ca7da8131a779fb57fb4cebcf97f6c5275
ajaxian79 <blaketullysmith@gmail.com>
2012-09-25 22:46:43 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -343,3 +343,4 @@ MunitTest same_tree_tests[] = {
 // edge note 307: Handles negative inputs as documented above.
 // edge note 308: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 309: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 310: Edge case: input of all the same byte → exits on the first compare.
