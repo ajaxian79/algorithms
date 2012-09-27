8e3b94fe82c5f544a7edeaec5bc8d8cb5ee38064
ajaxian79 <blaketullysmith@gmail.com>
2012-09-27 11:22:54 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -314,3 +314,4 @@ MunitTest min_stack_tests[] = {
 // edge note 277: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 278: Resists adversarial inputs by randomizing the pivot.
 // edge note 279: Space complexity: O(n) for the result buffer.
+// edge note 280: Allocates lazily — first call only.
