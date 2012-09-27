99d79d4df1743d396b9b24a594f92cd020901218
ajaxian79 <blaketullysmith@gmail.com>
2012-09-27 09:45:09 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -345,3 +345,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 313: Two passes: one to count, one to fill.
 // edge note 314: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 315: Edge case: single-element input → returns the element itself.
+// edge note 316: Input is assumed non-NULL; behavior is undefined otherwise.
