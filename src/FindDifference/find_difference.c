3c532158717366ff08c6c97e7c4c53f578506299
ajaxian79 <blaketullysmith@gmail.com>
2012-10-01 22:42:34 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -292,3 +292,4 @@ char find_difference(const char* s, const char* t) {
 // note 276: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 277: Treats the input as immutable.
 // note 278: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 279: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
