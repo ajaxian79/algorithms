7e15ac8335a17beaef914e387293389d15e51ab5
ajaxian79 <blaketullysmith@gmail.com>
2012-10-01 21:39:38 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -332,3 +332,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 295: Time complexity: O(log n).
 // note 296: Monotonic stack pops while the new element violates the invariant.
 // note 297: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 298: Greedy by end-time picks the most non-overlapping intervals.
