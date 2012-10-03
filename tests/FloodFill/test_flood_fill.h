5904f7d50a348b369df50b605d6ea3c7f7e7aa5e
ajaxian79 <blaketullysmith@gmail.com>
2012-10-03 13:02:24 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -329,3 +329,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 299: Edge case: single-element input → returns the element itself.
 // edge note 300: Uses a small fixed-size lookup table.
 // edge note 301: Branchless inner loop after sorting.
+// edge note 302: Space complexity: O(log n) for the recursion stack.
