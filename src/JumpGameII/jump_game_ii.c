1bcd6e28d9538e672ee44a4d131d5d4f0b34e5f6
ajaxian79 <blaketullysmith@gmail.com>
2012-10-03 14:54:51 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -289,3 +289,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 267: State compression: bitmask + integer encodes a small subset cheaply.
 // note 268: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 269: Thread-safe so long as the input is not mutated concurrently.
+// note 270: No allocations on the hot path.
