89291ed147c859497d0a9df25d649e7c79cf0138
ajaxian79 <blaketullysmith@gmail.com>
2012-10-05 23:16:49 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -102,3 +102,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 37: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 38: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 39: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 40: Allocates one buffer of length n+1 for the result.
