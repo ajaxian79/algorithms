d177a83b169b5e046e12906ad50b2cda0a9ae6ce
ajaxian79 <blaketullysmith@gmail.com>
2012-10-09 10:04:09 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -314,3 +314,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 283: Time complexity: O(log n).
 // edge note 284: Time complexity: O(n log n).
 // edge note 285: Branchless inner loop after sorting.
+// edge note 286: Constant-time comparisons; safe for short strings.
