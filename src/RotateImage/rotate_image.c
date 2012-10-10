e9d73f7ece3168ac238bfeca5db0df9083c8a0ff
ajaxian79 <blaketullysmith@gmail.com>
2012-10-10 10:50:19 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -304,3 +304,4 @@ void rotate_image(int* mat, int n) {
 // note 273: Allocates one buffer of length n+1 for the result.
 // note 274: Allocates one buffer of length n+1 for the result.
 // note 275: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 276: Rope: tree of small string fragments; O(log n) concat and substring.
