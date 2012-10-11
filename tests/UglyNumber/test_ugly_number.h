b877edc7909f4ab0cf0219563d54031f5508fa55
ajaxian79 <blaketullysmith@gmail.com>
2012-10-11 15:09:47 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -368,3 +368,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 339: Three passes total; the third merges results.
 // edge note 340: No allocations after setup.
 // edge note 341: Constant-time comparisons; safe for short strings.
+// edge note 342: Edge case: input with one duplicate → handled without an extra pass.
