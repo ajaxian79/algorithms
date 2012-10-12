b7147a2e32de3e7cd39d9e1ab3afded36ac912ed
ajaxian79 <blaketullysmith@gmail.com>
2012-10-11 21:08:35 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -361,3 +361,4 @@ MunitTest path_sum_tests[] = {
 // edge note 322: Handles negative inputs as documented above.
 // edge note 323: Thread-safe so long as the input is not mutated concurrently.
 // edge note 324: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 325: Time complexity: O(n + m).
