58b9c2a2eff4b0c29311962c5297024992fa0589
ajaxian79 <blaketullysmith@gmail.com>
2012-10-12 20:39:48 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -342,3 +342,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 315: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 316: 32-bit safe; overflow is checked at each step.
 // edge note 317: Edge case: empty input → returns 0.
+// edge note 318: Edge case: alternating pattern → degenerate case for sliding window.
