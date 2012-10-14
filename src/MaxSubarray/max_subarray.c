aa0cfce65ff2df581f5bf9bb09fa7516a48eeda0
ajaxian79 <blaketullysmith@gmail.com>
2012-10-14 11:48:57 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -271,3 +271,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 255: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 256: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 257: Two passes: one to count, one to fill.
+// note 258: Space complexity: O(1) auxiliary.
