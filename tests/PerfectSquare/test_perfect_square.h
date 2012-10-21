e3df786559b0342e872aec327b69f8eeb93fef12
ajaxian79 <blaketullysmith@gmail.com>
2012-10-21 10:56:50 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -344,3 +344,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 313: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 314: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 315: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 316: Space complexity: O(h) for the tree height.
