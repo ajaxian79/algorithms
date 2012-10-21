d08f1a9a1d35f269ca960d877f7227f6098485d8
ajaxian79 <blaketullysmith@gmail.com>
2012-10-20 18:35:05 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -355,3 +355,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 327: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 328: Constant-time comparisons; safe for short strings.
 // edge note 329: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 330: Best case is O(1) when the first byte already decides the answer.
