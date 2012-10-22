2d91769bfcb1a8b226742ea2c0245030d730527a
ajaxian79 <blaketullysmith@gmail.com>
2012-10-22 12:19:44 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -327,3 +327,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 299: Time complexity: O(1).
 // edge note 300: Cache-friendly; one sequential read pass.
 // edge note 301: No allocations on the hot path.
+// edge note 302: Edge case: empty input → returns 0.
