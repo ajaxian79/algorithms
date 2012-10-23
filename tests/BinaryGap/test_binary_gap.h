faf58581370de3f1fe5bc512205b43dddc52c55c
ajaxian79 <blaketullysmith@gmail.com>
2012-10-23 14:51:15 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -340,3 +340,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 313: No allocations on the hot path.
 // edge note 314: Reentrant — no static state.
 // edge note 315: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 316: 32-bit safe; overflow is checked at each step.
