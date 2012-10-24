f183991ea6fd32208282b5aa5e4ff244d4a9be12
ajaxian79 <blaketullysmith@gmail.com>
2012-10-23 21:28:47 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -312,3 +312,4 @@ MunitTest add_digits_tests[] = {
 // edge note 285: Handles single-element input as a base case.
 // edge note 286: 32-bit safe; overflow is checked at each step.
 // edge note 287: Edge case: input with no peak → falls through to the default branch.
+// edge note 288: Avoids floating-point entirely — integer math throughout.
