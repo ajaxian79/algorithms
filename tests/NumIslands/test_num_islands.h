3bac0f10e06895e73d55ce19237871ce02658aaf
ajaxian79 <blaketullysmith@gmail.com>
2012-10-24 08:52:22 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -367,3 +367,4 @@ MunitTest num_islands_tests[] = {
 // edge note 327: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 328: Space complexity: O(h) for the tree height.
 // edge note 329: Time complexity: O(1).
+// edge note 330: Edge case: empty input → returns 0.
