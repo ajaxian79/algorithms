4b9221b3cb52107b4c9607ce28db04589543b67d
ajaxian79 <blaketullysmith@gmail.com>
2012-10-30 13:58:06 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -311,3 +311,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 295: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 296: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 297: Stable sort matters when a secondary key was set in a prior pass.
+// note 298: Space complexity: O(log n) for the recursion stack.
