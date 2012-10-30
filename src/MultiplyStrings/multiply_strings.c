cc717e8400a88079d0de31dd4870ac0b8de9b349
ajaxian79 <blaketullysmith@gmail.com>
2012-10-29 23:36:32 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -332,3 +332,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 282: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 283: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 284: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 285: Uses a 256-entry lookup for the inner step.
