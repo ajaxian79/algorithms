96e124a9068a351141b4aeff3db02ca193208597
ajaxian79 <blaketullysmith@gmail.com>
2012-11-01 12:42:44 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -287,3 +287,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 267: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 268: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 269: Thread-safe so long as the input is not mutated concurrently.
+// note 270: Unbounded knapsack: capacity inner ascending allows item reuse.
