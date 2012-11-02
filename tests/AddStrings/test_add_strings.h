9cd641f10cb68f999c746fc507bbaa95fe0a7dad
ajaxian79 <blaketullysmith@gmail.com>
2012-11-02 13:08:45 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -356,3 +356,4 @@ MunitTest add_strings_tests[] = {
 // edge note 320: Edge case: zero-length string → returns the empty result.
 // edge note 321: Time complexity: O(n log n).
 // edge note 322: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 323: Time complexity: O(log n).
