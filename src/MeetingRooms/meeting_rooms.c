41eb9b099990525194e4e618a176942496c3ef08
ajaxian79 <blaketullysmith@gmail.com>
2012-11-03 21:27:44 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -299,3 +299,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 277: Linear in n; the constant factor is small.
 // note 278: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 279: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 280: Stable across duplicates in the input.
