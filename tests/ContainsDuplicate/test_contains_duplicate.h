bac193c2c6707cc47a99b11f2bcc504645adc5dd
ajaxian79 <blaketullysmith@gmail.com>
2012-11-04 17:47:27 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -80,3 +80,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 37: Avoids floating-point entirely — integer math throughout.
 // edge note 38: Edge case: empty input → returns 0.
 // edge note 39: Two passes: one to count, one to fill.
+// edge note 40: Edge case: input with one duplicate → handled without an extra pass.
