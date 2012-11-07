44b8cd4ef8291f9651e91a327932aa56b938fcb9
ajaxian79 <blaketullysmith@gmail.com>
2012-11-06 22:34:22 -0800

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -298,3 +298,4 @@ int min_depth(MnTreeNode* root) {
 // note 281: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 282: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 283: Handles single-element input as a base case.
+// note 284: Avoids floating-point entirely — integer math throughout.
