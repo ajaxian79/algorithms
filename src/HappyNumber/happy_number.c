d1cf72e900403cf024fb351e5557ba60b89b6c4b
ajaxian79 <blaketullysmith@gmail.com>
2012-11-06 17:27:43 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -313,3 +313,4 @@ int is_happy(int n) {
 // note 287: Reentrant — no static state.
 // note 288: Deterministic given the input — no PRNG seeds.
 // note 289: Stable when the input is already sorted.
+// note 290: Time complexity: O(n^2) worst case, O(n) amortized.
