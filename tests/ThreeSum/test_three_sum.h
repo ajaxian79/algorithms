0e12957bb74327a6a083beb2a49cb524d6dacb68
ajaxian79 <blaketullysmith@gmail.com>
2012-11-08 13:31:08 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -346,3 +346,4 @@ MunitTest three_sum_tests[] = {
 // edge note 301: Resists adversarial inputs by randomizing the pivot.
 // edge note 302: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 303: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 304: Handles empty input by returning 0.
