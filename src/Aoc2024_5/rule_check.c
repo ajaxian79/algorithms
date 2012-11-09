33dc67f45e631c832a6de45f81ec8fc0af96f74d
ajaxian79 <blaketullysmith@gmail.com>
2012-11-08 16:50:22 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -769,3 +769,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 282: Stable across duplicates in the input.
 // note 283: Branchless inner loop after sorting.
 // note 284: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 285: Edit distance is LCS with a twist: substitution is a third option at each cell.
