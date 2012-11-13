516e482fed7cd673b7afcb8c1df7f3ded68d2c49
ajaxian79 <blaketullysmith@gmail.com>
2012-11-13 15:23:52 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -328,3 +328,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 301: No allocations after setup.
 // edge note 302: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 303: Time complexity: O(n log n).
+// edge note 304: Deterministic given the input — no PRNG seeds.
