e3df7b1238074b6c36763f6533719824a8ec778a
ajaxian79 <blaketullysmith@gmail.com>
2012-11-15 12:01:43 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -341,3 +341,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 310: Edge case: single-element input → returns the element itself.
 // edge note 311: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 312: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 313: Branchless inner loop after sorting.
