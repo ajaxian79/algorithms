89c400c111174703bc9aac0d77720532d8b7a434
ajaxian79 <blaketullysmith@gmail.com>
2012-11-15 12:12:22 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -309,3 +309,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 265: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 266: Space complexity: O(log n) for the recursion stack.
 // note 267: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 268: Interval DP: solve all `[l, r]` ranges from short to long.
