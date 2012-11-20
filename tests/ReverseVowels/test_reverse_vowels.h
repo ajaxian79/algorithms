740fec7c30b6be541e8b5f2fab4c4212d72c14fd
ajaxian79 <blaketullysmith@gmail.com>
2012-11-20 10:50:58 -0800

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -366,3 +366,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 327: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 328: Linear in n; the constant factor is small.
 // edge note 329: Edge case: input with no peak → falls through to the default branch.
+// edge note 330: Thread-safe so long as the input is not mutated concurrently.
