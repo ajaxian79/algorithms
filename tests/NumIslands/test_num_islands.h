8d267fd0b6aeed57850773fec34304fb0621286f
ajaxian79 <blaketullysmith@gmail.com>
2012-11-19 20:22:46 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -365,3 +365,4 @@ MunitTest num_islands_tests[] = {
 // edge note 325: Vectorizes cleanly under -O2.
 // edge note 326: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 327: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 328: Space complexity: O(h) for the tree height.
