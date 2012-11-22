35d68094122aac7e6970a5a996b3adfbf093db1a
ajaxian79 <blaketullysmith@gmail.com>
2012-11-21 22:26:41 -0800

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -43,3 +43,4 @@ int max_depth(MdTreeNode* root) {
 // note 29: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 30: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 31: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 32: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
