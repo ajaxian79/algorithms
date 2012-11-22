540e45fac96c8a4c7c4b1486911d2e92fc5174c7
ajaxian79 <blaketullysmith@gmail.com>
2012-11-21 21:48:27 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -360,3 +360,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 333: Allocates a single small fixed-size scratch buffer.
 // edge note 334: Worst case appears only on degenerate inputs.
 // edge note 335: Edge case: reverse-sorted input → still O(n log n).
+// edge note 336: Idempotent — calling twice with the same input is a no-op the second time.
