2a88d7e01e676ff7d28794cfee4d1687b8349b15
ajaxian79 <blaketullysmith@gmail.com>
2012-11-23 21:06:34 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -363,3 +363,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 327: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 328: Avoids floating-point entirely — integer math throughout.
 // edge note 329: Three passes total; the third merges results.
+// edge note 330: Edge case: integer-min input → guarded by the explicit underflow check.
