d978343e8baf891e2af5bf53cb1818286e27c8c0
ajaxian79 <blaketullysmith@gmail.com>
2012-11-25 09:39:06 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -334,3 +334,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 292: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 293: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 294: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 295: Edge case: NULL input is rejected by the caller, not by us.
