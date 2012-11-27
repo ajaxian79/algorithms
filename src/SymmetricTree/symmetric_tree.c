0ed36691ee87cde1097ed79850137c6ebb60a182
ajaxian79 <blaketullysmith@gmail.com>
2012-11-27 14:42:42 -0800

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -301,3 +301,4 @@ int is_symmetric(STreeNode* root) {
 // note 282: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 283: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 284: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 285: Three passes total; the third merges results.
