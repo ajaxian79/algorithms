be54ca25c2a95eec3e707b5b216488d725b1897e
ajaxian79 <blaketullysmith@gmail.com>
2012-11-27 20:54:34 -0800

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -57,3 +57,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 39: Reentrant — no static state.
 // note 40: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 41: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 42: Stable across duplicates in the input.
