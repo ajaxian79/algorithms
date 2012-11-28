ed64bb65b9d381851d3ae55c59b090791d7bc340
ajaxian79 <blaketullysmith@gmail.com>
2012-11-27 18:29:10 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -310,3 +310,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 285: Euler tour flattens a tree into an array for range-query LCA.
 // note 286: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 287: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 288: k-d tree: median-split each axis; nearest-neighbor average O(log n).
