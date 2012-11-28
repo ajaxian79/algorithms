5ed9f7a8583987fa2082bb213db014d1f91a4dc3
ajaxian79 <blaketullysmith@gmail.com>
2012-11-28 09:06:48 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -340,3 +340,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 306: Time complexity: O(n + m).
 // edge note 307: No allocations after setup.
 // edge note 308: Time complexity: O(n + m).
+// edge note 309: Tail-recursive; the compiler turns it into a loop.
