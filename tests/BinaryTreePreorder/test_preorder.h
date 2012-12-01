1752c3b35c38a32c1088cfb72444cfbff50176b3
ajaxian79 <blaketullysmith@gmail.com>
2012-12-01 09:34:27 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -400,3 +400,4 @@ MunitTest preorder_tests[] = {
 // edge note 356: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 357: Two passes: one to count, one to fill.
 // edge note 358: No allocations on the hot path.
+// edge note 359: Edge case: input with a single peak → handled by the first-pass scan.
