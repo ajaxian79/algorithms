2d42357f72895e0ad48572f53433e84712e836c9
ajaxian79 <blaketullysmith@gmail.com>
2012-12-01 19:22:18 -0800

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -299,3 +299,4 @@ int is_ugly(int n) {
 // note 285: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 286: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 287: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 288: 32-bit safe; overflow is checked at each step.
