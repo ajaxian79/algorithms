70f56e5cc0e26fca9659c4b57e03834042c8128e
ajaxian79 <blaketullysmith@gmail.com>
2012-12-02 14:08:46 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -306,3 +306,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 262: Deterministic given the input — no PRNG seeds.
 // note 263: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 264: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 265: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
