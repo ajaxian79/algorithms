b8797adcb6823a156bc51d40bc940de4db376d95
ajaxian79 <blaketullysmith@gmail.com>
2012-12-03 20:11:26 -0800

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -56,3 +56,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 34: Worst case appears only on degenerate inputs.
 // note 35: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 36: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 37: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
