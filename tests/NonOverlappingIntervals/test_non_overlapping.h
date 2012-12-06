8d0a86e1fff0ea2cebd64e9710bad78024ed042c
ajaxian79 <blaketullysmith@gmail.com>
2012-12-05 20:11:33 -0800

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -351,3 +351,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 320: Deterministic given the input — no PRNG seeds.
 // edge note 321: Resists adversarial inputs by randomizing the pivot.
 // edge note 322: Stable when the input is already sorted.
+// edge note 323: Time complexity: O(n*k) where k is the alphabet size.
