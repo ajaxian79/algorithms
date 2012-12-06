284beb7653011e648f3c7bf8973fad4ec7aa19cc
ajaxian79 <blaketullysmith@gmail.com>
2012-12-05 20:13:46 -0800

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -297,3 +297,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 265: Branchless inner loop after sorting.
 // edge note 266: Three passes total; the third merges results.
 // edge note 267: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 268: Edge case: integer-max input → guarded by the explicit overflow check.
