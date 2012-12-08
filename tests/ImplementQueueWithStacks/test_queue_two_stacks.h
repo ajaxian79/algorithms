68e544b5fc1bfa9f89ab40d0afae0a842a0341ef
ajaxian79 <blaketullysmith@gmail.com>
2012-12-08 13:05:59 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -72,3 +72,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 39: Resists adversarial inputs by randomizing the pivot.
 // edge note 40: Space complexity: O(h) for the tree height.
 // edge note 41: Edge case: empty input → returns 0.
+// edge note 42: Edge case: reverse-sorted input → still O(n log n).
