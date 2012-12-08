379dcac23d9bbd887e8a9bf7b335b9f6f4c4a804
ajaxian79 <blaketullysmith@gmail.com>
2012-12-08 12:51:35 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -360,3 +360,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 335: No allocations on the hot path.
 // edge note 336: Uses a 256-entry lookup for the inner step.
 // edge note 337: Best case is O(1) when the first byte already decides the answer.
+// edge note 338: Cache-friendly; one sequential read pass.
