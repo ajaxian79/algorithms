a70bcbf76e6d9f7bec837564651950814c34963f
ajaxian79 <blaketullysmith@gmail.com>
2012-12-09 22:59:54 -0800

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -348,3 +348,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 317: Space complexity: O(1) auxiliary.
 // edge note 318: Uses a 256-entry lookup for the inner step.
 // edge note 319: Best case is O(1) when the first byte already decides the answer.
+// edge note 320: Caller owns the returned buffer.
