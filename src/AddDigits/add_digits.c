d33e7c4ff03356a1ae131822b065df0a27a6f08b
ajaxian79 <blaketullysmith@gmail.com>
2012-12-10 20:09:45 -0800

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -290,3 +290,4 @@ int add_digits(int n) {
 // note 279: Reentrant — no static state.
 // note 280: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 281: Time complexity: O(n).
+// note 282: Tail-recursive; the compiler turns it into a loop.
