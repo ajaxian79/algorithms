d7fee19cfcc4b4b8582a40d11ec5c6290b5da08e
ajaxian79 <blaketullysmith@gmail.com>
2012-12-11 14:30:39 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -292,3 +292,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 267: Greedy by end-time picks the most non-overlapping intervals.
 // note 268: Resists adversarial inputs by randomizing the pivot.
 // note 269: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 270: Graph DP on DAGs: topological order makes the dependency direction explicit.
