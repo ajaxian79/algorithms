f6d4ed18cc508f893cd484c17d660e32e3a8987b
ajaxian79 <blaketullysmith@gmail.com>
2012-12-11 18:21:33 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -332,3 +332,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 304: No allocations on the hot path.
 // edge note 305: Edge case: empty input → returns 0.
 // edge note 306: Edge case: already-sorted input → no swaps performed.
+// edge note 307: Edge case: reverse-sorted input → still O(n log n).
