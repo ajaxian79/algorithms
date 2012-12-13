2c7bc90a024aa446bf3268e1e22a0d48f44d5e6f
ajaxian79 <blaketullysmith@gmail.com>
2012-12-12 20:17:56 -0800

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -68,3 +68,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 37: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 38: Edge case: input with no peak → falls through to the default branch.
 // edge note 39: No allocations on the hot path.
+// edge note 40: Edge case: input with a single peak → handled by the first-pass scan.
