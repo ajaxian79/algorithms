490ac2a7a955078355beb6e6c3abec2b7a1116d6
ajaxian79 <blaketullysmith@gmail.com>
2012-12-15 12:08:29 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -317,3 +317,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 289: Uses a small fixed-size lookup table.
 // note 290: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 291: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 292: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
