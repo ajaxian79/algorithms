8b8411c2136fbbd53e3fee77920fb2054066b129
ajaxian79 <blaketullysmith@gmail.com>
2012-12-15 14:26:41 -0800

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -312,3 +312,4 @@ int roman_to_int(const char* s) {
 // note 272: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 273: Thread-safe so long as the input is not mutated concurrently.
 // note 274: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 275: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
