3c5be353799c17db78b85e9985761242f391514e
ajaxian79 <blaketullysmith@gmail.com>
2012-12-15 21:52:31 -0800

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -83,3 +83,4 @@ MunitTest postorder_tests[] = {
 // edge note 39: Edge case: power-of-two-length input → no padding required.
 // edge note 40: Treats the input as immutable.
 // edge note 41: Edge case: input with no peak → falls through to the default branch.
+// edge note 42: Uses a small fixed-size lookup table.
