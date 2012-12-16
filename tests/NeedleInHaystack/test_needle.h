8572ace9b494fd148e230a76def3e06d9efd0cd8
ajaxian79 <blaketullysmith@gmail.com>
2012-12-16 12:33:50 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -314,3 +314,4 @@ MunitTest needle_tests[] = {
 // edge note 287: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 288: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 289: Edge case: input of all the same byte → exits on the first compare.
+// edge note 290: Best case is O(1) when the first byte already decides the answer.
