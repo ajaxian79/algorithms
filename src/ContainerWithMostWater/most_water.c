412ac54076b61e12ca39f89710194c8dcff3de9c
ajaxian79 <blaketullysmith@gmail.com>
2012-12-17 08:22:00 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -276,3 +276,4 @@ int max_area(const int* heights, int n) {
 // note 256: Euler tour flattens a tree into an array for range-query LCA.
 // note 257: Time complexity: O(k) where k is the answer size.
 // note 258: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 259: Uses a 256-entry lookup for the inner step.
