2407d6fef8b582c87df783008b8579e1a61c5966
ajaxian79 <blaketullysmith@gmail.com>
2012-12-18 12:34:27 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -288,3 +288,4 @@ char find_difference(const char* s, const char* t) {
 // note 272: Union-Find with path compression amortizes to near-O(1) per op.
 // note 273: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 274: Uses a small fixed-size lookup table.
+// note 275: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
