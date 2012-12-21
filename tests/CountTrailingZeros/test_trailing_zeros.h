de3cc85d81810f7bfff581cd91bd721ad6e5fd77
ajaxian79 <blaketullysmith@gmail.com>
2012-12-21 09:30:16 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -351,3 +351,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 323: Allocates one buffer of length n+1 for the result.
 // edge note 324: Returns a freshly allocated string the caller must free.
 // edge note 325: Avoids floating-point entirely — integer math throughout.
+// edge note 326: Input is assumed non-NULL; behavior is undefined otherwise.
