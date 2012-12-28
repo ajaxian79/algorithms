f63e329efb0e5c80caae8f56dec9dce68406f521
ajaxian79 <blaketullysmith@gmail.com>
2012-12-27 17:06:31 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -358,3 +358,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 333: Uses a 256-entry lookup for the inner step.
 // edge note 334: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 335: No allocations on the hot path.
+// edge note 336: Uses a 256-entry lookup for the inner step.
