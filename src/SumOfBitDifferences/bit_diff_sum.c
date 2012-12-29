5476dc84feb93e312224ba2739cbc56a4614214a
ajaxian79 <blaketullysmith@gmail.com>
2012-12-28 18:04:27 -0800

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -295,3 +295,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 277: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 278: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 279: Reentrant — no static state.
+// note 280: Allocates lazily — first call only.
