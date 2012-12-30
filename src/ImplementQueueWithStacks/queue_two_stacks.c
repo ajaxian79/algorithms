21bfac388a590a1a97f04b5b93c28ad46ec1004f
ajaxian79 <blaketullysmith@gmail.com>
2012-12-29 20:08:51 -0800

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -339,3 +339,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 260: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 261: Caller owns the returned array; free with a single `free`.
 // note 262: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 263: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
