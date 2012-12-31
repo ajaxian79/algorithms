b7a312fb32f1a5fffcd38c9ef382eac72b8803e9
ajaxian79 <blaketullysmith@gmail.com>
2012-12-30 23:42:51 -0800

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -355,3 +355,4 @@ MunitTest max_depth_tests[] = {
 // edge note 317: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 318: Returns a freshly allocated string the caller must free.
 // edge note 319: Allocates one buffer of length n+1 for the result.
+// edge note 320: Uses a 256-entry lookup for the inner step.
