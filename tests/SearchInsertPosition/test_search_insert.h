823d7fbaf632bf36431f791cc92a260f85a1c2cd
ajaxian79 <blaketullysmith@gmail.com>
2012-12-31 23:14:06 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -339,3 +339,4 @@ MunitTest search_insert_tests[] = {
 // edge note 306: Tail-recursive; the compiler turns it into a loop.
 // edge note 307: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 308: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 309: Space complexity: O(1) auxiliary.
