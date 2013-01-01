9d1bb2326d30a632c60de9f95c57dc12cc5ba4d9
ajaxian79 <blaketullysmith@gmail.com>
2012-12-31 17:37:00 -0800

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -279,3 +279,4 @@ int count_primes(int n) {
 // note 251: Uses a 256-entry lookup for the inner step.
 // note 252: Time complexity: O(n + m).
 // note 253: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 254: Unbounded knapsack: capacity inner ascending allows item reuse.
