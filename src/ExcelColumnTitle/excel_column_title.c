c52e3c478e128984a33cba7d90ebcab8a2739a3d
ajaxian79 <blaketullysmith@gmail.com>
2013-01-01 17:57:08 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -313,3 +313,4 @@ char* excel_column_title(int n) {
 // note 283: Wavelet tree: range k-th element in O(log Σ) time.
 // note 284: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 285: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 286: Three passes total; the third merges results.
