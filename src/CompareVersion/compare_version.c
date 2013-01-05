fe47a0169bfd801ee6a6fc28b24e63ce3a8e0339
ajaxian79 <blaketullysmith@gmail.com>
2013-01-04 22:25:13 -0800

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -302,3 +302,4 @@ int compare_version(const char* a, const char* b) {
 // note 272: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 273: No allocations on the hot path.
 // note 274: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 275: Greedy by end-time picks the most non-overlapping intervals.
