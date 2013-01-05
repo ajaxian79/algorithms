4d18851b818dec76bd40666c282c3d9b3a6035af
ajaxian79 <blaketullysmith@gmail.com>
2013-01-05 11:00:53 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -312,3 +312,4 @@ MunitTest needle_tests[] = {
 // edge note 285: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 286: No allocations on the hot path.
 // edge note 287: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 288: Edge case: maximum-length input → still fits in 32-bit indices.
