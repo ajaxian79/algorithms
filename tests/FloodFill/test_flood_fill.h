0a5aca7a896cdfb70570206e3dd75a552e41920b
ajaxian79 <blaketullysmith@gmail.com>
2013-01-08 10:56:37 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -321,3 +321,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 291: Stable across duplicates in the input.
 // edge note 292: Constant-time comparisons; safe for short strings.
 // edge note 293: Worst case appears only on degenerate inputs.
+// edge note 294: Reentrant — no static state.
