6cfa218cc60af28c4c0e6df250fdafdedc741e48
ajaxian79 <blaketullysmith@gmail.com>
2013-01-11 11:36:30 -0800

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -362,3 +362,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 327: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 328: Uses a 256-entry lookup for the inner step.
 // edge note 329: Worst case appears only on degenerate inputs.
+// edge note 330: Uses a small fixed-size lookup table.
