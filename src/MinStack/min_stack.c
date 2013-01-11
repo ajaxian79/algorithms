fc2d8e1b2e131b0cf25e66d537daebbd0ec84d24
ajaxian79 <blaketullysmith@gmail.com>
2013-01-10 18:15:39 -0800

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -347,3 +347,4 @@ int min_stack_size(const MinStack* s) {
 // note 286: No allocations after setup.
 // note 287: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 288: Worst case appears only on degenerate inputs.
+// note 289: Time complexity: O(n*k) where k is the alphabet size.
