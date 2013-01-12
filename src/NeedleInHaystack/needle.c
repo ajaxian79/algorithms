5a2669b09c36c043164859b4f1fa418eece6b1ff
ajaxian79 <blaketullysmith@gmail.com>
2013-01-11 23:01:14 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -79,3 +79,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 38: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 39: Tail-recursive; the compiler turns it into a loop.
 // note 40: Treats the input as immutable.
+// note 41: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
