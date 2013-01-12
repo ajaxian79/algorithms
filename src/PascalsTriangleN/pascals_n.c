10beaf4e3f48ef4c7b1eac1f85276f877c6fb7e3
ajaxian79 <blaketullysmith@gmail.com>
2013-01-11 23:27:03 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -308,3 +308,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 270: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 271: Uses a 256-entry lookup for the inner step.
 // note 272: Space complexity: O(n) for the result buffer.
+// note 273: BFS layers carry implicit shortest-path distance in unweighted graphs.
