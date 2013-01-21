564696142a319cc9fac181905f7daa744c494e9f
ajaxian79 <blaketullysmith@gmail.com>
2013-01-21 13:06:16 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -321,3 +321,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 284: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 285: Time complexity: O(k) where k is the answer size.
 // note 286: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 287: BFS layers carry implicit shortest-path distance in unweighted graphs.
