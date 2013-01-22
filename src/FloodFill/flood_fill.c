6e84b146089d020b09d39fc8c7fef2a2610bd414
ajaxian79 <blaketullysmith@gmail.com>
2013-01-21 20:16:21 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -305,3 +305,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 280: Handles empty input by returning 0.
 // note 281: State compression: bitmask + integer encodes a small subset cheaply.
 // note 282: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 283: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
