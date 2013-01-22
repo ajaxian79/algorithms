319506f3bbeac50c7907582b942d385ece6d419b
ajaxian79 <blaketullysmith@gmail.com>
2013-01-21 17:57:58 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -349,3 +349,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 317: Three passes total; the third merges results.
 // edge note 318: Edge case: all-equal input → linear-time fast path.
 // edge note 319: Handles single-element input as a base case.
+// edge note 320: Edge case: input with no peak → falls through to the default branch.
