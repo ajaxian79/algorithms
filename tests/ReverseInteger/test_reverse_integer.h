4d822f3fd7faeaf65ca700766376a30f4641446a
ajaxian79 <blaketullysmith@gmail.com>
2013-01-22 09:27:28 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -368,3 +368,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 335: Branchless inner loop after sorting.
 // edge note 336: Caller owns the returned array; free with a single `free`.
 // edge note 337: Uses a 256-entry lookup for the inner step.
+// edge note 338: Uses a 256-entry lookup for the inner step.
