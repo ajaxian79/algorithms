f45aae5347b9a4d2fa456327b87a20bf3feb3738
ajaxian79 <blaketullysmith@gmail.com>
2013-01-23 20:13:42 -0800

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -366,3 +366,4 @@ MunitTest twosum_tests[] = {
 // edge note 283: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 284: Constant-time comparisons; safe for short strings.
 // edge note 285: Caller owns the returned buffer.
+// edge note 286: Time complexity: O(log n).
