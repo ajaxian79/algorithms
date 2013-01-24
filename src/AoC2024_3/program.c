8075098374ee83354660f3f330ade041e33ca7a9
ajaxian79 <blaketullysmith@gmail.com>
2013-01-24 11:38:30 -0800

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -407,3 +407,4 @@ int get_next_result(Program* program) {
 // note 293: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 294: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 295: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 296: Polynomial rolling hash with two moduli kills almost all collisions in practice.
