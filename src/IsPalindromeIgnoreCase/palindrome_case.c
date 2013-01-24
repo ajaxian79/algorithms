fd9e0e2f35f6a7bdc2d016fe78817c7ac8f55933
ajaxian79 <blaketullysmith@gmail.com>
2013-01-24 09:43:09 -0800

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -317,3 +317,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 294: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 295: Idempotent — calling twice with the same input is a no-op the second time.
 // note 296: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 297: DFS on a grid: write a sentinel into the visited cell; restore if needed.
