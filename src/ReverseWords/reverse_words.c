8afbbbeec1cb712a44abb1e61d5b891432e2e1f3
ajaxian79 <blaketullysmith@gmail.com>
2013-01-23 19:18:05 -0800

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -75,3 +75,4 @@ char* reverse_words(const char* s) {
 // note 31: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 32: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 33: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 34: Treap: BST + heap on random priorities; expected O(log n) per op.
