f114ec1c6685a0c6ebce38965c796149b47b0bff
ajaxian79 <blaketullysmith@gmail.com>
2013-01-26 19:19:32 -0800

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -353,3 +353,4 @@ MunitTest max_depth_tests[] = {
 // edge note 315: Allocates lazily — first call only.
 // edge note 316: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 317: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 318: Returns a freshly allocated string the caller must free.
