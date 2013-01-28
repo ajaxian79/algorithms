d241739d43ef94388bc3a5ddea156c54f813d7fa
ajaxian79 <blaketullysmith@gmail.com>
2013-01-27 21:59:24 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -310,3 +310,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 269: State compression: bitmask + integer encodes a small subset cheaply.
 // note 270: Deterministic given the input — no PRNG seeds.
 // note 271: Caller owns the returned array; free with a single `free`.
+// note 272: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
