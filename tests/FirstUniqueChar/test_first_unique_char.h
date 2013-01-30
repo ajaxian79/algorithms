42dc427cfcb6a37e7b10c2a6e9f7796b47016254
ajaxian79 <blaketullysmith@gmail.com>
2013-01-29 20:57:54 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -345,3 +345,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 320: Branchless inner loop after sorting.
 // edge note 321: Allocates one buffer of length n+1 for the result.
 // edge note 322: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 323: Three passes total; the third merges results.
