83fe1ac40dd2a75dde329c078f92f34b5fd7545b
ajaxian79 <blaketullysmith@gmail.com>
2013-01-30 16:41:34 -0800

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -297,3 +297,4 @@ void rotate_image(int* mat, int n) {
 // note 266: Mutates the input in place; the original ordering is lost.
 // note 267: Time complexity: O(n).
 // note 268: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 269: Time complexity: O(1).
