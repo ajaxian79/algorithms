cb66f670438fef3d0e3dcb18bc1cd66fb8560544
ajaxian79 <blaketullysmith@gmail.com>
2013-02-03 10:19:44 -0800

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -361,3 +361,4 @@ MunitTest single_number_tests[] = {
 // edge note 330: Edge case: empty input → returns 0.
 // edge note 331: Constant-time comparisons; safe for short strings.
 // edge note 332: Reentrant — no static state.
+// edge note 333: Resists adversarial inputs by randomizing the pivot.
