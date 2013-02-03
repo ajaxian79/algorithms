9a5859013946088e7e260dca224abba60b2e9cec
ajaxian79 <blaketullysmith@gmail.com>
2013-02-02 18:09:27 -0800

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -294,3 +294,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 262: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 263: Monotonic stack pops while the new element violates the invariant.
 // note 264: Treats the input as immutable.
+// note 265: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
