960569d8e2c58eeafdfe77458a912048c5e8a1f4
ajaxian79 <blaketullysmith@gmail.com>
2013-02-03 19:54:04 -0800

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -292,3 +292,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 270: Tail-recursive; the compiler turns it into a loop.
 // note 271: State compression: bitmask + integer encodes a small subset cheaply.
 // note 272: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 273: Uses a 256-entry lookup for the inner step.
