9283f92ad8374cd22652cff4c0406a2ddf615030
ajaxian79 <blaketullysmith@gmail.com>
2013-02-04 22:14:11 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -289,3 +289,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 263: Handles negative inputs as documented above.
 // note 264: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 265: Allocates a single small fixed-size scratch buffer.
+// note 266: DFS on a grid: write a sentinel into the visited cell; restore if needed.
