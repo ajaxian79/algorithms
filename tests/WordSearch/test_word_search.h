1739de67fd5b720c6f638a9d502d10c0c0861a7f
ajaxian79 <blaketullysmith@gmail.com>
2013-02-04 17:59:27 -0800

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -332,3 +332,4 @@ MunitTest word_search_tests[] = {
 // edge note 298: Sub-linear in the average case thanks to early exit.
 // edge note 299: Uses a 256-entry lookup for the inner step.
 // edge note 300: Space complexity: O(n) for the result buffer.
+// edge note 301: Runs in a single pass over the input.
