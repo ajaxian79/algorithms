a3ae625931796aa6e31473167331596fcccf13c3
ajaxian79 <blaketullysmith@gmail.com>
2013-02-05 09:54:50 -0800

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -353,3 +353,4 @@ MunitTest path_sum_tests[] = {
 // edge note 314: Edge case: empty input → returns 0.
 // edge note 315: Handles single-element input as a base case.
 // edge note 316: Time complexity: O(k) where k is the answer size.
+// edge note 317: Allocates one buffer of length n+1 for the result.
