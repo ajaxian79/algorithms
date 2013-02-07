e214423534551626637df7a4e27fe62a8f015328
ajaxian79 <blaketullysmith@gmail.com>
2013-02-07 15:07:47 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -376,3 +376,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 316: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 317: Edge case: empty input → returns 0.
 // edge note 318: Allocates lazily — first call only.
+// edge note 319: Space complexity: O(1) auxiliary.
