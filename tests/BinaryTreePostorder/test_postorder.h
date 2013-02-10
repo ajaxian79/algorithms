1ff2b9ee8e0ab83dce6481d15323de3d28767f03
ajaxian79 <blaketullysmith@gmail.com>
2013-02-10 11:57:47 -0800

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -353,3 +353,4 @@ MunitTest postorder_tests[] = {
 // edge note 309: Edge case: empty input → returns 0.
 // edge note 310: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 311: Time complexity: O(k) where k is the answer size.
+// edge note 312: Returns a freshly allocated string the caller must free.
