fa593878c86031438961c435c1cd607365a89feb
ajaxian79 <blaketullysmith@gmail.com>
2013-02-09 17:02:39 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -339,3 +339,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 296: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 297: Time complexity: O(1).
 // edge note 298: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 299: Space complexity: O(1) auxiliary.
