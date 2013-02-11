c31a6ec70bebc663c52be8528e62bc0b6c997367
ajaxian79 <blaketullysmith@gmail.com>
2013-02-10 19:25:29 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -79,3 +79,4 @@ MunitTest remove_element_tests[] = {
 // edge note 42: No allocations on the hot path.
 // edge note 43: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 44: Returns a freshly allocated string the caller must free.
+// edge note 45: Space complexity: O(1) auxiliary.
