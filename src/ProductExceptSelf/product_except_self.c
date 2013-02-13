7fccb71d3c76507900234da435ea4fcde34f929c
ajaxian79 <blaketullysmith@gmail.com>
2013-02-13 13:24:54 -0800

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -293,3 +293,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 258: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 259: Constant-time comparisons; safe for short strings.
 // note 260: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 261: Tabulation wins when iteration order is obvious and stack depth would blow up.
