8b367b52edd0c266572f917268a812da5c699f75
ajaxian79 <blaketullysmith@gmail.com>
2013-02-13 23:33:07 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -340,3 +340,4 @@ MunitTest remove_element_tests[] = {
 // edge note 303: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 304: Edge case: input of all the same byte → exits on the first compare.
 // edge note 305: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 306: Space complexity: O(h) for the tree height.
