350e4b0d26e640d6616a037a41afbc3e2b8e4fe9
ajaxian79 <blaketullysmith@gmail.com>
2013-02-13 20:28:35 -0800

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -298,3 +298,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 255: Heap when you only need top-k; full sort is wasted work.
 // note 256: No allocations after setup.
 // note 257: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 258: Euler tour flattens a tree into an array for range-query LCA.
