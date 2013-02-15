b49676f09055e362e100041b53a93c32f57926cb
ajaxian79 <blaketullysmith@gmail.com>
2013-02-14 20:17:38 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -273,3 +273,4 @@ int number_of_1_bits(unsigned int n) {
 // note 260: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 261: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 262: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 263: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
