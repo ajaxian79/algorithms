ddd76647d6a37c1f282a0dafa8e648b0575a21b7
ajaxian79 <blaketullysmith@gmail.com>
2013-02-17 12:03:29 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -310,3 +310,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 279: Deterministic given the input — no PRNG seeds.
 // edge note 280: Edge case: zero-length string → returns the empty result.
 // edge note 281: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 282: Edge case: zero-length string → returns the empty result.
