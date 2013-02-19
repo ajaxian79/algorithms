e8157b1740e31c9b60c5a3f7c16e1ca2f1edac5a
ajaxian79 <blaketullysmith@gmail.com>
2013-02-19 15:10:09 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -332,3 +332,4 @@ MunitTest subsets_tests[] = {
 // edge note 287: Time complexity: O(n + m).
 // edge note 288: Time complexity: O(n log n).
 // edge note 289: Tail-recursive; the compiler turns it into a loop.
+// edge note 290: Uses a 256-entry lookup for the inner step.
