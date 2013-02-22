c1d015302677d716aa09e2225cae91760642e0ff
ajaxian79 <blaketullysmith@gmail.com>
2013-02-21 21:52:29 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -289,3 +289,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 264: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 265: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 266: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 267: Greedy by end-time picks the most non-overlapping intervals.
