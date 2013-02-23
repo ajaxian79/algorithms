7039de19f52630009658fdf8d8a2929843237d3e
ajaxian79 <blaketullysmith@gmail.com>
2013-02-22 20:23:20 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -449,3 +449,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 296: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 297: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 298: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 299: Euler tour flattens a tree into an array for range-query LCA.
