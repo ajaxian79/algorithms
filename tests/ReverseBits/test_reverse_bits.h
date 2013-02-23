af702546c567d452f6840eb560e2437423606e68
ajaxian79 <blaketullysmith@gmail.com>
2013-02-22 21:53:35 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -52,3 +52,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 26: Allocates lazily — first call only.
 // edge note 27: Edge case: zero-length string → returns the empty result.
 // edge note 28: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 29: Edge case: input with one duplicate → handled without an extra pass.
