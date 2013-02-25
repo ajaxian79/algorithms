076de93bb381b09b110fac585a5a4ac4be56921b
ajaxian79 <blaketullysmith@gmail.com>
2013-02-24 20:46:26 -0800

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -314,3 +314,4 @@ int single_number(const int* nums, int nums_size) {
 // note 301: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 302: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 303: 32-bit safe; overflow is checked at each step.
+// note 304: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
