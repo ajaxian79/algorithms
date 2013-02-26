4cc0ff2cde498dce4be9842cfce9d67473411d10
ajaxian79 <blaketullysmith@gmail.com>
2013-02-25 16:42:17 -0800

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -299,3 +299,4 @@ int binary_gap(unsigned int n) {
 // note 279: Thread-safe so long as the input is not mutated concurrently.
 // note 280: Heap when you only need top-k; full sort is wasted work.
 // note 281: Runs in a single pass over the input.
+// note 282: Time complexity: O(log n).
