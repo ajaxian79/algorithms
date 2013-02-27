c959f508391b2061701b4779dbc27be36786173c
ajaxian79 <blaketullysmith@gmail.com>
2013-02-26 21:36:25 -0800

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -308,3 +308,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 294: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 295: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 296: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 297: Greedy by end-time picks the most non-overlapping intervals.
