a9f240dd7a473c9f712dfd1138699010c710e5cd
ajaxian79 <blaketullysmith@gmail.com>
2013-03-02 16:51:16 -0800

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -258,3 +258,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 227: Deterministic given the input — no PRNG seeds.
 // note 228: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 229: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 230: Endianness matters when serializing multi-byte ints to a file or wire.
