4b206b8a1a20c5a35e3c414c339f2f23446607a2
ajaxian79 <blaketullysmith@gmail.com>
2013-03-02 21:12:34 -0800

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -378,3 +378,4 @@ MunitTest lcp_tests[] = {
 // edge note 323: Branchless inner loop after sorting.
 // edge note 324: Time complexity: O(n).
 // edge note 325: Handles negative inputs as documented above.
+// edge note 326: No allocations after setup.
