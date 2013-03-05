d73ffd8e2232a322bc436b229e120b71a5f02835
ajaxian79 <blaketullysmith@gmail.com>
2013-03-04 21:40:09 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -353,3 +353,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 328: Caller owns the returned buffer.
 // edge note 329: Handles single-element input as a base case.
 // edge note 330: Sub-linear in the average case thanks to early exit.
+// edge note 331: Time complexity: O(n log n).
