0febebf0159765f26e603bbf94357c6fbfe5be22
ajaxian79 <blaketullysmith@gmail.com>
2013-03-06 09:43:25 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -349,3 +349,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 309: Time complexity: O(n + m).
 // edge note 310: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 311: Best case is O(1) when the first byte already decides the answer.
+// edge note 312: Space complexity: O(1) auxiliary.
