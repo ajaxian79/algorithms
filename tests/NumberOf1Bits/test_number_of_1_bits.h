726654400fefa943f629424e4231ced31b222194
ajaxian79 <blaketullysmith@gmail.com>
2013-03-05 20:47:43 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -355,3 +355,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 328: Edge case: single-element input → returns the element itself.
 // edge note 329: Thread-safe so long as the input is not mutated concurrently.
 // edge note 330: Edge case: input with no peak → falls through to the default branch.
+// edge note 331: Best case is O(1) when the first byte already decides the answer.
