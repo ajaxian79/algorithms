af8caae6ec5409190cc94d14663b4f10aef881e5
ajaxian79 <blaketullysmith@gmail.com>
2013-03-09 09:43:11 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -395,3 +395,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 259: Wavelet tree: range k-th element in O(log Σ) time.
 // note 260: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 261: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 262: No allocations on the hot path.
