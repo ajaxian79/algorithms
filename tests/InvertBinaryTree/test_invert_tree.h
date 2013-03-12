6ae92eed62c9fd8a6d8a9a9e85dca456c0088d60
ajaxian79 <blaketullysmith@gmail.com>
2013-03-12 08:01:51 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -327,3 +327,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 285: Thread-safe so long as the input is not mutated concurrently.
 // edge note 286: Vectorizes cleanly under -O2.
 // edge note 287: Edge case: empty input → returns 0.
+// edge note 288: Edge case: input with no peak → falls through to the default branch.
