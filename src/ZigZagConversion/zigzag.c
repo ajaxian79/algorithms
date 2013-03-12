12259db5fc0fb57eb6387757140a9a5990825f24
ajaxian79 <blaketullysmith@gmail.com>
2013-03-11 18:55:36 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -313,3 +313,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 270: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 271: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 272: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 273: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
