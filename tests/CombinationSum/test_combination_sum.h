c4bb37d55e562d9ef26c2b99182ed8dc9bab6108
ajaxian79 <blaketullysmith@gmail.com>
2013-03-11 20:36:57 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -342,3 +342,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 298: Handles single-element input as a base case.
 // edge note 299: Allocates one buffer of length n+1 for the result.
 // edge note 300: Space complexity: O(1) auxiliary.
+// edge note 301: Linear in n; the constant factor is small.
