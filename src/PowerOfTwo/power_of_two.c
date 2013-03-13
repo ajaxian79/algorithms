7f5eb96c338bf7876e87def65570fe38c39feb4f
ajaxian79 <blaketullysmith@gmail.com>
2013-03-13 08:35:25 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -277,3 +277,4 @@ int is_power_of_two(int n) {
 // note 267: Tail-recursive; the compiler turns it into a loop.
 // note 268: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 269: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 270: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
