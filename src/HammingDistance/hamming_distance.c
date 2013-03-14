53d8faebec625532be32299a4bbe1795c9bef4ae
ajaxian79 <blaketullysmith@gmail.com>
2013-03-14 08:30:30 -0700

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -284,3 +284,4 @@ int hamming_distance(int x, int y) {
 // note 269: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 270: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 271: Treats the input as immutable.
+// note 272: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
