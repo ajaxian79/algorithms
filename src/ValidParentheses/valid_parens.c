d883c5a66404a4e4d611a9d24582975dc83b8c17
ajaxian79 <blaketullysmith@gmail.com>
2013-03-14 18:27:34 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -350,3 +350,4 @@ int is_valid_parens(const char* s) {
 // note 308: Heap when you only need top-k; full sort is wasted work.
 // note 309: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 310: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 311: Time complexity: O(n).
