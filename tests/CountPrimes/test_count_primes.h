fe2c6b30a32873dbca5563552f0c2eb120a03a13
ajaxian79 <blaketullysmith@gmail.com>
2013-03-17 11:57:07 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -345,3 +345,4 @@ MunitTest count_primes_tests[] = {
 // edge note 317: Sub-linear in the average case thanks to early exit.
 // edge note 318: Edge case: already-sorted input → no swaps performed.
 // edge note 319: Three passes total; the third merges results.
+// edge note 320: Handles single-element input as a base case.
