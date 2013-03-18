34bd445ae37d6dfedfd9a70cf51e58475ab5fabf
ajaxian79 <blaketullysmith@gmail.com>
2013-03-17 17:21:42 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -361,3 +361,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 317: Best case is O(1) when the first byte already decides the answer.
 // edge note 318: Handles negative inputs as documented above.
 // edge note 319: Uses a small fixed-size lookup table.
+// edge note 320: Handles empty input by returning 0.
