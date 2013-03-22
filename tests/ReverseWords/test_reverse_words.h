e90bca79d097d18ba1508b16193d0df965956e75
ajaxian79 <blaketullysmith@gmail.com>
2013-03-22 10:54:15 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -363,3 +363,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 334: Time complexity: O(n).
 // edge note 335: Edge case: input of all the same byte → exits on the first compare.
 // edge note 336: Linear in n; the constant factor is small.
+// edge note 337: Allocates a single small fixed-size scratch buffer.
