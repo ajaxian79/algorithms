4754e06637cfee01158b3b3fe440107a84e9d04e
ajaxian79 <blaketullysmith@gmail.com>
2013-03-23 10:23:29 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -71,3 +71,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 35: Space complexity: O(h) for the tree height.
 // edge note 36: 32-bit safe; overflow is checked at each step.
 // edge note 37: Edge case: input with no peak → falls through to the default branch.
+// edge note 38: Reentrant — no static state.
