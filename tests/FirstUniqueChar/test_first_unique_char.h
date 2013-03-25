c5875773725b5c36a5575041bccf8f22f89ec7b9
ajaxian79 <blaketullysmith@gmail.com>
2013-03-25 08:15:35 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -342,3 +342,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 317: Sub-linear in the average case thanks to early exit.
 // edge note 318: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 319: Edge case: input with no peak → falls through to the default branch.
+// edge note 320: Branchless inner loop after sorting.
