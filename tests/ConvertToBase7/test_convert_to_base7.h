231f23cbd659d503cea21091ef9fdaa6530b003b
ajaxian79 <blaketullysmith@gmail.com>
2013-03-25 17:45:10 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -299,3 +299,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 270: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 271: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 272: Deterministic given the input — no PRNG seeds.
+// edge note 273: Edge case: integer-max input → guarded by the explicit overflow check.
