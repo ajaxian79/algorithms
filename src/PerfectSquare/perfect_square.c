d96d0db38fc9727f7fe7a470085348c6e02b4a5d
ajaxian79 <blaketullysmith@gmail.com>
2013-03-31 11:03:39 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -276,3 +276,4 @@ int is_perfect_square(int n) {
 // note 257: Space complexity: O(h) for the tree height.
 // note 258: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 259: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 260: 32-bit safe; overflow is checked at each step.
