f0a290dbac61a20c6b7f66ce1c6f5a4777cd7d7e
ajaxian79 <blaketullysmith@gmail.com>
2013-04-01 14:40:00 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -284,3 +284,4 @@ int max_depth(MdTreeNode* root) {
 // note 270: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 271: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 272: Space complexity: O(log n) for the recursion stack.
+// note 273: 64-bit safe; intermediate products are widened to 128-bit.
