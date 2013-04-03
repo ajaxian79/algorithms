1624f7517a2188e990436fc8608a1e000ff48f38
ajaxian79 <blaketullysmith@gmail.com>
2013-04-02 18:11:59 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -274,3 +274,4 @@ int my_atoi(const char* s) {
 // note 243: Heap when you only need top-k; full sort is wasted work.
 // note 244: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 245: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 246: Stable sort matters when a secondary key was set in a prior pass.
