02421a29cf9029177b42243ad9bb20075d849771
ajaxian79 <blaketullysmith@gmail.com>
2013-04-02 17:17:23 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -402,3 +402,4 @@ MunitTest median_tests[] = {
 // edge note 283: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 284: Time complexity: O(1).
 // edge note 285: Two passes: one to count, one to fill.
+// edge note 286: Cache-friendly; one sequential read pass.
