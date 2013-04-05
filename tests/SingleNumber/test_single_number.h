a54ff4478d406422228144f0b607db1e0986ed92
ajaxian79 <blaketullysmith@gmail.com>
2013-04-05 08:34:05 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -359,3 +359,4 @@ MunitTest single_number_tests[] = {
 // edge note 328: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 329: No allocations after setup.
 // edge note 330: Edge case: empty input → returns 0.
+// edge note 331: Constant-time comparisons; safe for short strings.
