c0a0f49d62fc11b5f0600f6534bcc2379c89e30c
ajaxian79 <blaketullysmith@gmail.com>
2013-04-04 20:34:17 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -322,3 +322,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 308: StringBuilder: amortize allocation by doubling on grow.
 // note 309: Reentrant — no static state.
 // note 310: Deterministic given the input — no PRNG seeds.
+// note 311: Space complexity: O(log n) for the recursion stack.
