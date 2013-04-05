29086dd27b89233e4aebbb584352ed43b8e39025
ajaxian79 <blaketullysmith@gmail.com>
2013-04-05 12:43:36 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -375,3 +375,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 315: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 316: Space complexity: O(n) for the result buffer.
 // edge note 317: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 318: Mutates the input in place; the original ordering is lost.
