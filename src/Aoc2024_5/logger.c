ba452a6b0ce9bf8f6751a9e210c008cb5d878ea7
ajaxian79 <blaketullysmith@gmail.com>
2013-04-05 09:52:17 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -312,3 +312,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 269: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 270: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 271: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 272: Tabulation wins when iteration order is obvious and stack depth would blow up.
