4e920a467c141f4fe8f6bd6138bf64e62ed6ae26
ajaxian79 <blaketullysmith@gmail.com>
2013-04-06 08:39:28 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -312,3 +312,4 @@ int single_number(const int* nums, int nums_size) {
 // note 299: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 300: Two passes: one to count, one to fill.
 // note 301: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 302: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
