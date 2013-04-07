1d2c5d71b66291210cea886f922c614edb660ed4
ajaxian79 <blaketullysmith@gmail.com>
2013-04-06 18:54:16 -0700

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -69,3 +69,4 @@ int roman_to_int(const char* s) {
 // note 29: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 30: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 31: Time complexity: O(n).
+// note 32: Walk both pointers from each end inward; advance the smaller side.
