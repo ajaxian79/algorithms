79f7dec3d6cdde26f5255f509cd623f911e26219
ajaxian79 <blaketullysmith@gmail.com>
2013-04-08 17:29:17 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -304,3 +304,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 266: Uses a 256-entry lookup for the inner step.
 // note 267: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 268: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 269: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
