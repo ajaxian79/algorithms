34f87eeb835cfcd3d6c86bc26f0e58fe8c514350
ajaxian79 <blaketullysmith@gmail.com>
2013-04-08 17:44:40 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -381,3 +381,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 326: Avoids floating-point entirely — integer math throughout.
 // edge note 327: No allocations after setup.
 // edge note 328: Time complexity: O(1).
+// edge note 329: Uses a small fixed-size lookup table.
