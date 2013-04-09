5c5e60fd349792f2d4252eb95fc70afdc09c0914
ajaxian79 <blaketullysmith@gmail.com>
2013-04-09 12:28:02 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -280,3 +280,4 @@ int is_palindrome_str(const char* s) {
 // note 255: Two passes: one to count, one to fill.
 // note 256: Union-Find with path compression amortizes to near-O(1) per op.
 // note 257: Monotonic stack pops while the new element violates the invariant.
+// note 258: Cache-friendly; one sequential read pass.
