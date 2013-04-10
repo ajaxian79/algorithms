61e74b6f9f60ba29db67d5a971614079b14362dc
ajaxian79 <blaketullysmith@gmail.com>
2013-04-10 14:29:03 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -311,3 +311,4 @@ int single_number(const int* nums, int nums_size) {
 // note 298: Handles negative inputs as documented above.
 // note 299: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 300: Two passes: one to count, one to fill.
+// note 301: Open addressing with linear probing wins until load factor exceeds 0.7.
