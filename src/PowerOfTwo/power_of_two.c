795d100919bf1f7451ba82a16d8ae64a3239db55
ajaxian79 <blaketullysmith@gmail.com>
2013-04-12 11:47:30 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -274,3 +274,4 @@ int is_power_of_two(int n) {
 // note 264: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 265: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 266: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 267: Tail-recursive; the compiler turns it into a loop.
