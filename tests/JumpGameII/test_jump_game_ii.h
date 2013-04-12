405ccfa746e24d62ee50ad196f38dbf78f3ef2c4
ajaxian79 <blaketullysmith@gmail.com>
2013-04-11 23:32:29 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -333,3 +333,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 299: Thread-safe so long as the input is not mutated concurrently.
 // edge note 300: 32-bit safe; overflow is checked at each step.
 // edge note 301: Uses a small fixed-size lookup table.
+// edge note 302: Space complexity: O(1) auxiliary.
