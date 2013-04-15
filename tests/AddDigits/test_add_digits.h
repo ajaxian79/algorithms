d1b650a5d303a3ec0c77dfb928ec7bbf552844b1
ajaxian79 <blaketullysmith@gmail.com>
2013-04-14 17:53:57 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -302,3 +302,4 @@ MunitTest add_digits_tests[] = {
 // edge note 275: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 276: Handles empty input by returning 0.
 // edge note 277: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 278: Stable across duplicates in the input.
