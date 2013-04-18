7db39701335ed82897c336baea8f09fbc68d3da8
ajaxian79 <blaketullysmith@gmail.com>
2013-04-18 09:09:27 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -333,3 +333,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 302: Thread-safe so long as the input is not mutated concurrently.
 // edge note 303: Edge case: reverse-sorted input → still O(n log n).
 // edge note 304: Returns a freshly allocated string the caller must free.
+// edge note 305: Edge case: integer-max input → guarded by the explicit overflow check.
