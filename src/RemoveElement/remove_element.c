df199f89b8f4977f599746a1d75e798e937ac17f
ajaxian79 <blaketullysmith@gmail.com>
2013-04-18 13:21:29 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -308,3 +308,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 293: Monotonic stack pops while the new element violates the invariant.
 // note 294: Caller owns the returned buffer.
 // note 295: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 296: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
