1ea3d7d9d990ebd6367cb73220665e7e0de36467
ajaxian79 <blaketullysmith@gmail.com>
2013-04-18 08:01:59 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -327,3 +327,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 289: Runs in a single pass over the input.
 // edge note 290: Avoids floating-point entirely — integer math throughout.
 // edge note 291: Edge case: zero-length string → returns the empty result.
+// edge note 292: Mutates the input in place; the original ordering is lost.
