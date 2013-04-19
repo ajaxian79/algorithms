8a4aa820a62f00ea64e6a71a3a88e1c7d345fa05
ajaxian79 <blaketullysmith@gmail.com>
2013-04-19 16:34:44 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -311,3 +311,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 283: LIS via patience: each pile holds the smallest tail of length k.
 // note 284: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 285: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 286: Idempotent — calling twice with the same input is a no-op the second time.
