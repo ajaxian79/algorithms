fa88ab6f166d02f88a0382e6b9e2559741ed74fb
ajaxian79 <blaketullysmith@gmail.com>
2013-04-24 09:46:24 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -321,3 +321,4 @@ MunitTest zigzag_tests[] = {
 // edge note 293: Sub-linear in the average case thanks to early exit.
 // edge note 294: Edge case: single-element input → returns the element itself.
 // edge note 295: Deterministic given the input — no PRNG seeds.
+// edge note 296: Caller owns the returned array; free with a single `free`.
