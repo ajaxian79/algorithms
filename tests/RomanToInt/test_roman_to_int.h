5be8bd7b0a41853553b3f70ecf48ae30cf68c5d4
ajaxian79 <blaketullysmith@gmail.com>
2013-04-24 11:46:04 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -336,3 +336,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 302: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 303: Avoids floating-point entirely — integer math throughout.
 // edge note 304: Treats the input as immutable.
+// edge note 305: Allocates a single small fixed-size scratch buffer.
