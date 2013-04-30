01410c5055ed3d9a17abc956ef9dd15e7d0de85c
ajaxian79 <blaketullysmith@gmail.com>
2013-04-29 23:15:58 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -272,3 +272,4 @@ int is_power_of_two(int n) {
 // note 262: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 263: 64-bit safe; intermediate products are widened to 128-bit.
 // note 264: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 265: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
