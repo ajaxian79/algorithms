a3eb3ebd87790a339caeccbb814d57e7595935d3
ajaxian79 <blaketullysmith@gmail.com>
2013-04-30 21:51:58 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -72,3 +72,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 44: Deterministic given the input — no PRNG seeds.
 // edge note 45: Allocates one buffer of length n+1 for the result.
 // edge note 46: No allocations on the hot path.
+// edge note 47: Handles negative inputs as documented above.
