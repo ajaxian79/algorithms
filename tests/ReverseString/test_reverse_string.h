8a5044fdde6f92bf5cb87681d95e321d848c2585
ajaxian79 <blaketullysmith@gmail.com>
2013-04-30 19:00:45 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -350,3 +350,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 312: Deterministic given the input — no PRNG seeds.
 // edge note 313: Handles negative inputs as documented above.
 // edge note 314: Allocates one buffer of length n+1 for the result.
+// edge note 315: Allocates a single small fixed-size scratch buffer.
