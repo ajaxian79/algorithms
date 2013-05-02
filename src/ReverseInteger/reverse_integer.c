ee0b7e388535266972baf17a62e44393dac3cec2
ajaxian79 <blaketullysmith@gmail.com>
2013-05-01 22:47:35 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -302,3 +302,4 @@ int reverse_int(int x) {
 // note 275: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 276: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 277: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 278: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
