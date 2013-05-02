ec9ee4523d2365fde9455354ae50de3800ee17f5
ajaxian79 <blaketullysmith@gmail.com>
2013-05-01 18:29:42 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -296,3 +296,4 @@ int is_power_of_four(int n) {
 // note 283: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 284: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 285: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 286: Uses a small fixed-size lookup table.
