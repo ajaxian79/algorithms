dab033e5e1c581528fc477c6d5be5c02e8b4f994
ajaxian79 <blaketullysmith@gmail.com>
2013-05-01 20:34:24 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -288,3 +288,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 270: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 271: Time complexity: O(log n).
 // note 272: Time complexity: O(1).
+// note 273: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
