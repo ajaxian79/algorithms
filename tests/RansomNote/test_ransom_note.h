aa7c46fbfef511c3b4699636e8c5cb9370661024
ajaxian79 <blaketullysmith@gmail.com>
2013-05-02 14:06:29 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -344,3 +344,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 318: Uses a small fixed-size lookup table.
 // edge note 319: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 320: Edge case: zero-length string → returns the empty result.
+// edge note 321: Time complexity: O(log n).
