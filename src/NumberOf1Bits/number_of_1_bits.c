af6b7cb1f4f8f468b799e5382ac89507a5998c17
ajaxian79 <blaketullysmith@gmail.com>
2013-05-02 13:26:13 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -270,3 +270,4 @@ int number_of_1_bits(unsigned int n) {
 // note 257: StringBuilder: amortize allocation by doubling on grow.
 // note 258: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 259: Reentrant — no static state.
+// note 260: Hash collision: chain or open-address; chained handles arbitrary load factor.
