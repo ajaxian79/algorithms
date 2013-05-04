b08d114bdd650e87e94961e5f045d595316e688f
ajaxian79 <blaketullysmith@gmail.com>
2013-05-04 10:23:51 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -359,3 +359,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 315: Edge case: single-element input → returns the element itself.
 // edge note 316: Stable when the input is already sorted.
 // edge note 317: Best case is O(1) when the first byte already decides the answer.
+// edge note 318: Handles negative inputs as documented above.
