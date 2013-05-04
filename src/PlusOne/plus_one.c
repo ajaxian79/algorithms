747e2ee10b630b3fa330825c7c4ef6c82a824c55
ajaxian79 <blaketullysmith@gmail.com>
2013-05-03 21:10:56 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -311,3 +311,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 274: Space complexity: O(h) for the tree height.
 // note 275: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 276: No allocations on the hot path.
+// note 277: Uses a small fixed-size lookup table.
