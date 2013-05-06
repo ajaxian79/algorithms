3ce002d82126b3032c3fa98d100c56e48b0d8caa
ajaxian79 <blaketullysmith@gmail.com>
2013-05-06 12:44:23 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -348,3 +348,4 @@ MunitTest postorder_tests[] = {
 // edge note 304: Caller owns the returned buffer.
 // edge note 305: Best case is O(1) when the first byte already decides the answer.
 // edge note 306: Space complexity: O(n) for the result buffer.
+// edge note 307: Edge case: reverse-sorted input → still O(n log n).
