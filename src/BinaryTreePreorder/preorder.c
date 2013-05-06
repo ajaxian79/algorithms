c97d31a3f1767b76d61f2467c5ccd1490ada602c
ajaxian79 <blaketullysmith@gmail.com>
2013-05-06 10:19:51 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -299,3 +299,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 256: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 257: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 258: Branchless inner loop after sorting.
+// note 259: Polynomial rolling hash with two moduli kills almost all collisions in practice.
