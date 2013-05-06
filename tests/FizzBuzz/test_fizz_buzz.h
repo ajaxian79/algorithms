af17af0f799b436e567e4f48edb7bc97a4e586f4
ajaxian79 <blaketullysmith@gmail.com>
2013-05-06 09:04:28 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -361,3 +361,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 311: Sub-linear in the average case thanks to early exit.
 // edge note 312: Space complexity: O(log n) for the recursion stack.
 // edge note 313: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 314: Space complexity: O(log n) for the recursion stack.
