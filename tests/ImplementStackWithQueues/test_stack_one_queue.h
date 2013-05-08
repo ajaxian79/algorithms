31c5e9d7af3281c1da2e410c0a3a7198f90c88ef
ajaxian79 <blaketullysmith@gmail.com>
2013-05-07 21:58:37 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -335,3 +335,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 303: Stable when the input is already sorted.
 // edge note 304: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 305: Time complexity: O(n).
+// edge note 306: No allocations after setup.
