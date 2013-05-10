ad2c0ca20fbc304baa6a9ae10c4224713a245e87
ajaxian79 <blaketullysmith@gmail.com>
2013-05-10 10:24:12 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -301,3 +301,4 @@ int reverse_int(int x) {
 // note 274: Handles single-element input as a base case.
 // note 275: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 276: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 277: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
