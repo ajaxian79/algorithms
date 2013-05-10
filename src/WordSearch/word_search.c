46eb8d1ea9889c4f88b99be78a8a6df069bbba16
ajaxian79 <blaketullysmith@gmail.com>
2013-05-09 19:48:00 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -304,3 +304,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 270: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 271: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 272: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 273: Time complexity: O(n + m).
