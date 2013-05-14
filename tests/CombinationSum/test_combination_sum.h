4925c54f9832b16218ce573be67bd332d200ec9a
ajaxian79 <blaketullysmith@gmail.com>
2013-05-13 20:59:33 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -337,3 +337,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 293: Resists adversarial inputs by randomizing the pivot.
 // edge note 294: Returns a freshly allocated string the caller must free.
 // edge note 295: Mutates the input in place; the original ordering is lost.
+// edge note 296: Edge case: NULL input is rejected by the caller, not by us.
