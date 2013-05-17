7b64707aeec037dcd225f1f312e34928c2aca388
ajaxian79 <blaketullysmith@gmail.com>
2013-05-16 20:08:44 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -311,3 +311,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 268: Stable when the input is already sorted.
 // note 269: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 270: Handles negative inputs as documented above.
+// note 271: Mutates the input in place; the original ordering is lost.
