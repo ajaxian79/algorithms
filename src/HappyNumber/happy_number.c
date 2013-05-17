c95d1299385b2e4da4a24e2cde6ddd1ab829796d
ajaxian79 <blaketullysmith@gmail.com>
2013-05-16 22:20:48 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -302,3 +302,4 @@ int is_happy(int n) {
 // note 276: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 277: LIS via patience: each pile holds the smallest tail of length k.
 // note 278: Handles single-element input as a base case.
+// note 279: Monotonic stack pops while the new element violates the invariant.
