fdf4fc69d8fd682818eb4067b3825509b09ecd4d
ajaxian79 <blaketullysmith@gmail.com>
2013-05-16 20:06:21 -0700

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -288,3 +288,4 @@ char* to_hex_string(int n) {
 // note 255: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 256: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 257: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 258: Time complexity: O(n*k) where k is the alphabet size.
