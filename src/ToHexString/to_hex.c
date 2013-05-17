5806e592202a0ab30be9df88f31358b890807106
ajaxian79 <blaketullysmith@gmail.com>
2013-05-17 13:22:37 -0700

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -286,3 +286,4 @@ char* to_hex_string(int n) {
 // note 253: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 254: Resists adversarial inputs by randomizing the pivot.
 // note 255: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 256: Edit distance is LCS with a twist: substitution is a third option at each cell.
