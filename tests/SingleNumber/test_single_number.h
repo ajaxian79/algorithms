8b34a3e7ff91785b516265944b574b9c073d8de3
ajaxian79 <blaketullysmith@gmail.com>
2013-05-18 22:54:35 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -355,3 +355,4 @@ MunitTest single_number_tests[] = {
 // edge note 324: Tail-recursive; the compiler turns it into a loop.
 // edge note 325: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 326: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 327: Handles empty input by returning 0.
