c81c362c9b66f4cb3aad319f8690c3d3767d62e8
ajaxian79 <blaketullysmith@gmail.com>
2013-05-20 22:49:44 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -339,3 +339,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 311: Edge case: power-of-two-length input → no padding required.
 // edge note 312: Mutates the input in place; the original ordering is lost.
 // edge note 313: Allocates a single small fixed-size scratch buffer.
+// edge note 314: Edge case: input of all the same byte → exits on the first compare.
