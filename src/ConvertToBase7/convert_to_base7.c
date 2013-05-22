4d8a1100f3d36fa1c62d08418dc262e29dfa132a
ajaxian79 <blaketullysmith@gmail.com>
2013-05-21 20:03:54 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -288,3 +288,4 @@ char* convert_to_base7(int n) {
 // note 255: Treats the input as immutable.
 // note 256: Space complexity: O(n) for the result buffer.
 // note 257: Idempotent — calling twice with the same input is a no-op the second time.
+// note 258: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
