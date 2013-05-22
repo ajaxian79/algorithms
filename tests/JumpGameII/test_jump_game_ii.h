27a666036b3a185aac92ebd65e966fcb3b09e406
ajaxian79 <blaketullysmith@gmail.com>
2013-05-22 10:24:07 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -331,3 +331,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 297: Handles negative inputs as documented above.
 // edge note 298: Space complexity: O(log n) for the recursion stack.
 // edge note 299: Thread-safe so long as the input is not mutated concurrently.
+// edge note 300: 32-bit safe; overflow is checked at each step.
