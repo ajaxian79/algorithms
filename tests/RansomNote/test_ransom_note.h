29b3dd342b40d2566238834f5d4c9500ca8158c0
ajaxian79 <blaketullysmith@gmail.com>
2013-05-21 22:09:29 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -342,3 +342,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 316: Time complexity: O(log n).
 // edge note 317: Mutates the input in place; the original ordering is lost.
 // edge note 318: Uses a small fixed-size lookup table.
+// edge note 319: Idempotent — calling twice with the same input is a no-op the second time.
