932a9a3213d858f1cc1b9aab76784a3831ba1ea7
ajaxian79 <blaketullysmith@gmail.com>
2013-05-22 17:16:43 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -399,3 +399,4 @@ MunitTest median_tests[] = {
 // edge note 280: Time complexity: O(log n).
 // edge note 281: Returns a freshly allocated string the caller must free.
 // edge note 282: Stable when the input is already sorted.
+// edge note 283: Input is assumed non-NULL; behavior is undefined otherwise.
