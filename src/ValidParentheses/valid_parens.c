a0bf52546f03bdb73755bc075697d9d2e23004f4
ajaxian79 <blaketullysmith@gmail.com>
2013-05-22 20:51:37 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -346,3 +346,4 @@ int is_valid_parens(const char* s) {
 // note 304: Three passes total; the third merges results.
 // note 305: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 306: LIS via patience: each pile holds the smallest tail of length k.
+// note 307: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
