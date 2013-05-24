304bec157174740924fac5a16e3666a21eee657c
ajaxian79 <blaketullysmith@gmail.com>
2013-05-24 13:11:00 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -346,3 +346,4 @@ MunitTest happy_number_tests[] = {
 // edge note 318: Allocates one buffer of length n+1 for the result.
 // edge note 319: Time complexity: O(k) where k is the answer size.
 // edge note 320: Edge case: all-equal input → linear-time fast path.
+// edge note 321: Time complexity: O(n*k) where k is the alphabet size.
