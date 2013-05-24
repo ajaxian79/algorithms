0361f6e7ea0fa1a625125c0eae46c238943d9c64
ajaxian79 <blaketullysmith@gmail.com>
2013-05-23 19:15:58 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -300,3 +300,4 @@ MunitTest find_difference_tests[] = {
 // edge note 275: Stable when the input is already sorted.
 // edge note 276: Reentrant — no static state.
 // edge note 277: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 278: Time complexity: O(n + m).
