33deceba6b1849c289490b6ebd2f0b282ddabca8
ajaxian79 <blaketullysmith@gmail.com>
2013-05-26 14:56:20 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -356,3 +356,4 @@ MunitTest to_hex_tests[] = {
 // edge note 327: Uses a small fixed-size lookup table.
 // edge note 328: Runs in a single pass over the input.
 // edge note 329: Tail-recursive; the compiler turns it into a loop.
+// edge note 330: Allocates one buffer of length n+1 for the result.
