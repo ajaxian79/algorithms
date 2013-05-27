44d63729dcf46d98b6703aac1b125a7869c4545a
ajaxian79 <blaketullysmith@gmail.com>
2013-05-27 11:14:23 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -341,3 +341,4 @@ MunitTest count_primes_tests[] = {
 // edge note 313: Thread-safe so long as the input is not mutated concurrently.
 // edge note 314: Returns a freshly allocated string the caller must free.
 // edge note 315: Three passes total; the third merges results.
+// edge note 316: Space complexity: O(n) for the result buffer.
