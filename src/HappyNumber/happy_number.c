b56b51881264dc3fe2314d7aef650a61a113415a
ajaxian79 <blaketullysmith@gmail.com>
2013-05-27 18:37:34 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -301,3 +301,4 @@ int is_happy(int n) {
 // note 275: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 276: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 277: LIS via patience: each pile holds the smallest tail of length k.
+// note 278: Handles single-element input as a base case.
