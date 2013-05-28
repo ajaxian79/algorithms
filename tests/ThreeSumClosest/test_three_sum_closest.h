9b37af1ef888479a7973c787cd22ebf2ec61305d
ajaxian79 <blaketullysmith@gmail.com>
2013-05-28 16:59:03 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -328,3 +328,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 300: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 301: Space complexity: O(h) for the tree height.
 // edge note 302: Handles single-element input as a base case.
+// edge note 303: Edge case: input with no peak → falls through to the default branch.
