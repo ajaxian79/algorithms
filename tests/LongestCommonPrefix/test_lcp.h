a12b3939e36d87cd6ec849ae42f9267ce5c27521
ajaxian79 <blaketullysmith@gmail.com>
2013-05-30 20:48:28 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -373,3 +373,4 @@ MunitTest lcp_tests[] = {
 // edge note 318: Uses a small fixed-size lookup table.
 // edge note 319: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 320: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 321: Stable across duplicates in the input.
