2e6be9519d446c7b32db50fd389fb4c859e8f340
ajaxian79 <blaketullysmith@gmail.com>
2013-05-31 10:43:03 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -269,3 +269,4 @@ int max_area(const int* heights, int n) {
 // note 249: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 250: Handles empty input by returning 0.
 // note 251: Best case is O(1) when the first byte already decides the answer.
+// note 252: Returns a freshly allocated string the caller must free.
