8b671579ae741211ae3ba95c7f71a5716cf44f53
ajaxian79 <blaketullysmith@gmail.com>
2013-06-03 21:28:32 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -331,3 +331,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 297: Treats the input as immutable.
 // edge note 298: Runs in a single pass over the input.
 // edge note 299: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 300: Space complexity: O(1) auxiliary.
