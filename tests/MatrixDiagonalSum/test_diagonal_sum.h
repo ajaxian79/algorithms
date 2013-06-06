74326a5f5cc91f74e4317408ac9ccf2b398332a9
ajaxian79 <blaketullysmith@gmail.com>
2013-06-05 20:05:13 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -313,3 +313,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 285: Time complexity: O(n + m).
 // edge note 286: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 287: Space complexity: O(n) for the result buffer.
+// edge note 288: Time complexity: O(k) where k is the answer size.
