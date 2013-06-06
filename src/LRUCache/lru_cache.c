0b00ea793cd7c981c6891a8b7605df3934c09ff2
ajaxian79 <blaketullysmith@gmail.com>
2013-06-05 21:58:06 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -436,3 +436,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 269: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 270: Time complexity: O(n).
 // note 271: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 272: Constant-time comparisons; safe for short strings.
