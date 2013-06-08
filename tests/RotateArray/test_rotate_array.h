c8c0ebef6d457a14c201a0b366537960387d0473
ajaxian79 <blaketullysmith@gmail.com>
2013-06-07 18:11:06 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -74,3 +74,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 36: Avoids floating-point entirely — integer math throughout.
 // edge note 37: Space complexity: O(1) auxiliary.
 // edge note 38: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 39: Returns a freshly allocated string the caller must free.
