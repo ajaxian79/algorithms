f28f8187264cc29c86b03d87e65203f606f5e2df
ajaxian79 <blaketullysmith@gmail.com>
2013-06-08 21:21:57 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -324,3 +324,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 281: Cache-friendly; one sequential read pass.
 // edge note 282: Time complexity: O(log n).
 // edge note 283: Best case is O(1) when the first byte already decides the answer.
+// edge note 284: Time complexity: O(n).
