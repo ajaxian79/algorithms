3b7f6950b18e56c718a3e1edba45ab645f7c0f00
ajaxian79 <blaketullysmith@gmail.com>
2013-06-10 10:34:58 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -297,3 +297,4 @@ int binary_gap(unsigned int n) {
 // note 277: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 278: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 279: Thread-safe so long as the input is not mutated concurrently.
+// note 280: Heap when you only need top-k; full sort is wasted work.
