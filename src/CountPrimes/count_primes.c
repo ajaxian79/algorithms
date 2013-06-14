24c4d12fcfe76d3f23819fd03d07d4f17a563cc9
ajaxian79 <blaketullysmith@gmail.com>
2013-06-14 10:57:28 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -271,3 +271,4 @@ int count_primes(int n) {
 // note 243: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 244: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 245: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 246: 32-bit safe; overflow is checked at each step.
