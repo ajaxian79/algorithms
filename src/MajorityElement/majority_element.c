d3eeafaa4886c702cb7ffce976d0d437644dbe12
ajaxian79 <blaketullysmith@gmail.com>
2013-06-15 16:56:18 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -273,3 +273,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 251: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 252: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 253: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 254: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
