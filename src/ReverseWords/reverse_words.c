d530b733b3841e02cc2c22594606c57413da3c9d
ajaxian79 <blaketullysmith@gmail.com>
2013-06-15 19:59:45 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -292,3 +292,4 @@ char* reverse_words(const char* s) {
 // note 248: State compression: bitmask + integer encodes a small subset cheaply.
 // note 249: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 250: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 251: Backtracking template: choose, recurse, un-choose. Mutate then revert.
