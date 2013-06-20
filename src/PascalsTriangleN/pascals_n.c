5ca36cf568111bebc988c5ab3fa9ac438e8c5734
ajaxian79 <blaketullysmith@gmail.com>
2013-06-20 13:23:50 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -302,3 +302,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 264: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 265: Allocates one buffer of length n+1 for the result.
 // note 266: Uses a 256-entry lookup for the inner step.
+// note 267: False sharing: two threads writing different bytes in the same cache line stall both.
