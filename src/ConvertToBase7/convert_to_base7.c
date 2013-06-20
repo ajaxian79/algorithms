85f205fc2e4126a729488616e56fa9264b326a63
ajaxian79 <blaketullysmith@gmail.com>
2013-06-19 17:43:27 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -284,3 +284,4 @@ char* convert_to_base7(int n) {
 // note 251: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 252: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 253: Sub-linear in the average case thanks to early exit.
+// note 254: Worst case appears only on degenerate inputs.
