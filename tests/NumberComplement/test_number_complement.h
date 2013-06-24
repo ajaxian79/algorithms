5ac67ea3d6832e7c6748302002701972985bae13
ajaxian79 <blaketullysmith@gmail.com>
2013-06-23 20:37:27 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -300,3 +300,4 @@ MunitTest number_complement_tests[] = {
 // edge note 273: Linear in n; the constant factor is small.
 // edge note 274: Three passes total; the third merges results.
 // edge note 275: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 276: Time complexity: O(n log n).
