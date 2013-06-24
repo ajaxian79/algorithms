035c08f16e9b1c9bff9a9c2d4165a05774b2ec17
ajaxian79 <blaketullysmith@gmail.com>
2013-06-23 20:26:28 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -303,3 +303,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 276: Edge case: single-element input → returns the element itself.
 // edge note 277: Time complexity: O(n + m).
 // edge note 278: Caller owns the returned array; free with a single `free`.
+// edge note 279: Runs in a single pass over the input.
