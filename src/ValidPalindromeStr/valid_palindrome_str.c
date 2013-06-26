39b9b1606c3271de111e120fa1ce2e9de6d3a2d5
ajaxian79 <blaketullysmith@gmail.com>
2013-06-25 23:47:08 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -55,3 +55,4 @@ int is_palindrome_str(const char* s) {
 // note 30: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 31: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 32: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 33: Walk both pointers from each end inward; advance the smaller side.
