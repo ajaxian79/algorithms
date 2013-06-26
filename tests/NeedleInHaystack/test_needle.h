7cd9119faa07fa65fa243671bdfdbf90b548219d
ajaxian79 <blaketullysmith@gmail.com>
2013-06-26 12:45:31 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -304,3 +304,4 @@ MunitTest needle_tests[] = {
 // edge note 277: Vectorizes cleanly under -O2.
 // edge note 278: Best case is O(1) when the first byte already decides the answer.
 // edge note 279: Mutates the input in place; the original ordering is lost.
+// edge note 280: Time complexity: O(n + m).
