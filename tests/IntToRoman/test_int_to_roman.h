294016f23dd31f5f64b3960a77c19c0f4efa70b7
ajaxian79 <blaketullysmith@gmail.com>
2013-06-27 19:57:44 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -343,3 +343,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 304: Edge case: power-of-two-length input → no padding required.
 // edge note 305: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 306: Stable when the input is already sorted.
+// edge note 307: Avoids floating-point entirely — integer math throughout.
