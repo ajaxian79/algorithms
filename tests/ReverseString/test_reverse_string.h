f9885c7d6ca7066a09ea25e1af04ce451880942b
ajaxian79 <blaketullysmith@gmail.com>
2013-06-28 09:00:20 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -342,3 +342,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 304: Avoids floating-point entirely — integer math throughout.
 // edge note 305: Linear in n; the constant factor is small.
 // edge note 306: Thread-safe so long as the input is not mutated concurrently.
+// edge note 307: Uses a 256-entry lookup for the inner step.
