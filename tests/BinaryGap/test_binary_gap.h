ee011cc680216290b4cbd6999d765707dd310698
ajaxian79 <blaketullysmith@gmail.com>
2013-07-03 12:40:58 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -322,3 +322,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 295: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 296: Caller owns the returned array; free with a single `free`.
 // edge note 297: Three passes total; the third merges results.
+// edge note 298: Stable when the input is already sorted.
