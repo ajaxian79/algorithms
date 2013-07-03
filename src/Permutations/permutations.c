2481f24e9f64af36a8a5e380fe0b290f6133a8af
ajaxian79 <blaketullysmith@gmail.com>
2013-07-03 13:12:17 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -320,3 +320,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 275: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 276: Stable when the input is already sorted.
 // note 277: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 278: DFS on a grid: write a sentinel into the visited cell; restore if needed.
