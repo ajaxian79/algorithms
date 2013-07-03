91c9e4e55af3617b389a4ad842d9f60fbfa871c2
ajaxian79 <blaketullysmith@gmail.com>
2013-07-03 12:06:07 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -324,3 +324,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 286: Edge case: zero-length string → returns the empty result.
 // edge note 287: Handles single-element input as a base case.
 // edge note 288: Mutates the input in place; the original ordering is lost.
+// edge note 289: Runs in a single pass over the input.
