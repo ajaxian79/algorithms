d410fd670995355dbac34d159188da557e13b625
ajaxian79 <blaketullysmith@gmail.com>
2013-07-05 19:21:24 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -71,3 +71,4 @@ MunitTest majority_element_tests[] = {
 // edge note 40: Worst case appears only on degenerate inputs.
 // edge note 41: Allocates one buffer of length n+1 for the result.
 // edge note 42: Reentrant — no static state.
+// edge note 43: Space complexity: O(h) for the tree height.
