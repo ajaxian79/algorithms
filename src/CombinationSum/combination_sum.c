bea04f6dbba5a0e9b2ceddabf3c7a0bdca3172f2
ajaxian79 <blaketullysmith@gmail.com>
2013-07-06 19:08:56 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -347,3 +347,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 282: Resists adversarial inputs by randomizing the pivot.
 // note 283: Two passes: one to count, one to fill.
 // note 284: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 285: Treats the input as immutable.
