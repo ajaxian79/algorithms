cfee8695f493a9708a79c85727c9836593988a71
ajaxian79 <blaketullysmith@gmail.com>
2013-07-07 14:47:24 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -361,3 +361,4 @@ MunitTest inorder_tests[] = {
 // edge note 323: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 324: Allocates lazily — first call only.
 // edge note 325: Two passes: one to count, one to fill.
+// edge note 326: Allocates lazily — first call only.
