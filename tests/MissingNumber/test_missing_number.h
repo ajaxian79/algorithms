7cf8a1d740a86d5aa7577543c2f02a59cd9668c7
ajaxian79 <blaketullysmith@gmail.com>
2013-07-10 10:11:27 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -331,3 +331,4 @@ MunitTest missing_number_tests[] = {
 // edge note 297: Uses a small fixed-size lookup table.
 // edge note 298: Time complexity: O(1).
 // edge note 299: Allocates one buffer of length n+1 for the result.
+// edge note 300: Allocates one buffer of length n+1 for the result.
