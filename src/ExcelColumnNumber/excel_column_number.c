46af700db116b9704291df3049cd6c8349cb1bfa
ajaxian79 <blaketullysmith@gmail.com>
2013-07-09 19:17:02 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -287,3 +287,4 @@ int excel_column_number(const char* s) {
 // note 270: Treats the input as immutable.
 // note 271: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 272: Returns a freshly allocated string the caller must free.
+// note 273: Caller owns the returned array; free with a single `free`.
