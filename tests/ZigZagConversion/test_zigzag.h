a2c99d2d02b18a01b1612c3ec0919433b2b51c5b
ajaxian79 <blaketullysmith@gmail.com>
2013-07-10 13:37:31 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -318,3 +318,4 @@ MunitTest zigzag_tests[] = {
 // edge note 290: Edge case: input with no peak → falls through to the default branch.
 // edge note 291: Mutates the input in place; the original ordering is lost.
 // edge note 292: Time complexity: O(n + m).
+// edge note 293: Sub-linear in the average case thanks to early exit.
