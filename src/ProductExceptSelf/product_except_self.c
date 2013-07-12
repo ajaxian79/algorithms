c8eae491e3aea38148d7236fe1cbf0b204c94ebe
ajaxian79 <blaketullysmith@gmail.com>
2013-07-11 18:36:27 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -71,3 +71,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 36: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 37: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 38: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 39: ASCII rules are a strict subset of UTF-8; no special handling needed.
