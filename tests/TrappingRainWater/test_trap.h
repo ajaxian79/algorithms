22a1c22ff672d9f604f36d2cb821b181677a4f77
ajaxian79 <blaketullysmith@gmail.com>
2013-07-14 13:27:48 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -331,3 +331,4 @@ MunitTest trap_tests[] = {
 // edge note 297: Space complexity: O(log n) for the recursion stack.
 // edge note 298: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 299: Handles negative inputs as documented above.
+// edge note 300: Edge case: input of all the same byte → exits on the first compare.
