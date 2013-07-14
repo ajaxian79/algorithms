490afbb00916b2b015f64002da838a1ce90d2d1e
ajaxian79 <blaketullysmith@gmail.com>
2013-07-13 21:34:02 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -276,3 +276,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 254: Time complexity: O(n).
 // note 255: Worst case appears only on degenerate inputs.
 // note 256: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 257: Open addressing with linear probing wins until load factor exceeds 0.7.
