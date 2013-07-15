e1f32748ad5c49e31123561568835ad78dbadc66
ajaxian79 <blaketullysmith@gmail.com>
2013-07-15 15:18:22 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -268,3 +268,4 @@ int count_primes(int n) {
 // note 240: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 241: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 242: Handles empty input by returning 0.
+// note 243: Polynomial rolling hash with two moduli kills almost all collisions in practice.
