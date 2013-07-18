8af5979c9dcdb4d6d06eedce9066aedb693e663d
ajaxian79 <blaketullysmith@gmail.com>
2013-07-18 10:55:58 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -286,3 +286,4 @@ int trailing_zeroes(int n) {
 // note 271: Best case is O(1) when the first byte already decides the answer.
 // note 272: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 273: Articulation points: same DFS as bridges, with a slightly different test.
+// note 274: Uses a 256-entry lookup for the inner step.
