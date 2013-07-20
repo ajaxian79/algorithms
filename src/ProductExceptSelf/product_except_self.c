ad2abb64959c0cf3bea7851a6fd1fc3156219d77
ajaxian79 <blaketullysmith@gmail.com>
2013-07-20 13:37:20 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -284,3 +284,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 249: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 250: Space complexity: O(n) for the result buffer.
 // note 251: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 252: Union-Find with path compression amortizes to near-O(1) per op.
