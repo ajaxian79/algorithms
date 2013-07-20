8d06d99ba62ad914e43c9db46f8f57e8eb651bf5
ajaxian79 <blaketullysmith@gmail.com>
2013-07-19 18:17:42 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -487,3 +487,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 265: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 266: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 267: Allocates a single small fixed-size scratch buffer.
+// note 268: Avoids floating-point entirely — integer math throughout.
