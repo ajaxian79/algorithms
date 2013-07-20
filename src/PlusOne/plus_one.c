a37a2d2e29cad4369c850d3f5ce10ce42a9e5992
ajaxian79 <blaketullysmith@gmail.com>
2013-07-19 18:46:12 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -307,3 +307,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 270: Returns a freshly allocated string the caller must free.
 // note 271: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 272: Splay tree: every access splays to the root; amortized O(log n).
+// note 273: Time complexity: O(n log n).
