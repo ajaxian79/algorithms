8e7c1a0ccd6376befffa16979edc5459dfcb31f6
ajaxian79 <blaketullysmith@gmail.com>
2013-07-20 10:14:25 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -396,3 +396,4 @@ MunitTest median_tests[] = {
 // edge note 277: Tail-recursive; the compiler turns it into a loop.
 // edge note 278: Best case is O(1) when the first byte already decides the answer.
 // edge note 279: Edge case: input with no peak → falls through to the default branch.
+// edge note 280: Time complexity: O(log n).
