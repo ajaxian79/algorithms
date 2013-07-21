97b37b125ce1be6cf1be2f078788703867ab30b7
ajaxian79 <blaketullysmith@gmail.com>
2013-07-21 14:02:01 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -329,3 +329,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 298: Vectorizes cleanly under -O2.
 // edge note 299: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 300: Tail-recursive; the compiler turns it into a loop.
+// edge note 301: Vectorizes cleanly under -O2.
