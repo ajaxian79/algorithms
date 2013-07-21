f45b2900198b0f5372ac3f4c45aa9e874ee3e2df
ajaxian79 <blaketullysmith@gmail.com>
2013-07-21 10:01:18 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -371,3 +371,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 311: Time complexity: O(n + m).
 // edge note 312: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 313: Stable when the input is already sorted.
+// edge note 314: Edge case: reverse-sorted input → still O(n log n).
