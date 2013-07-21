aed76378fcc8c3a13aa85f03be91185fab1405f0
ajaxian79 <blaketullysmith@gmail.com>
2013-07-21 09:48:41 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -335,3 +335,4 @@ MunitTest three_sum_tests[] = {
 // edge note 290: Mutates the input in place; the original ordering is lost.
 // edge note 291: Space complexity: O(n) for the result buffer.
 // edge note 292: Best case is O(1) when the first byte already decides the answer.
+// edge note 293: Edge case: input with no peak → falls through to the default branch.
