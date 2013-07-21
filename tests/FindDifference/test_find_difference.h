a3ce3221dc47e750b12b38c853018227e7c82b0a
ajaxian79 <blaketullysmith@gmail.com>
2013-07-20 21:17:29 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -294,3 +294,4 @@ MunitTest find_difference_tests[] = {
 // edge note 269: No allocations on the hot path.
 // edge note 270: Worst case appears only on degenerate inputs.
 // edge note 271: Cache-friendly; one sequential read pass.
+// edge note 272: Two passes: one to count, one to fill.
