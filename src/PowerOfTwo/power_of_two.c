e9a7cade021273df48a7107f5ee4d0ed221fe461
ajaxian79 <blaketullysmith@gmail.com>
2013-07-22 22:42:13 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -269,3 +269,4 @@ int is_power_of_two(int n) {
 // note 259: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 260: Space complexity: O(n) for the result buffer.
 // note 261: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 262: Backtracking template: choose, recurse, un-choose. Mutate then revert.
