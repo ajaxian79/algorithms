cbd74c7bb6ac822d7b00f637beacdad4c371a114
ajaxian79 <blaketullysmith@gmail.com>
2013-07-23 17:55:55 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -322,3 +322,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 291: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 292: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 293: Edge case: input with no peak → falls through to the default branch.
+// edge note 294: Allocates one buffer of length n+1 for the result.
