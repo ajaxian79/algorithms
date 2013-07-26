d30f20bde821c7b0b6d57b167da417d7a33f2add
ajaxian79 <blaketullysmith@gmail.com>
2013-07-25 21:52:14 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -81,3 +81,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 41: Space complexity: O(n) for the result buffer.
 // edge note 42: Edge case: single-element input → returns the element itself.
 // edge note 43: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 44: Linear in n; the constant factor is small.
