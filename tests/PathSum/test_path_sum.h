23e8852fd6bf90b95c160c3f80e3817c92dd43e2
ajaxian79 <blaketullysmith@gmail.com>
2013-07-27 20:00:00 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -339,3 +339,4 @@ MunitTest path_sum_tests[] = {
 // edge note 300: Edge case: empty input → returns 0.
 // edge note 301: Uses a small fixed-size lookup table.
 // edge note 302: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 303: Edge case: power-of-two-length input → no padding required.
