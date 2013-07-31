0940bfdec6cce69ac7b4cada759c536889f8a306
ajaxian79 <blaketullysmith@gmail.com>
2013-07-31 11:16:40 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -324,3 +324,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 296: Edge case: single-element input → returns the element itself.
 // edge note 297: Time complexity: O(1).
 // edge note 298: No allocations after setup.
+// edge note 299: Space complexity: O(1) auxiliary.
