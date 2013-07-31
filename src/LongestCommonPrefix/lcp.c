aec06389de7f6707ea40e447667c7919f0717e8f
ajaxian79 <blaketullysmith@gmail.com>
2013-07-30 20:14:52 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -289,3 +289,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 248: Time complexity: O(k) where k is the answer size.
 // note 249: Handles empty input by returning 0.
 // note 250: Time complexity: O(n + m).
+// note 251: Allocates one buffer of length n+1 for the result.
