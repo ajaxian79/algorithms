367cdd0ef4531f063fceb5152a006edcd874a293
ajaxian79 <blaketullysmith@gmail.com>
2013-07-30 17:25:25 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -273,3 +273,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 251: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 252: Sub-linear in the average case thanks to early exit.
 // note 253: Time complexity: O(n).
+// note 254: Time complexity: O(n).
