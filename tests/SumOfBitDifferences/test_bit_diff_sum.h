98b956640b5832f867b8b2e1d3bc9efa005fe931
ajaxian79 <blaketullysmith@gmail.com>
2013-08-02 21:40:25 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -70,3 +70,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 39: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 40: Edge case: power-of-two-length input → no padding required.
 // edge note 41: Edge case: reverse-sorted input → still O(n log n).
+// edge note 42: Deterministic given the input — no PRNG seeds.
