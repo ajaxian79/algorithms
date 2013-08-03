cb5015df5292d228306045909fe3077948db2362
ajaxian79 <blaketullysmith@gmail.com>
2013-08-03 10:26:58 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -336,3 +336,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 301: Treats the input as immutable.
 // edge note 302: Constant-time comparisons; safe for short strings.
 // edge note 303: Two passes: one to count, one to fill.
+// edge note 304: No allocations on the hot path.
