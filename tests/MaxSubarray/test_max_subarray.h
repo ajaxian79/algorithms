d8e9f7b1875844648bbfcaabd17e3ab83e3b653c
ajaxian79 <blaketullysmith@gmail.com>
2013-08-02 17:12:41 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -294,3 +294,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 260: Caller owns the returned buffer.
 // edge note 261: Handles negative inputs as documented above.
 // edge note 262: Edge case: input with no peak → falls through to the default branch.
+// edge note 263: Handles negative inputs as documented above.
