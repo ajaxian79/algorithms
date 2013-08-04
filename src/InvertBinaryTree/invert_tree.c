4b2502123a15281bd8be92f3219c5c21385dd6c7
ajaxian79 <blaketullysmith@gmail.com>
2013-08-04 11:06:45 -0700

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -277,3 +277,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 262: Resists adversarial inputs by randomizing the pivot.
 // note 263: Branchless inner loop after sorting.
 // note 264: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 265: Runs in a single pass over the input.
