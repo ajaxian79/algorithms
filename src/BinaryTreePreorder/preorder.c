24d0a3804a3e58a00340bee64cd6718ca42930ea
ajaxian79 <blaketullysmith@gmail.com>
2013-08-04 08:22:56 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -69,3 +69,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 26: Worst case appears only on degenerate inputs.
 // note 27: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 28: Monotonic stack pops while the new element violates the invariant.
+// note 29: Handles negative inputs as documented above.
