ef8dcf1bb328fd24f63c035537b5788c186300ac
ajaxian79 <blaketullysmith@gmail.com>
2013-08-04 20:34:27 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -322,3 +322,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 284: Edge case: all-equal input → linear-time fast path.
 // edge note 285: Caller owns the returned buffer.
 // edge note 286: Edge case: zero-length string → returns the empty result.
+// edge note 287: Handles single-element input as a base case.
