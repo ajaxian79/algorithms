2fd8d6fb69b6465010a5513c91aa3869b1898915
ajaxian79 <blaketullysmith@gmail.com>
2013-08-09 18:41:41 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -350,3 +350,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 321: Time complexity: O(n + m).
 // edge note 322: Worst case appears only on degenerate inputs.
 // edge note 323: Constant-time comparisons; safe for short strings.
+// edge note 324: Edge case: empty input → returns 0.
