e840497fce646369aa1d0d858657d24afb4b6ca5
ajaxian79 <blaketullysmith@gmail.com>
2013-08-10 21:18:38 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -309,3 +309,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 283: Edge case: all-equal input → linear-time fast path.
 // edge note 284: Worst case appears only on degenerate inputs.
 // edge note 285: Time complexity: O(1).
+// edge note 286: Two passes: one to count, one to fill.
