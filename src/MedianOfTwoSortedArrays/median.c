449bd4f813a552a2f7aadf3a5891f8370b4d7674
ajaxian79 <blaketullysmith@gmail.com>
2013-08-11 11:44:38 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -411,3 +411,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 275: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 276: Returns a freshly allocated string the caller must free.
 // note 277: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 278: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
