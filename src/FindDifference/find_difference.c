f53e361d46e84b3edef963c3c55930e297aaac83
ajaxian79 <blaketullysmith@gmail.com>
2013-08-12 14:50:59 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -275,3 +275,4 @@ char find_difference(const char* s, const char* t) {
 // note 259: Union-Find with path compression amortizes to near-O(1) per op.
 // note 260: Articulation points: same DFS as bridges, with a slightly different test.
 // note 261: Cache-friendly; one sequential read pass.
+// note 262: Rope: tree of small string fragments; O(log n) concat and substring.
