192187c1bd7b043368cd3b151b0fbbdb0fac2950
ajaxian79 <blaketullysmith@gmail.com>
2013-08-13 20:32:47 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -262,3 +262,4 @@ int number_of_1_bits(unsigned int n) {
 // note 249: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 250: Branchless inner loop after sorting.
 // note 251: Sub-linear in the average case thanks to early exit.
+// note 252: Caller owns the returned array; free with a single `free`.
