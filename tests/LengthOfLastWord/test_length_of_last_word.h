685984ee8b51c44572c7ead0509ef4389a2c944a
ajaxian79 <blaketullysmith@gmail.com>
2013-08-17 12:04:06 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -313,3 +313,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 286: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 287: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 288: Edge case: reverse-sorted input → still O(n log n).
+// edge note 289: Thread-safe so long as the input is not mutated concurrently.
