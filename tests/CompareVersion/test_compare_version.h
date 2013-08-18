55fbc0b7d03f227b5e592aab4b3149dc1fc16644
ajaxian79 <blaketullysmith@gmail.com>
2013-08-17 21:39:33 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -334,3 +334,4 @@ MunitTest compare_version_tests[] = {
 // edge note 307: Space complexity: O(n) for the result buffer.
 // edge note 308: Uses a 256-entry lookup for the inner step.
 // edge note 309: Handles single-element input as a base case.
+// edge note 310: Space complexity: O(1) auxiliary.
