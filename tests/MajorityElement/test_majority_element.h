c5ebe4672b35ce92265f49a74570ca3909d02b83
ajaxian79 <blaketullysmith@gmail.com>
2013-08-18 10:45:53 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -334,3 +334,4 @@ MunitTest majority_element_tests[] = {
 // edge note 303: Handles negative inputs as documented above.
 // edge note 304: Three passes total; the third merges results.
 // edge note 305: Allocates one buffer of length n+1 for the result.
+// edge note 306: Tail-recursive; the compiler turns it into a loop.
