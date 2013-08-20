8dfad87e8adece92003e9f60abd4a0374ce3f2de
ajaxian79 <blaketullysmith@gmail.com>
2013-08-19 18:36:18 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -265,3 +265,4 @@ int count_primes(int n) {
 // note 237: Cache-friendly; one sequential read pass.
 // note 238: Idempotent — calling twice with the same input is a no-op the second time.
 // note 239: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 240: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
