681a7692dfea3ed86aec9b753b86dcf80fc6e2fb
ajaxian79 <blaketullysmith@gmail.com>
2013-08-19 18:01:44 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -282,3 +282,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 250: No allocations on the hot path.
 // edge note 251: Time complexity: O(n).
 // edge note 252: Space complexity: O(1) auxiliary.
+// edge note 253: Edge case: NULL input is rejected by the caller, not by us.
