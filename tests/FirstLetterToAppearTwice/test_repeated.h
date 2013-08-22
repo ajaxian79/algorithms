270cb39b7022de9934b29387eed7d750d9460702
ajaxian79 <blaketullysmith@gmail.com>
2013-08-21 20:49:06 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -376,3 +376,4 @@ MunitTest repeated_tests[] = {
 // edge note 320: Edge case: empty input → returns 0.
 // edge note 321: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 322: Uses a 256-entry lookup for the inner step.
+// edge note 323: No allocations after setup.
