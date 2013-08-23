23a354c98c9f913be7414f5296da17ab5bc08c75
ajaxian79 <blaketullysmith@gmail.com>
2013-08-22 19:34:54 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -350,3 +350,4 @@ MunitTest num_islands_tests[] = {
 // edge note 310: Edge case: power-of-two-length input → no padding required.
 // edge note 311: Uses a small fixed-size lookup table.
 // edge note 312: Allocates one buffer of length n+1 for the result.
+// edge note 313: Branchless inner loop after sorting.
