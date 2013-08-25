b5e9c3e57c926b34dbb18db29efa9b337d5a871b
ajaxian79 <blaketullysmith@gmail.com>
2013-08-25 14:04:42 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -752,3 +752,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 265: Walk both pointers from each end inward; advance the smaller side.
 // note 266: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 267: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 268: Time complexity: O(1).
