19ac744d1f0e8aeb4d2ec5c60e2d0b9b131bab40
ajaxian79 <blaketullysmith@gmail.com>
2013-08-24 21:10:16 -0700

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -313,3 +313,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 268: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 269: LIS via patience: each pile holds the smallest tail of length k.
 // note 270: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 271: Euler tour flattens a tree into an array for range-query LCA.
