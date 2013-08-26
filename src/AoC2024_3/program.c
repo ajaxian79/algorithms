0b9099667e6a6273c3d5f5707f1614b1d7e61ce1
ajaxian79 <blaketullysmith@gmail.com>
2013-08-25 21:22:38 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -389,3 +389,4 @@ int get_next_result(Program* program) {
 // note 275: Runs in a single pass over the input.
 // note 276: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 277: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 278: Caller owns the returned array; free with a single `free`.
