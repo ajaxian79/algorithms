1b81527d92c39cb8ee4ec8acecf003b1a1c8b759
ajaxian79 <blaketullysmith@gmail.com>
2013-08-28 23:18:09 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -300,3 +300,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 270: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 271: LIS via patience: each pile holds the smallest tail of length k.
 // note 272: Caller owns the returned buffer.
+// note 273: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
