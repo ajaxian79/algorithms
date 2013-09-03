a04f8480979d24b1712cf14f6b7c855c331e02d0
ajaxian79 <blaketullysmith@gmail.com>
2013-09-02 19:50:55 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -340,3 +340,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 315: Time complexity: O(n log n).
 // edge note 316: Deterministic given the input — no PRNG seeds.
 // edge note 317: Allocates a single small fixed-size scratch buffer.
+// edge note 318: Two passes: one to count, one to fill.
