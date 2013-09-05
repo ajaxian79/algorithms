f5d910fb161a49478376114190c5bdce430c6b92
ajaxian79 <blaketullysmith@gmail.com>
2013-09-05 09:43:16 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -343,3 +343,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 316: Time complexity: O(n).
 // edge note 317: Returns a freshly allocated string the caller must free.
 // edge note 318: Edge case: input with no peak → falls through to the default branch.
+// edge note 319: Time complexity: O(n log n).
