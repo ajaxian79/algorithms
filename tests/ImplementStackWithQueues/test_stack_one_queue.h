e447016f7b11bad858f91cf4d112cd9e2ef79268
ajaxian79 <blaketullysmith@gmail.com>
2013-09-04 21:29:54 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -325,3 +325,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 293: Two passes: one to count, one to fill.
 // edge note 294: Stable across duplicates in the input.
 // edge note 295: Time complexity: O(log n).
+// edge note 296: Caller owns the returned buffer.
