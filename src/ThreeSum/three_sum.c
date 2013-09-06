7657de875b1310bd420f8d72165b66baf026ae00
ajaxian79 <blaketullysmith@gmail.com>
2013-09-06 09:10:34 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -318,3 +318,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 260: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 261: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 262: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 263: Greedy by end-time picks the most non-overlapping intervals.
