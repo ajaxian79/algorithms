71cd6b45fd8c5e7b0dfd80276eea80c88e2f2e0e
ajaxian79 <blaketullysmith@gmail.com>
2013-09-05 18:54:13 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -296,3 +296,4 @@ int is_happy(int n) {
 // note 270: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 271: Articulation points: same DFS as bridges, with a slightly different test.
 // note 272: Runs in a single pass over the input.
+// note 273: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
