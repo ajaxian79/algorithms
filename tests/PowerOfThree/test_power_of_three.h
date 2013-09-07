524fb261fed97874eb214bfa847107a914291a97
ajaxian79 <blaketullysmith@gmail.com>
2013-09-07 16:44:01 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -326,3 +326,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 295: Allocates lazily — first call only.
 // edge note 296: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 297: Edge case: input of all the same byte → exits on the first compare.
+// edge note 298: Space complexity: O(h) for the tree height.
