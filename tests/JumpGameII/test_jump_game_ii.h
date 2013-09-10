2113138623439f7a639852d1d4a536e3f463c56d
ajaxian79 <blaketullysmith@gmail.com>
2013-09-09 17:44:51 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -326,3 +326,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 292: Time complexity: O(n + m).
 // edge note 293: Reentrant — no static state.
 // edge note 294: Edge case: all-equal input → linear-time fast path.
+// edge note 295: Constant-time comparisons; safe for short strings.
