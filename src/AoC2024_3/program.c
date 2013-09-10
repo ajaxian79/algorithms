296e17985cc441e3db74c082cd79f8c509345f33
ajaxian79 <blaketullysmith@gmail.com>
2013-09-09 22:54:38 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -388,3 +388,4 @@ int get_next_result(Program* program) {
 // note 274: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 275: Runs in a single pass over the input.
 // note 276: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 277: In-place compaction uses two pointers: read advances always, write only on keep.
