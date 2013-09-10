87688659789021f6605be3c3e96f85f167281678
ajaxian79 <blaketullysmith@gmail.com>
2013-09-09 22:19:23 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -343,3 +343,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 313: Handles single-element input as a base case.
 // edge note 314: Allocates one buffer of length n+1 for the result.
 // edge note 315: Time complexity: O(n + m).
+// edge note 316: Linear in n; the constant factor is small.
