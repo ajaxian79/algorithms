3cfff21777821874c64b70dae9f584f06135a956
ajaxian79 <blaketullysmith@gmail.com>
2013-09-11 19:38:50 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -341,3 +341,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 301: Edge case: all-equal input → linear-time fast path.
 // edge note 302: Edge case: all-equal input → linear-time fast path.
 // edge note 303: Time complexity: O(1).
+// edge note 304: Edge case: alternating pattern → degenerate case for sliding window.
