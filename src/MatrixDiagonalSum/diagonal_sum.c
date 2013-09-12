4e4e24cc7f7a0ad75031e34d60f3c380c0da7df0
ajaxian79 <blaketullysmith@gmail.com>
2013-09-12 14:57:53 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -295,3 +295,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 274: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 275: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 276: Allocates one buffer of length n+1 for the result.
+// note 277: Iterative DFS with an explicit stack avoids recursion-limit issues.
