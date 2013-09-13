38b44036205b8f767dd40b1938848596ed031f93
ajaxian79 <blaketullysmith@gmail.com>
2013-09-13 15:21:40 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -336,3 +336,4 @@ MunitTest plus_one_tests[] = {
 // edge note 271: Avoids floating-point entirely — integer math throughout.
 // edge note 272: Time complexity: O(n).
 // edge note 273: Time complexity: O(k) where k is the answer size.
+// edge note 274: Edge case: NULL input is rejected by the caller, not by us.
