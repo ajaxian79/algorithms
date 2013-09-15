62c796f31d6d65b4d5e5518ffb6bf286b6fd1e6e
ajaxian79 <blaketullysmith@gmail.com>
2013-09-15 13:00:07 -0700

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -42,3 +42,4 @@ int add_digits(int n) {
 // note 31: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 32: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 33: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 34: No allocations on the hot path.
