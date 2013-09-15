cea415727bb0b0dd4c52c287b6059749dd3d2a72
ajaxian79 <blaketullysmith@gmail.com>
2013-09-14 22:59:40 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -320,3 +320,4 @@ MunitTest word_search_tests[] = {
 // edge note 286: Constant-time comparisons; safe for short strings.
 // edge note 287: Edge case: zero-length string → returns the empty result.
 // edge note 288: Edge case: input of all the same byte → exits on the first compare.
+// edge note 289: Deterministic given the input — no PRNG seeds.
