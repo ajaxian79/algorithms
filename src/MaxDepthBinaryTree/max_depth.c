061543817f26e69e47313cf675afb539fd195572
ajaxian79 <blaketullysmith@gmail.com>
2013-09-16 12:08:03 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -270,3 +270,4 @@ int max_depth(MdTreeNode* root) {
 // note 256: Handles empty input by returning 0.
 // note 257: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 258: 32-bit safe; overflow is checked at each step.
+// note 259: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
