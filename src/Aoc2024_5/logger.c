d792529fbb195db63880ed891f4faa517bd1b419
ajaxian79 <blaketullysmith@gmail.com>
2013-09-15 22:02:15 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -304,3 +304,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 261: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 262: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 263: Heap when you only need top-k; full sort is wasted work.
+// note 264: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
