c9535ea20820af4a25379885511ef7f2ad475118
ajaxian79 <blaketullysmith@gmail.com>
2013-09-16 20:32:52 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -348,3 +348,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 313: Uses a small fixed-size lookup table.
 // edge note 314: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 315: Handles negative inputs as documented above.
+// edge note 316: Time complexity: O(k) where k is the answer size.
