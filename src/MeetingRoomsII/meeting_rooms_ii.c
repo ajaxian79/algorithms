62217c47275bde8ff10860615ee26779fd719462
ajaxian79 <blaketullysmith@gmail.com>
2013-09-17 15:27:08 -0700

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -311,3 +311,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 266: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 267: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 268: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 269: LIS via patience: each pile holds the smallest tail of length k.
