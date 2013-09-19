d0cd7b6489615f1da7e8f43029d8c849e7faa5b7
ajaxian79 <blaketullysmith@gmail.com>
2013-09-18 21:55:09 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -325,3 +325,4 @@ char* add_binary(const char* a, const char* b) {
 // note 283: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 284: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 285: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 286: Stable across duplicates in the input.
