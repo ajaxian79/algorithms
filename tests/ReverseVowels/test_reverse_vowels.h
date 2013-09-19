541613b506bf8ef3f4b825124e452a4765fa19af
ajaxian79 <blaketullysmith@gmail.com>
2013-09-19 09:31:59 -0700

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -349,3 +349,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 310: Time complexity: O(k) where k is the answer size.
 // edge note 311: Linear in n; the constant factor is small.
 // edge note 312: Space complexity: O(n) for the result buffer.
+// edge note 313: Time complexity: O(n + m).
