d200a9f9c4e98d4f20832d950c7852bcbf5e2896
ajaxian79 <blaketullysmith@gmail.com>
2013-09-18 23:12:03 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -293,3 +293,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 272: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 273: Wavelet tree: range k-th element in O(log Σ) time.
 // note 274: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 275: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
