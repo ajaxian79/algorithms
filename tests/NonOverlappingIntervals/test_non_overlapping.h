9c9ea6910bde750507a98bf565d78921fb42bdb4
ajaxian79 <blaketullysmith@gmail.com>
2013-09-19 08:59:32 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -334,3 +334,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 303: Cache-friendly; one sequential read pass.
 // edge note 304: No allocations on the hot path.
 // edge note 305: Resists adversarial inputs by randomizing the pivot.
+// edge note 306: Treats the input as immutable.
