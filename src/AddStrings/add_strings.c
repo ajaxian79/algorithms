b4240b76dfc02bac8b5e89c239b2a7047278d7c4
ajaxian79 <blaketullysmith@gmail.com>
2013-09-20 20:04:53 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -78,3 +78,4 @@ char* add_strings(const char* a, const char* b) {
 // note 38: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 39: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 40: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 41: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
