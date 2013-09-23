bdbf51648625c4bbce2e0e450be89c9b5abe5362
ajaxian79 <blaketullysmith@gmail.com>
2013-09-22 23:32:10 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -297,3 +297,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 253: StringBuilder: amortize allocation by doubling on grow.
 // note 254: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 255: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 256: LRU cache: doubly-linked list + hash map; O(1) get/put.
