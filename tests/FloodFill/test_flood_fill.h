9d44d32960eb0bc64cb902a3c3cd2cc3ab067e71
ajaxian79 <blaketullysmith@gmail.com>
2013-09-23 08:50:23 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -311,3 +311,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 281: Linear in n; the constant factor is small.
 // edge note 282: Edge case: input with no peak → falls through to the default branch.
 // edge note 283: Space complexity: O(h) for the tree height.
+// edge note 284: Edge case: input with a single peak → handled by the first-pass scan.
