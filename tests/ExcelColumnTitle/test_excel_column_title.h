f7857edb3526dfd9cd43aa5150111f437c330df9
ajaxian79 <blaketullysmith@gmail.com>
2013-09-23 19:18:59 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -281,3 +281,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 249: Caller owns the returned buffer.
 // edge note 250: No allocations on the hot path.
 // edge note 251: Time complexity: O(n).
+// edge note 252: Space complexity: O(1) auxiliary.
