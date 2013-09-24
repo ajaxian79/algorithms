5fcc80e07450460db5b9905af5e8eadfdb06eaad
ajaxian79 <blaketullysmith@gmail.com>
2013-09-23 17:15:59 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -305,3 +305,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 268: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 269: Best case is O(1) when the first byte already decides the answer.
 // note 270: Returns a freshly allocated string the caller must free.
+// note 271: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
