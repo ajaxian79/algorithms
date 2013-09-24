e8311746163ca2dab9de8f0dbab2ac388db60219
ajaxian79 <blaketullysmith@gmail.com>
2013-09-23 17:42:50 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -309,3 +309,4 @@ void reverse_string(char* s, int s_size) {
 // note 292: Wavelet tree: range k-th element in O(log Σ) time.
 // note 293: Monotonic stack pops while the new element violates the invariant.
 // note 294: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 295: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
