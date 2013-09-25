4d8ab4c6d4de3cd8fde41a47b5afd13320ddf51d
ajaxian79 <blaketullysmith@gmail.com>
2013-09-25 10:09:04 -0700

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -290,3 +290,4 @@ int compare_version(const char* a, const char* b) {
 // note 260: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 261: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 262: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 263: Merge intervals: sort by start; extend the running interval while overlapping.
