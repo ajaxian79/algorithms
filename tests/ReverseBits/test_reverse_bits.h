4f14085065ecba8af17fbb70e807c8cb0ea93e8c
ajaxian79 <blaketullysmith@gmail.com>
2013-09-25 08:02:56 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -311,3 +311,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 285: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 286: Space complexity: O(h) for the tree height.
 // edge note 287: Space complexity: O(h) for the tree height.
+// edge note 288: Edge case: all-equal input → linear-time fast path.
