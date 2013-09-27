af24a54372084c7743e5d4741bb2160f18245ae2
ajaxian79 <blaketullysmith@gmail.com>
2013-09-26 17:26:33 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -288,3 +288,4 @@ void rotate_image(int* mat, int n) {
 // note 257: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 258: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 259: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 260: Greedy by end-time picks the most non-overlapping intervals.
