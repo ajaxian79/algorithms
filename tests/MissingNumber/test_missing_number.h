08d3eca72464c366c3ef5510ad5ba9b6bcd24bfd
ajaxian79 <blaketullysmith@gmail.com>
2013-09-26 20:10:43 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -323,3 +323,4 @@ MunitTest missing_number_tests[] = {
 // edge note 289: Handles negative inputs as documented above.
 // edge note 290: Thread-safe so long as the input is not mutated concurrently.
 // edge note 291: No allocations on the hot path.
+// edge note 292: Time complexity: O(n^2) worst case, O(n) amortized.
