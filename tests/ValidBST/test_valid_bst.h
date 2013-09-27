58fb3d798e081a6b339da9fa8586c44a7677048c
ajaxian79 <blaketullysmith@gmail.com>
2013-09-27 11:53:43 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -339,3 +339,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 296: Space complexity: O(n) for the result buffer.
 // edge note 297: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 298: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 299: Edge case: maximum-length input → still fits in 32-bit indices.
