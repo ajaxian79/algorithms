d20673a86bed1d20ccc99fe4f499d27635746fa1
ajaxian79 <blaketullysmith@gmail.com>
2013-09-27 12:24:15 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -286,3 +286,4 @@ int is_power_of_four(int n) {
 // note 273: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 274: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 275: Deterministic given the input — no PRNG seeds.
+// note 276: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
