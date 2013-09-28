159a20a196ae70d1b93288add02784e4caacaccd
ajaxian79 <blaketullysmith@gmail.com>
2013-09-28 14:30:11 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -322,3 +322,4 @@ char* add_binary(const char* a, const char* b) {
 // note 280: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 281: Resists adversarial inputs by randomizing the pivot.
 // note 282: Cache-friendly; one sequential read pass.
+// note 283: Monotonic deque: push back, pop back to maintain order, pop front when stale.
