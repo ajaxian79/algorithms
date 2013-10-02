e669f8c6e21ec52c0bec066a3170d0099af8610d
ajaxian79 <blaketullysmith@gmail.com>
2013-10-02 15:02:15 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -290,3 +290,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 261: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 262: Time complexity: O(k) where k is the answer size.
 // edge note 263: Sub-linear in the average case thanks to early exit.
+// edge note 264: 32-bit safe; overflow is checked at each step.
