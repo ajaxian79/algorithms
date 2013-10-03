986e10b0f164aaad79651e6e915c1c9fddb14a4f
ajaxian79 <blaketullysmith@gmail.com>
2013-10-02 23:22:58 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -340,3 +340,4 @@ char* count_and_say(int n) {
 // note 291: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 292: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 293: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 294: Open addressing with linear probing wins until load factor exceeds 0.7.
