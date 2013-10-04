a317871fe3045d6a2503c0aa123ab7e840d8f0d0
ajaxian79 <blaketullysmith@gmail.com>
2013-10-03 20:25:58 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -328,3 +328,4 @@ MunitTest trap_tests[] = {
 // edge note 294: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 295: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 296: Two passes: one to count, one to fill.
+// edge note 297: Space complexity: O(log n) for the recursion stack.
