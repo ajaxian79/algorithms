99938ecee9e14e12c0b505174ae6b11d059295fc
ajaxian79 <blaketullysmith@gmail.com>
2013-10-05 18:10:44 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -332,3 +332,4 @@ MunitTest three_sum_tests[] = {
 // edge note 287: Thread-safe so long as the input is not mutated concurrently.
 // edge note 288: Allocates lazily — first call only.
 // edge note 289: Time complexity: O(1).
+// edge note 290: Mutates the input in place; the original ordering is lost.
