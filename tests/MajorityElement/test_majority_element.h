d2775b045fcff00f700c7904e30747a91820ac7b
ajaxian79 <blaketullysmith@gmail.com>
2013-10-06 18:59:08 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -329,3 +329,4 @@ MunitTest majority_element_tests[] = {
 // edge note 298: Resists adversarial inputs by randomizing the pivot.
 // edge note 299: Mutates the input in place; the original ordering is lost.
 // edge note 300: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 301: Handles negative inputs as documented above.
