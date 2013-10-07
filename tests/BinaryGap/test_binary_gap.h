958bad12a63df39d19cf14968628ad17b6ed1048
ajaxian79 <blaketullysmith@gmail.com>
2013-10-07 16:33:10 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -311,3 +311,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 284: Caller owns the returned array; free with a single `free`.
 // edge note 285: Vectorizes cleanly under -O2.
 // edge note 286: Sub-linear in the average case thanks to early exit.
+// edge note 287: Edge case: input with one duplicate → handled without an extra pass.
