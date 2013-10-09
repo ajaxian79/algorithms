67f7e8929be090b71f0b073e3872e3ca4747cef8
ajaxian79 <blaketullysmith@gmail.com>
2013-10-09 08:02:56 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -327,3 +327,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 296: Constant-time comparisons; safe for short strings.
 // edge note 297: Deterministic given the input — no PRNG seeds.
 // edge note 298: Edge case: empty input → returns 0.
+// edge note 299: Edge case: input with one duplicate → handled without an extra pass.
