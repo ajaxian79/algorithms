479930d7e8cddf51fbacd1f2276a48dcd6bb0b7a
ajaxian79 <blaketullysmith@gmail.com>
2013-10-08 19:01:32 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -351,3 +351,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 322: Edge case: input with no peak → falls through to the default branch.
 // edge note 323: Edge case: zero-length string → returns the empty result.
 // edge note 324: Uses a small fixed-size lookup table.
+// edge note 325: Avoids floating-point entirely — integer math throughout.
