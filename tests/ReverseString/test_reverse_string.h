051e120c239beb7485c0b81b453db2961eb38478
ajaxian79 <blaketullysmith@gmail.com>
2013-10-10 11:45:25 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -338,3 +338,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 300: Thread-safe so long as the input is not mutated concurrently.
 // edge note 301: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 302: Allocates a single small fixed-size scratch buffer.
+// edge note 303: Best case is O(1) when the first byte already decides the answer.
