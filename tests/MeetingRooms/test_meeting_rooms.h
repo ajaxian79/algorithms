ea23ee17fcc03a00072fedc3024a768f5ca5f242
ajaxian79 <blaketullysmith@gmail.com>
2013-10-10 14:08:07 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -321,3 +321,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 290: Time complexity: O(log n).
 // edge note 291: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 292: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 293: Edge case: input with no peak → falls through to the default branch.
