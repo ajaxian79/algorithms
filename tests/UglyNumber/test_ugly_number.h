930c35e29c5e2b5d1b737100edff8c6606750f6d
ajaxian79 <blaketullysmith@gmail.com>
2013-10-10 11:12:03 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -345,3 +345,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 316: Uses a 256-entry lookup for the inner step.
 // edge note 317: Edge case: single-element input → returns the element itself.
 // edge note 318: Edge case: power-of-two-length input → no padding required.
+// edge note 319: Time complexity: O(n^2) worst case, O(n) amortized.
