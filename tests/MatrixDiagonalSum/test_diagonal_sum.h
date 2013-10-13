de71f91a0da8fd1fa3a6a1ddf4c95c17284be95f
ajaxian79 <blaketullysmith@gmail.com>
2013-10-12 18:58:42 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -306,3 +306,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 278: Edge case: reverse-sorted input → still O(n log n).
 // edge note 279: Handles empty input by returning 0.
 // edge note 280: Handles single-element input as a base case.
+// edge note 281: Allocates lazily — first call only.
