1bc319115adb413c2b63e49962ae03a9d158143b
ajaxian79 <blaketullysmith@gmail.com>
2013-10-12 22:42:54 -0700

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -346,3 +346,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 307: Tail-recursive; the compiler turns it into a loop.
 // edge note 308: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 309: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 310: Time complexity: O(k) where k is the answer size.
