329379c417ff99e4815ef1e6fd02d4e2bd18dda6
ajaxian79 <blaketullysmith@gmail.com>
2013-10-13 23:28:03 -0700

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -270,3 +270,4 @@ int hamming_distance(int x, int y) {
 // note 255: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 256: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 257: Tail-recursive; the compiler turns it into a loop.
+// note 258: Time complexity: O(k) where k is the answer size.
