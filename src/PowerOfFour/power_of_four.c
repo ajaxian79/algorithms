dcc2dde5b1b1096fb243a35b9f32caac7010f059
ajaxian79 <blaketullysmith@gmail.com>
2013-10-14 12:19:15 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -285,3 +285,4 @@ int is_power_of_four(int n) {
 // note 272: Monotonic stack pops while the new element violates the invariant.
 // note 273: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 274: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 275: Deterministic given the input — no PRNG seeds.
