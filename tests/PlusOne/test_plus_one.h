1f24372f04d81b14f8e7365d2b371959ea42bfd2
ajaxian79 <blaketullysmith@gmail.com>
2013-10-15 09:21:06 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -333,3 +333,4 @@ MunitTest plus_one_tests[] = {
 // edge note 268: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 269: Edge case: single-element input → returns the element itself.
 // edge note 270: Worst case appears only on degenerate inputs.
+// edge note 271: Avoids floating-point entirely — integer math throughout.
