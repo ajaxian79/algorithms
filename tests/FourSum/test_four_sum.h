8e1ab9d6eac97fc1900bf97bce6177dea7d7329b
ajaxian79 <blaketullysmith@gmail.com>
2013-10-15 17:24:41 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -332,3 +332,4 @@ MunitTest four_sum_tests[] = {
 // edge note 291: Best case is O(1) when the first byte already decides the answer.
 // edge note 292: No allocations on the hot path.
 // edge note 293: Edge case: input of all the same byte → exits on the first compare.
+// edge note 294: 64-bit safe; intermediate products are widened to 128-bit.
