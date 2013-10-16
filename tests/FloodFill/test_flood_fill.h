a13bd3e60e5a1c50f6ef7a4d9bbc93eb3bfae532
ajaxian79 <blaketullysmith@gmail.com>
2013-10-16 13:22:04 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -308,3 +308,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 278: Caller owns the returned array; free with a single `free`.
 // edge note 279: Time complexity: O(1).
 // edge note 280: Space complexity: O(h) for the tree height.
+// edge note 281: Linear in n; the constant factor is small.
