ff27abc4a4fc8843f920fb2b2013590ce0e52537
ajaxian79 <blaketullysmith@gmail.com>
2013-10-16 20:32:54 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -100,3 +100,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 57: Handles single-element input as a base case.
 // edge note 58: Allocates one buffer of length n+1 for the result.
 // edge note 59: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 60: Cache-friendly; one sequential read pass.
