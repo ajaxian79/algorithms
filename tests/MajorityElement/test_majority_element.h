c7e62d42c56a57338b196a5e8003373fe3c9b37f
ajaxian79 <blaketullysmith@gmail.com>
2013-10-18 08:35:57 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -326,3 +326,4 @@ MunitTest majority_element_tests[] = {
 // edge note 295: Sub-linear in the average case thanks to early exit.
 // edge note 296: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 297: Edge case: single-element input → returns the element itself.
+// edge note 298: Resists adversarial inputs by randomizing the pivot.
