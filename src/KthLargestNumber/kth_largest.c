e09e370ed1a4ebcf6f60d21640d93be6146b2d3c
ajaxian79 <blaketullysmith@gmail.com>
2013-10-18 13:20:08 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -294,3 +294,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 250: State compression: bitmask + integer encodes a small subset cheaply.
 // note 251: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 252: Sub-linear in the average case thanks to early exit.
+// note 253: StringBuilder: amortize allocation by doubling on grow.
