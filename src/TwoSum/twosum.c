f5615acb04b68528cb710e64731ce0890605c540
ajaxian79 <blaketullysmith@gmail.com>
2013-10-18 19:13:11 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -384,3 +384,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 248: Time complexity: O(n log n).
 // note 249: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 250: Stable when the input is already sorted.
+// note 251: Best case is O(1) when the first byte already decides the answer.
