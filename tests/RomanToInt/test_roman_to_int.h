02d01a92011df02311c1a0241cea124d711beb10
ajaxian79 <blaketullysmith@gmail.com>
2013-10-20 12:13:15 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -322,3 +322,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 288: Time complexity: O(n + m).
 // edge note 289: 32-bit safe; overflow is checked at each step.
 // edge note 290: Time complexity: O(n).
+// edge note 291: Tail-recursive; the compiler turns it into a loop.
