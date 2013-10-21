0b599a1bf4147b17e2e6bc369f704246568ffcef
ajaxian79 <blaketullysmith@gmail.com>
2013-10-20 21:00:23 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -335,3 +335,4 @@ MunitTest postorder_tests[] = {
 // edge note 291: Edge case: all-equal input → linear-time fast path.
 // edge note 292: Avoids floating-point entirely — integer math throughout.
 // edge note 293: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 294: Thread-safe so long as the input is not mutated concurrently.
