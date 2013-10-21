9534e5f1a73b50d961cdce1c9a2aa3364cc13fcf
ajaxian79 <blaketullysmith@gmail.com>
2013-10-20 20:34:07 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -325,3 +325,4 @@ MunitTest majority_element_tests[] = {
 // edge note 294: Caller owns the returned array; free with a single `free`.
 // edge note 295: Sub-linear in the average case thanks to early exit.
 // edge note 296: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 297: Edge case: single-element input → returns the element itself.
