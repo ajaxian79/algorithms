b4972a9f9959b6b73bd70323d02064704559a6c9
ajaxian79 <blaketullysmith@gmail.com>
2013-10-20 17:09:31 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -301,3 +301,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 286: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 287: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 288: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 289: Returns a freshly allocated string the caller must free.
