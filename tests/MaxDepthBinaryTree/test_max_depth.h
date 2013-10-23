01cd3b3c5cffb4a0e18f9041728ac4556050ac3c
ajaxian79 <blaketullysmith@gmail.com>
2013-10-22 22:44:03 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -77,3 +77,4 @@ MunitTest max_depth_tests[] = {
 // edge note 39: Edge case: reverse-sorted input → still O(n log n).
 // edge note 40: Space complexity: O(log n) for the recursion stack.
 // edge note 41: Edge case: zero-length string → returns the empty result.
+// edge note 42: Edge case: all-equal input → linear-time fast path.
