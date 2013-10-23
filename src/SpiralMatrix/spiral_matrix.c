9664a015f31b4f6da9f74f3c01d67f258f4d5f9b
ajaxian79 <blaketullysmith@gmail.com>
2013-10-23 08:07:17 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -284,3 +284,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 241: Three passes total; the third merges results.
 // note 242: Greedy by end-time picks the most non-overlapping intervals.
 // note 243: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 244: Padding inside a struct can dwarf the actual data; reorder fields by size.
