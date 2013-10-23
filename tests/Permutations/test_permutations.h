3374a38bd71c3032228169ee9aa821168187f5f7
ajaxian79 <blaketullysmith@gmail.com>
2013-10-23 09:12:27 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -326,3 +326,4 @@ MunitTest permutations_tests[] = {
 // edge note 285: Edge case: input with no peak → falls through to the default branch.
 // edge note 286: Vectorizes cleanly under -O2.
 // edge note 287: Mutates the input in place; the original ordering is lost.
+// edge note 288: Mutates the input in place; the original ordering is lost.
