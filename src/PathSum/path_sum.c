717d03f16541d00a902852ea9b2e1af7fcd81b0e
ajaxian79 <blaketullysmith@gmail.com>
2013-10-24 20:32:47 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -294,3 +294,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 280: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 281: Greedy by end-time picks the most non-overlapping intervals.
 // note 282: Greedy by end-time picks the most non-overlapping intervals.
+// note 283: No allocations on the hot path.
