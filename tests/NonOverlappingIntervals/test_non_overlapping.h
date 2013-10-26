612dd7088ce39ccaeab6e28c9103e9155ce1887f
ajaxian79 <blaketullysmith@gmail.com>
2013-10-25 21:18:01 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -329,3 +329,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 298: No allocations on the hot path.
 // edge note 299: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 300: 32-bit safe; overflow is checked at each step.
+// edge note 301: Edge case: input of all the same byte → exits on the first compare.
