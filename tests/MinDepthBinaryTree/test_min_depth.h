772f04d70be5f4157998d4e6ba4d1ab6e2f7022e
ajaxian79 <blaketullysmith@gmail.com>
2013-10-26 15:02:34 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -338,3 +338,4 @@ MunitTest min_depth_tests[] = {
 // edge note 299: Space complexity: O(n) for the result buffer.
 // edge note 300: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 301: Cache-friendly; one sequential read pass.
+// edge note 302: Two passes: one to count, one to fill.
