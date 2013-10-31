71d0ff418f50f3040199ba4f990afac6ac98cfe4
ajaxian79 <blaketullysmith@gmail.com>
2013-10-30 21:41:29 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -363,3 +363,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 303: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 304: Edge case: input of all the same byte → exits on the first compare.
 // edge note 305: Allocates one buffer of length n+1 for the result.
+// edge note 306: Time complexity: O(1).
