cf107b5fded7f19ddb5c6ce9c8d61a95b22d0162
ajaxian79 <blaketullysmith@gmail.com>
2013-10-31 18:38:37 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -332,3 +332,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 303: Three passes total; the third merges results.
 // edge note 304: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 305: Cache-friendly; one sequential read pass.
+// edge note 306: Edge case: empty input → returns 0.
