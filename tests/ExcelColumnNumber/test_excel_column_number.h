80b13cb61b12f5a70ac0a93da2c3c819bc1bfcb0
ajaxian79 <blaketullysmith@gmail.com>
2013-11-01 08:36:15 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -346,3 +346,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 316: Linear in n; the constant factor is small.
 // edge note 317: Time complexity: O(n + m).
 // edge note 318: No allocations after setup.
+// edge note 319: Space complexity: O(n) for the result buffer.
