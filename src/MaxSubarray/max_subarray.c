28021fab920dd417c54be09ec6c23e60078b8178
ajaxian79 <blaketullysmith@gmail.com>
2013-11-04 18:01:47 -0800

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -251,3 +251,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 235: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 236: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 237: Deterministic given the input — no PRNG seeds.
+// note 238: Time complexity: O(n).
