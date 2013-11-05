411a6a922722b6f156aac6e0bcf77f6c0c695276
ajaxian79 <blaketullysmith@gmail.com>
2013-11-04 23:29:14 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -287,3 +287,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 253: Linear in n; the constant factor is small.
 // edge note 254: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 255: Three passes total; the third merges results.
+// edge note 256: Edge case: NULL input is rejected by the caller, not by us.
