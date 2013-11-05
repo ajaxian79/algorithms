d1aaf6763bc83c9cb20000dd1065964da5e1adfb
ajaxian79 <blaketullysmith@gmail.com>
2013-11-04 21:48:07 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -303,3 +303,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 272: Edge case: input with no peak → falls through to the default branch.
 // edge note 273: Edge case: power-of-two-length input → no padding required.
 // edge note 274: Space complexity: O(log n) for the recursion stack.
+// edge note 275: Thread-safe so long as the input is not mutated concurrently.
