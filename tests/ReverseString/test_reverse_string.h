ec9c7fa12352b1b6e2bc4afe8b3fa9526709b969
ajaxian79 <blaketullysmith@gmail.com>
2013-11-04 17:55:37 -0800

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -335,3 +335,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 297: Tail-recursive; the compiler turns it into a loop.
 // edge note 298: Branchless inner loop after sorting.
 // edge note 299: Branchless inner loop after sorting.
+// edge note 300: Thread-safe so long as the input is not mutated concurrently.
