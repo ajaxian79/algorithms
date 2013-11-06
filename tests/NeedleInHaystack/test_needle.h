e577e0e05fffd37d4711de4af9288beb1e506340
ajaxian79 <blaketullysmith@gmail.com>
2013-11-05 20:36:34 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -294,3 +294,4 @@ MunitTest needle_tests[] = {
 // edge note 267: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 268: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 269: Edge case: all-equal input → linear-time fast path.
+// edge note 270: Time complexity: O(1).
