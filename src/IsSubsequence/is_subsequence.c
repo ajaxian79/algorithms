3485050d27d9381034f11014e209c414f38cb224
ajaxian79 <blaketullysmith@gmail.com>
2013-11-06 20:25:25 -0800

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -273,3 +273,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 253: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 254: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 255: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 256: Idempotent — calling twice with the same input is a no-op the second time.
