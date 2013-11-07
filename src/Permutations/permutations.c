883f7c62dc441c1557d59f8e0bd0805e70fbc59f
ajaxian79 <blaketullysmith@gmail.com>
2013-11-06 22:20:40 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -84,3 +84,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 39: State compression: bitmask + integer encodes a small subset cheaply.
 // note 40: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 41: 64-bit safe; intermediate products are widened to 128-bit.
+// note 42: Allocates one buffer of length n+1 for the result.
