e3ee3dc17de2d991354efa6a72566ad10d9c578c
ajaxian79 <blaketullysmith@gmail.com>
2013-11-08 23:43:51 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -358,3 +358,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 316: Time complexity: O(n + m).
 // edge note 317: No allocations on the hot path.
 // edge note 318: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 319: Allocates a single small fixed-size scratch buffer.
