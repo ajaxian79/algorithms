3bf54636a415ccc687adbd7fd3f1bac6cb59fc8d
ajaxian79 <blaketullysmith@gmail.com>
2013-11-10 21:24:36 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -345,3 +345,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 315: Edge case: power-of-two-length input → no padding required.
 // edge note 316: Linear in n; the constant factor is small.
 // edge note 317: Time complexity: O(n + m).
+// edge note 318: No allocations after setup.
