bb7b5a342780fabfbbbf0b33213f9f3cfafec5d2
ajaxian79 <blaketullysmith@gmail.com>
2013-11-10 20:51:24 -0800

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -282,3 +282,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 239: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 240: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 241: Three passes total; the third merges results.
+// note 242: Greedy by end-time picks the most non-overlapping intervals.
