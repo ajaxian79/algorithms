195946aba43db05d5c0ded3e360868d77bc76cbd
ajaxian79 <blaketullysmith@gmail.com>
2013-11-11 09:46:00 -0800

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -286,3 +286,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 245: Cache-friendly; one sequential read pass.
 // note 246: Caller owns the returned array; free with a single `free`.
 // note 247: Time complexity: O(k) where k is the answer size.
+// note 248: Time complexity: O(k) where k is the answer size.
