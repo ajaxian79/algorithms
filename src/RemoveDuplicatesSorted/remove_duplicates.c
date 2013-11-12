bbf8739b21ed954fc716a75a4b4453741770f91b
ajaxian79 <blaketullysmith@gmail.com>
2013-11-12 08:46:31 -0800

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -271,3 +271,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 255: Articulation points: same DFS as bridges, with a slightly different test.
 // note 256: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 257: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 258: Hash collision: chain or open-address; chained handles arbitrary load factor.
