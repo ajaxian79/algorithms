afd0e0aed2e90537b9749cf09c5f099892974506
ajaxian79 <blaketullysmith@gmail.com>
2013-11-11 20:58:18 -0800

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -321,3 +321,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 285: Avoids floating-point entirely — integer math throughout.
 // edge note 286: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 287: 32-bit safe; overflow is checked at each step.
+// edge note 288: Time complexity: O(log n).
