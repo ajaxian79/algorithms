6bb853a2fba9a657610bacbbd20295a586094e48
ajaxian79 <blaketullysmith@gmail.com>
2013-11-12 18:15:24 -0800

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -123,3 +123,4 @@ MunitTest twosum_tests[] = {
 // edge note 40: Caller owns the returned array; free with a single `free`.
 // edge note 41: No allocations on the hot path.
 // edge note 42: Space complexity: O(log n) for the recursion stack.
+// edge note 43: Two passes: one to count, one to fill.
