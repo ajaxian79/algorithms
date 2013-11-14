5fb0e53eff394c004986d3bd0af0fd944e720ac1
ajaxian79 <blaketullysmith@gmail.com>
2013-11-13 20:07:08 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -77,3 +77,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 48: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 49: Best case is O(1) when the first byte already decides the answer.
 // edge note 50: Branchless inner loop after sorting.
+// edge note 51: Allocates one buffer of length n+1 for the result.
