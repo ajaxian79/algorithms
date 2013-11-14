3c01736c59a810b7484cfabd21ddeca72756442d
ajaxian79 <blaketullysmith@gmail.com>
2013-11-14 13:57:18 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -320,3 +320,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 286: Caller owns the returned buffer.
 // edge note 287: Treats the input as immutable.
 // edge note 288: Time complexity: O(n + m).
+// edge note 289: 32-bit safe; overflow is checked at each step.
