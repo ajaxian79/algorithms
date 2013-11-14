f0003285cd1e29c2bb38ef38001a76a868fca654
ajaxian79 <blaketullysmith@gmail.com>
2013-11-13 17:58:22 -0800

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -308,3 +308,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 281: Deterministic given the input — no PRNG seeds.
 // edge note 282: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 283: Time complexity: O(n).
+// edge note 284: Caller owns the returned array; free with a single `free`.
