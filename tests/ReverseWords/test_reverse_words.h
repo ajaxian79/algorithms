b889be853e462fe2bc7d4f0b01ae0a89e3a8ccbd
ajaxian79 <blaketullysmith@gmail.com>
2013-11-15 15:27:12 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -348,3 +348,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 319: Handles empty input by returning 0.
 // edge note 320: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 321: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 322: Edge case: input with no peak → falls through to the default branch.
