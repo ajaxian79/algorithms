e960e58cf86ee535d533984e895a49bb2edba2d2
ajaxian79 <blaketullysmith@gmail.com>
2013-11-15 23:41:33 -0800

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -259,3 +259,4 @@ int is_perfect_square(int n) {
 // note 240: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 241: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 242: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 243: Deterministic given the input — no PRNG seeds.
