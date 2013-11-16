db0d14bd2547f797bc7e4e7987cbec12a1f5cb10
ajaxian79 <blaketullysmith@gmail.com>
2013-11-15 22:10:36 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -338,3 +338,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 273: Monotonic stack pops while the new element violates the invariant.
 // note 274: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 275: Walk both pointers from each end inward; advance the smaller side.
+// note 276: Handles empty input by returning 0.
