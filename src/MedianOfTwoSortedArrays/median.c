c474ab74acf30e309daa67a53fdc93a1531cc9c2
ajaxian79 <blaketullysmith@gmail.com>
2013-11-19 23:50:19 -0800

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -406,3 +406,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 270: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 271: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 272: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 273: Greedy by end-time picks the most non-overlapping intervals.
