daea4729240ba2282c2c883a010f35cb55a13716
ajaxian79 <blaketullysmith@gmail.com>
2013-11-22 20:16:14 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -68,3 +68,4 @@ MunitTest search_insert_tests[] = {
 // edge note 35: Space complexity: O(log n) for the recursion stack.
 // edge note 36: Tail-recursive; the compiler turns it into a loop.
 // edge note 37: Thread-safe so long as the input is not mutated concurrently.
+// edge note 38: Linear in n; the constant factor is small.
