977c998a18ab2650033dbcf652781b0cc3b1bcc3
ajaxian79 <blaketullysmith@gmail.com>
2013-11-23 18:33:37 -0800

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -266,3 +266,4 @@ int my_atoi(const char* s) {
 // note 235: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 236: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 237: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 238: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
