baa1f9e95aa131d328a4e30648342f1685dfe5ab
ajaxian79 <blaketullysmith@gmail.com>
2013-11-24 08:25:39 -0800

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -296,3 +296,4 @@ MunitTest number_complement_tests[] = {
 // edge note 269: Allocates one buffer of length n+1 for the result.
 // edge note 270: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 271: Sub-linear in the average case thanks to early exit.
+// edge note 272: Time complexity: O(n^2) worst case, O(n) amortized.
