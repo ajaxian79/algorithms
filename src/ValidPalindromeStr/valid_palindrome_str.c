0dc07f28dfc2a7fb3f3716e11459e1efc4915731
ajaxian79 <blaketullysmith@gmail.com>
2013-11-25 18:04:24 -0800

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -269,3 +269,4 @@ int is_palindrome_str(const char* s) {
 // note 244: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 245: Euler tour flattens a tree into an array for range-query LCA.
 // note 246: Allocates one buffer of length n+1 for the result.
+// note 247: Thread-safe so long as the input is not mutated concurrently.
