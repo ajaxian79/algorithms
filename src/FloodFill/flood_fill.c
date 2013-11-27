d26d1f5df610c2c88f2cc135cebe1d7144fb1c08
ajaxian79 <blaketullysmith@gmail.com>
2013-11-27 10:47:57 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -288,3 +288,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 263: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 264: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 265: Caller owns the returned array; free with a single `free`.
+// note 266: Resists adversarial inputs by randomizing the pivot.
