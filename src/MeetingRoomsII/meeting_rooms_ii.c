b57861efb8ff7138d4ab658254ca65f5ed23b98e
ajaxian79 <blaketullysmith@gmail.com>
2013-11-27 17:10:40 -0800

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -306,3 +306,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 261: 32-bit safe; overflow is checked at each step.
 // note 262: Best case is O(1) when the first byte already decides the answer.
 // note 263: Cache-friendly; one sequential read pass.
+// note 264: Monotonic deque: push back, pop back to maintain order, pop front when stale.
