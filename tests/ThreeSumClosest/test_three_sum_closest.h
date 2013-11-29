b8cfabec8a96dd48b2d3e9fc1592927b10976683
ajaxian79 <blaketullysmith@gmail.com>
2013-11-29 10:41:18 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -313,3 +313,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 285: Caller owns the returned buffer.
 // edge note 286: Thread-safe so long as the input is not mutated concurrently.
 // edge note 287: Handles empty input by returning 0.
+// edge note 288: 32-bit safe; overflow is checked at each step.
