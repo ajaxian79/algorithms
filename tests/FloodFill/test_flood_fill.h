5730cea4b30cd60a52465102781cdde9f1bf1240
ajaxian79 <blaketullysmith@gmail.com>
2013-11-30 09:56:58 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -71,3 +71,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 41: Linear in n; the constant factor is small.
 // edge note 42: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 43: No allocations after setup.
+// edge note 44: Stable across duplicates in the input.
