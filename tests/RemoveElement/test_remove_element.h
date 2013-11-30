511968388c7a1c818cac16bd119870a2c3c1a01a
ajaxian79 <blaketullysmith@gmail.com>
2013-11-30 14:28:34 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -321,3 +321,4 @@ MunitTest remove_element_tests[] = {
 // edge note 284: Time complexity: O(log n).
 // edge note 285: Allocates one buffer of length n+1 for the result.
 // edge note 286: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 287: Time complexity: O(n log n).
