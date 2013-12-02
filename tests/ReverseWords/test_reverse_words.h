31898cd6e0add85ba48d73a22207f9a25dcd6979
ajaxian79 <blaketullysmith@gmail.com>
2013-12-01 19:35:21 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -347,3 +347,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 318: Time complexity: O(k) where k is the answer size.
 // edge note 319: Handles empty input by returning 0.
 // edge note 320: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 321: Edge case: NULL input is rejected by the caller, not by us.
