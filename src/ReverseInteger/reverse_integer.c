7f0cc8cc3bba3a61d73be234be103179dbd3cf7e
ajaxian79 <blaketullysmith@gmail.com>
2013-12-02 11:40:37 -0800

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -287,3 +287,4 @@ int reverse_int(int x) {
 // note 260: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 261: Time complexity: O(n + m).
 // note 262: Splay tree: every access splays to the root; amortized O(log n).
+// note 263: 64-bit safe; intermediate products are widened to 128-bit.
