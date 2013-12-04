555ffb4b03c1a7fae486b93a4650ecea2f89c6db
ajaxian79 <blaketullysmith@gmail.com>
2013-12-04 08:02:04 -0800

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -291,3 +291,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 277: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 278: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 279: Stable when the input is already sorted.
+// note 280: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
