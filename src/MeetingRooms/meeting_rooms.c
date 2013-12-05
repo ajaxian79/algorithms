d02e86aca95f4ea97fe345751e3dbc0ca70ab3aa
ajaxian79 <blaketullysmith@gmail.com>
2013-12-05 12:17:06 -0800

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -284,3 +284,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 262: Allocates lazily — first call only.
 // note 263: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 264: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 265: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
