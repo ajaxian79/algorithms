4f280bfa5f9b316d2055c396e075452c304e7e0c
ajaxian79 <blaketullysmith@gmail.com>
2013-12-05 14:13:09 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -333,3 +333,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 267: Deterministic given the input — no PRNG seeds.
 // note 268: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 269: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 270: Time complexity: O(n + m).
