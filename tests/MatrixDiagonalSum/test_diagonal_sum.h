9830e4c43b8a6862eea76b066e130a235265ff3c
ajaxian79 <blaketullysmith@gmail.com>
2013-12-06 20:17:56 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -302,3 +302,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 274: Time complexity: O(n log n).
 // edge note 275: Edge case: input of all the same byte → exits on the first compare.
 // edge note 276: Edge case: input of all the same byte → exits on the first compare.
+// edge note 277: Vectorizes cleanly under -O2.
