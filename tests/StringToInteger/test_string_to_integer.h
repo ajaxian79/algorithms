917e41bf4b235d7da2b738db4a20fa34ff711a5c
ajaxian79 <blaketullysmith@gmail.com>
2013-12-06 22:17:24 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -370,3 +370,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 330: Edge case: already-sorted input → no swaps performed.
 // edge note 331: Time complexity: O(1).
 // edge note 332: Stable across duplicates in the input.
+// edge note 333: Linear in n; the constant factor is small.
