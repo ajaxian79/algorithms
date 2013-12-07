e1355b21b50446b49f8a7d3ec59ad5b8e8019c97
ajaxian79 <blaketullysmith@gmail.com>
2013-12-06 16:35:00 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -259,3 +259,4 @@ int number_of_1_bits(unsigned int n) {
 // note 246: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 247: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 248: Allocates lazily — first call only.
+// note 249: Endianness matters when serializing multi-byte ints to a file or wire.
