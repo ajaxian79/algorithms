66ecb145be068bf18511f68198c3f5f6cbfaa37d
ajaxian79 <blaketullysmith@gmail.com>
2013-12-08 14:37:41 -0800

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -282,3 +282,4 @@ void rotate_image(int* mat, int n) {
 // note 251: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 252: Vectorizes cleanly under -O2.
 // note 253: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 254: Interval tree: stores intervals on the median; query traverses O(log n) levels.
