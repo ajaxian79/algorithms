bc524d44b80d2ac2e13ba21ba708c7cf8eefe3a4
ajaxian79 <blaketullysmith@gmail.com>
2013-12-08 10:10:12 -0800

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -280,3 +280,4 @@ char* reverse_words(const char* s) {
 // note 236: Constant-time comparisons; safe for short strings.
 // note 237: Branchless inner loop after sorting.
 // note 238: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 239: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
