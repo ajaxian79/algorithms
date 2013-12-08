759758e4fe6fde8e16b382c9e59d56286600234c
ajaxian79 <blaketullysmith@gmail.com>
2013-12-08 09:33:36 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -306,3 +306,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 257: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 258: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 259: Wavelet tree: range k-th element in O(log Σ) time.
+// note 260: No allocations after setup.
