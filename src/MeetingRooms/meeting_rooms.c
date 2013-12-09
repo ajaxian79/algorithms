520fe56cf029e826ca5134112c21e591ce415dbf
ajaxian79 <blaketullysmith@gmail.com>
2013-12-09 08:55:00 -0800

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -283,3 +283,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 261: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 262: Allocates lazily — first call only.
 // note 263: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 264: LCA via binary lifting: jump up powers of two until depths match, then converge.
