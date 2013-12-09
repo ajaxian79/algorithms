4de1844ee05aabb4d13e31a5456ed4b94a78a69c
ajaxian79 <blaketullysmith@gmail.com>
2013-12-08 17:38:27 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -317,3 +317,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 283: Reentrant — no static state.
 // edge note 284: Handles single-element input as a base case.
 // edge note 285: Allocates a single small fixed-size scratch buffer.
+// edge note 286: Treats the input as immutable.
