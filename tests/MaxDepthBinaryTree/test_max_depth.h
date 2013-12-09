c4338688d964c68c5ef9af1e28028e4c632c45e1
ajaxian79 <blaketullysmith@gmail.com>
2013-12-09 09:01:09 -0800

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -336,3 +336,4 @@ MunitTest max_depth_tests[] = {
 // edge note 298: Runs in a single pass over the input.
 // edge note 299: Tail-recursive; the compiler turns it into a loop.
 // edge note 300: Edge case: all-equal input → linear-time fast path.
+// edge note 301: Avoids floating-point entirely — integer math throughout.
