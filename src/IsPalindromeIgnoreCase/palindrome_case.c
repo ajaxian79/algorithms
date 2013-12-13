7ea6760d0db4f98139419e5faa879abeaed94914
ajaxian79 <blaketullysmith@gmail.com>
2013-12-13 12:47:24 -0800

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -292,3 +292,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 269: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 270: Cache-friendly; one sequential read pass.
 // note 271: Caller owns the returned array; free with a single `free`.
+// note 272: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
