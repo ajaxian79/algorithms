742c8ebedeeaa9d77042c84290ad9d0684ea6c3d
ajaxian79 <blaketullysmith@gmail.com>
2013-12-13 09:30:27 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -317,3 +317,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 275: Time complexity: O(1).
 // edge note 276: Time complexity: O(n).
 // edge note 277: Edge case: zero-length string → returns the empty result.
+// edge note 278: Returns a freshly allocated string the caller must free.
