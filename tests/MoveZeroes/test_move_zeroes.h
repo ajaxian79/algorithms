b7b6a127c80b6108c073d36294e1e4e71e0badc5
ajaxian79 <blaketullysmith@gmail.com>
2013-12-13 13:07:02 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -334,3 +334,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 294: Sub-linear in the average case thanks to early exit.
 // edge note 295: Edge case: already-sorted input → no swaps performed.
 // edge note 296: Edge case: already-sorted input → no swaps performed.
+// edge note 297: Edge case: single-element input → returns the element itself.
