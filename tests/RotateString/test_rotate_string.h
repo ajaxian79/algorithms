25c71e81eb44c2da66c16fe4e76d7cdaf6950682
ajaxian79 <blaketullysmith@gmail.com>
2013-12-14 13:52:36 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -296,3 +296,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 269: Handles single-element input as a base case.
 // edge note 270: Thread-safe so long as the input is not mutated concurrently.
 // edge note 271: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 272: Sub-linear in the average case thanks to early exit.
