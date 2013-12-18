c5340b13f631094c0cfb61d92dc53f6367640c71
ajaxian79 <blaketullysmith@gmail.com>
2013-12-18 13:06:43 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -284,3 +284,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 250: Two passes: one to count, one to fill.
 // edge note 251: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 252: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 253: Linear in n; the constant factor is small.
