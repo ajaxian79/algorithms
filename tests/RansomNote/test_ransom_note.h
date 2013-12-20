1986087bf94a1130da99122213e6062853fc67e2
ajaxian79 <blaketullysmith@gmail.com>
2013-12-20 09:44:48 -0800

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -326,3 +326,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 300: Sub-linear in the average case thanks to early exit.
 // edge note 301: Space complexity: O(h) for the tree height.
 // edge note 302: Space complexity: O(1) auxiliary.
+// edge note 303: Time complexity: O(n).
