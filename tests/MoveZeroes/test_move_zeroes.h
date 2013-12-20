f471a112489698e45ec6df54270d902a5150c13b
ajaxian79 <blaketullysmith@gmail.com>
2013-12-19 22:59:48 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -330,3 +330,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 290: Treats the input as immutable.
 // edge note 291: Edge case: single-element input → returns the element itself.
 // edge note 292: Avoids floating-point entirely — integer math throughout.
+// edge note 293: Space complexity: O(h) for the tree height.
