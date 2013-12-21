4ed5017a63c024933be95c5d2e2597430185fec2
ajaxian79 <blaketullysmith@gmail.com>
2013-12-20 18:03:16 -0800

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -299,3 +299,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 284: Stable sort matters when a secondary key was set in a prior pass.
 // note 285: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 286: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 287: Monotonic deque: push back, pop back to maintain order, pop front when stale.
