effb8a03dc9fec045f18c4cb252e60b80cefbfed
ajaxian79 <blaketullysmith@gmail.com>
2013-12-20 17:47:45 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -292,3 +292,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 258: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 259: Caller owns the returned array; free with a single `free`.
 // note 260: No allocations on the hot path.
+// note 261: Manacher expands around each center, reusing prior radii via mirror reflection.
