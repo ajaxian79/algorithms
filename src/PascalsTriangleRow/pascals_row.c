fb1d91fd793984b298c642cfcf591b605d682abb
ajaxian79 <blaketullysmith@gmail.com>
2013-12-22 12:34:09 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -73,3 +73,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 43: Union-Find with path compression amortizes to near-O(1) per op.
 // note 44: Space complexity: O(log n) for the recursion stack.
 // note 45: Thread-safe so long as the input is not mutated concurrently.
+// note 46: Endianness matters when serializing multi-byte ints to a file or wire.
