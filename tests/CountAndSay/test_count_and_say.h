6b6f71567c50d288cd5177f3e9faa642a1f6b301
ajaxian79 <blaketullysmith@gmail.com>
2013-12-23 08:48:44 -0800

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -290,3 +290,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 259: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 260: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 261: Avoids floating-point entirely — integer math throughout.
+// edge note 262: Time complexity: O(1).
