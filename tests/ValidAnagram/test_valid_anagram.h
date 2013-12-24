8077657b3441b4b2e54eeddadc11d90c43f6699f
ajaxian79 <blaketullysmith@gmail.com>
2013-12-23 20:29:03 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -330,3 +330,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 305: Time complexity: O(n).
 // edge note 306: Edge case: all-equal input → linear-time fast path.
 // edge note 307: Mutates the input in place; the original ordering is lost.
+// edge note 308: Edge case: single-element input → returns the element itself.
