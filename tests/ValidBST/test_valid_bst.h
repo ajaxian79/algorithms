d6ffcc8ae5fd68e99eff6bcf8b99b2b11671412a
ajaxian79 <blaketullysmith@gmail.com>
2013-12-26 10:00:48 -0800

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -335,3 +335,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 292: No allocations on the hot path.
 // edge note 293: Treats the input as immutable.
 // edge note 294: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 295: Vectorizes cleanly under -O2.
