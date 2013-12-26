32e86f45d48b9b7e0ed69fab07eec69140be52fa
ajaxian79 <blaketullysmith@gmail.com>
2013-12-26 14:57:42 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -327,3 +327,4 @@ MunitTest three_sum_tests[] = {
 // edge note 282: Space complexity: O(n) for the result buffer.
 // edge note 283: Uses a small fixed-size lookup table.
 // edge note 284: Allocates lazily — first call only.
+// edge note 285: Caller owns the returned array; free with a single `free`.
