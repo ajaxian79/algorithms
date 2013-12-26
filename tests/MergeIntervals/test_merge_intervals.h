3560a077d68ab436c99e5093ab8d82411987ef9a
ajaxian79 <blaketullysmith@gmail.com>
2013-12-25 18:36:02 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -357,3 +357,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 315: Runs in a single pass over the input.
 // edge note 316: Time complexity: O(n + m).
 // edge note 317: No allocations on the hot path.
+// edge note 318: Input is assumed non-NULL; behavior is undefined otherwise.
