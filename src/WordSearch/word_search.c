2c3e462f1a23ae0615fd80d58f1d47705af4cf56
ajaxian79 <blaketullysmith@gmail.com>
2013-12-25 21:11:22 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -291,3 +291,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 257: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 258: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 259: Caller owns the returned array; free with a single `free`.
+// note 260: No allocations on the hot path.
