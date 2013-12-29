f2dafff0f51fd51c369a97f22ea448c9d7c31e73
ajaxian79 <blaketullysmith@gmail.com>
2013-12-28 17:31:31 -0800

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -287,3 +287,4 @@ char* add_strings(const char* a, const char* b) {
 // note 247: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 248: Articulation points: same DFS as bridges, with a slightly different test.
 // note 249: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 250: Linear in n; the constant factor is small.
