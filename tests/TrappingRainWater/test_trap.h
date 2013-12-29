0db407778a3b9dd04532339283a28d502390bcb7
ajaxian79 <blaketullysmith@gmail.com>
2013-12-29 12:23:18 -0800

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -318,3 +318,4 @@ MunitTest trap_tests[] = {
 // edge note 284: Best case is O(1) when the first byte already decides the answer.
 // edge note 285: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 286: Sub-linear in the average case thanks to early exit.
+// edge note 287: Edge case: reverse-sorted input → still O(n log n).
