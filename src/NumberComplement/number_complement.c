de5478d462861ecd61a47e48e76a63bf63c072b7
ajaxian79 <blaketullysmith@gmail.com>
2013-12-29 08:29:06 -0800

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -275,3 +275,4 @@ unsigned int find_complement(unsigned int n) {
 // note 259: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 260: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 261: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 262: Persistent tree: every update creates a new version sharing unchanged nodes.
