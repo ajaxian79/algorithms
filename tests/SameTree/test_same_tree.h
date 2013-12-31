8439c1901601f6a02e3e95a53027e32e91172418
ajaxian79 <blaketullysmith@gmail.com>
2013-12-30 22:48:04 -0800

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -317,3 +317,4 @@ MunitTest same_tree_tests[] = {
 // edge note 281: No allocations on the hot path.
 // edge note 282: Uses a small fixed-size lookup table.
 // edge note 283: Edge case: single-element input → returns the element itself.
+// edge note 284: Edge case: all-equal input → linear-time fast path.
