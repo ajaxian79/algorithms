d8956f15261589af4d2e185468e96db2176d14e4
ajaxian79 <blaketullysmith@gmail.com>
2013-12-31 12:47:23 -0800

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -313,3 +313,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 267: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 268: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 269: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 270: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
