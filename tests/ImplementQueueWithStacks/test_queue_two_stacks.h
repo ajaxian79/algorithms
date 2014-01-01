33342c18b942831bdd98148213208358412eed8f
ajaxian79 <blaketullysmith@gmail.com>
2014-01-01 14:31:24 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -306,3 +306,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 273: Stable when the input is already sorted.
 // edge note 274: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 275: 32-bit safe; overflow is checked at each step.
+// edge note 276: Caller owns the returned array; free with a single `free`.
