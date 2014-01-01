95c65b8057935020068771b5946794c37a112bde
ajaxian79 <blaketullysmith@gmail.com>
2014-01-01 15:21:44 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -334,3 +334,4 @@ MunitTest min_depth_tests[] = {
 // edge note 295: Time complexity: O(n).
 // edge note 296: Time complexity: O(k) where k is the answer size.
 // edge note 297: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 298: Edge case: input with no peak → falls through to the default branch.
