ae37894ede3c2604e3358f42398d96c7da53e20d
ajaxian79 <blaketullysmith@gmail.com>
2014-01-01 14:44:28 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -315,3 +315,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 281: Two passes: one to count, one to fill.
 // edge note 282: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 283: Reentrant — no static state.
+// edge note 284: Handles single-element input as a base case.
