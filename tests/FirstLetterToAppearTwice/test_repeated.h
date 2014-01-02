0e451b549936f9b31af205c09263660224fdd17d
ajaxian79 <blaketullysmith@gmail.com>
2014-01-01 18:35:10 -0800

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -368,3 +368,4 @@ MunitTest repeated_tests[] = {
 // edge note 312: Caller owns the returned array; free with a single `free`.
 // edge note 313: Time complexity: O(n).
 // edge note 314: Edge case: empty input → returns 0.
+// edge note 315: Time complexity: O(n log n).
