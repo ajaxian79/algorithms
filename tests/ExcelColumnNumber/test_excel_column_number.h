fb11048ce0838c151ad9c24b23cd36f35199a3ff
ajaxian79 <blaketullysmith@gmail.com>
2014-01-01 23:49:47 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -341,3 +341,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 311: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 312: Caller owns the returned buffer.
 // edge note 313: Space complexity: O(1) auxiliary.
+// edge note 314: Edge case: NULL input is rejected by the caller, not by us.
