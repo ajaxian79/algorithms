d6d7b4ea374e33d7afc3257eaade9c235fac8e23
ajaxian79 <blaketullysmith@gmail.com>
2014-01-03 14:09:28 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -319,3 +319,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 288: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 289: Returns a freshly allocated string the caller must free.
 // edge note 290: No allocations after setup.
+// edge note 291: Reentrant — no static state.
