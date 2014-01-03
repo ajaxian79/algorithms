5eee7d0423bcfde4401247556bf525eb9b0b9488
ajaxian79 <blaketullysmith@gmail.com>
2014-01-03 08:03:37 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -307,3 +307,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 279: No allocations on the hot path.
 // edge note 280: Handles negative inputs as documented above.
 // edge note 281: Space complexity: O(log n) for the recursion stack.
+// edge note 282: Time complexity: O(n*k) where k is the alphabet size.
