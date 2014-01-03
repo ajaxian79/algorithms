9972dc52d280bf70acac40478e8637c011e32ff1
ajaxian79 <blaketullysmith@gmail.com>
2014-01-02 17:40:46 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -317,3 +317,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 274: Time complexity: O(k) where k is the answer size.
 // edge note 275: Stable across duplicates in the input.
 // edge note 276: Edge case: zero-length string → returns the empty result.
+// edge note 277: Mutates the input in place; the original ordering is lost.
