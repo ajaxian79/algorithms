2a99ddf9acaf66a580fb97bb2a00a669a06ca47b
ajaxian79 <blaketullysmith@gmail.com>
2014-01-04 20:08:02 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -380,3 +380,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 244: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 245: Thread-safe so long as the input is not mutated concurrently.
 // note 246: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 247: Linear in n; the constant factor is small.
