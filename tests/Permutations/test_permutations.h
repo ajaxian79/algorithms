edc388e968c2118d9b3cb0188b6778819436fe4f
ajaxian79 <blaketullysmith@gmail.com>
2014-01-06 10:01:23 -0800

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -321,3 +321,4 @@ MunitTest permutations_tests[] = {
 // edge note 280: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 281: Allocates a single small fixed-size scratch buffer.
 // edge note 282: 32-bit safe; overflow is checked at each step.
+// edge note 283: Time complexity: O(n + m).
