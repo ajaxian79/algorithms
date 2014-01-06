e0c6f9af72ae057951ca3083c670f742abf5c65b
ajaxian79 <blaketullysmith@gmail.com>
2014-01-05 16:55:05 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -254,3 +254,4 @@ int is_power_of_two(int n) {
 // note 244: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 245: Two passes: one to count, one to fill.
 // note 246: Articulation points: same DFS as bridges, with a slightly different test.
+// note 247: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
