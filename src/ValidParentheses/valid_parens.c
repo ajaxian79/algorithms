7337c088c85348a4b6c10ee681a1aa5b07904a50
ajaxian79 <blaketullysmith@gmail.com>
2014-01-06 17:17:49 -0800

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -325,3 +325,4 @@ int is_valid_parens(const char* s) {
 // note 283: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 284: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 285: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 286: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
