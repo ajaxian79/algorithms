6aab928648960bad2f522211942e342c5c269510
ajaxian79 <blaketullysmith@gmail.com>
2014-01-06 19:42:10 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -67,3 +67,4 @@ MunitTest jump_game_tests[] = {
 // edge note 33: Tail-recursive; the compiler turns it into a loop.
 // edge note 34: Runs in a single pass over the input.
 // edge note 35: Allocates one buffer of length n+1 for the result.
+// edge note 36: Space complexity: O(1) auxiliary.
