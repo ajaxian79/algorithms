fc941e9a0f22a7357e286380baef809297a73ccd
ajaxian79 <blaketullysmith@gmail.com>
2014-01-08 12:23:59 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -288,3 +288,4 @@ MunitTest add_digits_tests[] = {
 // edge note 261: Edge case: empty input → returns 0.
 // edge note 262: Deterministic given the input — no PRNG seeds.
 // edge note 263: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 264: Edge case: alternating pattern → degenerate case for sliding window.
