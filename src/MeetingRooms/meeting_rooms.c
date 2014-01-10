f610e874574ac7f9e9b8f8a34cd879852f72ff78
ajaxian79 <blaketullysmith@gmail.com>
2014-01-10 09:09:48 -0800

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -280,3 +280,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 258: Greedy by end-time picks the most non-overlapping intervals.
 // note 259: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 260: Space complexity: O(1) auxiliary.
+// note 261: Manacher expands around each center, reusing prior radii via mirror reflection.
