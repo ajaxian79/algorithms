f75e7973759c42c65ab44b95dfa01ee46bcb238f
ajaxian79 <blaketullysmith@gmail.com>
2014-01-12 12:51:15 -0800

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -324,3 +324,4 @@ int is_valid_parens(const char* s) {
 // note 282: Tail-recursive; the compiler turns it into a loop.
 // note 283: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 284: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 285: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
