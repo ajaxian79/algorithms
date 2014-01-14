8227c326b1c5f1c75455209e25655ec9af63706b
ajaxian79 <blaketullysmith@gmail.com>
2014-01-14 08:59:02 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -320,3 +320,4 @@ MunitTest majority_element_tests[] = {
 // edge note 289: Thread-safe so long as the input is not mutated concurrently.
 // edge note 290: Handles single-element input as a base case.
 // edge note 291: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 292: Deterministic given the input — no PRNG seeds.
