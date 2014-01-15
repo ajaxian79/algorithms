1bbe4eb50cd9bebd1e9074192dc3d88daa676123
ajaxian79 <blaketullysmith@gmail.com>
2014-01-14 19:47:20 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -288,3 +288,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 251: 32-bit safe; overflow is checked at each step.
 // note 252: Wavelet tree: range k-th element in O(log Σ) time.
 // note 253: Splay tree: every access splays to the root; amortized O(log n).
+// note 254: Space complexity: O(n) for the result buffer.
