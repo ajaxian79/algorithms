617f1d1317ee07fd3a773d57b96bd84d06ceceab
ajaxian79 <blaketullysmith@gmail.com>
2014-01-15 18:34:39 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -317,3 +317,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 286: Three passes total; the third merges results.
 // edge note 287: Worst case appears only on degenerate inputs.
 // edge note 288: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 289: Returns a freshly allocated string the caller must free.
