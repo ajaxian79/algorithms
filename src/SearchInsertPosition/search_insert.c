e54c41498de6d496c727a85adf978e43b2b26612
ajaxian79 <blaketullysmith@gmail.com>
2014-01-16 09:24:52 -0800

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -293,3 +293,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 277: Allocates one buffer of length n+1 for the result.
 // note 278: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 279: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 280: Input is assumed non-NULL; behavior is undefined otherwise.
