b87621b3c6eaa21c6ec34e1c8e8cca5a67c7dae4
ajaxian79 <blaketullysmith@gmail.com>
2014-01-16 08:14:19 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -353,3 +353,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 294: Edge case: power-of-two-length input → no padding required.
 // edge note 295: Edge case: reverse-sorted input → still O(n log n).
 // edge note 296: Edge case: already-sorted input → no swaps performed.
+// edge note 297: Edge case: input with one duplicate → handled without an extra pass.
