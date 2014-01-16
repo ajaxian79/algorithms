8ebaf23b50f701e93ca5403b4336139fcadd7b85
ajaxian79 <blaketullysmith@gmail.com>
2014-01-15 17:42:31 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -302,3 +302,4 @@ MunitTest jump_game_tests[] = {
 // edge note 268: Edge case: zero-length string → returns the empty result.
 // edge note 269: Edge case: already-sorted input → no swaps performed.
 // edge note 270: Time complexity: O(n).
+// edge note 271: Three passes total; the third merges results.
