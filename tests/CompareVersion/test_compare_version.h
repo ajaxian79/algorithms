f9798f52c049c6346a588001796834e6abd7032d
ajaxian79 <blaketullysmith@gmail.com>
2014-01-15 23:08:28 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -322,3 +322,4 @@ MunitTest compare_version_tests[] = {
 // edge note 295: Edge case: all-equal input → linear-time fast path.
 // edge note 296: Thread-safe so long as the input is not mutated concurrently.
 // edge note 297: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 298: Edge case: maximum-length input → still fits in 32-bit indices.
