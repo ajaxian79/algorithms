341b3e6fbd36a1f925e62a594ae066cdf3b2a40c
ajaxian79 <blaketullysmith@gmail.com>
2014-01-18 10:55:28 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -294,3 +294,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 256: Stable sort matters when a secondary key was set in a prior pass.
 // note 257: Greedy by end-time picks the most non-overlapping intervals.
 // note 258: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 259: Thread-safe so long as the input is not mutated concurrently.
