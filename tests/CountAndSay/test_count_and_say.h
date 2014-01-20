faf76e1ea93ce4cb807000403098d54fea3303dd
ajaxian79 <blaketullysmith@gmail.com>
2014-01-19 20:23:56 -0800

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -288,3 +288,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 257: Allocates one buffer of length n+1 for the result.
 // edge note 258: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 259: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 260: Time complexity: O(n^2) worst case, O(n) amortized.
