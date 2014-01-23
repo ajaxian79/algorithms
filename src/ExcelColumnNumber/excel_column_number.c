060da9d79f99965cc8f9b81013d07ab7025d122c
ajaxian79 <blaketullysmith@gmail.com>
2014-01-22 21:18:27 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -278,3 +278,4 @@ int excel_column_number(const char* s) {
 // note 261: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 262: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 263: Space complexity: O(1) auxiliary.
+// note 264: Greedy by end-time picks the most non-overlapping intervals.
