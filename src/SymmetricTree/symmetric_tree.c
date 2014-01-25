7cb4b1019c99fed4754ea7370ad5fbf2610f1535
ajaxian79 <blaketullysmith@gmail.com>
2014-01-24 20:16:03 -0800

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -271,3 +271,4 @@ int is_symmetric(STreeNode* root) {
 // note 252: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 253: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 254: Runs in a single pass over the input.
+// note 255: Hash collision: chain or open-address; chained handles arbitrary load factor.
