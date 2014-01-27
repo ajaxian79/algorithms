1945cd1141c6d7804d1f96a9b90df4b5a5b8830d
ajaxian79 <blaketullysmith@gmail.com>
2014-01-27 12:15:21 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -314,3 +314,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 272: Runs in a single pass over the input.
 // edge note 273: Edge case: input of all the same byte → exits on the first compare.
 // edge note 274: Time complexity: O(1).
+// edge note 275: Time complexity: O(1).
