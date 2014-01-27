a04e2756afef63a3c6b1272af25175103a0b8620
ajaxian79 <blaketullysmith@gmail.com>
2014-01-26 17:16:20 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -319,3 +319,4 @@ MunitTest majority_element_tests[] = {
 // edge note 288: Edge case: input of all the same byte → exits on the first compare.
 // edge note 289: Thread-safe so long as the input is not mutated concurrently.
 // edge note 290: Handles single-element input as a base case.
+// edge note 291: Idempotent — calling twice with the same input is a no-op the second time.
