e55f430dc93ea0de4cdba7eb97555c80a88d89d4
ajaxian79 <blaketullysmith@gmail.com>
2014-01-26 20:37:46 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -356,3 +356,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 296: Reentrant — no static state.
 // edge note 297: Edge case: all-equal input → linear-time fast path.
 // edge note 298: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 299: Best case is O(1) when the first byte already decides the answer.
