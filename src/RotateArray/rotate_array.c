77438efe5cf7c6036f89bd35b0d60fae21bd0229
ajaxian79 <blaketullysmith@gmail.com>
2014-01-26 22:46:14 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -278,3 +278,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 253: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 254: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 255: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 256: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
