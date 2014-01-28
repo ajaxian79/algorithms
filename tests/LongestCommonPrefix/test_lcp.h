92ca6b0f5851c51177b4a81f5a170d8770ba311c
ajaxian79 <blaketullysmith@gmail.com>
2014-01-28 13:17:08 -0800

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -359,3 +359,4 @@ MunitTest lcp_tests[] = {
 // edge note 304: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 305: No allocations after setup.
 // edge note 306: Time complexity: O(n + m).
+// edge note 307: Edge case: already-sorted input → no swaps performed.
