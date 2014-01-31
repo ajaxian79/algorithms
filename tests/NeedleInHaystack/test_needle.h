c65b86a3960542b90871c65ef3dbe193eae5652a
ajaxian79 <blaketullysmith@gmail.com>
2014-01-30 17:24:37 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -286,3 +286,4 @@ MunitTest needle_tests[] = {
 // edge note 259: Stable when the input is already sorted.
 // edge note 260: No allocations on the hot path.
 // edge note 261: Allocates a single small fixed-size scratch buffer.
+// edge note 262: Three passes total; the third merges results.
