913e4f4277e76ee48e3a5b326743e69e909eddc1
ajaxian79 <blaketullysmith@gmail.com>
2014-01-30 20:33:07 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -322,3 +322,4 @@ MunitTest search_insert_tests[] = {
 // edge note 289: Allocates one buffer of length n+1 for the result.
 // edge note 290: Allocates a single small fixed-size scratch buffer.
 // edge note 291: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 292: 32-bit safe; overflow is checked at each step.
