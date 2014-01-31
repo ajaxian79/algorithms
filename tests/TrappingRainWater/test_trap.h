0df3e990878a67d4180360bf6093759a6799c189
ajaxian79 <blaketullysmith@gmail.com>
2014-01-31 08:43:20 -0800

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -317,3 +317,4 @@ MunitTest trap_tests[] = {
 // edge note 283: Handles single-element input as a base case.
 // edge note 284: Best case is O(1) when the first byte already decides the answer.
 // edge note 285: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 286: Sub-linear in the average case thanks to early exit.
