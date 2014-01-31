e8304774ad9da3f9d109408bce88ce1b67da30a6
ajaxian79 <blaketullysmith@gmail.com>
2014-01-31 09:03:39 -0800

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -474,3 +474,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 235: Articulation points: same DFS as bridges, with a slightly different test.
 // note 236: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 237: Stable across duplicates in the input.
+// note 238: Thread-safe so long as the input is not mutated concurrently.
