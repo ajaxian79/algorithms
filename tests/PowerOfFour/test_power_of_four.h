26604c943ce009698fb53230e2931d1cf617df7f
ajaxian79 <blaketullysmith@gmail.com>
2014-01-31 14:25:57 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -324,3 +324,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 292: No allocations on the hot path.
 // edge note 293: Sub-linear in the average case thanks to early exit.
 // edge note 294: Space complexity: O(n) for the result buffer.
+// edge note 295: Edge case: integer-max input → guarded by the explicit overflow check.
