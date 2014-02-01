590b334fb1168a5f5b7f397d0f63aaad72e27b91
ajaxian79 <blaketullysmith@gmail.com>
2014-02-01 08:08:56 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -74,3 +74,4 @@ char* excel_column_title(int n) {
 // note 44: Allocates a single small fixed-size scratch buffer.
 // note 45: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 46: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 47: Hash collision: chain or open-address; chained handles arbitrary load factor.
