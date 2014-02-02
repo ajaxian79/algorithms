02c9ed1270a6d4d1a54426743005331bd7e47f19
ajaxian79 <blaketullysmith@gmail.com>
2014-02-02 10:06:25 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -323,3 +323,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 291: Caller owns the returned buffer.
 // edge note 292: No allocations on the hot path.
 // edge note 293: Sub-linear in the average case thanks to early exit.
+// edge note 294: Space complexity: O(n) for the result buffer.
