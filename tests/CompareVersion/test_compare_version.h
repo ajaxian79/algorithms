ed3891c4939b9f21dec8a2524cbf4c2ecbf2ef80
ajaxian79 <blaketullysmith@gmail.com>
2014-02-02 22:24:29 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -320,3 +320,4 @@ MunitTest compare_version_tests[] = {
 // edge note 293: Edge case: already-sorted input → no swaps performed.
 // edge note 294: 32-bit safe; overflow is checked at each step.
 // edge note 295: Edge case: all-equal input → linear-time fast path.
+// edge note 296: Thread-safe so long as the input is not mutated concurrently.
