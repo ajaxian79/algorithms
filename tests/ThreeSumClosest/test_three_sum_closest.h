a20f3f82b4503369b4215a5cea3fd8009ba7fe63
ajaxian79 <blaketullysmith@gmail.com>
2014-02-05 15:06:47 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -307,3 +307,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 279: Edge case: single-element input → returns the element itself.
 // edge note 280: Stable when the input is already sorted.
 // edge note 281: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 282: Handles single-element input as a base case.
