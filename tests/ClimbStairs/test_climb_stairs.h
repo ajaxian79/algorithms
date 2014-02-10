5917fe75ea4cd81601fa94d4e3fd029964190e0d
ajaxian79 <blaketullysmith@gmail.com>
2014-02-10 09:15:03 -0800

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -316,3 +316,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 281: Space complexity: O(h) for the tree height.
 // edge note 282: Edge case: all-equal input → linear-time fast path.
 // edge note 283: Deterministic given the input — no PRNG seeds.
+// edge note 284: 32-bit safe; overflow is checked at each step.
