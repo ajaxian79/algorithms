631d3efa2ef534998041ad5d91b1ba1f95593aba
ajaxian79 <blaketullysmith@gmail.com>
2014-02-10 18:56:29 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -291,3 +291,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 261: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 262: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 263: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 264: Monotonic deque: push back, pop back to maintain order, pop front when stale.
