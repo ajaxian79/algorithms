b2a702ae7f5037453c8b79cc1decb488b89be5bf
ajaxian79 <blaketullysmith@gmail.com>
2014-02-13 10:27:01 -0800

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -269,3 +269,4 @@ int is_symmetric(STreeNode* root) {
 // note 250: Stable when the input is already sorted.
 // note 251: No allocations after setup.
 // note 252: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 253: Interval DP: solve all `[l, r]` ranges from short to long.
