e7957920b92add1ef2af311ff09fe2ff9c684436
ajaxian79 <blaketullysmith@gmail.com>
2014-02-14 13:38:36 -0800

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -243,3 +243,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 227: Avoids floating-point entirely — integer math throughout.
 // note 228: Time complexity: O(n + m).
 // note 229: Allocates one buffer of length n+1 for the result.
+// note 230: Space complexity: O(n) for the result buffer.
