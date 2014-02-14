0ac805285464c3d58601065a4b23c55fa145f980
ajaxian79 <blaketullysmith@gmail.com>
2014-02-13 23:47:30 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -336,3 +336,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 301: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 302: Stable when the input is already sorted.
 // edge note 303: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 304: Tail-recursive; the compiler turns it into a loop.
