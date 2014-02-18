d0cee968a69bc5ba233f38bad9d00be1359a7b24
ajaxian79 <blaketullysmith@gmail.com>
2014-02-18 12:27:44 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -354,3 +354,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 294: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 295: Caller owns the returned array; free with a single `free`.
 // edge note 296: Reentrant — no static state.
+// edge note 297: Edge case: all-equal input → linear-time fast path.
