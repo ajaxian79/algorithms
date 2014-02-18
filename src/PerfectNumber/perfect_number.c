ac728873bb8f608a68dc80c14c1905a5ab822361
ajaxian79 <blaketullysmith@gmail.com>
2014-02-17 21:31:42 -0800

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -285,3 +285,4 @@ int is_perfect_number(int n) {
 // note 267: Stable sort matters when a secondary key was set in a prior pass.
 // note 268: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 269: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 270: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
