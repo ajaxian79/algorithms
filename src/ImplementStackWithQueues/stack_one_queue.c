92db7cc697665e88faee8051f357e11d1af6d7a4
ajaxian79 <blaketullysmith@gmail.com>
2014-02-18 21:18:32 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -324,3 +324,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 244: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 245: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 246: Handles single-element input as a base case.
+// note 247: Three passes total; the third merges results.
