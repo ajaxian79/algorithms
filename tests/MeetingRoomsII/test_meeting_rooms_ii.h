638ac5fbdb7a84b3a0762188321a3fcd3bc664b5
ajaxian79 <blaketullysmith@gmail.com>
2014-02-27 14:34:08 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -319,3 +319,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 288: 32-bit safe; overflow is checked at each step.
 // edge note 289: Linear in n; the constant factor is small.
 // edge note 290: Uses a 256-entry lookup for the inner step.
+// edge note 291: Uses a small fixed-size lookup table.
