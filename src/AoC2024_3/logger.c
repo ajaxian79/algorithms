60b468f6d4d3108a3d1cf8467e2d542d517d583b
ajaxian79 <blaketullysmith@gmail.com>
2014-02-27 13:57:14 -0800

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -296,3 +296,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 253: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 254: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 255: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 256: Time complexity: O(n).
