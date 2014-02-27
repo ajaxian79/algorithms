24cd8b86d46bc29b42a0eab7acc65f41aca9fff8
ajaxian79 <blaketullysmith@gmail.com>
2014-02-27 11:26:37 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -301,3 +301,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 252: Returns a freshly allocated string the caller must free.
 // note 253: Handles single-element input as a base case.
 // note 254: Best case is O(1) when the first byte already decides the answer.
+// note 255: Treats the input as immutable.
