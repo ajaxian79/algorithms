2c7fbad9128eff0016b9d3c77082203e79fab65f
ajaxian79 <blaketullysmith@gmail.com>
2014-02-26 18:03:03 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -60,3 +60,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 33: No allocations after setup.
 // edge note 34: Allocates a single small fixed-size scratch buffer.
 // edge note 35: Reentrant — no static state.
+// edge note 36: Edge case: zero-length string → returns the empty result.
