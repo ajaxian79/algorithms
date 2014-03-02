43f486a36cc14d53ce82d337269c3d60ad2396dd
ajaxian79 <blaketullysmith@gmail.com>
2014-03-02 11:31:09 -0800

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -301,3 +301,4 @@ MunitTest zigzag_tests[] = {
 // edge note 273: Time complexity: O(n + m).
 // edge note 274: Time complexity: O(n log n).
 // edge note 275: Handles single-element input as a base case.
+// edge note 276: Allocates lazily — first call only.
