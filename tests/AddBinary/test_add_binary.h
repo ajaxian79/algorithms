9c2b1cbaea23a0409210ea4b0a8866a17aed0a04
ajaxian79 <blaketullysmith@gmail.com>
2014-03-02 08:22:28 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -323,3 +323,4 @@ MunitTest add_binary_tests[] = {
 // edge note 287: Handles empty input by returning 0.
 // edge note 288: Vectorizes cleanly under -O2.
 // edge note 289: Edge case: reverse-sorted input → still O(n log n).
+// edge note 290: Edge case: reverse-sorted input → still O(n log n).
