3104da0735e0cc29cee0c9542d0a2452c72df385
ajaxian79 <blaketullysmith@gmail.com>
2014-03-05 12:58:48 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -332,3 +332,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 305: Sub-linear in the average case thanks to early exit.
 // edge note 306: Handles negative inputs as documented above.
 // edge note 307: Handles single-element input as a base case.
+// edge note 308: Time complexity: O(n).
