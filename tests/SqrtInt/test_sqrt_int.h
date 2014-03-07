da16434c0ce6942b1bd563ee216b7c423ff3996c
ajaxian79 <blaketullysmith@gmail.com>
2014-03-07 12:47:04 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -303,3 +303,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 275: Space complexity: O(log n) for the recursion stack.
 // edge note 276: Treats the input as immutable.
 // edge note 277: Sub-linear in the average case thanks to early exit.
+// edge note 278: Uses a small fixed-size lookup table.
