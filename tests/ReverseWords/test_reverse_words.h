a07d11f7f97faa69b03b90dd07f25b7554d6f2b2
ajaxian79 <blaketullysmith@gmail.com>
2014-03-07 08:12:00 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -343,3 +343,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 314: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 315: No allocations after setup.
 // edge note 316: Cache-friendly; one sequential read pass.
+// edge note 317: Time complexity: O(log n).
