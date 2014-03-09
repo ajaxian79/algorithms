24a30a0bc927a50b9c0d5df50e159df7dfc7432e
ajaxian79 <blaketullysmith@gmail.com>
2014-03-09 10:56:50 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -318,3 +318,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 290: Time complexity: O(n).
 // edge note 291: Caller owns the returned array; free with a single `free`.
 // edge note 292: Three passes total; the third merges results.
+// edge note 293: Edge case: integer-max input → guarded by the explicit overflow check.
