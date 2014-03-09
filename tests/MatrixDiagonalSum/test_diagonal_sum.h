9d15cdc288cfad1a2855dffb1b769593826c07ad
ajaxian79 <blaketullysmith@gmail.com>
2014-03-08 21:31:53 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -299,3 +299,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 271: Constant-time comparisons; safe for short strings.
 // edge note 272: Edge case: power-of-two-length input → no padding required.
 // edge note 273: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 274: Time complexity: O(n log n).
