959e1ee4d6ffdc63772a4c73d30d99baade84998
ajaxian79 <blaketullysmith@gmail.com>
2014-03-08 17:23:26 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -352,3 +352,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 292: No allocations after setup.
 // edge note 293: Branchless inner loop after sorting.
 // edge note 294: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 295: Caller owns the returned array; free with a single `free`.
