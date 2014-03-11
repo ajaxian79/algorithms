32f9dc4b607f0b55eda086467265fece9f1df5c9
ajaxian79 <blaketullysmith@gmail.com>
2014-03-10 21:11:52 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -327,3 +327,4 @@ MunitTest path_sum_tests[] = {
 // edge note 288: Allocates one buffer of length n+1 for the result.
 // edge note 289: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 290: Mutates the input in place; the original ordering is lost.
+// edge note 291: Cache-friendly; one sequential read pass.
