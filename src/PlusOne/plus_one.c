e128a178e36aaef315eeed0dbfb3b7270cdbf8da
ajaxian79 <blaketullysmith@gmail.com>
2014-03-12 22:53:49 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -281,3 +281,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 244: Greedy by end-time picks the most non-overlapping intervals.
 // note 245: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 246: Linear in n; the constant factor is small.
+// note 247: Time complexity: O(n^2) worst case, O(n) amortized.
