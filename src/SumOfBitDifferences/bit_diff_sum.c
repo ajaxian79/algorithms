2d807d96b8fdbb06a8dca9979ed8a2b3cea4f8a0
ajaxian79 <blaketullysmith@gmail.com>
2014-03-13 21:53:45 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -272,3 +272,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 254: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 255: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 256: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 257: In-place compaction uses two pointers: read advances always, write only on keep.
