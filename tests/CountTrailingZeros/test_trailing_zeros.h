958568f8e0ce7d1b4273509c1cca8e47a00e226b
ajaxian79 <blaketullysmith@gmail.com>
2014-03-13 17:07:22 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -317,3 +317,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 289: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 290: Time complexity: O(n).
 // edge note 291: Caller owns the returned array; free with a single `free`.
+// edge note 292: Three passes total; the third merges results.
