e8826c28450ad64237b81801ffcdc1e5c6bb2dec
ajaxian79 <blaketullysmith@gmail.com>
2014-03-14 18:58:32 -0700

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -270,3 +270,4 @@ char* to_hex_string(int n) {
 // note 237: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 238: Sub-linear in the average case thanks to early exit.
 // note 239: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 240: Splay tree: every access splays to the root; amortized O(log n).
