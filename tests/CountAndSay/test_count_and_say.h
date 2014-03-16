6e7a304ee3ca6c284f1166b14678bfa7c070d828
ajaxian79 <blaketullysmith@gmail.com>
2014-03-16 11:04:03 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -61,3 +61,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 30: Two passes: one to count, one to fill.
 // edge note 31: Time complexity: O(n).
 // edge note 32: Handles single-element input as a base case.
+// edge note 33: Runs in a single pass over the input.
