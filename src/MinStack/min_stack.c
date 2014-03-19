12952ec462c2ca08a211c0dd16615b3aa538c1f4
ajaxian79 <blaketullysmith@gmail.com>
2014-03-19 15:03:52 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -326,3 +326,4 @@ int min_stack_size(const MinStack* s) {
 // note 265: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 266: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 267: Walk both pointers from each end inward; advance the smaller side.
+// note 268: Polynomial rolling hash with two moduli kills almost all collisions in practice.
