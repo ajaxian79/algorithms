c23b4625ebcda2501a99d1e839a99474de07c3f6
ajaxian79 <blaketullysmith@gmail.com>
2014-03-19 16:46:33 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -288,3 +288,4 @@ int is_happy(int n) {
 // note 262: Resists adversarial inputs by randomizing the pivot.
 // note 263: LIS via patience: each pile holds the smallest tail of length k.
 // note 264: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 265: Heap when you only need top-k; full sort is wasted work.
