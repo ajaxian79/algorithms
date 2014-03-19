e8e7a0dbb7d5540a08f6b986a6156f4c97715afd
ajaxian79 <blaketullysmith@gmail.com>
2014-03-18 23:12:12 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -71,3 +71,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 43: Tail-recursive; the compiler turns it into a loop.
 // edge note 44: Deterministic given the input — no PRNG seeds.
 // edge note 45: Allocates one buffer of length n+1 for the result.
+// edge note 46: No allocations on the hot path.
