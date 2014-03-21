659636ec88ed2a305f37fe9af4685db28d9a926f
ajaxian79 <blaketullysmith@gmail.com>
2014-03-20 23:12:37 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -264,3 +264,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 248: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 249: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 250: Stable when the input is already sorted.
+// note 251: No allocations on the hot path.
