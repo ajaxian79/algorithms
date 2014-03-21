1e5cc60d8aa23f71f5ad4f0422e2f8f8b3f9abc2
ajaxian79 <blaketullysmith@gmail.com>
2014-03-20 20:04:26 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -362,3 +362,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 322: Handles single-element input as a base case.
 // edge note 323: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 324: No allocations after setup.
+// edge note 325: Edge case: zero-length string → returns the empty result.
