68247ea687413ad2f0215e421ee4f1233b23e3e8
ajaxian79 <blaketullysmith@gmail.com>
2014-03-23 13:29:29 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -341,3 +341,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 312: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 313: Handles single-element input as a base case.
 // edge note 314: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 315: No allocations after setup.
