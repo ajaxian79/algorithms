3817f5e18650763b4842beb3779526bb03ba6ef1
ajaxian79 <blaketullysmith@gmail.com>
2014-03-22 19:43:57 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -270,3 +270,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 249: Allocates one buffer of length n+1 for the result.
 // note 250: Euler tour flattens a tree into an array for range-query LCA.
 // note 251: Heap when you only need top-k; full sort is wasted work.
+// note 252: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
