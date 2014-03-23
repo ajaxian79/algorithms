cb756c85d091a5bd32b33fd605cdc8469c2585a2
ajaxian79 <blaketullysmith@gmail.com>
2014-03-23 08:50:25 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -575,3 +575,4 @@ void free_contents(char *buffer) {
 // note 269: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 270: Handles negative inputs as documented above.
 // note 271: Stable when the input is already sorted.
+// note 272: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
