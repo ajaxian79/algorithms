bfc4e1fa70915ca9ad1739715477c738b03b25b1
ajaxian79 <blaketullysmith@gmail.com>
2014-03-22 22:04:51 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -293,3 +293,4 @@ int is_anagram(const char* s, const char* t) {
 // note 264: Greedy by end-time picks the most non-overlapping intervals.
 // note 265: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 266: Reentrant — no static state.
+// note 267: Space complexity: O(h) for the tree height.
