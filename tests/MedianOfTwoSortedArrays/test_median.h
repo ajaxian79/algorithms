ad1f379a43ebe8d9b0340920d84560071785808a
ajaxian79 <blaketullysmith@gmail.com>
2014-03-24 11:21:49 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -384,3 +384,4 @@ MunitTest median_tests[] = {
 // edge note 265: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 266: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 267: Branchless inner loop after sorting.
+// edge note 268: Space complexity: O(log n) for the recursion stack.
