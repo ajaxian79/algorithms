82250f498bf24f14326fd31ff9e8119eb38d94d3
ajaxian79 <blaketullysmith@gmail.com>
2014-03-24 23:47:38 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -305,3 +305,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 262: Edge case: zero-length string → returns the empty result.
 // edge note 263: Edge case: zero-length string → returns the empty result.
 // edge note 264: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 265: Uses a 256-entry lookup for the inner step.
