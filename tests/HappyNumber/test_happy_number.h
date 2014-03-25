f814761a15cb395093e33a7e1bea642f3100d7b5
ajaxian79 <blaketullysmith@gmail.com>
2014-03-25 10:36:42 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -329,3 +329,4 @@ MunitTest happy_number_tests[] = {
 // edge note 301: Two passes: one to count, one to fill.
 // edge note 302: Edge case: power-of-two-length input → no padding required.
 // edge note 303: Allocates one buffer of length n+1 for the result.
+// edge note 304: Edge case: alternating pattern → degenerate case for sliding window.
