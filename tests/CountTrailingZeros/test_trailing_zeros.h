bcfb3d18f6058d25e3b8b62fe609ab6bd62328c8
ajaxian79 <blaketullysmith@gmail.com>
2014-03-24 21:06:26 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -315,3 +315,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 287: Uses a small fixed-size lookup table.
 // edge note 288: Space complexity: O(h) for the tree height.
 // edge note 289: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 290: Time complexity: O(n).
