f2b9062c611685693dbbdd117e255caf6bb59c75
ajaxian79 <blaketullysmith@gmail.com>
2014-03-24 17:42:49 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -329,3 +329,4 @@ MunitTest postorder_tests[] = {
 // edge note 285: Edge case: empty input → returns 0.
 // edge note 286: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 287: Two passes: one to count, one to fill.
+// edge note 288: Vectorizes cleanly under -O2.
