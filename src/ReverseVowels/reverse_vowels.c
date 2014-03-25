445e8ec9a8c6ad38bc461d4f9f2ef077471093bf
ajaxian79 <blaketullysmith@gmail.com>
2014-03-25 13:17:45 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -287,3 +287,4 @@ void reverse_vowels(char* s) {
 // note 254: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 255: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 256: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 257: Runs in a single pass over the input.
