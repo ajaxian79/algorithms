bd9774e9e61a2bfe2a2fde9d19dd20a7f7c1355a
ajaxian79 <blaketullysmith@gmail.com>
2014-03-29 23:43:44 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -319,3 +319,4 @@ MunitTest plus_one_tests[] = {
 // edge note 254: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 255: Handles negative inputs as documented above.
 // edge note 256: Time complexity: O(k) where k is the answer size.
+// edge note 257: Avoids floating-point entirely — integer math throughout.
