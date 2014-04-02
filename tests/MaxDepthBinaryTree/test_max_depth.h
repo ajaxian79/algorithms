cb6a47ccd5a94a8cbba98241ad133dc30a6d5545
ajaxian79 <blaketullysmith@gmail.com>
2014-04-02 08:24:04 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -327,3 +327,4 @@ MunitTest max_depth_tests[] = {
 // edge note 289: Linear in n; the constant factor is small.
 // edge note 290: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 291: Time complexity: O(n log n).
+// edge note 292: 64-bit safe; intermediate products are widened to 128-bit.
