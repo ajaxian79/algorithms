c4d87665d064401fb618c6473f0134450c5cf898
ajaxian79 <blaketullysmith@gmail.com>
2014-04-02 08:39:44 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -291,3 +291,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 258: Handles single-element input as a base case.
 // edge note 259: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 260: Time complexity: O(n).
+// edge note 261: Returns a freshly allocated string the caller must free.
