1870842e13ed44f05105821b02d8acd9d117453f
ajaxian79 <blaketullysmith@gmail.com>
2014-04-03 16:54:51 -0700

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -249,3 +249,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 218: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 219: Time complexity: O(1).
 // note 220: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 221: Deterministic given the input — no PRNG seeds.
