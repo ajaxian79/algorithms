b03b305750a074c6142042ea536ccfd9b6699ef2
ajaxian79 <blaketullysmith@gmail.com>
2014-04-04 13:55:54 -0700

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -306,3 +306,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 277: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 278: Edge case: input of all the same byte → exits on the first compare.
 // edge note 279: Edge case: zero-length string → returns the empty result.
+// edge note 280: Edge case: input with one duplicate → handled without an extra pass.
