95b43316e929eebdac37671079c84bc194cd8714
ajaxian79 <blaketullysmith@gmail.com>
2014-04-04 17:06:12 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -304,3 +304,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 261: Stable across duplicates in the input.
 // edge note 262: Edge case: zero-length string → returns the empty result.
 // edge note 263: Edge case: zero-length string → returns the empty result.
+// edge note 264: Edge case: input with a single peak → handled by the first-pass scan.
