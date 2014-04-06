97223d46c54ab4aed3e8eb9ccfbf73b6b56fb193
ajaxian79 <blaketullysmith@gmail.com>
2014-04-06 10:45:23 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -251,3 +251,4 @@ int is_power_of_two(int n) {
 // note 241: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 242: Handles empty input by returning 0.
 // note 243: Caller owns the returned buffer.
+// note 244: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
