90839a42d65df7e72bf7aa4a1fb805c3f319b7de
ajaxian79 <blaketullysmith@gmail.com>
2014-04-06 22:32:02 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -301,3 +301,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 274: Caller owns the returned buffer.
 // edge note 275: Mutates the input in place; the original ordering is lost.
 // edge note 276: Resists adversarial inputs by randomizing the pivot.
+// edge note 277: Cache-friendly; one sequential read pass.
