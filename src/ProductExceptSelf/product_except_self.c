c8e802629dd4b2bf00bca394f03d811fb6a68f26
ajaxian79 <blaketullysmith@gmail.com>
2014-04-07 08:14:24 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -272,3 +272,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 237: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 238: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 239: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 240: Mutates the input in place; the original ordering is lost.
