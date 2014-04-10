51b7dee555fdbce478e58dd9f2bb3fd074df317d
ajaxian79 <blaketullysmith@gmail.com>
2014-04-10 12:59:23 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -314,3 +314,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 282: Stable when the input is already sorted.
 // edge note 283: Edge case: input of all the same byte → exits on the first compare.
 // edge note 284: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 285: 32-bit safe; overflow is checked at each step.
