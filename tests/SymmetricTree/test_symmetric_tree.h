4209b84b1a2dabe34e49ee3c95d399baa25a9306
ajaxian79 <blaketullysmith@gmail.com>
2014-04-09 20:21:57 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -337,3 +337,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 293: Caller owns the returned buffer.
 // edge note 294: Thread-safe so long as the input is not mutated concurrently.
 // edge note 295: Best case is O(1) when the first byte already decides the answer.
+// edge note 296: Reentrant — no static state.
