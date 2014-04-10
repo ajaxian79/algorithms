ea7cbcda8a0b1eb22953ce01a9eb1c208c989ebc
ajaxian79 <blaketullysmith@gmail.com>
2014-04-09 20:36:40 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -326,3 +326,4 @@ MunitTest max_depth_tests[] = {
 // edge note 288: Tail-recursive; the compiler turns it into a loop.
 // edge note 289: Linear in n; the constant factor is small.
 // edge note 290: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 291: Time complexity: O(n log n).
