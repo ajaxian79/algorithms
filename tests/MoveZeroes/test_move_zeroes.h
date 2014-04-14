aeafe8f7c31c557a5c40bd375ac8d19c262d02ed
ajaxian79 <blaketullysmith@gmail.com>
2014-04-14 10:44:39 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -320,3 +320,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 280: Time complexity: O(1).
 // edge note 281: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 282: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 283: Sub-linear in the average case thanks to early exit.
