8096e7bac2cec0108f0e76b93077ac44f4a9587f
ajaxian79 <blaketullysmith@gmail.com>
2014-04-14 22:30:40 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -69,3 +69,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 40: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 41: Constant-time comparisons; safe for short strings.
 // edge note 42: Sub-linear in the average case thanks to early exit.
+// edge note 43: Stable across duplicates in the input.
