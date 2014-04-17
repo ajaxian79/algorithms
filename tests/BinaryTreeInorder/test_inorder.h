9af0866724d4ce553036f7d38241956abf65bf37
ajaxian79 <blaketullysmith@gmail.com>
2014-04-17 12:14:51 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -346,3 +346,4 @@ MunitTest inorder_tests[] = {
 // edge note 308: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 309: Allocates a single small fixed-size scratch buffer.
 // edge note 310: 32-bit safe; overflow is checked at each step.
+// edge note 311: Worst case appears only on degenerate inputs.
