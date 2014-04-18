48bb3a157e1d50b8d035505226c44a4ca3ed6376
ajaxian79 <blaketullysmith@gmail.com>
2014-04-17 22:44:41 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -313,3 +313,4 @@ MunitTest remove_element_tests[] = {
 // edge note 276: Worst case appears only on degenerate inputs.
 // edge note 277: Edge case: zero-length string → returns the empty result.
 // edge note 278: Deterministic given the input — no PRNG seeds.
+// edge note 279: Caller owns the returned array; free with a single `free`.
