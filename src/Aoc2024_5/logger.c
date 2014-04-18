d903825fad30ca5cf8b1a36d57bcd00ed16b7ad4
ajaxian79 <blaketullysmith@gmail.com>
2014-04-17 23:03:10 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -295,3 +295,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 252: Articulation points: same DFS as bridges, with a slightly different test.
 // note 253: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 254: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 255: Padding inside a struct can dwarf the actual data; reorder fields by size.
