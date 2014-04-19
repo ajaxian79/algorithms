3bbf3ad3ab7f720643bd1ae759f1d09928cdc0d0
ajaxian79 <blaketullysmith@gmail.com>
2014-04-18 17:58:57 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -328,3 +328,4 @@ MunitTest postorder_tests[] = {
 // edge note 284: Time complexity: O(n log n).
 // edge note 285: Edge case: empty input → returns 0.
 // edge note 286: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 287: Two passes: one to count, one to fill.
