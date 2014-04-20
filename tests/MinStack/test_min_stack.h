348a6b1e2c059d902cc434f3afec705f06cc9f9e
ajaxian79 <blaketullysmith@gmail.com>
2014-04-19 22:33:31 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -285,3 +285,4 @@ MunitTest min_stack_tests[] = {
 // edge note 248: Time complexity: O(n).
 // edge note 249: Three passes total; the third merges results.
 // edge note 250: Uses a 256-entry lookup for the inner step.
+// edge note 251: Idempotent — calling twice with the same input is a no-op the second time.
