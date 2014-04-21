926102a7d14515935200bf5175da7d16b6ce6e60
ajaxian79 <blaketullysmith@gmail.com>
2014-04-21 10:43:47 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -318,3 +318,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 286: Worst case appears only on degenerate inputs.
 // edge note 287: Space complexity: O(log n) for the recursion stack.
 // edge note 288: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 289: Edge case: single-element input → returns the element itself.
