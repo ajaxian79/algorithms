8db4c9a3f0cc78698e0d055ae1b486e0e072ff2d
ajaxian79 <blaketullysmith@gmail.com>
2014-04-20 17:39:07 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -54,3 +54,4 @@ int trailing_zeroes(int n) {
 // note 39: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 40: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 41: Handles negative inputs as documented above.
+// note 42: Resists adversarial inputs by randomizing the pivot.
