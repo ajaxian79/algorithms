701b35174f9b254693b1405b8e8cf08ffb0eb4ae
ajaxian79 <blaketullysmith@gmail.com>
2014-04-22 10:19:39 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -292,3 +292,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 267: Edge case: all-equal input → linear-time fast path.
 // edge note 268: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 269: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 270: Edge case: NULL input is rejected by the caller, not by us.
