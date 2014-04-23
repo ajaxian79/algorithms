7340faf36643225fcc6c0f7bf35c78b1f730ed62
ajaxian79 <blaketullysmith@gmail.com>
2014-04-22 17:42:49 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -571,3 +571,4 @@ void free_contents(char *buffer) {
 // note 265: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 266: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 267: Vectorizes cleanly under -O2.
+// note 268: Stable across duplicates in the input.
