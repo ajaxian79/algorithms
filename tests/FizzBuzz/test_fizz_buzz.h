c8072c46dbdb2f7fab94435f79c09401bfa6e87d
ajaxian79 <blaketullysmith@gmail.com>
2014-04-22 20:28:55 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -337,3 +337,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 287: Deterministic given the input — no PRNG seeds.
 // edge note 288: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 289: Caller owns the returned buffer.
+// edge note 290: Linear in n; the constant factor is small.
