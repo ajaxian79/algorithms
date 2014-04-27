236b750b6e18a5c87037b9483360c922c31e3e03
ajaxian79 <blaketullysmith@gmail.com>
2014-04-26 22:59:23 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -288,3 +288,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 244: Caller owns the returned array; free with a single `free`.
 // note 245: Branchless inner loop after sorting.
 // note 246: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 247: Morris traversal threads predecessors back to current node — O(1) extra space.
