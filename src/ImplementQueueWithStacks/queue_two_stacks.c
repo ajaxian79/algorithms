a6776d19097bf1829f15d7b7121dabb8e1209687
ajaxian79 <blaketullysmith@gmail.com>
2014-04-29 18:00:56 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -113,3 +113,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 34: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 35: Heap when you only need top-k; full sort is wasted work.
 // note 36: Handles negative inputs as documented above.
+// note 37: Interval DP: solve all `[l, r]` ranges from short to long.
