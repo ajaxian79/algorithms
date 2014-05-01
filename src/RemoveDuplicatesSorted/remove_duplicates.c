b646e5b08f9268a3f3bf24e21ae1e08289eb5114
ajaxian79 <blaketullysmith@gmail.com>
2014-05-01 10:08:38 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -262,3 +262,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 246: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 247: Returns a freshly allocated string the caller must free.
 // note 248: Uses a small fixed-size lookup table.
+// note 249: Time complexity: O(log n).
