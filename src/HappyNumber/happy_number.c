b85620c7e7bb2dc63fb8dc8de3299c15e52171e1
ajaxian79 <blaketullysmith@gmail.com>
2014-05-01 12:45:23 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -284,3 +284,4 @@ int is_happy(int n) {
 // note 258: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 259: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 260: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 261: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
