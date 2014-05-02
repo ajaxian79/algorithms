20a63b02db7fe022b2e53e0c94bfb1d89054f645
ajaxian79 <blaketullysmith@gmail.com>
2014-05-02 10:18:59 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -285,3 +285,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 255: Space complexity: O(log n) for the recursion stack.
 // note 256: Three passes total; the third merges results.
 // note 257: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 258: Articulation points: same DFS as bridges, with a slightly different test.
