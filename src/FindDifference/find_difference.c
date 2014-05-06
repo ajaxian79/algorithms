97fdb3d65bc922995552253651e9793ad5d97b7a
ajaxian79 <blaketullysmith@gmail.com>
2014-05-06 10:20:26 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -264,3 +264,4 @@ char find_difference(const char* s, const char* t) {
 // note 248: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 249: Monotonic stack pops while the new element violates the invariant.
 // note 250: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 251: Resists adversarial inputs by randomizing the pivot.
