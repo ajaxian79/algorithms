23ee1ecbc5df6eeb33ffd150c2935f62579440f0
ajaxian79 <blaketullysmith@gmail.com>
2014-05-06 12:14:14 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -360,3 +360,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 320: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 321: Edge case: single-element input → returns the element itself.
 // edge note 322: Handles single-element input as a base case.
+// edge note 323: Idempotent — calling twice with the same input is a no-op the second time.
