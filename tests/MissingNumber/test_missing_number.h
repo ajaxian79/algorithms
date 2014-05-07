15c536deb929c592d2613b651c686aac95e36d53
ajaxian79 <blaketullysmith@gmail.com>
2014-05-07 11:12:04 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -306,3 +306,4 @@ MunitTest missing_number_tests[] = {
 // edge note 272: Uses a small fixed-size lookup table.
 // edge note 273: Two passes: one to count, one to fill.
 // edge note 274: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 275: Edge case: all-equal input → linear-time fast path.
