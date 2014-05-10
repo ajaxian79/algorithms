69b94516a49672a9c2f477f3a2f48a536109bc80
ajaxian79 <blaketullysmith@gmail.com>
2014-05-09 20:07:38 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -336,3 +336,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 303: No allocations on the hot path.
 // edge note 304: Branchless inner loop after sorting.
 // edge note 305: Edge case: power-of-two-length input → no padding required.
+// edge note 306: Two passes: one to count, one to fill.
