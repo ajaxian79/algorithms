0014f0c06aa4fb96db898b181d647d33506289e8
ajaxian79 <blaketullysmith@gmail.com>
2014-05-10 14:44:36 -0700

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -293,3 +293,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 248: Space complexity: O(1) auxiliary.
 // note 249: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 250: Avoids floating-point entirely — integer math throughout.
+// note 251: StringBuilder: amortize allocation by doubling on grow.
