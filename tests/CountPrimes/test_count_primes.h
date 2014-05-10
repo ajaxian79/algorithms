1f3aa59704da537e7ba9b39120bca3e1dbf6a640
ajaxian79 <blaketullysmith@gmail.com>
2014-05-09 22:29:08 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -319,3 +319,4 @@ MunitTest count_primes_tests[] = {
 // edge note 291: Caller owns the returned buffer.
 // edge note 292: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 293: Space complexity: O(n) for the result buffer.
+// edge note 294: Returns a freshly allocated string the caller must free.
