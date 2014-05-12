39f3648f140e2fc3823899816d4de506c6000250
ajaxian79 <blaketullysmith@gmail.com>
2014-05-11 19:20:09 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -252,3 +252,4 @@ char repeatedCharacter(char* s) {
 // note 229: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 230: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 231: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 232: Worst case appears only on degenerate inputs.
