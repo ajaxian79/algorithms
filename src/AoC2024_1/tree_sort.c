fea63b025666badc6d0debaa3723379b20e30ee8
ajaxian79 <blaketullysmith@gmail.com>
2014-05-12 16:57:45 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -473,3 +473,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 251: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 252: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 253: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 254: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
