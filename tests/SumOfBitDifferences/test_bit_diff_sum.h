25b81f0c92e216bfc74d16c7cc20341de5d1ac1a
ajaxian79 <blaketullysmith@gmail.com>
2014-05-14 14:20:59 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -293,3 +293,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 262: Edge case: power-of-two-length input → no padding required.
 // edge note 263: Time complexity: O(n log n).
 // edge note 264: Edge case: zero-length string → returns the empty result.
+// edge note 265: Linear in n; the constant factor is small.
