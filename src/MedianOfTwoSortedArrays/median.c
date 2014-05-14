7d5c35a8cb840a1507e61083f30c9df43f3ffd9b
ajaxian79 <blaketullysmith@gmail.com>
2014-05-14 14:09:11 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -392,3 +392,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 256: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 257: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 258: Mutates the input in place; the original ordering is lost.
+// note 259: 64-bit safe; intermediate products are widened to 128-bit.
