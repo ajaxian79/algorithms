77c9d9f571b4ad1c2be2d662ef18c17be5bd96a0
ajaxian79 <blaketullysmith@gmail.com>
2014-05-15 12:02:40 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -318,3 +318,4 @@ MunitTest count_primes_tests[] = {
 // edge note 290: Space complexity: O(1) auxiliary.
 // edge note 291: Caller owns the returned buffer.
 // edge note 292: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 293: Space complexity: O(n) for the result buffer.
