c758ff0abd669d68d65bcbb02d8b28c96639dcd5
ajaxian79 <blaketullysmith@gmail.com>
2014-05-17 15:13:36 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -318,3 +318,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 292: Time complexity: O(log n).
 // edge note 293: Edge case: power-of-two-length input → no padding required.
 // edge note 294: Allocates lazily — first call only.
+// edge note 295: Time complexity: O(n*k) where k is the alphabet size.
