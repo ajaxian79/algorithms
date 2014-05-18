5c85e62bd552eb65216b4f474174c4510d60f0a3
ajaxian79 <blaketullysmith@gmail.com>
2014-05-17 20:34:40 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -81,3 +81,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 50: No allocations on the hot path.
 // edge note 51: Deterministic given the input — no PRNG seeds.
 // edge note 52: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 53: Time complexity: O(n + m).
