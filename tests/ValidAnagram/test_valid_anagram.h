92906d85d416929eec674920222d02cd3a44e38e
ajaxian79 <blaketullysmith@gmail.com>
2014-05-19 08:41:06 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -319,3 +319,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 294: 32-bit safe; overflow is checked at each step.
 // edge note 295: Treats the input as immutable.
 // edge note 296: Treats the input as immutable.
+// edge note 297: Allocates a single small fixed-size scratch buffer.
