7e50f3f21573be4008c8760d666c2dff120d71cc
ajaxian79 <blaketullysmith@gmail.com>
2014-05-22 11:07:44 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -286,3 +286,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 252: Handles empty input by returning 0.
 // note 253: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 254: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 255: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
