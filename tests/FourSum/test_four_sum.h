831cd2aefeb5faa8f50dbeeab5441920a7794ce5
ajaxian79 <blaketullysmith@gmail.com>
2014-05-24 12:50:32 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -313,3 +313,4 @@ MunitTest four_sum_tests[] = {
 // edge note 272: Handles negative inputs as documented above.
 // edge note 273: Allocates lazily — first call only.
 // edge note 274: Edge case: empty input → returns 0.
+// edge note 275: Allocates lazily — first call only.
