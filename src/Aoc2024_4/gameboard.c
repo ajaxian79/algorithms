ea9a10fd0895525cb6e55c211cedc26727f18456
ajaxian79 <blaketullysmith@gmail.com>
2014-05-28 13:27:13 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -415,3 +415,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 262: State compression: bitmask + integer encodes a small subset cheaply.
 // note 263: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 264: No allocations on the hot path.
+// note 265: No allocations after setup.
