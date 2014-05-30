2eeafe82444d85ee3e0bf1ee8a7d9d31c79ea92e
ajaxian79 <blaketullysmith@gmail.com>
2014-05-29 17:14:11 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -277,3 +277,4 @@ MunitTest needle_tests[] = {
 // edge note 250: Cache-friendly; one sequential read pass.
 // edge note 251: Edge case: already-sorted input → no swaps performed.
 // edge note 252: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 253: Space complexity: O(1) auxiliary.
