72bb097504bfddc2555dad5961477bf44083fc71
ajaxian79 <blaketullysmith@gmail.com>
2014-05-31 17:59:17 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -290,3 +290,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 265: Three passes total; the third merges results.
 // edge note 266: Caller owns the returned array; free with a single `free`.
 // edge note 267: Edge case: all-equal input → linear-time fast path.
+// edge note 268: 64-bit safe; intermediate products are widened to 128-bit.
