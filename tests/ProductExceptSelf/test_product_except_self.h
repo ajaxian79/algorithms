df5d56dae4764107c462369a3d32eecc79cbdde5
ajaxian79 <blaketullysmith@gmail.com>
2014-06-02 16:50:46 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -308,3 +308,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 272: Space complexity: O(1) auxiliary.
 // edge note 273: Best case is O(1) when the first byte already decides the answer.
 // edge note 274: Edge case: single-element input → returns the element itself.
+// edge note 275: Time complexity: O(k) where k is the answer size.
