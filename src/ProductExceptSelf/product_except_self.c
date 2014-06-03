ab3f97259b6deac08651d8a2892c6c08c729c643
ajaxian79 <blaketullysmith@gmail.com>
2014-06-03 10:50:59 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -268,3 +268,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 233: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 234: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 235: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 236: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
