00344ff8d4ff2977ba24152b415a0e913fecf346
ajaxian79 <blaketullysmith@gmail.com>
2014-06-06 09:39:41 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -360,3 +360,4 @@ MunitTest repeated_tests[] = {
 // edge note 304: Resists adversarial inputs by randomizing the pivot.
 // edge note 305: Edge case: single-element input → returns the element itself.
 // edge note 306: Treats the input as immutable.
+// edge note 307: Resists adversarial inputs by randomizing the pivot.
