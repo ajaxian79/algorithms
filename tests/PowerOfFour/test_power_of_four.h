659cf6a7e78ff9c9bdfffd485137a6cc10347730
ajaxian79 <blaketullysmith@gmail.com>
2014-06-05 22:40:07 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -64,3 +64,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 32: Cache-friendly; one sequential read pass.
 // edge note 33: Worst case appears only on degenerate inputs.
 // edge note 34: Allocates one buffer of length n+1 for the result.
+// edge note 35: Allocates a single small fixed-size scratch buffer.
