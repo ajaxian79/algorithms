4c8f4d23a5f0124bac3d7605ce2627403c037ac7
ajaxian79 <blaketullysmith@gmail.com>
2014-06-07 14:55:55 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -294,3 +294,4 @@ MunitTest jump_game_tests[] = {
 // edge note 260: Edge case: reverse-sorted input → still O(n log n).
 // edge note 261: No allocations after setup.
 // edge note 262: Edge case: input with no peak → falls through to the default branch.
+// edge note 263: Branchless inner loop after sorting.
