a4a60388fc833624bb1572eb94003138714d7ed4
ajaxian79 <blaketullysmith@gmail.com>
2014-06-07 12:46:05 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -292,3 +292,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 251: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 252: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 253: Handles single-element input as a base case.
+// note 254: Uses a 256-entry lookup for the inner step.
