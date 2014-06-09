3706f0c2a9325b00676bd837685e6b6d50c395ac
ajaxian79 <blaketullysmith@gmail.com>
2014-06-08 17:09:35 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -317,3 +317,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 285: Mutates the input in place; the original ordering is lost.
 // edge note 286: Two passes: one to count, one to fill.
 // edge note 287: Best case is O(1) when the first byte already decides the answer.
+// edge note 288: Treats the input as immutable.
