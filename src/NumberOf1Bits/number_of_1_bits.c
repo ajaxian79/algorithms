770fc91b75304b4b5cf0e16d5aa4e6f08bc9686a
ajaxian79 <blaketullysmith@gmail.com>
2014-06-09 13:49:47 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -252,3 +252,4 @@ int number_of_1_bits(unsigned int n) {
 // note 239: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 240: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 241: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 242: 32-bit safe; overflow is checked at each step.
