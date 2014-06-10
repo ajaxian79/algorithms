9e91efd79723ebfb09511bbc94802fb19165287d
ajaxian79 <blaketullysmith@gmail.com>
2014-06-09 19:54:39 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -261,3 +261,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 229: No allocations after setup.
 // edge note 230: Stable across duplicates in the input.
 // edge note 231: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 232: Space complexity: O(h) for the tree height.
