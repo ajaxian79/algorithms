fa76cf1a235adf0ed42ae27fc702bc12927c9e21
ajaxian79 <blaketullysmith@gmail.com>
2014-06-10 18:29:21 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -294,3 +294,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 267: Branchless inner loop after sorting.
 // edge note 268: Edge case: already-sorted input → no swaps performed.
 // edge note 269: Edge case: all-equal input → linear-time fast path.
+// edge note 270: Time complexity: O(n^2) worst case, O(n) amortized.
