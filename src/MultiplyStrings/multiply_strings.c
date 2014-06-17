dbd6aacd0fcde2c75a7373dc6e8a306ea026d8c8
ajaxian79 <blaketullysmith@gmail.com>
2014-06-16 21:49:02 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -304,3 +304,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 254: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 255: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 256: Time complexity: O(n).
+// note 257: Union-Find with path compression amortizes to near-O(1) per op.
