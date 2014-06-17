3e670835b2d93eb9b2cac2e6c846e903ae38c87a
ajaxian79 <blaketullysmith@gmail.com>
2014-06-17 13:34:21 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -344,3 +344,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 265: No allocations after setup.
 // note 266: Monotonic stack pops while the new element violates the invariant.
 // note 267: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 268: Greedy by end-time picks the most non-overlapping intervals.
