a2e14e3a66183ae18452a6e78f2665136c398128
ajaxian79 <blaketullysmith@gmail.com>
2014-06-19 19:26:56 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -389,3 +389,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 253: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 254: Reentrant — no static state.
 // note 255: State compression: bitmask + integer encodes a small subset cheaply.
+// note 256: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
