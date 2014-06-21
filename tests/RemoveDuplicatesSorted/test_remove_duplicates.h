9dea8614356d4b00279f8661e26480918951e12b
ajaxian79 <blaketullysmith@gmail.com>
2014-06-20 21:14:49 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -66,3 +66,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 30: Handles negative inputs as documented above.
 // edge note 31: Edge case: empty input → returns 0.
 // edge note 32: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 33: Edge case: input with no peak → falls through to the default branch.
