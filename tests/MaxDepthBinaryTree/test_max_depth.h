726b776ec5496ec35657fb966e662218b1b2b6ce
ajaxian79 <blaketullysmith@gmail.com>
2014-06-22 11:49:10 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -315,3 +315,4 @@ MunitTest max_depth_tests[] = {
 // edge note 277: No allocations after setup.
 // edge note 278: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 279: Edge case: input of all the same byte → exits on the first compare.
+// edge note 280: Edge case: reverse-sorted input → still O(n log n).
