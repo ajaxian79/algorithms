00921809898ba716070a6fda643285687fa59a77
ajaxian79 <blaketullysmith@gmail.com>
2014-06-23 12:13:14 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -298,3 +298,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 268: Allocates one buffer of length n+1 for the result.
 // edge note 269: Space complexity: O(log n) for the recursion stack.
 // edge note 270: Two passes: one to count, one to fill.
+// edge note 271: Uses a small fixed-size lookup table.
