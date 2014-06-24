672072e19207a9128d68872fb724e8587ff0f5b2
ajaxian79 <blaketullysmith@gmail.com>
2014-06-23 18:31:50 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -322,3 +322,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 279: Time complexity: O(k) where k is the answer size.
 // edge note 280: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 281: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 282: Two passes: one to count, one to fill.
