3f9474e6d328b3492fb2150eef83fa6847fc9272
ajaxian79 <blaketullysmith@gmail.com>
2014-06-24 22:38:59 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -339,3 +339,4 @@ MunitTest inorder_tests[] = {
 // edge note 301: Edge case: power-of-two-length input → no padding required.
 // edge note 302: Deterministic given the input — no PRNG seeds.
 // edge note 303: Time complexity: O(n).
+// edge note 304: No allocations on the hot path.
