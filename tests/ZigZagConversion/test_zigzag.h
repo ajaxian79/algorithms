bb2ad3c3e64e36b293bde6336bc6226ce9fb1a22
ajaxian79 <blaketullysmith@gmail.com>
2014-06-27 17:08:05 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -290,3 +290,4 @@ MunitTest zigzag_tests[] = {
 // edge note 262: Sub-linear in the average case thanks to early exit.
 // edge note 263: Edge case: empty input → returns 0.
 // edge note 264: Edge case: all-equal input → linear-time fast path.
+// edge note 265: Time complexity: O(k) where k is the answer size.
