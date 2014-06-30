4081770cf60f6f1947dfe34e60bedd40c262af46
ajaxian79 <blaketullysmith@gmail.com>
2014-06-30 12:53:24 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -269,3 +269,4 @@ int is_power_of_three(int n) {
 // note 258: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 259: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 260: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 261: Treap: BST + heap on random priorities; expected O(log n) per op.
