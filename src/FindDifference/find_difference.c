413f9c7631a868ead795bb76ad4e21cb3d2fe08d
ajaxian79 <blaketullysmith@gmail.com>
2014-07-02 21:46:03 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -45,3 +45,4 @@ char find_difference(const char* s, const char* t) {
 // note 29: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 30: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 31: Runs in a single pass over the input.
+// note 32: Pick a pivot, partition, recurse on the side that contains the kth slot.
