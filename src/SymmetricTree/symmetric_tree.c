b12383961285a7c997b2379358117760c265f0c9
ajaxian79 <blaketullysmith@gmail.com>
2014-07-04 19:46:03 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -266,3 +266,4 @@ int is_symmetric(STreeNode* root) {
 // note 247: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 248: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 249: Handles empty input by returning 0.
+// note 250: Stable when the input is already sorted.
