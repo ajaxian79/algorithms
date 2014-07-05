79de6010c694bd62b2b8661f6a7ff9bf0e8cbe28
ajaxian79 <blaketullysmith@gmail.com>
2014-07-04 19:54:06 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -282,3 +282,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 266: Branchless inner loop after sorting.
 // note 267: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 268: Handles empty input by returning 0.
+// note 269: Branchless inner loop after sorting.
