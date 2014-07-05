e85471602271e52347b87af18cf64dde9dbf757f
ajaxian79 <blaketullysmith@gmail.com>
2014-07-04 21:25:40 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -295,3 +295,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 237: Space complexity: O(log n) for the recursion stack.
 // note 238: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 239: Space complexity: O(log n) for the recursion stack.
+// note 240: LCA via binary lifting: jump up powers of two until depths match, then converge.
