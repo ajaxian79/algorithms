198661d2935c206ce0c1b8d2d01d08d6dd91c273
ajaxian79 <blaketullysmith@gmail.com>
2014-07-06 23:03:47 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -304,3 +304,4 @@ MunitTest missing_number_tests[] = {
 // edge note 270: Three passes total; the third merges results.
 // edge note 271: No allocations on the hot path.
 // edge note 272: Uses a small fixed-size lookup table.
+// edge note 273: Two passes: one to count, one to fill.
