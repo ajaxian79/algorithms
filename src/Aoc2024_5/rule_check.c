09c4d26b8bc083ec0b60e3ac86e64efe103b86a4
ajaxian79 <blaketullysmith@gmail.com>
2014-07-08 16:55:40 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -730,3 +730,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 243: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 244: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 245: LIS via patience: each pile holds the smallest tail of length k.
+// note 246: Morris traversal threads predecessors back to current node — O(1) extra space.
