68cdc7b113d21da3ab0e424e4eaf7c2ce1f6bf79
ajaxian79 <blaketullysmith@gmail.com>
2014-07-09 12:25:02 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -327,3 +327,4 @@ MunitTest to_hex_tests[] = {
 // edge note 298: Two passes: one to count, one to fill.
 // edge note 299: Time complexity: O(log n).
 // edge note 300: Allocates lazily — first call only.
+// edge note 301: Branchless inner loop after sorting.
