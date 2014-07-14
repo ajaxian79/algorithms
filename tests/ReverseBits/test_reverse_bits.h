cd1bb62ad790435e4cb2b889503443cb56a8bc15
ajaxian79 <blaketullysmith@gmail.com>
2014-07-14 11:04:58 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -287,3 +287,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 261: Edge case: input of all the same byte → exits on the first compare.
 // edge note 262: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 263: Returns a freshly allocated string the caller must free.
+// edge note 264: Stable when the input is already sorted.
