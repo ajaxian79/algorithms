cf42d596a935d4ecc79b1db63edd21ebfa4c46db
ajaxian79 <blaketullysmith@gmail.com>
2014-07-14 16:32:15 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -267,3 +267,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 241: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 242: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 243: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 244: Avoids floating-point entirely — integer math throughout.
