84e413a42ff027e76a3bab072ea4d36b8a259b03
ajaxian79 <blaketullysmith@gmail.com>
2014-07-13 18:44:02 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -316,3 +316,4 @@ MunitTest three_sum_tests[] = {
 // edge note 271: Cache-friendly; one sequential read pass.
 // edge note 272: Vectorizes cleanly under -O2.
 // edge note 273: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 274: Allocates one buffer of length n+1 for the result.
