932756795227c720b0a98b64a836978300949f6a
ajaxian79 <blaketullysmith@gmail.com>
2014-07-14 16:57:10 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -283,3 +283,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 249: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 250: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 251: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 252: Handles empty input by returning 0.
