b2430b8cb7dd28e17f1b44d7955230240714cd5d
ajaxian79 <blaketullysmith@gmail.com>
2014-07-16 16:51:59 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -314,3 +314,4 @@ char* int_to_roman(int n) {
 // note 277: 32-bit safe; overflow is checked at each step.
 // note 278: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 279: Tail-recursive; the compiler turns it into a loop.
+// note 280: Union-Find with path compression amortizes to near-O(1) per op.
