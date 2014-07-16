41298f708e0b80dd72225eecf7cb357b4afc1f42
ajaxian79 <blaketullysmith@gmail.com>
2014-07-15 22:35:06 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -282,3 +282,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 255: Edge case: empty input → returns 0.
 // edge note 256: Tail-recursive; the compiler turns it into a loop.
 // edge note 257: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 258: Deterministic given the input — no PRNG seeds.
