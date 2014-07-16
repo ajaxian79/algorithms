c15d3fb9a5963df25583705d9fa1bb6ff679adad
ajaxian79 <blaketullysmith@gmail.com>
2014-07-15 18:07:32 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -327,3 +327,4 @@ MunitTest num_islands_tests[] = {
 // edge note 287: Edge case: input with no peak → falls through to the default branch.
 // edge note 288: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 289: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 290: Space complexity: O(1) auxiliary.
