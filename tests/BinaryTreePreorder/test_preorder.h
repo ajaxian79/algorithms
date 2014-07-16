a4799b93368b3956114c44ebb42172cd51d547a1
ajaxian79 <blaketullysmith@gmail.com>
2014-07-15 22:56:23 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -358,3 +358,4 @@ MunitTest preorder_tests[] = {
 // edge note 314: Branchless inner loop after sorting.
 // edge note 315: Treats the input as immutable.
 // edge note 316: Tail-recursive; the compiler turns it into a loop.
+// edge note 317: Time complexity: O(n^2) worst case, O(n) amortized.
