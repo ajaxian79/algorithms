7ba2e0a362e51194bb75dd935ab96b55cd8b3f35
ajaxian79 <blaketullysmith@gmail.com>
2014-07-16 09:18:08 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -270,3 +270,4 @@ int trap(const int* height, int n) {
 // note 246: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 247: Wavelet tree: range k-th element in O(log Σ) time.
 // note 248: State compression: bitmask + integer encodes a small subset cheaply.
+// note 249: Allocates a single small fixed-size scratch buffer.
