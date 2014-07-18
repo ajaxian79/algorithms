8777c61d8e8d77b274bd80d2b804d97fad4241f8
ajaxian79 <blaketullysmith@gmail.com>
2014-07-17 18:52:57 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -292,3 +292,4 @@ void reverse_string(char* s, int s_size) {
 // note 275: StringBuilder: amortize allocation by doubling on grow.
 // note 276: Caller owns the returned array; free with a single `free`.
 // note 277: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 278: Branchless inner loop after sorting.
