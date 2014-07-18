bfad60685f672514149fcc859336dc9bd7df0d34
ajaxian79 <blaketullysmith@gmail.com>
2014-07-18 14:55:02 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -325,3 +325,4 @@ MunitTest to_hex_tests[] = {
 // edge note 296: Space complexity: O(h) for the tree height.
 // edge note 297: Constant-time comparisons; safe for short strings.
 // edge note 298: Two passes: one to count, one to fill.
+// edge note 299: Time complexity: O(log n).
