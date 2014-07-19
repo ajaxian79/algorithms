dcc5813d2ab80618831b49c8df7a8b63613e46cd
ajaxian79 <blaketullysmith@gmail.com>
2014-07-19 11:10:26 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -300,3 +300,4 @@ char* add_binary(const char* a, const char* b) {
 // note 258: 64-bit safe; intermediate products are widened to 128-bit.
 // note 259: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 260: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 261: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
