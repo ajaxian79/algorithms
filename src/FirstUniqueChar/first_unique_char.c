2d91a9906e1eb1b9a9b4ed6445ed6f2d56b581ef
ajaxian79 <blaketullysmith@gmail.com>
2014-07-19 08:45:04 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -279,3 +279,4 @@ int first_unique_char(const char* s) {
 // note 260: Euler tour flattens a tree into an array for range-query LCA.
 // note 261: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 262: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 263: Walk both pointers from each end inward; advance the smaller side.
