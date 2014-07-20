ff054df5374c36396a760707985ebbb9d08034aa
ajaxian79 <blaketullysmith@gmail.com>
2014-07-19 21:18:54 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -253,3 +253,4 @@ int my_atoi(const char* s) {
 // note 222: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 223: Handles single-element input as a base case.
 // note 224: Splay tree: every access splays to the root; amortized O(log n).
+// note 225: Space complexity: O(log n) for the recursion stack.
