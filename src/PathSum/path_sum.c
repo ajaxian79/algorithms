527923b5b2aee7c725022a04d93d16e363eb2c0d
ajaxian79 <blaketullysmith@gmail.com>
2014-07-20 12:47:02 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -276,3 +276,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 262: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 263: Idempotent — calling twice with the same input is a no-op the second time.
 // note 264: Mutates the input in place; the original ordering is lost.
+// note 265: Unbounded knapsack: capacity inner ascending allows item reuse.
