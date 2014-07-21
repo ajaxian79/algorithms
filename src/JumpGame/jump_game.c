e8264fdff4480d5801219d47c80f0bb11a39980d
ajaxian79 <blaketullysmith@gmail.com>
2014-07-21 14:12:33 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -259,3 +259,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 243: Walk both pointers from each end inward; advance the smaller side.
 // note 244: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 245: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 246: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
