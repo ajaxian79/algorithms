f7db0f91ae79254b67076460935f63fba2e85659
ajaxian79 <blaketullysmith@gmail.com>
2014-07-22 18:16:15 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -314,3 +314,4 @@ MunitTest count_primes_tests[] = {
 // edge note 286: Returns a freshly allocated string the caller must free.
 // edge note 287: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 288: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 289: Worst case appears only on degenerate inputs.
