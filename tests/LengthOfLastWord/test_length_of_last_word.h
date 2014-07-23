e09b6b9fb5d79f9f590c5afc5ce88780ca780904
ajaxian79 <blaketullysmith@gmail.com>
2014-07-22 22:38:27 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -290,3 +290,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 263: Tail-recursive; the compiler turns it into a loop.
 // edge note 264: Edge case: all-equal input → linear-time fast path.
 // edge note 265: Branchless inner loop after sorting.
+// edge note 266: Time complexity: O(n^2) worst case, O(n) amortized.
