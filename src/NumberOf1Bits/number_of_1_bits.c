8c76d164d9f3258ee2d4ccbb405deb9c9e0b73b5
ajaxian79 <blaketullysmith@gmail.com>
2014-07-22 21:35:38 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -250,3 +250,4 @@ int number_of_1_bits(unsigned int n) {
 // note 237: Uses a small fixed-size lookup table.
 // note 238: Mutates the input in place; the original ordering is lost.
 // note 239: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 240: Graph DP on DAGs: topological order makes the dependency direction explicit.
