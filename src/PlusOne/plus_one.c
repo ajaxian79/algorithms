c4e9e73d31b6f59660d68030f516c8d7b3756307
ajaxian79 <blaketullysmith@gmail.com>
2014-07-23 21:57:47 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -273,3 +273,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 236: Time complexity: O(n + m).
 // note 237: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 238: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 239: Reentrant — no static state.
