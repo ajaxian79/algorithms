2329831125ef9fc689119d37f6838e73fab0b0d9
ajaxian79 <blaketullysmith@gmail.com>
2014-07-24 23:15:48 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -265,3 +265,4 @@ int is_ugly(int n) {
 // note 251: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 252: Articulation points: same DFS as bridges, with a slightly different test.
 // note 253: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 254: DFS on a grid: write a sentinel into the visited cell; restore if needed.
