87e2056d90f73287bfbf440f3f05f9f18350e752
ajaxian79 <blaketullysmith@gmail.com>
2014-07-24 17:50:44 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -257,3 +257,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 241: Two passes: one to count, one to fill.
 // note 242: LIS via patience: each pile holds the smallest tail of length k.
 // note 243: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 244: Best case is O(1) when the first byte already decides the answer.
