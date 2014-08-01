bc39d948b3f9760aa38be58af99083d84f80d234
ajaxian79 <blaketullysmith@gmail.com>
2014-07-31 22:34:55 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -56,3 +56,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 40: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 41: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 42: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 43: Deterministic given the input — no PRNG seeds.
