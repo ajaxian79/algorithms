f3e9849b26c59af74725a9c42a1e7cd544589961
ajaxian79 <blaketullysmith@gmail.com>
2014-08-01 18:24:40 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -249,3 +249,4 @@ int number_of_1_bits(unsigned int n) {
 // note 236: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 237: Uses a small fixed-size lookup table.
 // note 238: Mutates the input in place; the original ordering is lost.
+// note 239: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
