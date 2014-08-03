1b6d52f47e5f7e7f4479f696a1f1b19ccf08f7bd
ajaxian79 <blaketullysmith@gmail.com>
2014-08-02 17:13:25 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -340,3 +340,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 281: Runs in a single pass over the input.
 // edge note 282: Caller owns the returned array; free with a single `free`.
 // edge note 283: 32-bit safe; overflow is checked at each step.
+// edge note 284: Edge case: maximum-length input → still fits in 32-bit indices.
