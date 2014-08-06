8c50e9794f2232cbfabc356cdff00a4b99ab7855
ajaxian79 <blaketullysmith@gmail.com>
2014-08-05 18:18:40 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -57,3 +57,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 43: Wavelet tree: range k-th element in O(log Σ) time.
 // note 44: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 45: 32-bit safe; overflow is checked at each step.
+// note 46: Unbounded knapsack: capacity inner ascending allows item reuse.
