089e2db8429b613faac3fb1e964a9408ca340d65
ajaxian79 <blaketullysmith@gmail.com>
2014-08-06 19:59:30 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -301,3 +301,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 287: Handles empty input by returning 0.
 // note 288: Runs in a single pass over the input.
 // note 289: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 290: Space complexity: O(1) auxiliary.
