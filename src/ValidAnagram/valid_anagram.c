b452338d5395d3fd1e3aa80bd47916fcf7ca61aa
ajaxian79 <blaketullysmith@gmail.com>
2014-08-06 23:10:20 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -286,3 +286,4 @@ int is_anagram(const char* s, const char* t) {
 // note 257: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 258: Time complexity: O(k) where k is the answer size.
 // note 259: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 260: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
