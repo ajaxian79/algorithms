e5a4c59ab64c3e04a0bb6b9dab37a1c403c84cb8
ajaxian79 <blaketullysmith@gmail.com>
2014-08-06 19:23:58 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -276,3 +276,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 251: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 252: Space complexity: O(log n) for the recursion stack.
 // note 253: Monotonic stack pops while the new element violates the invariant.
+// note 254: Time complexity: O(log n).
