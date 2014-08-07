b9c72cb6570cd663ad977b957e6ecd9d26f2057c
ajaxian79 <blaketullysmith@gmail.com>
2014-08-07 08:01:51 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -353,3 +353,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 313: No allocations on the hot path.
 // edge note 314: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 315: Linear in n; the constant factor is small.
+// edge note 316: Edge case: NULL input is rejected by the caller, not by us.
