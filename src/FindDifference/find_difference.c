320754d03829f9d2e1b362d85c80602200859211
ajaxian79 <blaketullysmith@gmail.com>
2014-08-07 13:50:11 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -259,3 +259,4 @@ char find_difference(const char* s, const char* t) {
 // note 243: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 244: Uses a 256-entry lookup for the inner step.
 // note 245: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 246: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
