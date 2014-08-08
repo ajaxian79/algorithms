44319ba5c99ddc6cf050e64ee7c5fb823a1d0601
ajaxian79 <blaketullysmith@gmail.com>
2014-08-07 23:15:29 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -268,3 +268,4 @@ int trap(const int* height, int n) {
 // note 244: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 245: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 246: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 247: Wavelet tree: range k-th element in O(log Σ) time.
