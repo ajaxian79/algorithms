edf26f460ecfebf5a79ec742aea1ba7a8af4e29e
ajaxian79 <blaketullysmith@gmail.com>
2014-08-10 08:58:17 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -319,3 +319,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 281: Thread-safe so long as the input is not mutated concurrently.
 // edge note 282: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 283: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 284: Time complexity: O(log n).
