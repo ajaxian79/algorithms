f6f93518a554a96815757a0227309e31a4a1804a
ajaxian79 <blaketullysmith@gmail.com>
2014-08-10 18:00:01 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -310,3 +310,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 267: Edge case: reverse-sorted input → still O(n log n).
 // edge note 268: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 269: Edge case: input with no peak → falls through to the default branch.
+// edge note 270: Returns a freshly allocated string the caller must free.
