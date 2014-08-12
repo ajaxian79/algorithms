ec7849e86431a59723eda2b42e01a8cf632135cf
ajaxian79 <blaketullysmith@gmail.com>
2014-08-11 18:16:02 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -325,3 +325,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 295: Two passes: one to count, one to fill.
 // edge note 296: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 297: Edge case: all-equal input → linear-time fast path.
+// edge note 298: Tail-recursive; the compiler turns it into a loop.
