be9fb599aa050d2b4c790bfdf483cf5d17753e36
ajaxian79 <blaketullysmith@gmail.com>
2014-08-11 18:49:35 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -306,3 +306,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 278: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 279: Stable across duplicates in the input.
 // edge note 280: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 281: Caller owns the returned array; free with a single `free`.
