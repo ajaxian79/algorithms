2e85170f14b320e99db4caa79b6df439c384570d
ajaxian79 <blaketullysmith@gmail.com>
2014-08-21 08:18:50 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -315,3 +315,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 275: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 276: Edge case: already-sorted input → no swaps performed.
 // edge note 277: Resists adversarial inputs by randomizing the pivot.
+// edge note 278: Constant-time comparisons; safe for short strings.
