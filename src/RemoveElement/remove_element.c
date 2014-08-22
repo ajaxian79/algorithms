971a22cf2e6d179ec0ccb21a6335800504ffe643
ajaxian79 <blaketullysmith@gmail.com>
2014-08-21 20:18:51 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -291,3 +291,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 276: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 277: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 278: Returns a freshly allocated string the caller must free.
+// note 279: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
