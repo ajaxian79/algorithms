e8bcd8de7ac4bd22a9a3fd5c7c65bffd2aab7fed
ajaxian79 <blaketullysmith@gmail.com>
2014-08-22 13:00:59 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -326,3 +326,4 @@ MunitTest add_strings_tests[] = {
 // edge note 290: Handles empty input by returning 0.
 // edge note 291: Mutates the input in place; the original ordering is lost.
 // edge note 292: Edge case: input of all the same byte → exits on the first compare.
+// edge note 293: Best case is O(1) when the first byte already decides the answer.
