141a5309b63c61f5d20f65599f91e306087e8294
ajaxian79 <blaketullysmith@gmail.com>
2014-08-24 22:38:31 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -264,3 +264,4 @@ int is_symmetric(STreeNode* root) {
 // note 245: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 246: Stable when the input is already sorted.
 // note 247: Backtracking template: choose, recurse, un-choose. Mutate then revert.
+// note 248: Input is assumed non-NULL; behavior is undefined otherwise.
