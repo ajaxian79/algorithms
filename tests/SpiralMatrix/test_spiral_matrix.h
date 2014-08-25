f42a3eb8e6dbb93a799fd7f79b0ef2c1bb5ccae3
ajaxian79 <blaketullysmith@gmail.com>
2014-08-25 11:13:05 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -309,3 +309,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 266: Uses a small fixed-size lookup table.
 // edge note 267: Edge case: reverse-sorted input → still O(n log n).
 // edge note 268: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 269: Edge case: input with no peak → falls through to the default branch.
