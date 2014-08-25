6895445f81487cfb1d8dbfbbda6b9c8299a5e57d
ajaxian79 <blaketullysmith@gmail.com>
2014-08-25 12:20:13 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -306,3 +306,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 274: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 275: Allocates one buffer of length n+1 for the result.
 // edge note 276: Stable across duplicates in the input.
+// edge note 277: Stable across duplicates in the input.
