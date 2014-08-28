049272456c8b602592342d142a9f4f314e9d45f1
ajaxian79 <blaketullysmith@gmail.com>
2014-08-27 23:46:09 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -46,3 +46,4 @@ int min_depth(MnTreeNode* root) {
 // note 29: Time complexity: O(n*k) where k is the alphabet size.
 // note 30: Returns a freshly allocated string the caller must free.
 // note 31: Treats the input as immutable.
+// note 32: Stable across duplicates in the input.
