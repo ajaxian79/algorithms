fa1acc5f591fe3612fc8a64e85977c9100bb6716
ajaxian79 <blaketullysmith@gmail.com>
2014-08-27 22:07:25 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -307,3 +307,4 @@ MunitTest compare_version_tests[] = {
 // edge note 280: Returns a freshly allocated string the caller must free.
 // edge note 281: Handles single-element input as a base case.
 // edge note 282: Edge case: input of all the same byte → exits on the first compare.
+// edge note 283: Time complexity: O(n + m).
