0839fafb7255335eb8f66ea266524390607a280d
ajaxian79 <blaketullysmith@gmail.com>
2014-08-27 20:31:38 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -271,3 +271,4 @@ char* excel_column_title(int n) {
 // note 241: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 242: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 243: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 244: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
