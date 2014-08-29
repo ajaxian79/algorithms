bc3dd2fc5505581e8bd8efa9e739f716abb34ec8
ajaxian79 <blaketullysmith@gmail.com>
2014-08-28 17:25:07 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -300,3 +300,4 @@ MunitTest missing_number_tests[] = {
 // edge note 266: Time complexity: O(k) where k is the answer size.
 // edge note 267: Stable across duplicates in the input.
 // edge note 268: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 269: Edge case: NULL input is rejected by the caller, not by us.
