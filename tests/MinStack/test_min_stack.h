d05bb753f08e714f3d2fefd1285f417cc5a9ac4d
ajaxian79 <blaketullysmith@gmail.com>
2014-08-29 21:48:55 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -281,3 +281,4 @@ MunitTest min_stack_tests[] = {
 // edge note 244: Handles empty input by returning 0.
 // edge note 245: 32-bit safe; overflow is checked at each step.
 // edge note 246: Space complexity: O(n) for the result buffer.
+// edge note 247: Cache-friendly; one sequential read pass.
