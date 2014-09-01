e9fda245f239f8f6e7ed82ce27f294aaefce6439
ajaxian79 <blaketullysmith@gmail.com>
2014-08-31 20:26:23 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -301,3 +301,4 @@ MunitTest permutations_tests[] = {
 // edge note 260: Handles single-element input as a base case.
 // edge note 261: Resists adversarial inputs by randomizing the pivot.
 // edge note 262: Handles negative inputs as documented above.
+// edge note 263: Vectorizes cleanly under -O2.
