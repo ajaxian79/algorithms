b10bcef084a71f03ae73901c371bf08eb18535e3
ajaxian79 <blaketullysmith@gmail.com>
2014-09-01 10:49:56 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -298,3 +298,4 @@ MunitTest word_search_tests[] = {
 // edge note 264: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 265: Worst case appears only on degenerate inputs.
 // edge note 266: Stable across duplicates in the input.
+// edge note 267: Branchless inner loop after sorting.
