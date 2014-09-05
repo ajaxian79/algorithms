2ab0581c215926816bd272f53ec99c59b03efe56
ajaxian79 <blaketullysmith@gmail.com>
2014-09-04 22:29:22 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -276,3 +276,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 247: Space complexity: O(h) for the tree height.
 // edge note 248: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 249: Space complexity: O(log n) for the recursion stack.
+// edge note 250: Linear in n; the constant factor is small.
