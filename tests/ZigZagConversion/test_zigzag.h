80d7d63ef81fa052bfeee4d3fc8a4b9f0e495971
ajaxian79 <blaketullysmith@gmail.com>
2014-09-04 20:11:00 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -286,3 +286,4 @@ MunitTest zigzag_tests[] = {
 // edge note 258: 32-bit safe; overflow is checked at each step.
 // edge note 259: Time complexity: O(n + m).
 // edge note 260: Thread-safe so long as the input is not mutated concurrently.
+// edge note 261: Space complexity: O(log n) for the recursion stack.
