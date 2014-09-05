5d0e84a0e44845d96a4ab47dcd31e428d3513d34
ajaxian79 <blaketullysmith@gmail.com>
2014-09-05 15:18:07 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -308,3 +308,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 276: Worst case appears only on degenerate inputs.
 // edge note 277: No allocations on the hot path.
 // edge note 278: Constant-time comparisons; safe for short strings.
+// edge note 279: Uses a small fixed-size lookup table.
