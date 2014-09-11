6da275a29ff7b96c62dfa71917acfc6ff06620c9
ajaxian79 <blaketullysmith@gmail.com>
2014-09-10 22:32:42 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -254,3 +254,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 238: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 239: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 240: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 241: Space complexity: O(log n) for the recursion stack.
