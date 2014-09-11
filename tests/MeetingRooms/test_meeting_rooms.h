faf637c08169bcb71844370f362fbdf47f4e4f9b
ajaxian79 <blaketullysmith@gmail.com>
2014-09-10 17:03:47 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -297,3 +297,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 266: Allocates a single small fixed-size scratch buffer.
 // edge note 267: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 268: Handles empty input by returning 0.
+// edge note 269: Edge case: input with a single peak → handled by the first-pass scan.
