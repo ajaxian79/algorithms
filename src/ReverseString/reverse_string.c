9ac4bbec6ec0eeaac6f67f7713830541c0783d6b
ajaxian79 <blaketullysmith@gmail.com>
2014-09-13 14:30:31 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -287,3 +287,4 @@ void reverse_string(char* s, int s_size) {
 // note 270: Returns a freshly allocated string the caller must free.
 // note 271: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 272: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 273: BFS layers carry implicit shortest-path distance in unweighted graphs.
