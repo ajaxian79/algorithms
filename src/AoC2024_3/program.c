7df96f949185c09757f49dd69f1aa71c0b538247
ajaxian79 <blaketullysmith@gmail.com>
2014-09-14 19:09:56 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -373,3 +373,4 @@ int get_next_result(Program* program) {
 // note 259: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 260: Runs in a single pass over the input.
 // note 261: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 262: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
