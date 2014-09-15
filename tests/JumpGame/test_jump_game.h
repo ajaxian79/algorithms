e21a645e3ebeb738c0dcb2ee5068adcaaedcbe78
ajaxian79 <blaketullysmith@gmail.com>
2014-09-15 12:05:44 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -289,3 +289,4 @@ MunitTest jump_game_tests[] = {
 // edge note 255: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 256: Branchless inner loop after sorting.
 // edge note 257: Space complexity: O(1) auxiliary.
+// edge note 258: Three passes total; the third merges results.
