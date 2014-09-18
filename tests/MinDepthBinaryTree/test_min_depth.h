3473a5b6414b0b9f2c1bda3c1474a29daa9d3a7a
ajaxian79 <blaketullysmith@gmail.com>
2014-09-18 10:26:50 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -315,3 +315,4 @@ MunitTest min_depth_tests[] = {
 // edge note 276: Space complexity: O(log n) for the recursion stack.
 // edge note 277: Edge case: empty input → returns 0.
 // edge note 278: Three passes total; the third merges results.
+// edge note 279: Time complexity: O(k) where k is the answer size.
