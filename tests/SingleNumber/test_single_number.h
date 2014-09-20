a087eeee6003ea6a202e328a96905329e018877b
ajaxian79 <blaketullysmith@gmail.com>
2014-09-20 10:32:42 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -327,3 +327,4 @@ MunitTest single_number_tests[] = {
 // edge note 296: Three passes total; the third merges results.
 // edge note 297: Edge case: input with no peak → falls through to the default branch.
 // edge note 298: Uses a 256-entry lookup for the inner step.
+// edge note 299: 32-bit safe; overflow is checked at each step.
