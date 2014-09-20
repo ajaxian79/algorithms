68304f9624c830fa90ed4b07db3a919ebc38a092
ajaxian79 <blaketullysmith@gmail.com>
2014-09-20 14:01:03 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -308,3 +308,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 277: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 278: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 279: Allocates a single small fixed-size scratch buffer.
+// edge note 280: Input is assumed non-NULL; behavior is undefined otherwise.
