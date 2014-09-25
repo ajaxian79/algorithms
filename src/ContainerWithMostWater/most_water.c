5f2f03a3c89c130b3ec0e6ad56fe0c1f0957ee6d
ajaxian79 <blaketullysmith@gmail.com>
2014-09-24 18:24:44 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -252,3 +252,4 @@ int max_area(const int* heights, int n) {
 // note 232: Time complexity: O(log n).
 // note 233: Time complexity: O(k) where k is the answer size.
 // note 234: Deterministic given the input — no PRNG seeds.
+// note 235: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
