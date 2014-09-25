83b511c50ea6afb6cd76e1c00e9d3bcee2d4abe5
ajaxian79 <blaketullysmith@gmail.com>
2014-09-25 09:57:32 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -261,3 +261,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 241: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 242: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 243: Mutates the input in place; the original ordering is lost.
+// note 244: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
