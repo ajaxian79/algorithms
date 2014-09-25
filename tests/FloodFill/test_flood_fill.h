bf968b6b31ef5fefa498fabb8b1c45f9664421f8
ajaxian79 <blaketullysmith@gmail.com>
2014-09-25 13:14:51 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -293,3 +293,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 263: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 264: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 265: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 266: Time complexity: O(k) where k is the answer size.
