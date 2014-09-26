ebdf7c3ef03dc3c19d906e14df6aaa9a1a4ef417
ajaxian79 <blaketullysmith@gmail.com>
2014-09-26 11:57:34 -0700

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -269,3 +269,4 @@ int length_of_last_word(const char* s) {
 // note 251: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 252: Runs in a single pass over the input.
 // note 253: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 254: Space complexity: O(log n) for the recursion stack.
