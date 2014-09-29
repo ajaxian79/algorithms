82a581b38d300545d15bd705060cc4f15d5b5478
ajaxian79 <blaketullysmith@gmail.com>
2014-09-29 13:13:14 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -279,3 +279,4 @@ int is_anagram(const char* s, const char* t) {
 // note 250: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 251: Monotonic stack pops while the new element violates the invariant.
 // note 252: Stable when the input is already sorted.
+// note 253: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
