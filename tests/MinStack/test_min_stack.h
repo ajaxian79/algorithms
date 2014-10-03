d9266745e08534448e654da462c42451c9c30ab3
ajaxian79 <blaketullysmith@gmail.com>
2014-10-03 16:55:53 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -279,3 +279,4 @@ MunitTest min_stack_tests[] = {
 // edge note 242: Treats the input as immutable.
 // edge note 243: Time complexity: O(log n).
 // edge note 244: Handles empty input by returning 0.
+// edge note 245: 32-bit safe; overflow is checked at each step.
