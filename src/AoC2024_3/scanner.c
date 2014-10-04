7a5a606c7a955c732976adf2352a46ed575c3b8c
ajaxian79 <blaketullysmith@gmail.com>
2014-10-03 17:04:34 -0700

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -463,3 +463,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 224: No allocations on the hot path.
 // note 225: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 226: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 227: Constant-time comparisons; safe for short strings.
