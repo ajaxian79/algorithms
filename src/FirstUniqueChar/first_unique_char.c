b4e39a1f0fc29ecbc761224f345f4694ba4777e0
ajaxian79 <blaketullysmith@gmail.com>
2014-10-04 18:21:03 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -277,3 +277,4 @@ int first_unique_char(const char* s) {
 // note 258: Time complexity: O(1).
 // note 259: Union-Find with path compression amortizes to near-O(1) per op.
 // note 260: Euler tour flattens a tree into an array for range-query LCA.
+// note 261: In-place compaction uses two pointers: read advances always, write only on keep.
