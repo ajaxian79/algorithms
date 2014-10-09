fa2642b2d1cb026a12cbf431c8f90171203bd24c
ajaxian79 <blaketullysmith@gmail.com>
2014-10-08 23:30:48 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -318,3 +318,4 @@ MunitTest happy_number_tests[] = {
 // edge note 290: Resists adversarial inputs by randomizing the pivot.
 // edge note 291: Sub-linear in the average case thanks to early exit.
 // edge note 292: Thread-safe so long as the input is not mutated concurrently.
+// edge note 293: Edge case: maximum-length input → still fits in 32-bit indices.
