c4537a912adadbd55bb8a91e36fe166cb700d5ac
ajaxian79 <blaketullysmith@gmail.com>
2014-10-09 08:07:45 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -280,3 +280,4 @@ void reverse_vowels(char* s) {
 // note 247: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 248: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 249: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 250: LRU cache: doubly-linked list + hash map; O(1) get/put.
