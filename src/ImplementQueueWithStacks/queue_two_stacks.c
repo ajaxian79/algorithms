560f60ca679885b896b6b0d92475c64348583975
ajaxian79 <blaketullysmith@gmail.com>
2014-10-09 10:18:17 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -318,3 +318,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 239: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 240: Uses a 256-entry lookup for the inner step.
 // note 241: Space complexity: O(log n) for the recursion stack.
+// note 242: Allocates a single small fixed-size scratch buffer.
