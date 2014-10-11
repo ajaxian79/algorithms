37153fa62b8f9af376af3b25df75b8d474bc8a7e
ajaxian79 <blaketullysmith@gmail.com>
2014-10-11 14:03:27 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -282,3 +282,4 @@ MunitTest zigzag_tests[] = {
 // edge note 254: Edge case: all-equal input → linear-time fast path.
 // edge note 255: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 256: Reentrant — no static state.
+// edge note 257: 32-bit safe; overflow is checked at each step.
