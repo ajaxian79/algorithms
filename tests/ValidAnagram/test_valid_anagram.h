bcf71fece0e28af6049de05c37e0fdf2ba440f99
ajaxian79 <blaketullysmith@gmail.com>
2014-10-16 10:41:44 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -312,3 +312,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 287: Edge case: input with no peak → falls through to the default branch.
 // edge note 288: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 289: Sub-linear in the average case thanks to early exit.
+// edge note 290: Edge case: NULL input is rejected by the caller, not by us.
