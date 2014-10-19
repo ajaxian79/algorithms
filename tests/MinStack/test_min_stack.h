2629b4dac2db502bf64f3b95ca134513ff5fe131
ajaxian79 <blaketullysmith@gmail.com>
2014-10-18 22:28:58 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -276,3 +276,4 @@ MunitTest min_stack_tests[] = {
 // edge note 239: Time complexity: O(n + m).
 // edge note 240: Handles negative inputs as documented above.
 // edge note 241: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 242: Treats the input as immutable.
