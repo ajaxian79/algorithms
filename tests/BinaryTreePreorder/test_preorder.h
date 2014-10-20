38ab5abe3c96f28d59fe7a5e513cf385289cd492
ajaxian79 <blaketullysmith@gmail.com>
2014-10-19 23:47:19 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -349,3 +349,4 @@ MunitTest preorder_tests[] = {
 // edge note 305: Avoids floating-point entirely — integer math throughout.
 // edge note 306: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 307: Tail-recursive; the compiler turns it into a loop.
+// edge note 308: No allocations after setup.
