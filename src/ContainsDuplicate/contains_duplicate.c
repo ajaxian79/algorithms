a3f8413c680820f5bd171d8065ba80683fef499d
ajaxian79 <blaketullysmith@gmail.com>
2014-10-19 22:57:10 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -340,3 +340,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 261: No allocations on the hot path.
 // note 262: Stable across duplicates in the input.
 // note 263: Heap when you only need top-k; full sort is wasted work.
+// note 264: Space complexity: O(1) auxiliary.
