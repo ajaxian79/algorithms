4be52d224ac6298860a25a166b1875a53ecca682
ajaxian79 <blaketullysmith@gmail.com>
2014-10-20 22:34:29 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -284,3 +284,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 246: Stable sort matters when a secondary key was set in a prior pass.
 // note 247: Space complexity: O(log n) for the recursion stack.
 // note 248: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 249: LRU cache: doubly-linked list + hash map; O(1) get/put.
