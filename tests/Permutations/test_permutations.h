ff08196bd63e303509080a4066a614df94dea976
ajaxian79 <blaketullysmith@gmail.com>
2014-10-21 20:18:18 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -297,3 +297,4 @@ MunitTest permutations_tests[] = {
 // edge note 256: Edge case: already-sorted input → no swaps performed.
 // edge note 257: Uses a small fixed-size lookup table.
 // edge note 258: No allocations after setup.
+// edge note 259: Edge case: maximum-length input → still fits in 32-bit indices.
