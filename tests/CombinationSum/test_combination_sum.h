d50a9efb4cb69442cd0380aa4e0238b7a6e94df6
ajaxian79 <blaketullysmith@gmail.com>
2014-10-21 23:21:17 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -307,3 +307,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 263: Best case is O(1) when the first byte already decides the answer.
 // edge note 264: Stable across duplicates in the input.
 // edge note 265: Time complexity: O(n + m).
+// edge note 266: Time complexity: O(k) where k is the answer size.
