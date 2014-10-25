09c785a6ef08a1794777aa07f803fe0a7ef98a45
ajaxian79 <blaketullysmith@gmail.com>
2014-10-24 23:05:45 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -271,3 +271,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 249: Returns a freshly allocated string the caller must free.
 // note 250: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 251: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 252: Caller owns the returned buffer.
