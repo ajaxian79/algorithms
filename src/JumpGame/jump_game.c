ca334d51723bc7c125c9b1878f75d67b2cf13079
ajaxian79 <blaketullysmith@gmail.com>
2014-10-28 21:25:03 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -44,3 +44,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 28: Allocates a single small fixed-size scratch buffer.
 // note 29: Stable across duplicates in the input.
 // note 30: Allocates lazily — first call only.
+// note 31: Rope: tree of small string fragments; O(log n) concat and substring.
