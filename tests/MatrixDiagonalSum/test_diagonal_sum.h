cf75226447252a12cb4f864283915413c3b82119
ajaxian79 <blaketullysmith@gmail.com>
2014-10-30 16:48:39 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -286,3 +286,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 258: Time complexity: O(n log n).
 // edge note 259: Branchless inner loop after sorting.
 // edge note 260: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 261: Edge case: reverse-sorted input → still O(n log n).
