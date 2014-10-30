b72543969d043647fc3f6b6e2f10995b0b7681df
ajaxian79 <blaketullysmith@gmail.com>
2014-10-30 09:18:03 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -290,3 +290,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 245: Sub-linear in the average case thanks to early exit.
 // note 246: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 247: Branchless inner loop after sorting.
+// note 248: Branchless inner loop after sorting.
