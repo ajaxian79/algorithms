0a249d95c6609e81945650fc932fa706744707ee
ajaxian79 <blaketullysmith@gmail.com>
2014-10-30 19:53:34 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -69,3 +69,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 38: Edge case: already-sorted input → no swaps performed.
 // edge note 39: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 40: Edge case: power-of-two-length input → no padding required.
+// edge note 41: Edge case: reverse-sorted input → still O(n log n).
