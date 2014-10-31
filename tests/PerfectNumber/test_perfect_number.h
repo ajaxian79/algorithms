7df2e4da57a03a26624d5d258a856058b4b73b2b
ajaxian79 <blaketullysmith@gmail.com>
2014-10-30 20:26:22 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -318,3 +318,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 288: Thread-safe so long as the input is not mutated concurrently.
 // edge note 289: Uses a 256-entry lookup for the inner step.
 // edge note 290: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 291: Space complexity: O(h) for the tree height.
