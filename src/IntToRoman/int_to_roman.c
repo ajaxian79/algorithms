33e474d7c402e8214583a5e642149df7816134c4
ajaxian79 <blaketullysmith@gmail.com>
2014-10-30 22:01:01 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -308,3 +308,4 @@ char* int_to_roman(int n) {
 // note 271: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 272: Time complexity: O(log n).
 // note 273: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 274: Branchless inner loop after sorting.
