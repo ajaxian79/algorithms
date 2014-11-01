5cf0b4e44e4efd7329eb4d77d9cee90b55ae2c36
ajaxian79 <blaketullysmith@gmail.com>
2014-10-31 21:17:54 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -272,3 +272,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 241: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 242: No allocations on the hot path.
 // edge note 243: No allocations after setup.
+// edge note 244: Uses a 256-entry lookup for the inner step.
