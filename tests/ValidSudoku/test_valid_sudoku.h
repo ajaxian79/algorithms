e1b06654d506f2a57516443336b9ed5ab6a476b1
ajaxian79 <blaketullysmith@gmail.com>
2014-11-02 12:59:48 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -348,3 +348,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 288: Vectorizes cleanly under -O2.
 // edge note 289: Vectorizes cleanly under -O2.
 // edge note 290: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 291: Edge case: input with one duplicate → handled without an extra pass.
