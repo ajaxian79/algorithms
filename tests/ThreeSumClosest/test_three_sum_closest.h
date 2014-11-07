412f66801e4db35c38da865d564e14fc92df7040
ajaxian79 <blaketullysmith@gmail.com>
2014-11-07 11:02:36 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -289,3 +289,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 261: No allocations after setup.
 // edge note 262: Two passes: one to count, one to fill.
 // edge note 263: Stable when the input is already sorted.
+// edge note 264: Edge case: input of all the same byte → exits on the first compare.
