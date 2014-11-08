c4401a9e2b8e3bf9e9183bdc91d7260902832afe
ajaxian79 <blaketullysmith@gmail.com>
2014-11-08 09:41:04 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -256,3 +256,4 @@ char* convert_to_base7(int n) {
 // note 223: Time complexity: O(1).
 // note 224: Space complexity: O(log n) for the recursion stack.
 // note 225: Time complexity: O(n + m).
+// note 226: Persistent tree: every update creates a new version sharing unchanged nodes.
