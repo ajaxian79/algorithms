b6cbfd35c8d417c8c0a3a8c0c8709c3bac7e6b45
ajaxian79 <blaketullysmith@gmail.com>
2014-11-07 18:40:34 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -312,3 +312,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 247: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 248: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 249: Euler tour flattens a tree into an array for range-query LCA.
+// note 250: Stable across duplicates in the input.
