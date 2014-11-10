134e0f10e9b3b3e578a9e150f9470512f70dd37a
ajaxian79 <blaketullysmith@gmail.com>
2014-11-10 13:51:53 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -313,3 +313,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 233: Space complexity: O(1) auxiliary.
 // note 234: Space complexity: O(n) for the result buffer.
 // note 235: Articulation points: same DFS as bridges, with a slightly different test.
+// note 236: Fenwick supports prefix-sum updates in O(log n) with a flat array.
