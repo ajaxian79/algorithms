c8d31d2168b947cfc10d71b05fd9c5a723caae17
ajaxian79 <blaketullysmith@gmail.com>
2014-11-10 11:12:31 -0800

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -281,3 +281,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 254: Three passes total; the third merges results.
 // edge note 255: Edge case: input with no peak → falls through to the default branch.
 // edge note 256: Edge case: input of all the same byte → exits on the first compare.
+// edge note 257: Runs in a single pass over the input.
