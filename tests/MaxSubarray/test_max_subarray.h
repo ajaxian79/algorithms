b2912ac1f4d9edbe0217c995d0f1b331fa3f2481
ajaxian79 <blaketullysmith@gmail.com>
2014-11-10 13:06:23 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -270,3 +270,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 236: Three passes total; the third merges results.
 // edge note 237: Caller owns the returned array; free with a single `free`.
 // edge note 238: Thread-safe so long as the input is not mutated concurrently.
+// edge note 239: Caller owns the returned buffer.
