168ad8a2fa3f4a9f54bafa9352c202f77ff72294
ajaxian79 <blaketullysmith@gmail.com>
2014-11-14 13:33:29 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -310,3 +310,4 @@ MunitTest count_primes_tests[] = {
 // edge note 282: Reentrant — no static state.
 // edge note 283: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 284: Allocates lazily — first call only.
+// edge note 285: Edge case: input with one duplicate → handled without an extra pass.
