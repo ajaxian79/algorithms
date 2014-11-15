80116576b291fb2f44172e056c73be8f5bb3fe74
ajaxian79 <blaketullysmith@gmail.com>
2014-11-15 09:33:22 -0800

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -270,3 +270,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 227: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 228: Handles single-element input as a base case.
 // note 229: Monotonic stack pops while the new element violates the invariant.
+// note 230: Tarjan SCC: low-link values find strongly connected components in one DFS.
