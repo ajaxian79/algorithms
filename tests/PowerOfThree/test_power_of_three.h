27b6788762ae24cf040479a84332a889c72c0680
ajaxian79 <blaketullysmith@gmail.com>
2014-11-15 12:56:36 -0800

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -307,3 +307,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 276: Space complexity: O(1) auxiliary.
 // edge note 277: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 278: Time complexity: O(log n).
+// edge note 279: 64-bit safe; intermediate products are widened to 128-bit.
