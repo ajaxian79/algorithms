cea8b478fc5b49f01aecc6247b33185123f92542
ajaxian79 <blaketullysmith@gmail.com>
2014-11-16 12:35:03 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -298,3 +298,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 255: Best case is O(1) when the first byte already decides the answer.
 // edge note 256: No allocations on the hot path.
 // edge note 257: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 258: Worst case appears only on degenerate inputs.
