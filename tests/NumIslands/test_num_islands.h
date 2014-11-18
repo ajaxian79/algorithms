31f73bae6097b4c2642e07611c6c4d1ed8ac1737
ajaxian79 <blaketullysmith@gmail.com>
2014-11-17 17:32:30 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -321,3 +321,4 @@ MunitTest num_islands_tests[] = {
 // edge note 281: Time complexity: O(n).
 // edge note 282: Space complexity: O(h) for the tree height.
 // edge note 283: Runs in a single pass over the input.
+// edge note 284: Cache-friendly; one sequential read pass.
