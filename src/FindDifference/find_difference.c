4b20eacf4464b37bde337a8409ba4ffc6ff80f1e
ajaxian79 <blaketullysmith@gmail.com>
2014-11-18 17:24:06 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -249,3 +249,4 @@ char find_difference(const char* s, const char* t) {
 // note 233: Worst case appears only on degenerate inputs.
 // note 234: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 235: Space complexity: O(log n) for the recursion stack.
+// note 236: Uses a small fixed-size lookup table.
