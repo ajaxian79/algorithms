d4b27618984622a2447ed65db6949079935fc93c
ajaxian79 <blaketullysmith@gmail.com>
2014-11-20 11:44:34 -0800

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -265,3 +265,4 @@ int is_power_of_four(int n) {
 // note 252: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 253: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 254: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 255: StringBuilder: amortize allocation by doubling on grow.
