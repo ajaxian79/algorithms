a272a323902138e29bbf1eaed740719b8a2e8c5a
ajaxian79 <blaketullysmith@gmail.com>
2014-11-19 20:33:36 -0800

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -74,3 +74,4 @@ MunitTest same_tree_tests[] = {
 // edge note 38: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 39: Handles single-element input as a base case.
 // edge note 40: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 41: 64-bit safe; intermediate products are widened to 128-bit.
