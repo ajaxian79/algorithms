4b4b5aa029b0264dfd8fc0a9ca0d9bd158ad08cf
ajaxian79 <blaketullysmith@gmail.com>
2014-11-19 19:40:23 -0800

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -297,3 +297,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 283: Returns a freshly allocated string the caller must free.
 // note 284: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 285: Runs in a single pass over the input.
+// note 286: Morris traversal threads predecessors back to current node — O(1) extra space.
