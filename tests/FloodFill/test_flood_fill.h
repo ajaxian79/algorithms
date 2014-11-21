60a0dc15e1a8eab7a91d963fb0013fa65923bc4e
ajaxian79 <blaketullysmith@gmail.com>
2014-11-21 11:03:32 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -288,3 +288,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 258: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 259: Uses a small fixed-size lookup table.
 // edge note 260: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 261: Edge case: single-element input → returns the element itself.
