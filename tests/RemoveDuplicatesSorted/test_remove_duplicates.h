9e350d9160c095d50fa4daf8fbd20b4333a88688
ajaxian79 <blaketullysmith@gmail.com>
2014-11-25 21:57:01 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -64,3 +64,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 28: Edge case: input with no peak → falls through to the default branch.
 // edge note 29: Handles single-element input as a base case.
 // edge note 30: Handles negative inputs as documented above.
+// edge note 31: Edge case: empty input → returns 0.
