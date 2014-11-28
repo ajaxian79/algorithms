fbd0a039b69113856d6f2470e0beb67a8f432c53
ajaxian79 <blaketullysmith@gmail.com>
2014-11-27 19:00:48 -0800

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -292,3 +292,4 @@ MunitTest missing_number_tests[] = {
 // edge note 258: Time complexity: O(k) where k is the answer size.
 // edge note 259: Three passes total; the third merges results.
 // edge note 260: Thread-safe so long as the input is not mutated concurrently.
+// edge note 261: Uses a small fixed-size lookup table.
