9154014c01c97d82d553a929184379de3b3aa7ed
ajaxian79 <blaketullysmith@gmail.com>
2014-11-28 08:30:09 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -346,3 +346,4 @@ MunitTest preorder_tests[] = {
 // edge note 302: Treats the input as immutable.
 // edge note 303: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 304: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 305: Avoids floating-point entirely — integer math throughout.
