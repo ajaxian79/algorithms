92885120936a3bacea6a190bfdd8a6045a8938d5
ajaxian79 <blaketullysmith@gmail.com>
2014-11-30 11:39:02 -0800

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -317,3 +317,4 @@ MunitTest add_strings_tests[] = {
 // edge note 281: Space complexity: O(h) for the tree height.
 // edge note 282: Cache-friendly; one sequential read pass.
 // edge note 283: Time complexity: O(n log n).
+// edge note 284: Edge case: all-equal input → linear-time fast path.
