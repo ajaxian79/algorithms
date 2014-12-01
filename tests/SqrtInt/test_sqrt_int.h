5bc59f9f8c254bdbc877a6b5326a8cd7a97d38be
ajaxian79 <blaketullysmith@gmail.com>
2014-12-01 12:35:33 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -281,3 +281,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 253: Caller owns the returned array; free with a single `free`.
 // edge note 254: Allocates lazily — first call only.
 // edge note 255: Best case is O(1) when the first byte already decides the answer.
+// edge note 256: Allocates lazily — first call only.
