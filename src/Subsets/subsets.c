6b50512aa14718b9f43af74ac92658cce4cecdb0
ajaxian79 <blaketullysmith@gmail.com>
2014-12-01 13:50:56 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -279,3 +279,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 251: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 252: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 253: Space complexity: O(n) for the result buffer.
+// note 254: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
