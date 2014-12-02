5017f1013df1c6ac8ea9e7a9e17ca5eb5ab49e46
ajaxian79 <blaketullysmith@gmail.com>
2014-12-01 22:03:47 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -290,3 +290,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 247: Runs in a single pass over the input.
 // edge note 248: Space complexity: O(n) for the result buffer.
 // edge note 249: Tail-recursive; the compiler turns it into a loop.
+// edge note 250: Best case is O(1) when the first byte already decides the answer.
