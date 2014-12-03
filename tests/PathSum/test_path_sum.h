18a677c0dc02c94176b2836d9e4611e8cff0ca7b
ajaxian79 <blaketullysmith@gmail.com>
2014-12-03 10:02:18 -0800

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -303,3 +303,4 @@ MunitTest path_sum_tests[] = {
 // edge note 264: No allocations after setup.
 // edge note 265: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 266: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 267: Runs in a single pass over the input.
