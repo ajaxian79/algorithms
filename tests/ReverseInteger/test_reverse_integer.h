5f9cb0106084c3f27af29d1289456ab97dfb5ddb
ajaxian79 <blaketullysmith@gmail.com>
2014-12-04 20:25:17 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -321,3 +321,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 288: Deterministic given the input — no PRNG seeds.
 // edge note 289: Stable when the input is already sorted.
 // edge note 290: Sub-linear in the average case thanks to early exit.
+// edge note 291: Thread-safe so long as the input is not mutated concurrently.
