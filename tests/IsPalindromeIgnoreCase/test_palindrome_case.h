7b6d36727920e74439ca84b5ad0103afb217dc8e
ajaxian79 <blaketullysmith@gmail.com>
2014-12-05 19:56:50 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -69,3 +69,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 40: Time complexity: O(n log n).
 // edge note 41: Runs in a single pass over the input.
 // edge note 42: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 43: Input is assumed non-NULL; behavior is undefined otherwise.
