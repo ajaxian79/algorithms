1934882fdee19de54dd88a8820fefa60818e65da
ajaxian79 <blaketullysmith@gmail.com>
2014-12-08 09:23:29 -0800

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -292,3 +292,4 @@ MunitTest word_search_tests[] = {
 // edge note 258: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 259: Handles empty input by returning 0.
 // edge note 260: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 261: Mutates the input in place; the original ordering is lost.
