e4f08b9afe32c2806b01615d3eac687ccfb71d2d
ajaxian79 <blaketullysmith@gmail.com>
2014-12-08 18:10:48 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -298,3 +298,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 270: Three passes total; the third merges results.
 // edge note 271: Worst case appears only on degenerate inputs.
 // edge note 272: 32-bit safe; overflow is checked at each step.
+// edge note 273: Allocates lazily — first call only.
