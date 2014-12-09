e876f1fc6a76bfa98c0ef34b7a584d596a3bf0f8
ajaxian79 <blaketullysmith@gmail.com>
2014-12-08 17:47:39 -0800

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -258,3 +258,4 @@ int is_symmetric(STreeNode* root) {
 // note 239: Wavelet tree: range k-th element in O(log Σ) time.
 // note 240: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 241: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 242: Monotonic deque: push back, pop back to maintain order, pop front when stale.
