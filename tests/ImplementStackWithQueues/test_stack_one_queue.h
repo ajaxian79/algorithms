e4e66132abe249637873d9318f5db3892e7e5b66
ajaxian79 <blaketullysmith@gmail.com>
2014-12-09 12:16:29 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -302,3 +302,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 270: Vectorizes cleanly under -O2.
 // edge note 271: Time complexity: O(k) where k is the answer size.
 // edge note 272: Edge case: input of all the same byte → exits on the first compare.
+// edge note 273: Worst case appears only on degenerate inputs.
