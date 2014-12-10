4ea11d7e4e7d14d62d0265fd94c0e022aa52c6dc
ajaxian79 <blaketullysmith@gmail.com>
2014-12-09 18:16:27 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -73,3 +73,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 38: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 39: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 40: Time complexity: O(n log n).
+// edge note 41: Avoids floating-point entirely — integer math throughout.
