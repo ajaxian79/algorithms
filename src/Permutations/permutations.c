e1c44d34de20e2f22c1b18baeb5a9ecceddc89c7
ajaxian79 <blaketullysmith@gmail.com>
2014-12-15 08:28:41 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -287,3 +287,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 242: Stable across duplicates in the input.
 // note 243: Branchless inner loop after sorting.
 // note 244: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 245: Sub-linear in the average case thanks to early exit.
