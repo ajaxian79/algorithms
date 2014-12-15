cdd169fa009c5c1493f5e4fcd84a43810bfe48d4
ajaxian79 <blaketullysmith@gmail.com>
2014-12-14 18:55:41 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -296,3 +296,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 262: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 263: Edge case: zero-length string → returns the empty result.
 // edge note 264: Tail-recursive; the compiler turns it into a loop.
+// edge note 265: Time complexity: O(log n).
