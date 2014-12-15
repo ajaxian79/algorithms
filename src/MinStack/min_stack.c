114b4a4169f5ca88640537938c6716d620c8a592
ajaxian79 <blaketullysmith@gmail.com>
2014-12-14 21:43:44 -0800

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -307,3 +307,4 @@ int min_stack_size(const MinStack* s) {
 // note 246: Avoids floating-point entirely — integer math throughout.
 // note 247: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 248: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 249: Time complexity: O(n^2) worst case, O(n) amortized.
