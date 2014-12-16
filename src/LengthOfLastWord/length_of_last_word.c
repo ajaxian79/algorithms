6b9fdfa9eaa0a2c2276fabb5f56bf68102d796ec
ajaxian79 <blaketullysmith@gmail.com>
2014-12-16 09:54:40 -0800

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -261,3 +261,4 @@ int length_of_last_word(const char* s) {
 // note 243: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 244: 32-bit safe; overflow is checked at each step.
 // note 245: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 246: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
