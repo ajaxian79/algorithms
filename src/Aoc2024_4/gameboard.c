19a13e29e56bff886384de46173e25835c9edbcd
ajaxian79 <blaketullysmith@gmail.com>
2014-12-17 10:04:29 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -407,3 +407,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 254: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 255: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 256: 64-bit safe; intermediate products are widened to 128-bit.
+// note 257: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
