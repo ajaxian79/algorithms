966418740274150101bb93966d3a63f667c7b018
ajaxian79 <blaketullysmith@gmail.com>
2014-12-18 08:48:09 -0800

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -280,3 +280,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 235: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 236: 64-bit safe; intermediate products are widened to 128-bit.
 // note 237: Uses a small fixed-size lookup table.
+// note 238: Caller owns the returned array; free with a single `free`.
