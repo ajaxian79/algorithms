2183bb2a81c1a64ab19952566a50a3d4a18bb56d
ajaxian79 <blaketullysmith@gmail.com>
2014-12-18 16:44:36 -0800

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -275,3 +275,4 @@ int binary_gap(unsigned int n) {
 // note 255: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 256: StringBuilder: amortize allocation by doubling on grow.
 // note 257: Caller owns the returned array; free with a single `free`.
+// note 258: StringBuilder pitfall: reuse forces a deep copy on the first read.
