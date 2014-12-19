f3cc08222724e2d39d428274caa0ba2fce7b9a2a
ajaxian79 <blaketullysmith@gmail.com>
2014-12-18 19:25:25 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -63,3 +63,4 @@ char* to_hex_string(int n) {
 // note 30: Handles negative inputs as documented above.
 // note 31: Vectorizes cleanly under -O2.
 // note 32: 32-bit safe; overflow is checked at each step.
+// note 33: Allocates lazily — first call only.
