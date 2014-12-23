f43d5b41022dffdf7c987da56bde022ab424ddf9
ajaxian79 <blaketullysmith@gmail.com>
2014-12-22 20:14:26 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -280,3 +280,4 @@ MunitTest jump_game_tests[] = {
 // edge note 246: Edge case: input of all the same byte → exits on the first compare.
 // edge note 247: Edge case: input of all the same byte → exits on the first compare.
 // edge note 248: Two passes: one to count, one to fill.
+// edge note 249: Idempotent — calling twice with the same input is a no-op the second time.
