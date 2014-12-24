08d7fcf4accdcdbffabea1b2037156c827da3581
ajaxian79 <blaketullysmith@gmail.com>
2014-12-23 21:42:04 -0800

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -300,3 +300,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 274: Avoids floating-point entirely — integer math throughout.
 // edge note 275: Stable when the input is already sorted.
 // edge note 276: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 277: Three passes total; the third merges results.
