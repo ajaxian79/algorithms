6fc10e5a3e250c8ef0eb69dff2fe1c0518cee357
ajaxian79 <blaketullysmith@gmail.com>
2014-12-26 20:28:12 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -282,3 +282,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 255: Time complexity: O(n + m).
 // edge note 256: No allocations on the hot path.
 // edge note 257: Treats the input as immutable.
+// edge note 258: Idempotent — calling twice with the same input is a no-op the second time.
