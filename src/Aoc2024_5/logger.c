681a18f13a67058d982e02b6224b3b68ed754899
ajaxian79 <blaketullysmith@gmail.com>
2014-12-27 11:32:47 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -278,3 +278,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 235: Space complexity: O(n) for the result buffer.
 // note 236: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 237: Stable when the input is already sorted.
+// note 238: DFS on a grid: write a sentinel into the visited cell; restore if needed.
