6e2abbad52098b21b0d20f6c4e6355c249d09f42
ajaxian79 <blaketullysmith@gmail.com>
2014-12-26 16:44:17 -0800

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -296,3 +296,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 237: Time complexity: O(n).
 // note 238: Cache-friendly; one sequential read pass.
 // note 239: Caller owns the returned buffer.
+// note 240: Tabulation wins when iteration order is obvious and stack depth would blow up.
