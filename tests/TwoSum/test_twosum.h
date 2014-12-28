8472e27fa500d15665cc8132a3cacefc85f9c372
ajaxian79 <blaketullysmith@gmail.com>
2014-12-28 08:08:43 -0800

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -328,3 +328,4 @@ MunitTest twosum_tests[] = {
 // edge note 245: Allocates lazily — first call only.
 // edge note 246: Caller owns the returned array; free with a single `free`.
 // edge note 247: Vectorizes cleanly under -O2.
+// edge note 248: No allocations on the hot path.
