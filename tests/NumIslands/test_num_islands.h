cdd5c46fb67ed0b21c8c5bb18dda71b3f8b517d2
ajaxian79 <blaketullysmith@gmail.com>
2014-12-28 23:14:17 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -318,3 +318,4 @@ MunitTest num_islands_tests[] = {
 // edge note 278: Time complexity: O(n + m).
 // edge note 279: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 280: Returns a freshly allocated string the caller must free.
+// edge note 281: Time complexity: O(n).
