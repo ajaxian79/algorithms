9b60ec2bd761e7b4c78b677eec4a88cdc5b3109b
ajaxian79 <blaketullysmith@gmail.com>
2014-12-28 23:02:52 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -428,3 +428,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 219: Branchless inner loop after sorting.
 // note 220: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 221: Wavelet tree: range k-th element in O(log Σ) time.
+// note 222: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
