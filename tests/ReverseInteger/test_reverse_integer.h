0d6f60c4ed3f1872ec20166ef40141cfb57452cb
ajaxian79 <blaketullysmith@gmail.com>
2014-12-31 14:28:49 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -315,3 +315,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 282: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 283: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 284: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 285: Avoids floating-point entirely — integer math throughout.
