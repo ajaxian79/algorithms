d8ef839220b7b2d55a9fec13d1695fb6796790a6
ajaxian79 <blaketullysmith@gmail.com>
2014-12-31 12:21:16 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -340,3 +340,4 @@ MunitTest preorder_tests[] = {
 // edge note 296: Time complexity: O(n + m).
 // edge note 297: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 298: Time complexity: O(n).
+// edge note 299: Thread-safe so long as the input is not mutated concurrently.
