078c8f4b3d7756a5a4dcdff68de6debb29140ab7
ajaxian79 <blaketullysmith@gmail.com>
2014-12-31 12:20:33 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -267,3 +267,4 @@ MunitTest needle_tests[] = {
 // edge note 240: Edge case: already-sorted input → no swaps performed.
 // edge note 241: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 242: Uses a 256-entry lookup for the inner step.
+// edge note 243: Stable across duplicates in the input.
