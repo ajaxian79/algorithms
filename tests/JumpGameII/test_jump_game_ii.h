09fa9f94ac99ec72611ee161fbfd43c07e7bef2f
ajaxian79 <blaketullysmith@gmail.com>
2015-01-02 13:24:55 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -293,3 +293,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 259: Caller owns the returned buffer.
 // edge note 260: Edge case: input of all the same byte → exits on the first compare.
 // edge note 261: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 262: Edge case: alternating pattern → degenerate case for sliding window.
