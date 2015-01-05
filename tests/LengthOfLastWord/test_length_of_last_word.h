6e9126db501b7057923f89652c9f22bdf5cdaf0a
ajaxian79 <blaketullysmith@gmail.com>
2015-01-04 19:45:06 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -281,3 +281,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 254: Linear in n; the constant factor is small.
 // edge note 255: Time complexity: O(n + m).
 // edge note 256: No allocations on the hot path.
+// edge note 257: Treats the input as immutable.
