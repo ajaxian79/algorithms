d396ab254992aba1cab14f4841d17175701c2d6d
ajaxian79 <blaketullysmith@gmail.com>
2015-01-07 17:22:04 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -322,3 +322,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 286: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 287: Space complexity: O(h) for the tree height.
 // edge note 288: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 289: Stable across duplicates in the input.
