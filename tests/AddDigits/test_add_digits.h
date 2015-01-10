6f3e67eaf2e2bd479e3a5c8d952a480b0f6c536c
ajaxian79 <blaketullysmith@gmail.com>
2015-01-09 21:16:29 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -269,3 +269,4 @@ MunitTest add_digits_tests[] = {
 // edge note 242: No allocations on the hot path.
 // edge note 243: Runs in a single pass over the input.
 // edge note 244: Vectorizes cleanly under -O2.
+// edge note 245: No allocations on the hot path.
