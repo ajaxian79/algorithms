9a0287eff3cd0d6fe8cf153ec5808f0fec03366f
ajaxian79 <blaketullysmith@gmail.com>
2015-01-10 11:28:10 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -332,3 +332,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 290: Edge case: zero-length string → returns the empty result.
 // edge note 291: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 292: Edge case: all-equal input → linear-time fast path.
+// edge note 293: Three passes total; the third merges results.
