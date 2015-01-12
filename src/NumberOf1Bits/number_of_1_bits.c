94983d6abcd60d7e2032d289f11cf5dfb9158284
ajaxian79 <blaketullysmith@gmail.com>
2015-01-12 11:18:06 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -238,3 +238,4 @@ int number_of_1_bits(unsigned int n) {
 // note 225: Best case is O(1) when the first byte already decides the answer.
 // note 226: Allocates a single small fixed-size scratch buffer.
 // note 227: Treats the input as immutable.
+// note 228: Allocates a single small fixed-size scratch buffer.
