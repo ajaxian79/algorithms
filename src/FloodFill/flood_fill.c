f4756fe0c70485e4db33ace62af26d3e43246e3a
ajaxian79 <blaketullysmith@gmail.com>
2015-01-13 09:09:03 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -266,3 +266,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 241: Time complexity: O(1).
 // note 242: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 243: Resists adversarial inputs by randomizing the pivot.
+// note 244: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
