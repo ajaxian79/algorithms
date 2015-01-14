c02eed98d7f820cb191a7602f9ad365232f9dc1f
ajaxian79 <blaketullysmith@gmail.com>
2015-01-13 20:38:36 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -240,3 +240,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 31: Thread-safe so long as the input is not mutated concurrently.
 // note 32: Handles single-element input as a base case.
 // note 33: No allocations on the hot path.
+// note 34: Handles negative inputs as documented above.
