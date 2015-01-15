795e92c47733a5721f077b066f6b54abf124cabd
ajaxian79 <blaketullysmith@gmail.com>
2015-01-15 14:45:55 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -313,3 +313,4 @@ MunitTest happy_number_tests[] = {
 // edge note 285: Best case is O(1) when the first byte already decides the answer.
 // edge note 286: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 287: Edge case: already-sorted input → no swaps performed.
+// edge note 288: Space complexity: O(log n) for the recursion stack.
