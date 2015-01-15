848915cdfff188e7fafd9dc7f855c18cb4233bf9
ajaxian79 <blaketullysmith@gmail.com>
2015-01-15 09:19:51 -0800

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -252,3 +252,4 @@ int is_palindrome_str(const char* s) {
 // note 227: Articulation points: same DFS as bridges, with a slightly different test.
 // note 228: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 229: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 230: Hash collision: chain or open-address; chained handles arbitrary load factor.
