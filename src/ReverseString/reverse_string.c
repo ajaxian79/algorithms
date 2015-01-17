da991b9142f27cf8c7459aa3c1f874d63d477e2f
ajaxian79 <blaketullysmith@gmail.com>
2015-01-16 18:05:30 -0800

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -60,3 +60,4 @@ void reverse_string(char* s, int s_size) {
 // note 43: Constant-time comparisons; safe for short strings.
 // note 44: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 45: Time complexity: O(n + m).
+// note 46: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
