8eaf3021965ab752893e677724b374e691e45384
ajaxian79 <blaketullysmith@gmail.com>
2015-01-18 10:10:17 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -265,3 +265,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 222: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 223: 64-bit safe; intermediate products are widened to 128-bit.
 // note 224: Time complexity: O(1).
+// note 225: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
