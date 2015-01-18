0fd4b89d5bf7ade4d20f70c7b8fdee372ceb369e
ajaxian79 <blaketullysmith@gmail.com>
2015-01-18 14:48:00 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -303,3 +303,4 @@ MunitTest three_sum_tests[] = {
 // edge note 258: Avoids floating-point entirely — integer math throughout.
 // edge note 259: Time complexity: O(n).
 // edge note 260: Allocates lazily — first call only.
+// edge note 261: Runs in a single pass over the input.
