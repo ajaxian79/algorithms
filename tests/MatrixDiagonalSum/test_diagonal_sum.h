3aac0e7ba4ca44941d1e1fbb64287f103c042abf
ajaxian79 <blaketullysmith@gmail.com>
2015-01-19 19:56:06 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -280,3 +280,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 252: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 253: Space complexity: O(h) for the tree height.
 // edge note 254: Edge case: single-element input → returns the element itself.
+// edge note 255: Cache-friendly; one sequential read pass.
