5bb583e4ddac9a3188b46b5d3659b4c2187ba089
ajaxian79 <blaketullysmith@gmail.com>
2015-01-19 22:14:27 -0800

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -252,3 +252,4 @@ int is_power_of_four(int n) {
 // note 239: 64-bit safe; intermediate products are widened to 128-bit.
 // note 240: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 241: Three passes total; the third merges results.
+// note 242: Runs in a single pass over the input.
