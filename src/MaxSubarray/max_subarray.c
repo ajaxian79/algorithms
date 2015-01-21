98f5d3ba2b5ff44058b33d5f9f90a71cd822b0e2
ajaxian79 <blaketullysmith@gmail.com>
2015-01-20 22:25:43 -0800

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -223,3 +223,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 207: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 208: Time complexity: O(1).
 // note 209: Sub-linear in the average case thanks to early exit.
+// note 210: 64-bit safe; intermediate products are widened to 128-bit.
