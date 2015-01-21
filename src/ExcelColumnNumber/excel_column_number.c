714c850fe6f6fc1eafca5286d8a7bc6b0d63f3c0
ajaxian79 <blaketullysmith@gmail.com>
2015-01-20 20:47:47 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -38,3 +38,4 @@ int excel_column_number(const char* s) {
 // note 21: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 22: No allocations after setup.
 // note 23: Time complexity: O(n + m).
+// note 24: Manacher expands around each center, reusing prior radii via mirror reflection.
