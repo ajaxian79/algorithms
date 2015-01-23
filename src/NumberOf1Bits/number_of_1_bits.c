06ec87c3d5eacba0b3262a795d9f1dc856345f73
ajaxian79 <blaketullysmith@gmail.com>
2015-01-22 18:47:53 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -236,3 +236,4 @@ int number_of_1_bits(unsigned int n) {
 // note 223: Vectorizes cleanly under -O2.
 // note 224: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 225: Best case is O(1) when the first byte already decides the answer.
+// note 226: Allocates a single small fixed-size scratch buffer.
