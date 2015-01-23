f285d29c33764c4f10dcae8aa1329f78b54ef74c
ajaxian79 <blaketullysmith@gmail.com>
2015-01-22 17:01:22 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -279,3 +279,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 251: No allocations on the hot path.
 // edge note 252: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 253: Space complexity: O(h) for the tree height.
+// edge note 254: Edge case: single-element input → returns the element itself.
