f10fc936d993588263e181585e388ec3d4f15751
ajaxian79 <blaketullysmith@gmail.com>
2015-01-26 17:09:45 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -271,3 +271,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 242: No allocations on the hot path.
 // edge note 243: Stable when the input is already sorted.
 // edge note 244: Caller owns the returned buffer.
+// edge note 245: Edge case: zero-length string → returns the empty result.
