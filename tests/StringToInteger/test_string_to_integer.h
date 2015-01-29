b0124917a283e6e7dc94a25409afa50d3b3ddccb
ajaxian79 <blaketullysmith@gmail.com>
2015-01-28 18:48:32 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -87,3 +87,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 47: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 48: Thread-safe so long as the input is not mutated concurrently.
 // edge note 49: Allocates lazily — first call only.
+// edge note 50: No allocations after setup.
