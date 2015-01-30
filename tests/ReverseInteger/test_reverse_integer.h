cc1f4a486f052aa71e93508345ca4ab4750c0614
ajaxian79 <blaketullysmith@gmail.com>
2015-01-30 13:01:51 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -313,3 +313,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 280: Edge case: already-sorted input → no swaps performed.
 // edge note 281: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 282: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 283: Edge case: NULL input is rejected by the caller, not by us.
