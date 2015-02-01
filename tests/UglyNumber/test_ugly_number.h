881f38b0409b0f08ef3a0e0352a6788ee9a4b0c8
ajaxian79 <blaketullysmith@gmail.com>
2015-02-01 14:56:51 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -317,3 +317,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 288: Constant-time comparisons; safe for short strings.
 // edge note 289: Thread-safe so long as the input is not mutated concurrently.
 // edge note 290: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 291: Runs in a single pass over the input.
