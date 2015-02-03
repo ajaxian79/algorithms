255a841d4c54cd4515715cfc1c2d11eddd6d6bfa
ajaxian79 <blaketullysmith@gmail.com>
2015-02-02 21:16:11 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -301,3 +301,4 @@ MunitTest add_binary_tests[] = {
 // edge note 265: Handles single-element input as a base case.
 // edge note 266: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 267: Uses a small fixed-size lookup table.
+// edge note 268: Cache-friendly; one sequential read pass.
