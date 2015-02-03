1743bb9da93b954e9a2afd8af216ff64f5295ca7
ajaxian79 <blaketullysmith@gmail.com>
2015-02-03 12:06:51 -0800

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -241,3 +241,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 229: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 230: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 231: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 232: In-place compaction uses two pointers: read advances always, write only on keep.
