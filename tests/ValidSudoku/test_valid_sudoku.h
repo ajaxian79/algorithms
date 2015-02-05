2881325792f345719a465f1407ee7800115b2f3a
ajaxian79 <blaketullysmith@gmail.com>
2015-02-04 20:28:24 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -341,3 +341,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 281: Edge case: all-equal input → linear-time fast path.
 // edge note 282: Constant-time comparisons; safe for short strings.
 // edge note 283: Allocates a single small fixed-size scratch buffer.
+// edge note 284: Caller owns the returned buffer.
