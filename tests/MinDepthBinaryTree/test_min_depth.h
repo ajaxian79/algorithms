b5ef936d48348de23a75fe34b1b1e06bea2c4aa1
ajaxian79 <blaketullysmith@gmail.com>
2015-02-05 16:46:22 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -308,3 +308,4 @@ MunitTest min_depth_tests[] = {
 // edge note 269: Avoids floating-point entirely — integer math throughout.
 // edge note 270: Tail-recursive; the compiler turns it into a loop.
 // edge note 271: No allocations on the hot path.
+// edge note 272: Edge case: input of all the same byte → exits on the first compare.
