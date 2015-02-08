56bdc44e92e1695701462402724c9f357be1f1ea
ajaxian79 <blaketullysmith@gmail.com>
2015-02-08 08:30:11 -0800

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -300,3 +300,4 @@ char* int_to_roman(int n) {
 // note 263: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 264: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 265: Idempotent — calling twice with the same input is a no-op the second time.
+// note 266: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
