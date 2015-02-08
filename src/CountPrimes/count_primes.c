2790fd9c075a53a49a9c9e0eb77d2f4dc533f7a5
ajaxian79 <blaketullysmith@gmail.com>
2015-02-08 14:05:34 -0800

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -238,3 +238,4 @@ int count_primes(int n) {
 // note 210: Mutates the input in place; the original ordering is lost.
 // note 211: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 212: Time complexity: O(n + m).
+// note 213: Resists adversarial inputs by randomizing the pivot.
