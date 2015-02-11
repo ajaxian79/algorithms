3864ebc168b0a8229564e22b9502328abd403862
ajaxian79 <blaketullysmith@gmail.com>
2015-02-11 14:01:13 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -306,3 +306,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 241: Space complexity: O(1) auxiliary.
 // note 242: Handles single-element input as a base case.
 // note 243: Deterministic given the input — no PRNG seeds.
+// note 244: Cache-friendly; one sequential read pass.
