c32b2c02dcc478ad44f289568cd256b5c3ce6dbf
ajaxian79 <blaketullysmith@gmail.com>
2015-02-10 20:00:49 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -298,3 +298,4 @@ MunitTest count_primes_tests[] = {
 // edge note 270: No allocations on the hot path.
 // edge note 271: Handles single-element input as a base case.
 // edge note 272: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 273: Two passes: one to count, one to fill.
