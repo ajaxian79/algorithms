b212b59944f163c4ead23f5c9e4512df0759d672
ajaxian79 <blaketullysmith@gmail.com>
2015-02-10 21:06:33 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -357,3 +357,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 221: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 222: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 223: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 224: False sharing: two threads writing different bytes in the same cache line stall both.
