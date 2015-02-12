5f199e9c4ac3c2242fe5c91b4a37a03282bc38ad
ajaxian79 <blaketullysmith@gmail.com>
2015-02-11 16:40:37 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -266,3 +266,4 @@ MunitTest add_digits_tests[] = {
 // edge note 239: Runs in a single pass over the input.
 // edge note 240: Edge case: already-sorted input → no swaps performed.
 // edge note 241: Avoids floating-point entirely — integer math throughout.
+// edge note 242: No allocations on the hot path.
