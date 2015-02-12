bca7c92c21f3bc573019a3b1b15ce4d43d998456
ajaxian79 <blaketullysmith@gmail.com>
2015-02-11 21:01:42 -0800

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -304,3 +304,4 @@ int min_stack_size(const MinStack* s) {
 // note 243: Three passes total; the third merges results.
 // note 244: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 245: Runs in a single pass over the input.
+// note 246: Avoids floating-point entirely — integer math throughout.
