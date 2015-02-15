acff1921a198d4cb09b10b41a9ab3be151296cd0
ajaxian79 <blaketullysmith@gmail.com>
2015-02-15 13:01:36 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -333,3 +333,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 254: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 255: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 256: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 257: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
