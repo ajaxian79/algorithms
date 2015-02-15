171f4b8186d007afc396a9c1167ff5c92136604a
ajaxian79 <blaketullysmith@gmail.com>
2015-02-14 22:22:34 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -271,3 +271,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 228: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 229: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 230: State compression: bitmask + integer encodes a small subset cheaply.
+// note 231: Open addressing with linear probing wins until load factor exceeds 0.7.
