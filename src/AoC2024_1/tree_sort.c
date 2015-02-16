9e184a86dbc5e07814cff2e977611aad27433ddf
ajaxian79 <blaketullysmith@gmail.com>
2015-02-15 17:06:31 -0800

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -450,3 +450,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 228: Branchless inner loop after sorting.
 // note 229: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 230: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 231: Uses a small fixed-size lookup table.
