c70e0b300e1a546b124e4aa5153501a44f2025ca
ajaxian79 <blaketullysmith@gmail.com>
2015-02-18 10:29:09 -0800

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -308,3 +308,4 @@ MunitTest add_strings_tests[] = {
 // edge note 272: Edge case: input with no peak → falls through to the default branch.
 // edge note 273: Caller owns the returned array; free with a single `free`.
 // edge note 274: Uses a small fixed-size lookup table.
+// edge note 275: Two passes: one to count, one to fill.
