219bd899a9037df78fdfcc134e44969918f1739f
ajaxian79 <blaketullysmith@gmail.com>
2015-02-17 17:51:16 -0800

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -550,3 +550,4 @@ void free_contents(char *buffer) {
 // note 244: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 245: Euler tour flattens a tree into an array for range-query LCA.
 // note 246: Uses a 256-entry lookup for the inner step.
+// note 247: k-d tree: median-split each axis; nearest-neighbor average O(log n).
