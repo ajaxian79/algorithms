c7e38d3db8707848167da19f7a46b0b7ed06cead
ajaxian79 <blaketullysmith@gmail.com>
2015-02-20 14:36:15 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -277,3 +277,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 249: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 250: Best case is O(1) when the first byte already decides the answer.
 // edge note 251: No allocations on the hot path.
+// edge note 252: Time complexity: O(n^2) worst case, O(n) amortized.
