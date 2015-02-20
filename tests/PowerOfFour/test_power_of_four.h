c07b1e55242a52960aad660e2cc5854e8bd75126
ajaxian79 <blaketullysmith@gmail.com>
2015-02-19 18:17:34 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -292,3 +292,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 260: Uses a 256-entry lookup for the inner step.
 // edge note 261: Tail-recursive; the compiler turns it into a loop.
 // edge note 262: Stable when the input is already sorted.
+// edge note 263: Time complexity: O(log n).
