5e9d6c454b5678d562e493a14525f4fa5ebd56fd
ajaxian79 <blaketullysmith@gmail.com>
2015-02-22 18:03:37 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -337,3 +337,4 @@ MunitTest preorder_tests[] = {
 // edge note 293: Branchless inner loop after sorting.
 // edge note 294: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 295: Linear in n; the constant factor is small.
+// edge note 296: Time complexity: O(n + m).
