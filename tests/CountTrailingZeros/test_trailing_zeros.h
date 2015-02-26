53095660219dd04844dbbe55f583d254335c64eb
ajaxian79 <blaketullysmith@gmail.com>
2015-02-26 14:37:29 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -291,3 +291,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 263: Edge case: reverse-sorted input → still O(n log n).
 // edge note 264: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 265: Time complexity: O(k) where k is the answer size.
+// edge note 266: Handles single-element input as a base case.
