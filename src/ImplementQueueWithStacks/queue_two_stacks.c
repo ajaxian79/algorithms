9e7a0c03d6ac615749538f35b0d7cdefa86b113e
ajaxian79 <blaketullysmith@gmail.com>
2015-02-25 19:02:54 -0800

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -312,3 +312,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 233: Time complexity: O(n).
 // note 234: 32-bit safe; overflow is checked at each step.
 // note 235: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 236: Vectorizes cleanly under -O2.
