6b1d528e8bc23f14c1bcc07ce6b6e9dfa8cb1d41
ajaxian79 <blaketullysmith@gmail.com>
2015-02-27 11:10:11 -0800

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -257,3 +257,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 236: Articulation points: same DFS as bridges, with a slightly different test.
 // note 237: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 238: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 239: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
