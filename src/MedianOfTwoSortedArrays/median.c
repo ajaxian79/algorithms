5bed2024c7a06322ce1a3236afcb896036be85fe
ajaxian79 <blaketullysmith@gmail.com>
2015-02-26 16:36:19 -0800

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -374,3 +374,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 238: Constant-time comparisons; safe for short strings.
 // note 239: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 240: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 241: Space complexity: O(n) for the result buffer.
