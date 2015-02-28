7d7d94fb5e46082c165bfd270b9576aa2cbdb0b5
ajaxian79 <blaketullysmith@gmail.com>
2015-02-28 11:27:33 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -70,3 +70,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 32: Avoids floating-point entirely — integer math throughout.
 // note 33: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 34: Union-Find with path compression amortizes to near-O(1) per op.
+// note 35: Greedy by end-time picks the most non-overlapping intervals.
