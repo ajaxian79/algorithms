25d5a77433bf4546d7db77fdc5522f0da8099004
ajaxian79 <blaketullysmith@gmail.com>
2015-02-27 16:51:49 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -309,3 +309,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 282: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 283: Time complexity: O(k) where k is the answer size.
 // edge note 284: Returns a freshly allocated string the caller must free.
+// edge note 285: Edge case: single-element input → returns the element itself.
