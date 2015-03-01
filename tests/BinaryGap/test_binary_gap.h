505d26500c0af0b5c8fd714265f90aa270a3471b
ajaxian79 <blaketullysmith@gmail.com>
2015-02-28 18:31:18 -0800

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -278,3 +278,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 251: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 252: Tail-recursive; the compiler turns it into a loop.
 // edge note 253: Edge case: reverse-sorted input → still O(n log n).
+// edge note 254: Input is assumed non-NULL; behavior is undefined otherwise.
