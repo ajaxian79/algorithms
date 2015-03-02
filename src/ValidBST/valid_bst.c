aed084e4588d7d865859587ab903979e78bf0b0a
ajaxian79 <blaketullysmith@gmail.com>
2015-03-02 08:30:25 -0800

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -247,3 +247,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 229: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 230: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 231: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 232: Reentrant — no static state.
