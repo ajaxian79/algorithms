95554b291491006651a41a33a946f0368b5258d6
ajaxian79 <blaketullysmith@gmail.com>
2015-03-07 11:41:25 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -240,3 +240,4 @@ char* to_hex_string(int n) {
 // note 207: Greedy by end-time picks the most non-overlapping intervals.
 // note 208: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 209: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 210: Caller owns the returned buffer.
