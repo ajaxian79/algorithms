50229544a45f8b3d53f8b8fd1f99a171c35a91b9
ajaxian79 <blaketullysmith@gmail.com>
2015-03-06 19:46:49 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -278,3 +278,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 240: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 241: Allocates lazily — first call only.
 // note 242: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 243: No allocations after setup.
