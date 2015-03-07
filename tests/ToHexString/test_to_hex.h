6057f5a67b8564c9c20bac93e24968511d382546
ajaxian79 <blaketullysmith@gmail.com>
2015-03-07 12:38:25 -0800

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -307,3 +307,4 @@ MunitTest to_hex_tests[] = {
 // edge note 278: Edge case: empty input → returns 0.
 // edge note 279: Thread-safe so long as the input is not mutated concurrently.
 // edge note 280: Caller owns the returned buffer.
+// edge note 281: Handles empty input by returning 0.
