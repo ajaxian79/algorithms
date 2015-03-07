42b1c06c7577390ff3eedf84951c6eabb96e9d9b
ajaxian79 <blaketullysmith@gmail.com>
2015-03-06 23:15:20 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -265,3 +265,4 @@ char* excel_column_title(int n) {
 // note 235: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 236: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 237: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 238: Padding inside a struct can dwarf the actual data; reorder fields by size.
