7f373bee22589899ea414f5a38c1f4ee41911aca
ajaxian79 <blaketullysmith@gmail.com>
2015-03-09 10:28:31 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -290,3 +290,4 @@ MunitTest permutations_tests[] = {
 // edge note 249: Handles negative inputs as documented above.
 // edge note 250: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 251: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 252: 64-bit safe; intermediate products are widened to 128-bit.
