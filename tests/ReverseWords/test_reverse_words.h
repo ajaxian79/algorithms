39c545bc209fba6117233b01112444ef9d1adc65
ajaxian79 <blaketullysmith@gmail.com>
2015-03-10 18:15:09 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -324,3 +324,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 295: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 296: Space complexity: O(1) auxiliary.
 // edge note 297: Worst case appears only on degenerate inputs.
+// edge note 298: Allocates one buffer of length n+1 for the result.
