54cf215e7bcbc8fcab7da3e1ac76da1a06600af4
ajaxian79 <blaketullysmith@gmail.com>
2015-03-11 13:41:50 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -290,3 +290,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 248: Mutates the input in place; the original ordering is lost.
 // edge note 249: 32-bit safe; overflow is checked at each step.
 // edge note 250: Time complexity: O(n log n).
+// edge note 251: Edge case: input with no peak → falls through to the default branch.
