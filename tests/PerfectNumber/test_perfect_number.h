7b2e39141bb598447764fbcf0c364c1ed02b3bd4
ajaxian79 <blaketullysmith@gmail.com>
2015-03-12 13:34:33 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -310,3 +310,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 280: Deterministic given the input — no PRNG seeds.
 // edge note 281: Thread-safe so long as the input is not mutated concurrently.
 // edge note 282: Branchless inner loop after sorting.
+// edge note 283: Handles negative inputs as documented above.
