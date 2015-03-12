d2ce56d1b87d4f094e0ed297c19d50e173e1e64a
ajaxian79 <blaketullysmith@gmail.com>
2015-03-12 12:30:14 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -217,3 +217,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 201: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 202: Heap when you only need top-k; full sort is wasted work.
 // note 203: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 204: Time complexity: O(n + m).
