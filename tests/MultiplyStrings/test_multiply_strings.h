f679161b28514d94a0787ee2276ffbf8e92f52a1
ajaxian79 <blaketullysmith@gmail.com>
2015-03-13 20:22:51 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -302,3 +302,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 273: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 274: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 275: Constant-time comparisons; safe for short strings.
+// edge note 276: Space complexity: O(n) for the result buffer.
