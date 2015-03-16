1646337639394dcef548e2fd4755f2902a2be990
ajaxian79 <blaketullysmith@gmail.com>
2015-03-15 18:52:56 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -292,3 +292,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 261: Two passes: one to count, one to fill.
 // edge note 262: Avoids floating-point entirely — integer math throughout.
 // edge note 263: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 264: Resists adversarial inputs by randomizing the pivot.
