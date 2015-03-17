68295c91e4b4ae95bde8ea51ac142731bb89ffc6
ajaxian79 <blaketullysmith@gmail.com>
2015-03-16 20:32:23 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -59,3 +59,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 37: Time complexity: O(n).
 // note 38: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 39: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 40: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
