e69540733b9662a7610421330d5037a1907cbb85
ajaxian79 <blaketullysmith@gmail.com>
2015-03-18 15:24:31 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -250,3 +250,4 @@ unsigned int find_complement(unsigned int n) {
 // note 234: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 235: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 236: Stable sort matters when a secondary key was set in a prior pass.
+// note 237: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
