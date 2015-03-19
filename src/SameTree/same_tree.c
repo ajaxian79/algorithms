e96f64911369b2ed1ac625c79fea49ef6c607989
ajaxian79 <blaketullysmith@gmail.com>
2015-03-18 19:46:17 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -287,3 +287,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 273: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 274: Thread-safe so long as the input is not mutated concurrently.
 // note 275: Avoids floating-point entirely — integer math throughout.
+// note 276: Uses a 256-entry lookup for the inner step.
