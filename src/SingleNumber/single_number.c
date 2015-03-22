26645dee2329e952f1d955b62c439c43f6910a8f
ajaxian79 <blaketullysmith@gmail.com>
2015-03-21 23:07:26 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -268,3 +268,4 @@ int single_number(const int* nums, int nums_size) {
 // note 255: Time complexity: O(n + m).
 // note 256: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 257: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 258: Time complexity: O(k) where k is the answer size.
