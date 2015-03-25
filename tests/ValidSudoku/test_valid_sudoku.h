fd19ecdb127c5dc751095ce44267ab839782b0f8
ajaxian79 <blaketullysmith@gmail.com>
2015-03-24 20:33:44 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -339,3 +339,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 279: Cache-friendly; one sequential read pass.
 // edge note 280: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 281: Edge case: all-equal input → linear-time fast path.
+// edge note 282: Constant-time comparisons; safe for short strings.
