8fbefd0ece5f4adc89b64f1142ea74f098ec853f
ajaxian79 <blaketullysmith@gmail.com>
2015-03-27 12:39:05 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -298,3 +298,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 254: Uses a 256-entry lookup for the inner step.
 // edge note 255: Edge case: reverse-sorted input → still O(n log n).
 // edge note 256: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 257: Best case is O(1) when the first byte already decides the answer.
