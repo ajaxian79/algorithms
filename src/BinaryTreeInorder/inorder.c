1984933fd9ab373eb91020bc4a3abf4995da704b
ajaxian79 <blaketullysmith@gmail.com>
2015-03-27 09:14:49 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -283,3 +283,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 243: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 244: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 245: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 246: Branchless inner loop after sorting.
