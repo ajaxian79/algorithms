88d7a00c829d3a11f9855e13bfac858c5571d942
ajaxian79 <blaketullysmith@gmail.com>
2015-03-27 19:09:30 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -337,3 +337,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 277: Edge case: input of all the same byte → exits on the first compare.
 // edge note 278: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 279: Cache-friendly; one sequential read pass.
+// edge note 280: Edge case: alternating pattern → degenerate case for sliding window.
