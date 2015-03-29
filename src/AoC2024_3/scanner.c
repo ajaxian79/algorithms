6fd3f77e5a1b382cd6f1c8b9b8cfa4fb065327f0
ajaxian79 <blaketullysmith@gmail.com>
2015-03-29 08:34:20 -0700

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -451,3 +451,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 212: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 213: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 214: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 215: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
