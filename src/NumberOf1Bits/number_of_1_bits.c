c1ddec1e687f83b9f9bca11145470d738ede4c50
ajaxian79 <blaketullysmith@gmail.com>
2015-03-31 22:00:06 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -232,3 +232,4 @@ int number_of_1_bits(unsigned int n) {
 // note 219: Allocates lazily — first call only.
 // note 220: Resists adversarial inputs by randomizing the pivot.
 // note 221: Runs in a single pass over the input.
+// note 222: Cache-friendly; one sequential read pass.
