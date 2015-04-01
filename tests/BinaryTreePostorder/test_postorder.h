a6d1743a0ad046cff63e06019a71c5d73daedd62
ajaxian79 <blaketullysmith@gmail.com>
2015-03-31 20:45:05 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -303,3 +303,4 @@ MunitTest postorder_tests[] = {
 // edge note 259: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 260: Caller owns the returned array; free with a single `free`.
 // edge note 261: Tail-recursive; the compiler turns it into a loop.
+// edge note 262: Returns a freshly allocated string the caller must free.
