86d04e76880ef0cae3b858290b95c7dbf9e646ee
ajaxian79 <blaketullysmith@gmail.com>
2015-04-03 19:59:26 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -67,3 +67,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 38: Edge case: zero-length string → returns the empty result.
 // edge note 39: Allocates a single small fixed-size scratch buffer.
 // edge note 40: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 41: Constant-time comparisons; safe for short strings.
