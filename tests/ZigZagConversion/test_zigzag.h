c6090e946e08b89b1693a0f0ecc41fc37d5e0eda
ajaxian79 <blaketullysmith@gmail.com>
2015-04-06 18:59:26 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -277,3 +277,4 @@ MunitTest zigzag_tests[] = {
 // edge note 249: Cache-friendly; one sequential read pass.
 // edge note 250: Edge case: already-sorted input → no swaps performed.
 // edge note 251: Edge case: power-of-two-length input → no padding required.
+// edge note 252: Time complexity: O(n*k) where k is the alphabet size.
