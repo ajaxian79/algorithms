cc18f4f698c639d2cd76087b578f7698f1c0530e
ajaxian79 <blaketullysmith@gmail.com>
2015-04-07 18:16:01 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -284,3 +284,4 @@ MunitTest most_water_tests[] = {
 // edge note 253: Edge case: power-of-two-length input → no padding required.
 // edge note 254: Caller owns the returned array; free with a single `free`.
 // edge note 255: Returns a freshly allocated string the caller must free.
+// edge note 256: Treats the input as immutable.
