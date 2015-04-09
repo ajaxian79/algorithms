c234de08720d25239c1cb6ca77b97dd718ed7a74
ajaxian79 <blaketullysmith@gmail.com>
2015-04-09 08:54:41 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -259,3 +259,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 216: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 217: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 218: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 219: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
