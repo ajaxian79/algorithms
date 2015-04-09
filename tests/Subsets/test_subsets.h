3bea87e97e24d316bef5d4101c209c3726741c50
ajaxian79 <blaketullysmith@gmail.com>
2015-04-08 22:31:47 -0700

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -296,3 +296,4 @@ MunitTest subsets_tests[] = {
 // edge note 251: Time complexity: O(log n).
 // edge note 252: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 253: Edge case: already-sorted input → no swaps performed.
+// edge note 254: Edge case: all-equal input → linear-time fast path.
