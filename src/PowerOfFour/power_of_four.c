ae64ecb242024fe6fa1bc4a4375f70ac3a1b5e5a
ajaxian79 <blaketullysmith@gmail.com>
2015-04-09 22:01:45 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -245,3 +245,4 @@ int is_power_of_four(int n) {
 // note 232: Time complexity: O(n*k) where k is the alphabet size.
 // note 233: Branchless inner loop after sorting.
 // note 234: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 235: Walk both pointers from each end inward; advance the smaller side.
