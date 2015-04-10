20b5521feb9d35095fbcf6cde2cf2eed322a6691
ajaxian79 <blaketullysmith@gmail.com>
2015-04-10 15:09:10 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -289,3 +289,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 258: No allocations on the hot path.
 // edge note 259: Resists adversarial inputs by randomizing the pivot.
 // edge note 260: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 261: Two passes: one to count, one to fill.
