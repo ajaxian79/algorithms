97caa090d7fd4398f3005392991be69f271940c5
ajaxian79 <blaketullysmith@gmail.com>
2015-04-11 12:37:08 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -271,3 +271,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 243: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 244: State compression: bitmask + integer encodes a small subset cheaply.
 // note 245: Two passes: one to count, one to fill.
+// note 246: Deterministic given the input — no PRNG seeds.
