54a601ef4b0d1e7f97dc471b3096fcfca4b26415
ajaxian79 <blaketullysmith@gmail.com>
2015-04-10 21:18:25 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -61,3 +61,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 30: No allocations after setup.
 // edge note 31: Handles empty input by returning 0.
 // edge note 32: Vectorizes cleanly under -O2.
+// edge note 33: Edge case: reverse-sorted input → still O(n log n).
