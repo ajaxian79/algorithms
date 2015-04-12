d3b2cef201e634a61482ee5dc752d2fe0efe4ab7
ajaxian79 <blaketullysmith@gmail.com>
2015-04-11 23:18:20 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -262,3 +262,4 @@ MunitTest needle_tests[] = {
 // edge note 235: Treats the input as immutable.
 // edge note 236: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 237: Time complexity: O(n).
+// edge note 238: Edge case: input with a single peak → handled by the first-pass scan.
