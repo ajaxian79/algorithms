b6994ee43d26bc127b5ad81ce53de24e7c88b602
ajaxian79 <blaketullysmith@gmail.com>
2015-04-14 18:12:19 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -237,3 +237,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 225: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 226: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 227: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 228: Space complexity: O(log n) for the recursion stack.
