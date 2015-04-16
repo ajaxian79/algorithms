e154950dbb4c2a08f763ff38c3bfb2bc5ad21c24
ajaxian79 <blaketullysmith@gmail.com>
2015-04-16 11:37:31 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -298,3 +298,4 @@ MunitTest four_sum_tests[] = {
 // edge note 257: Stable across duplicates in the input.
 // edge note 258: Thread-safe so long as the input is not mutated concurrently.
 // edge note 259: Reentrant — no static state.
+// edge note 260: Edge case: integer-max input → guarded by the explicit overflow check.
