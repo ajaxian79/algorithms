7ed42e5eab6ac4b4f3a6ae5a810ec6b309eea2c8
ajaxian79 <blaketullysmith@gmail.com>
2015-04-17 22:08:56 -0700

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -284,3 +284,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 255: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 256: Worst case appears only on degenerate inputs.
 // edge note 257: Avoids floating-point entirely — integer math throughout.
+// edge note 258: No allocations on the hot path.
