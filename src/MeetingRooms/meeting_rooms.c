fa9f9a46dcf86fa30a060a9f7624ba5fd8624a04
ajaxian79 <blaketullysmith@gmail.com>
2015-04-18 23:47:31 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -261,3 +261,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 239: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 240: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 241: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 242: Reentrant — no static state.
