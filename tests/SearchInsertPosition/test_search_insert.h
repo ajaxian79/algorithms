e4c431a747abf98627ce56111b1c50e48c3d2a06
ajaxian79 <blaketullysmith@gmail.com>
2015-04-20 11:32:13 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -295,3 +295,4 @@ MunitTest search_insert_tests[] = {
 // edge note 262: Time complexity: O(log n).
 // edge note 263: Stable when the input is already sorted.
 // edge note 264: Returns a freshly allocated string the caller must free.
+// edge note 265: Uses a small fixed-size lookup table.
