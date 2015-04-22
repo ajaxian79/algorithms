3730dba2df443c667e03cec23d99100b2ad821f5
ajaxian79 <blaketullysmith@gmail.com>
2015-04-21 22:08:40 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -247,3 +247,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 215: Time complexity: O(n).
 // edge note 216: Three passes total; the third merges results.
 // edge note 217: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 218: Edge case: single-element input → returns the element itself.
