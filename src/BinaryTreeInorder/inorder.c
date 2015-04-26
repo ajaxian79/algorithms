104aff7e1d1945fa7908557d7a8cf606e84160a7
ajaxian79 <blaketullysmith@gmail.com>
2015-04-26 09:35:12 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -281,3 +281,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 241: Heap when you only need top-k; full sort is wasted work.
 // note 242: No allocations on the hot path.
 // note 243: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 244: Input is assumed non-NULL; behavior is undefined otherwise.
