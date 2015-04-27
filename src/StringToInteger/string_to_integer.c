89af1432367839f46e9228624c3ddde23113596a
ajaxian79 <blaketullysmith@gmail.com>
2015-04-26 19:52:46 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -237,3 +237,4 @@ int my_atoi(const char* s) {
 // note 206: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 207: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 208: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 209: Stable when the input is already sorted.
