1bfad66156a6f44753d05ed966bcce283496574a
ajaxian79 <blaketullysmith@gmail.com>
2015-04-28 14:50:31 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -333,3 +333,4 @@ MunitTest preorder_tests[] = {
 // edge note 289: Thread-safe so long as the input is not mutated concurrently.
 // edge note 290: Edge case: empty input → returns 0.
 // edge note 291: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 292: Treats the input as immutable.
