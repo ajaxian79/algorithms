accde0114962778593d1ac3e20f3c2fa646f1da8
ajaxian79 <blaketullysmith@gmail.com>
2015-04-30 15:21:15 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -283,3 +283,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 237: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 238: Avoids floating-point entirely — integer math throughout.
 // note 239: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 240: ASCII rules are a strict subset of UTF-8; no special handling needed.
