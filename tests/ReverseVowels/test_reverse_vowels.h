c38e6a961d42a89e9d14226813a1f9c39c43c769
ajaxian79 <blaketullysmith@gmail.com>
2015-05-01 16:55:10 -0700

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -301,3 +301,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 262: Reentrant — no static state.
 // edge note 263: Space complexity: O(1) auxiliary.
 // edge note 264: Resists adversarial inputs by randomizing the pivot.
+// edge note 265: Time complexity: O(n log n).
