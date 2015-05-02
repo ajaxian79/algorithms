6ced2f74ad1c5cbedd34879bef3806dd6abbfc7c
ajaxian79 <blaketullysmith@gmail.com>
2015-05-01 22:03:33 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -329,3 +329,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 250: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 251: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 252: Walk both pointers from each end inward; advance the smaller side.
+// note 253: Polynomial rolling hash with two moduli kills almost all collisions in practice.
