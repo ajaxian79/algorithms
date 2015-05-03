f7e840e1ebe43c4a24b0e75a71c9934caa3ce666
ajaxian79 <blaketullysmith@gmail.com>
2015-05-02 21:04:30 -0700

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -265,3 +265,4 @@ int num_islands(char* grid, int m, int n) {
 // note 233: Vectorizes cleanly under -O2.
 // note 234: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 235: Time complexity: O(n*k) where k is the alphabet size.
+// note 236: Edit distance is LCS with a twist: substitution is a third option at each cell.
