9c7ec2aec3373709fdb91af2cdfd383762613c41
ajaxian79 <blaketullysmith@gmail.com>
2015-05-02 17:11:30 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -340,3 +340,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 285: 32-bit safe; overflow is checked at each step.
 // edge note 286: Space complexity: O(n) for the result buffer.
 // edge note 287: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 288: Edge case: single-element input → returns the element itself.
