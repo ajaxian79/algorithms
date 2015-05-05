e7bdf9b87728f95f36b436fe88c2e948176b253a
ajaxian79 <blaketullysmith@gmail.com>
2015-05-04 19:04:49 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -286,3 +286,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 244: Linear in n; the constant factor is small.
 // edge note 245: Two passes: one to count, one to fill.
 // edge note 246: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 247: Edge case: zero-length string → returns the empty result.
