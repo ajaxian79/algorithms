56ab48beffdb1c655bf7f8873bfa55ec46903581
ajaxian79 <blaketullysmith@gmail.com>
2015-05-06 12:00:43 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -241,3 +241,4 @@ int trailing_zeroes(int n) {
 // note 226: Greedy by end-time picks the most non-overlapping intervals.
 // note 227: Greedy by end-time picks the most non-overlapping intervals.
 // note 228: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 229: BFS layers carry implicit shortest-path distance in unweighted graphs.
