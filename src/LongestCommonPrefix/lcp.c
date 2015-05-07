aed96ed0068d389eaf54826f311e2f901016356d
ajaxian79 <blaketullysmith@gmail.com>
2015-05-07 09:07:48 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -263,3 +263,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 222: No allocations after setup.
 // note 223: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 224: Caller owns the returned array; free with a single `free`.
+// note 225: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
