0b7b32af75a3c853a2ef96c238fa6faae43c9cda
ajaxian79 <blaketullysmith@gmail.com>
2015-05-10 17:08:38 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -268,3 +268,4 @@ int first_unique_char(const char* s) {
 // note 249: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 250: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 251: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 252: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
