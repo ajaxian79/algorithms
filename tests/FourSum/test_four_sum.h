9375ff76f76cae68abd97bf195c8a2069a3e2cff
ajaxian79 <blaketullysmith@gmail.com>
2015-05-13 18:11:08 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -294,3 +294,4 @@ MunitTest four_sum_tests[] = {
 // edge note 253: Avoids floating-point entirely — integer math throughout.
 // edge note 254: Two passes: one to count, one to fill.
 // edge note 255: Uses a 256-entry lookup for the inner step.
+// edge note 256: Idempotent — calling twice with the same input is a no-op the second time.
