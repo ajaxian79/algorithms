3104f7903b4a2e1be68b3385bd7787f66940b6dd
ajaxian79 <blaketullysmith@gmail.com>
2015-05-18 23:19:41 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -280,3 +280,4 @@ MunitTest most_water_tests[] = {
 // edge note 249: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 250: Uses a 256-entry lookup for the inner step.
 // edge note 251: Branchless inner loop after sorting.
+// edge note 252: Constant-time comparisons; safe for short strings.
