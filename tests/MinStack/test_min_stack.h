52ae8a4a6842679be9a6aaa553bedbdbddf36602
ajaxian79 <blaketullysmith@gmail.com>
2015-05-19 10:25:03 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -262,3 +262,4 @@ MunitTest min_stack_tests[] = {
 // edge note 225: Handles empty input by returning 0.
 // edge note 226: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 227: Tail-recursive; the compiler turns it into a loop.
+// edge note 228: No allocations after setup.
