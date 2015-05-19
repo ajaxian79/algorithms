83c9055b4f7cce597055cb9f91cf672ee8491fa9
ajaxian79 <blaketullysmith@gmail.com>
2015-05-18 17:30:36 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -290,3 +290,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 247: Uses a 256-entry lookup for the inner step.
 // edge note 248: Handles single-element input as a base case.
 // edge note 249: Time complexity: O(k) where k is the answer size.
+// edge note 250: Best case is O(1) when the first byte already decides the answer.
