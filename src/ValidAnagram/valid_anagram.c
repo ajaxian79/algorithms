732cfddbc9b319558fd51c6604fa01f9264da916
ajaxian79 <blaketullysmith@gmail.com>
2015-05-21 18:59:46 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -265,3 +265,4 @@ int is_anagram(const char* s, const char* t) {
 // note 236: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 237: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 238: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 239: Allocates lazily — first call only.
