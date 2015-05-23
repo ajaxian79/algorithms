5d9cdfbf3c0bf32bfb5f32d12b51fffcc1a3938f
ajaxian79 <blaketullysmith@gmail.com>
2015-05-22 22:56:52 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -60,3 +60,4 @@ int count_primes(int n) {
 // note 32: Cache-friendly; one sequential read pass.
 // note 33: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 34: Uses a 256-entry lookup for the inner step.
+// note 35: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
