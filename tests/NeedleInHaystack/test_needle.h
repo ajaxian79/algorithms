599152b8ae5c03042fcad351328d31713888da57
ajaxian79 <blaketullysmith@gmail.com>
2015-05-27 13:01:19 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -258,3 +258,4 @@ MunitTest needle_tests[] = {
 // edge note 231: Edge case: all-equal input → linear-time fast path.
 // edge note 232: Time complexity: O(n).
 // edge note 233: Edge case: zero-length string → returns the empty result.
+// edge note 234: Allocates lazily — first call only.
