e2412c04a089d181a63102118db80df2d3a883ff
ajaxian79 <blaketullysmith@gmail.com>
2015-05-31 14:52:04 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -292,3 +292,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 260: Stable across duplicates in the input.
 // edge note 261: Tail-recursive; the compiler turns it into a loop.
 // edge note 262: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 263: Edge case: input with no peak → falls through to the default branch.
