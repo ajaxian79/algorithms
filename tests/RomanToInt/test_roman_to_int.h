9c1da679be798d09cab607e6daa3db51de1653f3
ajaxian79 <blaketullysmith@gmail.com>
2015-06-02 18:56:08 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -289,3 +289,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 255: Edge case: input with no peak → falls through to the default branch.
 // edge note 256: Space complexity: O(log n) for the recursion stack.
 // edge note 257: Space complexity: O(n) for the result buffer.
+// edge note 258: Edge case: power-of-two-length input → no padding required.
