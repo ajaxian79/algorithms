30bab4cce6f1919b477cdb755fa67634deda0875
ajaxian79 <blaketullysmith@gmail.com>
2015-06-04 09:58:48 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -273,3 +273,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 246: Caller owns the returned array; free with a single `free`.
 // edge note 247: Time complexity: O(k) where k is the answer size.
 // edge note 248: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 249: Treats the input as immutable.
