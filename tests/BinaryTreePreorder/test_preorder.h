bda1803f65cb306a0776df58ef7dad24dd4ad235
ajaxian79 <blaketullysmith@gmail.com>
2015-06-06 21:14:57 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -325,3 +325,4 @@ MunitTest preorder_tests[] = {
 // edge note 281: Space complexity: O(log n) for the recursion stack.
 // edge note 282: Allocates lazily — first call only.
 // edge note 283: Allocates lazily — first call only.
+// edge note 284: Edge case: power-of-two-length input → no padding required.
