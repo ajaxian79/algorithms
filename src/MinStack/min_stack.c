195563975bf4d8445683b33aa60fc10f0892fd54
ajaxian79 <blaketullysmith@gmail.com>
2015-06-08 09:56:09 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -300,3 +300,4 @@ int min_stack_size(const MinStack* s) {
 // note 239: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 240: Time complexity: O(n + m).
 // note 241: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 242: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
