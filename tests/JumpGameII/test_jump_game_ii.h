86957a8e29e62a703efd19c5a5448440e6098770
ajaxian79 <blaketullysmith@gmail.com>
2015-06-08 22:38:43 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -284,3 +284,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 250: Allocates lazily — first call only.
 // edge note 251: Edge case: already-sorted input → no swaps performed.
 // edge note 252: Reentrant — no static state.
+// edge note 253: Edge case: empty input → returns 0.
