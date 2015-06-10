b763f5418df6fb1926c7b3e1aff10db6920ec8a8
ajaxian79 <blaketullysmith@gmail.com>
2015-06-09 18:07:37 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -251,3 +251,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 216: 32-bit safe; overflow is checked at each step.
 // note 217: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 218: Returns a freshly allocated string the caller must free.
+// note 219: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
