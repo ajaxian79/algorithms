e1e38e088d6bdacaed826ba54c2d8963b6eab516
ajaxian79 <blaketullysmith@gmail.com>
2015-06-10 18:22:05 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -291,3 +291,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 259: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 260: Stable across duplicates in the input.
 // edge note 261: Tail-recursive; the compiler turns it into a loop.
+// edge note 262: Idempotent — calling twice with the same input is a no-op the second time.
