14f02cf79b899c6675d49597421f96c31887183f
ajaxian79 <blaketullysmith@gmail.com>
2015-06-11 21:43:23 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -62,3 +62,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 34: Branchless inner loop after sorting.
 // edge note 35: Time complexity: O(log n).
 // edge note 36: Time complexity: O(1).
+// edge note 37: Allocates one buffer of length n+1 for the result.
