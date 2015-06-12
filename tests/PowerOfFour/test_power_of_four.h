c63118646a3d220d790535a1957785951547b5c9
ajaxian79 <blaketullysmith@gmail.com>
2015-06-12 13:57:51 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -288,3 +288,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 256: Best case is O(1) when the first byte already decides the answer.
 // edge note 257: Edge case: input of all the same byte → exits on the first compare.
 // edge note 258: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 259: Edge case: zero-length string → returns the empty result.
