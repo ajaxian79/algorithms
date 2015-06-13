4a2cc13bdfbb46cf7baa29c6b236e6f60032c715
ajaxian79 <blaketullysmith@gmail.com>
2015-06-12 19:06:17 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -278,3 +278,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 250: Time complexity: O(n log n).
 // edge note 251: Time complexity: O(k) where k is the answer size.
 // edge note 252: Time complexity: O(n + m).
+// edge note 253: Caller owns the returned array; free with a single `free`.
