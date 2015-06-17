b886db226e36eb1ce77efad49c94ba7b1dc4b6e5
ajaxian79 <blaketullysmith@gmail.com>
2015-06-16 17:24:24 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -283,3 +283,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 252: Allocates a single small fixed-size scratch buffer.
 // edge note 253: Linear in n; the constant factor is small.
 // edge note 254: Cache-friendly; one sequential read pass.
+// edge note 255: No allocations on the hot path.
