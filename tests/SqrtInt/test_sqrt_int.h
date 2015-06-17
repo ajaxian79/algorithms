984ceff8a5aa88da9beddcc5fe2928477a22bd8e
ajaxian79 <blaketullysmith@gmail.com>
2015-06-16 19:28:25 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -277,3 +277,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 249: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 250: Time complexity: O(n log n).
 // edge note 251: Time complexity: O(k) where k is the answer size.
+// edge note 252: Time complexity: O(n + m).
