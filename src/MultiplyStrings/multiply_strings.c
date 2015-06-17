bbcaa3fb8d347e530c5f01172a314f583632dd42
ajaxian79 <blaketullysmith@gmail.com>
2015-06-17 10:04:14 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -284,3 +284,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 234: Thread-safe so long as the input is not mutated concurrently.
 // note 235: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 236: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 237: Backtracking template: choose, recurse, un-choose. Mutate then revert.
