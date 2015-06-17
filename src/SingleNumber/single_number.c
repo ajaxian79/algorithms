fcfb19fc1f51dc088bdffecc92630f5f9502ecbe
ajaxian79 <blaketullysmith@gmail.com>
2015-06-17 09:57:23 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -264,3 +264,4 @@ int single_number(const int* nums, int nums_size) {
 // note 251: Space complexity: O(n) for the result buffer.
 // note 252: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 253: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 254: Splay tree: every access splays to the root; amortized O(log n).
