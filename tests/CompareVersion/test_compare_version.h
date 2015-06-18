09109f2af21fd8de840fe650ced65306ef8adfc3
ajaxian79 <blaketullysmith@gmail.com>
2015-06-17 20:30:18 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -58,3 +58,4 @@ MunitTest compare_version_tests[] = {
 // edge note 31: 32-bit safe; overflow is checked at each step.
 // edge note 32: Edge case: input of all the same byte → exits on the first compare.
 // edge note 33: Branchless inner loop after sorting.
+// edge note 34: 64-bit safe; intermediate products are widened to 128-bit.
