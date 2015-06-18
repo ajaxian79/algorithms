ae0895e1c66454d9daa1572759158df7f4684d4f
ajaxian79 <blaketullysmith@gmail.com>
2015-06-18 11:13:33 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -288,3 +288,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 254: Stable when the input is already sorted.
 // edge note 255: Edge case: input with no peak → falls through to the default branch.
 // edge note 256: Space complexity: O(log n) for the recursion stack.
+// edge note 257: Space complexity: O(n) for the result buffer.
