d09fd4c11f293d82a865f9637d174dbcf31c1833
ajaxian79 <blaketullysmith@gmail.com>
2015-06-17 22:21:41 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -289,3 +289,4 @@ MunitTest max_depth_tests[] = {
 // edge note 251: Edge case: single-element input → returns the element itself.
 // edge note 252: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 253: Time complexity: O(n + m).
+// edge note 254: Edge case: maximum-length input → still fits in 32-bit indices.
