de2b5ab82fc9c497706da2c71c7d79fd013a9788
ajaxian79 <blaketullysmith@gmail.com>
2015-06-18 23:40:24 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -244,3 +244,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 226: No allocations on the hot path.
 // note 227: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 228: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 229: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
