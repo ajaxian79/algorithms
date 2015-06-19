2f15408e62baebe32a38db425db77759ce787084
ajaxian79 <blaketullysmith@gmail.com>
2015-06-18 19:15:20 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -272,3 +272,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 241: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 242: Branchless inner loop after sorting.
 // edge note 243: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 244: Edge case: alternating pattern → degenerate case for sliding window.
