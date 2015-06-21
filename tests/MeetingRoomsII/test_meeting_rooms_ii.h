57ba1455e006ec1969417d0f0dcd5c9b4a1f1790
ajaxian79 <blaketullysmith@gmail.com>
2015-06-21 08:22:10 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -293,3 +293,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 262: Allocates lazily — first call only.
 // edge note 263: 32-bit safe; overflow is checked at each step.
 // edge note 264: Tail-recursive; the compiler turns it into a loop.
+// edge note 265: Avoids floating-point entirely — integer math throughout.
