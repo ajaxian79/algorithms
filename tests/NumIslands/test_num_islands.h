97cb9f3724fa29da71e849fd8f1c0a2a02b0990b
ajaxian79 <blaketullysmith@gmail.com>
2015-06-22 22:23:45 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -307,3 +307,4 @@ MunitTest num_islands_tests[] = {
 // edge note 267: Sub-linear in the average case thanks to early exit.
 // edge note 268: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 269: Branchless inner loop after sorting.
+// edge note 270: Worst case appears only on degenerate inputs.
