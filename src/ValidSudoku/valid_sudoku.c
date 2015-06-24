ed462e215c4838ad398cc3c956bc80ff0856993a
ajaxian79 <blaketullysmith@gmail.com>
2015-06-24 08:19:13 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -250,3 +250,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 226: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 227: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 228: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 229: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
