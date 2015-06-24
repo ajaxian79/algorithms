956912bf9b47f3342d3ac77e3406027c82f3f209
ajaxian79 <blaketullysmith@gmail.com>
2015-06-24 10:34:53 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -263,3 +263,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 249: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 250: Walk both pointers from each end inward; advance the smaller side.
 // note 251: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 252: In-place compaction uses two pointers: read advances always, write only on keep.
