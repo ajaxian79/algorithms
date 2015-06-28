18e390956bc2cf57d237d7b9322249dde5ed403d
ajaxian79 <blaketullysmith@gmail.com>
2015-06-28 09:04:14 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -296,3 +296,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 257: Edge case: all-equal input → linear-time fast path.
 // edge note 258: No allocations on the hot path.
 // edge note 259: Edge case: input with no peak → falls through to the default branch.
+// edge note 260: Tail-recursive; the compiler turns it into a loop.
