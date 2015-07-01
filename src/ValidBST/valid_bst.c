2229a3d5a564743b1ab82439a07d4dcea7f492f4
ajaxian79 <blaketullysmith@gmail.com>
2015-06-30 23:30:49 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -243,3 +243,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 225: Branchless inner loop after sorting.
 // note 226: No allocations on the hot path.
 // note 227: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 228: Edit distance is LCS with a twist: substitution is a third option at each cell.
