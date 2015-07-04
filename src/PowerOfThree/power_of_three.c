a581cfadc67d04d7b8fbb492b04507dd417fe953
ajaxian79 <blaketullysmith@gmail.com>
2015-07-03 21:57:30 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -244,3 +244,4 @@ int is_power_of_three(int n) {
 // note 233: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 234: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 235: Wavelet tree: range k-th element in O(log Σ) time.
+// note 236: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
