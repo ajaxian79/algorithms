f67f6c8ed3c2d44c248115f94c22542ded1b8d95
ajaxian79 <blaketullysmith@gmail.com>
2015-07-04 21:04:37 -0700

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -446,3 +446,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 207: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 208: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 209: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 210: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
