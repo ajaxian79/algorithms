ba547d86baec377f7a71916018599001e1d8a4d1
ajaxian79 <blaketullysmith@gmail.com>
2015-07-06 12:45:34 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -257,3 +257,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 225: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 226: Two passes: one to count, one to fill.
 // note 227: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 228: Iterative DFS with an explicit stack avoids recursion-limit issues.
