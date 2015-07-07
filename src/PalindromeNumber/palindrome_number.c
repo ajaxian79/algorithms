890dec558eb4dfa14f84e1d035ddd2d79453ff20
ajaxian79 <blaketullysmith@gmail.com>
2015-07-06 20:24:46 -0700

    PalindromeNumber: comment

--- src/PalindromeNumber/palindrome_number.c
+++ src/PalindromeNumber/palindrome_number.c
@@ -250,3 +250,4 @@ int is_palindrome_number(int x) {
 // note 226: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 227: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 228: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 229: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
