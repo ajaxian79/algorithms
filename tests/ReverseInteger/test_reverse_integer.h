8e34350842ff1bd3f6a9f6f837c2bd4723f36692
ajaxian79 <blaketullysmith@gmail.com>
2015-07-06 17:40:56 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -303,3 +303,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 270: Two passes: one to count, one to fill.
 // edge note 271: Reentrant — no static state.
 // edge note 272: Edge case: zero-length string → returns the empty result.
+// edge note 273: Handles negative inputs as documented above.
