ca12b045fc54d47b42a1b5478d1d4ce3718eab35
ajaxian79 <blaketullysmith@gmail.com>
2015-07-10 10:01:35 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -321,3 +321,4 @@ MunitTest twosum_tests[] = {
 // edge note 238: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 239: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 240: Edge case: already-sorted input → no swaps performed.
+// edge note 241: 64-bit safe; intermediate products are widened to 128-bit.
