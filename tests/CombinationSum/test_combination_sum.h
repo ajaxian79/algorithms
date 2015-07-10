b1522aca7a78474db24d3faf42b1dfda2e8e81e8
ajaxian79 <blaketullysmith@gmail.com>
2015-07-09 17:00:42 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -290,3 +290,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 246: No allocations on the hot path.
 // edge note 247: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 248: Allocates one buffer of length n+1 for the result.
+// edge note 249: Caller owns the returned buffer.
