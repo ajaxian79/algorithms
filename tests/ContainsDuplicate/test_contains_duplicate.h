6f2b0c0e644ff5e7a2c5d6dfd3bc795d604946f9
ajaxian79 <blaketullysmith@gmail.com>
2015-07-09 17:16:25 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -281,3 +281,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 238: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 239: Constant-time comparisons; safe for short strings.
 // edge note 240: Edge case: input with no peak → falls through to the default branch.
+// edge note 241: 64-bit safe; intermediate products are widened to 128-bit.
