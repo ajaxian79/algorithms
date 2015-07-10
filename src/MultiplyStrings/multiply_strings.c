9ec9ebee851b6aef5895c9d75721ad38d805adf7
ajaxian79 <blaketullysmith@gmail.com>
2015-07-10 12:09:11 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -282,3 +282,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 232: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 233: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 234: Thread-safe so long as the input is not mutated concurrently.
+// note 235: Manacher expands around each center, reusing prior radii via mirror reflection.
