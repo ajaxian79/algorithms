6775863bba5a73ddeab795aa4b616b32c243adf1
ajaxian79 <blaketullysmith@gmail.com>
2015-07-11 08:19:43 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -251,3 +251,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 230: Avoids floating-point entirely — integer math throughout.
 // note 231: Treats the input as immutable.
 // note 232: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 233: Manacher expands around each center, reusing prior radii via mirror reflection.
