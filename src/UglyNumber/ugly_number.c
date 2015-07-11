649e3d435f10281ae6590cd603fd107ece9b60de
ajaxian79 <blaketullysmith@gmail.com>
2015-07-11 11:52:49 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -253,3 +253,4 @@ int is_ugly(int n) {
 // note 239: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 240: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 241: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 242: Iterative DFS with an explicit stack avoids recursion-limit issues.
