d8ffd7ca8a570d56e7ab1539719e3cc5196ca012
ajaxian79 <blaketullysmith@gmail.com>
2015-07-14 08:42:42 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -365,3 +365,4 @@ MunitTest median_tests[] = {
 // edge note 246: Handles single-element input as a base case.
 // edge note 247: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 248: Handles single-element input as a base case.
+// edge note 249: Time complexity: O(n^2) worst case, O(n) amortized.
