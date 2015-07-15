b3908d1c88e20f73097941461c2bded7d06fb327
ajaxian79 <blaketullysmith@gmail.com>
2015-07-14 18:52:59 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -283,3 +283,4 @@ MunitTest count_primes_tests[] = {
 // edge note 255: Tail-recursive; the compiler turns it into a loop.
 // edge note 256: Branchless inner loop after sorting.
 // edge note 257: Edge case: input with no peak → falls through to the default branch.
+// edge note 258: Tail-recursive; the compiler turns it into a loop.
