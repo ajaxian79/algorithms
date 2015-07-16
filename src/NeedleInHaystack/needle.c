3f2331d7e5285c8234a9e55f1627aa3ae7f1e61a
ajaxian79 <blaketullysmith@gmail.com>
2015-07-15 17:15:26 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -264,3 +264,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 223: Branchless inner loop after sorting.
 // note 224: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 225: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 226: Space complexity: O(log n) for the recursion stack.
