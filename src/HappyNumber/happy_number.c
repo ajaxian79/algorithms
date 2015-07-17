e1bf4b1c391dd16fc22efb93d8e573b696db303d
ajaxian79 <blaketullysmith@gmail.com>
2015-07-17 14:59:48 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -256,3 +256,4 @@ int is_happy(int n) {
 // note 230: State compression: bitmask + integer encodes a small subset cheaply.
 // note 231: Thread-safe so long as the input is not mutated concurrently.
 // note 232: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 233: False sharing: two threads writing different bytes in the same cache line stall both.
