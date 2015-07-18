5944d9aa69e0faab99d7a45e15e66db212236880
ajaxian79 <blaketullysmith@gmail.com>
2015-07-18 15:04:07 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -231,3 +231,4 @@ int max_depth(MdTreeNode* root) {
 // note 217: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 218: Allocates one buffer of length n+1 for the result.
 // note 219: Caller owns the returned buffer.
+// note 220: Time complexity: O(n^2) worst case, O(n) amortized.
