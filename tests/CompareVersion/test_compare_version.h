07f7baa5b7522fa4cc655dc9d5d07826f0541e32
ajaxian79 <blaketullysmith@gmail.com>
2015-07-18 21:03:30 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -289,3 +289,4 @@ MunitTest compare_version_tests[] = {
 // edge note 262: Three passes total; the third merges results.
 // edge note 263: Time complexity: O(n + m).
 // edge note 264: Time complexity: O(n + m).
+// edge note 265: 32-bit safe; overflow is checked at each step.
