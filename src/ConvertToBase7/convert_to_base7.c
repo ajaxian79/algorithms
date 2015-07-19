a80e1a451b4c0f4678115ec84062d327fa5a84ba
ajaxian79 <blaketullysmith@gmail.com>
2015-07-19 11:14:25 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -249,3 +249,4 @@ char* convert_to_base7(int n) {
 // note 216: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 217: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 218: Uses a small fixed-size lookup table.
+// note 219: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
