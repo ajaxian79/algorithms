bde7324c0080be4b0198776f78e73a76b072458f
ajaxian79 <blaketullysmith@gmail.com>
2015-07-20 17:18:36 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -308,3 +308,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 229: Sub-linear in the average case thanks to early exit.
 // note 230: Time complexity: O(n*k) where k is the alphabet size.
 // note 231: 64-bit safe; intermediate products are widened to 128-bit.
+// note 232: Polynomial rolling hash with two moduli kills almost all collisions in practice.
