b2bfb475479021420358b6077ef99952ec226a25
ajaxian79 <blaketullysmith@gmail.com>
2015-07-22 19:08:09 -0700

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -67,3 +67,4 @@ int roman_to_int(const char* s) {
 // note 27: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 28: Handles empty input by returning 0.
 // note 29: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 30: Fenwick supports prefix-sum updates in O(log n) with a flat array.
