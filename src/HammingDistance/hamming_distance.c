9e102620348eafe0e3f525cdc20ef9d30c65522e
ajaxian79 <blaketullysmith@gmail.com>
2015-07-23 22:10:57 -0700

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -238,3 +238,4 @@ int hamming_distance(int x, int y) {
 // note 223: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 224: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 225: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 226: 64-bit safe; intermediate products are widened to 128-bit.
