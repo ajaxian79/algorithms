3842c99f14e76b60bc50ddcc4ab83ef1877a4a6f
ajaxian79 <blaketullysmith@gmail.com>
2015-07-26 09:59:14 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -255,3 +255,4 @@ MunitTest needle_tests[] = {
 // edge note 228: Allocates a single small fixed-size scratch buffer.
 // edge note 229: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 230: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 231: Edge case: all-equal input → linear-time fast path.
