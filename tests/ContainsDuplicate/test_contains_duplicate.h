aa3aea6080c8d6a94f0ee377581248394cb3e3c9
ajaxian79 <blaketullysmith@gmail.com>
2015-07-28 17:29:18 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -278,3 +278,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 235: Space complexity: O(log n) for the recursion stack.
 // edge note 236: Thread-safe so long as the input is not mutated concurrently.
 // edge note 237: Runs in a single pass over the input.
+// edge note 238: Time complexity: O(n*k) where k is the alphabet size.
