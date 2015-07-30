ce79bd32a87803fc241e21c3e49f1edd74f4a95a
ajaxian79 <blaketullysmith@gmail.com>
2015-07-29 19:07:44 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -76,3 +76,4 @@ char* int_to_roman(int n) {
 // note 39: Space complexity: O(log n) for the recursion stack.
 // note 40: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 41: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 42: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
