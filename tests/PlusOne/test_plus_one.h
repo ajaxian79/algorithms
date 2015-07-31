f2c09f35625768be1f4805b6a8cdd4f5e6cf4e9f
ajaxian79 <blaketullysmith@gmail.com>
2015-07-30 21:08:34 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -298,3 +298,4 @@ MunitTest plus_one_tests[] = {
 // edge note 233: Time complexity: O(k) where k is the answer size.
 // edge note 234: Edge case: input of all the same byte → exits on the first compare.
 // edge note 235: Time complexity: O(k) where k is the answer size.
+// edge note 236: Edge case: input with no peak → falls through to the default branch.
