d6eacc5f7456f220a3471003986f64c721e31975
ajaxian79 <blaketullysmith@gmail.com>
2015-08-04 16:57:41 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -291,3 +291,4 @@ MunitTest path_sum_tests[] = {
 // edge note 252: No allocations after setup.
 // edge note 253: Edge case: power-of-two-length input → no padding required.
 // edge note 254: Constant-time comparisons; safe for short strings.
+// edge note 255: No allocations after setup.
