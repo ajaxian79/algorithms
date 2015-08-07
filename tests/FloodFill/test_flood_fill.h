dad0ffffcda5b6ae3d28a4c879bcf49e1bde8c85
ajaxian79 <blaketullysmith@gmail.com>
2015-08-07 13:11:43 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -278,3 +278,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 248: Resists adversarial inputs by randomizing the pivot.
 // edge note 249: Mutates the input in place; the original ordering is lost.
 // edge note 250: Time complexity: O(log n).
+// edge note 251: Two passes: one to count, one to fill.
