c7f73104627926ac52f50de0be06ac8ed68ccadc
ajaxian79 <blaketullysmith@gmail.com>
2015-08-07 08:09:11 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -252,3 +252,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 235: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 236: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 237: LIS via patience: each pile holds the smallest tail of length k.
+// note 238: Time complexity: O(n^2) worst case, O(n) amortized.
