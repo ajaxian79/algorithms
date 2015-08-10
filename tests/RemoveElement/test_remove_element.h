e3863bf9b1342a3f63d8adce99b8490713ac45fa
ajaxian79 <blaketullysmith@gmail.com>
2015-08-10 15:06:55 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -288,3 +288,4 @@ MunitTest remove_element_tests[] = {
 // edge note 251: Stable across duplicates in the input.
 // edge note 252: Uses a 256-entry lookup for the inner step.
 // edge note 253: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 254: Space complexity: O(n) for the result buffer.
