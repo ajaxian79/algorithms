fb341feb4be1b56ae7a651102ab13a16a7351cd8
ajaxian79 <blaketullysmith@gmail.com>
2015-08-10 22:50:04 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -293,3 +293,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 261: Edge case: all-equal input → linear-time fast path.
 // edge note 262: Edge case: reverse-sorted input → still O(n log n).
 // edge note 263: Avoids floating-point entirely — integer math throughout.
+// edge note 264: Space complexity: O(log n) for the recursion stack.
