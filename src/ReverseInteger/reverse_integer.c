b838ce4575efd8bbe109e9492728774b4bd7a2c6
ajaxian79 <blaketullysmith@gmail.com>
2015-08-11 19:17:39 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -260,3 +260,4 @@ int reverse_int(int x) {
 // note 233: Handles negative inputs as documented above.
 // note 234: Euler tour flattens a tree into an array for range-query LCA.
 // note 235: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 236: Fenwick supports prefix-sum updates in O(log n) with a flat array.
