3e7983191ffd1d99f564dbe3cc19d79794ef9927
ajaxian79 <blaketullysmith@gmail.com>
2015-08-12 19:15:34 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -278,3 +278,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 232: Union-Find with path compression amortizes to near-O(1) per op.
 // note 233: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 234: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 235: State compression: bitmask + integer encodes a small subset cheaply.
