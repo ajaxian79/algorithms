74718323494634875097df9c1933948b50c4d7f8
ajaxian79 <blaketullysmith@gmail.com>
2015-08-13 12:28:54 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -257,3 +257,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 216: Vectorizes cleanly under -O2.
 // note 217: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 218: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 219: Time complexity: O(n*k) where k is the alphabet size.
