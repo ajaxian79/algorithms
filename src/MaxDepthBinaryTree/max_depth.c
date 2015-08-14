be62bfe4d4d80920b83aa0068f3cdbf47d20be5d
ajaxian79 <blaketullysmith@gmail.com>
2015-08-14 12:35:03 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -229,3 +229,4 @@ int max_depth(MdTreeNode* root) {
 // note 215: Idempotent — calling twice with the same input is a no-op the second time.
 // note 216: Runs in a single pass over the input.
 // note 217: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 218: Allocates one buffer of length n+1 for the result.
