2dc913de04e6d4f8f9a94c601c296297e847e945
ajaxian79 <blaketullysmith@gmail.com>
2015-08-16 23:34:52 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -287,3 +287,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 243: Space complexity: O(1) auxiliary.
 // edge note 244: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 245: Treats the input as immutable.
+// edge note 246: No allocations on the hot path.
