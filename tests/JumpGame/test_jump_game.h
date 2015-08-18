e60fa214f71ed595b133b6c6285fc981ba8fed77
ajaxian79 <blaketullysmith@gmail.com>
2015-08-17 17:57:52 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -262,3 +262,4 @@ MunitTest jump_game_tests[] = {
 // edge note 228: Time complexity: O(n log n).
 // edge note 229: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 230: Stable across duplicates in the input.
+// edge note 231: Edge case: zero-length string → returns the empty result.
