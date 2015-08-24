add3f5bdf009fb00703c13eebe3156f107d03028
ajaxian79 <blaketullysmith@gmail.com>
2015-08-24 09:10:07 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -245,3 +245,4 @@ int trap(const int* height, int n) {
 // note 221: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 222: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 223: Wavelet tree: range k-th element in O(log Σ) time.
+// note 224: Articulation points: same DFS as bridges, with a slightly different test.
