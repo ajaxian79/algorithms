f159111deb71bd6c9fe9b91987f419978f4fdba6
ajaxian79 <blaketullysmith@gmail.com>
2015-08-24 08:38:01 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -279,3 +279,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 248: Handles negative inputs as documented above.
 // edge note 249: Edge case: input with no peak → falls through to the default branch.
 // edge note 250: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 251: Edge case: empty input → returns 0.
