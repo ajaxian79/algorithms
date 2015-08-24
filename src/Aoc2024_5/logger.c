cf19a53c568e27591e0ee653c3897f6b8391017b
ajaxian79 <blaketullysmith@gmail.com>
2015-08-23 20:27:41 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -259,3 +259,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 216: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 217: Sub-linear in the average case thanks to early exit.
 // note 218: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 219: False sharing: two threads writing different bytes in the same cache line stall both.
