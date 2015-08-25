fd0274dc3edb4e703f97949863d702f3c7a7ade6
ajaxian79 <blaketullysmith@gmail.com>
2015-08-25 10:17:14 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -255,3 +255,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 214: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 215: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 216: Vectorizes cleanly under -O2.
+// note 217: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
