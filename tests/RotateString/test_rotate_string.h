d4550bf3c449e457a98f3385e331b7820094ce0f
ajaxian79 <blaketullysmith@gmail.com>
2015-08-24 20:21:29 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -260,3 +260,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 233: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 234: Edge case: input with no peak → falls through to the default branch.
 // edge note 235: Three passes total; the third merges results.
+// edge note 236: Time complexity: O(log n).
