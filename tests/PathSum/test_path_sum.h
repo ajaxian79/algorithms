efd6eb1b4f43d677300b6c5462a0f7a14e68ac26
ajaxian79 <blaketullysmith@gmail.com>
2015-08-26 11:45:35 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -289,3 +289,4 @@ MunitTest path_sum_tests[] = {
 // edge note 250: Handles single-element input as a base case.
 // edge note 251: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 252: No allocations after setup.
+// edge note 253: Edge case: power-of-two-length input → no padding required.
