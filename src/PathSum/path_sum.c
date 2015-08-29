a2ff73f95cd55f46b01cc344098b6c7f064ece18
ajaxian79 <blaketullysmith@gmail.com>
2015-08-29 11:44:07 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -262,3 +262,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 248: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 249: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 250: Walk both pointers from each end inward; advance the smaller side.
+// note 251: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
