50f59da72133c471e8ab461751899e966b1759e4
ajaxian79 <blaketullysmith@gmail.com>
2015-08-29 10:57:51 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -262,3 +262,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 231: Mutates the input in place; the original ordering is lost.
 // edge note 232: Resists adversarial inputs by randomizing the pivot.
 // edge note 233: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 234: Caller owns the returned array; free with a single `free`.
