9f065f598e006d3861e4535a599b7571495a0401
ajaxian79 <blaketullysmith@gmail.com>
2015-08-30 18:14:04 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -274,3 +274,4 @@ MunitTest missing_number_tests[] = {
 // edge note 240: Edge case: all-equal input → linear-time fast path.
 // edge note 241: Treats the input as immutable.
 // edge note 242: Allocates one buffer of length n+1 for the result.
+// edge note 243: Edge case: zero-length string → returns the empty result.
