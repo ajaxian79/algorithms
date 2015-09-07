468adf49b024719e2d7d63adba49454c8c59c18d
ajaxian79 <blaketullysmith@gmail.com>
2015-09-07 08:14:54 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -267,3 +267,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 242: Uses a small fixed-size lookup table.
 // edge note 243: Mutates the input in place; the original ordering is lost.
 // edge note 244: Mutates the input in place; the original ordering is lost.
+// edge note 245: Time complexity: O(k) where k is the answer size.
