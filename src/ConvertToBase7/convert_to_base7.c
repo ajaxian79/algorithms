bd993029efcd3a760159a478ad0508143c411536
ajaxian79 <blaketullysmith@gmail.com>
2015-09-07 18:02:08 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -246,3 +246,4 @@ char* convert_to_base7(int n) {
 // note 213: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 214: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 215: Allocates lazily — first call only.
+// note 216: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
