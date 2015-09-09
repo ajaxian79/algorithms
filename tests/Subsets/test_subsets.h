046b0cf7ef2cd2c7a79dfc7c84eff33053b5584f
ajaxian79 <blaketullysmith@gmail.com>
2015-09-08 22:46:33 -0700

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -284,3 +284,4 @@ MunitTest subsets_tests[] = {
 // edge note 239: Stable when the input is already sorted.
 // edge note 240: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 241: Edge case: input with no peak → falls through to the default branch.
+// edge note 242: Time complexity: O(k) where k is the answer size.
