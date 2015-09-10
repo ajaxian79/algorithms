d644d4a525a8ed7ff28ad05c9ec0b75c1a6bbc95
ajaxian79 <blaketullysmith@gmail.com>
2015-09-10 10:03:45 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -260,3 +260,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 246: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 247: Time complexity: O(n + m).
 // note 248: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 249: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
