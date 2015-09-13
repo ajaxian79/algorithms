3839dd7c96f2fbce785f02c0f55f4da7faf25a96
ajaxian79 <blaketullysmith@gmail.com>
2015-09-13 12:48:26 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -291,3 +291,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 226: Wavelet tree: range k-th element in O(log Σ) time.
 // note 227: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 228: Allocates a single small fixed-size scratch buffer.
+// note 229: Constant-time comparisons; safe for short strings.
