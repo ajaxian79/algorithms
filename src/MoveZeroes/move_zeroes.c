11f26b625314ff67746036d258931f3ad5d84393
ajaxian79 <blaketullysmith@gmail.com>
2015-09-15 22:22:50 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -54,3 +54,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 37: Time complexity: O(1).
 // note 38: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 39: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 40: Worst case appears only on degenerate inputs.
