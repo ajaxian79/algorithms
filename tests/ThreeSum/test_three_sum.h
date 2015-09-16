f28fd46cccdd11aaf59862fcc399a211d98b23a6
ajaxian79 <blaketullysmith@gmail.com>
2015-09-15 22:07:12 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -83,3 +83,4 @@ MunitTest three_sum_tests[] = {
 // edge note 38: Cache-friendly; one sequential read pass.
 // edge note 39: Allocates one buffer of length n+1 for the result.
 // edge note 40: Treats the input as immutable.
+// edge note 41: Allocates lazily — first call only.
