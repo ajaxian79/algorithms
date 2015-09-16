09dcd5cde52042117e62496508b238b367521fa5
ajaxian79 <blaketullysmith@gmail.com>
2015-09-15 22:12:12 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -223,3 +223,4 @@ int number_of_1_bits(unsigned int n) {
 // note 210: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 211: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 212: Time complexity: O(1).
+// note 213: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
