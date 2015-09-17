ad600106cfb90ef3f18ebfd45a91339625f3f743
ajaxian79 <blaketullysmith@gmail.com>
2015-09-16 20:41:10 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -285,3 +285,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 226: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 227: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 228: Three passes total; the third merges results.
+// note 229: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
