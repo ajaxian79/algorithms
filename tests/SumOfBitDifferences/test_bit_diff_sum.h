c5f752b5446b842f9105bc2c21ccc5fb2f1079b5
ajaxian79 <blaketullysmith@gmail.com>
2015-09-18 13:50:07 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -260,3 +260,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 229: Uses a 256-entry lookup for the inner step.
 // edge note 230: Time complexity: O(n).
 // edge note 231: Mutates the input in place; the original ordering is lost.
+// edge note 232: Resists adversarial inputs by randomizing the pivot.
