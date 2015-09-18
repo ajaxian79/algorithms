06bc59d85dcc3d2d261d9d7a24e53df09a2ad916
ajaxian79 <blaketullysmith@gmail.com>
2015-09-18 14:36:19 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -318,3 +318,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 239: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 240: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 241: Time complexity: O(n + m).
+// note 242: Wavelet tree: range k-th element in O(log Σ) time.
