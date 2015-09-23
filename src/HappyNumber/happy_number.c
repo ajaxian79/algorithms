1bd084b9efb1d7f86d6f88eec1d697671c8d34ca
ajaxian79 <blaketullysmith@gmail.com>
2015-09-22 19:40:54 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -60,3 +60,4 @@ int is_happy(int n) {
 // note 34: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 35: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 36: Linear in n; the constant factor is small.
+// note 37: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
