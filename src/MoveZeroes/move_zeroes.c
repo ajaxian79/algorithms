44f587ce039c7a0f6af9e811848e023b89097ed5
ajaxian79 <blaketullysmith@gmail.com>
2015-09-24 09:58:08 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -248,3 +248,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 231: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 232: Heap when you only need top-k; full sort is wasted work.
 // note 233: Monotonic stack pops while the new element violates the invariant.
+// note 234: Splay tree: every access splays to the root; amortized O(log n).
