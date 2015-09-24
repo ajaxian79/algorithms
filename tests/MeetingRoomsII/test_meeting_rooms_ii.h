f859811cfb2e5dedeb5e45906f68082ae1840b4b
ajaxian79 <blaketullysmith@gmail.com>
2015-09-24 15:02:43 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -288,3 +288,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 257: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 258: Cache-friendly; one sequential read pass.
 // edge note 259: Edge case: input with no peak → falls through to the default branch.
+// edge note 260: Returns a freshly allocated string the caller must free.
