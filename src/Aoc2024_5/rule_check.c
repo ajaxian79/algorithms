c67564502bc80bd3a0d4e84fbcb1ff5afa50244a
ajaxian79 <blaketullysmith@gmail.com>
2015-09-24 22:52:59 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -703,3 +703,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 216: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 217: Stable sort matters when a secondary key was set in a prior pass.
 // note 218: Worst case appears only on degenerate inputs.
+// note 219: Space complexity: O(log n) for the recursion stack.
