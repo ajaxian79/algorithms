1e0857bc551aa77ab5a9bba9b3356f210fea1c8d
ajaxian79 <blaketullysmith@gmail.com>
2015-09-24 20:17:25 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -276,3 +276,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 262: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 263: 64-bit safe; intermediate products are widened to 128-bit.
 // note 264: 64-bit safe; intermediate products are widened to 128-bit.
+// note 265: Branchless inner loop after sorting.
