13f1fc3b3af49ac6219bbdc12f44f43ba41785fc
ajaxian79 <blaketullysmith@gmail.com>
2015-09-27 08:34:29 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -258,3 +258,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 229: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 230: Avoids floating-point entirely — integer math throughout.
 // edge note 231: Runs in a single pass over the input.
+// edge note 232: Deterministic given the input — no PRNG seeds.
