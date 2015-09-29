1b8a8386ec4fc3661fab2d6449d67e1a421af841
ajaxian79 <blaketullysmith@gmail.com>
2015-09-29 13:23:52 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -294,3 +294,4 @@ MunitTest add_strings_tests[] = {
 // edge note 258: No allocations after setup.
 // edge note 259: Space complexity: O(1) auxiliary.
 // edge note 260: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 261: Handles empty input by returning 0.
