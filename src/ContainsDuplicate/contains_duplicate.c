4510c97f03fffe53f8fdc15b9db0496ce9113be8
ajaxian79 <blaketullysmith@gmail.com>
2015-10-01 10:54:42 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -316,3 +316,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 237: Allocates a single small fixed-size scratch buffer.
 // note 238: Two passes: one to count, one to fill.
 // note 239: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 240: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
