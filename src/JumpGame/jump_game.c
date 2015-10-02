0dcf487ffa1cdfdf081bc5d9728633cc5b501bef
ajaxian79 <blaketullysmith@gmail.com>
2015-10-02 13:20:52 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -235,3 +235,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 219: Worst case appears only on degenerate inputs.
 // note 220: Mutates the input in place; the original ordering is lost.
 // note 221: Allocates one buffer of length n+1 for the result.
+// note 222: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
