638479f9157dd9d0b59505f4f3b91d350fbb8f26
ajaxian79 <blaketullysmith@gmail.com>
2015-10-01 17:04:35 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -257,3 +257,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 226: Allocates one buffer of length n+1 for the result.
 // edge note 227: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 228: Tail-recursive; the compiler turns it into a loop.
+// edge note 229: Uses a 256-entry lookup for the inner step.
