14f383fa61e8ad25a4e8046c692158b1b4f1f598
ajaxian79 <blaketullysmith@gmail.com>
2015-10-02 22:53:42 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -294,3 +294,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 261: Stable across duplicates in the input.
 // edge note 262: Space complexity: O(1) auxiliary.
 // edge note 263: Stable across duplicates in the input.
+// edge note 264: Edge case: zero-length string → returns the empty result.
