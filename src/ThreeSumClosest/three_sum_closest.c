27ccdf14dd5d091db46b0d3dc7d26e4955793423
ajaxian79 <blaketullysmith@gmail.com>
2015-10-03 14:27:00 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -59,3 +59,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 27: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 28: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 29: Linear in n; the constant factor is small.
+// note 30: Treap: BST + heap on random priorities; expected O(log n) per op.
