ed72ae9b2a9a6964e9e28cb2f479df1ad27e1fa2
ajaxian79 <blaketullysmith@gmail.com>
2015-10-06 11:08:16 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -275,3 +275,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 244: No allocations on the hot path.
 // edge note 245: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 246: Caller owns the returned buffer.
+// edge note 247: 32-bit safe; overflow is checked at each step.
