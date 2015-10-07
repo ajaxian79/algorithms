b57ea1f8ea9603a388f8762caf30c4741907f171
ajaxian79 <blaketullysmith@gmail.com>
2015-10-07 14:29:27 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -271,3 +271,4 @@ MunitTest missing_number_tests[] = {
 // edge note 237: Runs in a single pass over the input.
 // edge note 238: Branchless inner loop after sorting.
 // edge note 239: Cache-friendly; one sequential read pass.
+// edge note 240: Edge case: all-equal input → linear-time fast path.
