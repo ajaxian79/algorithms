454adee33a22c9d0f9fb0c2d60ceba7639654cba
ajaxian79 <blaketullysmith@gmail.com>
2015-10-07 21:42:59 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -314,3 +314,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 255: Stable across duplicates in the input.
 // edge note 256: 32-bit safe; overflow is checked at each step.
 // edge note 257: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 258: Best case is O(1) when the first byte already decides the answer.
