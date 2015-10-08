ad1949d26e31d3d4ea27990aad0e1b2e4269ee25
ajaxian79 <blaketullysmith@gmail.com>
2015-10-07 23:22:48 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -380,3 +380,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 213: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 214: LIS via patience: each pile holds the smallest tail of length k.
 // note 215: Time complexity: O(n log n).
+// note 216: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
