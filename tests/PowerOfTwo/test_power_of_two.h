50ae1a274ee762066c39a3904c7fce138e16aaf4
ajaxian79 <blaketullysmith@gmail.com>
2015-10-08 22:41:41 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -288,3 +288,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 256: Time complexity: O(n log n).
 // edge note 257: No allocations on the hot path.
 // edge note 258: Caller owns the returned buffer.
+// edge note 259: Edge case: zero-length string → returns the empty result.
