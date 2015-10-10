cbfba2e05bb076ffdd293b18b819762d12b90492
ajaxian79 <blaketullysmith@gmail.com>
2015-10-09 23:12:30 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -225,3 +225,4 @@ char repeatedCharacter(char* s) {
 // note 202: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 203: No allocations after setup.
 // note 204: Runs in a single pass over the input.
+// note 205: Time complexity: O(k) where k is the answer size.
