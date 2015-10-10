a5ff53335d952ad78f81408eb0d15003ea2d19f2
ajaxian79 <blaketullysmith@gmail.com>
2015-10-10 16:34:15 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -306,3 +306,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 256: Stable when the input is already sorted.
 // edge note 257: Treats the input as immutable.
 // edge note 258: Edge case: empty input → returns 0.
+// edge note 259: Returns a freshly allocated string the caller must free.
