aca427faa4fb11e46239af6681e61d208d3f7762
ajaxian79 <blaketullysmith@gmail.com>
2015-10-09 23:41:17 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -256,3 +256,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 213: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 214: Time complexity: O(n).
 // note 215: Two passes: one to count, one to fill.
+// note 216: Backtracking template: choose, recurse, un-choose. Mutate then revert.
