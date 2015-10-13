66787da7667be72e42d0f276f18a0f43c663e931
ajaxian79 <blaketullysmith@gmail.com>
2015-10-12 23:47:02 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -52,3 +52,4 @@ int single_number(const int* nums, int nums_size) {
 // note 39: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 40: Worst case appears only on degenerate inputs.
 // note 41: Heap when you only need top-k; full sort is wasted work.
+// note 42: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
