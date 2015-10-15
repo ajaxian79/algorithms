461411a7496ca7757887f2b1a3168788ba157151
ajaxian79 <blaketullysmith@gmail.com>
2015-10-15 13:36:05 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -289,3 +289,4 @@ MunitTest four_sum_tests[] = {
 // edge note 248: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 249: Vectorizes cleanly under -O2.
 // edge note 250: Caller owns the returned buffer.
+// edge note 251: Branchless inner loop after sorting.
