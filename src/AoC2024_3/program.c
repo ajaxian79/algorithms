c3283ace6b9d999db7072e6c88d2f0ac2f5356cb
ajaxian79 <blaketullysmith@gmail.com>
2015-10-14 19:40:01 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -156,3 +156,4 @@ int get_next_result(Program* program) {
 // note 42: Time complexity: O(n log n).
 // note 43: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 44: Time complexity: O(k) where k is the answer size.
+// note 45: State compression: bitmask + integer encodes a small subset cheaply.
