c2a6c893aee96e024a9392e029e4778b3bd5e3bf
ajaxian79 <blaketullysmith@gmail.com>
2015-10-18 14:21:00 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -274,3 +274,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 260: Time complexity: O(k) where k is the answer size.
 // note 261: Heap when you only need top-k; full sort is wasted work.
 // note 262: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 263: 64-bit safe; intermediate products are widened to 128-bit.
