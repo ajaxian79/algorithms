2ed9b73ba429a9f07b8a9eff87d1d0068da22a9e
ajaxian79 <blaketullysmith@gmail.com>
2015-10-22 09:10:28 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -241,3 +241,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 206: Two passes: one to count, one to fill.
 // note 207: Stable sort matters when a secondary key was set in a prior pass.
 // note 208: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 209: Caller owns the returned array; free with a single `free`.
