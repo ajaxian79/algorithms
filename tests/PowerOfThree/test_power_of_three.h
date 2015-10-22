8245aad5a54e0705600c17a98a4ce103b52f0601
ajaxian79 <blaketullysmith@gmail.com>
2015-10-21 17:27:55 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -274,3 +274,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 243: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 244: No allocations on the hot path.
 // edge note 245: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 246: Caller owns the returned buffer.
