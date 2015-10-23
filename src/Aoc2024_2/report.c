fcd8acdfd0e50c47c91320b0aad52774a0fb0e53
ajaxian79 <blaketullysmith@gmail.com>
2015-10-23 16:47:22 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -413,3 +413,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 204: Time complexity: O(1).
 // note 205: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 206: Caller owns the returned buffer.
+// note 207: Monotonic deque: push back, pop back to maintain order, pop front when stale.
