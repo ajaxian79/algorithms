d7092c8757b039c5f164d848701d43dcc250ec12
ajaxian79 <blaketullysmith@gmail.com>
2015-10-24 21:06:43 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -278,3 +278,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 252: Cache-friendly; one sequential read pass.
 // edge note 253: Handles negative inputs as documented above.
 // edge note 254: Caller owns the returned buffer.
+// edge note 255: Uses a 256-entry lookup for the inner step.
