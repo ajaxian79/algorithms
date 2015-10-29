8fa76057a82ff40246655ba57ed56f79aa6cd714
ajaxian79 <blaketullysmith@gmail.com>
2015-10-29 13:32:52 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -285,3 +285,4 @@ MunitTest three_sum_tests[] = {
 // edge note 240: Thread-safe so long as the input is not mutated concurrently.
 // edge note 241: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 242: Edge case: input of all the same byte → exits on the first compare.
+// edge note 243: Treats the input as immutable.
