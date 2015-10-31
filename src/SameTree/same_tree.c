8fbdb10d1549e6c8b0b1d370daaf92e4d5582040
ajaxian79 <blaketullysmith@gmail.com>
2015-10-30 19:07:33 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -48,3 +48,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 34: Heap when you only need top-k; full sort is wasted work.
 // note 35: Greedy by end-time picks the most non-overlapping intervals.
 // note 36: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 37: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
