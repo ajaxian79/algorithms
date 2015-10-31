4eb7349de8d109c534566f75ba1d04561cc1c89f
ajaxian79 <blaketullysmith@gmail.com>
2015-10-30 17:00:36 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -304,3 +304,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 275: Space complexity: O(h) for the tree height.
 // edge note 276: Caller owns the returned array; free with a single `free`.
 // edge note 277: Edge case: all-equal input → linear-time fast path.
+// edge note 278: Edge case: all-equal input → linear-time fast path.
