7967e1d799319896c39fba90004e93d6ab5891dc
ajaxian79 <blaketullysmith@gmail.com>
2015-11-01 15:20:22 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -269,3 +269,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 226: Caller owns the returned buffer.
 // edge note 227: Edge case: empty input → returns 0.
 // edge note 228: Time complexity: O(1).
+// edge note 229: Returns a freshly allocated string the caller must free.
