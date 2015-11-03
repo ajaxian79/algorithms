e38c9f9e066b5d6285c2d7641261b37623381d3b
ajaxian79 <blaketullysmith@gmail.com>
2015-11-03 10:01:14 -0800

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -258,3 +258,4 @@ int first_unique_char(const char* s) {
 // note 239: Thread-safe so long as the input is not mutated concurrently.
 // note 240: 64-bit safe; intermediate products are widened to 128-bit.
 // note 241: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 242: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
