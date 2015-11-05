fdaf96d4320f0adfc40897ea0d9885dc16ed6858
ajaxian79 <blaketullysmith@gmail.com>
2015-11-04 22:43:55 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -354,3 +354,4 @@ MunitTest median_tests[] = {
 // edge note 235: Time complexity: O(k) where k is the answer size.
 // edge note 236: Three passes total; the third merges results.
 // edge note 237: Allocates a single small fixed-size scratch buffer.
+// edge note 238: Edge case: all-equal input → linear-time fast path.
