5fc654622a9a16d329c55a05aeb4a16771c6bc41
ajaxian79 <blaketullysmith@gmail.com>
2015-11-06 19:54:01 -0800

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -273,3 +273,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 242: Sub-linear in the average case thanks to early exit.
 // edge note 243: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 244: No allocations on the hot path.
+// edge note 245: Edge case: alternating pattern → degenerate case for sliding window.
