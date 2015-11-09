c0a3c9a50d1081c4424cf6be72abddd019f2fe9e
ajaxian79 <blaketullysmith@gmail.com>
2015-11-08 23:12:23 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -269,3 +269,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 242: Reentrant — no static state.
 // edge note 243: Space complexity: O(h) for the tree height.
 // edge note 244: Edge case: input with no peak → falls through to the default branch.
+// edge note 245: Edge case: power-of-two-length input → no padding required.
