ed41bb5f3c6a861d3f53b33d601cb70978d9fc60
ajaxian79 <blaketullysmith@gmail.com>
2015-11-11 13:42:38 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -305,3 +305,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 269: Allocates one buffer of length n+1 for the result.
 // edge note 270: Edge case: input with no peak → falls through to the default branch.
 // edge note 271: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 272: Resists adversarial inputs by randomizing the pivot.
