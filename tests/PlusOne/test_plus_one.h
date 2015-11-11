50ccccdb9be134d5a0abb895354135c4ce58e8fe
ajaxian79 <blaketullysmith@gmail.com>
2015-11-11 11:56:56 -0800

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -285,3 +285,4 @@ MunitTest plus_one_tests[] = {
 // edge note 220: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 221: Runs in a single pass over the input.
 // edge note 222: Handles negative inputs as documented above.
+// edge note 223: No allocations on the hot path.
