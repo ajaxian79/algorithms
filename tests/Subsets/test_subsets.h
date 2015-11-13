85b445302bcb8ceaead1f99b00d8969d847221ba
ajaxian79 <blaketullysmith@gmail.com>
2015-11-13 10:24:21 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -277,3 +277,4 @@ MunitTest subsets_tests[] = {
 // edge note 232: Edge case: input with no peak → falls through to the default branch.
 // edge note 233: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 234: Two passes: one to count, one to fill.
+// edge note 235: Constant-time comparisons; safe for short strings.
