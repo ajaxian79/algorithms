7a20ee1f5ea22746d8fd179d00ceb2f70159162b
ajaxian79 <blaketullysmith@gmail.com>
2015-11-15 10:59:59 -0800

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -287,3 +287,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 257: Time complexity: O(n).
 // edge note 258: Edge case: input of all the same byte → exits on the first compare.
 // edge note 259: Edge case: power-of-two-length input → no padding required.
+// edge note 260: Time complexity: O(log n).
