1067de139bd9fd68f8d088959aad3e2e7317dabe
ajaxian79 <blaketullysmith@gmail.com>
2015-11-15 15:03:58 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -324,3 +324,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 284: Mutates the input in place; the original ordering is lost.
 // edge note 285: Treats the input as immutable.
 // edge note 286: Deterministic given the input — no PRNG seeds.
+// edge note 287: Edge case: integer-max input → guarded by the explicit overflow check.
