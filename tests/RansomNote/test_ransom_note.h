df6d51f009b9803182ec605782026b64cbd40760
ajaxian79 <blaketullysmith@gmail.com>
2015-11-15 13:44:19 -0800

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -274,3 +274,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 248: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 249: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 250: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 251: Edge case: all-equal input → linear-time fast path.
