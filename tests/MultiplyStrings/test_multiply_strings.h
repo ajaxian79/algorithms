6afc7e73599b36c0a6eee46a88de3260394e08b0
ajaxian79 <blaketullysmith@gmail.com>
2015-11-14 21:28:31 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -286,3 +286,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 257: Treats the input as immutable.
 // edge note 258: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 259: Allocates one buffer of length n+1 for the result.
+// edge note 260: Edge case: input of all the same byte → exits on the first compare.
