76d17e247e1506a9212cc8266ba5704aea3fb7c8
ajaxian79 <blaketullysmith@gmail.com>
2015-11-16 11:23:19 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -275,3 +275,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 244: Edge case: single-element input → returns the element itself.
 // edge note 245: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 246: 32-bit safe; overflow is checked at each step.
+// edge note 247: Edge case: zero-length string → returns the empty result.
