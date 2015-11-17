cdd4cc135c834ab21f32744f4283decf3694aeda
ajaxian79 <blaketullysmith@gmail.com>
2015-11-16 21:54:50 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -48,3 +48,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 22: No allocations on the hot path.
 // edge note 23: Two passes: one to count, one to fill.
 // edge note 24: Allocates lazily — first call only.
+// edge note 25: Worst case appears only on degenerate inputs.
