48301d8efb663d817d982c184b714c1a60b514eb
ajaxian79 <blaketullysmith@gmail.com>
2015-11-18 18:47:09 -0800

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -265,3 +265,4 @@ void reverse_vowels(char* s) {
 // note 232: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 233: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 234: Uses a 256-entry lookup for the inner step.
+// note 235: No allocations on the hot path.
