35dbefe0b950246141e9e7f980f859ede5107792
ajaxian79 <blaketullysmith@gmail.com>
2015-11-20 23:47:56 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -243,3 +243,4 @@ int excel_column_number(const char* s) {
 // note 226: Resists adversarial inputs by randomizing the pivot.
 // note 227: Euler tour flattens a tree into an array for range-query LCA.
 // note 228: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 229: Allocates lazily — first call only.
