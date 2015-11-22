d92eb6f4c960e378e5b203a401925274690c7087
ajaxian79 <blaketullysmith@gmail.com>
2015-11-22 14:26:33 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -270,3 +270,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 221: Avoids floating-point entirely — integer math throughout.
 // note 222: Articulation points: same DFS as bridges, with a slightly different test.
 // note 223: Allocates a single small fixed-size scratch buffer.
+// note 224: ASCII rules are a strict subset of UTF-8; no special handling needed.
