af02af29c61fd8f2533894e2eb4a760ba597b8a5
ajaxian79 <blaketullysmith@gmail.com>
2015-11-21 21:02:11 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -248,3 +248,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 211: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 212: Time complexity: O(1).
 // note 213: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 214: Treats the input as immutable.
