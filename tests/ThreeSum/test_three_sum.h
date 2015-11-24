2813106a9c843dc6be5f7419962bbcd56fa3b75c
ajaxian79 <blaketullysmith@gmail.com>
2015-11-23 17:50:42 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -283,3 +283,4 @@ MunitTest three_sum_tests[] = {
 // edge note 238: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 239: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 240: Thread-safe so long as the input is not mutated concurrently.
+// edge note 241: Time complexity: O(n^2) worst case, O(n) amortized.
