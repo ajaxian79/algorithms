741a3ff743d1233422445f1f208baa1ea19c598b
ajaxian79 <blaketullysmith@gmail.com>
2015-11-25 22:55:15 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -288,3 +288,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 263: Uses a 256-entry lookup for the inner step.
 // edge note 264: Edge case: reverse-sorted input → still O(n log n).
 // edge note 265: Time complexity: O(k) where k is the answer size.
+// edge note 266: Cache-friendly; one sequential read pass.
