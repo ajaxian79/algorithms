cdd03ec01058d41e7cf5967dec9622b55ce919e3
ajaxian79 <blaketullysmith@gmail.com>
2015-11-25 17:01:02 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -262,3 +262,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 224: Two passes: one to count, one to fill.
 // note 225: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 226: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 227: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
