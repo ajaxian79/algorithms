5173f3bbb9d7371ada070cd17e50cda9251caa38
ajaxian79 <blaketullysmith@gmail.com>
2015-11-26 21:47:50 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -264,3 +264,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 219: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 220: Time complexity: O(n*k) where k is the alphabet size.
 // note 221: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 222: Union-Find with path compression amortizes to near-O(1) per op.
