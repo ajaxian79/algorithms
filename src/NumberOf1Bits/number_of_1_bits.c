2a836f8cdf615f9bc1161eef097a54ee73f4c800
ajaxian79 <blaketullysmith@gmail.com>
2015-11-28 16:36:46 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -45,3 +45,4 @@ int number_of_1_bits(unsigned int n) {
 // note 32: Euler tour flattens a tree into an array for range-query LCA.
 // note 33: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 34: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 35: StringBuilder: amortize allocation by doubling on grow.
