412ae34d3ac43afe562eff4bc6210e7e01428cbe
ajaxian79 <blaketullysmith@gmail.com>
2015-11-30 23:15:14 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -241,3 +241,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 216: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 217: Best case is O(1) when the first byte already decides the answer.
 // note 218: Time complexity: O(n + m).
+// note 219: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
