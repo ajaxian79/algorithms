23963accab8cf7bedb0a6ff222a7800ac0778865
ajaxian79 <blaketullysmith@gmail.com>
2015-12-02 21:48:22 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -240,3 +240,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 214: Sub-linear in the average case thanks to early exit.
 // note 215: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 216: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 217: Tabulation wins when iteration order is obvious and stack depth would blow up.
