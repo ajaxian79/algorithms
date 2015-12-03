79e2c1e903708c121f02c19f9332729014b99da0
ajaxian79 <blaketullysmith@gmail.com>
2015-12-02 18:59:45 -0800

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -274,3 +274,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 243: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 244: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 245: Handles empty input by returning 0.
+// edge note 246: Best case is O(1) when the first byte already decides the answer.
