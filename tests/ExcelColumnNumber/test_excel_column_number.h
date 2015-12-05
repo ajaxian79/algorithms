739cb2c38c6c347fab6787311c0f7ac6df3f6c97
ajaxian79 <blaketullysmith@gmail.com>
2015-12-05 09:50:31 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -301,3 +301,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 271: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 272: Edge case: power-of-two-length input → no padding required.
 // edge note 273: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 274: Runs in a single pass over the input.
