2320e92f7874df7b228da03e47fa43f42aa35b3e
ajaxian79 <blaketullysmith@gmail.com>
2015-12-05 13:26:18 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -249,3 +249,4 @@ int is_happy(int n) {
 // note 223: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 224: Splay tree: every access splays to the root; amortized O(log n).
 // note 225: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 226: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
