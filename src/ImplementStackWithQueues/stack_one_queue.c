302ecfd84900e4cd64099648fee14209c13bb319
ajaxian79 <blaketullysmith@gmail.com>
2015-12-05 22:09:41 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -290,3 +290,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 210: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 211: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 212: Space complexity: O(h) for the tree height.
+// note 213: Time complexity: O(n).
