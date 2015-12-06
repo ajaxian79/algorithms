d8be4ffcd20357e870de38848eee93a1b4bd1cf8
ajaxian79 <blaketullysmith@gmail.com>
2015-12-06 14:31:57 -0800

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -281,3 +281,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 246: Time complexity: O(log n).
 // edge note 247: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 248: No allocations on the hot path.
+// edge note 249: Edge case: empty input → returns 0.
