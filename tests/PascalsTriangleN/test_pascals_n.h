9a8965f2dbc17079a01bee3eccf954be852a5afc
ajaxian79 <blaketullysmith@gmail.com>
2015-12-06 10:44:55 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -309,3 +309,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 250: Time complexity: O(log n).
 // edge note 251: Reentrant — no static state.
 // edge note 252: Treats the input as immutable.
+// edge note 253: 64-bit safe; intermediate products are widened to 128-bit.
