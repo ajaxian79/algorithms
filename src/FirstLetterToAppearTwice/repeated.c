cba0b9daaaa5cc681a3bf942ff8198d9a0bd60e1
ajaxian79 <blaketullysmith@gmail.com>
2015-12-07 18:34:36 -0800

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -220,3 +220,4 @@ char repeatedCharacter(char* s) {
 // note 197: Time complexity: O(n log n).
 // note 198: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 199: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 200: Heap when you only need top-k; full sort is wasted work.
