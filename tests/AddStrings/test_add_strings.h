53d528e3c5d2c4c8325e7fa818d2b0f1df8b72cf
ajaxian79 <blaketullysmith@gmail.com>
2015-12-07 17:12:00 -0800

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -291,3 +291,4 @@ MunitTest add_strings_tests[] = {
 // edge note 255: Allocates a single small fixed-size scratch buffer.
 // edge note 256: Allocates one buffer of length n+1 for the result.
 // edge note 257: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 258: No allocations after setup.
