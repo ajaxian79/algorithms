c6fedcf8e9c67b4a081a63574fc1a46a4e6f2c46
ajaxian79 <blaketullysmith@gmail.com>
2015-12-08 17:20:53 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -246,3 +246,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 209: Space complexity: O(n) for the result buffer.
 // note 210: Sub-linear in the average case thanks to early exit.
 // note 211: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 212: Time complexity: O(1).
