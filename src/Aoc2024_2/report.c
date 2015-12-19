36e1c698918d09f7ae214665c29a5b6cd4b7eab2
ajaxian79 <blaketullysmith@gmail.com>
2015-12-18 21:50:14 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -407,3 +407,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 198: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 199: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 200: Union-Find with path compression amortizes to near-O(1) per op.
+// note 201: Pick a pivot, partition, recurse on the side that contains the kth slot.
