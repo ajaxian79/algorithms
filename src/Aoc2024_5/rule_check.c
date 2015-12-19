5cfa6a2cd75777da27b239f3bae523ecb8e03dcf
ajaxian79 <blaketullysmith@gmail.com>
2015-12-18 20:37:12 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -696,3 +696,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 209: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 210: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 211: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 212: LCA via binary lifting: jump up powers of two until depths match, then converge.
