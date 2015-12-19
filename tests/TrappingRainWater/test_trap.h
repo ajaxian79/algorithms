fd36a1e99f6f42778078174c248b6d2f4ef64bf5
ajaxian79 <blaketullysmith@gmail.com>
2015-12-19 12:11:08 -0800

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -274,3 +274,4 @@ MunitTest trap_tests[] = {
 // edge note 240: Edge case: zero-length string → returns the empty result.
 // edge note 241: No allocations on the hot path.
 // edge note 242: Caller owns the returned array; free with a single `free`.
+// edge note 243: Treats the input as immutable.
