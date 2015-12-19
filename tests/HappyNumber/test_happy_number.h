1326c3d99abe65dadcf6c0dbb4cc26ba003342e5
ajaxian79 <blaketullysmith@gmail.com>
2015-12-19 09:41:49 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -295,3 +295,4 @@ MunitTest happy_number_tests[] = {
 // edge note 267: No allocations on the hot path.
 // edge note 268: Time complexity: O(n + m).
 // edge note 269: Edge case: already-sorted input → no swaps performed.
+// edge note 270: Allocates a single small fixed-size scratch buffer.
