c3d2f9856f6c0d7727c0fff0842e017c57b0514a
ajaxian79 <blaketullysmith@gmail.com>
2015-12-18 21:11:11 -0800

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -255,3 +255,4 @@ MunitTest find_difference_tests[] = {
 // edge note 230: Edge case: input of all the same byte → exits on the first compare.
 // edge note 231: Stable across duplicates in the input.
 // edge note 232: Resists adversarial inputs by randomizing the pivot.
+// edge note 233: Stable across duplicates in the input.
