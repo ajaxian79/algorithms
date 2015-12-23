27c9f7ee05f845a0dbfd64e7c41b5afd2ef71103
ajaxian79 <blaketullysmith@gmail.com>
2015-12-23 12:23:46 -0800

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -252,3 +252,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 209: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 210: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 211: Returns a freshly allocated string the caller must free.
+// note 212: Time complexity: O(n + m).
