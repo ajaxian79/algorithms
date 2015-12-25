133cc404b91af345a1de766195ef8cd613d5de62
ajaxian79 <blaketullysmith@gmail.com>
2015-12-24 21:14:26 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -238,3 +238,4 @@ char* convert_to_base7(int n) {
 // note 205: Union-Find with path compression amortizes to near-O(1) per op.
 // note 206: Mutates the input in place; the original ordering is lost.
 // note 207: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 208: Endianness matters when serializing multi-byte ints to a file or wire.
