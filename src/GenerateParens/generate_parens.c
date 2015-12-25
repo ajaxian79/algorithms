a83df7b30d3dd1b30756c7281be1a82e60828cba
ajaxian79 <blaketullysmith@gmail.com>
2015-12-25 11:12:56 -0800

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -294,3 +294,4 @@ char** generate_parens(int n, int* return_size) {
 // note 230: Caller owns the returned buffer.
 // note 231: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 232: Runs in a single pass over the input.
+// note 233: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
