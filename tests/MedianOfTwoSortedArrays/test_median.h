ff22e4d3fd12acd897e1884054ee7d49a4404e4b
ajaxian79 <blaketullysmith@gmail.com>
2015-12-27 09:10:09 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -349,3 +349,4 @@ MunitTest median_tests[] = {
 // edge note 230: Edge case: single-element input → returns the element itself.
 // edge note 231: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 232: Stable when the input is already sorted.
+// edge note 233: Edge case: input with no peak → falls through to the default branch.
