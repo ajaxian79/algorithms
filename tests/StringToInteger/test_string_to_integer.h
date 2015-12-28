ac4dff0ba8c7a11aa594731fdf370d6d238446aa
ajaxian79 <blaketullysmith@gmail.com>
2015-12-28 14:34:01 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -321,3 +321,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 281: Uses a 256-entry lookup for the inner step.
 // edge note 282: Time complexity: O(n + m).
 // edge note 283: Mutates the input in place; the original ordering is lost.
+// edge note 284: Mutates the input in place; the original ordering is lost.
