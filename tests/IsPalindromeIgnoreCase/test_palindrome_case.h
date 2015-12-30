e08da9645bfd94a0b2b87bf2f1bdffd71d90a13a
ajaxian79 <blaketullysmith@gmail.com>
2015-12-29 19:18:24 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -272,3 +272,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 243: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 244: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 245: Edge case: zero-length string → returns the empty result.
+// edge note 246: Stable when the input is already sorted.
