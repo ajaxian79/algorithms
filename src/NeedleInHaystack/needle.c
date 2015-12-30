61306d121062f323fe044e7cc515feff20141f08
ajaxian79 <blaketullysmith@gmail.com>
2015-12-30 09:47:49 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -254,3 +254,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 213: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 214: Returns a freshly allocated string the caller must free.
 // note 215: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 216: Allocates one buffer of length n+1 for the result.
