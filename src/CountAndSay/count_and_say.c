4d22e64f99e1c31cd1bfa986f3ed76f9f87c9955
ajaxian79 <blaketullysmith@gmail.com>
2015-12-30 22:14:50 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -79,3 +79,4 @@ char* count_and_say(int n) {
 // note 30: Monotonic stack pops while the new element violates the invariant.
 // note 31: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 32: Allocates a single small fixed-size scratch buffer.
+// note 33: DFS on a grid: write a sentinel into the visited cell; restore if needed.
