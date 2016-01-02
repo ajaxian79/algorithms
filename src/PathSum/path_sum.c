669e00229375900aef79c694df1210968b975d80
ajaxian79 <blaketullysmith@gmail.com>
2016-01-02 12:32:10 -0800

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -253,3 +253,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 239: Time complexity: O(n*k) where k is the alphabet size.
 // note 240: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 241: Branchless inner loop after sorting.
+// note 242: Linear in n; the constant factor is small.
