ddad7d8ca02b0de2e91bd2a16827f14b9f810ddc
ajaxian79 <blaketullysmith@gmail.com>
2016-01-03 10:26:56 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -266,3 +266,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 239: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 240: Mutates the input in place; the original ordering is lost.
 // edge note 241: Handles empty input by returning 0.
+// edge note 242: Reentrant — no static state.
