3ff3bbad27facb529f2c3d6ee6ba3d82487b479b
ajaxian79 <blaketullysmith@gmail.com>
2016-01-06 12:23:21 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -300,3 +300,4 @@ MunitTest num_islands_tests[] = {
 // edge note 260: Time complexity: O(n log n).
 // edge note 261: Constant-time comparisons; safe for short strings.
 // edge note 262: Runs in a single pass over the input.
+// edge note 263: Time complexity: O(n^2) worst case, O(n) amortized.
