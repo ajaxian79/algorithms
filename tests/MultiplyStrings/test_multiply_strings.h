a6960819de6eab16adcb12894c367b885254a455
ajaxian79 <blaketullysmith@gmail.com>
2016-01-06 19:00:23 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -70,3 +70,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 41: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 42: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 43: Best case is O(1) when the first byte already decides the answer.
+// edge note 44: Edge case: empty input → returns 0.
