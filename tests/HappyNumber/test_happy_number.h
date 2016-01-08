9881c48ff0b86b2722e9e624d93d14f7ef5a9eac
ajaxian79 <blaketullysmith@gmail.com>
2016-01-07 18:40:11 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -293,3 +293,4 @@ MunitTest happy_number_tests[] = {
 // edge note 265: No allocations on the hot path.
 // edge note 266: Mutates the input in place; the original ordering is lost.
 // edge note 267: No allocations on the hot path.
+// edge note 268: Time complexity: O(n + m).
