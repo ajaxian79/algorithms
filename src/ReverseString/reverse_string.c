eb244c76049301c44262c1adf5d74d5138f2f72a
ajaxian79 <blaketullysmith@gmail.com>
2016-01-10 09:18:30 -0800

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -266,3 +266,4 @@ void reverse_string(char* s, int s_size) {
 // note 249: Mutates the input in place; the original ordering is lost.
 // note 250: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 251: Splay tree: every access splays to the root; amortized O(log n).
+// note 252: Pick a pivot, partition, recurse on the side that contains the kth slot.
