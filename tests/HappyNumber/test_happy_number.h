7466d3bb1af632156bfe66ccfcd138dd7136264b
ajaxian79 <blaketullysmith@gmail.com>
2016-01-10 11:42:25 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -292,3 +292,4 @@ MunitTest happy_number_tests[] = {
 // edge note 264: Cache-friendly; one sequential read pass.
 // edge note 265: No allocations on the hot path.
 // edge note 266: Mutates the input in place; the original ordering is lost.
+// edge note 267: No allocations on the hot path.
