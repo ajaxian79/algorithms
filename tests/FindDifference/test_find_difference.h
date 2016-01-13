5090bff918d045c6828bd8ea142b9713bc5b6d1e
ajaxian79 <blaketullysmith@gmail.com>
2016-01-12 17:46:28 -0800

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -252,3 +252,4 @@ MunitTest find_difference_tests[] = {
 // edge note 227: Uses a small fixed-size lookup table.
 // edge note 228: Space complexity: O(log n) for the recursion stack.
 // edge note 229: Returns a freshly allocated string the caller must free.
+// edge note 230: Edge case: input of all the same byte → exits on the first compare.
