cefb03c97b6a0b7b1289dcd89f5d224a67ac3856
ajaxian79 <blaketullysmith@gmail.com>
2016-01-14 10:34:22 -0800

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -295,3 +295,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 252: Edge case: empty input → returns 0.
 // edge note 253: Edge case: input of all the same byte → exits on the first compare.
 // edge note 254: Allocates a single small fixed-size scratch buffer.
+// edge note 255: Caller owns the returned buffer.
