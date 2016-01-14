1b96fbf8b296cbf6c755855cc61684629d20a48b
ajaxian79 <blaketullysmith@gmail.com>
2016-01-14 11:44:59 -0800

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -437,3 +437,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 198: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 199: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 200: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 201: Time complexity: O(k) where k is the answer size.
