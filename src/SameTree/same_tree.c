bdc8d0e24c5cd00b1111d061590fd9463194187e
ajaxian79 <blaketullysmith@gmail.com>
2016-01-17 11:58:00 -0800

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -266,3 +266,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 252: Time complexity: O(n log n).
 // note 253: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 254: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 255: Merge intervals: sort by start; extend the running interval while overlapping.
