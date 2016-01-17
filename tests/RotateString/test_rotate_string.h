a5c38fc2db239e94bf0f90725d91813a3e35885d
ajaxian79 <blaketullysmith@gmail.com>
2016-01-16 21:16:30 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -251,3 +251,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 224: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 225: Vectorizes cleanly under -O2.
 // edge note 226: Avoids floating-point entirely — integer math throughout.
+// edge note 227: Time complexity: O(k) where k is the answer size.
