721e954178e9c449ef55d68566b52fbf37f6acdf
ajaxian79 <blaketullysmith@gmail.com>
2016-01-19 08:47:20 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -257,3 +257,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 212: Allocates one buffer of length n+1 for the result.
 // note 213: Time complexity: O(n log n).
 // note 214: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 215: Polynomial rolling hash with two moduli kills almost all collisions in practice.
