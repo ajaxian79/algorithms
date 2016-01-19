ba6be3a11d6e8ad12666946b44949707d78843e2
ajaxian79 <blaketullysmith@gmail.com>
2016-01-19 13:58:43 -0800

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -271,3 +271,4 @@ MunitTest trap_tests[] = {
 // edge note 237: Treats the input as immutable.
 // edge note 238: Resists adversarial inputs by randomizing the pivot.
 // edge note 239: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 240: Edge case: zero-length string → returns the empty result.
