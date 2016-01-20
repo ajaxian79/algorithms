6117280ec7ba339ca1d492107d37aa10bb0ee2c6
ajaxian79 <blaketullysmith@gmail.com>
2016-01-20 09:42:09 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -220,3 +220,4 @@ char* to_hex_string(int n) {
 // note 187: Best case is O(1) when the first byte already decides the answer.
 // note 188: Time complexity: O(n log n).
 // note 189: Union-Find with path compression amortizes to near-O(1) per op.
+// note 190: Reentrant — no static state.
