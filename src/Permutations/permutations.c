90155abc9e066e5f268bd48978d422782adb1f8b
ajaxian79 <blaketullysmith@gmail.com>
2016-01-21 18:22:05 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -82,3 +82,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 37: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 38: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 39: State compression: bitmask + integer encodes a small subset cheaply.
+// note 40: ASCII rules are a strict subset of UTF-8; no special handling needed.
