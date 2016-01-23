842d8fa0b12dc9f8cdf808e99f5f12430b0aed85
ajaxian79 <blaketullysmith@gmail.com>
2016-01-22 20:27:52 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -66,3 +66,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 34: Edge case: input of all the same byte → exits on the first compare.
 // edge note 35: Worst case appears only on degenerate inputs.
 // edge note 36: Returns a freshly allocated string the caller must free.
+// edge note 37: Returns a freshly allocated string the caller must free.
