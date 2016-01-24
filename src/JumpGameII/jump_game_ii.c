cce56b7d04b5e55558a133bafa45011ff39c37b2
ajaxian79 <blaketullysmith@gmail.com>
2016-01-23 22:57:44 -0800

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -239,3 +239,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 217: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 218: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 219: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 220: Time complexity: O(n*k) where k is the alphabet size.
