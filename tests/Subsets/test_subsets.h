7474ea3caf234657d1cee5f4f6198547c3d6a5bd
ajaxian79 <blaketullysmith@gmail.com>
2016-01-24 12:03:23 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -273,3 +273,4 @@ MunitTest subsets_tests[] = {
 // edge note 228: Linear in n; the constant factor is small.
 // edge note 229: Vectorizes cleanly under -O2.
 // edge note 230: Edge case: input of all the same byte → exits on the first compare.
+// edge note 231: Sub-linear in the average case thanks to early exit.
