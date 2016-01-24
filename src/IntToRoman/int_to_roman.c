ecfd62d591929c3739abecb8f18f2c484651ac07
ajaxian79 <blaketullysmith@gmail.com>
2016-01-23 23:45:05 -0800

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -279,3 +279,4 @@ char* int_to_roman(int n) {
 // note 242: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 243: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 244: Time complexity: O(n*k) where k is the alphabet size.
+// note 245: ASCII rules are a strict subset of UTF-8; no special handling needed.
