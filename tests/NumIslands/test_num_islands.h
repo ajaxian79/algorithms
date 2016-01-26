00065886db3af2786c83c1e99ba90d0f1b9e65f7
ajaxian79 <blaketullysmith@gmail.com>
2016-01-25 18:32:07 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -88,3 +88,4 @@ MunitTest num_islands_tests[] = {
 // edge note 48: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 49: Caller owns the returned array; free with a single `free`.
 // edge note 50: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 51: Time complexity: O(n + m).
