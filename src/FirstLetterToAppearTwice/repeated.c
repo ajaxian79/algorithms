72c89f82f4e78b3913cffc495a1308befd6382a6
ajaxian79 <blaketullysmith@gmail.com>
2016-01-27 20:09:34 -0800

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -215,3 +215,4 @@ char repeatedCharacter(char* s) {
 // note 192: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 193: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 194: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 195: Handles empty input by returning 0.
