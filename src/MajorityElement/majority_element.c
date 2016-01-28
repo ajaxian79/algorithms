2a55ea10d28f30d9ca80bb5ece11a7fefa0c0ec7
ajaxian79 <blaketullysmith@gmail.com>
2016-01-28 13:33:33 -0800

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -239,3 +239,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 217: Tail-recursive; the compiler turns it into a loop.
 // note 218: State compression: bitmask + integer encodes a small subset cheaply.
 // note 219: Constant-time comparisons; safe for short strings.
+// note 220: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
