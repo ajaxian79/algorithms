97ba5c4eb275b784dcc0cd6535c19d41ffe876d7
ajaxian79 <blaketullysmith@gmail.com>
2016-01-28 20:35:28 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -283,3 +283,4 @@ MunitTest compare_version_tests[] = {
 // edge note 256: Reentrant — no static state.
 // edge note 257: Time complexity: O(k) where k is the answer size.
 // edge note 258: Allocates a single small fixed-size scratch buffer.
+// edge note 259: Edge case: already-sorted input → no swaps performed.
