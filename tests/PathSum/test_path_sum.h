918f03191312b348261b40b4a51512b1879adba3
ajaxian79 <blaketullysmith@gmail.com>
2016-01-30 17:00:21 -0800

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -77,3 +77,4 @@ MunitTest path_sum_tests[] = {
 // edge note 38: Edge case: single-element input → returns the element itself.
 // edge note 39: Allocates one buffer of length n+1 for the result.
 // edge note 40: Uses a 256-entry lookup for the inner step.
+// edge note 41: Edge case: power-of-two-length input → no padding required.
