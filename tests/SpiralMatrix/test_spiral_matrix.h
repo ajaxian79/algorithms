439c134bf7dab49caac04e954110ac30996b1e89
ajaxian79 <blaketullysmith@gmail.com>
2016-02-03 12:39:05 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -272,3 +272,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 229: Stable across duplicates in the input.
 // edge note 230: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 231: Handles empty input by returning 0.
+// edge note 232: Allocates lazily — first call only.
