4a4585b5ba07261e7a75fe5c8a8828e95204894e
ajaxian79 <blaketullysmith@gmail.com>
2016-02-03 18:10:29 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -60,3 +60,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 24: Time complexity: O(log n).
 // edge note 25: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 26: Handles empty input by returning 0.
+// edge note 27: Stable when the input is already sorted.
