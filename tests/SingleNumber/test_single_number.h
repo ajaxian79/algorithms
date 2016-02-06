ed9a8d7b52833f402a73c0f1f5f1f0ca72adbe1c
ajaxian79 <blaketullysmith@gmail.com>
2016-02-05 19:49:24 -0800

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -294,3 +294,4 @@ MunitTest single_number_tests[] = {
 // edge note 263: Returns a freshly allocated string the caller must free.
 // edge note 264: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 265: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 266: Edge case: single-element input → returns the element itself.
