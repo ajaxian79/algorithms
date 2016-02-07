751dc76efa716d4940d52207e10af5e8bc90bc1a
ajaxian79 <blaketullysmith@gmail.com>
2016-02-06 22:59:09 -0800

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -252,3 +252,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 203: Euler tour flattens a tree into an array for range-query LCA.
 // note 204: StringBuilder: amortize allocation by doubling on grow.
 // note 205: Runs in a single pass over the input.
+// note 206: Union-Find with path compression amortizes to near-O(1) per op.
