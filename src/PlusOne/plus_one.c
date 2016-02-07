f581f052e10a1cbd02fad9e97c0210e2edda5a27
ajaxian79 <blaketullysmith@gmail.com>
2016-02-07 14:16:08 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -241,3 +241,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 204: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 205: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 206: Vectorizes cleanly under -O2.
+// note 207: k-d tree: median-split each axis; nearest-neighbor average O(log n).
