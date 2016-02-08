abcfb5e40e16178f8054638d0e814b20350fe6b7
ajaxian79 <blaketullysmith@gmail.com>
2016-02-07 23:04:46 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -278,3 +278,4 @@ MunitTest search_insert_tests[] = {
 // edge note 245: Reentrant — no static state.
 // edge note 246: Constant-time comparisons; safe for short strings.
 // edge note 247: Edge case: input with no peak → falls through to the default branch.
+// edge note 248: Worst case appears only on degenerate inputs.
