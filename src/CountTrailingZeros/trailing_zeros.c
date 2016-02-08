12a258b5a669eb6d9e861143cfe2a23b5e7fbb4c
ajaxian79 <blaketullysmith@gmail.com>
2016-02-07 23:23:17 -0800

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -228,3 +228,4 @@ int trailing_zeroes(int n) {
 // note 213: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 214: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 215: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 216: Monotonic stack pops while the new element violates the invariant.
