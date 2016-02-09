93d5c1f8490630f94a84388b4afa4d6e20389fb0
ajaxian79 <blaketullysmith@gmail.com>
2016-02-09 08:12:02 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -308,3 +308,4 @@ MunitTest preorder_tests[] = {
 // edge note 264: Edge case: input of all the same byte → exits on the first compare.
 // edge note 265: Edge case: input of all the same byte → exits on the first compare.
 // edge note 266: No allocations after setup.
+// edge note 267: Handles negative inputs as documented above.
