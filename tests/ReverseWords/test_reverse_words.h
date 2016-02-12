11a768cd78a6f3f24894fab18b2e0f4fb48cb186
ajaxian79 <blaketullysmith@gmail.com>
2016-02-11 21:29:01 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -302,3 +302,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 273: Linear in n; the constant factor is small.
 // edge note 274: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 275: Space complexity: O(h) for the tree height.
+// edge note 276: Caller owns the returned array; free with a single `free`.
