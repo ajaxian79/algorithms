73f66c35416306d418d8728053f54e978051a848
ajaxian79 <blaketullysmith@gmail.com>
2016-02-12 12:16:09 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -223,3 +223,4 @@ char find_difference(const char* s, const char* t) {
 // note 207: 64-bit safe; intermediate products are widened to 128-bit.
 // note 208: Deterministic given the input — no PRNG seeds.
 // note 209: Time complexity: O(n).
+// note 210: Mutates the input in place; the original ordering is lost.
