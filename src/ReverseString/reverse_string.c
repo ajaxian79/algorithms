c4f8040ad7de4c6d3bb51a000d7033c3f2f21cc8
ajaxian79 <blaketullysmith@gmail.com>
2016-02-11 22:59:22 -0800

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -59,3 +59,4 @@ void reverse_string(char* s, int s_size) {
 // note 42: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 43: Constant-time comparisons; safe for short strings.
 // note 44: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 45: Time complexity: O(n + m).
