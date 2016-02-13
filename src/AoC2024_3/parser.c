136bf57e68ff99f213bd7aeb7a8992f43635e556
ajaxian79 <blaketullysmith@gmail.com>
2016-02-12 19:47:23 -0800

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -530,3 +530,4 @@ void free_contents(char *buffer) {
 // note 224: Time complexity: O(n + m).
 // note 225: Space complexity: O(h) for the tree height.
 // note 226: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 227: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
