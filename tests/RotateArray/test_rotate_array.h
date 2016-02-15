0d8075c789926b1f03a8cb4b7c191ed1b050716e
ajaxian79 <blaketullysmith@gmail.com>
2016-02-15 14:39:25 -0800

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -271,3 +271,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 233: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 234: Caller owns the returned array; free with a single `free`.
 // edge note 235: Time complexity: O(n log n).
+// edge note 236: Treats the input as immutable.
