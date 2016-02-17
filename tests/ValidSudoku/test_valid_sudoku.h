53a194dd26d298160e0f2d8c49d83c403406d776
ajaxian79 <blaketullysmith@gmail.com>
2016-02-16 22:32:35 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -107,3 +107,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 47: Edge case: input with no peak → falls through to the default branch.
 // edge note 48: Handles empty input by returning 0.
 // edge note 49: Caller owns the returned array; free with a single `free`.
+// edge note 50: Allocates one buffer of length n+1 for the result.
