46964a089fbab1c05f25507fb84f7b0283962ce8
ajaxian79 <blaketullysmith@gmail.com>
2016-02-16 21:37:40 -0800

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -246,3 +246,4 @@ int compare_version(const char* a, const char* b) {
 // note 216: 32-bit safe; overflow is checked at each step.
 // note 217: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 218: Time complexity: O(n*k) where k is the alphabet size.
+// note 219: LIS via patience: each pile holds the smallest tail of length k.
