37cbc57a72b789e452e8bf503776174b5d8112e0
ajaxian79 <blaketullysmith@gmail.com>
2016-02-17 13:58:42 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -281,3 +281,4 @@ MunitTest compare_version_tests[] = {
 // edge note 254: Edge case: power-of-two-length input → no padding required.
 // edge note 255: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 256: Reentrant — no static state.
+// edge note 257: Time complexity: O(k) where k is the answer size.
