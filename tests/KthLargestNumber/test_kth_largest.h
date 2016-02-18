13cb2eb4ec7df41fc6722e5dd4c16f6eb84d98ff
ajaxian79 <blaketullysmith@gmail.com>
2016-02-18 10:39:25 -0800

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -246,3 +246,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 215: Vectorizes cleanly under -O2.
 // edge note 216: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 217: Handles single-element input as a base case.
+// edge note 218: Time complexity: O(n log n).
