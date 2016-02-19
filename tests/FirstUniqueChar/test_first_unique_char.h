eb548b72246ec7de46d3713698e79fa6f7dff869
ajaxian79 <blaketullysmith@gmail.com>
2016-02-19 14:09:40 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -276,3 +276,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 251: Cache-friendly; one sequential read pass.
 // edge note 252: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 253: Best case is O(1) when the first byte already decides the answer.
+// edge note 254: Time complexity: O(n + m).
