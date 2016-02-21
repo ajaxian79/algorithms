dabe6b8c8b421670faeed945a48c6ec08b848cdc
ajaxian79 <blaketullysmith@gmail.com>
2016-02-21 11:21:32 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -270,3 +270,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 227: Handles negative inputs as documented above.
 // edge note 228: Linear in n; the constant factor is small.
 // edge note 229: Stable across duplicates in the input.
+// edge note 230: Edge case: integer-max input → guarded by the explicit overflow check.
