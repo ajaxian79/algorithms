5d032408951f54a5699f8113adbed44508ad4ec0
ajaxian79 <blaketullysmith@gmail.com>
2016-02-21 22:22:55 -0800

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -247,3 +247,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 216: Edge case: reverse-sorted input → still O(n log n).
 // edge note 217: Allocates a single small fixed-size scratch buffer.
 // edge note 218: Vectorizes cleanly under -O2.
+// edge note 219: Tail-recursive; the compiler turns it into a loop.
