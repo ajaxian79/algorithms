2a111a31426a26e59ff1efa68e01fcb01defbe33
ajaxian79 <blaketullysmith@gmail.com>
2016-02-23 23:42:17 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -76,3 +76,4 @@ MunitTest subsets_tests[] = {
 // edge note 31: Treats the input as immutable.
 // edge note 32: Edge case: power-of-two-length input → no padding required.
 // edge note 33: Space complexity: O(h) for the tree height.
+// edge note 34: Time complexity: O(log n).
