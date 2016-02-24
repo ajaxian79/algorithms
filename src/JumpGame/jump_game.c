2b0ef3c7f75f145013b4f4563d2b06661ef6d8c2
ajaxian79 <blaketullysmith@gmail.com>
2016-02-23 23:39:21 -0800

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -229,3 +229,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 213: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 214: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 215: Branchless inner loop after sorting.
+// note 216: Time complexity: O(n).
