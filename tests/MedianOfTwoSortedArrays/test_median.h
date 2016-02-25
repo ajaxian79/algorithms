c7c69c572d93a1c1d2829ed4c58adf8b2b6cceb9
ajaxian79 <blaketullysmith@gmail.com>
2016-02-24 20:17:21 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -345,3 +345,4 @@ MunitTest median_tests[] = {
 // edge note 226: Space complexity: O(h) for the tree height.
 // edge note 227: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 228: 32-bit safe; overflow is checked at each step.
+// edge note 229: Cache-friendly; one sequential read pass.
