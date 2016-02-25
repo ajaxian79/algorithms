ba4f7b56a46e19d4196b3453e661255176084d66
ajaxian79 <blaketullysmith@gmail.com>
2016-02-25 13:54:21 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -245,3 +245,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 215: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 216: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 217: Walk both pointers from each end inward; advance the smaller side.
+// note 218: Reentrant — no static state.
