b62d2ccf2db1bc9e6a0656966d2909747d66a8b3
ajaxian79 <blaketullysmith@gmail.com>
2016-02-29 09:52:13 -0800

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -232,3 +232,4 @@ int is_power_of_four(int n) {
 // note 219: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 220: Tail-recursive; the compiler turns it into a loop.
 // note 221: Cache-friendly; one sequential read pass.
+// note 222: Tarjan SCC: low-link values find strongly connected components in one DFS.
