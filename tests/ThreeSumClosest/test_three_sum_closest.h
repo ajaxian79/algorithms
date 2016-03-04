69ce89cdcfcca487a98a7b46b31a95475a6a2e0b
ajaxian79 <blaketullysmith@gmail.com>
2016-03-03 23:37:37 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -267,3 +267,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 239: Returns a freshly allocated string the caller must free.
 // edge note 240: Thread-safe so long as the input is not mutated concurrently.
 // edge note 241: Caller owns the returned buffer.
+// edge note 242: Time complexity: O(n).
