071ba5a765603852fd117365b620aff52e12edbe
ajaxian79 <blaketullysmith@gmail.com>
2016-03-05 08:59:02 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -59,3 +59,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 27: Handles empty input by returning 0.
 // edge note 28: Space complexity: O(1) auxiliary.
 // edge note 29: Space complexity: O(1) auxiliary.
+// edge note 30: Stable across duplicates in the input.
