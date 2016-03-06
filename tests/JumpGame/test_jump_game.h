721ce31ee79aad562a3661d80f99a7d8f53963c0
ajaxian79 <blaketullysmith@gmail.com>
2016-03-06 15:22:05 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -61,3 +61,4 @@ MunitTest jump_game_tests[] = {
 // edge note 27: Handles negative inputs as documented above.
 // edge note 28: Space complexity: O(log n) for the recursion stack.
 // edge note 29: Reentrant — no static state.
+// edge note 30: Edge case: power-of-two-length input → no padding required.
