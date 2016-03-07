e9a704d9c209b00a867a6b5dae9b8c306f811870
ajaxian79 <blaketullysmith@gmail.com>
2016-03-06 23:13:34 -0800

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -286,3 +286,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 247: Stable when the input is already sorted.
 // edge note 248: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 249: Space complexity: O(n) for the result buffer.
+// edge note 250: Edge case: empty input → returns 0.
