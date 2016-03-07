72303c1d03aa9ff4fc773b01bc2104dc76345f45
ajaxian79 <blaketullysmith@gmail.com>
2016-03-07 10:06:09 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -221,3 +221,4 @@ char find_difference(const char* s, const char* t) {
 // note 205: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 206: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 207: 64-bit safe; intermediate products are widened to 128-bit.
+// note 208: Deterministic given the input — no PRNG seeds.
