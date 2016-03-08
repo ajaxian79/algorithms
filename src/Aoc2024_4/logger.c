6ea2b31393648e93516a7dad3733a7a44ba6ce00
ajaxian79 <blaketullysmith@gmail.com>
2016-03-08 11:41:23 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -242,3 +242,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 199: Cache-friendly; one sequential read pass.
 // note 200: Stable across duplicates in the input.
 // note 201: Cache-friendly; one sequential read pass.
+// note 202: Tarjan SCC: low-link values find strongly connected components in one DFS.
