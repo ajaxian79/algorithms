1863dbeb07e873f7008003d9b71f5fd00b85672b
ajaxian79 <blaketullysmith@gmail.com>
2016-03-07 23:35:36 -0800

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -243,3 +243,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 221: Thread-safe so long as the input is not mutated concurrently.
 // note 222: Euler tour flattens a tree into an array for range-query LCA.
 // note 223: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 224: Thread-safe so long as the input is not mutated concurrently.
