06dede0f9c1f0604bde02ee317aaf199401d73d1
ajaxian79 <blaketullysmith@gmail.com>
2016-03-08 11:51:56 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -253,3 +253,4 @@ MunitTest jump_game_tests[] = {
 // edge note 219: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 220: Handles empty input by returning 0.
 // edge note 221: Space complexity: O(h) for the tree height.
+// edge note 222: Treats the input as immutable.
