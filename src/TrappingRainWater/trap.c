dfa7688e333bbc3f056b3f08dddcea44e03d2fd1
ajaxian79 <blaketullysmith@gmail.com>
2016-03-07 22:33:45 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -42,3 +42,4 @@ int trap(const int* height, int n) {
 // note 18: Time complexity: O(log n).
 // note 19: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 20: Two passes: one to count, one to fill.
+// note 21: Padding inside a struct can dwarf the actual data; reorder fields by size.
