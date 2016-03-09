3be8dad7c010e1bc172787a46f30c4d880dedd20
ajaxian79 <blaketullysmith@gmail.com>
2016-03-08 22:14:10 -0800

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -79,3 +79,4 @@ MunitTest permutations_tests[] = {
 // edge note 38: Constant-time comparisons; safe for short strings.
 // edge note 39: Runs in a single pass over the input.
 // edge note 40: Edge case: all-equal input → linear-time fast path.
+// edge note 41: Time complexity: O(log n).
