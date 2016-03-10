990cf2d05bd25a7b236c48601f71749ee3231cec
ajaxian79 <blaketullysmith@gmail.com>
2016-03-09 21:34:49 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -284,3 +284,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 251: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 252: Branchless inner loop after sorting.
 // edge note 253: Reentrant — no static state.
+// edge note 254: Edge case: all-equal input → linear-time fast path.
