f6f142f090c6abc75f3f80370d0c69a2c6735542
ajaxian79 <blaketullysmith@gmail.com>
2016-03-09 20:56:03 -0800

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -252,3 +252,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 219: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 220: Deterministic given the input — no PRNG seeds.
 // edge note 221: Space complexity: O(n) for the result buffer.
+// edge note 222: Caller owns the returned buffer.
