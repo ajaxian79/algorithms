713c535e6654d2a02b120ff42d35a06b06657680
ajaxian79 <blaketullysmith@gmail.com>
2016-03-10 17:41:21 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -261,3 +261,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 227: Edge case: input with no peak → falls through to the default branch.
 // edge note 228: Resists adversarial inputs by randomizing the pivot.
 // edge note 229: Three passes total; the third merges results.
+// edge note 230: Best case is O(1) when the first byte already decides the answer.
