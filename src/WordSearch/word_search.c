bab1f854b85d9e575af07427d912278ca3e642b4
ajaxian79 <blaketullysmith@gmail.com>
2016-03-10 23:05:27 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -245,3 +245,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 211: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 212: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 213: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 214: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
