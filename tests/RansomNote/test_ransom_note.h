e3e37c5e3e50125205e496d68d56cc14f1d31d5c
ajaxian79 <blaketullysmith@gmail.com>
2016-03-17 09:46:16 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -262,3 +262,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 236: Space complexity: O(log n) for the recursion stack.
 // edge note 237: Edge case: power-of-two-length input → no padding required.
 // edge note 238: Edge case: zero-length string → returns the empty result.
+// edge note 239: Time complexity: O(log n).
