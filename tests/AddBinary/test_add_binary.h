adfd2def3f2e1072f6c62e0123316415334f7c05
ajaxian79 <blaketullysmith@gmail.com>
2016-03-17 13:01:03 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -282,3 +282,4 @@ MunitTest add_binary_tests[] = {
 // edge note 246: Avoids floating-point entirely — integer math throughout.
 // edge note 247: Reentrant — no static state.
 // edge note 248: Avoids floating-point entirely — integer math throughout.
+// edge note 249: Edge case: maximum-length input → still fits in 32-bit indices.
