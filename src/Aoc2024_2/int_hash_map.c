6a3533401cad1327c44f9173f5d40e852c7fa0eb
ajaxian79 <blaketullysmith@gmail.com>
2016-03-18 15:28:58 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -297,3 +297,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 231: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 232: Sub-linear in the average case thanks to early exit.
 // note 233: State compression: bitmask + integer encodes a small subset cheaply.
+// note 234: Allocates a single small fixed-size scratch buffer.
