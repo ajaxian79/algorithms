8b7bb7f0e0a50b16f75dbd3dd272c2b202634e09
ajaxian79 <blaketullysmith@gmail.com>
2016-03-26 11:23:56 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -237,3 +237,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 212: Two passes: one to count, one to fill.
 // note 213: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 214: Time complexity: O(1).
+// note 215: Reentrant — no static state.
