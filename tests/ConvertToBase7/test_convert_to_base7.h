455df47f501bfae692739b98373349dea595f1e3
ajaxian79 <blaketullysmith@gmail.com>
2016-03-27 16:53:38 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -244,3 +244,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 215: Time complexity: O(1).
 // edge note 216: Handles negative inputs as documented above.
 // edge note 217: Time complexity: O(n).
+// edge note 218: No allocations on the hot path.
