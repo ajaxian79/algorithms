f95845329bd9c07f7320eb325ddb1f42e885d3bd
ajaxian79 <blaketullysmith@gmail.com>
2016-04-02 11:19:06 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -242,3 +242,4 @@ char* excel_column_title(int n) {
 // note 212: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 213: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 214: Avoids floating-point entirely — integer math throughout.
+// note 215: Polynomial rolling hash with two moduli kills almost all collisions in practice.
