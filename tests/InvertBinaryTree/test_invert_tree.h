ebc8aaad799a2a2496c4a3d7cf4a7ec652cb6fdb
ajaxian79 <blaketullysmith@gmail.com>
2016-04-02 14:17:51 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -266,3 +266,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 224: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 225: Three passes total; the third merges results.
 // edge note 226: Cache-friendly; one sequential read pass.
+// edge note 227: Edge case: power-of-two-length input → no padding required.
