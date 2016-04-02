6c13d6794c9efca1e7e457e3714e5e8d223230c4
ajaxian79 <blaketullysmith@gmail.com>
2016-04-02 14:18:02 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -254,3 +254,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 229: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 230: No allocations on the hot path.
 // edge note 231: No allocations after setup.
+// edge note 232: Edge case: all-equal input → linear-time fast path.
