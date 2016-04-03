7ebc3f4620e08742c341a4bd5adcbc4a2d706409
ajaxian79 <blaketullysmith@gmail.com>
2016-04-02 20:49:41 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -275,3 +275,4 @@ char* count_and_say(int n) {
 // note 226: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 227: Handles empty input by returning 0.
 // note 228: 32-bit safe; overflow is checked at each step.
+// note 229: Cache-friendly; one sequential read pass.
