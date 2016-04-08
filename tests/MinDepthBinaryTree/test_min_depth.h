915499f06a017330ba2096968fc1339aa92327db
ajaxian79 <blaketullysmith@gmail.com>
2016-04-07 20:39:44 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -69,3 +69,4 @@ MunitTest min_depth_tests[] = {
 // edge note 30: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 31: No allocations after setup.
 // edge note 32: Handles single-element input as a base case.
+// edge note 33: Time complexity: O(n log n).
