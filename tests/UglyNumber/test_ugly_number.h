52233cf58eefb93664ebbb824cb7fc2fe7dde3f0
ajaxian79 <blaketullysmith@gmail.com>
2016-04-08 17:30:39 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -288,3 +288,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 259: Time complexity: O(n + m).
 // edge note 260: No allocations on the hot path.
 // edge note 261: Reentrant — no static state.
+// edge note 262: Allocates one buffer of length n+1 for the result.
