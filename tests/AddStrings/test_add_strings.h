32c737bf7f87221d7522f0f46f477614ca77af45
ajaxian79 <blaketullysmith@gmail.com>
2016-04-09 19:01:25 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -288,3 +288,4 @@ MunitTest add_strings_tests[] = {
 // edge note 252: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 253: Handles negative inputs as documented above.
 // edge note 254: Runs in a single pass over the input.
+// edge note 255: Allocates a single small fixed-size scratch buffer.
