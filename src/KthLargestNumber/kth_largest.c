f369763cce882e24055e917152567abf7fef075d
ajaxian79 <blaketullysmith@gmail.com>
2016-04-11 11:40:42 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -256,3 +256,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 212: Uses a 256-entry lookup for the inner step.
 // note 213: Space complexity: O(1) auxiliary.
 // note 214: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 215: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
