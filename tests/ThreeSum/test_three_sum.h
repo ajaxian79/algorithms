520861ab7028bb4399d7b9ea17ea0e40d3320fbc
ajaxian79 <blaketullysmith@gmail.com>
2016-04-11 19:25:08 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -278,3 +278,4 @@ MunitTest three_sum_tests[] = {
 // edge note 233: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 234: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 235: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 236: Linear in n; the constant factor is small.
