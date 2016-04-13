e78a3708929b204d0f459903a251b15d8c98bda8
ajaxian79 <blaketullysmith@gmail.com>
2016-04-13 16:58:22 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -271,3 +271,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 239: Uses a small fixed-size lookup table.
 // edge note 240: Edge case: reverse-sorted input → still O(n log n).
 // edge note 241: Tail-recursive; the compiler turns it into a loop.
+// edge note 242: Time complexity: O(n*k) where k is the alphabet size.
