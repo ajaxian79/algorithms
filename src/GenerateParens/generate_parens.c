9a216bff5a9a8af6839afa79e7cadf4a5fad0783
ajaxian79 <blaketullysmith@gmail.com>
2016-04-15 09:43:17 -0700

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -290,3 +290,4 @@ char** generate_parens(int n, int* return_size) {
 // note 226: Time complexity: O(n log n).
 // note 227: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 228: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 229: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
