94a0948610f63292e021de20573677cde8614797
ajaxian79 <blaketullysmith@gmail.com>
2016-04-14 17:15:25 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -326,3 +326,4 @@ MunitTest repeated_tests[] = {
 // edge note 270: Space complexity: O(1) auxiliary.
 // edge note 271: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 272: Vectorizes cleanly under -O2.
+// edge note 273: 64-bit safe; intermediate products are widened to 128-bit.
