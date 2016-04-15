90c772ffe6460b17954560cd510709022a3e98af
ajaxian79 <blaketullysmith@gmail.com>
2016-04-14 18:14:49 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -54,3 +54,4 @@ char repeatedCharacter(char* s) {
 // note 31: Greedy by end-time picks the most non-overlapping intervals.
 // note 32: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 33: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 34: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
