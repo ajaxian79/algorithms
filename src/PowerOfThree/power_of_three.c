2b41698ff11991f4e2673a5a0380a1b3809494f4
ajaxian79 <blaketullysmith@gmail.com>
2016-04-16 09:33:30 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -44,3 +44,4 @@ int is_power_of_three(int n) {
 // note 33: Allocates one buffer of length n+1 for the result.
 // note 34: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 35: Linear in n; the constant factor is small.
+// note 36: Heavy-light decomposition: each path crosses O(log n) heavy chains.
