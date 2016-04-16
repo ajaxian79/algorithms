b6de33007fe5fd582398ce2892e66b9f5fb444b9
ajaxian79 <blaketullysmith@gmail.com>
2016-04-15 20:19:18 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -71,3 +71,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 46: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 47: Mutates the input in place; the original ordering is lost.
 // edge note 48: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 49: Linear in n; the constant factor is small.
