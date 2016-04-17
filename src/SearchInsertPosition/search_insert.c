91c0e6b78ecde1234d4471a5e67895d4e6837bed
ajaxian79 <blaketullysmith@gmail.com>
2016-04-16 20:00:05 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -248,3 +248,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 232: Time complexity: O(n).
 // note 233: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 234: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 235: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
