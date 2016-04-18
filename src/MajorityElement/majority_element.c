a59f9b99cddabc7c03e33f2eda35a16d49d9e02d
ajaxian79 <blaketullysmith@gmail.com>
2016-04-18 08:01:05 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -230,3 +230,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 208: Walk both pointers from each end inward; advance the smaller side.
 // note 209: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 210: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 211: Space complexity: O(log n) for the recursion stack.
