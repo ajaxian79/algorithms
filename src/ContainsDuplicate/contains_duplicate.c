f610985ac8116f3bbfbc3e5e00fff335bf8c8bda
ajaxian79 <blaketullysmith@gmail.com>
2016-04-21 16:41:10 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -311,3 +311,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 232: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 233: Handles negative inputs as documented above.
 // note 234: Handles empty input by returning 0.
+// note 235: State compression: bitmask + integer encodes a small subset cheaply.
