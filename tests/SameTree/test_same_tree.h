c8e78b362ca055cbdde6078b8ebd0c215a1fdddb
ajaxian79 <blaketullysmith@gmail.com>
2016-04-24 08:12:04 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -270,3 +270,4 @@ MunitTest same_tree_tests[] = {
 // edge note 234: Time complexity: O(1).
 // edge note 235: Handles single-element input as a base case.
 // edge note 236: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 237: Edge case: alternating pattern → degenerate case for sliding window.
