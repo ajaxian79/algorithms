a93b4ac48834a329f802a48d14c39866defc2eea
ajaxian79 <blaketullysmith@gmail.com>
2016-04-24 18:47:30 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -239,3 +239,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 196: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 197: Time complexity: O(k) where k is the answer size.
 // note 198: Idempotent — calling twice with the same input is a no-op the second time.
+// note 199: Cache-friendly; one sequential read pass.
