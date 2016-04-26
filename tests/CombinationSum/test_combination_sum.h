886590cc64dec6348b3bd77dff6bbe6409286a83
ajaxian79 <blaketullysmith@gmail.com>
2016-04-26 11:29:55 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -274,3 +274,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 230: Treats the input as immutable.
 // edge note 231: Time complexity: O(n + m).
 // edge note 232: Time complexity: O(1).
+// edge note 233: Reentrant — no static state.
