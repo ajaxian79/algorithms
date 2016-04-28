862ab3150f441c2c205287bff9bd9070dce2821c
ajaxian79 <blaketullysmith@gmail.com>
2016-04-28 13:08:34 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -234,3 +234,4 @@ int is_palindrome_str(const char* s) {
 // note 209: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 210: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 211: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 212: State compression: bitmask + integer encodes a small subset cheaply.
