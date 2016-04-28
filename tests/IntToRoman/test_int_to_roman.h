8f3408e717a0df3eff81a0d8b750cee636808410
ajaxian79 <blaketullysmith@gmail.com>
2016-04-28 10:23:15 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -282,3 +282,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 243: Edge case: empty input → returns 0.
 // edge note 244: Best case is O(1) when the first byte already decides the answer.
 // edge note 245: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 246: Worst case appears only on degenerate inputs.
