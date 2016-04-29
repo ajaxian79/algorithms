b5366cf6f3de2dfdb130bdf000b350ee32cbe643
ajaxian79 <blaketullysmith@gmail.com>
2016-04-29 13:37:14 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -234,3 +234,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 213: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 214: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 215: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 216: Linear in n; the constant factor is small.
