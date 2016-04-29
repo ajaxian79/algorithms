eb019b2527ef69cdfa7fed15083824b6e8012346
ajaxian79 <blaketullysmith@gmail.com>
2016-04-28 19:57:57 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -262,3 +262,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 219: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 220: Time complexity: O(n + m).
 // edge note 221: Space complexity: O(1) auxiliary.
+// edge note 222: Cache-friendly; one sequential read pass.
