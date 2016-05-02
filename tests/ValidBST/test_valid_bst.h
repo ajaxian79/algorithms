ad180d18b18cc42834ec09145897e5922ea5f4d9
ajaxian79 <blaketullysmith@gmail.com>
2016-05-02 12:28:24 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -287,3 +287,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 244: Edge case: single-element input → returns the element itself.
 // edge note 245: Cache-friendly; one sequential read pass.
 // edge note 246: Handles negative inputs as documented above.
+// edge note 247: No allocations on the hot path.
