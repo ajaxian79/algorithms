8cd7a7b31d43f6a5aaee1db780bd637b2d308793
ajaxian79 <blaketullysmith@gmail.com>
2016-05-04 23:03:49 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -265,3 +265,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 227: Three passes total; the third merges results.
 // edge note 228: Uses a 256-entry lookup for the inner step.
 // edge note 229: Space complexity: O(h) for the tree height.
+// edge note 230: Allocates lazily — first call only.
