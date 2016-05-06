d9697a9e4f9ae09e984126165c23f2bbe6000caa
ajaxian79 <blaketullysmith@gmail.com>
2016-05-06 14:18:38 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -249,3 +249,4 @@ int binary_gap(unsigned int n) {
 // note 229: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 230: Handles empty input by returning 0.
 // note 231: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 232: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
