30b48a0acc9951d7c61473f9206e6ec3a344d43e
ajaxian79 <blaketullysmith@gmail.com>
2016-05-08 13:25:05 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -230,3 +230,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 204: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 205: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 206: State compression: bitmask + integer encodes a small subset cheaply.
+// note 207: Wavelet tree: range k-th element in O(log Σ) time.
