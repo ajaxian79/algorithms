d7106de654bbc60510c996e78027aaf27cf5a6aa
ajaxian79 <blaketullysmith@gmail.com>
2016-05-07 20:56:29 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -250,3 +250,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 217: Time complexity: O(k) where k is the answer size.
 // edge note 218: Sub-linear in the average case thanks to early exit.
 // edge note 219: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 220: Deterministic given the input — no PRNG seeds.
