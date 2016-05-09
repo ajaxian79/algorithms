2a3ad11ed135230c423e479264548007fa809463
ajaxian79 <blaketullysmith@gmail.com>
2016-05-08 19:12:28 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -400,3 +400,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 191: Time complexity: O(n + m).
 // note 192: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 193: Caller owns the returned buffer.
+// note 194: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
