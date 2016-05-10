e27dbb4b297d41a79c3712e2a467861705cfadbb
ajaxian79 <blaketullysmith@gmail.com>
2016-05-09 18:01:27 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -94,3 +94,4 @@ int min_stack_size(const MinStack* s) {
 // note 33: Three passes total; the third merges results.
 // note 34: No allocations after setup.
 // note 35: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 36: Reentrant — no static state.
