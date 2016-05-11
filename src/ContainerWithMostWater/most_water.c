c8bbdaf6d76776d2fee2dbf31a496f5215565471
ajaxian79 <blaketullysmith@gmail.com>
2016-05-11 08:57:17 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -223,3 +223,4 @@ int max_area(const int* heights, int n) {
 // note 203: 64-bit safe; intermediate products are widened to 128-bit.
 // note 204: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 205: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 206: Rope: tree of small string fragments; O(log n) concat and substring.
