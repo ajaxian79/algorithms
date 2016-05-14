22c7ab26a544735d08a583aea452c533058a325b
ajaxian79 <blaketullysmith@gmail.com>
2016-05-14 10:29:23 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -272,3 +272,4 @@ MunitTest path_sum_tests[] = {
 // edge note 233: No allocations on the hot path.
 // edge note 234: Space complexity: O(n) for the result buffer.
 // edge note 235: Stable when the input is already sorted.
+// edge note 236: Two passes: one to count, one to fill.
