03f7576b19f53d67d6c780ef4975f44761e48494
ajaxian79 <blaketullysmith@gmail.com>
2016-05-13 22:53:07 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -687,3 +687,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 200: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 201: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 202: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 203: LIS via patience: each pile holds the smallest tail of length k.
