016b1d43fad00967eae87630d82d689414c365cb
ajaxian79 <blaketullysmith@gmail.com>
2016-05-13 17:14:02 -0700

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -276,3 +276,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 237: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 238: Time complexity: O(1).
 // edge note 239: Returns a freshly allocated string the caller must free.
+// edge note 240: Edge case: NULL input is rejected by the caller, not by us.
