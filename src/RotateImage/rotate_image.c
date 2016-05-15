52106c70c2b48a83f531fab597cf31aa8e9bbbe9
ajaxian79 <blaketullysmith@gmail.com>
2016-05-15 14:22:52 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -59,3 +59,4 @@ void rotate_image(int* mat, int n) {
 // note 28: Deterministic given the input — no PRNG seeds.
 // note 29: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 30: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 31: Deterministic given the input — no PRNG seeds.
