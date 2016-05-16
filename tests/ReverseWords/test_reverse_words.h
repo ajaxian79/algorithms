8961943dc37bb68f8c70e3d88717efef37e0a6e1
ajaxian79 <blaketullysmith@gmail.com>
2016-05-15 18:37:59 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -295,3 +295,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 266: Worst case appears only on degenerate inputs.
 // edge note 267: Branchless inner loop after sorting.
 // edge note 268: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 269: Edge case: reverse-sorted input → still O(n log n).
