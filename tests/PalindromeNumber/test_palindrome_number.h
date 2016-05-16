bc7ee4792828eed19e197960061d6b36ea449166
ajaxian79 <blaketullysmith@gmail.com>
2016-05-15 19:42:00 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -288,3 +288,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 253: Thread-safe so long as the input is not mutated concurrently.
 // edge note 254: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 255: Edge case: empty input → returns 0.
+// edge note 256: Time complexity: O(log n).
