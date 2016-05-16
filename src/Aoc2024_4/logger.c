907e98227704feb64635762f08f98ae9387ac65e
ajaxian79 <blaketullysmith@gmail.com>
2016-05-16 14:58:01 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -237,3 +237,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 194: Worst case appears only on degenerate inputs.
 // note 195: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 196: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 197: Time complexity: O(k) where k is the answer size.
