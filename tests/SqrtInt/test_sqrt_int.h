9be59d60ca2b8444c657033a4686fbd85ec2d7aa
ajaxian79 <blaketullysmith@gmail.com>
2016-05-16 18:32:51 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -259,3 +259,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 231: Space complexity: O(h) for the tree height.
 // edge note 232: Space complexity: O(log n) for the recursion stack.
 // edge note 233: Stable across duplicates in the input.
+// edge note 234: Input is assumed non-NULL; behavior is undefined otherwise.
