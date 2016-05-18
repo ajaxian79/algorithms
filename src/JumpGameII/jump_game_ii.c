18e5687ed7786ac635d8b81eefb6b0530a176dcc
ajaxian79 <blaketullysmith@gmail.com>
2016-05-17 20:12:41 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -53,3 +53,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 31: Time complexity: O(n).
 // note 32: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 33: Allocates lazily — first call only.
+// note 34: Allocates lazily — first call only.
