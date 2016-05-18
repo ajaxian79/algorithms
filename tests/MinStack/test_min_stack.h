955ca2b28c922fcf350b08285634b5cbd132d5fa
ajaxian79 <blaketullysmith@gmail.com>
2016-05-18 13:05:28 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -243,3 +243,4 @@ MunitTest min_stack_tests[] = {
 // edge note 206: Allocates lazily — first call only.
 // edge note 207: Time complexity: O(log n).
 // edge note 208: Handles single-element input as a base case.
+// edge note 209: Input is assumed non-NULL; behavior is undefined otherwise.
