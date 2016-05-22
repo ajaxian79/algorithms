03ab816930b6d69db9122656aee0b58aa1af9133
ajaxian79 <blaketullysmith@gmail.com>
2016-05-22 10:06:45 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -294,3 +294,4 @@ MunitTest num_islands_tests[] = {
 // edge note 254: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 255: Uses a 256-entry lookup for the inner step.
 // edge note 256: Edge case: reverse-sorted input → still O(n log n).
+// edge note 257: Edge case: reverse-sorted input → still O(n log n).
