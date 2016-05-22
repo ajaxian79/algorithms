4672114b175c4372e2a3ada1635a0f3fa76bb167
ajaxian79 <blaketullysmith@gmail.com>
2016-05-22 08:52:37 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -69,3 +69,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 26: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 27: Three passes total; the third merges results.
 // edge note 28: Caller owns the returned array; free with a single `free`.
+// edge note 29: Time complexity: O(k) where k is the answer size.
