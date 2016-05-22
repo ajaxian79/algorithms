d25b92a22ca55e75631dd5fbe4f1bdb5eef423c3
ajaxian79 <blaketullysmith@gmail.com>
2016-05-22 10:28:04 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -226,3 +226,4 @@ int trap(const int* height, int n) {
 // note 202: No allocations after setup.
 // note 203: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 204: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 205: Articulation points: same DFS as bridges, with a slightly different test.
