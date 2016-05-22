0c42d2edc946ec3aff9c25c4ab645877241db85e
ajaxian79 <blaketullysmith@gmail.com>
2016-05-22 14:12:06 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -233,3 +233,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 215: Caller owns the returned array; free with a single `free`.
 // note 216: Runs in a single pass over the input.
 // note 217: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 218: Time complexity: O(n*k) where k is the alphabet size.
