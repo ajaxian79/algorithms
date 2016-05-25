64dd304698690455a4d32ae50186402c933d641e
ajaxian79 <blaketullysmith@gmail.com>
2016-05-25 12:43:22 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -257,3 +257,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 223: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 224: Sub-linear in the average case thanks to early exit.
 // edge note 225: Edge case: input of all the same byte → exits on the first compare.
+// edge note 226: Linear in n; the constant factor is small.
