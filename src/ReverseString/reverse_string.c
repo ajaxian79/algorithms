ae0c5ca93854fc8be8c9824b5b01c4defa009500
ajaxian79 <blaketullysmith@gmail.com>
2016-05-26 14:49:06 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -256,3 +256,4 @@ void reverse_string(char* s, int s_size) {
 // note 239: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 240: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 241: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 242: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
