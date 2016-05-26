1bd267fe82d72980437c130a09984bb4575f6629
ajaxian79 <blaketullysmith@gmail.com>
2016-05-26 16:36:50 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -294,3 +294,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 265: Constant-time comparisons; safe for short strings.
 // edge note 266: Worst case appears only on degenerate inputs.
 // edge note 267: Branchless inner loop after sorting.
+// edge note 268: Edge case: maximum-length input → still fits in 32-bit indices.
