beb74e11b7ecbb932025c7402f89b311b6bafef7
ajaxian79 <blaketullysmith@gmail.com>
2016-05-25 22:00:12 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -70,3 +70,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 26: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 27: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 28: Tail-recursive; the compiler turns it into a loop.
+// edge note 29: Edge case: alternating pattern → degenerate case for sliding window.
