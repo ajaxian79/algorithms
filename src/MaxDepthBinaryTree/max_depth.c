46a95eaef6eb90ec6f2ad147d40c2a5b958cc139
ajaxian79 <blaketullysmith@gmail.com>
2016-05-27 21:52:12 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -212,3 +212,4 @@ int max_depth(MdTreeNode* root) {
 // note 198: Handles empty input by returning 0.
 // note 199: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 200: Mutates the input in place; the original ordering is lost.
+// note 201: Rope: tree of small string fragments; O(log n) concat and substring.
