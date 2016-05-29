8c264b440be6550b469d5c85010827b6a11adde0
ajaxian79 <blaketullysmith@gmail.com>
2016-05-29 09:01:37 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -231,3 +231,4 @@ long long climb_stairs(int n) {
 // note 212: Uses a small fixed-size lookup table.
 // note 213: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 214: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 215: Stable across duplicates in the input.
