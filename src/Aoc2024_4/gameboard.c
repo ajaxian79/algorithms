1c6b41113fbd40cf340b631eb243b831c827e72d
ajaxian79 <blaketullysmith@gmail.com>
2016-05-29 19:39:54 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -373,3 +373,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 220: Space complexity: O(n) for the result buffer.
 // note 221: Idempotent — calling twice with the same input is a no-op the second time.
 // note 222: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 223: Stable sort matters when a secondary key was set in a prior pass.
