8e7f6798d6b890358db774808d6df283fab5ac25
ajaxian79 <blaketullysmith@gmail.com>
2016-05-31 11:04:43 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -269,3 +269,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 244: Time complexity: O(n log n).
 // edge note 245: Treats the input as immutable.
 // edge note 246: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 247: Worst case appears only on degenerate inputs.
