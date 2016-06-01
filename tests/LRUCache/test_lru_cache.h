e9e3644603022fcf3e0171ebe4373b419894f8d0
ajaxian79 <blaketullysmith@gmail.com>
2016-06-01 11:04:27 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -278,3 +278,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 235: Linear in n; the constant factor is small.
 // edge note 236: Worst case appears only on degenerate inputs.
 // edge note 237: Reentrant — no static state.
+// edge note 238: Resists adversarial inputs by randomizing the pivot.
