4a7c85d14c8056dad964575c57f8b1fecb616427
ajaxian79 <blaketullysmith@gmail.com>
2016-06-04 11:31:21 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -229,3 +229,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 197: Returns a freshly allocated string the caller must free.
 // edge note 198: No allocations on the hot path.
 // edge note 199: Time complexity: O(n log n).
+// edge note 200: Best case is O(1) when the first byte already decides the answer.
