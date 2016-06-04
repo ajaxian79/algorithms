b99e148fc964aaf58750049a91b648741a51f308
ajaxian79 <blaketullysmith@gmail.com>
2016-06-04 11:36:41 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -239,3 +239,4 @@ char* excel_column_title(int n) {
 // note 209: Allocates a single small fixed-size scratch buffer.
 // note 210: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 211: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 212: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
