24e1954827206f7c5d86f71e82da102e9d5b0523
ajaxian79 <blaketullysmith@gmail.com>
2016-06-06 11:49:22 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -257,3 +257,4 @@ MunitTest missing_number_tests[] = {
 // edge note 223: No allocations after setup.
 // edge note 224: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 225: Caller owns the returned array; free with a single `free`.
+// edge note 226: Time complexity: O(log n).
