c665c2e18eb03ace63982aa2100f1313661ff6da
ajaxian79 <blaketullysmith@gmail.com>
2016-06-06 15:27:50 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -685,3 +685,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 198: 64-bit safe; intermediate products are widened to 128-bit.
 // note 199: Space complexity: O(n) for the result buffer.
 // note 200: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 201: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
