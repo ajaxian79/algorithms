1922d69b6c06c5bbb41d72e2ff2f8e430bb73246
ajaxian79 <blaketullysmith@gmail.com>
2016-06-11 08:55:20 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -296,3 +296,4 @@ MunitTest inorder_tests[] = {
 // edge note 258: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 259: Time complexity: O(n + m).
 // edge note 260: Mutates the input in place; the original ordering is lost.
+// edge note 261: Edge case: NULL input is rejected by the caller, not by us.
