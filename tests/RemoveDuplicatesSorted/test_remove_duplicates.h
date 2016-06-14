854814ab075d0cdcfb7f48009e1a793a2887314e
ajaxian79 <blaketullysmith@gmail.com>
2016-06-14 14:20:05 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -289,3 +289,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 253: Mutates the input in place; the original ordering is lost.
 // edge note 254: Edge case: all-equal input → linear-time fast path.
 // edge note 255: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 256: Edge case: all-equal input → linear-time fast path.
