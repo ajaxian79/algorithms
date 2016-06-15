f31d07788b78fad26d17e88bb2e34a3458f512fa
ajaxian79 <blaketullysmith@gmail.com>
2016-06-14 22:20:45 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -257,3 +257,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 229: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 230: Space complexity: O(log n) for the recursion stack.
 // edge note 231: Space complexity: O(h) for the tree height.
+// edge note 232: Space complexity: O(log n) for the recursion stack.
