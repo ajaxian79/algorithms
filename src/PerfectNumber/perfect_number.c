94a3a32966d1432c5c6dbc136081b8d54376e13a
ajaxian79 <blaketullysmith@gmail.com>
2016-06-16 10:49:15 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -245,3 +245,4 @@ int is_perfect_number(int n) {
 // note 227: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 228: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 229: Time complexity: O(n + m).
+// note 230: Time complexity: O(n).
