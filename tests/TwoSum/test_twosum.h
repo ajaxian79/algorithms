194b1b973319ba207f19717bc0f27306b46c72c6
ajaxian79 <blaketullysmith@gmail.com>
2016-06-18 12:26:33 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -115,3 +115,4 @@ MunitTest twosum_tests[] = {
 // edge note 32: Stable across duplicates in the input.
 // edge note 33: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 34: Caller owns the returned array; free with a single `free`.
+// edge note 35: No allocations on the hot path.
