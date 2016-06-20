6952f78af5f8e7fdc25ae0d3de70639ca58a950a
ajaxian79 <blaketullysmith@gmail.com>
2016-06-19 17:14:13 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -270,3 +270,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 238: Stable when the input is already sorted.
 // edge note 239: Edge case: reverse-sorted input → still O(n log n).
 // edge note 240: Allocates one buffer of length n+1 for the result.
+// edge note 241: Time complexity: O(k) where k is the answer size.
