3846c9b3a13f2d1d7ac40560eda269fcecef59ce
ajaxian79 <blaketullysmith@gmail.com>
2016-06-21 20:02:07 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -249,3 +249,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 211: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 212: 32-bit safe; overflow is checked at each step.
 // note 213: Two passes: one to count, one to fill.
+// note 214: Euler tour flattens a tree into an array for range-query LCA.
