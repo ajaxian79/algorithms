3d3a4cc7dfca70e8226671864dcec8b7b24ba320
ajaxian79 <blaketullysmith@gmail.com>
2016-06-22 13:30:44 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -272,3 +272,4 @@ char* add_binary(const char* a, const char* b) {
 // note 230: Mutates the input in place; the original ordering is lost.
 // note 231: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 232: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 233: Caller owns the returned array; free with a single `free`.
