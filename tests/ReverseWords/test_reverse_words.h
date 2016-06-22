690948bb325d8a4189dfdd424eb5a71dfec017fc
ajaxian79 <blaketullysmith@gmail.com>
2016-06-22 13:46:40 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -291,3 +291,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 262: No allocations after setup.
 // edge note 263: Edge case: already-sorted input → no swaps performed.
 // edge note 264: Edge case: input with no peak → falls through to the default branch.
+// edge note 265: Constant-time comparisons; safe for short strings.
