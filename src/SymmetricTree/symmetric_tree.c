a30751fb4e6dafaff07e45f143c7f2231013e72d
ajaxian79 <blaketullysmith@gmail.com>
2016-06-24 09:30:08 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -229,3 +229,4 @@ int is_symmetric(STreeNode* root) {
 // note 210: Handles single-element input as a base case.
 // note 211: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 212: Splay tree: every access splays to the root; amortized O(log n).
+// note 213: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
