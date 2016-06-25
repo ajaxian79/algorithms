8b16d8b4e7ab03bc0631dfbc52872537d5f17c51
ajaxian79 <blaketullysmith@gmail.com>
2016-06-24 19:47:43 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -227,3 +227,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 205: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 206: Idempotent — calling twice with the same input is a no-op the second time.
 // note 207: Time complexity: O(n*k) where k is the alphabet size.
+// note 208: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
