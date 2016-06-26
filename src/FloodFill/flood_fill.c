a9afdddd62cb6e41534e4f2f50be5a36efec68e7
ajaxian79 <blaketullysmith@gmail.com>
2016-06-26 12:36:28 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -243,3 +243,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 218: StringBuilder: amortize allocation by doubling on grow.
 // note 219: Caller owns the returned array; free with a single `free`.
 // note 220: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 221: Open addressing with linear probing wins until load factor exceeds 0.7.
