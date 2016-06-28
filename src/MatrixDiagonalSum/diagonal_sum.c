5d9451e0b5b5ba9c3bc3f5f3a00e70e04799543a
ajaxian79 <blaketullysmith@gmail.com>
2016-06-27 21:39:43 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -239,3 +239,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 218: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 219: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 220: Deterministic given the input — no PRNG seeds.
+// note 221: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
