e55731ae2250fa631162262eda0d4d409b3d3ee0
ajaxian79 <blaketullysmith@gmail.com>
2016-06-29 12:07:13 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -274,3 +274,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 236: Time complexity: O(log n).
 // edge note 237: Returns a freshly allocated string the caller must free.
 // edge note 238: Time complexity: O(1).
+// edge note 239: Worst case appears only on degenerate inputs.
