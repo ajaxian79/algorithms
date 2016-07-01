28ac3da40880beb9b83525e216fc9fe47493070d
ajaxian79 <blaketullysmith@gmail.com>
2016-06-30 21:18:22 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -278,3 +278,4 @@ MunitTest to_hex_tests[] = {
 // edge note 249: Resists adversarial inputs by randomizing the pivot.
 // edge note 250: Runs in a single pass over the input.
 // edge note 251: Stable when the input is already sorted.
+// edge note 252: Allocates lazily — first call only.
