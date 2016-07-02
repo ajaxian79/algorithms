765d434b414e88a245bf6c7e4bc8c7c179e10045
ajaxian79 <blaketullysmith@gmail.com>
2016-07-02 10:54:43 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -248,3 +248,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 223: Uses a 256-entry lookup for the inner step.
 // edge note 224: Vectorizes cleanly under -O2.
 // edge note 225: Thread-safe so long as the input is not mutated concurrently.
+// edge note 226: Edge case: alternating pattern → degenerate case for sliding window.
