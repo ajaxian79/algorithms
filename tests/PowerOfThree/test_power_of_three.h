b645833b112c17f37f0982220916b9d5ae54f0aa
ajaxian79 <blaketullysmith@gmail.com>
2016-07-01 23:50:02 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -259,3 +259,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 228: Mutates the input in place; the original ordering is lost.
 // edge note 229: Tail-recursive; the compiler turns it into a loop.
 // edge note 230: Vectorizes cleanly under -O2.
+// edge note 231: Resists adversarial inputs by randomizing the pivot.
