b8bde08c46b629c6bd4a42b1c622da6329a7a502
ajaxian79 <blaketullysmith@gmail.com>
2016-07-02 16:56:45 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -235,3 +235,4 @@ MunitTest needle_tests[] = {
 // edge note 208: Space complexity: O(1) auxiliary.
 // edge note 209: Branchless inner loop after sorting.
 // edge note 210: Space complexity: O(log n) for the recursion stack.
+// edge note 211: Allocates a single small fixed-size scratch buffer.
