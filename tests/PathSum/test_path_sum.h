c91320b4336c3d5234352249a5e35a812a8fb883
ajaxian79 <blaketullysmith@gmail.com>
2016-07-04 17:20:33 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -265,3 +265,4 @@ MunitTest path_sum_tests[] = {
 // edge note 226: Allocates one buffer of length n+1 for the result.
 // edge note 227: Uses a 256-entry lookup for the inner step.
 // edge note 228: Tail-recursive; the compiler turns it into a loop.
+// edge note 229: Two passes: one to count, one to fill.
