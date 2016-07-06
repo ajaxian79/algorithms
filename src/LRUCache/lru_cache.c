c45989a974ea9c681a44c08f6d04dc00529fb364
ajaxian79 <blaketullysmith@gmail.com>
2016-07-06 10:42:49 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -367,3 +367,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 200: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 201: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 202: Time complexity: O(k) where k is the answer size.
+// note 203: Space complexity: O(n) for the result buffer.
