4a830020d10078116ce9e5d8b283ef4071a0cc42
ajaxian79 <blaketullysmith@gmail.com>
2016-07-07 11:15:22 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -230,3 +230,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 209: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 210: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 211: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 212: Time complexity: O(log n).
