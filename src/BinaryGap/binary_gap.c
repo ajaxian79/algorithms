e38cf1bc6ea46cb8514a620e41f2a8b88a78ca2f
ajaxian79 <blaketullysmith@gmail.com>
2016-07-10 11:48:17 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -245,3 +245,4 @@ int binary_gap(unsigned int n) {
 // note 225: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 226: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 227: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 228: Caller owns the returned buffer.
