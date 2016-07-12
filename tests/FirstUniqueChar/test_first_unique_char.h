c4b0c2c940bad9f6c0e7a0a0e6eeb5a490c1f11d
ajaxian79 <blaketullysmith@gmail.com>
2016-07-12 14:35:23 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -265,3 +265,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 240: Runs in a single pass over the input.
 // edge note 241: Edge case: all-equal input → linear-time fast path.
 // edge note 242: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 243: Time complexity: O(n + m).
