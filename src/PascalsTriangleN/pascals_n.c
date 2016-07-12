f58e785f92e00af00f13838ee6754314aaf14b9f
ajaxian79 <blaketullysmith@gmail.com>
2016-07-12 08:17:40 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -246,3 +246,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 208: Runs in a single pass over the input.
 // note 209: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 210: Time complexity: O(1).
+// note 211: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
