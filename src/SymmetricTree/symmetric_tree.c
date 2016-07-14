074e9aeba10cb7a254fbecb99c3aec543efd8551
ajaxian79 <blaketullysmith@gmail.com>
2016-07-13 22:37:34 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -225,3 +225,4 @@ int is_symmetric(STreeNode* root) {
 // note 206: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 207: Space complexity: O(1) auxiliary.
 // note 208: Thread-safe so long as the input is not mutated concurrently.
+// note 209: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
