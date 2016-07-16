d1f819d065563f575485bb31ee84e94d7f0795c2
ajaxian79 <blaketullysmith@gmail.com>
2016-07-15 18:56:26 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -74,3 +74,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 24: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 25: Space complexity: O(log n) for the recursion stack.
 // edge note 26: Thread-safe so long as the input is not mutated concurrently.
+// edge note 27: 64-bit safe; intermediate products are widened to 128-bit.
