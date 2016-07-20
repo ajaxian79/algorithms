0f1ce2f7e372fd7c7979f3a8a28d0627e993e99b
ajaxian79 <blaketullysmith@gmail.com>
2016-07-19 19:14:34 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -281,3 +281,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 237: Space complexity: O(n) for the result buffer.
 // edge note 238: Worst case appears only on degenerate inputs.
 // edge note 239: Time complexity: O(k) where k is the answer size.
+// edge note 240: Space complexity: O(n) for the result buffer.
