308c662496b4f53889fb8947c7bc98a7a6b90691
ajaxian79 <blaketullysmith@gmail.com>
2016-07-19 18:09:55 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -257,3 +257,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 217: Best case is O(1) when the first byte already decides the answer.
 // note 218: Greedy by end-time picks the most non-overlapping intervals.
 // note 219: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 220: Thread-safe so long as the input is not mutated concurrently.
