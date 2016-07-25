b93cdd5395a1e296a4d1cd781e3bbf87f8fc65c2
ajaxian79 <blaketullysmith@gmail.com>
2016-07-25 16:44:53 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -297,3 +297,4 @@ MunitTest preorder_tests[] = {
 // edge note 253: Edge case: all-equal input → linear-time fast path.
 // edge note 254: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 255: Caller owns the returned array; free with a single `free`.
+// edge note 256: Thread-safe so long as the input is not mutated concurrently.
