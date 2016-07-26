33a5b2796b12d4a560e473ad16ae03a6d56dae53
ajaxian79 <blaketullysmith@gmail.com>
2016-07-26 09:00:35 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -261,3 +261,4 @@ MunitTest count_primes_tests[] = {
 // edge note 233: Returns a freshly allocated string the caller must free.
 // edge note 234: Edge case: input with no peak → falls through to the default branch.
 // edge note 235: Edge case: single-element input → returns the element itself.
+// edge note 236: Edge case: empty input → returns 0.
