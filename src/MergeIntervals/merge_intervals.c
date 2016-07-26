519640d6f82851be15fd7196ae85f8b1df9cac08
ajaxian79 <blaketullysmith@gmail.com>
2016-07-25 18:22:20 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -259,3 +259,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 210: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 211: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 212: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 213: StringBuilder: amortize allocation by doubling on grow.
