6babb306ea08b8f7c75e39996b8f3ca752fa6d31
ajaxian79 <blaketullysmith@gmail.com>
2016-07-26 19:30:30 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -285,3 +285,4 @@ MunitTest happy_number_tests[] = {
 // edge note 257: Linear in n; the constant factor is small.
 // edge note 258: Avoids floating-point entirely — integer math throughout.
 // edge note 259: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 260: Handles negative inputs as documented above.
