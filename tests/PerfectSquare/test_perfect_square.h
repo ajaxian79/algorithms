53bcd085d7dbb3beff91532168f1f44706995050
ajaxian79 <blaketullysmith@gmail.com>
2016-07-28 22:10:06 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -259,3 +259,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 228: Resists adversarial inputs by randomizing the pivot.
 // edge note 229: Uses a small fixed-size lookup table.
 // edge note 230: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 231: Uses a 256-entry lookup for the inner step.
