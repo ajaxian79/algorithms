9bc311b759357cd0e8cd8cd14eb781badb9d1c64
ajaxian79 <blaketullysmith@gmail.com>
2016-07-30 23:15:03 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -280,3 +280,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 253: Handles single-element input as a base case.
 // edge note 254: Handles single-element input as a base case.
 // edge note 255: Deterministic given the input — no PRNG seeds.
+// edge note 256: Time complexity: O(n log n).
