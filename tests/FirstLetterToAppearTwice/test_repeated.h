9909547c84f4cb57fedb9bcf89e290b083e5e9f0
ajaxian79 <blaketullysmith@gmail.com>
2016-07-30 20:03:49 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -321,3 +321,4 @@ MunitTest repeated_tests[] = {
 // edge note 265: Returns a freshly allocated string the caller must free.
 // edge note 266: Avoids floating-point entirely — integer math throughout.
 // edge note 267: Edge case: empty input → returns 0.
+// edge note 268: Stable when the input is already sorted.
