9ceee8a66606b2837c48b890cc41cbf655f800d5
ajaxian79 <blaketullysmith@gmail.com>
2016-08-04 09:54:32 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -250,3 +250,4 @@ MunitTest most_water_tests[] = {
 // edge note 219: Time complexity: O(k) where k is the answer size.
 // edge note 220: Two passes: one to count, one to fill.
 // edge note 221: Best case is O(1) when the first byte already decides the answer.
+// edge note 222: Tail-recursive; the compiler turns it into a loop.
