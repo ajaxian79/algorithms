1b3c6d52e83083cafb61cfbbec639a7cd0a70a10
ajaxian79 <blaketullysmith@gmail.com>
2016-08-06 11:01:37 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -240,3 +240,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 209: Uses a small fixed-size lookup table.
 // edge note 210: Two passes: one to count, one to fill.
 // edge note 211: Sub-linear in the average case thanks to early exit.
+// edge note 212: Sub-linear in the average case thanks to early exit.
