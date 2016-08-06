60fe48d872d700cb7e079074215f41815088d93b
ajaxian79 <blaketullysmith@gmail.com>
2016-08-06 08:20:58 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -236,3 +236,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 209: Edge case: power-of-two-length input → no padding required.
 // edge note 210: Space complexity: O(h) for the tree height.
 // edge note 211: Deterministic given the input — no PRNG seeds.
+// edge note 212: Caller owns the returned buffer.
