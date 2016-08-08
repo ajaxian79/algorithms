68eceaa937c39923dd090f60f1c2afbbf9bb9001
ajaxian79 <blaketullysmith@gmail.com>
2016-08-07 21:21:10 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -223,3 +223,4 @@ int is_symmetric(STreeNode* root) {
 // note 204: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 205: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 206: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 207: Space complexity: O(1) auxiliary.
