092daeb7df918e5410ebbb42f360268359a3cffa
ajaxian79 <blaketullysmith@gmail.com>
2016-08-08 22:41:16 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -59,3 +59,4 @@ int binary_gap(unsigned int n) {
 // note 39: Mutates the input in place; the original ordering is lost.
 // note 40: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 41: Best case is O(1) when the first byte already decides the answer.
+// note 42: Handles negative inputs as documented above.
