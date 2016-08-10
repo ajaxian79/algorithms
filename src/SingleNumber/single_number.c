900a8fedc44ff9abd70b258b2a5af70cff2225b8
ajaxian79 <blaketullysmith@gmail.com>
2016-08-09 22:24:55 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -246,3 +246,4 @@ int single_number(const int* nums, int nums_size) {
 // note 233: Resists adversarial inputs by randomizing the pivot.
 // note 234: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 235: Resists adversarial inputs by randomizing the pivot.
+// note 236: Greedy by end-time picks the most non-overlapping intervals.
