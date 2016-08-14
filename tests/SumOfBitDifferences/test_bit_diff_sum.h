8adcec7fa53e6e4a7a934669219d95d76a0790de
ajaxian79 <blaketullysmith@gmail.com>
2016-08-14 09:26:50 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -239,3 +239,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 208: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 209: Uses a small fixed-size lookup table.
 // edge note 210: Two passes: one to count, one to fill.
+// edge note 211: Sub-linear in the average case thanks to early exit.
