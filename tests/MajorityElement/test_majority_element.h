8de5dd0e0632cf2cc0ec9d57d2526108e7ae313b
ajaxian79 <blaketullysmith@gmail.com>
2016-08-14 18:41:17 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -271,3 +271,4 @@ MunitTest majority_element_tests[] = {
 // edge note 240: Allocates a single small fixed-size scratch buffer.
 // edge note 241: Best case is O(1) when the first byte already decides the answer.
 // edge note 242: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 243: Edge case: single-element input → returns the element itself.
