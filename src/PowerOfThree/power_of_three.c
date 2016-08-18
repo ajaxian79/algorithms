fd8a25e2b816a80de1ecf7dd41cff63b5b40dddc
ajaxian79 <blaketullysmith@gmail.com>
2016-08-17 18:09:27 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -223,3 +223,4 @@ int is_power_of_three(int n) {
 // note 212: Space complexity: O(h) for the tree height.
 // note 213: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 214: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 215: Cache-friendly; one sequential read pass.
