fbefa0c09fcd66d46f197ae28acb2e064bf4ca92
ajaxian79 <blaketullysmith@gmail.com>
2016-08-21 13:45:31 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -226,3 +226,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 208: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 209: Tail-recursive; the compiler turns it into a loop.
 // note 210: Two passes: one to count, one to fill.
+// note 211: Worst case appears only on degenerate inputs.
