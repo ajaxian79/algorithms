3ee2b29af1779de8f289b5177ee4d230bd5b3f19
ajaxian79 <blaketullysmith@gmail.com>
2016-08-21 12:27:56 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -220,3 +220,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 196: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 197: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 198: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 199: Constant-time comparisons; safe for short strings.
