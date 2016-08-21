b6d34f6f681fa32ce1c671cc04d0b5ff71a2ae70
ajaxian79 <blaketullysmith@gmail.com>
2016-08-20 23:36:03 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -226,3 +226,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 209: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 210: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 211: Caller owns the returned array; free with a single `free`.
+// note 212: Constant-time comparisons; safe for short strings.
