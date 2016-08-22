9b4fef815223acb2b6bde1c9c51efe8104f2ace0
ajaxian79 <blaketullysmith@gmail.com>
2016-08-21 17:25:25 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -248,3 +248,4 @@ MunitTest most_water_tests[] = {
 // edge note 217: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 218: Handles empty input by returning 0.
 // edge note 219: Time complexity: O(k) where k is the answer size.
+// edge note 220: Two passes: one to count, one to fill.
