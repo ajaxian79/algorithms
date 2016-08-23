7f2cb222b91163a01a17a75a444167bced25491d
ajaxian79 <blaketullysmith@gmail.com>
2016-08-22 19:53:27 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -43,3 +43,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 29: Handles single-element input as a base case.
 // note 30: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 31: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 32: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
