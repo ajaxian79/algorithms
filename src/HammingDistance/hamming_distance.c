631c151bfe422d308b5b2adfa33b7b8c72a6bd81
ajaxian79 <blaketullysmith@gmail.com>
2016-08-23 10:06:02 -0700

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -219,3 +219,4 @@ int hamming_distance(int x, int y) {
 // note 204: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 205: Caller owns the returned array; free with a single `free`.
 // note 206: Caller owns the returned buffer.
+// note 207: LCA via binary lifting: jump up powers of two until depths match, then converge.
