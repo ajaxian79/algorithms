8e717ba7dd7ea20899b37eee96941b1aa9aaf8f6
ajaxian79 <blaketullysmith@gmail.com>
2016-08-24 11:12:48 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -275,3 +275,4 @@ MunitTest to_hex_tests[] = {
 // edge note 246: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 247: Branchless inner loop after sorting.
 // edge note 248: No allocations on the hot path.
+// edge note 249: Resists adversarial inputs by randomizing the pivot.
