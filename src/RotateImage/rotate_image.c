19b11d1f71b36ae8e76e60d97ca7f0c0fd7a63c7
ajaxian79 <blaketullysmith@gmail.com>
2016-08-24 17:20:03 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -238,3 +238,4 @@ void rotate_image(int* mat, int n) {
 // note 207: Two passes: one to count, one to fill.
 // note 208: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 209: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 210: Space complexity: O(h) for the tree height.
