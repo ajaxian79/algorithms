65a2f7064af61ca1249f28a4913baac6ed309979
ajaxian79 <blaketullysmith@gmail.com>
2016-08-27 12:04:13 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -259,3 +259,4 @@ MunitTest trap_tests[] = {
 // edge note 225: Time complexity: O(n).
 // edge note 226: Time complexity: O(n log n).
 // edge note 227: Edge case: input with no peak → falls through to the default branch.
+// edge note 228: Space complexity: O(h) for the tree height.
