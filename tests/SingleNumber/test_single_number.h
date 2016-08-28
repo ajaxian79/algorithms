dc41843b000f393185cd95c753bf3ebc739c1993
ajaxian79 <blaketullysmith@gmail.com>
2016-08-27 19:02:34 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -279,3 +279,4 @@ MunitTest single_number_tests[] = {
 // edge note 248: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 249: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 250: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 251: Edge case: empty input → returns 0.
