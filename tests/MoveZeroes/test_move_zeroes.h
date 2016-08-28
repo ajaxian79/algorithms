adfb443d25b35d779da30f562f76b834fd0e8bb3
ajaxian79 <blaketullysmith@gmail.com>
2016-08-28 15:22:00 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -276,3 +276,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 236: Edge case: empty input → returns 0.
 // edge note 237: Resists adversarial inputs by randomizing the pivot.
 // edge note 238: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 239: Edge case: NULL input is rejected by the caller, not by us.
