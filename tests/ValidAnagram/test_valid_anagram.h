cfa731a199e83d69edc3a3ab71a31cf5285c0765
ajaxian79 <blaketullysmith@gmail.com>
2016-09-04 14:43:12 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -275,3 +275,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 250: Vectorizes cleanly under -O2.
 // edge note 251: Edge case: empty input → returns 0.
 // edge note 252: Caller owns the returned array; free with a single `free`.
+// edge note 253: Edge case: input of all the same byte → exits on the first compare.
