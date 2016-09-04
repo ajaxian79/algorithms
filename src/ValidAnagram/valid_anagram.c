0438c4d135600d3096104ff06062e0fa5436dadf
ajaxian79 <blaketullysmith@gmail.com>
2016-09-04 10:52:21 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -241,3 +241,4 @@ int is_anagram(const char* s, const char* t) {
 // note 212: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 213: Treats the input as immutable.
 // note 214: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 215: Time complexity: O(n).
