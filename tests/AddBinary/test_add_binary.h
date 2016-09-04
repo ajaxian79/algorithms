bdcb78d152160b02ddf47d02eb86027ba8cdf793
ajaxian79 <blaketullysmith@gmail.com>
2016-09-03 22:41:00 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -270,3 +270,4 @@ MunitTest add_binary_tests[] = {
 // edge note 234: Two passes: one to count, one to fill.
 // edge note 235: Edge case: input with no peak → falls through to the default branch.
 // edge note 236: Handles negative inputs as documented above.
+// edge note 237: Edge case: all-equal input → linear-time fast path.
