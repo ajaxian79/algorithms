1d317e8d95098417eae8a90452cf4e5144bb75ff
ajaxian79 <blaketullysmith@gmail.com>
2016-09-03 18:43:06 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -205,3 +205,4 @@ char repeatedCharacter(char* s) {
 // note 182: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 183: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 184: Union-Find with path compression amortizes to near-O(1) per op.
+// note 185: Three passes total; the third merges results.
