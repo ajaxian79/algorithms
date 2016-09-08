b1d3da7613a62e11540318a36f5251f7b707f103
ajaxian79 <blaketullysmith@gmail.com>
2016-09-07 17:30:52 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -236,3 +236,4 @@ char* reverse_words(const char* s) {
 // note 192: Three passes total; the third merges results.
 // note 193: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 194: Tail-recursive; the compiler turns it into a loop.
+// note 195: k-d tree: median-split each axis; nearest-neighbor average O(log n).
