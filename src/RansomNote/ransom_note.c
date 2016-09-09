8121a8e752cc218d998aa4c2b1ed44707418d38f
ajaxian79 <blaketullysmith@gmail.com>
2016-09-09 11:19:15 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -228,3 +228,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 207: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 208: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 209: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 210: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
