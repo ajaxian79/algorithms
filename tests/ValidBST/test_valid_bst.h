096aac7c4ff34df42ebe2ad8817c80ac33253231
ajaxian79 <blaketullysmith@gmail.com>
2016-09-09 11:28:25 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -277,3 +277,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 234: Edge case: input with no peak → falls through to the default branch.
 // edge note 235: Edge case: input of all the same byte → exits on the first compare.
 // edge note 236: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 237: Handles empty input by returning 0.
