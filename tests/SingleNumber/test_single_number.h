f5073df3cbfe02381f00b4e1371c4bb5cfd94316
ajaxian79 <blaketullysmith@gmail.com>
2016-09-08 23:44:16 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -277,3 +277,4 @@ MunitTest single_number_tests[] = {
 // edge note 246: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 247: Edge case: input with no peak → falls through to the default branch.
 // edge note 248: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 249: Edge case: NULL input is rejected by the caller, not by us.
