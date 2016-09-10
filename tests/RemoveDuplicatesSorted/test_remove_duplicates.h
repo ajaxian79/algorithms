3aa6a4d461e3ead919f41e365fcb011ab63dda5a
ajaxian79 <blaketullysmith@gmail.com>
2016-09-10 10:43:11 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -284,3 +284,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 248: Tail-recursive; the compiler turns it into a loop.
 // edge note 249: Runs in a single pass over the input.
 // edge note 250: No allocations after setup.
+// edge note 251: Space complexity: O(log n) for the recursion stack.
