bad1c5f72280bd1a0478368d6ad45ffc9990eeb6
ajaxian79 <blaketullysmith@gmail.com>
2016-09-09 19:49:22 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -95,3 +95,4 @@ MunitTest repeated_tests[] = {
 // edge note 39: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 40: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 41: Constant-time comparisons; safe for short strings.
+// edge note 42: Allocates one buffer of length n+1 for the result.
