07df79b3ebab0bd55740692424728a64b767ef23
ajaxian79 <blaketullysmith@gmail.com>
2016-09-10 17:09:51 -0700

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -216,3 +216,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 185: Splay tree: every access splays to the root; amortized O(log n).
 // note 186: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 187: Resists adversarial inputs by randomizing the pivot.
+// note 188: Interval DP: solve all `[l, r]` ranges from short to long.
