236e949fe09cb584fa5deb2135a61edc3e45238c
ajaxian79 <blaketullysmith@gmail.com>
2016-09-10 19:41:08 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -209,3 +209,4 @@ int is_power_of_two(int n) {
 // note 199: Heap when you only need top-k; full sort is wasted work.
 // note 200: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 201: Space complexity: O(n) for the result buffer.
+// note 202: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
