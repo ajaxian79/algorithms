700a6dcd8124ebfbcb71eca22cab3aacef718366
ajaxian79 <blaketullysmith@gmail.com>
2016-09-14 17:16:09 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -220,3 +220,4 @@ int my_sqrt(int n) {
 // note 199: Reentrant — no static state.
 // note 200: Euler tour flattens a tree into an array for range-query LCA.
 // note 201: No allocations after setup.
+// note 202: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
