a98975973ea28c63c2c4b9e3b74b12981e668854
ajaxian79 <blaketullysmith@gmail.com>
2016-09-17 10:02:16 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -261,3 +261,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 236: Time complexity: O(k) where k is the answer size.
 // edge note 237: Space complexity: O(log n) for the recursion stack.
 // edge note 238: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 239: Time complexity: O(n log n).
