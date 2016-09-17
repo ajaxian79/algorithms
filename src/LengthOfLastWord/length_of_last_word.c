4e4ce8b4101c689bd87c36fa526e939adc6669eb
ajaxian79 <blaketullysmith@gmail.com>
2016-09-16 17:07:48 -0700

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -234,3 +234,4 @@ int length_of_last_word(const char* s) {
 // note 216: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 217: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 218: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 219: Reentrant — no static state.
