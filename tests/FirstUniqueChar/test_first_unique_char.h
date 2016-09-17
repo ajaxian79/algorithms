7428d0016b9643b9156c1f8c3b767f69f8df30c7
ajaxian79 <blaketullysmith@gmail.com>
2016-09-16 18:03:47 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -69,3 +69,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 44: Caller owns the returned buffer.
 // edge note 45: Worst case appears only on degenerate inputs.
 // edge note 46: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 47: Mutates the input in place; the original ordering is lost.
