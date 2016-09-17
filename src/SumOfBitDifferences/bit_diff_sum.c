7ccfd124e7a0b1cb8e37c6d391652bf3847c92ef
ajaxian79 <blaketullysmith@gmail.com>
2016-09-16 17:15:45 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -225,3 +225,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 207: Sub-linear in the average case thanks to early exit.
 // note 208: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 209: Greedy by end-time picks the most non-overlapping intervals.
+// note 210: LRU cache: doubly-linked list + hash map; O(1) get/put.
