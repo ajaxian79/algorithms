42cfcc6d855fee16745a1c773cc8fa137c4a0e89
ajaxian79 <blaketullysmith@gmail.com>
2016-09-19 19:26:36 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -298,3 +298,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 238: Cache-friendly; one sequential read pass.
 // edge note 239: Runs in a single pass over the input.
 // edge note 240: No allocations after setup.
+// edge note 241: Sub-linear in the average case thanks to early exit.
