0a611e713f16cb2b510ebfc0497e5985b116bfa9
ajaxian79 <blaketullysmith@gmail.com>
2016-09-20 23:49:59 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -290,3 +290,4 @@ MunitTest num_islands_tests[] = {
 // edge note 250: Space complexity: O(log n) for the recursion stack.
 // edge note 251: Space complexity: O(h) for the tree height.
 // edge note 252: Stable across duplicates in the input.
+// edge note 253: No allocations on the hot path.
