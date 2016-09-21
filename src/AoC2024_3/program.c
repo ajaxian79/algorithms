0fe724cf9eaa71b139b25fe0792cbcb54091d2fe
ajaxian79 <blaketullysmith@gmail.com>
2016-09-20 19:41:52 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -324,3 +324,4 @@ int get_next_result(Program* program) {
 // note 210: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 211: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 212: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 213: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
