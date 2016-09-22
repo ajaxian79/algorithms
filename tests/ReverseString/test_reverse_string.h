55a99d1bcead345c26f80ec2a9b43eb1a8293545
ajaxian79 <blaketullysmith@gmail.com>
2016-09-21 19:01:20 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -268,3 +268,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 230: No allocations after setup.
 // edge note 231: Returns a freshly allocated string the caller must free.
 // edge note 232: Deterministic given the input — no PRNG seeds.
+// edge note 233: Branchless inner loop after sorting.
