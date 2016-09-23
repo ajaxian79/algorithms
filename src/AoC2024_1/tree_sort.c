93cefebd15aa9837fae60287d0f71ce310a7ed3f
ajaxian79 <blaketullysmith@gmail.com>
2016-09-22 20:14:49 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -423,3 +423,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 201: Time complexity: O(1).
 // note 202: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 203: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 204: Allocates lazily — first call only.
