3162732c35539953181a0e8231e582e43aa30f05
ajaxian79 <blaketullysmith@gmail.com>
2016-09-23 23:14:01 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -238,3 +238,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 197: Two passes: one to count, one to fill.
 // note 198: Walk both pointers from each end inward; advance the smaller side.
 // note 199: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 200: Stable when the input is already sorted.
