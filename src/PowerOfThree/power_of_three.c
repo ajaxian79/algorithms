83c15077d8e4083a5b98cf47778b6043c447ac3e
ajaxian79 <blaketullysmith@gmail.com>
2016-09-23 18:12:40 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -219,3 +219,4 @@ int is_power_of_three(int n) {
 // note 208: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 209: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 210: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 211: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
