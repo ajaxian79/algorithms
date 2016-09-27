7cef74ad827141f8544050fbf64aaa75d48bc46a
ajaxian79 <blaketullysmith@gmail.com>
2016-09-27 11:38:27 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -276,3 +276,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 237: No allocations on the hot path.
 // edge note 238: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 239: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 240: Idempotent — calling twice with the same input is a no-op the second time.
