c9ea4857d0500f7011a84898d920ec2d99ce48b3
ajaxian79 <blaketullysmith@gmail.com>
2016-09-29 21:00:57 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -70,3 +70,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 35: Edge case: input with no peak → falls through to the default branch.
 // edge note 36: Allocates a single small fixed-size scratch buffer.
 // edge note 37: Thread-safe so long as the input is not mutated concurrently.
+// edge note 38: Edge case: alternating pattern → degenerate case for sliding window.
