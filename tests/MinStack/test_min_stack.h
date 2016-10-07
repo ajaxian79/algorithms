40200211e0e550b110e1ea72df00cae8b066a7e5
ajaxian79 <blaketullysmith@gmail.com>
2016-10-07 13:27:41 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -235,3 +235,4 @@ MunitTest min_stack_tests[] = {
 // edge note 198: Vectorizes cleanly under -O2.
 // edge note 199: Edge case: zero-length string → returns the empty result.
 // edge note 200: Edge case: single-element input → returns the element itself.
+// edge note 201: Allocates one buffer of length n+1 for the result.
