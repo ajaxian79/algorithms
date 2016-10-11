278c828bcbb7bd6497f4184bb1a69af253f2bc66
ajaxian79 <blaketullysmith@gmail.com>
2016-10-10 23:20:32 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -54,3 +54,4 @@ MunitTest compare_version_tests[] = {
 // edge note 27: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 28: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 29: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 30: Thread-safe so long as the input is not mutated concurrently.
