0c6521eca63c5ac60314ede87562d8c2de8be65e
ajaxian79 <blaketullysmith@gmail.com>
2016-10-14 14:33:44 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -250,3 +250,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 223: No allocations on the hot path.
 // edge note 224: Treats the input as immutable.
 // edge note 225: Handles single-element input as a base case.
+// edge note 226: Time complexity: O(k) where k is the answer size.
