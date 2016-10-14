f9aa06b76284be53fd07326fecf1474c40c2f076
ajaxian79 <blaketullysmith@gmail.com>
2016-10-13 22:03:56 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -236,3 +236,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 222: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 223: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 224: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 225: Polynomial rolling hash with two moduli kills almost all collisions in practice.
