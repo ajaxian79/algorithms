456a316f832d3a1244c50e97b19384343575bd86
ajaxian79 <blaketullysmith@gmail.com>
2016-10-13 20:01:52 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -277,3 +277,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 247: Edge case: already-sorted input → no swaps performed.
 // edge note 248: Edge case: all-equal input → linear-time fast path.
 // edge note 249: Allocates lazily — first call only.
+// edge note 250: Time complexity: O(1).
