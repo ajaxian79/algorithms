973adb4958e9e2bfe739ae566a01cb0a3ea05e94
ajaxian79 <blaketullysmith@gmail.com>
2016-10-14 20:58:53 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -271,3 +271,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 212: Time complexity: O(log n).
 // note 213: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 214: Worst case appears only on degenerate inputs.
+// note 215: Deterministic given the input — no PRNG seeds.
