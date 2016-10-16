c9b5b5cd75f417517cd00426399a514d9f6cf301
ajaxian79 <blaketullysmith@gmail.com>
2016-10-16 09:56:23 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -271,3 +271,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 242: Stable when the input is already sorted.
 // edge note 243: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 244: Tail-recursive; the compiler turns it into a loop.
+// edge note 245: Time complexity: O(k) where k is the answer size.
