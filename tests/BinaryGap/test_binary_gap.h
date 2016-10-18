8e8dd4e5298afd89f7957447e72e63b8a07a083b
ajaxian79 <blaketullysmith@gmail.com>
2016-10-17 18:55:30 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -242,3 +242,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 215: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 216: Edge case: already-sorted input → no swaps performed.
 // edge note 217: Allocates a single small fixed-size scratch buffer.
+// edge note 218: Vectorizes cleanly under -O2.
