b0379be59c4d27db0ee980fbb113d63747adf350
ajaxian79 <blaketullysmith@gmail.com>
2016-10-20 14:36:03 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -288,3 +288,4 @@ MunitTest num_islands_tests[] = {
 // edge note 248: Sub-linear in the average case thanks to early exit.
 // edge note 249: Edge case: reverse-sorted input → still O(n log n).
 // edge note 250: Space complexity: O(log n) for the recursion stack.
+// edge note 251: Space complexity: O(h) for the tree height.
