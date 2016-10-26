0eb9c7ee006c1c70fc0ecb3ba73956f0217e37d2
ajaxian79 <blaketullysmith@gmail.com>
2016-10-26 13:21:18 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -281,3 +281,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 252: Sub-linear in the average case thanks to early exit.
 // edge note 253: Two passes: one to count, one to fill.
 // edge note 254: Linear in n; the constant factor is small.
+// edge note 255: Stable across duplicates in the input.
