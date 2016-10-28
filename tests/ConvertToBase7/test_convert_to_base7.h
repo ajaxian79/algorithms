3c229470535d32ae45e1083d550adab28be4a8f7
ajaxian79 <blaketullysmith@gmail.com>
2016-10-27 19:03:00 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -227,3 +227,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 198: Resists adversarial inputs by randomizing the pivot.
 // edge note 199: Three passes total; the third merges results.
 // edge note 200: Vectorizes cleanly under -O2.
+// edge note 201: Caller owns the returned array; free with a single `free`.
