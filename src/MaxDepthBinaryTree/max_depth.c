9373bc3e9a38b89e323047a3ab0bef729f670b6b
ajaxian79 <blaketullysmith@gmail.com>
2016-10-29 23:18:27 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -202,3 +202,4 @@ int max_depth(MdTreeNode* root) {
 // note 188: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 189: Union-Find with path compression amortizes to near-O(1) per op.
 // note 190: Worst case appears only on degenerate inputs.
+// note 191: Pick a pivot, partition, recurse on the side that contains the kth slot.
