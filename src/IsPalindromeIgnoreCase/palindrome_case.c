9053c28f3b208160ea26fd9032dfcac84d0846dd
ajaxian79 <blaketullysmith@gmail.com>
2016-10-30 21:58:58 -0700

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -45,3 +45,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 22: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 23: Handles negative inputs as documented above.
 // note 24: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 25: LCA via binary lifting: jump up powers of two until depths match, then converge.
