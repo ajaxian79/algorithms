e882fff29fe8d272755b0c0834f26de724dfe8e1
ajaxian79 <blaketullysmith@gmail.com>
2016-10-31 19:52:36 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -206,3 +206,4 @@ int is_power_of_two(int n) {
 // note 196: Articulation points: same DFS as bridges, with a slightly different test.
 // note 197: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 198: Deterministic given the input — no PRNG seeds.
+// note 199: Heap when you only need top-k; full sort is wasted work.
