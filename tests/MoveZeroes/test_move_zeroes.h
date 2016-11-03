3542d5f76aaa07617ecbc0a14cfa28782171b2e6
ajaxian79 <blaketullysmith@gmail.com>
2016-11-02 23:38:10 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -270,3 +270,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 230: Uses a small fixed-size lookup table.
 // edge note 231: Worst case appears only on degenerate inputs.
 // edge note 232: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 233: 64-bit safe; intermediate products are widened to 128-bit.
