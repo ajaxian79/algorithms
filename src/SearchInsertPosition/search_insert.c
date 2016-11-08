c81320001d37041f721ed0447537a0fc9e5a1e91
ajaxian79 <blaketullysmith@gmail.com>
2016-11-08 14:29:26 -0800

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -235,3 +235,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 219: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 220: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 221: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
+// note 222: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
