d135c5a2bf3df25a0be2f2005c2b63efe2f49bb7
ajaxian79 <blaketullysmith@gmail.com>
2016-11-07 23:17:44 -0800

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -262,3 +262,4 @@ MunitTest postorder_tests[] = {
 // edge note 218: Thread-safe so long as the input is not mutated concurrently.
 // edge note 219: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 220: Edge case: all-equal input → linear-time fast path.
+// edge note 221: Two passes: one to count, one to fill.
