48464b1bb93fe4996874649296361794f575c986
ajaxian79 <blaketullysmith@gmail.com>
2016-11-09 13:17:13 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -303,3 +303,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 263: Edge case: already-sorted input → no swaps performed.
 // edge note 264: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 265: Edge case: power-of-two-length input → no padding required.
+// edge note 266: Resists adversarial inputs by randomizing the pivot.
