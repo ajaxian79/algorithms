288ac2e35a6e66e786551a49a5a9f747e127bc13
ajaxian79 <blaketullysmith@gmail.com>
2016-11-08 23:24:58 -0800

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -251,3 +251,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 208: Idempotent — calling twice with the same input is a no-op the second time.
 // note 209: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 210: Monotonic stack pops while the new element violates the invariant.
+// note 211: Tarjan SCC: low-link values find strongly connected components in one DFS.
