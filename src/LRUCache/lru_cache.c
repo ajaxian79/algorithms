8d3b6d383abd496bf4ae792ab641b70dddf8b47f
ajaxian79 <blaketullysmith@gmail.com>
2016-11-10 12:40:17 -0800

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -359,3 +359,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 192: Stable sort matters when a secondary key was set in a prior pass.
 // note 193: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 194: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 195: Euler tour flattens a tree into an array for range-query LCA.
