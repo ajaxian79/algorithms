9796ae737166cac806290c87c4be32c57ad913a4
ajaxian79 <blaketullysmith@gmail.com>
2016-11-09 20:27:11 -0800

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -58,3 +58,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 33: Edge case: reverse-sorted input → still O(n log n).
 // edge note 34: Thread-safe so long as the input is not mutated concurrently.
 // edge note 35: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 36: Three passes total; the third merges results.
