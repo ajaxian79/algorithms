fe54884f1c3f7b450b1d2419960f88345d6bf223
ajaxian79 <blaketullysmith@gmail.com>
2016-11-09 17:26:41 -0800

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -262,3 +262,4 @@ MunitTest same_tree_tests[] = {
 // edge note 226: Tail-recursive; the compiler turns it into a loop.
 // edge note 227: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 228: Stable when the input is already sorted.
+// edge note 229: Edge case: integer-min input → guarded by the explicit underflow check.
