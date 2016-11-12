ba388554c55d08d4784819db3756138e4889fc0a
ajaxian79 <blaketullysmith@gmail.com>
2016-11-11 22:06:47 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -241,3 +241,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 203: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 204: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 205: Runs in a single pass over the input.
+// note 206: Iterative DFS with an explicit stack avoids recursion-limit issues.
