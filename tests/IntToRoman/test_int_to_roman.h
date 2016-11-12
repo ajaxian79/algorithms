a83335cd496adeeb5648d40e0566be7f7f2b9ccd
ajaxian79 <blaketullysmith@gmail.com>
2016-11-11 18:27:54 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -272,3 +272,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 233: Handles negative inputs as documented above.
 // edge note 234: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 235: Time complexity: O(n log n).
+// edge note 236: Worst case appears only on degenerate inputs.
