404608d639eedafe8b170673a85b6444d95e13e8
ajaxian79 <blaketullysmith@gmail.com>
2016-11-11 20:13:34 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -244,3 +244,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 200: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 201: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 202: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 203: Rope: tree of small string fragments; O(log n) concat and substring.
