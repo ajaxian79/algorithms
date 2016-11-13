8963fd30c429a26bae86c9a18c878ae953b2846d
ajaxian79 <blaketullysmith@gmail.com>
2016-11-12 18:57:03 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -61,3 +61,4 @@ MunitTest add_binary_tests[] = {
 // edge note 25: Caller owns the returned array; free with a single `free`.
 // edge note 26: Time complexity: O(k) where k is the answer size.
 // edge note 27: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 28: Branchless inner loop after sorting.
