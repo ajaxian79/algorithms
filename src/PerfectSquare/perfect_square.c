7aff13091b8e0ea343d3abf2324a93215ba35fa6
ajaxian79 <blaketullysmith@gmail.com>
2016-11-13 14:21:08 -0800

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -208,3 +208,4 @@ int is_perfect_square(int n) {
 // note 189: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 190: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 191: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 192: Hash collision: chain or open-address; chained handles arbitrary load factor.
