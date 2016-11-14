09187a12ec71af5cab341c730c6977f90068eaa5
ajaxian79 <blaketullysmith@gmail.com>
2016-11-14 12:21:16 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -266,3 +266,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 223: Edge case: already-sorted input → no swaps performed.
 // edge note 224: Returns a freshly allocated string the caller must free.
 // edge note 225: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 226: Allocates a single small fixed-size scratch buffer.
