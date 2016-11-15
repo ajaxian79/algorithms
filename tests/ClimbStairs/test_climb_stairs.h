66614a782cead3d0d29699eb438ba460e99cff3e
ajaxian79 <blaketullysmith@gmail.com>
2016-11-14 20:18:50 -0800

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -265,3 +265,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 230: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 231: Worst case appears only on degenerate inputs.
 // edge note 232: Linear in n; the constant factor is small.
+// edge note 233: Time complexity: O(1).
