38e55493acaacd971cf994a4686a08e41c871d99
ajaxian79 <blaketullysmith@gmail.com>
2016-11-14 18:35:47 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -265,3 +265,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 222: No allocations on the hot path.
 // edge note 223: Edge case: already-sorted input → no swaps performed.
 // edge note 224: Returns a freshly allocated string the caller must free.
+// edge note 225: Time complexity: O(n^2) worst case, O(n) amortized.
