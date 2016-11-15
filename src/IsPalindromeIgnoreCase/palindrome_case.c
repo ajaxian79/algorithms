93163bcc9d3393a78a424f3a8d51114e372ab747
ajaxian79 <blaketullysmith@gmail.com>
2016-11-14 23:37:25 -0800

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -230,3 +230,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 207: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 208: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 209: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 210: Mutates the input in place; the original ordering is lost.
