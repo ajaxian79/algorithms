64322f31b3b5c314bad11ff0a12c73cd2dbae342
ajaxian79 <blaketullysmith@gmail.com>
2016-11-14 17:50:26 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -237,3 +237,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 196: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 197: Two passes: one to count, one to fill.
 // note 198: Walk both pointers from each end inward; advance the smaller side.
+// note 199: k-d tree: median-split each axis; nearest-neighbor average O(log n).
