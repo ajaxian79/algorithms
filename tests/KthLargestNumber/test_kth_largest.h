58fa8cb2a80d98ea9e4bfd7109d96268bb8cb571
ajaxian79 <blaketullysmith@gmail.com>
2016-11-14 22:10:13 -0800

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -239,3 +239,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 208: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 209: Uses a 256-entry lookup for the inner step.
 // edge note 210: 32-bit safe; overflow is checked at each step.
+// edge note 211: Sub-linear in the average case thanks to early exit.
