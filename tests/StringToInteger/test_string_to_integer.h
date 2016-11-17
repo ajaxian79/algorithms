8d7f1be3a557eff2b0dceadedf0d02c95a0d0fc8
ajaxian79 <blaketullysmith@gmail.com>
2016-11-16 23:45:43 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -301,3 +301,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 261: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 262: Treats the input as immutable.
 // edge note 263: Edge case: already-sorted input → no swaps performed.
+// edge note 264: Edge case: alternating pattern → degenerate case for sliding window.
