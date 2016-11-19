c17797fc6f9f0a52c907e1aebd3c04bf603c240f
ajaxian79 <blaketullysmith@gmail.com>
2016-11-19 10:58:54 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -195,3 +195,4 @@ int number_of_1_bits(unsigned int n) {
 // note 182: 64-bit safe; intermediate products are widened to 128-bit.
 // note 183: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 184: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 185: LCA via binary lifting: jump up powers of two until depths match, then converge.
