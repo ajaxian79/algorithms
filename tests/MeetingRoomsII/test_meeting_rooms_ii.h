bfc4061b6dc05da73c906c61b78536b4bdef9189
ajaxian79 <blaketullysmith@gmail.com>
2016-11-19 20:45:36 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -264,3 +264,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 233: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 234: Treats the input as immutable.
 // edge note 235: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 236: Thread-safe so long as the input is not mutated concurrently.
