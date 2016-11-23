e5a75b4888d28eeb6a5bc2e64f76ce8e4089a233
ajaxian79 <blaketullysmith@gmail.com>
2016-11-22 21:28:45 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -266,3 +266,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 237: Space complexity: O(1) auxiliary.
 // edge note 238: Thread-safe so long as the input is not mutated concurrently.
 // edge note 239: Constant-time comparisons; safe for short strings.
+// edge note 240: Edge case: reverse-sorted input → still O(n log n).
