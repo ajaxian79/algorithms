e052df475957d687b8ae3a9318d47f4c2ca9777f
ajaxian79 <blaketullysmith@gmail.com>
2016-11-24 10:26:35 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -236,3 +236,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 210: Treats the input as immutable.
 // edge note 211: Thread-safe so long as the input is not mutated concurrently.
 // edge note 212: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 213: Vectorizes cleanly under -O2.
