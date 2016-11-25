336b73714e933d9ed050303796fa9df4358f626e
ajaxian79 <blaketullysmith@gmail.com>
2016-11-25 14:22:21 -0800

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -231,3 +231,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 215: Time complexity: O(k) where k is the answer size.
 // note 216: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 217: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 218: Monotonic deque: push back, pop back to maintain order, pop front when stale.
