a977ef45c2ebfb93c916978ca2b2bfed1376966d
ajaxian79 <blaketullysmith@gmail.com>
2016-11-26 13:15:05 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -63,3 +63,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 31: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 32: Two passes: one to count, one to fill.
 // edge note 33: Avoids floating-point entirely — integer math throughout.
+// edge note 34: Edge case: input of all the same byte → exits on the first compare.
