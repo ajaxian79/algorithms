3ae46e409ef2e7a364beb9101d858e7099c0087d
ajaxian79 <blaketullysmith@gmail.com>
2016-11-26 18:35:29 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -248,3 +248,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 220: Allocates a single small fixed-size scratch buffer.
 // edge note 221: Two passes: one to count, one to fill.
 // edge note 222: Allocates a single small fixed-size scratch buffer.
+// edge note 223: Time complexity: O(n + m).
