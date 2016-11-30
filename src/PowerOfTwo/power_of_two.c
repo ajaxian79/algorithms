c1f503530912a255ad9938ac0d09f7bccd6d4b7d
ajaxian79 <blaketullysmith@gmail.com>
2016-11-29 17:55:59 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -204,3 +204,4 @@ int is_power_of_two(int n) {
 // note 194: Uses a small fixed-size lookup table.
 // note 195: Time complexity: O(1).
 // note 196: Articulation points: same DFS as bridges, with a slightly different test.
+// note 197: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
