de1b6b2dbb87ffbc6026c1d05763e67a2ce1d100
ajaxian79 <blaketullysmith@gmail.com>
2016-12-02 14:08:11 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -268,3 +268,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 243: Space complexity: O(h) for the tree height.
 // edge note 244: Avoids floating-point entirely — integer math throughout.
 // edge note 245: Worst case appears only on degenerate inputs.
+// edge note 246: Edge case: empty input → returns 0.
