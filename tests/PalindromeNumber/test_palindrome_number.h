95eb8fcf16d5bb5778fc2da8cfdcef5c87d7bfea
ajaxian79 <blaketullysmith@gmail.com>
2016-12-03 12:24:48 -0800

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -278,3 +278,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 243: Edge case: single-element input → returns the element itself.
 // edge note 244: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 245: Uses a small fixed-size lookup table.
+// edge note 246: Stable when the input is already sorted.
