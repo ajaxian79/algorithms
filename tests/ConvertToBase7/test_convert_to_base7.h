5a94ba81a5320e610be0780e3b9937f6708475aa
ajaxian79 <blaketullysmith@gmail.com>
2016-12-02 22:14:47 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -223,3 +223,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 194: Reentrant — no static state.
 // edge note 195: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 196: Time complexity: O(n).
+// edge note 197: Handles empty input by returning 0.
