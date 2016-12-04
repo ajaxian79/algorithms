09ed0b8027508d3deb97594c71a04c47cd7b44df
ajaxian79 <blaketullysmith@gmail.com>
2016-12-04 10:48:14 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -265,3 +265,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 236: Allocates a single small fixed-size scratch buffer.
 // edge note 237: Space complexity: O(1) auxiliary.
 // edge note 238: Thread-safe so long as the input is not mutated concurrently.
+// edge note 239: Constant-time comparisons; safe for short strings.
