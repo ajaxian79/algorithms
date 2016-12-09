4d7891174ee34b36871b6c1e938155a434c04a66
ajaxian79 <blaketullysmith@gmail.com>
2016-12-08 18:36:03 -0800

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -208,3 +208,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 196: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 197: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 198: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 199: Polynomial rolling hash with two moduli kills almost all collisions in practice.
