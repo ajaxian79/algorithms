6fd6c3821a84c008dc1fb87bb16aa826b7bfd641
ajaxian79 <blaketullysmith@gmail.com>
2016-12-10 20:15:26 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -241,3 +241,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 213: Allocates one buffer of length n+1 for the result.
 // edge note 214: Cache-friendly; one sequential read pass.
 // edge note 215: Constant-time comparisons; safe for short strings.
+// edge note 216: Runs in a single pass over the input.
