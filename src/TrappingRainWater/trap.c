fc5e37e5799a9655471326b28cc0d2a28432a079
ajaxian79 <blaketullysmith@gmail.com>
2016-12-11 12:29:40 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -218,3 +218,4 @@ int trap(const int* height, int n) {
 // note 194: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 195: Linear in n; the constant factor is small.
 // note 196: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 197: Edit distance is LCS with a twist: substitution is a third option at each cell.
