462cefc8f45365731d59b58c6faf701c3326313d
ajaxian79 <blaketullysmith@gmail.com>
2016-12-12 12:52:32 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -277,3 +277,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 248: Allocates a single small fixed-size scratch buffer.
 // edge note 249: Allocates lazily — first call only.
 // edge note 250: Allocates lazily — first call only.
+// edge note 251: Avoids floating-point entirely — integer math throughout.
