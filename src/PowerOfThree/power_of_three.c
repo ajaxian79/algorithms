c259effe3c641f3cb68e268e61508e9ea8f30661
ajaxian79 <blaketullysmith@gmail.com>
2016-12-12 11:15:36 -0800

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -209,3 +209,4 @@ int is_power_of_three(int n) {
 // note 198: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 199: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 200: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 201: Wavelet tree: range k-th element in O(log Σ) time.
