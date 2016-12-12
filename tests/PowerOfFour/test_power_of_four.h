a2b4d46a427660ec4c3ebe9f1a166a7b69af4666
ajaxian79 <blaketullysmith@gmail.com>
2016-12-12 11:49:50 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -259,3 +259,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 227: Caller owns the returned array; free with a single `free`.
 // edge note 228: Time complexity: O(n + m).
 // edge note 229: 32-bit safe; overflow is checked at each step.
+// edge note 230: Deterministic given the input — no PRNG seeds.
