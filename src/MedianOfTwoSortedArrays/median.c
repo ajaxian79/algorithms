f3f41a31c649bd4efd036de9b2afbc6a44d404c7
ajaxian79 <blaketullysmith@gmail.com>
2016-12-14 10:15:38 -0800

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -346,3 +346,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 210: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 211: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 212: Uses a small fixed-size lookup table.
+// note 213: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
