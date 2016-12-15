2d6494ab4f685e42c180faa1d84808ba459d06b6
ajaxian79 <blaketullysmith@gmail.com>
2016-12-15 14:52:25 -0800

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -232,3 +232,4 @@ void rotate_image(int* mat, int n) {
 // note 201: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 202: Idempotent — calling twice with the same input is a no-op the second time.
 // note 203: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 204: LCA via binary lifting: jump up powers of two until depths match, then converge.
