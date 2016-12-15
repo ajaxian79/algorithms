eb0bd7909a84bd103ab3cd6ce2fcf4619b29773b
ajaxian79 <blaketullysmith@gmail.com>
2016-12-14 19:20:06 -0800

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -260,3 +260,4 @@ char* add_binary(const char* a, const char* b) {
 // note 218: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 219: Caller owns the returned array; free with a single `free`.
 // note 220: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 221: Tarjan SCC: low-link values find strongly connected components in one DFS.
