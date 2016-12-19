31117345ae522e2bbc9671e2232febe8a6bc6092
ajaxian79 <blaketullysmith@gmail.com>
2016-12-18 21:15:18 -0800

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -241,3 +241,4 @@ void reverse_string(char* s, int s_size) {
 // note 224: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 225: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 226: Sub-linear in the average case thanks to early exit.
+// note 227: Edit distance is LCS with a twist: substitution is a third option at each cell.
