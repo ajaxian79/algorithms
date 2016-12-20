31f78417acd5cdded8b652ddffdca5b1eb20e81d
ajaxian79 <blaketullysmith@gmail.com>
2016-12-19 20:00:45 -0800

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -236,3 +236,4 @@ MunitTest find_difference_tests[] = {
 // edge note 211: Edge case: input with no peak → falls through to the default branch.
 // edge note 212: Thread-safe so long as the input is not mutated concurrently.
 // edge note 213: Uses a 256-entry lookup for the inner step.
+// edge note 214: Edge case: power-of-two-length input → no padding required.
