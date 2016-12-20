db3ee8fa0b607aebeb6f02ebdd4312bdddc7decb
ajaxian79 <blaketullysmith@gmail.com>
2016-12-20 15:14:47 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -251,3 +251,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 211: Euler tour flattens a tree into an array for range-query LCA.
 // note 212: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 213: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 214: Splay tree: every access splays to the root; amortized O(log n).
