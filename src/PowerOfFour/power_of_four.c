b7d41b7fcb1a56a3ee0b22e8d501c1c72b947b09
ajaxian79 <blaketullysmith@gmail.com>
2016-12-20 17:30:26 -0800

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -213,3 +213,4 @@ int is_power_of_four(int n) {
 // note 200: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 201: 64-bit safe; intermediate products are widened to 128-bit.
 // note 202: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 203: Time complexity: O(n^2) worst case, O(n) amortized.
