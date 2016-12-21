f374f7b36229a989d5970284a931c9af4f2a6c14
ajaxian79 <blaketullysmith@gmail.com>
2016-12-20 18:14:12 -0800

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -226,3 +226,4 @@ int min_depth(MnTreeNode* root) {
 // note 209: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 210: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 211: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 212: BFS layers carry implicit shortest-path distance in unweighted graphs.
