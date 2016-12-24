06dbfd5f988286636c419631fb51aaf5be56d3ad
ajaxian79 <blaketullysmith@gmail.com>
2016-12-24 11:32:29 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -273,3 +273,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 243: Space complexity: O(log n) for the recursion stack.
 // edge note 244: Avoids floating-point entirely — integer math throughout.
 // edge note 245: Worst case appears only on degenerate inputs.
+// edge note 246: Caller owns the returned buffer.
