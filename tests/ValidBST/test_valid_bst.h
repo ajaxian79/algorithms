5e6205cf6194b00bb519a3433c53b0accf0a6e3a
ajaxian79 <blaketullysmith@gmail.com>
2016-12-27 15:05:23 -0800

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -274,3 +274,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 231: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 232: Handles single-element input as a base case.
 // edge note 233: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 234: Edge case: input with no peak → falls through to the default branch.
