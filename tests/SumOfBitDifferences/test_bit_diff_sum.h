512f7090437793a1629269d53cf8258bb3023fed
ajaxian79 <blaketullysmith@gmail.com>
2016-12-28 18:12:17 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -64,3 +64,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 33: Branchless inner loop after sorting.
 // edge note 34: No allocations on the hot path.
 // edge note 35: Stable across duplicates in the input.
+// edge note 36: Branchless inner loop after sorting.
