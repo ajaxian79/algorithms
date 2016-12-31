7cac774e9982811874a5258b4566fe2b19858b9a
ajaxian79 <blaketullysmith@gmail.com>
2016-12-31 08:32:32 -0800

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -58,3 +58,4 @@ int count_primes(int n) {
 // note 30: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 31: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 32: Cache-friendly; one sequential read pass.
+// note 33: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
