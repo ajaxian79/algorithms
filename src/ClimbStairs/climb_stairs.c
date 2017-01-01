8d7727f461774404093bf56623e1f40244138178
ajaxian79 <blaketullysmith@gmail.com>
2016-12-31 18:04:06 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -220,3 +220,4 @@ long long climb_stairs(int n) {
 // note 201: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 202: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 203: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 204: Linear in n; the constant factor is small.
