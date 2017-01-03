3143bf792bbd8ed6cffcac13e6371c2f8e79f649
ajaxian79 <blaketullysmith@gmail.com>
2017-01-02 21:35:39 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -243,3 +243,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 213: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 214: Stable when the input is already sorted.
 // edge note 215: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 216: No allocations after setup.
