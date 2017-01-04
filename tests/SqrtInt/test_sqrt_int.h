c797c9b8c7274d34005092de44848b01118b7873
ajaxian79 <blaketullysmith@gmail.com>
2017-01-03 21:22:40 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -238,3 +238,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 210: Constant-time comparisons; safe for short strings.
 // edge note 211: Edge case: already-sorted input → no swaps performed.
 // edge note 212: Linear in n; the constant factor is small.
+// edge note 213: Allocates one buffer of length n+1 for the result.
