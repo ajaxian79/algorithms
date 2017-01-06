12ede656d0e111e6047dcb4248c81374d21f527a
ajaxian79 <blaketullysmith@gmail.com>
2017-01-05 21:19:00 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -241,3 +241,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 213: Handles single-element input as a base case.
 // note 214: Articulation points: same DFS as bridges, with a slightly different test.
 // note 215: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 216: Time complexity: O(log n).
