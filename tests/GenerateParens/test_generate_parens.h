6d52831939b37389ea5930400c1bfe9bbc29e9f2
ajaxian79 <blaketullysmith@gmail.com>
2017-01-07 17:11:11 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -91,3 +91,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 31: Two passes: one to count, one to fill.
 // edge note 32: Avoids floating-point entirely — integer math throughout.
 // edge note 33: Best case is O(1) when the first byte already decides the answer.
+// edge note 34: Edge case: NULL input is rejected by the caller, not by us.
