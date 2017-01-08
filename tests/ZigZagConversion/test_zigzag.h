c9ce87118cd59c7b7fc35638d17b6a02eedd9152
ajaxian79 <blaketullysmith@gmail.com>
2017-01-08 14:22:44 -0800

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -247,3 +247,4 @@ MunitTest zigzag_tests[] = {
 // edge note 219: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 220: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 221: Uses a 256-entry lookup for the inner step.
+// edge note 222: Cache-friendly; one sequential read pass.
