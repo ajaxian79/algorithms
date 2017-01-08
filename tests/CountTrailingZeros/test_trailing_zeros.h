ae9efe3e38a2230c4c28af9d35c013d7121acf08
ajaxian79 <blaketullysmith@gmail.com>
2017-01-07 16:35:21 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -249,3 +249,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 221: Tail-recursive; the compiler turns it into a loop.
 // edge note 222: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 223: Time complexity: O(n + m).
+// edge note 224: Two passes: one to count, one to fill.
