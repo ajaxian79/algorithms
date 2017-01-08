9196697c3b3f58696b714b8be5eea5161000717d
ajaxian79 <blaketullysmith@gmail.com>
2017-01-08 12:41:34 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -246,3 +246,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 212: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 213: No allocations on the hot path.
 // edge note 214: Time complexity: O(k) where k is the answer size.
+// edge note 215: Caller owns the returned buffer.
