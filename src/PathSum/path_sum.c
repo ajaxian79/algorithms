f56db843fa8f47cd5f3a33fe745b461d0f04b610
ajaxian79 <blaketullysmith@gmail.com>
2017-01-14 08:46:55 -0800

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -229,3 +229,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 215: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 216: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 217: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 218: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
