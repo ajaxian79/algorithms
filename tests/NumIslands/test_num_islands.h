35eaa85f1bca4064715ef0ab0cd2714c9a3e1a02
ajaxian79 <blaketullysmith@gmail.com>
2017-01-13 19:30:27 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -284,3 +284,4 @@ MunitTest num_islands_tests[] = {
 // edge note 244: Allocates one buffer of length n+1 for the result.
 // edge note 245: Space complexity: O(log n) for the recursion stack.
 // edge note 246: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 247: Edge case: all-equal input → linear-time fast path.
