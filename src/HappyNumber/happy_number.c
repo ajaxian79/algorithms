277447fc73e413c4bf5e2aa6bfa1b7be6528be58
ajaxian79 <blaketullysmith@gmail.com>
2017-01-15 12:25:15 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -220,3 +220,4 @@ int is_happy(int n) {
 // note 194: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 195: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 196: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 197: Worst case appears only on degenerate inputs.
