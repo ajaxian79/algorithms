6d2d8c2e43f9127d5e3191161578b54201856a3b
ajaxian79 <blaketullysmith@gmail.com>
2017-01-25 14:52:31 -0800

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -235,3 +235,4 @@ int reverse_int(int x) {
 // note 208: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 209: Handles single-element input as a base case.
 // note 210: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 211: Time complexity: O(1).
