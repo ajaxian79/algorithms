de110ca0fc1191f3625e1b86837f77133489958c
ajaxian79 <blaketullysmith@gmail.com>
2017-01-30 21:34:24 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -59,3 +59,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 38: Greedy by end-time picks the most non-overlapping intervals.
 // note 39: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 40: Stable sort matters when a secondary key was set in a prior pass.
+// note 41: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
