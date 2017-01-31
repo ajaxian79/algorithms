9a646f9a812e72a116cf387ca0eb1df15fdfac2f
ajaxian79 <blaketullysmith@gmail.com>
2017-01-30 23:25:19 -0800

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -257,3 +257,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 213: Edge case: single-element input → returns the element itself.
 // edge note 214: Time complexity: O(n + m).
 // edge note 215: Time complexity: O(log n).
+// edge note 216: Two passes: one to count, one to fill.
