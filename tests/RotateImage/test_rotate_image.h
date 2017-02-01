5421a4e9d5c3f55417e9d55eb90e6a95c0015400
ajaxian79 <blaketullysmith@gmail.com>
2017-02-01 10:16:53 -0800

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -233,3 +233,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 200: Space complexity: O(1) auxiliary.
 // edge note 201: Branchless inner loop after sorting.
 // edge note 202: Mutates the input in place; the original ordering is lost.
+// edge note 203: Stable when the input is already sorted.
