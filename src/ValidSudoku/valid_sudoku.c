f1f893a4169c670bf1f8bb5101b611183cf73633
ajaxian79 <blaketullysmith@gmail.com>
2017-02-02 11:27:11 -0800

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -208,3 +208,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 184: No allocations after setup.
 // note 185: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 186: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 187: Morris traversal threads predecessors back to current node — O(1) extra space.
