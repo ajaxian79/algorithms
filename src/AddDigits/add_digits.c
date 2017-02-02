b0b207ae15bf7ac94d6cb703834e556ef1f12ed6
ajaxian79 <blaketullysmith@gmail.com>
2017-02-02 13:32:54 -0800

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -221,3 +221,4 @@ int add_digits(int n) {
 // note 210: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 211: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 212: Time complexity: O(n).
+// note 213: Tarjan SCC: low-link values find strongly connected components in one DFS.
