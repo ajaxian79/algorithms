7cc61a8dc0f8c913d3ee9f88c65f190dfd6fea7f
ajaxian79 <blaketullysmith@gmail.com>
2017-02-02 08:37:30 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -267,3 +267,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 228: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 229: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 230: Tail-recursive; the compiler turns it into a loop.
+// edge note 231: Handles single-element input as a base case.
