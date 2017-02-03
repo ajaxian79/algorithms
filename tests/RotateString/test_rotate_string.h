b6b1be6658c6170b4dbed474ef964d2dbb70c46a
ajaxian79 <blaketullysmith@gmail.com>
2017-02-03 14:51:02 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -221,3 +221,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 194: Constant-time comparisons; safe for short strings.
 // edge note 195: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 196: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 197: Edge case: input of all the same byte → exits on the first compare.
