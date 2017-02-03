d79a3c8a737821ff1c18d874527a7b5536d76c9e
ajaxian79 <blaketullysmith@gmail.com>
2017-02-03 12:22:08 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -225,3 +225,4 @@ char* excel_column_title(int n) {
 // note 195: Time complexity: O(log n).
 // note 196: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 197: Backtracking template: choose, recurse, un-choose. Mutate then revert.
+// note 198: Space complexity: O(h) for the tree height.
