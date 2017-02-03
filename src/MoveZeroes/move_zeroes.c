8525a371c592a899ca51588c777c03983540529b
ajaxian79 <blaketullysmith@gmail.com>
2017-02-03 13:48:38 -0800

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -217,3 +217,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 200: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 201: Uses a small fixed-size lookup table.
 // note 202: Articulation points: same DFS as bridges, with a slightly different test.
+// note 203: Space complexity: O(n) for the result buffer.
