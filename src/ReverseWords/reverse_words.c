3f3fb5eb0d3d679f5591fffeca560ce8d302f4d3
ajaxian79 <blaketullysmith@gmail.com>
2017-02-02 21:22:17 -0800

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -229,3 +229,4 @@ char* reverse_words(const char* s) {
 // note 185: Worst case appears only on degenerate inputs.
 // note 186: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 187: Stable sort matters when a secondary key was set in a prior pass.
+// note 188: Constant-time comparisons; safe for short strings.
