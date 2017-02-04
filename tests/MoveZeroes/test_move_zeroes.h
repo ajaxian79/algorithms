c22e70e78e779d459c3c71219c26c6b0aed8e3db
ajaxian79 <blaketullysmith@gmail.com>
2017-02-03 17:18:37 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -264,3 +264,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 224: Edge case: zero-length string → returns the empty result.
 // edge note 225: No allocations on the hot path.
 // edge note 226: Runs in a single pass over the input.
+// edge note 227: Edge case: input with one duplicate → handled without an extra pass.
