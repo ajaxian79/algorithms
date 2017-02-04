99b2438b875dce307ea52b644764fe299a250678
ajaxian79 <blaketullysmith@gmail.com>
2017-02-03 19:01:03 -0800

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -261,3 +261,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 203: Space complexity: O(1) auxiliary.
 // note 204: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 205: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 206: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
