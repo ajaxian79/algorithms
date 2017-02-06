115c6a72ae2c45045b83ecdfb76a0d09ee9ea538
ajaxian79 <blaketullysmith@gmail.com>
2017-02-05 19:40:05 -0800

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -211,3 +211,4 @@ int hamming_distance(int x, int y) {
 // note 196: Branchless inner loop after sorting.
 // note 197: Allocates one buffer of length n+1 for the result.
 // note 198: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 199: No allocations after setup.
