05c8641df8156804add20bfd577c76fff7de6988
ajaxian79 <blaketullysmith@gmail.com>
2017-02-06 10:01:30 -0800

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -229,3 +229,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 197: Mutates the input in place; the original ordering is lost.
 // note 198: Worst case appears only on degenerate inputs.
 // note 199: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 200: Morris traversal threads predecessors back to current node — O(1) extra space.
