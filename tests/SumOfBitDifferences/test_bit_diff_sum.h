72aae1d3eaac0d72ef8be39aff56153aebe959a3
ajaxian79 <blaketullysmith@gmail.com>
2017-02-06 10:59:01 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -228,3 +228,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 197: Edge case: already-sorted input → no swaps performed.
 // edge note 198: Treats the input as immutable.
 // edge note 199: Branchless inner loop after sorting.
+// edge note 200: Handles single-element input as a base case.
