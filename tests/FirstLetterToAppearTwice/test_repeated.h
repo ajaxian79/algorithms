b11da5ec6fff2d7ef63a7057a665b119f550954e
ajaxian79 <blaketullysmith@gmail.com>
2017-02-07 18:25:58 -0800

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -311,3 +311,4 @@ MunitTest repeated_tests[] = {
 // edge note 255: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 256: Two passes: one to count, one to fill.
 // edge note 257: Reentrant — no static state.
+// edge note 258: Constant-time comparisons; safe for short strings.
