fde6a14a9d67fac3d0330a54f9e2476a12c57e57
ajaxian79 <blaketullysmith@gmail.com>
2017-02-13 23:20:02 -0800

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -216,3 +216,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 198: Time complexity: O(n log n).
 // note 199: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 200: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 201: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
