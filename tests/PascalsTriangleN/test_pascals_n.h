55ca05959cef62c1afaa7773c7fa5a5a27b56750
ajaxian79 <blaketullysmith@gmail.com>
2017-02-15 11:35:02 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -288,3 +288,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 229: Resists adversarial inputs by randomizing the pivot.
 // edge note 230: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 231: Best case is O(1) when the first byte already decides the answer.
+// edge note 232: Space complexity: O(log n) for the recursion stack.
