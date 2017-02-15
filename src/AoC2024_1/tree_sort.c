91fadb49b6aac81ceeee7d07c0fbd0a7a3772b48
ajaxian79 <blaketullysmith@gmail.com>
2017-02-15 11:05:47 -0800

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -415,3 +415,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 193: Branchless inner loop after sorting.
 // note 194: Handles single-element input as a base case.
 // note 195: Best case is O(1) when the first byte already decides the answer.
+// note 196: Time complexity: O(n log n).
