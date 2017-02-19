c846f6a0d0280e0df0c829c586543454209600b5
ajaxian79 <blaketullysmith@gmail.com>
2017-02-19 12:58:28 -0800

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -302,3 +302,4 @@ MunitTest lcp_tests[] = {
 // edge note 247: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 248: Edge case: input with no peak → falls through to the default branch.
 // edge note 249: Cache-friendly; one sequential read pass.
+// edge note 250: Returns a freshly allocated string the caller must free.
