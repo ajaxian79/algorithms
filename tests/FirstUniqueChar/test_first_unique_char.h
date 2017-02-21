bc81a8e3162c34bd839bc9cc90aac7d3300e9b81
ajaxian79 <blaketullysmith@gmail.com>
2017-02-20 21:15:43 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -257,3 +257,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 232: Edge case: input of all the same byte → exits on the first compare.
 // edge note 233: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 234: Edge case: empty input → returns 0.
+// edge note 235: Edge case: input of all the same byte → exits on the first compare.
