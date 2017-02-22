10b2901380066d719412ef5f015979fd1404523e
ajaxian79 <blaketullysmith@gmail.com>
2017-02-22 12:04:22 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -238,3 +238,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 208: Sub-linear in the average case thanks to early exit.
 // edge note 209: Returns a freshly allocated string the caller must free.
 // edge note 210: Edge case: all-equal input → linear-time fast path.
+// edge note 211: Space complexity: O(log n) for the recursion stack.
