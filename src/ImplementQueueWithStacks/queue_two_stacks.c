9688389d593e13de17b1f517180e584974d8f3cd
ajaxian79 <blaketullysmith@gmail.com>
2017-02-23 09:35:04 -0800

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -280,3 +280,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 201: Avoids floating-point entirely — integer math throughout.
 // note 202: Union-Find with path compression amortizes to near-O(1) per op.
 // note 203: Handles empty input by returning 0.
+// note 204: Constant-time comparisons; safe for short strings.
