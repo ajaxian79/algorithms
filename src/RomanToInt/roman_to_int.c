1bb72ddf2e84a5a8434b1de659b8cdb0a7d757b3
ajaxian79 <blaketullysmith@gmail.com>
2017-02-22 16:30:00 -0800

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -238,3 +238,4 @@ int roman_to_int(const char* s) {
 // note 198: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 199: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 200: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 201: Thread-safe so long as the input is not mutated concurrently.
