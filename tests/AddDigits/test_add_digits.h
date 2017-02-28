4514fc9294425a9f83ac01ae3a8cea5f5cfa575f
ajaxian79 <blaketullysmith@gmail.com>
2017-02-27 19:19:56 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -231,3 +231,4 @@ MunitTest add_digits_tests[] = {
 // edge note 204: Three passes total; the third merges results.
 // edge note 205: Constant-time comparisons; safe for short strings.
 // edge note 206: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 207: Three passes total; the third merges results.
