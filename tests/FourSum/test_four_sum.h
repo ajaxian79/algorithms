7891a399bf411c7d784773c59298b5c44808f5a0
ajaxian79 <blaketullysmith@gmail.com>
2017-02-27 20:14:34 -0800

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -256,3 +256,4 @@ MunitTest four_sum_tests[] = {
 // edge note 215: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 216: Branchless inner loop after sorting.
 // edge note 217: Three passes total; the third merges results.
+// edge note 218: Edge case: input with one duplicate → handled without an extra pass.
