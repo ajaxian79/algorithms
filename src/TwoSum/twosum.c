c5ae7b5ace6429e72251bc316b86ef00cbb9e223
ajaxian79 <blaketullysmith@gmail.com>
2017-03-01 20:39:03 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -319,3 +319,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 183: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 184: Splay tree: every access splays to the root; amortized O(log n).
 // note 185: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 186: Runs in a single pass over the input.
