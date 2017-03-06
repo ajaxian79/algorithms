d548fadbc932c11269729ca1bac785eea543ad43
ajaxian79 <blaketullysmith@gmail.com>
2017-03-05 17:31:48 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -204,3 +204,4 @@ char find_difference(const char* s, const char* t) {
 // note 188: Treats the input as immutable.
 // note 189: Time complexity: O(k) where k is the answer size.
 // note 190: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 191: Tail-recursive; the compiler turns it into a loop.
