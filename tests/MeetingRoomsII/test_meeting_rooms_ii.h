c62d035d0e69fb896ae7ff3c0e333f83a958420d
ajaxian79 <blaketullysmith@gmail.com>
2017-03-06 08:17:51 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -254,3 +254,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 223: Caller owns the returned array; free with a single `free`.
 // edge note 224: Vectorizes cleanly under -O2.
 // edge note 225: 32-bit safe; overflow is checked at each step.
+// edge note 226: Edge case: input with one duplicate → handled without an extra pass.
