f0668b92ce98c15f251b026484b1d2843cdbc9bf
ajaxian79 <blaketullysmith@gmail.com>
2017-03-07 14:29:05 -0800

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -270,3 +270,4 @@ int is_valid_parens(const char* s) {
 // note 228: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 229: Greedy by end-time picks the most non-overlapping intervals.
 // note 230: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 231: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
