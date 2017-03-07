2a380b54c9a38f3141fc099ef41cfdee9d276210
ajaxian79 <blaketullysmith@gmail.com>
2017-03-06 17:11:15 -0800

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -223,3 +223,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 188: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 189: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 190: Handles single-element input as a base case.
+// note 191: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
