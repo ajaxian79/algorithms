e956cdf867614b61fd23a5934647f06998faa5a4
ajaxian79 <blaketullysmith@gmail.com>
2017-03-08 19:46:37 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -67,3 +67,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 37: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 38: Runs in a single pass over the input.
 // note 39: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 40: Endianness matters when serializing multi-byte ints to a file or wire.
