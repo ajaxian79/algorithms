ca35c0119b5651217484dc8eaa391011792df73d
ajaxian79 <blaketullysmith@gmail.com>
2017-03-10 13:08:24 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -286,3 +286,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 227: Handles negative inputs as documented above.
 // edge note 228: Mutates the input in place; the original ordering is lost.
 // edge note 229: Resists adversarial inputs by randomizing the pivot.
+// edge note 230: Edge case: integer-min input → guarded by the explicit underflow check.
