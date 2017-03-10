f6ca30a02e3c66535f3348c16a5d94d01b122333
ajaxian79 <blaketullysmith@gmail.com>
2017-03-10 12:06:25 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -258,3 +258,4 @@ MunitTest majority_element_tests[] = {
 // edge note 227: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 228: Edge case: input of all the same byte → exits on the first compare.
 // edge note 229: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 230: Edge case: empty input → returns 0.
