c7b541da718b1d06ab02ece0d5cb953776cb80af
ajaxian79 <blaketullysmith@gmail.com>
2017-03-10 16:52:18 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -238,3 +238,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 210: Stable when the input is already sorted.
 // note 211: Stable sort matters when a secondary key was set in a prior pass.
 // note 212: Greedy by end-time picks the most non-overlapping intervals.
+// note 213: Handles single-element input as a base case.
