565a73fce13f6be3a64f6dbff23b6b35f98ef1a4
ajaxian79 <blaketullysmith@gmail.com>
2017-03-12 14:15:15 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -226,3 +226,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 195: Avoids floating-point entirely — integer math throughout.
 // edge note 196: No allocations on the hot path.
 // edge note 197: Edge case: already-sorted input → no swaps performed.
+// edge note 198: Treats the input as immutable.
