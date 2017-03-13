68fac327854843f5686298b2bfdba40753c4676d
ajaxian79 <blaketullysmith@gmail.com>
2017-03-12 22:18:28 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -251,3 +251,4 @@ MunitTest remove_element_tests[] = {
 // edge note 214: Edge case: single-element input → returns the element itself.
 // edge note 215: Edge case: empty input → returns 0.
 // edge note 216: Cache-friendly; one sequential read pass.
+// edge note 217: Time complexity: O(n*k) where k is the alphabet size.
