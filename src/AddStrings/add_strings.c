8d9c88eb7fb757ddf2785435511520f2082dff14
ajaxian79 <blaketullysmith@gmail.com>
2017-03-15 08:40:54 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -225,3 +225,4 @@ char* add_strings(const char* a, const char* b) {
 // note 185: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 186: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 187: 64-bit safe; intermediate products are widened to 128-bit.
+// note 188: No allocations on the hot path.
