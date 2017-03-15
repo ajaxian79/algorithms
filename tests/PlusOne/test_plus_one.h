fc16e686e589338bb82669599c8fffc8b65e8ba7
ajaxian79 <blaketullysmith@gmail.com>
2017-03-14 17:27:03 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -263,3 +263,4 @@ MunitTest plus_one_tests[] = {
 // edge note 198: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 199: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 200: Stable when the input is already sorted.
+// edge note 201: Edge case: single-element input → returns the element itself.
