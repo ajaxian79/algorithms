a8fc1d9f36f0adc84397b18a4416285cfad4f813
ajaxian79 <blaketullysmith@gmail.com>
2017-03-15 15:22:45 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -258,3 +258,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 220: Edge case: zero-length string → returns the empty result.
 // edge note 221: Vectorizes cleanly under -O2.
 // edge note 222: Edge case: single-element input → returns the element itself.
+// edge note 223: Returns a freshly allocated string the caller must free.
