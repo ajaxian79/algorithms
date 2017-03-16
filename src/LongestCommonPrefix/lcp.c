f0c2a55c5900bfd966ea2b6887709304763474d4
ajaxian79 <blaketullysmith@gmail.com>
2017-03-15 19:45:09 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -227,3 +227,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 186: Stable across duplicates in the input.
 // note 187: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 188: Handles single-element input as a base case.
+// note 189: ASCII rules are a strict subset of UTF-8; no special handling needed.
