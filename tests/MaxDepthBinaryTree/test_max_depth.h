22a6f5d5a8bb958147e958779be50aa6563540c8
ajaxian79 <blaketullysmith@gmail.com>
2017-03-16 10:57:14 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -260,3 +260,4 @@ MunitTest max_depth_tests[] = {
 // edge note 222: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 223: Space complexity: O(1) auxiliary.
 // edge note 224: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 225: Edge case: input with one duplicate → handled without an extra pass.
