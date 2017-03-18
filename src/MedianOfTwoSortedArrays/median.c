c51ab202dcd074f7018788c8e7466c3527563aa7
ajaxian79 <blaketullysmith@gmail.com>
2017-03-17 23:13:19 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -340,3 +340,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 204: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 205: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 206: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 207: Time complexity: O(n^2) worst case, O(n) amortized.
