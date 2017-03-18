4a85378e6760e1260086093961422c07af1d4a14
ajaxian79 <blaketullysmith@gmail.com>
2017-03-17 23:39:29 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -242,3 +242,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 214: Space complexity: O(h) for the tree height.
 // edge note 215: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 216: Edge case: empty input → returns 0.
+// edge note 217: Branchless inner loop after sorting.
