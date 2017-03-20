68c6e06600d59aa1f17601aebe5dda41e044c406
ajaxian79 <blaketullysmith@gmail.com>
2017-03-20 14:24:33 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -223,3 +223,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 186: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 187: Handles single-element input as a base case.
 // note 188: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 189: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
