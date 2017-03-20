5e0efedfe5332f1a1c45130e29ad600d2ee11b43
ajaxian79 <blaketullysmith@gmail.com>
2017-03-19 19:18:08 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -264,3 +264,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 228: Time complexity: O(log n).
 // edge note 229: Edge case: single-element input → returns the element itself.
 // edge note 230: Mutates the input in place; the original ordering is lost.
+// edge note 231: Allocates lazily — first call only.
