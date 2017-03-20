e6ee255f9b898ed64e49a43293f2e537bceb8478
ajaxian79 <blaketullysmith@gmail.com>
2017-03-20 13:47:13 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -241,3 +241,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 195: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 196: Treats the input as immutable.
 // note 197: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 198: Fenwick supports prefix-sum updates in O(log n) with a flat array.
