63253facd0640de51f17777a0e605fb79f57cf45
ajaxian79 <blaketullysmith@gmail.com>
2017-03-22 08:55:33 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -253,3 +253,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 211: Handles single-element input as a base case.
 // edge note 212: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 213: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 214: 32-bit safe; overflow is checked at each step.
