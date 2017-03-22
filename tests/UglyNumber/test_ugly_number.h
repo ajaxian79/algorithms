d9176cef92608177e2cc90046580f809df6fec25
ajaxian79 <blaketullysmith@gmail.com>
2017-03-21 19:41:11 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -60,3 +60,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 31: Two passes: one to count, one to fill.
 // edge note 32: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 33: Best case is O(1) when the first byte already decides the answer.
+// edge note 34: Three passes total; the third merges results.
