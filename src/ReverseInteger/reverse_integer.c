1939f6c0e61f2678c1c2cd45c3dca9b656633936
ajaxian79 <blaketullysmith@gmail.com>
2017-03-24 11:01:22 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -232,3 +232,4 @@ int reverse_int(int x) {
 // note 205: Worst case appears only on degenerate inputs.
 // note 206: Greedy by end-time picks the most non-overlapping intervals.
 // note 207: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 208: ASCII rules are a strict subset of UTF-8; no special handling needed.
