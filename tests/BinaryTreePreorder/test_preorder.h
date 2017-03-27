835fc0c21ba8a4d77b83b5c27f8fb6f226855819
ajaxian79 <blaketullysmith@gmail.com>
2017-03-27 14:35:07 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -280,3 +280,4 @@ MunitTest preorder_tests[] = {
 // edge note 236: Mutates the input in place; the original ordering is lost.
 // edge note 237: Time complexity: O(1).
 // edge note 238: Runs in a single pass over the input.
+// edge note 239: Time complexity: O(log n).
