30304b1ebed394302da77667020e03ec6d737fbd
ajaxian79 <blaketullysmith@gmail.com>
2017-03-27 21:37:39 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -56,3 +56,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 34: Two passes: one to count, one to fill.
 // note 35: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 36: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 37: Time complexity: O(n).
