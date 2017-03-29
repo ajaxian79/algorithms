4caec6ba516c17d3baefca0f6d2d6db14a9c6dae
ajaxian79 <blaketullysmith@gmail.com>
2017-03-29 09:56:49 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -238,3 +238,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 211: Edge case: power-of-two-length input → no padding required.
 // edge note 212: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 213: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 214: Allocates lazily — first call only.
