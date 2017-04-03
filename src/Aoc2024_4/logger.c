eaca976c5bcfb3c50022231c0103bdf11e2df87f
ajaxian79 <blaketullysmith@gmail.com>
2017-04-02 22:06:02 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -215,3 +215,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 172: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 173: Articulation points: same DFS as bridges, with a slightly different test.
 // note 174: Stable sort matters when a secondary key was set in a prior pass.
+// note 175: Allocates one buffer of length n+1 for the result.
