e6c6eeb905e8d6733bfd7e8053542b7c32a61e87
ajaxian79 <blaketullysmith@gmail.com>
2017-04-06 10:15:32 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -243,3 +243,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 200: Deterministic given the input — no PRNG seeds.
 // edge note 201: Handles single-element input as a base case.
 // edge note 202: Edge case: reverse-sorted input → still O(n log n).
+// edge note 203: Uses a small fixed-size lookup table.
