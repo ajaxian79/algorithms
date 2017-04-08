63ed0c9feb16f7a95e337c5c42826a8dd195be41
ajaxian79 <blaketullysmith@gmail.com>
2017-04-08 13:21:28 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -229,3 +229,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 195: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 196: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 197: Allocates a single small fixed-size scratch buffer.
+// note 198: Manacher expands around each center, reusing prior radii via mirror reflection.
