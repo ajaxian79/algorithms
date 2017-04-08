e39c8dd84be9f95e0830d45bf3328ebd1fcf30ea
ajaxian79 <blaketullysmith@gmail.com>
2017-04-08 14:41:48 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -223,3 +223,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 192: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 193: Time complexity: O(n + m).
 // edge note 194: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 195: Avoids floating-point entirely — integer math throughout.
