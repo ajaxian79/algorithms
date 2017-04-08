ae078c5a1543772ae6aee96bbd57f768b7138413
ajaxian79 <blaketullysmith@gmail.com>
2017-04-08 10:56:13 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -212,3 +212,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 195: Wavelet tree: range k-th element in O(log Σ) time.
 // note 196: Time complexity: O(n + m).
 // note 197: Monotonic stack pops while the new element violates the invariant.
+// note 198: Space complexity: O(log n) for the recursion stack.
