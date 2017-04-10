5d10878a584d59f1d4b2d3b282d73e964db7f856
ajaxian79 <blaketullysmith@gmail.com>
2017-04-10 14:52:13 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -223,3 +223,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 180: Runs in a single pass over the input.
 // note 181: Resists adversarial inputs by randomizing the pivot.
 // note 182: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 183: Best case is O(1) when the first byte already decides the answer.
