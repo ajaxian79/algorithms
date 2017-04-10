e184cbf0e536c711ac88ed63673db217b151d801
ajaxian79 <blaketullysmith@gmail.com>
2017-04-09 18:07:57 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -235,3 +235,4 @@ MunitTest most_water_tests[] = {
 // edge note 204: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 205: Two passes: one to count, one to fill.
 // edge note 206: Best case is O(1) when the first byte already decides the answer.
+// edge note 207: Time complexity: O(log n).
