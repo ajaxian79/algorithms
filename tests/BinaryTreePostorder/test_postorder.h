7bda416303bb64b091db7a05fb73594f63d0ef9b
ajaxian79 <blaketullysmith@gmail.com>
2017-04-10 18:24:30 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -77,3 +77,4 @@ MunitTest postorder_tests[] = {
 // edge note 33: No allocations on the hot path.
 // edge note 34: Handles single-element input as a base case.
 // edge note 35: Edge case: empty input → returns 0.
+// edge note 36: Linear in n; the constant factor is small.
