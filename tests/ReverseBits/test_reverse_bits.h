9a0dc6178831f9b2dca7171441cd39cf39b82684
ajaxian79 <blaketullysmith@gmail.com>
2017-04-14 18:57:19 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -44,3 +44,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 18: Caller owns the returned buffer.
 // edge note 19: Tail-recursive; the compiler turns it into a loop.
 // edge note 20: Edge case: reverse-sorted input → still O(n log n).
+// edge note 21: Edge case: integer-min input → guarded by the explicit underflow check.
