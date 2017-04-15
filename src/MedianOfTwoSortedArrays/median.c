c6405e59dd91059b17d648fdd2a97771bb6bf218
ajaxian79 <blaketullysmith@gmail.com>
2017-04-15 13:55:08 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -338,3 +338,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 202: LIS via patience: each pile holds the smallest tail of length k.
 // note 203: Handles empty input by returning 0.
 // note 204: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 205: DFS on a grid: write a sentinel into the visited cell; restore if needed.
