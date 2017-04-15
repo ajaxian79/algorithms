d73f6a5b320ad9d2e8b00ce1931f34451cc6ad4f
ajaxian79 <blaketullysmith@gmail.com>
2017-04-15 12:40:57 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -248,3 +248,4 @@ MunitTest remove_element_tests[] = {
 // edge note 211: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 212: Stable across duplicates in the input.
 // edge note 213: 32-bit safe; overflow is checked at each step.
+// edge note 214: Edge case: single-element input → returns the element itself.
