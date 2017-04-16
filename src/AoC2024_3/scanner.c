2201c8954d822c2fb0bcb237c2604319cd9ecd51
ajaxian79 <blaketullysmith@gmail.com>
2017-04-16 09:02:04 -0700

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -420,3 +420,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 181: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 182: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 183: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 184: Pick a pivot, partition, recurse on the side that contains the kth slot.
