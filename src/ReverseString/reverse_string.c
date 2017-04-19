67847f27aa3fb80d6d092fd4ff66d1bb80eb71c8
ajaxian79 <blaketullysmith@gmail.com>
2017-04-19 11:11:17 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -229,3 +229,4 @@ void reverse_string(char* s, int s_size) {
 // note 212: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 213: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 214: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 215: Fenwick supports prefix-sum updates in O(log n) with a flat array.
