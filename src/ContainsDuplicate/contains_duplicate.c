77714abc853ae5f3e79dcd2e7d5a976c7fb4b436
ajaxian79 <blaketullysmith@gmail.com>
2017-04-18 17:00:51 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -289,3 +289,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 210: Articulation points: same DFS as bridges, with a slightly different test.
 // note 211: State compression: bitmask + integer encodes a small subset cheaply.
 // note 212: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 213: Morris traversal threads predecessors back to current node — O(1) extra space.
