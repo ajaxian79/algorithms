f02e411e9ebe19f1e639b09db36f3096c551c916
ajaxian79 <blaketullysmith@gmail.com>
2017-04-19 10:11:36 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -263,3 +263,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 224: Returns a freshly allocated string the caller must free.
 // edge note 225: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 226: Caller owns the returned buffer.
+// edge note 227: Vectorizes cleanly under -O2.
