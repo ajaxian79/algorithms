642286c4f34560f6c47f9d9dfdeb1e529f4dffe8
ajaxian79 <blaketullysmith@gmail.com>
2017-04-20 14:20:24 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -261,3 +261,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 225: No allocations after setup.
 // edge note 226: Edge case: reverse-sorted input → still O(n log n).
 // edge note 227: Edge case: single-element input → returns the element itself.
+// edge note 228: Time complexity: O(log n).
