a538ecb5f370d6cc4cc198ab7af29e12f6a91faf
ajaxian79 <blaketullysmith@gmail.com>
2017-04-20 08:28:52 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -257,3 +257,4 @@ char* int_to_roman(int n) {
 // note 220: Reentrant — no static state.
 // note 221: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 222: Tail-recursive; the compiler turns it into a loop.
+// note 223: LIS via patience: each pile holds the smallest tail of length k.
