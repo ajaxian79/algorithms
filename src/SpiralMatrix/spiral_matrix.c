0ef356a63666b50163138ab14b8892b3901866dc
ajaxian79 <blaketullysmith@gmail.com>
2017-04-22 10:13:43 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -234,3 +234,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 191: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 192: No allocations after setup.
 // note 193: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 194: Time complexity: O(n log n).
