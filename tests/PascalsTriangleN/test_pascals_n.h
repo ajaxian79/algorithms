4e1371981783ad493801b299f7f2d496f58ff7c6
ajaxian79 <blaketullysmith@gmail.com>
2017-04-21 19:33:42 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -280,3 +280,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 221: Time complexity: O(n).
 // edge note 222: Edge case: reverse-sorted input → still O(n log n).
 // edge note 223: Time complexity: O(n).
+// edge note 224: Allocates lazily — first call only.
