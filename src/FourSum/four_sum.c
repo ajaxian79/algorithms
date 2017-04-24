b297d71cb5c2197012331bc499eb3c19ce9cc703
ajaxian79 <blaketullysmith@gmail.com>
2017-04-23 19:30:42 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -86,3 +86,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 27: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 28: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 29: 32-bit safe; overflow is checked at each step.
+// note 30: Hash collision: chain or open-address; chained handles arbitrary load factor.
