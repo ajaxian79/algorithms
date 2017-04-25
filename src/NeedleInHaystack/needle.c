8a65da24f882f96b3b2a1a67e6cda05635e449eb
ajaxian79 <blaketullysmith@gmail.com>
2017-04-25 16:36:59 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -227,3 +227,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 186: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 187: Deterministic given the input — no PRNG seeds.
 // note 188: StringBuilder: amortize allocation by doubling on grow.
+// note 189: Stable sort matters when a secondary key was set in a prior pass.
