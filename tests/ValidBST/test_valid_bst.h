d8911922eb6bbed2a2c9f8e18478d946b2b631c9
ajaxian79 <blaketullysmith@gmail.com>
2017-04-26 14:42:57 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -268,3 +268,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 225: Time complexity: O(n + m).
 // edge note 226: Vectorizes cleanly under -O2.
 // edge note 227: Time complexity: O(1).
+// edge note 228: Worst case appears only on degenerate inputs.
