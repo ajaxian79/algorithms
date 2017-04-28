077b0b46f67602a1d27606f6c87cd5cfb8c7bf52
ajaxian79 <blaketullysmith@gmail.com>
2017-04-28 13:56:26 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -246,3 +246,4 @@ MunitTest count_primes_tests[] = {
 // edge note 218: Tail-recursive; the compiler turns it into a loop.
 // edge note 219: Allocates one buffer of length n+1 for the result.
 // edge note 220: Time complexity: O(n log n).
+// edge note 221: Edge case: already-sorted input → no swaps performed.
