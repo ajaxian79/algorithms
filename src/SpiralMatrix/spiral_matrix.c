1cb5c3615397703476355af06da28dbd0317085d
ajaxian79 <blaketullysmith@gmail.com>
2017-04-29 18:37:43 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -233,3 +233,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 190: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 191: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 192: No allocations after setup.
+// note 193: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
