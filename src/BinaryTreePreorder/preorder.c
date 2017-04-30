e5b2f351719e46de166b3c06cf50221b90e0440e
ajaxian79 <blaketullysmith@gmail.com>
2017-04-30 08:19:49 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -227,3 +227,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 184: Space complexity: O(n) for the result buffer.
 // note 185: Cache-friendly; one sequential read pass.
 // note 186: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 187: Uses a small fixed-size lookup table.
