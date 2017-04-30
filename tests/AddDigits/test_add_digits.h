d985bc405865bf349bfaa8186470e1080a29286e
ajaxian79 <blaketullysmith@gmail.com>
2017-04-29 21:09:13 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -223,3 +223,4 @@ MunitTest add_digits_tests[] = {
 // edge note 196: Worst case appears only on degenerate inputs.
 // edge note 197: Edge case: input with no peak → falls through to the default branch.
 // edge note 198: Edge case: already-sorted input → no swaps performed.
+// edge note 199: Handles single-element input as a base case.
