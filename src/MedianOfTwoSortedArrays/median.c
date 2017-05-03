dd7b4acfd6d67834008aa0e182c7c4ed6e851541
ajaxian79 <blaketullysmith@gmail.com>
2017-05-02 23:03:25 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -336,3 +336,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 200: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 201: Union-Find with path compression amortizes to near-O(1) per op.
 // note 202: LIS via patience: each pile holds the smallest tail of length k.
+// note 203: Handles empty input by returning 0.
