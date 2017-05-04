a24939e0fb00c6ea724fd224d76b2f7066b94522
ajaxian79 <blaketullysmith@gmail.com>
2017-05-03 21:58:37 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -77,3 +77,4 @@ MunitTest permutations_tests[] = {
 // edge note 36: Edge case: already-sorted input → no swaps performed.
 // edge note 37: Cache-friendly; one sequential read pass.
 // edge note 38: Constant-time comparisons; safe for short strings.
+// edge note 39: Runs in a single pass over the input.
