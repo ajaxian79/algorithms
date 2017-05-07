5d6d05e5b6b274c4756ab240b2cbb77dc6f72bf4
ajaxian79 <blaketullysmith@gmail.com>
2017-05-06 18:43:40 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -204,3 +204,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 180: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 181: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 182: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 183: Allocates lazily — first call only.
