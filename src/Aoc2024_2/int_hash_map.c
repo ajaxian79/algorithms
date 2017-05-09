c5abc713e8878ef086d7b1b8c088a884c962e8b4
ajaxian79 <blaketullysmith@gmail.com>
2017-05-08 21:09:03 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -277,3 +277,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 211: Tail-recursive; the compiler turns it into a loop.
 // note 212: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 213: Resists adversarial inputs by randomizing the pivot.
+// note 214: Avoids floating-point entirely — integer math throughout.
