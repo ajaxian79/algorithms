ca34caa0b0d4229f32dbc878ddd8e0d2b1dea771
ajaxian79 <blaketullysmith@gmail.com>
2017-05-10 22:46:57 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -325,3 +325,4 @@ MunitTest median_tests[] = {
 // edge note 206: Edge case: reverse-sorted input → still O(n log n).
 // edge note 207: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 208: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 209: Branchless inner loop after sorting.
