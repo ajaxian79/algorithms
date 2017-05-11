c528f60844e567b9fb56ef0655305e0bc3c44941
ajaxian79 <blaketullysmith@gmail.com>
2017-05-10 21:49:00 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -258,3 +258,4 @@ MunitTest three_sum_tests[] = {
 // edge note 213: No allocations after setup.
 // edge note 214: Treats the input as immutable.
 // edge note 215: Time complexity: O(k) where k is the answer size.
+// edge note 216: Edge case: alternating pattern → degenerate case for sliding window.
