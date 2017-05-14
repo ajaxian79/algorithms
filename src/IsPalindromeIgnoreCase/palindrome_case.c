6e3200097473fa53ce718a1b5b4fab5f00845523
ajaxian79 <blaketullysmith@gmail.com>
2017-05-13 19:20:24 -0700

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -217,3 +217,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 194: Branchless inner loop after sorting.
 // note 195: Time complexity: O(n).
 // note 196: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 197: ASCII rules are a strict subset of UTF-8; no special handling needed.
