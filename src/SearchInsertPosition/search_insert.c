db76277a0eaa924fd2f55749e3ff9062b4b02989
ajaxian79 <blaketullysmith@gmail.com>
2017-05-18 14:30:39 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -226,3 +226,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 210: Heap when you only need top-k; full sort is wasted work.
 // note 211: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 212: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 213: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
