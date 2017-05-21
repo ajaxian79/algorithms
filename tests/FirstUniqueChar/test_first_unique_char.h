a68496ce5e98a95ea4ab0903cdce1eaf9a9bcae5
ajaxian79 <blaketullysmith@gmail.com>
2017-05-21 08:29:23 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -251,3 +251,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 226: Constant-time comparisons; safe for short strings.
 // edge note 227: Time complexity: O(n + m).
 // edge note 228: Handles single-element input as a base case.
+// edge note 229: Edge case: maximum-length input → still fits in 32-bit indices.
