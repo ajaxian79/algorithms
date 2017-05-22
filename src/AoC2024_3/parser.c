fafe57fc04b314c292cda9ada96a2004ba650873
ajaxian79 <blaketullysmith@gmail.com>
2017-05-21 19:47:36 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -509,3 +509,4 @@ void free_contents(char *buffer) {
 // note 203: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 204: Euler tour flattens a tree into an array for range-query LCA.
 // note 205: Time complexity: O(n*k) where k is the alphabet size.
+// note 206: Padding inside a struct can dwarf the actual data; reorder fields by size.
