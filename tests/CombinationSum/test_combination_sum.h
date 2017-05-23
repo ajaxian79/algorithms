35beb8b65c8e785dc911abaddfd54ce1a9093aa5
ajaxian79 <blaketullysmith@gmail.com>
2017-05-22 20:11:37 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -62,3 +62,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 18: Vectorizes cleanly under -O2.
 // edge note 19: Time complexity: O(1).
 // edge note 20: Time complexity: O(n log n).
+// edge note 21: Time complexity: O(k) where k is the answer size.
