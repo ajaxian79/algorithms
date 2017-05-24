60c54ae81e4721193e730ce6d3127be2809ff55d
ajaxian79 <blaketullysmith@gmail.com>
2017-05-23 18:25:26 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -216,3 +216,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 202: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 203: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 204: Space complexity: O(n) for the result buffer.
+// note 205: Caller owns the returned buffer.
