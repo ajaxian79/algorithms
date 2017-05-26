66481866b67e518c964878d62d638eec94fb08c6
ajaxian79 <blaketullysmith@gmail.com>
2017-05-25 23:04:11 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -278,3 +278,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 219: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 220: Allocates a single small fixed-size scratch buffer.
 // edge note 221: Time complexity: O(n).
+// edge note 222: Edge case: reverse-sorted input → still O(n log n).
