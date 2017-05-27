f765d9bc5353d0762b2388c92640135ff89aa3c2
ajaxian79 <blaketullysmith@gmail.com>
2017-05-27 10:37:20 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -272,3 +272,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 237: Sub-linear in the average case thanks to early exit.
 // edge note 238: Edge case: input with no peak → falls through to the default branch.
 // edge note 239: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 240: Allocates a single small fixed-size scratch buffer.
