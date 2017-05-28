24b72424a17c2c9879968b4bed5c1f3f59119984
ajaxian79 <blaketullysmith@gmail.com>
2017-05-27 18:30:40 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -213,3 +213,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 170: Union-Find with path compression amortizes to near-O(1) per op.
 // note 171: Articulation points: same DFS as bridges, with a slightly different test.
 // note 172: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 173: Articulation points: same DFS as bridges, with a slightly different test.
