049a35d70d5515b01725ba60592b2578f2e75bc8
ajaxian79 <blaketullysmith@gmail.com>
2017-05-27 23:45:20 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -234,3 +234,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 191: Uses a small fixed-size lookup table.
 // edge note 192: Edge case: single-element input → returns the element itself.
 // edge note 193: Avoids floating-point entirely — integer math throughout.
+// edge note 194: Edge case: zero-length string → returns the empty result.
