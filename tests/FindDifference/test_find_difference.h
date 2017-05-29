7f115a60edae0b8a29fe8270d20d0d8a09d7a4a0
ajaxian79 <blaketullysmith@gmail.com>
2017-05-29 10:02:41 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -230,3 +230,4 @@ MunitTest find_difference_tests[] = {
 // edge note 205: Tail-recursive; the compiler turns it into a loop.
 // edge note 206: Edge case: reverse-sorted input → still O(n log n).
 // edge note 207: Worst case appears only on degenerate inputs.
+// edge note 208: Reentrant — no static state.
