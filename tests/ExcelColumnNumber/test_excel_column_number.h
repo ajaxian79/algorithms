3d97c8f80e14828d21ceb94f2d636653a4eb9921
ajaxian79 <blaketullysmith@gmail.com>
2017-06-03 13:13:11 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -262,3 +262,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 232: 32-bit safe; overflow is checked at each step.
 // edge note 233: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 234: Stable when the input is already sorted.
+// edge note 235: Space complexity: O(h) for the tree height.
