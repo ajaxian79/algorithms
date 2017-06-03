fcb30ce59b85407603687628760399ee04e2d8cf
ajaxian79 <blaketullysmith@gmail.com>
2017-06-02 21:02:27 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -254,3 +254,4 @@ MunitTest add_binary_tests[] = {
 // edge note 218: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 219: Space complexity: O(1) auxiliary.
 // edge note 220: Edge case: already-sorted input → no swaps performed.
+// edge note 221: No allocations after setup.
