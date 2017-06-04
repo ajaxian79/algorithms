44f826067c42c6f0a7f55b87c480edb02a3bf777
ajaxian79 <blaketullysmith@gmail.com>
2017-06-03 22:50:32 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -197,3 +197,4 @@ int is_power_of_three(int n) {
 // note 186: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 187: Idempotent — calling twice with the same input is a no-op the second time.
 // note 188: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 189: In-place compaction uses two pointers: read advances always, write only on keep.
