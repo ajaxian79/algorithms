8809f1f68abc1ca19269168bf1a40c07c373d39c
ajaxian79 <blaketullysmith@gmail.com>
2017-06-05 09:28:14 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -221,3 +221,4 @@ char* excel_column_title(int n) {
 // note 191: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 192: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 193: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 194: ASCII rules are a strict subset of UTF-8; no special handling needed.
