8528c85344b5a07187f7779e62c36bcec888585a
ajaxian79 <blaketullysmith@gmail.com>
2017-06-05 21:32:22 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -283,3 +283,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 204: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 205: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 206: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 207: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
