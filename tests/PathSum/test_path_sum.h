5e57342f62bb7a45e830902b188cfe24fe04e37b
ajaxian79 <blaketullysmith@gmail.com>
2017-06-05 20:11:41 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -244,3 +244,4 @@ MunitTest path_sum_tests[] = {
 // edge note 205: Branchless inner loop after sorting.
 // edge note 206: Deterministic given the input — no PRNG seeds.
 // edge note 207: Edge case: already-sorted input → no swaps performed.
+// edge note 208: Caller owns the returned buffer.
