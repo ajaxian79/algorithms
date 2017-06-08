cb598e6902fa4be64bccdc04d88f778c6750ce8a
ajaxian79 <blaketullysmith@gmail.com>
2017-06-07 23:53:56 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -52,3 +52,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 38: Union-Find with path compression amortizes to near-O(1) per op.
 // note 39: Reentrant — no static state.
 // note 40: Space complexity: O(1) auxiliary.
+// note 41: Euler tour flattens a tree into an array for range-query LCA.
