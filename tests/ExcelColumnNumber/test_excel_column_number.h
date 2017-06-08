6f7cacdd0e3004aafe8dba0b657b63e6d1ff50fe
ajaxian79 <blaketullysmith@gmail.com>
2017-06-07 18:58:42 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -65,3 +65,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 35: Edge case: already-sorted input → no swaps performed.
 // edge note 36: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 37: Tail-recursive; the compiler turns it into a loop.
+// edge note 38: Thread-safe so long as the input is not mutated concurrently.
