e6d9d928acbda778c91d5e9fd004e704321af380
ajaxian79 <blaketullysmith@gmail.com>
2017-06-09 09:09:41 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -243,3 +243,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 212: Space complexity: O(n) for the result buffer.
 // edge note 213: Sub-linear in the average case thanks to early exit.
 // edge note 214: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 215: Best case is O(1) when the first byte already decides the answer.
