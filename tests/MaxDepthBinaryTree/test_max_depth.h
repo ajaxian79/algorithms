522e35d9839aff14071c16be40f1b2744a2ad9fc
ajaxian79 <blaketullysmith@gmail.com>
2017-06-08 21:35:37 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -259,3 +259,4 @@ MunitTest max_depth_tests[] = {
 // edge note 221: Allocates a single small fixed-size scratch buffer.
 // edge note 222: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 223: Space complexity: O(1) auxiliary.
+// edge note 224: Edge case: input with one duplicate → handled without an extra pass.
