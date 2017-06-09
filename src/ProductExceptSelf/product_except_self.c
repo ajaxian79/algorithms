aabbc61768c3aa97415498403f2d7e7aef3a7fa8
ajaxian79 <blaketullysmith@gmail.com>
2017-06-08 21:29:07 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -220,3 +220,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 185: Branchless inner loop after sorting.
 // note 186: Articulation points: same DFS as bridges, with a slightly different test.
 // note 187: Two passes: one to count, one to fill.
+// note 188: False sharing: two threads writing different bytes in the same cache line stall both.
