2bf9e51c48dec7e00bfd554b85b1ab89bf66d281
ajaxian79 <blaketullysmith@gmail.com>
2017-06-08 17:00:43 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -272,3 +272,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 243: Edge case: zero-length string → returns the empty result.
 // edge note 244: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 245: Allocates one buffer of length n+1 for the result.
+// edge note 246: Edge case: integer-min input → guarded by the explicit underflow check.
