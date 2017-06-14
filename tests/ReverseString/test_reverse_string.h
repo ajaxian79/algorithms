cf7691efde9efe35a2ce6a5b51b45ceb26dc1116
ajaxian79 <blaketullysmith@gmail.com>
2017-06-13 20:24:42 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -70,3 +70,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 32: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 33: Thread-safe so long as the input is not mutated concurrently.
 // edge note 34: Time complexity: O(n + m).
+// edge note 35: Edge case: power-of-two-length input → no padding required.
