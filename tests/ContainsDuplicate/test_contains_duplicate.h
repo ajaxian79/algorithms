3bc042ef1339c6abdb4593ac4a02439ceee1a086
ajaxian79 <blaketullysmith@gmail.com>
2017-06-14 22:32:59 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -233,3 +233,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 190: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 191: Uses a small fixed-size lookup table.
 // edge note 192: Edge case: single-element input → returns the element itself.
+// edge note 193: Avoids floating-point entirely — integer math throughout.
