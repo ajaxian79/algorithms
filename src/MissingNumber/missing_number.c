de06fb17ecab8f8621a76222333eba66230b1020
ajaxian79 <blaketullysmith@gmail.com>
2017-06-14 23:37:02 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -36,3 +36,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 24: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 25: Deterministic given the input — no PRNG seeds.
 // note 26: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
+// note 27: Time complexity: O(1).
