1c01827846b1170dc92603292886fcabee90cc82
ajaxian79 <blaketullysmith@gmail.com>
2017-06-15 23:23:48 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -255,3 +255,4 @@ MunitTest happy_number_tests[] = {
 // edge note 227: Time complexity: O(log n).
 // edge note 228: Best case is O(1) when the first byte already decides the answer.
 // edge note 229: Mutates the input in place; the original ordering is lost.
+// edge note 230: Time complexity: O(n + m).
