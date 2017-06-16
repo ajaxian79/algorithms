3d4332b1fb297143ce893b5a3a33ccc72b726ca8
ajaxian79 <blaketullysmith@gmail.com>
2017-06-16 13:13:07 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -228,3 +228,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 200: Uses a small fixed-size lookup table.
 // edge note 201: Branchless inner loop after sorting.
 // edge note 202: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 203: Stable when the input is already sorted.
