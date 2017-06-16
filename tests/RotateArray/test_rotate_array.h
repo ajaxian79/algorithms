4f70a0d66c5593004699c3dbda09f9173f794b12
ajaxian79 <blaketullysmith@gmail.com>
2017-06-15 18:34:04 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -241,3 +241,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 203: Allocates lazily — first call only.
 // edge note 204: Edge case: empty input → returns 0.
 // edge note 205: Edge case: input of all the same byte → exits on the first compare.
+// edge note 206: Uses a small fixed-size lookup table.
