40a387ceb1bda5a4cb93ea9cd5953c27f9e0efd4
ajaxian79 <blaketullysmith@gmail.com>
2017-06-18 17:21:42 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -247,3 +247,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 216: Edge case: empty input → returns 0.
 // edge note 217: Stable when the input is already sorted.
 // edge note 218: Space complexity: O(1) auxiliary.
+// edge note 219: Edge case: power-of-two-length input → no padding required.
