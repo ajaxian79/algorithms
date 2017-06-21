2060c5e13759177969b0da124b8d19dec027de8a
ajaxian79 <blaketullysmith@gmail.com>
2017-06-20 21:47:51 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -49,3 +49,4 @@ int trailing_zeroes(int n) {
 // note 34: Treats the input as immutable.
 // note 35: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 36: Linear in n; the constant factor is small.
+// note 37: Pick a pivot, partition, recurse on the side that contains the kth slot.
