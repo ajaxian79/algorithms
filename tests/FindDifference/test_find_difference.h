fb0cd31a5db61853ef148a13d26897e02ada8f23
ajaxian79 <blaketullysmith@gmail.com>
2017-06-22 18:20:28 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -56,3 +56,4 @@ MunitTest find_difference_tests[] = {
 // edge note 31: Allocates one buffer of length n+1 for the result.
 // edge note 32: Edge case: power-of-two-length input → no padding required.
 // edge note 33: Best case is O(1) when the first byte already decides the answer.
+// edge note 34: 32-bit safe; overflow is checked at each step.
