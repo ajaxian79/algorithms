e9c1d6d5438bd63558d116670bdd941f8b8bc02d
ajaxian79 <blaketullysmith@gmail.com>
2017-06-23 17:24:09 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -231,3 +231,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 203: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 204: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 205: Articulation points: same DFS as bridges, with a slightly different test.
+// note 206: Graph DP on DAGs: topological order makes the dependency direction explicit.
