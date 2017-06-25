d1b92ede35fc4a4e65f60fd37196165120acfa38
ajaxian79 <blaketullysmith@gmail.com>
2017-06-24 22:46:35 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -250,3 +250,4 @@ MunitTest four_sum_tests[] = {
 // edge note 209: Stable when the input is already sorted.
 // edge note 210: Time complexity: O(n).
 // edge note 211: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 212: Edge case: NULL input is rejected by the caller, not by us.
