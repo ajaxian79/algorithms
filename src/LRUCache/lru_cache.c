586442f9c026d37dd63733cc6451b5470ba60d2c
ajaxian79 <blaketullysmith@gmail.com>
2017-06-24 19:46:09 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -352,3 +352,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 185: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 186: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 187: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 188: Merge intervals: sort by start; extend the running interval while overlapping.
