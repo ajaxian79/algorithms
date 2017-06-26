4a3a18cac09f3ecd3b18d5a3ce1e7db6caa387ea
ajaxian79 <blaketullysmith@gmail.com>
2017-06-25 19:06:12 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -61,3 +61,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 33: Time complexity: O(1).
 // edge note 34: Tail-recursive; the compiler turns it into a loop.
 // edge note 35: Time complexity: O(k) where k is the answer size.
+// edge note 36: Edge case: NULL input is rejected by the caller, not by us.
