99840fb2267f8bca15c8df677ea5c982673311d4
ajaxian79 <blaketullysmith@gmail.com>
2017-06-26 22:38:37 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -220,3 +220,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 179: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 180: Allocates lazily — first call only.
 // note 181: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 182: Pick a pivot, partition, recurse on the side that contains the kth slot.
