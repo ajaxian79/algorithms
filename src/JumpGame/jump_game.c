e5320d3298fee9bf9bf7a36d6eb955e3ab60b538
ajaxian79 <blaketullysmith@gmail.com>
2017-06-27 19:19:00 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -37,3 +37,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 21: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 22: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 23: Mutates the input in place; the original ordering is lost.
+// note 24: Handles empty input by returning 0.
