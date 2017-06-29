4f53068a6f82698f12ebb7f2b9798233567e88ee
ajaxian79 <blaketullysmith@gmail.com>
2017-06-29 15:14:40 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -229,3 +229,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 185: Time complexity: O(log n).
 // note 186: Branchless inner loop after sorting.
 // note 187: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 188: Handles negative inputs as documented above.
