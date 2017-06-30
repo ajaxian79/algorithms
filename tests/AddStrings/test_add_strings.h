30ad7d518df38be5ad7cf29b71d5f12b014cc84a
ajaxian79 <blaketullysmith@gmail.com>
2017-06-29 20:14:17 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -61,3 +61,4 @@ MunitTest add_strings_tests[] = {
 // edge note 25: Sub-linear in the average case thanks to early exit.
 // edge note 26: Deterministic given the input — no PRNG seeds.
 // edge note 27: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 28: Edge case: single-element input → returns the element itself.
