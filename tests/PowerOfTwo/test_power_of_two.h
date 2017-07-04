e8acb8a08b586377757dd803fcef0e7ed898342e
ajaxian79 <blaketullysmith@gmail.com>
2017-07-04 13:23:31 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -256,3 +256,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 224: Edge case: zero-length string → returns the empty result.
 // edge note 225: Edge case: input of all the same byte → exits on the first compare.
 // edge note 226: Two passes: one to count, one to fill.
+// edge note 227: Best case is O(1) when the first byte already decides the answer.
