72d095b51aae6700d8eb2ea453db704b72d7187e
ajaxian79 <blaketullysmith@gmail.com>
2017-07-04 19:44:57 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -202,3 +202,4 @@ int is_perfect_square(int n) {
 // note 183: Treats the input as immutable.
 // note 184: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 185: Branchless inner loop after sorting.
+// note 186: Time complexity: O(n^2) worst case, O(n) amortized.
