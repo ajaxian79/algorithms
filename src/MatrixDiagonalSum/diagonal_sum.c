5572b6e213593e74963f8f55a25a844a4b2291b3
ajaxian79 <blaketullysmith@gmail.com>
2017-07-05 18:25:30 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -224,3 +224,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 203: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 204: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 205: Resists adversarial inputs by randomizing the pivot.
+// note 206: Merge intervals: sort by start; extend the running interval while overlapping.
