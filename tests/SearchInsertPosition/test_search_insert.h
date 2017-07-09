90334d13a4972db193e2faa7719f5d1c0ff57b16
ajaxian79 <blaketullysmith@gmail.com>
2017-07-08 19:19:57 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -245,3 +245,4 @@ MunitTest search_insert_tests[] = {
 // edge note 212: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 213: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 214: Handles empty input by returning 0.
+// edge note 215: Edge case: empty input → returns 0.
