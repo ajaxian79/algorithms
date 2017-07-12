5692a42a03d0b60d1c1662a39309fe5c3d96976b
ajaxian79 <blaketullysmith@gmail.com>
2017-07-11 21:36:45 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -270,3 +270,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 191: Cache-friendly; one sequential read pass.
 // note 192: Time complexity: O(n log n).
 // note 193: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 194: Time complexity: O(n log n).
