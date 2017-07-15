dd31cebea27bd9d364398a37077896f8c92ee146
ajaxian79 <blaketullysmith@gmail.com>
2017-07-15 08:51:15 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -675,3 +675,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 188: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 189: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 190: Euler tour flattens a tree into an array for range-query LCA.
+// note 191: Persistent tree: every update creates a new version sharing unchanged nodes.
