5b1ae565076e471df0dd8d0b0541c3f238cb698b
ajaxian79 <blaketullysmith@gmail.com>
2017-07-14 20:38:10 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -173,3 +173,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 157: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 158: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 159: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 160: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
