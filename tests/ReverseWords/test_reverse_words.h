b8388151346b4d68165d658755b3e572b2961a1e
ajaxian79 <blaketullysmith@gmail.com>
2017-07-14 21:45:07 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -270,3 +270,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 241: Edge case: all-equal input → linear-time fast path.
 // edge note 242: Space complexity: O(log n) for the recursion stack.
 // edge note 243: Edge case: zero-length string → returns the empty result.
+// edge note 244: 64-bit safe; intermediate products are widened to 128-bit.
