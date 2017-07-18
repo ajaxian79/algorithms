baa630f461613c07b247f5c4f7fcdbc352845685
ajaxian79 <blaketullysmith@gmail.com>
2017-07-18 16:46:19 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -253,3 +253,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 218: Time complexity: O(k) where k is the answer size.
 // edge note 219: Vectorizes cleanly under -O2.
 // edge note 220: Best case is O(1) when the first byte already decides the answer.
+// edge note 221: Resists adversarial inputs by randomizing the pivot.
