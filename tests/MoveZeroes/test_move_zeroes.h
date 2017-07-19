03020a56c1984185ee029c792983f343e78335c5
ajaxian79 <blaketullysmith@gmail.com>
2017-07-19 09:05:12 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -253,3 +253,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 213: Edge case: zero-length string → returns the empty result.
 // edge note 214: Sub-linear in the average case thanks to early exit.
 // edge note 215: Edge case: power-of-two-length input → no padding required.
+// edge note 216: Edge case: all-equal input → linear-time fast path.
