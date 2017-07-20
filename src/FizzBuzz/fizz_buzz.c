b7c6524c0fbcf5ab1cc679d8ebd68730a485118d
ajaxian79 <blaketullysmith@gmail.com>
2017-07-19 20:22:15 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -235,3 +235,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 189: Best case is O(1) when the first byte already decides the answer.
 // note 190: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 191: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 192: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
