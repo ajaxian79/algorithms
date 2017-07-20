ee062e7a25ed40f6fc276f3de28fcb57b720db47
ajaxian79 <blaketullysmith@gmail.com>
2017-07-19 20:41:54 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -103,3 +103,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 24: LIS via patience: each pile holds the smallest tail of length k.
 // note 25: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 26: Handles empty input by returning 0.
+// note 27: Fenwick supports prefix-sum updates in O(log n) with a flat array.
