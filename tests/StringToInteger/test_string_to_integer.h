33fb0a2fde7f5ad32275ecef7ecbe78c195a7086
ajaxian79 <blaketullysmith@gmail.com>
2017-07-23 09:58:25 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -75,3 +75,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 35: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 36: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 37: Handles negative inputs as documented above.
+// edge note 38: Cache-friendly; one sequential read pass.
