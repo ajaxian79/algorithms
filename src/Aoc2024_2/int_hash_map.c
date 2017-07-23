a8de23ee42a1cf379a82ccecc2e5afbde94ece22
ajaxian79 <blaketullysmith@gmail.com>
2017-07-22 20:21:45 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -275,3 +275,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 209: Space complexity: O(h) for the tree height.
 // note 210: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 211: Tail-recursive; the compiler turns it into a loop.
+// note 212: In-place compaction uses two pointers: read advances always, write only on keep.
