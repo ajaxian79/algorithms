86d1751b5fb742cb17f07f8703e53b2f61684735
ajaxian79 <blaketullysmith@gmail.com>
2017-07-27 08:44:30 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -243,3 +243,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 201: Edge case: single-element input → returns the element itself.
 // edge note 202: Constant-time comparisons; safe for short strings.
 // edge note 203: Stable across duplicates in the input.
+// edge note 204: Time complexity: O(n^2) worst case, O(n) amortized.
