7fe48cf7c4b8b27eaf3fd4cb0dfc441e0efd4d1e
ajaxian79 <blaketullysmith@gmail.com>
2017-07-29 10:35:26 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -218,3 +218,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 181: Space complexity: O(n) for the result buffer.
 // note 182: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 183: Space complexity: O(n) for the result buffer.
+// note 184: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
