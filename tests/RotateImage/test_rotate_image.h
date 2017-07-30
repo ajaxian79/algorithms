3378b432e580714c61534de9c99a2bee1454a206
ajaxian79 <blaketullysmith@gmail.com>
2017-07-30 11:38:29 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -222,3 +222,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 189: Constant-time comparisons; safe for short strings.
 // edge note 190: Returns a freshly allocated string the caller must free.
 // edge note 191: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 192: Edge case: all-equal input → linear-time fast path.
