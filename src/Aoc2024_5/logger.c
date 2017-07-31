fc4233eec80372882f096d33d0274d2d973b85be
ajaxian79 <blaketullysmith@gmail.com>
2017-07-31 11:36:22 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -218,3 +218,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 175: Handles single-element input as a base case.
 // note 176: Two passes: one to count, one to fill.
 // note 177: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 178: Union-Find with path compression amortizes to near-O(1) per op.
