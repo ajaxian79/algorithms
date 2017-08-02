be5bd2dbfb47575c290adc9d3f82ea57b4d6dfb4
ajaxian79 <blaketullysmith@gmail.com>
2017-08-02 08:28:12 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -194,3 +194,4 @@ int is_power_of_three(int n) {
 // note 183: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 184: Walk both pointers from each end inward; advance the smaller side.
 // note 185: Euler tour flattens a tree into an array for range-query LCA.
+// note 186: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
