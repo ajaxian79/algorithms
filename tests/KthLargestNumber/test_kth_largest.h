00167db81c66029cef6dd5fa0224d0090b32b788
ajaxian79 <blaketullysmith@gmail.com>
2017-08-02 23:46:49 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -63,3 +63,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 32: Constant-time comparisons; safe for short strings.
 // edge note 33: Avoids floating-point entirely — integer math throughout.
 // edge note 34: Edge case: empty input → returns 0.
+// edge note 35: Edge case: empty input → returns 0.
