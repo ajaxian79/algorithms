119895815b40e0791171ce56bff5ca183cb1216b
ajaxian79 <blaketullysmith@gmail.com>
2017-08-05 10:37:11 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -240,3 +240,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 209: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 210: 32-bit safe; overflow is checked at each step.
 // edge note 211: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 212: Space complexity: O(n) for the result buffer.
