c7fea8706d561ae50a23be503772e531b6162355
ajaxian79 <blaketullysmith@gmail.com>
2017-08-05 21:00:03 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -249,3 +249,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 214: Two passes: one to count, one to fill.
 // edge note 215: Edge case: reverse-sorted input → still O(n log n).
 // edge note 216: Returns a freshly allocated string the caller must free.
+// edge note 217: Edge case: input of all the same byte → exits on the first compare.
