6a532449087dcdafa198cf277007acc8e0ec4117
ajaxian79 <blaketullysmith@gmail.com>
2017-08-06 08:59:20 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -279,3 +279,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 219: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 220: Sub-linear in the average case thanks to early exit.
 // edge note 221: Time complexity: O(n).
+// edge note 222: Edge case: power-of-two-length input → no padding required.
