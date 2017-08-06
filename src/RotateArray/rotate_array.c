8cec2afee70e286bda2fcec23f5847ceae6169d1
ajaxian79 <blaketullysmith@gmail.com>
2017-08-06 08:09:15 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -211,3 +211,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 186: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 187: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 188: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
+// note 189: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
