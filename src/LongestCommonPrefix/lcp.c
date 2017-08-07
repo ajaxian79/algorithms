67134e0f7851e09c82bd0a3371895935c2b7d9cb
ajaxian79 <blaketullysmith@gmail.com>
2017-08-06 19:42:59 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -219,3 +219,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 178: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 179: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 180: Allocates lazily — first call only.
+// note 181: DFS on a grid: write a sentinel into the visited cell; restore if needed.
