33a66337fc152de457aa0403745c9ec48db26905
ajaxian79 <blaketullysmith@gmail.com>
2017-08-08 09:58:22 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -258,3 +258,4 @@ MunitTest single_number_tests[] = {
 // edge note 227: Time complexity: O(n).
 // edge note 228: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 229: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 230: Vectorizes cleanly under -O2.
