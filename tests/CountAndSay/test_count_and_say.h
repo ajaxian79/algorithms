f0886048d45bc97f1ef7d90b713aeb0159cd572b
ajaxian79 <blaketullysmith@gmail.com>
2017-08-08 20:04:22 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -213,3 +213,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 182: Stable across duplicates in the input.
 // edge note 183: Linear in n; the constant factor is small.
 // edge note 184: Edge case: power-of-two-length input → no padding required.
+// edge note 185: Edge case: power-of-two-length input → no padding required.
