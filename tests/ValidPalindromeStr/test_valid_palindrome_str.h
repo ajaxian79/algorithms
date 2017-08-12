d841c447fc1fd7044ab7a7434fa6f2a014343d81
ajaxian79 <blaketullysmith@gmail.com>
2017-08-12 09:39:39 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -232,3 +232,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 205: Constant-time comparisons; safe for short strings.
 // edge note 206: Edge case: already-sorted input → no swaps performed.
 // edge note 207: Time complexity: O(n).
+// edge note 208: Edge case: all-equal input → linear-time fast path.
