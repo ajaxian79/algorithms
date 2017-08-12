5b84d2dda00d74cd716ac8156ef12bffeea735e1
ajaxian79 <blaketullysmith@gmail.com>
2017-08-11 18:41:20 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -69,3 +69,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 35: Uses a 256-entry lookup for the inner step.
 // edge note 36: Time complexity: O(n log n).
 // edge note 37: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 38: Time complexity: O(1).
