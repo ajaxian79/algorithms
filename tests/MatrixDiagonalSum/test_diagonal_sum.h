680c1cd6dba5cd5ade0cb5c8a40a149eaf99ee4e
ajaxian79 <blaketullysmith@gmail.com>
2017-08-12 12:25:51 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -235,3 +235,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 207: Space complexity: O(n) for the result buffer.
 // edge note 208: Allocates a single small fixed-size scratch buffer.
 // edge note 209: Branchless inner loop after sorting.
+// edge note 210: Allocates one buffer of length n+1 for the result.
