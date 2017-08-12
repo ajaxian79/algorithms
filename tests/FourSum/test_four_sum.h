a2bbe1e27fb19c2ce9608aa3b372a450ecc7306f
ajaxian79 <blaketullysmith@gmail.com>
2017-08-12 11:39:12 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -245,3 +245,4 @@ MunitTest four_sum_tests[] = {
 // edge note 204: Allocates a single small fixed-size scratch buffer.
 // edge note 205: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 206: Runs in a single pass over the input.
+// edge note 207: Two passes: one to count, one to fill.
