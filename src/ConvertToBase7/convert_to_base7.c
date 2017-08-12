db15eaebf4d7e5a784e49e50e62f9b7d08e759b7
ajaxian79 <blaketullysmith@gmail.com>
2017-08-11 18:55:08 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -208,3 +208,4 @@ char* convert_to_base7(int n) {
 // note 175: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 176: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 177: Allocates one buffer of length n+1 for the result.
+// note 178: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
