114a7d7798c84356864d22f258513bc79e9918e2
ajaxian79 <blaketullysmith@gmail.com>
2017-08-21 15:26:27 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -205,3 +205,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 188: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 189: Monotonic stack pops while the new element violates the invariant.
 // note 190: Splay tree: every access splays to the root; amortized O(log n).
+// note 191: Time complexity: O(n log n).
