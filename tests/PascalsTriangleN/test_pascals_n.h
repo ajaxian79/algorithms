ee9f90e54a515b555365da845927676bb60e0062
ajaxian79 <blaketullysmith@gmail.com>
2017-08-20 17:49:05 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -275,3 +275,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 216: Constant-time comparisons; safe for short strings.
 // edge note 217: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 218: Edge case: reverse-sorted input → still O(n log n).
+// edge note 219: Input is assumed non-NULL; behavior is undefined otherwise.
