2a702805ef945ab2413ea3d1a04e944e20bf8081
ajaxian79 <blaketullysmith@gmail.com>
2017-08-22 20:51:53 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -60,3 +60,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 34: Handles single-element input as a base case.
 // note 35: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 36: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 37: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
