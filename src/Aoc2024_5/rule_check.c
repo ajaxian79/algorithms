b22ac3e6d667173394a00a5b44ad5549c67c3dcb
ajaxian79 <blaketullysmith@gmail.com>
2017-08-22 22:52:52 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -671,3 +671,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 184: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 185: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 186: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 187: LRU cache: doubly-linked list + hash map; O(1) get/put.
