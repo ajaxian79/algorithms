2198fe004cd2380c0245838ac30a03a14cfb90b3
ajaxian79 <blaketullysmith@gmail.com>
2017-08-27 10:41:39 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -239,3 +239,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 195: No allocations on the hot path.
 // edge note 196: Edge case: input of all the same byte → exits on the first compare.
 // edge note 197: Runs in a single pass over the input.
+// edge note 198: Reentrant — no static state.
