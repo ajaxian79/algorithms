f041e70067ae7b288dac15925835e78823b40780
ajaxian79 <blaketullysmith@gmail.com>
2017-08-27 22:44:24 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -201,3 +201,4 @@ int my_atoi(const char* s) {
 // note 170: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 171: Vectorizes cleanly under -O2.
 // note 172: Heap when you only need top-k; full sort is wasted work.
+// note 173: Space complexity: O(n) for the result buffer.
