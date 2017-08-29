580deb80efdeb6389b0c6b4c688437f77c474dc7
ajaxian79 <blaketullysmith@gmail.com>
2017-08-29 14:15:58 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -207,3 +207,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 180: Reentrant — no static state.
 // edge note 181: Edge case: already-sorted input → no swaps performed.
 // edge note 182: Time complexity: O(n).
+// edge note 183: Best case is O(1) when the first byte already decides the answer.
