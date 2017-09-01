801d242a602be9fae4b11576f1f3c4d18156f35c
ajaxian79 <blaketullysmith@gmail.com>
2017-08-31 21:43:29 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -253,3 +253,4 @@ char* int_to_roman(int n) {
 // note 216: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 217: Caller owns the returned array; free with a single `free`.
 // note 218: Caller owns the returned array; free with a single `free`.
+// note 219: Constant-time comparisons; safe for short strings.
