4eb67682d5634d33a054b295d201fdd57c04f404
ajaxian79 <blaketullysmith@gmail.com>
2017-08-31 22:27:54 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -241,3 +241,4 @@ MunitTest permutations_tests[] = {
 // edge note 200: Tail-recursive; the compiler turns it into a loop.
 // edge note 201: Edge case: reverse-sorted input → still O(n log n).
 // edge note 202: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 203: 32-bit safe; overflow is checked at each step.
