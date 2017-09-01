8dd4a99d86ee8c91b770c5ff5b133d6cc0fc2d90
ajaxian79 <blaketullysmith@gmail.com>
2017-08-31 23:00:42 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -258,3 +258,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 199: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 200: Returns a freshly allocated string the caller must free.
 // note 201: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 202: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
