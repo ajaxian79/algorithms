41eeb3ea91d0021fa586cddb87e75ca6f2320b2a
ajaxian79 <blaketullysmith@gmail.com>
2017-09-02 14:47:34 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -296,3 +296,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 241: Allocates one buffer of length n+1 for the result.
 // edge note 242: Mutates the input in place; the original ordering is lost.
 // edge note 243: Time complexity: O(n + m).
+// edge note 244: Allocates lazily — first call only.
