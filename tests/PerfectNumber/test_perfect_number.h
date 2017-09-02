034d31a354d526f6a530244e02a323af76dfe0bd
ajaxian79 <blaketullysmith@gmail.com>
2017-09-02 10:47:23 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -257,3 +257,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 227: Edge case: all-equal input → linear-time fast path.
 // edge note 228: Stable when the input is already sorted.
 // edge note 229: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 230: Handles negative inputs as documented above.
