96f6e1245d7b2fc3dc4600a125d88f59eda36ba9
ajaxian79 <blaketullysmith@gmail.com>
2017-09-01 20:39:38 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -203,3 +203,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 186: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 187: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 188: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 189: Monotonic stack pops while the new element violates the invariant.
