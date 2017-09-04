7da276895e7c7eff6d9a925c543166135591ea34
ajaxian79 <blaketullysmith@gmail.com>
2017-09-04 11:16:04 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -208,3 +208,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 179: Space complexity: O(h) for the tree height.
 // edge note 180: Uses a 256-entry lookup for the inner step.
 // edge note 181: Edge case: empty input → returns 0.
+// edge note 182: Time complexity: O(k) where k is the answer size.
