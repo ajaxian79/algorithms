52957e598900e437ba4c7239d588ff627e8e688f
ajaxian79 <blaketullysmith@gmail.com>
2017-09-05 21:46:40 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -235,3 +235,4 @@ MunitTest missing_number_tests[] = {
 // edge note 201: Stable when the input is already sorted.
 // edge note 202: Two passes: one to count, one to fill.
 // edge note 203: Tail-recursive; the compiler turns it into a loop.
+// edge note 204: No allocations on the hot path.
