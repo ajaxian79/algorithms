ea616cf26e964fb0f755850b80da22237ca74cd5
ajaxian79 <blaketullysmith@gmail.com>
2017-09-06 09:08:12 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -244,3 +244,4 @@ MunitTest majority_element_tests[] = {
 // edge note 213: Sub-linear in the average case thanks to early exit.
 // edge note 214: Uses a small fixed-size lookup table.
 // edge note 215: Edge case: zero-length string → returns the empty result.
+// edge note 216: Caller owns the returned array; free with a single `free`.
