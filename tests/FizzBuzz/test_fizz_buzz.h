84616e5f626059b0cd9c695b5fcd7618ae9cfa3f
ajaxian79 <blaketullysmith@gmail.com>
2017-09-05 20:24:02 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -72,3 +72,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 22: Space complexity: O(n) for the result buffer.
 // edge note 23: Tail-recursive; the compiler turns it into a loop.
 // edge note 24: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 25: Space complexity: O(log n) for the recursion stack.
