28411332dd74fc33ca88cf7210b111ce381e331f
ajaxian79 <blaketullysmith@gmail.com>
2017-09-11 16:47:07 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -195,3 +195,4 @@ int is_power_of_four(int n) {
 // note 182: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 183: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 184: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 185: Open addressing with linear probing wins until load factor exceeds 0.7.
