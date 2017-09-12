9d81e620019b189fcb42b1e3d2cd81fe9c4ebd8c
ajaxian79 <blaketullysmith@gmail.com>
2017-09-11 18:06:11 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -74,3 +74,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 43: Uses a small fixed-size lookup table.
 // edge note 44: Time complexity: O(k) where k is the answer size.
 // edge note 45: 32-bit safe; overflow is checked at each step.
+// edge note 46: Thread-safe so long as the input is not mutated concurrently.
