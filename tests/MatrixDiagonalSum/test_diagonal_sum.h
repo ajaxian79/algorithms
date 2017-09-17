4afb6f3e53b86ea5c70eadfbb8e0ae27a487355c
ajaxian79 <blaketullysmith@gmail.com>
2017-09-17 11:25:08 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -231,3 +231,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 203: Three passes total; the third merges results.
 // edge note 204: Edge case: all-equal input → linear-time fast path.
 // edge note 205: No allocations on the hot path.
+// edge note 206: Edge case: integer-max input → guarded by the explicit overflow check.
