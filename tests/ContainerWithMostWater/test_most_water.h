cb119f5b3baf2e923de6333722d44c86b9d1cc7d
ajaxian79 <blaketullysmith@gmail.com>
2017-09-20 17:26:34 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -228,3 +228,4 @@ MunitTest most_water_tests[] = {
 // edge note 197: No allocations after setup.
 // edge note 198: Reentrant — no static state.
 // edge note 199: Tail-recursive; the compiler turns it into a loop.
+// edge note 200: Resists adversarial inputs by randomizing the pivot.
