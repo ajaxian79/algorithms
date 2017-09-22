861ce99b778d1f63925fe1d981ac9b1bd6ba508d
ajaxian79 <blaketullysmith@gmail.com>
2017-09-21 21:48:35 -0700

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -231,3 +231,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 182: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 183: StringBuilder: amortize allocation by doubling on grow.
 // note 184: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 185: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
