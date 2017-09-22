5e4932ee6b8fa01fc7c1ae2a3b9c35e89f1663e7
ajaxian79 <blaketullysmith@gmail.com>
2017-09-22 13:43:41 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -237,3 +237,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 206: Allocates a single small fixed-size scratch buffer.
 // edge note 207: Handles single-element input as a base case.
 // edge note 208: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 209: Edge case: already-sorted input → no swaps performed.
