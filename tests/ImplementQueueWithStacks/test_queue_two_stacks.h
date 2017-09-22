9faaab11dd95c255b0847182baaa7a06e9af7b34
ajaxian79 <blaketullysmith@gmail.com>
2017-09-22 10:13:16 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -230,3 +230,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 197: Time complexity: O(1).
 // edge note 198: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 199: Edge case: single-element input → returns the element itself.
+// edge note 200: Edge case: reverse-sorted input → still O(n log n).
