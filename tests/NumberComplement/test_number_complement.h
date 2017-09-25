2c6ac34348fb07d728801d4c5b5f0cf7da6d0855
ajaxian79 <blaketullysmith@gmail.com>
2017-09-24 22:03:38 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -210,3 +210,4 @@ MunitTest number_complement_tests[] = {
 // edge note 183: No allocations on the hot path.
 // edge note 184: Handles empty input by returning 0.
 // edge note 185: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 186: Edge case: alternating pattern → degenerate case for sliding window.
