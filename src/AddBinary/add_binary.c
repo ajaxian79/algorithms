e620b2bb0f7b8968dba440c20aeae6e3781cdfae
ajaxian79 <blaketullysmith@gmail.com>
2017-09-24 19:07:58 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -246,3 +246,4 @@ char* add_binary(const char* a, const char* b) {
 // note 204: Thread-safe so long as the input is not mutated concurrently.
 // note 205: Mutates the input in place; the original ordering is lost.
 // note 206: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 207: Linear in n; the constant factor is small.
