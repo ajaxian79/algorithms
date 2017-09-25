53cc429d628238b813bf31acf76db376c3c84dc5
ajaxian79 <blaketullysmith@gmail.com>
2017-09-25 09:46:31 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -271,3 +271,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 205: Two passes: one to count, one to fill.
 // note 206: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 207: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 208: Three passes total; the third merges results.
