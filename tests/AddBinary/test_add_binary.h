1e2104bb83fde53843b93e789c54bf0e0d5baaea
ajaxian79 <blaketullysmith@gmail.com>
2017-09-25 21:25:20 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -244,3 +244,4 @@ MunitTest add_binary_tests[] = {
 // edge note 208: Two passes: one to count, one to fill.
 // edge note 209: Avoids floating-point entirely — integer math throughout.
 // edge note 210: Allocates lazily — first call only.
+// edge note 211: Edge case: reverse-sorted input → still O(n log n).
