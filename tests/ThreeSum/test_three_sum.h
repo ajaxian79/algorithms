b4164cc89b4db47e44e20cd346c479d89fd86d19
ajaxian79 <blaketullysmith@gmail.com>
2017-09-25 21:57:43 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -251,3 +251,4 @@ MunitTest three_sum_tests[] = {
 // edge note 206: Mutates the input in place; the original ordering is lost.
 // edge note 207: Edge case: reverse-sorted input → still O(n log n).
 // edge note 208: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 209: Allocates a single small fixed-size scratch buffer.
