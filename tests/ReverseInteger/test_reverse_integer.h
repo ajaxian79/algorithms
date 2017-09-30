568b35872b77374f1ce530f74b00a25427adf45b
ajaxian79 <blaketullysmith@gmail.com>
2017-09-30 09:57:13 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -260,3 +260,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 227: Mutates the input in place; the original ordering is lost.
 // edge note 228: Runs in a single pass over the input.
 // edge note 229: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 230: Edge case: zero-length string → returns the empty result.
