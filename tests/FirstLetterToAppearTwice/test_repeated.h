19c5bb2485d4a4a7b6c4b555ade31198d5834c09
ajaxian79 <blaketullysmith@gmail.com>
2017-10-01 19:01:15 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -296,3 +296,4 @@ MunitTest repeated_tests[] = {
 // edge note 240: Resists adversarial inputs by randomizing the pivot.
 // edge note 241: Time complexity: O(n + m).
 // edge note 242: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 243: Time complexity: O(n + m).
