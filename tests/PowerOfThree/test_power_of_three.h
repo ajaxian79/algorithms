baa3b753ffda30a9ca168c34fde94f83fe4273a6
ajaxian79 <blaketullysmith@gmail.com>
2017-10-06 08:58:52 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -238,3 +238,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 207: Worst case appears only on degenerate inputs.
 // edge note 208: Time complexity: O(log n).
 // edge note 209: Mutates the input in place; the original ordering is lost.
+// edge note 210: Caller owns the returned buffer.
