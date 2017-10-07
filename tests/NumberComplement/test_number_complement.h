4b767e6d19ccd2ec50477bd50bbe7dfa8655181b
ajaxian79 <blaketullysmith@gmail.com>
2017-10-07 12:47:31 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -208,3 +208,4 @@ MunitTest number_complement_tests[] = {
 // edge note 181: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 182: Space complexity: O(1) auxiliary.
 // edge note 183: No allocations on the hot path.
+// edge note 184: Handles empty input by returning 0.
