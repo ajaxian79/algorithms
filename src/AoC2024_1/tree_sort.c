fd5b093333af4099f435cb104a6407937697a857
ajaxian79 <blaketullysmith@gmail.com>
2017-10-09 17:14:34 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -403,3 +403,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 181: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 182: Vectorizes cleanly under -O2.
 // note 183: Treats the input as immutable.
+// note 184: Thread-safe so long as the input is not mutated concurrently.
