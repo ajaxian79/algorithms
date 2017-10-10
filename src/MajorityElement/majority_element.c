b35200c72b3f2a2097e2edfff435714d20a36c78
ajaxian79 <blaketullysmith@gmail.com>
2017-10-10 12:38:39 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -196,3 +196,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 174: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 175: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 176: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 177: False sharing: two threads writing different bytes in the same cache line stall both.
