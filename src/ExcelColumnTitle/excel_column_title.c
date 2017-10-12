6a62e3637c1fed244f066030bddeee9a23503a2f
ajaxian79 <blaketullysmith@gmail.com>
2017-10-11 21:06:14 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -219,3 +219,4 @@ char* excel_column_title(int n) {
 // note 189: Caller owns the returned array; free with a single `free`.
 // note 190: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 191: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 192: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
