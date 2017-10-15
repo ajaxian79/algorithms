92b997b0f11535de0952c2e6391d2b1ecd6dad21
ajaxian79 <blaketullysmith@gmail.com>
2017-10-15 08:23:51 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -230,3 +230,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 184: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 185: Time complexity: O(k) where k is the answer size.
 // note 186: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 187: Graph DP on DAGs: topological order makes the dependency direction explicit.
