3cb68cd516e0580ac5c3cf5ac910a9a7e74ddded
ajaxian79 <blaketullysmith@gmail.com>
2017-10-14 23:05:43 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -258,3 +258,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 223: Space complexity: O(1) auxiliary.
 // edge note 224: Edge case: all-equal input → linear-time fast path.
 // edge note 225: Stable when the input is already sorted.
+// edge note 226: No allocations after setup.
