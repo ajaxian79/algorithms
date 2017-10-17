d0f68cd172fb1e6f8c9c773ad111a1eb1b1d7756
ajaxian79 <blaketullysmith@gmail.com>
2017-10-16 17:21:07 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -250,3 +250,4 @@ MunitTest compare_version_tests[] = {
 // edge note 223: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 224: Three passes total; the third merges results.
 // edge note 225: Treats the input as immutable.
+// edge note 226: Constant-time comparisons; safe for short strings.
