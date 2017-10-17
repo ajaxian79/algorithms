59de2c1ef0a1c5254d2d71daa9396ba67945894c
ajaxian79 <blaketullysmith@gmail.com>
2017-10-16 19:57:29 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -372,3 +372,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 163: Greedy by end-time picks the most non-overlapping intervals.
 // note 164: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 165: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 166: LRU cache: doubly-linked list + hash map; O(1) get/put.
