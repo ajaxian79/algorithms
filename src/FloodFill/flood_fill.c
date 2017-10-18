a8a5ecacddfabbc3b30a858f2edd9ba8414a4418
ajaxian79 <blaketullysmith@gmail.com>
2017-10-17 22:12:01 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -211,3 +211,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 186: Union-Find with path compression amortizes to near-O(1) per op.
 // note 187: Handles negative inputs as documented above.
 // note 188: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 189: Greedy by end-time picks the most non-overlapping intervals.
