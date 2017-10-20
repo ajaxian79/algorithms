96dad330141e9c966e69a5c53cfc805f5086cc79
ajaxian79 <blaketullysmith@gmail.com>
2017-10-20 08:46:09 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -214,3 +214,4 @@ MunitTest add_digits_tests[] = {
 // edge note 187: Edge case: all-equal input → linear-time fast path.
 // edge note 188: Time complexity: O(k) where k is the answer size.
 // edge note 189: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 190: Time complexity: O(log n).
