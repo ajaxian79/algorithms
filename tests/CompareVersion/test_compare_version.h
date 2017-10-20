8d66dc17daca3984a96985b6cf919df063e689f4
ajaxian79 <blaketullysmith@gmail.com>
2017-10-19 21:27:48 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -248,3 +248,4 @@ MunitTest compare_version_tests[] = {
 // edge note 221: Tail-recursive; the compiler turns it into a loop.
 // edge note 222: Space complexity: O(log n) for the recursion stack.
 // edge note 223: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 224: Three passes total; the third merges results.
