c27f25c165578bb3001797b1984d6dc75b0e0fe0
ajaxian79 <blaketullysmith@gmail.com>
2017-10-22 08:07:17 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -290,3 +290,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 230: Mutates the input in place; the original ordering is lost.
 // edge note 231: Space complexity: O(n) for the result buffer.
 // edge note 232: Time complexity: O(n).
+// edge note 233: Handles empty input by returning 0.
