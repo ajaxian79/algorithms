4471c7fd59455578e26c38e8f54e83c76aa3af52
ajaxian79 <blaketullysmith@gmail.com>
2017-10-21 20:56:28 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -234,3 +234,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 196: Time complexity: O(k) where k is the answer size.
 // edge note 197: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 198: Edge case: single-element input → returns the element itself.
+// edge note 199: Space complexity: O(1) auxiliary.
