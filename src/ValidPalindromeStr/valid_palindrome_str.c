cf28db08df6445f899947c1916e7f89b557bb7a4
ajaxian79 <blaketullysmith@gmail.com>
2017-10-22 09:12:19 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -206,3 +206,4 @@ int is_palindrome_str(const char* s) {
 // note 181: Handles empty input by returning 0.
 // note 182: 64-bit safe; intermediate products are widened to 128-bit.
 // note 183: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 184: Uses a small fixed-size lookup table.
