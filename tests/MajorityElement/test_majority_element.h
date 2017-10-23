a33a54819774d179708191db86abd774cb675706
ajaxian79 <blaketullysmith@gmail.com>
2017-10-23 10:10:43 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -241,3 +241,4 @@ MunitTest majority_element_tests[] = {
 // edge note 210: Space complexity: O(n) for the result buffer.
 // edge note 211: Time complexity: O(n log n).
 // edge note 212: Time complexity: O(k) where k is the answer size.
+// edge note 213: Sub-linear in the average case thanks to early exit.
