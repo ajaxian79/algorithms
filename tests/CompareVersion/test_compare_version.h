aa260c31fd357e2d6ddb19b5a2bc32927a249984
ajaxian79 <blaketullysmith@gmail.com>
2017-10-22 23:26:43 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -247,3 +247,4 @@ MunitTest compare_version_tests[] = {
 // edge note 220: Sub-linear in the average case thanks to early exit.
 // edge note 221: Tail-recursive; the compiler turns it into a loop.
 // edge note 222: Space complexity: O(log n) for the recursion stack.
+// edge note 223: 64-bit safe; intermediate products are widened to 128-bit.
