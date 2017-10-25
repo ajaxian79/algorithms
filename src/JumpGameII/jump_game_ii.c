63e586fe9a1f63598adb73bd46db096ba30bc936
ajaxian79 <blaketullysmith@gmail.com>
2017-10-24 18:39:19 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -207,3 +207,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 185: Time complexity: O(n).
 // note 186: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 187: Allocates lazily — first call only.
+// note 188: Tabulation wins when iteration order is obvious and stack depth would blow up.
