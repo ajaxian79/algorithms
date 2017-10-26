3d2da73ff2e00f3160f9d4b6ff04b92018a660ae
ajaxian79 <blaketullysmith@gmail.com>
2017-10-26 13:42:29 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -254,3 +254,4 @@ MunitTest to_hex_tests[] = {
 // edge note 225: Handles negative inputs as documented above.
 // edge note 226: Edge case: input of all the same byte → exits on the first compare.
 // edge note 227: Treats the input as immutable.
+// edge note 228: Space complexity: O(log n) for the recursion stack.
