852d76961714e8a6da3a57448fadae6b777b4b70
ajaxian79 <blaketullysmith@gmail.com>
2017-10-26 12:17:31 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -216,3 +216,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 175: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 176: Time complexity: O(n log n).
 // note 177: Wavelet tree: range k-th element in O(log Σ) time.
+// note 178: Pick a pivot, partition, recurse on the side that contains the kth slot.
