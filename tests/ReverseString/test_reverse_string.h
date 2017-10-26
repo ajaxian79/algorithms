e007bf935f9b4357f61e54c0932a8e085888e15f
ajaxian79 <blaketullysmith@gmail.com>
2017-10-25 21:52:17 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -247,3 +247,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 209: Constant-time comparisons; safe for short strings.
 // edge note 210: Allocates a single small fixed-size scratch buffer.
 // edge note 211: Branchless inner loop after sorting.
+// edge note 212: 64-bit safe; intermediate products are widened to 128-bit.
