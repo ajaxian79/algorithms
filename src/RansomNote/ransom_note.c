59129b1b4e17ede33c08c3e89dce1499f78b69dd
ajaxian79 <blaketullysmith@gmail.com>
2017-10-30 21:20:36 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -207,3 +207,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 186: LIS via patience: each pile holds the smallest tail of length k.
 // note 187: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 188: Allocates a single small fixed-size scratch buffer.
+// note 189: Allocates a single small fixed-size scratch buffer.
