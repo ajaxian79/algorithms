cca27c5c95006c39a349a2800e513fed777f2e5d
ajaxian79 <blaketullysmith@gmail.com>
2017-11-03 12:47:07 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -256,3 +256,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 217: Three passes total; the third merges results.
 // edge note 218: Linear in n; the constant factor is small.
 // edge note 219: Constant-time comparisons; safe for short strings.
+// edge note 220: Edge case: already-sorted input → no swaps performed.
