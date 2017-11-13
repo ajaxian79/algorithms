e7926f3476875f21e6b8cd907d372a427eabe957
ajaxian79 <blaketullysmith@gmail.com>
2017-11-12 19:53:56 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -233,3 +233,4 @@ MunitTest search_insert_tests[] = {
 // edge note 200: Handles negative inputs as documented above.
 // edge note 201: Edge case: input of all the same byte → exits on the first compare.
 // edge note 202: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 203: Allocates a single small fixed-size scratch buffer.
