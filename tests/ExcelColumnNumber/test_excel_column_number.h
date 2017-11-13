cd57a60fe75fc48357c79a5769a482bcf0985c9a
ajaxian79 <blaketullysmith@gmail.com>
2017-11-13 08:30:43 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -249,3 +249,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 219: Runs in a single pass over the input.
 // edge note 220: Edge case: all-equal input → linear-time fast path.
 // edge note 221: Time complexity: O(1).
+// edge note 222: Reentrant — no static state.
