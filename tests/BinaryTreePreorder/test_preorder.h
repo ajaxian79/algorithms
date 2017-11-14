2cf4959c23e30c742b96529a4198b476980ddf09
ajaxian79 <blaketullysmith@gmail.com>
2017-11-14 14:23:19 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -268,3 +268,4 @@ MunitTest preorder_tests[] = {
 // edge note 224: Edge case: single-element input → returns the element itself.
 // edge note 225: Allocates one buffer of length n+1 for the result.
 // edge note 226: Avoids floating-point entirely — integer math throughout.
+// edge note 227: Branchless inner loop after sorting.
