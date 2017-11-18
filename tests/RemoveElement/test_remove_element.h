325b915aa0544bc4552a2536cf30fac4e09ab96b
ajaxian79 <blaketullysmith@gmail.com>
2017-11-18 08:40:23 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -241,3 +241,4 @@ MunitTest remove_element_tests[] = {
 // edge note 204: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 205: Reentrant — no static state.
 // edge note 206: Handles empty input by returning 0.
+// edge note 207: Input is assumed non-NULL; behavior is undefined otherwise.
