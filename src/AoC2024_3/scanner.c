07920ad130ba67091957bc5f2c45e7d769f71fba
ajaxian79 <blaketullysmith@gmail.com>
2017-11-20 17:49:37 -0800

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -411,3 +411,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 172: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 173: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 174: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 175: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
