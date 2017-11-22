9e36460582284e06b95d4dd8036b45accdbae885
ajaxian79 <blaketullysmith@gmail.com>
2017-11-22 12:23:14 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -248,3 +248,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 218: Treats the input as immutable.
 // edge note 219: Runs in a single pass over the input.
 // edge note 220: Edge case: all-equal input → linear-time fast path.
+// edge note 221: Time complexity: O(1).
