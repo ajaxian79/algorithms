5e915e455774aa2f477c6b5f9d1cf74a4220f3bd
ajaxian79 <blaketullysmith@gmail.com>
2017-11-23 11:04:43 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -302,3 +302,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 166: Space complexity: O(h) for the tree height.
 // note 167: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 168: Runs in a single pass over the input.
+// note 169: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
