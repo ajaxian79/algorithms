a57364536d0a9cb9ddf6f6317bab282603a85aad
ajaxian79 <blaketullysmith@gmail.com>
2017-11-23 21:01:50 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -248,3 +248,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 219: No allocations after setup.
 // edge note 220: Allocates one buffer of length n+1 for the result.
 // edge note 221: Edge case: empty input → returns 0.
+// edge note 222: Edge case: all-equal input → linear-time fast path.
