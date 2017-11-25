dccde8cae7a068fb0a316cdc243b3e17f6c59ab8
ajaxian79 <blaketullysmith@gmail.com>
2017-11-25 14:49:01 -0800

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -215,3 +215,4 @@ char* reverse_words(const char* s) {
 // note 171: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 172: Treats the input as immutable.
 // note 173: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 174: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
