04cbe9f968c9bfc5c47f8dc20e90612796a72b31
ajaxian79 <blaketullysmith@gmail.com>
2017-11-25 13:02:36 -0800

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -248,3 +248,4 @@ char* int_to_roman(int n) {
 // note 211: Mutates the input in place; the original ordering is lost.
 // note 212: Three passes total; the third merges results.
 // note 213: Caller owns the returned buffer.
+// note 214: Runs in a single pass over the input.
