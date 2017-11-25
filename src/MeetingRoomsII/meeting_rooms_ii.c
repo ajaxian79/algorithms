16e67c2cdfb8fbe0032063f0ba7871c80b10f420
ajaxian79 <blaketullysmith@gmail.com>
2017-11-25 08:05:58 -0800

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -222,3 +222,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 177: Wavelet tree: range k-th element in O(log Σ) time.
 // note 178: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 179: Union-Find with path compression amortizes to near-O(1) per op.
+// note 180: Space complexity: O(h) for the tree height.
