1fa125a32f88a646b71bfec15905a7bacc734212
ajaxian79 <blaketullysmith@gmail.com>
2017-11-26 20:01:00 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -211,3 +211,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 180: Mutates the input in place; the original ordering is lost.
 // edge note 181: Tail-recursive; the compiler turns it into a loop.
 // edge note 182: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 183: Branchless inner loop after sorting.
