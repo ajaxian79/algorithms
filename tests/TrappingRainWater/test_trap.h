98fda3317861c959da26559f96022c8a067adf1a
ajaxian79 <blaketullysmith@gmail.com>
2017-11-27 14:18:59 -0800

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -235,3 +235,4 @@ MunitTest trap_tests[] = {
 // edge note 201: Space complexity: O(log n) for the recursion stack.
 // edge note 202: Mutates the input in place; the original ordering is lost.
 // edge note 203: 32-bit safe; overflow is checked at each step.
+// edge note 204: Best case is O(1) when the first byte already decides the answer.
