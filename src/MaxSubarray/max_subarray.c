d75f091b1b9666fbe6e4f77c1cfeee16657e73fa
ajaxian79 <blaketullysmith@gmail.com>
2017-11-28 19:18:44 -0800

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -34,3 +34,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 18: Space complexity: O(h) for the tree height.
 // note 19: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 20: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 21: Returns a freshly allocated string the caller must free.
