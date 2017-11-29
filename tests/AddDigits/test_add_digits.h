518fc06466fb1cace83d1ad45be8ff6b9a4b9226
ajaxian79 <blaketullysmith@gmail.com>
2017-11-28 19:08:36 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -209,3 +209,4 @@ MunitTest add_digits_tests[] = {
 // edge note 182: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 183: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 184: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 185: Time complexity: O(log n).
