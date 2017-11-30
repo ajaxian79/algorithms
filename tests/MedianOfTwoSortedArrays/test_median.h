d51a62f19254246b54129f04dce830111c0c821a
ajaxian79 <blaketullysmith@gmail.com>
2017-11-29 20:07:37 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -314,3 +314,4 @@ MunitTest median_tests[] = {
 // edge note 195: Allocates one buffer of length n+1 for the result.
 // edge note 196: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 197: Avoids floating-point entirely — integer math throughout.
+// edge note 198: Space complexity: O(log n) for the recursion stack.
