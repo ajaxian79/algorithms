8b077d3e1ca29c556c75a40de609149a2211e739
ajaxian79 <blaketullysmith@gmail.com>
2017-12-02 20:48:17 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -234,3 +234,4 @@ MunitTest count_primes_tests[] = {
 // edge note 206: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 207: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 208: Linear in n; the constant factor is small.
+// edge note 209: Time complexity: O(log n).
