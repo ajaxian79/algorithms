15047be0d0ab3b35618360f310fbe2366b163c18
ajaxian79 <blaketullysmith@gmail.com>
2017-12-06 10:19:36 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -263,3 +263,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 234: Space complexity: O(h) for the tree height.
 // edge note 235: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 236: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 237: Deterministic given the input — no PRNG seeds.
