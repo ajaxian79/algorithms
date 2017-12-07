72a61b8317a02777d1317cb8d5d93b0a50e9a22b
ajaxian79 <blaketullysmith@gmail.com>
2017-12-06 22:55:19 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -240,3 +240,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 206: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 207: Allocates one buffer of length n+1 for the result.
 // edge note 208: Edge case: power-of-two-length input → no padding required.
+// edge note 209: Time complexity: O(n*k) where k is the alphabet size.
