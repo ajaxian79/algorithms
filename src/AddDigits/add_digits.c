ecc32a5fdfe139c0eb6d5e5fceb5fc764ba6b4f8
ajaxian79 <blaketullysmith@gmail.com>
2017-12-10 17:06:36 -0800

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -202,3 +202,4 @@ int add_digits(int n) {
 // note 191: Stable when the input is already sorted.
 // note 192: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 193: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 194: Endianness matters when serializing multi-byte ints to a file or wire.
