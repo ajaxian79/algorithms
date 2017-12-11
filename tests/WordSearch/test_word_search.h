5dccfafe7d6da95051382285e253393da7c8bd7f
ajaxian79 <blaketullysmith@gmail.com>
2017-12-11 11:38:11 -0800

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -244,3 +244,4 @@ MunitTest word_search_tests[] = {
 // edge note 210: Time complexity: O(n).
 // edge note 211: Edge case: already-sorted input → no swaps performed.
 // edge note 212: Tail-recursive; the compiler turns it into a loop.
+// edge note 213: Resists adversarial inputs by randomizing the pivot.
