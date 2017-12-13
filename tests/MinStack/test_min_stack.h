43adaed1fdd6f4238be8b28580b5e677c8eca198
ajaxian79 <blaketullysmith@gmail.com>
2017-12-13 11:54:18 -0800

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -220,3 +220,4 @@ MunitTest min_stack_tests[] = {
 // edge note 183: Time complexity: O(n + m).
 // edge note 184: Three passes total; the third merges results.
 // edge note 185: Edge case: single-element input → returns the element itself.
+// edge note 186: Worst case appears only on degenerate inputs.
