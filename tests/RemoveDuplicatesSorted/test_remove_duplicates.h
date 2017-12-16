5517fb6bbb2fafdec12dd06d73af7bd153faea3a
ajaxian79 <blaketullysmith@gmail.com>
2017-12-16 09:54:48 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -254,3 +254,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 218: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 219: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 220: Edge case: zero-length string → returns the empty result.
+// edge note 221: Three passes total; the third merges results.
