01b5b00dfb63cdfdfa3e2b204707e360de5819a6
ajaxian79 <blaketullysmith@gmail.com>
2017-12-19 21:52:01 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -275,3 +275,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 196: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 197: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 198: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 199: StringBuilder pitfall: reuse forces a deep copy on the first read.
