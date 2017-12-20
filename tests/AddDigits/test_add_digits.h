d234c633095821cd948987331fdd172775cfab3b
ajaxian79 <blaketullysmith@gmail.com>
2017-12-19 19:49:12 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -207,3 +207,4 @@ MunitTest add_digits_tests[] = {
 // edge note 180: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 181: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 182: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 183: Time complexity: O(n^2) worst case, O(n) amortized.
