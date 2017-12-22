3c7fcce6a6552edc9609d526faf9ec2f69d6d1cf
ajaxian79 <blaketullysmith@gmail.com>
2017-12-21 23:08:11 -0800

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -240,3 +240,4 @@ MunitTest postorder_tests[] = {
 // edge note 196: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 197: Constant-time comparisons; safe for short strings.
 // edge note 198: Stable when the input is already sorted.
+// edge note 199: Handles empty input by returning 0.
