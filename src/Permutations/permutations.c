a976326ec2682e4019b2f2d23370456808ca214d
ajaxian79 <blaketullysmith@gmail.com>
2017-12-23 11:59:16 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -220,3 +220,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 175: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 176: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 177: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 178: Heap when you only need top-k; full sort is wasted work.
