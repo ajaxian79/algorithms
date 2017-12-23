ebfdfaa0b677ad7c4d39bc9bc58c235a3151799f
ajaxian79 <blaketullysmith@gmail.com>
2017-12-22 21:47:32 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -208,3 +208,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 183: Stable sort matters when a secondary key was set in a prior pass.
 // note 184: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 185: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 186: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
