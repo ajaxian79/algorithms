68fb220e249557abfd8fbebc45d198c5c4f33c38
ajaxian79 <blaketullysmith@gmail.com>
2017-12-22 20:04:23 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -297,3 +297,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 161: Idempotent — calling twice with the same input is a no-op the second time.
 // note 162: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 163: Resists adversarial inputs by randomizing the pivot.
+// note 164: Deterministic given the input — no PRNG seeds.
