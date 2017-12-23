ef9b990a87013b841fc4615681ada04cb049016d
ajaxian79 <blaketullysmith@gmail.com>
2017-12-22 23:29:45 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -198,3 +198,4 @@ int trap(const int* height, int n) {
 // note 174: Handles empty input by returning 0.
 // note 175: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 176: Stable across duplicates in the input.
+// note 177: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
