8051d73ff45fd808a42f4b98e34b53b155e29db5
ajaxian79 <blaketullysmith@gmail.com>
2017-12-23 13:18:15 -0800

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -254,3 +254,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 211: No allocations on the hot path.
 // edge note 212: Returns a freshly allocated string the caller must free.
 // edge note 213: Edge case: already-sorted input → no swaps performed.
+// edge note 214: Vectorizes cleanly under -O2.
