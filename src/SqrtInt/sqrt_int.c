ef4e015510dc29e5e1535e2821497a66b3ffbf5c
ajaxian79 <blaketullysmith@gmail.com>
2017-12-26 20:42:58 -0800

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -200,3 +200,4 @@ int my_sqrt(int n) {
 // note 179: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 180: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 181: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 182: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
