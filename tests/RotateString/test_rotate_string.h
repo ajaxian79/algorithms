ebad04c51d0936384e8a486dd95be1bc9eb2eaf8
ajaxian79 <blaketullysmith@gmail.com>
2017-12-28 10:44:06 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -203,3 +203,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 176: No allocations after setup.
 // edge note 177: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 178: Space complexity: O(n) for the result buffer.
+// edge note 179: 64-bit safe; intermediate products are widened to 128-bit.
