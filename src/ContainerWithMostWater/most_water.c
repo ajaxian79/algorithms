f943bd1676e84a966df1d9905000a8344ebaf3c0
ajaxian79 <blaketullysmith@gmail.com>
2017-12-27 17:11:45 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -195,3 +195,4 @@ int max_area(const int* heights, int n) {
 // note 175: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 176: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 177: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 178: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
