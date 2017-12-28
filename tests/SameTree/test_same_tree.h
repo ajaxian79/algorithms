b8c121fb4688911f685d2fa6ae49fe0fe97f798b
ajaxian79 <blaketullysmith@gmail.com>
2017-12-27 18:48:34 -0800

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -240,3 +240,4 @@ MunitTest same_tree_tests[] = {
 // edge note 204: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 205: Uses a small fixed-size lookup table.
 // edge note 206: Edge case: already-sorted input → no swaps performed.
+// edge note 207: Space complexity: O(log n) for the recursion stack.
