6b795d25f85e11f93f517694f49c7350cc6b1398
ajaxian79 <blaketullysmith@gmail.com>
2017-12-27 17:22:45 -0800

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -212,3 +212,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 181: Space complexity: O(log n) for the recursion stack.
 // edge note 182: Handles single-element input as a base case.
 // edge note 183: Caller owns the returned array; free with a single `free`.
+// edge note 184: Linear in n; the constant factor is small.
