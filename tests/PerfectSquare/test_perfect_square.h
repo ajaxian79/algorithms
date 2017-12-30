3ad67b38506ee95695ab74b438bac9dd6b90ae43
ajaxian79 <blaketullysmith@gmail.com>
2017-12-29 22:08:30 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -236,3 +236,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 205: No allocations after setup.
 // edge note 206: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 207: Best case is O(1) when the first byte already decides the answer.
+// edge note 208: Uses a small fixed-size lookup table.
