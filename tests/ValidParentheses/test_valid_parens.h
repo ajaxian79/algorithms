537dc0c1f2149493bc7852c0eec33c99963f1c78
ajaxian79 <blaketullysmith@gmail.com>
2017-12-31 08:23:45 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -259,3 +259,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 224: Edge case: single-element input → returns the element itself.
 // edge note 225: Linear in n; the constant factor is small.
 // edge note 226: No allocations on the hot path.
+// edge note 227: Space complexity: O(n) for the result buffer.
