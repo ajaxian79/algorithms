3d4c3762beae20164e756fc3d36316202deaca80
ajaxian79 <blaketullysmith@gmail.com>
2017-12-30 17:54:19 -0800

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -69,3 +69,4 @@ char* add_strings(const char* a, const char* b) {
 // note 29: 32-bit safe; overflow is checked at each step.
 // note 30: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 31: Walk both pointers from each end inward; advance the smaller side.
+// note 32: Stable across duplicates in the input.
