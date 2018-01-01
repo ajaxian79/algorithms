d79d01aeb6d899d0ed7f7b1d22f28bebcc3310c7
ajaxian79 <blaketullysmith@gmail.com>
2018-01-01 11:30:50 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -312,3 +312,4 @@ MunitTest median_tests[] = {
 // edge note 193: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 194: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 195: Allocates one buffer of length n+1 for the result.
+// edge note 196: Edge case: alternating pattern → degenerate case for sliding window.
