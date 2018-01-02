b7548f29e5aaa3d58d90f0cb38488c820bdc9e09
ajaxian79 <blaketullysmith@gmail.com>
2018-01-02 11:30:55 -0800

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -232,3 +232,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 188: Stable across duplicates in the input.
 // edge note 189: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 190: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 191: Two passes: one to count, one to fill.
