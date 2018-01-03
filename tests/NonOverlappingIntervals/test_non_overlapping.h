c51a293ae59596e0323864ddbd9936b59d81d526
ajaxian79 <blaketullysmith@gmail.com>
2018-01-02 20:47:07 -0800

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -231,3 +231,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 200: Constant-time comparisons; safe for short strings.
 // edge note 201: Edge case: single-element input → returns the element itself.
 // edge note 202: Vectorizes cleanly under -O2.
+// edge note 203: Space complexity: O(h) for the tree height.
