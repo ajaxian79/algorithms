cc16834748cbc06a34a0939712c4ae220ba45225
ajaxian79 <blaketullysmith@gmail.com>
2018-01-03 14:53:50 -0800

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -222,3 +222,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 195: No allocations after setup.
 // edge note 196: Tail-recursive; the compiler turns it into a loop.
 // edge note 197: Thread-safe so long as the input is not mutated concurrently.
+// edge note 198: Thread-safe so long as the input is not mutated concurrently.
