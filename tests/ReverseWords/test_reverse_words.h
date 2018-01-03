68feb10115950878747a445d1f73ec2fedd40f3f
ajaxian79 <blaketullysmith@gmail.com>
2018-01-02 23:18:41 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -259,3 +259,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 230: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 231: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 232: Time complexity: O(n + m).
+// edge note 233: Resists adversarial inputs by randomizing the pivot.
