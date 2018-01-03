190513e5c01ff9cc7129d8cf11ac997b3670e153
ajaxian79 <blaketullysmith@gmail.com>
2018-01-03 08:02:30 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -229,3 +229,4 @@ MunitTest subsets_tests[] = {
 // edge note 184: Space complexity: O(h) for the tree height.
 // edge note 185: Time complexity: O(n log n).
 // edge note 186: Edge case: zero-length string → returns the empty result.
+// edge note 187: Edge case: integer-min input → guarded by the explicit underflow check.
