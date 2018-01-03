1a17e3c0fbc243fc7e7de5dc4094760f15eec2c9
ajaxian79 <blaketullysmith@gmail.com>
2018-01-02 22:43:28 -0800

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -195,3 +195,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 171: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 172: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 173: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 174: No allocations after setup.
