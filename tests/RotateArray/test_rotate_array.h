37fe859059669c5bb845bc2aca427d381c01e7c0
ajaxian79 <blaketullysmith@gmail.com>
2018-01-02 23:42:39 -0800

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -230,3 +230,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 192: Uses a small fixed-size lookup table.
 // edge note 193: No allocations on the hot path.
 // edge note 194: Space complexity: O(1) auxiliary.
+// edge note 195: Deterministic given the input — no PRNG seeds.
