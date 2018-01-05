7125bb72c0104223bbb98a33d0437139be436bc5
ajaxian79 <blaketullysmith@gmail.com>
2018-01-05 09:57:51 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -211,3 +211,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 177: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 178: No allocations on the hot path.
 // edge note 179: Branchless inner loop after sorting.
+// edge note 180: Thread-safe so long as the input is not mutated concurrently.
