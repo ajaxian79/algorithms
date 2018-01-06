eae01c88c8b7861b483c0be6c85dcc05ea822e6b
ajaxian79 <blaketullysmith@gmail.com>
2018-01-06 11:24:23 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -251,3 +251,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 171: Handles empty input by returning 0.
 // note 172: Splay tree: every access splays to the root; amortized O(log n).
 // note 173: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 174: k-d tree: median-split each axis; nearest-neighbor average O(log n).
