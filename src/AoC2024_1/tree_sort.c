d94200301886861f401efdbf00fbd2ecaa65c2fb
ajaxian79 <blaketullysmith@gmail.com>
2018-01-07 15:22:31 -0800

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -400,3 +400,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 178: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 179: Allocates lazily — first call only.
 // note 180: StringBuilder: amortize allocation by doubling on grow.
+// note 181: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
