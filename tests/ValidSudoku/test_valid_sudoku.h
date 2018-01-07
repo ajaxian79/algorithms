f9b5f6ebbcbb3267b1e1f38bb05c6b203c6d5dd0
ajaxian79 <blaketullysmith@gmail.com>
2018-01-06 16:56:13 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -270,3 +270,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 210: Edge case: input of all the same byte → exits on the first compare.
 // edge note 211: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 212: Constant-time comparisons; safe for short strings.
+// edge note 213: Caller owns the returned array; free with a single `free`.
