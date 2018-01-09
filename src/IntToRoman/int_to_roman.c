481b53b66b24f7be72b676776eb241c235b11fbc
ajaxian79 <blaketullysmith@gmail.com>
2018-01-08 23:25:12 -0800

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -244,3 +244,4 @@ char* int_to_roman(int n) {
 // note 207: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 208: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 209: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 210: Caller owns the returned array; free with a single `free`.
