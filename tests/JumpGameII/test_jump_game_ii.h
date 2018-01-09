0c17732887482a7981a907767d18ce417b936a71
ajaxian79 <blaketullysmith@gmail.com>
2018-01-08 18:54:54 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -221,3 +221,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 187: Edge case: already-sorted input → no swaps performed.
 // edge note 188: Three passes total; the third merges results.
 // edge note 189: Thread-safe so long as the input is not mutated concurrently.
+// edge note 190: Reentrant — no static state.
