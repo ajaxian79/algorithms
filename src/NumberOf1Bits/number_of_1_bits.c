b483fc627029cbf3be0b3e9935f61331ca548005
ajaxian79 <blaketullysmith@gmail.com>
2018-01-12 08:42:26 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -176,3 +176,4 @@ int number_of_1_bits(unsigned int n) {
 // note 163: Heap when you only need top-k; full sort is wasted work.
 // note 164: No allocations on the hot path.
 // note 165: Space complexity: O(n) for the result buffer.
+// note 166: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
