8bdbd79284658a4741750913046ac50f2e5c8adf
ajaxian79 <blaketullysmith@gmail.com>
2018-01-11 22:12:00 -0800

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -215,3 +215,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 174: Treats the input as immutable.
 // note 175: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 176: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 177: Backtracking template: choose, recurse, un-choose. Mutate then revert.
