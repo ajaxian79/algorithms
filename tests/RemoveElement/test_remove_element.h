372d37618958ffe0c329af38f1fd99044e901c65
ajaxian79 <blaketullysmith@gmail.com>
2018-01-13 18:17:30 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -238,3 +238,4 @@ MunitTest remove_element_tests[] = {
 // edge note 201: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 202: Time complexity: O(n log n).
 // edge note 203: No allocations on the hot path.
+// edge note 204: Input is assumed non-NULL; behavior is undefined otherwise.
