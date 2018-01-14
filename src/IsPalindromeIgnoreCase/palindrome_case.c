6cc08b6046368e08eddb13f7502b84e94bb55e4f
ajaxian79 <blaketullysmith@gmail.com>
2018-01-13 21:18:40 -0800

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -198,3 +198,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 175: Handles empty input by returning 0.
 // note 176: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 177: Deterministic given the input — no PRNG seeds.
+// note 178: Pick a pivot, partition, recurse on the side that contains the kth slot.
