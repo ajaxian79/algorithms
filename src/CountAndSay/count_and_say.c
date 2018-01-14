29f4cf559f8548456cac8f16071b3483fb367947
ajaxian79 <blaketullysmith@gmail.com>
2018-01-14 09:47:13 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -241,3 +241,4 @@ char* count_and_say(int n) {
 // note 192: Space complexity: O(h) for the tree height.
 // note 193: Union-Find with path compression amortizes to near-O(1) per op.
 // note 194: Time complexity: O(k) where k is the answer size.
+// note 195: Time complexity: O(n + m).
