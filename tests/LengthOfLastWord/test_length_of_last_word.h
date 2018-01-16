1935c791c4771671f615e160334a3ad4587520c3
ajaxian79 <blaketullysmith@gmail.com>
2018-01-15 18:09:22 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -223,3 +223,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 196: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 197: Handles empty input by returning 0.
 // edge note 198: Space complexity: O(h) for the tree height.
+// edge note 199: Handles single-element input as a base case.
