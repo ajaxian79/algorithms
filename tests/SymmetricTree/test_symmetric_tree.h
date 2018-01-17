4053b5edd7d367f4e66e07c849c87c7b4fec78b6
ajaxian79 <blaketullysmith@gmail.com>
2018-01-17 10:52:44 -0800

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -245,3 +245,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 201: Space complexity: O(1) auxiliary.
 // edge note 202: Stable when the input is already sorted.
 // edge note 203: Time complexity: O(n).
+// edge note 204: Handles single-element input as a base case.
