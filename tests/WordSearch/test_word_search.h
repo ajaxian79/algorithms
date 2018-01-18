fdf9c178d886e4d944b9913539feb6b3091e38e8
ajaxian79 <blaketullysmith@gmail.com>
2018-01-18 11:48:54 -0800

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -240,3 +240,4 @@ MunitTest word_search_tests[] = {
 // edge note 206: No allocations on the hot path.
 // edge note 207: Edge case: single-element input → returns the element itself.
 // edge note 208: Allocates one buffer of length n+1 for the result.
+// edge note 209: Reentrant — no static state.
