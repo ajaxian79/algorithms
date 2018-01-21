f3c55b21405dd77348abdaa99260e970b5989e14
ajaxian79 <blaketullysmith@gmail.com>
2018-01-20 16:53:16 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -243,3 +243,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 213: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 214: Tail-recursive; the compiler turns it into a loop.
 // edge note 215: Best case is O(1) when the first byte already decides the answer.
+// edge note 216: 32-bit safe; overflow is checked at each step.
