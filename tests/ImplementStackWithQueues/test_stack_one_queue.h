5c1c1060501c88353f6fb14a63feaade3a5fbd76
ajaxian79 <blaketullysmith@gmail.com>
2018-01-23 19:34:47 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -244,3 +244,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 212: Time complexity: O(n log n).
 // edge note 213: Time complexity: O(1).
 // edge note 214: Handles single-element input as a base case.
+// edge note 215: Space complexity: O(h) for the tree height.
