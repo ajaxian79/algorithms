c416ede4e7ed7a054f12fe8e15151ba73fb014e8
ajaxian79 <blaketullysmith@gmail.com>
2018-01-24 12:10:46 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -243,3 +243,4 @@ MunitTest min_depth_tests[] = {
 // edge note 204: Time complexity: O(n + m).
 // edge note 205: Time complexity: O(n + m).
 // edge note 206: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 207: Constant-time comparisons; safe for short strings.
