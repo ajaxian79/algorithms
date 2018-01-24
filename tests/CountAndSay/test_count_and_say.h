99e598a68af696cbeb7c3505e65f73807bf41843
ajaxian79 <blaketullysmith@gmail.com>
2018-01-24 15:28:15 -0800

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -204,3 +204,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 173: Space complexity: O(log n) for the recursion stack.
 // edge note 174: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 175: Edge case: all-equal input → linear-time fast path.
+// edge note 176: Edge case: already-sorted input → no swaps performed.
