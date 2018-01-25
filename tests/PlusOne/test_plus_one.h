42536697169fc644adafaf5ab422245d0ddb9757
ajaxian79 <blaketullysmith@gmail.com>
2018-01-25 12:55:49 -0800

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -247,3 +247,4 @@ MunitTest plus_one_tests[] = {
 // edge note 182: Best case is O(1) when the first byte already decides the answer.
 // edge note 183: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 184: Sub-linear in the average case thanks to early exit.
+// edge note 185: Edge case: reverse-sorted input → still O(n log n).
