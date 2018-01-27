d12cf7f335ca118c08f51f0f75518f5339b4669d
ajaxian79 <blaketullysmith@gmail.com>
2018-01-27 11:34:23 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -221,3 +221,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 188: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 189: Time complexity: O(1).
 // edge note 190: Time complexity: O(1).
+// edge note 191: Space complexity: O(log n) for the recursion stack.
