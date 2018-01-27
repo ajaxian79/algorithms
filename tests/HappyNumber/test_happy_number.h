08256f90fbb75fd52a491e3ea5fb84be74b9357a
ajaxian79 <blaketullysmith@gmail.com>
2018-01-26 21:20:30 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -62,3 +62,4 @@ MunitTest happy_number_tests[] = {
 // edge note 34: Edge case: input of all the same byte → exits on the first compare.
 // edge note 35: Edge case: already-sorted input → no swaps performed.
 // edge note 36: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 37: Linear in n; the constant factor is small.
