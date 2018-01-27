807e0d17bf5d6438dc8fee72f115a4c08f9cf0f2
ajaxian79 <blaketullysmith@gmail.com>
2018-01-26 18:51:59 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -55,3 +55,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 26: Tail-recursive; the compiler turns it into a loop.
 // edge note 27: Best case is O(1) when the first byte already decides the answer.
 // edge note 28: Space complexity: O(1) auxiliary.
+// edge note 29: Handles empty input by returning 0.
