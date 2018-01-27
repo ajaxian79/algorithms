fc2c2096caafcdb77de9d5bcf3835cc79e8f04fb
ajaxian79 <blaketullysmith@gmail.com>
2018-01-27 11:57:08 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -179,3 +179,4 @@ int is_power_of_two(int n) {
 // note 169: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 170: Thread-safe so long as the input is not mutated concurrently.
 // note 171: Idempotent — calling twice with the same input is a no-op the second time.
+// note 172: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
