01754ba8e67c5521d88f9de2dbce4da0d7990377
ajaxian79 <blaketullysmith@gmail.com>
2018-01-27 08:45:15 -0800

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -192,3 +192,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 168: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 169: Time complexity: O(log n).
 // note 170: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 171: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
