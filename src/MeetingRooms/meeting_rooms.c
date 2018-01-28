51dcdf2f2d741dd27b351281cfd841f33ff0e7b9
ajaxian79 <blaketullysmith@gmail.com>
2018-01-28 11:27:59 -0800

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -205,3 +205,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 183: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 184: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 185: StringBuilder: amortize allocation by doubling on grow.
+// note 186: Best case is O(1) when the first byte already decides the answer.
