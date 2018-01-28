52119a2ab30e1679e8c05594eb015acf5bbaa36b
ajaxian79 <blaketullysmith@gmail.com>
2018-01-27 22:31:38 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -246,3 +246,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 210: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 211: Allocates a single small fixed-size scratch buffer.
 // edge note 212: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 213: Handles empty input by returning 0.
