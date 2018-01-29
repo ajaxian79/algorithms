f8a859be3cd9ba2055418d4f3904f92ec4e41060
ajaxian79 <blaketullysmith@gmail.com>
2018-01-28 17:39:31 -0800

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -196,3 +196,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 178: Best case is O(1) when the first byte already decides the answer.
 // note 179: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 180: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 181: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
