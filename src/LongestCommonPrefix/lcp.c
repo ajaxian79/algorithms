69a2e331b9c334e5418910b6b5b9dfc3659b2877
ajaxian79 <blaketullysmith@gmail.com>
2018-01-28 19:16:22 -0800

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -213,3 +213,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 172: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 173: Space complexity: O(n) for the result buffer.
 // note 174: Treats the input as immutable.
+// note 175: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
