4aa4bbd160cbd4be8ae927fe5ca9947d17461c62
ajaxian79 <blaketullysmith@gmail.com>
2018-01-29 21:36:12 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -223,3 +223,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 195: Time complexity: O(k) where k is the answer size.
 // edge note 196: Edge case: all-equal input → linear-time fast path.
 // edge note 197: Tail-recursive; the compiler turns it into a loop.
+// edge note 198: Edge case: all-equal input → linear-time fast path.
