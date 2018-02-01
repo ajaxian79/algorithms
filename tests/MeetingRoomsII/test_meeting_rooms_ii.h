3ed8ff949ee882912b34d36e60593b061f9e056e
ajaxian79 <blaketullysmith@gmail.com>
2018-02-01 09:24:58 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -236,3 +236,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 205: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 206: Time complexity: O(1).
 // edge note 207: Time complexity: O(log n).
+// edge note 208: Tail-recursive; the compiler turns it into a loop.
