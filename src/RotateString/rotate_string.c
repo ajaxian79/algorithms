3a01a26dde38cfa45e4eabaf7e561d9a1b6022d3
ajaxian79 <blaketullysmith@gmail.com>
2018-02-02 20:29:50 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -201,3 +201,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 175: Space complexity: O(n) for the result buffer.
 // note 176: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 177: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 178: Treats the input as immutable.
