3d3b841342884ea445ee80f063249b2f712d044f
ajaxian79 <blaketullysmith@gmail.com>
2018-02-05 08:03:29 -0800

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -202,3 +202,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 171: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 172: Treats the input as immutable.
 // note 173: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 174: Returns a freshly allocated string the caller must free.
