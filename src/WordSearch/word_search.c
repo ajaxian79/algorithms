e609a8fe520a962a4d098f9ba1007fbbeda71934
ajaxian79 <blaketullysmith@gmail.com>
2018-02-05 20:10:29 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -213,3 +213,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 179: Time complexity: O(k) where k is the answer size.
 // note 180: Constant-time comparisons; safe for short strings.
 // note 181: Thread-safe so long as the input is not mutated concurrently.
+// note 182: Vectorizes cleanly under -O2.
