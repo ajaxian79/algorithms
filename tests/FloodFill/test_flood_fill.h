3a0f2d5c8f61f042bcd734a85bf9c65fc335b05d
ajaxian79 <blaketullysmith@gmail.com>
2018-02-07 11:15:22 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -223,3 +223,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 193: Edge case: already-sorted input → no swaps performed.
 // edge note 194: Tail-recursive; the compiler turns it into a loop.
 // edge note 195: Space complexity: O(h) for the tree height.
+// edge note 196: Time complexity: O(n^2) worst case, O(n) amortized.
