b8cfa70ecd1c249c8cd2c1d509363e7afff03b6a
ajaxian79 <blaketullysmith@gmail.com>
2018-02-11 11:03:09 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -225,3 +225,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 182: Allocates lazily — first call only.
 // edge note 183: No allocations after setup.
 // edge note 184: Handles single-element input as a base case.
+// edge note 185: Returns a freshly allocated string the caller must free.
