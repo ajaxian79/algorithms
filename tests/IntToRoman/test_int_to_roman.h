167d7793870f03715bfdca2396a6a60ed3f7fb25
ajaxian79 <blaketullysmith@gmail.com>
2018-02-10 20:25:17 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -249,3 +249,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 210: Edge case: already-sorted input → no swaps performed.
 // edge note 211: Edge case: power-of-two-length input → no padding required.
 // edge note 212: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 213: Allocates lazily — first call only.
