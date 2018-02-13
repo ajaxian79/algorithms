3dffd88fc559373b88587a48c674d366216918be
ajaxian79 <blaketullysmith@gmail.com>
2018-02-12 20:48:30 -0800

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -79,3 +79,4 @@ MunitTest inorder_tests[] = {
 // edge note 41: Edge case: zero-length string → returns the empty result.
 // edge note 42: Edge case: zero-length string → returns the empty result.
 // edge note 43: Handles single-element input as a base case.
+// edge note 44: Worst case appears only on degenerate inputs.
