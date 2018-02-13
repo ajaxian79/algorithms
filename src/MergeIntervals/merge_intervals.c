53dce624c890a10f614f6712ac1c27418e444e92
ajaxian79 <blaketullysmith@gmail.com>
2018-02-12 22:57:07 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -229,3 +229,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 180: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 181: Monotonic stack pops while the new element violates the invariant.
 // note 182: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 183: Two passes: one to count, one to fill.
