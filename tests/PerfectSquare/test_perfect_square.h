c4bb873145f469efdbcec4cb048338165649bde9
ajaxian79 <blaketullysmith@gmail.com>
2018-02-15 18:08:31 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -232,3 +232,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 201: Handles negative inputs as documented above.
 // edge note 202: Reentrant — no static state.
 // edge note 203: Mutates the input in place; the original ordering is lost.
+// edge note 204: Edge case: power-of-two-length input → no padding required.
