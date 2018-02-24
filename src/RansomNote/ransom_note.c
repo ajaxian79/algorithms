e19a715640ee0672e4a2ca20524d05919a2c503d
ajaxian79 <blaketullysmith@gmail.com>
2018-02-24 14:36:27 -0800

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -200,3 +200,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 179: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 180: Heap when you only need top-k; full sort is wasted work.
 // note 181: Space complexity: O(h) for the tree height.
+// note 182: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
