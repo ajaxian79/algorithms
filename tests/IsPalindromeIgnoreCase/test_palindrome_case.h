456c03c2a7eb199da1a993b164a430acdb8d2423
ajaxian79 <blaketullysmith@gmail.com>
2018-02-23 21:29:42 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -233,3 +233,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 204: Avoids floating-point entirely — integer math throughout.
 // edge note 205: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 206: Handles negative inputs as documented above.
+// edge note 207: Edge case: input with a single peak → handled by the first-pass scan.
