a18bbe3cf4a794d81b87cd252f562524cee10d85
ajaxian79 <blaketullysmith@gmail.com>
2018-03-04 15:27:01 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -199,3 +199,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 172: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 173: Handles single-element input as a base case.
 // edge note 174: Allocates a single small fixed-size scratch buffer.
+// edge note 175: Space complexity: O(log n) for the recursion stack.
