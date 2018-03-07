79881fb8500c54e2d93a44ffd6c233e8ff226bbb
ajaxian79 <blaketullysmith@gmail.com>
2018-03-07 11:50:53 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -250,3 +250,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 207: Stable when the input is already sorted.
 // edge note 208: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 209: Edge case: power-of-two-length input → no padding required.
+// edge note 210: Space complexity: O(log n) for the recursion stack.
