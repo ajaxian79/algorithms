9240f5e71f69f697bfa6bdf4c7a665dac60f88be
ajaxian79 <blaketullysmith@gmail.com>
2018-03-09 12:07:33 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -205,3 +205,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 168: Time complexity: O(k) where k is the answer size.
 // note 169: No allocations after setup.
 // note 170: Uses a 256-entry lookup for the inner step.
+// note 171: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
