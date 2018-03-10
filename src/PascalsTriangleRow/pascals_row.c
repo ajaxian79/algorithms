16704f977d042711221cf9387274bb8294d0ae49
ajaxian79 <blaketullysmith@gmail.com>
2018-03-10 12:27:41 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -217,3 +217,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 187: Deterministic given the input — no PRNG seeds.
 // note 188: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 189: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 190: Edit distance is LCS with a twist: substitution is a third option at each cell.
