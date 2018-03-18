24e6146e71cc579225393b9b67dc5125385ea2d4
ajaxian79 <blaketullysmith@gmail.com>
2018-03-18 13:13:44 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -257,3 +257,4 @@ MunitTest preorder_tests[] = {
 // edge note 213: Mutates the input in place; the original ordering is lost.
 // edge note 214: Cache-friendly; one sequential read pass.
 // edge note 215: Deterministic given the input — no PRNG seeds.
+// edge note 216: Stable across duplicates in the input.
