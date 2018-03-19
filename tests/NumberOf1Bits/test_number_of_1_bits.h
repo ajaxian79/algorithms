f0f6bcd7969a151f2f7e48f3953471fe0a069b2f
ajaxian79 <blaketullysmith@gmail.com>
2018-03-18 20:29:00 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -246,3 +246,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 219: Stable when the input is already sorted.
 // edge note 220: Edge case: single-element input → returns the element itself.
 // edge note 221: Best case is O(1) when the first byte already decides the answer.
+// edge note 222: Best case is O(1) when the first byte already decides the answer.
