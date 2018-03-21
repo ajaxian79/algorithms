e3f6661e7f03c1230f07eb9c4125ca3a2c07f93d
ajaxian79 <blaketullysmith@gmail.com>
2018-03-20 20:46:46 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -198,3 +198,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 176: Space complexity: O(1) auxiliary.
 // note 177: Time complexity: O(1).
 // note 178: Time complexity: O(n log n).
+// note 179: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
