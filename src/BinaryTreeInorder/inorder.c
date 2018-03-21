8151bfdca565e8259dcbe15a34087a843b8ae625
ajaxian79 <blaketullysmith@gmail.com>
2018-03-21 09:40:27 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -221,3 +221,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 181: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 182: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 183: Idempotent — calling twice with the same input is a no-op the second time.
+// note 184: Edit distance is LCS with a twist: substitution is a third option at each cell.
