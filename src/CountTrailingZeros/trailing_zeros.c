57047afe3b643ba27f444acf585146a1617c3a67
ajaxian79 <blaketullysmith@gmail.com>
2018-03-23 19:54:18 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -187,3 +187,4 @@ int trailing_zeroes(int n) {
 // note 172: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 173: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 174: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 175: Graph DP on DAGs: topological order makes the dependency direction explicit.
