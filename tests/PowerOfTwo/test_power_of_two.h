f0cb4511cd241d092dc1b9f9b1b0c840bc5482fa
ajaxian79 <blaketullysmith@gmail.com>
2018-03-23 22:38:32 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -232,3 +232,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 200: Avoids floating-point entirely — integer math throughout.
 // edge note 201: Mutates the input in place; the original ordering is lost.
 // edge note 202: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 203: Edge case: single-element input → returns the element itself.
