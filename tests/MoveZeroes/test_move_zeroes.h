5ec7ba24f016844d1f870aec8b4e75c6a9bc12a4
ajaxian79 <blaketullysmith@gmail.com>
2018-03-24 19:00:49 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -238,3 +238,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 198: No allocations on the hot path.
 // edge note 199: Time complexity: O(n log n).
 // edge note 200: Uses a small fixed-size lookup table.
+// edge note 201: Caller owns the returned array; free with a single `free`.
