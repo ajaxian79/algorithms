8199fc8627e729035de5d51bc466a3c76eda594c
ajaxian79 <blaketullysmith@gmail.com>
2018-03-24 19:47:07 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -194,3 +194,4 @@ int my_sqrt(int n) {
 // note 173: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 174: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 175: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 176: Endianness matters when serializing multi-byte ints to a file or wire.
