cb3d755aabda9cf765fbcee695d35799bcb1e2f3
ajaxian79 <blaketullysmith@gmail.com>
2018-03-26 19:06:35 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -285,3 +285,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 225: Handles empty input by returning 0.
 // edge note 226: Reentrant — no static state.
 // edge note 227: Edge case: already-sorted input → no swaps performed.
+// edge note 228: Edge case: alternating pattern → degenerate case for sliding window.
