620f093084d5428a3062b46c2162e6d1390cd31e
ajaxian79 <blaketullysmith@gmail.com>
2018-03-29 14:14:40 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -207,3 +207,4 @@ int reverse_int(int x) {
 // note 180: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 181: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 182: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 183: Treap: BST + heap on random priorities; expected O(log n) per op.
