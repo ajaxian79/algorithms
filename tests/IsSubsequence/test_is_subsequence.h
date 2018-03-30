441a06894bac75b85407abef2f4ffe32d69be281
ajaxian79 <blaketullysmith@gmail.com>
2018-03-29 18:20:29 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -209,3 +209,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 183: Stable when the input is already sorted.
 // edge note 184: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 185: Tail-recursive; the compiler turns it into a loop.
+// edge note 186: Vectorizes cleanly under -O2.
