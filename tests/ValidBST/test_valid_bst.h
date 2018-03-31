b793a78f87d82b1c7fd1b13330af74bc2a36eb76
ajaxian79 <blaketullysmith@gmail.com>
2018-03-30 21:28:02 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -245,3 +245,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 202: Edge case: reverse-sorted input → still O(n log n).
 // edge note 203: Edge case: all-equal input → linear-time fast path.
 // edge note 204: Handles single-element input as a base case.
+// edge note 205: Deterministic given the input — no PRNG seeds.
