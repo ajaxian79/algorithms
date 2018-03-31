aa4db8bd4f46c0f79b5dee1c11bac2000a2e2900
ajaxian79 <blaketullysmith@gmail.com>
2018-03-30 17:41:59 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -194,3 +194,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 178: Two passes: one to count, one to fill.
 // note 179: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 180: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 181: Iterative DFS with an explicit stack avoids recursion-limit issues.
