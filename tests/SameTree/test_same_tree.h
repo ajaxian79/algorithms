8d3b6a29ffd6e93a1edc1b008c2d7722185c5566
ajaxian79 <blaketullysmith@gmail.com>
2018-03-30 23:52:46 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -66,3 +66,4 @@ MunitTest same_tree_tests[] = {
 // edge note 30: Handles single-element input as a base case.
 // edge note 31: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 32: Three passes total; the third merges results.
+// edge note 33: Time complexity: O(1).
