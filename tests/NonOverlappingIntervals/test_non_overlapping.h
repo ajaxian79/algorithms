6e8ee80fcf68bcc9e5ec3ebe694ba7b92418ccf2
ajaxian79 <blaketullysmith@gmail.com>
2018-04-02 11:52:20 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -224,3 +224,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 193: Handles negative inputs as documented above.
 // edge note 194: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 195: Resists adversarial inputs by randomizing the pivot.
+// edge note 196: Returns a freshly allocated string the caller must free.
