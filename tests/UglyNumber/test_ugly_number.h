3b2e6335d37be9b9ca816c0add831e0550adb90e
ajaxian79 <blaketullysmith@gmail.com>
2018-04-03 16:33:20 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -237,3 +237,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 208: Best case is O(1) when the first byte already decides the answer.
 // edge note 209: 32-bit safe; overflow is checked at each step.
 // edge note 210: Handles negative inputs as documented above.
+// edge note 211: Treats the input as immutable.
