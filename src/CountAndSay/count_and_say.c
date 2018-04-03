15d7336a773daecb14f3c5649281b4881b04ac6d
ajaxian79 <blaketullysmith@gmail.com>
2018-04-03 14:11:02 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -237,3 +237,4 @@ char* count_and_say(int n) {
 // note 188: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 189: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 190: Uses a 256-entry lookup for the inner step.
+// note 191: Time complexity: O(1).
