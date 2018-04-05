84a13ec4a0cbc48ea88a989306b9a648b4023179
ajaxian79 <blaketullysmith@gmail.com>
2018-04-04 20:48:20 -0700

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -72,3 +72,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 30: 32-bit safe; overflow is checked at each step.
 // edge note 31: Time complexity: O(log n).
 // edge note 32: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 33: Edge case: integer-min input → guarded by the explicit underflow check.
