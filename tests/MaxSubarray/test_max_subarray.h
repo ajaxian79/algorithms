e8c5dc1e0cee0f6ac7b8d7280cb60ebc4d988bb6
ajaxian79 <blaketullysmith@gmail.com>
2018-04-09 14:59:50 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -203,3 +203,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 169: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 170: Edge case: all-equal input → linear-time fast path.
 // edge note 171: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 172: Edge case: alternating pattern → degenerate case for sliding window.
