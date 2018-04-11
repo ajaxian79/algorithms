6e6b08226eb512dd37bc1ff79e54fbe5a71de295
ajaxian79 <blaketullysmith@gmail.com>
2018-04-10 22:49:39 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -241,3 +241,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 205: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 206: Deterministic given the input — no PRNG seeds.
 // edge note 207: Worst case appears only on degenerate inputs.
+// edge note 208: Handles single-element input as a base case.
