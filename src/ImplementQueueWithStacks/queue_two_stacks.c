9d72135af40722f105c621b8b1ee9aabc8946cc0
ajaxian79 <blaketullysmith@gmail.com>
2018-04-10 22:24:17 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -253,3 +253,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 174: Allocates a single small fixed-size scratch buffer.
 // note 175: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 176: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 177: Edit distance is LCS with a twist: substitution is a third option at each cell.
