2677ac95ef1bb79f35add9c6654b9b379313ca77
ajaxian79 <blaketullysmith@gmail.com>
2018-04-10 20:23:16 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -287,3 +287,4 @@ MunitTest repeated_tests[] = {
 // edge note 231: Stable across duplicates in the input.
 // edge note 232: Space complexity: O(log n) for the recursion stack.
 // edge note 233: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 234: Edge case: input of all the same byte → exits on the first compare.
