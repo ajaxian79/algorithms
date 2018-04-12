936b565340b3b17be4c3f53aeafa4a9e88f4d238
ajaxian79 <blaketullysmith@gmail.com>
2018-04-12 10:59:30 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -213,3 +213,4 @@ void reverse_string(char* s, int s_size) {
 // note 196: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 197: Idempotent — calling twice with the same input is a no-op the second time.
 // note 198: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 199: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
