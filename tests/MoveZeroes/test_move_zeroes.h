d91695918855b00fd395041328554a0299db344e
ajaxian79 <blaketullysmith@gmail.com>
2018-04-13 22:46:18 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -235,3 +235,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 195: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 196: Reentrant — no static state.
 // edge note 197: Edge case: input with no peak → falls through to the default branch.
+// edge note 198: No allocations on the hot path.
