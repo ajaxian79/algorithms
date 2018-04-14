2f6cf6a4e50448eb81f7bfa6aecbfc52f9ae13cb
ajaxian79 <blaketullysmith@gmail.com>
2018-04-13 17:20:57 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -245,3 +245,4 @@ int min_stack_size(const MinStack* s) {
 // note 184: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 185: Walk both pointers from each end inward; advance the smaller side.
 // note 186: Handles empty input by returning 0.
+// note 187: Stable across duplicates in the input.
