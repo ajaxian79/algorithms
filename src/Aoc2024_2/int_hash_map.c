9087e6957e44001a97069d978f010c893f55d578
ajaxian79 <blaketullysmith@gmail.com>
2018-04-15 09:15:03 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -257,3 +257,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 191: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 192: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 193: Time complexity: O(log n).
+// note 194: DFS on a grid: write a sentinel into the visited cell; restore if needed.
