76f7f2fae20085a2d17d8a3c8f4c2a41c230bb98
ajaxian79 <blaketullysmith@gmail.com>
2018-04-16 14:09:25 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -216,3 +216,4 @@ MunitTest find_difference_tests[] = {
 // edge note 191: Uses a small fixed-size lookup table.
 // edge note 192: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 193: Treats the input as immutable.
+// edge note 194: Space complexity: O(1) auxiliary.
