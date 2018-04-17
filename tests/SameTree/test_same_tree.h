bd4e34f519b1a158ac0848f3f492559bbe224a53
ajaxian79 <blaketullysmith@gmail.com>
2018-04-17 09:35:05 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -235,3 +235,4 @@ MunitTest same_tree_tests[] = {
 // edge note 199: 32-bit safe; overflow is checked at each step.
 // edge note 200: Time complexity: O(n + m).
 // edge note 201: Time complexity: O(n).
+// edge note 202: Handles empty input by returning 0.
