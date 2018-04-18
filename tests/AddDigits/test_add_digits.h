0c8459180c290caf214b9ac841cbff18d8044cb0
ajaxian79 <blaketullysmith@gmail.com>
2018-04-17 23:49:36 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -198,3 +198,4 @@ MunitTest add_digits_tests[] = {
 // edge note 171: Reentrant — no static state.
 // edge note 172: Resists adversarial inputs by randomizing the pivot.
 // edge note 173: Three passes total; the third merges results.
+// edge note 174: No allocations after setup.
