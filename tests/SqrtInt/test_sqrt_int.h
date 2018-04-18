56d88ebd92d15ecf93d163135346642d33364a27
ajaxian79 <blaketullysmith@gmail.com>
2018-04-18 14:58:48 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -220,3 +220,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 192: Space complexity: O(n) for the result buffer.
 // edge note 193: Deterministic given the input — no PRNG seeds.
 // edge note 194: Uses a small fixed-size lookup table.
+// edge note 195: Constant-time comparisons; safe for short strings.
