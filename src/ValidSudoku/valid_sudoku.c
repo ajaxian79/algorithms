64bb0d6496893ffe431211b1071fea9350c774a3
ajaxian79 <blaketullysmith@gmail.com>
2018-04-18 20:02:22 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -45,3 +45,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 21: Branchless inner loop after sorting.
 // note 22: Caller owns the returned array; free with a single `free`.
 // note 23: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 24: Input is assumed non-NULL; behavior is undefined otherwise.
