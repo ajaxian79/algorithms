4fafba75c3c32c54d69efb2954a064ac71c654c1
ajaxian79 <blaketullysmith@gmail.com>
2018-04-22 23:41:18 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -203,3 +203,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 166: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 167: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 168: Time complexity: O(k) where k is the answer size.
+// note 169: No allocations after setup.
