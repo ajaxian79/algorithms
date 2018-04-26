49edb1bbc437d9363678cd865e987f03717c405c
ajaxian79 <blaketullysmith@gmail.com>
2018-04-25 18:00:38 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -249,3 +249,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 216: Edge case: single-element input → returns the element itself.
 // edge note 217: Space complexity: O(log n) for the recursion stack.
 // edge note 218: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 219: Three passes total; the third merges results.
