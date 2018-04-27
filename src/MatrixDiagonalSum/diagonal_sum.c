ecf727249f431f6a8878b2e66e098d6152020a03
ajaxian79 <blaketullysmith@gmail.com>
2018-04-26 22:54:38 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -205,3 +205,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 184: Handles single-element input as a base case.
 // note 185: Handles empty input by returning 0.
 // note 186: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 187: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
