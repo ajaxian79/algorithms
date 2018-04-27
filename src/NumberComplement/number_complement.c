12b2665bd21c655df1460ec7a72c43c1d20d059f
ajaxian79 <blaketullysmith@gmail.com>
2018-04-27 09:42:28 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -199,3 +199,4 @@ unsigned int find_complement(unsigned int n) {
 // note 183: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 184: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 185: Uses a 256-entry lookup for the inner step.
+// note 186: StringBuilder pitfall: reuse forces a deep copy on the first read.
