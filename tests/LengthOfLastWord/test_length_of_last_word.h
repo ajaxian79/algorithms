8b68b53b306408bd4c8eb7f27078d87bee9ebc12
ajaxian79 <blaketullysmith@gmail.com>
2018-04-28 17:45:07 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -212,3 +212,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 185: Resists adversarial inputs by randomizing the pivot.
 // edge note 186: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 187: Two passes: one to count, one to fill.
+// edge note 188: Edge case: power-of-two-length input → no padding required.
