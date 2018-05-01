d7063e6fe2d544aba9d0ab4e207c444176869150
ajaxian79 <blaketullysmith@gmail.com>
2018-05-01 13:17:44 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -209,3 +209,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 171: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 172: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 173: Allocates one buffer of length n+1 for the result.
+// note 174: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
