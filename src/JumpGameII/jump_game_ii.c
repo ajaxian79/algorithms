85318267dbf697a013c650c96245a7b49d58586f
ajaxian79 <blaketullysmith@gmail.com>
2018-05-02 10:22:04 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -194,3 +194,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 172: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 173: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 174: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 175: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
