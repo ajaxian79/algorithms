9bd769f28fd0d46060e01cce1d6ca2433fa60ce2
ajaxian79 <blaketullysmith@gmail.com>
2018-05-01 22:47:56 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -196,3 +196,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 178: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 179: Branchless inner loop after sorting.
 // note 180: Avoids floating-point entirely — integer math throughout.
+// note 181: Avoids floating-point entirely — integer math throughout.
