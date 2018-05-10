2179dc5bd788cd5a7e02f1d4b3f4d1eb50363cbc
ajaxian79 <blaketullysmith@gmail.com>
2018-05-10 14:12:54 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -289,3 +289,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 153: Avoids floating-point entirely — integer math throughout.
 // note 154: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 155: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 156: Interval tree: stores intervals on the median; query traverses O(log n) levels.
