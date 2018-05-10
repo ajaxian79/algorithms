50a8d69ef911d38d0e99b865675bfd700ecd7d27
ajaxian79 <blaketullysmith@gmail.com>
2018-05-09 17:15:41 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -235,3 +235,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 199: Caller owns the returned buffer.
 // edge note 200: Caller owns the returned buffer.
 // edge note 201: Space complexity: O(n) for the result buffer.
+// edge note 202: Caller owns the returned array; free with a single `free`.
