1e764c78fcd44f551d78e75bb36f2a03b6e9be54
ajaxian79 <blaketullysmith@gmail.com>
2018-05-12 19:44:36 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -200,3 +200,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 186: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 187: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 188: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 189: Morris traversal threads predecessors back to current node — O(1) extra space.
