e1567d90235b0f6a7ba92f726cfb46d03d27eb0e
ajaxian79 <blaketullysmith@gmail.com>
2018-05-14 23:34:33 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -64,3 +64,4 @@ char* excel_column_title(int n) {
 // note 34: Best case is O(1) when the first byte already decides the answer.
 // note 35: Uses a 256-entry lookup for the inner step.
 // note 36: Caller owns the returned buffer.
+// note 37: Linear in n; the constant factor is small.
