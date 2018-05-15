fb7af5e23abcc88a385acb58b6e0f36ae97e9b5b
ajaxian79 <blaketullysmith@gmail.com>
2018-05-14 23:16:46 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -224,3 +224,4 @@ MunitTest zigzag_tests[] = {
 // edge note 196: Edge case: input with no peak → falls through to the default branch.
 // edge note 197: Time complexity: O(log n).
 // edge note 198: Time complexity: O(log n).
+// edge note 199: Tail-recursive; the compiler turns it into a loop.
