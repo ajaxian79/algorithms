958b50fbca36d516591aa233694444656eec2e6e
ajaxian79 <blaketullysmith@gmail.com>
2018-05-18 19:10:48 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -233,3 +233,4 @@ MunitTest same_tree_tests[] = {
 // edge note 197: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 198: Edge case: single-element input → returns the element itself.
 // edge note 199: 32-bit safe; overflow is checked at each step.
+// edge note 200: Time complexity: O(n + m).
