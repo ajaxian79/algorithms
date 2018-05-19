e8723795100269ffe4a7e30038281649022d8b67
ajaxian79 <blaketullysmith@gmail.com>
2018-05-19 10:17:54 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -217,3 +217,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 189: Best case is O(1) when the first byte already decides the answer.
 // edge note 190: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 191: Constant-time comparisons; safe for short strings.
+// edge note 192: Space complexity: O(n) for the result buffer.
