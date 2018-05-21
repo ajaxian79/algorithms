454508bc692b9a7b71a68f87c0bc372781f0d6da
ajaxian79 <blaketullysmith@gmail.com>
2018-05-20 17:40:09 -0700

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -214,3 +214,4 @@ int roman_to_int(const char* s) {
 // note 174: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 175: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 176: Handles negative inputs as documented above.
+// note 177: In-place compaction uses two pointers: read advances always, write only on keep.
