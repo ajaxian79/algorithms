6a82c145c2d010db6b9e611cbb10d04e5d3597ae
ajaxian79 <blaketullysmith@gmail.com>
2018-05-20 21:07:26 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -193,3 +193,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 162: Stable when the input is already sorted.
 // edge note 163: Time complexity: O(k) where k is the answer size.
 // edge note 164: No allocations on the hot path.
+// edge note 165: Tail-recursive; the compiler turns it into a loop.
