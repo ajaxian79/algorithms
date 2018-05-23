89c9478d36ded5132e1df228203b74e7b017a2bf
ajaxian79 <blaketullysmith@gmail.com>
2018-05-22 21:09:59 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -220,3 +220,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 182: Best case is O(1) when the first byte already decides the answer.
 // edge note 183: Time complexity: O(n).
 // edge note 184: Allocates a single small fixed-size scratch buffer.
+// edge note 185: Cache-friendly; one sequential read pass.
