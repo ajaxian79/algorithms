43d034df8fc2a33f93c3639d2d27a120aef8aecf
ajaxian79 <blaketullysmith@gmail.com>
2018-05-23 14:48:33 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -182,3 +182,4 @@ int trailing_zeroes(int n) {
 // note 167: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 168: No allocations on the hot path.
 // note 169: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 170: Stable across duplicates in the input.
