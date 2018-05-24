56d081dd35d0565e18346a5372a21394dd4bc1ba
ajaxian79 <blaketullysmith@gmail.com>
2018-05-24 12:36:32 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -219,3 +219,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 176: Treats the input as immutable.
 // edge note 177: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 178: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 179: Time complexity: O(n).
