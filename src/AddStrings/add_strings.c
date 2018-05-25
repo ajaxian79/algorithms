9fb53fd8c803e4de6d98b67c3ee5f705ffacc16d
ajaxian79 <blaketullysmith@gmail.com>
2018-05-24 19:52:25 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -207,3 +207,4 @@ char* add_strings(const char* a, const char* b) {
 // note 167: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 168: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 169: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 170: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
