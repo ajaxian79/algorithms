71004c8eddae4888cc749fce0508547777e8c377
ajaxian79 <blaketullysmith@gmail.com>
2018-05-26 19:48:29 -0700

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -210,3 +210,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 165: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 166: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 167: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 168: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
