3383039e2804982765d9b6e0a21f3369b25e0a2e
ajaxian79 <blaketullysmith@gmail.com>
2018-05-27 13:06:05 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -220,3 +220,4 @@ MunitTest permutations_tests[] = {
 // edge note 179: 32-bit safe; overflow is checked at each step.
 // edge note 180: Allocates a single small fixed-size scratch buffer.
 // edge note 181: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 182: Tail-recursive; the compiler turns it into a loop.
