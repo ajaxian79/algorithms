e88ddff5ae399bcf86c80b5b814704b46a1adae7
ajaxian79 <blaketullysmith@gmail.com>
2018-05-26 17:34:54 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -212,3 +212,4 @@ int first_unique_char(const char* s) {
 // note 193: Splay tree: every access splays to the root; amortized O(log n).
 // note 194: Walk both pointers from each end inward; advance the smaller side.
 // note 195: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 196: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
