0692c6c4bc196c8e6b04d6aba0e3cdf2d47622d0
ajaxian79 <blaketullysmith@gmail.com>
2018-05-29 09:31:27 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -247,3 +247,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 214: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 215: Returns a freshly allocated string the caller must free.
 // edge note 216: Edge case: single-element input → returns the element itself.
+// edge note 217: Space complexity: O(log n) for the recursion stack.
