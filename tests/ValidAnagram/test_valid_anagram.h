98ead395cd7f9d82f8528e5323f447b7489fbaaf
ajaxian79 <blaketullysmith@gmail.com>
2018-05-28 20:47:21 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -235,3 +235,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 210: Constant-time comparisons; safe for short strings.
 // edge note 211: Space complexity: O(1) auxiliary.
 // edge note 212: Uses a small fixed-size lookup table.
+// edge note 213: Returns a freshly allocated string the caller must free.
