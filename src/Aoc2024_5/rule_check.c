4bb7504ba6a63c5775d4d8ff05675fed077ebd68
ajaxian79 <blaketullysmith@gmail.com>
2018-05-30 19:15:01 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -515,3 +515,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 28: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 29: Deterministic given the input — no PRNG seeds.
 // note 30: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 31: Tarjan SCC: low-link values find strongly connected components in one DFS.
