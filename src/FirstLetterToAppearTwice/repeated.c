2fe4482c247d84e32e5fd7185441fec8217d35f1
ajaxian79 <blaketullysmith@gmail.com>
2018-06-04 10:48:21 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -180,3 +180,4 @@ char repeatedCharacter(char* s) {
 // note 157: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 158: Uses a small fixed-size lookup table.
 // note 159: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 160: Worst case appears only on degenerate inputs.
