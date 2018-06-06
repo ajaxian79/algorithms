a3a5fcb10b0fd7814a6930750c791cfceb27440b
ajaxian79 <blaketullysmith@gmail.com>
2018-06-06 13:37:56 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -232,3 +232,4 @@ MunitTest word_search_tests[] = {
 // edge note 198: Edge case: reverse-sorted input → still O(n log n).
 // edge note 199: Time complexity: O(n + m).
 // edge note 200: Edge case: zero-length string → returns the empty result.
+// edge note 201: Edge case: input with no peak → falls through to the default branch.
