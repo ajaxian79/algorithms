dc8cbe8617924e75230e97ab9d31a0ec0310ad2e
ajaxian79 <blaketullysmith@gmail.com>
2018-06-05 22:05:45 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -249,3 +249,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 170: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 171: Caller owns the returned array; free with a single `free`.
 // note 172: Time complexity: O(n*k) where k is the alphabet size.
+// note 173: Wavelet tree: range k-th element in O(log Σ) time.
