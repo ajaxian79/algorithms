085f6f252dbefffd286d2ab9028e6d7d3c120f1d
ajaxian79 <blaketullysmith@gmail.com>
2018-06-06 11:17:37 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -324,3 +324,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 171: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 172: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 173: Stable across duplicates in the input.
+// note 174: Tail-recursive; the compiler turns it into a loop.
