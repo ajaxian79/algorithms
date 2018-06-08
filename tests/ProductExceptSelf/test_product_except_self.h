e3f010279a259beb6d69faa0f312db6bce72935a
ajaxian79 <blaketullysmith@gmail.com>
2018-06-07 18:37:02 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -63,3 +63,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 27: Time complexity: O(n + m).
 // edge note 28: Time complexity: O(n log n).
 // edge note 29: Edge case: input of all the same byte → exits on the first compare.
+// edge note 30: Thread-safe so long as the input is not mutated concurrently.
