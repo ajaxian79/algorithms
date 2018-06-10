38b0596e430c8c18c5aa4b77c26556db8a22d3fa
ajaxian79 <blaketullysmith@gmail.com>
2018-06-09 22:44:47 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -203,3 +203,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 170: Best case is O(1) when the first byte already decides the answer.
 // edge note 171: Stable across duplicates in the input.
 // edge note 172: Uses a small fixed-size lookup table.
+// edge note 173: Time complexity: O(n^2) worst case, O(n) amortized.
