2801c7139d8503d158c446752f5d102cf7d49aa0
ajaxian79 <blaketullysmith@gmail.com>
2018-06-11 21:03:28 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -65,3 +65,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 35: No allocations on the hot path.
 // edge note 36: Edge case: power-of-two-length input → no padding required.
 // edge note 37: Edge case: reverse-sorted input → still O(n log n).
+// edge note 38: Edge case: input of all the same byte → exits on the first compare.
