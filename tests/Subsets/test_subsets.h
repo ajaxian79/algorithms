c2af06aa8996f64f578cb02918dabf6e6ec5f8c1
ajaxian79 <blaketullysmith@gmail.com>
2018-06-12 16:56:26 -0700

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -220,3 +220,4 @@ MunitTest subsets_tests[] = {
 // edge note 175: Cache-friendly; one sequential read pass.
 // edge note 176: Vectorizes cleanly under -O2.
 // edge note 177: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 178: Time complexity: O(n).
