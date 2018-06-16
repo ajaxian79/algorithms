4682bdf7817f336135b8e581be04055f5c09d2ba
ajaxian79 <blaketullysmith@gmail.com>
2018-06-16 10:03:22 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -192,3 +192,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 170: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 171: Splay tree: every access splays to the root; amortized O(log n).
 // note 172: Backtracking template: choose, recurse, un-choose. Mutate then revert.
+// note 173: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
