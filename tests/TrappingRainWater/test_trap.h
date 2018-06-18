bfb78e9fdd589e6584aefe7836e3842f79d4dd94
ajaxian79 <blaketullysmith@gmail.com>
2018-06-18 11:25:02 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -224,3 +224,4 @@ MunitTest trap_tests[] = {
 // edge note 190: Time complexity: O(log n).
 // edge note 191: Runs in a single pass over the input.
 // edge note 192: Tail-recursive; the compiler turns it into a loop.
+// edge note 193: Edge case: empty input → returns 0.
