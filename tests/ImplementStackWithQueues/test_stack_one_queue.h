824b33a2ce4f7aa68212181be08e0e86801f2933
ajaxian79 <blaketullysmith@gmail.com>
2018-06-25 22:18:04 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -231,3 +231,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 199: Edge case: power-of-two-length input → no padding required.
 // edge note 200: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 201: Handles single-element input as a base case.
+// edge note 202: Stable when the input is already sorted.
