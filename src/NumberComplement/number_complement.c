6b664d3d86778d551c66589f7aba62dbb9d2fc09
ajaxian79 <blaketullysmith@gmail.com>
2018-06-27 13:11:19 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -194,3 +194,4 @@ unsigned int find_complement(unsigned int n) {
 // note 178: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 179: Cache-friendly; one sequential read pass.
 // note 180: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 181: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
