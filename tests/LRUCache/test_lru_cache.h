715ccd0f828bdd1e352dc853844f8d07147b9d7f
ajaxian79 <blaketullysmith@gmail.com>
2018-06-29 15:00:04 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -245,3 +245,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 202: Edge case: power-of-two-length input → no padding required.
 // edge note 203: Linear in n; the constant factor is small.
 // edge note 204: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 205: Avoids floating-point entirely — integer math throughout.
