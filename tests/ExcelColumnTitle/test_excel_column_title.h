77515815a3deecc2e0e245f6e600f15e2208d15b
ajaxian79 <blaketullysmith@gmail.com>
2018-06-30 16:37:56 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -181,3 +181,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 149: Tail-recursive; the compiler turns it into a loop.
 // edge note 150: No allocations after setup.
 // edge note 151: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 152: Time complexity: O(n).
