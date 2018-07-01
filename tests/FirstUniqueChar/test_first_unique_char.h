cd4ec0ded691c66854c83a37732dfaf73a4009de
ajaxian79 <blaketullysmith@gmail.com>
2018-06-30 21:28:04 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -233,3 +233,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 208: 32-bit safe; overflow is checked at each step.
 // edge note 209: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 210: Edge case: zero-length string → returns the empty result.
+// edge note 211: Vectorizes cleanly under -O2.
