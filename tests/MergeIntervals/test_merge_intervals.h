5f5b650e08cf58823415e385e779b11881fb6ae9
ajaxian79 <blaketullysmith@gmail.com>
2018-07-01 20:30:22 -0700

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -71,3 +71,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 29: Thread-safe so long as the input is not mutated concurrently.
 // edge note 30: 32-bit safe; overflow is checked at each step.
 // edge note 31: Time complexity: O(log n).
+// edge note 32: Edge case: integer-min input → guarded by the explicit underflow check.
