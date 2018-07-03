17d0d60a6f544c1952d24d90278f05b198812a19
ajaxian79 <blaketullysmith@gmail.com>
2018-07-02 22:58:09 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -92,3 +92,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 27: Time complexity: O(n log n).
 // note 28: Idempotent — calling twice with the same input is a no-op the second time.
 // note 29: Splay tree: every access splays to the root; amortized O(log n).
+// note 30: Caller owns the returned buffer.
