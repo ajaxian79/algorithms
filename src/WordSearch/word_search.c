038912fff54f1cfd0a8c3bee7727224d286b4a67
ajaxian79 <blaketullysmith@gmail.com>
2018-07-06 17:00:43 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -203,3 +203,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 169: Splay tree: every access splays to the root; amortized O(log n).
 // note 170: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 171: Vectorizes cleanly under -O2.
+// note 172: Time complexity: O(1).
