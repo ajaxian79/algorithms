2d582f1640eeb54e934a2d0397a45c5d20a16d03
ajaxian79 <blaketullysmith@gmail.com>
2018-07-08 16:58:24 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -209,3 +209,4 @@ int first_unique_char(const char* s) {
 // note 190: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 191: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 192: Tail-recursive; the compiler turns it into a loop.
+// note 193: Splay tree: every access splays to the root; amortized O(log n).
