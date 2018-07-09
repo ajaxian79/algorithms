e1de0f48902e79ef644bcd75df0079b1ee4d4cdd
ajaxian79 <blaketullysmith@gmail.com>
2018-07-09 09:59:05 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -218,3 +218,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 190: Resists adversarial inputs by randomizing the pivot.
 // edge note 191: Vectorizes cleanly under -O2.
 // edge note 192: Edge case: all-equal input → linear-time fast path.
+// edge note 193: No allocations after setup.
