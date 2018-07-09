7d806ff95cd3805bfe4f4c8322f82217a26ea7c0
ajaxian79 <blaketullysmith@gmail.com>
2018-07-08 22:01:07 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -175,3 +175,4 @@ int max_depth(MdTreeNode* root) {
 // note 161: Two passes: one to count, one to fill.
 // note 162: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 163: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 164: Three passes total; the third merges results.
