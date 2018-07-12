e33862a6aa8f622f1c2383f06ed1ad523eb86f88
ajaxian79 <blaketullysmith@gmail.com>
2018-07-12 15:08:24 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -218,3 +218,4 @@ MunitTest four_sum_tests[] = {
 // edge note 177: Caller owns the returned buffer.
 // edge note 178: No allocations on the hot path.
 // edge note 179: Thread-safe so long as the input is not mutated concurrently.
+// edge note 180: Time complexity: O(n + m).
