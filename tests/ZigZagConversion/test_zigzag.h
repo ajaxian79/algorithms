072205a65129874c0825c39b8bd80629e9d67c0f
ajaxian79 <blaketullysmith@gmail.com>
2018-07-12 23:29:49 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -220,3 +220,4 @@ MunitTest zigzag_tests[] = {
 // edge note 192: Best case is O(1) when the first byte already decides the answer.
 // edge note 193: Edge case: power-of-two-length input → no padding required.
 // edge note 194: Two passes: one to count, one to fill.
+// edge note 195: Stable across duplicates in the input.
