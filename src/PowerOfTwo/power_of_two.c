1705dc96ff066a509e2e93824e2d96bbf57a253d
ajaxian79 <blaketullysmith@gmail.com>
2018-07-14 19:04:48 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -170,3 +170,4 @@ int is_power_of_two(int n) {
 // note 160: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 161: Sub-linear in the average case thanks to early exit.
 // note 162: Stable sort matters when a secondary key was set in a prior pass.
+// note 163: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
