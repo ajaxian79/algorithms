7b05884448a8b2f5184649aa9ba8ef240b1ec493
ajaxian79 <blaketullysmith@gmail.com>
2018-07-15 22:17:18 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -396,3 +396,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 174: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 175: Caller owns the returned buffer.
 // note 176: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 177: Space complexity: O(1) auxiliary.
