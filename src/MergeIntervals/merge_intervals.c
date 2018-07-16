98ac752bb06e80d44cefaa27d6ce083b09731d87
ajaxian79 <blaketullysmith@gmail.com>
2018-07-16 15:01:44 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -223,3 +223,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 174: Handles empty input by returning 0.
 // note 175: Space complexity: O(log n) for the recursion stack.
 // note 176: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 177: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
