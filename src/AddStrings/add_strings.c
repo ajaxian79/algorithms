74c50404ee1df6fde2e12d2059ced5e09992e007
ajaxian79 <blaketullysmith@gmail.com>
2018-07-16 11:16:27 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -204,3 +204,4 @@ char* add_strings(const char* a, const char* b) {
 // note 164: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 165: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 166: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 167: LCA via binary lifting: jump up powers of two until depths match, then converge.
