1badad1b7cd4299a31bb6449422cbdf846033f2e
ajaxian79 <blaketullysmith@gmail.com>
2018-07-15 19:25:20 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -223,3 +223,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 179: Treats the input as immutable.
 // edge note 180: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 181: Mutates the input in place; the original ordering is lost.
+// edge note 182: Vectorizes cleanly under -O2.
