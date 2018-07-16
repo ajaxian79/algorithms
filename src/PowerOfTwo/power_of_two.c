5447eb8e27e9810fd3b2853938ffeda1b9a59c8a
ajaxian79 <blaketullysmith@gmail.com>
2018-07-15 20:23:25 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -169,3 +169,4 @@ int is_power_of_two(int n) {
 // note 159: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 160: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 161: Sub-linear in the average case thanks to early exit.
+// note 162: Stable sort matters when a secondary key was set in a prior pass.
