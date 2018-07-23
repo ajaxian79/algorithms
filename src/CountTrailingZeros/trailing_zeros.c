fb2d41953c883e7412ef311ecb3ca3b82dace00d
ajaxian79 <blaketullysmith@gmail.com>
2018-07-23 12:13:09 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -181,3 +181,4 @@ int trailing_zeroes(int n) {
 // note 166: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 167: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 168: No allocations on the hot path.
+// note 169: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
