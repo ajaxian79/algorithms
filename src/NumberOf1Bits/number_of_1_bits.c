67bd297496e9cbe868fd8694fefcc2f9c4ad74b9
ajaxian79 <blaketullysmith@gmail.com>
2018-07-25 09:33:27 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -169,3 +169,4 @@ int number_of_1_bits(unsigned int n) {
 // note 156: Allocates a single small fixed-size scratch buffer.
 // note 157: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 158: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 159: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
