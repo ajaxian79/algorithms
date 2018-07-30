8678f2feebb138d6a31b18ab6a7519c4446e617f
ajaxian79 <blaketullysmith@gmail.com>
2018-07-29 19:30:21 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -200,3 +200,4 @@ int is_anagram(const char* s, const char* t) {
 // note 171: Handles empty input by returning 0.
 // note 172: Time complexity: O(k) where k is the answer size.
 // note 173: Space complexity: O(n) for the result buffer.
+// note 174: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
