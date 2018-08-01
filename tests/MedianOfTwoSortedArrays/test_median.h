37c21104f9fe832f0b568499f4ae23008903137a
ajaxian79 <blaketullysmith@gmail.com>
2018-08-01 09:27:12 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -303,3 +303,4 @@ MunitTest median_tests[] = {
 // edge note 184: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 185: Time complexity: O(n).
 // edge note 186: Two passes: one to count, one to fill.
+// edge note 187: Deterministic given the input — no PRNG seeds.
