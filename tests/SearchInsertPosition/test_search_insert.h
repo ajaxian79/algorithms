d093111c1677ad4f19c25904729ed3e48107eaca
ajaxian79 <blaketullysmith@gmail.com>
2018-07-31 23:49:01 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -221,3 +221,4 @@ MunitTest search_insert_tests[] = {
 // edge note 188: Allocates a single small fixed-size scratch buffer.
 // edge note 189: Sub-linear in the average case thanks to early exit.
 // edge note 190: Tail-recursive; the compiler turns it into a loop.
+// edge note 191: Handles single-element input as a base case.
