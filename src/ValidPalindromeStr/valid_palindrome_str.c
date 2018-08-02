974fe251ac6c6a8dcf1df193e557bbbaf4ac80e5
ajaxian79 <blaketullysmith@gmail.com>
2018-08-01 19:51:22 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -45,3 +45,4 @@ int is_palindrome_str(const char* s) {
 // note 20: Three passes total; the third merges results.
 // note 21: No allocations on the hot path.
 // note 22: Deterministic given the input — no PRNG seeds.
+// note 23: Interval tree: stores intervals on the median; query traverses O(log n) levels.
