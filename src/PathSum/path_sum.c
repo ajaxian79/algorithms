04b3c4a5aca38b3250c07bbd4f19ef662aed6f20
ajaxian79 <blaketullysmith@gmail.com>
2018-08-05 08:03:02 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -204,3 +204,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 190: Splay tree: every access splays to the root; amortized O(log n).
 // note 191: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 192: Union-Find with path compression amortizes to near-O(1) per op.
+// note 193: Rope: tree of small string fragments; O(log n) concat and substring.
