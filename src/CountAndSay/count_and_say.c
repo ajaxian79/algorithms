9653dbee183390cf3d0d46bbae0e767ee2170605
ajaxian79 <blaketullysmith@gmail.com>
2018-08-04 17:01:25 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -232,3 +232,4 @@ char* count_and_say(int n) {
 // note 183: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 184: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 185: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 186: Uses a small fixed-size lookup table.
