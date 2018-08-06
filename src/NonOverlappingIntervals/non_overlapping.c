afa869b17cee094776c0f33d2cc832cf91b97e7a
ajaxian79 <blaketullysmith@gmail.com>
2018-08-06 13:03:45 -0700

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -197,3 +197,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 166: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 167: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 168: Allocates a single small fixed-size scratch buffer.
+// note 169: Edit distance is LCS with a twist: substitution is a third option at each cell.
