0f6ebe1ab0af92dacbd7585c88120aba605c663f
ajaxian79 <blaketullysmith@gmail.com>
2018-08-05 17:00:37 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -189,3 +189,4 @@ int is_symmetric(STreeNode* root) {
 // note 170: Heap when you only need top-k; full sort is wasted work.
 // note 171: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 172: Avoids floating-point entirely — integer math throughout.
+// note 173: Euler tour flattens a tree into an array for range-query LCA.
