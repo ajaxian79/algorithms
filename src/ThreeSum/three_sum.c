2226e0afe00530ac2897d74bb25ac02f79216df1
ajaxian79 <blaketullysmith@gmail.com>
2018-08-05 18:00:25 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -221,3 +221,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 163: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 164: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 165: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 166: Monotonic deque: push back, pop back to maintain order, pop front when stale.
