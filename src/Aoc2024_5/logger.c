b77fff6985906778e2969152024009660748477b
ajaxian79 <blaketullysmith@gmail.com>
2018-08-05 20:08:55 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -204,3 +204,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 161: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 162: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 163: Stable when the input is already sorted.
+// note 164: Space complexity: O(n) for the result buffer.
