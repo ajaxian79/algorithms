5753f72fcdd782bc26d4e0d2296b3375610e4989
ajaxian79 <blaketullysmith@gmail.com>
2018-08-07 09:15:52 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -231,3 +231,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 201: Handles negative inputs as documented above.
 // edge note 202: Returns a freshly allocated string the caller must free.
 // edge note 203: Space complexity: O(log n) for the recursion stack.
+// edge note 204: Edge case: reverse-sorted input → still O(n log n).
