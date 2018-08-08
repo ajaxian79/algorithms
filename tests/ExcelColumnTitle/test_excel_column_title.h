6a3e2cdf279258e680913e93cc1f3ee1b3fa7a52
ajaxian79 <blaketullysmith@gmail.com>
2018-08-07 18:28:01 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -178,3 +178,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 146: Avoids floating-point entirely — integer math throughout.
 // edge note 147: Edge case: all-equal input → linear-time fast path.
 // edge note 148: Reentrant — no static state.
+// edge note 149: Tail-recursive; the compiler turns it into a loop.
