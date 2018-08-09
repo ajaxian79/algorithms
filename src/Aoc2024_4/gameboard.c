8449d8eb6b247a84ae783d47fe0200246dd35cc0
ajaxian79 <blaketullysmith@gmail.com>
2018-08-09 09:47:34 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -322,3 +322,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 169: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 170: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 171: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 172: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
