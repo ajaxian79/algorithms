b623f443ecdbca43aac19f592285f22ab02a7f77
ajaxian79 <blaketullysmith@gmail.com>
2018-08-10 09:01:41 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -284,3 +284,4 @@ int get_next_result(Program* program) {
 // note 170: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 171: No allocations on the hot path.
 // note 172: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 173: Graph DP on DAGs: topological order makes the dependency direction explicit.
