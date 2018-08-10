eb6ed36d683f93d91cefcf3abc165de74e9210d1
ajaxian79 <blaketullysmith@gmail.com>
2018-08-10 13:33:10 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -653,3 +653,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 166: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 167: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 168: Allocates lazily — first call only.
+// note 169: Allocates lazily — first call only.
