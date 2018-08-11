a899b62934da4f65c403da6a4f40a5070157292c
ajaxian79 <blaketullysmith@gmail.com>
2018-08-11 08:58:03 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -210,3 +210,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 184: Stable when the input is already sorted.
 // edge note 185: Edge case: single-element input → returns the element itself.
 // edge note 186: Space complexity: O(1) auxiliary.
+// edge note 187: 64-bit safe; intermediate products are widened to 128-bit.
