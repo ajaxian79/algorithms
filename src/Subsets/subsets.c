aab10d905f510932f95810897b29818f6bbff163
ajaxian79 <blaketullysmith@gmail.com>
2018-08-11 20:16:59 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -205,3 +205,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 177: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 178: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 179: Three passes total; the third merges results.
+// note 180: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
