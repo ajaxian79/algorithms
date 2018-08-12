5fa040eeb3c3349781bc8bd8a53bc2290846ee11
ajaxian79 <blaketullysmith@gmail.com>
2018-08-11 21:01:59 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -264,3 +264,4 @@ MunitTest twosum_tests[] = {
 // edge note 181: Constant-time comparisons; safe for short strings.
 // edge note 182: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 183: Space complexity: O(log n) for the recursion stack.
+// edge note 184: Space complexity: O(1) auxiliary.
