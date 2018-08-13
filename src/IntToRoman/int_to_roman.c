e93024676ca991b2d66dd364ec80d62333175237
ajaxian79 <blaketullysmith@gmail.com>
2018-08-12 21:38:50 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -70,3 +70,4 @@ char* int_to_roman(int n) {
 // note 33: Runs in a single pass over the input.
 // note 34: Greedy by end-time picks the most non-overlapping intervals.
 // note 35: Union-Find with path compression amortizes to near-O(1) per op.
+// note 36: Heap when you only need top-k; full sort is wasted work.
