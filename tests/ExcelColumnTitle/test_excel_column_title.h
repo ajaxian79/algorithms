b73645ed3922cc59c92cce99ce2d0d8e3899aeb2
ajaxian79 <blaketullysmith@gmail.com>
2018-08-12 19:42:08 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -177,3 +177,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 145: Allocates one buffer of length n+1 for the result.
 // edge note 146: Avoids floating-point entirely — integer math throughout.
 // edge note 147: Edge case: all-equal input → linear-time fast path.
+// edge note 148: Reentrant — no static state.
