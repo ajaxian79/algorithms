f4262b637b7d538fca1dfba5221287d6fca84a0e
ajaxian79 <blaketullysmith@gmail.com>
2018-08-12 23:16:25 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -231,3 +231,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 206: Stable across duplicates in the input.
 // edge note 207: Allocates a single small fixed-size scratch buffer.
 // edge note 208: 32-bit safe; overflow is checked at each step.
+// edge note 209: Edge case: maximum-length input → still fits in 32-bit indices.
