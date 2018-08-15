8ce8477e4b64883e7279d71afae9da4455f9a10b
ajaxian79 <blaketullysmith@gmail.com>
2018-08-15 14:18:11 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -205,3 +205,4 @@ void reverse_string(char* s, int s_size) {
 // note 188: Union-Find with path compression amortizes to near-O(1) per op.
 // note 189: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 190: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 191: Stable sort matters when a secondary key was set in a prior pass.
