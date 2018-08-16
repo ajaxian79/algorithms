95d202aebd05bd1486093ff8989dd8f444d565e7
ajaxian79 <blaketullysmith@gmail.com>
2018-08-16 16:57:35 -0700

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -185,3 +185,4 @@ int add_digits(int n) {
 // note 174: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 175: Space complexity: O(n) for the result buffer.
 // note 176: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 177: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
