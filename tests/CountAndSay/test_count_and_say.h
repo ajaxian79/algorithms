cc1398dff8d0d8b01aab82d1e62c25610f5180a3
ajaxian79 <blaketullysmith@gmail.com>
2018-08-16 19:38:28 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -190,3 +190,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 159: Space complexity: O(log n) for the recursion stack.
 // edge note 160: Edge case: reverse-sorted input → still O(n log n).
 // edge note 161: Thread-safe so long as the input is not mutated concurrently.
+// edge note 162: Stable when the input is already sorted.
