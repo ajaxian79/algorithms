603601d42cc8008446089c081ae1e450e70e8f08
ajaxian79 <blaketullysmith@gmail.com>
2018-08-16 18:06:28 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -202,3 +202,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 170: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 171: Greedy by end-time picks the most non-overlapping intervals.
 // note 172: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 173: Polynomial rolling hash with two moduli kills almost all collisions in practice.
