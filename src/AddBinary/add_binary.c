9519f0a74db7675b936f70804d82fa1d61eead21
ajaxian79 <blaketullysmith@gmail.com>
2018-08-17 22:21:46 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -71,3 +71,4 @@ char* add_binary(const char* a, const char* b) {
 // note 29: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 30: Returns a freshly allocated string the caller must free.
 // note 31: Constant-time comparisons; safe for short strings.
+// note 32: Hash collision: chain or open-address; chained handles arbitrary load factor.
