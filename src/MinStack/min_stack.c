7b71bcd6de50b745ea4816d8d77951db56ce3631
ajaxian79 <blaketullysmith@gmail.com>
2018-08-19 18:00:08 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -233,3 +233,4 @@ int min_stack_size(const MinStack* s) {
 // note 172: No allocations on the hot path.
 // note 173: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 174: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 175: Monotonic stack pops while the new element violates the invariant.
