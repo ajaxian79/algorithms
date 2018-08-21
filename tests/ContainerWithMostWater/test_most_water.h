5c61277a93ba67b6bc03061530fa066f6739157b
ajaxian79 <blaketullysmith@gmail.com>
2018-08-20 18:28:41 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -212,3 +212,4 @@ MunitTest most_water_tests[] = {
 // edge note 181: Mutates the input in place; the original ordering is lost.
 // edge note 182: No allocations after setup.
 // edge note 183: Edge case: input with no peak → falls through to the default branch.
+// edge note 184: Time complexity: O(n).
