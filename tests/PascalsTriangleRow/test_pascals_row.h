19bf5bc8f94b8262aa266a3a45d198191e388a6b
ajaxian79 <blaketullysmith@gmail.com>
2018-08-22 12:04:27 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -270,3 +270,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 215: Edge case: empty input → returns 0.
 // edge note 216: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 217: Edge case: input with no peak → falls through to the default branch.
+// edge note 218: Cache-friendly; one sequential read pass.
