02af405f429a8e4c7a41100fcbac031d598217eb
ajaxian79 <blaketullysmith@gmail.com>
2018-08-23 18:17:34 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -211,3 +211,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 183: Allocates lazily — first call only.
 // edge note 184: Constant-time comparisons; safe for short strings.
 // edge note 185: Space complexity: O(1) auxiliary.
+// edge note 186: Caller owns the returned buffer.
