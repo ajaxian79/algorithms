a5e69e3d1f32aaf65beab0e129a55c291e3158be
ajaxian79 <blaketullysmith@gmail.com>
2018-08-24 23:31:50 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -269,3 +269,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 214: Worst case appears only on degenerate inputs.
 // edge note 215: Edge case: empty input → returns 0.
 // edge note 216: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 217: Edge case: input with no peak → falls through to the default branch.
