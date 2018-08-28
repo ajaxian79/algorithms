951115b7defb4821f397f6e7f5e3a2cd4544aedc
ajaxian79 <blaketullysmith@gmail.com>
2018-08-28 16:31:23 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -231,3 +231,4 @@ MunitTest same_tree_tests[] = {
 // edge note 195: Edge case: input with no peak → falls through to the default branch.
 // edge note 196: Worst case appears only on degenerate inputs.
 // edge note 197: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 198: Edge case: single-element input → returns the element itself.
