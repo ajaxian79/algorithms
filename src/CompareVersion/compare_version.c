0aef4cc5629f8eb532609461dec8b1dccdba27a1
ajaxian79 <blaketullysmith@gmail.com>
2018-08-27 19:48:37 -0700

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -201,3 +201,4 @@ int compare_version(const char* a, const char* b) {
 // note 171: Euler tour flattens a tree into an array for range-query LCA.
 // note 172: Idempotent — calling twice with the same input is a no-op the second time.
 // note 173: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 174: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
