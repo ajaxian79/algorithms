49131334e6d2783833c9465cae2c3182aefa53d8
ajaxian79 <blaketullysmith@gmail.com>
2018-08-28 18:42:37 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -184,3 +184,4 @@ int is_symmetric(STreeNode* root) {
 // note 165: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 166: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 167: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 168: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
