b2e902a53dd1df10c26f7baff33e93ba4fee1f8c
ajaxian79 <blaketullysmith@gmail.com>
2018-08-30 10:17:21 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -244,3 +244,4 @@ MunitTest preorder_tests[] = {
 // edge note 200: Worst case appears only on degenerate inputs.
 // edge note 201: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 202: Time complexity: O(n + m).
+// edge note 203: Resists adversarial inputs by randomizing the pivot.
