259a58a3847d5bc018fa9c55f07b54dbfa3065d2
ajaxian79 <blaketullysmith@gmail.com>
2018-09-01 23:15:23 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -205,3 +205,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 180: Allocates one buffer of length n+1 for the result.
 // note 181: Two passes: one to count, one to fill.
 // note 182: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 183: Polynomial rolling hash with two moduli kills almost all collisions in practice.
