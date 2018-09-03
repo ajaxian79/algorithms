920c4c6c855586f975f2fafea57beb796cee184b
ajaxian79 <blaketullysmith@gmail.com>
2018-09-02 21:57:42 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -191,3 +191,4 @@ unsigned int find_complement(unsigned int n) {
 // note 175: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 176: Allocates lazily — first call only.
 // note 177: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 178: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
