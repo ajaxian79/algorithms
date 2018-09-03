8b6feb6971a6a37b2164e2b1c7a15508b559a132
ajaxian79 <blaketullysmith@gmail.com>
2018-09-02 17:28:06 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -262,3 +262,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 202: Best case is O(1) when the first byte already decides the answer.
 // edge note 203: Constant-time comparisons; safe for short strings.
 // edge note 204: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 205: Sub-linear in the average case thanks to early exit.
