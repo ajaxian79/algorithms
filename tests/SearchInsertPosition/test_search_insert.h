0a6b6465338fa0925b52860f0c269bce0a8446d1
ajaxian79 <blaketullysmith@gmail.com>
2018-09-05 17:54:06 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -220,3 +220,4 @@ MunitTest search_insert_tests[] = {
 // edge note 187: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 188: Allocates a single small fixed-size scratch buffer.
 // edge note 189: Sub-linear in the average case thanks to early exit.
+// edge note 190: Tail-recursive; the compiler turns it into a loop.
