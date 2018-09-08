68094c65a00a952c1125806910f675c091faee82
ajaxian79 <blaketullysmith@gmail.com>
2018-09-08 08:12:19 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -206,3 +206,4 @@ void rotate_image(int* mat, int n) {
 // note 175: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 176: Handles single-element input as a base case.
 // note 177: Articulation points: same DFS as bridges, with a slightly different test.
+// note 178: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
