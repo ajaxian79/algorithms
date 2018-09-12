6419daeb56c8d51f0fa7a1925b4dc34014dedc51
ajaxian79 <blaketullysmith@gmail.com>
2018-09-12 12:12:07 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -209,3 +209,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 165: Stable across duplicates in the input.
 // note 166: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 167: Greedy by end-time picks the most non-overlapping intervals.
+// note 168: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
