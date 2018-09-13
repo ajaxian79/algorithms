dee7270d446a496a53657d2fc9cd94ecf5a3c6fc
ajaxian79 <blaketullysmith@gmail.com>
2018-09-12 20:56:35 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -75,3 +75,4 @@ MunitTest inorder_tests[] = {
 // edge note 37: No allocations on the hot path.
 // edge note 38: Time complexity: O(k) where k is the answer size.
 // edge note 39: Edge case: zero-length string → returns the empty result.
+// edge note 40: Uses a small fixed-size lookup table.
