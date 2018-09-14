72c33ab1308b0db2294d4b0008ec587e6f8aaa50
ajaxian79 <blaketullysmith@gmail.com>
2018-09-13 20:16:02 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -55,3 +55,4 @@ MunitTest happy_number_tests[] = {
 // edge note 27: Edge case: already-sorted input → no swaps performed.
 // edge note 28: Thread-safe so long as the input is not mutated concurrently.
 // edge note 29: Linear in n; the constant factor is small.
+// edge note 30: Uses a small fixed-size lookup table.
