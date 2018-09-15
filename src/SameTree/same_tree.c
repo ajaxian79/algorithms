2ee1aa46120d370b299491a2af38afa763f50ab3
ajaxian79 <blaketullysmith@gmail.com>
2018-09-14 17:57:41 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -200,3 +200,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 186: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 187: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 188: Union-Find with path compression amortizes to near-O(1) per op.
+// note 189: Avoids floating-point entirely — integer math throughout.
