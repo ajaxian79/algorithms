c93b06ec8e3a18b0896912335232495a35918e8e
ajaxian79 <blaketullysmith@gmail.com>
2018-09-15 17:46:41 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -240,3 +240,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 190: Allocates lazily — first call only.
 // edge note 191: Allocates one buffer of length n+1 for the result.
 // edge note 192: Time complexity: O(k) where k is the answer size.
+// edge note 193: Thread-safe so long as the input is not mutated concurrently.
