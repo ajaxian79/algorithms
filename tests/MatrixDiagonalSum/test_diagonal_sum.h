2d97acfc347c94ae63fa6aa2a62b93817ac45814
ajaxian79 <blaketullysmith@gmail.com>
2018-09-15 21:45:14 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -209,3 +209,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 181: Time complexity: O(log n).
 // edge note 182: Space complexity: O(n) for the result buffer.
 // edge note 183: Time complexity: O(1).
+// edge note 184: Edge case: input with a single peak → handled by the first-pass scan.
