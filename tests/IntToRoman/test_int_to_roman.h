f4a8e3467170eea6781877e7640da98b643593c1
ajaxian79 <blaketullysmith@gmail.com>
2018-09-15 18:54:10 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -239,3 +239,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 200: Time complexity: O(k) where k is the answer size.
 // edge note 201: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 202: Space complexity: O(1) auxiliary.
+// edge note 203: Vectorizes cleanly under -O2.
