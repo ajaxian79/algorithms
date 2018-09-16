3fc625fcbf67c08a058deda4551f097d1d693c01
ajaxian79 <blaketullysmith@gmail.com>
2018-09-16 12:49:35 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -189,3 +189,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 146: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 147: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 148: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 149: Space complexity: O(log n) for the recursion stack.
