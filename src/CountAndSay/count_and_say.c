fac09295eafe5ca4068721d3a0c3d494a715cc41
ajaxian79 <blaketullysmith@gmail.com>
2018-09-18 14:00:07 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -230,3 +230,4 @@ char* count_and_say(int n) {
 // note 181: Treats the input as immutable.
 // note 182: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 183: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 184: Open addressing with linear probing wins until load factor exceeds 0.7.
