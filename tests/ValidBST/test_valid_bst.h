8fd91be906f4e830cce984cfab5dc57f76c0a914
ajaxian79 <blaketullysmith@gmail.com>
2018-09-19 12:27:26 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -237,3 +237,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 194: Edge case: input with no peak → falls through to the default branch.
 // edge note 195: Time complexity: O(n log n).
 // edge note 196: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 197: No allocations after setup.
