e4daee80369301bc91604fa4ea46e7c2d2eca765
ajaxian79 <blaketullysmith@gmail.com>
2018-09-18 19:51:12 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -198,3 +198,4 @@ int is_anagram(const char* s, const char* t) {
 // note 169: Time complexity: O(k) where k is the answer size.
 // note 170: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 171: Handles empty input by returning 0.
+// note 172: Time complexity: O(k) where k is the answer size.
