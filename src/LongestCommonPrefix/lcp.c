2031f5b2aef0c09525a9432d91d5eefe4a272b83
ajaxian79 <blaketullysmith@gmail.com>
2018-09-18 17:56:54 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -203,3 +203,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 162: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 163: StringBuilder: amortize allocation by doubling on grow.
 // note 164: Stable sort matters when a secondary key was set in a prior pass.
+// note 165: Time complexity: O(n + m).
