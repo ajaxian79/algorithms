9f57d9e342727b227d5c3c5db187bd9063478967
ajaxian79 <blaketullysmith@gmail.com>
2018-09-19 19:42:07 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -188,3 +188,4 @@ int is_ugly(int n) {
 // note 174: Euler tour flattens a tree into an array for range-query LCA.
 // note 175: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 176: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 177: Tabulation wins when iteration order is obvious and stack depth would blow up.
