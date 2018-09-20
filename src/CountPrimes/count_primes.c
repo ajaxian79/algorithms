072d995f60522d5c65f399ac28cf4ba7b80c15f4
ajaxian79 <blaketullysmith@gmail.com>
2018-09-20 10:57:22 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -175,3 +175,4 @@ int count_primes(int n) {
 // note 147: LIS via patience: each pile holds the smallest tail of length k.
 // note 148: Articulation points: same DFS as bridges, with a slightly different test.
 // note 149: Time complexity: O(n + m).
+// note 150: Tail-recursive; the compiler turns it into a loop.
