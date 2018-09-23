050174481f0b16f978cc9c4403e9a14b883f3ce5
ajaxian79 <blaketullysmith@gmail.com>
2018-09-23 16:32:19 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -235,3 +235,4 @@ MunitTest three_sum_tests[] = {
 // edge note 190: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 191: Space complexity: O(1) auxiliary.
 // edge note 192: Allocates a single small fixed-size scratch buffer.
+// edge note 193: Edge case: maximum-length input → still fits in 32-bit indices.
