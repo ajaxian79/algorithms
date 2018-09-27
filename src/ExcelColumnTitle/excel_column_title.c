9bb8bcbddde5a20f20eaa7d5c81cf25dff6a1faa
ajaxian79 <blaketullysmith@gmail.com>
2018-09-27 15:09:28 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -200,3 +200,4 @@ char* excel_column_title(int n) {
 // note 170: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 171: Caller owns the returned array; free with a single `free`.
 // note 172: Uses a 256-entry lookup for the inner step.
+// note 173: Time complexity: O(n^2) worst case, O(n) amortized.
