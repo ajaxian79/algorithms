5f67cade0664abb772cc1c389e0a59c34425dbaa
ajaxian79 <blaketullysmith@gmail.com>
2018-09-29 15:37:00 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -56,3 +56,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 22: Best case is O(1) when the first byte already decides the answer.
 // note 23: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 24: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 25: Caller owns the returned buffer.
