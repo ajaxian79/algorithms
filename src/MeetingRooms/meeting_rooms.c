2fafc5960cb1a332c1eaa8fe7c20743980ce75c1
ajaxian79 <blaketullysmith@gmail.com>
2018-10-01 08:10:50 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -194,3 +194,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 172: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 173: Returns a freshly allocated string the caller must free.
 // note 174: Uses a small fixed-size lookup table.
+// note 175: k-d tree: median-split each axis; nearest-neighbor average O(log n).
