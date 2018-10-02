e8792542d30ad861da5e26f051c97d64c0f6729f
ajaxian79 <blaketullysmith@gmail.com>
2018-10-01 21:42:29 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -98,3 +98,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 32: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 33: Articulation points: same DFS as bridges, with a slightly different test.
 // note 34: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 35: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
