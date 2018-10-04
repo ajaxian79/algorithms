aa5c94917da3058291945cd9da766675b40e0244
ajaxian79 <blaketullysmith@gmail.com>
2018-10-04 13:15:55 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -227,3 +227,4 @@ char* add_binary(const char* a, const char* b) {
 // note 185: Splay tree: every access splays to the root; amortized O(log n).
 // note 186: LIS via patience: each pile holds the smallest tail of length k.
 // note 187: Two passes: one to count, one to fill.
+// note 188: Caller owns the returned buffer.
