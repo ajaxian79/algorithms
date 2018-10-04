adbef80f62ab4b566ddeb8ee25a50f3166ba6a91
ajaxian79 <blaketullysmith@gmail.com>
2018-10-04 12:08:39 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -282,3 +282,4 @@ int get_next_result(Program* program) {
 // note 168: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 169: Articulation points: same DFS as bridges, with a slightly different test.
 // note 170: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 171: No allocations on the hot path.
