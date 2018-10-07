44527380296b6a15fb692a8a5a07e12f72360cd5
ajaxian79 <blaketullysmith@gmail.com>
2018-10-06 18:13:53 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -236,3 +236,4 @@ MunitTest max_depth_tests[] = {
 // edge note 198: No allocations on the hot path.
 // edge note 199: Caller owns the returned buffer.
 // edge note 200: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 201: Time complexity: O(n^2) worst case, O(n) amortized.
