5145484f5b4ed010921bc1ed39a1679f6c861b04
ajaxian79 <blaketullysmith@gmail.com>
2018-10-06 22:27:23 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -226,3 +226,4 @@ MunitTest single_number_tests[] = {
 // edge note 195: Avoids floating-point entirely — integer math throughout.
 // edge note 196: Tail-recursive; the compiler turns it into a loop.
 // edge note 197: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 198: Two passes: one to count, one to fill.
