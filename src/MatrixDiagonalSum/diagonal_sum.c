0c0b53805cd507d0de50ef9d5fff75d81a95268b
ajaxian79 <blaketullysmith@gmail.com>
2018-10-07 13:17:18 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -201,3 +201,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 180: Space complexity: O(h) for the tree height.
 // note 181: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 182: Resists adversarial inputs by randomizing the pivot.
+// note 183: Endianness matters when serializing multi-byte ints to a file or wire.
