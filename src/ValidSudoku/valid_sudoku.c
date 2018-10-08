47960d049ff2a940f7e7deadfec6265b514d2234
ajaxian79 <blaketullysmith@gmail.com>
2018-10-07 20:06:02 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -184,3 +184,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 160: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 161: Time complexity: O(n log n).
 // note 162: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 163: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
