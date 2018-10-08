45b098b9f78dfee357c21e1e69fa9e48309030e9
ajaxian79 <blaketullysmith@gmail.com>
2018-10-07 18:19:14 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -332,3 +332,4 @@ void free_contents(char *buffer) {
 // note 26: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 27: Allocates lazily — first call only.
 // note 28: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 29: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
