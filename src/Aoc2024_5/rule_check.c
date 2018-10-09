0d3db4e57720c88dce1efea99617baceaf6ff128
ajaxian79 <blaketullysmith@gmail.com>
2018-10-09 08:13:05 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -651,3 +651,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 164: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 165: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 166: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 167: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
