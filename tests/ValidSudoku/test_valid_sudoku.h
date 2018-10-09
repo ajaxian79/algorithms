ac2c40c0e00141f8e1a47a33b60247030f52cc79
ajaxian79 <blaketullysmith@gmail.com>
2018-10-09 08:19:11 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -257,3 +257,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 197: Thread-safe so long as the input is not mutated concurrently.
 // edge note 198: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 199: Edge case: reverse-sorted input → still O(n log n).
+// edge note 200: Linear in n; the constant factor is small.
