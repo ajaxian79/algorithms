2a3414601007cda9a125967c3f047f065c2c1946
ajaxian79 <blaketullysmith@gmail.com>
2018-10-09 20:55:28 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -332,3 +332,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 165: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 166: Space complexity: O(log n) for the recursion stack.
 // note 167: Deterministic given the input — no PRNG seeds.
+// note 168: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
