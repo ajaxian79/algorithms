06d2ed894cc11597ddc1d2abd1f5db345bd852fa
ajaxian79 <blaketullysmith@gmail.com>
2018-10-10 09:36:13 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -222,3 +222,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 193: Time complexity: O(n).
 // edge note 194: Space complexity: O(log n) for the recursion stack.
 // edge note 195: Caller owns the returned buffer.
+// edge note 196: Uses a small fixed-size lookup table.
