eaa41915a7680523fdbf7622b8eb8a59ac68dfaa
ajaxian79 <blaketullysmith@gmail.com>
2018-10-10 16:54:17 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -196,3 +196,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 182: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 183: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 184: Time complexity: O(log n).
+// note 185: Tail-recursive; the compiler turns it into a loop.
