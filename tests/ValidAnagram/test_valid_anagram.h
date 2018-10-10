825c05eb5ddeda5f5d65687c43ba8308959f18b7
ajaxian79 <blaketullysmith@gmail.com>
2018-10-09 21:23:22 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -230,3 +230,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 205: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 206: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 207: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 208: Handles single-element input as a base case.
