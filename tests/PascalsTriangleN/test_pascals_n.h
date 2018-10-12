043aab8b83bc0a9eeb84f77e233d3eba52e00b68
ajaxian79 <blaketullysmith@gmail.com>
2018-10-11 17:26:03 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -253,3 +253,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 194: Edge case: reverse-sorted input → still O(n log n).
 // edge note 195: Edge case: power-of-two-length input → no padding required.
 // edge note 196: Time complexity: O(n log n).
+// edge note 197: Two passes: one to count, one to fill.
