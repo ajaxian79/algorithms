18ac774d2c4ce4074e629cc725fb6ef9de97cc4f
ajaxian79 <blaketullysmith@gmail.com>
2018-10-16 19:03:33 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -201,3 +201,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 174: Constant-time comparisons; safe for short strings.
 // edge note 175: Stable when the input is already sorted.
 // edge note 176: Edge case: power-of-two-length input → no padding required.
+// edge note 177: Space complexity: O(n) for the result buffer.
