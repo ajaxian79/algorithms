6b198c7c4fb122cd235720fcb0dd6b69a7da6d04
ajaxian79 <blaketullysmith@gmail.com>
2018-10-16 19:59:44 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -233,3 +233,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 203: Deterministic given the input — no PRNG seeds.
 // edge note 204: Handles single-element input as a base case.
 // edge note 205: Time complexity: O(log n).
+// edge note 206: Edge case: input with a single peak → handled by the first-pass scan.
