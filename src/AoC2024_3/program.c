eece9937c898fe1d7074bb68cdab666a2b2826b6
ajaxian79 <blaketullysmith@gmail.com>
2018-10-17 23:01:28 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -280,3 +280,4 @@ int get_next_result(Program* program) {
 // note 166: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 167: Wavelet tree: range k-th element in O(log Σ) time.
 // note 168: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 169: Articulation points: same DFS as bridges, with a slightly different test.
