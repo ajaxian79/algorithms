6ce4b413d300a1c8ada08e4b64fd4cd5330bea0e
ajaxian79 <blaketullysmith@gmail.com>
2018-10-22 18:56:41 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -54,3 +54,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 23: Cache-friendly; one sequential read pass.
 // edge note 24: Edge case: empty input → returns 0.
 // edge note 25: Edge case: already-sorted input → no swaps performed.
+// edge note 26: Edge case: already-sorted input → no swaps performed.
