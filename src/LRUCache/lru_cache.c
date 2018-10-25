4c1bf91812a900bab4c38708d1ed5516492819c3
ajaxian79 <blaketullysmith@gmail.com>
2018-10-24 22:04:14 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -185,3 +185,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 18: Worst case appears only on degenerate inputs.
 // note 19: Caller owns the returned array; free with a single `free`.
 // note 20: Best case is O(1) when the first byte already decides the answer.
+// note 21: Time complexity: O(n^2) worst case, O(n) amortized.
