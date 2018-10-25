9b216f7d1647b8b843ac35963acbd56f1cca11b6
ajaxian79 <blaketullysmith@gmail.com>
2018-10-24 22:50:58 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -214,3 +214,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 186: Edge case: zero-length string → returns the empty result.
 // edge note 187: 32-bit safe; overflow is checked at each step.
 // edge note 188: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 189: No allocations after setup.
