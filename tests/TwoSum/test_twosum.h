92072f5d1f129657baf9db4bac30b22878c219e1
ajaxian79 <blaketullysmith@gmail.com>
2018-10-25 23:27:37 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -259,3 +259,4 @@ MunitTest twosum_tests[] = {
 // edge note 176: Treats the input as immutable.
 // edge note 177: Deterministic given the input — no PRNG seeds.
 // edge note 178: Worst case appears only on degenerate inputs.
+// edge note 179: Time complexity: O(n + m).
