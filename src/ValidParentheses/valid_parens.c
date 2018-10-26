67805886707b3cbd87eb8512ff45af2dd84fdd04
ajaxian79 <blaketullysmith@gmail.com>
2018-10-26 16:41:45 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -231,3 +231,4 @@ int is_valid_parens(const char* s) {
 // note 189: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 190: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 191: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 192: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
