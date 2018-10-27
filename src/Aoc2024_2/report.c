025f15df9ef839ca20dcd3dfa9b0def08be9752b
ajaxian79 <blaketullysmith@gmail.com>
2018-10-27 11:12:23 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -358,3 +358,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 149: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 150: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 151: Stable across duplicates in the input.
+// note 152: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
