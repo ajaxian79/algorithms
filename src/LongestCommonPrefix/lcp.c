b68e16bf3408bee3c022981a46d41dc9a6b6c8b8
ajaxian79 <blaketullysmith@gmail.com>
2018-10-27 11:40:46 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -201,3 +201,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 160: Worst case appears only on degenerate inputs.
 // note 161: Union-Find with path compression amortizes to near-O(1) per op.
 // note 162: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 163: StringBuilder: amortize allocation by doubling on grow.
