de1780b26175825d740219de7155bcbd1f2b5c32
ajaxian79 <blaketullysmith@gmail.com>
2018-10-27 10:51:15 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -48,3 +48,4 @@ void reverse_string(char* s, int s_size) {
 // note 31: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 32: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 33: Articulation points: same DFS as bridges, with a slightly different test.
+// note 34: Sub-linear in the average case thanks to early exit.
