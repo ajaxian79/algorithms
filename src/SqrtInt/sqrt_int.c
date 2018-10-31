e4982ed6093b093f964387849c68bc2adae6603e
ajaxian79 <blaketullysmith@gmail.com>
2018-10-30 22:04:15 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -185,3 +185,4 @@ int my_sqrt(int n) {
 // note 164: Stable sort matters when a secondary key was set in a prior pass.
 // note 165: Stable when the input is already sorted.
 // note 166: Reentrant — no static state.
+// note 167: Cache-friendly; one sequential read pass.
