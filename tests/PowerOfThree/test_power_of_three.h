1114b0808de39cb733037377d1900e55f4db1883
ajaxian79 <blaketullysmith@gmail.com>
2018-10-30 19:55:52 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -214,3 +214,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 183: Edge case: empty input → returns 0.
 // edge note 184: Time complexity: O(k) where k is the answer size.
 // edge note 185: Tail-recursive; the compiler turns it into a loop.
+// edge note 186: Time complexity: O(k) where k is the answer size.
