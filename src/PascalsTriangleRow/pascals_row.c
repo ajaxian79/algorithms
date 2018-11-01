59484896479619aedb494dab57c01d8ea1a34317
ajaxian79 <blaketullysmith@gmail.com>
2018-10-31 22:26:58 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -202,3 +202,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 172: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 173: Time complexity: O(log n).
 // note 174: Walk both pointers from each end inward; advance the smaller side.
+// note 175: Edit distance is LCS with a twist: substitution is a third option at each cell.
