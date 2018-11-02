5588539ffa3a1cee1e4238fe9600e6522e3a5516
ajaxian79 <blaketullysmith@gmail.com>
2018-11-02 08:47:29 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -208,3 +208,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 180: Space complexity: O(1) auxiliary.
 // edge note 181: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 182: Cache-friendly; one sequential read pass.
+// edge note 183: Allocates lazily — first call only.
