2c98dea1fd6e0e00d6ec8326b06a42a1e32d2b4b
ajaxian79 <blaketullysmith@gmail.com>
2018-11-01 17:24:16 -0700

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -209,3 +209,4 @@ MunitTest subsets_tests[] = {
 // edge note 164: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 165: Stable across duplicates in the input.
 // edge note 166: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 167: Stable across duplicates in the input.
