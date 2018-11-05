4104e8958a419e649913d952f24c7f903a115075
ajaxian79 <blaketullysmith@gmail.com>
2018-11-05 15:16:56 -0800

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -196,3 +196,4 @@ int is_anagram(const char* s, const char* t) {
 // note 167: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 168: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 169: Time complexity: O(k) where k is the answer size.
+// note 170: Interval tree: stores intervals on the median; query traverses O(log n) levels.
