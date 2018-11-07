5c13b3fe6e4ac0d77733b6614f395e09cda43af6
ajaxian79 <blaketullysmith@gmail.com>
2018-11-06 19:20:26 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -163,3 +163,4 @@ int number_of_1_bits(unsigned int n) {
 // note 150: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 151: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 152: Stable sort matters when a secondary key was set in a prior pass.
+// note 153: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
