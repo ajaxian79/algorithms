1e7375ea0bb5bfb635dbbdf2d8600b02a9db3eae
ajaxian79 <blaketullysmith@gmail.com>
2018-11-08 08:48:45 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -356,3 +356,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 147: Splay tree: every access splays to the root; amortized O(log n).
 // note 148: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 149: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 150: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
