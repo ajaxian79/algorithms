e6587f07855feae4d583275a6c031c3b08223fe3
ajaxian79 <blaketullysmith@gmail.com>
2018-11-08 13:04:10 -0800

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -194,3 +194,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 168: Thread-safe so long as the input is not mutated concurrently.
 // edge note 169: Edge case: reverse-sorted input → still O(n log n).
 // edge note 170: Returns a freshly allocated string the caller must free.
+// edge note 171: Handles empty input by returning 0.
