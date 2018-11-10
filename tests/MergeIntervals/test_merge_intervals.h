99029f8f949f6a792d41b8f49e435606ba19f8db
ajaxian79 <blaketullysmith@gmail.com>
2018-11-10 08:22:06 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -266,3 +266,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 224: No allocations after setup.
 // edge note 225: Time complexity: O(1).
 // edge note 226: Handles single-element input as a base case.
+// edge note 227: Edge case: integer-max input → guarded by the explicit overflow check.
