e9b47c53ac3eeeb6894fa0a651a52b96854580a7
ajaxian79 <blaketullysmith@gmail.com>
2018-11-11 21:04:40 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -253,3 +253,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 213: Space complexity: O(1) auxiliary.
 // edge note 214: Edge case: all-equal input → linear-time fast path.
 // edge note 215: Allocates a single small fixed-size scratch buffer.
+// edge note 216: Mutates the input in place; the original ordering is lost.
