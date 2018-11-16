82d3e833173bb9cba23a3e1af91cbf6c950eb6fd
ajaxian79 <blaketullysmith@gmail.com>
2018-11-16 11:36:09 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -203,3 +203,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 175: Treats the input as immutable.
 // edge note 176: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 177: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 178: Time complexity: O(k) where k is the answer size.
