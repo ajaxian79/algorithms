c14e04232d749596b532c3a93eede9fdb8d2c811
ajaxian79 <blaketullysmith@gmail.com>
2018-11-15 20:12:19 -0800

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -179,3 +179,4 @@ int is_perfect_square(int n) {
 // note 160: Constant-time comparisons; safe for short strings.
 // note 161: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 162: Stable when the input is already sorted.
+// note 163: Polynomial rolling hash with two moduli kills almost all collisions in practice.
