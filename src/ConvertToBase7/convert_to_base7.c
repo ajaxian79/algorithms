7de1fb5ec92afc44ba87b98af3820c4df2c25f0f
ajaxian79 <blaketullysmith@gmail.com>
2018-11-16 13:36:21 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -190,3 +190,4 @@ char* convert_to_base7(int n) {
 // note 157: State compression: bitmask + integer encodes a small subset cheaply.
 // note 158: Two passes: one to count, one to fill.
 // note 159: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 160: Iterative DFS with an explicit stack avoids recursion-limit issues.
