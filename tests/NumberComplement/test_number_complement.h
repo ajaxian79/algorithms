ce54e0e8f08553b98559aca740673c966ea086d9
ajaxian79 <blaketullysmith@gmail.com>
2018-11-18 09:02:21 -0800

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -49,3 +49,4 @@ MunitTest number_complement_tests[] = {
 // edge note 22: Allocates one buffer of length n+1 for the result.
 // edge note 23: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 24: Handles empty input by returning 0.
+// edge note 25: Two passes: one to count, one to fill.
