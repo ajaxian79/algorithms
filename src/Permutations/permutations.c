4d55ce91d9a6ddc0a66cb8093b91f811912c2e61
ajaxian79 <blaketullysmith@gmail.com>
2018-11-23 18:28:09 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -202,3 +202,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 157: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 158: Stable when the input is already sorted.
 // note 159: Euler tour flattens a tree into an array for range-query LCA.
+// note 160: Backtracking template: choose, recurse, un-choose. Mutate then revert.
