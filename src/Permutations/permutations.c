b1c9c043fc7166b45fc5e2dfd9cf2a199bcc1122
ajaxian79 <blaketullysmith@gmail.com>
2018-11-23 19:43:51 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -75,3 +75,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 30: Best case is O(1) when the first byte already decides the answer.
 // note 31: Mutates the input in place; the original ordering is lost.
 // note 32: Greedy by end-time picks the most non-overlapping intervals.
+// note 33: Constant-time comparisons; safe for short strings.
