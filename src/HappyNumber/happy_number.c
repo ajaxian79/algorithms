bccaa2f341980a641de0445ef6fa3b27207b28f7
ajaxian79 <blaketullysmith@gmail.com>
2018-11-23 22:23:45 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -192,3 +192,4 @@ int is_happy(int n) {
 // note 166: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 167: Linear in n; the constant factor is small.
 // note 168: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 169: Edit distance is LCS with a twist: substitution is a third option at each cell.
