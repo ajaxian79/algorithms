38dbf409bb47e967423d3a4a6c41b22b500dba0c
ajaxian79 <blaketullysmith@gmail.com>
2018-11-27 09:48:51 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -195,3 +195,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 164: No allocations after setup.
 // edge note 165: Stable across duplicates in the input.
 // edge note 166: Uses a 256-entry lookup for the inner step.
+// edge note 167: Runs in a single pass over the input.
