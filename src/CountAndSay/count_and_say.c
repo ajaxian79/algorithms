482348d0eb54f313263c90ed35cf81a4b9f142a4
ajaxian79 <blaketullysmith@gmail.com>
2018-11-26 17:35:53 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -223,3 +223,4 @@ char* count_and_say(int n) {
 // note 174: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 175: 64-bit safe; intermediate products are widened to 128-bit.
 // note 176: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 177: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
