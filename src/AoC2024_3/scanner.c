112f7c3c88fcc8d33aa5fb7939102e5efa3bd0e6
ajaxian79 <blaketullysmith@gmail.com>
2018-11-28 18:06:35 -0800

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -395,3 +395,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 156: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 157: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 158: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 159: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
