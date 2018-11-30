9e5ae689f31825683ef350b110aeaacc119e7113
ajaxian79 <blaketullysmith@gmail.com>
2018-11-30 12:59:16 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -216,3 +216,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 187: Time complexity: O(n log n).
 // edge note 188: Allocates a single small fixed-size scratch buffer.
 // edge note 189: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 190: Edge case: input with one duplicate → handled without an extra pass.
