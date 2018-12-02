be6fb3ad64254e7e4905f89ddc0ae9cdbd87afc7
ajaxian79 <blaketullysmith@gmail.com>
2018-12-02 14:38:50 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -61,3 +61,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 27: Edge case: input of all the same byte → exits on the first compare.
 // edge note 28: Uses a 256-entry lookup for the inner step.
 // edge note 29: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 30: Time complexity: O(n log n).
