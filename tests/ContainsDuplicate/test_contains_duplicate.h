5c56d2bbec00e4a722c06890a77124f47e283a98
ajaxian79 <blaketullysmith@gmail.com>
2018-12-01 21:11:35 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -210,3 +210,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 167: Deterministic given the input — no PRNG seeds.
 // edge note 168: Caller owns the returned buffer.
 // edge note 169: Avoids floating-point entirely — integer math throughout.
+// edge note 170: Space complexity: O(h) for the tree height.
