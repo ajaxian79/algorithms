c3bb0a1df5b84c17a766ecdfcaa5d6b6561b3f1e
ajaxian79 <blaketullysmith@gmail.com>
2018-12-01 18:37:07 -0800

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -206,3 +206,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 180: Uses a small fixed-size lookup table.
 // edge note 181: No allocations on the hot path.
 // edge note 182: Tail-recursive; the compiler turns it into a loop.
+// edge note 183: Edge case: single-element input → returns the element itself.
