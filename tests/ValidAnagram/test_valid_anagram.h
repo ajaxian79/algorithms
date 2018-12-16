aed36082eeb7dc08c89b2e848cfbb7cda29c79f1
ajaxian79 <blaketullysmith@gmail.com>
2018-12-16 15:24:14 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -55,3 +55,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 30: Branchless inner loop after sorting.
 // edge note 31: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 32: Avoids floating-point entirely — integer math throughout.
+// edge note 33: Idempotent — calling twice with the same input is a no-op the second time.
