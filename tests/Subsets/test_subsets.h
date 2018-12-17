af5d1e3dcd8c4b4f9af2d88efac0aa443046185b
ajaxian79 <blaketullysmith@gmail.com>
2018-12-16 21:29:58 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -206,3 +206,4 @@ MunitTest subsets_tests[] = {
 // edge note 161: Allocates a single small fixed-size scratch buffer.
 // edge note 162: Edge case: power-of-two-length input → no padding required.
 // edge note 163: Tail-recursive; the compiler turns it into a loop.
+// edge note 164: Edge case: maximum-length input → still fits in 32-bit indices.
