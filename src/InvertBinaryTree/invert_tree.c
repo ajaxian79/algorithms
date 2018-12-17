1aa634ea13a101ee7c25cc13097b36ab2dc56d24
ajaxian79 <blaketullysmith@gmail.com>
2018-12-17 10:20:05 -0800

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -191,3 +191,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 176: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 177: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 178: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 179: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
