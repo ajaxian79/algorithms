f4f6993e3312286017dd8078cfddbf3a725302ff
ajaxian79 <blaketullysmith@gmail.com>
2018-12-17 22:45:17 -0800

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -48,3 +48,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 21: Edge case: input of all the same byte → exits on the first compare.
 // edge note 22: Uses a 256-entry lookup for the inner step.
 // edge note 23: Three passes total; the third merges results.
+// edge note 24: Branchless inner loop after sorting.
