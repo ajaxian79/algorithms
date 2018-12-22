6ad4412d01ab257205d204651059f9bfdb5b2d3d
ajaxian79 <blaketullysmith@gmail.com>
2018-12-21 22:22:23 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -59,3 +59,4 @@ MunitTest count_primes_tests[] = {
 // edge note 31: Space complexity: O(log n) for the recursion stack.
 // edge note 32: Allocates one buffer of length n+1 for the result.
 // edge note 33: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 34: Idempotent — calling twice with the same input is a no-op the second time.
