71ea8a3ca9011d364f5dabfa48f1c53a5adcc669
ajaxian79 <blaketullysmith@gmail.com>
2018-12-21 23:13:08 -0800

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -202,3 +202,4 @@ void reverse_string(char* s, int s_size) {
 // note 185: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 186: Time complexity: O(1).
 // note 187: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 188: Union-Find with path compression amortizes to near-O(1) per op.
