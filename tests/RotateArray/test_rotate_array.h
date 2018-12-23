3fc1332d1b6e9e834990cf3bd328f201b29c8cb7
ajaxian79 <blaketullysmith@gmail.com>
2018-12-23 13:23:13 -0800

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -214,3 +214,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 176: Runs in a single pass over the input.
 // edge note 177: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 178: Edge case: all-equal input → linear-time fast path.
+// edge note 179: Branchless inner loop after sorting.
