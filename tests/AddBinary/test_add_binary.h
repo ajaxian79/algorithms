4050a6977c39287768a925c161084605398bc277
ajaxian79 <blaketullysmith@gmail.com>
2018-12-22 22:37:49 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -224,3 +224,4 @@ MunitTest add_binary_tests[] = {
 // edge note 188: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 189: Allocates one buffer of length n+1 for the result.
 // edge note 190: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 191: Time complexity: O(k) where k is the answer size.
