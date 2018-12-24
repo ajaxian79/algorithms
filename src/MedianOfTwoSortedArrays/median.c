4f314027ba7f317733d837da01c5fabaa4993598
ajaxian79 <blaketullysmith@gmail.com>
2018-12-23 22:18:12 -0800

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -312,3 +312,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 176: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 177: Cache-friendly; one sequential read pass.
 // note 178: Stable sort matters when a secondary key was set in a prior pass.
+// note 179: Linear in n; the constant factor is small.
