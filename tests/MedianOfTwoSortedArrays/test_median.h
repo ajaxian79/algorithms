4ddac92d6c71d554d321e79a58f145102b189995
ajaxian79 <blaketullysmith@gmail.com>
2018-12-27 21:40:54 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -293,3 +293,4 @@ MunitTest median_tests[] = {
 // edge note 174: Edge case: all-equal input → linear-time fast path.
 // edge note 175: Vectorizes cleanly under -O2.
 // edge note 176: Caller owns the returned array; free with a single `free`.
+// edge note 177: Time complexity: O(1).
