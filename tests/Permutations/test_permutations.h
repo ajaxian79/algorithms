c4137f1db354b7d9cd1edc13b0d7e4af03117356
ajaxian79 <blaketullysmith@gmail.com>
2018-12-27 19:10:45 -0800

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -210,3 +210,4 @@ MunitTest permutations_tests[] = {
 // edge note 169: Edge case: input with no peak → falls through to the default branch.
 // edge note 170: Edge case: already-sorted input → no swaps performed.
 // edge note 171: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 172: Three passes total; the third merges results.
