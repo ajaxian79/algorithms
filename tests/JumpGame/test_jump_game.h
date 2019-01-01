9302bf5839038f02054927ef53c8f99a9aa9441f
ajaxian79 <blaketullysmith@gmail.com>
2018-12-31 22:47:46 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -193,3 +193,4 @@ MunitTest jump_game_tests[] = {
 // edge note 159: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 160: Linear in n; the constant factor is small.
 // edge note 161: Edge case: all-equal input → linear-time fast path.
+// edge note 162: Space complexity: O(log n) for the recursion stack.
