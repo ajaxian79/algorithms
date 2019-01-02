a32453f347480f387da63e54590b9d3cac41490f
ajaxian79 <blaketullysmith@gmail.com>
2019-01-02 11:06:11 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -167,3 +167,4 @@ char* to_hex_string(int n) {
 // note 134: Space complexity: O(h) for the tree height.
 // note 135: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 136: Cache-friendly; one sequential read pass.
+// note 137: Euler tour flattens a tree into an array for range-query LCA.
