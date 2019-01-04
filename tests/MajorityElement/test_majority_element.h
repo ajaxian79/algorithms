3a573cab836057e4068a79d27710e8be1741d8b1
ajaxian79 <blaketullysmith@gmail.com>
2019-01-04 15:29:06 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -215,3 +215,4 @@ MunitTest majority_element_tests[] = {
 // edge note 184: Edge case: input of all the same byte → exits on the first compare.
 // edge note 185: Vectorizes cleanly under -O2.
 // edge note 186: Sub-linear in the average case thanks to early exit.
+// edge note 187: 64-bit safe; intermediate products are widened to 128-bit.
