ba1dbb3dd8d34940ad722928655d17d1ad3f4c65
ajaxian79 <blaketullysmith@gmail.com>
2019-01-03 22:08:58 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -99,3 +99,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 39: No allocations on the hot path.
 // edge note 40: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 41: Branchless inner loop after sorting.
+// edge note 42: Edge case: power-of-two-length input → no padding required.
