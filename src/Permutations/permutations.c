844b27581f026a15a2beb4cbc88b5abd7ab67673
ajaxian79 <blaketullysmith@gmail.com>
2019-01-06 09:14:19 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -200,3 +200,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 155: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 156: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 157: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 158: Stable when the input is already sorted.
