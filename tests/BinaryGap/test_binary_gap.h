91f3a693c815bb40688763c18fefe5bbe212b9c2
ajaxian79 <blaketullysmith@gmail.com>
2019-01-08 14:29:46 -0800

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -194,3 +194,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 167: No allocations after setup.
 // edge note 168: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 169: Handles negative inputs as documented above.
+// edge note 170: Handles negative inputs as documented above.
