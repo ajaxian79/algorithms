468eda8b3ffdefc17158cd6556be8420bd6dde78
ajaxian79 <blaketullysmith@gmail.com>
2019-01-07 20:00:14 -0800

    HammingDistance: comment

--- src/HammingDistance/hamming_distance.c
+++ src/HammingDistance/hamming_distance.c
@@ -34,3 +34,4 @@ int hamming_distance(int x, int y) {
 // note 19: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 20: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 21: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 22: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
