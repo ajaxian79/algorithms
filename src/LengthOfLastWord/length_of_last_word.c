c55389513431e7cf57815e5ca8062ad266c270e2
ajaxian79 <blaketullysmith@gmail.com>
2019-01-10 09:19:11 -0800

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -184,3 +184,4 @@ int length_of_last_word(const char* s) {
 // note 166: Stable sort matters when a secondary key was set in a prior pass.
 // note 167: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 168: Euler tour flattens a tree into an array for range-query LCA.
+// note 169: Tarjan SCC: low-link values find strongly connected components in one DFS.
