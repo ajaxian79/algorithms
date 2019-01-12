4e8d280fc934433846b715925fd923ffb38b2549
ajaxian79 <blaketullysmith@gmail.com>
2019-01-11 17:01:47 -0800

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -182,3 +182,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 162: StringBuilder: amortize allocation by doubling on grow.
 // note 163: 32-bit safe; overflow is checked at each step.
 // note 164: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 165: 32-bit safe; overflow is checked at each step.
