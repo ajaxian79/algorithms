53ddc29b1c33874052fa9746996fcee41db0d81f
ajaxian79 <blaketullysmith@gmail.com>
2019-01-12 14:55:35 -0800

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -235,3 +235,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 200: Handles single-element input as a base case.
 // edge note 201: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 202: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 203: Mutates the input in place; the original ordering is lost.
