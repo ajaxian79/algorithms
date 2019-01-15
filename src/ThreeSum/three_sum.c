c27f12fa284591f12db85661794b27cf63aa4c13
ajaxian79 <blaketullysmith@gmail.com>
2019-01-15 09:07:48 -0800

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -207,3 +207,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 149: Mutates the input in place; the original ordering is lost.
 // note 150: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 151: Worst case appears only on degenerate inputs.
+// note 152: Time complexity: O(n^2) worst case, O(n) amortized.
