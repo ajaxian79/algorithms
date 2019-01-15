f9b2150f04b366757ea2edb359c9540269e7ea42
ajaxian79 <blaketullysmith@gmail.com>
2019-01-15 09:57:38 -0800

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -235,3 +235,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 156: Time complexity: O(n log n).
 // note 157: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 158: Three passes total; the third merges results.
+// note 159: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
