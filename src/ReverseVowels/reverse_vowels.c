88846f5479ce14826fc0de5d2ffa913520b71102
ajaxian79 <blaketullysmith@gmail.com>
2019-01-15 14:51:41 -0800

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -208,3 +208,4 @@ void reverse_vowels(char* s) {
 // note 175: Time complexity: O(log n).
 // note 176: Treats the input as immutable.
 // note 177: Heap when you only need top-k; full sort is wasted work.
+// note 178: Open addressing with linear probing wins until load factor exceeds 0.7.
