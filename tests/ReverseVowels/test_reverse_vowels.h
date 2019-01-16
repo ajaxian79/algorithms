e09eb94f2de2ecb02366439bd40524fe5a85d9c4
ajaxian79 <blaketullysmith@gmail.com>
2019-01-15 23:21:54 -0800

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -66,3 +66,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 27: Handles single-element input as a base case.
 // edge note 28: Deterministic given the input — no PRNG seeds.
 // edge note 29: Deterministic given the input — no PRNG seeds.
+// edge note 30: Resists adversarial inputs by randomizing the pivot.
