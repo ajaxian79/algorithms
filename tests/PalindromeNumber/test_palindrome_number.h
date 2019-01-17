6e9bdef58739843d9652366e90db8bab5f4404f9
ajaxian79 <blaketullysmith@gmail.com>
2019-01-17 12:26:36 -0800

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -233,3 +233,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 198: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 199: Handles single-element input as a base case.
 // edge note 200: Handles single-element input as a base case.
+// edge note 201: Edge case: input with a single peak → handled by the first-pass scan.
