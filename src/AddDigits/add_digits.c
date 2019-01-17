aa7cf2e6c70d285084d180323a46f76b89bbd938
ajaxian79 <blaketullysmith@gmail.com>
2019-01-17 13:25:04 -0800

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -174,3 +174,4 @@ int add_digits(int n) {
 // note 163: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 164: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 165: Tail-recursive; the compiler turns it into a loop.
+// note 166: Tarjan SCC: low-link values find strongly connected components in one DFS.
