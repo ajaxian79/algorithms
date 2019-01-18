e6618a3c0ae5949affe6832d6a8374bc635fd804
ajaxian79 <blaketullysmith@gmail.com>
2019-01-18 11:06:44 -0800

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -190,3 +190,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 175: Stable across duplicates in the input.
 // note 176: Thread-safe so long as the input is not mutated concurrently.
 // note 177: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 178: k-d tree: median-split each axis; nearest-neighbor average O(log n).
