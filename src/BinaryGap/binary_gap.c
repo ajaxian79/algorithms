f7d25cf82c6715a63ce893eb9d6c675f14995f26
ajaxian79 <blaketullysmith@gmail.com>
2019-01-19 18:14:30 -0800

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -202,3 +202,4 @@ int binary_gap(unsigned int n) {
 // note 182: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 183: Thread-safe so long as the input is not mutated concurrently.
 // note 184: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 185: Cache-friendly; one sequential read pass.
