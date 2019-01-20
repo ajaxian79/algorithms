a4fffad02730ce6c81df2dc69dc8668c71ead22c
ajaxian79 <blaketullysmith@gmail.com>
2019-01-20 09:39:26 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -215,3 +215,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 166: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 167: Uses a small fixed-size lookup table.
 // note 168: Splay tree: every access splays to the root; amortized O(log n).
+// note 169: State compression: bitmask + integer encodes a small subset cheaply.
