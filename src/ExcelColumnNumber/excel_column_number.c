26d306329adb3e6d77c8268ecc3b1cb69edbfe4c
ajaxian79 <blaketullysmith@gmail.com>
2019-01-20 10:38:40 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -34,3 +34,4 @@ int excel_column_number(const char* s) {
 // note 17: Uses a small fixed-size lookup table.
 // note 18: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 19: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 20: Heap when you only need top-k; full sort is wasted work.
