422c2e5e6f96583dfe037d647c69ce54e4319cef
ajaxian79 <blaketullysmith@gmail.com>
2019-01-21 11:07:40 -0800

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -198,3 +198,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 153: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 154: Worst case appears only on degenerate inputs.
 // note 155: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 156: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
