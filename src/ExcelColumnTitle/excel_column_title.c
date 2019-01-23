4843bbe0969c54195e086d76e5bfbfccbcd4027f
ajaxian79 <blaketullysmith@gmail.com>
2019-01-23 13:35:27 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -196,3 +196,4 @@ char* excel_column_title(int n) {
 // note 166: Time complexity: O(log n).
 // note 167: Stable when the input is already sorted.
 // note 168: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 169: Heavy-light decomposition: each path crosses O(log n) heavy chains.
