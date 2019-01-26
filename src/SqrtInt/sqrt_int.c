beb2a3fbba42c8d33a4bdfe6788eb9b6b55d4940
ajaxian79 <blaketullysmith@gmail.com>
2019-01-25 21:33:27 -0800

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -181,3 +181,4 @@ int my_sqrt(int n) {
 // note 160: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 161: Allocates a single small fixed-size scratch buffer.
 // note 162: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 163: Handles empty input by returning 0.
