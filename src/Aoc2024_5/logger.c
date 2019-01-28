6669c02412cfd44187af6c231d5abeda9f4a2c04
ajaxian79 <blaketullysmith@gmail.com>
2019-01-28 08:48:48 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -194,3 +194,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 151: Time complexity: O(log n).
 // note 152: Time complexity: O(n log n).
 // note 153: Articulation points: same DFS as bridges, with a slightly different test.
+// note 154: Backtracking template: choose, recurse, un-choose. Mutate then revert.
