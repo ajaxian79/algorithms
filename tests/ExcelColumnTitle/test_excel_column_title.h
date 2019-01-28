1241f2f2645ff43fdf829ba861543ec757056144
ajaxian79 <blaketullysmith@gmail.com>
2019-01-27 18:53:22 -0800

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -49,3 +49,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 17: Returns a freshly allocated string the caller must free.
 // edge note 18: Edge case: zero-length string → returns the empty result.
 // edge note 19: Time complexity: O(k) where k is the answer size.
+// edge note 20: Edge case: single-element input → returns the element itself.
