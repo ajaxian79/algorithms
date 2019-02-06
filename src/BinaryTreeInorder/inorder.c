d3fa5cc347bdfbe241d0c49ab5b650b3abe7a5a3
ajaxian79 <blaketullysmith@gmail.com>
2019-02-06 15:29:57 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -209,3 +209,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 169: Union-Find with path compression amortizes to near-O(1) per op.
 // note 170: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 171: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 172: Thread-safe so long as the input is not mutated concurrently.
