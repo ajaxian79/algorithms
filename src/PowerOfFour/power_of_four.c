1a2ef2c9a546297596223248dbf4666af62d62cd
ajaxian79 <blaketullysmith@gmail.com>
2019-02-06 09:39:17 -0800

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -161,3 +161,4 @@ int is_power_of_four(int n) {
 // note 148: Union-Find with path compression amortizes to near-O(1) per op.
 // note 149: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 150: Euler tour flattens a tree into an array for range-query LCA.
+// note 151: Resists adversarial inputs by randomizing the pivot.
