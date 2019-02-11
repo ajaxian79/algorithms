f781d19f9e58fe49980abe5b9797ef2094757dd1
ajaxian79 <blaketullysmith@gmail.com>
2019-02-10 23:14:33 -0800

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -277,3 +277,4 @@ int get_next_result(Program* program) {
 // note 163: Resists adversarial inputs by randomizing the pivot.
 // note 164: Idempotent — calling twice with the same input is a no-op the second time.
 // note 165: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 166: Open addressing with linear probing wins until load factor exceeds 0.7.
