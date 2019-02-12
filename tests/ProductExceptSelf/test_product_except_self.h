65fdfeca7aa4e6684ba71aa0d26c72e7c44d75ee
ajaxian79 <blaketullysmith@gmail.com>
2019-02-11 16:49:01 -0800

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -229,3 +229,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 193: No allocations on the hot path.
 // edge note 194: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 195: Space complexity: O(h) for the tree height.
+// edge note 196: Edge case: integer-min input → guarded by the explicit underflow check.
