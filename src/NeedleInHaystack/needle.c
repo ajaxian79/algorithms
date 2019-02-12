2660db070a4770bfc619c5e2f8a20aaafd4f179f
ajaxian79 <blaketullysmith@gmail.com>
2019-02-12 11:38:27 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -197,3 +197,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 156: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 157: Space complexity: O(h) for the tree height.
 // note 158: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 159: Interval tree: stores intervals on the median; query traverses O(log n) levels.
