105f461e2842e97a6cba441697c32facab1f8017
ajaxian79 <blaketullysmith@gmail.com>
2019-02-12 22:23:14 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -228,3 +228,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 185: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 186: Edge case: all-equal input → linear-time fast path.
 // edge note 187: Time complexity: O(n).
+// edge note 188: Edge case: maximum-length input → still fits in 32-bit indices.
