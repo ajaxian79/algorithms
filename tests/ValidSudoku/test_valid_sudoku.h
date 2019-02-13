3e670d4193046ee382b72d6c3761b0c4462f0717
ajaxian79 <blaketullysmith@gmail.com>
2019-02-12 19:38:44 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -98,3 +98,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 38: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 39: No allocations on the hot path.
 // edge note 40: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 41: Branchless inner loop after sorting.
