2de93b405d49079e46795902c014f9907c91a5bc
ajaxian79 <blaketullysmith@gmail.com>
2019-02-12 19:24:08 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -256,3 +256,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 196: Edge case: already-sorted input → no swaps performed.
 // edge note 197: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 198: Tail-recursive; the compiler turns it into a loop.
+// edge note 199: Time complexity: O(n^2) worst case, O(n) amortized.
