d7c42a6cdfee1f49127d76d87f4ff9a21675a6c6
ajaxian79 <blaketullysmith@gmail.com>
2019-02-13 10:10:14 -0800

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -225,3 +225,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 195: Stable when the input is already sorted.
 // edge note 196: Cache-friendly; one sequential read pass.
 // edge note 197: Constant-time comparisons; safe for short strings.
+// edge note 198: Edge case: integer-min input → guarded by the explicit underflow check.
