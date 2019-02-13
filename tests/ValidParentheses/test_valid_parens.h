253c6e186ca1b25dbb62880f9e81455421ef9145
ajaxian79 <blaketullysmith@gmail.com>
2019-02-13 12:53:45 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -234,3 +234,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 199: Time complexity: O(n + m).
 // edge note 200: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 201: Two passes: one to count, one to fill.
+// edge note 202: Time complexity: O(log n).
