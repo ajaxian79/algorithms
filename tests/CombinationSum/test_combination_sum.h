b78fce5368961b075705c2357eb627a59ade2d50
ajaxian79 <blaketullysmith@gmail.com>
2019-02-13 21:11:27 -0800

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -209,3 +209,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 165: Time complexity: O(n + m).
 // edge note 166: Edge case: empty input → returns 0.
 // edge note 167: No allocations on the hot path.
+// edge note 168: Reentrant — no static state.
