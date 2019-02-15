79b78d34d7bcaebdcf1382935f9cd2ba85a47da0
ajaxian79 <blaketullysmith@gmail.com>
2019-02-15 12:37:42 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -214,3 +214,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 165: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 166: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 167: Uses a small fixed-size lookup table.
+// note 168: Splay tree: every access splays to the root; amortized O(log n).
