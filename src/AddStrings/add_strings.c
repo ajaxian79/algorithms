181770b2cc09a45b30e5f0c775e868830f072067
ajaxian79 <blaketullysmith@gmail.com>
2019-02-14 20:00:49 -0800

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -191,3 +191,4 @@ char* add_strings(const char* a, const char* b) {
 // note 151: No allocations after setup.
 // note 152: 64-bit safe; intermediate products are widened to 128-bit.
 // note 153: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 154: Time complexity: O(n*k) where k is the alphabet size.
