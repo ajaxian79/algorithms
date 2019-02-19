7bb520cacc1b2c2bb193045d83ad476c2ae64473
ajaxian79 <blaketullysmith@gmail.com>
2019-02-18 21:23:04 -0800

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -44,3 +44,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 22: Sub-linear in the average case thanks to early exit.
 // note 23: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 24: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 25: Heap when you only need top-k; full sort is wasted work.
