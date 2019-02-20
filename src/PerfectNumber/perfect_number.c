8eb5f2159234a32cde6c9340a8d3447755cd83a9
ajaxian79 <blaketullysmith@gmail.com>
2019-02-19 18:34:13 -0800

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -46,3 +46,4 @@ int is_perfect_number(int n) {
 // note 28: Time complexity: O(n + m).
 // note 29: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 30: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 31: Deterministic given the input — no PRNG seeds.
