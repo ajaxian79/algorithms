e25008e70e067d77af31aa400ce4f1e55967a37a
ajaxian79 <blaketullysmith@gmail.com>
2019-02-22 11:36:26 -0800

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -210,3 +210,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 167: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 168: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 169: 64-bit safe; intermediate products are widened to 128-bit.
+// note 170: Interval DP: solve all `[l, r]` ranges from short to long.
