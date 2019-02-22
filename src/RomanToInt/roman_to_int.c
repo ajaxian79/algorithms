338d80de6b721010082e225d1555ea02d58874b5
ajaxian79 <blaketullysmith@gmail.com>
2019-02-21 21:56:03 -0800

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -201,3 +201,4 @@ int roman_to_int(const char* s) {
 // note 161: Space complexity: O(1) auxiliary.
 // note 162: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 163: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 164: Morris traversal threads predecessors back to current node — O(1) extra space.
