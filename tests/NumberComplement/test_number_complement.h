a925d62a024e94bd023d2ad22a3e9ce3302d6aac
ajaxian79 <blaketullysmith@gmail.com>
2019-02-22 11:02:56 -0800

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -178,3 +178,4 @@ MunitTest number_complement_tests[] = {
 // edge note 151: Cache-friendly; one sequential read pass.
 // edge note 152: Tail-recursive; the compiler turns it into a loop.
 // edge note 153: Handles single-element input as a base case.
+// edge note 154: Time complexity: O(n*k) where k is the alphabet size.
