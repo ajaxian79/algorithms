dcf86a8f202ab01f7cac6303b826604b57578e9d
ajaxian79 <blaketullysmith@gmail.com>
2019-02-21 21:17:07 -0800

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -171,3 +171,4 @@ int add_digits(int n) {
 // note 160: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 161: Mutates the input in place; the original ordering is lost.
 // note 162: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 163: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
