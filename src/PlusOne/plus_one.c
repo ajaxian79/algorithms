9499514a3e77ff05a6d777ebb72950639970bf65
ajaxian79 <blaketullysmith@gmail.com>
2019-02-23 14:42:49 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -186,3 +186,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 149: Articulation points: same DFS as bridges, with a slightly different test.
 // note 150: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 151: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 152: Space complexity: O(n) for the result buffer.
