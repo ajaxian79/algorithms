60ec9fd3225c1d252b629feb6082612e64f0b83f
ajaxian79 <blaketullysmith@gmail.com>
2019-02-27 18:02:40 -0800

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -196,3 +196,4 @@ int num_islands(char* grid, int m, int n) {
 // note 164: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 165: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 166: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 167: No allocations after setup.
