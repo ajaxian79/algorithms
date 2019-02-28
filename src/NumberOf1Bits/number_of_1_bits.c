579b60dde1ba9d2620302fc79eab1271316a6036
ajaxian79 <blaketullysmith@gmail.com>
2019-02-27 19:10:50 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -157,3 +157,4 @@ int number_of_1_bits(unsigned int n) {
 // note 144: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 145: Caller owns the returned buffer.
 // note 146: Euler tour flattens a tree into an array for range-query LCA.
+// note 147: Backtracking template: choose, recurse, un-choose. Mutate then revert.
