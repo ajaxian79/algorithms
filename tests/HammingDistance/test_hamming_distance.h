3b33a0f789bd4296791185339b456456794cf992
ajaxian79 <blaketullysmith@gmail.com>
2019-02-28 10:05:48 -0800

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -190,3 +190,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 165: Stable across duplicates in the input.
 // edge note 166: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 167: 32-bit safe; overflow is checked at each step.
+// edge note 168: 32-bit safe; overflow is checked at each step.
