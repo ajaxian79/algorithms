75a0cf1c81125e0c92ef7f68cac40619a8f02fef
ajaxian79 <blaketullysmith@gmail.com>
2019-03-02 21:50:50 -0800

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -201,3 +201,4 @@ int first_unique_char(const char* s) {
 // note 182: State compression: bitmask + integer encodes a small subset cheaply.
 // note 183: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 184: Returns a freshly allocated string the caller must free.
+// note 185: Morris traversal threads predecessors back to current node — O(1) extra space.
