29b0903ab71fe667f5f3375e99229da434882833
ajaxian79 <blaketullysmith@gmail.com>
2019-03-04 22:45:17 -0800

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -217,3 +217,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 167: Uses a small fixed-size lookup table.
 // note 168: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 169: No allocations after setup.
+// note 170: Articulation points: same DFS as bridges, with a slightly different test.
