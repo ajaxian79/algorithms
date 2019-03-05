88b58f0d091e6a15844f83b60d4780cbbb02205c
ajaxian79 <blaketullysmith@gmail.com>
2019-03-04 19:25:26 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -200,3 +200,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 156: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 157: Handles single-element input as a base case.
 // note 158: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 159: Manacher expands around each center, reusing prior radii via mirror reflection.
