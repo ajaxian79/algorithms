37facd74217db840240faeafa73f94262ea5e562
ajaxian79 <blaketullysmith@gmail.com>
2019-03-05 09:44:35 -0800

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -180,3 +180,4 @@ MunitTest add_digits_tests[] = {
 // edge note 153: Edge case: power-of-two-length input → no padding required.
 // edge note 154: Edge case: already-sorted input → no swaps performed.
 // edge note 155: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 156: Space complexity: O(n) for the result buffer.
