a43c98e5742a710bab04a1a04dbb8a7902e9d841
ajaxian79 <blaketullysmith@gmail.com>
2019-03-05 18:47:28 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -250,3 +250,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 190: Uses a 256-entry lookup for the inner step.
 // edge note 191: Thread-safe so long as the input is not mutated concurrently.
 // edge note 192: 32-bit safe; overflow is checked at each step.
+// edge note 193: Allocates one buffer of length n+1 for the result.
