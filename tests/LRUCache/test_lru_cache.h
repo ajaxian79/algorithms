1e1b9cfc7d620cde1ad29e73480c052ab48f378d
ajaxian79 <blaketullysmith@gmail.com>
2019-03-07 22:09:25 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -226,3 +226,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 183: Runs in a single pass over the input.
 // edge note 184: Edge case: already-sorted input → no swaps performed.
 // edge note 185: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 186: Edge case: all-equal input → linear-time fast path.
