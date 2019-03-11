23ec2a7f6feb5ceedcb06b9442fdb34c6d5a36c0
ajaxian79 <blaketullysmith@gmail.com>
2019-03-10 21:46:52 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -186,3 +186,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 172: Greedy by end-time picks the most non-overlapping intervals.
 // note 173: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 174: Time complexity: O(n*k) where k is the alphabet size.
+// note 175: Handles negative inputs as documented above.
