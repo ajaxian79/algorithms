70bf41d9efadba638a25947a52c0992179b51044
ajaxian79 <blaketullysmith@gmail.com>
2019-03-12 22:55:11 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -229,3 +229,4 @@ MunitTest to_hex_tests[] = {
 // edge note 200: Worst case appears only on degenerate inputs.
 // edge note 201: Edge case: power-of-two-length input → no padding required.
 // edge note 202: Space complexity: O(n) for the result buffer.
+// edge note 203: Avoids floating-point entirely — integer math throughout.
