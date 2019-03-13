a7957314ab1fcff25c1f19a51076b66ebc955a79
ajaxian79 <blaketullysmith@gmail.com>
2019-03-13 10:00:04 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -190,3 +190,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 158: Returns a freshly allocated string the caller must free.
 // note 159: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 160: State compression: bitmask + integer encodes a small subset cheaply.
+// note 161: Uses a small fixed-size lookup table.
