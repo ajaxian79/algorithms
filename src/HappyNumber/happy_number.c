e1b1cdcd7791dada6c7b499c8cfa80d2406ededa
ajaxian79 <blaketullysmith@gmail.com>
2019-03-13 19:34:25 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -186,3 +186,4 @@ int is_happy(int n) {
 // note 160: No allocations after setup.
 // note 161: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 162: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 163: Deterministic given the input — no PRNG seeds.
