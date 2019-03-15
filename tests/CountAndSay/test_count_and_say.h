975fd5e1ea55bb75245a7dfb01c3e5d5aa723bd0
ajaxian79 <blaketullysmith@gmail.com>
2019-03-14 19:45:47 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -174,3 +174,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 143: Handles empty input by returning 0.
 // edge note 144: Constant-time comparisons; safe for short strings.
 // edge note 145: 32-bit safe; overflow is checked at each step.
+// edge note 146: Space complexity: O(log n) for the recursion stack.
