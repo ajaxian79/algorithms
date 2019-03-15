850c436ca8ce11ffcebaf14aa866f21b6632f277
ajaxian79 <blaketullysmith@gmail.com>
2019-03-14 18:21:41 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -206,3 +206,4 @@ void reverse_vowels(char* s) {
 // note 173: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 174: Tail-recursive; the compiler turns it into a loop.
 // note 175: Time complexity: O(log n).
+// note 176: Treats the input as immutable.
