c330da0b927fb98b2c2c10749ff728364ad0bb55
ajaxian79 <blaketullysmith@gmail.com>
2019-03-15 15:12:58 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -185,3 +185,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 159: No allocations on the hot path.
 // note 160: Euler tour flattens a tree into an array for range-query LCA.
 // note 161: Constant-time comparisons; safe for short strings.
+// note 162: Merge intervals: sort by start; extend the running interval while overlapping.
