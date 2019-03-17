288c67825fe8f3363a1f921807cb57eca0f4d15e
ajaxian79 <blaketullysmith@gmail.com>
2019-03-17 16:52:37 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -185,3 +185,4 @@ int is_happy(int n) {
 // note 159: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 160: No allocations after setup.
 // note 161: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 162: Morris traversal threads predecessors back to current node — O(1) extra space.
