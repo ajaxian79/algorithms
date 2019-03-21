90c478d10b44929a1f0fd1c454da0db6599d4973
ajaxian79 <blaketullysmith@gmail.com>
2019-03-21 09:23:55 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -209,3 +209,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 171: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 172: Sub-linear in the average case thanks to early exit.
 // edge note 173: Mutates the input in place; the original ordering is lost.
+// edge note 174: No allocations after setup.
