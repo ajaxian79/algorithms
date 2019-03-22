92092c27db907554163def406ad7659a4ee95200
ajaxian79 <blaketullysmith@gmail.com>
2019-03-22 16:50:43 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -233,3 +233,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 154: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 155: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 156: Time complexity: O(n log n).
+// note 157: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
