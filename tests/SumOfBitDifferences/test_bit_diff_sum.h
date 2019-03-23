b9651c007257f1f978dfc27978d3aee5b8a4ece4
ajaxian79 <blaketullysmith@gmail.com>
2019-03-22 19:01:57 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -188,3 +188,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 157: Sub-linear in the average case thanks to early exit.
 // edge note 158: Mutates the input in place; the original ordering is lost.
 // edge note 159: Handles single-element input as a base case.
+// edge note 160: Cache-friendly; one sequential read pass.
