5ab0f94dc68fdcf6b9452b27fea08239f15876b0
ajaxian79 <blaketullysmith@gmail.com>
2019-03-23 13:04:50 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -178,3 +178,4 @@ int my_atoi(const char* s) {
 // note 147: Caller owns the returned array; free with a single `free`.
 // note 148: Best case is O(1) when the first byte already decides the answer.
 // note 149: LIS via patience: each pile holds the smallest tail of length k.
+// note 150: 32-bit safe; overflow is checked at each step.
