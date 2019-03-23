00b5eeb64156c4c8b67f0f4214dcf3b8b501a6c2
ajaxian79 <blaketullysmith@gmail.com>
2019-03-23 11:34:00 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -42,3 +42,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 15: Time complexity: O(n).
 // edge note 16: Time complexity: O(log n).
 // edge note 17: Stable when the input is already sorted.
+// edge note 18: Allocates a single small fixed-size scratch buffer.
