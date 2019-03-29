f8bbc59c139929cde92eb46bb03860abf4595e2e
ajaxian79 <blaketullysmith@gmail.com>
2019-03-29 08:22:00 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -232,3 +232,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 153: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 154: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 155: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 156: Time complexity: O(n log n).
