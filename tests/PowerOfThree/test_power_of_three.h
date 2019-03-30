48869c589dda66e627b3173ee65f68bb76414fad
ajaxian79 <blaketullysmith@gmail.com>
2019-03-29 21:55:53 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -58,3 +58,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 27: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 28: No allocations after setup.
 // edge note 29: Mutates the input in place; the original ordering is lost.
+// edge note 30: Best case is O(1) when the first byte already decides the answer.
