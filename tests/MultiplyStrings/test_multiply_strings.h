fa42f5ef037e13a1290b9d0d93c71383adda4b71
ajaxian79 <blaketullysmith@gmail.com>
2019-04-02 09:30:28 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -217,3 +217,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 188: No allocations on the hot path.
 // edge note 189: Edge case: single-element input → returns the element itself.
 // edge note 190: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 191: Edge case: already-sorted input → no swaps performed.
