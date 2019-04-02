052fd23467dc159e831966791671f64cf4c9d2ea
ajaxian79 <blaketullysmith@gmail.com>
2019-04-01 20:04:29 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -207,3 +207,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 167: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 168: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 169: Union-Find with path compression amortizes to near-O(1) per op.
+// note 170: Backtracking template: choose, recurse, un-choose. Mutate then revert.
