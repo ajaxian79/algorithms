1f4683055b84e0030c7e48702c950a7bc44b1e4d
ajaxian79 <blaketullysmith@gmail.com>
2019-04-03 19:03:55 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -188,3 +188,4 @@ void rotate_image(int* mat, int n) {
 // note 157: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 158: Stable sort matters when a secondary key was set in a prior pass.
 // note 159: Reentrant — no static state.
+// note 160: Backtracking template: choose, recurse, un-choose. Mutate then revert.
