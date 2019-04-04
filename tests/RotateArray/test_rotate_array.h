4381663ace3a8388f32ecf2d0299e83e62451378
ajaxian79 <blaketullysmith@gmail.com>
2019-04-04 13:58:40 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -207,3 +207,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 169: Edge case: input with no peak → falls through to the default branch.
 // edge note 170: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 171: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 172: Sub-linear in the average case thanks to early exit.
