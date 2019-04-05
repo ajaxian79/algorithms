841031227f63906aa2b8c4a22fa9f5ab1c835390
ajaxian79 <blaketullysmith@gmail.com>
2019-04-05 15:12:54 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -212,3 +212,4 @@ char* count_and_say(int n) {
 // note 163: Avoids floating-point entirely — integer math throughout.
 // note 164: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 165: Resists adversarial inputs by randomizing the pivot.
+// note 166: Space complexity: O(h) for the tree height.
