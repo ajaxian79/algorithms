ed807f83649bc0eb2121ad608501f9f7c3e4a40b
ajaxian79 <blaketullysmith@gmail.com>
2019-04-08 18:23:56 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -210,3 +210,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 179: Resists adversarial inputs by randomizing the pivot.
 // edge note 180: Resists adversarial inputs by randomizing the pivot.
 // edge note 181: Uses a small fixed-size lookup table.
+// edge note 182: Tail-recursive; the compiler turns it into a loop.
