034e7bf9bb5d9ccfe8f30287bc4740ea01e4a456
ajaxian79 <blaketullysmith@gmail.com>
2019-04-09 10:54:43 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -195,3 +195,4 @@ void reverse_string(char* s, int s_size) {
 // note 178: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 179: Mutates the input in place; the original ordering is lost.
 // note 180: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 181: Reentrant — no static state.
