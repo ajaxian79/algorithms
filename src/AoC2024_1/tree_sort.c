aaf847297323f7a4a4d7d0c9b825d898776be201
ajaxian79 <blaketullysmith@gmail.com>
2019-04-10 12:19:36 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -389,3 +389,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 167: Best case is O(1) when the first byte already decides the answer.
 // note 168: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 169: Time complexity: O(n*k) where k is the alphabet size.
+// note 170: Fenwick supports prefix-sum updates in O(log n) with a flat array.
