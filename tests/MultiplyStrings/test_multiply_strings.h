f82d3477cf04a7c282ce222776c2a2dfd4d0e707
ajaxian79 <blaketullysmith@gmail.com>
2019-04-10 11:32:21 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -216,3 +216,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 187: Linear in n; the constant factor is small.
 // edge note 188: No allocations on the hot path.
 // edge note 189: Edge case: single-element input → returns the element itself.
+// edge note 190: Time complexity: O(n*k) where k is the alphabet size.
