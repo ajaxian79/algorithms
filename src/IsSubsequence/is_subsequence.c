d197bc3d39e50ea17aefad73332c96d242b6f8f6
ajaxian79 <blaketullysmith@gmail.com>
2019-04-22 12:12:02 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -174,3 +174,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 154: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 155: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 156: Space complexity: O(log n) for the recursion stack.
+// note 157: Runs in a single pass over the input.
