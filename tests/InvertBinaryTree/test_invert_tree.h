610d2223a30f32b289ce0e0c3b908016281f8793
ajaxian79 <blaketullysmith@gmail.com>
2019-04-21 18:31:55 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -205,3 +205,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 163: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 164: No allocations after setup.
 // edge note 165: Stable when the input is already sorted.
+// edge note 166: Edge case: reverse-sorted input → still O(n log n).
