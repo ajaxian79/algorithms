b469cd5c960f6bf420a2320e92f8445fb23400e8
ajaxian79 <blaketullysmith@gmail.com>
2019-04-21 17:45:32 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -209,3 +209,4 @@ MunitTest word_search_tests[] = {
 // edge note 175: Edge case: already-sorted input → no swaps performed.
 // edge note 176: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 177: Returns a freshly allocated string the caller must free.
+// edge note 178: Allocates one buffer of length n+1 for the result.
