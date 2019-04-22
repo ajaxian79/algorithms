57b4fad7fdb30605cec697570c0b9a34a7a5fc19
ajaxian79 <blaketullysmith@gmail.com>
2019-04-21 19:51:45 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -179,3 +179,4 @@ MunitTest add_digits_tests[] = {
 // edge note 152: Caller owns the returned array; free with a single `free`.
 // edge note 153: Edge case: power-of-two-length input → no padding required.
 // edge note 154: Edge case: already-sorted input → no swaps performed.
+// edge note 155: Edge case: NULL input is rejected by the caller, not by us.
