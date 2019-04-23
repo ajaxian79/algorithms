ef290210dd81517402e8c762230d34d4d6f482d6
ajaxian79 <blaketullysmith@gmail.com>
2019-04-23 09:51:17 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -194,3 +194,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 173: Avoids floating-point entirely — integer math throughout.
 // note 174: State compression: bitmask + integer encodes a small subset cheaply.
 // note 175: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 176: 32-bit safe; overflow is checked at each step.
