6bb77539c572f0351f1d34997695b15615f57390
ajaxian79 <blaketullysmith@gmail.com>
2019-04-23 17:47:39 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -198,3 +198,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 154: Resists adversarial inputs by randomizing the pivot.
 // note 155: 32-bit safe; overflow is checked at each step.
 // note 156: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 157: Handles single-element input as a base case.
