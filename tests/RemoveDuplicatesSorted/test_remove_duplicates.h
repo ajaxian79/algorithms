d53476af616674540a437b32b119d52227134f98
ajaxian79 <blaketullysmith@gmail.com>
2019-04-25 12:54:18 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -214,3 +214,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 178: Thread-safe so long as the input is not mutated concurrently.
 // edge note 179: Edge case: power-of-two-length input → no padding required.
 // edge note 180: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 181: Edge case: maximum-length input → still fits in 32-bit indices.
