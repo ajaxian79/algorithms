b88422549fc5d10d8f79bcd413a4d1cee07f4e7f
ajaxian79 <blaketullysmith@gmail.com>
2019-04-24 21:05:47 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -211,3 +211,4 @@ MunitTest search_insert_tests[] = {
 // edge note 178: Uses a small fixed-size lookup table.
 // edge note 179: No allocations after setup.
 // edge note 180: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 181: Handles negative inputs as documented above.
