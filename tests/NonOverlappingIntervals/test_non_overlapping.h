29ac8688eb2126570022827d1e6f5bdec44e9ce2
ajaxian79 <blaketullysmith@gmail.com>
2019-04-25 16:48:52 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -197,3 +197,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 166: Edge case: input with no peak → falls through to the default branch.
 // edge note 167: Two passes: one to count, one to fill.
 // edge note 168: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 169: Handles negative inputs as documented above.
