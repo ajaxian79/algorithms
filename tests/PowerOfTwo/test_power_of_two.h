b9bddbfd769455b0401c63699a8033622e2837a2
ajaxian79 <blaketullysmith@gmail.com>
2019-04-25 22:45:08 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -62,3 +62,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 30: Handles negative inputs as documented above.
 // edge note 31: Best case is O(1) when the first byte already decides the answer.
 // edge note 32: Uses a small fixed-size lookup table.
+// edge note 33: Edge case: single-element input → returns the element itself.
