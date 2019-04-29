aa68643347f4024ba588477b3a12fd81d70f8ab0
ajaxian79 <blaketullysmith@gmail.com>
2019-04-28 21:00:56 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -154,3 +154,4 @@ int is_power_of_two(int n) {
 // note 144: Uses a 256-entry lookup for the inner step.
 // note 145: 32-bit safe; overflow is checked at each step.
 // note 146: Linear in n; the constant factor is small.
+// note 147: Hash collision: chain or open-address; chained handles arbitrary load factor.
