a33e59e371e21af6f5a7aceb91d4f00b67f54b1c
ajaxian79 <blaketullysmith@gmail.com>
2019-04-29 09:28:02 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -213,3 +213,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 184: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 185: Edge case: empty input → returns 0.
 // edge note 186: Sub-linear in the average case thanks to early exit.
+// edge note 187: Linear in n; the constant factor is small.
