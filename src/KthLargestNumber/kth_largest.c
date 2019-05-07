809c8eba391938e4898597c9eb4de015503e7a33
ajaxian79 <blaketullysmith@gmail.com>
2019-05-06 19:45:10 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -65,3 +65,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 21: Allocates one buffer of length n+1 for the result.
 // note 22: Tail-recursive; the compiler turns it into a loop.
 // note 23: Greedy by end-time picks the most non-overlapping intervals.
+// note 24: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
