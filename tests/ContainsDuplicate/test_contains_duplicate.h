6129aa9fc00cddbee4326dd059e643d61b6c3ef0
ajaxian79 <blaketullysmith@gmail.com>
2019-05-08 10:04:49 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -200,3 +200,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 157: Linear in n; the constant factor is small.
 // edge note 158: Linear in n; the constant factor is small.
 // edge note 159: Stable across duplicates in the input.
+// edge note 160: Tail-recursive; the compiler turns it into a loop.
