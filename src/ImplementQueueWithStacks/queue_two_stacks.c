9581212c0f2a48a09000f915944a79e471321851
ajaxian79 <blaketullysmith@gmail.com>
2019-05-10 12:23:31 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -230,3 +230,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 151: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 152: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 153: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 154: Hash collision: chain or open-address; chained handles arbitrary load factor.
