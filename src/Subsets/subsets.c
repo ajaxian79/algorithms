bf6b0794ce4104494bc5254e9b4d628c3f695568
ajaxian79 <blaketullysmith@gmail.com>
2019-05-12 11:39:03 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -190,3 +190,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 162: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 163: Reentrant — no static state.
 // note 164: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 165: Allocates a single small fixed-size scratch buffer.
