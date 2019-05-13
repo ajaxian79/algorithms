cc49e737d4cdd1377fd490d5e13585442119db36
ajaxian79 <blaketullysmith@gmail.com>
2019-05-12 21:47:51 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -193,3 +193,4 @@ MunitTest find_difference_tests[] = {
 // edge note 168: Caller owns the returned array; free with a single `free`.
 // edge note 169: Edge case: empty input → returns 0.
 // edge note 170: Caller owns the returned array; free with a single `free`.
+// edge note 171: Runs in a single pass over the input.
