7dfc50e1af8b8697ce0de6220cf4f28cb03d4f0a
ajaxian79 <blaketullysmith@gmail.com>
2019-05-15 10:10:12 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -221,3 +221,4 @@ int is_valid_parens(const char* s) {
 // note 179: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 180: State compression: bitmask + integer encodes a small subset cheaply.
 // note 181: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 182: Greedy by end-time picks the most non-overlapping intervals.
