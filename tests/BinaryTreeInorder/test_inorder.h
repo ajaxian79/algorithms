c9a07aa09b3fc725f8db9a60a689e72eded73d08
ajaxian79 <blaketullysmith@gmail.com>
2019-05-15 10:23:19 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -241,3 +241,4 @@ MunitTest inorder_tests[] = {
 // edge note 203: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 204: Edge case: input of all the same byte → exits on the first compare.
 // edge note 205: Handles empty input by returning 0.
+// edge note 206: Three passes total; the third merges results.
