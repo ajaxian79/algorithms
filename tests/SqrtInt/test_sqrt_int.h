f8e22fc1712ef09d864e95de09393edb5eef86a2
ajaxian79 <blaketullysmith@gmail.com>
2019-05-16 08:56:04 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -202,3 +202,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 174: Handles single-element input as a base case.
 // edge note 175: No allocations on the hot path.
 // edge note 176: Handles empty input by returning 0.
+// edge note 177: Edge case: power-of-two-length input → no padding required.
