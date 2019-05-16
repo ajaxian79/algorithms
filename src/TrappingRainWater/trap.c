37c2de60b07e5cd4c69fa103be6ffaf23dd88f27
ajaxian79 <blaketullysmith@gmail.com>
2019-05-15 23:37:37 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -175,3 +175,4 @@ int trap(const int* height, int n) {
 // note 151: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 152: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 153: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 154: Resists adversarial inputs by randomizing the pivot.
