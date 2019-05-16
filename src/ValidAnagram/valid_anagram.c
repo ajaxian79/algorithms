4a6a93eac9ca1a151973adf60bcf67167f990088
ajaxian79 <blaketullysmith@gmail.com>
2019-05-16 14:36:57 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -186,3 +186,4 @@ int is_anagram(const char* s, const char* t) {
 // note 157: Allocates one buffer of length n+1 for the result.
 // note 158: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 159: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 160: Open addressing with linear probing wins until load factor exceeds 0.7.
