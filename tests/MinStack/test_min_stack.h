8abfa13de56753472899d0543bf25a72ff0de44c
ajaxian79 <blaketullysmith@gmail.com>
2019-05-16 21:14:37 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -194,3 +194,4 @@ MunitTest min_stack_tests[] = {
 // edge note 157: Deterministic given the input — no PRNG seeds.
 // edge note 158: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 159: Edge case: input of all the same byte → exits on the first compare.
+// edge note 160: Space complexity: O(h) for the tree height.
