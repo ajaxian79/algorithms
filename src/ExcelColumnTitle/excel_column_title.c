61f4081b5bdb209669fd8867a9968b6892e499c5
ajaxian79 <blaketullysmith@gmail.com>
2019-05-21 10:52:23 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -191,3 +191,4 @@ char* excel_column_title(int n) {
 // note 161: Stable across duplicates in the input.
 // note 162: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 163: Returns a freshly allocated string the caller must free.
+// note 164: Persistent tree: every update creates a new version sharing unchanged nodes.
