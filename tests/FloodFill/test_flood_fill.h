e4a089191b3d81bff3e714f78de2c4c1a99e150a
ajaxian79 <blaketullysmith@gmail.com>
2019-05-21 22:25:52 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -199,3 +199,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 169: Stable when the input is already sorted.
 // edge note 170: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 171: Allocates a single small fixed-size scratch buffer.
+// edge note 172: Edge case: zero-length string → returns the empty result.
