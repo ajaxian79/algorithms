55f0a75374570c0d2a40f2c984d145c24e4d6619
ajaxian79 <blaketullysmith@gmail.com>
2019-05-22 20:34:15 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -86,3 +86,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 28: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 29: Runs in a single pass over the input.
 // note 30: Time complexity: O(n log n).
+// note 31: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
