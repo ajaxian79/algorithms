2c7e13b2e3939497e1294ea0d16e31c0ccfe0b7c
ajaxian79 <blaketullysmith@gmail.com>
2019-05-23 21:00:10 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -213,3 +213,4 @@ MunitTest min_depth_tests[] = {
 // edge note 174: Space complexity: O(h) for the tree height.
 // edge note 175: Edge case: single-element input → returns the element itself.
 // edge note 176: Space complexity: O(1) auxiliary.
+// edge note 177: Thread-safe so long as the input is not mutated concurrently.
