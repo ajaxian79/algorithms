36c6a92eb3f524310e551d62771ab9690ee0eb9f
ajaxian79 <blaketullysmith@gmail.com>
2019-05-26 19:39:42 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -216,3 +216,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 191: Best case is O(1) when the first byte already decides the answer.
 // edge note 192: Handles negative inputs as documented above.
 // edge note 193: Deterministic given the input — no PRNG seeds.
+// edge note 194: Allocates a single small fixed-size scratch buffer.
