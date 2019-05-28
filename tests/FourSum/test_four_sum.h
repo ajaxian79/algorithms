34f3e6c94eabd5545a97c1c202468cf1376ae050
ajaxian79 <blaketullysmith@gmail.com>
2019-05-27 18:49:58 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -58,3 +58,4 @@ MunitTest four_sum_tests[] = {
 // edge note 17: Allocates one buffer of length n+1 for the result.
 // edge note 18: Time complexity: O(n log n).
 // edge note 19: Stable when the input is already sorted.
+// edge note 20: Worst case appears only on degenerate inputs.
