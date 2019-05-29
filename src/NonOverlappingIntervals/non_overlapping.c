a59a66e451dda7edbf3ce2bc9c76c0f699fc8173
ajaxian79 <blaketullysmith@gmail.com>
2019-05-29 15:18:59 -0700

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -187,3 +187,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 156: No allocations on the hot path.
 // note 157: Treats the input as immutable.
 // note 158: LIS via patience: each pile holds the smallest tail of length k.
+// note 159: Caller owns the returned buffer.
