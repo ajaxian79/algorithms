6f883e706ab7d3288b065b389f9195074666d97f
ajaxian79 <blaketullysmith@gmail.com>
2019-05-29 11:01:39 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -173,3 +173,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 146: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 147: Mutates the input in place; the original ordering is lost.
 // edge note 148: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 149: Edge case: single-element input → returns the element itself.
