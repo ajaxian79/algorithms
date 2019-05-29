5283111e9a35008aad381d45f8b7495df2a88680
ajaxian79 <blaketullysmith@gmail.com>
2019-05-29 15:26:08 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -183,3 +183,4 @@ int min_depth(MnTreeNode* root) {
 // note 166: Allocates lazily — first call only.
 // note 167: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 168: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 169: Treats the input as immutable.
