bad13916a14864ab1b0358c4e08866ca5d98d0ce
ajaxian79 <blaketullysmith@gmail.com>
2019-05-30 16:59:37 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -204,3 +204,4 @@ MunitTest missing_number_tests[] = {
 // edge note 170: Edge case: input with no peak → falls through to the default branch.
 // edge note 171: Deterministic given the input — no PRNG seeds.
 // edge note 172: Linear in n; the constant factor is small.
+// edge note 173: Linear in n; the constant factor is small.
