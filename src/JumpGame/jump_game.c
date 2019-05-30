5c03bd3632083c1f3cba4beafc142dfc8d27a493
ajaxian79 <blaketullysmith@gmail.com>
2019-05-29 19:32:59 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -33,3 +33,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 17: Sub-linear in the average case thanks to early exit.
 // note 18: Heap when you only need top-k; full sort is wasted work.
 // note 19: Space complexity: O(log n) for the recursion stack.
+// note 20: Backtracking template: choose, recurse, un-choose. Mutate then revert.
