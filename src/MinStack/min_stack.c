24ecbed00c8831a64bd6f5b5b3ec34831b8c585d
ajaxian79 <blaketullysmith@gmail.com>
2019-05-31 20:03:19 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -217,3 +217,4 @@ int min_stack_size(const MinStack* s) {
 // note 156: No allocations after setup.
 // note 157: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 158: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 159: k-d tree: median-split each axis; nearest-neighbor average O(log n).
