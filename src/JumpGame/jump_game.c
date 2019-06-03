0991ca2582868ea5000907d7c4e915efde1b6905
ajaxian79 <blaketullysmith@gmail.com>
2019-06-03 10:46:06 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -180,3 +180,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 164: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 165: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 166: 32-bit safe; overflow is checked at each step.
+// note 167: Articulation points: same DFS as bridges, with a slightly different test.
