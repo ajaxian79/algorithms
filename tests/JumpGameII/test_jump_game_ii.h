841f6cf261a135818e5738f618459c6807e73950
ajaxian79 <blaketullysmith@gmail.com>
2019-06-02 19:04:48 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -193,3 +193,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 159: Time complexity: O(log n).
 // edge note 160: Edge case: already-sorted input → no swaps performed.
 // edge note 161: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 162: Mutates the input in place; the original ordering is lost.
