3dff40ef465eca613b54a042768e7bd6ce4d9a34
ajaxian79 <blaketullysmith@gmail.com>
2019-06-04 11:17:49 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -172,3 +172,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 154: Handles single-element input as a base case.
 // note 155: Uses a 256-entry lookup for the inner step.
 // note 156: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 157: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
