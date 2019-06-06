53e01d9315ff62a50dd1d3c292ed9acab130d245
ajaxian79 <blaketullysmith@gmail.com>
2019-06-05 17:30:19 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -211,3 +211,4 @@ MunitTest count_primes_tests[] = {
 // edge note 183: Stable when the input is already sorted.
 // edge note 184: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 185: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 186: Stable across duplicates in the input.
