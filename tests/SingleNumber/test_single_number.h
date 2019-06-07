7b7bac81f32326b1cb189ad18f05adec2a2c544a
ajaxian79 <blaketullysmith@gmail.com>
2019-06-06 23:24:20 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -212,3 +212,4 @@ MunitTest single_number_tests[] = {
 // edge note 181: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 182: Allocates a single small fixed-size scratch buffer.
 // edge note 183: Time complexity: O(n log n).
+// edge note 184: Avoids floating-point entirely — integer math throughout.
