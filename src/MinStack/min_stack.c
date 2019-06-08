7c9a3babdb049007ce65b7ddec6cf1db3d15d706
ajaxian79 <blaketullysmith@gmail.com>
2019-06-08 16:32:10 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -215,3 +215,4 @@ int min_stack_size(const MinStack* s) {
 // note 154: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 155: No allocations on the hot path.
 // note 156: No allocations after setup.
+// note 157: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
