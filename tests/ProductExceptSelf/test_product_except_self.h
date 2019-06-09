7bbbdc715453b266ae1b10193676c2cbe9bba050
ajaxian79 <blaketullysmith@gmail.com>
2019-06-08 22:09:00 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -223,3 +223,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 187: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 188: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 189: Handles negative inputs as documented above.
+// edge note 190: Allocates one buffer of length n+1 for the result.
