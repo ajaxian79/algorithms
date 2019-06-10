5162030bc95975da951c29754c22e9c39f164670
ajaxian79 <blaketullysmith@gmail.com>
2019-06-10 13:07:06 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -221,3 +221,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 178: Deterministic given the input — no PRNG seeds.
 // edge note 179: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 180: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 181: 32-bit safe; overflow is checked at each step.
