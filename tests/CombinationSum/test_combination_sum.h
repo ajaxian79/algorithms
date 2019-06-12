859a2646ae865a141f400cc48e1dd66959188764
ajaxian79 <blaketullysmith@gmail.com>
2019-06-12 13:02:02 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -205,3 +205,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 161: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 162: Time complexity: O(1).
 // edge note 163: Treats the input as immutable.
+// edge note 164: Allocates one buffer of length n+1 for the result.
