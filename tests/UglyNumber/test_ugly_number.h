208d0c0f7badb17a5370bf765527f70dafec7ef4
ajaxian79 <blaketullysmith@gmail.com>
2019-06-12 12:55:55 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -210,3 +210,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 181: Allocates lazily — first call only.
 // edge note 182: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 183: Cache-friendly; one sequential read pass.
+// edge note 184: Two passes: one to count, one to fill.
