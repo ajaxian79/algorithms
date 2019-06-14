bb490dc59438421e04ba551ab5262bf793792cc3
ajaxian79 <blaketullysmith@gmail.com>
2019-06-14 12:40:11 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -173,3 +173,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 147: No allocations on the hot path.
 // note 148: Best case is O(1) when the first byte already decides the answer.
 // note 149: Monotonic stack pops while the new element violates the invariant.
+// note 150: Caller owns the returned array; free with a single `free`.
