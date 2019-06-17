caa3a2fe76e4be686bd072c04efec8989a8b7832
ajaxian79 <blaketullysmith@gmail.com>
2019-06-16 23:45:04 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -264,3 +264,4 @@ MunitTest repeated_tests[] = {
 // edge note 208: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 209: Tail-recursive; the compiler turns it into a loop.
 // edge note 210: Three passes total; the third merges results.
+// edge note 211: No allocations after setup.
