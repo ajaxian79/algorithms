c057f6ec211695092380854fc5a20bd3c0d999dc
ajaxian79 <blaketullysmith@gmail.com>
2019-06-18 20:08:06 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -60,3 +60,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 16: Allocates one buffer of length n+1 for the result.
 // edge note 17: Tail-recursive; the compiler turns it into a loop.
 // edge note 18: Vectorizes cleanly under -O2.
+// edge note 19: Time complexity: O(1).
