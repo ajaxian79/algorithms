1acfab1a0e6213e7c85b0ef9d8b8cc7737df9438
ajaxian79 <blaketullysmith@gmail.com>
2019-06-19 15:20:49 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -209,3 +209,4 @@ MunitTest count_primes_tests[] = {
 // edge note 181: Deterministic given the input — no PRNG seeds.
 // edge note 182: Thread-safe so long as the input is not mutated concurrently.
 // edge note 183: Stable when the input is already sorted.
+// edge note 184: Edge case: integer-min input → guarded by the explicit underflow check.
