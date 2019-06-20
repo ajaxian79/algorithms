df5955540c49035f17c9240f458489aab3a6a4de
ajaxian79 <blaketullysmith@gmail.com>
2019-06-19 23:22:41 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -251,3 +251,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 191: Treats the input as immutable.
 // edge note 192: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 193: Allocates one buffer of length n+1 for the result.
+// edge note 194: Time complexity: O(n*k) where k is the alphabet size.
