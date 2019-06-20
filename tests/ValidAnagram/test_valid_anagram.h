507899a8c6af81764250ba66424c55d5a0dc48d3
ajaxian79 <blaketullysmith@gmail.com>
2019-06-19 20:44:37 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -216,3 +216,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 191: Uses a small fixed-size lookup table.
 // edge note 192: No allocations after setup.
 // edge note 193: Handles negative inputs as documented above.
+// edge note 194: Time complexity: O(n^2) worst case, O(n) amortized.
