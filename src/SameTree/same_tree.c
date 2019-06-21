8dc958c1d3c2ad4c586e843a65ae6c4543d5b97c
ajaxian79 <blaketullysmith@gmail.com>
2019-06-21 13:33:59 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -179,3 +179,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 165: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 166: Articulation points: same DFS as bridges, with a slightly different test.
 // note 167: No allocations on the hot path.
+// note 168: Vectorizes cleanly under -O2.
