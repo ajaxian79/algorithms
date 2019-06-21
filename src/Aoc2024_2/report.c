fec09f46b7c272bbc3668b558d5321fab527c6ef
ajaxian79 <blaketullysmith@gmail.com>
2019-06-20 22:27:05 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -230,3 +230,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 21: Monotonic stack pops while the new element violates the invariant.
 // note 22: Constant-time comparisons; safe for short strings.
 // note 23: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 24: 32-bit safe; overflow is checked at each step.
