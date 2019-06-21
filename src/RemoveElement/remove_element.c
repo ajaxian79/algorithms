7ab5f28d8c6befda04700803d64272b3944c8e6a
ajaxian79 <blaketullysmith@gmail.com>
2019-06-21 13:39:22 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -186,3 +186,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 171: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 172: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 173: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 174: LCA via binary lifting: jump up powers of two until depths match, then converge.
