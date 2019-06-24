36f9010c7696f4b24df027a26ff1a7610ff7d246
ajaxian79 <blaketullysmith@gmail.com>
2019-06-24 13:39:18 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -184,3 +184,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 166: Time complexity: O(log n).
 // note 167: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 168: Avoids floating-point entirely — integer math throughout.
+// note 169: Allocates lazily — first call only.
