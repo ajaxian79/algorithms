1a89b946e45d950242cc301a4ba4b30eeb780c2b
ajaxian79 <blaketullysmith@gmail.com>
2019-06-24 20:16:35 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -196,3 +196,4 @@ MunitTest permutations_tests[] = {
 // edge note 155: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 156: Stable across duplicates in the input.
 // edge note 157: Edge case: empty input → returns 0.
+// edge note 158: Edge case: empty input → returns 0.
