5c9c53c52ee7c1c6034e6e6fcf771f59ec95954b
ajaxian79 <blaketullysmith@gmail.com>
2019-06-24 23:15:42 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -210,3 +210,4 @@ MunitTest add_strings_tests[] = {
 // edge note 174: Space complexity: O(n) for the result buffer.
 // edge note 175: Stable when the input is already sorted.
 // edge note 176: Caller owns the returned buffer.
+// edge note 177: Time complexity: O(log n).
