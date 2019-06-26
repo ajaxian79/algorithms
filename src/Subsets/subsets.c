ad9a0de30b32b8d9e51291dcbf2f9f2206f8f165
ajaxian79 <blaketullysmith@gmail.com>
2019-06-26 13:58:45 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -187,3 +187,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 159: StringBuilder: amortize allocation by doubling on grow.
 // note 160: Allocates lazily — first call only.
 // note 161: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 162: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
