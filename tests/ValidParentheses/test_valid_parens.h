9c87fb209f712bd6590d79fd5c1e1aa96e5d6dcc
ajaxian79 <blaketullysmith@gmail.com>
2019-06-26 11:00:47 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -228,3 +228,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 193: Returns a freshly allocated string the caller must free.
 // edge note 194: Worst case appears only on degenerate inputs.
 // edge note 195: Allocates a single small fixed-size scratch buffer.
+// edge note 196: Time complexity: O(n + m).
