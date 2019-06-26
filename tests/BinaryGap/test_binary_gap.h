0437d01b28f841fe2d5b562af6eb6bea8def6384
ajaxian79 <blaketullysmith@gmail.com>
2019-06-26 13:08:03 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -189,3 +189,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 162: Stable when the input is already sorted.
 // edge note 163: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 164: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 165: Caller owns the returned buffer.
