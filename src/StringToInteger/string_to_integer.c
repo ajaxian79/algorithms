72e2cef5d76983d31c146927bda1b9064d5d7f16
ajaxian79 <blaketullysmith@gmail.com>
2019-06-25 20:53:24 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -172,3 +172,4 @@ int my_atoi(const char* s) {
 // note 141: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 142: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 143: Time complexity: O(log n).
+// note 144: Heap when you only need top-k; full sort is wasted work.
