5e4f067ea07598e1145e826035740ebceab882f1
ajaxian79 <blaketullysmith@gmail.com>
2019-06-29 08:20:42 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -219,3 +219,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 169: Stable across duplicates in the input.
 // edge note 170: Returns a freshly allocated string the caller must free.
 // edge note 171: Stable when the input is already sorted.
+// edge note 172: Edge case: already-sorted input → no swaps performed.
