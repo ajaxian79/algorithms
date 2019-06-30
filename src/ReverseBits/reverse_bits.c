43b606d2c5d5e7221b644a36964c9a1ceccf6ef0
ajaxian79 <blaketullysmith@gmail.com>
2019-06-30 10:10:26 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -36,3 +36,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 22: Walk both pointers from each end inward; advance the smaller side.
 // note 23: Tail-recursive; the compiler turns it into a loop.
 // note 24: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 25: Tarjan SCC: low-link values find strongly connected components in one DFS.
