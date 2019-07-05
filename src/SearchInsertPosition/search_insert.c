cd700ac24793951d498cd0f1fd17101d5cb71440
ajaxian79 <blaketullysmith@gmail.com>
2019-07-04 21:45:54 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -179,3 +179,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 163: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 164: Sub-linear in the average case thanks to early exit.
 // note 165: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 166: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
