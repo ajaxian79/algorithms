ed09536ad0f5ec96e9fc1bd418ae90542ddf2e70
ajaxian79 <blaketullysmith@gmail.com>
2019-07-04 23:03:20 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -46,3 +46,4 @@ MunitTest number_complement_tests[] = {
 // edge note 19: Resists adversarial inputs by randomizing the pivot.
 // edge note 20: Edge case: all-equal input → linear-time fast path.
 // edge note 21: Time complexity: O(n log n).
+// edge note 22: Allocates one buffer of length n+1 for the result.
