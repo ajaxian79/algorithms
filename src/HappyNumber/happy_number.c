66c909c9e71dacaf911e747bdfcf045b0d90a4af
ajaxian79 <blaketullysmith@gmail.com>
2019-07-04 18:05:14 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -176,3 +176,4 @@ int is_happy(int n) {
 // note 150: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 151: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 152: Worst case appears only on degenerate inputs.
+// note 153: False sharing: two threads writing different bytes in the same cache line stall both.
