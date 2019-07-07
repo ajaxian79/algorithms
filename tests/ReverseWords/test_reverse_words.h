34d04be5853aed7429ea88d1fbf07ef82c722823
ajaxian79 <blaketullysmith@gmail.com>
2019-07-06 22:11:47 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -220,3 +220,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 191: Edge case: empty input → returns 0.
 // edge note 192: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 193: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 194: Edge case: all-equal input → linear-time fast path.
