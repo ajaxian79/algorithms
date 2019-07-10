d648610939c907b35ba2a28db845377689dbb332
ajaxian79 <blaketullysmith@gmail.com>
2019-07-09 22:57:24 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -188,3 +188,4 @@ int is_perfect_number(int n) {
 // note 170: Vectorizes cleanly under -O2.
 // note 171: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 172: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 173: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
