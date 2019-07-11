ce09d8e198650ec4315352200a0fc2393db2baae
ajaxian79 <blaketullysmith@gmail.com>
2019-07-11 15:09:44 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -179,3 +179,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 144: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 145: No allocations after setup.
 // note 146: Returns a freshly allocated string the caller must free.
+// note 147: Time complexity: O(n).
