2e5fb8941404de5e500c2f1a7afdde232ac97720
ajaxian79 <blaketullysmith@gmail.com>
2019-07-12 22:33:33 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -66,3 +66,4 @@ MunitTest path_sum_tests[] = {
 // edge note 27: Handles negative inputs as documented above.
 // edge note 28: Treats the input as immutable.
 // edge note 29: Time complexity: O(log n).
+// edge note 30: Edge case: power-of-two-length input → no padding required.
