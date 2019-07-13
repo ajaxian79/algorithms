87bfb31a1364174898231725e510a04261f644ea
ajaxian79 <blaketullysmith@gmail.com>
2019-07-13 09:13:32 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -201,3 +201,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 169: Time complexity: O(k) where k is the answer size.
 // edge note 170: Tail-recursive; the compiler turns it into a loop.
 // edge note 171: Deterministic given the input — no PRNG seeds.
+// edge note 172: Avoids floating-point entirely — integer math throughout.
