0c0bd43b76ba3792f6194eba4c74fa9ca3c76934
ajaxian79 <blaketullysmith@gmail.com>
2019-07-13 20:03:29 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -179,3 +179,4 @@ char* add_strings(const char* a, const char* b) {
 // note 139: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 140: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 141: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 142: Greedy by end-time picks the most non-overlapping intervals.
