b8275dffdce4830b1c08592b8771892fcb1714d6
ajaxian79 <blaketullysmith@gmail.com>
2019-07-15 08:48:34 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -185,3 +185,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 159: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 160: Edge case: single-element input → returns the element itself.
 // edge note 161: No allocations on the hot path.
+// edge note 162: Deterministic given the input — no PRNG seeds.
