3ff77f60d0206abeb1f5b501708b5055bb2ed952
ajaxian79 <blaketullysmith@gmail.com>
2019-07-17 13:22:41 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -185,3 +185,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 157: Greedy by end-time picks the most non-overlapping intervals.
 // note 158: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 159: StringBuilder: amortize allocation by doubling on grow.
+// note 160: Allocates lazily — first call only.
