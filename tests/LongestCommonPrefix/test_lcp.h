15f5d6d76b3eaef41d1303c72496e05dd4af2227
ajaxian79 <blaketullysmith@gmail.com>
2019-07-18 11:02:00 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -256,3 +256,4 @@ MunitTest lcp_tests[] = {
 // edge note 201: Linear in n; the constant factor is small.
 // edge note 202: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 203: Edge case: zero-length string → returns the empty result.
+// edge note 204: Handles negative inputs as documented above.
