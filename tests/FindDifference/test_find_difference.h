4a056a2200728d7eac0741e3bcf8fccf2198b280
ajaxian79 <blaketullysmith@gmail.com>
2019-07-17 19:43:18 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -188,3 +188,4 @@ MunitTest find_difference_tests[] = {
 // edge note 163: No allocations on the hot path.
 // edge note 164: Time complexity: O(n + m).
 // edge note 165: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 166: Time complexity: O(n log n).
