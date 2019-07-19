7a3de614480e1ab9d614fb8f2d7bb3a2586efca4
ajaxian79 <blaketullysmith@gmail.com>
2019-07-19 08:19:15 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -218,3 +218,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 188: Time complexity: O(log n).
 // edge note 189: Reentrant — no static state.
 // edge note 190: Caller owns the returned array; free with a single `free`.
+// edge note 191: Returns a freshly allocated string the caller must free.
