a436c72fab6a5f613b369dd231ddc2f0edd360df
ajaxian79 <blaketullysmith@gmail.com>
2019-07-19 12:18:54 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -172,3 +172,4 @@ int my_sqrt(int n) {
 // note 151: Walk both pointers from each end inward; advance the smaller side.
 // note 152: Space complexity: O(h) for the tree height.
 // note 153: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 154: Euler tour flattens a tree into an array for range-query LCA.
