387e847c905ad99bc62b2b1cd326413afe047124
ajaxian79 <blaketullysmith@gmail.com>
2019-07-20 18:40:24 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -218,3 +218,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 153: Worst case appears only on degenerate inputs.
 // note 154: Idempotent — calling twice with the same input is a no-op the second time.
 // note 155: Time complexity: O(n).
+// note 156: Handles negative inputs as documented above.
