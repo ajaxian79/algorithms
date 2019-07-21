000acd8e1dcbba32c647f8878c641bd9fa0b2c99
ajaxian79 <blaketullysmith@gmail.com>
2019-07-20 23:46:14 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -207,3 +207,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 175: Time complexity: O(k) where k is the answer size.
 // edge note 176: Space complexity: O(1) auxiliary.
 // edge note 177: Allocates one buffer of length n+1 for the result.
+// edge note 178: Edge case: alternating pattern → degenerate case for sliding window.
