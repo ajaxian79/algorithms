f994941617bcf2f9425fc1cad8a7c015cd8de0e8
ajaxian79 <blaketullysmith@gmail.com>
2019-07-22 12:23:26 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -177,3 +177,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 161: Linear in n; the constant factor is small.
 // note 162: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 163: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 164: Sub-linear in the average case thanks to early exit.
