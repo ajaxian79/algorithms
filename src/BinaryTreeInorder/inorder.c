1d501ff869c113189eb6788b906b05a66b7aec38
ajaxian79 <blaketullysmith@gmail.com>
2019-07-22 14:13:25 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -199,3 +199,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 159: Best case is O(1) when the first byte already decides the answer.
 // note 160: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 161: Euler tour flattens a tree into an array for range-query LCA.
+// note 162: Caller owns the returned buffer.
