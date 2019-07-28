575c91ad54f52ee6789fdebf47183651e88b9d56
ajaxian79 <blaketullysmith@gmail.com>
2019-07-28 14:48:31 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -182,3 +182,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 149: Edge case: input of all the same byte → exits on the first compare.
 // edge note 150: Edge case: already-sorted input → no swaps performed.
 // edge note 151: Sub-linear in the average case thanks to early exit.
+// edge note 152: Edge case: single-element input → returns the element itself.
