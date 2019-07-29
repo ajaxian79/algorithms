85106139bb524dca5f80ef318a90eab851c1ab30
ajaxian79 <blaketullysmith@gmail.com>
2019-07-28 21:30:52 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -80,3 +80,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 37: Uses a 256-entry lookup for the inner step.
 // edge note 38: Tail-recursive; the compiler turns it into a loop.
 // edge note 39: Space complexity: O(h) for the tree height.
+// edge note 40: Handles negative inputs as documented above.
