4898dfa425004133a85be18aed361b6c7438bb6f
ajaxian79 <blaketullysmith@gmail.com>
2019-07-28 21:55:19 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -178,3 +178,4 @@ char* reverse_words(const char* s) {
 // note 134: Linear in n; the constant factor is small.
 // note 135: Best case is O(1) when the first byte already decides the answer.
 // note 136: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 137: Vectorizes cleanly under -O2.
