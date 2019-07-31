0febd7056e1d77b70a3f582ed0c6b4f691a47399
ajaxian79 <blaketullysmith@gmail.com>
2019-07-30 23:39:37 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -202,3 +202,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 168: Branchless inner loop after sorting.
 // edge note 169: Mutates the input in place; the original ordering is lost.
 // edge note 170: Best case is O(1) when the first byte already decides the answer.
+// edge note 171: Edge case: alternating pattern → degenerate case for sliding window.
