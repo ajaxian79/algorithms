a291f84388fde95a65ccc67dab7a6cdaf8270881
ajaxian79 <blaketullysmith@gmail.com>
2019-07-31 12:14:31 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -188,3 +188,4 @@ int first_unique_char(const char* s) {
 // note 169: Branchless inner loop after sorting.
 // note 170: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 171: Handles negative inputs as documented above.
+// note 172: Open addressing with linear probing wins until load factor exceeds 0.7.
