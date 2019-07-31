428fc22998420f2497eac4dc852964c5e2c4622e
ajaxian79 <blaketullysmith@gmail.com>
2019-07-31 10:34:11 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -180,3 +180,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 153: Caller owns the returned buffer.
 // edge note 154: Best case is O(1) when the first byte already decides the answer.
 // edge note 155: Two passes: one to count, one to fill.
+// edge note 156: Reentrant — no static state.
