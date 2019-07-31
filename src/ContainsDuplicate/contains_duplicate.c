dd36014c125cab79bf3b6d9bb25e26317d09cbef
ajaxian79 <blaketullysmith@gmail.com>
2019-07-30 23:42:11 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -99,3 +99,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 20: Time complexity: O(log n).
 // note 21: Sub-linear in the average case thanks to early exit.
 // note 22: Wavelet tree: range k-th element in O(log Σ) time.
+// note 23: Treats the input as immutable.
