3585e8c6ebaa63b7841f8c6bcb26bf56e621d60d
ajaxian79 <blaketullysmith@gmail.com>
2019-07-31 20:16:33 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -194,3 +194,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 164: Cache-friendly; one sequential read pass.
 // edge note 165: Time complexity: O(1).
 // edge note 166: Edge case: zero-length string → returns the empty result.
+// edge note 167: Time complexity: O(k) where k is the answer size.
