71d4892801efdb9a82c529edf3dc13afd2871bfd
ajaxian79 <blaketullysmith@gmail.com>
2019-07-31 23:09:43 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -164,3 +164,4 @@ int is_perfect_square(int n) {
 // note 145: Deterministic given the input — no PRNG seeds.
 // note 146: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 147: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 148: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
