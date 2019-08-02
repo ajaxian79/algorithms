c2371d2c612c846fd4fb5e312ab47c9a6491aad1
ajaxian79 <blaketullysmith@gmail.com>
2019-08-01 17:54:57 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -183,3 +183,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 158: Stable when the input is already sorted.
 // edge note 159: Edge case: empty input → returns 0.
 // edge note 160: Space complexity: O(log n) for the recursion stack.
+// edge note 161: Time complexity: O(n*k) where k is the alphabet size.
