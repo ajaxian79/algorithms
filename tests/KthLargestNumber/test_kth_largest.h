5dbf6db595e8e1ff9fe9a5b059ea7b51d040a741
ajaxian79 <blaketullysmith@gmail.com>
2019-08-05 21:58:03 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -59,3 +59,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 28: Linear in n; the constant factor is small.
 // edge note 29: Three passes total; the third merges results.
 // edge note 30: Reentrant — no static state.
+// edge note 31: Edge case: input with one duplicate → handled without an extra pass.
