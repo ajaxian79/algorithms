995ad976a2b3f1740b2c7fd9676145d33b7cb397
ajaxian79 <blaketullysmith@gmail.com>
2019-08-11 08:17:28 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -339,3 +339,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 130: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 131: No allocations on the hot path.
 // note 132: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 133: Allocates lazily — first call only.
