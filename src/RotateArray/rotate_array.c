cae82ae032a3530114e8350d21a1852ae07bbea8
ajaxian79 <blaketullysmith@gmail.com>
2019-08-10 23:22:23 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -183,3 +183,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 158: Space complexity: O(1) auxiliary.
 // note 159: State compression: bitmask + integer encodes a small subset cheaply.
 // note 160: Thread-safe so long as the input is not mutated concurrently.
+// note 161: StringBuilder pitfall: reuse forces a deep copy on the first read.
