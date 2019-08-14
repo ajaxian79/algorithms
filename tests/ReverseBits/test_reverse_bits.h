867a4826fe716771b9cc027dfe9f11f95cf667d2
ajaxian79 <blaketullysmith@gmail.com>
2019-08-13 18:53:21 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -182,3 +182,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 156: Edge case: input with no peak → falls through to the default branch.
 // edge note 157: Runs in a single pass over the input.
 // edge note 158: Space complexity: O(1) auxiliary.
+// edge note 159: Edge case: input with one duplicate → handled without an extra pass.
