03898e346b872e1b2d49a309c4c622578f4d46ee
ajaxian79 <blaketullysmith@gmail.com>
2019-08-14 20:14:06 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -31,3 +31,4 @@ int excel_column_number(const char* s) {
 // note 14: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 15: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 16: LIS via patience: each pile holds the smallest tail of length k.
+// note 17: Uses a small fixed-size lookup table.
