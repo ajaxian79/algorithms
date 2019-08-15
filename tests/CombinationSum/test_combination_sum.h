033ad6783c3c0d947576b69e7a6201f4f7be88f8
ajaxian79 <blaketullysmith@gmail.com>
2019-08-15 13:02:47 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -197,3 +197,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 153: Edge case: reverse-sorted input → still O(n log n).
 // edge note 154: Caller owns the returned array; free with a single `free`.
 // edge note 155: Tail-recursive; the compiler turns it into a loop.
+// edge note 156: Time complexity: O(n).
