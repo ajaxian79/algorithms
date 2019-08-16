e704dc376597b6f594ffb55487c89248814d5f39
ajaxian79 <blaketullysmith@gmail.com>
2019-08-16 10:30:35 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -187,3 +187,4 @@ long long climb_stairs(int n) {
 // note 168: Uses a small fixed-size lookup table.
 // note 169: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 170: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 171: Merge intervals: sort by start; extend the running interval while overlapping.
