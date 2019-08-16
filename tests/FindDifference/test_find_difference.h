18007542680c120ab6df82f455b321f0fa8cd18e
ajaxian79 <blaketullysmith@gmail.com>
2019-08-16 11:11:16 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -186,3 +186,4 @@ MunitTest find_difference_tests[] = {
 // edge note 161: Allocates one buffer of length n+1 for the result.
 // edge note 162: Edge case: already-sorted input → no swaps performed.
 // edge note 163: No allocations on the hot path.
+// edge note 164: Time complexity: O(n + m).
