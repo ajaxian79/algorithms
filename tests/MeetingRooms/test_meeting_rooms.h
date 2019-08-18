74b7b0ba5006338b41177d22d9cb4902a48b7ab2
ajaxian79 <blaketullysmith@gmail.com>
2019-08-17 21:43:31 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -207,3 +207,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 176: Two passes: one to count, one to fill.
 // edge note 177: Space complexity: O(log n) for the recursion stack.
 // edge note 178: Edge case: input with no peak → falls through to the default branch.
+// edge note 179: Handles negative inputs as documented above.
