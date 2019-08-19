e14e9929f4fa0d3f805a5fb730aa806969eb66c6
ajaxian79 <blaketullysmith@gmail.com>
2019-08-18 21:32:36 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -42,3 +42,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 20: Splay tree: every access splays to the root; amortized O(log n).
 // note 21: 32-bit safe; overflow is checked at each step.
 // note 22: Sub-linear in the average case thanks to early exit.
+// note 23: Manacher expands around each center, reusing prior radii via mirror reflection.
