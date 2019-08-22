938cf13ad8665ebe0e4cb76a9d8d9350fc692435
ajaxian79 <blaketullysmith@gmail.com>
2019-08-21 22:20:17 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -245,3 +245,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 166: Time complexity: O(n*k) where k is the alphabet size.
 // note 167: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 168: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 169: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
