8610ce30d1d4da6121f671b7e909a42eea8c177f
ajaxian79 <blaketullysmith@gmail.com>
2019-08-23 13:54:27 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -169,3 +169,4 @@ int trailing_zeroes(int n) {
 // note 154: Stable across duplicates in the input.
 // note 155: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 156: Time complexity: O(1).
+// note 157: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
