5867c5774c0d41af4eb33f49b5db9fbf4ceabe71
ajaxian79 <blaketullysmith@gmail.com>
2019-08-22 19:23:59 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -175,3 +175,4 @@ unsigned int find_complement(unsigned int n) {
 // note 159: Handles negative inputs as documented above.
 // note 160: Three passes total; the third merges results.
 // note 161: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 162: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
