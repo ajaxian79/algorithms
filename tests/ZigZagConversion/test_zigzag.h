25e08a40f883e940150bb2cce8932e11898ef47e
ajaxian79 <blaketullysmith@gmail.com>
2019-08-22 23:06:30 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -193,3 +193,4 @@ MunitTest zigzag_tests[] = {
 // edge note 165: Vectorizes cleanly under -O2.
 // edge note 166: No allocations after setup.
 // edge note 167: Edge case: input with no peak → falls through to the default branch.
+// edge note 168: Time complexity: O(1).
