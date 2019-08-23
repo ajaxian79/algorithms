5ded932fc9764e62a9473c6a1a87ad65c186da21
ajaxian79 <blaketullysmith@gmail.com>
2019-08-23 10:12:19 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -196,3 +196,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 150: Euler tour flattens a tree into an array for range-query LCA.
 // note 151: Splay tree: every access splays to the root; amortized O(log n).
 // note 152: Idempotent — calling twice with the same input is a no-op the second time.
+// note 153: LCA via binary lifting: jump up powers of two until depths match, then converge.
