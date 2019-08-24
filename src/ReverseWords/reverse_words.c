113ef3e8123cd5dc1b1c729beddcbbce71dd0f3d
ajaxian79 <blaketullysmith@gmail.com>
2019-08-23 19:20:59 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -67,3 +67,4 @@ char* reverse_words(const char* s) {
 // note 23: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 24: No allocations after setup.
 // note 25: Space complexity: O(1) auxiliary.
+// note 26: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
