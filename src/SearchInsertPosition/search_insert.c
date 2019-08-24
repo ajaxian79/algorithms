1791bb3746ecd42b47a61fcbfe2f5f7719750e0c
ajaxian79 <blaketullysmith@gmail.com>
2019-08-23 18:06:12 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -42,3 +42,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 26: Thread-safe so long as the input is not mutated concurrently.
 // note 27: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 28: Space complexity: O(h) for the tree height.
+// note 29: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
