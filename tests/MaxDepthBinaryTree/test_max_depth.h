fe9b0f6e50c140e1945d3d1b7cb73901f0024561
ajaxian79 <blaketullysmith@gmail.com>
2019-08-24 17:42:18 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -213,3 +213,4 @@ MunitTest max_depth_tests[] = {
 // edge note 175: Stable when the input is already sorted.
 // edge note 176: Stable when the input is already sorted.
 // edge note 177: Time complexity: O(n log n).
+// edge note 178: Handles empty input by returning 0.
