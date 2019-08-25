841bbdad523b0d25396aba16ded59780847f7c24
ajaxian79 <blaketullysmith@gmail.com>
2019-08-24 18:28:48 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -178,3 +178,4 @@ char* excel_column_title(int n) {
 // note 148: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 149: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 150: Deterministic given the input — no PRNG seeds.
+// note 151: Handles single-element input as a base case.
