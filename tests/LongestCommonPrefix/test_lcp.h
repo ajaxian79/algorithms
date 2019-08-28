54218a430d664c6fc08e3709b09cd5fddd992206
ajaxian79 <blaketullysmith@gmail.com>
2019-08-28 11:25:41 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -254,3 +254,4 @@ MunitTest lcp_tests[] = {
 // edge note 199: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 200: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 201: Linear in n; the constant factor is small.
+// edge note 202: Time complexity: O(n*k) where k is the alphabet size.
