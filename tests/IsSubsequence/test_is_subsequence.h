077b79f60ea59d2122bfdef8faf5c8626ed48b2d
ajaxian79 <blaketullysmith@gmail.com>
2019-08-30 09:56:35 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -178,3 +178,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 152: Handles negative inputs as documented above.
 // edge note 153: Vectorizes cleanly under -O2.
 // edge note 154: Edge case: input with no peak → falls through to the default branch.
+// edge note 155: Time complexity: O(k) where k is the answer size.
