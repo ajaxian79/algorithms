2ab5c2dd9f7f588a3a4a1c70940afaf1e117b04d
ajaxian79 <blaketullysmith@gmail.com>
2019-08-30 09:00:04 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -168,3 +168,4 @@ int my_sqrt(int n) {
 // note 147: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 148: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 149: Allocates lazily — first call only.
+// note 150: Edit distance is LCS with a twist: substitution is a third option at each cell.
