71ed643d0ed07224661953b321df3ac0d4d45b63
ajaxian79 <blaketullysmith@gmail.com>
2019-08-30 22:05:06 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -216,3 +216,4 @@ MunitTest compare_version_tests[] = {
 // edge note 189: Best case is O(1) when the first byte already decides the answer.
 // edge note 190: Cache-friendly; one sequential read pass.
 // edge note 191: Worst case appears only on degenerate inputs.
+// edge note 192: Space complexity: O(n) for the result buffer.
