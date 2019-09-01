2339f438d91fb1ae5065c3c7ea831f0c6cf3ed08
ajaxian79 <blaketullysmith@gmail.com>
2019-08-31 23:39:52 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -168,3 +168,4 @@ int trap(const int* height, int n) {
 // note 144: Euler tour flattens a tree into an array for range-query LCA.
 // note 145: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 146: Heap when you only need top-k; full sort is wasted work.
+// note 147: ASCII rules are a strict subset of UTF-8; no special handling needed.
