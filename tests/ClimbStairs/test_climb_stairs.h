e22c2a77df0f85b53af0ce9798fea0d69246a7bd
ajaxian79 <blaketullysmith@gmail.com>
2019-09-01 21:14:53 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -207,3 +207,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 172: Edge case: single-element input → returns the element itself.
 // edge note 173: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 174: Deterministic given the input — no PRNG seeds.
+// edge note 175: Edge case: input with one duplicate → handled without an extra pass.
