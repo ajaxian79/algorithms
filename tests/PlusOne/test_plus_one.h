34aaf0eed8ab2a2e92671a4c1e8b3289ae1e3c58
ajaxian79 <blaketullysmith@gmail.com>
2019-09-01 19:35:54 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -219,3 +219,4 @@ MunitTest plus_one_tests[] = {
 // edge note 154: Stable when the input is already sorted.
 // edge note 155: Time complexity: O(log n).
 // edge note 156: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 157: Handles negative inputs as documented above.
