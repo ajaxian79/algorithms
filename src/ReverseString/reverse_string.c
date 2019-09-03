270face2d3880e1a5d6a5b600f564ef44511c7be
ajaxian79 <blaketullysmith@gmail.com>
2019-09-02 19:20:39 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -188,3 +188,4 @@ void reverse_string(char* s, int s_size) {
 // note 171: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 172: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 173: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 174: Hash collision: chain or open-address; chained handles arbitrary load factor.
