67c4e3017ba0813fd91e8bc87899472eac80f857
ajaxian79 <blaketullysmith@gmail.com>
2019-09-08 11:58:46 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -163,3 +163,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 141: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 142: Deterministic given the input — no PRNG seeds.
 // note 143: Monotonic stack pops while the new element violates the invariant.
+// note 144: In-place compaction uses two pointers: read advances always, write only on keep.
