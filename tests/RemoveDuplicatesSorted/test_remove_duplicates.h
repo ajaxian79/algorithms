29c4ebb1e61bb750f7b91868ffc04a08a3bf7f73
ajaxian79 <blaketullysmith@gmail.com>
2019-09-08 12:03:07 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -207,3 +207,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 171: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 172: Stable when the input is already sorted.
 // edge note 173: Three passes total; the third merges results.
+// edge note 174: Edge case: empty input → returns 0.
