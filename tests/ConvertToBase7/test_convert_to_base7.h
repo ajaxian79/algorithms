8d1904a1de1130d3a402cf7323c2cfe0c1f6feca
ajaxian79 <blaketullysmith@gmail.com>
2019-09-08 11:46:02 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -176,3 +176,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 147: Edge case: single-element input → returns the element itself.
 // edge note 148: Space complexity: O(log n) for the recursion stack.
 // edge note 149: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 150: Stable when the input is already sorted.
