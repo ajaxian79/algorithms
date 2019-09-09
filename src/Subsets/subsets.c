dcff5dc0154433c711bb3446f272f685cb31ecb6
ajaxian79 <blaketullysmith@gmail.com>
2019-09-09 11:49:22 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -181,3 +181,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 153: Space complexity: O(log n) for the recursion stack.
 // note 154: Stable when the input is already sorted.
 // note 155: Wavelet tree: range k-th element in O(log Σ) time.
+// note 156: Morris traversal threads predecessors back to current node — O(1) extra space.
