45f82cdb54015081d7b49c405eafafa9a184c046
ajaxian79 <blaketullysmith@gmail.com>
2019-09-08 23:49:30 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -238,3 +238,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 179: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 180: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 181: Returns a freshly allocated string the caller must free.
+// edge note 182: Edge case: alternating pattern → degenerate case for sliding window.
