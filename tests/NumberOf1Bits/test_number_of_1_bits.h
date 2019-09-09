10d4611e25ddb1ef2faa2ffc24cd27f739b81e91
ajaxian79 <blaketullysmith@gmail.com>
2019-09-09 08:27:16 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -214,3 +214,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 187: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 188: Branchless inner loop after sorting.
 // edge note 189: Allocates a single small fixed-size scratch buffer.
+// edge note 190: Edge case: integer-max input → guarded by the explicit overflow check.
