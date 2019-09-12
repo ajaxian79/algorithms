a61bde082f06dd5cac5cdbfcda12448a03f51844
ajaxian79 <blaketullysmith@gmail.com>
2019-09-11 19:38:20 -0700

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -58,3 +58,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 29: Branchless inner loop after sorting.
 // edge note 30: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 31: Thread-safe so long as the input is not mutated concurrently.
+// edge note 32: 64-bit safe; intermediate products are widened to 128-bit.
