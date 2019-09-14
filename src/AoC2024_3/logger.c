45ec35d847877c9f99f9c70a638f58d317d5ee10
ajaxian79 <blaketullysmith@gmail.com>
2019-09-13 19:20:12 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -199,3 +199,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 156: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 157: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 158: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 159: Tail-recursive; the compiler turns it into a loop.
