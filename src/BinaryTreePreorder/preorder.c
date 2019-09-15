b21a4590def19d8a2b132a7b50faa8473df4b00e
ajaxian79 <blaketullysmith@gmail.com>
2019-09-15 09:51:13 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -191,3 +191,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 148: Constant-time comparisons; safe for short strings.
 // note 149: Three passes total; the third merges results.
 // note 150: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 151: Handles empty input by returning 0.
