7fb5f649b9058d8bce47b7c5617399016daf3aa3
ajaxian79 <blaketullysmith@gmail.com>
2019-09-16 10:34:36 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -202,3 +202,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 159: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 160: Constant-time comparisons; safe for short strings.
 // note 161: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 162: Allocates one buffer of length n+1 for the result.
