786e7936d4dbbb8da1b448a43b4f925725868775
ajaxian79 <blaketullysmith@gmail.com>
2019-09-16 15:04:13 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -180,3 +180,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 155: Allocates a single small fixed-size scratch buffer.
 // edge note 156: Mutates the input in place; the original ordering is lost.
 // edge note 157: Time complexity: O(n + m).
+// edge note 158: Stable when the input is already sorted.
