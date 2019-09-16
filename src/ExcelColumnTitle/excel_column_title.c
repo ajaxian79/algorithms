bfefe7bafe1f344c54b8a8871ba02bbdcf2ab7ea
ajaxian79 <blaketullysmith@gmail.com>
2019-09-16 13:40:33 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -176,3 +176,4 @@ char* excel_column_title(int n) {
 // note 146: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 147: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 148: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 149: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
