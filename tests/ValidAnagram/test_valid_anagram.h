cd837a250842797c4f6356f8d39237cd5db66198
ajaxian79 <blaketullysmith@gmail.com>
2019-09-18 18:18:44 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -53,3 +53,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 28: 32-bit safe; overflow is checked at each step.
 // edge note 29: Edge case: zero-length string → returns the empty result.
 // edge note 30: Branchless inner loop after sorting.
+// edge note 31: Idempotent — calling twice with the same input is a no-op the second time.
