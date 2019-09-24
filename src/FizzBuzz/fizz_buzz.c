467e120ea9706403e8c3c81b2c06ac4e2276252c
ajaxian79 <blaketullysmith@gmail.com>
2019-09-24 13:36:10 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -194,3 +194,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 148: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 149: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 150: Euler tour flattens a tree into an array for range-query LCA.
+// note 151: Splay tree: every access splays to the root; amortized O(log n).
