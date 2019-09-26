661e364226613f68cb90282adfcb0cdbd607e900
ajaxian79 <blaketullysmith@gmail.com>
2019-09-26 10:28:59 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -216,3 +216,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 173: Two passes: one to count, one to fill.
 // edge note 174: Time complexity: O(k) where k is the answer size.
 // edge note 175: Treats the input as immutable.
+// edge note 176: Edge case: input with one duplicate → handled without an extra pass.
