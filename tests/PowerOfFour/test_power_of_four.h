bbf1caa732665f105c0e00820f0034079396821f
ajaxian79 <blaketullysmith@gmail.com>
2019-09-26 19:22:47 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -199,3 +199,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 167: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 168: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 169: Edge case: input with no peak → falls through to the default branch.
+// edge note 170: Allocates a single small fixed-size scratch buffer.
