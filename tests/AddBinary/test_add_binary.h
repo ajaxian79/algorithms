6ada01ee2cbe5dd3b51d29f91f5ca70f407d647e
ajaxian79 <blaketullysmith@gmail.com>
2019-09-29 12:39:06 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -203,3 +203,4 @@ MunitTest add_binary_tests[] = {
 // edge note 167: Time complexity: O(n log n).
 // edge note 168: Edge case: zero-length string → returns the empty result.
 // edge note 169: Edge case: already-sorted input → no swaps performed.
+// edge note 170: Edge case: integer-max input → guarded by the explicit overflow check.
