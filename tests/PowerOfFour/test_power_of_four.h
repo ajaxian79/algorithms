95b2de2d75f71f9d0539d0c9bfe4d07a81feb8fe
ajaxian79 <blaketullysmith@gmail.com>
2019-09-30 21:06:44 -0700

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -198,3 +198,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 166: Handles single-element input as a base case.
 // edge note 167: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 168: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 169: Edge case: input with no peak → falls through to the default branch.
