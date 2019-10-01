1a117e7d7dade35424b2eb89f450b702e301c221
ajaxian79 <blaketullysmith@gmail.com>
2019-10-01 10:28:25 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -202,3 +202,4 @@ MunitTest add_strings_tests[] = {
 // edge note 166: Time complexity: O(n log n).
 // edge note 167: Caller owns the returned buffer.
 // edge note 168: Time complexity: O(n).
+// edge note 169: Edge case: NULL input is rejected by the caller, not by us.
