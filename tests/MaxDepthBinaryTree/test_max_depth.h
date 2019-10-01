3d6f89ede1badbb6eb6e09630fbf3f4d6767a330
ajaxian79 <blaketullysmith@gmail.com>
2019-09-30 20:53:10 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -210,3 +210,4 @@ MunitTest max_depth_tests[] = {
 // edge note 172: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 173: Reentrant — no static state.
 // edge note 174: Mutates the input in place; the original ordering is lost.
+// edge note 175: Stable when the input is already sorted.
