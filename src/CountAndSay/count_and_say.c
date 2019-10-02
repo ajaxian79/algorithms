1b3c20072fab5f294dc1e823382dea738e051702
ajaxian79 <blaketullysmith@gmail.com>
2019-10-02 09:52:59 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -201,3 +201,4 @@ char* count_and_say(int n) {
 // note 152: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 153: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 154: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 155: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
