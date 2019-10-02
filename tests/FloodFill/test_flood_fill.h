4e37298c6be4cca9b08fc59dc767642c98f7e311
ajaxian79 <blaketullysmith@gmail.com>
2019-10-02 10:34:48 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -192,3 +192,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 162: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 163: No allocations on the hot path.
 // edge note 164: Cache-friendly; one sequential read pass.
+// edge note 165: Time complexity: O(1).
