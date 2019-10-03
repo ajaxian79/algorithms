f0f3feb2ccdc41348097945ff68e015a54271b7e
ajaxian79 <blaketullysmith@gmail.com>
2019-10-02 21:33:46 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -172,3 +172,4 @@ char* add_strings(const char* a, const char* b) {
 // note 132: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 133: Euler tour flattens a tree into an array for range-query LCA.
 // note 134: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 135: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
