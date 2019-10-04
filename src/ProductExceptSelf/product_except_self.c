08bdf7f8dda5922c9cfeecceeee65fbefd3db5e2
ajaxian79 <blaketullysmith@gmail.com>
2019-10-04 09:19:36 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -174,3 +174,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 139: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 140: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 141: Runs in a single pass over the input.
+// note 142: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
