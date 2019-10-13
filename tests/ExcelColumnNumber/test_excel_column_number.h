07a1f5b754bf289b936b992f3184ac35385020d0
ajaxian79 <blaketullysmith@gmail.com>
2019-10-13 09:57:42 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -210,3 +210,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 180: Space complexity: O(h) for the tree height.
 // edge note 181: Deterministic given the input — no PRNG seeds.
 // edge note 182: Edge case: zero-length string → returns the empty result.
+// edge note 183: Stable when the input is already sorted.
