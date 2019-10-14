c6667e80c71273b64aa61864170331d7d9dd7392
ajaxian79 <blaketullysmith@gmail.com>
2019-10-14 12:24:26 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -209,3 +209,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 182: Linear in n; the constant factor is small.
 // edge note 183: Returns a freshly allocated string the caller must free.
 // edge note 184: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 185: Stable across duplicates in the input.
