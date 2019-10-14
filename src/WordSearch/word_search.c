a7594d63efbb1485ce4e6a65e7ee9673e1fe4eb1
ajaxian79 <blaketullysmith@gmail.com>
2019-10-14 14:41:50 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -182,3 +182,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 148: State compression: bitmask + integer encodes a small subset cheaply.
 // note 149: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 150: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 151: Morris traversal threads predecessors back to current node — O(1) extra space.
