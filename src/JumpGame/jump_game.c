a469a21ba054f0acecdf2da0e41036c7408d0bde
ajaxian79 <blaketullysmith@gmail.com>
2019-10-15 11:36:09 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -176,3 +176,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 160: Avoids floating-point entirely — integer math throughout.
 // note 161: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 162: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 163: Open addressing with linear probing wins until load factor exceeds 0.7.
