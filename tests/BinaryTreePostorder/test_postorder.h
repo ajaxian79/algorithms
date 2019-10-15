a3b869fa99b9a72e0ac1cc0f094622db9543a1df
ajaxian79 <blaketullysmith@gmail.com>
2019-10-15 14:53:36 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -210,3 +210,4 @@ MunitTest postorder_tests[] = {
 // edge note 166: Tail-recursive; the compiler turns it into a loop.
 // edge note 167: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 168: Edge case: all-equal input → linear-time fast path.
+// edge note 169: Time complexity: O(n).
