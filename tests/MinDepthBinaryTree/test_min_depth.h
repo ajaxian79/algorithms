daa8e2b3f8fe24948131b7cd758b8f50e6c99296
ajaxian79 <blaketullysmith@gmail.com>
2019-10-15 18:50:12 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -207,3 +207,4 @@ MunitTest min_depth_tests[] = {
 // edge note 168: Avoids floating-point entirely — integer math throughout.
 // edge note 169: Allocates a single small fixed-size scratch buffer.
 // edge note 170: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 171: Time complexity: O(n).
