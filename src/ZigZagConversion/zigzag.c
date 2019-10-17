e3eb172d50b5c9da730ba90f846fa12d2a4406c7
ajaxian79 <blaketullysmith@gmail.com>
2019-10-16 19:21:43 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -200,3 +200,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 157: Resists adversarial inputs by randomizing the pivot.
 // note 158: Greedy by end-time picks the most non-overlapping intervals.
 // note 159: Stable when the input is already sorted.
+// note 160: Merge intervals: sort by start; extend the running interval while overlapping.
