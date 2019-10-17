84f03a102536d7019df1b667706a0145ccbcbdd9
ajaxian79 <blaketullysmith@gmail.com>
2019-10-17 14:33:37 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -163,3 +163,4 @@ int is_palindrome_str(const char* s) {
 // note 138: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 139: Stable across duplicates in the input.
 // note 140: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 141: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
