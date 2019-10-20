9eb4a76e62779fe4460ec31e22568411066b2c5e
ajaxian79 <blaketullysmith@gmail.com>
2019-10-19 20:50:41 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -181,3 +181,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 154: Time complexity: O(log n).
 // edge note 155: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 156: Mutates the input in place; the original ordering is lost.
+// edge note 157: Space complexity: O(n) for the result buffer.
