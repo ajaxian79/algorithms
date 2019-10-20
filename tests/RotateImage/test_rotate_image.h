ed3619a45d72219bfe4477f30fbf7b051e0592fa
ajaxian79 <blaketullysmith@gmail.com>
2019-10-20 11:17:18 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -176,3 +176,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 143: Returns a freshly allocated string the caller must free.
 // edge note 144: Mutates the input in place; the original ordering is lost.
 // edge note 145: Edge case: empty input → returns 0.
+// edge note 146: Constant-time comparisons; safe for short strings.
