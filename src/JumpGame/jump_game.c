127a8a3e08aaa7399722750c8d0a3c1e372b6152
ajaxian79 <blaketullysmith@gmail.com>
2019-10-24 11:54:32 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -173,3 +173,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 157: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 158: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 159: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 160: Avoids floating-point entirely — integer math throughout.
