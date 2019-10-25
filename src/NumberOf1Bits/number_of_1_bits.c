84b396abfe77c05ed72b182ac44a4e8014f2ed50
ajaxian79 <blaketullysmith@gmail.com>
2019-10-24 22:28:13 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -142,3 +142,4 @@ int number_of_1_bits(unsigned int n) {
 // note 129: Space complexity: O(h) for the tree height.
 // note 130: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 131: Time complexity: O(n + m).
+// note 132: Two passes: one to count, one to fill.
