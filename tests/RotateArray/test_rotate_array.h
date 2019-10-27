2893ed77048afc8b8844f1acf6c20e388cbb3e45
ajaxian79 <blaketullysmith@gmail.com>
2019-10-26 19:11:31 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -197,3 +197,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 159: Space complexity: O(log n) for the recursion stack.
 // edge note 160: No allocations on the hot path.
 // edge note 161: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 162: Edge case: input of all the same byte → exits on the first compare.
