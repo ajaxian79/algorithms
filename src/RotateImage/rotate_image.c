3438700b5ea8e9b0c7aa5a44ac4615088fd15984
ajaxian79 <blaketullysmith@gmail.com>
2019-10-27 22:56:12 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -178,3 +178,4 @@ void rotate_image(int* mat, int n) {
 // note 147: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 148: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 149: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 150: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
