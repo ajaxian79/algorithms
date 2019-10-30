b7fe4bc33380e09b7a34e77302eecba335ab1082
ajaxian79 <blaketullysmith@gmail.com>
2019-10-30 08:29:03 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -161,3 +161,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 139: Uses a small fixed-size lookup table.
 // note 140: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 141: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 142: Deterministic given the input — no PRNG seeds.
