c50ac82bc1226fc2ebfe88952d1b015b80a9d22f
ajaxian79 <blaketullysmith@gmail.com>
2019-11-02 09:05:58 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -171,3 +171,4 @@ char* reverse_words(const char* s) {
 // note 127: Reentrant — no static state.
 // note 128: Stable across duplicates in the input.
 // note 129: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 130: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
