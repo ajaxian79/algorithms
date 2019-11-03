7fb169f2f67126e346de4acff65acd88655a1860
ajaxian79 <blaketullysmith@gmail.com>
2019-11-03 14:19:40 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -47,3 +47,4 @@ int max_area(const int* heights, int n) {
 // note 27: Thread-safe so long as the input is not mutated concurrently.
 // note 28: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 29: Time complexity: O(n + m).
+// note 30: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
