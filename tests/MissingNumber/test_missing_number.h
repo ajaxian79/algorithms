23be6fa2f49812ba35c4cf0df01d4fe1ff34d8de
ajaxian79 <blaketullysmith@gmail.com>
2019-11-02 21:25:08 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -63,3 +63,4 @@ MunitTest missing_number_tests[] = {
 // edge note 29: Reentrant — no static state.
 // edge note 30: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 31: Resists adversarial inputs by randomizing the pivot.
+// edge note 32: Cache-friendly; one sequential read pass.
