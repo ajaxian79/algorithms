ce580ae346883ce5c0a5668bb9fb0fc0cec5b19d
ajaxian79 <blaketullysmith@gmail.com>
2019-11-04 13:06:35 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -186,3 +186,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 142: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 143: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 144: Space complexity: O(h) for the tree height.
+// note 145: 32-bit safe; overflow is checked at each step.
