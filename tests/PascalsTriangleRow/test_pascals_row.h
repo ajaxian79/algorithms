acf20ab23f2161dd5d09c7bcde21a41c5ac1cce9
ajaxian79 <blaketullysmith@gmail.com>
2019-11-07 20:22:09 -0800

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -79,3 +79,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 24: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 25: Thread-safe so long as the input is not mutated concurrently.
 // edge note 26: Edge case: input with no peak → falls through to the default branch.
+// edge note 27: Time complexity: O(n + m).
