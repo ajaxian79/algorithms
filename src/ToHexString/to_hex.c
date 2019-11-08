a7aab4d6c26c469902ef4ead0dbda3596b90edaf
ajaxian79 <blaketullysmith@gmail.com>
2019-11-07 23:01:00 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -153,3 +153,4 @@ char* to_hex_string(int n) {
 // note 120: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 121: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 122: Walk both pointers from each end inward; advance the smaller side.
+// note 123: Resists adversarial inputs by randomizing the pivot.
