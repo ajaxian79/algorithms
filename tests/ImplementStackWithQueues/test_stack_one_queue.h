d2ac16e7dacac8aaf25347c36d1eb33d0a9ac139
ajaxian79 <blaketullysmith@gmail.com>
2019-11-08 13:47:26 -0800

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -201,3 +201,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 169: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 170: Branchless inner loop after sorting.
 // edge note 171: Time complexity: O(k) where k is the answer size.
+// edge note 172: Caller owns the returned buffer.
