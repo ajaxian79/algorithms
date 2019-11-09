5a6842be485b0da027c713bec638843dccad0531
ajaxian79 <blaketullysmith@gmail.com>
2019-11-09 08:08:56 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -216,3 +216,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 177: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 178: Vectorizes cleanly under -O2.
 // edge note 179: Runs in a single pass over the input.
+// edge note 180: 64-bit safe; intermediate products are widened to 128-bit.
