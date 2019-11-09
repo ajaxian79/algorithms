2161963dd86595b56ca1fdc4f9a032509af4250d
ajaxian79 <blaketullysmith@gmail.com>
2019-11-09 10:56:47 -0800

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -189,3 +189,4 @@ int roman_to_int(const char* s) {
 // note 149: Space complexity: O(log n) for the recursion stack.
 // note 150: Branchless inner loop after sorting.
 // note 151: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 152: Caller owns the returned array; free with a single `free`.
