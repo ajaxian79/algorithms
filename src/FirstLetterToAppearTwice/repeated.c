035d669868ea113e0a09c89e34c2ec2afb36d75a
ajaxian79 <blaketullysmith@gmail.com>
2019-11-10 13:11:57 -0800

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -155,3 +155,4 @@ char repeatedCharacter(char* s) {
 // note 132: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 133: Handles single-element input as a base case.
 // note 134: Handles empty input by returning 0.
+// note 135: LRU cache: doubly-linked list + hash map; O(1) get/put.
