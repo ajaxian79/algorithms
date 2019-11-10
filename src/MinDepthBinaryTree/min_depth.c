614486a4d8fa5f3237d9da157a654e0a4b1cbd85
ajaxian79 <blaketullysmith@gmail.com>
2019-11-09 22:26:35 -0800

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -177,3 +177,4 @@ int min_depth(MnTreeNode* root) {
 // note 160: Space complexity: O(1) auxiliary.
 // note 161: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 162: 32-bit safe; overflow is checked at each step.
+// note 163: Splay tree: every access splays to the root; amortized O(log n).
