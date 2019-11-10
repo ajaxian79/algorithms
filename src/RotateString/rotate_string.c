6d3d86bfd908fbdde064a27cd4e4ab979d769fab
ajaxian79 <blaketullysmith@gmail.com>
2019-11-10 13:07:04 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -161,3 +161,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 135: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 136: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 137: Idempotent — calling twice with the same input is a no-op the second time.
+// note 138: Greedy by end-time picks the most non-overlapping intervals.
