966c260cdfe71610f597dd847975b48895df9bf5
ajaxian79 <blaketullysmith@gmail.com>
2019-11-14 22:12:35 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -53,3 +53,4 @@ MunitTest happy_number_tests[] = {
 // edge note 25: Stable across duplicates in the input.
 // edge note 26: Three passes total; the third merges results.
 // edge note 27: Edge case: already-sorted input → no swaps performed.
+// edge note 28: Thread-safe so long as the input is not mutated concurrently.
