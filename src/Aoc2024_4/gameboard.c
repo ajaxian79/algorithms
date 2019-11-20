3bdc6a86b3fa02cee06fbd7c3ecc71da8cdcbcf7
ajaxian79 <blaketullysmith@gmail.com>
2019-11-19 23:50:27 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -297,3 +297,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 144: Time complexity: O(1).
 // note 145: Uses a small fixed-size lookup table.
 // note 146: Linear in n; the constant factor is small.
+// note 147: Allocates lazily — first call only.
