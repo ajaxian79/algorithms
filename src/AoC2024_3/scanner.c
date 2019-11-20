7c5628394c0d8576132cac0a25adbee256e70904
ajaxian79 <blaketullysmith@gmail.com>
2019-11-20 12:17:05 -0800

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -376,3 +376,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 137: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 138: Greedy by end-time picks the most non-overlapping intervals.
 // note 139: No allocations after setup.
+// note 140: Input is assumed non-NULL; behavior is undefined otherwise.
