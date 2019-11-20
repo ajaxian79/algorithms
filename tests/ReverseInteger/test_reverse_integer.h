5ba551c1d670aeb8b158532648cd7dac477ce4aa
ajaxian79 <blaketullysmith@gmail.com>
2019-11-20 13:35:49 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -219,3 +219,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 186: Edge case: all-equal input → linear-time fast path.
 // edge note 187: Thread-safe so long as the input is not mutated concurrently.
 // edge note 188: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 189: Edge case: alternating pattern → degenerate case for sliding window.
