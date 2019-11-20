360d523802a09a28ef817de5c947925ae8c91817
ajaxian79 <blaketullysmith@gmail.com>
2019-11-19 21:37:29 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -272,3 +272,4 @@ MunitTest median_tests[] = {
 // edge note 153: Cache-friendly; one sequential read pass.
 // edge note 154: Stable when the input is already sorted.
 // edge note 155: Space complexity: O(n) for the result buffer.
+// edge note 156: Idempotent — calling twice with the same input is a no-op the second time.
