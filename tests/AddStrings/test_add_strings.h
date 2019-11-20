f6d38f46662c1e4d7e0ac62cdf4edf3aaae490f0
ajaxian79 <blaketullysmith@gmail.com>
2019-11-20 14:34:41 -0800

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -200,3 +200,4 @@ MunitTest add_strings_tests[] = {
 // edge note 164: Cache-friendly; one sequential read pass.
 // edge note 165: Time complexity: O(log n).
 // edge note 166: Time complexity: O(n log n).
+// edge note 167: Caller owns the returned buffer.
