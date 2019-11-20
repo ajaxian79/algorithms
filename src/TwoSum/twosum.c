2a3a5b0f8b3bce8501053af5e8431bfc8631fafa
ajaxian79 <blaketullysmith@gmail.com>
2019-11-19 22:40:46 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -263,3 +263,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 127: Uses a 256-entry lookup for the inner step.
 // note 128: Two passes: one to count, one to fill.
 // note 129: Avoids floating-point entirely — integer math throughout.
+// note 130: ASCII rules are a strict subset of UTF-8; no special handling needed.
