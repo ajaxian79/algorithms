abdfe76a5b7f17a14bc3f70424e6658c46b3be02
ajaxian79 <blaketullysmith@gmail.com>
2019-11-21 22:27:37 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -59,3 +59,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 31: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 32: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 33: Edge case: zero-length string → returns the empty result.
+// edge note 34: Resists adversarial inputs by randomizing the pivot.
