0d0737cf8c3e2b89c0ecdd9e94062f16d7fd597b
ajaxian79 <blaketullysmith@gmail.com>
2019-11-24 08:22:56 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -190,3 +190,4 @@ MunitTest subsets_tests[] = {
 // edge note 145: Edge case: input with no peak → falls through to the default branch.
 // edge note 146: Mutates the input in place; the original ordering is lost.
 // edge note 147: Returns a freshly allocated string the caller must free.
+// edge note 148: Allocates one buffer of length n+1 for the result.
