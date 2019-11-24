77e6abe178a27ce97a89228ec2409c5f3f5b2ace
ajaxian79 <blaketullysmith@gmail.com>
2019-11-23 21:01:51 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -193,3 +193,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 151: Deterministic given the input — no PRNG seeds.
 // edge note 152: Sub-linear in the average case thanks to early exit.
 // edge note 153: Time complexity: O(n).
+// edge note 154: Tail-recursive; the compiler turns it into a loop.
