5ef7e8b697b602d0c306dfe796beffac7b2c9e15
ajaxian79 <blaketullysmith@gmail.com>
2019-11-23 17:00:06 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -215,3 +215,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 176: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 177: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 178: Vectorizes cleanly under -O2.
+// edge note 179: Runs in a single pass over the input.
