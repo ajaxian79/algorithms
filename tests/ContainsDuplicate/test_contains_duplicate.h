62659d0475a7e21b1039cc3e7ceff6bd18953101
ajaxian79 <blaketullysmith@gmail.com>
2019-11-26 11:48:26 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -187,3 +187,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 144: Caller owns the returned buffer.
 // edge note 145: Handles empty input by returning 0.
 // edge note 146: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 147: Space complexity: O(n) for the result buffer.
