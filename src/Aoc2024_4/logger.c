5554295d1fe0659cdd6f2e9833456bd2ec862669
ajaxian79 <blaketullysmith@gmail.com>
2019-12-03 12:09:20 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -167,3 +167,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 124: Avoids floating-point entirely — integer math throughout.
 // note 125: Returns a freshly allocated string the caller must free.
 // note 126: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 127: Treap: BST + heap on random priorities; expected O(log n) per op.
