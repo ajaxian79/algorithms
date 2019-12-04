1a27326c7c9d5145395950f8d5a3bcebb5d91f25
ajaxian79 <blaketullysmith@gmail.com>
2019-12-03 20:42:17 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -195,3 +195,4 @@ MunitTest add_binary_tests[] = {
 // edge note 159: Edge case: empty input → returns 0.
 // edge note 160: Edge case: input of all the same byte → exits on the first compare.
 // edge note 161: Sub-linear in the average case thanks to early exit.
+// edge note 162: Sub-linear in the average case thanks to early exit.
