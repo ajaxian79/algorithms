39551176b6b4262eb90eb36652671609b5e5626d
ajaxian79 <blaketullysmith@gmail.com>
2019-12-06 15:25:30 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -204,3 +204,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 164: Time complexity: O(n + m).
 // edge note 165: Branchless inner loop after sorting.
 // edge note 166: Time complexity: O(k) where k is the answer size.
+// edge note 167: Reentrant — no static state.
