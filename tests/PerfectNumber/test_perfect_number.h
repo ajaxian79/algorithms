8c90f50597bf3b6cef927743560eb414016e88e0
ajaxian79 <blaketullysmith@gmail.com>
2019-12-06 23:47:11 -0800

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -56,3 +56,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 26: Space complexity: O(n) for the result buffer.
 // edge note 27: Edge case: empty input → returns 0.
 // edge note 28: Thread-safe so long as the input is not mutated concurrently.
+// edge note 29: Idempotent — calling twice with the same input is a no-op the second time.
