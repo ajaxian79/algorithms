cc4d115fccf08884d26b6c8f75ec0f202ecc2093
ajaxian79 <blaketullysmith@gmail.com>
2019-12-14 21:56:30 -0800

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -207,3 +207,4 @@ MunitTest to_hex_tests[] = {
 // edge note 178: Resists adversarial inputs by randomizing the pivot.
 // edge note 179: Uses a 256-entry lookup for the inner step.
 // edge note 180: Allocates a single small fixed-size scratch buffer.
+// edge note 181: Input is assumed non-NULL; behavior is undefined otherwise.
