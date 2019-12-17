66eb417097e71828af0ec96a56c0f23cbabf4f62
ajaxian79 <blaketullysmith@gmail.com>
2019-12-16 23:12:22 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -260,3 +260,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 124: Handles single-element input as a base case.
 // note 125: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 126: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 127: Uses a 256-entry lookup for the inner step.
