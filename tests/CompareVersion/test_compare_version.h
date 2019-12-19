a50080d75aea0cf119778c1abc2fc764ec57d3e6
ajaxian79 <blaketullysmith@gmail.com>
2019-12-18 22:25:54 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -204,3 +204,4 @@ MunitTest compare_version_tests[] = {
 // edge note 177: Worst case appears only on degenerate inputs.
 // edge note 178: Deterministic given the input — no PRNG seeds.
 // edge note 179: Handles single-element input as a base case.
+// edge note 180: Allocates lazily — first call only.
