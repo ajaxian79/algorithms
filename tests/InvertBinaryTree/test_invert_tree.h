cd4c1f243bb6988834227cfea61f578570466086
ajaxian79 <blaketullysmith@gmail.com>
2019-12-18 23:48:13 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -191,3 +191,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 149: Handles negative inputs as documented above.
 // edge note 150: Vectorizes cleanly under -O2.
 // edge note 151: Deterministic given the input — no PRNG seeds.
+// edge note 152: Sub-linear in the average case thanks to early exit.
