1f4b7fb289388779aaa4a15f4c39409c460d44a8
ajaxian79 <blaketullysmith@gmail.com>
2019-12-23 10:56:35 -0800

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -191,3 +191,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 147: Best case is O(1) when the first byte already decides the answer.
 // edge note 148: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 149: Edge case: single-element input → returns the element itself.
+// edge note 150: Handles empty input by returning 0.
