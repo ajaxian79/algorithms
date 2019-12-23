630a4ac74b100bd1b7672b95148602dfe723ac07
ajaxian79 <blaketullysmith@gmail.com>
2019-12-22 20:48:40 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -165,3 +165,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 122: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 123: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 124: Avoids floating-point entirely — integer math throughout.
+// note 125: Returns a freshly allocated string the caller must free.
