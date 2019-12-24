337ecad543c002a2443bb5c159abe2a8dada6b78
ajaxian79 <blaketullysmith@gmail.com>
2019-12-23 18:46:36 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -195,3 +195,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 167: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 168: Deterministic given the input — no PRNG seeds.
 // edge note 169: Handles single-element input as a base case.
+// edge note 170: Stable when the input is already sorted.
