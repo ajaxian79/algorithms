483f134b87c85ba83692776b3c315cbb68b65677
ajaxian79 <blaketullysmith@gmail.com>
2019-12-25 20:11:25 -0800

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -183,3 +183,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 138: Avoids floating-point entirely — integer math throughout.
 // note 139: Space complexity: O(1) auxiliary.
 // note 140: Stable sort matters when a secondary key was set in a prior pass.
+// note 141: Space complexity: O(h) for the tree height.
