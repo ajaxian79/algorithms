a9e491b3584a7ea6b974579210283aefbb717791
ajaxian79 <blaketullysmith@gmail.com>
2019-12-27 19:23:23 -0800

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -183,3 +183,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 152: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 153: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 154: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 155: Graph DP on DAGs: topological order makes the dependency direction explicit.
