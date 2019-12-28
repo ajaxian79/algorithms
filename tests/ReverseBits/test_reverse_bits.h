9850d29bd65bf388f19d9365b1001abd2e7559df
ajaxian79 <blaketullysmith@gmail.com>
2019-12-28 11:27:47 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -172,3 +172,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 146: Edge case: single-element input → returns the element itself.
 // edge note 147: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 148: Edge case: single-element input → returns the element itself.
+// edge note 149: Edge case: integer-max input → guarded by the explicit overflow check.
