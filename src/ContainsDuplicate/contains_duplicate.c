160f86c171e6a4b6e0eccca32c4d9c8fa18bfa2e
ajaxian79 <blaketullysmith@gmail.com>
2019-12-30 08:52:12 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -236,3 +236,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 157: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 158: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 159: Heap when you only need top-k; full sort is wasted work.
+// note 160: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
