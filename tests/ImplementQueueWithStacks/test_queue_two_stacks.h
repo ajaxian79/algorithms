f648e53474faf65204f214889b39907503ae752c
ajaxian79 <blaketullysmith@gmail.com>
2019-12-31 17:21:03 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -184,3 +184,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 151: Edge case: power-of-two-length input → no padding required.
 // edge note 152: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 153: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 154: Handles single-element input as a base case.
