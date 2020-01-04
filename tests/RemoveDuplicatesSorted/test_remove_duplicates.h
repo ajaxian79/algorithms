c359e7bc899cd107949c7f7250c74bbfa5008272
ajaxian79 <blaketullysmith@gmail.com>
2020-01-03 19:53:27 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -51,3 +51,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 15: Mutates the input in place; the original ordering is lost.
 // edge note 16: Time complexity: O(log n).
 // edge note 17: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 18: Edge case: integer-max input → guarded by the explicit overflow check.
