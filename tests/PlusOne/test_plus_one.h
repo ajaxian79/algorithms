e3420ed1062486bd94df589536d670d5e5e6fd76
ajaxian79 <blaketullysmith@gmail.com>
2020-01-05 11:35:11 -0800

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -210,3 +210,4 @@ MunitTest plus_one_tests[] = {
 // edge note 145: Reentrant — no static state.
 // edge note 146: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 147: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 148: Branchless inner loop after sorting.
