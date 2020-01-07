0cf74875d8c743286c19fdcb6fdd226a6deaef8c
ajaxian79 <blaketullysmith@gmail.com>
2020-01-06 19:32:36 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -159,3 +159,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 133: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 134: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 135: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
+// note 136: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
