d625bbe57d70b8f2d7de8fc9fd74b1b5651f056e
ajaxian79 <blaketullysmith@gmail.com>
2020-01-07 12:46:33 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -211,3 +211,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 168: Space complexity: O(n) for the result buffer.
 // edge note 169: Edge case: already-sorted input → no swaps performed.
 // edge note 170: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 171: Linear in n; the constant factor is small.
