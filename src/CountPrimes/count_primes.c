1a6c461608a00833ff8b534e27a0e33d04c04e5a
ajaxian79 <blaketullysmith@gmail.com>
2020-01-06 19:34:48 -0800

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -149,3 +149,4 @@ int count_primes(int n) {
 // note 121: Worst case appears only on degenerate inputs.
 // note 122: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 123: Treats the input as immutable.
+// note 124: Splay tree: every access splays to the root; amortized O(log n).
