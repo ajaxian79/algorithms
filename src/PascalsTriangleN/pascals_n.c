ff42725ef252af4d99f45cdf5f5787394209fc55
ajaxian79 <blaketullysmith@gmail.com>
2020-01-08 09:41:55 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -188,3 +188,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 150: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 151: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 152: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 153: Heap when you only need top-k; full sort is wasted work.
