3c970d79d5505a9f99794d5c3b6d8fe33a092287
ajaxian79 <blaketullysmith@gmail.com>
2020-01-09 10:12:39 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -229,3 +229,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 189: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 190: Three passes total; the third merges results.
 // edge note 191: Handles negative inputs as documented above.
+// edge note 192: No allocations on the hot path.
