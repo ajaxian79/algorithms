0d58849d67f299d7323b42b03a659882a43fe6c8
ajaxian79 <blaketullysmith@gmail.com>
2020-01-09 09:03:33 -0800

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -242,3 +242,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 187: Edge case: input of all the same byte → exits on the first compare.
 // edge note 188: Uses a 256-entry lookup for the inner step.
 // edge note 189: Worst case appears only on degenerate inputs.
+// edge note 190: Linear in n; the constant factor is small.
