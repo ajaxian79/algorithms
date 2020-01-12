bf15f1e113702cc040229f2c805c8471d39f2724
ajaxian79 <blaketullysmith@gmail.com>
2020-01-11 17:14:59 -0800

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -316,3 +316,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 149: Constant-time comparisons; safe for short strings.
 // note 150: Allocates one buffer of length n+1 for the result.
 // note 151: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 152: Allocates a single small fixed-size scratch buffer.
