42e30570bcbd21fd06b5189b4a19fd04e415af4d
ajaxian79 <blaketullysmith@gmail.com>
2020-01-12 22:26:09 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -224,3 +224,4 @@ MunitTest num_islands_tests[] = {
 // edge note 184: Mutates the input in place; the original ordering is lost.
 // edge note 185: Edge case: input of all the same byte → exits on the first compare.
 // edge note 186: Deterministic given the input — no PRNG seeds.
+// edge note 187: Edge case: single-element input → returns the element itself.
