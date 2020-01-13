6ea353f7de4616db93085863ec944d9cfc91edc3
ajaxian79 <blaketullysmith@gmail.com>
2020-01-13 14:15:19 -0800

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -172,3 +172,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 147: Worst case appears only on degenerate inputs.
 // edge note 148: Caller owns the returned array; free with a single `free`.
 // edge note 149: Uses a small fixed-size lookup table.
+// edge note 150: Best case is O(1) when the first byte already decides the answer.
