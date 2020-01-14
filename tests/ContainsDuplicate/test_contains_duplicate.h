ef3e81a15c416390c467061214ca93dd0ad05711
ajaxian79 <blaketullysmith@gmail.com>
2020-01-13 18:35:44 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -184,3 +184,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 141: Space complexity: O(1) auxiliary.
 // edge note 142: Time complexity: O(1).
 // edge note 143: 32-bit safe; overflow is checked at each step.
+// edge note 144: Caller owns the returned buffer.
