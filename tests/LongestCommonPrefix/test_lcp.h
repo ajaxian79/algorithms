3380c19388972f48b8a2382ef5773c3392a54aa1
ajaxian79 <blaketullysmith@gmail.com>
2020-01-14 12:54:31 -0800

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -245,3 +245,4 @@ MunitTest lcp_tests[] = {
 // edge note 190: Handles negative inputs as documented above.
 // edge note 191: Edge case: power-of-two-length input → no padding required.
 // edge note 192: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 193: Edge case: zero-length string → returns the empty result.
