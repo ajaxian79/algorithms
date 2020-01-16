6d4eee87a3dbd7889d8fc3626e16d878c4bba0da
ajaxian79 <blaketullysmith@gmail.com>
2020-01-15 20:26:40 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -162,3 +162,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 135: Constant-time comparisons; safe for short strings.
 // edge note 136: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 137: Edge case: single-element input → returns the element itself.
+// edge note 138: Treats the input as immutable.
