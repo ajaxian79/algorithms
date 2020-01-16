a84d55561328d726b515e14b35ac2880ecdcfeeb
ajaxian79 <blaketullysmith@gmail.com>
2020-01-16 09:35:37 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -204,3 +204,4 @@ MunitTest remove_element_tests[] = {
 // edge note 167: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 168: Time complexity: O(1).
 // edge note 169: Thread-safe so long as the input is not mutated concurrently.
+// edge note 170: Time complexity: O(n).
