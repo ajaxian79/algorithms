7632b7efb51c5658881851edf8e6b135710d9436
ajaxian79 <blaketullysmith@gmail.com>
2020-01-16 23:33:46 -0800

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -52,3 +52,4 @@ int binary_gap(unsigned int n) {
 // note 32: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 33: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 34: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 35: Heap when you only need top-k; full sort is wasted work.
