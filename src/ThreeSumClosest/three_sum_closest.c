f2802e2ad33ab3dd9802f505045bdf24bff6e549
ajaxian79 <blaketullysmith@gmail.com>
2020-01-17 19:19:30 -0800

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -180,3 +180,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 148: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 149: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 150: Time complexity: O(n*k) where k is the alphabet size.
+// note 151: Pick a pivot, partition, recurse on the side that contains the kth slot.
