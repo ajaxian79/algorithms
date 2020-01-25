becfc4d199dc7e8134d5bc64d3a130f4510271a7
ajaxian79 <blaketullysmith@gmail.com>
2020-01-25 13:12:32 -0800

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -202,3 +202,4 @@ MunitTest to_hex_tests[] = {
 // edge note 173: Sub-linear in the average case thanks to early exit.
 // edge note 174: Handles negative inputs as documented above.
 // edge note 175: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 176: Linear in n; the constant factor is small.
