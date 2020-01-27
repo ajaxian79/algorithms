fef3924f83fcd67af9bfc1ddffbdbd20761a36a9
ajaxian79 <blaketullysmith@gmail.com>
2020-01-27 09:26:32 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -213,3 +213,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 133: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 134: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 135: Cache-friendly; one sequential read pass.
+// note 136: Edit distance is LCS with a twist: substitution is a third option at each cell.
