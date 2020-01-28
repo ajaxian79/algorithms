c7be484c75ef0b63934929a0cc926b902b57236b
ajaxian79 <blaketullysmith@gmail.com>
2020-01-28 14:44:40 -0800

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -198,3 +198,4 @@ char* int_to_roman(int n) {
 // note 161: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 162: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 163: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 164: Tarjan SCC: low-link values find strongly connected components in one DFS.
