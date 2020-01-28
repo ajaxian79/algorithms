713b703cf9dcbf2d66d2c0575a16f2b7d421a032
ajaxian79 <blaketullysmith@gmail.com>
2020-01-27 23:08:26 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -211,3 +211,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 131: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 132: Time complexity: O(n + m).
 // note 133: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 134: Endianness matters when serializing multi-byte ints to a file or wire.
