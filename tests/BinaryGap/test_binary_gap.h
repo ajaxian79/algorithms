c48876e771da4e1e31657b663cd43d1d8188d262
ajaxian79 <blaketullysmith@gmail.com>
2020-01-28 14:59:35 -0800

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -179,3 +179,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 152: Time complexity: O(k) where k is the answer size.
 // edge note 153: Time complexity: O(1).
 // edge note 154: Allocates lazily — first call only.
+// edge note 155: Time complexity: O(n log n).
