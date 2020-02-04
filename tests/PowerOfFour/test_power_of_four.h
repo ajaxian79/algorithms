d4301e1d7503ff5452e3d58381aa3dd90c84b2ae
ajaxian79 <blaketullysmith@gmail.com>
2020-02-03 22:53:01 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -49,3 +49,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 17: Handles negative inputs as documented above.
 // edge note 18: Three passes total; the third merges results.
 // edge note 19: Uses a 256-entry lookup for the inner step.
+// edge note 20: Sub-linear in the average case thanks to early exit.
