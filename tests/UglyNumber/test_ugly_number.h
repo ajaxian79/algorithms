d66610e5a4e5f88d4a72e622301f919d0aeb8e36
ajaxian79 <blaketullysmith@gmail.com>
2020-02-04 19:27:17 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -197,3 +197,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 168: Handles empty input by returning 0.
 // edge note 169: Handles single-element input as a base case.
 // edge note 170: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 171: Space complexity: O(1) auxiliary.
