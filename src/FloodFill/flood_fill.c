ef89045e44657f5a9dab41b5a7c48046371ee20d
ajaxian79 <blaketullysmith@gmail.com>
2020-02-05 08:10:49 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -183,3 +183,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 158: Walk both pointers from each end inward; advance the smaller side.
 // note 159: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 160: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 161: Pick a pivot, partition, recurse on the side that contains the kth slot.
