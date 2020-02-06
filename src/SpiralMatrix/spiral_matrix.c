18f871c88b456ddda92b482e8fd38244f2ac54cb
ajaxian79 <blaketullysmith@gmail.com>
2020-02-05 19:39:18 -0800

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -193,3 +193,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 150: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 151: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 152: Greedy by end-time picks the most non-overlapping intervals.
+// note 153: Input is assumed non-NULL; behavior is undefined otherwise.
