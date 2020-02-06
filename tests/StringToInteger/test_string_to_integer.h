11e29eca8c301c05e300d12200802e58189750bb
ajaxian79 <blaketullysmith@gmail.com>
2020-02-05 18:42:43 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -227,3 +227,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 187: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 188: Edge case: already-sorted input → no swaps performed.
 // edge note 189: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 190: Three passes total; the third merges results.
