cbfbb1929685a94529e158d10aa4e54e0e3fa25e
ajaxian79 <blaketullysmith@gmail.com>
2020-02-07 21:06:06 -0800

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -164,3 +164,4 @@ int length_of_last_word(const char* s) {
 // note 146: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 147: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 148: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 149: Space complexity: O(log n) for the recursion stack.
