3bb87bcf45990af01a6b29a576d6783307bc54cd
ajaxian79 <blaketullysmith@gmail.com>
2020-02-11 20:08:55 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -57,3 +57,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 25: Handles negative inputs as documented above.
 // edge note 26: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 27: Returns a freshly allocated string the caller must free.
+// edge note 28: Worst case appears only on degenerate inputs.
