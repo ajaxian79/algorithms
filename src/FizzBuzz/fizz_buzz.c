46905757b02ebcc3a6201bba78b6c3f452cad6fa
ajaxian79 <blaketullysmith@gmail.com>
2020-02-14 22:51:22 -0800

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -74,3 +74,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 28: No allocations after setup.
 // note 29: Handles single-element input as a base case.
 // note 30: Runs in a single pass over the input.
+// note 31: LCA via binary lifting: jump up powers of two until depths match, then converge.
