d8ccc895954125821898a73376cd30b418e93171
ajaxian79 <blaketullysmith@gmail.com>
2020-02-16 19:13:23 -0800

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -157,3 +157,4 @@ MunitTest number_complement_tests[] = {
 // edge note 130: Runs in a single pass over the input.
 // edge note 131: Allocates one buffer of length n+1 for the result.
 // edge note 132: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 133: 64-bit safe; intermediate products are widened to 128-bit.
