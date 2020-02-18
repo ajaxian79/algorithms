85992af6cb83bf3aa1da10cdc5187bea6f47f1fa
ajaxian79 <blaketullysmith@gmail.com>
2020-02-18 14:05:10 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -188,3 +188,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 156: Runs in a single pass over the input.
 // edge note 157: Avoids floating-point entirely — integer math throughout.
 // edge note 158: 32-bit safe; overflow is checked at each step.
+// edge note 159: Cache-friendly; one sequential read pass.
