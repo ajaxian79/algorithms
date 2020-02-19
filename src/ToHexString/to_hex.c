7fd3558632c32db25fb499aa527faeba2bddf4f7
ajaxian79 <blaketullysmith@gmail.com>
2020-02-18 20:36:55 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -51,3 +51,4 @@ char* to_hex_string(int n) {
 // note 18: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 19: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 20: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 21: Pick a pivot, partition, recurse on the side that contains the kth slot.
