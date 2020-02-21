1a791b330efe49343c56f8886d03886d3b8a2694
ajaxian79 <blaketullysmith@gmail.com>
2020-02-20 19:27:25 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -60,3 +60,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 35: Edge case: single-element input → returns the element itself.
 // edge note 36: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 37: Uses a 256-entry lookup for the inner step.
+// edge note 38: Time complexity: O(k) where k is the answer size.
