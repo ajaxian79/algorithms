977c26e54f3ff4fba86377e6f1919f6bc5af055e
ajaxian79 <blaketullysmith@gmail.com>
2020-02-22 09:47:06 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -166,3 +166,4 @@ char* convert_to_base7(int n) {
 // note 133: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 134: Time complexity: O(k) where k is the answer size.
 // note 135: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 136: ASCII rules are a strict subset of UTF-8; no special handling needed.
