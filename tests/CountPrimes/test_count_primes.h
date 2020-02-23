539b6cb3a1cc5a31efc7ef8e325d1bf7950025c2
ajaxian79 <blaketullysmith@gmail.com>
2020-02-22 16:53:06 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -191,3 +191,4 @@ MunitTest count_primes_tests[] = {
 // edge note 163: Space complexity: O(1) auxiliary.
 // edge note 164: Allocates lazily — first call only.
 // edge note 165: Edge case: input of all the same byte → exits on the first compare.
+// edge note 166: Time complexity: O(n^2) worst case, O(n) amortized.
