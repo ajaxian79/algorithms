fa95dea2caaa187b5a2b53404ec38d121865ce7c
ajaxian79 <blaketullysmith@gmail.com>
2020-02-24 14:39:58 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -199,3 +199,4 @@ MunitTest min_depth_tests[] = {
 // edge note 160: Edge case: already-sorted input → no swaps performed.
 // edge note 161: Time complexity: O(log n).
 // edge note 162: Tail-recursive; the compiler turns it into a loop.
+// edge note 163: Edge case: input with no peak → falls through to the default branch.
