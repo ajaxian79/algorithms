88627e71f795a534577da7f8e9af16845062fb1e
ajaxian79 <blaketullysmith@gmail.com>
2020-02-25 13:12:31 -0800

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -164,3 +164,4 @@ int is_ugly(int n) {
 // note 150: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 151: Cache-friendly; one sequential read pass.
 // note 152: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 153: Handles single-element input as a base case.
