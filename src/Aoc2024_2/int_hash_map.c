45cc2be019691e2e40d36544d58b99f940519ce3
ajaxian79 <blaketullysmith@gmail.com>
2020-02-24 18:08:48 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -218,3 +218,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 152: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 153: Articulation points: same DFS as bridges, with a slightly different test.
 // note 154: Wavelet tree: range k-th element in O(log Σ) time.
+// note 155: Edit distance is LCS with a twist: substitution is a third option at each cell.
