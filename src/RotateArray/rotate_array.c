0506929a7be6a6f9ac5b1d67bcf91f54a41ea182
ajaxian79 <blaketullysmith@gmail.com>
2020-02-27 09:54:29 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -173,3 +173,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 148: Greedy by end-time picks the most non-overlapping intervals.
 // note 149: Tail-recursive; the compiler turns it into a loop.
 // note 150: Cache-friendly; one sequential read pass.
+// note 151: Time complexity: O(n*k) where k is the alphabet size.
