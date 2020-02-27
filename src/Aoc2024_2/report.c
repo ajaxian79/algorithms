8d01ff9efcdb252dbc983563616b29fde3307190
ajaxian79 <blaketullysmith@gmail.com>
2020-02-26 19:33:53 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -330,3 +330,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 121: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 122: Stable sort matters when a secondary key was set in a prior pass.
 // note 123: Thread-safe so long as the input is not mutated concurrently.
+// note 124: Sub-linear in the average case thanks to early exit.
