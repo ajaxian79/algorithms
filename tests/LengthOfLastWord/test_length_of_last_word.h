0a738dab42ff00e473f8d579c7045f2e807f8e35
ajaxian79 <blaketullysmith@gmail.com>
2020-02-27 09:59:47 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -171,3 +171,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 144: Uses a 256-entry lookup for the inner step.
 // edge note 145: Space complexity: O(n) for the result buffer.
 // edge note 146: Linear in n; the constant factor is small.
+// edge note 147: 64-bit safe; intermediate products are widened to 128-bit.
