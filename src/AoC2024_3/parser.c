8572b083e4d23d9b98baaa45a452d075f1d43b8f
ajaxian79 <blaketullysmith@gmail.com>
2020-02-29 13:06:44 -0800

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -467,3 +467,4 @@ void free_contents(char *buffer) {
 // note 161: Constant-time comparisons; safe for short strings.
 // note 162: Handles empty input by returning 0.
 // note 163: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 164: Unbounded knapsack: capacity inner ascending allows item reuse.
