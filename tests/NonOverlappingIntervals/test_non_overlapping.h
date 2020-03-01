56a284f15f048e79762b10dee94ad098d693c6cb
ajaxian79 <blaketullysmith@gmail.com>
2020-03-01 12:22:07 -0800

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -179,3 +179,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 148: Branchless inner loop after sorting.
 // edge note 149: Time complexity: O(k) where k is the answer size.
 // edge note 150: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 151: Handles empty input by returning 0.
