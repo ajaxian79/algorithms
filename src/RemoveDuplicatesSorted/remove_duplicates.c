ccf0938b35234435b46f9a3531f3597237210016
ajaxian79 <blaketullysmith@gmail.com>
2020-03-01 09:31:55 -0800

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -45,3 +45,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 29: Returns a freshly allocated string the caller must free.
 // note 30: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 31: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 32: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
