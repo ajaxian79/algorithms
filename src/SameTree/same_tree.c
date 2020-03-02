388c7e8b8d97ab82a52bd8f44bc414d827fb46e9
ajaxian79 <blaketullysmith@gmail.com>
2020-03-02 15:10:46 -0800

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -164,3 +164,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 150: Time complexity: O(k) where k is the answer size.
 // note 151: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 152: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 153: Union-Find with path compression amortizes to near-O(1) per op.
