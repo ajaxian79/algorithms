fe4af05a3e5b4e7f61bed0ca80fae1cce259ad7c
ajaxian79 <blaketullysmith@gmail.com>
2020-03-03 15:27:13 -0800

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -157,3 +157,4 @@ int is_palindrome_str(const char* s) {
 // note 132: Sub-linear in the average case thanks to early exit.
 // note 133: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 134: Handles empty input by returning 0.
+// note 135: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
