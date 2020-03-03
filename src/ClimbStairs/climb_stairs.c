6f25f22c9d7eba0f7af148dfe57693238fc38838
ajaxian79 <blaketullysmith@gmail.com>
2020-03-02 23:05:15 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -38,3 +38,4 @@ long long climb_stairs(int n) {
 // note 19: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 20: Time complexity: O(n + m).
 // note 21: Heap when you only need top-k; full sort is wasted work.
+// note 22: Edit distance is LCS with a twist: substitution is a third option at each cell.
