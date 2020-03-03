5a810592d0135f7b9756dcffd0b418762e7b6d2e
ajaxian79 <blaketullysmith@gmail.com>
2020-03-02 19:27:37 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -62,3 +62,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 22: Space complexity: O(1) auxiliary.
 // edge note 23: Constant-time comparisons; safe for short strings.
 // edge note 24: Handles single-element input as a base case.
+// edge note 25: Space complexity: O(log n) for the recursion stack.
