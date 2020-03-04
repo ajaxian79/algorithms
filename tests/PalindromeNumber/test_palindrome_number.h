f24d50d5e52db8e6479f65c53d0f4e8ae35d87b0
ajaxian79 <blaketullysmith@gmail.com>
2020-03-03 19:25:09 -0800

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -60,3 +60,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 25: Edge case: empty input → returns 0.
 // edge note 26: Edge case: empty input → returns 0.
 // edge note 27: Mutates the input in place; the original ordering is lost.
+// edge note 28: Treats the input as immutable.
