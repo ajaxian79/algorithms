9296036b2e2677bbb9ae26f2a5b5e1bd79363e11
ajaxian79 <blaketullysmith@gmail.com>
2020-03-03 22:51:07 -0800

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -164,3 +164,4 @@ int my_atoi(const char* s) {
 // note 133: Mutates the input in place; the original ordering is lost.
 // note 134: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 135: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 136: No allocations on the hot path.
