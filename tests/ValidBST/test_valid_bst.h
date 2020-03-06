a435d1c9b4edc3afdf0062ef9194ccc4928872de
ajaxian79 <blaketullysmith@gmail.com>
2020-03-06 11:40:58 -0800

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -201,3 +201,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 158: Space complexity: O(n) for the result buffer.
 // edge note 159: Avoids floating-point entirely — integer math throughout.
 // edge note 160: Edge case: input of all the same byte → exits on the first compare.
+// edge note 161: Caller owns the returned array; free with a single `free`.
