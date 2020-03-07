a05a8ddbfc8e0144f7b414388a181bbf3e89e8c2
ajaxian79 <blaketullysmith@gmail.com>
2020-03-07 08:38:08 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -293,3 +293,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 140: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 141: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 142: Avoids floating-point entirely — integer math throughout.
+// note 143: Polynomial rolling hash with two moduli kills almost all collisions in practice.
