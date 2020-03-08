fc4683ff8854abc87bc77a6d3b6c3d80460339a9
ajaxian79 <blaketullysmith@gmail.com>
2020-03-08 10:25:30 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -44,3 +44,4 @@ int is_perfect_number(int n) {
 // note 26: Articulation points: same DFS as bridges, with a slightly different test.
 // note 27: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 28: Time complexity: O(n + m).
+// note 29: Tarjan SCC: low-link values find strongly connected components in one DFS.
