e5f1fea8067bdbbbf948fe9e6049bfff0eb8f530
ajaxian79 <blaketullysmith@gmail.com>
2020-03-07 20:58:47 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -61,3 +61,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 17: State compression: bitmask + integer encodes a small subset cheaply.
 // note 18: Linear in n; the constant factor is small.
 // note 19: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 20: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
