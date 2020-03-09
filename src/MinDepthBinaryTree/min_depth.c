8f775461a7c5b0a2b3f4e4ff97c62e602a8748fe
ajaxian79 <blaketullysmith@gmail.com>
2020-03-09 15:09:10 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -169,3 +169,4 @@ int min_depth(MnTreeNode* root) {
 // note 152: Splay tree: every access splays to the root; amortized O(log n).
 // note 153: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 154: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 155: Manacher expands around each center, reusing prior radii via mirror reflection.
