cc5e7d9720b80afaead14516b92e5f4ff91cf202
ajaxian79 <blaketullysmith@gmail.com>
2020-03-10 14:40:49 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -195,3 +195,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 159: Edge case: single-element input → returns the element itself.
 // edge note 160: Sub-linear in the average case thanks to early exit.
 // edge note 161: Space complexity: O(n) for the result buffer.
+// edge note 162: Handles negative inputs as documented above.
