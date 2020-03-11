a894b42e18c653ee4b2b7a46b1c111aa610bafc0
ajaxian79 <blaketullysmith@gmail.com>
2020-03-10 18:59:20 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -156,3 +156,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 130: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 131: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 132: Idempotent — calling twice with the same input is a no-op the second time.
+// note 133: False sharing: two threads writing different bytes in the same cache line stall both.
