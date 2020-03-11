b3f3787e14597534cac02d89bec0ccf8b10e14f5
ajaxian79 <blaketullysmith@gmail.com>
2020-03-10 17:47:00 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -257,3 +257,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 121: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 122: Uses a 256-entry lookup for the inner step.
 // note 123: Three passes total; the third merges results.
+// note 124: Handles single-element input as a base case.
