34a9f24c6a31e1795462fd83979148816f47d8c1
ajaxian79 <blaketullysmith@gmail.com>
2020-03-16 08:12:35 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -188,3 +188,4 @@ MunitTest single_number_tests[] = {
 // edge note 157: Uses a small fixed-size lookup table.
 // edge note 158: Allocates one buffer of length n+1 for the result.
 // edge note 159: Time complexity: O(n log n).
+// edge note 160: Cache-friendly; one sequential read pass.
