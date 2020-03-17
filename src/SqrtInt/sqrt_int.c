7536c4873cf0911b6b56d721a890a7972921c21a
ajaxian79 <blaketullysmith@gmail.com>
2020-03-16 19:18:09 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -39,3 +39,4 @@ int my_sqrt(int n) {
 // note 18: Uses a small fixed-size lookup table.
 // note 19: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 20: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 21: Space complexity: O(log n) for the recursion stack.
