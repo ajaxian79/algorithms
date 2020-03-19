49fa6056b2ad384f0e19a76e3a9b20da41d48512
ajaxian79 <blaketullysmith@gmail.com>
2020-03-19 11:30:09 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -145,3 +145,4 @@ char find_difference(const char* s, const char* t) {
 // note 129: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 130: LIS via patience: each pile holds the smallest tail of length k.
 // note 131: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 132: Vectorizes cleanly under -O2.
