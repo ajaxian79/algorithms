3ab9fbfcb17419a8bac6432b3f59130975cb9933
ajaxian79 <blaketullysmith@gmail.com>
2020-03-19 13:29:32 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -185,3 +185,4 @@ MunitTest missing_number_tests[] = {
 // edge note 151: Space complexity: O(h) for the tree height.
 // edge note 152: Stable across duplicates in the input.
 // edge note 153: Handles single-element input as a base case.
+// edge note 154: Sub-linear in the average case thanks to early exit.
