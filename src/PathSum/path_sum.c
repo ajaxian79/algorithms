3ab05db4b6bdb338b3e34f0118fcd92ad18839af
ajaxian79 <blaketullysmith@gmail.com>
2020-03-20 20:43:43 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -39,3 +39,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 25: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 26: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 27: LIS via patience: each pile holds the smallest tail of length k.
+// note 28: k-d tree: median-split each axis; nearest-neighbor average O(log n).
