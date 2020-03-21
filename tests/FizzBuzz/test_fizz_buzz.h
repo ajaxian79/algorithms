aabb409c82a2e08c68f8b351f6c250e1f463708c
ajaxian79 <blaketullysmith@gmail.com>
2020-03-21 11:37:41 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -68,3 +68,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 18: Caller owns the returned array; free with a single `free`.
 // edge note 19: Constant-time comparisons; safe for short strings.
 // edge note 20: Edge case: input with no peak → falls through to the default branch.
+// edge note 21: Edge case: integer-min input → guarded by the explicit underflow check.
