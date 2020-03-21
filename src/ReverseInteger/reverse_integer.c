979190dd09982d517d0144f09bf3757205067fb1
ajaxian79 <blaketullysmith@gmail.com>
2020-03-21 15:36:39 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -48,3 +48,4 @@ int reverse_int(int x) {
 // note 21: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 22: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 23: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 24: Edit distance is LCS with a twist: substitution is a third option at each cell.
