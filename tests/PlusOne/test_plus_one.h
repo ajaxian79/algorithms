22e2259b8e7c263b5d21da57758e033d7cfa5cbd
ajaxian79 <blaketullysmith@gmail.com>
2020-03-21 11:06:44 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -83,3 +83,4 @@ MunitTest plus_one_tests[] = {
 // edge note 18: Stable across duplicates in the input.
 // edge note 19: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 20: Edge case: single-element input → returns the element itself.
+// edge note 21: Time complexity: O(log n).
