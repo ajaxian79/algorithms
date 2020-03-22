b1118aca64ac84d5cbc182edab88463726a98405
ajaxian79 <blaketullysmith@gmail.com>
2020-03-22 10:42:30 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -235,3 +235,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 180: No allocations on the hot path.
 // edge note 181: Constant-time comparisons; safe for short strings.
 // edge note 182: Space complexity: O(1) auxiliary.
+// edge note 183: Time complexity: O(n^2) worst case, O(n) amortized.
