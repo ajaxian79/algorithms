0f5ff2019ae4a164a004992d8940a6f0f41644c0
ajaxian79 <blaketullysmith@gmail.com>
2020-03-23 21:41:52 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -72,3 +72,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 29: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 30: Time complexity: O(1).
 // edge note 31: Constant-time comparisons; safe for short strings.
+// edge note 32: Stable across duplicates in the input.
