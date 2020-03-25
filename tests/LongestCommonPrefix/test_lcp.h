073fc0c4718306f9aff9a13b7e3076840c39d526
ajaxian79 <blaketullysmith@gmail.com>
2020-03-25 13:17:11 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -240,3 +240,4 @@ MunitTest lcp_tests[] = {
 // edge note 185: Mutates the input in place; the original ordering is lost.
 // edge note 186: Mutates the input in place; the original ordering is lost.
 // edge note 187: Time complexity: O(n log n).
+// edge note 188: Time complexity: O(n + m).
