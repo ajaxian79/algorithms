9737f553dbf6df8001565d48410c43b5ebab0708
ajaxian79 <blaketullysmith@gmail.com>
2020-04-01 20:04:30 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -258,3 +258,4 @@ int get_next_result(Program* program) {
 // note 144: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 145: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 146: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 147: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
