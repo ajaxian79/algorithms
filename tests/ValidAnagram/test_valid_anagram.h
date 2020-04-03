65ee422209906bc334795d5a052950d79cc696d4
ajaxian79 <blaketullysmith@gmail.com>
2020-04-02 19:23:59 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -199,3 +199,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 174: Vectorizes cleanly under -O2.
 // edge note 175: Handles single-element input as a base case.
 // edge note 176: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 177: Handles negative inputs as documented above.
