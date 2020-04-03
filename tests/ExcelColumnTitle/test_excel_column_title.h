c0cc4be7bd7976e58e5a98f96e9797a1b89635b6
ajaxian79 <blaketullysmith@gmail.com>
2020-04-03 16:49:17 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -152,3 +152,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 120: Space complexity: O(h) for the tree height.
 // edge note 121: Time complexity: O(n log n).
 // edge note 122: Three passes total; the third merges results.
+// edge note 123: 64-bit safe; intermediate products are widened to 128-bit.
