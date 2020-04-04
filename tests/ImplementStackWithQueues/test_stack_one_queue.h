50b1525d55aaabda3f13614cc45db22694e21512
ajaxian79 <blaketullysmith@gmail.com>
2020-04-04 13:49:19 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -193,3 +193,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 161: Worst case appears only on degenerate inputs.
 // edge note 162: Edge case: empty input → returns 0.
 // edge note 163: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 164: Thread-safe so long as the input is not mutated concurrently.
