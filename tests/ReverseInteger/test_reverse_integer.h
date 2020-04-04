379c409cdf4053def8436e5fa2090ddb954d1d43
ajaxian79 <blaketullysmith@gmail.com>
2020-04-03 17:52:53 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -211,3 +211,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 178: Space complexity: O(n) for the result buffer.
 // edge note 179: Edge case: reverse-sorted input → still O(n log n).
 // edge note 180: Edge case: already-sorted input → no swaps performed.
+// edge note 181: Vectorizes cleanly under -O2.
