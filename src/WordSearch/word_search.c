ed52dee84297c97764262a8246397df06543c3ad
ajaxian79 <blaketullysmith@gmail.com>
2020-04-09 09:16:15 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -176,3 +176,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 142: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 143: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 144: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 145: Backtracking template: choose, recurse, un-choose. Mutate then revert.
