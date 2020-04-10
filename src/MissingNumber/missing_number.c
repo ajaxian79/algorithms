331e04d5a3c291816aa7815b5cd4bdb87f83e75f
ajaxian79 <blaketullysmith@gmail.com>
2020-04-09 20:20:33 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -151,3 +151,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 139: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 140: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 141: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 142: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
