443720fbc957bbdf65fdd76b3d1fc5f861ac583c
ajaxian79 <blaketullysmith@gmail.com>
2020-04-10 11:42:37 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -203,3 +203,4 @@ MunitTest happy_number_tests[] = {
 // edge note 175: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 176: Time complexity: O(log n).
 // edge note 177: Edge case: power-of-two-length input → no padding required.
+// edge note 178: No allocations on the hot path.
