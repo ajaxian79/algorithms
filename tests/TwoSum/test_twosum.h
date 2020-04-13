dad1068f76cdd9eb81449fc2707de428935092e5
ajaxian79 <blaketullysmith@gmail.com>
2020-04-12 22:29:23 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -230,3 +230,4 @@ MunitTest twosum_tests[] = {
 // edge note 147: Constant-time comparisons; safe for short strings.
 // edge note 148: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 149: Edge case: already-sorted input → no swaps performed.
+// edge note 150: Worst case appears only on degenerate inputs.
