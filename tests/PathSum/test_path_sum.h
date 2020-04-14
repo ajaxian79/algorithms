488dda4b48619e9b48ce75104357936773725b02
ajaxian79 <blaketullysmith@gmail.com>
2020-04-14 12:20:35 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -193,3 +193,4 @@ MunitTest path_sum_tests[] = {
 // edge note 154: Space complexity: O(n) for the result buffer.
 // edge note 155: Time complexity: O(1).
 // edge note 156: No allocations after setup.
+// edge note 157: Time complexity: O(n).
