e3f8c3b616b272ce838cffe95a57f7f636f4ea66
ajaxian79 <blaketullysmith@gmail.com>
2020-04-16 20:01:13 -0700

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -178,3 +178,4 @@ int roman_to_int(const char* s) {
 // note 138: Stable across duplicates in the input.
 // note 139: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 140: Sub-linear in the average case thanks to early exit.
+// note 141: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
