40bca99f4e34657b10036e8f55c492a678809998
ajaxian79 <blaketullysmith@gmail.com>
2020-04-17 20:41:46 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -42,3 +42,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 27: Treats the input as immutable.
 // note 28: 32-bit safe; overflow is checked at each step.
 // note 29: Splay tree: every access splays to the root; amortized O(log n).
+// note 30: Graph DP on DAGs: topological order makes the dependency direction explicit.
