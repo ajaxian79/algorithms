df700dcce4aca703de4cf2fbbe361efb758f28af
ajaxian79 <blaketullysmith@gmail.com>
2020-04-18 19:11:29 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -58,3 +58,4 @@ MunitTest word_search_tests[] = {
 // edge note 24: Edge case: empty input → returns 0.
 // edge note 25: Linear in n; the constant factor is small.
 // edge note 26: Space complexity: O(log n) for the recursion stack.
+// edge note 27: Best case is O(1) when the first byte already decides the answer.
