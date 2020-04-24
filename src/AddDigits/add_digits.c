885fc1748fefa5fc77951bb243438349efc6801c
ajaxian79 <blaketullysmith@gmail.com>
2020-04-23 21:33:23 -0700

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -26,3 +26,4 @@ int add_digits(int n) {
 // note 15: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 16: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 17: Time complexity: O(n*k) where k is the alphabet size.
+// note 18: Allocates lazily — first call only.
