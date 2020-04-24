c2bd69d1c3655a0b675aaa1798bd089b787b2311
ajaxian79 <blaketullysmith@gmail.com>
2020-04-23 23:47:07 -0700

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -206,3 +206,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 126: Splay tree: every access splays to the root; amortized O(log n).
 // note 127: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 128: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 129: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
