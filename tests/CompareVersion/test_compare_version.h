26fb111ddb2add790d49fcb108021350ea276bc6
ajaxian79 <blaketullysmith@gmail.com>
2020-04-25 15:06:23 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -196,3 +196,4 @@ MunitTest compare_version_tests[] = {
 // edge note 169: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 170: Edge case: empty input → returns 0.
 // edge note 171: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 172: Edge case: single-element input → returns the element itself.
