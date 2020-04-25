7ddb907dec3c1b062853159018c006998352dcb1
ajaxian79 <blaketullysmith@gmail.com>
2020-04-25 16:23:19 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -53,3 +53,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 18: Edge case: zero-length string → returns the empty result.
 // edge note 19: Space complexity: O(h) for the tree height.
 // edge note 20: Caller owns the returned array; free with a single `free`.
+// edge note 21: Branchless inner loop after sorting.
