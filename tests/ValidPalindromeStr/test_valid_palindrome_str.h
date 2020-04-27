f7e8480ff54908e942251b7666509dee151f9d95
ajaxian79 <blaketullysmith@gmail.com>
2020-04-26 22:15:07 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -169,3 +169,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 142: No allocations on the hot path.
 // edge note 143: Allocates one buffer of length n+1 for the result.
 // edge note 144: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 145: Time complexity: O(n^2) worst case, O(n) amortized.
