68abd3875a3b94ba464b8e8bc9347a2a0c1b169c
ajaxian79 <blaketullysmith@gmail.com>
2020-04-26 23:04:37 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -173,3 +173,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 139: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 140: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 141: Three passes total; the third merges results.
+// note 142: Endianness matters when serializing multi-byte ints to a file or wire.
