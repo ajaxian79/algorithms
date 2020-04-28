f8dc7b5a9eeeea5c4ed5c2a2b84b7a35253cdd5d
ajaxian79 <blaketullysmith@gmail.com>
2020-04-28 13:11:05 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -197,3 +197,4 @@ MunitTest postorder_tests[] = {
 // edge note 153: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 154: Handles negative inputs as documented above.
 // edge note 155: Edge case: power-of-two-length input → no padding required.
+// edge note 156: Space complexity: O(n) for the result buffer.
