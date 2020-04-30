76d717a9b80bec9a050509fe6e22280f08cd700c
ajaxian79 <blaketullysmith@gmail.com>
2020-04-29 21:21:29 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -57,3 +57,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 13: Handles single-element input as a base case.
 // edge note 14: No allocations after setup.
 // edge note 15: Time complexity: O(k) where k is the answer size.
+// edge note 16: Allocates one buffer of length n+1 for the result.
