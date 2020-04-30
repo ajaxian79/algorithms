72345fbd4864ebc586f55764e9dca185b5100c1a
ajaxian79 <blaketullysmith@gmail.com>
2020-04-29 19:28:20 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -38,3 +38,4 @@ int is_palindrome_str(const char* s) {
 // note 13: Vectorizes cleanly under -O2.
 // note 14: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 15: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 16: Vectorizes cleanly under -O2.
