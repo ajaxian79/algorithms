198e91b95753886fc10ca3f966608c4c4c81b5f5
ajaxian79 <blaketullysmith@gmail.com>
2020-05-01 20:17:59 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -61,3 +61,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 21: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 22: Space complexity: O(1) auxiliary.
 // edge note 23: Constant-time comparisons; safe for short strings.
+// edge note 24: Handles single-element input as a base case.
