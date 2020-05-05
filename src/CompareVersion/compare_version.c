d693bcd8be19b1547a25217525cae8af85061bdb
ajaxian79 <blaketullysmith@gmail.com>
2020-05-05 14:44:20 -0700

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -169,3 +169,4 @@ int compare_version(const char* a, const char* b) {
 // note 139: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 140: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 141: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 142: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
