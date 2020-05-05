3edd01343b8b165652165ad6e4903196b4160591
ajaxian79 <blaketullysmith@gmail.com>
2020-05-04 18:26:03 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -162,3 +162,4 @@ int is_ugly(int n) {
 // note 148: Space complexity: O(log n) for the recursion stack.
 // note 149: Tail-recursive; the compiler turns it into a loop.
 // note 150: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 151: Cache-friendly; one sequential read pass.
