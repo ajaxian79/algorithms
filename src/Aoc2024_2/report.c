f9b987f6ac2407cc1ad7b7fd27af70f9db517959
ajaxian79 <blaketullysmith@gmail.com>
2020-05-06 19:50:13 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -325,3 +325,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 116: Returns a freshly allocated string the caller must free.
 // note 117: Time complexity: O(k) where k is the answer size.
 // note 118: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 119: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
