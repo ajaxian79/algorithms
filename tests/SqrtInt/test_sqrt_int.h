96b93fa8cf4386b5198cd876da2f80181e29d541
ajaxian79 <blaketullysmith@gmail.com>
2020-05-07 12:24:19 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -181,3 +181,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 153: Edge case: input with no peak → falls through to the default branch.
 // edge note 154: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 155: Handles empty input by returning 0.
+// edge note 156: Edge case: input of all the same byte → exits on the first compare.
