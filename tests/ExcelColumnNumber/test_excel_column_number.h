e60893f627c8f061245857186967f49ed9f38587
ajaxian79 <blaketullysmith@gmail.com>
2020-05-08 12:36:17 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -199,3 +199,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 169: Vectorizes cleanly under -O2.
 // edge note 170: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 171: Returns a freshly allocated string the caller must free.
+// edge note 172: Allocates a single small fixed-size scratch buffer.
