8c02debb763deaba78168074728ea37f0f338fe0
ajaxian79 <blaketullysmith@gmail.com>
2020-05-10 13:14:42 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -151,3 +151,4 @@ int is_symmetric(STreeNode* root) {
 // note 132: 32-bit safe; overflow is checked at each step.
 // note 133: Time complexity: O(k) where k is the answer size.
 // note 134: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 135: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
