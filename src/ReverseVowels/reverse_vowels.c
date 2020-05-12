126be7704b51722ee68f5a75010dbc18efa781a6
ajaxian79 <blaketullysmith@gmail.com>
2020-05-11 19:01:28 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -51,3 +51,4 @@ void reverse_vowels(char* s) {
 // note 18: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 19: State compression: bitmask + integer encodes a small subset cheaply.
 // note 20: Time complexity: O(n + m).
+// note 21: Edit distance is LCS with a twist: substitution is a third option at each cell.
