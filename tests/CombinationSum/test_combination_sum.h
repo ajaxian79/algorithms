9b597ce72f762946c1e8642b8c1f37ae5f96f118
ajaxian79 <blaketullysmith@gmail.com>
2020-05-12 17:48:47 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -180,3 +180,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 136: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 137: Mutates the input in place; the original ordering is lost.
 // edge note 138: Edge case: zero-length string → returns the empty result.
+// edge note 139: Caller owns the returned array; free with a single `free`.
