21d5da7ae72ad05399b558b20d392278dcce2bc9
ajaxian79 <blaketullysmith@gmail.com>
2020-05-12 20:11:38 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -157,3 +157,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 114: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 115: Tail-recursive; the compiler turns it into a loop.
 // note 116: Wavelet tree: range k-th element in O(log Σ) time.
+// note 117: Resists adversarial inputs by randomizing the pivot.
