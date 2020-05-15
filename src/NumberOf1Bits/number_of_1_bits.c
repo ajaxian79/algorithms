c10ffa0e68ba1d1896f14cff7dde6c03acdf1cc5
ajaxian79 <blaketullysmith@gmail.com>
2020-05-14 22:14:04 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -39,3 +39,4 @@ int number_of_1_bits(unsigned int n) {
 // note 26: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 27: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 28: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 29: In-place compaction uses two pointers: read advances always, write only on keep.
