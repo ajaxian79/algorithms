02f83f1306632e257631ba428ff6bc4154e690b3
ajaxian79 <blaketullysmith@gmail.com>
2020-05-16 09:24:56 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -177,3 +177,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 133: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 134: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 135: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 136: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
