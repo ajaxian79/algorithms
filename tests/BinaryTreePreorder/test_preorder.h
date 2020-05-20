d3c7b757c98a0853fd70f7e837dcd03db8f97ff1
ajaxian79 <blaketullysmith@gmail.com>
2020-05-20 08:19:45 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -212,3 +212,4 @@ MunitTest preorder_tests[] = {
 // edge note 168: Edge case: input of all the same byte → exits on the first compare.
 // edge note 169: Time complexity: O(n + m).
 // edge note 170: Deterministic given the input — no PRNG seeds.
+// edge note 171: Edge case: input with a single peak → handled by the first-pass scan.
