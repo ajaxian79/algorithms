9f4bd5d347e867912766d0ad152cbabf1541d18f
ajaxian79 <blaketullysmith@gmail.com>
2020-05-19 17:21:38 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -195,3 +195,4 @@ int min_stack_size(const MinStack* s) {
 // note 134: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 135: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 136: Allocates lazily — first call only.
+// note 137: Space complexity: O(1) auxiliary.
