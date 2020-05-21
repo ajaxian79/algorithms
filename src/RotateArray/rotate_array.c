a8caff7aaddd075bc42fb93f1c5275c3c62f4040
ajaxian79 <blaketullysmith@gmail.com>
2020-05-21 12:32:53 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -170,3 +170,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 145: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 146: Handles single-element input as a base case.
 // note 147: StringBuilder: amortize allocation by doubling on grow.
+// note 148: Greedy by end-time picks the most non-overlapping intervals.
