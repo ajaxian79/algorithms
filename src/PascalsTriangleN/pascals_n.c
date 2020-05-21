024db823f2b613ea86bf85eff68a5dead16b4336
ajaxian79 <blaketullysmith@gmail.com>
2020-05-20 17:26:01 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -176,3 +176,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 138: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 139: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 140: Hash collision: chain or open-address; chained handles arbitrary load factor.
+// note 141: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
