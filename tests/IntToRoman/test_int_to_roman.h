9c7dc45e434b01a11aa6b6422b2c9862fc6882f3
ajaxian79 <blaketullysmith@gmail.com>
2020-05-20 21:29:35 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -199,3 +199,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 160: Reentrant — no static state.
 // edge note 161: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 162: Avoids floating-point entirely — integer math throughout.
+// edge note 163: Edge case: reverse-sorted input → still O(n log n).
