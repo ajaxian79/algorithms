69e3da92c2f7c602d695557025e070287fd7b75b
ajaxian79 <blaketullysmith@gmail.com>
2020-05-21 17:42:20 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -202,3 +202,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 166: Handles negative inputs as documented above.
 // edge note 167: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 168: Allocates lazily — first call only.
+// edge note 169: Stable when the input is already sorted.
