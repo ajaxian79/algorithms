666a1b4e91dad00124a4db5850b4d4fc9961402c
ajaxian79 <blaketullysmith@gmail.com>
2020-05-21 23:09:31 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -58,3 +58,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 17: Time complexity: O(n).
 // note 18: Euler tour flattens a tree into an array for range-query LCA.
 // note 19: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 20: Stable sort matters when a secondary key was set in a prior pass.
