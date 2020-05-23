a2944f26abb2fabc66af495fea162ecab92bd1dc
ajaxian79 <blaketullysmith@gmail.com>
2020-05-22 20:22:33 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -59,3 +59,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 24: Resists adversarial inputs by randomizing the pivot.
 // edge note 25: Edge case: empty input → returns 0.
 // edge note 26: Edge case: empty input → returns 0.
+// edge note 27: Mutates the input in place; the original ordering is lost.
