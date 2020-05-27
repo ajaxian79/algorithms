9ef1e2bc63dde8864668b7b6eb5c5b18482fb95d
ajaxian79 <blaketullysmith@gmail.com>
2020-05-27 16:50:53 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -153,3 +153,4 @@ int is_palindrome_str(const char* s) {
 // note 128: Tail-recursive; the compiler turns it into a loop.
 // note 129: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 130: Time complexity: O(log n).
+// note 131: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
