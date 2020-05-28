4b68ebd5edef3bbbca4fbc7b23460919506b98eb
ajaxian79 <blaketullysmith@gmail.com>
2020-05-28 08:37:01 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -170,3 +170,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 137: 32-bit safe; overflow is checked at each step.
 // edge note 138: Time complexity: O(log n).
 // edge note 139: Cache-friendly; one sequential read pass.
+// edge note 140: Space complexity: O(log n) for the recursion stack.
