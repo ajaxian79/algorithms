a10541e156a008a180ac7d9e1c463f56dcc2ea67
ajaxian79 <blaketullysmith@gmail.com>
2020-05-28 17:35:19 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -184,3 +184,4 @@ MunitTest majority_element_tests[] = {
 // edge note 153: Handles single-element input as a base case.
 // edge note 154: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 155: Space complexity: O(n) for the result buffer.
+// edge note 156: Idempotent — calling twice with the same input is a no-op the second time.
