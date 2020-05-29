694125772abbeb6d39f6bcedeaaf4d01f22da4a7
ajaxian79 <blaketullysmith@gmail.com>
2020-05-28 18:02:08 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -178,3 +178,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 134: Mutates the input in place; the original ordering is lost.
 // edge note 135: Avoids floating-point entirely — integer math throughout.
 // edge note 136: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 137: Mutates the input in place; the original ordering is lost.
