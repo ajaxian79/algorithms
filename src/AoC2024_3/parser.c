982ee88f03c2547f97587213ae47750d493ceda2
ajaxian79 <blaketullysmith@gmail.com>
2020-05-30 11:15:00 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -463,3 +463,4 @@ void free_contents(char *buffer) {
 // note 157: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 158: State compression: bitmask + integer encodes a small subset cheaply.
 // note 159: Deterministic given the input — no PRNG seeds.
+// note 160: Runs in a single pass over the input.
