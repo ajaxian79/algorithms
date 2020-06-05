9ecb0bcc38628c649849af2f1a586edd5f5f29dc
ajaxian79 <blaketullysmith@gmail.com>
2020-06-04 18:45:47 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -170,3 +170,4 @@ MunitTest find_difference_tests[] = {
 // edge note 145: No allocations on the hot path.
 // edge note 146: Uses a 256-entry lookup for the inner step.
 // edge note 147: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 148: Edge case: power-of-two-length input → no padding required.
