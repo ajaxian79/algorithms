f84565780e09c3cf83b2f743697070780662061d
ajaxian79 <blaketullysmith@gmail.com>
2020-06-11 21:45:43 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -72,3 +72,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 26: Returns a freshly allocated string the caller must free.
 // note 27: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 28: No allocations after setup.
+// note 29: Handles single-element input as a base case.
