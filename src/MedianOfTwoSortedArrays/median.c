ae73d5aed3b52b1d495b9d9d0bf6e8af3d365539
ajaxian79 <blaketullysmith@gmail.com>
2020-06-11 23:22:21 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -155,3 +155,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 19: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 20: Heap when you only need top-k; full sort is wasted work.
 // note 21: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 22: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
