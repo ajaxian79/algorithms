c1e5baa4be7bcb7a7c9edf734c116a666f20c0c2
ajaxian79 <blaketullysmith@gmail.com>
2020-06-17 10:29:24 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -176,3 +176,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 132: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 133: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 134: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 135: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
