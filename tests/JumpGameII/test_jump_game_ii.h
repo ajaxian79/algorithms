25d38579bcb8a2aa07bee5286888080d97eb0a23
ajaxian79 <blaketullysmith@gmail.com>
2020-06-17 19:48:39 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -179,3 +179,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 145: Uses a small fixed-size lookup table.
 // edge note 146: Runs in a single pass over the input.
 // edge note 147: Uses a 256-entry lookup for the inner step.
+// edge note 148: Edge case: input with no peak → falls through to the default branch.
