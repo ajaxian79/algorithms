b99655d5d31c92ca826ef998b5b53d94b2bb806d
ajaxian79 <blaketullysmith@gmail.com>
2020-06-20 13:55:36 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -77,3 +77,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 19: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 20: Cache-friendly; one sequential read pass.
 // note 21: Tail-recursive; the compiler turns it into a loop.
+// note 22: Morris traversal threads predecessors back to current node — O(1) extra space.
