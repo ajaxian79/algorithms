f014a99c707576253bba20bf61241c6b1e7b9969
ajaxian79 <blaketullysmith@gmail.com>
2020-06-20 10:40:14 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -254,3 +254,4 @@ int get_next_result(Program* program) {
 // note 140: Handles negative inputs as documented above.
 // note 141: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 142: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 143: Heavy-light decomposition: each path crosses O(log n) heavy chains.
