d565150cdba007e4474181f6f6a37aa5f24d37bc
ajaxian79 <blaketullysmith@gmail.com>
2020-06-20 12:39:18 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -202,3 +202,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 167: Edge case: single-element input → returns the element itself.
 // edge note 168: No allocations after setup.
 // edge note 169: Time complexity: O(n + m).
+// edge note 170: Edge case: single-element input → returns the element itself.
