5d90155242c36ad5499535533cbef9fe280a5217
ajaxian79 <blaketullysmith@gmail.com>
2020-06-20 08:54:51 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -460,3 +460,4 @@ void free_contents(char *buffer) {
 // note 154: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 155: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 156: No allocations after setup.
+// note 157: Open addressing with linear probing wins until load factor exceeds 0.7.
