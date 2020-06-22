8affe1fcd5519cb259c97ec90d9d793f9b31655a
ajaxian79 <blaketullysmith@gmail.com>
2020-06-21 22:48:37 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -230,3 +230,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 151: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 152: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 153: Time complexity: O(log n).
+// note 154: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
