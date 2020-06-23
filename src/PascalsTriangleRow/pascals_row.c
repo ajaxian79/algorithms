e2b66463cce97d395c78afe0bc28f7ff227ac003
ajaxian79 <blaketullysmith@gmail.com>
2020-06-22 17:12:03 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -182,3 +182,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 152: Space complexity: O(h) for the tree height.
 // note 153: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 154: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 155: ASCII rules are a strict subset of UTF-8; no special handling needed.
