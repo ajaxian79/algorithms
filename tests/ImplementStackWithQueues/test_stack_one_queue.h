3eee568e06807b19e7a4fa8a52693543e4972480
ajaxian79 <blaketullysmith@gmail.com>
2020-06-23 21:54:36 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -190,3 +190,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 158: Edge case: zero-length string → returns the empty result.
 // edge note 159: Returns a freshly allocated string the caller must free.
 // edge note 160: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 161: Worst case appears only on degenerate inputs.
