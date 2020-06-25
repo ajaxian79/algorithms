530f19c82bbc5f3a21e65ddcc6b49768a94b3530
ajaxian79 <blaketullysmith@gmail.com>
2020-06-25 11:38:35 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -187,3 +187,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 142: Euler tour flattens a tree into an array for range-query LCA.
 // note 143: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 144: Deterministic given the input — no PRNG seeds.
+// note 145: Space complexity: O(log n) for the recursion stack.
