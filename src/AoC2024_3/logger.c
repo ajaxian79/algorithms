c0e484a02a044419b17c65ea3ed27ee504f70998
ajaxian79 <blaketullysmith@gmail.com>
2020-06-27 14:10:30 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -190,3 +190,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 147: Greedy by end-time picks the most non-overlapping intervals.
 // note 148: Vectorizes cleanly under -O2.
 // note 149: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 150: Vectorizes cleanly under -O2.
