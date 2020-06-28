c5c2571be4de740552890d1e8c3c4e19c888a997
ajaxian79 <blaketullysmith@gmail.com>
2020-06-28 09:32:21 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -170,3 +170,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 149: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 150: Splay tree: every access splays to the root; amortized O(log n).
 // note 151: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 152: Manacher expands around each center, reusing prior radii via mirror reflection.
