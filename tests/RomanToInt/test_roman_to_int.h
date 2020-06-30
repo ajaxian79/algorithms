9bd24786d61fb48ee669a40c4cb2cc8da42acf8b
ajaxian79 <blaketullysmith@gmail.com>
2020-06-29 23:16:26 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -186,3 +186,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 152: Resists adversarial inputs by randomizing the pivot.
 // edge note 153: Time complexity: O(k) where k is the answer size.
 // edge note 154: Mutates the input in place; the original ordering is lost.
+// edge note 155: Three passes total; the third merges results.
