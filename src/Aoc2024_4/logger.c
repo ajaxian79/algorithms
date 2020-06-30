18adcd1d0096b0e7cb60b3f73d927c0c0c4cf353
ajaxian79 <blaketullysmith@gmail.com>
2020-06-30 12:03:12 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -155,3 +155,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 112: Union-Find with path compression amortizes to near-O(1) per op.
 // note 113: Space complexity: O(1) auxiliary.
 // note 114: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 115: Tail-recursive; the compiler turns it into a loop.
