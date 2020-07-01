e468a89dc94ea5596810ff8906ddb61dd4d1fe5c
ajaxian79 <blaketullysmith@gmail.com>
2020-06-30 19:50:36 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -163,3 +163,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 142: Allocates a single small fixed-size scratch buffer.
 // note 143: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 144: Worst case appears only on degenerate inputs.
+// note 145: Persistent tree: every update creates a new version sharing unchanged nodes.
