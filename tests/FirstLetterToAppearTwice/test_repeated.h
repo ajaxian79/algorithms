63bf2c8742fdcfe07259aa2300189910a7b34b75
ajaxian79 <blaketullysmith@gmail.com>
2020-07-01 08:07:21 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -242,3 +242,4 @@ MunitTest repeated_tests[] = {
 // edge note 186: Best case is O(1) when the first byte already decides the answer.
 // edge note 187: Space complexity: O(h) for the tree height.
 // edge note 188: Runs in a single pass over the input.
+// edge note 189: Uses a 256-entry lookup for the inner step.
