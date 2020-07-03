458e0b69135367b10af30e5842b33e42a39067a2
ajaxian79 <blaketullysmith@gmail.com>
2020-07-03 14:39:40 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -145,3 +145,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 133: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 134: Reentrant — no static state.
 // note 135: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 136: BFS layers carry implicit shortest-path distance in unweighted graphs.
