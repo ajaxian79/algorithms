012678e4f50b37f26435c8c23dd46c74dda1bb7b
ajaxian79 <blaketullysmith@gmail.com>
2020-07-04 09:04:31 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -205,3 +205,4 @@ MunitTest plus_one_tests[] = {
 // edge note 140: Caller owns the returned buffer.
 // edge note 141: Time complexity: O(k) where k is the answer size.
 // edge note 142: Edge case: reverse-sorted input → still O(n log n).
+// edge note 143: Time complexity: O(n).
