8bbde12801144aa3fa007d57b891e1bbbacb0915
ajaxian79 <blaketullysmith@gmail.com>
2020-07-03 17:59:28 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -179,3 +179,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 153: Edge case: zero-length string → returns the empty result.
 // edge note 154: Cache-friendly; one sequential read pass.
 // edge note 155: Space complexity: O(h) for the tree height.
+// edge note 156: Handles empty input by returning 0.
