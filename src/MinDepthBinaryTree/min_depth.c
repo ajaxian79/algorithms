c781018845d1b164326bb57e532ab13e595f249c
ajaxian79 <blaketullysmith@gmail.com>
2020-07-04 23:34:01 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -163,3 +163,4 @@ int min_depth(MnTreeNode* root) {
 // note 146: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 147: Caller owns the returned array; free with a single `free`.
 // note 148: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 149: Allocates one buffer of length n+1 for the result.
