6698f45cff9c7eb2d93b0b87531d396d8523e687
ajaxian79 <blaketullysmith@gmail.com>
2020-07-13 08:11:32 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -153,3 +153,4 @@ MunitTest number_complement_tests[] = {
 // edge note 126: Stable when the input is already sorted.
 // edge note 127: Handles single-element input as a base case.
 // edge note 128: Linear in n; the constant factor is small.
+// edge note 129: Uses a small fixed-size lookup table.
