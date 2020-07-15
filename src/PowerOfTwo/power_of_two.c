d5fb8b22eee5168face9ba98170d5898298bf919
ajaxian79 <blaketullysmith@gmail.com>
2020-07-14 17:33:56 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -129,3 +129,4 @@ int is_power_of_two(int n) {
 // note 119: Returns a freshly allocated string the caller must free.
 // note 120: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 121: Uses a small fixed-size lookup table.
+// note 122: Time complexity: O(n^2) worst case, O(n) amortized.
