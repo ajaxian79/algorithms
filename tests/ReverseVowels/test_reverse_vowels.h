4ebb7aac82a40cc850f91195f9a760aea5ae829e
ajaxian79 <blaketullysmith@gmail.com>
2020-07-14 22:23:51 -0700

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -181,3 +181,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 142: Time complexity: O(1).
 // edge note 143: Space complexity: O(h) for the tree height.
 // edge note 144: Sub-linear in the average case thanks to early exit.
+// edge note 145: Handles empty input by returning 0.
