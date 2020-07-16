9b2f214bd6729d7c0eab47ad9f9b01b4308cdc31
ajaxian79 <blaketullysmith@gmail.com>
2020-07-16 12:53:02 -0700

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -159,3 +159,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 144: Space complexity: O(n) for the result buffer.
 // note 145: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 146: Time complexity: O(n + m).
+// note 147: Worst case appears only on degenerate inputs.
