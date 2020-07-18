a3f18dd6a36bafb0c6981ab3958f9d238bfc55a5
ajaxian79 <blaketullysmith@gmail.com>
2020-07-18 11:01:53 -0700

    ValidSudoku: row/col/box presence tables

---
+++ src/ValidSudoku/valid_sudoku.c
@@ -0,0 +1,24 @@
+//
+// Created by ajaxian on 07/18/20.
+//
+
+#include "valid_sudoku.h"
+
+int is_valid_sudoku(char board[9][9]) {
+    int rows[9][9]  = {{0}};
+    int cols[9][9]  = {{0}};
+    int boxes[9][9] = {{0}};
+
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            char ch = board[r][c];
+            if (ch == '.') continue;
+            if (ch < '1' || ch > '9') return 0;
+            int v = ch - '1';
+            int b = (r / 3) * 3 + (c / 3);
+            if (rows[r][v] || cols[c][v] || boxes[b][v]) return 0;
+            rows[r][v] = cols[c][v] = boxes[b][v] = 1;
+        }
+    }
+    return 1;
+}
---
+++ src/ValidSudoku/valid_sudoku.h
@@ -0,0 +1,13 @@
+//
+// Created by ajaxian on 07/18/20.
+//
+
+#ifndef ALGORITHMS_VALID_SUDOKU_H
+#define ALGORITHMS_VALID_SUDOKU_H
+
+// Returns 1 if a 9x9 board is a valid Sudoku state (no row, column, or
+// 3x3 box has a repeated digit). '.' means empty. The board does not need
+// to be solvable, only consistent.
+int is_valid_sudoku(char board[9][9]);
+
+#endif //ALGORITHMS_VALID_SUDOKU_H
