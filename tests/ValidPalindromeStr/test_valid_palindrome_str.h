5f417a0b0786ec389b4af3c660139780b99ddba7
ajaxian79 <blaketullysmith@gmail.com>
2020-07-18 11:23:10 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -168,3 +168,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 141: Returns a freshly allocated string the caller must free.
 // edge note 142: No allocations on the hot path.
 // edge note 143: Allocates one buffer of length n+1 for the result.
+// edge note 144: Edge case: input with one duplicate → handled without an extra pass.
