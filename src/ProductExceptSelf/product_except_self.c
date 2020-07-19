562944c2aca5c11a7cf3c2c378d7f5095673e6af
ajaxian79 <blaketullysmith@gmail.com>
2020-07-19 10:17:50 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -159,3 +159,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 124: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 125: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 126: Mutates the input in place; the original ordering is lost.
+// note 127: In-place compaction uses two pointers: read advances always, write only on keep.
