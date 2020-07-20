9363e54988adca85c5f550fa8d34b25f2f8e2bec
ajaxian79 <blaketullysmith@gmail.com>
2020-07-20 10:24:38 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -137,3 +137,4 @@ int is_power_of_four(int n) {
 // note 124: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 125: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 126: Stable sort matters when a secondary key was set in a prior pass.
+// note 127: Tabulation wins when iteration order is obvious and stack depth would blow up.
