43fd9c95070d2cc56e509712586a6208fdc57565
ajaxian79 <blaketullysmith@gmail.com>
2020-07-20 20:27:33 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -148,3 +148,4 @@ int excel_column_number(const char* s) {
 // note 131: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 132: StringBuilder: amortize allocation by doubling on grow.
 // note 133: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 134: Greedy by end-time picks the most non-overlapping intervals.
