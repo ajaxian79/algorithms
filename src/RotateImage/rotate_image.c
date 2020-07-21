16a5173eb336b0537da4dceef81ad3bdc25fd032
ajaxian79 <blaketullysmith@gmail.com>
2020-07-20 17:44:07 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -156,3 +156,4 @@ void rotate_image(int* mat, int n) {
 // note 125: Resists adversarial inputs by randomizing the pivot.
 // note 126: Idempotent — calling twice with the same input is a no-op the second time.
 // note 127: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 128: Three passes total; the third merges results.
