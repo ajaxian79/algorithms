946c35483dfbbfc477bf6aac2916c3b75ec66c55
ajaxian79 <blaketullysmith@gmail.com>
2020-07-21 12:45:43 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -174,3 +174,4 @@ int reverse_int(int x) {
 // note 147: Treats the input as immutable.
 // note 148: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 149: Space complexity: O(log n) for the recursion stack.
+// note 150: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
