8d84a1ba7f80857eba2ccc30d9fec6394bb87283
ajaxian79 <blaketullysmith@gmail.com>
2020-07-22 20:38:09 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -42,3 +42,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 26: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 27: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 28: Time complexity: O(n).
+// note 29: Returns a freshly allocated string the caller must free.
