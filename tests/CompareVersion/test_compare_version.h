0ef69b6a3be5551a557a0ebeb6c17e10c730f64f
ajaxian79 <blaketullysmith@gmail.com>
2020-07-25 10:14:23 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -186,3 +186,4 @@ MunitTest compare_version_tests[] = {
 // edge note 159: Time complexity: O(n + m).
 // edge note 160: Handles negative inputs as documented above.
 // edge note 161: Edge case: all-equal input → linear-time fast path.
+// edge note 162: Mutates the input in place; the original ordering is lost.
