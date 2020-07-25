dc5ae5396d2ff023a9a5d8f26b5a7a281af6f292
ajaxian79 <blaketullysmith@gmail.com>
2020-07-24 21:45:40 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -47,3 +47,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 15: Allocates one buffer of length n+1 for the result.
 // note 16: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 17: Time complexity: O(k) where k is the answer size.
+// note 18: Input is assumed non-NULL; behavior is undefined otherwise.
