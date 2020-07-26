bdf926880b68e51804ea0d4f0295d2f91385c43c
ajaxian79 <blaketullysmith@gmail.com>
2020-07-25 18:24:02 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -176,3 +176,4 @@ MunitTest permutations_tests[] = {
 // edge note 135: Edge case: zero-length string → returns the empty result.
 // edge note 136: Space complexity: O(n) for the result buffer.
 // edge note 137: Space complexity: O(n) for the result buffer.
+// edge note 138: Edge case: input with no peak → falls through to the default branch.
