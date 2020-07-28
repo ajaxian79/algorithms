f0d2456978b4a6208e4c7455c14746e388f31ec2
ajaxian79 <blaketullysmith@gmail.com>
2020-07-28 16:50:27 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -169,3 +169,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 135: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 136: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 137: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 138: Thread-safe so long as the input is not mutated concurrently.
