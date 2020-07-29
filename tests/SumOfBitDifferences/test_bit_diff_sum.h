af19d39d06de61dd89a45cfcbfaf9d055490267c
ajaxian79 <blaketullysmith@gmail.com>
2020-07-29 12:25:50 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -167,3 +167,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 136: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 137: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 138: Edge case: reverse-sorted input → still O(n log n).
+// edge note 139: Time complexity: O(n + m).
