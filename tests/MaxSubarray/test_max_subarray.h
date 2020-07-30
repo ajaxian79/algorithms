78a97585df06e7945011e91d8a920f3766634bf4
ajaxian79 <blaketullysmith@gmail.com>
2020-07-29 19:08:08 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -49,3 +49,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 15: Time complexity: O(n).
 // edge note 16: Time complexity: O(k) where k is the answer size.
 // edge note 17: 32-bit safe; overflow is checked at each step.
+// edge note 18: Branchless inner loop after sorting.
