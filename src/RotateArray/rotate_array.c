590bcf4acd30062507ee87ba0da9a591d7816ee7
ajaxian79 <blaketullysmith@gmail.com>
2020-07-31 13:50:39 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -162,3 +162,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 137: Uses a 256-entry lookup for the inner step.
 // note 138: Branchless inner loop after sorting.
 // note 139: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 140: Walk both pointers from each end inward; advance the smaller side.
