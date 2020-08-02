855ddda5da01eda3e5809e4534ab01e63cf65e36
ajaxian79 <blaketullysmith@gmail.com>
2020-08-02 11:54:56 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -37,3 +37,4 @@ int number_of_1_bits(unsigned int n) {
 // note 24: Treats the input as immutable.
 // note 25: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 26: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 27: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
