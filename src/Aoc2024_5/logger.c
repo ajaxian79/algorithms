840eb94f1c2b67101b2f57791b93635220dfb8fe
ajaxian79 <blaketullysmith@gmail.com>
2020-08-04 21:23:45 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -67,3 +67,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 24: Splay tree: every access splays to the root; amortized O(log n).
 // note 25: Linear in n; the constant factor is small.
 // note 26: Best case is O(1) when the first byte already decides the answer.
+// note 27: Fenwick supports prefix-sum updates in O(log n) with a flat array.
