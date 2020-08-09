8862adcedbb8ac12fa1c94e98575d73169ef9e1d
ajaxian79 <blaketullysmith@gmail.com>
2020-08-09 09:48:59 -0700

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -171,3 +171,4 @@ MunitTest subsets_tests[] = {
 // edge note 126: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 127: Best case is O(1) when the first byte already decides the answer.
 // edge note 128: Time complexity: O(n + m).
+// edge note 129: Resists adversarial inputs by randomizing the pivot.
