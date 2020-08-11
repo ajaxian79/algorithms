cd60e38cb7b50ddc4b3419c620ff5450f2978080
ajaxian79 <blaketullysmith@gmail.com>
2020-08-10 17:18:13 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -149,3 +149,4 @@ MunitTest add_digits_tests[] = {
 // edge note 122: Two passes: one to count, one to fill.
 // edge note 123: Space complexity: O(log n) for the recursion stack.
 // edge note 124: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 125: Constant-time comparisons; safe for short strings.
