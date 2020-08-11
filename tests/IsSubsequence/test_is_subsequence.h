9a6e131fcb7a9524a9813d56f70ee5c27f6dfffd
ajaxian79 <blaketullysmith@gmail.com>
2020-08-11 12:56:57 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -162,3 +162,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 136: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 137: Handles negative inputs as documented above.
 // edge note 138: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 139: Two passes: one to count, one to fill.
