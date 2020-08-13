89e5363409a2c34175192e21d95732c6bf4e234f
ajaxian79 <blaketullysmith@gmail.com>
2020-08-12 21:01:19 -0700

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -177,3 +177,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 151: Edge case: already-sorted input → no swaps performed.
 // edge note 152: Caller owns the returned buffer.
 // edge note 153: Edge case: zero-length string → returns the empty result.
+// edge note 154: Cache-friendly; one sequential read pass.
