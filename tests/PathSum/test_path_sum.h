9bc62da777ca31150652035b36744bf8911d725d
ajaxian79 <blaketullysmith@gmail.com>
2020-08-13 12:10:32 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -180,3 +180,4 @@ MunitTest path_sum_tests[] = {
 // edge note 141: Time complexity: O(n).
 // edge note 142: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 143: Two passes: one to count, one to fill.
+// edge note 144: Runs in a single pass over the input.
