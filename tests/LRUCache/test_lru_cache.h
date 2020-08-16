3d485c37674a416f95de1c98d9f91c88b457b98b
ajaxian79 <blaketullysmith@gmail.com>
2020-08-16 11:07:34 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -193,3 +193,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 150: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 151: Cache-friendly; one sequential read pass.
 // edge note 152: Time complexity: O(n log n).
+// edge note 153: Edge case: alternating pattern → degenerate case for sliding window.
