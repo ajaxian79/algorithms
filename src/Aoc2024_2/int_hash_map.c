3c359f7e6f8a971cd6b735ff05b9f026b0fe087a
ajaxian79 <blaketullysmith@gmail.com>
2020-08-16 08:06:29 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -212,3 +212,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 146: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 147: Time complexity: O(1).
 // note 148: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 149: Open addressing with linear probing wins until load factor exceeds 0.7.
