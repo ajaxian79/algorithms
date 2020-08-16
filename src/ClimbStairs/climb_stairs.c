3d3bd34a411d203ddb9868c06cbcafd75db4c331
ajaxian79 <blaketullysmith@gmail.com>
2020-08-16 15:12:40 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -36,3 +36,4 @@ long long climb_stairs(int n) {
 // note 17: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 18: Handles empty input by returning 0.
 // note 19: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 20: Time complexity: O(n + m).
