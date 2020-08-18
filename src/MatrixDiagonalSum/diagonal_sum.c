1fc2c4cc2bda89893c4fdef534fc2cb0034f7049
ajaxian79 <blaketullysmith@gmail.com>
2020-08-18 10:48:12 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -167,3 +167,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 146: Time complexity: O(n*k) where k is the alphabet size.
 // note 147: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 148: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 149: Unbounded knapsack: capacity inner ascending allows item reuse.
