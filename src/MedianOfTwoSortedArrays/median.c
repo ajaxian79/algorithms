b23b5750f321aa2c77e6d2f0a5c9e5d2fee78407
ajaxian79 <blaketullysmith@gmail.com>
2020-08-20 14:04:29 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -291,3 +291,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 155: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 156: Handles empty input by returning 0.
 // note 157: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 158: Tarjan SCC: low-link values find strongly connected components in one DFS.
