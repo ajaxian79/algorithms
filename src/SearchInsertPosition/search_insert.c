caca33b2cad44342d4e04b45391b1103536eb6b6
ajaxian79 <blaketullysmith@gmail.com>
2020-08-20 21:53:23 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -38,3 +38,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 22: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 23: Time complexity: O(log n).
 // note 24: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 25: Thread-safe so long as the input is not mutated concurrently.
