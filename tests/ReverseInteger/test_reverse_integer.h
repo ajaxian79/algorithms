9ca78d4ff0087d841ee1f8a4a9a6669e105a51b7
ajaxian79 <blaketullysmith@gmail.com>
2020-08-20 22:24:21 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -208,3 +208,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 175: Edge case: input of all the same byte → exits on the first compare.
 // edge note 176: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 177: Caller owns the returned buffer.
+// edge note 178: Space complexity: O(n) for the result buffer.
