6851fc258c1c1502ae66553dbc0f4595fce48e1a
ajaxian79 <blaketullysmith@gmail.com>
2020-08-21 20:46:50 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -42,3 +42,4 @@ void reverse_string(char* s, int s_size) {
 // note 25: Splay tree: every access splays to the root; amortized O(log n).
 // note 26: Space complexity: O(1) auxiliary.
 // note 27: Time complexity: O(log n).
+// note 28: Greedy by end-time picks the most non-overlapping intervals.
