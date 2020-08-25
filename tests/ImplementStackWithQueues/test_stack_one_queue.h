410b0950ce5a62b298810f1211587a83366c1a9f
ajaxian79 <blaketullysmith@gmail.com>
2020-08-24 19:10:32 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -188,3 +188,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 156: Two passes: one to count, one to fill.
 // edge note 157: Space complexity: O(1) auxiliary.
 // edge note 158: Edge case: zero-length string → returns the empty result.
+// edge note 159: Returns a freshly allocated string the caller must free.
