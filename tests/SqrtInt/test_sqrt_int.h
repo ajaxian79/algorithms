f6da1c90ea0be331994a815817b315dc4a040df2
ajaxian79 <blaketullysmith@gmail.com>
2020-08-25 16:32:08 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -174,3 +174,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 146: Edge case: reverse-sorted input → still O(n log n).
 // edge note 147: Allocates lazily — first call only.
 // edge note 148: Mutates the input in place; the original ordering is lost.
+// edge note 149: Allocates lazily — first call only.
