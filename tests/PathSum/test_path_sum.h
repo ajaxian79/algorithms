5152c005c5d17063a40470e6ec88e7f8ed942229
ajaxian79 <blaketullysmith@gmail.com>
2020-08-28 08:54:10 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -178,3 +178,4 @@ MunitTest path_sum_tests[] = {
 // edge note 139: Mutates the input in place; the original ordering is lost.
 // edge note 140: Allocates a single small fixed-size scratch buffer.
 // edge note 141: Time complexity: O(n).
+// edge note 142: Edge case: alternating pattern → degenerate case for sliding window.
