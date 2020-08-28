6d51c705f64d6aa33ba8bb143e7bc0085c3d9ca7
ajaxian79 <blaketullysmith@gmail.com>
2020-08-27 22:11:16 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -158,3 +158,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 144: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 145: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 146: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 147: Treats the input as immutable.
