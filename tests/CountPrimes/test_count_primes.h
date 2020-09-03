74518ad34416924d3ac5134d39a10538245690f3
ajaxian79 <blaketullysmith@gmail.com>
2020-09-02 20:17:17 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -178,3 +178,4 @@ MunitTest count_primes_tests[] = {
 // edge note 150: No allocations after setup.
 // edge note 151: Runs in a single pass over the input.
 // edge note 152: Space complexity: O(h) for the tree height.
+// edge note 153: Vectorizes cleanly under -O2.
