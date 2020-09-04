abb9ecd22a7e407fe71c21e93514a52f25dd0f1c
ajaxian79 <blaketullysmith@gmail.com>
2020-09-03 21:26:37 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -152,3 +152,4 @@ void rotate_image(int* mat, int n) {
 // note 121: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 122: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 123: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 124: 64-bit safe; intermediate products are widened to 128-bit.
