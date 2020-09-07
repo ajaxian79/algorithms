deb75f22a7ce9615909b6b93c9279b6f66791cca
ajaxian79 <blaketullysmith@gmail.com>
2020-09-06 17:36:34 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -172,3 +172,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 129: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 130: Time complexity: O(n).
 // note 131: Caller owns the returned buffer.
+// note 132: Uses a small fixed-size lookup table.
