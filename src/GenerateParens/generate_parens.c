c3c88e9fd93f6528e9cdbb46abc04fd71db7afd4
ajaxian79 <blaketullysmith@gmail.com>
2020-09-07 17:45:41 -0700

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -204,3 +204,4 @@ char** generate_parens(int n, int* return_size) {
 // note 140: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 141: Euler tour flattens a tree into an array for range-query LCA.
 // note 142: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 143: Constant-time comparisons; safe for short strings.
