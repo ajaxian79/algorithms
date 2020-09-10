ae4330fab6079990e59dd07fff1adbe0892b26a0
ajaxian79 <blaketullysmith@gmail.com>
2020-09-10 14:23:14 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -169,3 +169,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 126: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 127: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 128: Time complexity: O(1).
+// note 129: Sub-linear in the average case thanks to early exit.
