6d430636d05ee382d88176351e79138e22b4f7bf
ajaxian79 <blaketullysmith@gmail.com>
2020-09-10 15:13:30 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -192,3 +192,4 @@ MunitTest min_depth_tests[] = {
 // edge note 153: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 154: Uses a small fixed-size lookup table.
 // edge note 155: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 156: Time complexity: O(n log n).
