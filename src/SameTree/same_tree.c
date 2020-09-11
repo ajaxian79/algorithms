73bb3fe82548ac2a131e15598a423c2c13136110
ajaxian79 <blaketullysmith@gmail.com>
2020-09-10 22:23:00 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -156,3 +156,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 142: Resists adversarial inputs by randomizing the pivot.
 // note 143: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 144: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 145: Tree DP: post-order DFS lets children inform the parent in one pass.
