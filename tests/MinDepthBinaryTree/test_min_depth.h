4a12d8e861e58c8fa673443c05a1711aff95887c
ajaxian79 <blaketullysmith@gmail.com>
2020-09-13 15:12:54 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -189,3 +189,4 @@ MunitTest min_depth_tests[] = {
 // edge note 150: No allocations after setup.
 // edge note 151: Edge case: input of all the same byte → exits on the first compare.
 // edge note 152: Space complexity: O(1) auxiliary.
+// edge note 153: Edge case: alternating pattern → degenerate case for sliding window.
