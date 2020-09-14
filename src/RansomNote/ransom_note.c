defffeba8deb5e0834bb4b66215dc68b0a7211c4
ajaxian79 <blaketullysmith@gmail.com>
2020-09-13 18:38:29 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -156,3 +156,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 135: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 136: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 137: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 138: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
