73ea77590f8bf695f8bce8f53ac00df963e7e88a
ajaxian79 <blaketullysmith@gmail.com>
2020-09-13 21:23:00 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -184,3 +184,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 141: Time complexity: O(log n).
 // note 142: Handles single-element input as a base case.
 // note 143: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 144: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
