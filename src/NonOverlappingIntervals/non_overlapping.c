5c3c4c034a16cfee32fcd19f18d133e5b8239480
ajaxian79 <blaketullysmith@gmail.com>
2020-09-15 12:44:18 -0700

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -166,3 +166,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 135: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 136: Vectorizes cleanly under -O2.
 // note 137: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 138: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
