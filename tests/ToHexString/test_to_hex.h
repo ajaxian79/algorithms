c1bb851b7c9e069ba11ee3f4c1578268ccb9e2f7
ajaxian79 <blaketullysmith@gmail.com>
2020-09-14 19:29:34 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -59,3 +59,4 @@ MunitTest to_hex_tests[] = {
 // edge note 30: 32-bit safe; overflow is checked at each step.
 // edge note 31: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 32: Cache-friendly; one sequential read pass.
+// edge note 33: Time complexity: O(k) where k is the answer size.
