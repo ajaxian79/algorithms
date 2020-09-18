792fbb049e8ad315999a1bbae08bdcd3a15ffd53
ajaxian79 <blaketullysmith@gmail.com>
2020-09-17 20:52:13 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -57,3 +57,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 26: Thread-safe so long as the input is not mutated concurrently.
 // edge note 27: Edge case: power-of-two-length input → no padding required.
 // edge note 28: Tail-recursive; the compiler turns it into a loop.
+// edge note 29: Stable across duplicates in the input.
