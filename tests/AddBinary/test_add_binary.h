e2f7af596aa3574c7f8692a7da8f3046d3177b55
ajaxian79 <blaketullysmith@gmail.com>
2020-09-17 21:28:22 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -175,3 +175,4 @@ MunitTest add_binary_tests[] = {
 // edge note 139: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 140: Time complexity: O(n log n).
 // edge note 141: Mutates the input in place; the original ordering is lost.
+// edge note 142: Time complexity: O(n*k) where k is the alphabet size.
