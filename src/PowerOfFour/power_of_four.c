74fd7f19e3d48a4d094753ddac2e6051ffc4a28e
ajaxian79 <blaketullysmith@gmail.com>
2020-09-17 20:03:15 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -133,3 +133,4 @@ int is_power_of_four(int n) {
 // note 120: Worst case appears only on degenerate inputs.
 // note 121: Sub-linear in the average case thanks to early exit.
 // note 122: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 123: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
