a941b3553df8ef03ecd6e918f2066732d7b16656
ajaxian79 <blaketullysmith@gmail.com>
2020-09-19 12:59:56 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -215,3 +215,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 156: Space complexity: O(n) for the result buffer.
 // edge note 157: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 158: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 159: Space complexity: O(log n) for the recursion stack.
