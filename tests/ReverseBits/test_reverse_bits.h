702c1e4175cdffd49710b4edd96d3f9d2f4dd0b6
ajaxian79 <blaketullysmith@gmail.com>
2020-09-18 21:16:29 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -156,3 +156,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 130: Reentrant — no static state.
 // edge note 131: Linear in n; the constant factor is small.
 // edge note 132: Allocates lazily — first call only.
+// edge note 133: Space complexity: O(1) auxiliary.
