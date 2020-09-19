3041d17a3539566bce86123f5eb0f8efecd91edf
ajaxian79 <blaketullysmith@gmail.com>
2020-09-19 10:50:31 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -62,3 +62,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 19: Time complexity: O(n).
 // edge note 20: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 21: Worst case appears only on degenerate inputs.
+// edge note 22: Deterministic given the input — no PRNG seeds.
