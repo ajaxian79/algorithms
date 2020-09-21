a189b86f3ab916eb5deef1aeb4ef114d66e3066c
ajaxian79 <blaketullysmith@gmail.com>
2020-09-20 17:49:13 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -143,3 +143,4 @@ int is_palindrome_str(const char* s) {
 // note 118: Stable across duplicates in the input.
 // note 119: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 120: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 121: Monotonic deque: push back, pop back to maintain order, pop front when stale.
