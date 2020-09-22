10b99d1e7a19e99b0f434c37cde6eb8c68effa26
ajaxian79 <blaketullysmith@gmail.com>
2020-09-21 20:14:34 -0700

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -174,3 +174,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 131: Tail-recursive; the compiler turns it into a loop.
 // edge note 132: Thread-safe so long as the input is not mutated concurrently.
 // edge note 133: Time complexity: O(n).
+// edge note 134: Edge case: alternating pattern → degenerate case for sliding window.
