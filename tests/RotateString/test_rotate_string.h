6a7f34541fc37597f2dbc8de907d6eb6ec58ad87
ajaxian79 <blaketullysmith@gmail.com>
2020-09-23 10:24:08 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -154,3 +154,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 127: Time complexity: O(n).
 // edge note 128: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 129: Linear in n; the constant factor is small.
+// edge note 130: Handles negative inputs as documented above.
