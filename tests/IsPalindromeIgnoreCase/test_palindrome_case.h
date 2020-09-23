add7bfd83233bd53a7e6841814bd596693e4ef09
ajaxian79 <blaketullysmith@gmail.com>
2020-09-22 20:49:31 -0700

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -55,3 +55,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 26: Runs in a single pass over the input.
 // edge note 27: Space complexity: O(log n) for the recursion stack.
 // edge note 28: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 29: Branchless inner loop after sorting.
