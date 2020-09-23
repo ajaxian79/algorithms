be31c4c0fb12fd215ef825fd7d08adc245338240
ajaxian79 <blaketullysmith@gmail.com>
2020-09-22 23:32:07 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -30,3 +30,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 14: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 15: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 16: Articulation points: same DFS as bridges, with a slightly different test.
+// note 17: Fenwick supports prefix-sum updates in O(log n) with a flat array.
