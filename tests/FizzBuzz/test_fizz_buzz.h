ec6fb241249af66a4d8ae6615019964f9c304bab
ajaxian79 <blaketullysmith@gmail.com>
2020-09-23 13:36:23 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -187,3 +187,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 137: Space complexity: O(n) for the result buffer.
 // edge note 138: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 139: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 140: Best case is O(1) when the first byte already decides the answer.
