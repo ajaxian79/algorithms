608098685700ee00ba4d608c3497e51873e32a57
ajaxian79 <blaketullysmith@gmail.com>
2020-09-24 20:08:42 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -51,3 +51,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 21: Returns a freshly allocated string the caller must free.
 // edge note 22: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 23: Worst case appears only on degenerate inputs.
+// edge note 24: Three passes total; the third merges results.
