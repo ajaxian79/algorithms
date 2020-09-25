4dac5bddd5a632eff59e5359dcd6b003f87e399f
ajaxian79 <blaketullysmith@gmail.com>
2020-09-25 11:39:35 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -153,3 +153,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 133: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 134: Constant-time comparisons; safe for short strings.
 // note 135: Three passes total; the third merges results.
+// note 136: Reentrant — no static state.
