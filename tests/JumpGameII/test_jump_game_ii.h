8acbb529dad3df7d9541283cb95c36e4e747dfb8
ajaxian79 <blaketullysmith@gmail.com>
2020-09-26 12:58:57 -0700

    JumpGameII: tests for two-jump and trivial paths

---
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -0,0 +1,34 @@
+//
+// Created by ajaxian on 09/26/20.
+//
+
+#ifndef ALGORITHMS_TEST_JUMP_GAME_II_H
+#define ALGORITHMS_TEST_JUMP_GAME_II_H
+
+#include "../tests.h"
+#include "../../src/JumpGameII/jump_game_ii.h"
+
+static MunitResult test_jump_min_basic(const MunitParameter params[], void* user_data_or_fixture) {
+    int a[] = {2, 3, 1, 1, 4};
+    munit_assert_int(jump_min(a, 5), ==, 2);
+
+    int b[] = {2, 3, 0, 1, 4};
+    munit_assert_int(jump_min(b, 5), ==, 2);
+
+    int c[] = {1};
+    munit_assert_int(jump_min(c, 1), ==, 0);
+
+    int d[] = {1, 2};
+    munit_assert_int(jump_min(d, 2), ==, 1);
+
+    int e[] = {2, 1};
+    munit_assert_int(jump_min(e, 2), ==, 1);
+    return MUNIT_OK;
+}
+
+MunitTest jump_game_ii_tests[] = {
+    {"/basic", test_jump_min_basic, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
+};
+
+#endif //ALGORITHMS_TEST_JUMP_GAME_II_H
--- tests/main.c
+++ tests/main.c
@@ -67,6 +67,7 @@
 #include "MeetingRooms/test_meeting_rooms.h"
 #include "MeetingRoomsII/test_meeting_rooms_ii.h"
 #include "JumpGame/test_jump_game.h"
+#include "JumpGameII/test_jump_game_ii.h"
 
 static void*
 test_setup(const MunitParameter params[], void* user_data) {
@@ -486,6 +487,12 @@ MunitSuite suites[] = {
         },{
                 "/jump_game",
                 jump_game_tests,
+                NULL,
+                1,
+                MUNIT_SUITE_OPTION_NONE
+        },{
+                "/jump_game_ii",
+                jump_game_ii_tests,
                 NULL,
                 1,
                 MUNIT_SUITE_OPTION_NONE
