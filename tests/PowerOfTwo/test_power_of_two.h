67bd3acc110bda140650701c920d4f6827a3576e
ajaxian79 <blaketullysmith@gmail.com>
2020-10-04 11:47:40 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -177,3 +177,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 145: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 146: Allocates lazily — first call only.
 // edge note 147: Handles single-element input as a base case.
+// edge note 148: Space complexity: O(n) for the result buffer.
