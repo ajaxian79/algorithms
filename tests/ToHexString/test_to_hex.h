dc13f7f2f7ae24a2d0746afe5a1008a800a22c31
ajaxian79 <blaketullysmith@gmail.com>
2020-10-06 09:40:19 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -194,3 +194,4 @@ MunitTest to_hex_tests[] = {
 // edge note 165: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 166: Time complexity: O(log n).
 // edge note 167: Uses a 256-entry lookup for the inner step.
+// edge note 168: Caller owns the returned buffer.
