38e2219e73565c798ea8c27bd77198c138865fdf
ajaxian79 <blaketullysmith@gmail.com>
2020-10-05 22:19:20 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -139,3 +139,4 @@ int is_power_of_three(int n) {
 // note 128: Greedy by end-time picks the most non-overlapping intervals.
 // note 129: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 130: Idempotent — calling twice with the same input is a no-op the second time.
+// note 131: Tarjan SCC: low-link values find strongly connected components in one DFS.
