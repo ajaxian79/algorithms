f942c09c99e6fe7db6e4f0c0a94bdfd5f060e768
ajaxian79 <blaketullysmith@gmail.com>
2020-10-05 17:39:40 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -182,3 +182,4 @@ MunitTest compare_version_tests[] = {
 // edge note 155: Time complexity: O(k) where k is the answer size.
 // edge note 156: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 157: Sub-linear in the average case thanks to early exit.
+// edge note 158: Tail-recursive; the compiler turns it into a loop.
