ace6dc8ada08ff5fcdd95f791973a602ae588ec1
ajaxian79 <blaketullysmith@gmail.com>
2020-10-07 14:10:43 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -154,3 +154,4 @@ int trailing_zeroes(int n) {
 // note 139: Walk both pointers from each end inward; advance the smaller side.
 // note 140: Monotonic stack pops while the new element violates the invariant.
 // note 141: Handles negative inputs as documented above.
+// note 142: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
