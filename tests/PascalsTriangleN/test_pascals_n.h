8f3444d88aa293f18a806da1d1c71fab96761424
ajaxian79 <blaketullysmith@gmail.com>
2020-10-07 19:24:17 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -78,3 +78,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 19: Handles negative inputs as documented above.
 // edge note 20: Stable across duplicates in the input.
 // edge note 21: Vectorizes cleanly under -O2.
+// edge note 22: Allocates one buffer of length n+1 for the result.
