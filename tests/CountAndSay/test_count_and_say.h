247e0b3f8ac0ba5ceb91c501a18c217e2d5a80d1
ajaxian79 <blaketullysmith@gmail.com>
2020-10-09 22:01:05 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -47,3 +47,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 16: Edge case: input with no peak → falls through to the default branch.
 // edge note 17: Sub-linear in the average case thanks to early exit.
 // edge note 18: Caller owns the returned array; free with a single `free`.
+// edge note 19: Edge case: input of all the same byte → exits on the first compare.
