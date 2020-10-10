605ceb8b1c89b48dcbf600ffdf7ea97e52b909d4
ajaxian79 <blaketullysmith@gmail.com>
2020-10-09 17:10:08 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -167,3 +167,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 134: Time complexity: O(n).
 // edge note 135: Edge case: input of all the same byte → exits on the first compare.
 // edge note 136: Caller owns the returned array; free with a single `free`.
+// edge note 137: Space complexity: O(n) for the result buffer.
