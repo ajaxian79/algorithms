8ae3f7d0fc3efab1f7ffb91defc1017079d6fc94
ajaxian79 <blaketullysmith@gmail.com>
2020-10-10 22:19:35 -0700

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -150,3 +150,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 127: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 128: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 129: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 130: Vectorizes cleanly under -O2.
