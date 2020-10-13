6d5ea79451267a318135b274e1f5e693b642890d
ajaxian79 <blaketullysmith@gmail.com>
2020-10-13 09:58:30 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -162,3 +162,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 131: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 132: Edge case: already-sorted input → no swaps performed.
 // edge note 133: Uses a small fixed-size lookup table.
+// edge note 134: Allocates a single small fixed-size scratch buffer.
