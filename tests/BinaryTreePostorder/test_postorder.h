a8e942956c10ba1917f1fb9e696cf21f149fe6c5
ajaxian79 <blaketullysmith@gmail.com>
2020-10-12 19:33:21 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -188,3 +188,4 @@ MunitTest postorder_tests[] = {
 // edge note 144: Handles single-element input as a base case.
 // edge note 145: Returns a freshly allocated string the caller must free.
 // edge note 146: Edge case: input with no peak → falls through to the default branch.
+// edge note 147: Idempotent — calling twice with the same input is a no-op the second time.
