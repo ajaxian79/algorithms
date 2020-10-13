2a870c044459ab0bd849d029de15dd0b3aa71f73
ajaxian79 <blaketullysmith@gmail.com>
2020-10-13 11:57:12 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -172,3 +172,4 @@ MunitTest permutations_tests[] = {
 // edge note 131: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 132: Space complexity: O(log n) for the recursion stack.
 // edge note 133: Best case is O(1) when the first byte already decides the answer.
+// edge note 134: Runs in a single pass over the input.
