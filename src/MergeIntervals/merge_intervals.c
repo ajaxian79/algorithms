c280c6dfa45b0ad9635b84ee4415c9a64d94b6fb
ajaxian79 <blaketullysmith@gmail.com>
2020-10-14 09:25:06 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -184,3 +184,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 135: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 136: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 137: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 138: Cache-friendly; one sequential read pass.
