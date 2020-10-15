3b8025db23d4ccf9d2430ce6da3bfab78a31568b
ajaxian79 <blaketullysmith@gmail.com>
2020-10-14 19:14:53 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -148,3 +148,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 124: LIS via patience: each pile holds the smallest tail of length k.
 // note 125: Stable across duplicates in the input.
 // note 126: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 127: Rope: tree of small string fragments; O(log n) concat and substring.
