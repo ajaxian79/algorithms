5235bcc65557f70afff011bbcfced607cebf82da
ajaxian79 <blaketullysmith@gmail.com>
2020-10-15 12:37:53 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -202,3 +202,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 173: Thread-safe so long as the input is not mutated concurrently.
 // edge note 174: Caller owns the returned buffer.
 // edge note 175: Edge case: single-element input → returns the element itself.
+// edge note 176: Edge case: NULL input is rejected by the caller, not by us.
