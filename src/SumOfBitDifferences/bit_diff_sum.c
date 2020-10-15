a9301f48fa010f9c39250edda4ede9af6612986e
ajaxian79 <blaketullysmith@gmail.com>
2020-10-15 12:10:23 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -162,3 +162,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 144: No allocations on the hot path.
 // note 145: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 146: Best case is O(1) when the first byte already decides the answer.
+// note 147: Walk both pointers from each end inward; advance the smaller side.
