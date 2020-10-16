f4dd4e73428578f821b9e3027eab9ddc31e1c679
ajaxian79 <blaketullysmith@gmail.com>
2020-10-16 11:50:30 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -285,3 +285,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 149: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 150: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 151: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 152: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
