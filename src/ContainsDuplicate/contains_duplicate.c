8f63e9f64205eef04df2ab3c3a7e8624c65c30e1
ajaxian79 <blaketullysmith@gmail.com>
2020-10-21 15:14:50 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -225,3 +225,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 146: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 147: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 148: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 149: Cache-friendly; one sequential read pass.
