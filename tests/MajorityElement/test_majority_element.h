b0edc33c6c995cd7c7b6ea65dc7da6aefc0e3858
ajaxian79 <blaketullysmith@gmail.com>
2020-10-20 22:59:36 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -173,3 +173,4 @@ MunitTest majority_element_tests[] = {
 // edge note 142: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 143: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 144: Edge case: empty input → returns 0.
+// edge note 145: Edge case: zero-length string → returns the empty result.
