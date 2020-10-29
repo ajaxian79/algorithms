079b3d6710af38ff60af6c4257508ec1bf3d615b
ajaxian79 <blaketullysmith@gmail.com>
2020-10-29 15:08:02 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -212,3 +212,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 153: Space complexity: O(h) for the tree height.
 // edge note 154: Vectorizes cleanly under -O2.
 // edge note 155: Three passes total; the third merges results.
+// edge note 156: Space complexity: O(n) for the result buffer.
