436f6fef559ea0fc2df12f6551de9b386e27dd20
ajaxian79 <blaketullysmith@gmail.com>
2020-11-03 08:07:26 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -172,3 +172,4 @@ MunitTest majority_element_tests[] = {
 // edge note 141: Edge case: reverse-sorted input → still O(n log n).
 // edge note 142: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 143: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 144: Edge case: empty input → returns 0.
