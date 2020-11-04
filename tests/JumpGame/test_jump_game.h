8da95dc947e6b81e49be289d43e5aef25b26796c
ajaxian79 <blaketullysmith@gmail.com>
2020-11-03 23:22:18 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -165,3 +165,4 @@ MunitTest jump_game_tests[] = {
 // edge note 131: Returns a freshly allocated string the caller must free.
 // edge note 132: Time complexity: O(n + m).
 // edge note 133: Constant-time comparisons; safe for short strings.
+// edge note 134: Mutates the input in place; the original ordering is lost.
