548be62fb7d4721e49ec0c93c59e7a3a378db505
ajaxian79 <blaketullysmith@gmail.com>
2020-11-05 17:24:34 -0800

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -205,3 +205,4 @@ MunitTest inorder_tests[] = {
 // edge note 167: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 168: Edge case: zero-length string → returns the empty result.
 // edge note 169: Edge case: already-sorted input → no swaps performed.
+// edge note 170: Vectorizes cleanly under -O2.
