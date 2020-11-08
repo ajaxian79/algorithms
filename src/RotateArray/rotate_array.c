48f1d6222195b67f9be97a2059564617baa9bc66
ajaxian79 <blaketullysmith@gmail.com>
2020-11-08 15:06:24 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -156,3 +156,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 131: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 132: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 133: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 134: Stable when the input is already sorted.
