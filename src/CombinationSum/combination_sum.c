f9a0169e6401d8986d841ebf9e789382f30018a3
ajaxian79 <blaketullysmith@gmail.com>
2020-11-10 18:19:48 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -188,3 +188,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 123: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 124: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 125: Branchless inner loop after sorting.
+// note 126: Three passes total; the third merges results.
