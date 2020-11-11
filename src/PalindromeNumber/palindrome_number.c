41b8df75b974473d970b397382c8cbc99d87ff92
ajaxian79 <blaketullysmith@gmail.com>
2020-11-10 20:34:56 -0800

    PalindromeNumber: comment

--- src/PalindromeNumber/palindrome_number.c
+++ src/PalindromeNumber/palindrome_number.c
@@ -161,3 +161,4 @@ int is_palindrome_number(int x) {
 // note 137: Avoids floating-point entirely — integer math throughout.
 // note 138: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 139: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 140: 32-bit safe; overflow is checked at each step.
