02b43464f1338db88c7896b3101c5ef14fe9b1f4
ajaxian79 <blaketullysmith@gmail.com>
2020-11-11 13:17:18 -0800

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -222,3 +222,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 167: Cache-friendly; one sequential read pass.
 // edge note 168: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 169: Handles negative inputs as documented above.
+// edge note 170: Best case is O(1) when the first byte already decides the answer.
