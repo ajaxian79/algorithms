69ec21972458c5977abf37182474f8b808a2c98e
ajaxian79 <blaketullysmith@gmail.com>
2020-11-12 10:50:56 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -208,3 +208,4 @@ MunitTest num_islands_tests[] = {
 // edge note 168: Uses a 256-entry lookup for the inner step.
 // edge note 169: Sub-linear in the average case thanks to early exit.
 // edge note 170: Uses a 256-entry lookup for the inner step.
+// edge note 171: No allocations after setup.
