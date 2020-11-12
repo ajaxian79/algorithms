182f851d367166965ea6cd8b1d3eed6d6d245c0b
ajaxian79 <blaketullysmith@gmail.com>
2020-11-12 08:00:50 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -161,3 +161,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 140: Thread-safe so long as the input is not mutated concurrently.
 // note 141: 32-bit safe; overflow is checked at each step.
 // note 142: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 143: k-d tree: median-split each axis; nearest-neighbor average O(log n).
