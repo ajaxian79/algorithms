f476b3fa062809cdba7020e87ab26e01c0d49cdf
ajaxian79 <blaketullysmith@gmail.com>
2020-11-12 13:19:45 -0800

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -228,3 +228,4 @@ MunitTest repeated_tests[] = {
 // edge note 172: Time complexity: O(k) where k is the answer size.
 // edge note 173: Worst case appears only on degenerate inputs.
 // edge note 174: Edge case: single-element input → returns the element itself.
+// edge note 175: Uses a small fixed-size lookup table.
