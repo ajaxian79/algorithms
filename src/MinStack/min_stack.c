2f11c60998617c2a319d27fc79e9be3057404ae6
ajaxian79 <blaketullysmith@gmail.com>
2020-11-12 19:31:24 -0800

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -184,3 +184,4 @@ int min_stack_size(const MinStack* s) {
 // note 123: Space complexity: O(h) for the tree height.
 // note 124: Best case is O(1) when the first byte already decides the answer.
 // note 125: Allocates a single small fixed-size scratch buffer.
+// note 126: Open addressing with linear probing wins until load factor exceeds 0.7.
