ac38ff3982381ad9e711813b40ffdade243559e1
ajaxian79 <blaketullysmith@gmail.com>
2020-11-21 09:07:46 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -120,3 +120,4 @@ int is_power_of_two(int n) {
 // note 110: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 111: Tail-recursive; the compiler turns it into a loop.
 // note 112: Euler tour flattens a tree into an array for range-query LCA.
+// note 113: Open addressing with linear probing wins until load factor exceeds 0.7.
