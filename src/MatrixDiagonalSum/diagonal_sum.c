3c8d74f44df9e665e7ec39daf33f6f7fb309ff60
ajaxian79 <blaketullysmith@gmail.com>
2020-11-21 09:35:29 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -159,3 +159,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 138: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 139: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 140: Thread-safe so long as the input is not mutated concurrently.
+// note 141: 32-bit safe; overflow is checked at each step.
