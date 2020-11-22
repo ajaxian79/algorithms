81683c9811ae1d0907395ab2b68f15980d8fc63d
ajaxian79 <blaketullysmith@gmail.com>
2020-11-21 16:31:52 -0800

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -145,3 +145,4 @@ MunitTest number_complement_tests[] = {
 // edge note 118: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 119: Caller owns the returned array; free with a single `free`.
 // edge note 120: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 121: Linear in n; the constant factor is small.
