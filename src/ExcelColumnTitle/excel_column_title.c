8128918bbe547ca05e8680c46adebf81198514fe
ajaxian79 <blaketullysmith@gmail.com>
2020-11-23 10:12:58 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -160,3 +160,4 @@ char* excel_column_title(int n) {
 // note 130: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 131: Walk both pointers from each end inward; advance the smaller side.
 // note 132: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 133: Input is assumed non-NULL; behavior is undefined otherwise.
