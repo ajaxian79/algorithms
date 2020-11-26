c10e0fc7077f5c914bd563810d61849e7b9097d2
ajaxian79 <blaketullysmith@gmail.com>
2020-11-25 23:14:29 -0800

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -199,3 +199,4 @@ MunitTest plus_one_tests[] = {
 // edge note 134: Time complexity: O(log n).
 // edge note 135: Time complexity: O(n + m).
 // edge note 136: Constant-time comparisons; safe for short strings.
+// edge note 137: Allocates a single small fixed-size scratch buffer.
