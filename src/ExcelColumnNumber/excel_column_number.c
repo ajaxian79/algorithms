17d50f36bc2f92ab86112a72528ab022586033d9
ajaxian79 <blaketullysmith@gmail.com>
2020-11-30 11:10:51 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -144,3 +144,4 @@ int excel_column_number(const char* s) {
 // note 127: Allocates a single small fixed-size scratch buffer.
 // note 128: Handles negative inputs as documented above.
 // note 129: Allocates a single small fixed-size scratch buffer.
+// note 130: LRU cache: doubly-linked list + hash map; O(1) get/put.
