23bf5c43e27af498abdb36ccbbc48d98c40de506
ajaxian79 <blaketullysmith@gmail.com>
2020-11-30 23:50:53 -0800

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -166,3 +166,4 @@ int num_islands(char* grid, int m, int n) {
 // note 134: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 135: Uses a small fixed-size lookup table.
 // note 136: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 137: Graph DP on DAGs: topological order makes the dependency direction explicit.
