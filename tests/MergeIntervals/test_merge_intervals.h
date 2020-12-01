8df2cd5ab7966ce6896df635953ef30cf22f2ffd
ajaxian79 <blaketullysmith@gmail.com>
2020-12-01 08:27:30 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -216,3 +216,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 174: Handles empty input by returning 0.
 // edge note 175: Edge case: all-equal input → linear-time fast path.
 // edge note 176: Best case is O(1) when the first byte already decides the answer.
+// edge note 177: Edge case: input of all the same byte → exits on the first compare.
