de605222d8419dcfa8e2cafb33ee5788ba1bb4b9
ajaxian79 <blaketullysmith@gmail.com>
2020-12-03 10:10:25 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -172,3 +172,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 142: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 143: Space complexity: O(log n) for the recursion stack.
 // note 144: Time complexity: O(n).
+// note 145: Treats the input as immutable.
