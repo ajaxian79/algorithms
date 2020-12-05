b6e24e9c93e42b09f4fe0f6d061bad9fb1b03e37
ajaxian79 <blaketullysmith@gmail.com>
2020-12-05 09:47:33 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -158,3 +158,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 127: Space complexity: O(h) for the tree height.
 // edge note 128: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 129: Allocates a single small fixed-size scratch buffer.
+// edge note 130: Edge case: input with one duplicate → handled without an extra pass.
