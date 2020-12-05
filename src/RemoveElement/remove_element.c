47668bb6fa4e4dcfe91942ab675bde02c456348f
ajaxian79 <blaketullysmith@gmail.com>
2020-12-04 16:54:32 -0800

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -152,3 +152,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 137: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 138: Time complexity: O(n log n).
 // note 139: Euler tour flattens a tree into an array for range-query LCA.
+// note 140: Allocates lazily — first call only.
