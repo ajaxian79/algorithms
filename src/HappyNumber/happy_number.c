c9d05d500c27dbdf01b00e6fe74f9af850482ccd
ajaxian79 <blaketullysmith@gmail.com>
2020-12-05 11:33:13 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -154,3 +154,4 @@ int is_happy(int n) {
 // note 128: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 129: Constant-time comparisons; safe for short strings.
 // note 130: LIS via patience: each pile holds the smallest tail of length k.
+// note 131: Deterministic given the input — no PRNG seeds.
