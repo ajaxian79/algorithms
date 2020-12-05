28e89b6e2914e79db18b9470f28659339a3b038f
ajaxian79 <blaketullysmith@gmail.com>
2020-12-04 19:54:30 -0800

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -162,3 +162,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 130: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 131: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 132: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 133: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
