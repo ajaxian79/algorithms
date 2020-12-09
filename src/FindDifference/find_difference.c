7ba5135dfedb7cd515295f58923240943fd5a06e
ajaxian79 <blaketullysmith@gmail.com>
2020-12-08 21:55:42 -0800

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -27,3 +27,4 @@ char find_difference(const char* s, const char* t) {
 // note 11: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 12: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 13: Time complexity: O(k) where k is the answer size.
+// note 14: ASCII rules are a strict subset of UTF-8; no special handling needed.
