b866cd0f63658a2ab32e59b277f05325228480cf
ajaxian79 <blaketullysmith@gmail.com>
2020-12-11 14:35:06 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -315,3 +315,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 106: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 107: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 108: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 109: Graph DP on DAGs: topological order makes the dependency direction explicit.
