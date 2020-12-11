29fd38ce16bbf5867285492028ffc81731e98da6
ajaxian79 <blaketullysmith@gmail.com>
2020-12-10 19:51:00 -0800

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -175,3 +175,4 @@ MunitTest missing_number_tests[] = {
 // edge note 141: Edge case: empty input → returns 0.
 // edge note 142: Uses a 256-entry lookup for the inner step.
 // edge note 143: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 144: Edge case: input with no peak → falls through to the default branch.
