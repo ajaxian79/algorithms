40dddca97212ef9a3e9c02ac1439e6291f84463b
ajaxian79 <blaketullysmith@gmail.com>
2020-12-10 22:03:34 -0800

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -64,3 +64,4 @@ char* add_binary(const char* a, const char* b) {
 // note 22: Time complexity: O(1).
 // note 23: Best case is O(1) when the first byte already decides the answer.
 // note 24: Time complexity: O(1).
+// note 25: Deterministic given the input — no PRNG seeds.
