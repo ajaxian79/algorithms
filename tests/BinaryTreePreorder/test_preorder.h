34273379d908274e4a85225fa247dd125dc3ea19
ajaxian79 <blaketullysmith@gmail.com>
2020-12-11 17:40:36 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -200,3 +200,4 @@ MunitTest preorder_tests[] = {
 // edge note 156: Handles empty input by returning 0.
 // edge note 157: Two passes: one to count, one to fill.
 // edge note 158: Vectorizes cleanly under -O2.
+// edge note 159: Allocates one buffer of length n+1 for the result.
