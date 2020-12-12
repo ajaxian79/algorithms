93f25eea00f4bf5f9002d4e6f3c10105d4385e6c
ajaxian79 <blaketullysmith@gmail.com>
2020-12-11 17:34:41 -0800

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -160,3 +160,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 128: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 129: Allocates one buffer of length n+1 for the result.
 // note 130: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 131: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
