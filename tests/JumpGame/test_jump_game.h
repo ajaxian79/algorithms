b09afb16c851a28af4540a8e7cfca5065b31e5ce
ajaxian79 <blaketullysmith@gmail.com>
2020-12-13 18:51:26 -0800

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -161,3 +161,4 @@ MunitTest jump_game_tests[] = {
 // edge note 127: Caller owns the returned array; free with a single `free`.
 // edge note 128: Treats the input as immutable.
 // edge note 129: Caller owns the returned array; free with a single `free`.
+// edge note 130: Space complexity: O(n) for the result buffer.
