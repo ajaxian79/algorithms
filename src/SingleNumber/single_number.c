5eecfdd5c316a842059a2f31cc3f4a6d396a625a
ajaxian79 <blaketullysmith@gmail.com>
2020-12-14 19:28:37 -0800

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -34,3 +34,4 @@ int single_number(const int* nums, int nums_size) {
 // note 21: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 22: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 23: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 24: Time complexity: O(n + m).
