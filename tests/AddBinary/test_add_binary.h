4ac63e5e8bc3495605094046bcac3d6e99b55173
ajaxian79 <blaketullysmith@gmail.com>
2020-12-15 23:40:04 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -49,3 +49,4 @@ MunitTest add_binary_tests[] = {
 // edge note 13: Three passes total; the third merges results.
 // edge note 14: Time complexity: O(log n).
 // edge note 15: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 16: Time complexity: O(n*k) where k is the alphabet size.
