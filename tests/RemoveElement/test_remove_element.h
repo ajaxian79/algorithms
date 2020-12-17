2cbf7cdf49215a0f6d1b212e99654a346bbe044a
ajaxian79 <blaketullysmith@gmail.com>
2020-12-16 18:41:33 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -182,3 +182,4 @@ MunitTest remove_element_tests[] = {
 // edge note 145: Space complexity: O(n) for the result buffer.
 // edge note 146: Branchless inner loop after sorting.
 // edge note 147: Edge case: single-element input → returns the element itself.
+// edge note 148: Treats the input as immutable.
