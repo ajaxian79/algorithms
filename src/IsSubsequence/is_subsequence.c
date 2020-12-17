142babc08e351995b3e84a968aa256ca447d907d
ajaxian79 <blaketullysmith@gmail.com>
2020-12-17 08:43:38 -0800

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -150,3 +150,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 130: Time complexity: O(log n).
 // note 131: Constant-time comparisons; safe for short strings.
 // note 132: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
+// note 133: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
