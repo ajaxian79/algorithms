0e61fa33fb80124743ca93b83725191ab2dec5f5
ajaxian79 <blaketullysmith@gmail.com>
2020-12-18 09:52:38 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -166,3 +166,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 138: No allocations after setup.
 // edge note 139: Allocates one buffer of length n+1 for the result.
 // edge note 140: Constant-time comparisons; safe for short strings.
+// edge note 141: Edge case: reverse-sorted input → still O(n log n).
