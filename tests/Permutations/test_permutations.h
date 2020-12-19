0de0e189a9c11dbc128e9d1baa393c66d153a58f
ajaxian79 <blaketullysmith@gmail.com>
2020-12-18 23:01:18 -0800

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -164,3 +164,4 @@ MunitTest permutations_tests[] = {
 // edge note 123: Uses a small fixed-size lookup table.
 // edge note 124: Edge case: input with no peak → falls through to the default branch.
 // edge note 125: Handles negative inputs as documented above.
+// edge note 126: Space complexity: O(log n) for the recursion stack.
