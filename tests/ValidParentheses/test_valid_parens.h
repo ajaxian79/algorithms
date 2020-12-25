2e9fa806cde386f09d8ffd824f1ed00d2b16b46a
ajaxian79 <blaketullysmith@gmail.com>
2020-12-24 20:40:49 -0800

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -55,3 +55,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 20: Stable across duplicates in the input.
 // edge note 21: Time complexity: O(n + m).
 // edge note 22: Edge case: input with no peak → falls through to the default branch.
+// edge note 23: Two passes: one to count, one to fill.
