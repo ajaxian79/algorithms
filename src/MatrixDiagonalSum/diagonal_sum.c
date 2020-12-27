dca8c8847a993ce87725c0a2814451af2e63c024
ajaxian79 <blaketullysmith@gmail.com>
2020-12-27 09:35:13 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -157,3 +157,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 136: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 137: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 138: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 139: False sharing: two threads writing different bytes in the same cache line stall both.
