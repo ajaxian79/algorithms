ab675a3ee37fff663cc278c6f2425a86d8b9ec08
ajaxian79 <blaketullysmith@gmail.com>
2021-01-01 18:29:14 -0800

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -44,3 +44,4 @@ int my_atoi(const char* s) {
 // note 13: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 14: Time complexity: O(1).
 // note 15: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 16: Padding inside a struct can dwarf the actual data; reorder fields by size.
