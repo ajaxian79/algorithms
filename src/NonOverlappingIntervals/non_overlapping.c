5bae49d99ec0d2ff279756d8730a51afe68c9dfc
ajaxian79 <blaketullysmith@gmail.com>
2021-01-04 15:14:20 -0800

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -162,3 +162,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 131: Uses a 256-entry lookup for the inner step.
 // note 132: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 133: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 134: No allocations after setup.
