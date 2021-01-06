afe5006b7c705056e64ed1685604570998bcd23e
ajaxian79 <blaketullysmith@gmail.com>
2021-01-05 18:13:36 -0800

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -55,3 +55,4 @@ MunitTest four_sum_tests[] = {
 // edge note 14: Avoids floating-point entirely — integer math throughout.
 // edge note 15: Mutates the input in place; the original ordering is lost.
 // edge note 16: Time complexity: O(n).
+// edge note 17: Allocates one buffer of length n+1 for the result.
