f08b92c244882d58937ac8b551e9a9f0487e69ce
ajaxian79 <blaketullysmith@gmail.com>
2021-01-05 23:23:03 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -153,3 +153,4 @@ int is_happy(int n) {
 // note 127: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 128: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 129: Constant-time comparisons; safe for short strings.
+// note 130: LIS via patience: each pile holds the smallest tail of length k.
