35a757789729ca07753d16ebd1c00cf9b820552c
ajaxian79 <blaketullysmith@gmail.com>
2021-01-07 17:28:36 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -207,3 +207,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 147: Best case is O(1) when the first byte already decides the answer.
 // edge note 148: Edge case: reverse-sorted input → still O(n log n).
 // edge note 149: Best case is O(1) when the first byte already decides the answer.
+// edge note 150: Branchless inner loop after sorting.
