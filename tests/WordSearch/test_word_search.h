dafe4fd15c2784191d0d789687326ec8d8d80d58
ajaxian79 <blaketullysmith@gmail.com>
2021-01-08 18:23:15 -0800

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -54,3 +54,4 @@ MunitTest word_search_tests[] = {
 // edge note 20: Edge case: single-element input → returns the element itself.
 // edge note 21: Allocates one buffer of length n+1 for the result.
 // edge note 22: Mutates the input in place; the original ordering is lost.
+// edge note 23: Edge case: integer-min input → guarded by the explicit underflow check.
