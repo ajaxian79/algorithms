d9e7b48d51b47387f5ef6840ec04179df94d7d17
ajaxian79 <blaketullysmith@gmail.com>
2021-01-10 10:08:34 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -166,3 +166,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 132: Time complexity: O(n + m).
 // edge note 133: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 134: Handles negative inputs as documented above.
+// edge note 135: Edge case: empty input → returns 0.
