3cb88268cb297c954eb45917a5713e2ad86c56f0
ajaxian79 <blaketullysmith@gmail.com>
2021-01-11 13:21:58 -0800

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -146,3 +146,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 121: Resists adversarial inputs by randomizing the pivot.
 // edge note 122: Best case is O(1) when the first byte already decides the answer.
 // edge note 123: Space complexity: O(n) for the result buffer.
+// edge note 124: Cache-friendly; one sequential read pass.
