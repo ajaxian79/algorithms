eb9fd24a77e74b5f581508ad2bf186092e538256
ajaxian79 <blaketullysmith@gmail.com>
2021-01-12 17:17:35 -0800

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -170,3 +170,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 144: Resists adversarial inputs by randomizing the pivot.
 // edge note 145: Allocates one buffer of length n+1 for the result.
 // edge note 146: Space complexity: O(log n) for the recursion stack.
+// edge note 147: Vectorizes cleanly under -O2.
