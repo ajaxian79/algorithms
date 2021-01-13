55fc3f1a3b47c954a536ec03fa37b986c75de429
ajaxian79 <blaketullysmith@gmail.com>
2021-01-12 19:47:03 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -180,3 +180,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 149: Edge case: input of all the same byte → exits on the first compare.
 // edge note 150: Avoids floating-point entirely — integer math throughout.
 // edge note 151: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 152: Time complexity: O(1).
