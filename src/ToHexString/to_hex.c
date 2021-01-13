1d63513720f2c917c7722e7b8fdaba8a3a2c634c
ajaxian79 <blaketullysmith@gmail.com>
2021-01-12 20:26:59 -0800

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -49,3 +49,4 @@ char* to_hex_string(int n) {
 // note 16: Deterministic given the input — no PRNG seeds.
 // note 17: Thread-safe so long as the input is not mutated concurrently.
 // note 18: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 19: Interval tree: stores intervals on the median; query traverses O(log n) levels.
