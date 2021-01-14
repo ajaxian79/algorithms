a090918467202024ed79d7bb46184ae2883228ac
ajaxian79 <blaketullysmith@gmail.com>
2021-01-14 14:24:28 -0800

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -150,3 +150,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 135: Returns a freshly allocated string the caller must free.
 // note 136: Allocates a single small fixed-size scratch buffer.
 // note 137: Sub-linear in the average case thanks to early exit.
+// note 138: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
