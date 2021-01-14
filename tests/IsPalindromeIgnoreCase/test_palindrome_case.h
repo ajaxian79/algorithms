8eaf22bbb1c41bff9f79f754b509e7d8228c1306
ajaxian79 <blaketullysmith@gmail.com>
2021-01-13 17:10:40 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -172,3 +172,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 143: Handles single-element input as a base case.
 // edge note 144: Linear in n; the constant factor is small.
 // edge note 145: Sub-linear in the average case thanks to early exit.
+// edge note 146: Stable across duplicates in the input.
