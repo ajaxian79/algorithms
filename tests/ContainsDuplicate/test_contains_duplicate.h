85e9d7c0984e7668f799538a95bc468f9032786c
ajaxian79 <blaketullysmith@gmail.com>
2021-01-14 08:41:13 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -172,3 +172,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 129: Caller owns the returned array; free with a single `free`.
 // edge note 130: Caller owns the returned array; free with a single `free`.
 // edge note 131: Space complexity: O(1) auxiliary.
+// edge note 132: Edge case: input of all the same byte → exits on the first compare.
