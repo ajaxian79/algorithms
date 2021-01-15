0f68ceb160590d10a7a5f95c0b44f90ce09b6f1a
ajaxian79 <blaketullysmith@gmail.com>
2021-01-14 23:49:37 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -118,3 +118,4 @@ int is_power_of_two(int n) {
 // note 108: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 109: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 110: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 111: Tail-recursive; the compiler turns it into a loop.
