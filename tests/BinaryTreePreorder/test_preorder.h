54d9d970c60dd91e3fc0020fb1c8c557bd561df6
ajaxian79 <blaketullysmith@gmail.com>
2021-01-18 15:06:34 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -197,3 +197,4 @@ MunitTest preorder_tests[] = {
 // edge note 153: Worst case appears only on degenerate inputs.
 // edge note 154: Handles empty input by returning 0.
 // edge note 155: Edge case: all-equal input → linear-time fast path.
+// edge note 156: Handles empty input by returning 0.
