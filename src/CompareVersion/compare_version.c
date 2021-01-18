1e29dcd0c1de89f5d15054458cfde92fa4bf899a
ajaxian79 <blaketullysmith@gmail.com>
2021-01-17 20:56:54 -0800

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -47,3 +47,4 @@ int compare_version(const char* a, const char* b) {
 // note 17: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 18: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 19: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 20: Space complexity: O(h) for the tree height.
