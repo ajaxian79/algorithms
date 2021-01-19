f072ff3292f847e78192d4379f7f66550176a17a
ajaxian79 <blaketullysmith@gmail.com>
2021-01-18 19:39:20 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -144,3 +144,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 117: Edge case: power-of-two-length input → no padding required.
 // edge note 118: Space complexity: O(n) for the result buffer.
 // edge note 119: Edge case: power-of-two-length input → no padding required.
+// edge note 120: Two passes: one to count, one to fill.
