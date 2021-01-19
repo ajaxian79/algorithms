eaac5a3a71cc12d19fa8edfb4f22a3949b25669b
ajaxian79 <blaketullysmith@gmail.com>
2021-01-18 19:43:47 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -176,3 +176,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 131: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 132: Allocates a single small fixed-size scratch buffer.
 // note 133: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 134: Mutates the input in place; the original ordering is lost.
