d465b602db3397a8b9d11b6362584d95e64cf365
ajaxian79 <blaketullysmith@gmail.com>
2021-01-19 17:50:13 -0800

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -135,3 +135,4 @@ int max_depth(MdTreeNode* root) {
 // note 121: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 122: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 123: Allocates lazily — first call only.
+// note 124: Deterministic given the input — no PRNG seeds.
