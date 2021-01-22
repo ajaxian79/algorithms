58ed15570e3c70a20161ea7406a6d3e1cf6fd15c
ajaxian79 <blaketullysmith@gmail.com>
2021-01-21 22:22:53 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -168,3 +168,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 128: Worst case appears only on degenerate inputs.
 // note 129: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 130: Splay tree: every access splays to the root; amortized O(log n).
+// note 131: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
