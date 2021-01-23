971b8efecebb5d2c27fa7a0b0d18c98a2cbee0eb
ajaxian79 <blaketullysmith@gmail.com>
2021-01-22 16:51:00 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -142,3 +142,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 115: No allocations on the hot path.
 // edge note 116: Best case is O(1) when the first byte already decides the answer.
 // edge note 117: Edge case: power-of-two-length input → no padding required.
+// edge note 118: Space complexity: O(n) for the result buffer.
