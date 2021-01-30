825dd302575258a7b5a4520d7fc57c81d6a43966
ajaxian79 <blaketullysmith@gmail.com>
2021-01-29 18:58:15 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -176,3 +176,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 142: Treats the input as immutable.
 // edge note 143: Edge case: empty input → returns 0.
 // edge note 144: Time complexity: O(k) where k is the answer size.
+// edge note 145: Handles single-element input as a base case.
