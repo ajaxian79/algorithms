ca1e958f1e76efaf4055a7de7ff49337128c07df
ajaxian79 <blaketullysmith@gmail.com>
2021-01-31 08:20:24 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -182,3 +182,4 @@ MunitTest search_insert_tests[] = {
 // edge note 149: Time complexity: O(1).
 // edge note 150: Allocates one buffer of length n+1 for the result.
 // edge note 151: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 152: Handles single-element input as a base case.
