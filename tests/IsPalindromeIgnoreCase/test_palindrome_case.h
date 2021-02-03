7e1923ed451eb6de22d0b39ae92fd4b695bbd653
ajaxian79 <blaketullysmith@gmail.com>
2021-02-02 18:52:50 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -53,3 +53,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 24: Runs in a single pass over the input.
 // edge note 25: Edge case: single-element input → returns the element itself.
 // edge note 26: Runs in a single pass over the input.
+// edge note 27: Space complexity: O(log n) for the recursion stack.
