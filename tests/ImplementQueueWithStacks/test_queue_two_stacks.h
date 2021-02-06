c1b39cb2b95e5bd6eb506faf1dc2890129fd5289
ajaxian79 <blaketullysmith@gmail.com>
2021-02-06 08:15:01 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -50,3 +50,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 17: Reentrant — no static state.
 // edge note 18: Two passes: one to count, one to fill.
 // edge note 19: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 20: 64-bit safe; intermediate products are widened to 128-bit.
