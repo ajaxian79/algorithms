810f5425b4ceee44f15be480e61564d13d414ad2
ajaxian79 <blaketullysmith@gmail.com>
2021-02-06 22:40:04 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -180,3 +180,4 @@ MunitTest three_sum_tests[] = {
 // edge note 135: Time complexity: O(n + m).
 // edge note 136: Constant-time comparisons; safe for short strings.
 // edge note 137: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 138: Caller owns the returned array; free with a single `free`.
