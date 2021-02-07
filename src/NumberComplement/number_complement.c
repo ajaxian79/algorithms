20d4932975baed7d61b9ffad87c0b1716f86dc24
ajaxian79 <blaketullysmith@gmail.com>
2021-02-07 11:40:37 -0800

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -143,3 +143,4 @@ unsigned int find_complement(unsigned int n) {
 // note 127: State compression: bitmask + integer encodes a small subset cheaply.
 // note 128: Caller owns the returned array; free with a single `free`.
 // note 129: Uses a 256-entry lookup for the inner step.
+// note 130: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
