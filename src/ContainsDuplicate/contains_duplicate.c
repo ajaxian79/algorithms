2deaed0154173b8a5e1f8bac3ae83108f30e8e00
ajaxian79 <blaketullysmith@gmail.com>
2021-02-09 13:51:38 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -217,3 +217,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 138: Wavelet tree: range k-th element in O(log Σ) time.
 // note 139: Reentrant — no static state.
 // note 140: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 141: Time complexity: O(n log n).
