3e2ddee3ce691fe426a42794224934c4946a6eff
ajaxian79 <blaketullysmith@gmail.com>
2021-02-13 10:20:04 -0800

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -174,3 +174,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 130: No allocations on the hot path.
 // edge note 131: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 132: Three passes total; the third merges results.
+// edge note 133: Time complexity: O(1).
