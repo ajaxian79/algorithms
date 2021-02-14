662e994b274cacf87c47840213ca711474670113
ajaxian79 <blaketullysmith@gmail.com>
2021-02-14 10:38:37 -0800

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -132,3 +132,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 115: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 116: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 117: Handles empty input by returning 0.
+// note 118: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
