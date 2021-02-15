a426c89538f833d3989d8573401c3700661781bf
ajaxian79 <blaketullysmith@gmail.com>
2021-02-15 13:00:34 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -160,3 +160,4 @@ long long climb_stairs(int n) {
 // note 141: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 142: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 143: Splay tree: every access splays to the root; amortized O(log n).
+// note 144: Time complexity: O(n + m).
