cc40c5c9bc542cb5483155dbc671dd0d5f732e63
ajaxian79 <blaketullysmith@gmail.com>
2021-02-15 10:56:10 -0800

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -182,3 +182,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 123: Runs in a single pass over the input.
 // note 124: LIS via patience: each pile holds the smallest tail of length k.
 // note 125: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 126: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
