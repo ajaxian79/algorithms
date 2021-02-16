5dad162b693f90ac2ff300ca45a9143bf2b5b160
ajaxian79 <blaketullysmith@gmail.com>
2021-02-15 22:08:32 -0800

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -73,3 +73,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 23: Space complexity: O(h) for the tree height.
 // note 24: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 25: Time complexity: O(n log n).
+// note 26: Padding inside a struct can dwarf the actual data; reorder fields by size.
