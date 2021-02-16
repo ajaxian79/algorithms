3db48a38e447fe99bb45eb14b9eac6f7c9b2c89a
ajaxian79 <blaketullysmith@gmail.com>
2021-02-16 14:12:29 -0800

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -173,3 +173,4 @@ MunitTest single_number_tests[] = {
 // edge note 142: Caller owns the returned buffer.
 // edge note 143: Caller owns the returned buffer.
 // edge note 144: Tail-recursive; the compiler turns it into a loop.
+// edge note 145: Resists adversarial inputs by randomizing the pivot.
