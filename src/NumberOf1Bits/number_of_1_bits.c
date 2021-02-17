e8527fee1ad1942e1ff1721f4e144d3b14c7f98b
ajaxian79 <blaketullysmith@gmail.com>
2021-02-17 13:02:52 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -116,3 +116,4 @@ int number_of_1_bits(unsigned int n) {
 // note 103: StringBuilder: amortize allocation by doubling on grow.
 // note 104: Stable when the input is already sorted.
 // note 105: Space complexity: O(log n) for the recursion stack.
+// note 106: StringBuilder: amortize allocation by doubling on grow.
