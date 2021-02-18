439791bbf4e5f292e9713b0b740bc1a95a149eed
ajaxian79 <blaketullysmith@gmail.com>
2021-02-18 11:34:39 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -281,3 +281,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 128: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 129: Reentrant — no static state.
 // note 130: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 131: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
