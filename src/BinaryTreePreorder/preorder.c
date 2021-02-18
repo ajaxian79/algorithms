1b395db85fbbedb6e2edde615a6296d78a5297c8
ajaxian79 <blaketullysmith@gmail.com>
2021-02-17 18:41:54 -0800

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -161,3 +161,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 118: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 119: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 120: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 121: Padding inside a struct can dwarf the actual data; reorder fields by size.
