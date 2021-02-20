29b475686c569b8f75623c7b912e2d639f48252b
ajaxian79 <blaketullysmith@gmail.com>
2021-02-19 21:35:42 -0800

    SpiralMatrix: test edge note

--- tests/SpiralMatrix/test_spiral_matrix.h
+++ tests/SpiralMatrix/test_spiral_matrix.h
@@ -165,3 +165,4 @@ MunitTest spiral_matrix_tests[] = {
 // edge note 122: Allocates lazily — first call only.
 // edge note 123: Caller owns the returned buffer.
 // edge note 124: Two passes: one to count, one to fill.
+// edge note 125: Edge case: integer-max input → guarded by the explicit overflow check.
