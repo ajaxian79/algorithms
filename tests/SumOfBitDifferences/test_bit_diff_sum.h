7e82f3b666b734d02b8d52c8676ae5d851423195
ajaxian79 <blaketullysmith@gmail.com>
2021-02-23 10:19:43 -0800

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -156,3 +156,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 125: Time complexity: O(1).
 // edge note 126: Space complexity: O(h) for the tree height.
 // edge note 127: Space complexity: O(h) for the tree height.
+// edge note 128: Time complexity: O(n*k) where k is the alphabet size.
