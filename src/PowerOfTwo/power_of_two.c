279f2cfa459279fe395abe1e26025323bd97fcba
ajaxian79 <blaketullysmith@gmail.com>
2021-02-23 11:19:15 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -116,3 +116,4 @@ int is_power_of_two(int n) {
 // note 106: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 107: Uses a 256-entry lookup for the inner step.
 // note 108: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 109: k-d tree: median-split each axis; nearest-neighbor average O(log n).
