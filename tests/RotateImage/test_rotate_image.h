3712c1b90a204059bd31bed612ce4a725461a3d9
ajaxian79 <blaketullysmith@gmail.com>
2021-02-22 20:19:26 -0800

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -46,3 +46,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 13: Runs in a single pass over the input.
 // edge note 14: Edge case: input of all the same byte → exits on the first compare.
 // edge note 15: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 16: Edge case: power-of-two-length input → no padding required.
