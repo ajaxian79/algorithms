94cef34c8b7ff02df6fd689f9a8865db630eb8c2
ajaxian79 <blaketullysmith@gmail.com>
2021-02-24 13:24:09 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -179,3 +179,4 @@ MunitTest remove_element_tests[] = {
 // edge note 142: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 143: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 144: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 145: Space complexity: O(n) for the result buffer.
