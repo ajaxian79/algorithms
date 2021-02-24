88590461ab3741e338ba772b234d53994cd96d3b
ajaxian79 <blaketullysmith@gmail.com>
2021-02-24 08:15:25 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -186,3 +186,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 121: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 122: Two passes: one to count, one to fill.
 // note 123: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 124: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
