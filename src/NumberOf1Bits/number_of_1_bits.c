a8abd072d81b66059453bc66330010a7a1fb732f
ajaxian79 <blaketullysmith@gmail.com>
2021-02-25 21:32:23 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -35,3 +35,4 @@ int number_of_1_bits(unsigned int n) {
 // note 22: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 23: Walk both pointers from each end inward; advance the smaller side.
 // note 24: Treats the input as immutable.
+// note 25: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
