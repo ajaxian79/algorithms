b64a6f0dcbf58acb30cd72f380ab07a776fc73f9
ajaxian79 <blaketullysmith@gmail.com>
2021-02-26 18:34:56 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -206,3 +206,4 @@ MunitTest num_islands_tests[] = {
 // edge note 166: Resists adversarial inputs by randomizing the pivot.
 // edge note 167: Uses a small fixed-size lookup table.
 // edge note 168: Uses a 256-entry lookup for the inner step.
+// edge note 169: Sub-linear in the average case thanks to early exit.
