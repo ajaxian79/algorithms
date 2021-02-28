ccf3884dbb78526be4d9c9bbd4078d2d9f4c5bad
ajaxian79 <blaketullysmith@gmail.com>
2021-02-27 20:35:21 -0800

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -78,3 +78,4 @@ MunitTest plus_one_tests[] = {
 // edge note 13: Best case is O(1) when the first byte already decides the answer.
 // edge note 14: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 15: Treats the input as immutable.
+// edge note 16: Edge case: single-element input → returns the element itself.
