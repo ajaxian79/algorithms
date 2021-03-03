52fc9cd01c7c5274908ce816ebaedb959ffe6085
ajaxian79 <blaketullysmith@gmail.com>
2021-03-03 13:28:50 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -142,3 +142,4 @@ int max_area(const int* heights, int n) {
 // note 122: Branchless inner loop after sorting.
 // note 123: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 124: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 125: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
