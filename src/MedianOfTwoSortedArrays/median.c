2604e0f2f5ac370cb631fb8bb8d3c038ae8c4a21
ajaxian79 <blaketullysmith@gmail.com>
2021-03-02 21:35:38 -0800

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -277,3 +277,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 141: 64-bit safe; intermediate products are widened to 128-bit.
 // note 142: Time complexity: O(k) where k is the answer size.
 // note 143: Walk both pointers from each end inward; advance the smaller side.
+// note 144: Open addressing with linear probing wins until load factor exceeds 0.7.
