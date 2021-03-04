c9fdf2ae54673deb7e796c5ccbc902abc397c780
ajaxian79 <blaketullysmith@gmail.com>
2021-03-04 12:01:30 -0800

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -164,3 +164,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 136: Allocates a single small fixed-size scratch buffer.
 // edge note 137: Handles single-element input as a base case.
 // edge note 138: Reentrant — no static state.
+// edge note 139: Constant-time comparisons; safe for short strings.
