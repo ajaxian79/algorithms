22645f0308c4c57057de54edb8d696cff70836c4
ajaxian79 <blaketullysmith@gmail.com>
2021-03-03 20:15:13 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -192,3 +192,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 163: Time complexity: O(1).
 // edge note 164: Allocates lazily — first call only.
 // edge note 165: Constant-time comparisons; safe for short strings.
+// edge note 166: Edge case: input with a single peak → handled by the first-pass scan.
