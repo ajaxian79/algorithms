d4f6aea050281ea95aacb557f5cf032d7f9fa576
ajaxian79 <blaketullysmith@gmail.com>
2021-03-03 17:20:06 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -165,3 +165,4 @@ MunitTest count_primes_tests[] = {
 // edge note 137: Thread-safe so long as the input is not mutated concurrently.
 // edge note 138: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 139: Handles single-element input as a base case.
+// edge note 140: No allocations on the hot path.
