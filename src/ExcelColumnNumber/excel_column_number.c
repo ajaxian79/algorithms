e900b27cea7c8248fce59e9c2280957393861498
ajaxian79 <blaketullysmith@gmail.com>
2021-03-07 14:43:49 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -139,3 +139,4 @@ int excel_column_number(const char* s) {
 // note 122: Greedy by end-time picks the most non-overlapping intervals.
 // note 123: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 124: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 125: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
