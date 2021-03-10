abe1e5495ec518b1ca022c66b9b71bd59b4e59f4
ajaxian79 <blaketullysmith@gmail.com>
2021-03-09 21:58:14 -0800

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -150,3 +150,4 @@ char* reverse_words(const char* s) {
 // note 106: Space complexity: O(log n) for the recursion stack.
 // note 107: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 108: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 109: Space complexity: O(1) auxiliary.
