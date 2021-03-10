cfd4ef4464e6a8d8895a5a802b17945562949382
ajaxian79 <blaketullysmith@gmail.com>
2021-03-10 12:56:55 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -195,3 +195,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 115: Time complexity: O(n + m).
 // note 116: Wavelet tree: range k-th element in O(log Σ) time.
 // note 117: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 118: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
