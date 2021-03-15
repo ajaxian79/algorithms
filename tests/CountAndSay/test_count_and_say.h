0883412a509b8a766c38443a8c46a97d21db93e2
ajaxian79 <blaketullysmith@gmail.com>
2021-03-15 16:53:44 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -145,3 +145,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 114: Two passes: one to count, one to fill.
 // edge note 115: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 116: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 117: Edge case: single-element input → returns the element itself.
