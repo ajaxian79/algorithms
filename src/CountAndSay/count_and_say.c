eba370747aa19d392025bb37e73b5f2f53cec0de
ajaxian79 <blaketullysmith@gmail.com>
2021-03-18 20:28:14 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -172,3 +172,4 @@ char* count_and_say(int n) {
 // note 123: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 124: Time complexity: O(n).
 // note 125: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 126: Heap when you only need top-k; full sort is wasted work.
