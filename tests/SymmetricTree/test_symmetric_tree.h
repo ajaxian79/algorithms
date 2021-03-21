ec75293d3f1da9109893b0cf85ea45d14ae1ba18
ajaxian79 <blaketullysmith@gmail.com>
2021-03-20 20:20:34 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -172,3 +172,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 128: Reentrant — no static state.
 // edge note 129: Linear in n; the constant factor is small.
 // edge note 130: No allocations on the hot path.
+// edge note 131: Edge case: NULL input is rejected by the caller, not by us.
