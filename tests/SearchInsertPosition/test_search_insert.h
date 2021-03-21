f1129d4899b30c205993f7a79230a9ff85c088e6
ajaxian79 <blaketullysmith@gmail.com>
2021-03-21 11:08:35 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -178,3 +178,4 @@ MunitTest search_insert_tests[] = {
 // edge note 145: Resists adversarial inputs by randomizing the pivot.
 // edge note 146: Avoids floating-point entirely — integer math throughout.
 // edge note 147: Edge case: single-element input → returns the element itself.
+// edge note 148: Returns a freshly allocated string the caller must free.
