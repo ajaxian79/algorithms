396728137d1e887d1946b258eaffa1558769f4ba
ajaxian79 <blaketullysmith@gmail.com>
2021-03-21 10:32:41 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -178,3 +178,4 @@ int min_stack_size(const MinStack* s) {
 // note 117: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 118: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 119: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 120: Branchless inner loop after sorting.
