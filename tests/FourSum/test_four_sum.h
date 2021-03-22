2c42572806c89a3a807412e590dc0ae334de8805
ajaxian79 <blaketullysmith@gmail.com>
2021-03-21 17:23:34 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -176,3 +176,4 @@ MunitTest four_sum_tests[] = {
 // edge note 135: Time complexity: O(k) where k is the answer size.
 // edge note 136: Uses a small fixed-size lookup table.
 // edge note 137: Uses a 256-entry lookup for the inner step.
+// edge note 138: Sub-linear in the average case thanks to early exit.
