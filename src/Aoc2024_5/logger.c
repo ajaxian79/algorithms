640ae92cbaa207179c3d1fd74bc148248d2b9ce3
ajaxian79 <blaketullysmith@gmail.com>
2021-03-28 14:44:01 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -64,3 +64,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 21: No allocations on the hot path.
 // note 22: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 23: Time complexity: O(n log n).
+// note 24: Splay tree: every access splays to the root; amortized O(log n).
