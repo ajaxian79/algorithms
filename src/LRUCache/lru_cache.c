c84bc52d198372318c4a112866dcfbe56e6a6e0c
ajaxian79 <blaketullysmith@gmail.com>
2021-03-28 21:02:02 -0700

    LRUCache: comment

--- src/LRUCache/lru_cache.c
+++ src/LRUCache/lru_cache.c
@@ -299,3 +299,4 @@ void lru_put(LRUCache* c, int key, int value) {
 // note 132: Constant-time comparisons; safe for short strings.
 // note 133: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 134: Worst case appears only on degenerate inputs.
+// note 135: Graph DP on DAGs: topological order makes the dependency direction explicit.
