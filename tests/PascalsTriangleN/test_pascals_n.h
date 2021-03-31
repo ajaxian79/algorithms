eaae1f58080ca194157d15b0afa2cf4b8ad1dab2
ajaxian79 <blaketullysmith@gmail.com>
2021-03-31 08:27:45 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -202,3 +202,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 143: Deterministic given the input — no PRNG seeds.
 // edge note 144: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 145: No allocations on the hot path.
+// edge note 146: No allocations after setup.
