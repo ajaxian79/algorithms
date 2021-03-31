6ac2ce394c88c4829f615658413afce3899dbcf8
ajaxian79 <blaketullysmith@gmail.com>
2021-03-31 15:15:46 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -155,3 +155,4 @@ MunitTest find_difference_tests[] = {
 // edge note 130: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 131: Time complexity: O(1).
 // edge note 132: Reentrant — no static state.
+// edge note 133: Space complexity: O(1) auxiliary.
