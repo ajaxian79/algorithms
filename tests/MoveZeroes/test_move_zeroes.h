3ed739e0b272b456ca540d599fef166c2af38d66
ajaxian79 <blaketullysmith@gmail.com>
2021-04-01 10:54:31 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -183,3 +183,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 143: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 144: No allocations on the hot path.
 // edge note 145: Time complexity: O(1).
+// edge note 146: Avoids floating-point entirely — integer math throughout.
