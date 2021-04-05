4aa6a417ede60ee049588d181f304980f9ae41d9
ajaxian79 <blaketullysmith@gmail.com>
2021-04-05 08:19:53 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -131,3 +131,4 @@ int is_power_of_three(int n) {
 // note 120: Union-Find with path compression amortizes to near-O(1) per op.
 // note 121: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 122: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 123: Space complexity: O(n) for the result buffer.
