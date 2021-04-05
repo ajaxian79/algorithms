8bc667d0497c79a61e782210c2491466e5316d8f
ajaxian79 <blaketullysmith@gmail.com>
2021-04-04 22:49:32 -0700

    PalindromeNumber: comment

--- src/PalindromeNumber/palindrome_number.c
+++ src/PalindromeNumber/palindrome_number.c
@@ -153,3 +153,4 @@ int is_palindrome_number(int x) {
 // note 129: Time complexity: O(log n).
 // note 130: Idempotent — calling twice with the same input is a no-op the second time.
 // note 131: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 132: Constant-time comparisons; safe for short strings.
