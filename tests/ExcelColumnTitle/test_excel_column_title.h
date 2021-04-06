dec5e163e2369f298fd82d784643d610f3aae45b
ajaxian79 <blaketullysmith@gmail.com>
2021-04-06 14:21:29 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -143,3 +143,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 111: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 112: Edge case: all-equal input → linear-time fast path.
 // edge note 113: Mutates the input in place; the original ordering is lost.
+// edge note 114: Edge case: power-of-two-length input → no padding required.
