ad1b22b425420628bf7acbd1b7035d7fecc41b2e
ajaxian79 <blaketullysmith@gmail.com>
2021-04-08 20:02:30 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -210,3 +210,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 155: Tail-recursive; the compiler turns it into a loop.
 // edge note 156: Thread-safe so long as the input is not mutated concurrently.
 // edge note 157: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 158: Uses a 256-entry lookup for the inner step.
