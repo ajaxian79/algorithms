265f7eb12ba4d2b46576ad99a88ab62e6eadcbdb
ajaxian79 <blaketullysmith@gmail.com>
2021-04-12 22:59:56 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -166,3 +166,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 135: Edge case: input with no peak → falls through to the default branch.
 // edge note 136: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 137: No allocations after setup.
+// edge note 138: Time complexity: O(1).
