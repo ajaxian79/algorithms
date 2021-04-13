951be38cdaad5cf1d88c3c1b1e4e3d56301062d5
ajaxian79 <blaketullysmith@gmail.com>
2021-04-13 14:28:20 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -160,3 +160,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 132: Deterministic given the input — no PRNG seeds.
 // edge note 133: Stable when the input is already sorted.
 // edge note 134: Edge case: already-sorted input → no swaps performed.
+// edge note 135: Three passes total; the third merges results.
