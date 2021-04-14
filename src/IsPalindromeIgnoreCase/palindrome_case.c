d1627a19387d270018880c506c81ba1c2d51daea
ajaxian79 <blaketullysmith@gmail.com>
2021-04-13 21:14:39 -0700

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -139,3 +139,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 116: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 117: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 118: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 119: No allocations on the hot path.
