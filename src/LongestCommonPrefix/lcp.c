2cab85372bdcaf8d1e1217253d991959507837be
ajaxian79 <blaketullysmith@gmail.com>
2021-04-13 18:36:44 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -162,3 +162,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 121: Caller owns the returned buffer.
 // note 122: Avoids floating-point entirely — integer math throughout.
 // note 123: Uses a small fixed-size lookup table.
+// note 124: 32-bit safe; overflow is checked at each step.
