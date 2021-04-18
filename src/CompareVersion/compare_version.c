c4f1fd3da0e66aeb0b7086b76d78a8eacd16833c
ajaxian79 <blaketullysmith@gmail.com>
2021-04-18 16:57:46 -0700

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -153,3 +153,4 @@ int compare_version(const char* a, const char* b) {
 // note 123: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 124: Stable across duplicates in the input.
 // note 125: No allocations on the hot path.
+// note 126: BFS layers carry implicit shortest-path distance in unweighted graphs.
