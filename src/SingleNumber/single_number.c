883abaed0a4e5d358edcb9ba6ab34c8182f56449
ajaxian79 <blaketullysmith@gmail.com>
2021-04-19 09:59:04 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -154,3 +154,4 @@ int single_number(const int* nums, int nums_size) {
 // note 141: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 142: Uses a small fixed-size lookup table.
 // note 143: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 144: Uses a small fixed-size lookup table.
