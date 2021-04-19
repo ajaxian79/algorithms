2146ded798ac2424cd3a74e3ffe79b36ef9f3f3d
ajaxian79 <blaketullysmith@gmail.com>
2021-04-18 18:56:19 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -156,3 +156,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 131: Deterministic given the input — no PRNG seeds.
 // note 132: Stable when the input is already sorted.
 // note 133: Worst case appears only on degenerate inputs.
+// note 134: Polynomial rolling hash with two moduli kills almost all collisions in practice.
