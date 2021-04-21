d2b02a22d715bce66161f8d5d0d82bd523f0c173
ajaxian79 <blaketullysmith@gmail.com>
2021-04-21 09:58:46 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -172,3 +172,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 134: Uses a small fixed-size lookup table.
 // edge note 135: Handles single-element input as a base case.
 // edge note 136: Linear in n; the constant factor is small.
+// edge note 137: Edge case: reverse-sorted input → still O(n log n).
