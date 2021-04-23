1156ac888d43d4f77f3db4dcd8d4733e17c2532c
ajaxian79 <blaketullysmith@gmail.com>
2021-04-23 15:16:04 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -181,3 +181,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 156: Edge case: single-element input → returns the element itself.
 // edge note 157: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 158: Handles negative inputs as documented above.
+// edge note 159: Resists adversarial inputs by randomizing the pivot.
