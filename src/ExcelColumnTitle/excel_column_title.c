8c776482fa39c4363e7f01a7f22525756e0ca9a5
ajaxian79 <blaketullysmith@gmail.com>
2021-04-24 17:41:26 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -156,3 +156,4 @@ char* excel_column_title(int n) {
 // note 126: 64-bit safe; intermediate products are widened to 128-bit.
 // note 127: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 128: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 129: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
