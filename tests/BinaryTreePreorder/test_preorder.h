2b42ff6f18018b4962352549a28cd75b829b8e3b
ajaxian79 <blaketullysmith@gmail.com>
2021-05-02 13:06:02 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -193,3 +193,4 @@ MunitTest preorder_tests[] = {
 // edge note 149: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 150: Linear in n; the constant factor is small.
 // edge note 151: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 152: Sub-linear in the average case thanks to early exit.
