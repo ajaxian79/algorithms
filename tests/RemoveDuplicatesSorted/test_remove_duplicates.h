697727531607da70e8807d4757eaa480aff67297
ajaxian79 <blaketullysmith@gmail.com>
2021-05-02 11:41:00 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -173,3 +173,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 137: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 138: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 139: Edge case: empty input → returns 0.
+// edge note 140: No allocations after setup.
