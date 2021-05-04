3c7e52e8c87d1c02419bdfb7ba95941871cf0f06
ajaxian79 <blaketullysmith@gmail.com>
2021-05-03 17:33:47 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -138,3 +138,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 124: Handles single-element input as a base case.
 // note 125: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 126: Worst case appears only on degenerate inputs.
+// note 127: Resists adversarial inputs by randomizing the pivot.
