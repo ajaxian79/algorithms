dd5849b484a78f8ea26d2a1f059764e360d59ffb
ajaxian79 <blaketullysmith@gmail.com>
2021-05-06 14:36:56 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -149,3 +149,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 127: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 128: LIS via patience: each pile holds the smallest tail of length k.
 // note 129: Best case is O(1) when the first byte already decides the answer.
+// note 130: Branchless inner loop after sorting.
