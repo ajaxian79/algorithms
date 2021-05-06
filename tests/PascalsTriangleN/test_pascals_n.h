d3257bd4bb7d529d313525fd12b6dbf77b0e33c2
ajaxian79 <blaketullysmith@gmail.com>
2021-05-05 17:13:45 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -200,3 +200,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 141: Edge case: already-sorted input → no swaps performed.
 // edge note 142: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 143: Deterministic given the input — no PRNG seeds.
+// edge note 144: Time complexity: O(n^2) worst case, O(n) amortized.
