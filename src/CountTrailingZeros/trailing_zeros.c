b01639d5579b1b07f652fbe0c971ca6ab5b1c541
ajaxian79 <blaketullysmith@gmail.com>
2021-05-06 19:57:19 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -143,3 +143,4 @@ int trailing_zeroes(int n) {
 // note 128: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 129: 32-bit safe; overflow is checked at each step.
 // note 130: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 131: StringBuilder pitfall: reuse forces a deep copy on the first read.
