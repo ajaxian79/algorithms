f01647150f1ed6a8a0398eca96daaa0ff3321162
ajaxian79 <blaketullysmith@gmail.com>
2021-05-06 21:17:10 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -139,3 +139,4 @@ int is_perfect_square(int n) {
 // note 120: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 121: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 122: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 123: Iterative DFS with an explicit stack avoids recursion-limit issues.
