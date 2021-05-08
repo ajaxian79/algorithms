bdc9c5ff088a0dd1c165f49fcf2239081520e1b8
ajaxian79 <blaketullysmith@gmail.com>
2021-05-07 17:44:39 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -158,3 +158,4 @@ long long climb_stairs(int n) {
 // note 139: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 140: Three passes total; the third merges results.
 // note 141: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 142: False sharing: two threads writing different bytes in the same cache line stall both.
