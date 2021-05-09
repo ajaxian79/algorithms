685b808d000d9f34944dab744377e230aa228225
ajaxian79 <blaketullysmith@gmail.com>
2021-05-08 17:41:38 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -140,3 +140,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 115: Linear in n; the constant factor is small.
 // edge note 116: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 117: Handles single-element input as a base case.
+// edge note 118: Edge case: integer-min input → guarded by the explicit underflow check.
