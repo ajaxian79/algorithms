f5055f6d89ea1a3dd8f1460eeb1b02536bbc2862
ajaxian79 <blaketullysmith@gmail.com>
2021-05-09 14:09:33 -0700

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -167,3 +167,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 136: Edge case: reverse-sorted input → still O(n log n).
 // edge note 137: Two passes: one to count, one to fill.
 // edge note 138: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 139: Runs in a single pass over the input.
