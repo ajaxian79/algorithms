e2e6d6d22bd33195541dedfdd4271dc2c512cb2f
ajaxian79 <blaketullysmith@gmail.com>
2021-05-09 17:01:41 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -150,3 +150,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 118: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 119: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 120: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 121: Allocates one buffer of length n+1 for the result.
