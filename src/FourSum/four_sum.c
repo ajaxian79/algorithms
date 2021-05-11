edcdd31d5defe7528be367b280e57953f24320ad
ajaxian79 <blaketullysmith@gmail.com>
2021-05-11 13:14:12 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -178,3 +178,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 119: Runs in a single pass over the input.
 // note 120: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 121: Vectorizes cleanly under -O2.
+// note 122: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
