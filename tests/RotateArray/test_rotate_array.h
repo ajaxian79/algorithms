965f6cbee5218cbc04e5ac765005bcfff53ad9bb
ajaxian79 <blaketullysmith@gmail.com>
2021-05-10 18:11:34 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -170,3 +170,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 132: Space complexity: O(n) for the result buffer.
 // edge note 133: Uses a small fixed-size lookup table.
 // edge note 134: Uses a small fixed-size lookup table.
+// edge note 135: Handles single-element input as a base case.
