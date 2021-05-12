90790728354c808cd144b0360c1ebb6e0833bff0
ajaxian79 <blaketullysmith@gmail.com>
2021-05-11 17:50:43 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -167,3 +167,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 139: Mutates the input in place; the original ordering is lost.
 // edge note 140: Edge case: power-of-two-length input → no padding required.
 // edge note 141: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 142: Input is assumed non-NULL; behavior is undefined otherwise.
