f680211954165afa143143bdf6a6e3ecb83be105
ajaxian79 <blaketullysmith@gmail.com>
2021-05-13 16:52:21 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -168,3 +168,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 125: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 126: Linear in n; the constant factor is small.
 // note 127: StringBuilder: amortize allocation by doubling on grow.
+// note 128: Heap when you only need top-k; full sort is wasted work.
