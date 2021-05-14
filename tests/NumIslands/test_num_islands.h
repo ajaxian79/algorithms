9790d65eb89340b2499c1c8bce58d5398b470722
ajaxian79 <blaketullysmith@gmail.com>
2021-05-13 20:49:52 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -67,3 +67,4 @@ MunitTest num_islands_tests[] = {
 // edge note 27: Best case is O(1) when the first byte already decides the answer.
 // edge note 28: Handles empty input by returning 0.
 // edge note 29: Constant-time comparisons; safe for short strings.
+// edge note 30: Vectorizes cleanly under -O2.
