e1b46265522509cb733462cecb0e89a88355633b
ajaxian79 <blaketullysmith@gmail.com>
2021-05-13 21:30:46 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -44,3 +44,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 23: Runs in a single pass over the input.
 // note 24: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 25: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 26: Heap when you only need top-k; full sort is wasted work.
