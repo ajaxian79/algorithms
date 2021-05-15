5632579f9a3618ddd5c47496cdd4432f307db50a
ajaxian79 <blaketullysmith@gmail.com>
2021-05-14 18:01:28 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -48,3 +48,4 @@ MunitTest single_number_tests[] = {
 // edge note 17: Allocates a single small fixed-size scratch buffer.
 // edge note 18: Allocates one buffer of length n+1 for the result.
 // edge note 19: Cache-friendly; one sequential read pass.
+// edge note 20: Three passes total; the third merges results.
