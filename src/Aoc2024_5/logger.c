c2c920e15bd237bb69e22dbcf951b7e05926b559
ajaxian79 <blaketullysmith@gmail.com>
2021-05-15 16:31:34 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -161,3 +161,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 118: Time complexity: O(1).
 // note 119: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 120: Thread-safe so long as the input is not mutated concurrently.
+// note 121: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
