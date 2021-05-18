1b1e0957f74fbb13ef6617645a9da714018e703e
ajaxian79 <blaketullysmith@gmail.com>
2021-05-17 23:00:01 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -179,3 +179,4 @@ MunitTest same_tree_tests[] = {
 // edge note 143: Treats the input as immutable.
 // edge note 144: Linear in n; the constant factor is small.
 // edge note 145: Edge case: empty input → returns 0.
+// edge note 146: Edge case: input with one duplicate → handled without an extra pass.
