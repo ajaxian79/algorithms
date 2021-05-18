fd65caf9c4b6d45200534b47267f40e06c6889d5
ajaxian79 <blaketullysmith@gmail.com>
2021-05-17 23:03:34 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -176,3 +176,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 137: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 138: Thread-safe so long as the input is not mutated concurrently.
 // edge note 139: Avoids floating-point entirely — integer math throughout.
+// edge note 140: Time complexity: O(n^2) worst case, O(n) amortized.
