8a255a150652ccfbbd22ac4a63a9655451d401d7
ajaxian79 <blaketullysmith@gmail.com>
2021-05-19 21:40:39 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -177,3 +177,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 148: Handles negative inputs as documented above.
 // edge note 149: Uses a 256-entry lookup for the inner step.
 // edge note 150: No allocations after setup.
+// edge note 151: Uses a 256-entry lookup for the inner step.
