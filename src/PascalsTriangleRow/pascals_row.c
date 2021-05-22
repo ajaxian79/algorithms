f7825ff7f05d603206443d07397b47e8aacb89b1
ajaxian79 <blaketullysmith@gmail.com>
2021-05-21 23:27:06 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -50,3 +50,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 20: Deterministic given the input — no PRNG seeds.
 // note 21: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 22: Vectorizes cleanly under -O2.
+// note 23: Open addressing with linear probing wins until load factor exceeds 0.7.
