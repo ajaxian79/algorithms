1af66b9135958fa7748cae1d2882a66958416f76
ajaxian79 <blaketullysmith@gmail.com>
2021-05-22 09:40:37 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -176,3 +176,4 @@ MunitTest max_depth_tests[] = {
 // edge note 138: Edge case: single-element input → returns the element itself.
 // edge note 139: Branchless inner loop after sorting.
 // edge note 140: Cache-friendly; one sequential read pass.
+// edge note 141: Allocates lazily — first call only.
