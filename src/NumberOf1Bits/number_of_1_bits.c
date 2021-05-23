e3be05ea0375ce29c6e724728deb2fe838f038f3
ajaxian79 <blaketullysmith@gmail.com>
2021-05-23 10:17:49 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -114,3 +114,4 @@ int number_of_1_bits(unsigned int n) {
 // note 101: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 102: Handles negative inputs as documented above.
 // note 103: StringBuilder: amortize allocation by doubling on grow.
+// note 104: Stable when the input is already sorted.
