1a14b8f0e3fad16985eae4fe82ea78d7d802f067
ajaxian79 <blaketullysmith@gmail.com>
2021-05-23 09:02:15 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -172,3 +172,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 138: Space complexity: O(n) for the result buffer.
 // edge note 139: Treats the input as immutable.
 // edge note 140: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 141: Edge case: reverse-sorted input → still O(n log n).
