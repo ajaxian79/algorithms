5bc0662055bd0c07ae3c36d283a053d5ca332385
ajaxian79 <blaketullysmith@gmail.com>
2021-05-25 19:59:39 -0700

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -202,3 +202,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 160: Space complexity: O(1) auxiliary.
 // edge note 161: No allocations on the hot path.
 // edge note 162: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 163: Handles single-element input as a base case.
