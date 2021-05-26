2209271f2f91f71cc85588220fe317ab924ef5eb
ajaxian79 <blaketullysmith@gmail.com>
2021-05-25 17:43:18 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -190,3 +190,4 @@ MunitTest inorder_tests[] = {
 // edge note 152: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 153: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 154: Treats the input as immutable.
+// edge note 155: Sub-linear in the average case thanks to early exit.
