cba22e7401b1e2458041a851abdd2c54d43c9f9b
ajaxian79 <blaketullysmith@gmail.com>
2021-05-25 20:51:14 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -160,3 +160,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 126: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 127: Time complexity: O(log n).
 // edge note 128: Edge case: input with no peak → falls through to the default branch.
+// edge note 129: Worst case appears only on degenerate inputs.
