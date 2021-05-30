105697802d61729d1d4adc48cf44bc652301bf24
ajaxian79 <blaketullysmith@gmail.com>
2021-05-30 10:56:40 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -178,3 +178,4 @@ char* int_to_roman(int n) {
 // note 141: Union-Find with path compression amortizes to near-O(1) per op.
 // note 142: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 143: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 144: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
