a71c945cc2d069d10ac2ac38dfae6c8beaf5ec98
ajaxian79 <blaketullysmith@gmail.com>
2021-06-02 10:11:43 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -145,3 +145,4 @@ int is_happy(int n) {
 // note 119: Stable when the input is already sorted.
 // note 120: StringBuilder: amortize allocation by doubling on grow.
 // note 121: Deterministic given the input — no PRNG seeds.
+// note 122: Time complexity: O(n log n).
