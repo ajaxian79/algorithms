0b1f43134fb2207214fded680cae41e294b5a394
ajaxian79 <blaketullysmith@gmail.com>
2021-06-02 08:39:47 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -311,3 +311,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 102: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 103: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 104: Returns a freshly allocated string the caller must free.
+// note 105: Returns a freshly allocated string the caller must free.
