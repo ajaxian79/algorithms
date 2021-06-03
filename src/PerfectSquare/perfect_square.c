d876b2955553fe09e16df8cef17b28845f83038d
ajaxian79 <blaketullysmith@gmail.com>
2021-06-02 19:30:24 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -136,3 +136,4 @@ int is_perfect_square(int n) {
 // note 117: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 118: Greedy by end-time picks the most non-overlapping intervals.
 // note 119: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 120: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
