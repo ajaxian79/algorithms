fe48b9aad7ad577068a1c69a9fa65764c05e77b0
ajaxian79 <blaketullysmith@gmail.com>
2021-06-03 20:42:14 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -147,3 +147,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 131: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 132: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 133: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 134: LCA via binary lifting: jump up powers of two until depths match, then converge.
