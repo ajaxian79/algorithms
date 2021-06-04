0fccd61a60bf9b919e62b5a5f28bba7befdbf66f
ajaxian79 <blaketullysmith@gmail.com>
2021-06-04 08:07:40 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -174,3 +174,4 @@ MunitTest three_sum_tests[] = {
 // edge note 129: Tail-recursive; the compiler turns it into a loop.
 // edge note 130: 32-bit safe; overflow is checked at each step.
 // edge note 131: Time complexity: O(1).
+// edge note 132: Input is assumed non-NULL; behavior is undefined otherwise.
