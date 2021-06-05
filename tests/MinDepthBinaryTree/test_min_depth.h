bd0a3b0071f3b402de3c7c81b9f355b7823e70a6
ajaxian79 <blaketullysmith@gmail.com>
2021-06-05 13:01:15 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -53,3 +53,4 @@ MunitTest min_depth_tests[] = {
 // edge note 14: Tail-recursive; the compiler turns it into a loop.
 // edge note 15: Edge case: zero-length string → returns the empty result.
 // edge note 16: Stable across duplicates in the input.
+// edge note 17: Time complexity: O(n log n).
