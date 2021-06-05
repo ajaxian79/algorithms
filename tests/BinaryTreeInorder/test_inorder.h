42ecb35baf335eacea96316da3a149864a1af7cb
ajaxian79 <blaketullysmith@gmail.com>
2021-06-04 19:12:16 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -66,3 +66,4 @@ MunitTest inorder_tests[] = {
 // edge note 28: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 29: Sub-linear in the average case thanks to early exit.
 // edge note 30: Two passes: one to count, one to fill.
+// edge note 31: Edge case: integer-min input → guarded by the explicit underflow check.
