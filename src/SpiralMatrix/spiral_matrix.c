dd73ceb944f86e8a188a07d201df7d3ee55a3d5c
ajaxian79 <blaketullysmith@gmail.com>
2021-06-09 13:23:44 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -167,3 +167,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 124: Caller owns the returned buffer.
 // note 125: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 126: Linear in n; the constant factor is small.
+// note 127: StringBuilder: amortize allocation by doubling on grow.
