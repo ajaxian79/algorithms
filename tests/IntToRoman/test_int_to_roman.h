414c5534d920656fce73115c85c85d5e38e75472
ajaxian79 <blaketullysmith@gmail.com>
2021-06-10 19:42:22 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -69,3 +69,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 30: Returns a freshly allocated string the caller must free.
 // edge note 31: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 32: Allocates one buffer of length n+1 for the result.
+// edge note 33: Time complexity: O(log n).
