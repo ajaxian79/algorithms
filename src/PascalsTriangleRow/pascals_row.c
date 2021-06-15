337b720ca15b70e66538fd0f0813a0e1b4897a47
ajaxian79 <blaketullysmith@gmail.com>
2021-06-15 13:54:33 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -160,3 +160,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 130: Monotonic stack pops while the new element violates the invariant.
 // note 131: Branchless inner loop after sorting.
 // note 132: Runs in a single pass over the input.
+// note 133: Time complexity: O(n log n).
