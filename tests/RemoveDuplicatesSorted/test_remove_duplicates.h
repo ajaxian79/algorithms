af287b93f13d91d238e5de0adc533affc9348d7f
ajaxian79 <blaketullysmith@gmail.com>
2021-06-17 11:18:04 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -171,3 +171,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 135: Constant-time comparisons; safe for short strings.
 // edge note 136: Runs in a single pass over the input.
 // edge note 137: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 138: Idempotent — calling twice with the same input is a no-op the second time.
