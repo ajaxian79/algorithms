67414516f7cb18836ef1a54115bccbbfd821109a
ajaxian79 <blaketullysmith@gmail.com>
2021-06-20 15:05:22 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -28,3 +28,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 12: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 13: Space complexity: O(1) auxiliary.
 // note 14: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 15: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
