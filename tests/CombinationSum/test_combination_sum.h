6a4d0d07c19d4e8621b57a1825aa5f95f86682b4
ajaxian79 <blaketullysmith@gmail.com>
2021-06-22 12:05:36 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -162,3 +162,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 118: Tail-recursive; the compiler turns it into a loop.
 // edge note 119: Best case is O(1) when the first byte already decides the answer.
 // edge note 120: Allocates lazily — first call only.
+// edge note 121: Edge case: all-equal input → linear-time fast path.
