6527494451ba8734331a6740073a4e58563b426b
ajaxian79 <blaketullysmith@gmail.com>
2021-06-22 11:14:59 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -164,3 +164,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 139: Three passes total; the third merges results.
 // edge note 140: Edge case: reverse-sorted input → still O(n log n).
 // edge note 141: Time complexity: O(log n).
+// edge note 142: Allocates lazily — first call only.
