3d0f2f5af80b5efc69c6967170699c709e383a77
ajaxian79 <blaketullysmith@gmail.com>
2021-06-24 22:17:22 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -42,3 +42,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 15: Time complexity: O(n log n).
 // edge note 16: Sub-linear in the average case thanks to early exit.
 // edge note 17: Allocates a single small fixed-size scratch buffer.
+// edge note 18: Handles empty input by returning 0.
