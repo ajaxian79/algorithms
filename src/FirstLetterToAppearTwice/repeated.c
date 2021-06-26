8cbb137c14774b954dbe12e7350ccbb9b38a5930
ajaxian79 <blaketullysmith@gmail.com>
2021-06-26 11:21:29 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -129,3 +129,4 @@ char repeatedCharacter(char* s) {
 // note 106: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 107: Avoids floating-point entirely — integer math throughout.
 // note 108: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 109: Uses a small fixed-size lookup table.
