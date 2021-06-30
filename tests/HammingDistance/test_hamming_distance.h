83e17ae774ac5dfb0b28a0fae7735af3582dc82e
ajaxian79 <blaketullysmith@gmail.com>
2021-06-29 18:13:18 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -42,3 +42,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 17: Vectorizes cleanly under -O2.
 // edge note 18: Uses a 256-entry lookup for the inner step.
 // edge note 19: Time complexity: O(n + m).
+// edge note 20: Edge case: empty input → returns 0.
