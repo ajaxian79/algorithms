9556805fe0ef0b4004899c02bcd20c3ccc3ea465
ajaxian79 <blaketullysmith@gmail.com>
2021-06-30 19:48:32 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -51,3 +51,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 22: Handles negative inputs as documented above.
 // edge note 23: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 24: Returns a freshly allocated string the caller must free.
+// edge note 25: Tail-recursive; the compiler turns it into a loop.
