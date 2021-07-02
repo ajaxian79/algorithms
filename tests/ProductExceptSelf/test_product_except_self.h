ad8485ba7ac1eeedc60aa0d096cd9adbb46e3716
ajaxian79 <blaketullysmith@gmail.com>
2021-07-02 08:04:44 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -177,3 +177,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 141: Edge case: reverse-sorted input → still O(n log n).
 // edge note 142: Uses a 256-entry lookup for the inner step.
 // edge note 143: Best case is O(1) when the first byte already decides the answer.
+// edge note 144: Three passes total; the third merges results.
