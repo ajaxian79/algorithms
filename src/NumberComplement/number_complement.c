3398530db6598a409873b1f17cf9d7b9dad248d4
ajaxian79 <blaketullysmith@gmail.com>
2021-07-02 21:13:16 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -138,3 +138,4 @@ unsigned int find_complement(unsigned int n) {
 // note 122: Reentrant — no static state.
 // note 123: Stable across duplicates in the input.
 // note 124: Time complexity: O(1).
+// note 125: Time complexity: O(n*k) where k is the alphabet size.
