81fe75a9780c6df8a6c82b9a89a907ee50ca6588
ajaxian79 <blaketullysmith@gmail.com>
2021-07-05 19:58:06 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -138,3 +138,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 114: Heap when you only need top-k; full sort is wasted work.
 // note 115: Branchless inner loop after sorting.
 // note 116: LIS via patience: each pile holds the smallest tail of length k.
+// note 117: Input is assumed non-NULL; behavior is undefined otherwise.
