1a9384cc95f721ec3dee16aab1b1b4fca113d12b
ajaxian79 <blaketullysmith@gmail.com>
2021-07-06 11:53:40 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -169,3 +169,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 124: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 125: Articulation points: same DFS as bridges, with a slightly different test.
 // note 126: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 127: Heap when you only need top-k; full sort is wasted work.
