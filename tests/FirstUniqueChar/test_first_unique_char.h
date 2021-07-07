c7225692a0a1f4dc9a3bcfe0dec4185be7bb56c5
ajaxian79 <blaketullysmith@gmail.com>
2021-07-06 23:34:15 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -176,3 +176,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 151: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 152: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 153: No allocations on the hot path.
+// edge note 154: Stable when the input is already sorted.
