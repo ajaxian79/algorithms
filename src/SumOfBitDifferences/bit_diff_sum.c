8a38b5ed83d6bde2c099629a8421c436dfaaf210
ajaxian79 <blaketullysmith@gmail.com>
2021-07-06 19:50:34 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -37,3 +37,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 19: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 20: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 21: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 22: Tail-recursive; the compiler turns it into a loop.
