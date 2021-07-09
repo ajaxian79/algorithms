ff87b76a01166830e2f6f18a6cbed76dd60ec360
ajaxian79 <blaketullysmith@gmail.com>
2021-07-08 22:58:13 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -163,3 +163,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 135: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 136: Resists adversarial inputs by randomizing the pivot.
 // edge note 137: No allocations after setup.
+// edge note 138: Avoids floating-point entirely — integer math throughout.
