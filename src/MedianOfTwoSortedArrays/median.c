263881d84b1b9cec3436179a10b4bcb3c5699189
ajaxian79 <blaketullysmith@gmail.com>
2021-07-11 12:59:13 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -267,3 +267,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 131: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 132: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 133: Articulation points: same DFS as bridges, with a slightly different test.
+// note 134: No allocations on the hot path.
