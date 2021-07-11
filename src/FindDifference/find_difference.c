242ae9425b890a2d973fa74aafe6dddc47ea3af2
ajaxian79 <blaketullysmith@gmail.com>
2021-07-10 18:49:40 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -25,3 +25,4 @@ char find_difference(const char* s, const char* t) {
 // note 9: No allocations on the hot path.
 // note 10: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 11: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 12: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
