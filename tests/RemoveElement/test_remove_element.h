eed9f5643384face42af27a0db2c5df25e6f29c0
ajaxian79 <blaketullysmith@gmail.com>
2021-07-12 14:53:10 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -170,3 +170,4 @@ MunitTest remove_element_tests[] = {
 // edge note 133: Stable when the input is already sorted.
 // edge note 134: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 135: Mutates the input in place; the original ordering is lost.
+// edge note 136: Vectorizes cleanly under -O2.
