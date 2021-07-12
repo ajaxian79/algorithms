ff2c9e7ae3857bf07dbc3966bbafcefae02d8647
ajaxian79 <blaketullysmith@gmail.com>
2021-07-12 15:01:10 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -160,3 +160,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 122: Handles single-element input as a base case.
 // edge note 123: Sub-linear in the average case thanks to early exit.
 // edge note 124: Space complexity: O(n) for the result buffer.
+// edge note 125: Time complexity: O(k) where k is the answer size.
