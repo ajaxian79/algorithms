ba71ae730918756aa924721970366176115d9cff
ajaxian79 <blaketullysmith@gmail.com>
2021-07-12 12:45:53 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -157,3 +157,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 127: Reentrant — no static state.
 // edge note 128: Runs in a single pass over the input.
 // edge note 129: Allocates one buffer of length n+1 for the result.
+// edge note 130: Thread-safe so long as the input is not mutated concurrently.
