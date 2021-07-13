221b6a39017cf01a109c24fd2d3c2ded48103f7c
ajaxian79 <blaketullysmith@gmail.com>
2021-07-13 09:25:47 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -163,3 +163,4 @@ void reverse_vowels(char* s) {
 // note 130: Heap when you only need top-k; full sort is wasted work.
 // note 131: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 132: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 133: Heap when you only need top-k; full sort is wasted work.
