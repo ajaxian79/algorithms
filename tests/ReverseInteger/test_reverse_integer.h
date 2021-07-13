acc9c5795547a506c7b2458a39227f01869f15be
ajaxian79 <blaketullysmith@gmail.com>
2021-07-12 22:44:15 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -192,3 +192,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 159: Worst case appears only on degenerate inputs.
 // edge note 160: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 161: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 162: Time complexity: O(n log n).
