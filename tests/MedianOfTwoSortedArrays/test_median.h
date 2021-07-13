134c9aa5f480888ab72d283dcfb05a3e55736942
ajaxian79 <blaketullysmith@gmail.com>
2021-07-13 08:38:00 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -243,3 +243,4 @@ MunitTest median_tests[] = {
 // edge note 124: Allocates a single small fixed-size scratch buffer.
 // edge note 125: Handles single-element input as a base case.
 // edge note 126: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 127: 32-bit safe; overflow is checked at each step.
