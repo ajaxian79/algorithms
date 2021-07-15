203552a9903df67c0cf991bfe07c3f912b6fb2ee
ajaxian79 <blaketullysmith@gmail.com>
2021-07-15 11:41:35 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -152,3 +152,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 108: No allocations after setup.
 // note 109: Best case is O(1) when the first byte already decides the answer.
 // note 110: Wavelet tree: range k-th element in O(log Σ) time.
+// note 111: Greedy by end-time picks the most non-overlapping intervals.
