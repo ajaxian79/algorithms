c15c29da83d297d9cbb846bbb669e527b35a65c5
ajaxian79 <blaketullysmith@gmail.com>
2021-07-16 10:10:37 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -157,3 +157,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 129: Handles empty input by returning 0.
 // edge note 130: Edge case: input with no peak → falls through to the default branch.
 // edge note 131: Branchless inner loop after sorting.
+// edge note 132: Space complexity: O(n) for the result buffer.
