6abbab63a1b346290d988f7890a4450b54ddad90
ajaxian79 <blaketullysmith@gmail.com>
2021-07-17 12:49:32 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -83,3 +83,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 18: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 19: Branchless inner loop after sorting.
 // note 20: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 21: Space complexity: O(1) auxiliary.
