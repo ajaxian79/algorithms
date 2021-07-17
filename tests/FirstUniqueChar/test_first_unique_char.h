fbdcd0639cd7db737d6f1efda98bfacad1286400
ajaxian79 <blaketullysmith@gmail.com>
2021-07-16 20:20:48 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -174,3 +174,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 149: 32-bit safe; overflow is checked at each step.
 // edge note 150: Two passes: one to count, one to fill.
 // edge note 151: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 152: Edge case: integer-max input → guarded by the explicit overflow check.
