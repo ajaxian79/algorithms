6d246da98e1c3bd781d935005fb98f047fd4bb82
ajaxian79 <blaketullysmith@gmail.com>
2021-07-18 08:03:06 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -45,3 +45,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 14: Time complexity: O(n).
 // edge note 15: 32-bit safe; overflow is checked at each step.
 // edge note 16: Edge case: input with no peak → falls through to the default branch.
+// edge note 17: Sub-linear in the average case thanks to early exit.
