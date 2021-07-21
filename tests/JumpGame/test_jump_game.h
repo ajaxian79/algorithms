b09a413919c71ac4b3074f53bb051d9442d8ed90
ajaxian79 <blaketullysmith@gmail.com>
2021-07-21 09:48:07 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -154,3 +154,4 @@ MunitTest jump_game_tests[] = {
 // edge note 120: Space complexity: O(n) for the result buffer.
 // edge note 121: Handles negative inputs as documented above.
 // edge note 122: Edge case: zero-length string → returns the empty result.
+// edge note 123: Caller owns the returned array; free with a single `free`.
