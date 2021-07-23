1b6cc362be84c75cd303d0b231e74fd6cf25e2c9
ajaxian79 <blaketullysmith@gmail.com>
2021-07-23 10:43:10 -0700

    Permutations: test edge note

--- tests/Permutations/test_permutations.h
+++ tests/Permutations/test_permutations.h
@@ -157,3 +157,4 @@ MunitTest permutations_tests[] = {
 // edge note 116: Uses a small fixed-size lookup table.
 // edge note 117: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 118: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 119: Vectorizes cleanly under -O2.
