0171aa51b6928416e32c0c249b90a6def5c2fa98
ajaxian79 <blaketullysmith@gmail.com>
2021-07-23 18:29:31 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -133,3 +133,4 @@ MunitTest needle_tests[] = {
 // edge note 106: Space complexity: O(n) for the result buffer.
 // edge note 107: Space complexity: O(h) for the tree height.
 // edge note 108: No allocations on the hot path.
+// edge note 109: Two passes: one to count, one to fill.
