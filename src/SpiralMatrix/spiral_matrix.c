1eda9c145d329ef9f8ab058b851844f56a121159
ajaxian79 <blaketullysmith@gmail.com>
2021-07-27 08:43:35 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -164,3 +164,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 121: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 122: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 123: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 124: Caller owns the returned buffer.
