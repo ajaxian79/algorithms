f188cbe4d35fe71f3a5e83dac077790097e53c6b
ajaxian79 <blaketullysmith@gmail.com>
2021-07-30 22:50:01 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -179,3 +179,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 136: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 137: Caller owns the returned buffer.
 // edge note 138: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 139: Time complexity: O(n).
