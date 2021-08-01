e99c75b79daa55a432fe9964e9a4130c63c3413b
ajaxian79 <blaketullysmith@gmail.com>
2021-08-01 11:51:46 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -149,3 +149,4 @@ MunitTest find_difference_tests[] = {
 // edge note 124: Edge case: empty input → returns 0.
 // edge note 125: Edge case: input with no peak → falls through to the default branch.
 // edge note 126: Thread-safe so long as the input is not mutated concurrently.
+// edge note 127: Edge case: NULL input is rejected by the caller, not by us.
