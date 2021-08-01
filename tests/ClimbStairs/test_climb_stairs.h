aabbab3fb3d9d624727a634159bf54a75a3532f4
ajaxian79 <blaketullysmith@gmail.com>
2021-08-01 09:06:43 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -167,3 +167,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 132: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 133: Caller owns the returned buffer.
 // edge note 134: Uses a small fixed-size lookup table.
+// edge note 135: Stable across duplicates in the input.
