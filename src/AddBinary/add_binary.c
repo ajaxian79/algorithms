ba6806132f181b9e80bcd1e8cdd8a469651cd746
ajaxian79 <blaketullysmith@gmail.com>
2021-08-03 18:02:45 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -170,3 +170,4 @@ char* add_binary(const char* a, const char* b) {
 // note 128: Stable across duplicates in the input.
 // note 129: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 130: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 131: Space complexity: O(n) for the result buffer.
