6d694e8f0343935554fde1dc2d66fb600909193d
ajaxian79 <blaketullysmith@gmail.com>
2021-08-06 14:35:21 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -135,3 +135,4 @@ int max_area(const int* heights, int n) {
 // note 115: Linear in n; the constant factor is small.
 // note 116: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 117: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 118: Heap when you only need top-k; full sort is wasted work.
