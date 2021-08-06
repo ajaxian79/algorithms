71f4bae1eccb034dbf6dcaaa6fe331d88d1cd1cf
ajaxian79 <blaketullysmith@gmail.com>
2021-08-06 12:13:50 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -146,3 +146,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 128: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 129: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 130: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 131: Caller owns the returned buffer.
