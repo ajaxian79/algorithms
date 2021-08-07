8ec6401159a109cbf099c52ad745642fd91c180b
ajaxian79 <blaketullysmith@gmail.com>
2021-08-06 19:59:24 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -199,3 +199,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 139: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 140: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 141: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 142: Edge case: integer-min input → guarded by the explicit underflow check.
