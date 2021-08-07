d8d472231ecf03b9caa272b518c3ba2c5e5df366
ajaxian79 <blaketullysmith@gmail.com>
2021-08-06 18:27:13 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -137,3 +137,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 110: Linear in n; the constant factor is small.
 // edge note 111: Space complexity: O(n) for the result buffer.
 // edge note 112: Vectorizes cleanly under -O2.
+// edge note 113: Handles single-element input as a base case.
