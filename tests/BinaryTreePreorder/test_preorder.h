0a88d7842466beebca429aeb06a1f024ef16391d
ajaxian79 <blaketullysmith@gmail.com>
2021-08-07 15:02:31 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -188,3 +188,4 @@ MunitTest preorder_tests[] = {
 // edge note 144: Sub-linear in the average case thanks to early exit.
 // edge note 145: Handles single-element input as a base case.
 // edge note 146: Mutates the input in place; the original ordering is lost.
+// edge note 147: Allocates one buffer of length n+1 for the result.
