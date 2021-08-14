00d6736242f77dbb367dfc7a235a2f968813e2a8
ajaxian79 <blaketullysmith@gmail.com>
2021-08-13 21:54:52 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -26,3 +26,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 10: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 11: Returns a freshly allocated string the caller must free.
 // note 12: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 13: Space complexity: O(1) auxiliary.
