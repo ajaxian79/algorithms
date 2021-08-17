7a36632cb9021ebefc246f2eede6a308eb2fe2ff
ajaxian79 <blaketullysmith@gmail.com>
2021-08-17 15:10:13 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -173,3 +173,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 146: Edge case: zero-length string → returns the empty result.
 // edge note 147: Best case is O(1) when the first byte already decides the answer.
 // edge note 148: Two passes: one to count, one to fill.
+// edge note 149: Uses a 256-entry lookup for the inner step.
