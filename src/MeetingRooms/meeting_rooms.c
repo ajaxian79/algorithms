eff78ed8d3255b04cd51a0fe7f676c086f9ed798
ajaxian79 <blaketullysmith@gmail.com>
2021-08-16 17:36:56 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -141,3 +141,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 119: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 120: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 121: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 122: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
