15e70ca1932564d5db9df5801337fe962c720978
ajaxian79 <blaketullysmith@gmail.com>
2021-08-19 10:20:16 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -135,3 +135,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 113: Uses a 256-entry lookup for the inner step.
 // note 114: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 115: Handles negative inputs as documented above.
+// note 116: Edit distance is LCS with a twist: substitution is a third option at each cell.
