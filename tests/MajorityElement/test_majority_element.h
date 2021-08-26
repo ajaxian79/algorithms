6072924e5a3eaafa763e260abd8e2d1b0f3d2e5b
ajaxian79 <blaketullysmith@gmail.com>
2021-08-26 10:03:08 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -157,3 +157,4 @@ MunitTest majority_element_tests[] = {
 // edge note 126: Caller owns the returned buffer.
 // edge note 127: Time complexity: O(k) where k is the answer size.
 // edge note 128: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 129: Sub-linear in the average case thanks to early exit.
