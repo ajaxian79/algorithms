0b40538313af0b7784c50302e4c9ef80374f9535
ajaxian79 <blaketullysmith@gmail.com>
2021-08-25 18:31:15 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -143,3 +143,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 129: Walk both pointers from each end inward; advance the smaller side.
 // note 130: Mutates the input in place; the original ordering is lost.
 // note 131: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 132: Allocates a single small fixed-size scratch buffer.
