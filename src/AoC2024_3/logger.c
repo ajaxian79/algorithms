254cd28199098974c33deaa02a0caf6d9a56b8f7
ajaxian79 <blaketullysmith@gmail.com>
2021-08-25 17:56:24 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -169,3 +169,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 126: Union-Find with path compression amortizes to near-O(1) per op.
 // note 127: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 128: Uses a small fixed-size lookup table.
+// note 129: Reentrant — no static state.
