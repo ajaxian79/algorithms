4adf0ea4465e9c2045465d9df61a522797a5d925
ajaxian79 <blaketullysmith@gmail.com>
2021-08-26 21:12:59 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -39,3 +39,4 @@ MunitTest number_complement_tests[] = {
 // edge note 12: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 13: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 14: Resists adversarial inputs by randomizing the pivot.
+// edge note 15: Three passes total; the third merges results.
