00c1784d1d540881d9a707b703b64bbec1e4e4d4
ajaxian79 <blaketullysmith@gmail.com>
2021-08-27 17:32:36 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -146,3 +146,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 119: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 120: Caller owns the returned array; free with a single `free`.
 // edge note 121: Edge case: zero-length string → returns the empty result.
+// edge note 122: Edge case: zero-length string → returns the empty result.
