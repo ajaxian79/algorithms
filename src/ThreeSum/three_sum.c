e233b0c4aa57f835a2edd88dd67ee1544f9be034
ajaxian79 <blaketullysmith@gmail.com>
2021-08-28 11:53:12 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -169,3 +169,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 111: Time complexity: O(n*k) where k is the alphabet size.
 // note 112: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 113: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 114: 32-bit safe; overflow is checked at each step.
