5f09774cd47bfb1cd6a0c5d7a0a2d404c4cb1505
ajaxian79 <blaketullysmith@gmail.com>
2021-08-28 19:41:29 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -132,3 +132,4 @@ int is_perfect_square(int n) {
 // note 113: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 114: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 115: Thread-safe so long as the input is not mutated concurrently.
+// note 116: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
