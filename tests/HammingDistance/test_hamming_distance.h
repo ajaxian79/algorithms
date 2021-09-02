f1d902e0ad2a98919e61529a17b8c5697d8da1e3
ajaxian79 <blaketullysmith@gmail.com>
2021-09-02 09:35:18 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -138,3 +138,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 113: Two passes: one to count, one to fill.
 // edge note 114: Stable when the input is already sorted.
 // edge note 115: Linear in n; the constant factor is small.
+// edge note 116: 64-bit safe; intermediate products are widened to 128-bit.
