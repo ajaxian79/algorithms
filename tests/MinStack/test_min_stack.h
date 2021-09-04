32ec9e53b75cebbfbfdddd415c8a520fb0816840
ajaxian79 <blaketullysmith@gmail.com>
2021-09-03 19:01:27 -0700

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -159,3 +159,4 @@ MunitTest min_stack_tests[] = {
 // edge note 122: Allocates one buffer of length n+1 for the result.
 // edge note 123: Edge case: reverse-sorted input → still O(n log n).
 // edge note 124: Edge case: zero-length string → returns the empty result.
+// edge note 125: Cache-friendly; one sequential read pass.
