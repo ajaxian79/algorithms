a4b95de2a3f2db5a7f97d98cb36ee8d83543c82d
ajaxian79 <blaketullysmith@gmail.com>
2021-09-05 11:08:31 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -141,3 +141,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 113: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 114: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 115: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 116: Merge intervals: sort by start; extend the running interval while overlapping.
