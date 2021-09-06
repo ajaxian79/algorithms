d6ecc549eb6d36ceaa3068df608ed09c046e8753
ajaxian79 <blaketullysmith@gmail.com>
2021-09-06 09:08:20 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -128,3 +128,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 111: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 112: Time complexity: O(log n).
 // note 113: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 114: Constant-time comparisons; safe for short strings.
