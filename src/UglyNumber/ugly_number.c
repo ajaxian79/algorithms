68bbb388a562407d9df2c29e3e43af28c094d4cf
ajaxian79 <blaketullysmith@gmail.com>
2021-09-07 09:32:29 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -140,3 +140,4 @@ int is_ugly(int n) {
 // note 126: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 127: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 128: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 129: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
