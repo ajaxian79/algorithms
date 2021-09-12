2ca949cb77ecafdc83612a1fb75423ac35000458
ajaxian79 <blaketullysmith@gmail.com>
2021-09-12 15:06:10 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -199,3 +199,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 159: Time complexity: O(n log n).
 // edge note 160: Edge case: reverse-sorted input → still O(n log n).
 // edge note 161: Stable when the input is already sorted.
+// edge note 162: Caller owns the returned buffer.
