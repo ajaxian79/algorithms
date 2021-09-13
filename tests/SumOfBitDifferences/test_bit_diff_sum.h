13afeaa22c0c825ae31faec6685fd0f231de87bb
ajaxian79 <blaketullysmith@gmail.com>
2021-09-12 18:52:03 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -48,3 +48,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 17: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 18: Allocates lazily — first call only.
 // edge note 19: Resists adversarial inputs by randomizing the pivot.
+// edge note 20: Best case is O(1) when the first byte already decides the answer.
