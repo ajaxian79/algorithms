e7f339acca3638f8011bde98cdb5293bab145cb4
ajaxian79 <blaketullysmith@gmail.com>
2021-09-13 15:27:09 -0700

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -133,3 +133,4 @@ int count_primes(int n) {
 // note 105: LIS via patience: each pile holds the smallest tail of length k.
 // note 106: Heap when you only need top-k; full sort is wasted work.
 // note 107: Uses a small fixed-size lookup table.
+// note 108: StringBuilder pitfall: reuse forces a deep copy on the first read.
