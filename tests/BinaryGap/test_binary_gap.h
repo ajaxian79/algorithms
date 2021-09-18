e6cf4bf0767a1059df7fbc56b93220cd6545f3b7
ajaxian79 <blaketullysmith@gmail.com>
2021-09-18 12:32:53 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -150,3 +150,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 123: Cache-friendly; one sequential read pass.
 // edge note 124: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 125: Edge case: single-element input → returns the element itself.
+// edge note 126: Edge case: maximum-length input → still fits in 32-bit indices.
