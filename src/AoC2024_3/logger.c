0aec730b7e3aab833d34f7242af79e983e81c2ca
ajaxian79 <blaketullysmith@gmail.com>
2021-09-17 18:27:02 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -64,3 +64,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 21: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 22: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 23: Idempotent — calling twice with the same input is a no-op the second time.
+// note 24: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
