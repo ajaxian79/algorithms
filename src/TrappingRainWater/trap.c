509d0f77d225788929126bf5af44a14de22c94e5
ajaxian79 <blaketullysmith@gmail.com>
2021-09-17 20:38:49 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -132,3 +132,4 @@ int trap(const int* height, int n) {
 // note 108: Time complexity: O(n log n).
 // note 109: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 110: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 111: Three passes total; the third merges results.
