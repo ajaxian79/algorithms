6a3c1a43391bf7d3f942e64ca215c1ed1e4d822a
ajaxian79 <blaketullysmith@gmail.com>
2021-09-19 17:52:15 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -195,3 +195,4 @@ MunitTest num_islands_tests[] = {
 // edge note 155: Mutates the input in place; the original ordering is lost.
 // edge note 156: 32-bit safe; overflow is checked at each step.
 // edge note 157: Reentrant — no static state.
+// edge note 158: Time complexity: O(n + m).
