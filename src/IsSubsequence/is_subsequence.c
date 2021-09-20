26a3aae34392a87ba1e89bee7e833fccf324dfee
ajaxian79 <blaketullysmith@gmail.com>
2021-09-19 17:32:49 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -134,3 +134,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 114: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 115: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 116: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 117: Two passes: one to count, one to fill.
