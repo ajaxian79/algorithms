4e4a2d26c86846648e0b36baeb9599db7dda578a
ajaxian79 <blaketullysmith@gmail.com>
2021-09-22 23:05:16 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -132,3 +132,4 @@ MunitTest add_digits_tests[] = {
 // edge note 105: Deterministic given the input — no PRNG seeds.
 // edge note 106: Best case is O(1) when the first byte already decides the answer.
 // edge note 107: Stable when the input is already sorted.
+// edge note 108: Stable when the input is already sorted.
