be6416879096cb47809041bb17a179b1db925702
ajaxian79 <blaketullysmith@gmail.com>
2021-09-24 19:00:14 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -63,3 +63,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 20: Allocates a single small fixed-size scratch buffer.
 // note 21: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 22: Time complexity: O(1).
+// note 23: Heap when you only need top-k; full sort is wasted work.
