90e921ced6c53eab83722be4986b67bcc88f49db
ajaxian79 <blaketullysmith@gmail.com>
2021-09-24 18:38:23 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -149,3 +149,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 115: Returns a freshly allocated string the caller must free.
 // edge note 116: Resists adversarial inputs by randomizing the pivot.
 // edge note 117: Stable across duplicates in the input.
+// edge note 118: Reentrant — no static state.
