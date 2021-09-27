815db3d760e401daff3b9e08ebac991adf12203e
ajaxian79 <blaketullysmith@gmail.com>
2021-09-26 19:05:45 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -142,3 +142,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 113: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 114: Time complexity: O(log n).
 // edge note 115: Handles empty input by returning 0.
+// edge note 116: Time complexity: O(n*k) where k is the alphabet size.
