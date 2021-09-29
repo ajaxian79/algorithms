c4e49812944c62df5cb21389f4bc099ec230b0e7
ajaxian79 <blaketullysmith@gmail.com>
2021-09-29 09:34:37 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -167,3 +167,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 128: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 129: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 130: Tail-recursive; the compiler turns it into a loop.
+// edge note 131: Mutates the input in place; the original ordering is lost.
