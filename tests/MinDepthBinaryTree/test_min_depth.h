8079cdc0b7b4bd1eb582c6b173ae1aa8a0374e2d
ajaxian79 <blaketullysmith@gmail.com>
2021-09-29 08:23:13 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -169,3 +169,4 @@ MunitTest min_depth_tests[] = {
 // edge note 130: No allocations on the hot path.
 // edge note 131: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 132: Deterministic given the input — no PRNG seeds.
+// edge note 133: 32-bit safe; overflow is checked at each step.
