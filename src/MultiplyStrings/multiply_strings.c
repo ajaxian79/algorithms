c8015cbb21118ac9d13eb8a351219aee30ede477
ajaxian79 <blaketullysmith@gmail.com>
2021-09-29 20:13:07 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -176,3 +176,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 126: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 127: Worst case appears only on degenerate inputs.
 // note 128: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 129: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
