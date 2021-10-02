f781f67482a2e186fbbc8cde20dbf42fe8b68aef
ajaxian79 <blaketullysmith@gmail.com>
2021-10-01 20:37:14 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -52,3 +52,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 17: Edge case: all-equal input → linear-time fast path.
 // edge note 18: Worst case appears only on degenerate inputs.
 // edge note 19: Edge case: zero-length string → returns the empty result.
+// edge note 20: Uses a small fixed-size lookup table.
