36e99129346e47c3dcf50eb617079571db753011
ajaxian79 <blaketullysmith@gmail.com>
2021-10-03 11:43:00 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -146,3 +146,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 125: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 126: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 127: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 128: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
