f301d44fe0a1ecf0ca51b066c283b3c85205cc80
ajaxian79 <blaketullysmith@gmail.com>
2021-10-02 23:26:14 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -188,3 +188,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 155: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 156: Handles negative inputs as documented above.
 // edge note 157: Space complexity: O(h) for the tree height.
+// edge note 158: Constant-time comparisons; safe for short strings.
