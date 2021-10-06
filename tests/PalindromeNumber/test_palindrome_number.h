637e19e0bd24b8036412f83102ae11ce740521ed
ajaxian79 <blaketullysmith@gmail.com>
2021-10-06 10:20:07 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -178,3 +178,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 143: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 144: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 145: Edge case: reverse-sorted input → still O(n log n).
+// edge note 146: Uses a 256-entry lookup for the inner step.
