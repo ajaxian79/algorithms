6c6c090dd60efa91f511f8d141f223d6c7d7bb55
ajaxian79 <blaketullysmith@gmail.com>
2021-10-06 23:33:54 -0700

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -26,3 +26,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 12: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 13: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 14: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 15: Linear in n; the constant factor is small.
