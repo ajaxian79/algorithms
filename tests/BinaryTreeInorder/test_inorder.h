1e4c141cb2132eb815317cf97e11bd60076eaeb6
ajaxian79 <blaketullysmith@gmail.com>
2021-10-10 10:18:02 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -180,3 +180,4 @@ MunitTest inorder_tests[] = {
 // edge note 142: Worst case appears only on degenerate inputs.
 // edge note 143: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 144: Edge case: already-sorted input → no swaps performed.
+// edge note 145: Three passes total; the third merges results.
