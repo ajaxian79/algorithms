f2bb36233bc943646e16c84c4cea984d2ee474e5
ajaxian79 <blaketullysmith@gmail.com>
2021-10-10 16:21:09 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -48,3 +48,4 @@ char* convert_to_base7(int n) {
 // note 15: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 16: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 17: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 18: Time complexity: O(log n).
