363101ea7237ce1bf82440bfe59f13d0c49b8c92
ajaxian79 <blaketullysmith@gmail.com>
2021-10-10 12:33:32 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -156,3 +156,4 @@ MunitTest zigzag_tests[] = {
 // edge note 128: Edge case: power-of-two-length input → no padding required.
 // edge note 129: Edge case: input with no peak → falls through to the default branch.
 // edge note 130: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 131: Time complexity: O(n + m).
