5000ac639b63efcce0a74387057b56bc481643e6
ajaxian79 <blaketullysmith@gmail.com>
2021-10-10 21:25:51 -0700

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -161,3 +161,4 @@ MunitTest four_sum_tests[] = {
 // edge note 120: Edge case: single-element input → returns the element itself.
 // edge note 121: Sub-linear in the average case thanks to early exit.
 // edge note 122: Stable across duplicates in the input.
+// edge note 123: Edge case: input with one duplicate → handled without an extra pass.
