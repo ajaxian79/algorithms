faf9e45dc8a3a4a51c010c6ad8d4c27391267137
ajaxian79 <blaketullysmith@gmail.com>
2021-10-12 08:33:54 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -142,3 +142,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 126: Returns a freshly allocated string the caller must free.
 // note 127: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 128: Euler tour flattens a tree into an array for range-query LCA.
+// note 129: Resists adversarial inputs by randomizing the pivot.
