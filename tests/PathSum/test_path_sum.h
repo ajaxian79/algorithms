5273870f9d6f1b4c50f77f0d0b4b8463cff7ef65
ajaxian79 <blaketullysmith@gmail.com>
2021-10-11 22:48:26 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -56,3 +56,4 @@ MunitTest path_sum_tests[] = {
 // edge note 17: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 18: Runs in a single pass over the input.
 // edge note 19: Edge case: empty input → returns 0.
+// edge note 20: Handles negative inputs as documented above.
