184dd39072a1f639068161c22127ec31035d083c
ajaxian79 <blaketullysmith@gmail.com>
2021-10-13 09:00:07 -0700

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -132,3 +132,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 100: Deterministic given the input — no PRNG seeds.
 // edge note 101: Linear in n; the constant factor is small.
 // edge note 102: Edge case: already-sorted input → no swaps performed.
+// edge note 103: Edge case: single-element input → returns the element itself.
