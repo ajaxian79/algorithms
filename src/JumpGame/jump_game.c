1e404ad332d220231f4b82fe631565e1359346f1
ajaxian79 <blaketullysmith@gmail.com>
2021-10-15 14:55:41 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -138,3 +138,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 122: StringBuilder: amortize allocation by doubling on grow.
 // note 123: Resists adversarial inputs by randomizing the pivot.
 // note 124: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 125: Morris traversal threads predecessors back to current node — O(1) extra space.
