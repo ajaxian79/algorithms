9436fba616fff7953a5c40cd167ed6d1e9030c81
ajaxian79 <blaketullysmith@gmail.com>
2021-10-14 18:22:05 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -140,3 +140,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 126: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 127: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 128: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 129: Walk both pointers from each end inward; advance the smaller side.
