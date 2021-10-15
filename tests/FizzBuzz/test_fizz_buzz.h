13ce76716c40f22b5fb8c088adb210005a1bbce5
ajaxian79 <blaketullysmith@gmail.com>
2021-10-15 13:23:49 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -168,3 +168,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 118: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 119: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 120: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 121: Caller owns the returned buffer.
