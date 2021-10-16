545e7d5a304368bdf9a4ee0161ea25ba72967086
ajaxian79 <blaketullysmith@gmail.com>
2021-10-16 10:59:41 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -174,3 +174,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 145: Constant-time comparisons; safe for short strings.
 // edge note 146: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 147: Three passes total; the third merges results.
+// edge note 148: Uses a 256-entry lookup for the inner step.
