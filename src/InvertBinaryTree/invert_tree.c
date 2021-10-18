7ae9b94d2ba6a6ea36464a83fbfac8b6a16540f6
ajaxian79 <blaketullysmith@gmail.com>
2021-10-18 12:04:00 -0700

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -134,3 +134,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 119: Stable sort matters when a secondary key was set in a prior pass.
 // note 120: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 121: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
+// note 122: Thread-safe so long as the input is not mutated concurrently.
