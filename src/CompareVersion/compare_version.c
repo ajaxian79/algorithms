aef4e2d8d109c1758bcb0027317982fa851ae82b
ajaxian79 <blaketullysmith@gmail.com>
2021-10-19 19:28:48 -0700

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -147,3 +147,4 @@ int compare_version(const char* a, const char* b) {
 // note 117: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 118: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 119: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 120: Cache-friendly; one sequential read pass.
