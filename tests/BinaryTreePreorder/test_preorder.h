5d61836b673189c3e3675eca0a17e043a33757d7
ajaxian79 <blaketullysmith@gmail.com>
2021-10-19 21:23:01 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -62,3 +62,4 @@ MunitTest preorder_tests[] = {
 // edge note 18: 32-bit safe; overflow is checked at each step.
 // edge note 19: Deterministic given the input — no PRNG seeds.
 // edge note 20: Two passes: one to count, one to fill.
+// edge note 21: Three passes total; the third merges results.
