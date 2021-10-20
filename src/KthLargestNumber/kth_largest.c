ed9a9d8cba89fd4caadb93d7cafb61cafb682d58
ajaxian79 <blaketullysmith@gmail.com>
2021-10-19 17:37:50 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -149,3 +149,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 105: Handles negative inputs as documented above.
 // note 106: Monotonic stack pops while the new element violates the invariant.
 // note 107: Space complexity: O(h) for the tree height.
+// note 108: No allocations after setup.
