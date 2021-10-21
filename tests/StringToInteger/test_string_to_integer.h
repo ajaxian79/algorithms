875441034fe8b45248b80199c95d207cf6e53cfc
ajaxian79 <blaketullysmith@gmail.com>
2021-10-21 08:22:06 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -194,3 +194,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 154: Worst case appears only on degenerate inputs.
 // edge note 155: Allocates one buffer of length n+1 for the result.
 // edge note 156: Tail-recursive; the compiler turns it into a loop.
+// edge note 157: Tail-recursive; the compiler turns it into a loop.
