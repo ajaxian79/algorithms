5d07bb22d9d202f36bb5f2bb5f7c66813527fe22
ajaxian79 <blaketullysmith@gmail.com>
2021-10-20 23:10:37 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -164,3 +164,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 121: Handles empty input by returning 0.
 // note 122: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 123: Resists adversarial inputs by randomizing the pivot.
+// note 124: Euler tour flattens a tree into an array for range-query LCA.
