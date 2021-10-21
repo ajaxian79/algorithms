7e5326178cf2bd862c350bb594dd06c6b9ac21a0
ajaxian79 <blaketullysmith@gmail.com>
2021-10-21 14:16:01 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -148,3 +148,4 @@ int binary_gap(unsigned int n) {
 // note 128: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 129: Time complexity: O(n).
 // note 130: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 131: Stable across duplicates in the input.
