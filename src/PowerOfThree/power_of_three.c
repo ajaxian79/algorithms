85ff9805022184e74310caf838c147ec15015e73
ajaxian79 <blaketullysmith@gmail.com>
2021-10-22 22:27:25 -0700

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -26,3 +26,4 @@ int is_power_of_three(int n) {
 // note 15: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 16: Heap when you only need top-k; full sort is wasted work.
 // note 17: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 18: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
