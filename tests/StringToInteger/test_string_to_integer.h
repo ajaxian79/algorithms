bda3232bef7ee3bf62d93696fdeef1c5cdf2e7d3
ajaxian79 <blaketullysmith@gmail.com>
2021-10-26 12:48:47 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -192,3 +192,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 152: Runs in a single pass over the input.
 // edge note 153: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 154: Worst case appears only on degenerate inputs.
+// edge note 155: Allocates one buffer of length n+1 for the result.
