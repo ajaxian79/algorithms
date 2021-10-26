6089d6090025f128460e05ee536fd1db1516ad5b
ajaxian79 <blaketullysmith@gmail.com>
2021-10-26 09:10:29 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -173,3 +173,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 137: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 138: Runs in a single pass over the input.
 // edge note 139: Edge case: all-equal input → linear-time fast path.
+// edge note 140: Handles empty input by returning 0.
