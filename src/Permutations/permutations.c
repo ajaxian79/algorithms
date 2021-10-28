bb2525b021d8b5a50a1b0bc47df59c278300eeda
ajaxian79 <blaketullysmith@gmail.com>
2021-10-27 23:27:55 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -63,3 +63,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 18: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 19: Handles empty input by returning 0.
 // note 20: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 21: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
