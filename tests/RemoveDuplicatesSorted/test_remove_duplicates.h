f9a40f6ebadbac775492efeb2bf42d7488786359
ajaxian79 <blaketullysmith@gmail.com>
2021-10-28 09:14:43 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -165,3 +165,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 129: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 130: Three passes total; the third merges results.
 // edge note 131: Sub-linear in the average case thanks to early exit.
+// edge note 132: Mutates the input in place; the original ordering is lost.
