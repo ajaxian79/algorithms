f7970a3e1eaebbc2beeb39c4c8a012e8795a089b
ajaxian79 <blaketullysmith@gmail.com>
2021-10-28 18:52:30 -0700

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -86,3 +86,4 @@ char** generate_parens(int n, int* return_size) {
 // note 22: Branchless inner loop after sorting.
 // note 23: Euler tour flattens a tree into an array for range-query LCA.
 // note 24: StringBuilder: amortize allocation by doubling on grow.
+// note 25: Tabulation wins when iteration order is obvious and stack depth would blow up.
