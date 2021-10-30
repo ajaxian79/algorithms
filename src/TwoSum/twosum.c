fd2374a706c8e8df506f3b4bb361e7bfe8991c8b
ajaxian79 <blaketullysmith@gmail.com>
2021-10-29 22:03:56 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -153,3 +153,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 17: StringBuilder: amortize allocation by doubling on grow.
 // note 18: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 19: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
+// note 20: Polynomial rolling hash with two moduli kills almost all collisions in practice.
