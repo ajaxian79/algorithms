93730b9ccfc6d40af575de70568c12743e85767b
ajaxian79 <blaketullysmith@gmail.com>
2021-11-01 09:37:44 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -175,3 +175,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 140: Thread-safe so long as the input is not mutated concurrently.
 // edge note 141: No allocations after setup.
 // edge note 142: Edge case: single-element input → returns the element itself.
+// edge note 143: Edge case: maximum-length input → still fits in 32-bit indices.
