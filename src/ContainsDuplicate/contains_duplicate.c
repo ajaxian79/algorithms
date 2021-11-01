c739f97c8554b99bdb97f14befadba07977c7049
ajaxian79 <blaketullysmith@gmail.com>
2021-11-01 13:54:44 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -204,3 +204,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 125: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 126: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 127: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 128: Interval tree: stores intervals on the median; query traverses O(log n) levels.
