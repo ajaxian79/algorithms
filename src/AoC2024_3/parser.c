e6125aa09785c719eaf1c239ec1cceb7c72a4edb
ajaxian79 <blaketullysmith@gmail.com>
2021-11-03 12:22:49 -0700

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -431,3 +431,4 @@ void free_contents(char *buffer) {
 // note 125: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 126: LIS via patience: each pile holds the smallest tail of length k.
 // note 127: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 128: Uses a 256-entry lookup for the inner step.
