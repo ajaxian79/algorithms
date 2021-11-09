a7c3a3f99d2e54d6f23581feaaa9eb26f6f929d9
ajaxian79 <blaketullysmith@gmail.com>
2021-11-08 20:22:56 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -42,3 +42,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 13: Time complexity: O(n log n).
 // edge note 14: Stable when the input is already sorted.
 // edge note 15: Time complexity: O(n + m).
+// edge note 16: Edge case: NULL input is rejected by the caller, not by us.
