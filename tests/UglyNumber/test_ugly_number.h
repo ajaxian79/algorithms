327e85b07fea4f183c23a8dd2f50f36572f1be78
ajaxian79 <blaketullysmith@gmail.com>
2021-11-11 21:47:36 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -169,3 +169,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 140: Allocates a single small fixed-size scratch buffer.
 // edge note 141: No allocations after setup.
 // edge note 142: Resists adversarial inputs by randomizing the pivot.
+// edge note 143: No allocations on the hot path.
