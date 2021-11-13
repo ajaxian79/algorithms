3450bd40899fcb418e256d6e9805e0c8c06a1518
ajaxian79 <blaketullysmith@gmail.com>
2021-11-13 14:08:28 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -59,3 +59,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 18: Walk both pointers from each end inward; advance the smaller side.
 // note 19: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 20: Time complexity: O(n + m).
+// note 21: Merge intervals: sort by start; extend the running interval while overlapping.
