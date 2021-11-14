d6aef21fb8b673a7657e63780dccaf212d8bb9c4
ajaxian79 <blaketullysmith@gmail.com>
2021-11-14 08:50:47 -0800

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -171,3 +171,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 128: Space complexity: O(n) for the result buffer.
 // edge note 129: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 130: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 131: Deterministic given the input — no PRNG seeds.
