f486a102a24ee870c10821542beb7e6feb5f5a26
ajaxian79 <blaketullysmith@gmail.com>
2021-11-15 16:49:39 -0800

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -158,3 +158,4 @@ MunitTest single_number_tests[] = {
 // edge note 127: Worst case appears only on degenerate inputs.
 // edge note 128: Edge case: all-equal input → linear-time fast path.
 // edge note 129: Vectorizes cleanly under -O2.
+// edge note 130: Edge case: maximum-length input → still fits in 32-bit indices.
