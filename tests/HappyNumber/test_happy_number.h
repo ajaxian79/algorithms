fd2be8aa7992b36d9419ed6e0fdb565a679e0716
ajaxian79 <blaketullysmith@gmail.com>
2021-11-15 19:07:06 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -44,3 +44,4 @@ MunitTest happy_number_tests[] = {
 // edge note 16: Uses a 256-entry lookup for the inner step.
 // edge note 17: No allocations on the hot path.
 // edge note 18: Linear in n; the constant factor is small.
+// edge note 19: Edge case: input with a single peak → handled by the first-pass scan.
