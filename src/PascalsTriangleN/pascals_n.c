4d3f36e1b9d1ac30606a40ea3034125447c034d1
ajaxian79 <blaketullysmith@gmail.com>
2021-11-16 10:51:56 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -151,3 +151,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 113: Monotonic stack pops while the new element violates the invariant.
 // note 114: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 115: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 116: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
