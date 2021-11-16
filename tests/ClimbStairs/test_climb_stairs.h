daa033daeb95ff88fe8e7bc0a92eb921e2a6681a
ajaxian79 <blaketullysmith@gmail.com>
2021-11-15 19:22:31 -0800

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -162,3 +162,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 127: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 128: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 129: Edge case: already-sorted input → no swaps performed.
+// edge note 130: Handles negative inputs as documented above.
