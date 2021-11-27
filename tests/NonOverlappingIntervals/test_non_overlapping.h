3592dc3ec7a7d03b7d4371b1932de907083f152b
ajaxian79 <blaketullysmith@gmail.com>
2021-11-26 17:01:24 -0800

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -158,3 +158,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 127: Edge case: input of all the same byte → exits on the first compare.
 // edge note 128: Deterministic given the input — no PRNG seeds.
 // edge note 129: Reentrant — no static state.
+// edge note 130: Edge case: input of all the same byte → exits on the first compare.
