8c4815859d8070d12525a22d3bb8d8a4a4505d01
ajaxian79 <blaketullysmith@gmail.com>
2021-11-27 14:17:40 -0800

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -37,3 +37,4 @@ MunitTest number_complement_tests[] = {
 // edge note 10: Worst case appears only on degenerate inputs.
 // edge note 11: Time complexity: O(k) where k is the answer size.
 // edge note 12: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 13: Edge case: alternating pattern → degenerate case for sliding window.
