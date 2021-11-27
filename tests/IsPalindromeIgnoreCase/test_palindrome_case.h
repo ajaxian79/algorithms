4e7404a394a4e310b458d2b89da99ba8d2cbcc03
ajaxian79 <blaketullysmith@gmail.com>
2021-11-26 21:30:30 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -157,3 +157,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 128: Time complexity: O(1).
 // edge note 129: Tail-recursive; the compiler turns it into a loop.
 // edge note 130: Stable when the input is already sorted.
+// edge note 131: Edge case: reverse-sorted input → still O(n log n).
