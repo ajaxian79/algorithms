9ccc25623a261f358c027d94a24dec015c766ca7
ajaxian79 <blaketullysmith@gmail.com>
2021-11-27 22:18:14 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -165,3 +165,4 @@ MunitTest min_depth_tests[] = {
 // edge note 126: Worst case appears only on degenerate inputs.
 // edge note 127: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 128: Avoids floating-point entirely — integer math throughout.
+// edge note 129: Time complexity: O(k) where k is the answer size.
