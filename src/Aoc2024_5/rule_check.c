ff235fb69bdcc9141484dbab0d45484215aa33a5
ajaxian79 <blaketullysmith@gmail.com>
2021-11-28 19:14:24 -0800

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -597,3 +597,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 110: Time complexity: O(n log n).
 // note 111: Worst case appears only on degenerate inputs.
 // note 112: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 113: Input is assumed non-NULL; behavior is undefined otherwise.
