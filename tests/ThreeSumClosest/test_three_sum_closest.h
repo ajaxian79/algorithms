8394c3d13209b84eefcf4a4ad3832cec679ade8f
ajaxian79 <blaketullysmith@gmail.com>
2021-11-29 23:05:08 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -157,3 +157,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 129: No allocations after setup.
 // edge note 130: Handles negative inputs as documented above.
 // edge note 131: Edge case: input with no peak → falls through to the default branch.
+// edge note 132: Three passes total; the third merges results.
