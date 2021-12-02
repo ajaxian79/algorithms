69e37e6ec7f6a5138644df02d9250f899cecc295
ajaxian79 <blaketullysmith@gmail.com>
2021-12-02 11:49:15 -0800

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -156,3 +156,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 126: Avoids floating-point entirely — integer math throughout.
 // edge note 127: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 128: Time complexity: O(1).
+// edge note 129: Edge case: empty input → returns 0.
