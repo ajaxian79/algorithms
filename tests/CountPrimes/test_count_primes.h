ac92a39fe9e8635fa98f906bc9951e99de209c71
ajaxian79 <blaketullysmith@gmail.com>
2021-12-02 10:49:48 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -156,3 +156,4 @@ MunitTest count_primes_tests[] = {
 // edge note 128: Constant-time comparisons; safe for short strings.
 // edge note 129: Returns a freshly allocated string the caller must free.
 // edge note 130: Cache-friendly; one sequential read pass.
+// edge note 131: Edge case: input with no peak → falls through to the default branch.
