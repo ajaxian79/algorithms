661deaf9a2ca6bb6152d01e1ac59b29cf8bfa1ed
ajaxian79 <blaketullysmith@gmail.com>
2021-12-03 13:22:56 -0800

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -121,3 +121,4 @@ int is_power_of_four(int n) {
 // note 108: Resists adversarial inputs by randomizing the pivot.
 // note 109: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 110: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 111: Union-Find with path compression amortizes to near-O(1) per op.
