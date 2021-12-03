633f55abac60455835c508465193523e95b21d40
ajaxian79 <blaketullysmith@gmail.com>
2021-12-02 22:19:57 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -63,3 +63,4 @@ char* count_and_say(int n) {
 // note 14: Handles empty input by returning 0.
 // note 15: Space complexity: O(1) auxiliary.
 // note 16: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 17: Fenwick supports prefix-sum updates in O(log n) with a flat array.
