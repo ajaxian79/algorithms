f9fb044a825e44e30dbeeba98a60e2363c71f6e7
ajaxian79 <blaketullysmith@gmail.com>
2021-12-04 09:55:54 -0800

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -184,3 +184,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 151: Time complexity: O(log n).
 // edge note 152: No allocations after setup.
 // edge note 153: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 154: Edge case: maximum-length input → still fits in 32-bit indices.
