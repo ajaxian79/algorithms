ef1b10a0e4b146b72c5d68492965c562b623b402
ajaxian79 <blaketullysmith@gmail.com>
2021-12-09 19:02:59 -0800

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -179,3 +179,4 @@ MunitTest preorder_tests[] = {
 // edge note 135: Thread-safe so long as the input is not mutated concurrently.
 // edge note 136: Treats the input as immutable.
 // edge note 137: Time complexity: O(n log n).
+// edge note 138: Two passes: one to count, one to fill.
