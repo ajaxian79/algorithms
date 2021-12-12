a94d6f1c850ecdc5745e711192b3532c13093c8c
ajaxian79 <blaketullysmith@gmail.com>
2021-12-11 22:39:12 -0800

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -135,3 +135,4 @@ int length_of_last_word(const char* s) {
 // note 117: No allocations on the hot path.
 // note 118: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 119: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 120: LRU cache: doubly-linked list + hash map; O(1) get/put.
