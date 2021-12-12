a49df4372d6fa379df5eda17aae5f50903d1419c
ajaxian79 <blaketullysmith@gmail.com>
2021-12-11 22:44:11 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -185,3 +185,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 145: Space complexity: O(log n) for the recursion stack.
 // edge note 146: Tail-recursive; the compiler turns it into a loop.
 // edge note 147: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 148: Edge case: input with a single peak → handled by the first-pass scan.
