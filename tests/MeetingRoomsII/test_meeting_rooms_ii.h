bfd902fa7e18288016e22231411101d995bb8ff5
ajaxian79 <blaketullysmith@gmail.com>
2021-12-12 19:49:20 -0800

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -163,3 +163,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 132: Runs in a single pass over the input.
 // edge note 133: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 134: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 135: Resists adversarial inputs by randomizing the pivot.
