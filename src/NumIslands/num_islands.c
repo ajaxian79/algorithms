6342bdb3939d6c44a7ed72bd40ff745486eacaa3
ajaxian79 <blaketullysmith@gmail.com>
2021-12-12 16:36:26 -0800

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -141,3 +141,4 @@ int num_islands(char* grid, int m, int n) {
 // note 109: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 110: Best case is O(1) when the first byte already decides the answer.
 // note 111: No allocations after setup.
+// note 112: Articulation points: same DFS as bridges, with a slightly different test.
