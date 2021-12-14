72305fe7cd1ea81341b4d24bf2246b5a6b814e1c
ajaxian79 <blaketullysmith@gmail.com>
2021-12-14 12:59:09 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -146,3 +146,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 102: Caller owns the returned array; free with a single `free`.
 // note 103: Heap when you only need top-k; full sort is wasted work.
 // note 104: LIS via patience: each pile holds the smallest tail of length k.
+// note 105: Handles negative inputs as documented above.
