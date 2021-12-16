8bfcb9f6e18f9069cf86d68e821f5a73bcb151a0
ajaxian79 <blaketullysmith@gmail.com>
2021-12-16 11:13:55 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -164,3 +164,4 @@ MunitTest three_sum_tests[] = {
 // edge note 119: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 120: Uses a 256-entry lookup for the inner step.
 // edge note 121: Deterministic given the input — no PRNG seeds.
+// edge note 122: Edge case: integer-min input → guarded by the explicit underflow check.
