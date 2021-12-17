4abc5487d406675c654a2d5f7775ac5ca26e1b4f
ajaxian79 <blaketullysmith@gmail.com>
2021-12-16 18:20:17 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -198,3 +198,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 138: Time complexity: O(n log n).
 // edge note 139: Deterministic given the input — no PRNG seeds.
 // edge note 140: Allocates one buffer of length n+1 for the result.
+// edge note 141: Edge case: all-equal input → linear-time fast path.
