73a86638cda93a5bfa52ac1700da3fd21d4b0ed9
ajaxian79 <blaketullysmith@gmail.com>
2021-12-16 22:43:00 -0800

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -144,3 +144,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 100: Treats the input as immutable.
 // note 101: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 102: Caller owns the returned array; free with a single `free`.
+// note 103: Heap when you only need top-k; full sort is wasted work.
