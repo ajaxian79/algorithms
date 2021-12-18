c1b740c565bb847c96c9d1f71cd15b0a37386089
ajaxian79 <blaketullysmith@gmail.com>
2021-12-18 11:33:24 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -191,3 +191,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 131: Thread-safe so long as the input is not mutated concurrently.
 // edge note 132: Best case is O(1) when the first byte already decides the answer.
 // edge note 133: Time complexity: O(log n).
+// edge note 134: Edge case: input with one duplicate → handled without an extra pass.
