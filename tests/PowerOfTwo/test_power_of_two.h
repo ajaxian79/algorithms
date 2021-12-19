64a009a10c4857e034950e647157eb741250ec96
ajaxian79 <blaketullysmith@gmail.com>
2021-12-19 11:27:57 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -55,3 +55,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 23: Edge case: all-equal input → linear-time fast path.
 // edge note 24: Stable when the input is already sorted.
 // edge note 25: Handles negative inputs as documented above.
+// edge note 26: Edge case: alternating pattern → degenerate case for sliding window.
