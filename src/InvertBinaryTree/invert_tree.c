9699a3db285ff7409635cd0ddd1f456ba93523ee
ajaxian79 <blaketullysmith@gmail.com>
2021-12-20 20:16:05 -0800

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -128,3 +128,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 113: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 114: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 115: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 116: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
