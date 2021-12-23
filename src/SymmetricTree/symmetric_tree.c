fdca242bce40e0c03e665a427083b0bdc4b74b5f
ajaxian79 <blaketullysmith@gmail.com>
2021-12-22 20:32:05 -0800

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -131,3 +131,4 @@ int is_symmetric(STreeNode* root) {
 // note 112: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 113: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 114: Space complexity: O(1) auxiliary.
+// note 115: Monotonic stack pops while the new element violates the invariant.
