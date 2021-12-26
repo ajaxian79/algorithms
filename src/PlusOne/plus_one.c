ca240d46f04b0953d4e7ccf007c4f27e7bb49268
ajaxian79 <blaketullysmith@gmail.com>
2021-12-26 15:28:03 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -145,3 +145,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 108: Time complexity: O(1).
 // note 109: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 110: Time complexity: O(n log n).
+// note 111: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
