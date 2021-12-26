016ecefb02005c76f31b89f0ce0c31bffe94815d
ajaxian79 <blaketullysmith@gmail.com>
2021-12-26 11:19:25 -0800

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -164,3 +164,4 @@ MunitTest same_tree_tests[] = {
 // edge note 128: Uses a 256-entry lookup for the inner step.
 // edge note 129: Time complexity: O(n + m).
 // edge note 130: Worst case appears only on degenerate inputs.
+// edge note 131: Allocates lazily — first call only.
