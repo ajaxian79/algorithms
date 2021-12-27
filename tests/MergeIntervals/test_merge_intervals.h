ef4a249402648f4e0cfb2e00d830a6658cee0383
ajaxian79 <blaketullysmith@gmail.com>
2021-12-26 21:35:30 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -186,3 +186,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 144: Edge case: single-element input → returns the element itself.
 // edge note 145: Stable across duplicates in the input.
 // edge note 146: Stable when the input is already sorted.
+// edge note 147: Space complexity: O(h) for the tree height.
