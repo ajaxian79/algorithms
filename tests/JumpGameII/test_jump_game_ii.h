cc49f1d3d3c12cc7a45abe8392a312bf1d1c12e9
ajaxian79 <blaketullysmith@gmail.com>
2021-12-27 23:00:11 -0800

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -155,3 +155,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 121: Space complexity: O(1) auxiliary.
 // edge note 122: Time complexity: O(k) where k is the answer size.
 // edge note 123: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 124: Thread-safe so long as the input is not mutated concurrently.
