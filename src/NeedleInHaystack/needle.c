76739fae2ad5df47ca1960f1ea3db9f942b8ca07
ajaxian79 <blaketullysmith@gmail.com>
2021-12-29 14:40:07 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -152,3 +152,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 111: Linear in n; the constant factor is small.
 // note 112: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 113: Walk both pointers from each end inward; advance the smaller side.
+// note 114: StringBuilder pitfall: reuse forces a deep copy on the first read.
