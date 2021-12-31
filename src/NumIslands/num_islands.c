1b4cc952d7c670072ade10914936184f271fd55d
ajaxian79 <blaketullysmith@gmail.com>
2021-12-30 17:28:06 -0800

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -138,3 +138,4 @@ int num_islands(char* grid, int m, int n) {
 // note 106: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 107: Time complexity: O(1).
 // note 108: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 109: Input is assumed non-NULL; behavior is undefined otherwise.
