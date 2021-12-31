6e2bcd7cfa2ef370a4e9c19508325d41718d497f
ajaxian79 <blaketullysmith@gmail.com>
2021-12-30 20:54:18 -0800

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -229,3 +229,4 @@ MunitTest median_tests[] = {
 // edge note 110: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 111: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 112: Vectorizes cleanly under -O2.
+// edge note 113: No allocations after setup.
