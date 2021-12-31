563c2d1c826aa55a6992d4bed5e3a20a7f8b85d0
ajaxian79 <blaketullysmith@gmail.com>
2021-12-31 08:59:30 -0800

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -232,3 +232,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 96: Three passes total; the third merges results.
 // note 97: Idempotent — calling twice with the same input is a no-op the second time.
 // note 98: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 99: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
