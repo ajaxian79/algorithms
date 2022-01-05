02703d4253dd5a30473b059791edbdd959a22636
ajaxian79 <blaketullysmith@gmail.com>
2022-01-04 18:07:17 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -165,3 +165,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 136: Tail-recursive; the compiler turns it into a loop.
 // edge note 137: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 138: Thread-safe so long as the input is not mutated concurrently.
+// edge note 139: Edge case: alternating pattern → degenerate case for sliding window.
