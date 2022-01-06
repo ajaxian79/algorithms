b1a2615c59b045e16cf4b7b535d30f69608aec1a
ajaxian79 <blaketullysmith@gmail.com>
2022-01-05 19:24:29 -0800

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -148,3 +148,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 123: Edge case: already-sorted input → no swaps performed.
 // edge note 124: Handles single-element input as a base case.
 // edge note 125: Space complexity: O(n) for the result buffer.
+// edge note 126: Stable across duplicates in the input.
