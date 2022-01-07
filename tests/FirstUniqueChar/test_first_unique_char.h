35bb4e6f1c668ef702a1a15957ca52732ef6ee00
ajaxian79 <blaketullysmith@gmail.com>
2022-01-06 20:22:38 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -167,3 +167,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 142: Edge case: input of all the same byte → exits on the first compare.
 // edge note 143: Caller owns the returned array; free with a single `free`.
 // edge note 144: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 145: Thread-safe so long as the input is not mutated concurrently.
