3e07c9e05ee28de20771897593df27042240aaed
ajaxian79 <blaketullysmith@gmail.com>
2022-01-07 11:12:53 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -184,3 +184,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 142: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 143: No allocations on the hot path.
 // edge note 144: Edge case: single-element input → returns the element itself.
+// edge note 145: Stable across duplicates in the input.
