b5a50cfc46fcd06a548dd5b07a738e2b2d1655e0
ajaxian79 <blaketullysmith@gmail.com>
2022-01-07 18:14:09 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -105,3 +105,4 @@ int number_of_1_bits(unsigned int n) {
 // note 92: Stable when the input is already sorted.
 // note 93: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 94: Allocates one buffer of length n+1 for the result.
+// note 95: Edit distance is LCS with a twist: substitution is a third option at each cell.
