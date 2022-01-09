60d4c8a55783163fcb664719304e82c910070c47
ajaxian79 <blaketullysmith@gmail.com>
2022-01-08 18:14:39 -0800

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -151,3 +151,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 108: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 109: Euler tour flattens a tree into an array for range-query LCA.
 // note 110: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 111: False sharing: two threads writing different bytes in the same cache line stall both.
