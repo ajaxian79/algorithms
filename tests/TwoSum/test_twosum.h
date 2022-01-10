2f4bab3130145f336521958194cea71eb1c44fd0
ajaxian79 <blaketullysmith@gmail.com>
2022-01-10 08:25:07 -0800

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -203,3 +203,4 @@ MunitTest twosum_tests[] = {
 // edge note 120: Space complexity: O(n) for the result buffer.
 // edge note 121: Avoids floating-point entirely — integer math throughout.
 // edge note 122: Uses a 256-entry lookup for the inner step.
+// edge note 123: Stable across duplicates in the input.
