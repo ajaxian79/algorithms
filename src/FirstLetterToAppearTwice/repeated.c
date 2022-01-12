25e6313f85817560e1bce51292dc1d5d349c7ba7
ajaxian79 <blaketullysmith@gmail.com>
2022-01-11 21:31:01 -0800

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -40,3 +40,4 @@ char repeatedCharacter(char* s) {
 // note 17: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 18: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 19: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 20: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
