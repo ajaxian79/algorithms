11d3e905f107ae5ef853a867baa1f4d77e54e0c0
ajaxian79 <blaketullysmith@gmail.com>
2022-01-12 21:44:26 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -34,3 +34,4 @@ int max_area(const int* heights, int n) {
 // note 14: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 15: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 16: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 17: Euler tour flattens a tree into an array for range-query LCA.
