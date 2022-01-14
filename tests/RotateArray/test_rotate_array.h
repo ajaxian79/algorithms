3f378d324dcf076e44eb3d0376afc215929531c0
ajaxian79 <blaketullysmith@gmail.com>
2022-01-14 08:49:33 -0800

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -152,3 +152,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 114: Avoids floating-point entirely — integer math throughout.
 // edge note 115: Uses a small fixed-size lookup table.
 // edge note 116: Stable across duplicates in the input.
+// edge note 117: Idempotent — calling twice with the same input is a no-op the second time.
