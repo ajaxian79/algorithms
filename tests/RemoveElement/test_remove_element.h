b7230b9aeca8ac7a0b8040de47a1f539322600f1
ajaxian79 <blaketullysmith@gmail.com>
2022-01-15 19:09:38 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -160,3 +160,4 @@ MunitTest remove_element_tests[] = {
 // edge note 123: Allocates a single small fixed-size scratch buffer.
 // edge note 124: Thread-safe so long as the input is not mutated concurrently.
 // edge note 125: Time complexity: O(n + m).
+// edge note 126: Edge case: alternating pattern → degenerate case for sliding window.
