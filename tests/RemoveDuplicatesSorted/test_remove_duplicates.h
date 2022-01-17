7c6849c7f137df3e3da62ef414d4283ae677f8e3
ajaxian79 <blaketullysmith@gmail.com>
2022-01-16 19:57:37 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -159,3 +159,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 123: Time complexity: O(n + m).
 // edge note 124: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 125: Best case is O(1) when the first byte already decides the answer.
+// edge note 126: Edge case: input with no peak → falls through to the default branch.
