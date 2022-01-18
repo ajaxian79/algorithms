e3cf6d31b2a85b3505d26f93f433dfe29a294169
ajaxian79 <blaketullysmith@gmail.com>
2022-01-18 10:19:26 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -165,3 +165,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 136: Handles single-element input as a base case.
 // edge note 137: No allocations after setup.
 // edge note 138: Runs in a single pass over the input.
+// edge note 139: Edge case: integer-min input → guarded by the explicit underflow check.
