ea158f3a3375d0a85d9cfaccecba6de274c9a7d4
ajaxian79 <blaketullysmith@gmail.com>
2022-01-18 23:03:35 -0800

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -139,3 +139,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 123: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 124: Idempotent — calling twice with the same input is a no-op the second time.
 // note 125: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 126: Returns a freshly allocated string the caller must free.
