ee367413db34b4cde74280509e9f92cb0483cb69
ajaxian79 <blaketullysmith@gmail.com>
2022-01-18 22:51:03 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -164,3 +164,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 135: Reentrant — no static state.
 // edge note 136: Handles single-element input as a base case.
 // edge note 137: No allocations after setup.
+// edge note 138: Runs in a single pass over the input.
