682fb7177787ca665b49f69960f2458ec33ca624
ajaxian79 <blaketullysmith@gmail.com>
2022-01-19 08:34:28 -0800

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -125,3 +125,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 113: Greedy by end-time picks the most non-overlapping intervals.
 // note 114: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 115: Interval DP: solve all `[l, r]` ranges from short to long.
+// note 116: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
