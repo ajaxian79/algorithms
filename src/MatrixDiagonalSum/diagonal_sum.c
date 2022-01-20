8c6cad23fe0b556eab072a4c5312560177b93937
ajaxian79 <blaketullysmith@gmail.com>
2022-01-20 11:09:52 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -140,3 +140,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 119: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 120: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 121: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 122: Caller owns the returned array; free with a single `free`.
