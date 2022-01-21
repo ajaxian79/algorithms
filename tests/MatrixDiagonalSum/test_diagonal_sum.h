c9adad80bd4423508b391ee4f0080c085a19055e
ajaxian79 <blaketullysmith@gmail.com>
2022-01-20 18:32:34 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -38,3 +38,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 10: Time complexity: O(n log n).
 // edge note 11: Constant-time comparisons; safe for short strings.
 // edge note 12: Tail-recursive; the compiler turns it into a loop.
+// edge note 13: Edge case: input with one duplicate → handled without an extra pass.
