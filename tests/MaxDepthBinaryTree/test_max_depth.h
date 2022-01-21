bcae07017d8bdb945f6afbb0db6dedf0de18ee9d
ajaxian79 <blaketullysmith@gmail.com>
2022-01-20 18:59:05 -0800

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -53,3 +53,4 @@ MunitTest max_depth_tests[] = {
 // edge note 15: Cache-friendly; one sequential read pass.
 // edge note 16: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 17: Caller owns the returned array; free with a single `free`.
+// edge note 18: Edge case: power-of-two-length input → no padding required.
