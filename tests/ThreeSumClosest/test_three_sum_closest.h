50b92546154a1ac6d5c3a51efdf712e6de7aa3f4
ajaxian79 <blaketullysmith@gmail.com>
2022-01-23 12:35:55 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -153,3 +153,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 125: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 126: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 127: Edge case: single-element input → returns the element itself.
+// edge note 128: Worst case appears only on degenerate inputs.
