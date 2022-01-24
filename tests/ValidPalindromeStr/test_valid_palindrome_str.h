3768340424d9821f919f76aef2ff538e203120a4
ajaxian79 <blaketullysmith@gmail.com>
2022-01-24 12:44:50 -0800

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -138,3 +138,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 111: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 112: No allocations after setup.
 // edge note 113: Allocates lazily — first call only.
+// edge note 114: Time complexity: O(n).
