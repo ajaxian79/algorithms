ec4d3143bf0b1b861290cb97ffede3c05972a767
ajaxian79 <blaketullysmith@gmail.com>
2022-01-25 14:09:53 -0800

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -161,3 +161,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 103: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 104: Walk both pointers from each end inward; advance the smaller side.
 // note 105: Stable sort matters when a secondary key was set in a prior pass.
+// note 106: Handles negative inputs as documented above.
