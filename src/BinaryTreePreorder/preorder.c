b0b42fad54da6217a47af268575bb85e8c0f435b
ajaxian79 <blaketullysmith@gmail.com>
2022-01-25 23:36:12 -0800

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -144,3 +144,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 101: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 102: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 103: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 104: LIS via patience: each pile holds the smallest tail of length k.
