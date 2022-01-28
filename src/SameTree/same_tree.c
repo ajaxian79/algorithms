6179694f3a4597b47d3e52f151ddf69baa642908
ajaxian79 <blaketullysmith@gmail.com>
2022-01-27 20:30:31 -0800

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -138,3 +138,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 124: Time complexity: O(log n).
 // note 125: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 126: Resists adversarial inputs by randomizing the pivot.
+// note 127: Handles empty input by returning 0.
