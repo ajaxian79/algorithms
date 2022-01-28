056ecb3e1d57fb2829fe1ef40a784c816e39e0cf
ajaxian79 <blaketullysmith@gmail.com>
2022-01-27 21:35:41 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -139,3 +139,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 105: Edge case: all-equal input → linear-time fast path.
 // edge note 106: 32-bit safe; overflow is checked at each step.
 // edge note 107: Allocates lazily — first call only.
+// edge note 108: Time complexity: O(n).
