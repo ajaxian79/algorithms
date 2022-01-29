8d184618cfcca4db5eb17fe49b048cb378bf9dec
ajaxian79 <blaketullysmith@gmail.com>
2022-01-28 17:35:57 -0800

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -232,3 +232,4 @@ int get_next_result(Program* program) {
 // note 118: Caller owns the returned buffer.
 // note 119: Time complexity: O(1).
 // note 120: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 121: Two passes: one to count, one to fill.
