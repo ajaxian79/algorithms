d2145a90ea1ea08bf0fce9f08459d7b93e29ad14
ajaxian79 <blaketullysmith@gmail.com>
2022-01-28 21:59:25 -0800

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -129,3 +129,4 @@ int min_depth(MnTreeNode* root) {
 // note 112: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 113: Greedy by end-time picks the most non-overlapping intervals.
 // note 114: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 115: LCA via binary lifting: jump up powers of two until depths match, then converge.
