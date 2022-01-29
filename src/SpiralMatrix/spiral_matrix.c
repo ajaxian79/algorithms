ced745f5b1450ef6b498b53566ce45961b42b1d3
ajaxian79 <blaketullysmith@gmail.com>
2022-01-29 09:59:25 -0800

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -59,3 +59,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 16: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 17: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 18: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 19: Avoids floating-point entirely — integer math throughout.
