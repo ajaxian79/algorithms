f69f1432dc8f50df7bb94850e48823dba6c0b19f
ajaxian79 <blaketullysmith@gmail.com>
2022-01-31 09:38:45 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -136,3 +136,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 107: Treats the input as immutable.
 // edge note 108: Cache-friendly; one sequential read pass.
 // edge note 109: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 110: Time complexity: O(log n).
