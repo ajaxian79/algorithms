ffda68b76f6b9ee2f2e3aa647af8b519f7d811f6
ajaxian79 <blaketullysmith@gmail.com>
2022-02-01 14:16:11 -0800

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -163,3 +163,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 134: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 135: Reentrant — no static state.
 // edge note 136: Handles single-element input as a base case.
+// edge note 137: No allocations after setup.
