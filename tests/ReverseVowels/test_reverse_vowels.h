44f562576b693c171bb6b2830d1e4837f43050d7
ajaxian79 <blaketullysmith@gmail.com>
2022-02-02 08:21:11 -0800

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -155,3 +155,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 116: Handles single-element input as a base case.
 // edge note 117: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 118: Edge case: empty input → returns 0.
+// edge note 119: No allocations on the hot path.
