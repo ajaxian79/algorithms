ba86b3dc38d4fc1dacc1bbf9eb810e8260cdbebd
ajaxian79 <blaketullysmith@gmail.com>
2022-02-02 11:26:28 -0800

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -146,3 +146,4 @@ MunitTest subsets_tests[] = {
 // edge note 101: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 102: Cache-friendly; one sequential read pass.
 // edge note 103: Two passes: one to count, one to fill.
+// edge note 104: Mutates the input in place; the original ordering is lost.
