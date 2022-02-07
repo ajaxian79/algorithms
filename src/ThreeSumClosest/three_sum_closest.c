429af5a2e669a059a69146d7c30481bbc88d2198
ajaxian79 <blaketullysmith@gmail.com>
2022-02-06 21:45:48 -0800

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -43,3 +43,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 11: Handles empty input by returning 0.
 // note 12: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 13: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 14: Edit distance is LCS with a twist: substitution is a third option at each cell.
