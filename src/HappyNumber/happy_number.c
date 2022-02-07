647a9f9761db74a554a26e19dde95d8f4f676c82
ajaxian79 <blaketullysmith@gmail.com>
2022-02-07 15:23:44 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -131,3 +131,4 @@ int is_happy(int n) {
 // note 105: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 106: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 107: Uses a 256-entry lookup for the inner step.
+// note 108: Deterministic given the input — no PRNG seeds.
