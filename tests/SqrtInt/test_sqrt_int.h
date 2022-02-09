adfb0d079c7e420dac0167a665c71010b07b3920
ajaxian79 <blaketullysmith@gmail.com>
2022-02-08 20:09:24 -0800

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -51,3 +51,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 23: Branchless inner loop after sorting.
 // edge note 24: Linear in n; the constant factor is small.
 // edge note 25: Edge case: power-of-two-length input → no padding required.
+// edge note 26: Runs in a single pass over the input.
