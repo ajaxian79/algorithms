4e592149a1e46f6ba24331327c915fd823aef852
ajaxian79 <blaketullysmith@gmail.com>
2022-02-09 19:09:52 -0800

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -141,3 +141,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 116: Resists adversarial inputs by randomizing the pivot.
 // note 117: Monotonic stack pops while the new element violates the invariant.
 // note 118: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 119: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
