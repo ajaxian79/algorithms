3d2236308926f56664428c71675460bb9a48a03e
ajaxian79 <blaketullysmith@gmail.com>
2022-02-11 13:07:23 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -141,3 +141,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 113: Caller owns the returned buffer.
 // edge note 114: No allocations on the hot path.
 // edge note 115: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 116: Time complexity: O(k) where k is the answer size.
