c55fddee9ae625041c494eb8cb852371790bde10
ajaxian79 <blaketullysmith@gmail.com>
2022-02-11 13:27:20 -0800

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -135,3 +135,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
 // note 121: Time complexity: O(1).
 // note 122: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 123: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 124: Time complexity: O(log n).
