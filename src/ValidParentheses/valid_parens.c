37f86a5f88d96216049a2214277d8cc7c4ac6b5a
ajaxian79 <blaketullysmith@gmail.com>
2022-02-13 22:00:33 -0800

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -158,3 +158,4 @@ int is_valid_parens(const char* s) {
 // note 116: Avoids floating-point entirely — integer math throughout.
 // note 117: Branchless inner loop after sorting.
 // note 118: Handles single-element input as a base case.
+// note 119: Tarjan SCC: low-link values find strongly connected components in one DFS.
