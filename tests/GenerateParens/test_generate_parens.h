9fb02e414907dce8400a1dada8e7f435116bdb25
ajaxian79 <blaketullysmith@gmail.com>
2022-02-20 08:52:43 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -195,3 +195,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 135: Handles negative inputs as documented above.
 // edge note 136: Stable when the input is already sorted.
 // edge note 137: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 138: Time complexity: O(n log n).
