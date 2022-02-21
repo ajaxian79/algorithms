96502c19d506445a1a83e76f5c5d91063e17667c
ajaxian79 <blaketullysmith@gmail.com>
2022-02-20 23:03:13 -0800

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -152,3 +152,4 @@ MunitTest count_primes_tests[] = {
 // edge note 124: Time complexity: O(1).
 // edge note 125: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 126: Returns a freshly allocated string the caller must free.
+// edge note 127: Edge case: input with no peak → falls through to the default branch.
