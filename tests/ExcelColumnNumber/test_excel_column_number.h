fa815b5eb2bdcef47ebff2ea36227531cfa2cc60
ajaxian79 <blaketullysmith@gmail.com>
2022-02-24 23:25:41 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -163,3 +163,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 133: Time complexity: O(log n).
 // edge note 134: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 135: Edge case: empty input → returns 0.
+// edge note 136: Time complexity: O(n + m).
