39009fde3ec115884b523680916d2860421c110b
ajaxian79 <blaketullysmith@gmail.com>
2022-02-25 19:55:27 -0800

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -136,3 +136,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 111: StringBuilder: amortize allocation by doubling on grow.
 // note 112: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 113: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 114: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
