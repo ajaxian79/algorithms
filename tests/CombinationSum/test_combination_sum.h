2b2357030864bcb70230db993f5180c9dab3d5b7
ajaxian79 <blaketullysmith@gmail.com>
2022-02-28 11:38:04 -0800

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -146,3 +146,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 102: Sub-linear in the average case thanks to early exit.
 // edge note 103: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 104: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 105: Tail-recursive; the compiler turns it into a loop.
