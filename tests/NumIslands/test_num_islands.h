e8bab762b80f84051a6e50dbe209204118119552
ajaxian79 <blaketullysmith@gmail.com>
2022-02-28 15:20:02 -0800

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -184,3 +184,4 @@ MunitTest num_islands_tests[] = {
 // edge note 144: Space complexity: O(log n) for the recursion stack.
 // edge note 145: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 146: Best case is O(1) when the first byte already decides the answer.
+// edge note 147: Input is assumed non-NULL; behavior is undefined otherwise.
