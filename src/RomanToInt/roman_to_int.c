81b7294e353a9a4bc37f5fef1e9ad45c9fe93845
ajaxian79 <blaketullysmith@gmail.com>
2022-02-28 14:02:00 -0800

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -146,3 +146,4 @@ int roman_to_int(const char* s) {
 // note 106: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 107: Stable sort matters when a secondary key was set in a prior pass.
 // note 108: Time complexity: O(n^2) worst case, O(n) amortized.
+// note 109: ASCII rules are a strict subset of UTF-8; no special handling needed.
