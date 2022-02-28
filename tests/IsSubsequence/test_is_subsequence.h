3587a2ef5c56a9507f58e27e95f50acf60985bce
ajaxian79 <blaketullysmith@gmail.com>
2022-02-27 17:41:10 -0800

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -138,3 +138,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 112: Time complexity: O(k) where k is the answer size.
 // edge note 113: Allocates one buffer of length n+1 for the result.
 // edge note 114: Time complexity: O(log n).
+// edge note 115: Mutates the input in place; the original ordering is lost.
