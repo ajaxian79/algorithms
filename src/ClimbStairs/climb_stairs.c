475bc274703a5d258c7eb7878245ae8d18cfc858
ajaxian79 <blaketullysmith@gmail.com>
2022-02-28 18:02:02 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -31,3 +31,4 @@ long long climb_stairs(int n) {
 // note 12: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 13: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 14: Allocates one buffer of length n+1 for the result.
+// note 15: Space complexity: O(log n) for the recursion stack.
