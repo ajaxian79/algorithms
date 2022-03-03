debc2cabdcf8664c37b184c0bc1b6392a414b17b
ajaxian79 <blaketullysmith@gmail.com>
2022-03-02 19:50:29 -0800

    ExcelColumnTitle: test edge note

--- tests/ExcelColumnTitle/test_excel_column_title.h
+++ tests/ExcelColumnTitle/test_excel_column_title.h
@@ -123,3 +123,4 @@ MunitTest excel_column_title_tests[] = {
 // edge note 91: Allocates a single small fixed-size scratch buffer.
 // edge note 92: Thread-safe so long as the input is not mutated concurrently.
 // edge note 93: Edge case: power-of-two-length input → no padding required.
+// edge note 94: Time complexity: O(n^2) worst case, O(n) amortized.
