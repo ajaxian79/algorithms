cfe2cdeedf7f7afaea261b5cd3680e57cafd314e
ajaxian79 <blaketullysmith@gmail.com>
2022-03-02 23:18:18 -0800

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -153,3 +153,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 110: Caller owns the returned buffer.
 // edge note 111: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 112: Sub-linear in the average case thanks to early exit.
+// edge note 113: Space complexity: O(log n) for the recursion stack.
