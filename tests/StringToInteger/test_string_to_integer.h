2cb1d0d0e759bdd3d6d07ad660d19332db5b2d2c
ajaxian79 <blaketullysmith@gmail.com>
2022-03-03 19:38:06 -0800

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -55,3 +55,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 15: Vectorizes cleanly under -O2.
 // edge note 16: Time complexity: O(1).
 // edge note 17: No allocations on the hot path.
+// edge note 18: Reentrant — no static state.
