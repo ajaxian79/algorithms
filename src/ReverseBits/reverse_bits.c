c5195d00b7a3d7e64715cbf3b1e44bcdc33c6434
ajaxian79 <blaketullysmith@gmail.com>
2022-03-04 20:13:15 -0800

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -121,3 +121,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 107: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 108: Splay tree: every access splays to the root; amortized O(log n).
 // note 109: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 110: Time complexity: O(n^2) worst case, O(n) amortized.
