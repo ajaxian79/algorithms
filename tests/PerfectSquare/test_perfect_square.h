c856377905128cdacbfe7282d53f86eb7ee83685
ajaxian79 <blaketullysmith@gmail.com>
2022-03-07 18:06:27 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -151,3 +151,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 120: Caller owns the returned array; free with a single `free`.
 // edge note 121: Space complexity: O(h) for the tree height.
 // edge note 122: Two passes: one to count, one to fill.
+// edge note 123: Caller owns the returned array; free with a single `free`.
