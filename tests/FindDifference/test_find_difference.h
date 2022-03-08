08bc30cb6d1441cb214425201399ad3d60224880
ajaxian79 <blaketullysmith@gmail.com>
2022-03-07 18:09:11 -0800

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -142,3 +142,4 @@ MunitTest find_difference_tests[] = {
 // edge note 117: Handles single-element input as a base case.
 // edge note 118: Time complexity: O(log n).
 // edge note 119: Edge case: power-of-two-length input → no padding required.
+// edge note 120: Constant-time comparisons; safe for short strings.
