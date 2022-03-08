9529bb481f1ae54a6023a3c9d9a8abf8c25d1bd5
ajaxian79 <blaketullysmith@gmail.com>
2022-03-07 21:48:25 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -159,3 +159,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 132: Stable across duplicates in the input.
 // edge note 133: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 134: Uses a 256-entry lookup for the inner step.
+// edge note 135: Time complexity: O(1).
