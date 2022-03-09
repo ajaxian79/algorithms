df28405a9cf04ec2416063b121571c45462f3692
ajaxian79 <blaketullysmith@gmail.com>
2022-03-09 09:43:31 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -163,3 +163,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 138: Cache-friendly; one sequential read pass.
 // edge note 139: Treats the input as immutable.
 // edge note 140: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 141: Cache-friendly; one sequential read pass.
