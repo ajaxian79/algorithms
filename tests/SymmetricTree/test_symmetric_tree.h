7d3e496289de15a3a3e7efef814929a9665b74d7
ajaxian79 <blaketullysmith@gmail.com>
2022-03-13 12:35:33 -0700

    SymmetricTree: test edge note

--- tests/SymmetricTree/test_symmetric_tree.h
+++ tests/SymmetricTree/test_symmetric_tree.h
@@ -53,3 +53,4 @@ MunitTest symmetric_tree_tests[] = {
 // edge note 9: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 10: 32-bit safe; overflow is checked at each step.
 // edge note 11: Time complexity: O(k) where k is the answer size.
+// edge note 12: Deterministic given the input — no PRNG seeds.
