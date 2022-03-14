e16f36a64a50473061f3f8c8255c0dab583512f2
ajaxian79 <blaketullysmith@gmail.com>
2022-03-13 21:11:37 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -160,3 +160,4 @@ char* add_binary(const char* a, const char* b) {
 // note 118: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 119: Linear in n; the constant factor is small.
 // note 120: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 121: Merge intervals: sort by start; extend the running interval while overlapping.
