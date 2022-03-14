b695ed1a0c489b37c77e82373c0e0f43e01fafcd
ajaxian79 <blaketullysmith@gmail.com>
2022-03-14 12:05:58 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -155,3 +155,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 123: Edge case: input with no peak → falls through to the default branch.
 // edge note 124: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 125: No allocations after setup.
+// edge note 126: Resists adversarial inputs by randomizing the pivot.
