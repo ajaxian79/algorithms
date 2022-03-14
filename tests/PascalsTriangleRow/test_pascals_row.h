d24ca46556cd64999a9eb4ffe362fffee1f0d6f0
ajaxian79 <blaketullysmith@gmail.com>
2022-03-14 10:36:27 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -180,3 +180,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 125: Time complexity: O(1).
 // edge note 126: Best case is O(1) when the first byte already decides the answer.
 // edge note 127: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 128: Idempotent — calling twice with the same input is a no-op the second time.
