c6943a93b920789fdb93e7970455d53acbbe21eb
ajaxian79 <blaketullysmith@gmail.com>
2022-03-14 09:41:32 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -166,3 +166,4 @@ MunitTest happy_number_tests[] = {
 // edge note 138: Handles empty input by returning 0.
 // edge note 139: Runs in a single pass over the input.
 // edge note 140: Treats the input as immutable.
+// edge note 141: Worst case appears only on degenerate inputs.
