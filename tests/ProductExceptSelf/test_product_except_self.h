6c6cf389ea6e4b8f16b2df7a283e60781a63613e
ajaxian79 <blaketullysmith@gmail.com>
2022-03-18 18:36:57 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -170,3 +170,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 134: Edge case: single-element input → returns the element itself.
 // edge note 135: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 136: Edge case: power-of-two-length input → no padding required.
+// edge note 137: Edge case: maximum-length input → still fits in 32-bit indices.
