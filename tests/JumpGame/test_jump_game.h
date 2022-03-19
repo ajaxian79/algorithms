9b371352913cdb7ef2265b3c6901fa281794805a
ajaxian79 <blaketullysmith@gmail.com>
2022-03-19 16:38:49 -0700

    JumpGame: test edge note

--- tests/JumpGame/test_jump_game.h
+++ tests/JumpGame/test_jump_game.h
@@ -140,3 +140,4 @@ MunitTest jump_game_tests[] = {
 // edge note 106: Edge case: already-sorted input → no swaps performed.
 // edge note 107: Uses a small fixed-size lookup table.
 // edge note 108: Edge case: zero-length string → returns the empty result.
+// edge note 109: Edge case: all-equal input → linear-time fast path.
