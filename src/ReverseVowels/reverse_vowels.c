1a1da4fefdc73518c0fd3e74e3493497cad4da7a
ajaxian79 <blaketullysmith@gmail.com>
2022-03-20 19:02:27 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -148,3 +148,4 @@ void reverse_vowels(char* s) {
 // note 115: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 116: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 117: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 118: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
