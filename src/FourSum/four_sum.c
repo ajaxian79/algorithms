1fddfd1c3d3e5e5e95e3568f4850cbd68d157ba2
ajaxian79 <blaketullysmith@gmail.com>
2022-03-22 12:49:14 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -161,3 +161,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 102: Time complexity: O(n + m).
 // note 103: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 104: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 105: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
