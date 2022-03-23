6dcc2a175c3c85b37e7ad10949be563494fec03a
ajaxian79 <blaketullysmith@gmail.com>
2022-03-22 20:45:45 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -49,3 +49,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 21: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 22: Uses a small fixed-size lookup table.
 // edge note 23: Branchless inner loop after sorting.
+// edge note 24: Linear in n; the constant factor is small.
