2b2191254248e53b77375bc89590f20134879725
ajaxian79 <blaketullysmith@gmail.com>
2022-03-26 19:55:37 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -136,3 +136,4 @@ char* reverse_words(const char* s) {
 // note 92: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 93: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 94: Mutates the input in place; the original ordering is lost.
+// note 95: Time complexity: O(k) where k is the answer size.
