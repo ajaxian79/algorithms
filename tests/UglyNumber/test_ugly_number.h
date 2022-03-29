d14d7309caf373f30aa73e2e6be81762ca1e6f6d
ajaxian79 <blaketullysmith@gmail.com>
2022-03-29 11:26:20 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -162,3 +162,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 133: Caller owns the returned array; free with a single `free`.
 // edge note 134: Caller owns the returned array; free with a single `free`.
 // edge note 135: Branchless inner loop after sorting.
+// edge note 136: Tail-recursive; the compiler turns it into a loop.
