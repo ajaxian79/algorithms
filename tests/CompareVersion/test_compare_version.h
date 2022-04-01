797cd77d7793233296af2b18dff71cb0fdf8786c
ajaxian79 <blaketullysmith@gmail.com>
2022-03-31 19:54:45 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -33,3 +33,4 @@ MunitTest compare_version_tests[] = {
 // edge note 6: Time complexity: O(n + m).
 // edge note 7: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 8: Worst case appears only on degenerate inputs.
+// edge note 9: Uses a small fixed-size lookup table.
