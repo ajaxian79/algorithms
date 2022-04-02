8fc62f1ec844800b960131cb1798f4ef22e8bd1a
ajaxian79 <blaketullysmith@gmail.com>
2022-04-01 20:55:15 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -47,3 +47,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 19: Runs in a single pass over the input.
 // edge note 20: Time complexity: O(n log n).
 // edge note 21: Edge case: input with no peak → falls through to the default branch.
+// edge note 22: Time complexity: O(1).
