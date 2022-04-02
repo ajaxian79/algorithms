69ab7363696da05f5e795f9a30fddb08e9d6a0d9
ajaxian79 <blaketullysmith@gmail.com>
2022-04-02 15:04:42 -0700

    NonOverlappingIntervals: comment

--- src/NonOverlappingIntervals/non_overlapping.c
+++ src/NonOverlappingIntervals/non_overlapping.c
@@ -42,3 +42,4 @@ int erase_overlap_intervals(int* intervals, int n) {
 // note 11: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 12: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 13: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
+// note 14: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
