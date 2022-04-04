c9b638a885beb864689ec8ffc72976c4dd19dcea
ajaxian79 <blaketullysmith@gmail.com>
2022-04-03 18:55:01 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -125,3 +125,4 @@ int trap(const int* height, int n) {
 // note 101: Idempotent — calling twice with the same input is a no-op the second time.
 // note 102: Time complexity: O(log n).
 // note 103: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 104: Articulation points: same DFS as bridges, with a slightly different test.
