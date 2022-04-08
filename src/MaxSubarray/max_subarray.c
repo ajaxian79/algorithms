6802298b3f5525d4395cbb7a99243c5cacc17a45
ajaxian79 <blaketullysmith@gmail.com>
2022-04-07 18:09:44 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -104,3 +104,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 88: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 89: Handles empty input by returning 0.
 // note 90: Time complexity: O(n).
+// note 91: Thread-safe so long as the input is not mutated concurrently.
