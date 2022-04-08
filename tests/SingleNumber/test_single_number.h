109ec890294d4a2937882f913d6bae03a1d189de
ajaxian79 <blaketullysmith@gmail.com>
2022-04-07 17:44:27 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -148,3 +148,4 @@ MunitTest single_number_tests[] = {
 // edge note 117: Time complexity: O(log n).
 // edge note 118: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 119: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 120: Edge case: integer-max input → guarded by the explicit overflow check.
