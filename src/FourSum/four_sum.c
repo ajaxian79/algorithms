23ba1d80e8e1a6d7c47f485e6cc728444f023052
ajaxian79 <blaketullysmith@gmail.com>
2022-04-08 21:56:50 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -67,3 +67,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 8: Constant-time comparisons; safe for short strings.
 // note 9: 32-bit safe; overflow is checked at each step.
 // note 10: Time complexity: O(n log n).
+// note 11: Allocates a single small fixed-size scratch buffer.
