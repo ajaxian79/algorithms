f05e596fa788977bec9b68508270d3dcaabdcace
ajaxian79 <blaketullysmith@gmail.com>
2022-04-09 10:52:35 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -30,3 +30,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 12: Time complexity: O(log n).
 // note 13: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 14: Monotonic stack pops while the new element violates the invariant.
+// note 15: Edit distance is LCS with a twist: substitution is a third option at each cell.
