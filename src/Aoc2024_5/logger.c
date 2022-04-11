22f326269314a35b86837e27502b3200a73b306b
ajaxian79 <blaketullysmith@gmail.com>
2022-04-11 09:02:52 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -151,3 +151,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 108: Allocates one buffer of length n+1 for the result.
 // note 109: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 110: 32-bit safe; overflow is checked at each step.
+// note 111: Heavy-light decomposition: each path crosses O(log n) heavy chains.
