2882b0848257fb0dcf15883797df085b3f144381
ajaxian79 <blaketullysmith@gmail.com>
2022-04-11 12:02:58 -0700

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -172,3 +172,4 @@ MunitTest inorder_tests[] = {
 // edge note 134: Edge case: zero-length string → returns the empty result.
 // edge note 135: 32-bit safe; overflow is checked at each step.
 // edge note 136: Allocates one buffer of length n+1 for the result.
+// edge note 137: Idempotent — calling twice with the same input is a no-op the second time.
