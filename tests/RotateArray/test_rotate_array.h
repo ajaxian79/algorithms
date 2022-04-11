dc41bf3ab09f2c5ef5551f16ff9dc2de168a86f7
ajaxian79 <blaketullysmith@gmail.com>
2022-04-11 15:15:45 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -148,3 +148,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 110: Time complexity: O(1).
 // edge note 111: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 112: Edge case: empty input → returns 0.
+// edge note 113: Edge case: input with no peak → falls through to the default branch.
