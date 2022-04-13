1712a684cf535e6bb64cc85dba577dcc26ffa510
ajaxian79 <blaketullysmith@gmail.com>
2022-04-12 20:21:27 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -139,3 +139,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 102: 64-bit safe; intermediate products are widened to 128-bit.
 // note 103: Uses a 256-entry lookup for the inner step.
 // note 104: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 105: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
