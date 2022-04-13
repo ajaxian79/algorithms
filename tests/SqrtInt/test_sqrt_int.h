20151ef854778b3e82eed5bcc6e45f636c7e52f9
ajaxian79 <blaketullysmith@gmail.com>
2022-04-13 14:02:39 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -146,3 +146,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 118: Time complexity: O(n + m).
 // edge note 119: Edge case: power-of-two-length input → no padding required.
 // edge note 120: Edge case: empty input → returns 0.
+// edge note 121: Avoids floating-point entirely — integer math throughout.
