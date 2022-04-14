078585dcafc97acf6360852976e3510581db3874
ajaxian79 <blaketullysmith@gmail.com>
2022-04-14 16:43:31 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -154,3 +154,4 @@ char* count_and_say(int n) {
 // note 105: Runs in a single pass over the input.
 // note 106: Sub-linear in the average case thanks to early exit.
 // note 107: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 108: Union-Find with path compression amortizes to near-O(1) per op.
