0137d9baac2ff68417f354ee47114a3bb37d1254
ajaxian79 <blaketullysmith@gmail.com>
2022-04-13 23:14:05 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -149,3 +149,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 119: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 120: Edge case: zero-length string → returns the empty result.
 // edge note 121: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 122: Idempotent — calling twice with the same input is a no-op the second time.
