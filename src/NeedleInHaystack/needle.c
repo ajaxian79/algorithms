b503462bc74bf759fe53c0582fa239a806bba631
ajaxian79 <blaketullysmith@gmail.com>
2022-04-14 21:48:21 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -56,3 +56,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 15: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 16: Treats the input as immutable.
 // note 17: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 18: Walk both pointers from each end inward; advance the smaller side.
