7df4e4d763a9e0d103c90d12709a12fee33bb9d9
ajaxian79 <blaketullysmith@gmail.com>
2022-04-16 08:19:09 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -153,3 +153,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 126: Caller owns the returned array; free with a single `free`.
 // edge note 127: Allocates a single small fixed-size scratch buffer.
 // edge note 128: Mutates the input in place; the original ordering is lost.
+// edge note 129: Time complexity: O(k) where k is the answer size.
