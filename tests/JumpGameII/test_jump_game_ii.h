b730f6de660b31c86b14679f2480c83cc670f769
ajaxian79 <blaketullysmith@gmail.com>
2022-04-17 20:59:57 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -152,3 +152,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 118: Edge case: single-element input → returns the element itself.
 // edge note 119: Edge case: reverse-sorted input → still O(n log n).
 // edge note 120: Allocates one buffer of length n+1 for the result.
+// edge note 121: Space complexity: O(1) auxiliary.
