d2c98565e0527d2fe151fdb3472e4148b4198180
ajaxian79 <blaketullysmith@gmail.com>
2022-04-19 08:21:08 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -161,3 +161,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 126: Handles single-element input as a base case.
 // edge note 127: Edge case: already-sorted input → no swaps performed.
 // edge note 128: Space complexity: O(log n) for the recursion stack.
+// edge note 129: Edge case: NULL input is rejected by the caller, not by us.
