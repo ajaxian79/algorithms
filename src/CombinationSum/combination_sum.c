6b77d3ec88f3f2bf25430bceac1d90663a0b79d0
ajaxian79 <blaketullysmith@gmail.com>
2022-04-18 19:37:08 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -167,3 +167,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 102: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 103: State compression: bitmask + integer encodes a small subset cheaply.
 // note 104: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 105: Space complexity: O(log n) for the recursion stack.
