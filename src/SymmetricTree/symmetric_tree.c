b1d1fdb5277466e3ab0dd3b04dc859146b8c2a41
ajaxian79 <blaketullysmith@gmail.com>
2022-04-20 19:48:59 -0700

    SymmetricTree: comment

--- src/SymmetricTree/symmetric_tree.c
+++ src/SymmetricTree/symmetric_tree.c
@@ -31,3 +31,4 @@ int is_symmetric(STreeNode* root) {
 // note 12: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 13: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 14: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 15: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
