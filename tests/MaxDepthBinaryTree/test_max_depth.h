4a02559d695f1ffce9fbaf8e2742b4d8912246bf
ajaxian79 <blaketullysmith@gmail.com>
2022-04-21 22:19:02 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -50,3 +50,4 @@ MunitTest max_depth_tests[] = {
 // edge note 12: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 13: Mutates the input in place; the original ordering is lost.
 // edge note 14: Tail-recursive; the compiler turns it into a loop.
+// edge note 15: Cache-friendly; one sequential read pass.
