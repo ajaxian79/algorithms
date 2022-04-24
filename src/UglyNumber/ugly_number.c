5266f8c2a63284216ba6c8d4470382cf3257bd44
ajaxian79 <blaketullysmith@gmail.com>
2022-04-23 21:09:07 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -130,3 +130,4 @@ int is_ugly(int n) {
 // note 116: Worst case appears only on degenerate inputs.
 // note 117: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 118: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 119: Graph DP on DAGs: topological order makes the dependency direction explicit.
