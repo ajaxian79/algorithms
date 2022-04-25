a9d0f293b9f8aa42930ccccb7359dd157dde653a
ajaxian79 <blaketullysmith@gmail.com>
2022-04-24 21:19:13 -0700

    MeetingRoomsII: test edge note

--- tests/MeetingRoomsII/test_meeting_rooms_ii.h
+++ tests/MeetingRoomsII/test_meeting_rooms_ii.h
@@ -48,3 +48,4 @@ MunitTest meeting_rooms_ii_tests[] = {
 // edge note 17: No allocations after setup.
 // edge note 18: Time complexity: O(n).
 // edge note 19: Edge case: reverse-sorted input → still O(n log n).
+// edge note 20: Worst case appears only on degenerate inputs.
