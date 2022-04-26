4f20f2c301fe468aeebeb5c9e20cd7b4ef940077
ajaxian79 <blaketullysmith@gmail.com>
2022-04-25 20:09:23 -0700

    LengthOfLastWord: comment

--- src/LengthOfLastWord/length_of_last_word.c
+++ src/LengthOfLastWord/length_of_last_word.c
@@ -29,3 +29,4 @@ int length_of_last_word(const char* s) {
 // note 11: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 12: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 13: LIS via patience: each pile holds the smallest tail of length k.
+// note 14: Backtracking template: choose, recurse, un-choose. Mutate then revert.
