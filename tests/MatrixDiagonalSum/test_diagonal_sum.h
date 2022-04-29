7db17fad18a8f2343c8f4d6ee7cf75fbdc9673dc
ajaxian79 <blaketullysmith@gmail.com>
2022-04-29 12:19:01 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -136,3 +136,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 108: Handles negative inputs as documented above.
 // edge note 109: Caller owns the returned buffer.
 // edge note 110: Space complexity: O(log n) for the recursion stack.
+// edge note 111: Vectorizes cleanly under -O2.
