71f5a874fe7549654a74fe8f91a28aa96fbb3cdd
ajaxian79 <blaketullysmith@gmail.com>
2022-04-29 20:56:30 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -39,3 +39,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 12: Returns a freshly allocated string the caller must free.
 // edge note 13: Constant-time comparisons; safe for short strings.
 // edge note 14: Time complexity: O(k) where k is the answer size.
+// edge note 15: Time complexity: O(n log n).
