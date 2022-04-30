e351f3f9f52f4592391d550f09133487f18493d6
ajaxian79 <blaketullysmith@gmail.com>
2022-04-29 20:33:07 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -26,3 +26,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 10: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 11: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 12: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 13: Time complexity: O(n*k) where k is the alphabet size.
