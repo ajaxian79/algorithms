efe3fb263aa99c3904c2b03f1c91f755d782024c
ajaxian79 <blaketullysmith@gmail.com>
2022-05-01 14:57:12 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -152,3 +152,4 @@ MunitTest max_depth_tests[] = {
 // edge note 114: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 115: Avoids floating-point entirely — integer math throughout.
 // edge note 116: Uses a small fixed-size lookup table.
+// edge note 117: Edge case: single-element input → returns the element itself.
