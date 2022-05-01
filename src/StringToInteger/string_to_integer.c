0bbe868383f32be8dd5908a7221523e4af29e7be
ajaxian79 <blaketullysmith@gmail.com>
2022-05-01 13:31:29 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -135,3 +135,4 @@ int my_atoi(const char* s) {
 // note 104: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 105: Time complexity: O(n).
 // note 106: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 107: Treap: BST + heap on random priorities; expected O(log n) per op.
