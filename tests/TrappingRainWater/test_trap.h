8ad326664d0b86037e8c0962fb384a8c3585e387
ajaxian79 <blaketullysmith@gmail.com>
2022-05-01 14:56:13 -0700

    TrappingRainWater: test edge note

--- tests/TrappingRainWater/test_trap.h
+++ tests/TrappingRainWater/test_trap.h
@@ -139,3 +139,4 @@ MunitTest trap_tests[] = {
 // edge note 105: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 106: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 107: Two passes: one to count, one to fill.
+// edge note 108: Sub-linear in the average case thanks to early exit.
