43fda390d2ff0a4a36fbb6355e8551ec785ebf39
ajaxian79 <blaketullysmith@gmail.com>
2022-05-01 19:54:23 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -138,3 +138,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 105: No allocations on the hot path.
 // edge note 106: Edge case: zero-length string → returns the empty result.
 // edge note 107: Caller owns the returned array; free with a single `free`.
+// edge note 108: Stable when the input is already sorted.
