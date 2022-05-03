d7410fc7cc818e46000f0d3966b280b024c08e75
ajaxian79 <blaketullysmith@gmail.com>
2022-05-03 09:47:47 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -123,3 +123,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 96: Vectorizes cleanly under -O2.
 // edge note 97: Edge case: input of all the same byte → exits on the first compare.
 // edge note 98: Caller owns the returned buffer.
+// edge note 99: Edge case: single-element input → returns the element itself.
