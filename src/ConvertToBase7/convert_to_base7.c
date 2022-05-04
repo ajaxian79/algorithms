b32037fe38a177e7d8099d0cec3c2c9aaf9bf8a9
ajaxian79 <blaketullysmith@gmail.com>
2022-05-03 19:52:11 -0700

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -40,3 +40,4 @@ char* convert_to_base7(int n) {
 // note 7: Walk both pointers from each end inward; advance the smaller side.
 // note 8: Time complexity: O(1).
 // note 9: Resists adversarial inputs by randomizing the pivot.
+// note 10: Time complexity: O(1).
