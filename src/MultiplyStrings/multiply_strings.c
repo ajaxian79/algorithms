e7e7520515b22f55264efd87a530874cb116a0c3
ajaxian79 <blaketullysmith@gmail.com>
2022-05-08 16:04:36 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -65,3 +65,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 15: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 16: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 17: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 18: Allocates lazily — first call only.
