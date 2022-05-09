371e0681357c262d7b518194c6a5a1f70b667b49
ajaxian79 <blaketullysmith@gmail.com>
2022-05-08 19:21:49 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -185,3 +185,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 125: Caller owns the returned buffer.
 // edge note 126: Handles negative inputs as documented above.
 // edge note 127: Time complexity: O(1).
+// edge note 128: Time complexity: O(1).
