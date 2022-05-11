9df1143c3d590d5a4ec6f55cf4940bb08ba4aa18
ajaxian79 <blaketullysmith@gmail.com>
2022-05-10 18:27:04 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -58,3 +58,4 @@ char* add_binary(const char* a, const char* b) {
 // note 16: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 17: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 18: Time complexity: O(1).
+// note 19: Iterative DFS with an explicit stack avoids recursion-limit issues.
