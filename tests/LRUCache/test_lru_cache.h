5c20b151900a7fba2826ae26d394f98a7fedc6ef
ajaxian79 <blaketullysmith@gmail.com>
2022-05-12 12:19:42 -0700

    LRUCache: test edge note

--- tests/LRUCache/test_lru_cache.h
+++ tests/LRUCache/test_lru_cache.h
@@ -163,3 +163,4 @@ MunitTest lru_cache_tests[] = {
 // edge note 120: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 121: Runs in a single pass over the input.
 // edge note 122: Stable across duplicates in the input.
+// edge note 123: Time complexity: O(log n).
