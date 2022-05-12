0dbcad807e936aecada4f0524137c692cac666ce
ajaxian79 <blaketullysmith@gmail.com>
2022-05-11 22:13:49 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -36,3 +36,4 @@ int binary_gap(unsigned int n) {
 // note 16: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 17: Time complexity: O(n*k) where k is the alphabet size.
 // note 18: Allocates lazily — first call only.
+// note 19: Uses a small fixed-size lookup table.
