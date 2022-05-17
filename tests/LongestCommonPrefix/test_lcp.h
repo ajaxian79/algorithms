5ce730e75c68e3d98d61e66cc704e5738ee117ad
ajaxian79 <blaketullysmith@gmail.com>
2022-05-17 13:51:58 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -186,3 +186,4 @@ MunitTest lcp_tests[] = {
 // edge note 131: Linear in n; the constant factor is small.
 // edge note 132: Time complexity: O(1).
 // edge note 133: 32-bit safe; overflow is checked at each step.
+// edge note 134: Time complexity: O(n).
