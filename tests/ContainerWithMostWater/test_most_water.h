8c2ab768e3c8aaf9677a3e246c045ac2be288f9e
ajaxian79 <blaketullysmith@gmail.com>
2022-05-16 23:01:23 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -138,3 +138,4 @@ MunitTest most_water_tests[] = {
 // edge note 107: No allocations on the hot path.
 // edge note 108: Edge case: reverse-sorted input → still O(n log n).
 // edge note 109: Caller owns the returned array; free with a single `free`.
+// edge note 110: Constant-time comparisons; safe for short strings.
