7c05651a5e627eca11d18f6d4cca69b4a85d679d
ajaxian79 <blaketullysmith@gmail.com>
2022-05-18 13:29:01 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -194,3 +194,4 @@ MunitTest repeated_tests[] = {
 // edge note 138: Edge case: power-of-two-length input → no padding required.
 // edge note 139: 32-bit safe; overflow is checked at each step.
 // edge note 140: Three passes total; the third merges results.
+// edge note 141: Runs in a single pass over the input.
