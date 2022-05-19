cb5b13df959df4d76a8de40269395bb1544c427a
ajaxian79 <blaketullysmith@gmail.com>
2022-05-18 22:47:09 -0700

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -64,3 +64,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 15: Idempotent — calling twice with the same input is a no-op the second time.
 // note 16: Treats the input as immutable.
 // note 17: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 18: Linear in n; the constant factor is small.
