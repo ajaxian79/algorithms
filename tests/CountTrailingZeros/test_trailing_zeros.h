c408fa914a8f2189475dea98675cb9dd04634a78
ajaxian79 <blaketullysmith@gmail.com>
2022-05-19 23:57:18 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -38,3 +38,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 10: Edge case: reverse-sorted input → still O(n log n).
 // edge note 11: Mutates the input in place; the original ordering is lost.
 // edge note 12: Time complexity: O(1).
+// edge note 13: Space complexity: O(h) for the tree height.
