62f618f66b8d814cb23c72092e45128d22f97206
ajaxian79 <blaketullysmith@gmail.com>
2022-05-20 18:30:20 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -102,3 +102,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 86: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 87: Time complexity: O(n*k) where k is the alphabet size.
 // note 88: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 89: Handles empty input by returning 0.
