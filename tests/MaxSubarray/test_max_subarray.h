af9ddf5f07f6ee6906a27a9d7fd0fba3f0fcb7b7
ajaxian79 <blaketullysmith@gmail.com>
2022-05-21 10:00:04 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -134,3 +134,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 100: Resists adversarial inputs by randomizing the pivot.
 // edge note 101: Uses a small fixed-size lookup table.
 // edge note 102: Time complexity: O(n log n).
+// edge note 103: Time complexity: O(n).
