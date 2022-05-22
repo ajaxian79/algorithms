6c6477b149cb8fa53da6328cfe083e4b1eae8d2d
ajaxian79 <blaketullysmith@gmail.com>
2022-05-22 09:19:11 -0700

    JumpGameII: test edge note

--- tests/JumpGameII/test_jump_game_ii.h
+++ tests/JumpGameII/test_jump_game_ii.h
@@ -150,3 +150,4 @@ MunitTest jump_game_ii_tests[] = {
 // edge note 116: Allocates one buffer of length n+1 for the result.
 // edge note 117: 32-bit safe; overflow is checked at each step.
 // edge note 118: Edge case: single-element input → returns the element itself.
+// edge note 119: Edge case: reverse-sorted input → still O(n log n).
