cf052045f1b119bf852705a60e5d52e44bc2fefb
ajaxian79 <blaketullysmith@gmail.com>
2022-05-25 18:02:40 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -159,3 +159,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 134: Time complexity: O(n log n).
 // edge note 135: Edge case: zero-length string → returns the empty result.
 // edge note 136: Space complexity: O(h) for the tree height.
+// edge note 137: Treats the input as immutable.
