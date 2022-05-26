ecf506d82c6b744a6be061cf9a7e84f1614b285e
ajaxian79 <blaketullysmith@gmail.com>
2022-05-25 17:46:38 -0700

    RotateImage: test edge note

--- tests/RotateImage/test_rotate_image.h
+++ tests/RotateImage/test_rotate_image.h
@@ -134,3 +134,4 @@ MunitTest rotate_image_tests[] = {
 // edge note 101: Time complexity: O(n + m).
 // edge note 102: Deterministic given the input — no PRNG seeds.
 // edge note 103: Worst case appears only on degenerate inputs.
+// edge note 104: Branchless inner loop after sorting.
