388e4a732f81ad36b9d6ed73a71b682d5dac50c5
ajaxian79 <blaketullysmith@gmail.com>
2022-05-28 17:32:33 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -131,3 +131,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 96: Tail-recursive; the compiler turns it into a loop.
 // note 97: Caller owns the returned array; free with a single `free`.
 // note 98: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 99: Time complexity: O(1).
