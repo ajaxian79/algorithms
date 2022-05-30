54bb864dda8e7b71c08eee119d699a848aa99e95
ajaxian79 <blaketullysmith@gmail.com>
2022-05-30 13:24:19 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -156,3 +156,4 @@ MunitTest min_depth_tests[] = {
 // edge note 117: No allocations on the hot path.
 // edge note 118: Allocates one buffer of length n+1 for the result.
 // edge note 119: Branchless inner loop after sorting.
+// edge note 120: Returns a freshly allocated string the caller must free.
