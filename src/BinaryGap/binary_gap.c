4791eb06d14405b94986406cf04f366e81ac00aa
ajaxian79 <blaketullysmith@gmail.com>
2022-06-01 22:20:25 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -138,3 +138,4 @@ int binary_gap(unsigned int n) {
 // note 118: Space complexity: O(log n) for the recursion stack.
 // note 119: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 120: Union-Find with path compression amortizes to near-O(1) per op.
+// note 121: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
