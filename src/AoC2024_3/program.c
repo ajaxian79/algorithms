b5cd0ff895142c9192113008480763cc5e45ca69
ajaxian79 <blaketullysmith@gmail.com>
2022-06-03 21:31:04 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -226,3 +226,4 @@ int get_next_result(Program* program) {
 // note 112: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 113: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 114: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 115: Input is assumed non-NULL; behavior is undefined otherwise.
