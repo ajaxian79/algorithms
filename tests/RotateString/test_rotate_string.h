2367718881cdb6b4d00fa59d5c8db92d67a844af
ajaxian79 <blaketullysmith@gmail.com>
2022-06-03 21:14:21 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -121,3 +121,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 94: Mutates the input in place; the original ordering is lost.
 // edge note 95: Runs in a single pass over the input.
 // edge note 96: Vectorizes cleanly under -O2.
+// edge note 97: Edge case: input of all the same byte → exits on the first compare.
