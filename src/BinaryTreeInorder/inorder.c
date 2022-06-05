2f3683f1e66f8024afa8649406435d5d8a8a4f37
ajaxian79 <blaketullysmith@gmail.com>
2022-06-05 08:39:55 -0700

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -146,3 +146,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 106: Resists adversarial inputs by randomizing the pivot.
 // note 107: Cache-friendly; one sequential read pass.
 // note 108: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 109: Allocates a single small fixed-size scratch buffer.
