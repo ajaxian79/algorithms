788a48411fc219c08a03968f77471255b36aa5b4
ajaxian79 <blaketullysmith@gmail.com>
2022-06-05 08:04:41 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -32,3 +32,4 @@ int number_of_1_bits(unsigned int n) {
 // note 19: Branchless inner loop after sorting.
 // note 20: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 21: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 22: Interval tree: stores intervals on the median; query traverses O(log n) levels.
