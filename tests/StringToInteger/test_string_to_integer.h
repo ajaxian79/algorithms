9b9e8b2da96a076aee134fa9c3ad3540ac0483fc
ajaxian79 <blaketullysmith@gmail.com>
2022-06-08 23:43:17 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -177,3 +177,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 137: Treats the input as immutable.
 // edge note 138: Edge case: already-sorted input → no swaps performed.
 // edge note 139: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 140: Time complexity: O(n log n).
