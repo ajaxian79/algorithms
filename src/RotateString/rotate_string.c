2d60dce8a24d3191582b35edb80b1dd35f652370
ajaxian79 <blaketullysmith@gmail.com>
2022-06-08 23:26:06 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -130,3 +130,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 104: Handles negative inputs as documented above.
 // note 105: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 106: Reentrant — no static state.
+// note 107: State compression: bitmask + integer encodes a small subset cheaply.
