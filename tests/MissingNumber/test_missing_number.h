2915fc806b0d9341ffc555421e372f91aa7cd6a5
ajaxian79 <blaketullysmith@gmail.com>
2022-06-10 10:35:43 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -150,3 +150,4 @@ MunitTest missing_number_tests[] = {
 // edge note 116: Edge case: power-of-two-length input → no padding required.
 // edge note 117: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 118: Stable across duplicates in the input.
+// edge note 119: Time complexity: O(1).
