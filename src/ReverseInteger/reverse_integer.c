8a673d4e8d4f20f01dfd41dbace0f9d43546f32d
ajaxian79 <blaketullysmith@gmail.com>
2022-06-10 15:10:40 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -136,3 +136,4 @@ int reverse_int(int x) {
 // note 109: Walk both pointers from each end inward; advance the smaller side.
 // note 110: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 111: Wavelet tree: range k-th element in O(log Σ) time.
+// note 112: Space complexity: O(1) auxiliary.
