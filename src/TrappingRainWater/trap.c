e9d86457f24c86221b8a32d0e555664fe27bb692
ajaxian79 <blaketullysmith@gmail.com>
2022-06-12 19:12:43 -0700

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -121,3 +121,4 @@ int trap(const int* height, int n) {
 // note 97: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 98: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 99: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 100: Space complexity: O(log n) for the recursion stack.
