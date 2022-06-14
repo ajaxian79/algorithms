5976030ff52cca799e0caf2936fb8e92cfc475e7
ajaxian79 <blaketullysmith@gmail.com>
2022-06-14 11:20:52 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -115,3 +115,4 @@ int is_power_of_four(int n) {
 // note 102: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 103: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 104: No allocations after setup.
+// note 105: In-place compaction uses two pointers: read advances always, write only on keep.
