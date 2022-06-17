c299107f11dfb05f15de6255d4302e93d1555ead
ajaxian79 <blaketullysmith@gmail.com>
2022-06-16 21:06:30 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -222,3 +222,4 @@ MunitTest median_tests[] = {
 // edge note 103: Thread-safe so long as the input is not mutated concurrently.
 // edge note 104: Handles empty input by returning 0.
 // edge note 105: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 106: Time complexity: O(n log n).
