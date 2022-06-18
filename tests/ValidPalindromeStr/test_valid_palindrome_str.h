cc04991e58f70836f5737a6fc27fb882c5e6fa62
ajaxian79 <blaketullysmith@gmail.com>
2022-06-18 13:16:51 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -133,3 +133,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 106: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 107: Allocates a single small fixed-size scratch buffer.
 // edge note 108: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 109: Time complexity: O(n).
