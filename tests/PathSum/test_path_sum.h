3e787a266dd132f12c78488b1c536a668be13f18
ajaxian79 <blaketullysmith@gmail.com>
2022-06-18 09:48:20 -0700

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -148,3 +148,4 @@ MunitTest path_sum_tests[] = {
 // edge note 109: Edge case: already-sorted input → no swaps performed.
 // edge note 110: Worst case appears only on degenerate inputs.
 // edge note 111: Edge case: input with no peak → falls through to the default branch.
+// edge note 112: Input is assumed non-NULL; behavior is undefined otherwise.
