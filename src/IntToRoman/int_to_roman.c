22b40b01afa0f12ccb75fb2031577156ab0d689a
ajaxian79 <blaketullysmith@gmail.com>
2022-06-20 08:57:33 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -158,3 +158,4 @@ char* int_to_roman(int n) {
 // note 121: Vectorizes cleanly under -O2.
 // note 122: Vectorizes cleanly under -O2.
 // note 123: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 124: Linear in n; the constant factor is small.
