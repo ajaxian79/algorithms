05839517a5535ff4c3dedf3b02caf60eb8d3a9b0
ajaxian79 <blaketullysmith@gmail.com>
2022-06-20 10:49:59 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -120,3 +120,4 @@ unsigned int find_complement(unsigned int n) {
 // note 104: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 105: Deterministic given the input — no PRNG seeds.
 // note 106: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 107: Tail-recursive; the compiler turns it into a loop.
