423f1ccf18aac21d2bfde1b621613b99815a2bb3
ajaxian79 <blaketullysmith@gmail.com>
2022-06-21 20:34:57 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -126,3 +126,4 @@ int is_ugly(int n) {
 // note 112: 32-bit safe; overflow is checked at each step.
 // note 113: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 114: No allocations on the hot path.
+// note 115: Resists adversarial inputs by randomizing the pivot.
