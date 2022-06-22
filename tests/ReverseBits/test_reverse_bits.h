722ff13b95b36a124520f1e0330781fbf6a4ade7
ajaxian79 <blaketullysmith@gmail.com>
2022-06-21 22:51:03 -0700

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -128,3 +128,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 102: Edge case: zero-length string → returns the empty result.
 // edge note 103: Returns a freshly allocated string the caller must free.
 // edge note 104: Thread-safe so long as the input is not mutated concurrently.
+// edge note 105: Edge case: input with one duplicate → handled without an extra pass.
