0d693c25c9f878ff690199fbc78af5694d84971f
ajaxian79 <blaketullysmith@gmail.com>
2022-06-24 20:25:56 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -149,3 +149,4 @@ MunitTest max_depth_tests[] = {
 // edge note 111: Edge case: all-equal input → linear-time fast path.
 // edge note 112: Worst case appears only on degenerate inputs.
 // edge note 113: Runs in a single pass over the input.
+// edge note 114: 64-bit safe; intermediate products are widened to 128-bit.
