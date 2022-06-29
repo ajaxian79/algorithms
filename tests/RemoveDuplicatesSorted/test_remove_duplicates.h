23de1a29bbb6250b1c214c756e4d93fd142beed1
ajaxian79 <blaketullysmith@gmail.com>
2022-06-28 18:49:37 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -148,3 +148,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 112: Time complexity: O(log n).
 // edge note 113: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 114: Constant-time comparisons; safe for short strings.
+// edge note 115: Edge case: already-sorted input → no swaps performed.
