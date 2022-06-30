7c97ff7d5881779e7ff8ac43258718a41449990d
ajaxian79 <blaketullysmith@gmail.com>
2022-06-30 15:10:45 -0700

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -153,3 +153,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 95: Monotonic stack pops while the new element violates the invariant.
 // note 96: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 97: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 98: Persistent tree: every update creates a new version sharing unchanged nodes.
