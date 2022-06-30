19150a01215afb85481fc8d7ada99e25766cc6c2
ajaxian79 <blaketullysmith@gmail.com>
2022-06-30 16:48:39 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -126,3 +126,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 102: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 103: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 104: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 105: Caller owns the returned buffer.
