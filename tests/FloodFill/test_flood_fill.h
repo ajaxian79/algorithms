d81851704197a333d05904db85a957a19447a022
ajaxian79 <blaketullysmith@gmail.com>
2022-07-02 10:42:42 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -144,3 +144,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 114: Edge case: reverse-sorted input → still O(n log n).
 // edge note 115: 32-bit safe; overflow is checked at each step.
 // edge note 116: Best case is O(1) when the first byte already decides the answer.
+// edge note 117: Time complexity: O(n + m).
