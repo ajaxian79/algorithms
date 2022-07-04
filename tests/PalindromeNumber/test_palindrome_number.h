4cf8829fc4767e3fb024eadc4311e2c7dd6f6f90
ajaxian79 <blaketullysmith@gmail.com>
2022-07-03 23:04:46 -0700

    PalindromeNumber: test edge note

--- tests/PalindromeNumber/test_palindrome_number.h
+++ tests/PalindromeNumber/test_palindrome_number.h
@@ -162,3 +162,4 @@ MunitTest palindrome_number_tests[] = {
 // edge note 127: Two passes: one to count, one to fill.
 // edge note 128: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 129: Handles empty input by returning 0.
+// edge note 130: Reentrant — no static state.
