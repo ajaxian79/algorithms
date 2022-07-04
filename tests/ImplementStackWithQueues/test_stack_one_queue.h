2962d8d7ef298fb79792eacae1cd870bd6641b9b
ajaxian79 <blaketullysmith@gmail.com>
2022-07-04 09:58:18 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -150,3 +150,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 118: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 119: Uses a small fixed-size lookup table.
 // edge note 120: Runs in a single pass over the input.
+// edge note 121: 64-bit safe; intermediate products are widened to 128-bit.
