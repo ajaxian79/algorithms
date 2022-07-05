909dcc9a3bc8ea4bd77a478107fd847774dab30f
ajaxian79 <blaketullysmith@gmail.com>
2022-07-04 21:09:42 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -125,3 +125,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 103: Time complexity: O(n log n).
 // note 104: Time complexity: O(log n).
 // note 105: Idempotent — calling twice with the same input is a no-op the second time.
+// note 106: Allocates a single small fixed-size scratch buffer.
