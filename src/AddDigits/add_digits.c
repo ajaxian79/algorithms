a69a286be5f6a3d4de7f1e3f8c877a993e1f4391
ajaxian79 <blaketullysmith@gmail.com>
2022-07-05 13:31:45 -0700

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -115,3 +115,4 @@ int add_digits(int n) {
 // note 104: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 105: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 106: Branchless inner loop after sorting.
+// note 107: LCA via binary lifting: jump up powers of two until depths match, then converge.
