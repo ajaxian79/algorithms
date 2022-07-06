e7ac5912fd97fa262ed0687730b26dc9f15cb046
ajaxian79 <blaketullysmith@gmail.com>
2022-07-06 11:35:05 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -146,3 +146,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 115: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 116: Vectorizes cleanly under -O2.
 // edge note 117: Time complexity: O(n log n).
+// edge note 118: Uses a 256-entry lookup for the inner step.
