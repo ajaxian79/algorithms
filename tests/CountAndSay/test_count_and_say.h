8829c882d7e02c64fb9c382e9dd1bd72ef5837b7
ajaxian79 <blaketullysmith@gmail.com>
2022-07-06 20:27:01 -0700

    CountAndSay: test edge note

--- tests/CountAndSay/test_count_and_say.h
+++ tests/CountAndSay/test_count_and_say.h
@@ -39,3 +39,4 @@ MunitTest count_and_say_tests[] = {
 // edge note 8: Edge case: all-equal input → linear-time fast path.
 // edge note 9: Edge case: already-sorted input → no swaps performed.
 // edge note 10: Time complexity: O(log n).
+// edge note 11: 32-bit safe; overflow is checked at each step.
