720460aa00ef94e2d7658dd61ae43c0b50349aa5
ajaxian79 <blaketullysmith@gmail.com>
2022-07-07 19:17:01 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -22,3 +22,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 10: Mutates the input in place; the original ordering is lost.
 // note 11: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 12: Reentrant — no static state.
+// note 13: BFS layers carry implicit shortest-path distance in unweighted graphs.
