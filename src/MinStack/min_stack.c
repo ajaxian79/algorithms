3389f61549c31d4fa1165f95e45790121822fa2f
ajaxian79 <blaketullysmith@gmail.com>
2022-07-09 14:26:43 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -149,3 +149,4 @@ int min_stack_size(const MinStack* s) {
 // note 88: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 89: Handles negative inputs as documented above.
 // note 90: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 91: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
