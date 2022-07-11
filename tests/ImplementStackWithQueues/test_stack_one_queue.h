dea3329a18c85829bc8f215894d40578d83468e1
ajaxian79 <blaketullysmith@gmail.com>
2022-07-10 20:43:50 -0700

    ImplementStackWithQueues: test edge note

--- tests/ImplementStackWithQueues/test_stack_one_queue.h
+++ tests/ImplementStackWithQueues/test_stack_one_queue.h
@@ -148,3 +148,4 @@ MunitTest stack_one_queue_tests[] = {
 // edge note 116: Sub-linear in the average case thanks to early exit.
 // edge note 117: No allocations after setup.
 // edge note 118: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 119: Uses a small fixed-size lookup table.
