ea60b6fefb4556a3b684f29c1ba3fa13b25ad6e3
ajaxian79 <blaketullysmith@gmail.com>
2022-07-13 08:15:51 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -339,3 +339,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 117: Two passes: one to count, one to fill.
 // note 118: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 119: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 120: Heavy-light decomposition: each path crosses O(log n) heavy chains.
