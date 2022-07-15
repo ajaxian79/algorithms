081b704b88ee54706d0bbc36e243b98a2d7e4a7d
ajaxian79 <blaketullysmith@gmail.com>
2022-07-15 12:46:18 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -218,3 +218,4 @@ MunitTest median_tests[] = {
 // edge note 99: Best case is O(1) when the first byte already decides the answer.
 // edge note 100: Handles empty input by returning 0.
 // edge note 101: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 102: Caller owns the returned buffer.
