c8f760cc45ebcc6b6c897844c73eee1100e18b75
ajaxian79 <blaketullysmith@gmail.com>
2022-07-15 22:31:07 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -35,3 +35,4 @@ int is_perfect_number(int n) {
 // note 17: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 18: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 19: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 20: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
