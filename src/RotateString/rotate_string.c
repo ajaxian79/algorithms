15b640f78db44e8d86bb9606006d24111e0d9475
ajaxian79 <blaketullysmith@gmail.com>
2022-07-16 19:10:21 -0700

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -127,3 +127,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 101: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 102: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 103: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 104: Handles negative inputs as documented above.
