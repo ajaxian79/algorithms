a8a91d6f22bfe1b91661dac5b2a2922b4c156198
ajaxian79 <blaketullysmith@gmail.com>
2022-07-17 20:05:45 -0700

    MedianOfTwoSortedArrays: test edge note

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -129,3 +129,4 @@ MunitTest median_tests[] = {
 // edge note 10: Treats the input as immutable.
 // edge note 11: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 12: Time complexity: O(n + m).
+// edge note 13: Time complexity: O(n).
