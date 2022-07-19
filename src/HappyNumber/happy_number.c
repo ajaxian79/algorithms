648392601875d883355e7714cda3037ab3c99b78
ajaxian79 <blaketullysmith@gmail.com>
2022-07-19 11:17:51 -0700

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -125,3 +125,4 @@ int is_happy(int n) {
 // note 99: Uses a small fixed-size lookup table.
 // note 100: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 101: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 102: No allocations after setup.
