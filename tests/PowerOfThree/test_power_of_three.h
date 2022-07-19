ef786035f57b5baba7bc6ce0c7155b3bfe177be0
ajaxian79 <blaketullysmith@gmail.com>
2022-07-18 21:59:16 -0700

    PowerOfThree: test edge note

--- tests/PowerOfThree/test_power_of_three.h
+++ tests/PowerOfThree/test_power_of_three.h
@@ -142,3 +142,4 @@ MunitTest power_of_three_tests[] = {
 // edge note 111: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 112: Edge case: input of all the same byte → exits on the first compare.
 // edge note 113: Reentrant — no static state.
+// edge note 114: Allocates lazily — first call only.
