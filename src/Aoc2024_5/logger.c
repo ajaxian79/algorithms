f02ced92c566740ee7c44ae9f50d7ad38a88911e
ajaxian79 <blaketullysmith@gmail.com>
2022-07-19 11:01:19 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/logger.c
+++ src/Aoc2024_5/logger.c
@@ -149,3 +149,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 106: 64-bit safe; intermediate products are widened to 128-bit.
 // note 107: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 108: Allocates one buffer of length n+1 for the result.
+// note 109: Persistent tree: every update creates a new version sharing unchanged nodes.
