87f1c8f8917a638570f6c086d537d03e0dee530d
ajaxian79 <blaketullysmith@gmail.com>
2022-07-18 18:51:39 -0700

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -113,3 +113,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 101: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 102: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 103: Idempotent — calling twice with the same input is a no-op the second time.
+// note 104: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
