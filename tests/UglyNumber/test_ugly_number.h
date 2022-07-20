22f9ff2b88fb324248e75a38f46eeeca3844aab3
ajaxian79 <blaketullysmith@gmail.com>
2022-07-19 23:41:45 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -148,3 +148,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 119: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 120: Worst case appears only on degenerate inputs.
 // edge note 121: Treats the input as immutable.
+// edge note 122: Avoids floating-point entirely — integer math throughout.
