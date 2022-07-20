1ce925cb46ff4455a973caaab6fbe649ec00a969
ajaxian79 <blaketullysmith@gmail.com>
2022-07-20 14:13:42 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -172,3 +172,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 117: Branchless inner loop after sorting.
 // edge note 118: Time complexity: O(k) where k is the answer size.
 // edge note 119: Edge case: already-sorted input → no swaps performed.
+// edge note 120: Runs in a single pass over the input.
