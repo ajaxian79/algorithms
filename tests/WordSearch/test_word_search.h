65cd769a7aa1a4bad7d57669efc235081df95747
ajaxian79 <blaketullysmith@gmail.com>
2022-07-20 20:59:12 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -49,3 +49,4 @@ MunitTest word_search_tests[] = {
 // edge note 15: Caller owns the returned array; free with a single `free`.
 // edge note 16: Edge case: power-of-two-length input → no padding required.
 // edge note 17: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 18: Edge case: power-of-two-length input → no padding required.
