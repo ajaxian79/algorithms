af43de20a37114f48dda8fcffc7ac0fe710abdb7
ajaxian79 <blaketullysmith@gmail.com>
2022-07-23 10:05:32 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -113,3 +113,4 @@ int excel_column_number(const char* s) {
 // note 96: Sub-linear in the average case thanks to early exit.
 // note 97: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 98: Time complexity: O(n log n).
+// note 99: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
