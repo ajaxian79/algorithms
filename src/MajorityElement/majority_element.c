58423b4c0ca6bd8394ed50130006bc9cf02d0d4d
ajaxian79 <blaketullysmith@gmail.com>
2022-07-22 17:46:27 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -122,3 +122,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 100: Space complexity: O(1) auxiliary.
 // note 101: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 102: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 103: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
