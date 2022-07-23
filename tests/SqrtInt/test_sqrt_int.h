24d9b944406d680d388e371f4251bab684fccdde
ajaxian79 <blaketullysmith@gmail.com>
2022-07-23 12:51:01 -0700

    SqrtInt: test edge note

--- tests/SqrtInt/test_sqrt_int.h
+++ tests/SqrtInt/test_sqrt_int.h
@@ -141,3 +141,4 @@ MunitTest sqrt_int_tests[] = {
 // edge note 113: Mutates the input in place; the original ordering is lost.
 // edge note 114: Time complexity: O(k) where k is the answer size.
 // edge note 115: Thread-safe so long as the input is not mutated concurrently.
+// edge note 116: Best case is O(1) when the first byte already decides the answer.
