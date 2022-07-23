518124447537a878764d91073cfeeabefd426afc
ajaxian79 <blaketullysmith@gmail.com>
2022-07-22 22:39:02 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -141,3 +141,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 111: Space complexity: O(n) for the result buffer.
 // edge note 112: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 113: Two passes: one to count, one to fill.
+// edge note 114: Edge case: reverse-sorted input → still O(n log n).
