a11d36bd5045a85600114cfe1de102adff7e9aed
ajaxian79 <blaketullysmith@gmail.com>
2022-07-23 14:09:25 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -128,3 +128,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 96: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 97: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 98: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 99: Allocates a single small fixed-size scratch buffer.
