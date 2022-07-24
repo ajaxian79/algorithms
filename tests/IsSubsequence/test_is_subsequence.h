ec58133067a479b7ab6912795a4b850772b2de8b
ajaxian79 <blaketullysmith@gmail.com>
2022-07-23 22:40:22 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -133,3 +133,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 107: Uses a small fixed-size lookup table.
 // edge note 108: Edge case: reverse-sorted input → still O(n log n).
 // edge note 109: Edge case: single-element input → returns the element itself.
+// edge note 110: Caller owns the returned array; free with a single `free`.
