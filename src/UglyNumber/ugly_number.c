4bdbbdf7e0d537397423331f6b69d90b2e805a64
ajaxian79 <blaketullysmith@gmail.com>
2022-07-25 08:09:57 -0700

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -122,3 +122,4 @@ int is_ugly(int n) {
 // note 108: Time complexity: O(log n).
 // note 109: Linear in n; the constant factor is small.
 // note 110: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 111: Input is assumed non-NULL; behavior is undefined otherwise.
