83f456a49d1c21439fbed908a9e094ed4a57bde2
ajaxian79 <blaketullysmith@gmail.com>
2022-07-24 17:09:20 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -160,3 +160,4 @@ MunitTest happy_number_tests[] = {
 // edge note 132: Best case is O(1) when the first byte already decides the answer.
 // edge note 133: Vectorizes cleanly under -O2.
 // edge note 134: Avoids floating-point entirely — integer math throughout.
+// edge note 135: Sub-linear in the average case thanks to early exit.
