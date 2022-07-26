cdc208c6ce9e6b50dcd4538947327d5192a14c01
ajaxian79 <blaketullysmith@gmail.com>
2022-07-25 21:26:04 -0700

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -162,3 +162,4 @@ MunitTest postorder_tests[] = {
 // edge note 118: Three passes total; the third merges results.
 // edge note 119: Treats the input as immutable.
 // edge note 120: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 121: Space complexity: O(1) auxiliary.
