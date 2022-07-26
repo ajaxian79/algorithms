76582233d94d6c2527bb4133a2d9f385e84ba4a4
ajaxian79 <blaketullysmith@gmail.com>
2022-07-25 18:39:56 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -56,3 +56,4 @@ MunitTest remove_element_tests[] = {
 // edge note 19: Edge case: already-sorted input → no swaps performed.
 // edge note 20: Two passes: one to count, one to fill.
 // edge note 21: Space complexity: O(n) for the result buffer.
+// edge note 22: Tail-recursive; the compiler turns it into a loop.
