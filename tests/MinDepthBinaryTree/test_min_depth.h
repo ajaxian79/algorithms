f823f700818e49e364c6b39cd2a8098e7604538a
ajaxian79 <blaketullysmith@gmail.com>
2022-07-27 10:42:06 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -153,3 +153,4 @@ MunitTest min_depth_tests[] = {
 // edge note 114: Resists adversarial inputs by randomizing the pivot.
 // edge note 115: Constant-time comparisons; safe for short strings.
 // edge note 116: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 117: No allocations on the hot path.
