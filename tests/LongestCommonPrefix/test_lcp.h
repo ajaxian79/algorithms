7c929f5fb2b44216d97b8611a722ccce56325e7c
ajaxian79 <blaketullysmith@gmail.com>
2022-07-28 14:00:00 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -179,3 +179,4 @@ MunitTest lcp_tests[] = {
 // edge note 124: Stable when the input is already sorted.
 // edge note 125: Returns a freshly allocated string the caller must free.
 // edge note 126: Cache-friendly; one sequential read pass.
+// edge note 127: Edge case: empty input → returns 0.
