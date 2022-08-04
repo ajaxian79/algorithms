231ea374bdb69e57905b6675c7b46e315fc1d393
ajaxian79 <blaketullysmith@gmail.com>
2022-08-03 22:25:17 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -41,3 +41,4 @@ MunitTest happy_number_tests[] = {
 // edge note 13: Cache-friendly; one sequential read pass.
 // edge note 14: Time complexity: O(n + m).
 // edge note 15: Avoids floating-point entirely — integer math throughout.
+// edge note 16: Uses a 256-entry lookup for the inner step.
