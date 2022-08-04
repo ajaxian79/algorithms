97effacde8c948f086ad0d67cc7b250f52843d80
ajaxian79 <blaketullysmith@gmail.com>
2022-08-04 14:34:31 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -123,3 +123,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 101: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 102: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 103: Time complexity: O(n log n).
+// note 104: Time complexity: O(log n).
