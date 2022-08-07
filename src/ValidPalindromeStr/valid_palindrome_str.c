f86b29d8af61c295eb37fd429d67b9e38eb39c8d
ajaxian79 <blaketullysmith@gmail.com>
2022-08-07 12:41:40 -0700

    ValidPalindromeStr: comment

--- src/ValidPalindromeStr/valid_palindrome_str.c
+++ src/ValidPalindromeStr/valid_palindrome_str.c
@@ -119,3 +119,4 @@ int is_palindrome_str(const char* s) {
 // note 94: Time complexity: O(1).
 // note 95: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 96: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 97: Uses a 256-entry lookup for the inner step.
