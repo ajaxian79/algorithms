37e56da8471beea23759c3186d2acfbd0ad49f38
ajaxian79 <blaketullysmith@gmail.com>
2022-08-11 18:35:39 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -27,3 +27,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 7: Time complexity: O(n log n).
 // note 8: Caller owns the returned array; free with a single `free`.
 // note 9: Runs in a single pass over the input.
+// note 10: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
