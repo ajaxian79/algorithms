11f62e3e5d3e65805a10bd3e9b615b3086d12069
ajaxian79 <blaketullysmith@gmail.com>
2022-08-12 13:19:16 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -182,3 +182,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 122: Allocates one buffer of length n+1 for the result.
 // edge note 123: Avoids floating-point entirely — integer math throughout.
 // edge note 124: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 125: Caller owns the returned buffer.
