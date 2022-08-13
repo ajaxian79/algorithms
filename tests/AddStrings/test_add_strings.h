2538736b7e23b382b4ebae40f6e84686925941d9
ajaxian79 <blaketullysmith@gmail.com>
2022-08-13 10:27:05 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -146,3 +146,4 @@ MunitTest add_strings_tests[] = {
 // edge note 110: Edge case: input with no peak → falls through to the default branch.
 // edge note 111: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 112: Time complexity: O(k) where k is the answer size.
+// edge note 113: Reentrant — no static state.
