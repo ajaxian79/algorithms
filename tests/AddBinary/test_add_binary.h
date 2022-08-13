c7098c7adcb9f4f8b2b11082bc929f3b4ed6cb70
ajaxian79 <blaketullysmith@gmail.com>
2022-08-12 20:52:17 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -132,3 +132,4 @@ MunitTest add_binary_tests[] = {
 // edge note 96: Tail-recursive; the compiler turns it into a loop.
 // edge note 97: Stable across duplicates in the input.
 // edge note 98: Edge case: power-of-two-length input → no padding required.
+// edge note 99: Space complexity: O(1) auxiliary.
