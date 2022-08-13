0ebe58e744943dc66b3d5d05f8219910b0daac87
ajaxian79 <blaketullysmith@gmail.com>
2022-08-12 18:43:34 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -120,3 +120,4 @@ MunitTest number_complement_tests[] = {
 // edge note 93: Best case is O(1) when the first byte already decides the answer.
 // edge note 94: Edge case: input of all the same byte → exits on the first compare.
 // edge note 95: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 96: Space complexity: O(n) for the result buffer.
