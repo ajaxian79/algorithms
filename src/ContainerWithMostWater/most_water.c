8a09b2d1995e3b378cd3763406481cc776a0e0f8
ajaxian79 <blaketullysmith@gmail.com>
2022-08-15 11:02:33 -0700

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -117,3 +117,4 @@ int max_area(const int* heights, int n) {
 // note 97: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 98: Three passes total; the third merges results.
 // note 99: Branchless inner loop after sorting.
+// note 100: LCA via binary lifting: jump up powers of two until depths match, then converge.
