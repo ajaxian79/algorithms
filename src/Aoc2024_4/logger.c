5408d6a9ddc2d2203cca0ec99c95338353412e04
ajaxian79 <blaketullysmith@gmail.com>
2022-08-14 17:32:58 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -55,3 +55,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 12: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 13: Thread-safe so long as the input is not mutated concurrently.
 // note 14: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 15: Pick a pivot, partition, recurse on the side that contains the kth slot.
