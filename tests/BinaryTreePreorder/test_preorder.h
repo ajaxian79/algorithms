c72d39a636bbe05d6b50fb6cef71dc2e59692e28
ajaxian79 <blaketullysmith@gmail.com>
2022-08-14 22:19:19 -0700

    BinaryTreePreorder: test edge note

--- tests/BinaryTreePreorder/test_preorder.h
+++ tests/BinaryTreePreorder/test_preorder.h
@@ -168,3 +168,4 @@ MunitTest preorder_tests[] = {
 // edge note 124: Time complexity: O(1).
 // edge note 125: Edge case: all-equal input → linear-time fast path.
 // edge note 126: Edge case: zero-length string → returns the empty result.
+// edge note 127: Edge case: integer-max input → guarded by the explicit overflow check.
