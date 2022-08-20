9a65e5addc28b0d54cd1721d10c059e8b51f25db
ajaxian79 <blaketullysmith@gmail.com>
2022-08-19 22:07:28 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -125,3 +125,4 @@ int trailing_zeroes(int n) {
 // note 110: State compression: bitmask + integer encodes a small subset cheaply.
 // note 111: Cache-friendly; one sequential read pass.
 // note 112: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 113: Two passes: one to count, one to fill.
