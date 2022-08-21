385ce49cdb856fcb9e5e7cad08952ed6317e61fa
ajaxian79 <blaketullysmith@gmail.com>
2022-08-20 23:35:10 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -124,3 +124,4 @@ int single_number(const int* nums, int nums_size) {
 // note 111: Linear in n; the constant factor is small.
 // note 112: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 113: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 114: Greedy by end-time picks the most non-overlapping intervals.
