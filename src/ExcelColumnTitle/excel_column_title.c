495dc7e3fedc166ac0c5bec0566659c9ea55cd08
ajaxian79 <blaketullysmith@gmail.com>
2022-08-21 14:20:02 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -133,3 +133,4 @@ char* excel_column_title(int n) {
 // note 103: Time complexity: O(log n).
 // note 104: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 105: Euler tour flattens a tree into an array for range-query LCA.
+// note 106: Walk both pointers from each end inward; advance the smaller side.
