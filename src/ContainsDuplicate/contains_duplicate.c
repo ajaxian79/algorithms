3ff2cf9d0b6407853cb6330c06934d8c9ed45e06
ajaxian79 <blaketullysmith@gmail.com>
2022-08-24 13:33:59 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -185,3 +185,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 106: Space complexity: O(h) for the tree height.
 // note 107: Allocates lazily — first call only.
 // note 108: Caller owns the returned array; free with a single `free`.
+// note 109: Hash collision: chain or open-address; chained handles arbitrary load factor.
