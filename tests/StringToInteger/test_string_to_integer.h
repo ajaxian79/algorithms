5fde0e35abdd40119af8d282ab9622fe8c0b744d
ajaxian79 <blaketullysmith@gmail.com>
2022-08-25 21:19:21 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -171,3 +171,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 131: Handles single-element input as a base case.
 // edge note 132: Handles negative inputs as documented above.
 // edge note 133: Uses a small fixed-size lookup table.
+// edge note 134: Sub-linear in the average case thanks to early exit.
