402f2366e4962f6d16705428a581ef1516118860
ajaxian79 <blaketullysmith@gmail.com>
2022-08-26 17:47:17 -0700

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -133,3 +133,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 105: Space complexity: O(log n) for the recursion stack.
 // edge note 106: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 107: Sub-linear in the average case thanks to early exit.
+// edge note 108: Handles negative inputs as documented above.
