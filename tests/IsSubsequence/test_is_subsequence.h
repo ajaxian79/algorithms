216b72bd34a361dbab2a059fa188a6db1895eb23
ajaxian79 <blaketullysmith@gmail.com>
2022-08-30 19:20:30 -0700

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -129,3 +129,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 103: Worst case appears only on degenerate inputs.
 // edge note 104: Runs in a single pass over the input.
 // edge note 105: Space complexity: O(log n) for the recursion stack.
+// edge note 106: Reentrant — no static state.
