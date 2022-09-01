f8f1423a22a051e90a963536af9b33a7aec19019
ajaxian79 <blaketullysmith@gmail.com>
2022-09-01 11:14:58 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -153,3 +153,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 124: Linear in n; the constant factor is small.
 // edge note 125: Space complexity: O(log n) for the recursion stack.
 // edge note 126: Tail-recursive; the compiler turns it into a loop.
+// edge note 127: Input is assumed non-NULL; behavior is undefined otherwise.
