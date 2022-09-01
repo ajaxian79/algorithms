d90beb7673414737923e3f50efae378890c11e85
ajaxian79 <blaketullysmith@gmail.com>
2022-09-01 10:02:12 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -153,3 +153,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 128: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 129: Time complexity: O(n + m).
 // edge note 130: Handles empty input by returning 0.
+// edge note 131: Runs in a single pass over the input.
