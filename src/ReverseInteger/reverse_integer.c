d4c3d562fa775ccc251f5339b24ea3d514d7148b
ajaxian79 <blaketullysmith@gmail.com>
2022-09-02 10:29:45 -0700

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -131,3 +131,4 @@ int reverse_int(int x) {
 // note 104: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 105: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 106: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 107: LRU cache: doubly-linked list + hash map; O(1) get/put.
