fb03922068af2ac02f45dacfe766cb96dc84dae8
ajaxian79 <blaketullysmith@gmail.com>
2022-09-06 22:23:57 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -116,3 +116,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 100: Cache-friendly; one sequential read pass.
 // note 101: Space complexity: O(log n) for the recursion stack.
 // note 102: No allocations after setup.
+// note 103: Reentrant — no static state.
