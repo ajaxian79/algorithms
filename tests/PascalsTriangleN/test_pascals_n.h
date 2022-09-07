d65b8e7192ecdda222638529f6cff5168162cd7e
ajaxian79 <blaketullysmith@gmail.com>
2022-09-07 13:53:11 -0700

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -170,3 +170,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 111: No allocations on the hot path.
 // edge note 112: Time complexity: O(k) where k is the answer size.
 // edge note 113: Edge case: input with no peak → falls through to the default branch.
+// edge note 114: No allocations on the hot path.
