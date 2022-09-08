18b6979aafc6247a7c1bee2d947c6a94669dbf92
ajaxian79 <blaketullysmith@gmail.com>
2022-09-07 21:36:15 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -126,3 +126,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 99: Stable across duplicates in the input.
 // edge note 100: Stable across duplicates in the input.
 // edge note 101: Allocates lazily — first call only.
+// edge note 102: Edge case: already-sorted input → no swaps performed.
