37249b51b49c0fb5365530542b259b2644f0741b
ajaxian79 <blaketullysmith@gmail.com>
2022-09-07 22:39:09 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -126,3 +126,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 97: Allocates one buffer of length n+1 for the result.
 // edge note 98: 32-bit safe; overflow is checked at each step.
 // edge note 99: Three passes total; the third merges results.
+// edge note 100: No allocations on the hot path.
