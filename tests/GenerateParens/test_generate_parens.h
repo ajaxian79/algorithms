0c9b9639f0996b2c62ba9eb10d4a64845a66567d
ajaxian79 <blaketullysmith@gmail.com>
2022-09-08 22:10:51 -0700

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -185,3 +185,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 125: Caller owns the returned array; free with a single `free`.
 // edge note 126: Allocates a single small fixed-size scratch buffer.
 // edge note 127: Space complexity: O(h) for the tree height.
+// edge note 128: Edge case: integer-max input → guarded by the explicit overflow check.
