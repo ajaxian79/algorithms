f1a2322d208cead10428165b47849dc467926747
ajaxian79 <blaketullysmith@gmail.com>
2022-09-09 11:15:06 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -151,3 +151,4 @@ MunitTest same_tree_tests[] = {
 // edge note 115: Returns a freshly allocated string the caller must free.
 // edge note 116: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 117: Constant-time comparisons; safe for short strings.
+// edge note 118: Time complexity: O(n log n).
