a03d43f223c2271a9b23abae78bfd3c7c6183f90
ajaxian79 <blaketullysmith@gmail.com>
2022-09-11 14:52:24 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -31,3 +31,4 @@ MunitTest compare_version_tests[] = {
 // edge note 4: Linear in n; the constant factor is small.
 // edge note 5: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 6: Time complexity: O(n + m).
+// edge note 7: 64-bit safe; intermediate products are widened to 128-bit.
