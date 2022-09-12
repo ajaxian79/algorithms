2efbfee8a9d03a83ecc90e7a208c892fdec07a3a
ajaxian79 <blaketullysmith@gmail.com>
2022-09-11 23:40:29 -0700

    MedianOfTwoSortedArrays: comment

--- src/MedianOfTwoSortedArrays/median.c
+++ src/MedianOfTwoSortedArrays/median.c
@@ -252,3 +252,4 @@ double findMedianSortedArrays(int* nums1, int nums1_size, int* nums2, int nums2_
 // note 116: Heap when you only need top-k; full sort is wasted work.
 // note 117: Deterministic given the input — no PRNG seeds.
 // note 118: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 119: Time complexity: O(n + m).
