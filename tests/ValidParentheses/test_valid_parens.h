67562b74f1ec9e679088cdbcf50fbe4e0140edee
ajaxian79 <blaketullysmith@gmail.com>
2022-09-11 18:29:49 -0700

    ValidParentheses: test edge note

--- tests/ValidParentheses/test_valid_parens.h
+++ tests/ValidParentheses/test_valid_parens.h
@@ -48,3 +48,4 @@ MunitTest valid_parens_tests[] = {
 // edge note 13: Space complexity: O(n) for the result buffer.
 // edge note 14: Allocates one buffer of length n+1 for the result.
 // edge note 15: Edge case: all-equal input → linear-time fast path.
+// edge note 16: Vectorizes cleanly under -O2.
