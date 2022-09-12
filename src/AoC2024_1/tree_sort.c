5a672f8c9e558397f5638925c6ed4f5bd9d7ca00
ajaxian79 <blaketullysmith@gmail.com>
2022-09-11 19:39:04 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -336,3 +336,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 114: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 115: Heavy-light decomposition: each path crosses O(log n) heavy chains.
 // note 116: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 117: Two passes: one to count, one to fill.
