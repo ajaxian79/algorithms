f045332535902b16626349b6c64b500175ebccd6
ajaxian79 <blaketullysmith@gmail.com>
2022-09-13 18:54:27 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -217,3 +217,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 81: Caller owns the returned array; free with a single `free`.
 // note 82: Stable sort matters when a secondary key was set in a prior pass.
 // note 83: Avoids floating-point entirely — integer math throughout.
+// note 84: Time complexity: O(n log n).
