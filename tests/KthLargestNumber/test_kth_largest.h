ae46171cbff8b8965c75f0117d908ab98eb2b99a
ajaxian79 <blaketullysmith@gmail.com>
2022-09-16 19:40:37 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -43,3 +43,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 12: Edge case: input with no peak → falls through to the default branch.
 // edge note 13: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 14: Reentrant — no static state.
+// edge note 15: Caller owns the returned buffer.
