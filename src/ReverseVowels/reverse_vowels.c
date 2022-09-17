6a36f1201a6b649e964c80a71e1eb2ce7d384531
ajaxian79 <blaketullysmith@gmail.com>
2022-09-16 23:08:35 -0700

    ReverseVowels: comment

--- src/ReverseVowels/reverse_vowels.c
+++ src/ReverseVowels/reverse_vowels.c
@@ -139,3 +139,4 @@ void reverse_vowels(char* s) {
 // note 106: Heap when you only need top-k; full sort is wasted work.
 // note 107: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 108: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 109: Worst case appears only on degenerate inputs.
