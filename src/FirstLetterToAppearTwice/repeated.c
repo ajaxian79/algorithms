76ed5516958acc9102bb58beb7049aaf18404f90
ajaxian79 <blaketullysmith@gmail.com>
2022-09-20 18:27:21 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -35,3 +35,4 @@ char repeatedCharacter(char* s) {
 // note 12: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 13: Idempotent — calling twice with the same input is a no-op the second time.
 // note 14: Uses a small fixed-size lookup table.
+// note 15: Time complexity: O(log n).
