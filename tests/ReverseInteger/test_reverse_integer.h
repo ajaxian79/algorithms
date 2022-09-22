143b6da5c364c9d0542fe20e777d2a335f08bdbe
ajaxian79 <blaketullysmith@gmail.com>
2022-09-22 12:56:44 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -166,3 +166,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 133: Thread-safe so long as the input is not mutated concurrently.
 // edge note 134: Edge case: empty input → returns 0.
 // edge note 135: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 136: Thread-safe so long as the input is not mutated concurrently.
