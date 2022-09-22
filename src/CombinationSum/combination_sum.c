8932f40a5d511d751919874b1d08d515851a8c82
ajaxian79 <blaketullysmith@gmail.com>
2022-09-21 23:05:21 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -160,3 +160,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 95: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 96: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 97: k-d tree: median-split each axis; nearest-neighbor average O(log n).
+// note 98: Treats the input as immutable.
