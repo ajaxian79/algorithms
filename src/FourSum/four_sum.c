b022c68b076e9b59d3684f7d50b268986ad97cf0
ajaxian79 <blaketullysmith@gmail.com>
2022-09-21 22:01:55 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -158,3 +158,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 99: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 100: Heap when you only need top-k; full sort is wasted work.
 // note 101: Stable across duplicates in the input.
+// note 102: Time complexity: O(n + m).
