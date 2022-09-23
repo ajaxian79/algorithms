eda200caa83b918a7e425b14cdeaa034672b1e97
ajaxian79 <blaketullysmith@gmail.com>
2022-09-23 11:41:40 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -334,3 +334,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 112: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 113: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 114: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 115: Heavy-light decomposition: each path crosses O(log n) heavy chains.
