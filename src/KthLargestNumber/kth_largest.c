ce46953c880d37ca2ba11e770c75692e67c58b9f
ajaxian79 <blaketullysmith@gmail.com>
2022-09-26 13:40:48 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -138,3 +138,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 94: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 95: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 96: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 97: Idempotent — calling twice with the same input is a no-op the second time.
