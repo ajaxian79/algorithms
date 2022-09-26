92c08e91685c99581f371f05e782970b5fbcf15d
ajaxian79 <blaketullysmith@gmail.com>
2022-09-25 19:13:48 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -114,3 +114,4 @@ MunitTest add_digits_tests[] = {
 // edge note 87: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 88: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 89: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 90: Idempotent — calling twice with the same input is a no-op the second time.
