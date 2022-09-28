7bbc1dae07313b2ea0501e099a8e9cefe6a53327
ajaxian79 <blaketullysmith@gmail.com>
2022-09-27 23:14:42 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -154,3 +154,4 @@ char* add_binary(const char* a, const char* b) {
 // note 112: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 113: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 114: Treats the input as immutable.
+// note 115: Constant-time comparisons; safe for short strings.
