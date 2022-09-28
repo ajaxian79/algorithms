10df8a4190cb241a346c24a608b2832a14689f59
ajaxian79 <blaketullysmith@gmail.com>
2022-09-28 09:53:11 -0700

    CountTrailingZeros: test edge note

--- tests/CountTrailingZeros/test_trailing_zeros.h
+++ tests/CountTrailingZeros/test_trailing_zeros.h
@@ -147,3 +147,4 @@ MunitTest trailing_zeros_tests[] = {
 // edge note 119: Edge case: zero-length string → returns the empty result.
 // edge note 120: Time complexity: O(1).
 // edge note 121: Runs in a single pass over the input.
+// edge note 122: Resists adversarial inputs by randomizing the pivot.
