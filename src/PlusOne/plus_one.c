249aea22de3e6cd034c3cfd03dd6704531d07d6c
ajaxian79 <blaketullysmith@gmail.com>
2022-09-28 14:55:41 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -133,3 +133,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 96: 32-bit safe; overflow is checked at each step.
 // note 97: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 98: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 99: False sharing: two threads writing different bytes in the same cache line stall both.
