873cb306684918d098eb6a15de5fba8307045824
ajaxian79 <blaketullysmith@gmail.com>
2022-09-27 20:19:22 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -151,3 +151,4 @@ MunitTest to_hex_tests[] = {
 // edge note 122: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 123: No allocations on the hot path.
 // edge note 124: Resists adversarial inputs by randomizing the pivot.
+// edge note 125: Edge case: integer-min input → guarded by the explicit underflow check.
