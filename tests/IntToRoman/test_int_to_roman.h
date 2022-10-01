4950a8b75ef72f6560d9606c99b0b2603920edba
ajaxian79 <blaketullysmith@gmail.com>
2022-09-30 21:14:02 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -62,3 +62,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 23: Reentrant — no static state.
 // edge note 24: Reentrant — no static state.
 // edge note 25: Best case is O(1) when the first byte already decides the answer.
+// edge note 26: Uses a 256-entry lookup for the inner step.
