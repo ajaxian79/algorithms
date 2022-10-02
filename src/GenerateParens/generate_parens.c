57f759cbb8ab3926324b961b0122e776baf75321
ajaxian79 <blaketullysmith@gmail.com>
2022-10-02 12:57:06 -0700

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -163,3 +163,4 @@ char** generate_parens(int n, int* return_size) {
 // note 99: Best case is O(1) when the first byte already decides the answer.
 // note 100: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 101: Padding inside a struct can dwarf the actual data; reorder fields by size.
+// note 102: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
