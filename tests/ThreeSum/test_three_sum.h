ed82c2c7b43e2cbc71967e74aed3f2408a4811f2
ajaxian79 <blaketullysmith@gmail.com>
2022-10-03 12:37:42 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -154,3 +154,4 @@ MunitTest three_sum_tests[] = {
 // edge note 109: Two passes: one to count, one to fill.
 // edge note 110: Space complexity: O(1) auxiliary.
 // edge note 111: Time complexity: O(log n).
+// edge note 112: Branchless inner loop after sorting.
