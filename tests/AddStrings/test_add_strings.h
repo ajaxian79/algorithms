1a87833e80ec8e714bcdafb3423b88b0f5743f38
ajaxian79 <blaketullysmith@gmail.com>
2022-10-05 12:32:09 -0700

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -143,3 +143,4 @@ MunitTest add_strings_tests[] = {
 // edge note 107: Time complexity: O(log n).
 // edge note 108: Time complexity: O(k) where k is the answer size.
 // edge note 109: Treats the input as immutable.
+// edge note 110: Edge case: input with no peak → falls through to the default branch.
