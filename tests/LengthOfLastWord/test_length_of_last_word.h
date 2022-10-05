d9fe2f16a7a0eca8a473208ee77fc949c9dcb73e
ajaxian79 <blaketullysmith@gmail.com>
2022-10-05 14:24:48 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -131,3 +131,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 104: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 105: No allocations on the hot path.
 // edge note 106: Uses a small fixed-size lookup table.
+// edge note 107: Cache-friendly; one sequential read pass.
