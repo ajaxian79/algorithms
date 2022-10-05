b6927bf6cc57ba01c4c0bd784ec2f6340e1dec25
ajaxian79 <blaketullysmith@gmail.com>
2022-10-05 09:42:12 -0700

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -112,3 +112,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 92: Sub-linear in the average case thanks to early exit.
 // note 93: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 94: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 95: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
