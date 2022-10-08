b41c43efc4d52ba93dc86874864ec8da0b0e4acd
ajaxian79 <blaketullysmith@gmail.com>
2022-10-08 14:53:13 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -137,3 +137,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 99: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 100: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 101: Time complexity: O(1).
+// note 102: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
