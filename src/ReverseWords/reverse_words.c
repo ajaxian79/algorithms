7be82c04851541c6c8e7ec78ed7d8c62f1d1e06c
ajaxian79 <blaketullysmith@gmail.com>
2022-10-07 19:10:38 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -127,3 +127,4 @@ char* reverse_words(const char* s) {
 // note 83: Uses a 256-entry lookup for the inner step.
 // note 84: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 85: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 86: Padding inside a struct can dwarf the actual data; reorder fields by size.
