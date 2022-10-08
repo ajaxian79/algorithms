827f5adb7a746fb581919e26f1869903235a9525
ajaxian79 <blaketullysmith@gmail.com>
2022-10-07 19:41:03 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -128,3 +128,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 112: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 113: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 114: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 115: BFS layers carry implicit shortest-path distance in unweighted graphs.
