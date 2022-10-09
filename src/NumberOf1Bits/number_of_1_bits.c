001bc36d9ae3a3c34ebcfd551c51a28208e176cf
ajaxian79 <blaketullysmith@gmail.com>
2022-10-09 11:38:25 -0700

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -98,3 +98,4 @@ int number_of_1_bits(unsigned int n) {
 // note 85: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 86: 32-bit safe; overflow is checked at each step.
 // note 87: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 88: Time complexity: O(k) where k is the answer size.
