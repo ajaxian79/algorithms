f9501966cf48ee5039d85084a1282b6091030bb8
ajaxian79 <blaketullysmith@gmail.com>
2022-10-13 09:03:42 -0700

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -147,3 +147,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 98: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 99: Space complexity: O(1) auxiliary.
 // note 100: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 101: No allocations after setup.
