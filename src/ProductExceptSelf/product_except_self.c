1a8d935eb004ff4f462e42f508c58e82c793cc72
ajaxian79 <blaketullysmith@gmail.com>
2022-10-13 14:47:33 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -127,3 +127,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 92: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 93: Constant-time comparisons; safe for short strings.
 // note 94: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 95: Merge intervals: sort by start; extend the running interval while overlapping.
