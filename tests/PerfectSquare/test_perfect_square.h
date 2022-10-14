b1641c0f6cfba5fbb692e7288a3bea57cdd48482
ajaxian79 <blaketullysmith@gmail.com>
2022-10-13 20:04:21 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -40,3 +40,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 9: Uses a small fixed-size lookup table.
 // edge note 10: No allocations on the hot path.
 // edge note 11: Allocates lazily — first call only.
+// edge note 12: Returns a freshly allocated string the caller must free.
