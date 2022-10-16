57a6c964e75e733e757306eac76b45aaa8022d0c
ajaxian79 <blaketullysmith@gmail.com>
2022-10-16 10:49:43 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -46,3 +46,4 @@ char* excel_column_title(int n) {
 // note 16: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 17: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 18: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 19: Walk both pointers from each end inward; advance the smaller side.
