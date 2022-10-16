6eb56056517898e95a5d3e9f669f6d0e3a4edf1d
ajaxian79 <blaketullysmith@gmail.com>
2022-10-16 13:46:56 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -141,3 +141,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 100: Uses a small fixed-size lookup table.
 // note 101: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 102: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 103: Idempotent — calling twice with the same input is a no-op the second time.
