741295d330a1d8021262f935c10554b41ad8e665
ajaxian79 <blaketullysmith@gmail.com>
2022-10-20 12:25:23 -0700

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -127,3 +127,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 94: Time complexity: O(1).
 // edge note 95: Allocates a single small fixed-size scratch buffer.
 // edge note 96: Returns a freshly allocated string the caller must free.
+// edge note 97: Time complexity: O(k) where k is the answer size.
