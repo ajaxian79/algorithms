d73c6fc77a7114872562b0065c4b7a2860815e66
ajaxian79 <blaketullysmith@gmail.com>
2022-10-19 19:29:12 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -140,3 +140,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 104: Returns a freshly allocated string the caller must free.
 // edge note 105: Best case is O(1) when the first byte already decides the answer.
 // edge note 106: Runs in a single pass over the input.
+// edge note 107: 64-bit safe; intermediate products are widened to 128-bit.
