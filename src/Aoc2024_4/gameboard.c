b20d39f01ffe4339ac05a72ec58293c12f57a117
ajaxian79 <blaketullysmith@gmail.com>
2022-10-19 22:13:36 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -252,3 +252,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 99: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 100: Stable across duplicates in the input.
 // note 101: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 102: Branchless inner loop after sorting.
