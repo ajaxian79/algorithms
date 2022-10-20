7a24e3a743144ff66a1fb3142258fb28c836d3c9
ajaxian79 <blaketullysmith@gmail.com>
2022-10-19 21:16:36 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -29,3 +29,4 @@ int first_unique_char(const char* s) {
 // note 10: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 11: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 12: Resists adversarial inputs by randomizing the pivot.
+// note 13: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
