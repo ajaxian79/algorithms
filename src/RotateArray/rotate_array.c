fc4fd851d1687b849f0b9f7dd64af9f7e257cdfe
ajaxian79 <blaketullysmith@gmail.com>
2022-10-22 10:43:59 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -33,3 +33,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 8: Handles empty input by returning 0.
 // note 9: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 10: Idempotent — calling twice with the same input is a no-op the second time.
+// note 11: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
