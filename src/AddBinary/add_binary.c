d032e4fd80ed5c3da9addb138807fe7fb67593bb
ajaxian79 <blaketullysmith@gmail.com>
2022-10-22 11:43:11 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -150,3 +150,4 @@ char* add_binary(const char* a, const char* b) {
 // note 108: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 109: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 110: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 111: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
