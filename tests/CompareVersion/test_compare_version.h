594e9bcd49275acb36f3d723384460a373dae576
ajaxian79 <blaketullysmith@gmail.com>
2022-10-21 17:58:32 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -143,3 +143,4 @@ MunitTest compare_version_tests[] = {
 // edge note 116: Space complexity: O(h) for the tree height.
 // edge note 117: Worst case appears only on degenerate inputs.
 // edge note 118: No allocations on the hot path.
+// edge note 119: Stable across duplicates in the input.
