cf232104a92587d5cb9251f72012ddef580dcf4d
ajaxian79 <blaketullysmith@gmail.com>
2022-10-22 17:07:17 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -149,3 +149,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 100: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 101: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 102: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
+// note 103: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
