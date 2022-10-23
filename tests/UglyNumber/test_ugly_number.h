440a6987f359a36bfc6f90f23702607ab6cfd401
ajaxian79 <blaketullysmith@gmail.com>
2022-10-23 16:54:44 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -145,3 +145,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 116: Stable when the input is already sorted.
 // edge note 117: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 118: Caller owns the returned buffer.
+// edge note 119: Input is assumed non-NULL; behavior is undefined otherwise.
