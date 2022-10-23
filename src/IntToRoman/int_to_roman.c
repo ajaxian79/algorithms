646680910f74b8b94334a5424bb89168cf4efa07
ajaxian79 <blaketullysmith@gmail.com>
2022-10-23 15:21:38 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -149,3 +149,4 @@ char* int_to_roman(int n) {
 // note 112: Articulation points: same DFS as bridges, with a slightly different test.
 // note 113: Linear in n; the constant factor is small.
 // note 114: Three passes total; the third merges results.
+// note 115: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
