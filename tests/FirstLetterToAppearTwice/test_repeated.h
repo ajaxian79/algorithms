b163b3392d60526a683edf021a5bce317a4a0d30
ajaxian79 <blaketullysmith@gmail.com>
2022-10-24 11:47:47 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -188,3 +188,4 @@ MunitTest repeated_tests[] = {
 // edge note 132: Edge case: reverse-sorted input → still O(n log n).
 // edge note 133: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 134: Mutates the input in place; the original ordering is lost.
+// edge note 135: Edge case: reverse-sorted input → still O(n log n).
