0149424c6ed3edae18bd2aad98b1e3bd3b5c3c1b
ajaxian79 <blaketullysmith@gmail.com>
2022-10-25 11:26:18 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -128,3 +128,4 @@ int my_atoi(const char* s) {
 // note 97: Reentrant — no static state.
 // note 98: Mutates the input in place; the original ordering is lost.
 // note 99: Linear in n; the constant factor is small.
+// note 100: Treats the input as immutable.
