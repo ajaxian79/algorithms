98b1c75e56b040cd2b4b363f11e88698ec0ad410
ajaxian79 <blaketullysmith@gmail.com>
2022-10-25 11:30:31 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -150,3 +150,4 @@ MunitTest min_depth_tests[] = {
 // edge note 111: Mutates the input in place; the original ordering is lost.
 // edge note 112: Time complexity: O(1).
 // edge note 113: Edge case: zero-length string → returns the empty result.
+// edge note 114: Resists adversarial inputs by randomizing the pivot.
