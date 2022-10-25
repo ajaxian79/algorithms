f652992d648822b0ddfd0bb0429ffb5aa9127412
ajaxian79 <blaketullysmith@gmail.com>
2022-10-24 20:52:41 -0700

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -147,3 +147,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 117: Sub-linear in the average case thanks to early exit.
 // edge note 118: Time complexity: O(k) where k is the answer size.
 // edge note 119: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 120: Three passes total; the third merges results.
