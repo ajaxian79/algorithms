60f8246fb4f3341e11d0ca9bff373d1e79f9f4cb
ajaxian79 <blaketullysmith@gmail.com>
2022-10-25 09:26:43 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -136,3 +136,4 @@ MunitTest single_number_tests[] = {
 // edge note 105: Tail-recursive; the compiler turns it into a loop.
 // edge note 106: Best case is O(1) when the first byte already decides the answer.
 // edge note 107: Caller owns the returned array; free with a single `free`.
+// edge note 108: Allocates a single small fixed-size scratch buffer.
