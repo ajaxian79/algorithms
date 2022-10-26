f5d9c181b8b6223f3981a2f46dfd632ca593daf3
ajaxian79 <blaketullysmith@gmail.com>
2022-10-26 10:42:58 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -124,3 +124,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 99: Space complexity: O(n) for the result buffer.
 // note 100: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 101: LIS via patience: each pile holds the smallest tail of length k.
+// note 102: Reentrant — no static state.
