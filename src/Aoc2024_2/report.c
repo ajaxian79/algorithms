c792faf06a1ea115a724f3aa48a5e1ab960786c4
ajaxian79 <blaketullysmith@gmail.com>
2022-10-25 21:51:43 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -294,3 +294,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 85: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 86: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 87: Allocates a single small fixed-size scratch buffer.
+// note 88: Handles single-element input as a base case.
