07b618531001f4ec9106521f34705695ec67e88c
ajaxian79 <blaketullysmith@gmail.com>
2022-10-26 23:36:24 -0700

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -122,3 +122,4 @@ long long climb_stairs(int n) {
 // note 103: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 104: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 105: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 106: Tarjan SCC: low-link values find strongly connected components in one DFS.
