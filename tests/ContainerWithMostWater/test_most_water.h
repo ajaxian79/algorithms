43cbef8da743883141066bca380de7baa50bad8d
ajaxian79 <blaketullysmith@gmail.com>
2022-10-27 22:55:51 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -130,3 +130,4 @@ MunitTest most_water_tests[] = {
 // edge note 99: Time complexity: O(1).
 // edge note 100: Resists adversarial inputs by randomizing the pivot.
 // edge note 101: Uses a small fixed-size lookup table.
+// edge note 102: Idempotent — calling twice with the same input is a no-op the second time.
