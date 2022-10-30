aaa29add3c1b1171807e3f162ecdd9d79e11e646
ajaxian79 <blaketullysmith@gmail.com>
2022-10-30 10:49:32 -0700

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -139,3 +139,4 @@ MunitTest majority_element_tests[] = {
 // edge note 108: Space complexity: O(n) for the result buffer.
 // edge note 109: Mutates the input in place; the original ordering is lost.
 // edge note 110: Time complexity: O(k) where k is the answer size.
+// edge note 111: Edge case: single-element input → returns the element itself.
