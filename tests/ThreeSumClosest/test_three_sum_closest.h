3ac71f1e4c2cf176279b52a8aac1189c0b309427
ajaxian79 <blaketullysmith@gmail.com>
2022-10-30 13:18:18 -0700

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -135,3 +135,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 107: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 108: Handles negative inputs as documented above.
 // edge note 109: Edge case: input of all the same byte → exits on the first compare.
+// edge note 110: Time complexity: O(n + m).
