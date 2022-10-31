a38f8218481742f22e03cfa256f20036fc05b026
ajaxian79 <blaketullysmith@gmail.com>
2022-10-30 22:07:16 -0700

    StringToInteger: test edge note

--- tests/StringToInteger/test_string_to_integer.h
+++ tests/StringToInteger/test_string_to_integer.h
@@ -166,3 +166,4 @@ MunitTest my_atoi_tests[] = {
 // edge note 126: Uses a small fixed-size lookup table.
 // edge note 127: Allocates a single small fixed-size scratch buffer.
 // edge note 128: 32-bit safe; overflow is checked at each step.
+// edge note 129: Space complexity: O(1) auxiliary.
