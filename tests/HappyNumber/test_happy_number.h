0949c565b12e8062f67e2108c9d915565f2374df
ajaxian79 <blaketullysmith@gmail.com>
2022-11-01 23:04:54 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -39,3 +39,4 @@ MunitTest happy_number_tests[] = {
 // edge note 11: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 12: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 13: Cache-friendly; one sequential read pass.
+// edge note 14: Time complexity: O(n + m).
