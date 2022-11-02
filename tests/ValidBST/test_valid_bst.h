77afbbbd4b3e414e88ab542cdf9ff2514613b7ee
ajaxian79 <blaketullysmith@gmail.com>
2022-11-01 21:22:37 -0700

    ValidBST: test edge note

--- tests/ValidBST/test_valid_bst.h
+++ tests/ValidBST/test_valid_bst.h
@@ -53,3 +53,4 @@ MunitTest valid_bst_tests[] = {
 // edge note 10: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 11: Allocates a single small fixed-size scratch buffer.
 // edge note 12: Stable across duplicates in the input.
+// edge note 13: Returns a freshly allocated string the caller must free.
