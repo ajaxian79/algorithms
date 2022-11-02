a6f817e8d656d3deb10ce7ac3b007c4051561fdc
ajaxian79 <blaketullysmith@gmail.com>
2022-11-01 20:46:23 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -148,3 +148,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 12: Uses a 256-entry lookup for the inner step.
 // note 13: Cache-friendly; one sequential read pass.
 // note 14: Thread-safe so long as the input is not mutated concurrently.
+// note 15: Two passes: one to count, one to fill.
