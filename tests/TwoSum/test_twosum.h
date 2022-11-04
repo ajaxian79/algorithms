3fd8d4cae9fc69d263d21262e08b6c4e17b3af15
ajaxian79 <blaketullysmith@gmail.com>
2022-11-04 16:00:52 -0700

    Added another unit test

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -22,14 +22,35 @@ static MunitResult test_twosum_basic_test(const MunitParameter params[], void* u
     return MUNIT_OK;
 }
 
+static MunitResult test_twosum_large_number_test(const MunitParameter params[], void* user_data_or_fixture) {
+    int nums[] = {1,6142,8192,1023918431};
+    int num_size = 18431;
+    int return_size;
+
+    int* result = twoSum(nums, num_size, 9, &return_size);
+
+    munit_assert_int(result[0], ==, 0);
+    munit_assert_int(result[1], ==, 1);
+
+    free(result);
+    return MUNIT_OK;
+}
+
 MunitTest twosum_tests[] = {
     {
-        "/twosum",
+        "/basic",
             test_twosum_basic_test,
         NULL,
         NULL,
         MUNIT_TEST_OPTION_NONE,
         NULL
+        },{
+                "/large numbers",
+                test_twosum_large_number_test,
+                NULL,
+                NULL,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
         },
         /* Mark the end of the array with an entry where the test function is NULL */
         {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
