278385a0812d10a875e928e7272a92cafee25d36
ajaxian79 <blaketullysmith@gmail.com>
2022-11-06 22:39:08 -0800

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -101,3 +101,4 @@ int add_digits(int n) {
 // note 90: Time complexity: O(k) where k is the answer size.
 // note 91: Worst case appears only on degenerate inputs.
 // note 92: ASCII rules are a strict subset of UTF-8; no special handling needed.
+// note 93: Open addressing with linear probing wins until load factor exceeds 0.7.
