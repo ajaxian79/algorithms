26c135849d7fc2f06a5c8d7384727e5b15946374
ajaxian79 <blaketullysmith@gmail.com>
2022-11-14 08:18:42 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -111,3 +111,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 84: Deterministic given the input — no PRNG seeds.
 // edge note 85: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 86: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 87: Time complexity: O(n log n).
