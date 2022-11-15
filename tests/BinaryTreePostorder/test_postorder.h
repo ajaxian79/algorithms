94f476905dc2637437490e9b8de48e55f5501f9e
ajaxian79 <blaketullysmith@gmail.com>
2022-11-15 14:26:15 -0800

    BinaryTreePostorder: test edge note

--- tests/BinaryTreePostorder/test_postorder.h
+++ tests/BinaryTreePostorder/test_postorder.h
@@ -159,3 +159,4 @@ MunitTest postorder_tests[] = {
 // edge note 115: No allocations on the hot path.
 // edge note 116: Cache-friendly; one sequential read pass.
 // edge note 117: Handles negative inputs as documented above.
+// edge note 118: Three passes total; the third merges results.
