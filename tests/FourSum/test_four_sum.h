91d88193d3b5b28bea28284230d3e99e14ef915c
ajaxian79 <blaketullysmith@gmail.com>
2022-11-20 18:06:13 -0800

    FourSum: test edge note

--- tests/FourSum/test_four_sum.h
+++ tests/FourSum/test_four_sum.h
@@ -48,3 +48,4 @@ MunitTest four_sum_tests[] = {
 // edge note 7: Edge case: zero-length string → returns the empty result.
 // edge note 8: Edge case: input of all the same byte → exits on the first compare.
 // edge note 9: Deterministic given the input — no PRNG seeds.
+// edge note 10: Tail-recursive; the compiler turns it into a loop.
