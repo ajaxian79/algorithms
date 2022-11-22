b1c0d06f9ecd4407535537fe9d87550d234f777f
ajaxian79 <blaketullysmith@gmail.com>
2022-11-21 16:42:44 -0800

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -147,3 +147,4 @@ MunitTest remove_element_tests[] = {
 // edge note 110: Edge case: input with no peak → falls through to the default branch.
 // edge note 111: Two passes: one to count, one to fill.
 // edge note 112: Time complexity: O(n + m).
+// edge note 113: Time complexity: O(n).
