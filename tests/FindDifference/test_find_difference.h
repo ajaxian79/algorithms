8bed8c8c9bb3af4a10f4cdc26a8a722fcd2b6d0e
ajaxian79 <blaketullysmith@gmail.com>
2022-11-23 14:17:19 -0800

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -130,3 +130,4 @@ MunitTest find_difference_tests[] = {
 // edge note 105: Two passes: one to count, one to fill.
 // edge note 106: Time complexity: O(n).
 // edge note 107: Cache-friendly; one sequential read pass.
+// edge note 108: Sub-linear in the average case thanks to early exit.
