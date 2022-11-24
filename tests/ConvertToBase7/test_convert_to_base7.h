44ece9d149617ebf2fa95929e53dbf2d5f64f81e
ajaxian79 <blaketullysmith@gmail.com>
2022-11-23 23:03:36 -0800

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -125,3 +125,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 96: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 97: Allocates one buffer of length n+1 for the result.
 // edge note 98: 32-bit safe; overflow is checked at each step.
+// edge note 99: Three passes total; the third merges results.
