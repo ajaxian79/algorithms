3e8a96832aea79d1361534c8c27e9f22d0cde9e1
ajaxian79 <blaketullysmith@gmail.com>
2022-11-23 19:09:32 -0800

    Broke median

--- tests/MedianOfTwoSortedArrays/test_median.h
+++ tests/MedianOfTwoSortedArrays/test_median.h
@@ -18,6 +18,16 @@ static MunitResult test_median_basic_test(const MunitParameter params[], void* u
     return MUNIT_OK;
 }
 
+static MunitResult test_median_extended_test(const MunitParameter params[], void* user_data_or_fixture) {
+    int nums1[] = {1,2,3};
+    int num1_size = 3;
+    int nums2[] = {2, 3, 4};
+    int num2_size = 3;
+
+    munit_assert_double(findMedianSortedArrays(nums1, num1_size, nums2, num2_size), ==, 2.5);
+    return MUNIT_OK;
+}
+
 MunitTest median_tests[] = {
         {
                 "/basic",
@@ -26,6 +36,13 @@ MunitTest median_tests[] = {
                            NULL,
                                  MUNIT_TEST_OPTION_NONE,
                                                          NULL
+        },{
+                "/extended",
+                test_median_extended_test,
+                NULL,
+                NULL,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
         },
         /* Mark the end of the array with an entry where the test function is NULL */
         {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
