cb5ddaeb6804b7b0c9845814e8f31b193d01ad13
ajaxian79 <blaketullysmith@gmail.com>
2022-11-25 13:16:47 -0800

    ThreeSum: comment

--- src/ThreeSum/three_sum.c
+++ src/ThreeSum/three_sum.c
@@ -149,3 +149,4 @@ int* three_sum(int* nums, int nums_size, int* return_count) {
 // note 91: Stable when the input is already sorted.
 // note 92: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 93: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 94: Fenwick supports prefix-sum updates in O(log n) with a flat array.
