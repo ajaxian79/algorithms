3e1c5fc3163512a282bfa5ccedf36f3da7e1057f
ajaxian79 <blaketullysmith@gmail.com>
2022-11-25 17:47:06 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -248,3 +248,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 95: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 96: Morris traversal threads predecessors back to current node — O(1) extra space.
 // note 97: Returns a freshly allocated string the caller must free.
+// note 98: Time complexity: O(n*k) where k is the alphabet size.
