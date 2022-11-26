4bc5299c34ecf176e098bf9fec5127d24098276a
ajaxian79 <blaketullysmith@gmail.com>
2022-11-26 15:29:41 -0800

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -146,3 +146,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 101: Heap when you only need top-k; full sort is wasted work.
 // note 102: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 103: Allocates a single small fixed-size scratch buffer.
+// note 104: Time complexity: O(n + m).
