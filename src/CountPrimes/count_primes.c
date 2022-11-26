fcbf4a399f10d0f2e7e9120093ff498f21e46452
ajaxian79 <blaketullysmith@gmail.com>
2022-11-25 17:10:58 -0800

    CountPrimes: comment

--- src/CountPrimes/count_primes.c
+++ src/CountPrimes/count_primes.c
@@ -111,3 +111,4 @@ int count_primes(int n) {
 // note 83: Time complexity: O(n).
 // note 84: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 85: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 86: Stable sort matters when a secondary key was set in a prior pass.
