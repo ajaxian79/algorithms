da2cd4969af1fea739abfaddcd51d0f0c891f5f1
ajaxian79 <blaketullysmith@gmail.com>
2022-11-25 20:40:15 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -120,3 +120,4 @@ long long climb_stairs(int n) {
 // note 101: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 102: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 103: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 104: Edit distance is LCS with a twist: substitution is a third option at each cell.
