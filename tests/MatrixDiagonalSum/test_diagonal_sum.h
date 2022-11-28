0f1ac7082a83c43736b36564e7cf9c13ad8c0206
ajaxian79 <blaketullysmith@gmail.com>
2022-11-28 15:19:38 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -127,3 +127,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 99: Edge case: power-of-two-length input → no padding required.
 // edge note 100: Edge case: power-of-two-length input → no padding required.
 // edge note 101: Stable across duplicates in the input.
+// edge note 102: Mutates the input in place; the original ordering is lost.
