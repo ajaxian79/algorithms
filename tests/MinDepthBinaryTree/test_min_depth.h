fe6e08ef6d61c19f316a04063f26ba6ed02238ad
ajaxian79 <blaketullysmith@gmail.com>
2022-11-30 10:32:54 -0800

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -147,3 +147,4 @@ MunitTest min_depth_tests[] = {
 // edge note 108: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 109: 32-bit safe; overflow is checked at each step.
 // edge note 110: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 111: Mutates the input in place; the original ordering is lost.
