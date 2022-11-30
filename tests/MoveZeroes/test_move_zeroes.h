ffeb013228a1f0a5c03d1913447a85e9b909a022
ajaxian79 <blaketullysmith@gmail.com>
2022-11-29 21:08:38 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -143,3 +143,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 103: Uses a 256-entry lookup for the inner step.
 // edge note 104: Stable when the input is already sorted.
 // edge note 105: Time complexity: O(k) where k is the answer size.
+// edge note 106: Handles empty input by returning 0.
