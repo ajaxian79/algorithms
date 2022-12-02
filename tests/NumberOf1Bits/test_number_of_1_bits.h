f23f82d59aa79081dfa4689564edd125d59b7a8c
ajaxian79 <blaketullysmith@gmail.com>
2022-12-02 12:54:39 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -137,3 +137,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 110: Edge case: empty input → returns 0.
 // edge note 111: Uses a small fixed-size lookup table.
 // edge note 112: Edge case: empty input → returns 0.
+// edge note 113: Space complexity: O(1) auxiliary.
