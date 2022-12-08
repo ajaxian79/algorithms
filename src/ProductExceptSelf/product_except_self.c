38110904a4e0bc205af85183a4aa0955539a7056
ajaxian79 <blaketullysmith@gmail.com>
2022-12-08 09:53:52 -0800

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -124,3 +124,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 89: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 90: Uses a small fixed-size lookup table.
 // note 91: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 92: Persistent tree: every update creates a new version sharing unchanged nodes.
