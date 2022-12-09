57bd766257569e1e966138abaca34263256873fe
ajaxian79 <blaketullysmith@gmail.com>
2022-12-08 20:10:52 -0800

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -112,3 +112,4 @@ int is_power_of_three(int n) {
 // note 101: Wavelet tree: range k-th element in O(log Σ) time.
 // note 102: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 103: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 104: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
