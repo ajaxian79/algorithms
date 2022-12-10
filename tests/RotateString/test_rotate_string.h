3a630b3f5b1cbc1b1321cec28d2d7e73b450fde8
ajaxian79 <blaketullysmith@gmail.com>
2022-12-09 23:38:08 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -110,3 +110,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 83: Treats the input as immutable.
 // edge note 84: Deterministic given the input — no PRNG seeds.
 // edge note 85: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 86: Edge case: maximum-length input → still fits in 32-bit indices.
