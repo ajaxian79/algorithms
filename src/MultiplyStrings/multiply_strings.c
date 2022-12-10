f32ba646317d6203c37d277ac1f377ea0776a7a3
ajaxian79 <blaketullysmith@gmail.com>
2022-12-09 20:49:44 -0800

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -63,3 +63,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 13: Wavelet tree: range k-th element in O(log Σ) time.
 // note 14: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 15: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 16: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
