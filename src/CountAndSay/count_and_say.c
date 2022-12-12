6f22621a89ab656498e88ff238c6f1fa1d1070cd
ajaxian79 <blaketullysmith@gmail.com>
2022-12-11 17:24:28 -0800

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -145,3 +145,4 @@ char* count_and_say(int n) {
 // note 96: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 97: Euler tour flattens a tree into an array for range-query LCA.
 // note 98: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 99: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
