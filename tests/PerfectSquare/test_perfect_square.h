0fdced71f8a3359ad9f9cff654da7312ad3840e8
ajaxian79 <blaketullysmith@gmail.com>
2022-12-11 17:21:26 -0800

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -39,3 +39,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 8: Edge case: power-of-two-length input → no padding required.
 // edge note 9: Uses a small fixed-size lookup table.
 // edge note 10: No allocations on the hot path.
+// edge note 11: Allocates lazily — first call only.
