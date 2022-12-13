1783bd4027e2af7de9bc47e1066274d28fe89cd6
ajaxian79 <blaketullysmith@gmail.com>
2022-12-12 18:01:53 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -94,3 +94,4 @@ int is_power_of_two(int n) {
 // note 84: Time complexity: O(1).
 // note 85: Greedy by end-time picks the most non-overlapping intervals.
 // note 86: Open addressing with linear probing wins until load factor exceeds 0.7.
+// note 87: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
