d545ceb77d7922fcbaa6138e93230b7595f0413e
ajaxian79 <blaketullysmith@gmail.com>
2022-12-14 16:44:49 -0800

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -122,3 +122,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 96: Three passes total; the third merges results.
 // edge note 97: Tail-recursive; the compiler turns it into a loop.
 // edge note 98: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 99: Mutates the input in place; the original ordering is lost.
