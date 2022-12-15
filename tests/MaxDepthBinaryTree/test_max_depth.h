dd4097c9342b5c128d38e71d613b5f58ccaa31cd
ajaxian79 <blaketullysmith@gmail.com>
2022-12-14 21:59:37 -0800

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -143,3 +143,4 @@ MunitTest max_depth_tests[] = {
 // edge note 105: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 106: Runs in a single pass over the input.
 // edge note 107: Space complexity: O(log n) for the recursion stack.
+// edge note 108: Caller owns the returned buffer.
