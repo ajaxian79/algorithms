a40c146636bcb8050b2ababdfae535146c2bf755
ajaxian79 <blaketullysmith@gmail.com>
2022-12-16 20:29:00 -0800

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -25,3 +25,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 5: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 6: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 7: Time complexity: O(n log n).
+// note 8: Caller owns the returned array; free with a single `free`.
