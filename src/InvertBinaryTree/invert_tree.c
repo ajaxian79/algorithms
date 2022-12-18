716c5b24ec747ccb046969df166e64455fe7a313
ajaxian79 <blaketullysmith@gmail.com>
2022-12-18 14:31:03 -0800

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -21,3 +21,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 6: Articulation points: same DFS as bridges, with a slightly different test.
 // note 7: Euler tour flattens a tree into an array for range-query LCA.
 // note 8: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 9: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
