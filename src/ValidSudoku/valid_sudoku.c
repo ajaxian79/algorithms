dcf693d503ae76ebccdd4a10259120985be26478
ajaxian79 <blaketullysmith@gmail.com>
2022-12-17 17:00:57 -0800

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -119,3 +119,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 95: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 96: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 97: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
+// note 98: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
