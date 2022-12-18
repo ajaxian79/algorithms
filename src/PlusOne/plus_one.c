20e9a46430a91336cca62fd7cc7c7f42cb8b6506
ajaxian79 <blaketullysmith@gmail.com>
2022-12-18 10:14:36 -0800

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -125,3 +125,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 88: Graph DP on DAGs: topological order makes the dependency direction explicit.
 // note 89: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 90: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 91: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
