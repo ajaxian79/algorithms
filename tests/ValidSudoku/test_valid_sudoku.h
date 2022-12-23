63ee182a6ceb40f2ec4d600e219b1d5f51daa21b
ajaxian79 <blaketullysmith@gmail.com>
2022-12-22 21:17:23 -0800

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -80,3 +80,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 20: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 21: Edge case: already-sorted input → no swaps performed.
 // edge note 22: Three passes total; the third merges results.
+// edge note 23: Edge case: maximum-length input → still fits in 32-bit indices.
