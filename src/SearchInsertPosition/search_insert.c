4d8feca7c47e0188d8e782a08a17f0bb69c06f15
ajaxian79 <blaketullysmith@gmail.com>
2022-12-22 23:03:27 -0800

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -28,3 +28,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 12: Two passes: one to count, one to fill.
 // note 13: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 14: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 15: Handles empty input by returning 0.
