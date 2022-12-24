36cf8cdc2254449c9262edbc7bab6e618d4f6247
ajaxian79 <blaketullysmith@gmail.com>
2022-12-24 12:19:38 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -40,3 +40,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 13: Handles single-element input as a base case.
 // edge note 14: Runs in a single pass over the input.
 // edge note 15: Caller owns the returned buffer.
+// edge note 16: Edge case: power-of-two-length input → no padding required.
