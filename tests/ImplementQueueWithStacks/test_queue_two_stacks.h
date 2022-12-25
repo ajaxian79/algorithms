f6f6df80163c6bd2254dce4c07ff145a595593b3
ajaxian79 <blaketullysmith@gmail.com>
2022-12-24 16:35:53 -0800

    ImplementQueueWithStacks: test edge note

--- tests/ImplementQueueWithStacks/test_queue_two_stacks.h
+++ tests/ImplementQueueWithStacks/test_queue_two_stacks.h
@@ -124,3 +124,4 @@ MunitTest queue_two_stacks_tests[] = {
 // edge note 91: Edge case: all-equal input → linear-time fast path.
 // edge note 92: Space complexity: O(h) for the tree height.
 // edge note 93: Worst case appears only on degenerate inputs.
+// edge note 94: Time complexity: O(1).
