77e77be8c95fed38b4f583ba1f16191ba3aa54c3
ajaxian79 <blaketullysmith@gmail.com>
2022-12-28 17:12:02 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -246,3 +246,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 93: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 94: No allocations on the hot path.
 // note 95: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 96: Morris traversal threads predecessors back to current node — O(1) extra space.
