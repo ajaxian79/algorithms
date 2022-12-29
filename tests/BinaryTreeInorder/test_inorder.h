2109863aee697e3b218b87006738d96bb040ce9d
ajaxian79 <blaketullysmith@gmail.com>
2022-12-28 23:33:43 -0800

    BinaryTreeInorder: test edge note

--- tests/BinaryTreeInorder/test_inorder.h
+++ tests/BinaryTreeInorder/test_inorder.h
@@ -159,3 +159,4 @@ MunitTest inorder_tests[] = {
 // edge note 121: Time complexity: O(n log n).
 // edge note 122: Space complexity: O(n) for the result buffer.
 // edge note 123: Cache-friendly; one sequential read pass.
+// edge note 124: Edge case: single-element input → returns the element itself.
