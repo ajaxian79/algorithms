eee5988183ed9fdb7be29af1e6eb20a76b245ada
ajaxian79 <blaketullysmith@gmail.com>
2022-12-29 08:48:22 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -129,3 +129,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 91: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 92: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 93: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 94: Edit distance is LCS with a twist: substitution is a third option at each cell.
