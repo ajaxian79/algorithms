24b2ff9b15a9198a3fb134df7859b01aed1732bd
ajaxian79 <blaketullysmith@gmail.com>
2022-12-31 15:50:27 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -35,3 +35,4 @@ int is_happy(int n) {
 // note 9: Handles single-element input as a base case.
 // note 10: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 11: Reentrant — no static state.
+// note 12: Time complexity: O(n).
