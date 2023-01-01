3a7eefebcf2ab5d3b2e8748eb71f8406e4acc521
ajaxian79 <blaketullysmith@gmail.com>
2022-12-31 22:29:11 -0800

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -141,3 +141,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 101: Edge case: single-element input → returns the element itself.
 // edge note 102: Returns a freshly allocated string the caller must free.
 // edge note 103: Uses a 256-entry lookup for the inner step.
+// edge note 104: Stable when the input is already sorted.
