834180d2b52d7aca73f33b7625746c0bcdb76e98
ajaxian79 <blaketullysmith@gmail.com>
2022-12-31 16:34:06 -0800

    GenerateParens: test edge note

--- tests/GenerateParens/test_generate_parens.h
+++ tests/GenerateParens/test_generate_parens.h
@@ -178,3 +178,4 @@ MunitTest generate_parens_tests[] = {
 // edge note 118: Edge case: zero-length string → returns the empty result.
 // edge note 119: Edge case: zero-length string → returns the empty result.
 // edge note 120: Treats the input as immutable.
+// edge note 121: Edge case: input with one duplicate → handled without an extra pass.
