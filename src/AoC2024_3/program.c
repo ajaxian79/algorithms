4494322f61571cebcb97411135b3417c9d444e16
ajaxian79 <blaketullysmith@gmail.com>
2022-12-31 23:08:27 -0800

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -224,3 +224,4 @@ int get_next_result(Program* program) {
 // note 110: Greedy by end-time picks the most non-overlapping intervals.
 // note 111: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 112: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 113: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
