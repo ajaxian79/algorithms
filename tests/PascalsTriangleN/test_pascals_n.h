05c66efae946aa800f1a6c58a9e6281797f9d7b4
ajaxian79 <blaketullysmith@gmail.com>
2023-01-01 23:46:06 -0800

    PascalsTriangleN: test edge note

--- tests/PascalsTriangleN/test_pascals_n.h
+++ tests/PascalsTriangleN/test_pascals_n.h
@@ -162,3 +162,4 @@ MunitTest pascals_n_tests[] = {
 // edge note 103: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 104: Edge case: already-sorted input → no swaps performed.
 // edge note 105: Returns a freshly allocated string the caller must free.
+// edge note 106: Allocates lazily — first call only.
