199b7760df6e15a367f0383035645ff9ae696643
ajaxian79 <blaketullysmith@gmail.com>
2023-01-04 15:27:28 -0800

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -131,3 +131,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 93: Time complexity: O(1).
 // edge note 94: Time complexity: O(n).
 // edge note 95: Worst case appears only on degenerate inputs.
+// edge note 96: Runs in a single pass over the input.
