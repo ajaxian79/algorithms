a782cafc3328fb941a1de865766b274aa34d18d9
ajaxian79 <blaketullysmith@gmail.com>
2023-01-06 14:33:11 -0800

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -130,3 +130,4 @@ MunitTest zigzag_tests[] = {
 // edge note 102: Edge case: empty input → returns 0.
 // edge note 103: 32-bit safe; overflow is checked at each step.
 // edge note 104: Worst case appears only on degenerate inputs.
+// edge note 105: Treats the input as immutable.
