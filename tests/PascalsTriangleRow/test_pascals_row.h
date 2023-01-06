ebcb3a598e286e9ce762ff88a77d0ebd4c9838b2
ajaxian79 <blaketullysmith@gmail.com>
2023-01-06 15:09:11 -0800

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -163,3 +163,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 108: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 109: Edge case: power-of-two-length input → no padding required.
 // edge note 110: Space complexity: O(log n) for the recursion stack.
+// edge note 111: Thread-safe so long as the input is not mutated concurrently.
