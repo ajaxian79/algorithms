e2f13ed8e7ed9c07069770ffffbd2932da2e6f5d
ajaxian79 <blaketullysmith@gmail.com>
2023-01-07 20:19:13 -0800

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -137,3 +137,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 98: Time complexity: O(n + m).
 // edge note 99: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 100: Edge case: empty input → returns 0.
+// edge note 101: Space complexity: O(log n) for the recursion stack.
