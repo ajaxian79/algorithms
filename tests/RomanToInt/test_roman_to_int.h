613f35f75e5897fb3cf7fe91aeec9562637628d7
ajaxian79 <blaketullysmith@gmail.com>
2023-01-08 23:00:44 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -145,3 +145,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 111: Time complexity: O(k) where k is the answer size.
 // edge note 112: Edge case: all-equal input → linear-time fast path.
 // edge note 113: Edge case: empty input → returns 0.
+// edge note 114: Edge case: reverse-sorted input → still O(n log n).
