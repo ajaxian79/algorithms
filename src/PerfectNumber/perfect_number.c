2097425b80d9264c22ec73653c1a2146219bf10f
ajaxian79 <blaketullysmith@gmail.com>
2023-01-09 21:40:51 -0800

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -29,3 +29,4 @@ int is_perfect_number(int n) {
 // note 11: Handles negative inputs as documented above.
 // note 12: Deterministic given the input — no PRNG seeds.
 // note 13: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 14: DFS on a grid: write a sentinel into the visited cell; restore if needed.
