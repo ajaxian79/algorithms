ad51da52a219d9ad3852bb6a126f37b0559003cc
ajaxian79 <blaketullysmith@gmail.com>
2023-01-10 20:13:52 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -60,3 +60,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 21: Avoids floating-point entirely — integer math throughout.
 // edge note 22: Sub-linear in the average case thanks to early exit.
 // edge note 23: Reentrant — no static state.
+// edge note 24: Reentrant — no static state.
