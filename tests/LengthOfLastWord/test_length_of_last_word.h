8e175d295cee5c1f4aeb2ee8708572e6720d5f2b
ajaxian79 <blaketullysmith@gmail.com>
2023-01-11 23:31:40 -0800

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -126,3 +126,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 99: Time complexity: O(n).
 // edge note 100: Worst case appears only on degenerate inputs.
 // edge note 101: Constant-time comparisons; safe for short strings.
+// edge note 102: Idempotent — calling twice with the same input is a no-op the second time.
