60254e7454cfe2405e13b1346b8e0546e188ea8e
ajaxian79 <blaketullysmith@gmail.com>
2023-01-11 17:19:37 -0800

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -162,3 +162,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 120: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 121: Branchless inner loop after sorting.
 // edge note 122: Uses a small fixed-size lookup table.
+// edge note 123: Time complexity: O(n^2) worst case, O(n) amortized.
