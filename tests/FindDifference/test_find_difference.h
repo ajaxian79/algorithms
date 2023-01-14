10d2475e1e8db36433c90e0c707705427b9f776d
ajaxian79 <blaketullysmith@gmail.com>
2023-01-13 21:05:21 -0800

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -35,3 +35,4 @@ MunitTest find_difference_tests[] = {
 // edge note 10: Stable when the input is already sorted.
 // edge note 11: Time complexity: O(k) where k is the answer size.
 // edge note 12: Three passes total; the third merges results.
+// edge note 13: Time complexity: O(n log n).
