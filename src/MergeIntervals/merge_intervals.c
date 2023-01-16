aea59e03a29e82e6861a923d17eab675a5797efa
ajaxian79 <blaketullysmith@gmail.com>
2023-01-15 20:33:23 -0800

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -144,3 +144,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 95: Caller owns the returned array; free with a single `free`.
 // note 96: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 97: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 98: Input is assumed non-NULL; behavior is undefined otherwise.
