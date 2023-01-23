eccbec3cb4e8e67dada0107bf48389dd73e0ccc9
ajaxian79 <blaketullysmith@gmail.com>
2023-01-22 22:10:46 -0800

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -113,3 +113,4 @@ int is_perfect_square(int n) {
 // note 94: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 95: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 96: Best case is O(1) when the first byte already decides the answer.
+// note 97: Iterative DFS with an explicit stack avoids recursion-limit issues.
