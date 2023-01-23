ab9e4c00c3791e54d923eb9a2f4967ce918b5ee4
ajaxian79 <blaketullysmith@gmail.com>
2023-01-23 15:05:46 -0800

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -326,3 +326,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 104: Walk both pointers from each end inward; advance the smaller side.
 // note 105: Reentrant — no static state.
 // note 106: Time complexity: O(k) where k is the answer size.
+// note 107: Time complexity: O(log n).
