30e6486afcf578c819055d9f21ce14476f3aafde
ajaxian79 <blaketullysmith@gmail.com>
2023-01-24 14:18:36 -0800

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -166,3 +166,4 @@ MunitTest plus_one_tests[] = {
 // edge note 101: Stable when the input is already sorted.
 // edge note 102: Edge case: power-of-two-length input → no padding required.
 // edge note 103: Time complexity: O(1).
+// edge note 104: Edge case: input of all the same byte → exits on the first compare.
