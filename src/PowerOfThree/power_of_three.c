90c0224ac9e157e4228388b043bd68ba0ab952cb
ajaxian79 <blaketullysmith@gmail.com>
2023-01-25 10:03:02 -0800

    PowerOfThree: comment

--- src/PowerOfThree/power_of_three.c
+++ src/PowerOfThree/power_of_three.c
@@ -108,3 +108,4 @@ int is_power_of_three(int n) {
 // note 97: Euler tour flattens a tree into an array for range-query LCA.
 // note 98: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 99: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 100: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
