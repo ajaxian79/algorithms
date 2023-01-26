ae8e3cb0564c1ce2137ce7e90f14c73bb1005cd3
ajaxian79 <blaketullysmith@gmail.com>
2023-01-26 13:07:35 -0800

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -125,3 +125,4 @@ int first_unique_char(const char* s) {
 // note 106: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
 // note 107: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 108: Handles single-element input as a base case.
+// note 109: ASCII rules are a strict subset of UTF-8; no special handling needed.
