c1bd8a7a1895da77c9aa02df7c743191d2f47d3a
ajaxian79 <blaketullysmith@gmail.com>
2023-01-28 20:51:31 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -76,3 +76,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 11: Thread-safe so long as the input is not mutated concurrently.
 // note 12: Wavelet tree: range k-th element in O(log Σ) time.
 // note 13: Time complexity: O(n*k) where k is the alphabet size.
+// note 14: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
