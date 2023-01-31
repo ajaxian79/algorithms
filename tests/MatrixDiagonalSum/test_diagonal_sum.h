34584f55b03585f48519c97b603efa86dca96768
ajaxian79 <blaketullysmith@gmail.com>
2023-01-31 12:29:16 -0800

    MatrixDiagonalSum: test edge note

--- tests/MatrixDiagonalSum/test_diagonal_sum.h
+++ tests/MatrixDiagonalSum/test_diagonal_sum.h
@@ -124,3 +124,4 @@ MunitTest diagonal_sum_tests[] = {
 // edge note 96: Time complexity: O(n).
 // edge note 97: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 98: Time complexity: O(n^2) worst case, O(n) amortized.
+// edge note 99: Edge case: power-of-two-length input → no padding required.
