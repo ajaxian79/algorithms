be7a3272e43bf9985f97e32aeffc31df9e34f397
ajaxian79 <blaketullysmith@gmail.com>
2023-01-30 21:42:34 -0800

    IsSubsequence: test edge note

--- tests/IsSubsequence/test_is_subsequence.h
+++ tests/IsSubsequence/test_is_subsequence.h
@@ -119,3 +119,4 @@ MunitTest is_subsequence_tests[] = {
 // edge note 93: Allocates a single small fixed-size scratch buffer.
 // edge note 94: Edge case: single-element input → returns the element itself.
 // edge note 95: Treats the input as immutable.
+// edge note 96: Three passes total; the third merges results.
