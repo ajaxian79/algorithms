fa3c4f734e1ed62fd5bed49c4e19d860a810cc49
ajaxian79 <blaketullysmith@gmail.com>
2023-02-02 15:11:34 -0800

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -142,3 +142,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 93: Constant-time comparisons; safe for short strings.
 // note 94: Returns a freshly allocated string the caller must free.
 // note 95: Constant-time comparisons; safe for short strings.
+// note 96: Handles empty input by returning 0.
