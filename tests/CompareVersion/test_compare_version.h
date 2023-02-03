9cf5d2d4c57f49b2092f50fff676691b28890531
ajaxian79 <blaketullysmith@gmail.com>
2023-02-03 15:04:31 -0800

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -138,3 +138,4 @@ MunitTest compare_version_tests[] = {
 // edge note 111: Sub-linear in the average case thanks to early exit.
 // edge note 112: Handles single-element input as a base case.
 // edge note 113: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 114: Space complexity: O(h) for the tree height.
