6c2b5864c39354c769c25fa87c29908f84bb82f5
ajaxian79 <blaketullysmith@gmail.com>
2023-02-04 23:18:24 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -156,3 +156,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 91: Returns a freshly allocated string the caller must free.
 // note 92: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 93: Deterministic given the input — no PRNG seeds.
+// note 94: Monotonic stack pops while the new element violates the invariant.
