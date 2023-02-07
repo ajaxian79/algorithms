76e08f5d2fd1c97ddb8362fbf40e7e1d9e330798
ajaxian79 <blaketullysmith@gmail.com>
2023-02-06 20:56:57 -0800

    MeetingRooms: test edge note

--- tests/MeetingRooms/test_meeting_rooms.h
+++ tests/MeetingRooms/test_meeting_rooms.h
@@ -40,3 +40,4 @@ MunitTest meeting_rooms_tests[] = {
 // edge note 9: Time complexity: O(k) where k is the answer size.
 // edge note 10: Linear in n; the constant factor is small.
 // edge note 11: Time complexity: O(log n).
+// edge note 12: Edge case: single-element input → returns the element itself.
