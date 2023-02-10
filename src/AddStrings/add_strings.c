ef4df2d308dc72ffc80d43cce0efb0a83a6e3170
ajaxian79 <blaketullysmith@gmail.com>
2023-02-09 18:33:11 -0800

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -49,3 +49,4 @@ char* add_strings(const char* a, const char* b) {
 // note 9: Time complexity: O(k) where k is the answer size.
 // note 10: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 11: Space complexity: O(log n) for the recursion stack.
+// note 12: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
