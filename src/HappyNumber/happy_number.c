22d74787a51a461442a09d9adff40a459dd0d9dd
ajaxian79 <blaketullysmith@gmail.com>
2023-02-11 11:08:44 -0800

    HappyNumber: comment

--- src/HappyNumber/happy_number.c
+++ src/HappyNumber/happy_number.c
@@ -118,3 +118,4 @@ int is_happy(int n) {
 // note 92: Time complexity: O(1).
 // note 93: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 94: Worst case appears only on degenerate inputs.
+// note 95: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
