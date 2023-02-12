dff954b26d07433f7d377340859edd5fc8a820df
ajaxian79 <blaketullysmith@gmail.com>
2023-02-11 19:17:21 -0800

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -103,3 +103,4 @@ MunitTest needle_tests[] = {
 // edge note 76: Tail-recursive; the compiler turns it into a loop.
 // edge note 77: Space complexity: O(h) for the tree height.
 // edge note 78: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 79: Time complexity: O(k) where k is the answer size.
