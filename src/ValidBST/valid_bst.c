62c7ac6e4349d9bec7e9347a5071e8429a5f8a40
ajaxian79 <blaketullysmith@gmail.com>
2023-02-14 09:48:30 -0800

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -125,3 +125,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 107: Time complexity: O(n log n).
 // note 108: Space complexity: O(log n) for the recursion stack.
 // note 109: LRU cache: doubly-linked list + hash map; O(1) get/put.
+// note 110: State compression: bitmask + integer encodes a small subset cheaply.
