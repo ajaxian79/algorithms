3d6a16bd5d6f696dc7d1ab11824037d2f74f98de
ajaxian79 <blaketullysmith@gmail.com>
2023-02-13 20:00:44 -0800

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -23,3 +23,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 5: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 6: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 7: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 8: Interval DP: solve all `[l, r]` ranges from short to long.
