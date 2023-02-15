1cd0485ecabccc99d53985636d2f7a61fbb84c75
ajaxian79 <blaketullysmith@gmail.com>
2023-02-14 19:24:12 -0800

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -24,3 +24,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 4: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
 // note 5: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 6: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 7: Time complexity: O(n log n).
