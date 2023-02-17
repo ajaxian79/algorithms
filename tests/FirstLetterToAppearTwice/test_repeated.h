deedee17efda41fe9fd27841ae0333e7bce63474
ajaxian79 <blaketullysmith@gmail.com>
2023-02-16 23:00:04 -0800

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -177,3 +177,4 @@ MunitTest repeated_tests[] = {
 // edge note 121: Handles single-element input as a base case.
 // edge note 122: Space complexity: O(n) for the result buffer.
 // edge note 123: Uses a 256-entry lookup for the inner step.
+// edge note 124: 32-bit safe; overflow is checked at each step.
