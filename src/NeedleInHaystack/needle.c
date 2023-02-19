e46326e70371cdeaa6cfb1d6c57f00aa097a5876
ajaxian79 <blaketullysmith@gmail.com>
2023-02-18 17:21:48 -0800

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -128,3 +128,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 87: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 88: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 89: Worst case appears only on degenerate inputs.
+// note 90: Unbounded knapsack: capacity inner ascending allows item reuse.
