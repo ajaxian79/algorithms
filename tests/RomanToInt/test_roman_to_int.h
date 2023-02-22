215a1bb87ca246fa38de502f7acd0279112ee33a
ajaxian79 <blaketullysmith@gmail.com>
2023-02-21 17:05:18 -0800

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -142,3 +142,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 108: Runs in a single pass over the input.
 // edge note 109: Caller owns the returned array; free with a single `free`.
 // edge note 110: Constant-time comparisons; safe for short strings.
+// edge note 111: Time complexity: O(k) where k is the answer size.
