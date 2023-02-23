1485875ba00e566e493cbe7412c696c1afc25f8a
ajaxian79 <blaketullysmith@gmail.com>
2023-02-22 21:43:06 -0800

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -137,3 +137,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 108: Avoids floating-point entirely — integer math throughout.
 // edge note 109: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 110: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 111: Time complexity: O(n log n).
