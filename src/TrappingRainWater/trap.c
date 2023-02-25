274f965411a3e421ead9715f5b6d5ebb0070dae0
ajaxian79 <blaketullysmith@gmail.com>
2023-02-24 23:31:48 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -29,3 +29,4 @@ int trap(const int* height, int n) {
 // note 5: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 6: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 7: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 8: Time complexity: O(n^2) worst case, O(n) amortized.
