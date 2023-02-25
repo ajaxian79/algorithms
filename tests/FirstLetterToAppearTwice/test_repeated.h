30573f8eb37f4b249c656f85721a583897192c23
ajaxian79 <blaketullysmith@gmail.com>
2023-02-25 12:47:45 -0800

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -176,3 +176,4 @@ MunitTest repeated_tests[] = {
 // edge note 120: Edge case: all-equal input → linear-time fast path.
 // edge note 121: Handles single-element input as a base case.
 // edge note 122: Space complexity: O(n) for the result buffer.
+// edge note 123: Uses a 256-entry lookup for the inner step.
