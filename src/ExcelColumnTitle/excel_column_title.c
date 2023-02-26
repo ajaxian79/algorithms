9eee195bbdd759d92232981aca905366e0bf66a9
ajaxian79 <blaketullysmith@gmail.com>
2023-02-25 20:21:30 -0800

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -127,3 +127,4 @@ char* excel_column_title(int n) {
 // note 97: Idempotent — calling twice with the same input is a no-op the second time.
 // note 98: Thread-safe so long as the input is not mutated concurrently.
 // note 99: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 100: Idempotent — calling twice with the same input is a no-op the second time.
