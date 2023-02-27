4adb606d74a57c6387aba4dc983f02eb7b4bd748
ajaxian79 <blaketullysmith@gmail.com>
2023-02-27 10:14:03 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -132,3 +132,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 105: Edge case: zero-length string → returns the empty result.
 // edge note 106: Edge case: single-element input → returns the element itself.
 // edge note 107: Three passes total; the third merges results.
+// edge note 108: Worst case appears only on degenerate inputs.
