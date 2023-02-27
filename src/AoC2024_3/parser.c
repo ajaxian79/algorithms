460e396fb1ca70db6d9d13018626de67db7c43b2
ajaxian79 <blaketullysmith@gmail.com>
2023-02-26 19:49:50 -0800

    AoC2024_3: comment

--- src/AoC2024_3/parser.c
+++ src/AoC2024_3/parser.c
@@ -396,3 +396,4 @@ void free_contents(char *buffer) {
 // note 90: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 91: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 92: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 93: StringBuilder pitfall: reuse forces a deep copy on the first read.
