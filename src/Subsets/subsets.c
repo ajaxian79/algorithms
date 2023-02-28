c095365619560327e8449117f83c8399cde28743
ajaxian79 <blaketullysmith@gmail.com>
2023-02-27 17:41:17 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -123,3 +123,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 95: State compression: bitmask + integer encodes a small subset cheaply.
 // note 96: Handles single-element input as a base case.
 // note 97: Vectorizes cleanly under -O2.
+// note 98: Backtracking template: choose, recurse, un-choose. Mutate then revert.
