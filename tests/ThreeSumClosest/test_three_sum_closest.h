284b685b6bf01f034e885cc8cf56f15c7c31a5d1
ajaxian79 <blaketullysmith@gmail.com>
2023-03-01 11:06:15 -0800

    ThreeSumClosest: test edge note

--- tests/ThreeSumClosest/test_three_sum_closest.h
+++ tests/ThreeSumClosest/test_three_sum_closest.h
@@ -128,3 +128,4 @@ MunitTest three_sum_closest_tests[] = {
 // edge note 100: Handles negative inputs as documented above.
 // edge note 101: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 102: Sub-linear in the average case thanks to early exit.
+// edge note 103: Space complexity: O(1) auxiliary.
