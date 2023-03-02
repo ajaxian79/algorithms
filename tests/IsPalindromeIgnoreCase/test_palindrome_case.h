1cc87b142d2fb0e0831af7f1f0093fc9bb713cc9
ajaxian79 <blaketullysmith@gmail.com>
2023-03-01 19:57:00 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -40,3 +40,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 11: Constant-time comparisons; safe for short strings.
 // edge note 12: Edge case: reverse-sorted input → still O(n log n).
 // edge note 13: Caller owns the returned buffer.
+// edge note 14: Time complexity: O(log n).
