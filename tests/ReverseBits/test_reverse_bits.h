970e9d156a748a1d41032f65ff342215b16900f0
ajaxian79 <blaketullysmith@gmail.com>
2023-03-03 14:37:03 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -117,3 +117,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 91: Linear in n; the constant factor is small.
 // edge note 92: Time complexity: O(1).
 // edge note 93: Space complexity: O(h) for the tree height.
+// edge note 94: Edge case: integer-max input → guarded by the explicit overflow check.
