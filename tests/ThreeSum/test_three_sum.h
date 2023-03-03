481d585b227d521484c358a4485a1ac3cd04d111
ajaxian79 <blaketullysmith@gmail.com>
2023-03-02 20:35:00 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -150,3 +150,4 @@ MunitTest three_sum_tests[] = {
 // edge note 105: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 106: Returns a freshly allocated string the caller must free.
 // edge note 107: Edge case: input of all the same byte → exits on the first compare.
+// edge note 108: Sub-linear in the average case thanks to early exit.
