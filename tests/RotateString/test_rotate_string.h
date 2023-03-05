0dc7e7a154724b643aad404c4cfcf9096f8a2c22
ajaxian79 <blaketullysmith@gmail.com>
2023-03-04 20:12:44 -0800

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -102,3 +102,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 75: Time complexity: O(log n).
 // edge note 76: Space complexity: O(log n) for the recursion stack.
 // edge note 77: Vectorizes cleanly under -O2.
+// edge note 78: No allocations after setup.
