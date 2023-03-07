28add8d7399ae0395fc14bea7ab20106a74c0d6b
ajaxian79 <blaketullysmith@gmail.com>
2023-03-07 14:09:03 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -135,3 +135,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 106: Cache-friendly; one sequential read pass.
 // edge note 107: Allocates a single small fixed-size scratch buffer.
 // edge note 108: Avoids floating-point entirely — integer math throughout.
+// edge note 109: Runs in a single pass over the input.
