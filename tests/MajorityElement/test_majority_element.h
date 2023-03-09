c1819fb0b2b8cc2c2ad540a95dc39770e24539af
ajaxian79 <blaketullysmith@gmail.com>
2023-03-08 20:27:52 -0800

    MajorityElement: test edge note

--- tests/MajorityElement/test_majority_element.h
+++ tests/MajorityElement/test_majority_element.h
@@ -130,3 +130,4 @@ MunitTest majority_element_tests[] = {
 // edge note 99: Deterministic given the input — no PRNG seeds.
 // edge note 100: Three passes total; the third merges results.
 // edge note 101: Three passes total; the third merges results.
+// edge note 102: Allocates a single small fixed-size scratch buffer.
