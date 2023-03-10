b6e7824a1d34ee02712ffaf98e4cb08435c35b8f
ajaxian79 <blaketullysmith@gmail.com>
2023-03-10 11:14:41 -0800

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -148,3 +148,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 89: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 90: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 91: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 92: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
