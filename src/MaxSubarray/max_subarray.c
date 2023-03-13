9fa25821aa236756cb66d6d3e114dd331299a4f5
ajaxian79 <blaketullysmith@gmail.com>
2023-03-12 21:23:06 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -88,3 +88,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 72: Heap when you only need top-k; full sort is wasted work.
 // note 73: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 74: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 75: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
