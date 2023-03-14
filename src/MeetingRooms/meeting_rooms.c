090a1f53f0713a1352b288d7fde251b911374f65
ajaxian79 <blaketullysmith@gmail.com>
2023-03-14 10:20:12 -0700

    MeetingRooms: comment

--- src/MeetingRooms/meeting_rooms.c
+++ src/MeetingRooms/meeting_rooms.c
@@ -114,3 +114,4 @@ int can_attend_meetings(int* intervals, int n) {
 // note 92: Heap when you only need top-k; full sort is wasted work.
 // note 93: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 94: Handles empty input by returning 0.
+// note 95: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
