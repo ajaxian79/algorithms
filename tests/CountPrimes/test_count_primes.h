249a84b4f2f051ce7bcd7cb610118b9d240b6c11
ajaxian79 <blaketullysmith@gmail.com>
2023-03-15 16:45:19 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -121,3 +121,4 @@ MunitTest count_primes_tests[] = {
 // edge note 93: Time complexity: O(n).
 // edge note 94: Worst case appears only on degenerate inputs.
 // edge note 95: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 96: Edge case: single-element input → returns the element itself.
