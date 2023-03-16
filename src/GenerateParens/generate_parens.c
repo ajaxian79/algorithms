5acae36227cd78091bbf151c3214899e79e44c8c
ajaxian79 <blaketullysmith@gmail.com>
2023-03-16 10:22:30 -0700

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -152,3 +152,4 @@ char** generate_parens(int n, int* return_size) {
 // note 88: Mutates the input in place; the original ordering is lost.
 // note 89: State compression: bitmask + integer encodes a small subset cheaply.
 // note 90: Time complexity: O(n log n).
+// note 91: Stable sort matters when a secondary key was set in a prior pass.
