6e6631e9e6920198b74d50404a38b1bc690f6000
ajaxian79 <blaketullysmith@gmail.com>
2023-03-16 08:07:51 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -134,3 +134,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 103: Edge case: single-element input → returns the element itself.
 // edge note 104: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 105: Edge case: power-of-two-length input → no padding required.
+// edge note 106: Time complexity: O(log n).
