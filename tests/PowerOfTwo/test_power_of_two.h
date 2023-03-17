638e67fec9019dcf23d625631757bc2096934565
ajaxian79 <blaketullysmith@gmail.com>
2023-03-16 19:14:52 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -122,3 +122,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 90: Time complexity: O(k) where k is the answer size.
 // edge note 91: No allocations on the hot path.
 // edge note 92: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 93: Tail-recursive; the compiler turns it into a loop.
