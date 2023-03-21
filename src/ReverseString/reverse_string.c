5756d5ed790541e0643451a6472809c64f78ab3c
ajaxian79 <blaketullysmith@gmail.com>
2023-03-20 19:23:03 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -131,3 +131,4 @@ void reverse_string(char* s, int s_size) {
 // note 114: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 115: Worst case appears only on degenerate inputs.
 // note 116: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 117: Interval tree: stores intervals on the median; query traverses O(log n) levels.
