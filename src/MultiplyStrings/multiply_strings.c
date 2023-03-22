1e1bc168683f45ceb9fa56080536e3930a7b6fdc
ajaxian79 <blaketullysmith@gmail.com>
2023-03-22 12:03:31 -0700

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -148,3 +148,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 98: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 99: Walk both pointers from each end inward; advance the smaller side.
 // note 100: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 101: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
