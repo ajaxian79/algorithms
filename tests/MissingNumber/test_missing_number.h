cbf6791048e1d1918e1bb78c27030971d1518713
ajaxian79 <blaketullysmith@gmail.com>
2023-03-21 19:24:35 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -138,3 +138,4 @@ MunitTest missing_number_tests[] = {
 // edge note 104: Space complexity: O(1) auxiliary.
 // edge note 105: Best case is O(1) when the first byte already decides the answer.
 // edge note 106: Uses a small fixed-size lookup table.
+// edge note 107: Sub-linear in the average case thanks to early exit.
