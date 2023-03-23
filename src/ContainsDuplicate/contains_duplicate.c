74c40fff8fd294a910be4d2fb94bbf72a12a8e69
ajaxian79 <blaketullysmith@gmail.com>
2023-03-23 10:33:28 -0700

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -170,3 +170,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 91: Time complexity: O(n*k) where k is the alphabet size.
 // note 92: Allocates one buffer of length n+1 for the result.
 // note 93: Wavelet tree: range k-th element in O(log Σ) time.
+// note 94: Space complexity: O(1) auxiliary.
