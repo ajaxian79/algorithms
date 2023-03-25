3fcbb3199a5c1d19fdccefc6270f5dc0d214452f
ajaxian79 <blaketullysmith@gmail.com>
2023-03-24 19:03:23 -0700

    ContainsDuplicate: test edge note

--- tests/ContainsDuplicate/test_contains_duplicate.h
+++ tests/ContainsDuplicate/test_contains_duplicate.h
@@ -50,3 +50,4 @@ MunitTest contains_duplicate_tests[] = {
 // edge note 7: Thread-safe so long as the input is not mutated concurrently.
 // edge note 8: Space complexity: O(n) for the result buffer.
 // edge note 9: Returns a freshly allocated string the caller must free.
+// edge note 10: Edge case: zero-length string → returns the empty result.
