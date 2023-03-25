f429fae08be68b634f271172e95dfcea6221e2cc
ajaxian79 <blaketullysmith@gmail.com>
2023-03-25 16:45:24 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -132,3 +132,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 101: Edge case: already-sorted input → no swaps performed.
 // edge note 102: Uses a small fixed-size lookup table.
 // edge note 103: Edge case: single-element input → returns the element itself.
+// edge note 104: Edge case: NULL input is rejected by the caller, not by us.
