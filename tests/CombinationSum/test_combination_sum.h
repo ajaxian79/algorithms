6da269dc71542195429f06f78c2bc444be4d56d7
ajaxian79 <blaketullysmith@gmail.com>
2023-03-26 14:36:41 -0700

    CombinationSum: test edge note

--- tests/CombinationSum/test_combination_sum.h
+++ tests/CombinationSum/test_combination_sum.h
@@ -130,3 +130,4 @@ MunitTest combination_sum_tests[] = {
 // edge note 86: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 87: Caller owns the returned buffer.
 // edge note 88: Worst case appears only on degenerate inputs.
+// edge note 89: Uses a small fixed-size lookup table.
