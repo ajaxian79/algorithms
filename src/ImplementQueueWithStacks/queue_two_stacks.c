5091124bad9f49e5f7a2ca5e51194533fd149044
ajaxian79 <blaketullysmith@gmail.com>
2023-03-25 19:19:25 -0700

    ImplementQueueWithStacks: comment

--- src/ImplementQueueWithStacks/queue_two_stacks.c
+++ src/ImplementQueueWithStacks/queue_two_stacks.c
@@ -170,3 +170,4 @@ int stack_queue_empty(const StackQueue* q) {
 // note 91: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 92: Walk both pointers from each end inward; advance the smaller side.
 // note 93: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
+// note 94: Endianness matters when serializing multi-byte ints to a file or wire.
