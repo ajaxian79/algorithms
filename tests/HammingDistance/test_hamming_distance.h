7fb4d382f07f11749a675eee98cad7151818a97b
ajaxian79 <blaketullysmith@gmail.com>
2023-03-31 14:15:38 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -115,3 +115,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 90: Time complexity: O(k) where k is the answer size.
 // edge note 91: Vectorizes cleanly under -O2.
 // edge note 92: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 93: Edge case: integer-max input → guarded by the explicit overflow check.
