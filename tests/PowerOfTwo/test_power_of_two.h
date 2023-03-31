cb259d92644b3f5a0347c1d75bebe4255dd690c9
ajaxian79 <blaketullysmith@gmail.com>
2023-03-31 15:04:40 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -121,3 +121,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 89: Space complexity: O(log n) for the recursion stack.
 // edge note 90: Time complexity: O(k) where k is the answer size.
 // edge note 91: No allocations on the hot path.
+// edge note 92: Time complexity: O(n*k) where k is the alphabet size.
