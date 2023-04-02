0169eb7306019bfa47ef8fe90222a6e52eb6043f
ajaxian79 <blaketullysmith@gmail.com>
2023-04-02 10:22:37 -0700

    MinDepthBinaryTree: test edge note

--- tests/MinDepthBinaryTree/test_min_depth.h
+++ tests/MinDepthBinaryTree/test_min_depth.h
@@ -139,3 +139,4 @@ MunitTest min_depth_tests[] = {
 // edge note 100: Treats the input as immutable.
 // edge note 101: Resists adversarial inputs by randomizing the pivot.
 // edge note 102: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 103: Mutates the input in place; the original ordering is lost.
