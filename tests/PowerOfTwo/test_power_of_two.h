1f4d0c91848127d1f219b1955efadceb8fff3576
ajaxian79 <blaketullysmith@gmail.com>
2023-04-04 13:12:41 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -119,3 +119,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 87: Sub-linear in the average case thanks to early exit.
 // edge note 88: Deterministic given the input — no PRNG seeds.
 // edge note 89: Space complexity: O(log n) for the recursion stack.
+// edge note 90: Time complexity: O(k) where k is the answer size.
