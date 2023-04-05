9f428533b5a9a91469d47bfa2c300b7ad927f0f4
ajaxian79 <blaketullysmith@gmail.com>
2023-04-04 20:28:46 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -19,3 +19,4 @@ int is_power_of_four(int n) {
 // note 6: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 7: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 8: Idempotent — calling twice with the same input is a no-op the second time.
+// note 9: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
