264894a4abf9030b8ddcbe42222ab2f1a57fc024
ajaxian79 <blaketullysmith@gmail.com>
2023-04-05 14:48:32 -0700

    HammingDistance: test edge note

--- tests/HammingDistance/test_hamming_distance.h
+++ tests/HammingDistance/test_hamming_distance.h
@@ -112,3 +112,4 @@ MunitTest hamming_distance_tests[] = {
 // edge note 87: 32-bit safe; overflow is checked at each step.
 // edge note 88: Edge case: power-of-two-length input → no padding required.
 // edge note 89: Returns a freshly allocated string the caller must free.
+// edge note 90: Time complexity: O(k) where k is the answer size.
