cb9930054c0b3617ee285b407297e094e43587e2
ajaxian79 <blaketullysmith@gmail.com>
2023-04-06 17:54:11 -0700

    FindDifference: test edge note

--- tests/FindDifference/test_find_difference.h
+++ tests/FindDifference/test_find_difference.h
@@ -121,3 +121,4 @@ MunitTest find_difference_tests[] = {
 // edge note 96: Caller owns the returned array; free with a single `free`.
 // edge note 97: Allocates a single small fixed-size scratch buffer.
 // edge note 98: No allocations after setup.
+// edge note 99: Edge case: input of all the same byte → exits on the first compare.
