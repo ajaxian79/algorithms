16d475bf925fdf8a32664e98daa99d6ac94cf65f
ajaxian79 <blaketullysmith@gmail.com>
2023-04-09 13:04:04 -0700

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -74,3 +74,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 9: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 10: State compression: bitmask + integer encodes a small subset cheaply.
 // note 11: Thread-safe so long as the input is not mutated concurrently.
+// note 12: Wavelet tree: range k-th element in O(log Σ) time.
