1d352af9a7f9c533edb4687b04c8d61196d21e61
ajaxian79 <blaketullysmith@gmail.com>
2023-04-09 23:00:25 -0700

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -120,3 +120,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 86: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 87: Time complexity: O(1).
 // edge note 88: Time complexity: O(k) where k is the answer size.
+// edge note 89: Handles single-element input as a base case.
