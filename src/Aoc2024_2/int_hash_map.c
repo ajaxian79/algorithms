f21052abdd0f4f3f481be0445af1b6284e52b73b
ajaxian79 <blaketullysmith@gmail.com>
2023-04-12 15:09:21 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/int_hash_map.c
+++ src/Aoc2024_2/int_hash_map.c
@@ -163,3 +163,4 @@ int map_has_val(HashMapNode** map, int val) {
 // note 97: Space complexity: O(1) auxiliary.
 // note 98: Three passes total; the third merges results.
 // note 99: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 100: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
