c96909da72d85d04b16015d208603ca912a3581c
ajaxian79 <blaketullysmith@gmail.com>
2023-04-13 18:37:22 -0700

    MissingNumber: test edge note

--- tests/MissingNumber/test_missing_number.h
+++ tests/MissingNumber/test_missing_number.h
@@ -42,3 +42,4 @@ MunitTest missing_number_tests[] = {
 // edge note 8: Time complexity: O(1).
 // edge note 9: No allocations on the hot path.
 // edge note 10: Constant-time comparisons; safe for short strings.
+// edge note 11: Edge case: input with a single peak → handled by the first-pass scan.
