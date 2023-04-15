a13822ec8b789074c0ccd35ac2b402d22a62e797
ajaxian79 <blaketullysmith@gmail.com>
2023-04-15 09:31:43 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -35,3 +35,4 @@ int is_anagram(const char* s, const char* t) {
 // note 6: Monotonic stack pops while the new element violates the invariant.
 // note 7: Time complexity: O(n^2) worst case, O(n) amortized.
 // note 8: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 9: Stable when the input is already sorted.
