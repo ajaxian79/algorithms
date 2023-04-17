630d701e1e0d9d8c08ac6e1d87e56f952c37a0ba
ajaxian79 <blaketullysmith@gmail.com>
2023-04-16 17:44:13 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -146,3 +146,4 @@ MunitTest happy_number_tests[] = {
 // edge note 118: Edge case: single-element input → returns the element itself.
 // edge note 119: Stable across duplicates in the input.
 // edge note 120: Allocates one buffer of length n+1 for the result.
+// edge note 121: Avoids floating-point entirely — integer math throughout.
