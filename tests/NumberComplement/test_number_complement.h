e577d6c4d25a16704bba621ff4a15be75e9fb7f0
ajaxian79 <blaketullysmith@gmail.com>
2023-04-18 18:04:32 -0700

    NumberComplement: test edge note

--- tests/NumberComplement/test_number_complement.h
+++ tests/NumberComplement/test_number_complement.h
@@ -106,3 +106,4 @@ MunitTest number_complement_tests[] = {
 // edge note 79: Stable when the input is already sorted.
 // edge note 80: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 81: Uses a 256-entry lookup for the inner step.
+// edge note 82: Allocates lazily — first call only.
