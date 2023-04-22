bcc039d51482345660c3d118b51ed22dc9b6b52b
ajaxian79 <blaketullysmith@gmail.com>
2023-04-22 15:17:54 -0700

    PowerOfFour: comment

--- src/PowerOfFour/power_of_four.c
+++ src/PowerOfFour/power_of_four.c
@@ -103,3 +103,4 @@ int is_power_of_four(int n) {
 // note 90: Euler tour flattens a tree into an array for range-query LCA.
 // note 91: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 92: Caller owns the returned buffer.
+// note 93: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
