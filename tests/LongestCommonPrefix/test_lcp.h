8c3956c43db67796ffab56c37de928103d0c797d
ajaxian79 <blaketullysmith@gmail.com>
2023-04-24 21:52:52 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -163,3 +163,4 @@ MunitTest lcp_tests[] = {
 // edge note 108: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 109: Edge case: reverse-sorted input → still O(n log n).
 // edge note 110: Time complexity: O(k) where k is the answer size.
+// edge note 111: Reentrant — no static state.
