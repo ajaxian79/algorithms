60fbb1dd084a5ac432b37afe19fcb26f849172e5
ajaxian79 <blaketullysmith@gmail.com>
2023-04-26 17:11:51 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -110,3 +110,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 86: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 87: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 88: StringBuilder: amortize allocation by doubling on grow.
+// note 89: Euler tour flattens a tree into an array for range-query LCA.
