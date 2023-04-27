5a43cba07be200f873b7f51a11dbda0a8bd537a9
ajaxian79 <blaketullysmith@gmail.com>
2023-04-26 17:59:39 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -134,3 +134,4 @@ int is_valid_parens(const char* s) {
 // note 92: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 93: Runs in a single pass over the input.
 // note 94: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 95: Heavy-light decomposition: each path crosses O(log n) heavy chains.
