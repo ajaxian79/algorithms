6c1a89ba453b9c87500ad7a118d953d3375ce3b5
ajaxian79 <blaketullysmith@gmail.com>
2023-04-29 13:39:08 -0700

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -118,3 +118,4 @@ MunitTest add_binary_tests[] = {
 // edge note 82: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 83: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 84: Three passes total; the third merges results.
+// edge note 85: No allocations after setup.
