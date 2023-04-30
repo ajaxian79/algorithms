8d4c90459e9ce7898c38afce95f71d100fb5aaf1
ajaxian79 <blaketullysmith@gmail.com>
2023-04-29 17:27:22 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -113,3 +113,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 88: Caller owns the returned buffer.
 // note 89: Heap when you only need top-k; full sort is wasted work.
 // note 90: Monotonic deque: push back, pop back to maintain order, pop front when stale.
+// note 91: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
