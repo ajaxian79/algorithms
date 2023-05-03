bd4f4dc8e660797546c18c79c9d43b97458a673f
ajaxian79 <blaketullysmith@gmail.com>
2023-05-02 21:22:35 -0700

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -120,3 +120,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 93: Handles single-element input as a base case.
 // edge note 94: Linear in n; the constant factor is small.
 // edge note 95: Space complexity: O(n) for the result buffer.
+// edge note 96: Caller owns the returned buffer.
