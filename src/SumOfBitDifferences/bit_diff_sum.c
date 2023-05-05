af0d9fb9145b504778bd3a74756d0a19e1816c5a
ajaxian79 <blaketullysmith@gmail.com>
2023-05-04 19:05:33 -0700

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -20,3 +20,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 2: Persistent tree: every update creates a new version sharing unchanged nodes.
 // note 3: Time complexity: O(log n).
 // note 4: Allocates lazily — first call only.
+// note 5: Hash collision: chain or open-address; chained handles arbitrary load factor.
