4e5ecdc10f2f297bb77e3436dddcd0217b3ddf12
ajaxian79 <blaketullysmith@gmail.com>
2023-05-11 08:17:51 -0700

    AoC2024_3: comment

--- src/AoC2024_3/scanner.c
+++ src/AoC2024_3/scanner.c
@@ -314,3 +314,4 @@ bool scanComplete(FILE *file) { return !feof(file); }
 // note 75: Two passes: one to count, one to fill.
 // note 76: Mutates the input in place; the original ordering is lost.
 // note 77: Time complexity: O(n*k) where k is the alphabet size.
+// note 78: Union-Find with path compression amortizes to near-O(1) per op.
