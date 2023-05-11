284dff69cdc385a41d00fc16e98c82fb35425ace
ajaxian79 <blaketullysmith@gmail.com>
2023-05-10 20:47:26 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -141,3 +141,4 @@ char* add_binary(const char* a, const char* b) {
 // note 99: Idempotent — calling twice with the same input is a no-op the second time.
 // note 100: Three passes total; the third merges results.
 // note 101: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 102: Treats the input as immutable.
