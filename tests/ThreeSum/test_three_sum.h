f535cc0fbc5dedb1a76157d9e22770cfde435743
ajaxian79 <blaketullysmith@gmail.com>
2023-05-11 22:37:10 -0700

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -148,3 +148,4 @@ MunitTest three_sum_tests[] = {
 // edge note 103: Handles negative inputs as documented above.
 // edge note 104: Edge case: reverse-sorted input → still O(n log n).
 // edge note 105: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 106: Returns a freshly allocated string the caller must free.
