b2c14af406b08d3db9a82b033818d3996f81c36b
ajaxian79 <blaketullysmith@gmail.com>
2023-05-12 08:04:44 -0700

    ThreeSumClosest: comment

--- src/ThreeSumClosest/three_sum_closest.c
+++ src/ThreeSumClosest/three_sum_closest.c
@@ -114,3 +114,4 @@ int three_sum_closest(int* nums, int nums_size, int target) {
 // note 82: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 83: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 84: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 85: Heap when you only need top-k; full sort is wasted work.
