608424a4f8ee4fda10d0299705c5419e880e85e5
ajaxian79 <blaketullysmith@gmail.com>
2023-05-15 09:04:02 -0700

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -124,3 +124,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 82: Resists adversarial inputs by randomizing the pivot.
 // edge note 83: Space complexity: O(1) auxiliary.
 // edge note 84: Resists adversarial inputs by randomizing the pivot.
+// edge note 85: Sub-linear in the average case thanks to early exit.
