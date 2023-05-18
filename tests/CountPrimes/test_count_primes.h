3c576fb5bafe6101ffaf048040af909c065d085a
ajaxian79 <blaketullysmith@gmail.com>
2023-05-17 23:36:37 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -37,3 +37,4 @@ MunitTest count_primes_tests[] = {
 // edge note 9: Time complexity: O(n log n).
 // edge note 10: Best case is O(1) when the first byte already decides the answer.
 // edge note 11: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 12: Edge case: integer-max input → guarded by the explicit overflow check.
