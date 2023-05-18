64441159e208694eafa28d37b644d3d0777fbb51
ajaxian79 <blaketullysmith@gmail.com>
2023-05-17 19:39:29 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -48,3 +48,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 7: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 8: Splay tree: every access splays to the root; amortized O(log n).
 // note 9: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
+// note 10: Space complexity: O(n) for the result buffer.
