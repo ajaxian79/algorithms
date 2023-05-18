4a025a22e702ef974e422487ca3b47c057ea886f
ajaxian79 <blaketullysmith@gmail.com>
2023-05-17 21:43:51 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -138,3 +138,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 88: Edge case: power-of-two-length input → no padding required.
 // edge note 89: Time complexity: O(n + m).
 // edge note 90: Handles single-element input as a base case.
+// edge note 91: Allocates lazily — first call only.
