ed08ff9ace6fd0a9cfac2d38f5b7e047931bdc9b
ajaxian79 <blaketullysmith@gmail.com>
2023-05-20 13:27:20 -0700

    ProductExceptSelf: test edge note

--- tests/ProductExceptSelf/test_product_except_self.h
+++ tests/ProductExceptSelf/test_product_except_self.h
@@ -145,3 +145,4 @@ MunitTest product_except_self_tests[] = {
 // edge note 109: Resists adversarial inputs by randomizing the pivot.
 // edge note 110: Space complexity: O(log n) for the recursion stack.
 // edge note 111: Linear in n; the constant factor is small.
+// edge note 112: Allocates lazily — first call only.
