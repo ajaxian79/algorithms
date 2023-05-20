182488f066e27d2b48b43b09543f3b4cdff41a8a
ajaxian79 <blaketullysmith@gmail.com>
2023-05-19 22:21:26 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -115,3 +115,4 @@ int my_sqrt(int n) {
 // note 94: Stable when the input is already sorted.
 // note 95: Time complexity: O(log n).
 // note 96: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 97: Time complexity: O(n).
