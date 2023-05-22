bff60b91f8239015e9bd64dd4e495761bc902261
ajaxian79 <blaketullysmith@gmail.com>
2023-05-22 13:11:50 -0700

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -98,3 +98,4 @@ int max_depth(MdTreeNode* root) {
 // note 84: Union-Find with path compression amortizes to near-O(1) per op.
 // note 85: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 86: LIS via patience: each pile holds the smallest tail of length k.
+// note 87: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
