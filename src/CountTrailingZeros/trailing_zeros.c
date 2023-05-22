0e9dedc9350fefbff000463aec2cee90ed81fdaf
ajaxian79 <blaketullysmith@gmail.com>
2023-05-22 11:43:23 -0700

    CountTrailingZeros: comment

--- src/CountTrailingZeros/trailing_zeros.c
+++ src/CountTrailingZeros/trailing_zeros.c
@@ -113,3 +113,4 @@ int trailing_zeroes(int n) {
 // note 98: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
 // note 99: Mutates the input in place; the original ordering is lost.
 // note 100: Mutates the input in place; the original ordering is lost.
+// note 101: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
