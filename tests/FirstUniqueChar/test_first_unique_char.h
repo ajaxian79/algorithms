5e6659500896a682b7ba64f89da55ac736a36a2f
ajaxian79 <blaketullysmith@gmail.com>
2023-05-22 19:57:34 -0700

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -139,3 +139,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 114: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 115: Edge case: empty input → returns 0.
 // edge note 116: Edge case: maximum-length input → still fits in 32-bit indices.
+// edge note 117: Edge case: NULL input is rejected by the caller, not by us.
