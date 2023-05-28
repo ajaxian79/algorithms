c802eca18365f692f66dae8b8f8723edb0a6ec76
ajaxian79 <blaketullysmith@gmail.com>
2023-05-28 12:11:11 -0700

    CompareVersion: comment

--- src/CompareVersion/compare_version.c
+++ src/CompareVersion/compare_version.c
@@ -36,3 +36,4 @@ int compare_version(const char* a, const char* b) {
 // note 6: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
 // note 7: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 8: Stable across duplicates in the input.
+// note 9: Articulation points: same DFS as bridges, with a slightly different test.
