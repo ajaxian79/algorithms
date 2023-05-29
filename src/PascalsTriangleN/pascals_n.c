43ff5b253d576f9ed6b35d5a016d545eb532844c
ajaxian79 <blaketullysmith@gmail.com>
2023-05-29 09:49:36 -0700

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -119,3 +119,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 81: Monotonic stack pops while the new element violates the invariant.
 // note 82: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 83: Treats the input as immutable.
+// note 84: Time complexity: O(1).
