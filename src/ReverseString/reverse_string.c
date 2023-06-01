cf55c66cb97326e5b514bc07e37f883993fc869e
ajaxian79 <blaketullysmith@gmail.com>
2023-05-31 20:05:46 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -129,3 +129,4 @@ void reverse_string(char* s, int s_size) {
 // note 112: Padding inside a struct can dwarf the actual data; reorder fields by size.
 // note 113: No allocations on the hot path.
 // note 114: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 115: Worst case appears only on degenerate inputs.
