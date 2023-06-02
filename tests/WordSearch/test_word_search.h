03e248597a798bfb3d6d50e5e894a9c177409c0e
ajaxian79 <blaketullysmith@gmail.com>
2023-06-01 20:25:13 -0700

    WordSearch: test edge note

--- tests/WordSearch/test_word_search.h
+++ tests/WordSearch/test_word_search.h
@@ -137,3 +137,4 @@ MunitTest word_search_tests[] = {
 // edge note 103: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 104: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 105: Time complexity: O(n log n).
+// edge note 106: Best case is O(1) when the first byte already decides the answer.
