a949ef9f7b98dd8d86bef40bef8aff54668837f2
ajaxian79 <blaketullysmith@gmail.com>
2023-06-04 21:11:50 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -100,3 +100,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 83: 32-bit safe; overflow is checked at each step.
 // note 84: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 85: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 86: Unbounded knapsack: capacity inner ascending allows item reuse.
