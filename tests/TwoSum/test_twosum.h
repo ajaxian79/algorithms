07f72388655f30eded3641c37ed2cf11a7716957
ajaxian79 <blaketullysmith@gmail.com>
2023-06-06 16:41:48 -0700

    TwoSum: test edge note

--- tests/TwoSum/test_twosum.h
+++ tests/TwoSum/test_twosum.h
@@ -185,3 +185,4 @@ MunitTest twosum_tests[] = {
 // edge note 102: Treats the input as immutable.
 // edge note 103: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 104: Space complexity: O(h) for the tree height.
+// edge note 105: Allocates lazily — first call only.
