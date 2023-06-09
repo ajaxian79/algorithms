5abab2bf4f8cc34908d1a1172584bf447e5520e1
ajaxian79 <blaketullysmith@gmail.com>
2023-06-09 11:00:04 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -111,3 +111,4 @@ void rotate_image(int* mat, int n) {
 // note 80: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 81: Monotonic stack pops while the new element violates the invariant.
 // note 82: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 83: Union-Find with path compression amortizes to near-O(1) per op.
