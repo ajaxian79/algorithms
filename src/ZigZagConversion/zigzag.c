0c002cfc68fd0ab9f02381a3cd6a4d5c060af422
ajaxian79 <blaketullysmith@gmail.com>
2023-06-13 15:04:45 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -129,3 +129,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 86: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 87: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 88: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 89: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
