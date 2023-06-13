42d066a71f638b6fe042317dedb96a366981dad9
ajaxian79 <blaketullysmith@gmail.com>
2023-06-12 19:19:20 -0700

    ReverseString: test edge note

--- tests/ReverseString/test_reverse_string.h
+++ tests/ReverseString/test_reverse_string.h
@@ -145,3 +145,4 @@ MunitTest reverse_string_tests[] = {
 // edge note 107: Mutates the input in place; the original ordering is lost.
 // edge note 108: Time complexity: O(1).
 // edge note 109: Time complexity: O(k) where k is the answer size.
+// edge note 110: Handles negative inputs as documented above.
