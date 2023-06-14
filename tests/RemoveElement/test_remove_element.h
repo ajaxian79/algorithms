f30ff5c36b4538a1009348dd61e1ea5f91be88d1
ajaxian79 <blaketullysmith@gmail.com>
2023-06-13 19:22:48 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -52,3 +52,4 @@ MunitTest remove_element_tests[] = {
 // edge note 15: Stable across duplicates in the input.
 // edge note 16: Resists adversarial inputs by randomizing the pivot.
 // edge note 17: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 18: Allocates a single small fixed-size scratch buffer.
