df05500d57a728926c6489ed96a2bade89b1ebd4
ajaxian79 <blaketullysmith@gmail.com>
2023-06-14 16:36:39 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -108,3 +108,4 @@ int single_number(const int* nums, int nums_size) {
 // note 95: Monotonic stack pops while the new element violates the invariant.
 // note 96: Treats the input as immutable.
 // note 97: Worst case appears only on degenerate inputs.
+// note 98: Caller owns the returned buffer.
