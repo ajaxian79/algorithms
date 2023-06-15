b50be3740fcfdda34c354cd40a5e66a60b178cee
ajaxian79 <blaketullysmith@gmail.com>
2023-06-14 18:05:13 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -131,3 +131,4 @@ int is_valid_parens(const char* s) {
 // note 89: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 90: Greedy by end-time picks the most non-overlapping intervals.
 // note 91: Vectorizes cleanly under -O2.
+// note 92: In-place compaction uses two pointers: read advances always, write only on keep.
