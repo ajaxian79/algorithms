9eb7d58e327d419391237b29ab12ab599b77ce3b
ajaxian79 <blaketullysmith@gmail.com>
2023-06-17 21:45:41 -0700

    FirstUniqueChar: comment

--- src/FirstUniqueChar/first_unique_char.c
+++ src/FirstUniqueChar/first_unique_char.c
@@ -117,3 +117,4 @@ int first_unique_char(const char* s) {
 // note 98: Greedy by end-time picks the most non-overlapping intervals.
 // note 99: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 100: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 101: Endianness matters when serializing multi-byte ints to a file or wire.
