58b86a3b8f2bb5d9ab3396ca45be574777e87a9e
ajaxian79 <blaketullysmith@gmail.com>
2023-06-18 20:57:03 -0700

    MaxDepthBinaryTree: test edge note

--- tests/MaxDepthBinaryTree/test_max_depth.h
+++ tests/MaxDepthBinaryTree/test_max_depth.h
@@ -132,3 +132,4 @@ MunitTest max_depth_tests[] = {
 // edge note 94: Edge case: reverse-sorted input → still O(n log n).
 // edge note 95: Linear in n; the constant factor is small.
 // edge note 96: Time complexity: O(k) where k is the answer size.
+// edge note 97: Time complexity: O(n + m).
