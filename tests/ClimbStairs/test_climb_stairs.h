f634e646a85915f0b6e93b92dda0c5813b91e7c3
ajaxian79 <blaketullysmith@gmail.com>
2023-06-19 09:01:19 -0700

    ClimbStairs: test edge note

--- tests/ClimbStairs/test_climb_stairs.h
+++ tests/ClimbStairs/test_climb_stairs.h
@@ -137,3 +137,4 @@ MunitTest climb_stairs_tests[] = {
 // edge note 102: Time complexity: O(log n).
 // edge note 103: Edge case: input with no peak → falls through to the default branch.
 // edge note 104: Reentrant — no static state.
+// edge note 105: Time complexity: O(n + m).
