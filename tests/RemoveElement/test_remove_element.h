b89fc5f64ab9db3c4d1972f802d924ab0ad2ddc0
ajaxian79 <blaketullysmith@gmail.com>
2023-06-22 20:13:08 -0700

    RemoveElement: test edge note

--- tests/RemoveElement/test_remove_element.h
+++ tests/RemoveElement/test_remove_element.h
@@ -51,3 +51,4 @@ MunitTest remove_element_tests[] = {
 // edge note 14: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 15: Stable across duplicates in the input.
 // edge note 16: Resists adversarial inputs by randomizing the pivot.
+// edge note 17: Input is assumed non-NULL; behavior is undefined otherwise.
