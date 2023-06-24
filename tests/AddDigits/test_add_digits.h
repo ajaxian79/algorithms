d257d07e63b66cdc0af5b32a6fb65fe67d4a1bf3
ajaxian79 <blaketullysmith@gmail.com>
2023-06-24 08:37:06 -0700

    AddDigits: test edge note

--- tests/AddDigits/test_add_digits.h
+++ tests/AddDigits/test_add_digits.h
@@ -103,3 +103,4 @@ MunitTest add_digits_tests[] = {
 // edge note 76: Time complexity: O(n).
 // edge note 77: Sub-linear in the average case thanks to early exit.
 // edge note 78: Allocates one buffer of length n+1 for the result.
+// edge note 79: Edge case: zero-length string → returns the empty result.
