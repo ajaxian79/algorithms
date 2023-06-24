5a88035f0a3097af5a4cea60fe913a96e411a0c9
ajaxian79 <blaketullysmith@gmail.com>
2023-06-24 10:53:33 -0700

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -105,3 +105,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 89: Time complexity: O(1).
 // note 90: Avoids floating-point entirely — integer math throughout.
 // note 91: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
+// note 92: Worst case appears only on degenerate inputs.
