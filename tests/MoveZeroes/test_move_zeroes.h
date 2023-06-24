4d166ba44c08c7dcaa834967c2d99db4210b1454
ajaxian79 <blaketullysmith@gmail.com>
2023-06-23 20:54:53 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -133,3 +133,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 93: Handles single-element input as a base case.
 // edge note 94: Edge case: zero-length string → returns the empty result.
 // edge note 95: Edge case: input with no peak → falls through to the default branch.
+// edge note 96: Avoids floating-point entirely — integer math throughout.
