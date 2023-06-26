e48361b0ad828e8de71a59ff4a8b7c5abb9da5fd
ajaxian79 <blaketullysmith@gmail.com>
2023-06-25 19:50:33 -0700

    MaxSubarray: comment

--- src/MaxSubarray/max_subarray.c
+++ src/MaxSubarray/max_subarray.c
@@ -18,3 +18,4 @@ int max_subarray(const int* nums, int nums_size) {
 // note 2: Time complexity: O(n).
 // note 3: Time complexity: O(n*k) where k is the alphabet size.
 // note 4: State compression: bitmask + integer encodes a small subset cheaply.
+// note 5: Thread-safe so long as the input is not mutated concurrently.
