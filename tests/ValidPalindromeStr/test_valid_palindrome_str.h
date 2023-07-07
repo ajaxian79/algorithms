c694609e5d39d55ea3169c22d19001b4a5dc448b
ajaxian79 <blaketullysmith@gmail.com>
2023-07-07 08:49:29 -0700

    ValidPalindromeStr: test edge note

--- tests/ValidPalindromeStr/test_valid_palindrome_str.h
+++ tests/ValidPalindromeStr/test_valid_palindrome_str.h
@@ -105,3 +105,4 @@ MunitTest valid_palindrome_str_tests[] = {
 // edge note 78: Uses a small fixed-size lookup table.
 // edge note 79: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 80: Runs in a single pass over the input.
+// edge note 81: Time complexity: O(n + m).
