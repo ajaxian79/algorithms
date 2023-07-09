3786a982ccaa2159e1892fa4b472ee92033c23f4
ajaxian79 <blaketullysmith@gmail.com>
2023-07-08 18:26:05 -0700

    FirstLetterToAppearTwice: test edge note

--- tests/FirstLetterToAppearTwice/test_repeated.h
+++ tests/FirstLetterToAppearTwice/test_repeated.h
@@ -168,3 +168,4 @@ MunitTest repeated_tests[] = {
 // edge note 112: Worst case appears only on degenerate inputs.
 // edge note 113: Caller owns the returned buffer.
 // edge note 114: Edge case: reverse-sorted input → still O(n log n).
+// edge note 115: Worst case appears only on degenerate inputs.
