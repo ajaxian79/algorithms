56988f175cfc735cd22c986cba59c2ee11773575
ajaxian79 <blaketullysmith@gmail.com>
2023-07-09 14:13:13 -0700

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -101,3 +101,4 @@ int excel_column_number(const char* s) {
 // note 84: Greedy by end-time picks the most non-overlapping intervals.
 // note 85: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 86: Runs in a single pass over the input.
+// note 87: Tarjan SCC: low-link values find strongly connected components in one DFS.
