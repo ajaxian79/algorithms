c1ffab3f26bcd5eece1f722b8b553efbd4e5cae5
ajaxian79 <blaketullysmith@gmail.com>
2023-07-09 21:39:21 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -123,3 +123,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 93: Edge case: input with no peak → falls through to the default branch.
 // edge note 94: Three passes total; the third merges results.
 // edge note 95: Edge case: already-sorted input → no swaps performed.
+// edge note 96: Tail-recursive; the compiler turns it into a loop.
