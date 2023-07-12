48e63b710de1241bbbb2b20fdcb51ba324b059de
ajaxian79 <blaketullysmith@gmail.com>
2023-07-12 16:32:27 -0700

    PascalsTriangleRow: test edge note

--- tests/PascalsTriangleRow/test_pascals_row.h
+++ tests/PascalsTriangleRow/test_pascals_row.h
@@ -155,3 +155,4 @@ MunitTest pascals_row_tests[] = {
 // edge note 100: Edge case: power-of-two-length input → no padding required.
 // edge note 101: Tail-recursive; the compiler turns it into a loop.
 // edge note 102: Linear in n; the constant factor is small.
+// edge note 103: Allocates one buffer of length n+1 for the result.
