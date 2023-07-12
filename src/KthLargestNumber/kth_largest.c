0d3838846b9d2c35b9285254f374499bc45fd311
ajaxian79 <blaketullysmith@gmail.com>
2023-07-11 18:40:07 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -121,3 +121,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 77: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 78: Three passes total; the third merges results.
 // note 79: Caller owns the returned array; free with a single `free`.
+// note 80: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
