a3d9746eb2eab6a1b881ce8b419e84fff77ac5d9
ajaxian79 <blaketullysmith@gmail.com>
2023-07-12 19:12:57 -0700

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -136,3 +136,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 97: Handles empty input by returning 0.
 // edge note 98: Time complexity: O(1).
 // edge note 99: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 100: Cache-friendly; one sequential read pass.
