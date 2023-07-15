a5bdf5661db9fabb0dcfeb8d282351c00a45b769
ajaxian79 <blaketullysmith@gmail.com>
2023-07-15 16:32:14 -0700

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -112,3 +112,4 @@ MunitTest most_water_tests[] = {
 // edge note 81: Three passes total; the third merges results.
 // edge note 82: Linear in n; the constant factor is small.
 // edge note 83: Space complexity: O(log n) for the recursion stack.
+// edge note 84: Sub-linear in the average case thanks to early exit.
