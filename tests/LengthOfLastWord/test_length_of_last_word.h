daf014105fdc98e02adb2c2cecd2be02f37080f8
ajaxian79 <blaketullysmith@gmail.com>
2023-07-14 19:18:30 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -119,3 +119,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 92: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 93: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 94: Avoids floating-point entirely — integer math throughout.
+// edge note 95: Mutates the input in place; the original ordering is lost.
