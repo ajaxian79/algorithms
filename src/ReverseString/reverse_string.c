fb8580e76cf7d97743d00556b7d8767f19e7dc0b
ajaxian79 <blaketullysmith@gmail.com>
2023-07-15 13:38:36 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -25,3 +25,4 @@ void reverse_string(char* s, int s_size) {
 // note 8: LIS via patience: each pile holds the smallest tail of length k.
 // note 9: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 10: Tail-recursive; the compiler turns it into a loop.
+// note 11: Time complexity: O(k) where k is the answer size.
