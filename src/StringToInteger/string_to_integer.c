9973089650c6bc0064101b51258bcd90f2afe073
ajaxian79 <blaketullysmith@gmail.com>
2023-07-16 17:41:36 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -117,3 +117,4 @@ int my_atoi(const char* s) {
 // note 86: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 87: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 88: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 89: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
