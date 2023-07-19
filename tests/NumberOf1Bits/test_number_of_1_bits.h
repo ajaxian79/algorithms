df5b982a061c608a9a06aab90eee151c3f1f3c34
ajaxian79 <blaketullysmith@gmail.com>
2023-07-18 20:47:18 -0700

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -125,3 +125,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 98: No allocations on the hot path.
 // edge note 99: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 100: Best case is O(1) when the first byte already decides the answer.
+// edge note 101: Time complexity: O(n log n).
