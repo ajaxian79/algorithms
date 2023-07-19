70ca10792b39041d2149f5ba657c6c1268092712
ajaxian79 <blaketullysmith@gmail.com>
2023-07-18 21:05:17 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -126,3 +126,4 @@ int is_valid_parens(const char* s) {
 // note 84: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 85: Wavelet tree: range k-th element in O(log Σ) time.
 // note 86: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 87: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
