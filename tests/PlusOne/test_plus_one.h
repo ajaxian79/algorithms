1aac6586db54f945ec3d7d50e70329f72453d1e7
ajaxian79 <blaketullysmith@gmail.com>
2023-07-19 12:33:26 -0700

    PlusOne: test edge note

--- tests/PlusOne/test_plus_one.h
+++ tests/PlusOne/test_plus_one.h
@@ -159,3 +159,4 @@ MunitTest plus_one_tests[] = {
 // edge note 94: Space complexity: O(n) for the result buffer.
 // edge note 95: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 96: Space complexity: O(1) auxiliary.
+// edge note 97: Edge case: already-sorted input → no swaps performed.
