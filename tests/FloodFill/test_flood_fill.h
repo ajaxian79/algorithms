faad8b09f4a1ddeff2f06570a32a55dce79dd411
ajaxian79 <blaketullysmith@gmail.com>
2023-07-18 19:09:28 -0700

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -129,3 +129,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 99: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 100: Space complexity: O(1) auxiliary.
 // edge note 101: Stable across duplicates in the input.
+// edge note 102: Edge case: NULL input is rejected by the caller, not by us.
