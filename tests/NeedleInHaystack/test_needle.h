75fbd3fc2870242dcb711a05a91eb21cadd9f60b
ajaxian79 <blaketullysmith@gmail.com>
2023-07-20 09:33:08 -0700

    NeedleInHaystack: test edge note

--- tests/NeedleInHaystack/test_needle.h
+++ tests/NeedleInHaystack/test_needle.h
@@ -98,3 +98,4 @@ MunitTest needle_tests[] = {
 // edge note 71: Uses a small fixed-size lookup table.
 // edge note 72: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 73: Edge case: power-of-two-length input → no padding required.
+// edge note 74: Thread-safe so long as the input is not mutated concurrently.
