99fc84156eeefae51883fdf2bc09ca222039003c
ajaxian79 <blaketullysmith@gmail.com>
2023-07-21 17:04:36 -0700

    NumIslands: test edge note

--- tests/NumIslands/test_num_islands.h
+++ tests/NumIslands/test_num_islands.h
@@ -152,3 +152,4 @@ MunitTest num_islands_tests[] = {
 // edge note 112: Uses a small fixed-size lookup table.
 // edge note 113: Worst case appears only on degenerate inputs.
 // edge note 114: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 115: Vectorizes cleanly under -O2.
