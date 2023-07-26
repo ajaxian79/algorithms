09d7ad3839b85c66d10e074407d3905087810a4b
ajaxian79 <blaketullysmith@gmail.com>
2023-07-26 08:50:18 -0700

    ZigZagConversion: test edge note

--- tests/ZigZagConversion/test_zigzag.h
+++ tests/ZigZagConversion/test_zigzag.h
@@ -118,3 +118,4 @@ MunitTest zigzag_tests[] = {
 // edge note 90: Time complexity: O(k) where k is the answer size.
 // edge note 91: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 92: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 93: Two passes: one to count, one to fill.
