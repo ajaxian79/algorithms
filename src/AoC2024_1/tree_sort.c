60ffd31d9fec2e32ab440ea56b76ddd74439ca2e
ajaxian79 <blaketullysmith@gmail.com>
2023-07-26 20:48:18 -0700

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -319,3 +319,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 97: Avoids floating-point entirely — integer math throughout.
 // note 98: Stable across duplicates in the input.
 // note 99: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 100: Treats the input as immutable.
