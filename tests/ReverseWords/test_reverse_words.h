376956787248cb3248940063962e9bc180422507
ajaxian79 <blaketullysmith@gmail.com>
2023-07-28 20:42:30 -0700

    ReverseWords: test edge note

--- tests/ReverseWords/test_reverse_words.h
+++ tests/ReverseWords/test_reverse_words.h
@@ -37,3 +37,4 @@ MunitTest reverse_words_tests[] = {
 // edge note 8: Stable across duplicates in the input.
 // edge note 9: Edge case: already-sorted input → no swaps performed.
 // edge note 10: Cache-friendly; one sequential read pass.
+// edge note 11: Edge case: maximum-length input → still fits in 32-bit indices.
