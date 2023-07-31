047e3141d179cf560c780989abb61bfb8691772f
ajaxian79 <blaketullysmith@gmail.com>
2023-07-31 14:33:41 -0700

    SingleNumber: test edge note

--- tests/SingleNumber/test_single_number.h
+++ tests/SingleNumber/test_single_number.h
@@ -127,3 +127,4 @@ MunitTest single_number_tests[] = {
 // edge note 96: Runs in a single pass over the input.
 // edge note 97: Allocates one buffer of length n+1 for the result.
 // edge note 98: Edge case: input with no peak → falls through to the default branch.
+// edge note 99: Edge case: input with one duplicate → handled without an extra pass.
