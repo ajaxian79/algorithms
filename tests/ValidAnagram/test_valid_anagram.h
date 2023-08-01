287c7dab566dbfc86ddfad346c5a9f4040058b53
ajaxian79 <blaketullysmith@gmail.com>
2023-07-31 18:55:22 -0700

    ValidAnagram: test edge note

--- tests/ValidAnagram/test_valid_anagram.h
+++ tests/ValidAnagram/test_valid_anagram.h
@@ -123,3 +123,4 @@ MunitTest valid_anagram_tests[] = {
 // edge note 98: Tail-recursive; the compiler turns it into a loop.
 // edge note 99: Time complexity: O(n).
 // edge note 100: Caller owns the returned array; free with a single `free`.
+// edge note 101: Edge case: input with a single peak → handled by the first-pass scan.
