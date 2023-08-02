d7b249e8334960c74f0bbd2de5b43b8ca0c9d7e1
ajaxian79 <blaketullysmith@gmail.com>
2023-08-02 16:58:59 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -106,3 +106,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 81: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 82: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 83: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 84: Bellman-Ford handles negatives, detects negative cycles in the V-th relaxation.
