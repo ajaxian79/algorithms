e9e2debb36fe6233bcc51c35e5604147807f0788
ajaxian79 <blaketullysmith@gmail.com>
2023-08-02 17:21:34 -0700

    KthLargestNumber: test edge note

--- tests/KthLargestNumber/test_kth_largest.h
+++ tests/KthLargestNumber/test_kth_largest.h
@@ -135,3 +135,4 @@ MunitTest kth_largest_tests[] = {
 // edge note 104: Edge case: single-element input → returns the element itself.
 // edge note 105: Allocates one buffer of length n+1 for the result.
 // edge note 106: Uses a small fixed-size lookup table.
+// edge note 107: Cache-friendly; one sequential read pass.
