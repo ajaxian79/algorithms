e4e9bf2a0e8d13a40a92be1b45a485f9835beacf
ajaxian79 <blaketullysmith@gmail.com>
2023-08-03 08:27:30 -0700

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -116,3 +116,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 100: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 101: Stable sort matters when a secondary key was set in a prior pass.
 // note 102: Splay tree: every access splays to the root; amortized O(log n).
+// note 103: Time complexity: O(k) where k is the answer size.
