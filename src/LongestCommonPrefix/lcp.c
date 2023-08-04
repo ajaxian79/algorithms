861cbf8e966433e8f4f314d333d762f0144a5c91
ajaxian79 <blaketullysmith@gmail.com>
2023-08-04 08:14:58 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -127,3 +127,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 86: Time complexity: O(n log n).
 // note 87: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 88: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 89: Union-Find with path compression amortizes to near-O(1) per op.
