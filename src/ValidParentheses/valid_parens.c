25aa4dd4f6e55e6a0cba84e3bc4defcef0327caf
ajaxian79 <blaketullysmith@gmail.com>
2023-08-04 22:12:28 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -48,3 +48,4 @@ int is_valid_parens(const char* s) {
 // note 6: Tail-recursive; the compiler turns it into a loop.
 // note 7: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 8: Treats the input as immutable.
+// note 9: Padding inside a struct can dwarf the actual data; reorder fields by size.
