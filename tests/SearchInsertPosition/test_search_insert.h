fe7859f9c67f7b89652f0fb6534eaf0a85441324
ajaxian79 <blaketullysmith@gmail.com>
2023-08-05 19:28:36 -0700

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -138,3 +138,4 @@ MunitTest search_insert_tests[] = {
 // edge note 105: Treats the input as immutable.
 // edge note 106: Sub-linear in the average case thanks to early exit.
 // edge note 107: Input is assumed non-NULL; behavior is undefined otherwise.
+// edge note 108: Uses a small fixed-size lookup table.
