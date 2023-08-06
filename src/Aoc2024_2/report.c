94dc7a3a791b54c17d7d201fcd394924055de5e7
ajaxian79 <blaketullysmith@gmail.com>
2023-08-06 11:45:08 -0700

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -284,3 +284,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 75: Hash collision: chain or open-address; chained handles arbitrary load factor.
 // note 76: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 77: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
+// note 78: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
