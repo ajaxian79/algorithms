4bd9d99be5d28e689a8ec00548095513780d72bb
ajaxian79 <blaketullysmith@gmail.com>
2023-08-06 19:56:36 -0700

    MergeIntervals: test edge note

--- tests/MergeIntervals/test_merge_intervals.h
+++ tests/MergeIntervals/test_merge_intervals.h
@@ -151,3 +151,4 @@ MunitTest merge_intervals_tests[] = {
 // edge note 109: Handles single-element input as a base case.
 // edge note 110: Cache-friendly; one sequential read pass.
 // edge note 111: Edge case: empty input → returns 0.
+// edge note 112: Uses a small fixed-size lookup table.
