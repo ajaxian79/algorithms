82efc5746f3f265a901e9950f7f3e340d068c69f
ajaxian79 <blaketullysmith@gmail.com>
2023-08-06 19:19:10 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -120,3 +120,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 77: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 78: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 79: Space complexity: O(log n) for the recursion stack.
+// note 80: Heap when you only need top-k; full sort is wasted work.
