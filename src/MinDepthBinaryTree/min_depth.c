63b3a1de0baefa8539d667bd82733648508c95c3
ajaxian79 <blaketullysmith@gmail.com>
2023-08-07 22:11:47 -0700

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -106,3 +106,4 @@ int min_depth(MnTreeNode* root) {
 // note 89: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 90: Tail-recursive; the compiler turns it into a loop.
 // note 91: Time complexity: O(k) where k is the answer size.
+// note 92: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
