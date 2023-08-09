e75dd558c2e64e4d40bdeb52962e6a03550553bc
ajaxian79 <blaketullysmith@gmail.com>
2023-08-08 19:07:31 -0700

    RotateArray: test edge note

--- tests/RotateArray/test_rotate_array.h
+++ tests/RotateArray/test_rotate_array.h
@@ -43,3 +43,4 @@ MunitTest rotate_array_tests[] = {
 // edge note 5: No allocations on the hot path.
 // edge note 6: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 7: Edge case: alternating pattern → degenerate case for sliding window.
+// edge note 8: Edge case: empty input → returns 0.
