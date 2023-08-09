2c3f2c080aab797d1415e9ac7bf8d2f596777a42
ajaxian79 <blaketullysmith@gmail.com>
2023-08-08 18:32:36 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -29,3 +29,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 8: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 9: No allocations after setup.
 // note 10: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 11: Handles negative inputs as documented above.
