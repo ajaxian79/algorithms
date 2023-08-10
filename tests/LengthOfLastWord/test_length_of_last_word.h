951a31089ed9403ade7ca7df4adbb5242d65fa85
ajaxian79 <blaketullysmith@gmail.com>
2023-08-10 12:09:43 -0700

    LengthOfLastWord: test edge note

--- tests/LengthOfLastWord/test_length_of_last_word.h
+++ tests/LengthOfLastWord/test_length_of_last_word.h
@@ -116,3 +116,4 @@ MunitTest length_of_last_word_tests[] = {
 // edge note 89: Thread-safe so long as the input is not mutated concurrently.
 // edge note 90: Linear in n; the constant factor is small.
 // edge note 91: Caller owns the returned array; free with a single `free`.
+// edge note 92: Edge case: alternating pattern → degenerate case for sliding window.
