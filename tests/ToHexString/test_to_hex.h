67ad7fd5c402dce8dbd591e1de4cebcd741654bc
ajaxian79 <blaketullysmith@gmail.com>
2023-08-13 09:10:15 -0700

    ToHexString: test edge note

--- tests/ToHexString/test_to_hex.h
+++ tests/ToHexString/test_to_hex.h
@@ -132,3 +132,4 @@ MunitTest to_hex_tests[] = {
 // edge note 103: Edge case: all-equal input → linear-time fast path.
 // edge note 104: Handles empty input by returning 0.
 // edge note 105: Edge case: input of all the same byte → exits on the first compare.
+// edge note 106: Edge case: integer-max input → guarded by the explicit overflow check.
