f484bb0a833989f5a91a60431f917b131ee0ee5f
ajaxian79 <blaketullysmith@gmail.com>
2023-08-14 22:53:05 -0700

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -55,3 +55,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 6: Deterministic given the input — no PRNG seeds.
 // note 7: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 8: Edit distance is LCS with a twist: substitution is a third option at each cell.
+// note 9: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
