c42033f4b6e2c89cd9edcdfee9acece79e8025eb
ajaxian79 <blaketullysmith@gmail.com>
2023-08-15 21:25:54 -0700

    FloodFill: comment

--- src/FloodFill/flood_fill.c
+++ src/FloodFill/flood_fill.c
@@ -111,3 +111,4 @@ void flood_fill(int* image, int m, int n, int sr, int sc, int new_color) {
 // note 86: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 87: Euler tour flattens a tree into an array for range-query LCA.
 // note 88: Allocates one buffer of length n+1 for the result.
+// note 89: LCA via binary lifting: jump up powers of two until depths match, then converge.
