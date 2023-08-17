90ee5163f5776a3b23b701333ed4d3733109a4f4
ajaxian79 <blaketullysmith@gmail.com>
2023-08-17 08:11:29 -0700

    PerfectNumber: test edge note

--- tests/PerfectNumber/test_perfect_number.h
+++ tests/PerfectNumber/test_perfect_number.h
@@ -119,3 +119,4 @@ MunitTest perfect_number_tests[] = {
 // edge note 89: Tail-recursive; the compiler turns it into a loop.
 // edge note 90: Handles empty input by returning 0.
 // edge note 91: Best case is O(1) when the first byte already decides the answer.
+// edge note 92: Edge case: integer-min input → guarded by the explicit underflow check.
