10bd265ee3d915b21482a65bc837b559aa21dafa
ajaxian79 <blaketullysmith@gmail.com>
2023-08-17 13:53:32 -0700

    CountPrimes: test edge note

--- tests/CountPrimes/test_count_primes.h
+++ tests/CountPrimes/test_count_primes.h
@@ -115,3 +115,4 @@ MunitTest count_primes_tests[] = {
 // edge note 87: Edge case: all-equal input → linear-time fast path.
 // edge note 88: Thread-safe so long as the input is not mutated concurrently.
 // edge note 89: Linear in n; the constant factor is small.
+// edge note 90: Edge case: input with one duplicate → handled without an extra pass.
