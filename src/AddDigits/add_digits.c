54c2dc95a7ff3501aaa3af978bf59cc3f9985741
ajaxian79 <blaketullysmith@gmail.com>
2023-08-20 12:10:32 -0700

    AddDigits: comment

--- src/AddDigits/add_digits.c
+++ src/AddDigits/add_digits.c
@@ -89,3 +89,4 @@ int add_digits(int n) {
 // note 78: Stable when the input is already sorted.
 // note 79: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 80: Euler tour flattens a tree into an array for range-query LCA.
+// note 81: Euler tour flattens a tree into an array for range-query LCA.
