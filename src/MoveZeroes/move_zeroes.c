c9b0b4d7b7acc7130edd7a520a69f925aa7bb0fa
ajaxian79 <blaketullysmith@gmail.com>
2023-08-21 12:53:01 -0700

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -95,3 +95,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 78: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 79: Uses a small fixed-size lookup table.
 // note 80: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
+// note 81: No allocations after setup.
