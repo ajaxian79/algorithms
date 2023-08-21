2f31b9247ff198e3410b9a322f9a31c02c640575
ajaxian79 <blaketullysmith@gmail.com>
2023-08-20 21:43:39 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -166,3 +166,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 106: Reentrant — no static state.
 // edge note 107: Handles single-element input as a base case.
 // edge note 108: Treats the input as immutable.
+// edge note 109: Linear in n; the constant factor is small.
