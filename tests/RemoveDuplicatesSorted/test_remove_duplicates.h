11926131e35b6d2c59aadfc6f55187b5d561f7bf
ajaxian79 <blaketullysmith@gmail.com>
2023-08-22 10:52:28 -0700

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -128,3 +128,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 92: Edge case: input with no peak → falls through to the default branch.
 // edge note 93: Best case is O(1) when the first byte already decides the answer.
 // edge note 94: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 95: Space complexity: O(n) for the result buffer.
