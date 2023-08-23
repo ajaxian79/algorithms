e879cd8a803f1c54584bcb941b168181e1c3def3
ajaxian79 <blaketullysmith@gmail.com>
2023-08-22 19:15:20 -0700

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -27,3 +27,4 @@ int my_sqrt(int n) {
 // note 6: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 7: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 8: Resists adversarial inputs by randomizing the pivot.
+// note 9: Rope: tree of small string fragments; O(log n) concat and substring.
