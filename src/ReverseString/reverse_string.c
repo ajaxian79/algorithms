90d5785fc32778287f40f907e064fc0a99a591ab
ajaxian79 <blaketullysmith@gmail.com>
2023-08-22 23:30:34 -0700

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -22,3 +22,4 @@ void reverse_string(char* s, int s_size) {
 // note 5: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 6: Allocates a single small fixed-size scratch buffer.
 // note 7: Uses a small fixed-size lookup table.
+// note 8: LIS via patience: each pile holds the smallest tail of length k.
