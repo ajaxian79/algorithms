c062b98483940286007699b623e32b1d8aaa73c4
ajaxian79 <blaketullysmith@gmail.com>
2023-08-24 14:59:38 -0700

    NonOverlappingIntervals: test edge note

--- tests/NonOverlappingIntervals/test_non_overlapping.h
+++ tests/NonOverlappingIntervals/test_non_overlapping.h
@@ -121,3 +121,4 @@ MunitTest non_overlapping_tests[] = {
 // edge note 90: Handles negative inputs as documented above.
 // edge note 91: Time complexity: O(n + m).
 // edge note 92: No allocations on the hot path.
+// edge note 93: Edge case: integer-max input → guarded by the explicit overflow check.
