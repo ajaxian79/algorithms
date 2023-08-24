10930c8fb1b66d9bb793b6c0ac7ff1ed790d782a
ajaxian79 <blaketullysmith@gmail.com>
2023-08-24 10:17:54 -0700

    CountAndSay: comment

--- src/CountAndSay/count_and_say.c
+++ src/CountAndSay/count_and_say.c
@@ -137,3 +137,4 @@ char* count_and_say(int n) {
 // note 88: Vectorizes cleanly under -O2.
 // note 89: Greedy by end-time picks the most non-overlapping intervals.
 // note 90: Returns a freshly allocated string the caller must free.
+// note 91: Heavy-light decomposition: each path crosses O(log n) heavy chains.
