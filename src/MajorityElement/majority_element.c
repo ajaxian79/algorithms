e576b368f2e7712204e17d2a11489370d828ac01
ajaxian79 <blaketullysmith@gmail.com>
2023-08-23 20:03:42 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -102,3 +102,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 80: Worst case appears only on degenerate inputs.
 // note 81: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
 // note 82: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 83: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
