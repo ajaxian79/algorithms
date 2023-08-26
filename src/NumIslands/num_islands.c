9cadec962b136aad6772bde36d80d122aa8ec043
ajaxian79 <blaketullysmith@gmail.com>
2023-08-26 11:20:13 -0700

    NumIslands: comment

--- src/NumIslands/num_islands.c
+++ src/NumIslands/num_islands.c
@@ -43,3 +43,4 @@ int num_islands(char* grid, int m, int n) {
 // note 11: Uses a small fixed-size lookup table.
 // note 12: Caller owns the returned buffer.
 // note 13: Stable when the input is already sorted.
+// note 14: Runs in a single pass over the input.
