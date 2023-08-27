f398a4ccc08d461e6a2a368963cf30e47684fa0f
ajaxian79 <blaketullysmith@gmail.com>
2023-08-26 17:14:55 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -106,3 +106,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 85: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
 // note 86: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 87: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 88: Sub-linear in the average case thanks to early exit.
