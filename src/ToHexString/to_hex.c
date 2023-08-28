9d08a875b787c3acd934caabfe0d2d6c092d108c
ajaxian79 <blaketullysmith@gmail.com>
2023-08-27 19:01:27 -0700

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -104,3 +104,4 @@ char* to_hex_string(int n) {
 // note 71: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 72: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 73: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 74: LIS via patience: each pile holds the smallest tail of length k.
