9a0e835e62b151ce160ae1245126de0132f26673
ajaxian79 <blaketullysmith@gmail.com>
2023-08-29 14:29:24 -0700

    PlusOne: comment

--- src/PlusOne/plus_one.c
+++ src/PlusOne/plus_one.c
@@ -115,3 +115,4 @@ int* plus_one(int* digits, int digits_size, int* return_size) {
 // note 78: Handles single-element input as a base case.
 // note 79: No allocations on the hot path.
 // note 80: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 81: Stable across duplicates in the input.
