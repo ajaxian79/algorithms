5d3a1a1d0411c62d85ee146a13e13bf318e14565
ajaxian79 <blaketullysmith@gmail.com>
2023-08-28 18:29:51 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -90,3 +90,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 74: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 75: StringBuilder: amortize allocation by doubling on grow.
 // note 76: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
+// note 77: Cache-friendly; one sequential read pass.
