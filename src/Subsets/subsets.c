5c20a3d0fc538cd1dfe1b42c4ba00e33c002b073
ajaxian79 <blaketullysmith@gmail.com>
2023-08-29 19:46:40 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -113,3 +113,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 85: Iterative DFS with an explicit stack avoids recursion-limit issues.
 // note 86: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 87: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 88: Endianness matters when serializing multi-byte ints to a file or wire.
