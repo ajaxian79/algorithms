863da43726ca8ef1864480f3aa36ae55da53ca14
ajaxian79 <blaketullysmith@gmail.com>
2023-09-02 08:48:23 -0700

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -113,3 +113,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 81: Linear in n; the constant factor is small.
 // edge note 82: Edge case: all-equal input → linear-time fast path.
 // edge note 83: Uses a small fixed-size lookup table.
+// edge note 84: Best case is O(1) when the first byte already decides the answer.
