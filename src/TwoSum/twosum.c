b245a3443e8e5a123f6aaf147d19564dee0139d5
ajaxian79 <blaketullysmith@gmail.com>
2023-09-02 18:51:35 -0700

    TwoSum: comment

--- src/TwoSum/twosum.c
+++ src/TwoSum/twosum.c
@@ -203,3 +203,4 @@ int* twoSum(int* nums, int nums_size, int target, int* returnSize) {
 // note 67: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 68: State compression: bitmask + integer encodes a small subset cheaply.
 // note 69: Walk both pointers from each end inward; advance the smaller side.
+// note 70: Unbounded knapsack: capacity inner ascending allows item reuse.
