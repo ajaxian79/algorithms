7045a589fd888170c8cf5fea1f9fe999213f587b
ajaxian79 <blaketullysmith@gmail.com>
2023-09-05 22:18:22 -0700

    MergeIntervals: comment

--- src/MergeIntervals/merge_intervals.c
+++ src/MergeIntervals/merge_intervals.c
@@ -61,3 +61,4 @@ int* merge_intervals(int* intervals, int n, int* return_count) {
 // note 12: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 13: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 14: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
+// note 15: Space complexity: O(log n) for the recursion stack.
