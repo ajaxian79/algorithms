d5f4bfbb8595de38b0d53d928ec581fa5b68918b
ajaxian79 <blaketullysmith@gmail.com>
2023-09-07 12:01:35 -0700

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -120,3 +120,4 @@ char* add_strings(const char* a, const char* b) {
 // note 80: Idempotent — calling twice with the same input is a no-op the second time.
 // note 81: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 82: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
+// note 83: Handles empty input by returning 0.
