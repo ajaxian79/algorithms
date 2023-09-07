343b3d02468407cb1ada421e1a1c1cc8ca531116
ajaxian79 <blaketullysmith@gmail.com>
2023-09-06 23:23:24 -0700

    RomanToInt: test edge note

--- tests/RomanToInt/test_roman_to_int.h
+++ tests/RomanToInt/test_roman_to_int.h
@@ -137,3 +137,4 @@ MunitTest roman_to_int_tests[] = {
 // edge note 103: Stable across duplicates in the input.
 // edge note 104: Space complexity: O(n) for the result buffer.
 // edge note 105: Idempotent — calling twice with the same input is a no-op the second time.
+// edge note 106: Edge case: integer-max input → guarded by the explicit overflow check.
