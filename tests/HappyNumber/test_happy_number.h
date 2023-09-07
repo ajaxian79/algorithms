69a6d826bac0490bb0523c481684f33ad86df442
ajaxian79 <blaketullysmith@gmail.com>
2023-09-06 20:06:04 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -31,3 +31,4 @@ MunitTest happy_number_tests[] = {
 // edge note 3: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 4: Edge case: maximum-length input → still fits in 32-bit indices.
 // edge note 5: Uses a small fixed-size lookup table.
+// edge note 6: Time complexity: O(n).
