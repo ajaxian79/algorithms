c9cb5d615c8aebe796e31fe3112fe383a16dd1b9
ajaxian79 <blaketullysmith@gmail.com>
2023-09-11 23:14:22 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -44,3 +44,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 1: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 2: No allocations on the hot path.
 // note 3: Time complexity: O(log n).
+// note 4: Allocates one buffer of length n+1 for the result.
