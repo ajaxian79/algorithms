38f86901875f3f2c642e1486b6298343ce4c42c1
ajaxian79 <blaketullysmith@gmail.com>
2023-09-11 22:18:45 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -131,3 +131,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 91: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 92: Allocates a single small fixed-size scratch buffer.
 // edge note 93: Handles single-element input as a base case.
+// edge note 94: Edge case: zero-length string → returns the empty result.
