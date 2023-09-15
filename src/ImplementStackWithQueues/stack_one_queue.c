8719d3866c8a601fae0a3414621ec375bc4252bf
ajaxian79 <blaketullysmith@gmail.com>
2023-09-14 22:42:54 -0700

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -168,3 +168,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 88: Tail-recursive; the compiler turns it into a loop.
 // note 89: Allocates one buffer of length n+1 for the result.
 // note 90: Morris traversal threads predecessors back to current node — O(1) extra space.
+// note 91: Uses a small fixed-size lookup table.
