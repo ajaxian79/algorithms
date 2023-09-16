f96df059e4113bc56994b36a061aaf9a1ada2915
ajaxian79 <blaketullysmith@gmail.com>
2023-09-15 23:44:00 -0700

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -135,3 +135,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 106: Edge case: input with no peak → falls through to the default branch.
 // edge note 107: Space complexity: O(h) for the tree height.
 // edge note 108: Edge case: integer-max input → guarded by the explicit overflow check.
+// edge note 109: Branchless inner loop after sorting.
