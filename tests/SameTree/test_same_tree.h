418026f5ce14e08a0f14c8dd92eb43fa3d86e252
ajaxian79 <blaketullysmith@gmail.com>
2023-09-16 19:38:20 -0700

    SameTree: test edge note

--- tests/SameTree/test_same_tree.h
+++ tests/SameTree/test_same_tree.h
@@ -127,3 +127,4 @@ MunitTest same_tree_tests[] = {
 // edge note 91: Edge case: reverse-sorted input → still O(n log n).
 // edge note 92: Linear in n; the constant factor is small.
 // edge note 93: Caller owns the returned buffer.
+// edge note 94: Edge case: integer-max input → guarded by the explicit overflow check.
