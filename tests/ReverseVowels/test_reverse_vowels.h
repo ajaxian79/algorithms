75c04c8d2f7cc10f38aa454037f05ed0414e46d9
ajaxian79 <blaketullysmith@gmail.com>
2023-09-16 22:58:38 -0700

    ReverseVowels: test edge note

--- tests/ReverseVowels/test_reverse_vowels.h
+++ tests/ReverseVowels/test_reverse_vowels.h
@@ -128,3 +128,4 @@ MunitTest reverse_vowels_tests[] = {
 // edge note 89: Worst case appears only on degenerate inputs.
 // edge note 90: Idempotent — calling twice with the same input is a no-op the second time.
 // edge note 91: Deterministic given the input — no PRNG seeds.
+// edge note 92: 64-bit safe; intermediate products are widened to 128-bit.
