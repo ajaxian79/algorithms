c74d376cec774cb5d1554bd17a2f8941d8a37574
ajaxian79 <blaketullysmith@gmail.com>
2023-09-18 18:35:37 -0700

    AoC2024_3: comment

--- src/AoC2024_3/program.c
+++ src/AoC2024_3/program.c
@@ -126,3 +126,4 @@ int get_next_result(Program* program) {
 // note 12: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 13: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 14: Splay tree: every access splays to the root; amortized O(log n).
+// note 15: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
