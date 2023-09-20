1721adf8985b898ecf8494f0d83afaf1e4d85300
ajaxian79 <blaketullysmith@gmail.com>
2023-09-20 10:01:13 -0700

    UglyNumber: test edge note

--- tests/UglyNumber/test_ugly_number.h
+++ tests/UglyNumber/test_ugly_number.h
@@ -121,3 +121,4 @@ MunitTest ugly_number_tests[] = {
 // edge note 92: Returns a freshly allocated string the caller must free.
 // edge note 93: Uses a 256-entry lookup for the inner step.
 // edge note 94: Returns a freshly allocated string the caller must free.
+// edge note 95: Space complexity: O(h) for the tree height.
