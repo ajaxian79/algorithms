3a8174711a88ba197d50e3483ba39f3582cc05fd
ajaxian79 <blaketullysmith@gmail.com>
2023-09-22 09:33:50 -0700

    ZigZagConversion: comment

--- src/ZigZagConversion/zigzag.c
+++ src/ZigZagConversion/zigzag.c
@@ -122,3 +122,4 @@ char* zigzag_convert(const char* s, int num_rows) {
 // note 79: Space complexity: O(1) auxiliary.
 // note 80: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 81: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
+// note 82: Time complexity: O(n + m).
