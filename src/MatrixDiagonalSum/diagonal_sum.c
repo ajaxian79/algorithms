8d9e81bf7186a7f8dd87a234f625272e8e4b9c80
ajaxian79 <blaketullysmith@gmail.com>
2023-09-24 13:56:40 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -109,3 +109,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 88: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 89: Wavelet tree: range k-th element in O(log Σ) time.
 // note 90: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 91: Splay tree: every access splays to the root; amortized O(log n).
