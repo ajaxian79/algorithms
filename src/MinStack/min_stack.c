b506e66625af3d48dab47df14922ffda8b179701
ajaxian79 <blaketullysmith@gmail.com>
2023-09-27 23:07:42 -0700

    MinStack: comment

--- src/MinStack/min_stack.c
+++ src/MinStack/min_stack.c
@@ -136,3 +136,4 @@ int min_stack_size(const MinStack* s) {
 // note 75: Idempotent — calling twice with the same input is a no-op the second time.
 // note 76: Caller owns the returned array; free with a single `free`.
 // note 77: Constant-time comparisons; safe for short strings.
+// note 78: Interval tree: stores intervals on the median; query traverses O(log n) levels.
