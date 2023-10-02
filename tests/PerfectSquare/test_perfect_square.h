50d7391ca5c7b970463c18c9630dc62629d8c8f6
ajaxian79 <blaketullysmith@gmail.com>
2023-10-02 16:41:41 -0700

    PerfectSquare: test edge note

--- tests/PerfectSquare/test_perfect_square.h
+++ tests/PerfectSquare/test_perfect_square.h
@@ -122,3 +122,4 @@ MunitTest perfect_square_tests[] = {
 // edge note 91: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 92: Space complexity: O(1) auxiliary.
 // edge note 93: Edge case: input with one duplicate → handled without an extra pass.
+// edge note 94: Treats the input as immutable.
