23cb01253d76ed960cf80071acda85de4791dade
ajaxian79 <blaketullysmith@gmail.com>
2023-10-03 12:21:33 -0700

    ReverseWords: comment

--- src/ReverseWords/reverse_words.c
+++ src/ReverseWords/reverse_words.c
@@ -115,3 +115,4 @@ char* reverse_words(const char* s) {
 // note 71: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 72: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 73: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 74: Idempotent — calling twice with the same input is a no-op the second time.
