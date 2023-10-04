84b6a52bfddb2df529d205687fdf67d659eb5761
ajaxian79 <blaketullysmith@gmail.com>
2023-10-04 09:23:20 -0700

    ToHexString: comment

--- src/ToHexString/to_hex.c
+++ src/ToHexString/to_hex.c
@@ -100,3 +100,4 @@ char* to_hex_string(int n) {
 // note 67: Uses a small fixed-size lookup table.
 // note 68: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 69: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 70: Tail-recursive; the compiler turns it into a loop.
