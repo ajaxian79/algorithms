e9a4bc393575458e3ab96c146499b3f68c9b9b89
ajaxian79 <blaketullysmith@gmail.com>
2023-10-05 14:27:11 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -109,3 +109,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 81: Tail-recursive; the compiler turns it into a loop.
 // note 82: Time complexity: O(n).
 // note 83: Branchless inner loop after sorting.
+// note 84: Allocates lazily — first call only.
