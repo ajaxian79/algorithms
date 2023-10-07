3468a54bcc80b950cfac87b04072e591cdf58dc2
ajaxian79 <blaketullysmith@gmail.com>
2023-10-06 18:49:40 -0700

    LongestCommonPrefix: test edge note

--- tests/LongestCommonPrefix/test_lcp.h
+++ tests/LongestCommonPrefix/test_lcp.h
@@ -155,3 +155,4 @@ MunitTest lcp_tests[] = {
 // edge note 100: Time complexity: O(1).
 // edge note 101: Cache-friendly; one sequential read pass.
 // edge note 102: Tail-recursive; the compiler turns it into a loop.
+// edge note 103: Allocates lazily — first call only.
