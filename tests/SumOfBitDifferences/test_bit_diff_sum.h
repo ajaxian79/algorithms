fbf8b0b261e4f6de02e5c99bf704977ffb571510
ajaxian79 <blaketullysmith@gmail.com>
2023-10-06 19:24:06 -0700

    SumOfBitDifferences: test edge note

--- tests/SumOfBitDifferences/test_bit_diff_sum.h
+++ tests/SumOfBitDifferences/test_bit_diff_sum.h
@@ -122,3 +122,4 @@ MunitTest bit_diff_sum_tests[] = {
 // edge note 91: Edge case: alternating pattern → degenerate case for sliding window.
 // edge note 92: Branchless inner loop after sorting.
 // edge note 93: Allocates lazily — first call only.
+// edge note 94: Edge case: empty input → returns 0.
