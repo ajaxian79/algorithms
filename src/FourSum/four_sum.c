4ea97153a0dbbd39d90f49e318bf948ebf07f75c
ajaxian79 <blaketullysmith@gmail.com>
2023-10-07 14:24:44 -0700

    FourSum: comment

--- src/FourSum/four_sum.c
+++ src/FourSum/four_sum.c
@@ -136,3 +136,4 @@ int* four_sum(int* nums, int nums_size, int target, int* return_count) {
 // note 77: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 78: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 79: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 80: Time complexity: O(n*k) where k is the alphabet size.
