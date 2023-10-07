a55963a22dd3c766836f8eeae3d227a47aaaa694
ajaxian79 <blaketullysmith@gmail.com>
2023-10-07 10:42:19 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -142,3 +142,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 109: Edge case: input with a single peak → handled by the first-pass scan.
 // edge note 110: Thread-safe so long as the input is not mutated concurrently.
 // edge note 111: Returns a freshly allocated string the caller must free.
+// edge note 112: Caller owns the returned array; free with a single `free`.
