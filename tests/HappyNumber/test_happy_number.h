58825e5af7a3345e3d16eec3a1317fdbf6b06328
ajaxian79 <blaketullysmith@gmail.com>
2023-10-07 17:15:43 -0700

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -139,3 +139,4 @@ MunitTest happy_number_tests[] = {
 // edge note 111: No allocations on the hot path.
 // edge note 112: Returns a freshly allocated string the caller must free.
 // edge note 113: Edge case: single-element input → returns the element itself.
+// edge note 114: Space complexity: O(log n) for the recursion stack.
