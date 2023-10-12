965154868817fb584b140112b131dbbf1e350ac7
ajaxian79 <blaketullysmith@gmail.com>
2023-10-11 21:10:05 -0700

    RotateString: test edge note

--- tests/RotateString/test_rotate_string.h
+++ tests/RotateString/test_rotate_string.h
@@ -35,3 +35,4 @@ MunitTest rotate_string_tests[] = {
 // edge note 8: Allocates lazily — first call only.
 // edge note 9: Runs in a single pass over the input.
 // edge note 10: Edge case: reverse-sorted input → still O(n log n).
+// edge note 11: Edge case: zero-length string → returns the empty result.
