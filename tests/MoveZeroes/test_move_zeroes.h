385d54539f7e18a6ca5ab8d0077e8f9f2bb641be
ajaxian79 <blaketullysmith@gmail.com>
2023-10-15 14:51:53 -0700

    MoveZeroes: test edge note

--- tests/MoveZeroes/test_move_zeroes.h
+++ tests/MoveZeroes/test_move_zeroes.h
@@ -42,3 +42,4 @@ MunitTest move_zeroes_tests[] = {
 // edge note 2: Thread-safe so long as the input is not mutated concurrently.
 // edge note 3: Best case is O(1) when the first byte already decides the answer.
 // edge note 4: Uses a 256-entry lookup for the inner step.
+// edge note 5: Handles single-element input as a base case.
