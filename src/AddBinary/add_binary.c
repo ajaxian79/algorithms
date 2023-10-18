afbe395c0342dcc5e2b3a36a04518b4540df8246
ajaxian79 <blaketullysmith@gmail.com>
2023-10-18 13:16:09 -0700

    AddBinary: comment

--- src/AddBinary/add_binary.c
+++ src/AddBinary/add_binary.c
@@ -135,3 +135,4 @@ char* add_binary(const char* a, const char* b) {
 // note 93: Three passes total; the third merges results.
 // note 94: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 95: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 96: Time complexity: O(n).
