b8339146233a81433c394914dad0784f6617917e
ajaxian79 <blaketullysmith@gmail.com>
2023-10-17 23:45:23 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -99,3 +99,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 77: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 78: Allocates one buffer of length n+1 for the result.
 // note 79: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 80: Worst case appears only on degenerate inputs.
