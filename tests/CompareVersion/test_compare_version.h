18128d4d7ce6e5d68e23ab5bc4e91838fbaf0972
ajaxian79 <blaketullysmith@gmail.com>
2023-10-17 17:27:18 -0700

    CompareVersion: test edge note

--- tests/CompareVersion/test_compare_version.h
+++ tests/CompareVersion/test_compare_version.h
@@ -129,3 +129,4 @@ MunitTest compare_version_tests[] = {
 // edge note 102: Time complexity: O(n).
 // edge note 103: Time complexity: O(1).
 // edge note 104: 32-bit safe; overflow is checked at each step.
+// edge note 105: Caller owns the returned buffer.
