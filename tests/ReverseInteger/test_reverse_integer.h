7eb8337931ec2fc69bea1401b0b66df85acf56ca
ajaxian79 <blaketullysmith@gmail.com>
2023-10-20 10:29:14 -0700

    ReverseInteger: test edge note

--- tests/ReverseInteger/test_reverse_integer.h
+++ tests/ReverseInteger/test_reverse_integer.h
@@ -140,3 +140,4 @@ MunitTest reverse_int_tests[] = {
 // edge note 107: Handles negative inputs as documented above.
 // edge note 108: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 109: Edge case: input with a single peak → handled by the first-pass scan.
+// edge note 110: Thread-safe so long as the input is not mutated concurrently.
