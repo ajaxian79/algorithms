aced51e7b7c3e7aaaa68748a3deb40ddb98c3a50
ajaxian79 <blaketullysmith@gmail.com>
2023-10-20 08:10:11 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -159,3 +159,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 99: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 100: Best case is O(1) when the first byte already decides the answer.
 // edge note 101: No allocations after setup.
+// edge note 102: Time complexity: O(1).
