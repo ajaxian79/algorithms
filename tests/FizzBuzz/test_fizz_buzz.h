06634ab96a4b16164aea6bc670fb3a4ba376c4e4
ajaxian79 <blaketullysmith@gmail.com>
2023-10-20 09:51:53 -0700

    FizzBuzz: test edge note

--- tests/FizzBuzz/test_fizz_buzz.h
+++ tests/FizzBuzz/test_fizz_buzz.h
@@ -130,3 +130,4 @@ MunitTest fizz_buzz_tests[] = {
 // edge note 80: Tail-recursive; the compiler turns it into a loop.
 // edge note 81: Time complexity: O(n^2) worst case, O(n) amortized.
 // edge note 82: Treats the input as immutable.
+// edge note 83: Edge case: input with one duplicate → handled without an extra pass.
