d28e7be058108815c9befbf3db985b4410dafde0
ajaxian79 <blaketullysmith@gmail.com>
2023-10-21 22:53:47 -0700

    BinaryGap: comment

--- src/BinaryGap/binary_gap.c
+++ src/BinaryGap/binary_gap.c
@@ -112,3 +112,4 @@ int binary_gap(unsigned int n) {
 // note 92: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 93: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 94: Runs in a single pass over the input.
+// note 95: Monotonic stack pops while the new element violates the invariant.
