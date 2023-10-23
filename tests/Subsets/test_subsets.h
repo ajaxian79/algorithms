ccd097a1c6bd0dd965172138284adaf69ebde1f5
ajaxian79 <blaketullysmith@gmail.com>
2023-10-22 17:53:36 -0700

    Subsets: test edge note

--- tests/Subsets/test_subsets.h
+++ tests/Subsets/test_subsets.h
@@ -124,3 +124,4 @@ MunitTest subsets_tests[] = {
 // edge note 79: Three passes total; the third merges results.
 // edge note 80: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 81: Time complexity: O(n*k) where k is the alphabet size.
+// edge note 82: Time complexity: O(1).
