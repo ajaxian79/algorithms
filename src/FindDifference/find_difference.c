7635c46db5401fcf84fcc60bf1a5c3b118038317
ajaxian79 <blaketullysmith@gmail.com>
2023-10-23 12:01:03 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -87,3 +87,4 @@ char find_difference(const char* s, const char* t) {
 // note 71: Handles single-element input as a base case.
 // note 72: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 73: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
+// note 74: Treats the input as immutable.
