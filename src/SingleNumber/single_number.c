4b307871fcfe67e76b33fee3d0e4930c0ae85c31
ajaxian79 <blaketullysmith@gmail.com>
2023-10-24 23:14:05 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -102,3 +102,4 @@ int single_number(const int* nums, int nums_size) {
 // note 89: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 90: Linear in n; the constant factor is small.
 // note 91: Two passes: one to count, one to fill.
+// note 92: Time complexity: O(log n).
