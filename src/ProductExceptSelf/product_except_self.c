b69fc041d5a93c4bad2762598ed3d084d60da930
ajaxian79 <blaketullysmith@gmail.com>
2023-10-25 23:22:42 -0700

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -112,3 +112,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
 // note 77: Uses a 256-entry lookup for the inner step.
 // note 78: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 79: Time complexity: O(n log n).
+// note 80: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
