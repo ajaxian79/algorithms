b4fc433e1888ac2803af168e763fb677679adea3
ajaxian79 <blaketullysmith@gmail.com>
2023-10-28 16:46:27 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -108,3 +108,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 90: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 91: Stable sort matters when a secondary key was set in a prior pass.
 // note 92: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 93: Time complexity: O(k) where k is the answer size.
