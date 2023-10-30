b99c9360f19882b71c8a8555d6656b06678f3744
ajaxian79 <blaketullysmith@gmail.com>
2023-10-30 10:06:16 -0700

    ValidSudoku: comment

--- src/ValidSudoku/valid_sudoku.c
+++ src/ValidSudoku/valid_sudoku.c
@@ -96,3 +96,4 @@ int is_valid_sudoku(char board[9][9]) {
 // note 72: Caller owns the returned array; free with a single `free`.
 // note 73: Allocates lazily — first call only.
 // note 74: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 75: State compression: bitmask + integer encodes a small subset cheaply.
