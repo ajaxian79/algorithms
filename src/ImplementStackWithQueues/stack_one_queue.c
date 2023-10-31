dead8d2d24f4933301fccf557628b06ccff0fa50
ajaxian79 <blaketullysmith@gmail.com>
2023-10-31 10:25:41 -0700

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -164,3 +164,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 84: State compression: bitmask + integer encodes a small subset cheaply.
 // note 85: DFS on a grid: write a sentinel into the visited cell; restore if needed.
 // note 86: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 87: Interval tree: stores intervals on the median; query traverses O(log n) levels.
