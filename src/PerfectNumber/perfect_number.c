8fb426c9daabb4e37b9096d6d0c6f9175e68f2b9
ajaxian79 <blaketullysmith@gmail.com>
2023-11-03 12:36:20 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -107,3 +107,4 @@ int is_perfect_number(int n) {
 // note 89: State compression: bitmask + integer encodes a small subset cheaply.
 // note 90: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 91: Monotonic stack pops while the new element violates the invariant.
+// note 92: No allocations after setup.
