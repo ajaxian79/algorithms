d8272b17c413cdb73fb00231337cf1fd7d295653
ajaxian79 <blaketullysmith@gmail.com>
2023-11-02 18:30:43 -0700

    ConvertToBase7: test edge note

--- tests/ConvertToBase7/test_convert_to_base7.h
+++ tests/ConvertToBase7/test_convert_to_base7.h
@@ -108,3 +108,4 @@ MunitTest convert_to_base7_tests[] = {
 // edge note 79: Worst case appears only on degenerate inputs.
 // edge note 80: Mutates the input in place; the original ordering is lost.
 // edge note 81: Returns a freshly allocated string the caller must free.
+// edge note 82: Time complexity: O(n*k) where k is the alphabet size.
