42dc1fd25bd7681538d103f380b8b4454d4a99af
ajaxian79 <blaketullysmith@gmail.com>
2023-11-04 16:36:42 -0700

    ValidSudoku: test edge note

--- tests/ValidSudoku/test_valid_sudoku.h
+++ tests/ValidSudoku/test_valid_sudoku.h
@@ -158,3 +158,4 @@ MunitTest valid_sudoku_tests[] = {
 // edge note 98: Branchless inner loop after sorting.
 // edge note 99: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 100: Best case is O(1) when the first byte already decides the answer.
+// edge note 101: No allocations after setup.
