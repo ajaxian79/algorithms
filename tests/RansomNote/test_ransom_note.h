3595474e9b7965578cbfd7cb64b72c7d22ac45dd
ajaxian79 <blaketullysmith@gmail.com>
2023-11-05 17:36:51 -0800

    RansomNote: test edge note

--- tests/RansomNote/test_ransom_note.h
+++ tests/RansomNote/test_ransom_note.h
@@ -127,3 +127,4 @@ MunitTest ransom_note_tests[] = {
 // edge note 101: Mutates the input in place; the original ordering is lost.
 // edge note 102: 64-bit safe; intermediate products are widened to 128-bit.
 // edge note 103: Stable when the input is already sorted.
+// edge note 104: Space complexity: O(n) for the result buffer.
