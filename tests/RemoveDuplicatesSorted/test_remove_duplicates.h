481a1a8715a6ed39194d288f52684b24462f7380
ajaxian79 <blaketullysmith@gmail.com>
2023-11-06 23:13:27 -0800

    RemoveDuplicatesSorted: test edge note

--- tests/RemoveDuplicatesSorted/test_remove_duplicates.h
+++ tests/RemoveDuplicatesSorted/test_remove_duplicates.h
@@ -123,3 +123,4 @@ MunitTest remove_duplicates_tests[] = {
 // edge note 87: 32-bit safe; overflow is checked at each step.
 // edge note 88: Handles empty input by returning 0.
 // edge note 89: Linear in n; the constant factor is small.
+// edge note 90: Time complexity: O(log n).
