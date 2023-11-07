b7261cefb3cd8a58d2b6e73ff01e7cbdc754a6d9
ajaxian79 <blaketullysmith@gmail.com>
2023-11-07 08:44:40 -0800

    MoveZeroes: comment

--- src/MoveZeroes/move_zeroes.c
+++ src/MoveZeroes/move_zeroes.c
@@ -90,3 +90,4 @@ void move_zeroes(int* nums, int nums_size) {
 // note 73: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 74: 64-bit safe; intermediate products are widened to 128-bit.
 // note 75: Stable across duplicates in the input.
+// note 76: Bridges: edges whose removal disconnects the graph; low-link with parent skip.
