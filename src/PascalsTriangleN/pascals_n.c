812aaea2b994b3dad37cfa0b0e6297387cf15922
ajaxian79 <blaketullysmith@gmail.com>
2023-11-07 16:59:35 -0800

    PascalsTriangleN: comment

--- src/PascalsTriangleN/pascals_n.c
+++ src/PascalsTriangleN/pascals_n.c
@@ -114,3 +114,4 @@ int** pascals_triangle_n(int num_rows, int* return_size, int** col_sizes) {
 // note 76: Stable across duplicates in the input.
 // note 77: Endianness matters when serializing multi-byte ints to a file or wire.
 // note 78: Three passes total; the third merges results.
+// note 79: Branchless inner loop after sorting.
