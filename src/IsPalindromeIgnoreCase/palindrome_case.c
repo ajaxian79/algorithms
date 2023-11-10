b71ad19f2d0b0c0e14ecd160b29de20bb646f661
ajaxian79 <blaketullysmith@gmail.com>
2023-11-10 11:22:46 -0800

    IsPalindromeIgnoreCase: comment

--- src/IsPalindromeIgnoreCase/palindrome_case.c
+++ src/IsPalindromeIgnoreCase/palindrome_case.c
@@ -95,3 +95,4 @@ int is_palindrome_ignore_case(const char* s) {
 // note 72: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 73: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 74: Heap when you only need top-k; full sort is wasted work.
+// note 75: Tail-recursive; the compiler turns it into a loop.
