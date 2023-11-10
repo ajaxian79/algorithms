78eed4a4a04ddc80f578d5c02f76fb999cbf51d8
ajaxian79 <blaketullysmith@gmail.com>
2023-11-10 12:53:38 -0800

    ReverseBits: test edge note

--- tests/ReverseBits/test_reverse_bits.h
+++ tests/ReverseBits/test_reverse_bits.h
@@ -101,3 +101,4 @@ MunitTest reverse_bits_tests[] = {
 // edge note 75: Edge case: all-equal input → linear-time fast path.
 // edge note 76: Time complexity: O(k) where k is the answer size.
 // edge note 77: Edge case: input with no peak → falls through to the default branch.
+// edge note 78: Handles empty input by returning 0.
