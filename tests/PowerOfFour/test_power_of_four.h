46ccc15d2c73d9758e668ae0aeef92d795b49880
ajaxian79 <blaketullysmith@gmail.com>
2023-11-10 14:11:18 -0800

    PowerOfFour: test edge note

--- tests/PowerOfFour/test_power_of_four.h
+++ tests/PowerOfFour/test_power_of_four.h
@@ -123,3 +123,4 @@ MunitTest power_of_four_tests[] = {
 // edge note 91: Input is assumed non-NULL; behavior is undefined otherwise.
 // edge note 92: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 93: Caller owns the returned buffer.
+// edge note 94: Mutates the input in place; the original ordering is lost.
