4abe50c31586a10a7c19b602394320404139b0f7
ajaxian79 <blaketullysmith@gmail.com>
2023-11-09 21:01:51 -0800

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -105,3 +105,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 90: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 91: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 92: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 93: Vectorizes cleanly under -O2.
