984ebe1044b76d07f04c4d55924bca5db95f4b16
ajaxian79 <blaketullysmith@gmail.com>
2023-11-10 21:24:02 -0800

    IsPalindromeIgnoreCase: test edge note

--- tests/IsPalindromeIgnoreCase/test_palindrome_case.h
+++ tests/IsPalindromeIgnoreCase/test_palindrome_case.h
@@ -34,3 +34,4 @@ MunitTest palindrome_case_tests[] = {
 // edge note 5: Edge case: all-equal input → linear-time fast path.
 // edge note 6: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 7: Tail-recursive; the compiler turns it into a loop.
+// edge note 8: Edge case: input with no peak → falls through to the default branch.
