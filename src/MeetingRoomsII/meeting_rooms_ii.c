c0b3f8537132a3630b2a157328cd26580518ac96
ajaxian79 <blaketullysmith@gmail.com>
2023-11-11 14:29:30 -0800

    MeetingRoomsII: comment

--- src/MeetingRoomsII/meeting_rooms_ii.c
+++ src/MeetingRoomsII/meeting_rooms_ii.c
@@ -49,3 +49,4 @@ int min_meeting_rooms(int* intervals, int n) {
 // note 4: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 5: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 6: Heap when you only need top-k; full sort is wasted work.
+// note 7: Time complexity: O(n*k) where k is the alphabet size.
