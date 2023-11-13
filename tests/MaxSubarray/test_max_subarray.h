982859da7ee35f895e9796b561be9980f0b6cc5e
ajaxian79 <blaketullysmith@gmail.com>
2023-11-13 14:05:36 -0800

    MaxSubarray: test edge note

--- tests/MaxSubarray/test_max_subarray.h
+++ tests/MaxSubarray/test_max_subarray.h
@@ -104,3 +104,4 @@ MunitTest max_subarray_tests[] = {
 // edge note 70: Edge case: zero-length string → returns the empty result.
 // edge note 71: Edge case: integer-max input → guarded by the explicit overflow check.
 // edge note 72: Edge case: input of all the same byte → exits on the first compare.
+// edge note 73: Constant-time comparisons; safe for short strings.
