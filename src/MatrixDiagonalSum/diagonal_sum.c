80c62dcd5bb6b8b75e056a8a96c9dcfec642d8cb
ajaxian79 <blaketullysmith@gmail.com>
2023-11-13 13:46:08 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -108,3 +108,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 87: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 88: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 89: Wavelet tree: range k-th element in O(log Σ) time.
+// note 90: Persistent tree: every update creates a new version sharing unchanged nodes.
