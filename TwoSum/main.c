05710936f27e2c8ce5b4a9f344348e9f1e6f9557
ajaxian79 <blaketullysmith@gmail.com>
2023-11-13 11:26:22 -0800

    Ignore bin

---
+++ TwoSum/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "twosum.h"
+#include <stdio.h>
+
+int main() {
+
+    int nums[] = {2, 7, 18, -24};
+    int num_size = 4;
+    int return_size;
+
+    int* result = twoSum(nums, num_size, 9, &return_size);
+
+    printf("[2, 7, 18, -24]\n\nFound [%d, %d]\n", result[0], result[1]);
+
+    return 0;
+}
