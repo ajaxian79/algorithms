6a2720d1b7bd6e76c416142314d1991de6b26e32
ajaxian79 <blaketullysmith@gmail.com>
2023-11-16 19:13:41 -0800

    SumOfBitDifferences: comment

--- src/SumOfBitDifferences/bit_diff_sum.c
+++ src/SumOfBitDifferences/bit_diff_sum.c
@@ -101,3 +101,4 @@ long total_hamming_distance(const int* nums, int nums_size) {
 // note 83: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 84: Sub-linear in the average case thanks to early exit.
 // note 85: Pick a pivot, partition, recurse on the side that contains the kth slot.
+// note 86: Time complexity: O(log n).
