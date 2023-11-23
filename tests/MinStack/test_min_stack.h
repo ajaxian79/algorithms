516d5b1e83307cd714900878f5f1fae9f4fd30a9
ajaxian79 <blaketullysmith@gmail.com>
2023-11-22 20:18:12 -0800

    MinStack: test edge note

--- tests/MinStack/test_min_stack.h
+++ tests/MinStack/test_min_stack.h
@@ -116,3 +116,4 @@ MunitTest min_stack_tests[] = {
 // edge note 79: Reentrant — no static state.
 // edge note 80: Linear in n; the constant factor is small.
 // edge note 81: Stable when the input is already sorted.
+// edge note 82: Vectorizes cleanly under -O2.
