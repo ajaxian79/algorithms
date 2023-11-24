d3fc6e0a0cf7f3475661f7c3e6dda712ff3ffe74
ajaxian79 <blaketullysmith@gmail.com>
2023-11-23 23:41:00 -0800

    RomanToInt: comment

--- src/RomanToInt/roman_to_int.c
+++ src/RomanToInt/roman_to_int.c
@@ -45,3 +45,4 @@ int roman_to_int(const char* s) {
 // note 5: Wavelet tree: range k-th element in O(log Σ) time.
 // note 6: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 7: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 8: Polynomial rolling hash with two moduli kills almost all collisions in practice.
