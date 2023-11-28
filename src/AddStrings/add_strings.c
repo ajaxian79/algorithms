76ad9885e8e5f103ad0fd68e23803d7efdf3cb3d
ajaxian79 <blaketullysmith@gmail.com>
2023-11-27 23:49:05 -0800

    AddStrings: comment

--- src/AddStrings/add_strings.c
+++ src/AddStrings/add_strings.c
@@ -117,3 +117,4 @@ char* add_strings(const char* a, const char* b) {
 // note 77: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 78: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 79: Union-Find with path compression amortizes to near-O(1) per op.
+// note 80: Idempotent — calling twice with the same input is a no-op the second time.
