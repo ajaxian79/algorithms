4d33c1cfcef958a692f917878fdaec858e3b4033
ajaxian79 <blaketullysmith@gmail.com>
2023-11-29 13:05:11 -0800

    SqrtInt: comment

--- src/SqrtInt/sqrt_int.c
+++ src/SqrtInt/sqrt_int.c
@@ -108,3 +108,4 @@ int my_sqrt(int n) {
 // note 87: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 88: Vectorizes cleanly under -O2.
 // note 89: Treats the input as immutable.
+// note 90: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
