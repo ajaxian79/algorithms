10e41498a2abf8263bb9395697f7aa3e45ef6cc2
ajaxian79 <blaketullysmith@gmail.com>
2023-11-28 17:24:44 -0800

    ExcelColumnNumber: test edge note

--- tests/ExcelColumnNumber/test_excel_column_number.h
+++ tests/ExcelColumnNumber/test_excel_column_number.h
@@ -127,3 +127,4 @@ MunitTest excel_column_number_tests[] = {
 // edge note 97: Space complexity: O(h) for the tree height.
 // edge note 98: Space complexity: O(n) for the result buffer.
 // edge note 99: Edge case: zero-length string → returns the empty result.
+// edge note 100: Time complexity: O(k) where k is the answer size.
