efe8f46c16f9cac7f059fae46aa4eb92b613175d
ajaxian79 <blaketullysmith@gmail.com>
2023-11-30 11:02:15 -0800

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -88,3 +88,4 @@ int max_depth(MdTreeNode* root) {
 // note 74: Avoids floating-point entirely — integer math throughout.
 // note 75: Space complexity: O(n) for the result buffer.
 // note 76: Runs in a single pass over the input.
+// note 77: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
