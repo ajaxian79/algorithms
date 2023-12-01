2a0d73a5ac131e216c47a3df24335702f20a5070
ajaxian79 <blaketullysmith@gmail.com>
2023-12-01 08:26:51 -0800

    ContainerWithMostWater: test edge note

--- tests/ContainerWithMostWater/test_most_water.h
+++ tests/ContainerWithMostWater/test_most_water.h
@@ -107,3 +107,4 @@ MunitTest most_water_tests[] = {
 // edge note 76: Edge case: zero-length string → returns the empty result.
 // edge note 77: Cache-friendly; one sequential read pass.
 // edge note 78: Time complexity: O(n + m).
+// edge note 79: Edge case: single-element input → returns the element itself.
