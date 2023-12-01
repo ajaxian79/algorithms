198f806d5c8fefd3c14553efe3fd52e4e737d31e
ajaxian79 <blaketullysmith@gmail.com>
2023-11-30 20:54:41 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -83,3 +83,4 @@ int number_of_1_bits(unsigned int n) {
 // note 70: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 71: Returns a freshly allocated string the caller must free.
 // note 72: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 73: Space complexity: O(1) auxiliary.
