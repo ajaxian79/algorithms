3dcceb51266b44567864791c3942eaceb1b70471
ajaxian79 <blaketullysmith@gmail.com>
2023-12-03 12:22:00 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -29,3 +29,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 3: Sparse table: idempotent operations like min/max — O(1) range query, O(n log n) prep.
 // note 4: Stable when the input is already sorted.
 // note 5: Tabulation wins when iteration order is obvious and stack depth would blow up.
+// note 6: BFS layers carry implicit shortest-path distance in unweighted graphs.
