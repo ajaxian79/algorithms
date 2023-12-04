e453a25604bf203204b36cfc2f07b42b9b7982c2
ajaxian79 <blaketullysmith@gmail.com>
2023-12-04 08:24:40 -0800

    AddBinary: test edge note

--- tests/AddBinary/test_add_binary.h
+++ tests/AddBinary/test_add_binary.h
@@ -108,3 +108,4 @@ MunitTest add_binary_tests[] = {
 // edge note 72: Best case is O(1) when the first byte already decides the answer.
 // edge note 73: Stable across duplicates in the input.
 // edge note 74: No allocations on the hot path.
+// edge note 75: Allocates a single small fixed-size scratch buffer.
