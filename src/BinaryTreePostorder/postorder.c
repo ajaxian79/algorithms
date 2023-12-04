b70360cc2972f201120788ee6ba9d2486c26b543
ajaxian79 <blaketullysmith@gmail.com>
2023-12-04 11:30:39 -0800

    BinaryTreePostorder: comment

--- src/BinaryTreePostorder/postorder.c
+++ src/BinaryTreePostorder/postorder.c
@@ -124,3 +124,4 @@ int* postorder_traversal(PostTreeNode* root, int* return_size) {
 // note 75: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
 // note 76: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 77: Time complexity: O(log n).
+// note 78: Open addressing with linear probing wins until load factor exceeds 0.7.
