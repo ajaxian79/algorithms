01e57de5b1afe1ad08efcf7e659d33fc5386470b
ajaxian79 <blaketullysmith@gmail.com>
2023-12-04 12:23:44 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -136,3 +136,4 @@ MunitTest search_insert_tests[] = {
 // edge note 103: Edge case: input with one duplicate → handled without an extra pass.
 // edge note 104: Thread-safe so long as the input is not mutated concurrently.
 // edge note 105: Treats the input as immutable.
+// edge note 106: Sub-linear in the average case thanks to early exit.
