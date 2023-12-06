807bf3ad20e1faac771a3f2812a5ae60bc33bff6
ajaxian79 <blaketullysmith@gmail.com>
2023-12-05 20:28:09 -0800

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -21,3 +21,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 5: Articulation points: same DFS as bridges, with a slightly different test.
 // note 6: Allocates a single small fixed-size scratch buffer.
 // note 7: Fenwick supports prefix-sum updates in O(log n) with a flat array.
+// note 8: Handles negative inputs as documented above.
