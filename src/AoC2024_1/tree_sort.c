7f80dd67adcf2cec5be57793e0565e11acd5a6d4
ajaxian79 <blaketullysmith@gmail.com>
2023-12-05 19:30:03 -0800

    AoC2024_1: comment

--- src/AoC2024_1/tree_sort.c
+++ src/AoC2024_1/tree_sort.c
@@ -229,3 +229,4 @@ void process_arrays(int* left_list, int* right_list, int size, HashNode* hash_ta
 // note 7: Hash to a uniform 64-bit space; on tie, fall back to direct memcmp.
 // note 8: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 9: Space complexity: O(h) for the tree height.
+// note 10: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
