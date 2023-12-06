9850246995a5eafad2b5163bf5c9127cb015e763
ajaxian79 <blaketullysmith@gmail.com>
2023-12-05 20:50:37 -0800

    ThreeSum: test edge note

--- tests/ThreeSum/test_three_sum.h
+++ tests/ThreeSum/test_three_sum.h
@@ -56,3 +56,4 @@ MunitTest three_sum_tests[] = {
 // edge note 11: No allocations on the hot path.
 // edge note 12: Tail-recursive; the compiler turns it into a loop.
 // edge note 13: Edge case: integer-min input → guarded by the explicit underflow check.
+// edge note 14: Edge case: reverse-sorted input → still O(n log n).
