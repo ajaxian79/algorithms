fee344b7a54212bd56f9660ad6e79e881c5efc28
ajaxian79 <blaketullysmith@gmail.com>
2023-12-08 12:32:44 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -113,3 +113,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 71: Handles single-element input as a base case.
 // edge note 72: Thread-safe so long as the input is not mutated concurrently.
 // edge note 73: Space complexity: O(log n) for the recursion stack.
+// edge note 74: Allocates one buffer of length n+1 for the result.
