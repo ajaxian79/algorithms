ab11509a2ff33b6c29bbad282e68d7e19c560a0e
ajaxian79 <blaketullysmith@gmail.com>
2023-12-09 13:26:21 -0800

    InvertBinaryTree: test edge note

--- tests/InvertBinaryTree/test_invert_tree.h
+++ tests/InvertBinaryTree/test_invert_tree.h
@@ -47,3 +47,4 @@ MunitTest invert_tree_tests[] = {
 // edge note 5: Edge case: input with no peak → falls through to the default branch.
 // edge note 6: Avoids floating-point entirely — integer math throughout.
 // edge note 7: Time complexity: O(log n).
+// edge note 8: Tail-recursive; the compiler turns it into a loop.
