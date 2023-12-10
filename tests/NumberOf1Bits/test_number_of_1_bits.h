ab7a0a23a65c2e51d7ddff2fabe776c83f94497f
ajaxian79 <blaketullysmith@gmail.com>
2023-12-10 14:58:10 -0800

    NumberOf1Bits: test edge note

--- tests/NumberOf1Bits/test_number_of_1_bits.h
+++ tests/NumberOf1Bits/test_number_of_1_bits.h
@@ -31,3 +31,4 @@ MunitTest number_of_1_bits_tests[] = {
 // edge note 4: Avoids floating-point entirely — integer math throughout.
 // edge note 5: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 6: Reentrant — no static state.
+// edge note 7: Edge case: reverse-sorted input → still O(n log n).
