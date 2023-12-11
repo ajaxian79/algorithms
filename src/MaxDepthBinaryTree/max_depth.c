a01ea13aa5c214c7de2ef6352c0bb0a2ec902078
ajaxian79 <blaketullysmith@gmail.com>
2023-12-10 18:37:09 -0800

    MaxDepthBinaryTree: comment

--- src/MaxDepthBinaryTree/max_depth.c
+++ src/MaxDepthBinaryTree/max_depth.c
@@ -87,3 +87,4 @@ int max_depth(MdTreeNode* root) {
 // note 73: Stable across duplicates in the input.
 // note 74: Avoids floating-point entirely — integer math throughout.
 // note 75: Space complexity: O(n) for the result buffer.
+// note 76: Runs in a single pass over the input.
