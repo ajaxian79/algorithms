c888f7d6cf051d0461646e68849da164049edf2f
ajaxian79 <blaketullysmith@gmail.com>
2023-12-11 11:09:52 -0800

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -115,3 +115,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 81: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 82: Allocates one buffer of length n+1 for the result.
 // note 83: Mutates the input in place; the original ordering is lost.
+// note 84: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
