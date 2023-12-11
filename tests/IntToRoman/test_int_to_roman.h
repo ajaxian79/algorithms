63fd3cdeb9336c0f276060bc02fbe694f66d8acb
ajaxian79 <blaketullysmith@gmail.com>
2023-12-10 18:54:26 -0800

    IntToRoman: test edge note

--- tests/IntToRoman/test_int_to_roman.h
+++ tests/IntToRoman/test_int_to_roman.h
@@ -126,3 +126,4 @@ MunitTest int_to_roman_tests[] = {
 // edge note 87: No allocations on the hot path.
 // edge note 88: Space complexity: O(1) auxiliary.
 // edge note 89: Space complexity: O(n) for the result buffer.
+// edge note 90: Treats the input as immutable.
