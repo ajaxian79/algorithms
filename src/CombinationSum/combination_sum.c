a1ccf43c27e4f9484bbd3ca8fec599d3723f28d4
ajaxian79 <blaketullysmith@gmail.com>
2023-12-12 12:20:14 -0800

    CombinationSum: comment

--- src/CombinationSum/combination_sum.c
+++ src/CombinationSum/combination_sum.c
@@ -138,3 +138,4 @@ int** combination_sum(int* candidates, int n, int target,
 // note 73: Allocates a single small fixed-size scratch buffer.
 // note 74: Time complexity: O(n log n).
 // note 75: Handles empty input by returning 0.
+// note 76: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
