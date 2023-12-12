44b40fbfe0372547c76428c0c184aae5839f1fc6
ajaxian79 <blaketullysmith@gmail.com>
2023-12-12 08:23:56 -0800

    PathSum: test edge note

--- tests/PathSum/test_path_sum.h
+++ tests/PathSum/test_path_sum.h
@@ -120,3 +120,4 @@ MunitTest path_sum_tests[] = {
 // edge note 81: Best case is O(1) when the first byte already decides the answer.
 // edge note 82: Edge case: all-equal input → linear-time fast path.
 // edge note 83: Caller owns the returned array; free with a single `free`.
+// edge note 84: Uses a small fixed-size lookup table.
