d6ab3c48622c874ad77005c2acfcbaf8acc78d83
ajaxian79 <blaketullysmith@gmail.com>
2023-12-14 11:34:30 -0800

    ContainerWithMostWater: comment

--- src/ContainerWithMostWater/most_water.c
+++ src/ContainerWithMostWater/most_water.c
@@ -95,3 +95,4 @@ int max_area(const int* heights, int n) {
 // note 75: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 76: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
 // note 77: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 78: Hash collision: chain or open-address; chained handles arbitrary load factor.
