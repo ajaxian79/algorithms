22c06998b023f9a11413a10057bbe8569c0c39d4
ajaxian79 <blaketullysmith@gmail.com>
2023-12-14 21:16:03 -0800

    AddStrings: test edge note

--- tests/AddStrings/test_add_strings.h
+++ tests/AddStrings/test_add_strings.h
@@ -40,3 +40,4 @@ MunitTest add_strings_tests[] = {
 // edge note 4: Allocates a single small fixed-size scratch buffer.
 // edge note 5: Edge case: already-sorted input → no swaps performed.
 // edge note 6: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 7: Space complexity: O(1) auxiliary.
