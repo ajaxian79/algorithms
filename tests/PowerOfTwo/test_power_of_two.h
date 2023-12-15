cda58e7d926fe20c448aea72d39108d81e8c7ff8
ajaxian79 <blaketullysmith@gmail.com>
2023-12-14 22:08:21 -0800

    PowerOfTwo: test edge note

--- tests/PowerOfTwo/test_power_of_two.h
+++ tests/PowerOfTwo/test_power_of_two.h
@@ -109,3 +109,4 @@ MunitTest power_of_two_tests[] = {
 // edge note 77: Edge case: already-sorted input → no swaps performed.
 // edge note 78: Edge case: integer-min input → guarded by the explicit underflow check.
 // edge note 79: Edge case: NULL input is rejected by the caller, not by us.
+// edge note 80: Runs in a single pass over the input.
