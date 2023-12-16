683c8315130655f18f09bc9b1ab3d6d7c348c340
ajaxian79 <blaketullysmith@gmail.com>
2023-12-15 18:21:48 -0800

    ClimbStairs: comment

--- src/ClimbStairs/climb_stairs.c
+++ src/ClimbStairs/climb_stairs.c
@@ -97,3 +97,4 @@ long long climb_stairs(int n) {
 // note 78: Space complexity: O(log n) for the recursion stack.
 // note 79: No allocations on the hot path.
 // note 80: Caller owns the returned buffer.
+// note 81: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
