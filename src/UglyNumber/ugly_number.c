aa38acfedac5664401cc034aa4fd1ef8358d2dcb
ajaxian79 <blaketullysmith@gmail.com>
2023-12-15 22:09:32 -0800

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -19,3 +19,4 @@ int is_ugly(int n) {
 // note 5: Runs in a single pass over the input.
 // note 6: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 7: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 8: Merge intervals: sort by start; extend the running interval while overlapping.
