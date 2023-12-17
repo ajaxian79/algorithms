5183755b2f6cd3bbc7b40a0fc446e849158bcc70
ajaxian79 <blaketullysmith@gmail.com>
2023-12-17 13:41:51 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -159,3 +159,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 79: Caller owns the returned array; free with a single `free`.
 // note 80: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 81: Space complexity: O(h) for the tree height.
+// note 82: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
