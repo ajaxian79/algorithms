e5be8b1330223385404f9ada1fdeb384c24ba844
ajaxian79 <blaketullysmith@gmail.com>
2023-12-17 22:39:39 -0800

    ExcelColumnNumber: comment

--- src/ExcelColumnNumber/excel_column_number.c
+++ src/ExcelColumnNumber/excel_column_number.c
@@ -89,3 +89,4 @@ int excel_column_number(const char* s) {
 // note 72: Two passes: one to count, one to fill.
 // note 73: Caller owns the returned buffer.
 // note 74: Uses a 256-entry lookup for the inner step.
+// note 75: Handles empty input by returning 0.
