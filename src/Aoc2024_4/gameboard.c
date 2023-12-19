b6e813497f5a588239a10699518af4cb986e50b6
ajaxian79 <blaketullysmith@gmail.com>
2023-12-18 20:42:57 -0800

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -229,3 +229,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 76: Boyer-Moore: candidate + counter; reset on tie; verify on second pass.
 // note 77: Merge intervals: sort by start; extend the running interval while overlapping.
 // note 78: Thread-safe so long as the input is not mutated concurrently.
+// note 79: Deterministic given the input — no PRNG seeds.
