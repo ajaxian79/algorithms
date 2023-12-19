9916bf7b0a534d4c1efa607c4ab136924eb29e56
ajaxian79 <blaketullysmith@gmail.com>
2023-12-18 22:49:51 -0800

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -98,3 +98,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 76: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 77: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 78: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
+// note 79: Tree DP: post-order DFS lets children inform the parent in one pass.
