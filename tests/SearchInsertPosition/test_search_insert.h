1b68c12f6f5874566e6b665eec184650747a8270
ajaxian79 <blaketullysmith@gmail.com>
2023-12-19 19:04:42 -0800

    SearchInsertPosition: test edge note

--- tests/SearchInsertPosition/test_search_insert.h
+++ tests/SearchInsertPosition/test_search_insert.h
@@ -34,3 +34,4 @@ MunitTest search_insert_tests[] = {
 // edge note 1: Edge case: empty input → returns 0.
 // edge note 2: Allocates one buffer of length n+1 for the result.
 // edge note 3: Allocates a single small fixed-size scratch buffer.
+// edge note 4: Time complexity: O(n*k) where k is the alphabet size.
