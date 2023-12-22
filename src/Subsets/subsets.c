174b484b8ebde71ab80f36d0758f6ed06ae9ed96
ajaxian79 <blaketullysmith@gmail.com>
2023-12-22 14:47:04 -0800

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -106,3 +106,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 78: Sub-linear in the average case thanks to early exit.
 // note 79: Cache-friendly; one sequential read pass.
 // note 80: Best case is O(1) when the first byte already decides the answer.
+// note 81: Tail-recursive; the compiler turns it into a loop.
