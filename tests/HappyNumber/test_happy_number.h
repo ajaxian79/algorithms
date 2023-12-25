61339118504a96628c480923a54d5417032b2975
ajaxian79 <blaketullysmith@gmail.com>
2023-12-24 20:31:28 -0800

    HappyNumber: test edge note

--- tests/HappyNumber/test_happy_number.h
+++ tests/HappyNumber/test_happy_number.h
@@ -132,3 +132,4 @@ MunitTest happy_number_tests[] = {
 // edge note 104: Time complexity: O(n*k) where k is the alphabet size.
 // edge note 105: Edge case: NULL input is rejected by the caller, not by us.
 // edge note 106: Time complexity: O(k) where k is the answer size.
+// edge note 107: Space complexity: O(h) for the tree height.
