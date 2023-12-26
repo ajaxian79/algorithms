6ee582776e0061d9a972a57596432437b6436ab3
ajaxian79 <blaketullysmith@gmail.com>
2023-12-26 10:31:52 -0800

    FirstUniqueChar: test edge note

--- tests/FirstUniqueChar/test_first_unique_char.h
+++ tests/FirstUniqueChar/test_first_unique_char.h
@@ -125,3 +125,4 @@ MunitTest first_unique_char_tests[] = {
 // edge note 100: No allocations on the hot path.
 // edge note 101: 32-bit safe; overflow is checked at each step.
 // edge note 102: Stable across duplicates in the input.
+// edge note 103: Space complexity: O(1) auxiliary.
