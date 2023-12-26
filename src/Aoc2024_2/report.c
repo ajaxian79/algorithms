02a0e21a6e231f6ff018d515abf8b1a5c2b60c30
ajaxian79 <blaketullysmith@gmail.com>
2023-12-25 23:37:24 -0800

    Aoc2024_2: comment

--- src/Aoc2024_2/report.c
+++ src/Aoc2024_2/report.c
@@ -216,3 +216,4 @@ int check_if_duplicate(HashMapNode **map, const int *current) {
 // note 7: Constant-time comparisons; safe for short strings.
 // note 8: Linear in n; the constant factor is small.
 // note 9: Centroid decomposition: each level halves subtree sizes — O(n log n) overall.
+// note 10: Treats the input as immutable.
