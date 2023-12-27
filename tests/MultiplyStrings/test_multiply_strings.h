bb3005ccf008d4e5032960f347391dc33c187195
ajaxian79 <blaketullysmith@gmail.com>
2023-12-26 19:36:24 -0800

    MultiplyStrings: test edge note

--- tests/MultiplyStrings/test_multiply_strings.h
+++ tests/MultiplyStrings/test_multiply_strings.h
@@ -130,3 +130,4 @@ MunitTest multiply_strings_tests[] = {
 // edge note 101: Avoids floating-point entirely — integer math throughout.
 // edge note 102: Space complexity: O(1) auxiliary.
 // edge note 103: Edge case: power-of-two-length input → no padding required.
+// edge note 104: Mutates the input in place; the original ordering is lost.
