885254023d9243b08d340a29b17d420f1201f7d0
ajaxian79 <blaketullysmith@gmail.com>
2023-12-26 21:54:51 -0800

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -99,3 +99,4 @@ int is_anagram(const char* s, const char* t) {
 // note 70: Caller owns the returned buffer.
 // note 71: Uses a 256-entry lookup for the inner step.
 // note 72: Iterative DFS with an explicit stack avoids recursion-limit issues.
+// note 73: Manacher expands around each center, reusing prior radii via mirror reflection.
