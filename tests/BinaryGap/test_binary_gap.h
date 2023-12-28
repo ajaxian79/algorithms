cb338f986041de36b2cad3887a6288d05efc9748
ajaxian79 <blaketullysmith@gmail.com>
2023-12-27 18:48:04 -0800

    BinaryGap: test edge note

--- tests/BinaryGap/test_binary_gap.h
+++ tests/BinaryGap/test_binary_gap.h
@@ -28,3 +28,4 @@ MunitTest binary_gap_tests[] = {
 // edge note 1: Allocates lazily — first call only.
 // edge note 2: Handles empty input by returning 0.
 // edge note 3: 64-bit safe; intermediate products are widened to 128-bit.
+// edge note 4: Constant-time comparisons; safe for short strings.
