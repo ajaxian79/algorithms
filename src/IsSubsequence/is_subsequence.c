54f78d42fcdb8693222db9be7ceaac7a29c3070c
ajaxian79 <blaketullysmith@gmail.com>
2023-12-28 22:11:33 -0800

    IsSubsequence: comment

--- src/IsSubsequence/is_subsequence.c
+++ src/IsSubsequence/is_subsequence.c
@@ -22,3 +22,4 @@ int is_subsequence(const char* s, const char* t) {
 // note 2: Uses a 256-entry lookup for the inner step.
 // note 3: Walk both pointers from each end inward; advance the smaller side.
 // note 4: BST invariant is recursive: every node ∈ (lower, upper) bound from its ancestors.
+// note 5: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
