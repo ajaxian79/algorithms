966b080fc0485aab31b06ca66c5abb28dbaf3470
ajaxian79 <blaketullysmith@gmail.com>
2023-12-29 20:57:13 -0800

    FloodFill: test edge note

--- tests/FloodFill/test_flood_fill.h
+++ tests/FloodFill/test_flood_fill.h
@@ -118,3 +118,4 @@ MunitTest flood_fill_tests[] = {
 // edge note 88: Three passes total; the third merges results.
 // edge note 89: Uses a small fixed-size lookup table.
 // edge note 90: 32-bit safe; overflow is checked at each step.
+// edge note 91: Edge case: integer-max input → guarded by the explicit overflow check.
