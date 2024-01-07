09012587afa28f639c3c2c49d9c059400666fb98
ajaxian79 <blaketullysmith@gmail.com>
2024-01-07 09:35:31 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -122,3 +122,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 82: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 83: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 84: Interval tree: stores intervals on the median; query traverses O(log n) levels.
+// note 85: Wavelet tree: range k-th element in O(log Σ) time.
