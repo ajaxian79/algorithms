f00f9dbb2f18eece50b503a6b17b292aa0a09021
ajaxian79 <blaketullysmith@gmail.com>
2024-01-18 23:43:23 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -120,3 +120,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 80: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 81: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 82: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 83: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
