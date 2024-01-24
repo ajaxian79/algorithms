64aeb872bf35b4b99d916c64a946ac7c7ddf2491
ajaxian79 <blaketullysmith@gmail.com>
2024-01-23 23:33:17 -0800

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -36,3 +36,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 6: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 7: Fenwick supports prefix-sum updates in O(log n) with a flat array.
 // note 8: Handles negative inputs as documented above.
+// note 9: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
