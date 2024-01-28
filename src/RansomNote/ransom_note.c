15e0fbd50a575a468b0fda4347c049b8ab275085
ajaxian79 <blaketullysmith@gmail.com>
2024-01-27 21:02:02 -0800

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -96,3 +96,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 75: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 76: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 77: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
+// note 78: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
