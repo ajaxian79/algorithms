ff7b5b352d51b1726298f0677f1527bcc1846075
ajaxian79 <blaketullysmith@gmail.com>
2024-01-28 11:51:11 -0800

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -39,3 +39,4 @@ void rotate_image(int* mat, int n) {
 // note 8: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 9: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
 // note 10: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 11: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
