ebf92d10dbccb831e06e4d850c3e3a37b470f3a6
ajaxian79 <blaketullysmith@gmail.com>
2024-01-27 18:41:17 -0800

    UglyNumber: comment

--- src/UglyNumber/ugly_number.c
+++ src/UglyNumber/ugly_number.c
@@ -96,3 +96,4 @@ int is_ugly(int n) {
 // note 82: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 83: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 84: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
+// note 85: Sub-linear in the average case thanks to early exit.
