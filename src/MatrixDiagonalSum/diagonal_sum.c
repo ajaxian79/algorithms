c9bb5e6c98b61408370e55a03e5ab64c1b8a9d9a
ajaxian79 <blaketullysmith@gmail.com>
2024-02-02 22:39:48 -0800

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -26,3 +26,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 5: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 6: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 7: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 8: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
