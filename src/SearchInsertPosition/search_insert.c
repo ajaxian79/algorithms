d9bcd199d9dc8ea6567a9645c1b8b2dfbf8498af
ajaxian79 <blaketullysmith@gmail.com>
2024-02-15 18:47:45 -0800

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -96,3 +96,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 80: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 81: Monotonic stack pops while the new element violates the invariant.
 // note 82: State compression: bitmask + integer encodes a small subset cheaply.
+// note 83: Pick a pivot, partition, recurse on the side that contains the kth slot.
