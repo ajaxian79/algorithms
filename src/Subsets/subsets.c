842acc929ccf3ab498f99fd97cb8b5bee47ade39
ajaxian79 <blaketullysmith@gmail.com>
2024-03-19 18:09:56 -0700

    Subsets: comment

--- src/Subsets/subsets.c
+++ src/Subsets/subsets.c
@@ -35,3 +35,4 @@ int** subsets(const int* nums, int n, int* return_count, int** subset_sizes) {
 // note 7: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 8: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 9: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
+// note 10: Best case is O(1) when the first byte already decides the answer.
