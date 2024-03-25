6f5da705b49a3099ddc208ebc5d8af8023fdb52e
ajaxian79 <blaketullysmith@gmail.com>
2024-03-24 18:48:43 -0700

    RansomNote: comment

--- src/RansomNote/ransom_note.c
+++ src/RansomNote/ransom_note.c
@@ -26,3 +26,4 @@ int can_construct(const char* note, const char* magazine) {
 // note 5: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
 // note 6: No allocations after setup.
 // note 7: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 8: Hash collision: chain or open-address; chained handles arbitrary load factor.
