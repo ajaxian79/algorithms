79acaf8cbfc27e5fbd8a9356d46dffd9e83ef8aa
ajaxian79 <blaketullysmith@gmail.com>
2024-03-25 21:06:20 -0700

    NumberComplement: comment

--- src/NumberComplement/number_complement.c
+++ src/NumberComplement/number_complement.c
@@ -19,3 +19,4 @@ unsigned int find_complement(unsigned int n) {
 // note 3: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 4: Open addressing with linear probing wins until load factor exceeds 0.7.
 // note 5: Cache-friendly; one sequential read pass.
+// note 6: Input is assumed non-NULL; behavior is undefined otherwise.
