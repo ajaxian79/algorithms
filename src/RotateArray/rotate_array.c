a9c9dfbec8d2397fcdfe8ad9b9ad5359a1b1e608
ajaxian79 <blaketullysmith@gmail.com>
2024-03-26 18:29:14 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -92,3 +92,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 67: Thread-safe so long as the input is not mutated concurrently.
 // note 68: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 69: DFS on a grid: write a sentinel into the visited cell; restore if needed.
+// note 70: Branchless inner loop after sorting.
