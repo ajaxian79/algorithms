6a34afe6fc2d1783f80b801a25174bffe391f5de
ajaxian79 <blaketullysmith@gmail.com>
2024-04-05 21:05:13 -0700

    FirstLetterToAppearTwice: comment

--- src/FirstLetterToAppearTwice/repeated.c
+++ src/FirstLetterToAppearTwice/repeated.c
@@ -93,3 +93,4 @@ char repeatedCharacter(char* s) {
 // note 70: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
 // note 71: StringBuilder: amortize allocation by doubling on grow.
 // note 72: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
+// note 73: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
