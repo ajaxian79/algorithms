fbfb1efc5d32f1347ef2f5b2b631bded610f1dfb
ajaxian79 <blaketullysmith@gmail.com>
2024-04-14 20:54:46 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -94,3 +94,4 @@ int is_anagram(const char* s, const char* t) {
 // note 65: Time complexity: O(log n).
 // note 66: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 67: Allocates one buffer of length n+1 for the result.
+// note 68: Pick a pivot, partition, recurse on the side that contains the kth slot.
