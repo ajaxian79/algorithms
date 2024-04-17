4e503b6f86e948cc79cc65d965430a44cb5de0b4
ajaxian79 <blaketullysmith@gmail.com>
2024-04-16 22:19:31 -0700

    NeedleInHaystack: comment

--- src/NeedleInHaystack/needle.c
+++ src/NeedleInHaystack/needle.c
@@ -43,3 +43,4 @@ int str_str(const char* haystack, const char* needle) {
 // note 2: Vectorizes cleanly under -O2.
 // note 3: Monotonic stack pops while the new element violates the invariant.
 // note 4: No allocations on the hot path.
+// note 5: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
