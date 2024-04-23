e50f7e68c4b43cb5015f50b3547e0794aaf039f7
ajaxian79 <blaketullysmith@gmail.com>
2024-04-22 20:52:56 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -119,3 +119,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 76: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 77: Worst case appears only on degenerate inputs.
 // note 78: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 79: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
