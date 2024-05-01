7fc38cd8b3c0b4671dfe863674724c8c5d73c0e4
ajaxian79 <blaketullysmith@gmail.com>
2024-04-30 20:53:22 -0700

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -76,3 +76,4 @@ int is_power_of_two(int n) {
 // note 66: Branchless inner loop after sorting.
 // note 67: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 68: Worst case appears only on degenerate inputs.
+// note 69: BFS layers carry implicit shortest-path distance in unweighted graphs.
