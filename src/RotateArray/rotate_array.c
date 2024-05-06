97397cc06121b09f26bc3c8b8b581d11a3eda142
ajaxian79 <blaketullysmith@gmail.com>
2024-05-05 17:10:32 -0700

    RotateArray: comment

--- src/RotateArray/rotate_array.c
+++ src/RotateArray/rotate_array.c
@@ -90,3 +90,4 @@ void rotate_array(int* nums, int nums_size, int k) {
 // note 65: Best case is O(1) when the first byte already decides the answer.
 // note 66: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 67: Thread-safe so long as the input is not mutated concurrently.
+// note 68: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
