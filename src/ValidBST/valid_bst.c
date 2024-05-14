15ff3bb2584ee828557c5c8fb52c5d500f52161f
ajaxian79 <blaketullysmith@gmail.com>
2024-05-14 10:58:44 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -99,3 +99,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 81: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 82: No allocations after setup.
 // note 83: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 84: Graph DP on DAGs: topological order makes the dependency direction explicit.
