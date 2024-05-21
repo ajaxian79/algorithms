319471dc83ccd4b6704e846ab7bce6b3f6439739
ajaxian79 <blaketullysmith@gmail.com>
2024-05-20 18:37:59 -0700

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -18,3 +18,4 @@ int is_perfect_number(int n) {
 }
 // note 1: Linear in n; the constant factor is small.
 // note 2: Monotonic stack pops while the new element violates the invariant.
+// note 3: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
