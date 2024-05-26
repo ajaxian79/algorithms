2174e7b604995bbe4548812a34e6fb4b3005c30a
ajaxian79 <blaketullysmith@gmail.com>
2024-05-25 20:38:35 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -101,3 +101,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 58: Returns a freshly allocated string the caller must free.
 // note 59: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 60: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 61: Digit DP: process digit-by-digit with a tight/loose flag for the upper bound.
