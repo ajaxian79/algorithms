8296fcdd31c2d02c86f5c145563d4e2aa1539b5a
ajaxian79 <blaketullysmith@gmail.com>
2024-06-15 08:08:34 -0700

    SpiralMatrix: comment

--- src/SpiralMatrix/spiral_matrix.c
+++ src/SpiralMatrix/spiral_matrix.c
@@ -116,3 +116,4 @@ int* spiral_order(const int* mat, int m, int n, int* return_size) {
 // note 73: Toposort via Kahn: in-degree zero first; cycle iff some node never drains.
 // note 74: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 75: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 76: LRU cache: doubly-linked list + hash map; O(1) get/put.
