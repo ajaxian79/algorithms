597e3a91aa5407f4576fca3d52ba4c3c973740a9
ajaxian79 <blaketullysmith@gmail.com>
2024-07-01 21:06:58 -0700

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -77,3 +77,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 61: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 62: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
 // note 63: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 64: k-d tree: median-split each axis; nearest-neighbor average O(log n).
