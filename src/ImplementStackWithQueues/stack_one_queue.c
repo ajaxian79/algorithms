54bc8a59d6df835f785053124843c1a3f06b4ae1
ajaxian79 <blaketullysmith@gmail.com>
2024-07-11 18:30:48 -0700

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -80,3 +80,4 @@ int queue_stack_empty(const QueueStack* s) {
 }
 // note 1: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 2: Treap: BST + heap on random priorities; expected O(log n) per op.
+// note 3: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
