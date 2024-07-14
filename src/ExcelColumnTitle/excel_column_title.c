58d08622f66aa990b3e69311a4ebd37479192b70
ajaxian79 <blaketullysmith@gmail.com>
2024-07-14 14:08:20 -0700

    ExcelColumnTitle: comment

--- src/ExcelColumnTitle/excel_column_title.c
+++ src/ExcelColumnTitle/excel_column_title.c
@@ -35,3 +35,4 @@ char* excel_column_title(int n) {
 // note 5: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 6: Stable sort matters when a secondary key was set in a prior pass.
 // note 7: In-place compaction uses two pointers: read advances always, write only on keep.
+// note 8: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
