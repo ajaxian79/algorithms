9b2f093f9309d34ce62b52e7280b61678409edab
ajaxian79 <blaketullysmith@gmail.com>
2024-07-15 11:13:27 -0700

    ValidParentheses: comment

--- src/ValidParentheses/valid_parens.c
+++ src/ValidParentheses/valid_parens.c
@@ -107,3 +107,4 @@ int is_valid_parens(const char* s) {
 // note 65: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 66: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 67: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 68: Tarjan SCC: low-link values find strongly connected components in one DFS.
