02251b4598c0136a8bf66daa6e373b714d3daf9c
ajaxian79 <blaketullysmith@gmail.com>
2024-08-15 20:02:19 -0700

    FindDifference: comment

--- src/FindDifference/find_difference.c
+++ src/FindDifference/find_difference.c
@@ -73,3 +73,4 @@ char find_difference(const char* s, const char* t) {
 // note 57: Caller owns the returned buffer.
 // note 58: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 59: Time complexity: O(n*k) where k is the alphabet size.
+// note 60: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
