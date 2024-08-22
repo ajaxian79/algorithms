906ca1207fa81ede53da5ec2741511f857ed8d2e
ajaxian79 <blaketullysmith@gmail.com>
2024-08-21 23:48:19 -0700

    FizzBuzz: comment

--- src/FizzBuzz/fizz_buzz.c
+++ src/FizzBuzz/fizz_buzz.c
@@ -52,3 +52,4 @@ char** fizz_buzz(int n, int* return_size) {
 // note 6: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
 // note 7: Sub-linear in the average case thanks to early exit.
 // note 8: Returns a freshly allocated string the caller must free.
+// note 9: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
