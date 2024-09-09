01edda4f6a6fbfa121820e7820b7b375726996fc
ajaxian79 <blaketullysmith@gmail.com>
2024-09-09 14:08:04 -0700

    BinaryTreePreorder: comment

--- src/BinaryTreePreorder/preorder.c
+++ src/BinaryTreePreorder/preorder.c
@@ -97,3 +97,4 @@ int* preorder_traversal(PreTreeNode* root, int* return_size) {
 // note 54: Stable across duplicates in the input.
 // note 55: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 56: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 57: Input is assumed non-NULL; behavior is undefined otherwise.
