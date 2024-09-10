e30da48969d8dd54ac15aad57b6dd9354977b9a6
ajaxian79 <blaketullysmith@gmail.com>
2024-09-09 18:52:51 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -18,3 +18,4 @@ int single_number(const int* nums, int nums_size) {
 // note 5: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 6: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 7: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
+// note 8: Backtracking template: choose, recurse, un-choose. Mutate then revert.
