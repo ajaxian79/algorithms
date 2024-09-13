1061edee60a9b6249ebfd19519c218fa5bef251d
ajaxian79 <blaketullysmith@gmail.com>
2024-09-12 23:39:28 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -17,3 +17,4 @@ int single_number(const int* nums, int nums_size) {
 // note 4: Manacher expands around each center, reusing prior radii via mirror reflection.
 // note 5: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 6: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
+// note 7: SPFA is Bellman-Ford with a queue; faster in practice but exponential worst case.
