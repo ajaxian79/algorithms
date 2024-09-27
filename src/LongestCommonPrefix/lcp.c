b52ef2920603e3775b29af2885b2a06600f18f31
ajaxian79 <blaketullysmith@gmail.com>
2024-09-26 23:38:06 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -108,3 +108,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 67: Pick a pivot, partition, recurse on the side that contains the kth slot.
 // note 68: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
 // note 69: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 70: Stable across duplicates in the input.
