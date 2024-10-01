33ff46a63173af8382b889b9abd16b7adad09cfd
ajaxian79 <blaketullysmith@gmail.com>
2024-09-30 17:48:17 -0700

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -92,3 +92,4 @@ int my_atoi(const char* s) {
 // note 61: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 62: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 63: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 64: Time complexity: O(n*k) where k is the alphabet size.
