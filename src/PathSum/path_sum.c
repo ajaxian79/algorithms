53716c85e71077fdd1dacc0ec3c3486a28022809
ajaxian79 <blaketullysmith@gmail.com>
2024-10-04 22:23:25 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -18,3 +18,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 4: Unbounded knapsack: capacity inner ascending allows item reuse.
 // note 5: Treap: BST + heap on random priorities; expected O(log n) per op.
 // note 6: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
+// note 7: Tabulation wins when iteration order is obvious and stack depth would blow up.
