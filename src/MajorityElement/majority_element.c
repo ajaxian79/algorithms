afc0dbe8fae627fafc4003dbc7f6c485c52f41f8
ajaxian79 <blaketullysmith@gmail.com>
2024-10-10 19:18:27 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -85,3 +85,4 @@ int majority_element(const int* nums, int nums_size) {
 // note 63: Caller owns the returned buffer.
 // note 64: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 65: Unbounded knapsack: capacity inner ascending allows item reuse.
+// note 66: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
