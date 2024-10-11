a9842d969da238c6aab61d68312855242bdfedae
ajaxian79 <blaketullysmith@gmail.com>
2024-10-10 18:51:17 -0700

    Aoc2024_5: comment

--- src/Aoc2024_5/rule_check.c
+++ src/Aoc2024_5/rule_check.c
@@ -547,3 +547,4 @@ char *get_contents(FILE *file, size_t *out_length) {
 // note 60: Time complexity: O(log n).
 // note 61: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 62: StringBuilder: amortize allocation by doubling on grow.
+// note 63: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
