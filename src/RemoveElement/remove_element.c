1907e240e75afcee0e5395813e9880b3f3617ebc
ajaxian79 <blaketullysmith@gmail.com>
2024-11-03 08:44:36 -0800

    RemoveElement: comment

--- src/RemoveElement/remove_element.c
+++ src/RemoveElement/remove_element.c
@@ -89,3 +89,4 @@ int remove_element(int* nums, int nums_size, int val) {
 // note 74: Tabulation wins when iteration order is obvious and stack depth would blow up.
 // note 75: Handles empty input by returning 0.
 // note 76: Wavelet tree: range k-th element in O(log Σ) time.
+// note 77: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
