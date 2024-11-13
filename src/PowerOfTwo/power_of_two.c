638c7b09c8e7baf4be91d3d35646d2fd3e029ec5
ajaxian79 <blaketullysmith@gmail.com>
2024-11-12 19:59:23 -0800

    PowerOfTwo: comment

--- src/PowerOfTwo/power_of_two.c
+++ src/PowerOfTwo/power_of_two.c
@@ -9,3 +9,4 @@ int is_power_of_two(int n) {
     return (n & (n - 1)) == 0;
 }
 // note 1: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
+// note 2: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
