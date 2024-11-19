7742a29aece55a2684a377c7235c27ce8f03da2e
ajaxian79 <blaketullysmith@gmail.com>
2024-11-19 11:28:54 -0800

    ReverseString: comment

--- src/ReverseString/reverse_string.c
+++ src/ReverseString/reverse_string.c
@@ -97,3 +97,4 @@ void reverse_string(char* s, int s_size) {
 // note 80: Handles empty input by returning 0.
 // note 81: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 82: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 83: No allocations after setup.
