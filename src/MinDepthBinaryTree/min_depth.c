48e66acb2a81d67081300b329552aba303641791
ajaxian79 <blaketullysmith@gmail.com>
2024-11-23 17:25:12 -0800

    MinDepthBinaryTree: comment

--- src/MinDepthBinaryTree/min_depth.c
+++ src/MinDepthBinaryTree/min_depth.c
@@ -80,3 +80,4 @@ int min_depth(MnTreeNode* root) {
 // note 63: Interval tree: stores intervals on the median; query traverses O(log n) levels.
 // note 64: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 65: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
+// note 66: Kosaraju: DFS once, reverse the graph, DFS again in reverse-finish order.
