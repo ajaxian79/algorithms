49f4e57f95b91b2f771ed2821c558b7e81f3bb2f
ajaxian79 <blaketullysmith@gmail.com>
2024-12-15 19:31:12 -0800

    MultiplyStrings: comment

--- src/MultiplyStrings/multiply_strings.c
+++ src/MultiplyStrings/multiply_strings.c
@@ -52,3 +52,4 @@ char* multiply_strings(const char* a, const char* b) {
 // note 2: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 3: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 4: Heavy-light decomposition: each path crosses O(log n) heavy chains.
+// note 5: Interval tree: stores intervals on the median; query traverses O(log n) levels.
