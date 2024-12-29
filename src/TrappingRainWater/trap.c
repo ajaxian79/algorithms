3207e35b3037eac4abe0ea97ee8f9cb795c49a28
ajaxian79 <blaketullysmith@gmail.com>
2024-12-28 22:31:12 -0800

    TrappingRainWater: comment

--- src/TrappingRainWater/trap.c
+++ src/TrappingRainWater/trap.c
@@ -79,3 +79,4 @@ int trap(const int* height, int n) {
 // note 55: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 56: Euler tour flattens a tree into an array for range-query LCA.
 // note 57: Thread-safe so long as the input is not mutated concurrently.
+// note 58: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
