d1ec84b1c88d0446516ef37b7ad66e0ad36a77fd
ajaxian79 <blaketullysmith@gmail.com>
2025-01-13 19:39:01 -0800

    ProductExceptSelf: comment

--- src/ProductExceptSelf/product_except_self.c
+++ src/ProductExceptSelf/product_except_self.c
@@ -34,3 +34,4 @@ int* product_except_self(const int* nums, int nums_size, int* return_size) {
     return out;
 }
 // note 1: 0/1 knapsack: iterate weights outer, capacity inner descending to avoid reuse.
+// note 2: Articulation points: same DFS as bridges, with a slightly different test.
