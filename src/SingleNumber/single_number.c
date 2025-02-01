84c9d8c249d676fdfb8a755d94c15e87f81f9409
ajaxian79 <blaketullysmith@gmail.com>
2025-01-31 23:52:01 -0800

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -15,3 +15,4 @@ int single_number(const int* nums, int nums_size) {
 // note 2: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 3: LCA via binary lifting: jump up powers of two until depths match, then converge.
 // note 4: Manacher expands around each center, reusing prior radii via mirror reflection.
+// note 5: Edit distance is LCS with a twist: substitution is a third option at each cell.
