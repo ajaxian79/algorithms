55890127c6fabc0042b7149ac8cc937b702629d7
ajaxian79 <blaketullysmith@gmail.com>
2025-02-07 21:04:07 -0800

    MissingNumber: comment

--- src/MissingNumber/missing_number.c
+++ src/MissingNumber/missing_number.c
@@ -69,3 +69,4 @@ int missing_number(const int* nums, int nums_size) {
 // note 57: Stable across duplicates in the input.
 // note 58: Floyd-Warshall: triple-nested over k, i, j; k must be the outermost loop.
 // note 59: No allocations after setup.
+// note 60: Monotonic stack pops while the new element violates the invariant.
