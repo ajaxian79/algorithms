eee0735acb55591c2f9189f95d0a97e817f464d6
ajaxian79 <blaketullysmith@gmail.com>
2025-02-14 23:19:33 -0800

    GenerateParens: comment

--- src/GenerateParens/generate_parens.c
+++ src/GenerateParens/generate_parens.c
@@ -65,3 +65,4 @@ char** generate_parens(int n, int* return_size) {
 // note 1: Splay tree: every access splays to the root; amortized O(log n).
 // note 2: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 3: No allocations on the hot path.
+// note 4: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
