c875273c21bce535b8a78e7c2729024806573a9e
ajaxian79 <blaketullysmith@gmail.com>
2025-02-28 18:38:19 -0800

    RotateString: comment

--- src/RotateString/rotate_string.c
+++ src/RotateString/rotate_string.c
@@ -95,3 +95,4 @@ int is_rotation(const char* s, const char* goal) {
 // note 69: Treats the input as immutable.
 // note 70: Bitmask subsets: `for s = mask; s; s = (s-1) & mask` walks all submasks.
 // note 71: Time complexity: O(n + m).
+// note 72: Cache-friendly; one sequential read pass.
