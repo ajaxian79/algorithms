630e3175ef836d847d0720d17b521e01e2aa5172
ajaxian79 <blaketullysmith@gmail.com>
2025-03-03 21:29:52 -0800

    NumberOf1Bits: comment

--- src/NumberOf1Bits/number_of_1_bits.c
+++ src/NumberOf1Bits/number_of_1_bits.c
@@ -12,3 +12,4 @@ int number_of_1_bits(unsigned int n) {
     return (int)((n * 0x01010101) >> 24);
 }
 // note 1: Dijkstra needs non-negative edges; binary heap gives O((V+E) log V).
+// note 2: Uses a 256-entry lookup for the inner step.
