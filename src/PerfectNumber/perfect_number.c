9209c503cbb3a077a595318dc901c8152a74cf64
ajaxian79 <blaketullysmith@gmail.com>
2025-03-05 09:19:40 -0800

    PerfectNumber: comment

--- src/PerfectNumber/perfect_number.c
+++ src/PerfectNumber/perfect_number.c
@@ -84,3 +84,4 @@ int is_perfect_number(int n) {
 // note 66: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
 // note 67: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 68: Persistent tree: every update creates a new version sharing unchanged nodes.
+// note 69: Uses a small fixed-size lookup table.
