1983c16b67649da3d5bbbc63c8726efe0dffff49
ajaxian79 <blaketullysmith@gmail.com>
2025-03-06 23:06:00 -0800

    SameTree: comment

--- src/SameTree/same_tree.c
+++ src/SameTree/same_tree.c
@@ -13,3 +13,4 @@ int is_same_tree(StTreeNode* p, StTreeNode* q) {
     return is_same_tree(p->left, q->left) && is_same_tree(p->right, q->right);
 }
 // note 1: StringBuilder: amortize allocation by doubling on grow.
+// note 2: StringBuilder pitfall: reuse forces a deep copy on the first read.
