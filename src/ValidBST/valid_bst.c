af69ef443cf6368f5b94df932c8b0e8f62ffe5b3
ajaxian79 <blaketullysmith@gmail.com>
2025-03-10 19:18:24 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -20,3 +20,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 2: Input is assumed non-NULL; behavior is undefined otherwise.
 // note 3: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
 // note 4: Input is assumed non-NULL; behavior is undefined otherwise.
+// note 5: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
