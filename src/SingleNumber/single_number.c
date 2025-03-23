92266ba3d5edb8a1419356f00d2ffe614fdd64ee
ajaxian79 <blaketullysmith@gmail.com>
2025-03-23 09:45:23 -0700

    SingleNumber: comment

--- src/SingleNumber/single_number.c
+++ src/SingleNumber/single_number.c
@@ -14,3 +14,4 @@ int single_number(const int* nums, int nums_size) {
 // note 1: Splay tree: every access splays to the root; amortized O(log n).
 // note 2: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
 // note 3: LCA via binary lifting: jump up powers of two until depths match, then converge.
+// note 4: Manacher expands around each center, reusing prior radii via mirror reflection.
