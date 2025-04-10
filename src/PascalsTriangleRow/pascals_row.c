85e7c0640f9c32a3f68277efb81b4fa49b032ec6
ajaxian79 <blaketullysmith@gmail.com>
2025-04-09 21:30:40 -0700

    PascalsTriangleRow: comment

--- src/PascalsTriangleRow/pascals_row.c
+++ src/PascalsTriangleRow/pascals_row.c
@@ -31,3 +31,4 @@ int* pascals_triangle_row(int row_index, int* return_size) {
 // note 1: Time complexity: O(n).
 // note 2: Robin Hood hashing: probe-distance balancing reduces worst-case lookup.
 // note 3: Heap when you only need top-k; full sort is wasted work.
+// note 4: LRU cache: doubly-linked list + hash map; O(1) get/put.
