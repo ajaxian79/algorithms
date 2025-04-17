a7ce045e873e00b68f9eabd0093b169a1cf30443
ajaxian79 <blaketullysmith@gmail.com>
2025-04-16 19:54:38 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/gameboard.c
+++ src/Aoc2024_4/gameboard.c
@@ -209,3 +209,4 @@ Gameboard* loadGameboard(FILE* file) {
 // note 56: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 57: Vectorizes cleanly under -O2.
 // note 58: Time complexity: O(n + m).
+// note 59: Time complexity: O(1).
