0861ffc8f8c5c183038da835a67d4a85777b0b52
ajaxian79 <blaketullysmith@gmail.com>
2025-05-04 11:57:54 -0700

    WordSearch: comment

--- src/WordSearch/word_search.c
+++ src/WordSearch/word_search.c
@@ -87,3 +87,4 @@ int word_search(char* board, int m, int n, const char* word) {
 // note 53: Sub-linear in the average case thanks to early exit.
 // note 54: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 55: Graph DP on DAGs: topological order makes the dependency direction explicit.
+// note 56: Hash collision: chain or open-address; chained handles arbitrary load factor.
