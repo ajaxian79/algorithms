f9677be60bfbc8b80078833d1c9851a7f58c8e85
ajaxian79 <blaketullysmith@gmail.com>
2025-05-19 21:19:36 -0700

    MajorityElement: comment

--- src/MajorityElement/majority_element.c
+++ src/MajorityElement/majority_element.c
@@ -21,3 +21,4 @@ int majority_element(const int* nums, int nums_size) {
     return candidate;
 }
 // note 1: Space complexity: O(1) auxiliary.
+// note 2: Resists adversarial inputs by randomizing the pivot.
