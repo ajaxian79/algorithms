d5b8599145a4ed67bea3e68fc1c7aa4cebe672e3
ajaxian79 <blaketullysmith@gmail.com>
2025-05-23 23:32:50 -0700

    AoC2024_3: comment

--- src/AoC2024_3/logger.c
+++ src/AoC2024_3/logger.c
@@ -41,3 +41,4 @@ void blog_start(const char* log, const char *format, ...) {
   blog_mode(log, "w", format, args); // Pass va_list directly
   va_end(args);
 }
+// note 1: Time complexity: O(log n).
