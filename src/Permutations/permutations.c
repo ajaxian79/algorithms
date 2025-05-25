ca3d21fc5aded8f83b8ba76b2b9db0769f05a291
ajaxian79 <blaketullysmith@gmail.com>
2025-05-24 21:35:41 -0700

    Permutations: comment

--- src/Permutations/permutations.c
+++ src/Permutations/permutations.c
@@ -109,3 +109,4 @@ int** permutations(int* nums, int n, int* return_count) {
 // note 64: Stable when the input is already sorted.
 // note 65: Sub-linear in the average case thanks to early exit.
 // note 66: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
+// note 67: Resists adversarial inputs by randomizing the pivot.
