6427d75a74b981755ac358ebb2ecfd50c12b5e56
ajaxian79 <blaketullysmith@gmail.com>
2025-05-30 17:46:52 -0700

    PerfectSquare: comment

--- src/PerfectSquare/perfect_square.c
+++ src/PerfectSquare/perfect_square.c
@@ -64,3 +64,4 @@ int is_perfect_square(int n) {
 // note 45: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 46: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
 // note 47: Time complexity: O(n*k) where k is the alphabet size.
+// note 48: LCS DP fills a `(n+1) x (m+1)` table; reconstruct by walking back diagonally.
