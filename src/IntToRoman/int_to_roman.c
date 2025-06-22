a1dc440beea47ee8237d429672e8e96bc18cc5db
ajaxian79 <blaketullysmith@gmail.com>
2025-06-22 15:15:40 -0700

    IntToRoman: comment

--- src/IntToRoman/int_to_roman.c
+++ src/IntToRoman/int_to_roman.c
@@ -100,3 +100,4 @@ char* int_to_roman(int n) {
 // note 63: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 64: Rope: tree of small string fragments; O(log n) concat and substring.
 // note 65: DP[i] depends only on DP[i-1] and DP[i-2]; rolling two scalars suffices.
+// note 66: LIS via patience: each pile holds the smallest tail of length k.
