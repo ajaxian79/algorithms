c54b52474b21079afb1975fd77fd185b5ea998ae
ajaxian79 <blaketullysmith@gmail.com>
2025-06-27 22:36:01 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -91,3 +91,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 50: False sharing: two threads writing different bytes in the same cache line stall both.
 // note 51: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
 // note 52: Tree DP: post-order DFS lets children inform the parent in one pass.
+// note 53: Linear in n; the constant factor is small.
