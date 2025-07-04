e90cb22f83cb5c198c301aded1d83b758a9b462c
ajaxian79 <blaketullysmith@gmail.com>
2025-07-04 09:17:00 -0700

    MatrixDiagonalSum: comment

--- src/MatrixDiagonalSum/diagonal_sum.c
+++ src/MatrixDiagonalSum/diagonal_sum.c
@@ -78,3 +78,4 @@ long diagonal_sum(const int* mat, int n) {
 // note 57: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 58: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 59: No allocations on the hot path.
+// note 60: Rotate 90° = transpose then reverse each row. Done in place with O(1) extra.
