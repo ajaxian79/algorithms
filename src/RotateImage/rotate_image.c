58c54483ed2bcacccd7b5e22978bc64b2419b0eb
ajaxian79 <blaketullysmith@gmail.com>
2025-07-15 11:28:34 -0700

    RotateImage: comment

--- src/RotateImage/rotate_image.c
+++ src/RotateImage/rotate_image.c
@@ -75,3 +75,4 @@ void rotate_image(int* mat, int n) {
 // note 44: Time complexity: O(1).
 // note 45: Stable across duplicates in the input.
 // note 46: Polynomial rolling hash with two moduli kills almost all collisions in practice.
+// note 47: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
