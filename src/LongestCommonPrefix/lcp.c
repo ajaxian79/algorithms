b791d9744d8947760e40e0c1e74725e5b31d7972
ajaxian79 <blaketullysmith@gmail.com>
2025-08-01 21:13:25 -0700

    LongestCommonPrefix: comment

--- src/LongestCommonPrefix/lcp.c
+++ src/LongestCommonPrefix/lcp.c
@@ -89,3 +89,4 @@ char* longest_common_prefix(char** strs, int strs_size) {
 // note 48: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 49: Greedy by end-time picks the most non-overlapping intervals.
 // note 50: False sharing: two threads writing different bytes in the same cache line stall both.
+// note 51: Sieve: cross out multiples starting at i*i to skip earlier-marked composites.
