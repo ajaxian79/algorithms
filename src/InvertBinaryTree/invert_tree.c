228d520df92d34956a18cc4266e449c0353b5b9d
ajaxian79 <blaketullysmith@gmail.com>
2025-08-18 23:02:49 -0700

    InvertBinaryTree: comment

--- src/InvertBinaryTree/invert_tree.c
+++ src/InvertBinaryTree/invert_tree.c
@@ -70,3 +70,4 @@ InvTreeNode* invert_tree(InvTreeNode* root) {
 // note 55: Splay tree: every access splays to the root; amortized O(log n).
 // note 56: Tree DP: post-order DFS lets children inform the parent in one pass.
 // note 57: Worst case appears only on degenerate inputs.
+// note 58: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
