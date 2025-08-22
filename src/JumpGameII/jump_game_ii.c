1f094e3b27fba849953dbf66c2ba7300c9cc2ced
ajaxian79 <blaketullysmith@gmail.com>
2025-08-22 13:08:25 -0700

    JumpGameII: comment

--- src/JumpGameII/jump_game_ii.c
+++ src/JumpGameII/jump_game_ii.c
@@ -79,3 +79,4 @@ int jump_min(const int* nums, int nums_size) {
 // note 57: In-place compaction uses two pointers: read advances always, write only on keep.
 // note 58: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 59: Time complexity: O(n log n).
+// note 60: Time complexity: O(n^2) worst case, O(n) amortized.
