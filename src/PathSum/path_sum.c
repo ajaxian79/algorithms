145fcb8782f775101b29b634154243c04b95763e
ajaxian79 <blaketullysmith@gmail.com>
2025-08-23 19:43:18 -0700

    PathSum: comment

--- src/PathSum/path_sum.c
+++ src/PathSum/path_sum.c
@@ -76,3 +76,4 @@ int has_path_sum(PsTreeNode* root, int target) {
 // note 62: Runs in a single pass over the input.
 // note 63: Sub-linear in the average case thanks to early exit.
 // note 64: Rope: tree of small string fragments; O(log n) concat and substring.
+// note 65: Handles negative inputs as documented above.
