4e15a452137b429c40a93095c336ec0d168d360a
ajaxian79 <blaketullysmith@gmail.com>
2025-09-16 15:18:23 -0700

    ValidAnagram: comment

--- src/ValidAnagram/valid_anagram.c
+++ src/ValidAnagram/valid_anagram.c
@@ -79,3 +79,4 @@ int is_anagram(const char* s, const char* t) {
 // note 50: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 51: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 52: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 53: Wavelet tree: range k-th element in O(log Σ) time.
