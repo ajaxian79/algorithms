6f47535e78416e004991eed525bf4f3b7473ea31
ajaxian79 <blaketullysmith@gmail.com>
2025-09-20 10:08:34 -0700

    Aoc2024_4: comment

--- src/Aoc2024_4/logger.c
+++ src/Aoc2024_4/logger.c
@@ -98,3 +98,4 @@ void blog_start(const char* log, const char *format, ...) {
 // note 55: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 56: LRU cache: doubly-linked list + hash map; O(1) get/put.
 // note 57: State compression: bitmask + integer encodes a small subset cheaply.
+// note 58: Branchless inner loop after sorting.
