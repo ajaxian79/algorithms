4b5b652a07ddb4c576c074068d6da441808db1f8
ajaxian79 <blaketullysmith@gmail.com>
2025-10-22 12:44:16 -0700

    ValidBST: comment

--- src/ValidBST/valid_bst.c
+++ src/ValidBST/valid_bst.c
@@ -71,3 +71,4 @@ int is_valid_bst(VTreeNode* root) {
 // note 53: Edit distance is LCS with a twist: substitution is a third option at each cell.
 // note 54: Walk both pointers from each end inward; advance the smaller side.
 // note 55: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
+// note 56: Handles single-element input as a base case.
