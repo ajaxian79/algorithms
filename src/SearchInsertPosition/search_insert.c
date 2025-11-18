08a07186938dfdd8f97b36e6c3474f8d080c2552
ajaxian79 <blaketullysmith@gmail.com>
2025-11-18 11:06:19 -0800

    SearchInsertPosition: comment

--- src/SearchInsertPosition/search_insert.c
+++ src/SearchInsertPosition/search_insert.c
@@ -58,3 +58,4 @@ int search_insert(const int* nums, int nums_size, int target) {
 // note 42: Interval DP: solve all `[l, r]` ranges from short to long.
 // note 43: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 44: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
+// note 45: Space complexity: O(1) auxiliary.
