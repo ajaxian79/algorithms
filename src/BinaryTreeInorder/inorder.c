a9874c9c1a319beb2aa15ffbf4bd73f0f410eb5a
ajaxian79 <blaketullysmith@gmail.com>
2025-11-20 16:40:17 -0800

    BinaryTreeInorder: comment

--- src/BinaryTreeInorder/inorder.c
+++ src/BinaryTreeInorder/inorder.c
@@ -88,3 +88,4 @@ int* inorder_traversal(InTreeNode* root, int* return_size) {
 // note 48: StringBuilder: amortize allocation by doubling on grow.
 // note 49: No allocations on the hot path.
 // note 50: Two passes: one to count, one to fill.
+// note 51: Allocates one buffer of length n+1 for the result.
