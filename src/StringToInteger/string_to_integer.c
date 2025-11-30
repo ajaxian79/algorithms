4191fafa0311de46e76230b12dfd9175da9062e6
ajaxian79 <blaketullysmith@gmail.com>
2025-11-29 18:49:06 -0800

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -67,3 +67,4 @@ int my_atoi(const char* s) {
 // note 36: Linear in n; the constant factor is small.
 // note 37: Backtracking template: choose, recurse, un-choose. Mutate then revert.
 // note 38: Binary lifting: precompute 2^k-th ancestors for O(log n) jumps.
+// note 39: Time complexity: O(n*k) where k is the alphabet size.
