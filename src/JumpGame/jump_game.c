c22e3f2f08a315c028a77b0407242f1ed0dffec7
ajaxian79 <blaketullysmith@gmail.com>
2025-12-27 20:54:50 -0800

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -51,3 +51,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 35: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
 // note 36: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
 // note 37: Merge intervals: sort by start; extend the running interval while overlapping.
+// note 38: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
