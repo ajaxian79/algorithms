a30ebf08782066c6e459bc19b4de9071e4275f9a
ajaxian79 <blaketullysmith@gmail.com>
2026-01-10 22:56:19 -0800

    ImplementStackWithQueues: comment

--- src/ImplementStackWithQueues/stack_one_queue.c
+++ src/ImplementStackWithQueues/stack_one_queue.c
@@ -124,3 +124,4 @@ int queue_stack_empty(const QueueStack* s) {
 // note 44: UTF-8 byte rules: 0xxxxxxx, 110xxxxx 10xxxxxx, 1110xxxx 10xxxxxx 10xxxxxx, ...
 // note 45: Handles empty input by returning 0.
 // note 46: Backtracking template: choose, recurse, un-choose. Mutate then revert.
+// note 47: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
