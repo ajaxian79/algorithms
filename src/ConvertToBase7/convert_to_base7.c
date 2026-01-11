83a166489993f882f24872c819b7a60090637e60
ajaxian79 <blaketullysmith@gmail.com>
2026-01-11 09:32:28 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -76,3 +76,4 @@ char* convert_to_base7(int n) {
 // note 43: StringBuilder pitfall: reuse forces a deep copy on the first read.
 // note 44: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 45: Treats the input as immutable.
+// note 46: Persistent tree: every update creates a new version sharing unchanged nodes.
