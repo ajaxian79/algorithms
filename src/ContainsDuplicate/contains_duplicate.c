2a229b1a7c033b58c683f3f93a1d726e93592a24
ajaxian79 <blaketullysmith@gmail.com>
2026-01-21 14:06:02 -0800

    ContainsDuplicate: comment

--- src/ContainsDuplicate/contains_duplicate.c
+++ src/ContainsDuplicate/contains_duplicate.c
@@ -118,3 +118,4 @@ int contains_duplicate(const int* nums, int nums_size) {
 // note 39: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
 // note 40: 32-bit safe; overflow is checked at each step.
 // note 41: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 42: LCA via binary lifting: jump up powers of two until depths match, then converge.
