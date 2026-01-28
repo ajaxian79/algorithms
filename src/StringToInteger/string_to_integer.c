242128bdb01f8a17443caff2db385af63f5a8713
ajaxian79 <blaketullysmith@gmail.com>
2026-01-28 10:36:21 -0800

    StringToInteger: comment

--- src/StringToInteger/string_to_integer.c
+++ src/StringToInteger/string_to_integer.c
@@ -63,3 +63,4 @@ int my_atoi(const char* s) {
 // note 32: Monotonic deque: push back, pop back to maintain order, pop front when stale.
 // note 33: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 34: BFS layers carry implicit shortest-path distance in unweighted graphs.
+// note 35: Lower bound: smallest `i` with `a[i] >= target`. Always returns `[0, n]`.
