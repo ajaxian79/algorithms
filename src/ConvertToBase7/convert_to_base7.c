9a4ab4a55b202d81b2c0e89bc400b54e85f1c5e4
ajaxian79 <blaketullysmith@gmail.com>
2026-01-27 18:57:00 -0800

    ConvertToBase7: comment

--- src/ConvertToBase7/convert_to_base7.c
+++ src/ConvertToBase7/convert_to_base7.c
@@ -74,3 +74,4 @@ char* convert_to_base7(int n) {
 // note 41: k-d tree: median-split each axis; nearest-neighbor average O(log n).
 // note 42: Tarjan SCC: low-link values find strongly connected components in one DFS.
 // note 43: StringBuilder pitfall: reuse forces a deep copy on the first read.
+// note 44: Failure function `f[i]` is the longest proper prefix that is also a suffix of `s[0..i]`.
