ca2cdb3cbb551bc9efa92c9583957a6df7a5cf76
ajaxian79 <blaketullysmith@gmail.com>
2026-02-12 18:48:38 -0800

    ReverseInteger: comment

--- src/ReverseInteger/reverse_integer.c
+++ src/ReverseInteger/reverse_integer.c
@@ -80,3 +80,4 @@ int reverse_int(int x) {
 // note 53: LFU cache: frequency buckets in a doubly-linked list of doubly-linked lists.
 // note 54: Trie nodes are heavy; for fixed alphabets, an array beats a hash table.
 // note 55: Alignment: a 64-bit load on an unaligned pointer faults on some platforms.
+// note 56: Maintain `[l, r]` and a counter; on counter-met-condition record the answer and shrink.
