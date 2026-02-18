6207e74523290ed68ce25c42c1b77975b7363bda
ajaxian79 <blaketullysmith@gmail.com>
2026-02-17 17:15:47 -0800

    JumpGame: comment

--- src/JumpGame/jump_game.c
+++ src/JumpGame/jump_game.c
@@ -49,3 +49,4 @@ int can_jump(const int* nums, int nums_size) {
 // note 33: Polynomial rolling hash with two moduli kills almost all collisions in practice.
 // note 34: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 35: Skip list: probabilistic balancing, expected O(log n) per op, simpler than B-trees.
+// note 36: Memoization: top-down with a cache map; tabulation: bottom-up with an array.
