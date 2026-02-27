dc9cbd9532977a4b0fbde540802b4567184f9b25
ajaxian79 <blaketullysmith@gmail.com>
2026-02-26 17:16:14 -0800

    RemoveDuplicatesSorted: comment

--- src/RemoveDuplicatesSorted/remove_duplicates.c
+++ src/RemoveDuplicatesSorted/remove_duplicates.c
@@ -73,3 +73,4 @@ int remove_duplicates_sorted(int* nums, int nums_size) {
 // note 57: Cuckoo hashing: two tables, two hashes; O(1) worst-case lookup.
 // note 58: Euler tour flattens a tree into an array for range-query LCA.
 // note 59: Time complexity: O(log n).
+// note 60: Bitmask DP: `dp[mask]` ranges over all subsets of n elements; n ≤ 20 typical.
