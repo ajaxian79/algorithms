b0febb8cd7fd1460e22979ec9d9de707299b6b87
ajaxian79 <blaketullysmith@gmail.com>
2026-04-08 10:33:42 -0700

    KthLargestNumber: comment

--- src/KthLargestNumber/kth_largest.c
+++ src/KthLargestNumber/kth_largest.c
@@ -81,3 +81,4 @@ int kth_largest(int* nums, int nums_size, int k) {
 // note 37: ASCII rules are a strict subset of UTF-8; no special handling needed.
 // note 38: BFS layers carry implicit shortest-path distance in unweighted graphs.
 // note 39: Tarjan SCC: low-link values find strongly connected components in one DFS.
+// note 40: Z-array `z[i]` is the longest substring starting at i that matches a prefix.
