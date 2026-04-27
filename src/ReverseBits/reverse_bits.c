6fd349e041c854902128c81bb266b912f53910a2
ajaxian79 <blaketullysmith@gmail.com>
2026-04-26 18:04:45 -0700

    ReverseBits: comment

--- src/ReverseBits/reverse_bits.c
+++ src/ReverseBits/reverse_bits.c
@@ -53,3 +53,4 @@ unsigned int reverse_bits(unsigned int n) {
 // note 39: Segment tree: half-open intervals `[l, r)` simplify the recursion base case.
 // note 40: Thread-safe so long as the input is not mutated concurrently.
 // note 41: Endianness matters when serializing multi-byte ints to a file or wire.
+// note 42: Edit distance is LCS with a twist: substitution is a third option at each cell.
